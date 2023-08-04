bd3b75e046d6a8abde518388d78ba8f695328f44
Athira Rajeev <atrajeev@linux.vnet.ibm.com>
2023-08-04 10:30:47 +0530

    tools/perf/tests: perf all metricgroups test fails when perf_event access is restricted
    Perf all metricgroups test fails as below when perf_event access
    is restricted.
    
        ./perf test -v "perf all metricgroups test"
        Testing Memory_BW
        Error:
        Access to performance monitoring and observability operations is limited.
        Enforced MAC policy settings (SELinux) can limit access to performance
        access to performance monitoring and observability operations for processes
        without CAP_PERFMON, CAP_SYS_PTRACE or CAP_SYS_ADMIN Linux capability.
    
        test child finished with -1
        ---- end ----
        perf all metricgroups test: FAILED!
    
    Fix the testcase to skip those metric events which needs perf_event access
    explicitly. The exit code of the testcase is based on return code of
    the perf stat command ( enabled by set -e option ). Hence save the
    exit status in a variable and use that to decide success or fail for the
    testcase.
    
    Signed-off-by: Athira Rajeev <atrajeev@linux.vnet.ibm.com>

--- tools/perf/tests/shell/stat_all_metricgroups.sh
+++ tools/perf/tests/shell/stat_all_metricgroups.sh
@@ -2,11 +2,19 @@
 # perf all metricgroups test
 # SPDX-License-Identifier: GPL-2.0
 
-set -e
-
 for m in $(perf list --raw-dump metricgroups); do
   echo "Testing $m"
-  perf stat -M "$m" -a true
+  result=$(perf stat -M "$m" -a true 2>&1)
+  rc=$?
+  # Skip if there is no access to perf_events monitoring
+  # Otherwise exit based on the return code of perf comamnd.
+  if echo "$result" | grep -q "Access to performance monitoring and observability operations is limited";
+  then
+      continue
+  else
+      [ $rc -ne 0 ] && exit $rc
+  fi
+
 done
 
 exit 0
