9798962d57dc0e11efd0eb81c33596f6c7d20e7b
Zhen Lei <thunder.leizhen@huawei.com>
2023-08-05 15:21:16 +0800

    hexdump: add a new dump prefix DUMP_PREFIX_ADDRESS_LOW16
    Currently, function print_hex_dump() supports three dump prefixes:
    DUMP_PREFIX_NONE, DUMP_PREFIX_ADDRESS and DUMP_PREFIX_OFFSET. But for some
    usage scenarios, they don't work perfectly. For example, dump the content
    of one task's stack. In order to quickly identify a stack frame,
    DUMP_PREFIX_ADDRESS is preferred. But printing multiple 64-bit addresses
    is a bit unwise when the 'sp' value is already printed. It is redundant
    and unintuitive.
    
    For example:
    dump memory at sp=ffff800080883a90:
    ffff800080883a90: 80883ac0 ffff8000 3d8e936c ffffbd5b
    ffff800080883aa0: 5833f000 ffff3580 00000001 00000000
    ffff800080883ab0: 40299840 ffff3580 590dfa00 ffff3580
    ffff800080883ac0: 80883b30 ffff8000 3d938b28 ffffbd5b
    ffff800080883ad0: 40877180 ffff3580 590dfa00 ffff3580
    ffff800080883ae0: 4090f600 ffff3580 80883cb0 ffff8000
    ffff800080883af0: 00000010 00000000 00000000 00000000
    ffff800080883b00: 4090f700 ffff3580 00000001 00000000
    
    Generally, we do not dump more than 64 KB memory. It is sufficient to
    print only the lower 16 bits of the address.
    
    dump memory at sp=ffff800080883a90:
    3a90: 80883ac0 ffff8000 3d8e936c ffffbd5b
    3aa0: 5833f000 ffff3580 00000001 00000000
    3ab0: 40299840 ffff3580 590dfa00 ffff3580
    3ac0: 80883b30 ffff8000 3d938b28 ffffbd5b
    3ad0: 40877180 ffff3580 590dfa00 ffff3580
    3ae0: 4090f600 ffff3580 80883cb0 ffff8000
    3af0: 00000010 00000000 00000000 00000000
    3b00: 4090f700 ffff3580 00000001 00000000
    
    Another benefit of adding DUMP_PREFIX_ADDRESS_LOW16 is that we don't have
    to worry about %p outputting address as hashed value.
    
    Signed-off-by: Zhen Lei <thunder.leizhen@huawei.com>

--- include/linux/printk.h
+++ include/linux/printk.h
@@ -704,6 +704,7 @@ extern const struct file_operations kmsg_fops;
 enum {
 	DUMP_PREFIX_NONE,
 	DUMP_PREFIX_ADDRESS,
+	DUMP_PREFIX_ADDRESS_LOW16,
 	DUMP_PREFIX_OFFSET
 };
 extern int hex_dump_to_buffer(const void *buf, size_t len, int rowsize,
--- lib/hexdump.c
+++ lib/hexdump.c
@@ -290,6 +290,10 @@ void print_hex_dump(const char *level, const char *prefix_str, int prefix_type,
 			printk("%s%s%p: %s\n",
 			       level, prefix_str, ptr + i, linebuf);
 			break;
+		case DUMP_PREFIX_ADDRESS_LOW16:
+			printk("%s%s%04lx: %s\n", level,
+			       prefix_str, 0xffff & (unsigned long)(ptr + i), linebuf);
+			break;
 		case DUMP_PREFIX_OFFSET:
 			printk("%s%s%0*x: %s\n", level, prefix_str, width, i, linebuf);
 			break;
