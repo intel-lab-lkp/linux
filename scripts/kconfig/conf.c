42f77b0801886f215b3728a10afbd61466aa0e4c
Yoann Congal <yoann.congal@smile.fr>
2023-08-05 11:57:09 +0200

    kconfig: avoid an infinite loop in oldconfig/syncconfig
    Exit on error when asking for value that has an invalid default value
    and stdin has reached EOF. This happens in particular for hex/int
    configs without an explicit default value.
    
    Previously, this case would loop:
    * oldconfig prompts for the value but stdin has reached EOF
    * It gets the global default value : an empty string
    * This is not a valid hex/int value so it prompts again, hence the infinite loop.
    
    This case happens with a configuration like this (a hex config without a
    valid default value):
      config TEST_KCONFIG
           hex "Test KConfig"
           # default 0x0
    
    And using:
      make oldconfig < /dev/null
    
    This was discovered when working on Yocto bug[0] on a downstream
    kconfig user (U-boot)
    
    [0]: https://bugzilla.yoctoproject.org/show_bug.cgi?id=14136
    
    Signed-off-by: Yoann Congal <yoann.congal@smile.fr>

--- scripts/kconfig/conf.c
+++ scripts/kconfig/conf.c
@@ -374,8 +374,16 @@ static int conf_string(struct menu *menu)
 			line[strlen(line)-1] = 0;
 			def = line;
 		}
-		if (def && sym_set_string_value(sym, def))
+		if (def && sym_set_string_value(sym, def)) {
 			return 0;
+		} else {
+			if (feof(stdin) && !sym_string_valid(sym, sym_get_string_value(sym))) {
+				fprintf(stderr,
+					"Symbol %s has invalid default value and stdin reached EOF\n",
+					sym->name);
+				exit(1);
+			}
+		}
 	}
 }
 
