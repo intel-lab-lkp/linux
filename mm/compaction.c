d72caa3ace26d96e0dce7c68d79ff5113ba0f61c
Kemeng Shi <shikemeng@huaweicloud.com>
2023-08-05 19:07:07 +0800

    mm/compaction: remove repeat compact_blockskip_flush check in reset_isolation_suitable
    We have compact_blockskip_flush check in __reset_isolation_suitable, just
    remove repeat check before __reset_isolation_suitable in
    compact_blockskip_flush.
    
    Signed-off-by: Kemeng Shi <shikemeng@huaweicloud.com>

--- mm/compaction.c
+++ mm/compaction.c
@@ -434,9 +434,7 @@ void reset_isolation_suitable(pg_data_t *pgdat)
 		if (!populated_zone(zone))
 			continue;
 
-		/* Only flush if a full compaction finished recently */
-		if (zone->compact_blockskip_flush)
-			__reset_isolation_suitable(zone);
+		__reset_isolation_suitable(zone);
 	}
 }
 
