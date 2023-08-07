d12b5dd34cb179a718b3aeeccbf679a2fecf0b6d
Jérôme Pouiller <jerome.pouiller@silabs.com>
2023-08-07 17:05:40 +0200

    wifi: wfx: allow to send frames during ROC
    Until now, all the traffic was blocked during Remain On Channel (ROC).
    This patch allows to handle IEEE80211_TX_CTL_TX_OFFCHAN frames.
    
    These frames need to be sent on the virtual interface #2. Until now,
    this interface was only used by the device for internal purpose. But
    since API 3.9, it can be used to send data during scan operation (we
    hijack the scan process to implement ROC).
    
    Thus, we need to change a bit the way we match the frames with the
    interface.
    
    Fortunately, the frames received during the scan are marked with the
    correct interface number. So there is no change to do on this part.
    
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- drivers/net/wireless/silabs/wfx/scan.c
+++ drivers/net/wireless/silabs/wfx/scan.c
@@ -160,7 +160,7 @@ void wfx_remain_on_channel_work(struct work_struct *work)
 		dev_info(wvif->wdev->dev, "abort in-progress REQ_JOIN");
 		wfx_reset(wvif);
 	}
-	wfx_tx_lock_flush(wvif->wdev);
+	wfx_tx_flush(wvif->wdev);
 
 	reinit_completion(&wvif->scan_complete);
 	ret = wfx_hif_scan_uniq(wvif, chan, duration);
@@ -178,9 +178,9 @@ void wfx_remain_on_channel_work(struct work_struct *work)
 		dev_err(wvif->wdev->dev, "roc didn't stop\n");
 	ieee80211_remain_on_channel_expired(wvif->wdev->hw);
 end:
-	wfx_tx_unlock(wvif->wdev);
 	mutex_unlock(&wvif->scan_lock);
 	mutex_unlock(&wvif->wdev->conf_mutex);
+	wfx_bh_request_tx(wvif->wdev);
 }
 
 int wfx_remain_on_channel(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
