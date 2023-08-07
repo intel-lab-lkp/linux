308b02a374831808811ad3ad4f662c12e1167d5f
Shida Zhang <zhangshida@kylinos.cn>
2023-08-07 09:26:54 +0800

    ext4: Modify the rec_len helpers to accommodate future cases
    Following Andreas' suggestion, it is time to adapt these helpers
    to handle larger records during runtime, especially in preparation
    for the eventual support of ext4 with a block size greater than
    PAGE_SIZE.
    
    Suggested-by: Andreas Dilger <adilger@dilger.ca>
    Signed-off-by: Shida Zhang <zhangshida@kylinos.cn>
    Link: https://lore.kernel.org/lkml/A9ECDF14-95A1-4B1E-A815-4B6ABF4916C6@dilger.ca/

--- fs/ext4/ext4.h
+++ fs/ext4/ext4.h
@@ -2386,39 +2386,41 @@ static inline unsigned int ext4_dir_rec_len(__u8 name_len,
 }
 
 /*
- * If we ever get support for fs block sizes > page_size, we'll need
- * to remove the #if statements in the next two functions...
+ * The next two helpers facilitate the conversion between the encoded
+ * and plain forms of rec_len. Try to access rec_len through these helpers
+ * rather than accessing it directly.
  */
-static inline unsigned int
-ext4_rec_len_from_disk(__le16 dlen, unsigned blocksize)
+static inline
+unsigned int ext4_rec_len_from_disk(__le16 dlen, unsigned blocksize)
 {
 	unsigned len = le16_to_cpu(dlen);
 
-#if (PAGE_SIZE >= 65536)
+	if (blocksize < 65536)
+		return len;
+
 	if (len == EXT4_MAX_REC_LEN || len == 0)
 		return blocksize;
+
 	return (len & 65532) | ((len & 3) << 16);
-#else
-	return len;
-#endif
 }
 
 static inline __le16 ext4_rec_len_to_disk(unsigned len, unsigned blocksize)
 {
-	BUG_ON((len > blocksize) || (blocksize > (1 << 18)) || (len & 3));
-#if (PAGE_SIZE >= 65536)
-	if (len < 65536)
+	BUG_ON(len > blocksize);
+	BUG_ON(blocksize > (1 << 18));
+	BUG_ON(len & 3);
+
+	if (len < 65536) /* always true for blocksize < 65536 */
 		return cpu_to_le16(len);
+
 	if (len == blocksize) {
 		if (blocksize == 65536)
 			return cpu_to_le16(EXT4_MAX_REC_LEN);
-		else
-			return cpu_to_le16(0);
+
+		return cpu_to_le16(0);
 	}
+
 	return cpu_to_le16((len & 65532) | ((len >> 16) & 3));
-#else
-	return cpu_to_le16(len);
-#endif
 }
 
 /*
