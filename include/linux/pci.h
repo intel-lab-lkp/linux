74005a43bb846527a84ff88a4bfd2c5f39788be1
Xiongfeng Wang <wangxiongfeng2@huawei.com>
2023-08-07 11:18:45 +0800

    PCI: Add pci_find_next_dvsec_capability to find next designated VSEC
    Some devices may have several DVSEC(Designated Vendor-Specific Extended
    Capability) entries with the same DVSEC ID. Add
    pci_find_next_dvsec_capability() to find them all.
    
    Signed-off-by: Xiongfeng Wang <wangxiongfeng2@huawei.com>

--- include/linux/pci.h
+++ include/linux/pci.h
@@ -1168,6 +1168,8 @@ u16 pci_find_next_ext_capability(struct pci_dev *dev, u16 pos, int cap);
 struct pci_bus *pci_find_next_bus(const struct pci_bus *from);
 u16 pci_find_vsec_capability(struct pci_dev *dev, u16 vendor, int cap);
 u16 pci_find_dvsec_capability(struct pci_dev *dev, u16 vendor, u16 dvsec);
+u16 pci_find_next_dvsec_capability(struct pci_dev *dev, u16 start, u16 vendor,
+				   u16 dvsec);
 
 u64 pci_get_dsn(struct pci_dev *dev);
 
