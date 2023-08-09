893488ffb11730771cb25453a84a6099f6d81655
Zhengchao Shao <shaozhengchao@huawei.com>
2023-08-09 20:41:04 +0800

    bonding: remove warning printing in bond_create_debugfs
    Because debugfs_create_dir returns ERR_PTR, so warning printing will never
    be invoked in bond_create_debugfs, remove it. If failed to create
    directory, failure information will be printed in debugfs_create_dir.
    
    Signed-off-by: Zhengchao Shao <shaozhengchao@huawei.com>

--- drivers/net/bonding/bond_debugfs.c
+++ drivers/net/bonding/bond_debugfs.c
@@ -87,9 +87,6 @@ void bond_debug_reregister(struct bonding *bond)
 void __init bond_create_debugfs(void)
 {
 	bonding_debug_root = debugfs_create_dir("bonding", NULL);
-
-	if (!bonding_debug_root)
-		pr_warn("Warning: Cannot create bonding directory in debugfs\n");
 }
 
 void bond_destroy_debugfs(void)
