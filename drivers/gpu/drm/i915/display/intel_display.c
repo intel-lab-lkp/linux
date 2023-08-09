799b3cf78749690337254436df7270978f5489e7
Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>
2023-08-09 11:29:18 +0300

    drm/i915: Fix mistake in intel_modeset_all_pipes condition
    It is supposed to be "!intel_crtc_needs_modeset" - otherwise,
    we are active exactly vice versa for active pipes: skipping if modeset
    is needed and not skipping if not needed.
    
    Signed-off-by: Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -5439,7 +5439,7 @@ int intel_modeset_all_pipes(struct intel_atomic_state *state,
 			return PTR_ERR(crtc_state);
 
 		if (!crtc_state->hw.active ||
-		    intel_crtc_needs_modeset(crtc_state))
+		    !intel_crtc_needs_modeset(crtc_state))
 			continue;
 
 		drm_dbg_kms(&dev_priv->drm, "[CRTC:%d:%s] Full modeset due to %s\n",
