3ecadae3555574881cb2e1c030415974a6d3e8ff
Evan Quan <evan.quan@amd.com>
2023-08-10 15:38:02 +0800

    drm/amd/pm: enable Wifi RFI mitigation feature support for SMU13.0.0
    Fulfill the SMU13.0.0 support for Wifi RFI mitigation feature.
    
    Signed-off-by: Evan Quan <evan.quan@amd.com>
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>

--- drivers/gpu/drm/amd/pm/swsmu/inc/smu_v13_0.h
+++ drivers/gpu/drm/amd/pm/swsmu/inc/smu_v13_0.h
@@ -299,5 +299,8 @@ int smu_v13_0_update_pcie_parameters(struct smu_context *smu,
 				     uint32_t pcie_gen_cap,
 				     uint32_t pcie_width_cap);
 
+int smu_v13_0_enable_uclk_shadow(struct smu_context *smu,
+				 bool enablement);
+
 #endif
 #endif
