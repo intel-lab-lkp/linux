2d98a148d06eb942a21df7ab59d77cd3d96a6ea4
Ratheesh Kannoth <rkannoth@marvell.com>
2023-08-10 08:14:22 +0530

    octeontx2-pf: Set page pool size
    page pool infra does direct recycling aggressively.
    This would often keep ptr_ring left unused. Save
    memory by configuring ptr_ring to a constant value(2K).
    
    Please find discussion at
    https://lore.kernel.org/netdev/
    	15d32b22-22b0-64e3-a49e-88d780c24616@kernel.org/T/
    
    Fixes: b2e3406a38f0 ("octeontx2-pf: Add support for page pool")
    Suggested-by: Alexander Lobakin <aleksander.lobakin@intel.com>
    Signed-off-by: Ratheesh Kannoth <rkannoth@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_common.c
@@ -1432,7 +1432,8 @@ int otx2_pool_init(struct otx2_nic *pfvf, u16 pool_id,
 	}
 
 	pp_params.flags = PP_FLAG_PAGE_FRAG | PP_FLAG_DMA_MAP;
-	pp_params.pool_size = numptrs;
+#define OTX2_PAGE_POOL_SZ 2048
+	pp_params.pool_size = OTX2_PAGE_POOL_SZ;
 	pp_params.nid = NUMA_NO_NODE;
 	pp_params.dev = pfvf->dev;
 	pp_params.dma_dir = DMA_FROM_DEVICE;
