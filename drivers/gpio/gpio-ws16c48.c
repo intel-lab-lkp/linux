1e21eadea00ea4ce06d666a53d509b72f6afb768
William Breathitt Gray <william.gray@linaro.org>
2023-08-10 18:00:44 -0400

    gpio: ws16c48: Fix off-by-one error in WS16C48 resource region extent
    The WinSystems WS16C48 I/O address region spans offsets 0x0 through 0xA,
    which is a total of 11 bytes. Fix the WS16C48_EXTENT define to the
    correct value of 11 so that access to necessary device registers is
    properly requested in the ws16c48_probe() callback by the
    devm_request_region() function call.
    
    Fixes: 2c05a0f29f41 ("gpio: ws16c48: Implement and utilize register structures")
    Cc: stable@vger.kernel.org
    Cc: Paul Demetrotion <pdemetrotion@winsystems.com>
    Signed-off-by: William Breathitt Gray <william.gray@linaro.org>

--- drivers/gpio/gpio-ws16c48.c
+++ drivers/gpio/gpio-ws16c48.c
@@ -17,7 +17,7 @@
 #include <linux/regmap.h>
 #include <linux/types.h>
 
-#define WS16C48_EXTENT 10
+#define WS16C48_EXTENT 11
 #define MAX_NUM_WS16C48 max_num_isa_dev(WS16C48_EXTENT)
 
 static unsigned int base[MAX_NUM_WS16C48];
