b4ea3055eff4d46c7b5cc4177b6c31b5174a7046
Yann Sionneau <yann@sionneau.net>
2023-08-16 22:04:08 +0200

    i2c: at91-master: devm_pinctrl_get() cannot return NULL
    Remove unnecessary check against NULL for devm_pinctrl_get() return value.
    
    Signed-off-by: Yann Sionneau <yann@sionneau.net>

--- drivers/i2c/busses/i2c-at91-master.c
+++ drivers/i2c/busses/i2c-at91-master.c
@@ -831,7 +831,7 @@ static int at91_init_twi_recovery_gpio(struct platform_device *pdev,
 	struct i2c_bus_recovery_info *rinfo = &dev->rinfo;
 
 	rinfo->pinctrl = devm_pinctrl_get(&pdev->dev);
-	if (!rinfo->pinctrl || IS_ERR(rinfo->pinctrl)) {
+	if (IS_ERR(rinfo->pinctrl)) {
 		dev_info(dev->dev, "can't get pinctrl, bus recovery not supported\n");
 		return PTR_ERR(rinfo->pinctrl);
 	}
