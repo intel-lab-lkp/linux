af0483f65d1665ec61ec5d9d4d47e672998edea7
Siddh Raman Pant <code@siddh.me>
2023-08-20 17:10:40 +0530

    drm: Remove usage of deprecated DRM_NOTE in DRM core
    drm_print.h says DRM_NOTE is deprecated in favor of drm_notice().
    
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Signed-off-by: Siddh Raman Pant <code@siddh.me>

--- drivers/gpu/drm/drm_kms_helper_common.c
+++ drivers/gpu/drm/drm_kms_helper_common.c
@@ -41,7 +41,7 @@ MODULE_LICENSE("GPL and additional rights");
 /* Backward compatibility for drm_kms_helper.edid_firmware */
 static int edid_firmware_set(const char *val, const struct kernel_param *kp)
 {
-	DRM_NOTE("drm_kms_helper.edid_firmware is deprecated, please use drm.edid_firmware instead.\n");
+	drm_notice(NULL, "drm_kms_helper.edid_firmware is deprecated, please use drm.edid_firmware instead.\n");
 
 	return __drm_set_edid_firmware_path(val);
 }
