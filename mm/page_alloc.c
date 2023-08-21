320673d6ed0911b4fdafeeb867c408ec17642927
Zhiguo Jiang <justinjiang@vivo.com>
2023-08-21 17:04:49 +0800

    mm: ALLOC_HIGHATOMIC flag allocation issue
    In case of alloc_flags contain ALLOC_HIGHATOMIC and alloc order
    is order1/2/3/10 in rmqueue(), if pages are alloced successfully
    from pcplist cache, and move a free pageblock from the alloced
    migratetype freelist to MIGRATE_HIGHATOMIC freelist, rather than
    alloc from MIGRATE_HIGHATOMIC freelist firstly, so this will result
    in an increasing number of pages on the MIGRATE_HIGHATOMIC freelist,
    pages in other migratetype freelist are reduced and more likely to
    allocation failure.
    
    Currently the sequence of ALLOC_HIGHATOMIC allocation is:
    pcplist cache --> buddy (batch >> order) allocation migratetype
    freelist --> buddy MIGRATE_HIGHATOMIC freelist --> buddy allocation
    migratetype freelist.
    
    Due to the fact that requesting pages from the pcplist cache is faster
    than buddy, the sequence of modifying the ALLOC_HIGHATOMIC allocation is:
    pcplist --> buddy MIGRATE_HIGHATOMIC freelist --> buddy allocation
    migrate freelist.
    
    This patch can solve the allocation failure of the Non-ALLOC_HIGHATOMIC
    alloc_flag due to excessive pages reservations in MIGRATE_HIGHATOMIC
    freelists.
    
    Signed-off-by: Zhiguo Jiang <justinjiang@vivo.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -2722,6 +2722,8 @@ struct page *__rmqueue_pcplist(struct zone *zone, unsigned int order,
 			int batch = READ_ONCE(pcp->batch);
 			int alloced;
 
+			if (alloc_flags & ALLOC_HIGHATOMIC)
+				goto out;
 			/*
 			 * Scale batch relative to order if batch implies
 			 * free pages can be stored on the PCP. Batch can
@@ -2736,6 +2738,7 @@ struct page *__rmqueue_pcplist(struct zone *zone, unsigned int order,
 					migratetype, alloc_flags);
 
 			pcp->count += alloced << order;
+out:
 			if (unlikely(list_empty(list)))
 				return NULL;
 		}
@@ -2798,7 +2801,7 @@ __no_sanitize_memory
 static inline
 struct page *rmqueue(struct zone *preferred_zone,
 			struct zone *zone, unsigned int order,
-			gfp_t gfp_flags, unsigned int alloc_flags,
+			gfp_t gfp_flags, unsigned int *alloc_flags,
 			int migratetype)
 {
 	struct page *page;
@@ -2814,21 +2817,23 @@ struct page *rmqueue(struct zone *preferred_zone,
 		 * MIGRATE_MOVABLE pcplist could have the pages on CMA area and
 		 * we need to skip it when CMA area isn't allowed.
 		 */
-		if (!IS_ENABLED(CONFIG_CMA) || alloc_flags & ALLOC_CMA ||
+		if (!IS_ENABLED(CONFIG_CMA) || *alloc_flags & ALLOC_CMA ||
 				migratetype != MIGRATE_MOVABLE) {
 			page = rmqueue_pcplist(preferred_zone, zone, order,
-					migratetype, alloc_flags);
-			if (likely(page))
+					migratetype, *alloc_flags);
+			if (likely(page)) {
+				*alloc_flags |= ALLOC_PCPLIST;
 				goto out;
+			}
 		}
 	}
 
-	page = rmqueue_buddy(preferred_zone, zone, order, alloc_flags,
+	page = rmqueue_buddy(preferred_zone, zone, order, *alloc_flags,
 							migratetype);
 
 out:
 	/* Separate test+clear to avoid unnecessary atomics */
-	if ((alloc_flags & ALLOC_KSWAPD) &&
+	if ((*alloc_flags & ALLOC_KSWAPD) &&
 	    unlikely(test_bit(ZONE_BOOSTED_WATERMARK, &zone->flags))) {
 		clear_bit(ZONE_BOOSTED_WATERMARK, &zone->flags);
 		wakeup_kswapd(zone, 0, 0, zone_idx(zone));
@@ -3208,7 +3213,7 @@ retry:
 
 try_this_zone:
 		page = rmqueue(ac->preferred_zoneref->zone, zone, order,
-				gfp_mask, alloc_flags, ac->migratetype);
+				gfp_mask, &alloc_flags, ac->migratetype);
 		if (page) {
 			prep_new_page(page, order, gfp_mask, alloc_flags);
 
@@ -3216,7 +3221,8 @@ try_this_zone:
 			 * If this is a high-order atomic allocation then check
 			 * if the pageblock should be reserved for the future
 			 */
-			if (unlikely(alloc_flags & ALLOC_HIGHATOMIC))
+			if (unlikely(alloc_flags & ALLOC_HIGHATOMIC)
+				&& unlikely(!(alloc_flags & ALLOC_PCPLIST)))
 				reserve_highatomic_pageblock(page, zone);
 
 			return page;
