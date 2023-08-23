133b5d2265a650d3c048d00959cc6840fe2fd4e3
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2023-08-23 17:24:24 +0530

    drm/display/dp: Default 8 bpc support when DSC is supported
    As per DP v1.4, a DP DSC Sink device shall support 8bpc in DPCD 6Ah.
    Apparently some panels that do support DSC, are not setting the bit for
    8bpc.
    
    So always assume 8bpc support by DSC decoder, when DSC is claimed to be
    supported.
    
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>

--- drivers/gpu/drm/display/drm_dp_helper.c
+++ drivers/gpu/drm/display/drm_dp_helper.c
@@ -2447,14 +2447,19 @@ int drm_dp_dsc_sink_supported_input_bpcs(const u8 dsc_dpcd[DP_DSC_RECEIVER_CAP_S
 					 u8 dsc_bpc[3])
 {
 	int num_bpc = 0;
+
+	if (!dsc_dpcd[DP_DSC_SUPPORT] & DP_DSC_DECOMPRESSION_IS_SUPPORTED)
+		return 0;
+
 	u8 color_depth = dsc_dpcd[DP_DSC_DEC_COLOR_DEPTH_CAP - DP_DSC_SUPPORT];
 
 	if (color_depth & DP_DSC_12_BPC)
 		dsc_bpc[num_bpc++] = 12;
 	if (color_depth & DP_DSC_10_BPC)
 		dsc_bpc[num_bpc++] = 10;
-	if (color_depth & DP_DSC_8_BPC)
-		dsc_bpc[num_bpc++] = 8;
+
+	/* A DP DSC Sink devices shall support 8 bpc. */
+	dsc_bpc[num_bpc++] = 8;
 
 	return num_bpc;
 }
