8ef89aa9b6f6ac98c4a16b54cb6b362e812b074b
Paul M Stillwell Jr <paul.m.stillwell.jr@intel.com>
2023-08-23 15:21:49 -0700

    ice: configure FW logging
    Users want the ability to debug FW issues by retrieving the
    FW logs from the E8xx devices. Use debugfs to allow the user to
    read/write the FW log configuration by adding a 'fwlog/modules' file.
    Reading the file will show either the currently running configuration or
    the next configuration (if the user has changed the configuration).
    Writing to the file will update the configuration, but NOT enable the
    configuration (that is a separate command).
    
    To see the status of FW logging then read the 'fwlog/modules' file like
    this:
    
    cat /sys/kernel/debug/ice/0000\:18\:00.0/fwlog/modules
    
    To change the configuration of FW logging then write to the 'fwlog/modules'
    file like this:
    
    echo DCB NORMAL > /sys/kernel/debug/ice/0000\:18\:00.0/fwlog/modules
    
    The format to change the configuration is
    
    echo <fwlog_module> <fwlog_level> > /sys/kernel/debug/ice/<pci device>/fwlog/modules
    
    where
    
    * fwlog_level is a name as described below. Each level includes the
      messages from the previous/lower level
    
          *	NONE
          *	ERROR
          *	WARNING
          *	NORMAL
          *	VERBOSE
    
    * fwlog_event is a name that represents the module to receive events for.
      The module names are
    
          *	GENERAL
          *	CTRL
          *	LINK
          *	LINK_TOPO
          *	DNL
          *	I2C
          *	SDP
          *	MDIO
          *	ADMINQ
          *	HDMA
          *	LLDP
          *	DCBX
          *	DCB
          *	XLR
          *	NVM
          *	AUTH
          *	VPD
          *	IOSF
          *	PARSER
          *	SW
          *	SCHEDULER
          *	TXQ
          *	RSVD
          *	POST
          *	WATCHDOG
          *	TASK_DISPATCH
          *	MNG
          *	SYNCE
          *	HEALTH
          *	TSDRV
          *	PFREG
          *	MDLVER
          *	ALL
    
    The name ALL is special and specifies setting all of the modules to the
    specified fwlog_level.
    
    If the NVM supports FW logging then the file 'fwlog' will be created
    under the PCI device ID for the ice driver. If the file does not exist
    then either the NVM doesn't support FW logging or debugfs is not enabled
    on the system.
    
    In addition to configuring the modules, the user can also configure the
    number of log messages (resolution) to include in a single Admin Receive
    Queue (ARQ) event.The range is 1-128 (1 means push every log message, 128
    means push only when the max AQ command buffer is full). The suggested
    value is 10.
    
    To see/change the resolution the user can read/write the
    'fwlog/resolution' file. An example changing the value to 50 is
    
    echo 50 > /sys/kernel/debug/ice/0000\:18\:00.0/fwlog/resolution
    
    Signed-off-by: Paul M Stillwell Jr <paul.m.stillwell.jr@intel.com>

--- drivers/net/ethernet/intel/ice/ice_main.c
+++ drivers/net/ethernet/intel/ice/ice_main.c
@@ -4356,6 +4356,22 @@ static void ice_print_wake_reason(struct ice_pf *pf)
 }
 
 /**
+ * ice_pf_fwlog_update_module - update 1 module
+ * @pf: pointer to the PF struct
+ * @log_level: log_level to use for the @module
+ * @module: module to update
+ */
+void ice_pf_fwlog_update_module(struct ice_pf *pf, int log_level, int module)
+{
+	struct ice_fwlog_module_entry *entries;
+	struct ice_hw *hw = &pf->hw;
+
+	entries = (struct ice_fwlog_module_entry *)hw->fwlog_cfg.module_entries;
+
+	entries[module].log_level = log_level;
+}
+
+/**
  * ice_register_netdev - register netdev
  * @vsi: pointer to the VSI struct
  */
@@ -5175,6 +5191,8 @@ static void ice_remove(struct pci_dev *pdev)
 		msleep(100);
 	}
 
+	ice_debugfs_exit();
+
 	if (test_bit(ICE_FLAG_SRIOV_ENA, pf->flags)) {
 		set_bit(ICE_VF_RESETS_DISABLED, pf->state);
 		ice_free_vfs(pf);
@@ -5636,6 +5654,8 @@ static int __init ice_module_init(void)
 		goto err_dest_wq;
 	}
 
+	ice_debugfs_init();
+
 	status = pci_register_driver(&ice_driver);
 	if (status) {
 		pr_err("failed to register PCI driver, err %d\n", status);
@@ -5646,6 +5666,7 @@ static int __init ice_module_init(void)
 
 err_dest_lag_wq:
 	destroy_workqueue(ice_lag_wq);
+	ice_debugfs_exit();
 err_dest_wq:
 	destroy_workqueue(ice_wq);
 	return status;
