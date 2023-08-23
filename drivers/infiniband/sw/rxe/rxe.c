2a7e82544e3955a3be3725879da5a742f9f7d5d4
Li Zhijian <lizhijian@fujitsu.com>
2023-08-23 10:13:05 +0800

    RDMA/rxe: add missing newline to rxe_set_mtu message
    A newline help flushing message out.
    
    Signed-off-by: Li Zhijian <lizhijian@fujitsu.com>

--- drivers/infiniband/sw/rxe/rxe.c
+++ drivers/infiniband/sw/rxe/rxe.c
@@ -161,7 +161,7 @@ void rxe_set_mtu(struct rxe_dev *rxe, unsigned int ndev_mtu)
 	port->attr.active_mtu = mtu;
 	port->mtu_cap = ib_mtu_enum_to_int(mtu);
 
-	rxe_info_dev(rxe, "Set mtu to %d", port->mtu_cap);
+	rxe_info_dev(rxe, "Set mtu to %d\n", port->mtu_cap);
 }
 
 /* called by ifc layer to create new rxe device.
