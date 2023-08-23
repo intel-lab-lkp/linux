902557b7b93ad24a714a559c386f058ba1656c1e
Wang You <wangyoua@uniontech.com>
2023-08-23 18:07:05 +0800

    remove preempt in do_sched_yield
    I see do_sched_yield's irq disable cover preempt disable. In this
    function, I think preempt op may not work, or it may have some
    special effect ? Thanks.
    
    Signed-off-by: Wang You <wangyoua@uniontech.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8535,10 +8535,7 @@ static void do_sched_yield(void)
 
 	schedstat_inc(rq->yld_count);
 	current->sched_class->yield_task(rq);
-
-	preempt_disable();
 	rq_unlock_irq(rq, &rf);
-	sched_preempt_enable_no_resched();
 
 	schedule();
 }
