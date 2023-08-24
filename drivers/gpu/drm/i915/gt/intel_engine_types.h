710c6683bea01ecb807070353fbd79ac3e5d7e66
Nirmoy Das <nirmoy.das@intel.com>
2023-08-24 17:03:22 -0400

    drm/i915: Create a bind context for GGTT updates
    Create a separate blitter context if a platform requires
    GGTT updates using MI_UPDATE_GTT blitter command.
    
    Subsequent patch will introduce methods to update
    GGTT using this blitter context and MI_UPDATE_GTT blitter
    command.
    
    v2: Fix a typo in comment. (Oak)
    v3: s/blitter_context/bind_context/g (Chris)
    
    Signed-off-by: Nirmoy Das <nirmoy.das@intel.com>
    Signed-off-by: Oak Zeng <oak.zeng@intel.com>

--- drivers/gpu/drm/i915/gt/intel_engine_types.h
+++ drivers/gpu/drm/i915/gt/intel_engine_types.h
@@ -415,6 +415,9 @@ struct intel_engine_cs {
 	struct llist_head barrier_tasks;
 
 	struct intel_context *kernel_context; /* pinned */
+	struct intel_context *bind_context; /* pinned, only for BCS0 */
+	/* mark the blitter engine's availability status */
+	atomic_t bind_context_ready;
 
 	/**
 	 * pinned_contexts_list: List of pinned contexts. This list is only
