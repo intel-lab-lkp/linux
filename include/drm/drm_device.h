9026a66e00fcc407cd76615dd3d8da4ea035f97a
Dmitry Osipenko <dmitry.osipenko@collabora.com>
2023-08-27 20:54:44 +0300

    drm/shmem-helper: Add memory shrinker
    Introduce common drm-shmem shrinker for DRM drivers.
    
    To start using drm-shmem shrinker drivers should do the following:
    
    1. Implement evict() callback of GEM object where driver should check
       whether object is purgeable or evictable using drm-shmem helpers and
       perform the shrinking action
    
    2. Initialize drm-shmem internals using drmm_gem_shmem_init(drm_device),
       which will register drm-shmem shrinker
    
    3. Implement madvise IOCTL that will use drm_gem_shmem_madvise()
    
    Signed-off-by: Daniel Almeida <daniel.almeida@collabora.com>
    Signed-off-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>

--- include/drm/drm_device.h
+++ include/drm/drm_device.h
@@ -16,6 +16,7 @@ struct drm_vblank_crtc;
 struct drm_vma_offset_manager;
 struct drm_vram_mm;
 struct drm_fb_helper;
+struct drm_gem_shmem_shrinker;
 
 struct inode;
 
@@ -290,8 +291,13 @@ struct drm_device {
 	/** @vma_offset_manager: GEM information */
 	struct drm_vma_offset_manager *vma_offset_manager;
 
-	/** @vram_mm: VRAM MM memory manager */
-	struct drm_vram_mm *vram_mm;
+	union {
+		/** @vram_mm: VRAM MM memory manager */
+		struct drm_vram_mm *vram_mm;
+
+		/** @shmem_mm: SHMEM GEM memory manager */
+		struct drm_gem_shmem *shmem_mm;
+	};
 
 	/**
 	 * @switch_power_state:
