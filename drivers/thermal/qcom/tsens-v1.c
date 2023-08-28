341ff4f012ae39c40c8abd3d2a1020ae90ab3c5f
Sricharan Ramabadhran <quic_srichara@quicinc.com>
2023-08-29 00:48:23 +0530

    thermal/drivers/qcom/tsens: Add support for IPQ5018 tsens
    IPQ5018 has tsens IP V1.0, 4 sensors and 1 interrupt.
    The soc does not have a RPM, hence tsens has to be reset and
    enabled in the driver init. Adding the driver support for same.
    
    Signed-off-by: Sricharan Ramabadhran <quic_srichara@quicinc.com>

--- drivers/thermal/qcom/tsens-v1.c
+++ drivers/thermal/qcom/tsens-v1.c
@@ -42,6 +42,59 @@ static struct tsens_legacy_calibration_format tsens_qcs404_nvmem = {
 	},
 };
 
+struct tsens_legacy_calibration_format tsens_ipq5018_nvmem = {
+	.base_len = 8,
+	.base_shift = 2,
+	.sp_len = 6,
+	.mode = { 0, 8 },
+	.invalid = { 0, 2 },
+	.base = { { 0, 11 }, { 0, 19 } },
+	.sp = {
+		{ { 0, 27 },  { 1, 1 } },
+		{ { 1, 7 },  { 1, 13 } },
+		{ { 1, 19 },  { 1, 25 } },
+		{ { 1, 31 },  { 2, 5 } },
+		{ { 2, 11 },  { 3, 0 } },
+	},
+};
+
+static void fixup_ipq5018_points(int mode, u32 *p1, u32 *p2)
+{
+	if (mode == NO_PT_CALIB) {
+		p1[0] = 403;
+		p2[0] = 688;
+		p1[1] = 390;
+		p2[1] = 674;
+		p1[2] = 341;
+		p2[2] = 635;
+		p1[3] = 387;
+		p2[3] = 673;
+		p1[4] = 347;
+		p2[4] = 639;
+	}
+}
+
+static int calibrate_ipq5018(struct tsens_priv *priv)
+{
+	u32 p1[10], p2[10];
+	u32 *qfprom_cdata;
+	int mode;
+
+	qfprom_cdata = (u32 *)qfprom_read(priv->dev, "calib");
+	if (IS_ERR(qfprom_cdata))
+		return PTR_ERR(qfprom_cdata);
+
+	mode = tsens_read_calibration_legacy(priv, &tsens_ipq5018_nvmem,
+					     p1, p2,
+					     qfprom_cdata, NULL);
+
+	fixup_ipq5018_points(mode, p1, p2);
+	compute_intercept_slope(priv, p1, p2, mode);
+	kfree(qfprom_cdata);
+
+	return 0;
+}
+
 static int calibrate_v1(struct tsens_priv *priv)
 {
 	u32 p1[10], p2[10];
@@ -77,6 +130,18 @@ static struct tsens_features tsens_v1_feat = {
 	.max_sensors	= 11,
 	.trip_min_temp	= -40000,
 	.trip_max_temp	= 120000,
+};
+
+static struct tsens_features tsens_v1_ipq5018_feat = {
+	.ver_major	= VER_1_X,
+	.crit_int	= 0,
+	.combo_int	= 0,
+	.adc		= 1,
+	.srot_split	= 1,
+	.max_sensors	= 11,
+	.trip_min_temp	= -40000,
+	.trip_max_temp	= 120000,
+	.no_early_init	= 1,
 };
 
 static const struct reg_field tsens_v1_regfields[MAX_REGFIELDS] = {
@@ -150,6 +215,43 @@ static int __init init_8956(struct tsens_priv *priv) {
 	return init_common(priv);
 }
 
+static int init_ipq5018(struct tsens_priv *priv)
+{
+	int ret;
+	u32 mask;
+
+	init_common(priv);
+	if (!priv->tm_map)
+		return -ENODEV;
+
+	ret = regmap_field_write(priv->rf[TSENS_SW_RST], 1);
+	if (ret) {
+		dev_err(priv->dev, "Reset failed\n");
+		return ret;
+	}
+
+	mask = GENMASK(10, 0);
+	ret = regmap_field_update_bits(priv->rf[SENSOR_EN], mask, mask);
+	if (ret) {
+		dev_err(priv->dev, "Sensor Enable failed\n");
+		return ret;
+	}
+
+	ret = regmap_field_write(priv->rf[TSENS_EN], 1);
+	if (ret) {
+		dev_err(priv->dev, "Enable failed\n");
+		return ret;
+	}
+
+	ret = regmap_field_write(priv->rf[TSENS_SW_RST], 0);
+	if (ret) {
+		dev_err(priv->dev, "Reset failed\n");
+		return ret;
+	}
+
+	return 0;
+}
+
 static const struct tsens_ops ops_generic_v1 = {
 	.init		= init_common,
 	.calibrate	= calibrate_v1,
@@ -185,5 +287,18 @@ struct tsens_plat_data data_8976 = {
 	.num_sensors	= 11,
 	.ops		= &ops_8976,
 	.feat		= &tsens_v1_feat,
+	.fields		= tsens_v1_regfields,
+};
+
+const struct tsens_ops ops_ipq5018 = {
+	.init		= init_ipq5018,
+	.calibrate	= calibrate_ipq5018,
+	.get_temp	= get_temp_tsens_valid,
+};
+
+struct tsens_plat_data data_ipq5018 = {
+	.num_sensors	= 5,
+	.ops		= &ops_ipq5018,
+	.feat		= &tsens_v1_ipq5018_feat,
 	.fields		= tsens_v1_regfields,
 };
