9cf56ac2663eedde44ee144a204619a71b221000
Daniel Wagner <dwagner@suse.de>
2023-08-29 11:13:47 +0200

    nvmet-trace: null terminate device name string correctly
    Signed-off-by: Daniel Wagner <dwagner@suse.de>

--- drivers/nvme/target/trace.h
+++ drivers/nvme/target/trace.h
@@ -55,8 +55,8 @@ static inline void __assign_req_name(char *name, struct nvmet_req *req)
 		return;
 	}
 
-	strncpy(name, req->ns->device_path,
-		min_t(size_t, DISK_NAME_LEN, strlen(req->ns->device_path)));
+	strscpy(name, req->ns->device_path,
+		min_t(size_t, DISK_NAME_LEN, strlen(req->ns->device_path) + 1));
 }
 #endif
 
