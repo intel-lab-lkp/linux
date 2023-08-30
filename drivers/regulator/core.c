585dbcbb4a4d581c310edc47b3e8897cbc99febc
Michał Mirosław <mirq-linux@rere.qmqm.pl>
2023-08-30 23:38:56 +0200

    regulator/core: regulator_ena_gpio_ctrl: pull in ena_gpio state handling
    Deduplicate `ena_gpio_state` handling by pulling it into
    regulator_ena_gpio_ctrl().
    
    Signed-off-by: Michał Mirosław <mirq-linux@rere.qmqm.pl>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -2612,6 +2612,9 @@ static int regulator_ena_gpio_ctrl(struct regulator_dev *rdev, bool enable)
 	if (!pin)
 		return -EINVAL;
 
+	if (rdev->ena_gpio_state == enable)
+		return 0;
+
 	if (enable) {
 		/* Enable GPIO at initial use */
 		if (pin->enable_count == 0)
@@ -2619,18 +2622,14 @@ static int regulator_ena_gpio_ctrl(struct regulator_dev *rdev, bool enable)
 
 		pin->enable_count++;
 	} else {
-		if (pin->enable_count > 1) {
-			pin->enable_count--;
-			return 0;
-		}
-
 		/* Disable GPIO if not used */
-		if (pin->enable_count <= 1) {
+		if (pin->enable_count-- <= 1) {
 			gpiod_set_value_cansleep(pin->gpiod, 0);
 			pin->enable_count = 0;
 		}
 	}
 
+	rdev->ena_gpio_state = enable;
 	return 0;
 }
 
@@ -2731,12 +2730,9 @@ static int _regulator_do_enable(struct regulator_dev *rdev)
 	}
 
 	if (rdev->ena_pin) {
-		if (!rdev->ena_gpio_state) {
-			ret = regulator_ena_gpio_ctrl(rdev, true);
-			if (ret < 0)
-				return ret;
-			rdev->ena_gpio_state = 1;
-		}
+		ret = regulator_ena_gpio_ctrl(rdev, true);
+		if (ret < 0)
+			return ret;
 	} else if (rdev->desc->ops->enable) {
 		ret = rdev->desc->ops->enable(rdev);
 		if (ret < 0)
@@ -2949,13 +2945,9 @@ static int _regulator_do_disable(struct regulator_dev *rdev)
 	trace_regulator_disable(rdev_get_name(rdev));
 
 	if (rdev->ena_pin) {
-		if (rdev->ena_gpio_state) {
-			ret = regulator_ena_gpio_ctrl(rdev, false);
-			if (ret < 0)
-				return ret;
-			rdev->ena_gpio_state = 0;
-		}
-
+		ret = regulator_ena_gpio_ctrl(rdev, false);
+		if (ret < 0)
+			return ret;
 	} else if (rdev->desc->ops->disable) {
 		ret = rdev->desc->ops->disable(rdev);
 		if (ret != 0)
