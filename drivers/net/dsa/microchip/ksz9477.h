cd0b1ea1a1e55ffff701f97537aca438835fa9b6
Lukasz Majewski <lukma@denx.de>
2023-08-31 13:18:26 +0200

    net: dsa: hsr: Enable in KSZ9477 switch HW HSR offloading
    This patch adds functions for providing in KSZ9477 switch HSR
    (High-availability Seamless Redundancy) hardware offloading.
    
    According to AN3474 application note following features are provided:
    - TX packet duplication from host to switch (NETIF_F_HW_HSR_DUP)
    - RX packet duplication discarding
    - Prevention of packet loop
    
    For last two ones - there is a probability that some packets will not
    be filtered in HW (in some special cases). Hence, the HSR core code
    shall be used to discard those not caught frames.
    
    Moreover, some switch registers adjustments are required - like setting
    MAC address of HSR network interface.
    
    Signed-off-by: Lukasz Majewski <lukma@denx.de>

--- drivers/net/dsa/microchip/ksz9477.h
+++ drivers/net/dsa/microchip/ksz9477.h
@@ -58,5 +58,9 @@ int ksz9477_dsa_init(struct ksz_device *dev);
 int ksz9477_switch_init(struct ksz_device *dev);
 void ksz9477_switch_exit(struct ksz_device *dev);
 void ksz9477_port_queue_split(struct ksz_device *dev, int port);
+int ksz9477_hsr_join(struct dsa_switch *ds, int port, struct net_device *hsr,
+		     struct dsa_port *partner);
+int ksz9477_hsr_leave(struct dsa_switch *ds, int port, struct net_device *hsr,
+		      struct dsa_port *partner);
 
 #endif
