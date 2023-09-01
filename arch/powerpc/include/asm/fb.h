9bfe7a6c014e2531dd64da245a9be6a7c1ebb4fd
Thomas Zimmermann <tzimmermann@suse.de>
2023-09-01 16:16:35 +0200

    arch/powerpc: Call internal __phys_mem_access_prot() in fbdev code
    Call __phys_mem_access_prot() from the fbdev mmap helper fb_pgprotect().
    Allows us to avoid the file argument, which can then be removed from
    fB_pgprotect() entirely. No other architecture uses the parameter.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>

--- arch/powerpc/include/asm/fb.h
+++ arch/powerpc/include/asm/fb.h
@@ -9,9 +9,9 @@
 static inline void fb_pgprotect(struct file *file, struct vm_area_struct *vma,
 				unsigned long off)
 {
-	vma->vm_page_prot = phys_mem_access_prot(file, off >> PAGE_SHIFT,
-						 vma->vm_end - vma->vm_start,
-						 vma->vm_page_prot);
+	vma->vm_page_prot = __phys_mem_access_prot(PHYS_PFN(off),
+						   vma->vm_end - vma->vm_start,
+						   vma->vm_page_prot);
 }
 #define fb_pgprotect fb_pgprotect
 
