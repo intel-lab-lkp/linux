03f0d48f782523b5b3a99aec4d205b733fd3db25
Ziyang Xuan <william.xuanziyang@huawei.com>
2023-09-02 17:20:07 +0800

    team: fix null-ptr-deref when team device type is changed
    Get a null-ptr-deref bug as follows with reproducer [1].
    
    BUG: kernel NULL pointer dereference, address: 0000000000000228
    ...
    RIP: 0010:vlan_dev_hard_header+0x35/0x140 [8021q]
    ...
    Call Trace:
     <TASK>
     ? __die+0x24/0x70
     ? page_fault_oops+0x82/0x150
     ? exc_page_fault+0x69/0x150
     ? asm_exc_page_fault+0x26/0x30
     ? vlan_dev_hard_header+0x35/0x140 [8021q]
     ? vlan_dev_hard_header+0x8e/0x140 [8021q]
     neigh_connected_output+0xb2/0x100
     ip6_finish_output2+0x1cb/0x520
     ? nf_hook_slow+0x43/0xc0
     ? ip6_mtu+0x46/0x80
     ip6_finish_output+0x2a/0xb0
     mld_sendpack+0x18f/0x250
     mld_ifc_work+0x39/0x160
     process_one_work+0x1e6/0x3f0
     worker_thread+0x4d/0x2f0
     ? __pfx_worker_thread+0x10/0x10
     kthread+0xe5/0x120
     ? __pfx_kthread+0x10/0x10
     ret_from_fork+0x34/0x50
     ? __pfx_kthread+0x10/0x10
     ret_from_fork_asm+0x1b/0x30
    
    [1]
    $ teamd -t team0 -d -c '{"runner": {"name": "loadbalance"}}'
    $ ip link add name t-dummy type dummy
    $ ip link add link t-dummy name t-dummy.100 type vlan id 100
    $ ip link add name t-nlmon type nlmon
    $ ip link set t-nlmon master team0
    $ ip link set t-nlmon nomaster
    $ ip link set t-dummy up
    $ ip link set team0 up
    $ ip link set t-dummy.100 down
    $ ip link set t-dummy.100 master team0
    
    When enslave a vlan device to team device and team device type is changed
    from non-ether to ether, header_ops of team device is changed to
    vlan_header_ops. That is incorrect and will trigger null-ptr-deref
    for vlan->real_dev in vlan_dev_hard_header() because team device is not
    a vlan device.
    
    Use ether_setup() for team device when its type is changed from non-ether
    to ether to fix the bug.
    
    Fixes: 1d76efe1577b ("team: add support for non-ethernet devices")
    Signed-off-by: Ziyang Xuan <william.xuanziyang@huawei.com>

--- drivers/net/team/team.c
+++ drivers/net/team/team.c
@@ -2115,14 +2115,19 @@ static const struct ethtool_ops team_ethtool_ops = {
 static void team_setup_by_port(struct net_device *dev,
 			       struct net_device *port_dev)
 {
-	dev->header_ops	= port_dev->header_ops;
-	dev->type = port_dev->type;
-	dev->hard_header_len = port_dev->hard_header_len;
-	dev->needed_headroom = port_dev->needed_headroom;
-	dev->addr_len = port_dev->addr_len;
-	dev->mtu = port_dev->mtu;
-	memcpy(dev->broadcast, port_dev->broadcast, port_dev->addr_len);
-	eth_hw_addr_inherit(dev, port_dev);
+	if (port_dev->type == ARPHRD_ETHER) {
+		ether_setup(dev);
+		eth_hw_addr_random(dev);
+	} else {
+		dev->header_ops	= port_dev->header_ops;
+		dev->type = port_dev->type;
+		dev->hard_header_len = port_dev->hard_header_len;
+		dev->needed_headroom = port_dev->needed_headroom;
+		dev->addr_len = port_dev->addr_len;
+		dev->mtu = port_dev->mtu;
+		memcpy(dev->broadcast, port_dev->broadcast, port_dev->addr_len);
+		eth_hw_addr_inherit(dev, port_dev);
+	}
 
 	if (port_dev->flags & IFF_POINTOPOINT) {
 		dev->flags &= ~(IFF_BROADCAST | IFF_MULTICAST);
