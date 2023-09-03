5fb4036bff9de3b82ca99ed4db04d4f1097d3b4e
Dmitry Osipenko <dmitry.osipenko@collabora.com>
2023-09-03 20:07:36 +0300

    drm/panfrost: Switch to generic memory shrinker
    Replace Panfrost's custom memory shrinker with a common drm-shmem
    memory shrinker.
    
    Tested-by: Steven Price <steven.price@arm.com> # Firefly-RK3288
    Reviewed-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>

--- drivers/gpu/drm/panfrost/panfrost_gem.h
+++ drivers/gpu/drm/panfrost/panfrost_gem.h
@@ -30,12 +30,6 @@ struct panfrost_gem_object {
 		struct mutex lock;
 	} mappings;
 
-	/*
-	 * Count the number of jobs referencing this BO so we don't let the
-	 * shrinker reclaim this object prematurely.
-	 */
-	atomic_t gpu_usecount;
-
 	bool noexec		:1;
 	bool is_heap		:1;
 };
@@ -80,8 +74,5 @@ panfrost_gem_mapping_get(struct panfrost_gem_object *bo,
 			 struct panfrost_file_priv *priv);
 void panfrost_gem_mapping_put(struct panfrost_gem_mapping *mapping);
 void panfrost_gem_teardown_mappings_locked(struct panfrost_gem_object *bo);
-
-void panfrost_gem_shrinker_init(struct drm_device *dev);
-void panfrost_gem_shrinker_cleanup(struct drm_device *dev);
 
 #endif /* __PANFROST_GEM_H__ */
