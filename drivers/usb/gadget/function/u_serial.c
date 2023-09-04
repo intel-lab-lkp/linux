cbf8662885dd0cec44d478edb626ca5bacfe45e1
Jiazi.Li <jqqlijiazi@gmail.com>
2023-09-04 18:54:55 +0800

    usb: gadget: u_serial: Add null pointer check in gs_close
    If kfifo_alloc return err in gs_open, tty->driver_data will not
    be assigned a legal value.
    This will result in a NULL pointer issue when calling gs_close in
    the following error handling:
    tty_open
        ->tty_release
            ->gs_close
    Add a null pointer check in gs_close to prevent this.
    
    Signed-off-by: Jiazi.Li <jiazi.li@transsion.com>

--- drivers/usb/gadget/function/u_serial.c
+++ drivers/usb/gadget/function/u_serial.c
@@ -680,6 +680,9 @@ static void gs_close(struct tty_struct *tty, struct file *file)
 	struct gs_port *port = tty->driver_data;
 	struct gserial	*gser;
 
+	if (!port)
+		return;
+
 	spin_lock_irq(&port->port_lock);
 
 	if (port->port.count != 1) {
