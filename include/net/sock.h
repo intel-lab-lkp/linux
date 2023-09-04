ea2163200b5d9a26ec4f1cf8a36db70acad69f24
Breno Leitao <leitao@debian.org>
2023-09-04 09:24:57 -0700

    net/socket: Break down __sys_getsockopt
    Split __sys_getsockopt() into two functions by removing the core
    logic into a sub-function (do_sock_getsockopt()). This will avoid
    code duplication when doing the same operation in other callers, for
    instance.
    
    do_sock_getsockopt() will be called by io_uring getsockopt() command
    operation in the following patch.
    
    The same was done for the setsockopt pair.
    
    Suggested-by: Martin KaFai Lau <martin.lau@linux.dev>
    Signed-off-by: Breno Leitao <leitao@debian.org>

--- include/net/sock.h
+++ include/net/sock.h
@@ -1863,6 +1863,8 @@ int sock_setsockopt(struct socket *sock, int level, int op,
 		    sockptr_t optval, unsigned int optlen);
 int do_sock_setsockopt(struct socket *sock, bool compat, int level,
 		       int optname, sockptr_t optval, int optlen);
+int do_sock_getsockopt(struct socket *sock, bool compat, int level,
+		       int optname, sockptr_t optval, sockptr_t optlen);
 
 int sk_getsockopt(struct sock *sk, int level, int optname,
 		  sockptr_t optval, sockptr_t optlen);
