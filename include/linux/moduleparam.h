a8f0dc1aa67ac5aa27717eb10bac2f0f8c6b4f24
Kees Cook <keescook@chromium.org>
2023-09-05 12:13:37 -0700

    module: Clarify documentation of module_param_call()
    Commit 9bbb9e5a3310 ("param: use ops in struct kernel_param, rather than
    get and set fns directly") added the comment that module_param_call()
    was deprecated, during a large scale refactoring to bring sanity to type
    casting back then. In 2017 following more cleanups, it became useful
    against as it wraps a common pattern of creating an ops struct for a
    given get/set pair:
    
      b2f270e87473 ("module: Prepare to convert all module_param_call() prototypes")
      ece1996a21ee ("module: Do not paper over type mismatches in module_param_call()")
    
            static const struct kernel_param_ops __param_ops_##name = \
                    { .flags = 0, .set = _set, .get = _get }; \
            __module_param_call(MODULE_PARAM_PREFIX, \
                                name, &__param_ops_##name, arg, perm, -1, 0)
    
            __module_param_call(MODULE_PARAM_PREFIX, name, ops, arg, perm, -1, 0)
    
    Many users of module_param_cb() appear to be almost universally
    open-coding the same thing that module_param_call() does now. Don't
    discourage[1] people from using module_param_call() but clarifying the
    comment: module_param_cb() is useful if you repeatedly use the same pair
    of get/set functions.
    
    [1] https://lore.kernel.org/lkml/202308301546.5C789E5EC@keescook/
    
    Cc: Luis Chamberlain <mcgrof@kernel.org>
    Cc: Johan Hovold <johan@kernel.org>
    Cc: Jessica Yu <jeyu@kernel.org>
    Cc: Sagi Grimberg <sagi@grimberg.me>
    Cc: Nick Desaulniers <ndesaulniers@gooogle.com>
    Cc: Miguel Ojeda <ojeda@kernel.org>
    Cc: Joe Perches <joe@perches.com>
    Cc: Azeem Shaikh <azeemshaikh38@gmail.com>
    Cc: linux-modules@vger.kernel.org
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- include/linux/moduleparam.h
+++ include/linux/moduleparam.h
@@ -293,7 +293,7 @@ struct kparam_array
 	= { __param_str_##name, THIS_MODULE, ops,			\
 	    VERIFY_OCTAL_PERMISSIONS(perm), level, flags, { arg } }
 
-/* Obsolete - use module_param_cb() */
+/* For repeated _set & _get usage use module_param_cb() */
 #define module_param_call(name, _set, _get, arg, perm)			\
 	static const struct kernel_param_ops __param_ops_##name =	\
 		{ .flags = 0, .set = _set, .get = _get };		\
