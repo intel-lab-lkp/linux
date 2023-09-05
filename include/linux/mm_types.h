f3266f8c3a6caa9d6e0c79aec705e08e75c2c65a
Tina Zhang <tina.zhang@intel.com>
2023-09-05 08:09:28 +0800

    mm: Add structure to keep sva information
    Introduce iommu_mm_data structure to keep sva information (pasid and the
    related sva domains). Add iommu_mm pointer, pointing to an instance of
    iommu_mm_data structure, to mm.
    
    Signed-off-by: Tina Zhang <tina.zhang@intel.com>

--- include/linux/mm_types.h
+++ include/linux/mm_types.h
@@ -581,6 +581,7 @@ struct mm_cid {
 #endif
 
 struct kioctx_table;
+struct iommu_mm_data;
 struct mm_struct {
 	struct {
 		/*
@@ -780,6 +781,7 @@ struct mm_struct {
 
 #ifdef CONFIG_IOMMU_SVA
 		u32 pasid;
+		struct iommu_mm_data *iommu_mm;
 #endif
 #ifdef CONFIG_KSM
 		/*
