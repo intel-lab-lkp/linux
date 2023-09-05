ddfba5d2fc3b1882aa7502ed63848fd2500f68f8
Shubh <shubhisroking@gmail.com>
2023-09-05 09:46:14 +0530

    net: dsa: mt7530: refactor deprecated strncpy
    Prefer `strscpy_pad` to `strncpy`.
    
    Signed-off-by: Shubh <shubhisroking@gmail.com>

--- drivers/net/dsa/mt7530.c
+++ drivers/net/dsa/mt7530.c
@@ -836,7 +836,7 @@ mt7530_get_strings(struct dsa_switch *ds, int port, u32 stringset,
 		return;
 
 	for (i = 0; i < ARRAY_SIZE(mt7530_mib); i++)
-		strncpy(data + i * ETH_GSTRING_LEN, mt7530_mib[i].name,
+		strscpy_pad(data + i * ETH_GSTRING_LEN, mt7530_mib[i].name,
 			ETH_GSTRING_LEN);
 }
 
