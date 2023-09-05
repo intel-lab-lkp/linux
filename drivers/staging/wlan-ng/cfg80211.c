58aad87ab07edb8d4e735ebb29ef3bd1e07e2c30
Angus Gardner <angusg778@gmail.com>
2023-09-05 13:47:36 +1000

    staging: wlan-ng
    Unnecessary parentheses identified by checkpatch.pl removed.
    
    Signed-off-by: Angus Gardner <angusg778@gmail.com>

--- drivers/staging/wlan-ng/cfg80211.c
+++ drivers/staging/wlan-ng/cfg80211.c
@@ -351,7 +351,7 @@ static int prism2_scan(struct wiphy *wiphy,
 		msg2->bssindex.data = i;
 
 		result = p80211req_dorequest(wlandev, (u8 *)&msg2);
-		if ((result != 0) ||
+		if (result != 0 ||
 		    (msg2->resultcode.data != P80211ENUM_resultcode_success)) {
 			break;
 		}
