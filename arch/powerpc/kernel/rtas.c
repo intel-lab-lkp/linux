4b16122c274bab24dce18a91d2a104d4b1f23ff9
Michal Suchanek <msuchanek@suse.de>
2023-09-06 14:08:46 +0200

    powerpc/rtas: Make it possible to disable sys_rtas
    Additional patch suggestion to go with the rtas devices:
    
    -----------------------------------------------------------------------
    
    With most important rtas functions available through different
    interfaces the sys_rtas interface can be disabled completely.
    
    Do not remove it for now to make it possible to run older versions of
    userspace tools that don't support other interfaces.
    
    Signed-off-by: Michal Suchanek <msuchanek@suse.de>

--- arch/powerpc/kernel/rtas.c
+++ arch/powerpc/kernel/rtas.c
@@ -1684,6 +1684,7 @@ noinstr struct pseries_errorlog *get_pseries_errorlog(struct rtas_error_log *log
 	return NULL;
 }
 
+#ifdef PPC_RTAS_SYSCALL
 /*
  * The sys_rtas syscall, as originally designed, allows root to pass
  * arbitrary physical addresses to RTAS calls. A number of RTAS calls
@@ -1893,6 +1894,7 @@ SYSCALL_DEFINE1(rtas, struct rtas_args __user *, uargs)
 
 	return 0;
 }
+#endif
 
 static void __init rtas_function_table_init(void)
 {
