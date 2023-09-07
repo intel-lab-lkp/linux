0ab8cb46153090de721aa7f3315909232e7eb8af
Dnyaneshwar Bhadane <dnyaneshwar.bhadane@intel.com>
2023-09-07 12:27:34 +0530

    drm/i915: Added Wa_18022495364
    This workaround has two different implementations,
    one for gen 12 to DG2 and another for DG2 and later.
    Bspec :  11354 , 56551.
    
    Signed-off-by: Dnyaneshwar Bhadane <dnyaneshwar.bhadane@intel.com>

--- drivers/gpu/drm/i915/gt/intel_workarounds.c
+++ drivers/gpu/drm/i915/gt/intel_workarounds.c
@@ -712,6 +712,9 @@ static void gen12_ctx_workarounds_init(struct intel_engine_cs *engine,
 			    GEN9_PREEMPT_GPGPU_LEVEL_MASK,
 			    GEN9_PREEMPT_GPGPU_THREAD_GROUP_LEVEL);
 
+	/* Wa_18022495364 :tgl,rkl,dg1,adl-s,adl-p */
+	wa_masked_en(wal, GEN12_CS_DEBUG_MODE2,
+		     GEN12_GLOBAL_DEBUG_ENABLE);
 	/*
 	 * Wa_16011163337 - GS_TIMER
 	 *
