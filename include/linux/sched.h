9c0ab02b1cdcbba98f47154c9821b227bfe8ddf3
Elliot Berman <quic_eberman@quicinc.com>
2023-09-08 13:06:02 -0700

    freezer,sched: Use saved_state to reduce some spurious wakeups
    After commit f5d39b020809 ("freezer,sched: Rewrite core freezer logic"),
    tasks that transition directly from TASK_FREEZABLE to TASK_FROZEN  are
    always woken up on the thaw path. Prior to that commit, tasks could ask
    freezer to consider them "frozen enough" via freezer_do_not_count(). The
    commit replaced freezer_do_not_count() with a TASK_FREEZABLE state which
    allows freezer to immediately mark the task as TASK_FROZEN without
    waking up the task.  This is efficient for the suspend path, but on the
    thaw path, the task is always woken up even if the task didn't need to
    wake up and goes back to its TASK_(UN)INTERRUPTIBLE state. Although
    these tasks are capable of handling of the wakeup, we can observe a
    power/perf impact from the extra wakeup.
    
    We observed on Android many tasks wait in the TASK_FREEZABLE state
    (particularly due to many of them being binder clients). We observed
    nearly 4x the number of tasks and a corresponding linear increase in
    latency and power consumption when thawing the system. The latency
    increased from ~15ms to ~50ms.
    
    Avoid the spurious wakeups by saving the state of TASK_FREEZABLE tasks.
    If the task was running before entering TASK_FROZEN state
    (__refrigerator()) or if the task received a wake up for the saved
    state, then the task is woken on thaw. saved_state from PREEMPT_RT locks
    can be re-used because freezer would not stomp on the rtlock wait flow:
    TASK_RTLOCK_WAIT isn't considered freezable.
    
    Reported-by: Prakash Viswalingam <quic_prakashv@quicinc.com>
    Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -745,10 +745,8 @@ struct task_struct {
 #endif
 	unsigned int			__state;
 
-#ifdef CONFIG_PREEMPT_RT
-	/* saved state for "spinlock sleepers" */
+	/* saved state for "spinlock sleepers" and freezer */
 	unsigned int			saved_state;
-#endif
 
 	/*
 	 * This begins the randomizable portion of task_struct. Only
