ba01605004fdccfa660e5964a6a79d4852af44aa
Bragatheswaran Manickavel <bragathemanick0908@gmail.com>
2023-09-09 16:33:43 +0530

    drm: fix doc warnings related to drm connector
    Addressing drm dp/hdmi connector related kernel documentation
    warning and add more information about these values.
    
    Signed-off-by: Bragatheswaran Manickavel <bragathemanick0908@gmail.com>

--- include/drm/drm_connector.h
+++ include/drm/drm_connector.h
@@ -498,6 +498,8 @@ enum drm_privacy_screen_status {
  *   ITU-R BT.601 colorimetry format
  *   The DP spec does not say whether this is the 525 or the 625
  *   line version.
+ * @DRM_MODE_COLORIMETRY_COUNT:
+ *   Represents the count of colorspaces.
  */
 enum drm_colorspace {
 	/* For Default case, driver will set the colorspace */
