4ae83f97425e507c8f5f8e0d68749ccaa6c3ebe1
Hamza Mahfooz <someguy@effective-light.com>
2023-09-10 12:37:48 -0400

    USB: add ignore remote wakeup quirk for one of Logitech's receivers
    This device causes the system to wake up from suspend, as soon as it
    enters it (even if the device attached to the receiver is powered off).
    So, ignore remote wakeup events from it.
    
    Cc: stable@vger.kernel.org
    Signed-off-by: Hamza Mahfooz <someguy@effective-light.com>

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -264,6 +264,10 @@ static const struct usb_device_id usb_quirk_list[] = {
 	/* Logitech Harmony 700-series */
 	{ USB_DEVICE(0x046d, 0xc122), .driver_info = USB_QUIRK_DELAY_INIT },
 
+	/* Logitech lightspeed receiver (0xc547) */
+	{ USB_DEVICE(0x046d, 0xc547), .driver_info =
+			USB_QUIRK_IGNORE_REMOTE_WAKEUP },
+
 	/* Philips PSC805 audio device */
 	{ USB_DEVICE(0x0471, 0x0155), .driver_info = USB_QUIRK_RESET_RESUME },
 
