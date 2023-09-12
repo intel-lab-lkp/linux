80ee1b87e90e6e4122d50b8b13fcad9203bb41f2
Ken Xue <Ken.Xue@amd.com>
2023-09-12 21:16:06 +0800

    acpi: trigger wakeup key event from power button
    Andorid can wakeup from various wakeup sources,
    but only several wakeup sources can wake up screen
    with right events(POWER, WAKEUP) from input device.
    
    Regarding pressing acpi power button, it can resume system and
    ACPI_BITMASK_WAKE_STATUS and ACPI_BITMASK_POWER_BUTTON_STATUS
    are set in pm1a_sts, but kernel does not report any key
    event to user space during resume by default.
    
    So, trigger wakeup key event to user space during resume
    from power button.
    
    Signed-off-by: Ken Xue <Ken.Xue@amd.com>

--- include/acpi/button.h
+++ include/acpi/button.h
@@ -2,17 +2,21 @@
 #ifndef ACPI_BUTTON_H
 #define ACPI_BUTTON_H
 
+struct acpi_device;
+
 #define ACPI_BUTTON_HID_POWER	"PNP0C0C"
 #define ACPI_BUTTON_HID_LID	"PNP0C0D"
 #define ACPI_BUTTON_HID_SLEEP	"PNP0C0E"
 
 #if IS_ENABLED(CONFIG_ACPI_BUTTON)
 extern int acpi_lid_open(void);
+extern void acpi_power_button_wakeup(struct acpi_device *dev);
 #else
 static inline int acpi_lid_open(void)
 {
 	return 1;
 }
+static inline void acpi_power_button_wakeup(struct acpi_device *dev) {}
 #endif /* IS_ENABLED(CONFIG_ACPI_BUTTON) */
 
 #endif /* ACPI_BUTTON_H */
