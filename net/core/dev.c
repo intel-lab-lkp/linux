1c6a88d1a4238fded41524220bb68771f6d6fafa
Edward Cree <ecree.xilinx@gmail.com>
2023-09-12 15:21:37 +0100

    net: ethtool: attach an IDR of custom RSS contexts to a netdevice
    Each context stores the RXFH settings (indir, key, and hfunc) as well
     as optionally some driver private data.
    Delete any still-existing contexts at netdev unregister time.
    
    Signed-off-by: Edward Cree <ecree.xilinx@gmail.com>

--- net/core/dev.c
+++ net/core/dev.c
@@ -10050,6 +10050,9 @@ int register_netdevice(struct net_device *dev)
 	if (ret)
 		return ret;
 
+	/* rss ctx ID 0 is reserved for the default context, start from 1 */
+	idr_init_base(&dev->ethtool->rss_ctx, 1);
+
 	spin_lock_init(&dev->addr_list_lock);
 	netdev_set_addr_lockdep_class(dev);
 
@@ -10852,6 +10855,24 @@ void synchronize_net(void)
 }
 EXPORT_SYMBOL(synchronize_net);
 
+static void netdev_rss_contexts_free(struct net_device *dev)
+{
+	struct ethtool_rxfh_context *ctx;
+	u32 context;
+
+	if (!dev->ethtool_ops->set_rxfh_context)
+		return;
+	idr_for_each_entry(&dev->ethtool->rss_ctx, ctx, context) {
+		u32 *indir = ethtool_rxfh_context_indir(ctx);
+		u8 *key = ethtool_rxfh_context_key(ctx);
+
+		idr_remove(&dev->ethtool->rss_ctx, context);
+		dev->ethtool_ops->set_rxfh_context(dev, indir, key, ctx->hfunc,
+						   &context, true);
+		kfree(ctx);
+	}
+}
+
 /**
  *	unregister_netdevice_queue - remove device from the kernel
  *	@dev: device
@@ -10955,6 +10976,8 @@ void unregister_netdevice_many_notify(struct list_head *head,
 
 		netdev_name_node_alt_flush(dev);
 		netdev_name_node_free(dev->name_node);
+
+		netdev_rss_contexts_free(dev);
 
 		call_netdevice_notifiers(NETDEV_PRE_UNINIT, dev);
 
