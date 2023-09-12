4bd2b944528f163cf8ff0012abc67ea30ced6ae1
Vinayak Yadawad <vinayak.yadawad@broadcom.com>
2023-09-12 12:24:51 +0530

    cfg80211: Allow AP/P2PGO to indicate port authorization to peer STA/P2PClient
    In 4way handshake offload, cfg80211_port_authorized
    enables driver to indicate successful 4way handshake to cfg80211 layer.
    Currently this path of port authorization is restricted to
    interface type NL80211_IFTYPE_STATION and NL80211_IFTYPE_P2P_CLIENT.
    This patch extends the support for NL80211_IFTYPE_AP and
    NL80211_IFTYPE_P2P_GO interfaces to authorize peer STA/P2P_CLIENT,
    whenever authentication is offloaded on the AP/P2P_GO interface.
    
    Signed-off-by: Vinayak Yadawad <vinayak.yadawad@broadcom.com>

--- net/wireless/sme.c
+++ net/wireless/sme.c
@@ -1294,24 +1294,28 @@ out:
 }
 EXPORT_SYMBOL(cfg80211_roamed);
 
-void __cfg80211_port_authorized(struct wireless_dev *wdev, const u8 *bssid,
+void __cfg80211_port_authorized(struct wireless_dev *wdev, const u8 *peer_mac,
 					const u8 *td_bitmap, u8 td_bitmap_len)
 {
 	ASSERT_WDEV_LOCK(wdev);
 
 	if (WARN_ON(wdev->iftype != NL80211_IFTYPE_STATION &&
-		    wdev->iftype != NL80211_IFTYPE_P2P_CLIENT))
+		    wdev->iftype != NL80211_IFTYPE_P2P_CLIENT &&
+		    wdev->iftype != NL80211_IFTYPE_AP &&
+		    wdev->iftype != NL80211_IFTYPE_P2P_GO))
 		return;
 
 	if (WARN_ON(!wdev->connected) ||
-	    WARN_ON(!ether_addr_equal(wdev->u.client.connected_addr, bssid)))
+	    WARN_ON((!ether_addr_equal(wdev->u.client.connected_addr, peer_mac) &&
+	    (wdev->iftype == NL80211_IFTYPE_STATION ||
+	    wdev->iftype == NL80211_IFTYPE_P2P_CLIENT))))
 		return;
 
 	nl80211_send_port_authorized(wiphy_to_rdev(wdev->wiphy), wdev->netdev,
-				     bssid, td_bitmap, td_bitmap_len);
+				     peer_mac, td_bitmap, td_bitmap_len);
 }
 
-void cfg80211_port_authorized(struct net_device *dev, const u8 *bssid,
+void cfg80211_port_authorized(struct net_device *dev, const u8 *peer_mac,
 			      const u8 *td_bitmap, u8 td_bitmap_len, gfp_t gfp)
 {
 	struct wireless_dev *wdev = dev->ieee80211_ptr;
@@ -1319,7 +1323,7 @@ void cfg80211_port_authorized(struct net_device *dev, const u8 *bssid,
 	struct cfg80211_event *ev;
 	unsigned long flags;
 
-	if (WARN_ON(!bssid))
+	if (WARN_ON(!peer_mac))
 		return;
 
 	ev = kzalloc(sizeof(*ev) + td_bitmap_len, gfp);
@@ -1327,7 +1331,7 @@ void cfg80211_port_authorized(struct net_device *dev, const u8 *bssid,
 		return;
 
 	ev->type = EVENT_PORT_AUTHORIZED;
-	memcpy(ev->pa.bssid, bssid, ETH_ALEN);
+	memcpy(ev->pa.bssid, peer_mac, ETH_ALEN);
 	ev->pa.td_bitmap = ((u8 *)ev) + sizeof(*ev);
 	ev->pa.td_bitmap_len = td_bitmap_len;
 	memcpy((void *)ev->pa.td_bitmap, td_bitmap, td_bitmap_len);
