7f6804968e6cfce344e7a8e40c88658e0d12635d
Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
2023-09-13 13:50:01 +0200

    gpiolib: remove gpiod_toggle_active_low()
    With all users now having switched to gpiod_set_active_[low|high](), we
    can now remove gpiod_toggle_active_low().
    
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- include/linux/gpio/consumer.h
+++ include/linux/gpio/consumer.h
@@ -159,7 +159,6 @@ int gpiod_set_raw_array_value_cansleep(unsigned int array_size,
 
 int gpiod_set_config(struct gpio_desc *desc, unsigned long config);
 int gpiod_set_debounce(struct gpio_desc *desc, unsigned int debounce);
-void gpiod_toggle_active_low(struct gpio_desc *desc);
 void gpiod_set_active_low(struct gpio_desc *desc);
 void gpiod_set_active_high(struct gpio_desc *desc);
 
@@ -493,12 +492,6 @@ static inline int gpiod_set_debounce(struct gpio_desc *desc, unsigned int deboun
 	/* GPIO can never have been requested */
 	WARN_ON(desc);
 	return -ENOSYS;
-}
-
-static inline void gpiod_toggle_active_low(struct gpio_desc *desc)
-{
-	/* GPIO can never have been requested */
-	WARN_ON(desc);
 }
 
 static inline void gpiod_set_active_low(struct gpio_desc *desc
