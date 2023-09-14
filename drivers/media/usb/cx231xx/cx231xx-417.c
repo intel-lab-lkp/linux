a59e1de70db32471d0234095fc465776e443ec06
Benjamin Gaignard <benjamin.gaignard@collabora.com>
2023-09-14 15:33:12 +0200

    media: usb: cx231xx: Stop direct calls to queue num_buffers field
    Use vb2_get_num_buffers() to avoid using queue num_buffer field directly.
    
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@collabora.com>

--- drivers/media/usb/cx231xx/cx231xx-417.c
+++ drivers/media/usb/cx231xx/cx231xx-417.c
@@ -1218,13 +1218,14 @@ static int queue_setup(struct vb2_queue *vq,
 		       unsigned int sizes[], struct device *alloc_devs[])
 {
 	struct cx231xx *dev = vb2_get_drv_priv(vq);
+	unsigned int q_num_bufs = vb2_get_num_buffers(vq);
 	unsigned int size = mpeglinesize * mpeglines;
 
 	dev->ts1.ts_packet_size  = mpeglinesize;
 	dev->ts1.ts_packet_count = mpeglines;
 
-	if (vq->num_buffers + *nbuffers < CX231XX_MIN_BUF)
-		*nbuffers = CX231XX_MIN_BUF - vq->num_buffers;
+	if (q_num_bufs + *nbuffers < CX231XX_MIN_BUF)
+		*nbuffers = CX231XX_MIN_BUF - q_num_bufs;
 
 	if (*nplanes)
 		return sizes[0] < size ? -EINVAL : 0;
