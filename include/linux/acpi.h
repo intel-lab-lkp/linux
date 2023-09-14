3c6be593dc92ec8a2017102a33f86601884efbc3
Oza Pawandeep <quic_poza@quicinc.com>
2023-09-14 11:48:39 -0700

    cpuidle, ACPI: Evaluate LPI arch_flags for broadcast timer
    Arm® Functional Fixed Hardware Specification defines LPI states,
    which provide an architectural context loss flags field that can
    be used to describe the context that might be lost when an LPI
    state is entered.
    
    - Core context Lost
            - General purpose registers.
            - Floating point and SIMD registers.
            - System registers, include the System register based
            - generic timer for the core.
            - Debug register in the core power domain.
            - PMU registers in the core power domain.
            - Trace register in the core power domain.
    - Trace context loss
    - GICR
    - GICD
    
    Qualcomm's custom CPUs preserves the architectural state,
    including keeping the power domain for local timers active.
    when core is power gated, the local timers are sufficient to
    wake the core up without needing broadcast timer.
    
    The patch fixes the evaluation of cpuidle arch_flags, and moves only to
    broadcast timer if core context lost is defined in ACPI LPI.
    
    Fixes: a36a7fecfe607 ("Add support for Low Power Idle(LPI) states")
    Reviewed-by: Sudeep Holla <sudeep.holla@arm.com>
    Signed-off-by: Oza Pawandeep <quic_poza@quicinc.com>

--- include/linux/acpi.h
+++ include/linux/acpi.h
@@ -1480,6 +1480,10 @@ static inline int lpit_read_residency_count_address(u64 *address)
 }
 #endif
 
+#ifndef arch_update_idle_state_flags
+#define arch_update_idle_state_flags(af, sf)	do {} while (0)
+#endif
+
 #ifdef CONFIG_ACPI_PPTT
 int acpi_pptt_cpu_is_thread(unsigned int cpu);
 int find_acpi_cpu_topology(unsigned int cpu, int level);
