34a51dda21743a440181bf58be986323e3e5d13e
Benjamin Gaignard <benjamin.gaignard@collabora.com>
2023-09-14 15:33:00 +0200

    media: pci: tw68: Stop direct calls to queue num_buffers field
    Use vb2_get_num_buffers() to avoid using queue num_buffer field directly.
    
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@collabora.com>

--- drivers/media/pci/tw68/tw68-video.c
+++ drivers/media/pci/tw68/tw68-video.c
@@ -360,13 +360,13 @@ static int tw68_queue_setup(struct vb2_queue *q,
 			   unsigned int sizes[], struct device *alloc_devs[])
 {
 	struct tw68_dev *dev = vb2_get_drv_priv(q);
-	unsigned tot_bufs = q->num_buffers + *num_buffers;
+	unsigned tot_bufs = vb2_get_num_buffers(q) + *num_buffers;
 	unsigned size = (dev->fmt->depth * dev->width * dev->height) >> 3;
 
 	if (tot_bufs < 2)
 		tot_bufs = 2;
 	tot_bufs = tw68_buffer_count(size, tot_bufs);
-	*num_buffers = tot_bufs - q->num_buffers;
+	*num_buffers = tot_bufs - vb2_get_num_buffers(q);
 	/*
 	 * We allow create_bufs, but only if the sizeimage is >= as the
 	 * current sizeimage. The tw68_buffer_count calculation becomes quite
