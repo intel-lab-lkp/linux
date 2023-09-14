cae336e966ff32ec3369c1bcbbe01993748cb5f8
Benjamin Gaignard <benjamin.gaignard@collabora.com>
2023-09-14 15:32:40 +0200

    media: mediatek: vdec: Use vb2_get_buffer() instead of directly access to buffers array
    Use vb2_get_buffer() instead of directly access to vb2_buffer buffer array.
    This could allow to change the type bufs[] field of vb2_buffer structure if
    needed.
    After each call to vb2_get_buffer() we need to be sure that we get
    a valid pointer so check the return value of all of them.
    
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@collabora.com>

--- drivers/media/platform/mediatek/vcodec/decoder/vdec/vdec_vp9_req_lat_if.c
+++ drivers/media/platform/mediatek/vcodec/decoder/vdec/vdec_vp9_req_lat_if.c
@@ -1696,7 +1696,7 @@ static int vdec_vp9_slice_setup_core_buffer(struct vdec_vp9_slice_instance *inst
 
 	/* update internal buffer's width/height */
 	for (i = 0; i < vq->num_buffers; i++) {
-		if (vb == vq->bufs[i]) {
+		if (vb == vb2_get_buffer(vq, i)) {
 			instance->dpb[i].width = w;
 			instance->dpb[i].height = h;
 			break;
