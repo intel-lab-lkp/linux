9f485d79cacd6f786f4d3235cf270d860e4d167b
Víctor Gonzalo <victor.gonzalo@anddroptable.net>
2023-09-14 16:51:00 +0300

    media: ipu3: Use MODULE_FIRMWARE to add firmware files metadata
    The ipu3 driver requests firmware files but does not use the
    MODULE_FIRMWARE macro to show them in the module metadata
    
    Signed-off-by: Víctor Gonzalo <victor.gonzalo@anddroptable.net>

--- drivers/staging/media/ipu3/ipu3.c
+++ drivers/staging/media/ipu3/ipu3.c
@@ -13,6 +13,7 @@
 #include <linux/pm_runtime.h>
 
 #include "ipu3.h"
+#include "ipu3-css-fw.h"
 #include "ipu3-dmamap.h"
 #include "ipu3-mmu.h"
 
@@ -866,3 +867,5 @@ MODULE_AUTHOR("Yuning Pu <yuning.pu@intel.com>");
 MODULE_AUTHOR("Yong Zhi <yong.zhi@intel.com>");
 MODULE_LICENSE("GPL v2");
 MODULE_DESCRIPTION("Intel ipu3_imgu PCI driver");
+MODULE_FIRMWARE(IMGU_FW_NAME);
+MODULE_FIRMWARE(IMGU_FW_NAME_20161208);
