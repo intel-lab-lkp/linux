10d954511af14bf3637a7848b50383bd10034573
Ma Ke <make_ruc2021@163.com>
2023-09-15 18:50:36 +0800

    net: sched: qfq: dont intepret cls results when asked to drop
    If asked to drop a packet via TC_ACT_SHOT it is unsafe to
    assume that res.class contains a valid pointer.
    
    Signed-off-by: Ma Ke <make_ruc2021@163.com>

--- net/sched/sch_qfq.c
+++ net/sched/sch_qfq.c
@@ -695,6 +695,8 @@ static struct qfq_class *qfq_classify(struct sk_buff *skb, struct Qdisc *sch,
 	*qerr = NET_XMIT_SUCCESS | __NET_XMIT_BYPASS;
 	fl = rcu_dereference_bh(q->filter_list);
 	result = tcf_classify(skb, NULL, fl, &res, false);
+	if (result == TC_ACT_SHOT)
+		return NULL;
 	if (result >= 0) {
 #ifdef CONFIG_NET_CLS_ACT
 		switch (result) {
