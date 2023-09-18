884a3228f9594662670a2c7cf722b535954d97e8
AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
2023-09-18 17:00:43 +0200

    drm/bridge: panel: Fix device link for DRM_BRIDGE_ATTACH_NO_CONNECTOR
    When external bridges are attached with DRM_BRIDGE_ATTACH_NO_CONNECTOR,
    the panel bridge may also get the same flag, but in the .attach()
    callback for the panel bridge a device link is added only when this
    flag is not present; To make things worse, the .detach() callback
    tries to delete the device link unconditionally and without checking
    if it was created in the first place, crashing the kernel with a NULL
    pointer kernel panic upon calling panel_bridge_detach().
    
    Fix that by moving the device_link_add() call before checking if the
    DRM_BRIDGE_ATTACH_NO_CONNECTOR flag is present.
    
    Fixes: 199cf07ebd2b ("drm/bridge: panel: Add a device link between drm device and panel device")
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>

--- drivers/gpu/drm/bridge/panel.c
+++ drivers/gpu/drm/bridge/panel.c
@@ -67,20 +67,20 @@ static int panel_bridge_attach(struct drm_bridge *bridge,
 	struct drm_device *drm_dev = bridge->dev;
 	int ret;
 
-	if (flags & DRM_BRIDGE_ATTACH_NO_CONNECTOR)
-		return 0;
-
-	if (!bridge->encoder) {
-		DRM_ERROR("Missing encoder\n");
-		return -ENODEV;
-	}
-
 	panel_bridge->link = device_link_add(drm_dev->dev, panel->dev,
 					     DL_FLAG_STATELESS);
 	if (!panel_bridge->link) {
 		DRM_ERROR("Failed to add device link between %s and %s\n",
 			  dev_name(drm_dev->dev), dev_name(panel->dev));
 		return -EINVAL;
+	}
+
+	if (flags & DRM_BRIDGE_ATTACH_NO_CONNECTOR)
+		return 0;
+
+	if (!bridge->encoder) {
+		DRM_ERROR("Missing encoder\n");
+		return -ENODEV;
 	}
 
 	drm_connector_helper_add(connector,
