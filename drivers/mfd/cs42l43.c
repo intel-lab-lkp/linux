28d27497d7bc0424bbb89f031e169fc44a3a5c52
Raag Jadav <raag.jadav@intel.com>
2023-09-18 13:39:50 +0530

    mfd: cs42l43: convert to EXPORT_NS_GPL_RUNTIME_PM_OPS()
    With original macro being renamed to EXPORT_NS_GPL_RUNTIME_PM_OPS(),
    use the new macro.
    
    Signed-off-by: Raag Jadav <raag.jadav@intel.com>

--- drivers/mfd/cs42l43.c
+++ drivers/mfd/cs42l43.c
@@ -1177,7 +1177,7 @@ err:
 	return ret;
 }
 
-EXPORT_NS_GPL_DEV_PM_OPS(cs42l43_pm_ops, MFD_CS42L43) = {
+EXPORT_NS_GPL_RUNTIME_PM_OPS(cs42l43_pm_ops, MFD_CS42L43) = {
 	SET_SYSTEM_SLEEP_PM_OPS(cs42l43_suspend, cs42l43_resume)
 	SET_RUNTIME_PM_OPS(cs42l43_runtime_suspend, cs42l43_runtime_resume, NULL)
 };
