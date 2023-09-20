eba232bc4e33b1b519f037c1f5fc5a2f30478324
Maxime Ripard <mripard@kernel.org>
2023-09-20 16:35:20 +0200

    drm/connector: hdmi: Add output BPC to the connector state
    We'll add automatic selection of the output BPC in a following patch,
    but let's add it to the HDMI connector state already.
    
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- drivers/gpu/drm/drm_atomic_state_helper.c
+++ drivers/gpu/drm/drm_atomic_state_helper.c
@@ -569,6 +569,9 @@ EXPORT_SYMBOL(drm_atomic_helper_connector_tv_reset);
 void __drm_atomic_helper_connector_hdmi_reset(struct drm_connector *connector,
 					      struct drm_connector_state *new_state)
 {
+	new_state->max_bpc = 8;
+	new_state->max_requested_bpc = 8;
+	new_state->hdmi.output_bpc = 8;
 	new_state->hdmi.broadcast_rgb = DRM_HDMI_BROADCAST_RGB_AUTO;
 }
 EXPORT_SYMBOL(__drm_atomic_helper_connector_hdmi_reset);
@@ -651,7 +654,8 @@ int drm_atomic_helper_connector_hdmi_check(struct drm_connector *connector,
 	struct drm_connector_state *new_state =
 		drm_atomic_get_new_connector_state(state, connector);
 
-	if (old_state->hdmi.broadcast_rgb != new_state->hdmi.broadcast_rgb) {
+	if (old_state->hdmi.broadcast_rgb != new_state->hdmi.broadcast_rgb ||
+	    old_state->hdmi.output_bpc != new_state->hdmi.output_bpc) {
 		struct drm_crtc *crtc = new_state->crtc;
 		struct drm_crtc_state *crtc_state;
 
