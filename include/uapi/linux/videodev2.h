cadb4e391d78b1d7ed39ca1b6846b1b1c281374f
Shengjiu Wang <shengjiu.wang@nxp.com>
2023-09-20 17:32:46 +0800

    media: uapi: Add V4L2_AUDIO_FMT_LPCM fourcc format
    Linear Pulse-Code Modulation is used to represent
    audio samples in buffer, the samples for each channel
    are interleaved.
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -839,6 +839,9 @@ struct v4l2_pix_format {
 #define V4L2_META_FMT_RK_ISP1_PARAMS	v4l2_fourcc('R', 'K', '1', 'P') /* Rockchip ISP1 3A Parameters */
 #define V4L2_META_FMT_RK_ISP1_STAT_3A	v4l2_fourcc('R', 'K', '1', 'S') /* Rockchip ISP1 3A Statistics */
 
+/* Audio-data formats */
+#define V4L2_AUDIO_FMT_LPCM       v4l2_fourcc('L', 'P', 'C', 'M') /* audio lpcm */
+
 /* priv field value to indicates that subsequent fields are valid. */
 #define V4L2_PIX_FMT_PRIV_MAGIC		0xfeedcafe
 
