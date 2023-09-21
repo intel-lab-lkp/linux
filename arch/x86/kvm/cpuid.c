84d34cf16f19d3b7d6f1cfd09c9f17f8e789cedc
Xiong Zhang <xiong.y.zhang@intel.com>
2023-09-21 16:29:55 +0800

    KVM: x86/pmu: Add fixed counter enumeration for pmu v5
    With Arch PMU v5, CPUID.0AH.ECX is a bit mask which enumerates the
    supported Fixed Counters. If bit 'i' is set, it implies that Fixed
    Counter 'i' is supported.
    
    This commit add CPUID.0AH.ECX emulation for vPMU version 5, KVM
    support Fixed Counter enumeration starting from 0 by default,
    user could modify it through SET_CPUID2 ioctl.
    
    When perf driver supports fixed counter bitmap and exports it,
    KVM will get it from perf driver, and set it as
    KVM supported CPUID.0AH.ECX.
    
    Signed-off-by: Xiong Zhang <xiong.y.zhang@intel.com>

--- arch/x86/kvm/cpuid.c
+++ arch/x86/kvm/cpuid.c
@@ -1004,7 +1004,15 @@ static inline int __do_cpuid_func(struct kvm_cpuid_array *array, u32 function)
 
 		entry->eax = eax.full;
 		entry->ebx = kvm_pmu_cap.events_mask;
-		entry->ecx = 0;
+		if (kvm_pmu_cap.version < 5)
+			entry->ecx = 0;
+		else
+			/*
+			 * Todo: When perf driver supports Fixed counter bitmap
+			 * and export it, KVM will get it from perf driver and set
+			 * it into entry->ecx.
+			 */
+			entry->ecx = (1ULL << kvm_pmu_cap.num_counters_fixed) - 1;
 		entry->edx = edx.full;
 		break;
 	}
