2339dc3a90f4e07c785c8111d693a18279d89f02
Wenchao Hao <haowenchao2@huawei.com>
2023-09-22 17:28:57 +0800

    scsi: scsi_debug: create scsi_debug directory in the debugfs filesystem
    Create directory scsi_debug in the root of the debugfs filesystem.
    Prepare to add interface for manage error injection.
    
    Acked-by: Douglas Gilbert <dgilbert@interlog.com>
    Signed-off-by: Wenchao Hao <haowenchao2@huawei.com>

--- drivers/scsi/scsi_debug.c
+++ drivers/scsi/scsi_debug.c
@@ -41,6 +41,7 @@
 #include <linux/random.h>
 #include <linux/xarray.h>
 #include <linux/prefetch.h>
+#include <linux/debugfs.h>
 
 #include <net/checksum.h>
 
@@ -861,6 +862,8 @@ static const int device_qfull_result =
 	(DID_ABORT << 16) | SAM_STAT_TASK_SET_FULL;
 
 static const int condition_met_result = SAM_STAT_CONDITION_MET;
+
+static struct dentry *sdebug_debugfs_root;
 
 
 /* Only do the extra work involved in logical block provisioning if one or
@@ -7011,6 +7014,8 @@ static int __init scsi_debug_init(void)
 		goto driver_unreg;
 	}
 
+	sdebug_debugfs_root = debugfs_create_dir("scsi_debug", NULL);
+
 	for (k = 0; k < hosts_to_add; k++) {
 		if (want_store && k == 0) {
 			ret = sdebug_add_host_helper(idx);
@@ -7057,6 +7062,7 @@ static void __exit scsi_debug_exit(void)
 
 	sdebug_erase_all_stores(false);
 	xa_destroy(per_store_ap);
+	debugfs_remove(sdebug_debugfs_root);
 }
 
 device_initcall(scsi_debug_init);
