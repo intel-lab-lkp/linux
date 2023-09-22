4866441dfb903869f937c4d90158578796866c61
Shekhar Chauhan <shekhar.chauhan@intel.com>
2023-09-22 09:20:24 +0530

    drm/i915: Add Wa_18028616096
    Drop UGM per set fragment threshold to 3
    
    BSpec: 54833
    Signed-off-by: Shekhar Chauhan <shekhar.chauhan@intel.com>

--- drivers/gpu/drm/i915/gt/intel_workarounds.c
+++ drivers/gpu/drm/i915/gt/intel_workarounds.c
@@ -2914,6 +2914,9 @@ general_render_compute_wa_init(struct intel_engine_cs *engine, struct i915_wa_li
 		 * Wa_22015475538:dg2
 		 */
 		wa_mcr_write_or(wal, LSC_CHICKEN_BIT_0_UDW, DIS_CHAIN_2XSIMD8);
+
+		/* Wa_18028616096 */
+		wa_mcr_write_or(wal, LSC_CHICKEN_BIT_0_UDW, UGM_FRAGMENT_THRESHOLD_TO_3);
 	}
 
 	if (IS_DG2_G11(i915)) {
