7acfac98d5298e1d483b1e02c787407868b3d685
Basavaraj Natikar <Basavaraj.Natikar@amd.com>
2023-09-22 23:20:56 +0530

    platform/x86/amd/pmf: Add PMF-AMDSFH interface for ALS
    AMDSFH has information about the Ambient light via the Ambient
    Light Sensor (ALS) which is part of the AMD sensor fusion hub.
    Add PMF and AMDSFH interface to get this information.
    
    Co-developed-by: Shyam Sundar S K <Shyam-sundar.S-k@amd.com>
    Signed-off-by: Shyam Sundar S K <Shyam-sundar.S-k@amd.com>
    Signed-off-by: Basavaraj Natikar <Basavaraj.Natikar@amd.com>

--- include/linux/amd-pmf-io.h
+++ include/linux/amd-pmf-io.h
@@ -31,6 +31,7 @@ int amd_pmf_set_gfx_data(struct amd_gpu_pmf_data *pmf);
 /* amd-sfh */
 enum sfh_message_type {
 	MT_HPD,
+	MT_ALS,
 };
 
 enum hpd_info {
@@ -40,6 +41,7 @@ enum hpd_info {
 };
 
 struct amd_sfh_info {
+	u32 ambient_light;
 	u8 user_present;
 	/* add future caps below */
 };
