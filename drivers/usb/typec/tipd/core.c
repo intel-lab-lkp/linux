ae5282e4e6452260f616753aae87ad1706ba7e26
Abdel Alkuor <abdelalkuor@geotab.com>
2023-09-23 03:39:52 -0400

    USB: typec: Apply patch again after power resume
    TPS25750 PD controller might be powered off externally at power suspend,
    after resuming PD controller power back, apply the patch again.
    
    Signed-off-by: Abdel Alkuor <abdelalkuor@geotab.com>

--- drivers/usb/typec/tipd/core.c
+++ drivers/usb/typec/tipd/core.c
@@ -1204,6 +1204,18 @@ static int __maybe_unused tps6598x_resume(struct device *dev)
 {
 	struct i2c_client *client = to_i2c_client(dev);
 	struct tps6598x *tps = i2c_get_clientdata(client);
+	struct device_node *np = client->dev.of_node;
+	int ret;
+
+	ret = tps6598x_check_mode(tps);
+	if (ret < 0)
+		return ret;
+
+	if (of_device_is_compatible(np, "ti,tps25750") && ret == TPS_MODE_PTCH) {
+		ret = tps25750_apply_patch(tps);
+		if (ret)
+			return ret;
+	}
 
 	if (tps->wakeup) {
 		disable_irq_wake(client->irq);
