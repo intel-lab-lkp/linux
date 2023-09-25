b981465342b95320d3f692f9f002c33de68125a8
Hugh Dickins <hughd@google.com>
2023-09-25 01:21:10 -0700

    hugetlbfs: drop shared NUMA mempolicy pretence
    hugetlbfs_fallocate() goes through the motions of pasting a shared NUMA
    mempolicy onto its pseudo-vma, but how could there ever be a shared NUMA
    mempolicy for this file?  hugetlb_vm_ops has never offered a set_policy
    method, and hugetlbfs_parse_param() has never supported any mpol options
    for a mount-wide default policy.
    
    It's just an illusion: clean it away so as not to confuse others, giving
    us more freedom to adjust shmem's set_policy/get_policy implementation.
    But hugetlbfs_inode_info is still required, just to accommodate seals.
    
    Yes, shared NUMA mempolicy support could be added to hugetlbfs, with a
    set_policy method and/or mpol mount option (Andi's first posting did
    include an admitted-unsatisfactory hugetlb_set_policy()); but it seems
    that nobody has bothered to add that in the nineteen years since v2.6.7
    made it possible, and there is at least one company that has invested
    enough into hugetlbfs, that I guess they have learnt well enough how to
    manage its NUMA, without needing shared mempolicy.
    
    Signed-off-by: Hugh Dickins <hughd@google.com>

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -30,7 +30,6 @@ void free_huge_folio(struct folio *folio);
 
 #ifdef CONFIG_HUGETLB_PAGE
 
-#include <linux/mempolicy.h>
 #include <linux/shm.h>
 #include <asm/tlbflush.h>
 
@@ -512,7 +511,6 @@ static inline struct hugetlbfs_sb_info *HUGETLBFS_SB(struct super_block *sb)
 }
 
 struct hugetlbfs_inode_info {
-	struct shared_policy policy;
 	struct inode vfs_inode;
 	unsigned int seals;
 };
