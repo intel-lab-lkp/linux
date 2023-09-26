7248422ece081608a907df16d4ebc2ee38d82b69
Paul Durrant <pdurrant@amazon.com>
2023-09-26 12:20:04 +0000

    KVM: pfncache: add a mark-dirty helper
    At the moment pages are marked dirty by open-coded calls to
    mark_page_dirty_in_slot(), directly deferefencing the gpa and memslot
    from the cache. After a subsequent patch these may not always be set
    so add a helper now so that caller will protected from the need to know
    about this detail.
    
    NOTE: Pages are now marked dirty while the cache lock is held. This is
          to ensure that gpa and memslot are mutually consistent.
    
    Signed-off-by: Paul Durrant <pdurrant@amazon.com>
    Reviewed-by: David Woodhouse <dwmw@amazon.co.uk>

--- include/linux/kvm_host.h
+++ include/linux/kvm_host.h
@@ -1367,6 +1367,13 @@ int kvm_gpc_refresh(struct gfn_to_pfn_cache *gpc, unsigned long len);
  */
 void kvm_gpc_deactivate(struct gfn_to_pfn_cache *gpc);
 
+/**
+ * kvm_gpc_mark_dirty - mark a cached page as dirty.
+ *
+ * @gpc:	   struct gfn_to_pfn_cache object.
+ */
+void kvm_gpc_mark_dirty(struct gfn_to_pfn_cache *gpc);
+
 void kvm_sigset_activate(struct kvm_vcpu *vcpu);
 void kvm_sigset_deactivate(struct kvm_vcpu *vcpu);
 
