3d57f7ae89162300eec3d57b2333452b91863f51
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2023-09-26 13:53:26 +0530

    drm/i915/display: Consider fractional vdsc bpp while computing m_n values
    MTL+ supports fractional compressed bits_per_pixel, with precision of
    1/16. This compressed bpp is stored in U6.4 format.
    Accommodate this precision while computing m_n values.
    
    v1:
    Replace the computation of 'data_clock' with 'data_clock =
    DIV_ROUND_UP(data_clock, 16).' (Sui Jingfeng).
    
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>
    Signed-off-by: Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>
    Reviewed-by: Suraj Kandpal <suraj.kandpal@intel.com>

--- drivers/gpu/drm/i915/display/intel_dp_mst.c
+++ drivers/gpu/drm/i915/display/intel_dp_mst.c
@@ -172,7 +172,8 @@ static int intel_dp_mst_compute_link_config(struct intel_encoder *encoder,
 			       adjusted_mode->crtc_clock,
 			       crtc_state->port_clock,
 			       &crtc_state->dp_m_n,
-			       crtc_state->fec_enable);
+			       crtc_state->fec_enable,
+			       false);
 	crtc_state->dp_m_n.tu = slots;
 
 	return 0;
@@ -269,7 +270,8 @@ static int intel_dp_dsc_mst_compute_link_config(struct intel_encoder *encoder,
 			       adjusted_mode->crtc_clock,
 			       crtc_state->port_clock,
 			       &crtc_state->dp_m_n,
-			       crtc_state->fec_enable);
+			       crtc_state->fec_enable,
+			       crtc_state->dsc.compression_enable);
 	crtc_state->dp_m_n.tu = slots;
 
 	return 0;
