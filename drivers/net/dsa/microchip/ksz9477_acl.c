94307472af4c55933554feeb48ade6c691eb633a
Dan Carpenter <dan.carpenter@linaro.org>
2023-09-27 15:53:37 +0300

    net: dsa: microchip: Uninitialized variable in ksz9477_acl_move_entries()
    Smatch complains that if "src_idx" equals "dst_idx" then
    ksz9477_validate_and_get_src_count() doesn't initialized "src_count".
    Set it to zero for this situation.
    
    Fixes: 002841be134e ("net: dsa: microchip: Add partial ACL support for ksz9477 switches")
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

--- drivers/net/dsa/microchip/ksz9477_acl.c
+++ drivers/net/dsa/microchip/ksz9477_acl.c
@@ -554,7 +554,8 @@ static int ksz9477_acl_move_entries(struct ksz_device *dev, int port,
 	struct ksz9477_acl_entry buffer[KSZ9477_ACL_MAX_ENTRIES];
 	struct ksz9477_acl_priv *acl = dev->ports[port].acl_priv;
 	struct ksz9477_acl_entries *acles = &acl->acles;
-	int src_count, ret, dst_count;
+	int ret, dst_count;
+	int src_count = 0;
 
 	ret = ksz9477_validate_and_get_src_count(dev, port, src_idx, dst_idx,
 						 &src_count, &dst_count);
