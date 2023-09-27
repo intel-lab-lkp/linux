29bc6fd2a384f65e9334d48c4d714e773eefbf2e
Jouni Högander <jouni.hogander@intel.com>
2023-09-27 13:27:07 +0300

    drm/i915: Ignore set frontbuffer return value on release
    i915_gem_object_set_frontbuffer returns set frontbuffer pointer.
    When we are releasing frontbuffer we are clearing the pointer from
    the object and the value can be ignored.
    
    Signed-off-by: Jouni Högander <jouni.hogander@intel.com>

--- drivers/gpu/drm/i915/display/intel_frontbuffer.c
+++ drivers/gpu/drm/i915/display/intel_frontbuffer.c
@@ -259,7 +259,7 @@ static void frontbuffer_release(struct kref *ref)
 
 	i915_ggtt_clear_scanout(obj);
 
-	i915_gem_object_set_frontbuffer(obj, NULL);
+	(void)i915_gem_object_set_frontbuffer(obj, NULL);
 	spin_unlock(&intel_bo_to_i915(obj)->display.fb_tracking.lock);
 
 	i915_active_fini(&front->write);
