5b939970f2c1a96f6bf19a40877bdc9dac191827
Dan Williams <dan.j.williams@intel.com>
2023-09-27 17:43:22 +0200

    cxl/port: Fix @host confusion in cxl_dport_setup_regs()
    commit 5d2ffbe4b81a ("cxl/port: Store the downstream port's Component Register mappings in struct cxl_dport")
    
    ...moved the dport component registers from a raw component_reg_phys
    passed in at dport instantiation time to a 'struct cxl_register_map'
    populated with both the component register data *and* the "host" device
    for mapping operations.
    
    While typical CXL switch dports are mapped by their associated 'struct
    cxl_port', an RCH host bridge dport registered by cxl_acpi needs to wait
    until the cxl_mem driver makes the attachment to map the registers. This
    is because there are no intervening 'struct cxl_port' instances between
    the root cxl_port and the endpoint port in an RCH topology.
    
    For now just mark the host as NULL in the RCH dport case until code that
    needs to map the dport registers arrives. Name the field @reg_map,
    because @reg_map->host will be used for mapping operations beyond
    component registers (i.e. AER registers).
    
    This patch is not flagged for -stable since nothing in the current
    driver uses the dport->reg_map.
    
    Now, I am slightly uneasy that cxl_setup_comp_regs() sets map->host to a
    wrong value and then cxl_dport_setup_regs() fixes it up, but the
    alternatives I came up with are more messy. For example, adding an
    @logdev to 'struct cxl_register_map' that the dev_printk()s can fall
    back to when @host is NULL. I settled on "post-fixup+comment" since it
    is only RCH dports that have this special case where register probing is
    split between a host-bridge RCRB lookup and when cxl_mem_probe() does
    the association of the cxl_memdev and endpoint port.
    
    Fixes: 5d2ffbe4b81a ("cxl/port: Store the downstream port's Component Register mappings in struct cxl_dport")
    Cc: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Signed-off-by: Dan Williams <dan.j.williams@intel.com>
    [kept dev_dbg() message]
    Signed-off-by: Robert Richter <rrichter@amd.com>

--- drivers/cxl/cxl.h
+++ drivers/cxl/cxl.h
@@ -620,7 +620,7 @@ struct cxl_rcrb_info {
 /**
  * struct cxl_dport - CXL downstream port
  * @dport_dev: PCI bridge or firmware device representing the downstream link
- * @comp_map: component register capability mappings
+ * @reg_map: component and ras register mapping parameters
  * @port_id: unique hardware identifier for dport in decoder target list
  * @rcrb: Data about the Root Complex Register Block layout
  * @rch: Indicate whether this dport was enumerated in RCH or VH mode
@@ -628,7 +628,7 @@ struct cxl_rcrb_info {
  */
 struct cxl_dport {
 	struct device *dport_dev;
-	struct cxl_register_map comp_map;
+	struct cxl_register_map reg_map;
 	int port_id;
 	struct cxl_rcrb_info rcrb;
 	bool rch;
