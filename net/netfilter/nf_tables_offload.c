ff27ff8d794e29ae14ff4fe086fe56838db484f8
Joao Moreira <joao.moreira@intel.com>
2023-09-27 09:47:15 -0700

    Make num_actions unsigned
    Currently, in nft_flow_rule_create function, num_actions is a signed
    integer. Yet, it is processed within a loop which increments its
    value. To prevent an overflow from occurring, make it unsigned and
    also check if it reaches 256 when being incremented.
    
    Accordingly to discussions around v2, 256 actions are more than enough
    for the frontend actions.
    
    After checking with maintainers, it was mentioned that front-end will
    cap the num_actions value and that it is not possible to reach such
    condition for an overflow. Yet, for correctness, it is still better to
    fix this.
    
    This issue was observed by the commit author while reviewing a write-up
    regarding a CVE within the same subsystem [1].
    
    1 - https://nickgregory.me/post/2022/03/12/cve-2022-25636/
    
    Signed-off-by: Joao Moreira <joao.moreira@intel.com>

--- net/netfilter/nf_tables_offload.c
+++ net/netfilter/nf_tables_offload.c
@@ -90,7 +90,8 @@ struct nft_flow_rule *nft_flow_rule_create(struct net *net,
 {
 	struct nft_offload_ctx *ctx;
 	struct nft_flow_rule *flow;
-	int num_actions = 0, err;
+	unsigned int num_actions = 0;
+	int err;
 	struct nft_expr *expr;
 
 	expr = nft_expr_first(rule);
@@ -98,6 +99,10 @@ struct nft_flow_rule *nft_flow_rule_create(struct net *net,
 		if (expr->ops->offload_action &&
 		    expr->ops->offload_action(expr))
 			num_actions++;
+
+		/* 2^8 is enough for frontend actions, avoid overflow */
+		if (num_actions == 256)
+			return ERR_PTR(-ENOMEM);
 
 		expr = nft_expr_next(expr);
 	}
