aab46f3fd6cd6442f10c6a48facfc194c2048ee0
Stephen Rothwell <sfr@canb.auug.org.au>
2023-09-27 22:25:58 +0200

    fixup for "power: Explicitly include correct DT includes"
    interacting with commit
    
      12cc585f36b8 ("power: supply: rt5033_charger: Add cable detection and USB OTG supply")
    
    Signed-off-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Signed-off-by: Jakob Hauser <jahau@rocketmail.com>

--- drivers/power/supply/rt5033_charger.c
+++ drivers/power/supply/rt5033_charger.c
@@ -8,6 +8,7 @@
 
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
+#include <linux/of.h>
 #include <linux/platform_device.h>
 #include <linux/power_supply.h>
 #include <linux/regmap.h>
