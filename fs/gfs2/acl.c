74d5e3dd1ea3f5a39716178800ccf9f94a1c603b
Deepak R Varma <drv@mailo.com>
2023-09-28 23:37:42 +0530

    gfs2: fix 'passing zero to ERR_PTR()' warning
    Resolve the following Smatch static checker warning:
    	fs/gfs2/acl.c:54 __gfs2_get_acl() warn: passing zero to 'ERR_PTR'
    
    by returning NULL when an extended attribute length is zero, instead of
    passing on zero to the ERR_PTR().
    
    Signed-off-by: Deepak R Varma <drv@mailo.com>

--- fs/gfs2/acl.c
+++ fs/gfs2/acl.c
@@ -50,8 +50,11 @@ static struct posix_acl *__gfs2_get_acl(struct inode *inode, int type)
 
 	name = gfs2_acl_name(type);
 	len = gfs2_xattr_acl_get(ip, name, &data);
-	if (len <= 0)
+	if (len == 0)
+		return NULL;
+	if (len < 0)
 		return ERR_PTR(len);
+
 	acl = posix_acl_from_xattr(&init_user_ns, data, len);
 	kfree(data);
 	return acl;
