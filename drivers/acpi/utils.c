3605e12f5fd4ebb8ea1bbcbd5c76c6adf43adf17
Jonathan Bergh <bergh.jonathan@gmail.com>
2023-09-28 23:48:07 +0200

    drivers: acpi: Fix label indentation and leading comma formatting issues
    Fixed the following formatting issues flagged by checkpatch
    in utils.c:
     * Remove incorrect indentation before goto label
     * Remove whitespace ahead of comma in parameter list
    
    Signed-off-by: Jonathan Bergh <bergh.jonathan@gmail.com>

--- drivers/acpi/utils.c
+++ drivers/acpi/utils.c
@@ -399,7 +399,7 @@ acpi_evaluate_reference(acpi_handle handle,
 		acpi_handle_debug(list->handles[i], "Found in reference list\n");
 	}
 
-      end:
+end:
 	if (ACPI_FAILURE(status)) {
 		list->count = 0;
 		//kfree(list->handles);
@@ -523,7 +523,7 @@ acpi_handle_printk(const char *level, acpi_handle handle, const char *fmt, ...)
 	vaf.va = &args;
 
 	path = acpi_handle_path(handle);
-	printk("%sACPI: %s: %pV", level, path ? path : "<n/a>" , &vaf);
+	printk("%sACPI: %s: %pV", level, path ? path : "<n/a>", &vaf);
 
 	va_end(args);
 	kfree(path);
