56afbbff1a2a1dfa81d81942053a9dac08c4c8f4
Prathap Kumar Valsan <prathap.kumar.valsan@intel.com>
2023-09-29 08:23:14 -0700

    drm/i915: Define and use GuC and CTB TLB invalidation routines
    The GuC firmware had defined the interface for Translation Look-Aside
    Buffer (TLB) invalidation.  We should use this interface when
    invalidating the engine and GuC TLBs.
    Add additional functionality to intel_gt_invalidate_tlb, invalidating
    the GuC TLBs and falling back to GT invalidation when the GuC is
    disabled.
    The invalidation is done by sending a request directly to the GuC
    tlb_lookup that invalidates the table.  The invalidation is submitted as
    a wait request and is performed in the CT event handler.  This means we
    cannot perform this TLB invalidation path if the CT is not enabled.
    If the request isn't fulfilled in two seconds, this would constitute
    an error in the invalidation as that would constitute either a lost
    request or a severe GuC overload.
    
    With this new invalidation routine, we can perform GuC-based GGTT
    invalidations.  We should only do this when GuC is enabled and fall
    back to the original path when GuC is disabled to prevent concurrent
    issuance between GuC and KMD.
    
    Signed-off-by: Prathap Kumar Valsan <prathap.kumar.valsan@intel.com>
    Signed-off-by: Bruce Chang <yu.bruce.chang@intel.com>
    Signed-off-by: Chris Wilson <chris.p.wilson@intel.com>
    Signed-off-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
    Signed-off-by: Jonathan Cavitt <jonathan.cavitt@intel.com>
    Signed-off-by: Aravind Iddamsetty <aravind.iddamsetty@intel.com>
    Signed-off-by: Fei Yang <fei.yang@intel.com>
    CC: Andi Shyti <andi.shyti@linux.intel.com>

--- drivers/gpu/drm/i915/gt/uc/intel_guc.h
+++ drivers/gpu/drm/i915/gt/uc/intel_guc.h
@@ -79,6 +79,18 @@ struct intel_guc {
 	 */
 	atomic_t outstanding_submission_g2h;
 
+	/** @tlb_lookup: xarray to store all pending TLB invalidation requests */
+	struct xarray tlb_lookup;
+
+	/**
+	 * @serial_slot: id to the initial waiter created in tlb_lookup,
+	 * which is used only when failed to allocate new waiter.
+	 */
+	u32 serial_slot;
+
+	/** @next_seqno: the next id (sequence no.) to allocate. */
+	u32 next_seqno;
+
 	/** @interrupts: pointers to GuC interrupt-managing functions. */
 	struct {
 		bool enabled;
@@ -296,6 +308,11 @@ struct intel_guc {
 #define MAKE_GUC_VER_STRUCT(ver)	MAKE_GUC_VER((ver).major, (ver).minor, (ver).patch)
 #define GUC_SUBMIT_VER(guc)		MAKE_GUC_VER_STRUCT((guc)->submission_version)
 
+struct intel_guc_tlb_wait {
+	struct wait_queue_head wq;
+	u8 status;
+} __aligned(4);
+
 static inline struct intel_guc *log_to_guc(struct intel_guc_log *log)
 {
 	return container_of(log, struct intel_guc, log);
@@ -412,6 +429,11 @@ int intel_guc_allocate_and_map_vma(struct intel_guc *guc, u32 size,
 				   struct i915_vma **out_vma, void **out_vaddr);
 int intel_guc_self_cfg32(struct intel_guc *guc, u16 key, u32 value);
 int intel_guc_self_cfg64(struct intel_guc *guc, u16 key, u64 value);
+
+int intel_guc_invalidate_tlb_full(struct intel_guc *guc);
+int intel_guc_invalidate_tlb(struct intel_guc *guc);
+int intel_guc_tlb_invalidation_done(struct intel_guc *guc, const u32 *hxg,
+				    u32 size);
 
 static inline bool intel_guc_is_supported(struct intel_guc *guc)
 {
