8f19e26377200ccfdd984291b83dbd97c7096544
Basavaraj Natikar <Basavaraj.Natikar@amd.com>
2023-09-30 14:07:15 +0530

    platform/x86/amd/pmf: Add PMF-AMDSFH interface for ALS
    AMDSFH has information about the Ambient light via the Ambient
    Light Sensor (ALS) which is part of the AMD sensor fusion hub.
    Add PMF and AMDSFH interface to get this information.
    
    make amd_sfh_float_to_int() as non-static function so that this can
    be called outside of the current file.
    
    Co-developed-by: Shyam Sundar S K <Shyam-sundar.S-k@amd.com>
    Signed-off-by: Shyam Sundar S K <Shyam-sundar.S-k@amd.com>
    Signed-off-by: Basavaraj Natikar <Basavaraj.Natikar@amd.com>

--- drivers/hid/amd-sfh-hid/sfh1_1/amd_sfh_desc.c
+++ drivers/hid/amd-sfh-hid/sfh1_1/amd_sfh_desc.c
@@ -132,7 +132,7 @@ static void get_common_inputs(struct common_input_property *common, int report_i
 	common->event_type = HID_USAGE_SENSOR_EVENT_DATA_UPDATED_ENUM;
 }
 
-static int amd_sfh_float_to_int(u32 flt32_val)
+int amd_sfh_float_to_int(u32 flt32_val)
 {
 	int fraction, shift, mantissa, sign, exp, zeropre;
 
--- drivers/hid/amd-sfh-hid/sfh1_1/amd_sfh_interface.h
+++ drivers/hid/amd-sfh-hid/sfh1_1/amd_sfh_interface.h
@@ -149,6 +149,7 @@ struct hpd_status {
 	};
 };
 
+int amd_sfh_float_to_int(u32 flt32_val);
 void sfh_interface_init(struct amd_mp2_dev *mp2);
 void amd_sfh1_1_set_desc_ops(struct amd_mp2_ops *mp2_ops);
 #endif
