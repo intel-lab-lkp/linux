7a8bdacb2ca2f581e75e106ccc399b24ee217c81
Dan Carpenter <dan.carpenter@linaro.org>
2023-10-02 10:03:05 +0300

    clk: imx: imx8: Fix error code in probe
    Preserve the error code from devm_clk_hw_register_mux_parent_data_table()
    instead of returning success.
    
    Fixes: d3a0946d7ac9 ("clk: imx: imx8: add audio clock mux driver")
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

--- drivers/clk/imx/clk-imx8-acm.c
+++ drivers/clk/imx/clk-imx8-acm.c
@@ -372,6 +372,7 @@ static int imx8_acm_clk_probe(struct platform_device *pdev)
 										0, NULL, NULL);
 		if (IS_ERR(hws[sels[i].clkid])) {
 			pm_runtime_disable(&pdev->dev);
+			ret = PTR_ERR(hws[sels[i].clkid]);
 			goto err_clk_register;
 		}
 	}
