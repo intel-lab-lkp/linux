9658ccc295c712b7a5c7b64631d99cf4287acc4a
Michael Pratt <mcpratt@pm.me>
2023-10-02 23:40:07 +0000

    mac_pton: support MAC addresses without delimiters
    Some network hardware vendors may do something unique
    when storing the MAC address into hardware in ASCII,
    like leaving out delimiters in order to avoid
    using more than a single 16-byte logical addressing line.
    
    Allow parsing of MAC addresses without a delimiter.
    
    Signed-off-by: Michael Pratt <mcpratt@pm.me>

--- lib/net_utils.c
+++ lib/net_utils.c
@@ -7,8 +7,13 @@
 
 bool mac_pton(const char *s, u8 *mac)
 {
+	size_t minlen = 2 * ETH_ALEN;
 	size_t maxlen = 3 * ETH_ALEN - 1;
 	int i;
+
+	/* AABBCCDDEEFF */
+	if (strnlen(s, maxlen) == minlen)
+		goto no_delim;
 
 	/* XX:XX:XX:XX:XX:XX */
 	if (strnlen(s, maxlen) < maxlen)
@@ -23,6 +28,16 @@ bool mac_pton(const char *s, u8 *mac)
 	}
 	for (i = 0; i < ETH_ALEN; i++) {
 		mac[i] = (hex_to_bin(s[i * 3]) << 4) | hex_to_bin(s[i * 3 + 1]);
+	}
+	return true;
+
+no_delim:
+	for (i = 0; i < minlen; i++) {
+		if (!isxdigit(s[i]))
+			return false;
+	}
+	for (i = 0; i < ETH_ALEN; i++) {
+		mac[i] = (hex_to_bin(s[i * 2]) << 4) | hex_to_bin(s[i * 2 + 1]);
 	}
 	return true;
 }
