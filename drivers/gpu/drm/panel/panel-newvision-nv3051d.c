b56f534c002506627343a5bf5a58ad5b2e89f3bc
Chris Morgan <macromorgan@hotmail.com>
2023-10-02 14:30:16 -0500

    drm/panel: nv3051d: Add Support for Anbernic 351V
    Add support for the Anbernic 351V. Just like the 353 series the
    underlying vendor is unknown/unmarked (at least not visible in a
    non-destructive manner). The panel had slightly different init
    sequences and timings in the BSP kernel, but works fine with the
    same ones used in the existing driver. The panel will not work without
    the inclusion of the MIPI_DSI_CLOCK_NON_CONTINUOUS flag, and this flag
    prevents the 353 series from working correctly, so a new compatible
    string is added.
    
    Tested colors and timings using modetest and all seem to work identical
    to the 353 otherwise.
    
    Signed-off-by: Chris Morgan <macromorgan@hotmail.com>

--- drivers/gpu/drm/panel/panel-newvision-nv3051d.c
+++ drivers/gpu/drm/panel/panel-newvision-nv3051d.c
@@ -354,6 +354,7 @@ static const struct drm_panel_funcs panel_nv3051d_funcs = {
 static int panel_nv3051d_probe(struct mipi_dsi_device *dsi)
 {
 	struct device *dev = &dsi->dev;
+	struct device_node *np = dev->of_node;
 	struct panel_nv3051d *ctx;
 	int ret;
 
@@ -387,6 +388,13 @@ static int panel_nv3051d_probe(struct mipi_dsi_device *dsi)
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_BURST |
 			  MIPI_DSI_MODE_LPM | MIPI_DSI_MODE_NO_EOT_PACKET;
+
+	/*
+	 * The panel in the RG351V is identical to the 353P, except it
+	 * requires MIPI_DSI_CLOCK_NON_CONTINUOUS to operate correctly.
+	 */
+	if (of_device_is_compatible(np, "anbernic,rg351v-panel"))
+		dsi->mode_flags |= MIPI_DSI_CLOCK_NON_CONTINUOUS;
 
 	drm_panel_init(&ctx->panel, &dsi->dev, &panel_nv3051d_funcs,
 		       DRM_MODE_CONNECTOR_DSI);
