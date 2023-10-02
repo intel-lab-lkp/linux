5f9ede7b3679308ba605940e28c17420c7471f40
Fei Yang <fei.yang@intel.com>
2023-10-02 10:24:17 -0700

    drm/i915: No TLB invalidation on wedged or suspended GT
    In case of GT is suspended or wedged, don't allow submission of new TLB
    invalidation request and cancel all pending requests.  The TLB entries
    will be invalidated either during GuC reload or on system resume.
    
    Signed-off-by: Fei Yang <fei.yang@intel.com>
    Signed-off-by: Jonathan Cavitt <jonathan.cavitt@intel.com>
    CC: John Harrison <john.c.harrison@intel.com>

--- drivers/gpu/drm/i915/i915_driver.c
+++ drivers/gpu/drm/i915/i915_driver.c
@@ -72,6 +72,7 @@
 #include "gt/intel_gt.h"
 #include "gt/intel_gt_pm.h"
 #include "gt/intel_rc6.h"
+#include "gt/uc/intel_guc.h"
 
 #include "pxp/intel_pxp.h"
 #include "pxp/intel_pxp_debugfs.h"
@@ -1092,6 +1093,9 @@ static int i915_drm_suspend(struct drm_device *dev)
 	intel_dp_mst_suspend(dev_priv);
 
 	intel_runtime_pm_disable_interrupts(dev_priv);
+
+	wake_up_all_tlb_invalidate(&to_gt(dev_priv)->uc.guc);
+
 	intel_hpd_cancel_work(dev_priv);
 
 	intel_suspend_encoders(dev_priv);
@@ -1262,6 +1266,11 @@ static int i915_drm_resume(struct drm_device *dev)
 	intel_power_domains_enable(dev_priv);
 
 	intel_gvt_resume(dev_priv);
+
+	if (INTEL_GUC_SUPPORTS_TLB_INVALIDATION(&to_gt(dev_priv)->uc.guc)) {
+		intel_guc_invalidate_tlb_full(&to_gt(dev_priv)->uc.guc);
+		intel_guc_invalidate_tlb(&to_gt(dev_priv)->uc.guc);
+	}
 
 	enable_rpm_wakeref_asserts(&dev_priv->runtime_pm);
 
