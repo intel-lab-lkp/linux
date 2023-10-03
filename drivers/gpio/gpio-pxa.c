b0857ad3c2bd4b86868fdf2438a37361d901b12c
Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
2023-10-03 16:50:48 +0200

    gpio: pxa: use new pinctrl GPIO helpers
    Replace the pinctrl helpers taking the global GPIO number as argument
    with the improved variants that instead take a pointer to the GPIO chip
    and the controller-relative offset.
    
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- drivers/gpio/gpio-pxa.c
+++ drivers/gpio/gpio-pxa.c
@@ -259,7 +259,7 @@ static int pxa_gpio_direction_input(struct gpio_chip *chip, unsigned offset)
 	int ret;
 
 	if (pxa_gpio_has_pinctrl()) {
-		ret = pinctrl_gpio_direction_input(chip->base + offset);
+		ret = pinctrl_gpio_direction_input_new(chip, offset);
 		if (ret)
 			return ret;
 	}
@@ -288,7 +288,7 @@ static int pxa_gpio_direction_output(struct gpio_chip *chip,
 	writel_relaxed(mask, base + (value ? GPSR_OFFSET : GPCR_OFFSET));
 
 	if (pxa_gpio_has_pinctrl()) {
-		ret = pinctrl_gpio_direction_output(chip->base + offset);
+		ret = pinctrl_gpio_direction_output_new(chip, offset);
 		if (ret)
 			return ret;
 	}
