9094f9e816ee68cdc5ef51a128e052830cdd0acc
Breno Leitao <leitao@debian.org>
2023-10-04 10:11:26 -0700

    selftests/mm: Add a new test for madv and hugetlb
    Create a selftest that exercises the conflict between page faults and
    madvise(MADV_DONTNEED) in the same huge page. Do it by running two
    threads that touches the huge page and madvise(MADV_DONTNEED) at the same
    time.
    
    In case of a SIGBUS coming at pagefault, the test should fail, since we
    hit the bug.
    
    The test doesn't have a signal handler, and if it fails, it fails like
    the following
    
      ----------------------------------
      running ./hugetlb_fault_after_madv
      ----------------------------------
      ./run_vmtests.sh: line 186: 595563 Bus error    (core dumped) "$@"
      [FAIL]
    
    This selftest goes together with the fix of the bug[1] itself.
    
    [1] https://lore.kernel.org/all/20231001005659.2185316-1-riel@surriel.com/#r
    
    Signed-off-by: Breno Leitao <leitao@debian.org>

---
+++ tools/testing/selftests/mm/hugetlb_fault_after_madv.c
@@ -0,0 +1,82 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include "vm_util.h"
+
+#define MMAP_SIZE (1 << 21)
+#define INLOOP_ITER 100
+
+char *huge_ptr;
+
+/* Touch the memory while it is being madvised() */
+void *touch(void *unused)
+{
+	char *ptr = (char *)huge_ptr;
+
+	if (!ptr) {
+		fprintf(stderr, "Failed to allocate memory\n");
+		perror("");
+	}
+
+	for (int i = 0; i < INLOOP_ITER; i++)
+		ptr[0] = '.';
+
+	return NULL;
+}
+
+void *madv(void *unused)
+{
+	usleep(rand() % 10);
+	if (!huge_ptr)
+		return NULL;
+
+	for (int i = 0; i < INLOOP_ITER; i++)
+		madvise(huge_ptr, MMAP_SIZE, MADV_DONTNEED);
+
+	return NULL;
+}
+
+int main(void)
+{
+	unsigned long free_hugepages;
+	pthread_t thread1, thread2;
+	/*
+	 * On kernel 6.4, we are able to reproduce the problem with ~1000
+	 * interactions
+	 */
+	int max = 10000;
+
+	srand(getpid());
+
+	free_hugepages = get_free_hugepages();
+	if (free_hugepages != 1) {
+		fprintf(stderr,
+			"This test needs one and only one page to execute. Got %lu\n",
+			free_hugepages);
+		exit(1);
+	}
+
+	while (max--) {
+		huge_ptr = mmap(NULL, MMAP_SIZE, PROT_READ | PROT_WRITE,
+				MAP_PRIVATE | MAP_ANONYMOUS | MAP_HUGETLB, -1, 0);
+
+		if ((unsigned long)huge_ptr == -1) {
+			perror("Failed to allocate\n");
+			continue;
+		}
+
+		pthread_create(&thread1, NULL, madv, NULL);
+		pthread_create(&thread2, NULL, touch, NULL);
+
+		pthread_join(thread1, NULL);
+		pthread_join(thread2, NULL);
+		munmap(huge_ptr, MMAP_SIZE);
+	}
+
+	return 0;
+}
