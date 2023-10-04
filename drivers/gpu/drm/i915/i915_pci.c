f659482f4f94296ef96b3d87212e65d5405319b1
Jonathan Cavitt <jonathan.cavitt@intel.com>
2023-10-04 15:07:39 -0700

    drm/i915: Enable GuC TLB invalidations for MTL
    Enable GuC TLB invalidations for MTL.  Though more platforms than just
    MTL support GuC TLB invalidations, MTL is presently the only platform
    that requires it for any purpose, so only enable it there for now to
    minimize cross-platform impact.
    
    Signed-off-by: Jonathan Cavitt <jonathan.cavitt@intel.com>

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -837,6 +837,7 @@ static const struct intel_device_info mtl_info = {
 	.memory_regions = REGION_SMEM | REGION_STOLEN_LMEM,
 	.platform_engine_mask = BIT(RCS0) | BIT(BCS0) | BIT(CCS0),
 	.require_force_probe = 1,
+	.has_guc_tlb_invalidation = 1,
 	MTL_CACHELEVEL,
 };
 
