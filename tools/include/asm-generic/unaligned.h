a38b5c7c6c89395b7e8546dfc5213763a6957867
Adrian Hunter <adrian.hunter@intel.com>
2023-10-05 22:04:47 +0300

    perf tools: Add get_unaligned_leNN()
    Add get_unaligned_le16(), get_unaligned_le32 and get_unaligned_le64, same
    as include/asm-generic/unaligned.h.
    
    Use diagnostic pragmas to ignore -Wpacked used by perf build.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- tools/include/asm-generic/unaligned.h
+++ tools/include/asm-generic/unaligned.h
@@ -6,6 +6,9 @@
 #ifndef __TOOLS_LINUX_ASM_GENERIC_UNALIGNED_H
 #define __TOOLS_LINUX_ASM_GENERIC_UNALIGNED_H
 
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpacked"
+
 #define __get_unaligned_t(type, ptr) ({						\
 	const struct { type x; } __packed *__pptr = (typeof(__pptr))(ptr);	\
 	__pptr->x;								\
@@ -18,6 +21,23 @@
 
 #define get_unaligned(ptr)	__get_unaligned_t(typeof(*(ptr)), (ptr))
 #define put_unaligned(val, ptr) __put_unaligned_t(typeof(*(ptr)), (val), (ptr))
+
+static inline u16 get_unaligned_le16(const void *p)
+{
+	return le16_to_cpu(__get_unaligned_t(__le16, p));
+}
+
+static inline u32 get_unaligned_le32(const void *p)
+{
+	return le32_to_cpu(__get_unaligned_t(__le32, p));
+}
+
+static inline u64 get_unaligned_le64(const void *p)
+{
+	return le64_to_cpu(__get_unaligned_t(__le64, p));
+}
+
+#pragma GCC diagnostic pop
 
 #endif /* __TOOLS_LINUX_ASM_GENERIC_UNALIGNED_H */
 
