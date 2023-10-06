b316ee8039a097babc29da45f889f22031cbd6e4
Kuogee Hsieh <quic_khsieh@quicinc.com>
2023-10-06 14:38:32 -0700

    drm/msm/dp: tie dp_display_irq_handler() with dp driver
    Currently the dp_display_request_irq() is executed at msm_dp_modeset_init()
    which ties irq registering to the DPU device's life cycle, while depending on
    resources that are released as the DP device is torn down. Move register DP
    driver irq handler to dp_display_probe() to have dp_display_irq_handler()
    IRQ tied with DP device. In addition, use platform_get_irq() to retrieve irq
    number from platform device directly.
    
    Changes in v5:
    -- reworded commit text as review comments at change #4
    -- tear down component if failed at dp_display_request_irq()
    
    Changes in v4:
    -- delete dp->irq check at dp_display_request_irq()
    
    Changes in v3:
    -- move calling dp_display_irq_handler() to probe
    
    Signed-off-by: Kuogee Hsieh <quic_khsieh@quicinc.com>
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -1193,26 +1193,18 @@ static irqreturn_t dp_display_irq_handler(int irq, void *dev_id)
 	return ret;
 }
 
-int dp_display_request_irq(struct msm_dp *dp_display)
+static int dp_display_request_irq(struct dp_display_private *dp)
 {
 	int rc = 0;
-	struct dp_display_private *dp;
+	struct device *dev = &dp->pdev->dev;
 
-	if (!dp_display) {
-		DRM_ERROR("invalid input\n");
-		return -EINVAL;
-	}
-
-	dp = container_of(dp_display, struct dp_display_private, dp_display);
-
-	dp->irq = irq_of_parse_and_map(dp->pdev->dev.of_node, 0);
+	dp->irq = platform_get_irq(dp->pdev, 0);
 	if (!dp->irq) {
 		DRM_ERROR("failed to get irq\n");
 		return -EINVAL;
 	}
 
-	rc = devm_request_irq(dp_display->drm_dev->dev, dp->irq,
-			dp_display_irq_handler,
+	rc = devm_request_irq(dev, dp->irq, dp_display_irq_handler,
 			IRQF_TRIGGER_HIGH, "dp_display_isr", dp);
 	if (rc < 0) {
 		DRM_ERROR("failed to request IRQ%u: %d\n",
@@ -1287,12 +1279,20 @@ static int dp_display_probe(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, &dp->dp_display);
 
+	rc = dp_display_request_irq(dp);
+	if (rc)
+		goto err;
+
 	rc = component_add(&pdev->dev, &dp_display_comp_ops);
 	if (rc) {
 		DRM_ERROR("component add failed, rc=%d\n", rc);
-		dp_display_deinit_sub_modules(dp);
+		goto err;
 	}
 
+	return rc;
+
+err:
+	dp_display_deinit_sub_modules(dp);
 	return rc;
 }
 
@@ -1548,12 +1548,6 @@ int msm_dp_modeset_init(struct msm_dp *dp_display, struct drm_device *dev,
 	dp_display->drm_dev = dev;
 
 	dp_priv = container_of(dp_display, struct dp_display_private, dp_display);
-
-	ret = dp_display_request_irq(dp_display);
-	if (ret) {
-		DRM_ERROR("request_irq failed, ret=%d\n", ret);
-		return ret;
-	}
 
 	ret = dp_display_get_next_bridge(dp_display);
 	if (ret)
