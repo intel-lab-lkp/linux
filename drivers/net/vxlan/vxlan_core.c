695502b0e0da34a333fc0861edd135c3855163d7
Alce Lafranque <alce@lafranque.net>
2023-10-07 09:26:24 -0500

    vxlan: add support for flowlabel inherit
    By default, VXLAN encapsulation over IPv6 sets the flow label to 0, with
    an option for a fixed value. This commits add the ability to inherit the
    flow label from the inner packet, like for other tunnel implementations.
    This enables devices using only L3 headers for ECMP to correctly balance
    VXLAN-encapsulated IPv6 packets.
    
    ```
    $ ./ip/ip link add dummy1 type dummy
    $ ./ip/ip addr add 2001:db8::2/64 dev dummy1
    $ ./ip/ip link set up dev dummy1
    $ ./ip/ip link add vxlan1 type vxlan id 100 flowlabel inherit remote 2001:db8::1 local 2001:db8::2
    $ ./ip/ip link set up dev vxlan1
    $ ./ip/ip addr add 2001:db8:1::2/64 dev vxlan1
    $ ./ip/ip link set arp off dev vxlan1
    $ ping -q 2001:db8:1::1 &
    $ tshark -d udp.port==8472,vxlan -Vpni dummy1 -c1
    [...]
    Internet Protocol Version 6, Src: 2001:db8::2, Dst: 2001:db8::1
        0110 .... = Version: 6
        .... 0000 0000 .... .... .... .... .... = Traffic Class: 0x00 (DSCP: CS0, ECN: Not-ECT)
            .... 0000 00.. .... .... .... .... .... = Differentiated Services Codepoint: Default (0)
            .... .... ..00 .... .... .... .... .... = Explicit Congestion Notification: Not ECN-Capable Transport (0)
        .... 1011 0001 1010 1111 1011 = Flow Label: 0xb1afb
    [...]
    Virtual eXtensible Local Area Network
        Flags: 0x0800, VXLAN Network ID (VNI)
        Group Policy ID: 0
        VXLAN Network Identifier (VNI): 100
    [...]
    Internet Protocol Version 6, Src: 2001:db8:1::2, Dst: 2001:db8:1::1
        0110 .... = Version: 6
        .... 0000 0000 .... .... .... .... .... = Traffic Class: 0x00 (DSCP: CS0, ECN: Not-ECT)
            .... 0000 00.. .... .... .... .... .... = Differentiated Services Codepoint: Default (0)
            .... .... ..00 .... .... .... .... .... = Explicit Congestion Notification: Not ECN-Capable Transport (0)
        .... 1011 0001 1010 1111 1011 = Flow Label: 0xb1afb
    ```
    
    Signed-off-by: Alce Lafranque <alce@lafranque.net>
    Co-developed-by: Vincent Bernat <vincent@bernat.ch>
    Signed-off-by: Vincent Bernat <vincent@bernat.ch>

--- drivers/net/vxlan/vxlan_core.c
+++ drivers/net/vxlan/vxlan_core.c
@@ -2475,7 +2475,14 @@ void vxlan_xmit_one(struct sk_buff *skb, struct net_device *dev,
 		else
 			udp_sum = !(flags & VXLAN_F_UDP_ZERO_CSUM6_TX);
 #if IS_ENABLED(CONFIG_IPV6)
-		label = vxlan->cfg.label;
+		switch (vxlan->cfg.label_behavior) {
+		case VXLAN_LABEL_FIXED:
+			label = vxlan->cfg.label;
+			break;
+		case VXLAN_LABEL_INHERIT:
+			label = ip_tunnel_get_flowlabel(old_iph, skb);
+			break;
+		}
 #endif
 	} else {
 		if (!info) {
@@ -3286,6 +3293,7 @@ static const struct nla_policy vxlan_policy[IFLA_VXLAN_MAX + 1] = {
 	[IFLA_VXLAN_DF]		= { .type = NLA_U8 },
 	[IFLA_VXLAN_VNIFILTER]	= { .type = NLA_U8 },
 	[IFLA_VXLAN_LOCALBYPASS]	= NLA_POLICY_MAX(NLA_U8, 1),
+	[IFLA_VXLAN_LABEL_BEHAVIOR]	= NLA_POLICY_MAX(NLA_U8, VXLAN_LABEL_MAX),
 };
 
 static int vxlan_validate(struct nlattr *tb[], struct nlattr *data[],
@@ -4003,6 +4011,9 @@ static int vxlan_nl2conf(struct nlattr *tb[], struct nlattr *data[],
 		conf->label = nla_get_be32(data[IFLA_VXLAN_LABEL]) &
 			     IPV6_FLOWLABEL_MASK;
 
+	if (data[IFLA_VXLAN_LABEL_BEHAVIOR])
+		conf->label_behavior = nla_get_u8(data[IFLA_VXLAN_LABEL_BEHAVIOR]);
+
 	if (data[IFLA_VXLAN_LEARNING]) {
 		err = vxlan_nl2flag(conf, data, IFLA_VXLAN_LEARNING,
 				    VXLAN_F_LEARN, changelink, true,
@@ -4315,6 +4326,7 @@ static size_t vxlan_get_size(const struct net_device *dev)
 		nla_total_size(sizeof(__u8)) +	/* IFLA_VXLAN_TOS */
 		nla_total_size(sizeof(__u8)) +	/* IFLA_VXLAN_DF */
 		nla_total_size(sizeof(__be32)) + /* IFLA_VXLAN_LABEL */
+		nla_total_size(sizeof(__u8)) +  /* IFLA_VXLAN_LABEL_BEHAVIOR */
 		nla_total_size(sizeof(__u8)) +	/* IFLA_VXLAN_LEARNING */
 		nla_total_size(sizeof(__u8)) +	/* IFLA_VXLAN_PROXY */
 		nla_total_size(sizeof(__u8)) +	/* IFLA_VXLAN_RSC */
@@ -4387,6 +4399,7 @@ static int vxlan_fill_info(struct sk_buff *skb, const struct net_device *dev)
 	    nla_put_u8(skb, IFLA_VXLAN_TOS, vxlan->cfg.tos) ||
 	    nla_put_u8(skb, IFLA_VXLAN_DF, vxlan->cfg.df) ||
 	    nla_put_be32(skb, IFLA_VXLAN_LABEL, vxlan->cfg.label) ||
+	    nla_put_u8(skb, IFLA_VXLAN_LABEL_BEHAVIOR, vxlan->cfg.label_behavior) ||
 	    nla_put_u8(skb, IFLA_VXLAN_LEARNING,
 		       !!(vxlan->cfg.flags & VXLAN_F_LEARN)) ||
 	    nla_put_u8(skb, IFLA_VXLAN_PROXY,
