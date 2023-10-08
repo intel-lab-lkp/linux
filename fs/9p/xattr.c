8ad75f7b4d581412a636a53ab793b27af491fdbf
Su Hui <suhui@nfschina.com>
2023-10-08 14:01:39 +0800

    fs/9p/xattr.c: avoid format-overflow warning
    with gcc and W=1 option, there's a warning like this:
    
    In file included from fs/9p/xattr.c:12:
    In function ‘v9fs_xattr_get’,
        inlined from ‘v9fs_listxattr’ at fs/9p/xattr.c:142:9:
    include/net/9p/9p.h:55:2: error: ‘%s’ directive argument is null
    [-Werror=format-overflow=]
       55 |  _p9_debug(level, __func__, fmt, ##__VA_ARGS__)
          |  ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    
    use "" replace NULL to silence this warning.
    
    Signed-off-by: Su Hui <suhui@nfschina.com>

--- fs/9p/xattr.c
+++ fs/9p/xattr.c
@@ -139,7 +139,7 @@ int v9fs_fid_xattr_set(struct p9_fid *fid, const char *name,
 
 ssize_t v9fs_listxattr(struct dentry *dentry, char *buffer, size_t buffer_size)
 {
-	return v9fs_xattr_get(dentry, NULL, buffer, buffer_size);
+	return v9fs_xattr_get(dentry, "", buffer, buffer_size);
 }
 
 static int v9fs_xattr_handler_get(const struct xattr_handler *handler,
