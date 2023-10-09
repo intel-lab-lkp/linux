9e8026eb81bc83252e355a3bf217d19e6e6f9303
Michal Wilczynski <michal.wilczynski@intel.com>
2023-10-09 05:07:09 -0400

    ice: Enable switching default Tx scheduler topology
    Introduce support for Tx scheduler topology change, based on user
    selection, from default 9-layer to 5-layer.
    Change requires NVM (version 3.20 or newer) and DDP package (OS Package
    1.3.30 or newer - available for over a year in linux-firmware, since
    commit aed71f296637 in linux-firmware ("ice: Update package to 1.3.30.0"))
    https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/commit/?id=aed71f296637
    
    Enable 5-layer topology switch in init path of the driver. To accomplish
    that upload of the DDP package needs to be delayed, until change in Tx
    topology is finished. To trigger the Tx change user selection should be
    changed in NVM using devlink. Then the platform should be rebooted.
    
    Signed-off-by: Michal Wilczynski <michal.wilczynski@intel.com>
    Reviewed-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>
    Co-developed-by: Mateusz Polchlopek <mateusz.polchlopek@intel.com>
    Signed-off-by: Mateusz Polchlopek <mateusz.polchlopek@intel.com>

--- drivers/net/ethernet/intel/ice/ice_ddp.h
+++ drivers/net/ethernet/intel/ice/ice_ddp.h
@@ -432,6 +432,6 @@ u16 ice_pkg_buf_get_active_sections(struct ice_buf_build *bld);
 void *ice_pkg_enum_section(struct ice_seg *ice_seg, struct ice_pkg_enum *state,
 			   u32 sect_type);
 
-int ice_cfg_tx_topo(struct ice_hw *hw, u8 *buf, u32 len);
+int ice_cfg_tx_topo(struct ice_hw *hw, const u8 *buf, u32 len);
 
 #endif
