c4529c5cf03e38cddcc1fd3f5f9d5150329274ae
Prathap Kumar Valsan <prathap.kumar.valsan@intel.com>
2023-10-10 11:46:34 -0700

    drm/i915: Define and use GuC and CTB TLB invalidation routines
    The GuC firmware had defined the interface for Translation Look-Aside
    Buffer (TLB) invalidation.  We should use this interface when
    invalidating the engine and GuC TLBs.
    Add additional functionality to intel_gt_invalidate_tlb, invalidating
    the GuC TLBs and falling back to GT invalidation when the GuC is
    disabled.
    The invalidation is done by sending a request directly to the GuC
    tlb_lookup that invalidates the table.  The invalidation is submitted as
    a wait request and is performed in the CT event handler.  This means we
    cannot perform this TLB invalidation path if the CT is not enabled.
    If the request isn't fulfilled in two seconds, this would constitute
    an error in the invalidation as that would constitute either a lost
    request or a severe GuC overload.
    
    With this new invalidation routine, we can perform GuC-based GGTT
    invalidations.  GuC-based GGTT invalidation is incompatible with
    MMIO invalidation so we should not perform MMIO invalidation when
    GuC-based GGTT invalidation is expected.
    
    The additional complexity incurred in this patch will be necessary for
    range-based tlb invalidations, which will be platformed in the future.
    
    Signed-off-by: Prathap Kumar Valsan <prathap.kumar.valsan@intel.com>
    Signed-off-by: Bruce Chang <yu.bruce.chang@intel.com>
    Signed-off-by: Chris Wilson <chris.p.wilson@intel.com>
    Signed-off-by: Umesh Nerlige Ramappa <umesh.nerlige.ramappa@intel.com>
    Signed-off-by: Jonathan Cavitt <jonathan.cavitt@intel.com>
    Signed-off-by: Aravind Iddamsetty <aravind.iddamsetty@intel.com>
    Signed-off-by: Fei Yang <fei.yang@intel.com>
    CC: Andi Shyti <andi.shyti@linux.intel.com>

--- drivers/gpu/drm/i915/gt/uc/intel_guc.h
+++ drivers/gpu/drm/i915/gt/uc/intel_guc.h
@@ -79,6 +79,18 @@ struct intel_guc {
 	 */
 	atomic_t outstanding_submission_g2h;
 
+	/** @tlb_lookup: xarray to store all pending TLB invalidation requests */
+	struct xarray tlb_lookup;
+
+	/**
+	 * @serial_slot: id to the initial waiter created in tlb_lookup,
+	 * which is used only when failed to allocate new waiter.
+	 */
+	u32 serial_slot;
+
+	/** @next_seqno: the next id (sequence no.) to allocate. */
+	u32 next_seqno;
+
 	/** @interrupts: pointers to GuC interrupt-managing functions. */
 	struct {
 		bool enabled;
@@ -297,6 +309,11 @@ struct intel_guc {
 #define GUC_SUBMIT_VER(guc)		MAKE_GUC_VER_STRUCT((guc)->submission_version)
 #define GUC_FIRMWARE_VER(guc)		MAKE_GUC_VER_STRUCT((guc)->fw.file_selected.ver)
 
+struct intel_guc_tlb_wait {
+	struct wait_queue_head wq;
+	bool busy;
+};
+
 static inline struct intel_guc *log_to_guc(struct intel_guc_log *log)
 {
 	return container_of(log, struct intel_guc, log);
@@ -418,6 +435,11 @@ static inline bool intel_guc_is_supported(struct intel_guc *guc)
 {
 	return intel_uc_fw_is_supported(&guc->fw);
 }
+
+int intel_guc_invalidate_tlb_engines(struct intel_guc *guc);
+int intel_guc_invalidate_tlb_guc(struct intel_guc *guc);
+int intel_guc_tlb_invalidation_done(struct intel_guc *guc,
+				    const u32 *payload, u32 len);
 
 static inline bool intel_guc_is_wanted(struct intel_guc *guc)
 {
--- drivers/gpu/drm/i915/gt/uc/intel_guc_ct.c
+++ drivers/gpu/drm/i915/gt/uc/intel_guc_ct.c
@@ -1115,6 +1115,9 @@ static int ct_process_request(struct intel_guc_ct *ct, struct ct_incoming_msg *r
 	case INTEL_GUC_ACTION_NOTIFY_EXCEPTION:
 		ret = intel_guc_crash_process_msg(guc, action);
 		break;
+	case INTEL_GUC_ACTION_TLB_INVALIDATION_DONE:
+		ret = intel_guc_tlb_invalidation_done(guc, payload, len);
+		break;
 	default:
 		ret = -EOPNOTSUPP;
 		break;
@@ -1186,6 +1189,7 @@ static int ct_handle_event(struct intel_guc_ct *ct, struct ct_incoming_msg *requ
 	switch (action) {
 	case INTEL_GUC_ACTION_SCHED_CONTEXT_MODE_DONE:
 	case INTEL_GUC_ACTION_DEREGISTER_CONTEXT_DONE:
+	case INTEL_GUC_ACTION_TLB_INVALIDATION_DONE:
 		g2h_release_space(ct, request->size);
 	}
 
