aef39c3de48e72848f831a37b7d59aee34ccda36
Jacky Huang <ychuang3@nuvoton.com>
2023-10-11 09:05:08 +0000

    dt-bindings: pinctrl: Document nuvoton ma35d1 pin control
    Add the dt-bindings header for nuvoton ma35d1 pinctrl, that gets shared
    between the pin control driver and pin configuration in the dts.
    
    Add documentation to describe nuvoton ma35d1 pin control and GPIO.
    
    Signed-off-by: Jacky Huang <ychuang3@nuvoton.com>

---
+++ include/dt-bindings/pinctrl/ma35d1-pinfunc.h
@@ -0,0 +1,38 @@
+/* SPDX-License-Identifier: (GPL-2.0 OR BSD-2-Clause) */
+/*
+ * Copyright (C) 2023 Nuvoton Technologies.
+ */
+
+#ifndef __DT_BINDINGS_PINCTRL_NUVOTON_MA35D1_H
+#define __DT_BINDINGS_PINCTRL_NUVOTON_MA35D1_H
+
+#define MA35_SYS_REG_GPA_L	0x80
+#define MA35_SYS_REG_GPA_H	0x84
+#define MA35_SYS_REG_GPB_L	0x88
+#define MA35_SYS_REG_GPB_H	0x8c
+#define MA35_SYS_REG_GPC_L	0x90
+#define MA35_SYS_REG_GPC_H	0x94
+#define MA35_SYS_REG_GPD_L	0x98
+#define MA35_SYS_REG_GPD_H	0x9c
+#define MA35_SYS_REG_GPE_L	0xa0
+#define MA35_SYS_REG_GPE_H	0xa4
+#define MA35_SYS_REG_GPF_L	0xa8
+#define MA35_SYS_REG_GPF_H	0xac
+#define MA35_SYS_REG_GPG_L	0xb0
+#define MA35_SYS_REG_GPG_H	0xb4
+#define MA35_SYS_REG_GPH_L	0xb8
+#define MA35_SYS_REG_GPH_H	0xbc
+#define MA35_SYS_REG_GPI_L	0xc0
+#define MA35_SYS_REG_GPI_H	0xc4
+#define MA35_SYS_REG_GPJ_L	0xc8
+#define MA35_SYS_REG_GPJ_H	0xcc
+#define MA35_SYS_REG_GPK_L	0xd0
+#define MA35_SYS_REG_GPK_H	0xd4
+#define MA35_SYS_REG_GPL_L	0xd8
+#define MA35_SYS_REG_GPL_H	0xdc
+#define MA35_SYS_REG_GPM_L	0xe0
+#define MA35_SYS_REG_GPM_H	0xe4
+#define MA35_SYS_REG_GPN_L	0xe8
+#define MA35_SYS_REG_GPN_H	0xec
+
+#endif /* __DT_BINDINGS_PINCTRL_NUVOTON_MA35D1_H */
