e3795930c404be94a381254a9e369a3e41876548
Uros Bizjak <ubizjak@gmail.com>
2023-10-12 22:12:04 +0200

    x86/percpu: Introduce PER_CPU_ARG and use it in cmpxchg{8,16}b_emu.S
    PER_CPU_VAR macro is intended to be applied to a symbol and should not
    be used with general operands. Introduce new PER_CPU_ARG macro and
    use it in cmpxchg{8,16}b_emu.S instead.
    
    PER_CPU_VAR macro will be repurposed for %rip-relative addressing.
    
    Also add a missing function comment to this_cpu_cmpxchg8b_emu.
    
    No functional changes intended.
    
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Signed-off-by: Uros Bizjak <ubizjak@gmail.com>
    --
    v2: Introduce PER_CPU_ARG macro to conditionally enable
        segment registers in cmpxchg{8,16}b_emu.S for CONFIG_SMP.

--- arch/x86/include/asm/percpu.h
+++ arch/x86/include/asm/percpu.h
@@ -11,8 +11,10 @@
 #ifdef __ASSEMBLY__
 
 #ifdef CONFIG_SMP
+#define PER_CPU_ARG(arg)	%__percpu_seg:arg
 #define PER_CPU_VAR(var)	%__percpu_seg:var
 #else /* ! SMP */
+#define PER_CPU_ARG(arg)	arg
 #define PER_CPU_VAR(var)	var
 #endif	/* SMP */
 
