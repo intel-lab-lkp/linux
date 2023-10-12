b8f9a6a89dc20b35ea771b33df8c6b0c5851ca30
Md Sadre Alam <quic_mdalam@quicinc.com>
2023-10-12 11:21:48 +0530

    mtd: spi-nor: Ensure operation completion before shutdown
    Ensure that there are no pending spi operation inprogress
    when system halts.
    
    Signed-off-by: Md Sadre Alam <quic_mdalam@quicinc.com>

--- drivers/mtd/spi-nor/core.c
+++ drivers/mtd/spi-nor/core.c
@@ -3690,6 +3690,9 @@ static void spi_nor_shutdown(struct spi_mem *spimem)
 {
 	struct spi_nor *nor = spi_mem_get_drvdata(spimem);
 
+	/* Ensure no pending spi operation in progress */
+	spi_nor_wait_till_ready(nor);
+
 	spi_nor_restore(nor);
 }
 
