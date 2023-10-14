b887a8d33fb8d908c7f3633ba8e317b3258c9739
Yoshinori Sato <ysato@users.sourceforge.jp>
2023-10-14 23:53:46 +0900

    include/dt-bindings/clock/sh7750.h: cpg-sh7750 binding header.
    SH7750 CPG driver binding definition.
    
    Signed-off-by: Yoshinori Sato <ysato@users.sourceforge.jp>

---
+++ include/dt-bindings/clock/sh7750.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+ *
+ * Copyright 2023 Yoshinori Sato
+ */
+
+#ifndef __DT_BINDINGS_CLOCK_SH7750_H__
+#define __DT_BINDINGS_CLOCK_SH7750_H__
+
+#define SH7750_CPG_PLLOUT	0
+
+#define SH7750_CPG_FCK		1
+#define SH7750_CPG_BCK		2
+#define SH7750_CPG_ICK		3
+
+#define SH7750_CPG_SCI_CLK	4
+#define SH7750_CPG_RTC_CLK	5
+#define SH7750_CPG_TMU012_CLK	6
+#define SH7750_CPG_SCIF_CLK	7
+#define SH7750_CPG_DMAC_CLK	8
+#define SH7750_CPG_UBC_CLK	9
+#define SH7750_CPG_SQ_CLK	10
+#define SH7750_CPG_INTC_CLK	11
+#define SH7750_CPG_TMU34_CLK	12
+#define SH7750_CPG_PCIC_CLK	13
+
+#endif
