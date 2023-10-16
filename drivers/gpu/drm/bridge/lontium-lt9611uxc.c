0806935f7524ac5008973e28ef71c0670e32502a
Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
2023-10-16 19:53:54 +0300

    drm/bridge: lt9611uxc: implement automatic DSI power control
    The Lontium LT9611UXC driver doesn't need to control DSI power
    lines manually. Mark it for automatic power control.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>

--- drivers/gpu/drm/bridge/lontium-lt9611uxc.c
+++ drivers/gpu/drm/bridge/lontium-lt9611uxc.c
@@ -279,7 +279,7 @@ static struct mipi_dsi_device *lt9611uxc_attach_dsi(struct lt9611uxc *lt9611uxc,
 	dsi->lanes = 4;
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->mode_flags = MIPI_DSI_MODE_VIDEO | MIPI_DSI_MODE_VIDEO_SYNC_PULSE |
-			  MIPI_DSI_MODE_VIDEO_HSE;
+			  MIPI_DSI_MODE_VIDEO_HSE | MIPI_DSI_AUTO_POWERUP;
 
 	ret = devm_mipi_dsi_attach(dev, dsi);
 	if (ret < 0) {
