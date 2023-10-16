254951b45ab80cca3e7f08846d891d4b7a54ce0b
Namhyung Kim <namhyung@kernel.org>
2023-10-15 21:42:25 -0700

    perf bench sched pipe: Add -G/--cgroups option
    The -G/--cgroups option is to put sender and receiver in different
    cgroups in order to measure cgroup context switch overheads.
    
    Users need to make sure the cgroups exist and accessible.
    
      # perf stat -e context-switches,cgroup-switches \
      > taskset -c 0 perf bench sched pipe -l 10000 > /dev/null
    
       Performance counter stats for 'taskset -c 0 perf bench sched pipe -l 10000':
    
                  20,001      context-switches
                       2      cgroup-switches
    
             0.053449651 seconds time elapsed
    
             0.011286000 seconds user
             0.041869000 seconds sys
    
      # perf stat -e context-switches,cgroup-switches \
      > taskset -c 0 perf bench sched pipe -l 10000 -G AAA,BBB > /dev/null
    
       Performance counter stats for 'taskset -c 0 perf bench sched pipe -l 10000 -G AAA,BBB':
    
                  20,001      context-switches
                  20,001      cgroup-switches
    
             0.052768627 seconds time elapsed
    
             0.006284000 seconds user
             0.046266000 seconds sys
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/bench/sched-pipe.c
+++ tools/perf/bench/sched-pipe.c
@@ -10,7 +10,9 @@
  * Ported to perf by Hitoshi Mitake <mitake@dcl.info.waseda.ac.jp>
  */
 #include <subcmd/parse-options.h>
+#include <api/fs/fs.h>
 #include "bench.h"
+#include "util/cgroup.h"
 
 #include <unistd.h>
 #include <stdio.h>
@@ -19,6 +21,7 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <assert.h>
 #include <sys/time.h>
 #include <sys/types.h>
@@ -31,6 +34,7 @@ struct thread_data {
 	int			nr;
 	int			pipe_read;
 	int			pipe_write;
+	bool			cgroup_failed;
 	pthread_t		pthread;
 };
 
@@ -40,9 +44,55 @@ static	int			loops = LOOPS_DEFAULT;
 /* Use processes by default: */
 static bool			threaded;
 
+static struct cgroup *cgrp_send = NULL;
+static struct cgroup *cgrp_recv = NULL;
+
+static int parse_two_cgroups(const struct option *opt __maybe_unused,
+			     const char *str, int unset __maybe_unused)
+{
+	char *p = strdup(str);
+	char *q;
+	int ret = -1;
+
+	if (p == NULL) {
+		fprintf(stderr, "memory allocation failure");
+		return -1;
+	}
+
+	q = strchr(p, ',');
+	if (q == NULL) {
+		fprintf(stderr, "it should have two cgroup names: %s", p);
+		goto out;
+	}
+	*q = '\0';
+
+	cgrp_send = cgroup__new(p, /*do_open=*/true);
+	if (cgrp_send == NULL) {
+		fprintf(stderr, "cannot open sender cgroup: %s", p);
+		goto out;
+	}
+
+	/* skip ',' */
+	q++;
+
+	cgrp_recv = cgroup__new(q, /*do_open=*/true);
+	if (cgrp_recv == NULL) {
+		fprintf(stderr, "cannot open receiver cgroup: %s", q);
+		goto out;
+	}
+	ret = 0;
+
+out:
+	free(p);
+	return ret;
+}
+
 static const struct option options[] = {
 	OPT_INTEGER('l', "loop",	&loops,		"Specify number of loops"),
 	OPT_BOOLEAN('T', "threaded",	&threaded,	"Specify threads/process based task setup"),
+	OPT_CALLBACK('G', "cgroups", NULL, "SEND,RECV",
+		     "Put sender and receivers in given cgroups",
+		     parse_two_cgroups),
 	OPT_END()
 };
 
@@ -51,11 +101,70 @@ static const char * const bench_sched_pipe_usage[] = {
 	NULL
 };
 
+static int enter_cgroup(struct cgroup *cgrp)
+{
+	char buf[32];
+	int fd, len, ret;
+	pid_t pid;
+
+	if (cgrp == NULL)
+		return 0;
+
+	if (threaded)
+		pid = syscall(__NR_gettid);
+	else
+		pid = getpid();
+
+	snprintf(buf, sizeof(buf), "%d\n", pid);
+	len = strlen(buf);
+
+	/* try cgroup v2 interface first */
+	if (threaded)
+		fd = openat(cgrp->fd, "cgroup.threads", O_WRONLY);
+	else
+		fd = openat(cgrp->fd, "cgroup.procs", O_WRONLY);
+
+	/* try cgroup v1 if failed */
+	if (fd < 0)
+		fd = openat(cgrp->fd, "tasks", O_WRONLY);
+
+	if (fd < 0) {
+		char mnt[PATH_MAX];
+
+		printf("Failed to open cgroup file in %s\n", cgrp->name);
+
+		if (cgroupfs_find_mountpoint(mnt, sizeof(mnt), "perf_event") == 0)
+			printf(" Hint: create the cgroup first, like 'mkdir %s/%s'\n",
+			       mnt, cgrp->name);
+		return -1;
+	}
+
+	ret = write(fd, buf, len);
+	close(fd);
+
+	if (ret != len) {
+		printf("Cannot enter to cgroup: %s\n", cgrp->name);
+		return -1;
+	}
+	return 0;
+}
+
 static void *worker_thread(void *__tdata)
 {
 	struct thread_data *td = __tdata;
 	int m = 0, i;
 	int ret;
+
+	if (td->nr)
+		ret = enter_cgroup(cgrp_send);
+	else
+		ret = enter_cgroup(cgrp_recv);
+
+	if (ret < 0) {
+		td->cgroup_failed = true;
+		return NULL;
+	}
+	td->cgroup_failed = false;
 
 	for (i = 0; i < loops; i++) {
 		if (!td->nr) {
@@ -112,9 +221,7 @@ int bench_sched_pipe(int argc, const char **argv)
 		}
 	}
 
-
 	if (threaded) {
-
 		for (t = 0; t < nr_threads; t++) {
 			td = threads + t;
 
@@ -128,7 +235,6 @@ int bench_sched_pipe(int argc, const char **argv)
 			ret = pthread_join(td->pthread, NULL);
 			BUG_ON(ret);
 		}
-
 	} else {
 		pid = fork();
 		assert(pid >= 0);
@@ -146,6 +252,12 @@ int bench_sched_pipe(int argc, const char **argv)
 
 	gettimeofday(&stop, NULL);
 	timersub(&stop, &start, &diff);
+
+	cgroup__put(cgrp_send);
+	cgroup__put(cgrp_recv);
+
+	if (threads[0].cgroup_failed || threads[1].cgroup_failed)
+		return 0;
 
 	switch (bench_format) {
 	case BENCH_FORMAT_DEFAULT:
