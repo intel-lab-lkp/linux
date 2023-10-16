a44613b06aa651436c3abf36f1599621d3c40cfb
Jouni Högander <jouni.hogander@intel.com>
2023-10-16 14:16:35 +0300

    drm/i915/display: Add framework to add parameters specific to display
    Currently all module parameters are handled by i915_param.c/h. This
    is a problem for display parameters when Xe driver is used. Add
    a mechanism to add parameters specific to the display. This is mainly
    copied from i915_[debugfs]_params.[ch]. Parameters are not yet moved. This
    is done by subsequent patches.
    
    Signed-off-by: Jouni Högander <jouni.hogander@intel.com>

---
+++ drivers/gpu/drm/i915/display/intel_display_debugfs_params.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Copyright © 2023 Intel Corporation
+ */
+
+#ifndef __INTEL_DISPLAY_DEBUGFS_PARAMS__
+#define __INTEL_DISPLAY_DEBUGFS_PARAMS__
+
+struct dentry;
+struct drm_i915_private;
+
+void intel_display_debugfs_params(struct drm_i915_private *i915);
+
+#endif /* __INTEL_DISPLAY_DEBUGFS_PARAMS__ */
