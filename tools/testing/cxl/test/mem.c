31ed0cb8d0a8bb84378f7dce1541b7752ae21160
Robert Richter <rrichter@amd.com>
2023-10-18 19:17:01 +0200

    cxl/pci: Remove Component Register base address from struct cxl_dev_state
    The Component Register base address @component_reg_phys is no longer
    used after the rework of the Component Register setup which now uses
    struct member @reg_map instead. Remove the base address.
    
    Signed-off-by: Terry Bowman <terry.bowman@amd.com>
    Signed-off-by: Robert Richter <rrichter@amd.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>

--- tools/testing/cxl/test/mem.c
+++ tools/testing/cxl/test/mem.c
@@ -1423,7 +1423,6 @@ static int cxl_mock_mem_probe(struct platform_device *pdev)
 	cxlds->serial = pdev->id;
 	if (is_rcd(pdev)) {
 		cxlds->rcd = true;
-		cxlds->component_reg_phys = CXL_RESOURCE_NONE;
 	}
 
 	rc = cxl_enumerate_cmds(mds);
