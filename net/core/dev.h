e4e4bc40821479981ce5074c7efce410d35e763c
Amritha Nambiar <amritha.nambiar@intel.com>
2023-10-18 17:06:33 -0700

    netdev-genl: Add netlink framework functions for napi
    Implement the netdev netlink framework functions for
    napi support. The netdev structure tracks all the napi
    instances and napi fields. The napi instances and associated
    parameters can be retrieved this way.
    
    Signed-off-by: Amritha Nambiar <amritha.nambiar@intel.com>
    Reviewed-by: Sridhar Samudrala <sridhar.samudrala@intel.com>

--- net/core/dev.h
+++ net/core/dev.h
@@ -136,4 +136,6 @@ static inline void netif_set_gro_ipv4_max_size(struct net_device *dev,
 }
 
 int rps_cpumask_housekeeping(struct cpumask *mask);
+
+struct napi_struct *napi_by_id(unsigned int napi_id);
 #endif
