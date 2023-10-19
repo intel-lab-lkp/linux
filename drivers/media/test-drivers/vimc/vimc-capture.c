a48f1bdaf0242b71d73e16fe1e782c1dad587169
Benjamin Gaignard <benjamin.gaignard@collabora.com>
2023-10-19 14:52:22 +0200

    media: test-drivers: Use helper for DELETE_BUFS ioctl
    Allow test drivers to use DELETE_BUFS by adding vb2_ioctl_delete_bufs() helper.
    
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@collabora.com>

--- drivers/media/test-drivers/vimc/vimc-capture.c
+++ drivers/media/test-drivers/vimc/vimc-capture.c
@@ -221,6 +221,7 @@ static const struct v4l2_ioctl_ops vimc_capture_ioctl_ops = {
 	.vidioc_expbuf = vb2_ioctl_expbuf,
 	.vidioc_streamon = vb2_ioctl_streamon,
 	.vidioc_streamoff = vb2_ioctl_streamoff,
+	.vidioc_delete_bufs = vb2_ioctl_delete_bufs,
 };
 
 static void vimc_capture_return_all_buffers(struct vimc_capture_device *vcapture,
@@ -435,6 +436,7 @@ static struct vimc_ent_device *vimc_capture_add(struct vimc_device *vimc,
 	q->min_buffers_needed = 2;
 	q->lock = &vcapture->lock;
 	q->dev = v4l2_dev->dev;
+	q->supports_delete_bufs = true;
 
 	ret = vb2_queue_init(q);
 	if (ret) {
