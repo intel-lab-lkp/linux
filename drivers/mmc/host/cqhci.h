95cd846b51ea9cccbc1df1b659335431563aaaa0
Jyan Chou <jyanchou@realtek.com>
2023-10-20 11:49:18 +0800

    mmc: solve DMA boundary limitation of CQHCI driver
    Due to synopsys data book's description, it had a limitation
    while using DMA that buffer size and start address must not
    exceed 128 MB.
    
    We add an option setup_tran_desc to make tran_desc setting flexible.
    
    Signed-off-by: Jyan Chou <jyanchou@realtek.com>

--- drivers/mmc/host/cqhci.h
+++ drivers/mmc/host/cqhci.h
@@ -216,6 +216,7 @@ union cqhci_crypto_cfg_entry {
 struct cqhci_host_ops;
 struct mmc_host;
 struct mmc_request;
+struct mmc_data;
 struct cqhci_slot;
 
 struct cqhci_host {
@@ -289,6 +290,8 @@ struct cqhci_host_ops {
 				 u64 *data);
 	void (*pre_enable)(struct mmc_host *mmc);
 	void (*post_disable)(struct mmc_host *mmc);
+	void (*setup_tran_desc)(struct mmc_data *data,
+				struct cqhci_host *cq_host, u8 *desc, int sg_count);
 #ifdef CONFIG_MMC_CRYPTO
 	int (*program_key)(struct cqhci_host *cq_host,
 			   const union cqhci_crypto_cfg_entry *cfg, int slot);
@@ -322,6 +325,8 @@ static inline int cqhci_suspend(struct mmc_host *mmc)
 {
 	return cqhci_deactivate(mmc);
 }
+
+void cqhci_set_tran_desc(u8 *desc, dma_addr_t addr, int len, bool end, bool dma64);
 int cqhci_resume(struct mmc_host *mmc);
 
 #endif
