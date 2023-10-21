0e62244d5a75c7f78694c8707675258b4c0afbbc
Chengming Zhou <zhouchengming@bytedance.com>
2023-10-21 14:43:13 +0000

    slub: Prepare __slab_free() for unfrozen partial slab out of node partial list
    Now the partial slub will be frozen when taken out of node partial list,
    so the __slab_free() will know from "was_frozen" that the partial slab
    is not on node partial list and is used by one kmem_cache_cpu.
    
    But we will change this, make partial slabs leave the node partial list
    with unfrozen state, so we need to change __slab_free() to use the new
    slab_test_node_partial() we just introduced.
    
    Signed-off-by: Chengming Zhou <zhouchengming@bytedance.com>

--- mm/slub.c
+++ mm/slub.c
@@ -3610,6 +3610,7 @@ static void __slab_free(struct kmem_cache *s, struct slab *slab,
 	unsigned long counters;
 	struct kmem_cache_node *n = NULL;
 	unsigned long flags;
+	bool on_node_partial;
 
 	stat(s, FREE_SLOWPATH);
 
@@ -3657,6 +3658,7 @@ static void __slab_free(struct kmem_cache *s, struct slab *slab,
 				 */
 				spin_lock_irqsave(&n->list_lock, flags);
 
+				on_node_partial = slab_test_node_partial(slab);
 			}
 		}
 
@@ -3682,6 +3684,15 @@ static void __slab_free(struct kmem_cache *s, struct slab *slab,
 			stat(s, CPU_PARTIAL_FREE);
 		}
 
+		return;
+	}
+
+	/*
+	 * This slab was not full and not on the per-node partial list either,
+	 * in which case we shouldn't manipulate its list, just early return.
+	 */
+	if (prior && !on_node_partial) {
+		spin_unlock_irqrestore(&n->list_lock, flags);
 		return;
 	}
 
