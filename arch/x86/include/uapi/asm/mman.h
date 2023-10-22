2d7f00f5ae832e1a39ec7990fd37d869249ab436
Dmitry V. Levin <ldv@strace.io>
2023-10-23 01:21:34 +0300

    x86/uapi: fix SHADOW_STACK_SET_TOKEN type
    Fix the type of SHADOW_STACK_SET_TOKEN to match the type of the
    corresponding "flags" argument of map_shadow_stack syscall which
    is of type "unsigned int".
    
    Fixes: c35559f94ebc3 ("x86/shstk: Introduce map_shadow_stack syscall")
    Signed-off-by: Dmitry V. Levin <ldv@strace.io>

--- arch/x86/include/uapi/asm/mman.h
+++ arch/x86/include/uapi/asm/mman.h
@@ -6,7 +6,7 @@
 #define MAP_ABOVE4G	0x80		/* only map above 4GB */
 
 /* Flags for map_shadow_stack(2) */
-#define SHADOW_STACK_SET_TOKEN	(1ULL << 0)	/* Set up a restore token in the shadow stack */
+#define SHADOW_STACK_SET_TOKEN	(1U << 0)	/* Set up a restore token in the shadow stack */
 
 #include <asm-generic/mman.h>
 
