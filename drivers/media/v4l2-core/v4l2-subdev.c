46ba3f07d9f5be6c0b5930ec8a54a42e1d529dc3
Sakari Ailus <sakari.ailus@linux.intel.com>
2023-10-23 15:33:03 +0300

    media: v4l: subdev: Also return pads array information on stream functions
    There are two sets of functions that return information from sub-device
    state, one for stream-unaware users and another for stream-aware users.
    Add support for stream-aware functions to return format, crop and compose
    information from pad-based array that are functionally equivalent to the
    old, stream-unaware ones.
    
    Also check state is non-NULL, in order to guard against old drivers
    potentially calling this with NULL state for active formats or selection
    rectangles.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- drivers/media/v4l2-core/v4l2-subdev.c
+++ drivers/media/v4l2-core/v4l2-subdev.c
@@ -1682,6 +1682,19 @@ v4l2_subdev_state_get_stream_format(struct v4l2_subdev_state *state,
 	struct v4l2_subdev_stream_configs *stream_configs;
 	unsigned int i;
 
+	if (WARN_ON(!state))
+		return NULL;
+
+	if (state->pads) {
+		if (stream)
+			return NULL;
+
+		if (WARN_ON(pad >= state->sd->entity.num_pads))
+			pad = 0;
+
+		return &state->pads[pad].try_fmt;
+	}
+
 	lockdep_assert_held(state->lock);
 
 	stream_configs = &state->stream_configs;
@@ -1703,6 +1716,19 @@ v4l2_subdev_state_get_stream_crop(struct v4l2_subdev_state *state,
 	struct v4l2_subdev_stream_configs *stream_configs;
 	unsigned int i;
 
+	if (WARN_ON(!state))
+		return NULL;
+
+	if (state->pads) {
+		if (stream)
+			return NULL;
+
+		if (WARN_ON(pad >= state->sd->entity.num_pads))
+			pad = 0;
+
+		return &state->pads[pad].try_crop;
+	}
+
 	lockdep_assert_held(state->lock);
 
 	stream_configs = &state->stream_configs;
@@ -1723,6 +1749,19 @@ v4l2_subdev_state_get_stream_compose(struct v4l2_subdev_state *state,
 {
 	struct v4l2_subdev_stream_configs *stream_configs;
 	unsigned int i;
+
+	if (WARN_ON(!state))
+		return NULL;
+
+	if (state->pads) {
+		if (stream)
+			return NULL;
+
+		if (WARN_ON(pad >= state->sd->entity.num_pads))
+			pad = 0;
+
+		return &state->pads[pad].try_compose;
+	}
 
 	lockdep_assert_held(state->lock);
 
