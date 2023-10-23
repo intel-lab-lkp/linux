6750a9425c7d09882240ac8bde16a21e08af1cce
Brian Gerst <brgerst@gmail.com>
2023-10-23 17:17:29 -0400

    percpu: Remove PER_CPU_FIRST_SECTION
    x86-64 was the only user.
    
    Signed-off-by: Brian Gerst <brgerst@gmail.com>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -1032,7 +1032,6 @@
  */
 #define PERCPU_INPUT(cacheline)						\
 	__per_cpu_start = .;						\
-	*(.data..percpu..first)						\
 	. = ALIGN(PAGE_SIZE);						\
 	*(.data..percpu..page_aligned)					\
 	. = ALIGN(cacheline);						\
