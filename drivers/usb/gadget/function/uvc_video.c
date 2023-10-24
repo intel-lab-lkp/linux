40ef16edbbd70cb62b43789455c698333c2223bb
Avichal Rakesh <arakesh@google.com>
2023-10-24 11:36:04 -0700

    usb: gadget: uvc: move video disable logic to its own function
    This patch refactors the video disable logic in uvcg_video_enable
    into its own separate function 'uvcg_video_disable'.
    
    Suggested-by: Michael Grzeschik <m.grzeschik@pengutronix.de>
    Signed-off-by: Avichal Rakesh <arakesh@google.com>

--- drivers/usb/gadget/function/uvc_video.c
+++ drivers/usb/gadget/function/uvc_video.c
@@ -494,12 +494,32 @@ static void uvcg_video_pump(struct work_struct *work)
 }
 
 /*
+ * Disable video stream
+ */
+static int
+uvcg_video_disable(struct uvc_video *video)
+{
+	struct uvc_request *ureq;
+
+	cancel_work_sync(&video->pump);
+	uvcg_queue_cancel(&video->queue, 0);
+
+	list_for_each_entry(ureq, &video->ureqs, list) {
+		if (ureq->req)
+			usb_ep_dequeue(video->ep, ureq->req);
+	}
+
+	uvc_video_free_requests(video);
+	uvcg_queue_enable(&video->queue, 0);
+	return 0;
+}
+
+/*
  * Enable or disable the video stream.
  */
 int uvcg_video_enable(struct uvc_video *video, int enable)
 {
 	int ret;
-	struct uvc_request *ureq;
 
 	if (video->ep == NULL) {
 		uvcg_info(&video->uvc->func,
@@ -507,19 +527,8 @@ int uvcg_video_enable(struct uvc_video *video, int enable)
 		return -ENODEV;
 	}
 
-	if (!enable) {
-		cancel_work_sync(&video->pump);
-		uvcg_queue_cancel(&video->queue, 0);
-
-		list_for_each_entry(ureq, &video->ureqs, list) {
-			if (ureq->req)
-				usb_ep_dequeue(video->ep, ureq->req);
-		}
-
-		uvc_video_free_requests(video);
-		uvcg_queue_enable(&video->queue, 0);
-		return 0;
-	}
+	if (!enable)
+		return uvcg_video_disable(video);
 
 	if ((ret = uvcg_queue_enable(&video->queue, 1)) < 0)
 		return ret;
