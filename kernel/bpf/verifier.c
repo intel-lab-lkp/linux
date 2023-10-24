c840bab2de9f6a703c6d2f961a7bfae761e9507f
Chuyi Zhou <zhouchuyi@bytedance.com>
2023-10-24 10:42:39 +0800

    bpf: Relax allowlist for css_task iter
    The newly added open-coded css_task iter would try to hold the global
    css_set_lock in bpf_iter_css_task_new, so the bpf side has to be careful in
    where it allows to use this iter. The mainly concern is dead locking on
    css_set_lock. check_css_task_iter_allowlist() in verifier enforced css_task
    can only be used in bpf_lsm hooks and sleepable bpf_iter.
    
    This patch relax the allowlist for css_task iter. Any lsm and any iter
    (even non-sleepable) and any sleepable are safe since they would not hold
    the css_set_lock before entering BPF progs context.
    
    This patch also fixes the misused BPF_TRACE_ITER in
    check_css_task_iter_allowlist which compared bpf_prog_type with
    bpf_attach_type.
    
    Fixes: 9c66dc94b62ae ("bpf: Introduce css_task open-coded iterator kfuncs")
    Signed-off-by: Chuyi Zhou <zhouchuyi@bytedance.com>

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -11088,18 +11088,23 @@ static int process_kf_arg_ptr_to_rbtree_node(struct bpf_verifier_env *env,
 						  &meta->arg_rbtree_root.field);
 }
 
+/*
+ * css_task iter allowlist is needed to avoid dead locking on css_set_lock.
+ * LSM hooks and iters (both sleepable and non-sleepable) are safe.
+ * Any sleepable progs are also safe since bpf_check_attach_target() enforce
+ * them can only be attached to some specific hook points.
+ */
 static bool check_css_task_iter_allowlist(struct bpf_verifier_env *env)
 {
 	enum bpf_prog_type prog_type = resolve_prog_type(env->prog);
 
-	switch (prog_type) {
-	case BPF_PROG_TYPE_LSM:
+	if (prog_type == BPF_PROG_TYPE_LSM)
 		return true;
-	case BPF_TRACE_ITER:
-		return env->prog->aux->sleepable;
-	default:
-		return false;
-	}
+
+	if (env->prog->expected_attach_type == BPF_TRACE_ITER)
+		return true;
+
+	return env->prog->aux->sleepable;
 }
 
 static int check_kfunc_args(struct bpf_verifier_env *env, struct bpf_kfunc_call_arg_meta *meta,
@@ -11357,7 +11362,7 @@ static int check_kfunc_args(struct bpf_verifier_env *env, struct bpf_kfunc_call_
 		case KF_ARG_PTR_TO_ITER:
 			if (meta->func_id == special_kfunc_list[KF_bpf_iter_css_task_new]) {
 				if (!check_css_task_iter_allowlist(env)) {
-					verbose(env, "css_task_iter is only allowed in bpf_lsm and bpf iter-s\n");
+					verbose(env, "css_task_iter is only allowed in bpf_lsm, bpf_iter and sleepable progs\n");
 					return -EINVAL;
 				}
 			}
