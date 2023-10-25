a779df4e587b9953a4378c4bbad1ce821907f33f
Fei Qin <fei.qin@corigine.com>
2023-10-25 09:41:46 +0200

    nfp: using napi_build_skb() to replace build_skb()
    The napi_build_skb() can reuse the skb in skb cache per CPU or
    can allocate skbs in bulk, which helps improve the performance.
    
    Signed-off-by: Fei Qin <fei.qin@corigine.com>
    Signed-off-by: Louis Peens <louis.peens@corigine.com>

--- drivers/net/ethernet/netronome/nfp/nfdk/dp.c
+++ drivers/net/ethernet/netronome/nfp/nfdk/dp.c
@@ -1189,7 +1189,7 @@ static int nfp_nfdk_rx(struct nfp_net_rx_ring *rx_ring, int budget)
 				nfp_repr_inc_rx_stats(netdev, pkt_len);
 		}
 
-		skb = build_skb(rxbuf->frag, true_bufsz);
+		skb = napi_build_skb(rxbuf->frag, true_bufsz);
 		if (unlikely(!skb)) {
 			nfp_nfdk_rx_drop(dp, r_vec, rx_ring, rxbuf, NULL);
 			continue;
@@ -1525,7 +1525,7 @@ nfp_ctrl_rx_one(struct nfp_net *nn, struct nfp_net_dp *dp,
 		return true;
 	}
 
-	skb = build_skb(rxbuf->frag, dp->fl_bufsz);
+	skb = napi_build_skb(rxbuf->frag, dp->fl_bufsz);
 	if (unlikely(!skb)) {
 		nfp_nfdk_rx_drop(dp, r_vec, rx_ring, rxbuf, NULL);
 		return true;
