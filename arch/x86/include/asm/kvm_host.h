1cf7d5e59551dcfed1d14a9efc76de454310f139
Vitaly Kuznetsov <vkuznets@redhat.com>
2023-10-25 17:24:02 +0200

    KVM: x86: Make Hyper-V emulation optional
    Hyper-V emulation in KVM is a fairly big chunk and in some cases it may be
    desirable to not compile it in to reduce module sizes as well as the attack
    surface. Introduce CONFIG_KVM_HYPERV option to make it possible.
    
    Note, there's room for further nVMX/nSVM code optimizations when
    !CONFIG_KVM_HYPERV, this will be done in follow-up patches.
    
    Reorganize Makefile a bit so all CONFIG_HYPERV and CONFIG_KVM_HYPERV files
    are grouped together.
    
    Signed-off-by: Vitaly Kuznetsov <vkuznets@redhat.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1084,6 +1084,7 @@ enum hv_tsc_page_status {
 	HV_TSC_PAGE_BROKEN,
 };
 
+#ifdef CONFIG_KVM_HYPERV
 /* Hyper-V emulation context */
 struct kvm_hv {
 	struct mutex hv_lock;
@@ -1116,6 +1117,7 @@ struct kvm_hv {
 
 	struct kvm_hv_syndbg hv_syndbg;
 };
+#endif
 
 struct msr_bitmap_range {
 	u32 flags;
@@ -1337,7 +1339,9 @@ struct kvm_arch {
 	/* reads protected by irq_srcu, writes by irq_lock */
 	struct hlist_head mask_notifier_list;
 
+#ifdef CONFIG_KVM_HYPERV
 	struct kvm_hv hyperv;
+#endif
 
 #ifdef CONFIG_KVM_XEN
 	struct kvm_xen xen;
