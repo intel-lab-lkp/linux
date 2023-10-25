1b4d1851801f2cd4527d4f2f83c13181be043589
Arnd Bergmann <arnd@arndb.de>
2023-10-25 09:19:31 +0200

    arm64: pi: add early_map_kernel prototype
    The newly added function only has a caller from assembler code, so it
    works without an extern prototype, but we get a warning if it's missing:
    
    arch/arm64/kernel/pi/map_kernel.c:224:24: error: no previous prototype for 'early_map_kernel' [-Werror=missing-prototypes]
    
    Fixes: 478ea5689054 ("arm64: head: Move early kernel mapping routines into C code")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm64/kernel/pi/pi.h
+++ arch/arm64/kernel/pi/pi.h
@@ -28,3 +28,5 @@ void map_range(u64 *pgd, u64 start, u64 end, u64 pa, pgprot_t prot,
 	       int level, pte_t *tbl, bool may_use_cont, u64 va_offset);
 
 asmlinkage u64 create_init_idmap(pgd_t *pgd, pteval_t clrmask);
+
+asmlinkage void early_map_kernel(u64 boot_status, void *fdt);
