34d0fa51d4aeb86558bed39c8a79b91092d6d63f
Jacob Keller <jacob.e.keller@intel.com>
2023-10-25 14:13:22 +0200

    ice: pass reset type to PTP reset functions
    The ice_ptp_prepare_for_reset() and ice_ptp_reset() functions currently
    check the pf->flags ICE_FLAG_PFR_REQ bit to determine if the current
    reset is a PF reset or not.
    
    This is problematic, because it is possible that a PF reset and a higher
    level reset (CORE reset, GLOBAL reset, EMP reset) are requested
    simultaneously. In that case, the driver performs the highest level
    reset requested. However, the ICE_FLAG_PFR_REQ flag will still be set.
    
    The main driver reset functions take an enum ice_reset_req indicating
    which reset is actually being performed. Pass this data into the PTP
    functions and rely on this instead of relying on the driver flags.
    
    This ensures that the PTP code performs the proper level of reset that
    the driver is actually undergoing.
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Signed-off-by: Karol Kolacinski <karol.kolacinski@intel.com>
    Reviewed-by: Jacob Keller <jacob.e.keller@intel.com>

--- drivers/net/ethernet/intel/ice/ice_ptp.h
+++ drivers/net/ethernet/intel/ice/ice_ptp.h
@@ -311,8 +311,9 @@ enum ice_tx_tstamp_work ice_ptp_process_ts(struct ice_pf *pf);
 void
 ice_ptp_rx_hwtstamp(struct ice_rx_ring *rx_ring,
 		    union ice_32b_rx_flex_desc *rx_desc, struct sk_buff *skb);
-void ice_ptp_reset(struct ice_pf *pf);
-void ice_ptp_prepare_for_reset(struct ice_pf *pf);
+void ice_ptp_reset(struct ice_pf *pf, enum ice_reset_req reset_type);
+void ice_ptp_prepare_for_reset(struct ice_pf *pf,
+			       enum ice_reset_req reset_type);
 void ice_ptp_init(struct ice_pf *pf);
 void ice_ptp_release(struct ice_pf *pf);
 void ice_ptp_link_change(struct ice_pf *pf, u8 port, bool linkup);
@@ -343,8 +344,15 @@ static inline bool ice_ptp_process_ts(struct ice_pf *pf)
 static inline void
 ice_ptp_rx_hwtstamp(struct ice_rx_ring *rx_ring,
 		    union ice_32b_rx_flex_desc *rx_desc, struct sk_buff *skb) { }
-static inline void ice_ptp_reset(struct ice_pf *pf) { }
-static inline void ice_ptp_prepare_for_reset(struct ice_pf *pf) { }
+static inline void ice_ptp_reset(struct ice_pf *pf,
+				 enum ice_reset_req reset_type)
+{
+}
+
+static inline void ice_ptp_prepare_for_reset(struct ice_pf *pf,
+					     enum ice_reset_req reset_type)
+{
+}
 static inline void ice_ptp_init(struct ice_pf *pf) { }
 static inline void ice_ptp_release(struct ice_pf *pf) { }
 static inline void ice_ptp_link_change(struct ice_pf *pf, u8 port, bool linkup)
