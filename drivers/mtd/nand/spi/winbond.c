269c4ad1060c0d119adf0dd98389e7b69b8ed98f
Eberhard Stoll <eberhard.stoll@kontron.de>
2023-10-26 17:23:04 +0200

    mtd: spinand: winbond: Add rx sample delay for W25N02KV
    Add tCLQV (Clock Low to Output Valid) parameter for Winbond W25N02KV
    SPI NAND chip.
    
    Signed-off-by: Eberhard Stoll <eberhard.stoll@kontron.de>
    Signed-off-by: Frieder Schrempf <frieder.schrempf@kontron.de>

--- drivers/mtd/nand/spi/winbond.c
+++ drivers/mtd/nand/spi/winbond.c
@@ -168,7 +168,8 @@ static const struct spinand_info winbond_spinand_table[] = {
 					      &write_cache_variants,
 					      &update_cache_variants),
 		     0,
-		     SPINAND_ECCINFO(&w25n02kv_ooblayout, w25n02kv_ecc_get_status)),
+		     SPINAND_ECCINFO(&w25n02kv_ooblayout, w25n02kv_ecc_get_status),
+		     SPINAND_RX_SAMPLE_DELAY(7)),
 };
 
 static int winbond_spinand_init(struct spinand_device *spinand)
