4bf8e2b251303903438d4e9e61dd6e9a2233b12d
Elinor Montmasson <elinor.montmasson@savoirfairelinux.com>
2023-10-27 16:47:33 +0200

    ASoC: fsl-asoc-card: add dts properties "cpu-sysclk-freq"
    Add new optional dts property "cpu-sysclk-freq" to set
    custom sysclk frequencies for the CPU DAI with the generic codec.
    The way values are used is up to the CPU DAI driver implementation.
    
    Signed-off-by: Elinor Montmasson <elinor.montmasson@savoirfairelinux.com>
    Co-authored-by: Philip-Dylan Gleonec <philip-dylan.gleonec@savoirfairelinux.com>

--- sound/soc/fsl/fsl-asoc-card.c
+++ sound/soc/fsl/fsl-asoc-card.c
@@ -726,6 +726,10 @@ static int fsl_asoc_card_probe(struct platform_device *pdev)
 		priv->dai_fmt |= SND_SOC_DAIFMT_CBP_CFP;
 		of_property_read_u32(np, "cpu-slot-width", &priv->cpu_priv.slot_width);
 		of_property_read_u32(np, "cpu-slot-num", &priv->cpu_priv.slot_num);
+		of_property_read_u32(np, "cpu-sysclk-freq-rx",
+					(u32 *)&priv->cpu_priv.sysclk_freq[RX]);
+		of_property_read_u32(np, "cpu-sysclk-freq-tx",
+					(u32 *)&priv->cpu_priv.sysclk_freq[TX]);
 	} else {
 		dev_err(&pdev->dev, "unknown Device Tree compatible\n");
 		ret = -EINVAL;
