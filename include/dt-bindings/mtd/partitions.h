b7e6abad70751d0e8a11f439405e69daf4f6c033
Edward Chow <equu@openmail.cc>
2023-10-27 17:46:09 +0800

    dt-bindings: mtd: partitions: Export special values
    Export special "offset" and "size" values defined and documented in
    linux/mtd/partitions.h to dt-bindings/mtd/partitions.h.
    
    Signed-off-by: Edward Chow <equu@openmail.cc>

---
+++ include/dt-bindings/mtd/partitions.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
+/*
+ * Device Tree constants identical to those in include/linux/mtd/partitions.h
+ */
+
+#ifndef _DT_BINDINGS_MTD_PARTITIONS_H
+#define _DT_BINDINGS_MTD_PARTITIONS_H
+
+#define MTDPART_OFS_SPECIAL	(-1)
+#define MTDPART_OFS_RETAIN	(-3)
+#define MTDPART_OFS_NXTBLK	(-2)
+#define MTDPART_OFS_APPEND	(-1)
+#define MTDPART_SIZ_FULL	(0)
+
+#endif
