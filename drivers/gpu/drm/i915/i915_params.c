2692df4761fde6e0317d75a1269891485872803b
John Harrison <John.C.Harrison@Intel.com>
2023-10-27 14:18:14 -0700

    drm/i915/mtl: Add module parameter override for Wa_16019325821/Wa_14019159160
    These w/a's can have signficant performance implications for any
    workload which uses both RCS and CCS. On the other hand, the hang
    itself is only seen in one or two very specific workloads. So add a
    module parameter to control whether the w/a's are enabled or not and
    default to not.
    
    Signed-off-by: John Harrison <John.C.Harrison@Intel.com>

--- drivers/gpu/drm/i915/i915_params.c
+++ drivers/gpu/drm/i915/i915_params.c
@@ -134,6 +134,9 @@ i915_param_named_unsafe(lmem_size, uint, 0400,
 i915_param_named_unsafe(lmem_bar_size, uint, 0400,
 			"Set the lmem bar size(in MiB).");
 
+i915_param_named(enable_mtl_rcs_ccs_wa, bool, 0400,
+	"Enable the RCS/CCS switchout hold workaround for MTL (only some workloads are affected by issue and w/a has a performance penalty) (default:false)");
+
 static void _param_print_bool(struct drm_printer *p, const char *name,
 			      bool val)
 {
