6aaf0c7185147cb3eb867dd2512b2fe2cf8f0ef3
Dmitry Osipenko <dmitry.osipenko@collabora.com>
2023-10-30 02:01:43 +0300

    drm/shmem-helper: Refactor locked/unlocked functions
    Add locked and remove unlocked postfixes from drm-shmem function names,
    making names consistent with the drm/gem core code.
    
    Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>
    Suggested-by: Boris Brezillon <boris.brezillon@collabora.com>
    Signed-off-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>

--- drivers/gpu/drm/panfrost/panfrost_gem.c
+++ drivers/gpu/drm/panfrost/panfrost_gem.c
@@ -192,7 +192,7 @@ static int panfrost_gem_pin(struct drm_gem_object *obj)
 	if (bo->is_heap)
 		return -EINVAL;
 
-	return drm_gem_shmem_pin(&bo->base);
+	return drm_gem_shmem_object_pin(obj);
 }
 
 static enum drm_gem_object_status panfrost_gem_status(struct drm_gem_object *obj)
@@ -231,8 +231,8 @@ static const struct drm_gem_object_funcs panfrost_gem_funcs = {
 	.pin = panfrost_gem_pin,
 	.unpin = drm_gem_shmem_object_unpin,
 	.get_sg_table = drm_gem_shmem_object_get_sg_table,
-	.vmap = drm_gem_shmem_object_vmap,
-	.vunmap = drm_gem_shmem_object_vunmap,
+	.vmap = drm_gem_shmem_object_vmap_locked,
+	.vunmap = drm_gem_shmem_object_vunmap_locked,
 	.mmap = drm_gem_shmem_object_mmap,
 	.status = panfrost_gem_status,
 	.rss = panfrost_gem_rss,
