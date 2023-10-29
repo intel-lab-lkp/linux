5e9a48ed360a06ab7c3530106a5df62974c9ce9d
Mirsad Goran Todorovac <mirsad.todorovac@alu.unizg.hr>
2023-10-29 19:36:05 +0100

    r8169: Coalesce mac ocp write and modify for 8125 and 8125B start to reduce spinlocks
    Repeated calls to r8168_mac_ocp_write() and r8168_mac_ocp_modify() in
    the startup of 8125 and 8125B involve implicit spin_lock_irqsave() and
    spin_unlock_irqrestore() on each invocation.
    
    Coalesced with the corresponding helpers r8168_mac_ocp_write_seq() and
    r8168_mac_ocp_modify_seq() into sequential write or modidy with a sinqle
    pair of spin_lock_irqsave() and spin_unlock_irqrestore(), these calls
    reduce overall lock contention.
    
    Fixes: f1bce4ad2f1ce ("r8169: add support for RTL8125")
    Fixes: 0439297be9511 ("r8169: add support for RTL8125B")
    Cc: Heiner Kallweit <hkallweit1@gmail.com>
    Cc: Marco Elver <elver@google.com>
    Cc: nic_swsd@realtek.com
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Eric Dumazet <edumazet@google.com>
    Cc: Jakub Kicinski <kuba@kernel.org>
    Cc: Paolo Abeni <pabeni@redhat.com>
    Cc: netdev@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Link: https://lore.kernel.org/lkml/20231028005153.2180411-1-mirsad.todorovac@alu.unizg.hr/
    Link: https://lore.kernel.org/lkml/20231028110459.2644926-1-mirsad.todorovac@alu.unizg.hr/
    Signed-off-by: Mirsad Goran Todorovac <mirsad.todorovac@alu.unizg.hr>

--- drivers/net/ethernet/realtek/r8169_main.c
+++ drivers/net/ethernet/realtek/r8169_main.c
@@ -3553,6 +3553,28 @@ DECLARE_RTL_COND(rtl_mac_ocp_e00e_cond)
 
 static void rtl_hw_start_8125_common(struct rtl8169_private *tp)
 {
+	static const struct e_info_regmaskset e_info_8125_common_1[] = {
+		{ 0xd3e2, 0x0fff, 0x03a9 },
+		{ 0xd3e4, 0x00ff, 0x0000 },
+		{ 0xe860, 0x0000, 0x0080 },
+	};
+
+	static const struct e_info_regmaskset e_info_8125_common_2[] = {
+		{ 0xc0b4, 0x0000, 0x000c },
+		{ 0xeb6a, 0x00ff, 0x0033 },
+		{ 0xeb50, 0x03e0, 0x0040 },
+		{ 0xe056, 0x00f0, 0x0030 },
+		{ 0xe040, 0x1000, 0x0000 },
+		{ 0xea1c, 0x0003, 0x0001 },
+		{ 0xe0c0, 0x4f0f, 0x4403 },
+		{ 0xe052, 0x0080, 0x0068 },
+		{ 0xd430, 0x0fff, 0x047f },
+		{ 0xea1c, 0x0004, 0x0000 },
+		{ 0xeb54, 0x0000, 0x0001 },
+	};
+
+	unsigned long flags;
+
 	rtl_pcie_state_l2l3_disable(tp);
 
 	RTL_W16(tp, 0x382, 0x221b);
@@ -3560,47 +3582,38 @@ static void rtl_hw_start_8125_common(struct rtl8169_private *tp)
 	RTL_W16(tp, 0x4800, 0);
 
 	/* disable UPS */
-	r8168_mac_ocp_modify(tp, 0xd40a, 0x0010, 0x0000);
+
+	raw_spin_lock_irqsave(&tp->mac_ocp_lock, flags);
+	__r8168_mac_ocp_modify(tp, 0xd40a, 0x0010, 0x0000);
 
 	RTL_W8(tp, Config1, RTL_R8(tp, Config1) & ~0x10);
 
-	r8168_mac_ocp_write(tp, 0xc140, 0xffff);
-	r8168_mac_ocp_write(tp, 0xc142, 0xffff);
+	__r8168_mac_ocp_write(tp, 0xc140, 0xffff);
+	__r8168_mac_ocp_write(tp, 0xc142, 0xffff);
 
-	r8168_mac_ocp_modify(tp, 0xd3e2, 0x0fff, 0x03a9);
-	r8168_mac_ocp_modify(tp, 0xd3e4, 0x00ff, 0x0000);
-	r8168_mac_ocp_modify(tp, 0xe860, 0x0000, 0x0080);
+	__r8168_mac_ocp_modify_seq(tp, e_info_8125_common_1);
 
 	/* disable new tx descriptor format */
-	r8168_mac_ocp_modify(tp, 0xeb58, 0x0001, 0x0000);
+	__r8168_mac_ocp_modify(tp, 0xeb58, 0x0001, 0x0000);
 
-	if (tp->mac_version == RTL_GIGA_MAC_VER_63)
-		r8168_mac_ocp_modify(tp, 0xe614, 0x0700, 0x0200);
-	else
-		r8168_mac_ocp_modify(tp, 0xe614, 0x0700, 0x0400);
+	if (tp->mac_version == RTL_GIGA_MAC_VER_63) {
+		__r8168_mac_ocp_modify(tp, 0xe614, 0x0700, 0x0200);
+		__r8168_mac_ocp_modify(tp, 0xe63e, 0x0c30, 0x0000);
+	} else {
+		__r8168_mac_ocp_modify(tp, 0xe614, 0x0700, 0x0400);
+		__r8168_mac_ocp_modify(tp, 0xe63e, 0x0c30, 0x0020);
+	}
 
-	if (tp->mac_version == RTL_GIGA_MAC_VER_63)
-		r8168_mac_ocp_modify(tp, 0xe63e, 0x0c30, 0x0000);
-	else
-		r8168_mac_ocp_modify(tp, 0xe63e, 0x0c30, 0x0020);
+	__r8168_mac_ocp_modify_seq(tp, e_info_8125_common_2);
+	raw_spin_unlock_irqrestore(&tp->mac_ocp_lock, flags);
 
-	r8168_mac_ocp_modify(tp, 0xc0b4, 0x0000, 0x000c);
-	r8168_mac_ocp_modify(tp, 0xeb6a, 0x00ff, 0x0033);
-	r8168_mac_ocp_modify(tp, 0xeb50, 0x03e0, 0x0040);
-	r8168_mac_ocp_modify(tp, 0xe056, 0x00f0, 0x0030);
-	r8168_mac_ocp_modify(tp, 0xe040, 0x1000, 0x0000);
-	r8168_mac_ocp_modify(tp, 0xea1c, 0x0003, 0x0001);
-	r8168_mac_ocp_modify(tp, 0xe0c0, 0x4f0f, 0x4403);
-	r8168_mac_ocp_modify(tp, 0xe052, 0x0080, 0x0068);
-	r8168_mac_ocp_modify(tp, 0xd430, 0x0fff, 0x047f);
-
-	r8168_mac_ocp_modify(tp, 0xea1c, 0x0004, 0x0000);
-	r8168_mac_ocp_modify(tp, 0xeb54, 0x0000, 0x0001);
 	udelay(1);
-	r8168_mac_ocp_modify(tp, 0xeb54, 0x0001, 0x0000);
-	RTL_W16(tp, 0x1880, RTL_R16(tp, 0x1880) & ~0x0030);
 
-	r8168_mac_ocp_write(tp, 0xe098, 0xc302);
+	raw_spin_lock_irqsave(&tp->mac_ocp_lock, flags);
+	__r8168_mac_ocp_modify(tp, 0xeb54, 0x0001, 0x0000);
+	RTL_W16(tp, 0x1880, RTL_R16(tp, 0x1880) & ~0x0030);
+	__r8168_mac_ocp_write(tp, 0xe098, 0xc302);
+	raw_spin_unlock_irqrestore(&tp->mac_ocp_lock, flags);
 
 	rtl_loop_wait_low(tp, &rtl_mac_ocp_e00e_cond, 1000, 10);
 
