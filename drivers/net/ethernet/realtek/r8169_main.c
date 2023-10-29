d68c69fc3f8553eee12a3fe3aca1377c6ac85b2d
Mirsad Goran Todorovac <mirsad.todorovac@alu.unizg.hr>
2023-10-29 19:36:01 +0100

    r8169: Coalesce mac ocp write and modify for 8168ep start to reduce spinlock contention
    Repeated calls to r8168_mac_ocp_write() and r8168_mac_ocp_modify() in
    the startup of RTL8168ep involve implicit spin_lock_irqsave() and
    spin_unlock_irqrestore() on each invocation.
    
    Coalesced with the corresponding helpers into r8168_mac_ocp_write_seq() and
    r8168_mac_ocp_modify_seq() with a sinqle paired spin_lock_irqsave() and
    spin_unlock_irqrestore(), these calls help reduce overall spinlock contention.
    
    Fixes: ef712ede3541d ("r8169: add helper r8168_mac_ocp_modify")
    Cc: Heiner Kallweit <hkallweit1@gmail.com>
    Cc: Marco Elver <elver@google.com>
    Cc: nic_swsd@realtek.com
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Eric Dumazet <edumazet@google.com>
    Cc: Jakub Kicinski <kuba@kernel.org>
    Cc: Paolo Abeni <pabeni@redhat.com>
    Cc: netdev@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Link: https://lore.kernel.org/lkml/20231028005153.2180411-1-mirsad.todorovac@alu.unizg.hr/
    Link: https://lore.kernel.org/lkml/20231028110459.2644926-1-mirsad.todorovac@alu.unizg.hr/
    Signed-off-by: Mirsad Goran Todorovac <mirsad.todorovac@alu.unizg.hr>

--- drivers/net/ethernet/realtek/r8169_main.c
+++ drivers/net/ethernet/realtek/r8169_main.c
@@ -3326,6 +3326,12 @@ static void rtl_hw_start_8168ep_3(struct rtl8169_private *tp)
 		{ 0x1e, 0x0000,	0x2000 },
 	};
 
+	static const struct e_info_regmaskset e_info_8168ep_3_mod_1[] = {
+		{ 0xd3e2, 0x0fff, 0x0271 },
+		{ 0xd3e4, 0x00ff, 0x0000 },
+		{ 0xe860, 0x0000, 0x0080 },
+	};
+
 	rtl_ephy_init(tp, e_info_8168ep_3);
 
 	rtl_hw_start_8168ep(tp);
@@ -3333,9 +3339,7 @@ static void rtl_hw_start_8168ep_3(struct rtl8169_private *tp)
 	RTL_W8(tp, DLLPR, RTL_R8(tp, DLLPR) & ~PFM_EN);
 	RTL_W8(tp, MISC_1, RTL_R8(tp, MISC_1) & ~PFM_D3COLD_EN);
 
-	r8168_mac_ocp_modify(tp, 0xd3e2, 0x0fff, 0x0271);
-	r8168_mac_ocp_modify(tp, 0xd3e4, 0x00ff, 0x0000);
-	r8168_mac_ocp_modify(tp, 0xe860, 0x0000, 0x0080);
+	r8168_mac_ocp_modify_seq(tp, e_info_8168ep_3_mod_1);
 }
 
 static void rtl_hw_start_8117(struct rtl8169_private *tp)
