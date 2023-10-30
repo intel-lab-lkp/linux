8cef3140587b2ba914e9ac229295b3ba1beaaada
NeilBrown <neilb@suse.de>
2023-10-30 12:08:38 +1100

    nfsd: rename nfsd_last_thread() to nfsd_destroy_serv()
    As this function now destroys the svc_serv, this is a better name.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- fs/nfsd/nfsctl.c
+++ fs/nfsd/nfsctl.c
@@ -707,7 +707,7 @@ static ssize_t __write_ports_addfd(char *buf, struct net *net, const struct cred
 
 	if (!nn->nfsd_serv->sv_nrthreads &&
 	    list_empty(&nn->nfsd_serv->sv_permsocks))
-		nfsd_last_thread(net);
+		nfsd_destroy_serv(net);
 
 	return err;
 }
@@ -754,7 +754,7 @@ out_close:
 out_err:
 	if (!nn->nfsd_serv->sv_nrthreads &&
 	    list_empty(&nn->nfsd_serv->sv_permsocks))
-		nfsd_last_thread(net);
+		nfsd_destroy_serv(net);
 
 	return err;
 }
