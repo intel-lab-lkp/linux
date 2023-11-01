69b8e002e69a685cc9a9af4635b9957902820621
Qu Wenruo <wqu@suse.com>
2023-11-02 07:54:50 +1030

    btrfs: add dmesg output when mounting and unmounting
    There is a feature request to add dmesg output when unmounting a btrfs.
    
    There are several alternative methods to do the same thing, but with
    their problems:
    
    - Use eBPF to watch btrfs_put_super()/open_ctree()
      Not end user friendly, they have to dip their head into the source
      code.
    
    - Watch for /sys/fs/<uuid>/
      This is way more simpler, but still requires some simple device -> uuid
      lookups.
      And a script needs to use inotify to watch /sys/fs/.
    
    Compared to all these, directly outputting the information into dmesg
    would be the most simple one, with both device and UUID included.
    
    And since we're here, also add the output when mounting a btrfs, to keep
    the dmesg paired.
    
    Now mounting a btrfs with all default mkfs options would look like this:
    
    [   81.906566] BTRFS info (device dm-8): mounting filesystem 633b5c16-afe3-4b79-b195-138fe145e4f2
    [   81.907494] BTRFS info (device dm-8): using crc32c (crc32c-intel) checksum algorithm
    [   81.908258] BTRFS info (device dm-8): using free space tree
    [   81.912644] BTRFS info (device dm-8): auto enabling async discard
    [   81.913277] BTRFS info (device dm-8): checking UUID tree
    [   91.668256] BTRFS info (device dm-8): unmounting filesystem 633b5c16-afe3-4b79-b195-138fe145e4f2
    
    Link: https://github.com/kdave/btrfs-progs/issues/689
    Signed-off-by: Qu Wenruo <wqu@suse.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -80,7 +80,11 @@ static int btrfs_remount(struct super_block *sb, int *flags, char *data);
 
 static void btrfs_put_super(struct super_block *sb)
 {
-	close_ctree(btrfs_sb(sb));
+	struct btrfs_fs_info *fs_info = btrfs_sb(sb);
+
+	btrfs_info(fs_info, "unmounting filesystem %pU",
+		   fs_info->fs_devices->fsid);
+	close_ctree(fs_info);
 }
 
 enum {
