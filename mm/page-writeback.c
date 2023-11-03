5485114b244d88ad9e363094195d4486856c8567
Li Dongyang <dongyangli@ddn.com>
2023-11-03 16:09:49 +1100

    mm: folio_wait_stable() should check for bdev
    folio_wait_stable() now checks SB_I_STABLE_WRITES
    flag on the superblock instead of backing_dev_info,
    this could trigger a false block integrity error when
    doing buffered write directly to the block device,
    as folio_wait_stable() is a noop for bdev and the
    content could be modified during writeback.
    
    Check if the folio's superblock is bdev and wait for
    writeback if the backing device requires stables_writes.
    
    Fixes: 1cb039f3dc16 ("bdi: replace BDI_CAP_STABLE_WRITES with a queue and a sb flag")
    Signed-off-by: Li Dongyang <dongyangli@ddn.com>

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -3107,7 +3107,11 @@ EXPORT_SYMBOL_GPL(folio_wait_writeback_killable);
  */
 void folio_wait_stable(struct folio *folio)
 {
-	if (folio_inode(folio)->i_sb->s_iflags & SB_I_STABLE_WRITES)
+	struct inode *inode = folio_inode(folio);
+
+	if (inode->i_sb->s_iflags & SB_I_STABLE_WRITES ||
+	    (sb_is_blkdev_sb(inode->i_sb) &&
+	     bdev_stable_writes(I_BDEV(inode))))
 		folio_wait_writeback(folio);
 }
 EXPORT_SYMBOL_GPL(folio_wait_stable);
