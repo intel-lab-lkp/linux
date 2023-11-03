99a936032887da8d7bdcece977023db242c92d10
Kefeng Wang <wangkefeng.wang@huawei.com>
2023-11-03 22:01:14 +0800

    mm: memory: use a folio in insert_page_into_pte_locked()
    Use mm_counter_file_folio() to save one compound_head() call in
    insert_page_into_pte_locked().
    
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>

--- mm/memory.c
+++ mm/memory.c
@@ -1845,11 +1845,14 @@ static int validate_page_before_insert(struct page *page)
 static int insert_page_into_pte_locked(struct vm_area_struct *vma, pte_t *pte,
 			unsigned long addr, struct page *page, pgprot_t prot)
 {
+	struct folio *folio;
+
 	if (!pte_none(ptep_get(pte)))
 		return -EBUSY;
+	folio = page_folio(page);
 	/* Ok, finally just insert the thing.. */
-	get_page(page);
-	inc_mm_counter(vma->vm_mm, mm_counter_file(page));
+	folio_get(folio);
+	inc_mm_counter(vma->vm_mm, mm_counter_file_folio(folio));
 	page_add_file_rmap(page, vma, false);
 	set_pte_at(vma->vm_mm, addr, pte, mk_pte(page, prot));
 	return 0;
