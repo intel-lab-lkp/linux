6f69498ee58c052e507f2f81a446b5f4592e1cb3
Peter Zijlstra <peterz@infradead.org>
2023-11-04 11:59:22 +0100

    sched/fair: Add trivial fair server
    Use deadline servers to service fair tasks.
    
    This patch adds a fair_server deadline entity which acts as a container
    for fair entities and can be used to fix starvation when higher priority
    (wrt fair) tasks are monopolizing CPU(s).
    
    [ dl_server do not account for rt ]
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Daniel Bristot de Oliveira <bristot@kernel.org>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -1383,6 +1383,13 @@ throttle:
 	}
 
 	/*
+	 * The fair server (sole dl_server) does not account for real-time
+	 * workload because it is running fair work.
+	 */
+	if (dl_server(dl_se))
+		return;
+
+	/*
 	 * Because -- for now -- we share the rt bandwidth, we need to
 	 * account our runtime there too, otherwise actual rt tasks
 	 * would be able to exceed the shared quota.
