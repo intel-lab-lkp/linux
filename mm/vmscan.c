d917c9927e144df6aa565611fd3bf799ec8800c7
Liu Shixin <liushixin2@huawei.com>
2023-11-04 22:03:13 +0800

    mm: vmscan: try to reclaim swapcache pages if no swap space
    When spaces of swap devices are exhausted, only file pages can be
    reclaimed.  But there are still some swapcache pages in anon lru list.
    This can lead to a premature out-of-memory.
    
    The problem is found with such step:
    
     Firstly, set a 9MB disk swap space, then create a cgroup with 10MB
     memory limit, then runs an program to allocates about 15MB memory.
    
    The problem occurs occasionally, which may need about 100 times [1].
    
    Fix it by checking number of swapcache pages in can_reclaim_anon_pages().
    If the number is not zero, return true and set swapcache_only to 1.
    When scan anon lru list in swapcache_only mode, non-swapcache pages will
    be skipped to isolate in order to accelerate reclaim efficiency.
    
    However, in swapcache_only mode, the scan count still increased when scan
    non-swapcache pages because there are large number of non-swapcache pages
    and rare swapcache pages in swapcache_only mode, and if the non-swapcache
    is skipped and do not count, the scan of pages in isolate_lru_folios() can
    eventually lead to hung task, just as Sachin reported [2].
    
    By the way, since there are enough times of memory reclaim before OOM, it
    is not need to isolate too much swapcache pages in one times.
    
    [1]. https://lore.kernel.org/lkml/CAJD7tkZAfgncV+KbKr36=eDzMnT=9dZOT0dpMWcurHLr6Do+GA@mail.gmail.com/
    [2]. https://lore.kernel.org/linux-mm/CAJD7tkafz_2XAuqE8tGLPEcpLngewhUo=5US14PAtSM9tLBUQg@mail.gmail.com/
    
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>
    Tested-by: Yosry Ahmed <yosryahmed@google.com>
    Reviewed-by: "Huang, Ying" <ying.huang@intel.com>
    Reviewed-by: Yosry Ahmed <yosryahmed@google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -136,6 +136,9 @@ struct scan_control {
 	/* Always discard instead of demoting to lower tier memory */
 	unsigned int no_demotion:1;
 
+	/* Swap space is exhausted, only reclaim swapcache for anon LRU */
+	unsigned int swapcache_only:1;
+
 	/* Allocation order */
 	s8 order;
 
@@ -308,6 +311,12 @@ static bool can_demote(int nid, struct scan_control *sc)
 	return true;
 }
 
+static void set_swapcache_mode(struct scan_control *sc, bool swapcache_only)
+{
+	if (sc)
+		sc->swapcache_only = swapcache_only;
+}
+
 static inline bool can_reclaim_anon_pages(struct mem_cgroup *memcg,
 					  int nid,
 					  struct scan_control *sc)
@@ -317,12 +326,26 @@ static inline bool can_reclaim_anon_pages(struct mem_cgroup *memcg,
 		 * For non-memcg reclaim, is there
 		 * space in any swap device?
 		 */
-		if (get_nr_swap_pages() > 0)
+		if (get_nr_swap_pages() > 0) {
+			set_swapcache_mode(sc, false);
 			return true;
+		}
+		/* Is there any swapcache pages to reclaim? */
+		if (total_swapcache_pages() > 0) {
+			set_swapcache_mode(sc, true);
+			return true;
+		}
 	} else {
 		/* Is the memcg below its swap limit? */
-		if (mem_cgroup_get_nr_swap_pages(memcg) > 0)
+		if (mem_cgroup_get_nr_swap_pages(memcg) > 0) {
+			set_swapcache_mode(sc, false);
 			return true;
+		}
+		/* Is there any swapcache pages in memcg to reclaim? */
+		if (mem_cgroup_get_nr_swapcache_pages(memcg) > 0) {
+			set_swapcache_mode(sc, true);
+			return true;
+		}
 	}
 
 	/*
@@ -1641,6 +1664,15 @@ static unsigned long isolate_lru_folios(unsigned long nr_to_scan,
 		 * Account all pages in a folio.
 		 */
 		scan += nr_pages;
+
+		/*
+		 * Count non-swapcache too because the swapcache pages may
+		 * be rare and it takes too much times here if not count
+		 * the non-swapcache pages.
+		 */
+		if (unlikely(sc->swapcache_only && !is_file_lru(lru) &&
+		    !folio_test_swapcache(folio)))
+			goto move;
 
 		if (!folio_test_lru(folio))
 			goto move;
