41b84b81e083567121a54c3ee041f87674d71ab5
Harry Wentland <harry.wentland@amd.com>
2023-11-08 11:36:34 -0500

    drm/vkms: Add enumerated 1D curve colorop
    This patch introduces a VKMS color pipeline that includes two
    drm_colorops for named transfer functions. For now the only ones
    supported are sRGB EOTF, sRGB Inverse EOTF, and a Linear TF.
    We will expand this in the future but I don't want to do so
    without accompanying IGT tests.
    
    We introduce a new vkms_luts.c file that hard-codes sRGB EOTF,
    sRGB Inverse EOTF, and a linear EOTF LUT. These have been
    generated with 256 entries each as IGT is currently testing
    only 8 bpc surfaces. We will likely need higher precision
    but I'm reluctant to make that change without clear indication
    that we need it. We'll revisit and, if necessary, regenerate
    the LUTs when we have IGT tests for higher precision buffers.
    
    v2:
     - Add commit description
     - Fix sRGB EOTF LUT definition
     - Add linear and sRGB inverse EOTF LUTs
    
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/vkms/vkms_composer.c
+++ drivers/gpu/drm/vkms/vkms_composer.c
@@ -12,6 +12,7 @@
 #include <linux/minmax.h>
 
 #include "vkms_drv.h"
+#include "vkms_luts.h"
 
 static u16 pre_mul_blend_channel(u16 src, u16 dst, u16 alpha)
 {
@@ -163,6 +164,47 @@ static void apply_lut(const struct vkms_crtc_state *crtc_state, struct line_buff
 	}
 }
 
+static void pre_blend_color_transform(const struct vkms_plane_state *plane_state, struct line_buffer *output_buffer)
+{
+	struct drm_colorop *colorop = plane_state->base.base.color_pipeline;
+
+	while (colorop) {
+		struct drm_colorop_state *colorop_state;
+
+		if (!colorop)
+			return;
+
+		/* TODO this is probably wrong */
+		colorop_state = colorop->state;
+
+		if (!colorop_state)
+			return;
+
+		for (size_t x = 0; x < output_buffer->n_pixels; x++) {
+			struct pixel_argb_u16 *pixel = &output_buffer->pixels[x];
+
+			if (colorop->type == DRM_COLOROP_1D_CURVE &&
+				colorop_state->bypass == false) {
+				switch (colorop_state->curve_1d_type) {
+					case DRM_COLOROP_1D_CURVE_SRGB_INV_EOTF:
+						pixel->r = apply_lut_to_channel_value(&srgb_inv_eotf, pixel->r, LUT_RED);
+						pixel->g = apply_lut_to_channel_value(&srgb_inv_eotf, pixel->g, LUT_GREEN);
+						pixel->b = apply_lut_to_channel_value(&srgb_inv_eotf, pixel->b, LUT_BLUE);
+						break;
+					case DRM_COLOROP_1D_CURVE_SRGB_EOTF:
+					default:
+						pixel->r = apply_lut_to_channel_value(&srgb_eotf, pixel->r, LUT_RED);
+						pixel->g = apply_lut_to_channel_value(&srgb_eotf, pixel->g, LUT_GREEN);
+						pixel->b = apply_lut_to_channel_value(&srgb_eotf, pixel->b, LUT_BLUE);
+						break;
+				}
+			}
+		}
+
+		colorop = colorop->next;
+	}
+}
+
 /**
  * blend - blend the pixels from all planes and compute crc
  * @wb: The writeback frame buffer metadata
@@ -200,6 +242,9 @@ static void blend(struct vkms_writeback_job *wb,
 				continue;
 
 			vkms_compose_row(stage_buffer, plane[i], y_pos);
+
+			pre_blend_color_transform(plane[i], stage_buffer);
+
 			pre_mul_alpha_blend(plane[i]->frame_info, stage_buffer,
 					    output_buffer);
 		}
