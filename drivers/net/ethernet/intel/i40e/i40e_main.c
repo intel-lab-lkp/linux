62e640cfc594d9325943be7f6948a01b9bd422a9
Ivan Vecera <ivecera@redhat.com>
2023-11-08 16:10:18 +0100

    i40e: Fix max frame size check
    Commit 3a2c6ced90e1 ("i40e: Add a check to see if MFS is set") added
    a check for port's MFS (max frame size) value. The value is stored
    in PRTGL_SAH register for each physical port. According datasheet this
    register is defined as:
    
    PRTGL_SAH[PRT]: (0x001E2140 + 0x4*PRT, PRT=0...3)
    
    where PRT is physical port number.
    
    The existing check does not take port number into account and reads
    actually MFS value always for port 0 that is correct for PF 0 but
    not for other PFs.
    
    Update PRTGL_SAH register definition so it takes a port number as
    an argument and fix the check by passing the port number.
    Also fix the warning message that use for a port number a local
    variable 'i' that really does not contain such information.
    
    Fixes: 3a2c6ced90e1 ("i40e: Add a check to see if MFS is set")
    Cc: Todd Fujinaka <todd.fujinaka@intel.com>
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

--- drivers/net/ethernet/intel/i40e/i40e_main.c
+++ drivers/net/ethernet/intel/i40e/i40e_main.c
@@ -16219,11 +16219,11 @@ static int i40e_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	/* make sure the MFS hasn't been set lower than the default */
 #define MAX_FRAME_SIZE_DEFAULT 0x2600
-	val = (rd32(&pf->hw, I40E_PRTGL_SAH) &
+	val = (rd32(&pf->hw, I40E_PRTGL_SAH(pf->hw.port)) &
 	       I40E_PRTGL_SAH_MFS_MASK) >> I40E_PRTGL_SAH_MFS_SHIFT;
 	if (val < MAX_FRAME_SIZE_DEFAULT)
 		dev_warn(&pdev->dev, "MFS for port %x has been set below the default: %x\n",
-			 i, val);
+			 pf->hw.port, val);
 
 	/* Add a filter to drop all Flow control frames from any VSI from being
 	 * transmitted. By doing so we stop a malicious VF from sending out
