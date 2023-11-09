940f03d08b4afb8b66a40c5839d5a33ab6a050cc
Kuan-Wei Chiu <visitorckw@gmail.com>
2023-11-10 02:54:36 +0800

    platform/chrome: Fix typo 'lantency' in comment
    Replace 'lantency' with 'latency' in the comment.
    
    Signed-off-by: Kuan-Wei Chiu <visitorckw@gmail.com>

--- drivers/platform/chrome/cros_ec_sensorhub_ring.c
+++ drivers/platform/chrome/cros_ec_sensorhub_ring.c
@@ -176,7 +176,7 @@ static s64 cros_ec_sensor_ring_median(s64 *array, size_t length)
  * While a and b are recorded at accurate times (due to the EC real time
  * nature); c is pretty untrustworthy, even though it's recorded the
  * first thing in ec_irq_handler(). There is a very good change we'll get
- * added lantency due to:
+ * added latency due to:
  *   other irqs
  *   ddrfreq
  *   cpuidle
