fea17dec2ea852e5d484258217fe5fa968d7ff8e
Manikanta Guntupalli <manikanta.guntupalli@amd.com>
2023-11-09 18:06:40 +0530

    serial: uartlite: Use dynamic allocation for major number
    Device number 204 has a range of minors on major number.
    uart_register_driver is failing due to lack of minor numbers
    when more number of uart ports used. So, use dynamic allocation
    for major number to avoid minor number limitation on 204 major
    number.
    
    https://docs.kernel.org/arch/arm/sa1100/serial_uart.html
    
    Signed-off-by: Manikanta Guntupalli <manikanta.guntupalli@amd.com>

--- drivers/tty/serial/uartlite.c
+++ drivers/tty/serial/uartlite.c
@@ -24,8 +24,8 @@
 #include <linux/pm_runtime.h>
 
 #define ULITE_NAME		"ttyUL"
-#define ULITE_MAJOR		204
-#define ULITE_MINOR		187
+#define ULITE_MAJOR		0	/* use dynamic node allocation */
+#define ULITE_MINOR		0
 #define ULITE_NR_UARTS		CONFIG_SERIAL_UARTLITE_NR_UARTS
 
 /* ---------------------------------------------------------------------
