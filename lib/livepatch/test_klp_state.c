5a3d581460eba7900527e0c0ed097ddabcab6e23
Petr Mladek <pmladek@suse.com>
2023-11-10 18:04:24 +0100

    livepatch: Use per-state callbacks in state API tests
    Recent changes in the livepatch core have allowed to connect states,
    shadow variables, and callbacks. Use these new features in
    the state tests.
    
    Use the shadow variable API to store the original loglevel. It is
    better suited for this purpose than directly accessing the .data
    pointer in state klp_state.
    
    Another big advantage is that the shadow variable is preserved
    when the current patch is replaced by a new version. As a result,
    there is not need to copy the pointer.
    
    Finally, the lifetime of the shadow variable is connected with
    the lifetime of the state. It is freed automatically when
    it is not longer supported.
    
    This results into the following changes in the code:
    
      + Rename CONSOLE_LOGLEVEL_STATE -> CONSOLE_LOGLEVEL_FIX_ID
        because it will be used also the for shadow variable
    
      + Remove the extra code for module coming and going states
        because the new callback are per-state.
    
      + Remove callbacks needed to transfer the pointer between
        states.
    
      + Keep the versioning of the state to prevent downgrade.
        The problem is artificial because no callbacks are
        needed to transfer or free the shadow variable anymore.
    
    Signed-off-by: Petr Mladek <pmladek@suse.com>

--- lib/livepatch/test_klp_state.c
+++ lib/livepatch/test_klp_state.c
@@ -9,108 +9,109 @@
 #include <linux/printk.h>
 #include <linux/livepatch.h>
 
-#define CONSOLE_LOGLEVEL_STATE 1
-/* Version 1 does not support migration. */
-#define CONSOLE_LOGLEVEL_STATE_VERSION 1
+#define CONSOLE_LOGLEVEL_FIX_ID 1
 
-static const char *const module_state[] = {
-	[MODULE_STATE_LIVE]	= "[MODULE_STATE_LIVE] Normal state",
-	[MODULE_STATE_COMING]	= "[MODULE_STATE_COMING] Full formed, running module_init",
-	[MODULE_STATE_GOING]	= "[MODULE_STATE_GOING] Going away",
-	[MODULE_STATE_UNFORMED]	= "[MODULE_STATE_UNFORMED] Still setting it up",
-};
-
-static void callback_info(const char *callback, struct klp_object *obj)
-{
-	if (obj->mod)
-		pr_info("%s: %s -> %s\n", callback, obj->mod->name,
-			module_state[obj->mod->state]);
-	else
-		pr_info("%s: vmlinux\n", callback);
-}
+/*
+ * Version of the state which defines compatibility of livepaches.
+ * The value is artificial. It set just for testing the compatibility
+ * checks. In reality, all versions are compatible because all
+ * the callbacks do nothing and the shadow variable clean up
+ * is done by the core.
+ */
+#ifndef CONSOLE_LOGLEVEL_FIX_VERSION
+#define CONSOLE_LOGLEVEL_FIX_VERSION 1
+#endif
 
 static struct klp_patch patch;
 
 static int allocate_loglevel_state(void)
 {
-	struct klp_state *loglevel_state;
+	int *shadow_console_loglevel;
 
-	loglevel_state = klp_get_state(&patch, CONSOLE_LOGLEVEL_STATE);
-	if (!loglevel_state)
-		return -EINVAL;
+	/* Make sure that the shadow variable does not exist yet. */
+	shadow_console_loglevel =
+		klp_shadow_alloc(&console_loglevel, CONSOLE_LOGLEVEL_FIX_ID,
+				 sizeof(*shadow_console_loglevel), GFP_KERNEL,
+				 NULL, NULL);
 
-	loglevel_state->data = kzalloc(sizeof(console_loglevel), GFP_KERNEL);
-	if (!loglevel_state->data)
+	if (!shadow_console_loglevel) {
+		pr_err("%s: failed to allocated shadow variable for storing original loglevel\n",
+		       __func__);
 		return -ENOMEM;
+	}
 
 	pr_info("%s: allocating space to store console_loglevel\n",
 		__func__);
+
 	return 0;
 }
 
 static void fix_console_loglevel(void)
 {
-	struct klp_state *loglevel_state;
+	int *shadow_console_loglevel;
 
-	loglevel_state = klp_get_state(&patch, CONSOLE_LOGLEVEL_STATE);
-	if (!loglevel_state)
+	shadow_console_loglevel =
+		(int *)klp_shadow_get(&console_loglevel, CONSOLE_LOGLEVEL_FIX_ID);
+	if (!shadow_console_loglevel)
 		return;
 
 	pr_info("%s: fixing console_loglevel\n", __func__);
-	*(int *)loglevel_state->data = console_loglevel;
+	*shadow_console_loglevel = console_loglevel;
 	console_loglevel = CONSOLE_LOGLEVEL_MOTORMOUTH;
 }
 
 static void restore_console_loglevel(void)
 {
-	struct klp_state *loglevel_state;
+	int *shadow_console_loglevel;
 
-	loglevel_state = klp_get_state(&patch, CONSOLE_LOGLEVEL_STATE);
-	if (!loglevel_state)
+	shadow_console_loglevel =
+		(int *)klp_shadow_get(&console_loglevel, CONSOLE_LOGLEVEL_FIX_ID);
+	if (!shadow_console_loglevel)
 		return;
 
 	pr_info("%s: restoring console_loglevel\n", __func__);
-	console_loglevel = *(int *)loglevel_state->data;
+	console_loglevel = *shadow_console_loglevel;
 }
 
 static void free_loglevel_state(void)
 {
-	struct klp_state *loglevel_state;
+	int *shadow_console_loglevel;
 
-	loglevel_state = klp_get_state(&patch, CONSOLE_LOGLEVEL_STATE);
-	if (!loglevel_state)
+	shadow_console_loglevel =
+		(int *)klp_shadow_get(&console_loglevel, CONSOLE_LOGLEVEL_FIX_ID);
+	if (!shadow_console_loglevel)
 		return;
 
 	pr_info("%s: freeing space for the stored console_loglevel\n",
 		__func__);
-	kfree(loglevel_state->data);
+	klp_shadow_free(&console_loglevel, CONSOLE_LOGLEVEL_FIX_ID, NULL);
 }
 
-/* Executed on object patching (ie, patch enablement) */
-static int pre_patch_callback(struct klp_object *obj)
+/* Executed before patching when the state is new. */
+static int setup_state_callback(struct klp_patch *patch, struct klp_state *state)
 {
-	callback_info(__func__, obj);
+	pr_info("%s: state %lu\n", __func__, state->id);
 	return allocate_loglevel_state();
 }
 
-/* Executed on object unpatching (ie, patch disablement) */
-static void post_patch_callback(struct klp_object *obj)
+/* Executed after patching when the state is new. */
+static void enable_state_callback(struct klp_patch *patch, struct klp_state *state)
 {
-	callback_info(__func__, obj);
+	pr_info("%s: state %lu\n", __func__, state->id);
 	fix_console_loglevel();
 }
 
-/* Executed on object unpatching (ie, patch disablement) */
-static void pre_unpatch_callback(struct klp_object *obj)
+/* Executed before unpatching when the state is obsoleted. */
+static void disable_state_callback(struct klp_patch *patch, struct klp_state *state)
 {
-	callback_info(__func__, obj);
+	pr_info("%s: state %lu\n", __func__, state->id);
 	restore_console_loglevel();
 }
 
-/* Executed on object unpatching (ie, patch disablement) */
-static void post_unpatch_callback(struct klp_object *obj)
+/* Executed after unpatching when the state is obsoleted. */
+static void release_state_callback(struct klp_patch *patch, struct klp_state *state)
 {
-	callback_info(__func__, obj);
+	pr_info("%s: state %lu\n", __func__, state->id);
 	free_loglevel_state();
 }
 
@@ -122,19 +123,19 @@ static struct klp_object objs[] = {
 	{
 		.name = NULL,	/* vmlinux */
 		.funcs = no_funcs,
-		.callbacks = {
-			.pre_patch = pre_patch_callback,
-			.post_patch = post_patch_callback,
-			.pre_unpatch = pre_unpatch_callback,
-			.post_unpatch = post_unpatch_callback,
-		},
 	}, { }
 };
 
 static struct klp_state states[] = {
 	{
-		.id = CONSOLE_LOGLEVEL_STATE,
-		.version = CONSOLE_LOGLEVEL_STATE_VERSION,
+		.id = CONSOLE_LOGLEVEL_FIX_ID,
+		.version = CONSOLE_LOGLEVEL_FIX_VERSION,
+		.callbacks = {
+			.setup = setup_state_callback,
+			.enable = enable_state_callback,
+			.disable = disable_state_callback,
+			.release = release_state_callback,
+		},
 	}, { }
 };
 
