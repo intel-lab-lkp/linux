c2f2cb0948ace4b958a1dc2f2583cedda7d31e90
Manikanta Mylavarapu <quic_mmanikan@quicinc.com>
2023-11-10 14:49:34 +0530

    firmware: qcom_scm: ipq5332: add support to pass metadata size
    IPQ5332 security software running under trustzone
    requires metadata size. With V2 cmd, pass metadata
    size as well.
    
    Signed-off-by: Manikanta Mylavarapu <quic_mmanikan@quicinc.com>

--- drivers/firmware/qcom/qcom_scm.h
+++ drivers/firmware/qcom/qcom_scm.h
@@ -93,6 +93,7 @@ int scm_legacy_call(struct device *dev, const struct qcom_scm_desc *desc,
 
 #define QCOM_SCM_SVC_PIL		0x02
 #define QCOM_SCM_PIL_PAS_INIT_IMAGE	0x01
+#define QCOM_SCM_PAS_INIT_IMAGE_V2	0x1a
 #define QCOM_SCM_PIL_PAS_MEM_SETUP	0x02
 #define QCOM_SCM_PIL_PAS_AUTH_AND_RESET	0x05
 #define QCOM_SCM_PIL_PAS_SHUTDOWN	0x06
