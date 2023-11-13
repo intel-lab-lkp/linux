5527f1e82087f5b7bdb2068fca44cf121140fc13
Lukas Bulwahn <lukas.bulwahn@gmail.com>
2023-11-13 13:55:22 +0100

    lib/xz: remove IA-64 support from library
    Among many removals, commit cf8e8658100d ("arch: Remove Itanium (IA-64)
    architecture") removes the config XZ_DEC_IA64, but does not remove the code
    for IA64 architecture support in lib/xz/.
    
    Remove the definitions XZ_DEC_IA64, bcj_ia64(), BCJ_IA64 and all its
    references.
    
    Note that in the ifdef expression for architectures to enable the generic
    support for BCJ encoders, XZ_DEC_ARM was listed twice, so drop this
    duplicated conditional while removing XZ_DEC_IA64 from the condition.
    
    Signed-off-by: Lukas Bulwahn <lukas.bulwahn@gmail.com>

--- lib/xz/xz_private.h
+++ lib/xz/xz_private.h
@@ -25,9 +25,6 @@
 #		ifdef CONFIG_XZ_DEC_POWERPC
 #			define XZ_DEC_POWERPC
 #		endif
-#		ifdef CONFIG_XZ_DEC_IA64
-#			define XZ_DEC_IA64
-#		endif
 #		ifdef CONFIG_XZ_DEC_ARM
 #			define XZ_DEC_ARM
 #		endif
@@ -98,7 +95,6 @@
  */
 #ifndef XZ_DEC_BCJ
 #	if defined(XZ_DEC_X86) || defined(XZ_DEC_POWERPC) \
-			|| defined(XZ_DEC_IA64) || defined(XZ_DEC_ARM) \
 			|| defined(XZ_DEC_ARM) || defined(XZ_DEC_ARMTHUMB) \
 			|| defined(XZ_DEC_SPARC)
 #		define XZ_DEC_BCJ
