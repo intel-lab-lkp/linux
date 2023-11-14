58d79ed1d457fc898b305dcdf0b1d1a98a7af97c
Bagas Sanjaya <bagasdotme@gmail.com>
2023-11-14 16:12:43 +0700

    fs: anon-inode: Prepend blank line separator to anon_inode_create_getfile() reasons list
    Stephen Rothwell reported htmldocs warning when merging kvm tree:
    
    Documentation/filesystems/api-summary:74: fs/anon_inodes.c:167: ERROR: Unexpected indentation.
    Documentation/filesystems/api-summary:74: fs/anon_inodes.c:168: WARNING: Block quote ends without a blank line; unexpected unindent.
    
    Fix them by prepending blank line separator to reasons list of
    anon_inode_create_getfile() to render the list with correct items.
    
    Fixes: e4c866e72563ed ("fs: Rename anon_inode_getfile_secure() and anon_inode_getfd_secure()")
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Closes: https://lore.kernel.org/linux-next/20231114141326.38a3dcd4@canb.auug.org.au/
    Signed-off-by: Bagas Sanjaya <bagasdotme@gmail.com>

--- fs/anon_inodes.c
+++ fs/anon_inodes.c
@@ -163,6 +163,7 @@ EXPORT_SYMBOL_GPL(anon_inode_getfile);
  *
  * Create a new anonymous inode and file pair.  This can be done for two
  * reasons:
+ *
  * - for the inode to have its own security context, so that LSMs can enforce
  *   policy on the inode's creation;
  * - if the caller needs a unique inode, for example in order to customize
