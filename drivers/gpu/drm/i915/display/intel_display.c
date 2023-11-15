33d7bbf1a282e93ebd6940f586669eb5aa39dd4b
Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>
2023-11-15 19:13:26 +0530

    drm/i915: Compute CMRR and calculate vtotal
    Compute Fixed Average Vtotal/CMRR with resepect to
    userspace VRR enablement. Also calculate required
    parameters in case of CMRR is  enabled. During
    intel_vrr_compute_config, CMRR is getting enabled
    based on userspace has enabled Adaptive Sync Vtotal
    mode (Legacy VRR) or not.
    
    Signed-off-by: Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -5482,6 +5482,7 @@ intel_pipe_config_compare(const struct intel_crtc_state *current_config,
 		PIPE_CONF_CHECK_I(vrr.guardband);
 		PIPE_CONF_CHECK_LLI(cmrr.cmrr_m);
 		PIPE_CONF_CHECK_LLI(cmrr.cmrr_n);
+		PIPE_CONF_CHECK_BOOL(cmrr.enable);
 	}
 
 #undef PIPE_CONF_CHECK_X
