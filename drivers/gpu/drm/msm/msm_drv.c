681e1065bc1209b51ca96fbd4048994c6f8e51c3
Dipam Turkar <dipamt1729@gmail.com>
2023-11-15 19:49:29 +0530

    Remove custom dumb_map_offset implementation in msm driver
    Make msm use drm_gem_create_map_offset() instead of its custom
    implementation for associating GEM object with a fake offset. Since,
    we already have this generic implementation, we don't need the custom
    implementation and it is better to standardize the code for GEM based
    drivers. This also removes the outdated locking leftovers.
    
    Signed-off-by: Dipam Turkar <dipamt1729@gmail.com>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -809,7 +809,7 @@ static const struct drm_driver msm_driver = {
 	.open               = msm_open,
 	.postclose          = msm_postclose,
 	.dumb_create        = msm_gem_dumb_create,
-	.dumb_map_offset    = msm_gem_dumb_map_offset,
+	.dumb_map_offset    = drm_gem_dumb_map_offset,
 	.gem_prime_import_sg_table = msm_gem_prime_import_sg_table,
 #ifdef CONFIG_DEBUG_FS
 	.debugfs_init       = msm_debugfs_init,
