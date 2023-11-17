3c7864bf1bf723b452561e929554a68fdcbb68f1
Sakari Ailus <sakari.ailus@linux.intel.com>
2023-11-17 13:14:27 +0200

    pm: runtime: Simplify pm_runtime_get_if_active() usage
    The majority of the callers currently using pm_runtime_get_if_active()
    call it with ign_usage_count argument set to true. There's only one driver
    using this feature (and natually implementation of
    pm_runtime_get_if_in_use()).
    
    To make this function more practical to use, remove the ign_usage_count
    argument from the function. The main implementation is renamed as
    __pm_runtime_get_conditional().
    
    This function is expected to gain a large number of users in the future

--- drivers/base/power/runtime.c
+++ drivers/base/power/runtime.c
@@ -1175,7 +1175,7 @@ int __pm_runtime_resume(struct device *dev, int rpmflags)
 EXPORT_SYMBOL_GPL(__pm_runtime_resume);
 
 /**
- * pm_runtime_get_if_active - Conditionally bump up device usage counter.
+ * __pm_runtime_get_conditional - Conditionally bump up device usage counter.
  * @dev: Device to handle.
  * @ign_usage_count: Whether or not to look at the current usage counter value.
  *
@@ -1195,8 +1195,11 @@ EXPORT_SYMBOL_GPL(__pm_runtime_resume);
  *
  * The caller is responsible for decrementing the runtime PM usage counter of
  * @dev after this function has returned a positive value for it.
+ *
+ * This function is not intended to be called by drivers, use
+ * pm_runtime_get_if_active() or pm_runtime_get_if_in_use() instead.
  */
-int pm_runtime_get_if_active(struct device *dev, bool ign_usage_count)
+int __pm_runtime_get_conditional(struct device *dev, bool ign_usage_count)
 {
 	unsigned long flags;
 	int retval;
@@ -1217,7 +1220,7 @@ int pm_runtime_get_if_active(struct device *dev, bool ign_usage_count)
 
 	return retval;
 }
-EXPORT_SYMBOL_GPL(pm_runtime_get_if_active);
+EXPORT_SYMBOL_GPL(__pm_runtime_get_conditional);
 
 /**
  * __pm_runtime_set_status - Set runtime PM status of a device.
