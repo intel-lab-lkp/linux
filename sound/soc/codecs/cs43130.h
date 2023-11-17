89753ac57cd61ee68baff9b625875d2ee72555e4
Maciej Strozek <mstrozek@opensource.cirrus.com>
2023-11-17 14:13:41 +0000

    ASoC: cs43130: Store device in private struct and use it more consistently
    Also remove one unnecessary debug print
    
    Signed-off-by: Maciej Strozek <mstrozek@opensource.cirrus.com>

--- sound/soc/codecs/cs43130.h
+++ sound/soc/codecs/cs43130.h
@@ -501,6 +501,7 @@ struct cs43130_dai {
 };
 
 struct	cs43130_private {
+	struct device			*dev;
 	struct snd_soc_component	*component;
 	struct regmap			*regmap;
 	struct regulator_bulk_data	supplies[CS43130_NUM_SUPPLIES];
