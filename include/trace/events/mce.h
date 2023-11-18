7a9759c108ff417943ae2e7f3fabaa6e61f97b11
Avadhut Naik <Avadhut.Naik@amd.com>
2023-11-18 13:32:45 -0600

    x86/mce: Add wrapper for struct mce to export vendor specific info
    Currently, exporting new additional machine check error information
    involves adding new fields for the same at the end of the struct mce.
    This additional information can then be consumed through mcelog or
    tracepoint.
    
    However, as new MSRs are being added (and will be added in the future)
    by CPU vendors on their newer CPUs with additional machine check error
    information to be exported, the size of struct mce will balloon on some
    CPUs, unnecessarily, since those fields are vendor-specific. Moreover,
    different CPU vendors may export the additional information in varying
    sizes.
    
    The problem particularly intensifies since struct mce is exposed to
    userspace as part of UAPI. It's bloating through vendor-specific data
    should be avoided to limit the information being sent out to userspace.
    
    Add a new structure mce_hw_err to wrap the existing struct mce. The same
    will prevent its ballooning since vendor-specifc data, if any, can now be
    exported through a union within the wrapper structure and through
    __dynamic_array in mce_record tracepoint.
    
    Furthermore, new internal kernel fields can be added to the wrapper
    struct without impacting the user space API.
    
    Note: Some Checkpatch checks have been ignored to maintain coding style.
    
    [Yazen: Add last commit message paragraph. Rebase on other MCA updates.]
    
    Suggested-by: Borislav Petkov (AMD) <bp@alien8.de>
    Signed-off-by: Avadhut Naik <Avadhut.Naik@amd.com>
    Signed-off-by: Yazen Ghannam <yazen.ghannam@amd.com>

--- include/trace/events/mce.h
+++ include/trace/events/mce.h
@@ -11,9 +11,9 @@
 
 TRACE_EVENT(mce_record,
 
-	TP_PROTO(struct mce *m),
+	TP_PROTO(struct mce_hw_err *err),
 
-	TP_ARGS(m),
+	TP_ARGS(err),
 
 	TP_STRUCT__entry(
 		__field(	u64,		mcgcap		)
@@ -36,23 +36,23 @@ TRACE_EVENT(mce_record,
 	),
 
 	TP_fast_assign(
-		__entry->mcgcap		= m->mcgcap;
-		__entry->mcgstatus	= m->mcgstatus;
-		__entry->status		= m->status;
-		__entry->addr		= m->addr;
-		__entry->misc		= m->misc;
-		__entry->synd		= m->synd;
-		__entry->ipid		= m->ipid;
-		__entry->ip		= m->ip;
-		__entry->tsc		= m->tsc;
-		__entry->walltime	= m->time;
-		__entry->cpu		= m->extcpu;
-		__entry->cpuid		= m->cpuid;
-		__entry->apicid		= m->apicid;
-		__entry->socketid	= m->socketid;
-		__entry->cs		= m->cs;
-		__entry->bank		= m->bank;
-		__entry->cpuvendor	= m->cpuvendor;
+		__entry->mcgcap		= err->m.mcgcap;
+		__entry->mcgstatus	= err->m.mcgstatus;
+		__entry->status		= err->m.status;
+		__entry->addr		= err->m.addr;
+		__entry->misc		= err->m.misc;
+		__entry->synd		= err->m.synd;
+		__entry->ipid		= err->m.ipid;
+		__entry->ip		= err->m.ip;
+		__entry->tsc		= err->m.tsc;
+		__entry->walltime	= err->m.time;
+		__entry->cpu		= err->m.extcpu;
+		__entry->cpuid		= err->m.cpuid;
+		__entry->apicid		= err->m.apicid;
+		__entry->socketid	= err->m.socketid;
+		__entry->cs		= err->m.cs;
+		__entry->bank		= err->m.bank;
+		__entry->cpuvendor	= err->m.cpuvendor;
 	),
 
 	TP_printk("CPU: %d, MCGc/s: %llx/%llx, MC%d: %016Lx, IPID: %016Lx, ADDR/MISC/SYND: %016Lx/%016Lx/%016Lx, RIP: %02x:<%016Lx>, TSC: %llx, PROCESSOR: %u:%x, TIME: %llu, SOCKET: %u, APIC: %x",
