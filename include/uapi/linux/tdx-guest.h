intel/td-partitioning

virt: tdx-guest: Add RTMR update interface
Intel TDX guests support 1 MRTD and 4 RTMR registers to record the
various details of the TDX guest. It is mainly used to support
applications like attestation and trusted boot. The TDX guest's static
information, such as the TDVF code, configuration, etc., are recorded
in the MRTD register. RTMR measurement registers are used to record
measurements related to data loaded into the TDX guest at run-time
(like TDVF configuration, GPT, kernel, initrd, command line params,
etc). To facilitate RTMR register update, the TDX Module provides
support for TDG.MR.RTMR.EXTEND TDCALL  which can be used to securely
extend the RTMR registers. More details about TDG.MR.RTMR.EXTEND can be
found in TDX Module specificaiton, v1.0, section titled
"TDG.MR.RTMR.EXTEND Leaf".

As per TDVF specification, section titled "Measurement Register Usage
in TD", following is the recommended usage of RTMR registers.

RTMR[0] is for the TDVF configuration.
RTMR[1] is for the TD OS loader or kernel.
RTMR[2] is for the OS application.
RTMR[3] is reserved for special usage only.

Add an IOCTL interface to allow userspace to update RTMR registers > 1
since RTMR[2] and RTMR[3] can be used by OS applications.

Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

include/uapi/linux/tdx-guest.h
@@ -20,6 +20,9 @@
 /* Length of the REPORTMACSTRUCT */
 #define TDX_REPORTMACSTRUCT_LEN         256
 
+/* Length of RTMR extend data */
+#define TDX_EXTEND_RTMR_DATA_LEN        48
+
 /**
  * struct tdx_report_req - Request struct for TDX_CMD_GET_REPORT0 IOCTL.
  *
@@ -46,6 +49,19 @@ struct tdx_verify_report_req {
 	__u64 err_code;
 };
 
+/**
+ * struct tdx_extend_rtmr_req - Request struct for TDX_CMD_EXTEND_RTMR IOCTL.
+ *
+ * @data: User buffer with RTMR extend data.
+ * @index: Index of RTMR register to be extended. RTMR0 and RTMR1 registers
+ *         are used by kernel and BIOS and hence are not allowed to be extended
+ *         by the userspace.
+ */
+struct tdx_extend_rtmr_req {
+	__u8 data[TDX_EXTEND_RTMR_DATA_LEN];
+	__u8 index;
+};
+
 /*
  * TDX_CMD_GET_REPORT0 - Get TDREPORT0 (a.k.a. TDREPORT subtype 0) using
  *                       TDCALL[TDG.MR.REPORT]
@@ -62,5 +78,13 @@ struct tdx_verify_report_req {
  * Returns 0 on success, and standard errono on other failures.
  */
 #define TDX_CMD_VERIFY_REPORT		_IOR('T', 2, struct tdx_verify_report_req)
+
+/*
+ * TDX_CMD_EXTEND_RTMR - Extend RTMR registers with user data using
+ *                       TDG.MR.RTMR.EXTEND TDCALL.
+ *
+ * Returns 0 on success, and standard errono on other failures.
+ */
+#define TDX_CMD_EXTEND_RTMR		_IOR('T', 3, struct tdx_extend_rtmr_req)
 
 #endif /* _UAPI_LINUX_TDX_GUEST_H_ */

intel-lab-lkp/linux

include/uapi/linux/tdx-guest.h
@@ -20,6 +20,9 @@
 /* Length of the REPORTMACSTRUCT */
 #define TDX_REPORTMACSTRUCT_LEN         256
 
+/* Length of RTMR extend data */
+#define TDX_EXTEND_RTMR_DATA_LEN        48
+
 /**
  * struct tdx_report_req - Request struct for TDX_CMD_GET_REPORT0 IOCTL.
  *
@@ -46,6 +49,19 @@ struct tdx_verify_report_req {
 	__u64 err_code;
 };
 
+/**
+ * struct tdx_extend_rtmr_req - Request struct for TDX_CMD_EXTEND_RTMR IOCTL.
+ *
+ * @data: User buffer with RTMR extend data.
+ * @index: Index of RTMR register to be extended. RTMR0 and RTMR1 registers
+ *         are used by kernel and BIOS and hence are not allowed to be extended
+ *         by the userspace.
+ */
+struct tdx_extend_rtmr_req {
+	__u8 data[TDX_EXTEND_RTMR_DATA_LEN];
+	__u8 index;
+};
+
 /*
  * TDX_CMD_GET_REPORT0 - Get TDREPORT0 (a.k.a. TDREPORT subtype 0) using
  *                       TDCALL[TDG.MR.REPORT]
@@ -62,5 +78,13 @@ struct tdx_verify_report_req {
  * Returns 0 on success, and standard errono on other failures.
  */
 #define TDX_CMD_VERIFY_REPORT		_IOWR('T', 2, struct tdx_verify_report_req)
+
+/*
+ * TDX_CMD_EXTEND_RTMR - Extend RTMR registers with user data using
+ *                       TDG.MR.RTMR.EXTEND TDCALL.
+ *
+ * Returns 0 on success, and standard errono on other failures.
+ */
+#define TDX_CMD_EXTEND_RTMR		_IOW('T', 3, struct tdx_extend_rtmr_req)
 
 #endif /* _UAPI_LINUX_TDX_GUEST_H_ */