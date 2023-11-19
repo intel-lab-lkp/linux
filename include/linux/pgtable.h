66bddf9dbdcc9c3284cc4818d9de63b726e37e67
Alexandru Elisei <alexandru.elisei@arm.com>
2023-11-19 16:57:03 +0000

    mm: Allow an arch to hook into folio allocation when VMA is known
    An architecture might want to fixup the gfp flags based on the type of VMA
    where the page will be mapped.
    
    On arm64, this is currently used if the VMA is MTE enabled. When
    __GFP_TAGGED is set, for performance reasons, tag zeroing is performed at
    the same time as the data is zeroed, instead of being performed separately,
    in set_pte_at() -> mte_sync_tags().
    
    Its usage will be expanded when the storage for the tags will have to be
    explicitely managed by the kernel.
    
    Signed-off-by: Alexandru Elisei <alexandru.elisei@arm.com>

--- include/linux/pgtable.h
+++ include/linux/pgtable.h
@@ -873,6 +873,13 @@ static inline void arch_do_swap_page(struct mm_struct *mm,
 }
 #endif
 
+#ifndef __HAVE_ARCH_CALC_VMA_GFP
+static inline gfp_t arch_calc_vma_gfp(struct vm_area_struct *vma, gfp_t gfp)
+{
+	return 0;
+}
+#endif
+
 #ifndef __HAVE_ARCH_PREP_NEW_PAGE
 static inline int arch_prep_new_page(struct page *page, int order, gfp_t gfp)
 {
