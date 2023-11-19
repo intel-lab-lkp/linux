8162bc73d2d294d7f3eda8b559f6721006157715
Alexandru Elisei <alexandru.elisei@arm.com>
2023-11-19 16:57:01 +0000

    mm: page_alloc: Add an arch hook to filter MIGRATE_CMA allocations
    As an architecture might have specific requirements around the allocation
    of CMA pages, add an arch hook that can disable allocations from
    MIGRATE_CMA, if the allocation was otherwise allowed.
    
    This will be used by arm64, which will put tag storage pages on the
    MIGRATE_CMA list, pages which have specific limitations.
    
    Signed-off-by: Alexandru Elisei <alexandru.elisei@arm.com>

--- include/linux/pgtable.h
+++ include/linux/pgtable.h
@@ -884,6 +884,13 @@ static inline int arch_prep_new_page(struct page *page, int order, gfp_t gfp)
 static inline void arch_free_pages_prepare(struct page *page, int order) { }
 #endif
 
+#ifndef __HAVE_ARCH_ALLOC_CMA
+static inline bool arch_alloc_cma(gfp_t gfp)
+{
+	return true;
+}
+#endif
+
 #ifndef __HAVE_ARCH_UNMAP_ONE
 /*
  * Some architectures support metadata associated with a page. When a
