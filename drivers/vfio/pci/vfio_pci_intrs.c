6ad30eb4d836025ada1d6ffa3722556617c706be
JianChunfu <chunfu.jian@shingroup.cn>
2023-11-20 11:17:52 +0800

    vfio/pci: Separate INTx-enabled vfio_pci_device from unenabled to make the code logic clearer.
    It seems a little unclear when dealing with vfio_intx_set_signal()
    because of vfio_pci_device which is irq_none,
    so separate the two situations.
    
    Signed-off-by: JianChunfu <chunfu.jian@shingroup.cn>

--- drivers/vfio/pci/vfio_pci_intrs.c
+++ drivers/vfio/pci/vfio_pci_intrs.c
@@ -455,6 +455,8 @@ static int vfio_pci_set_intx_trigger(struct vfio_pci_device *vdev,
 				     unsigned index, unsigned start,
 				     unsigned count, uint32_t flags, void *data)
 {
+	int32_t fd = *(int32_t *)data;
+
 	if (is_intx(vdev) && !count && (flags & VFIO_IRQ_SET_DATA_NONE)) {
 		vfio_intx_disable(vdev);
 		return 0;
@@ -463,28 +465,25 @@ static int vfio_pci_set_intx_trigger(struct vfio_pci_device *vdev,
 	if (!(is_intx(vdev) || is_irq_none(vdev)) || start != 0 || count != 1)
 		return -EINVAL;
 
-	if (flags & VFIO_IRQ_SET_DATA_EVENTFD) {
-		int32_t fd = *(int32_t *)data;
+	if (!is_intx(vdev)) {
 		int ret;
+		if (flags & VFIO_IRQ_SET_DATA_EVENTFD) {
+			ret = vfio_intx_enable(vdev);
+			if (ret)
+				return ret;
 
-		if (is_intx(vdev))
-			return vfio_intx_set_signal(vdev, fd);
+			ret = vfio_intx_set_signal(vdev, fd);
+			if (ret)
+				vfio_intx_disable(vdev);
 
-		ret = vfio_intx_enable(vdev);
-		if (ret)
 			return ret;
-
-		ret = vfio_intx_set_signal(vdev, fd);
-		if (ret)
-			vfio_intx_disable(vdev);
-
-		return ret;
+		} else
+			return -EINVAL;
 	}
 
-	if (!is_intx(vdev))
-		return -EINVAL;
-
-	if (flags & VFIO_IRQ_SET_DATA_NONE) {
+	if (flags & VFIO_IRQ_SET_DATA_EVENTFD) {
+		return vfio_intx_set_signal(vdev, fd);
+	} else if (flags & VFIO_IRQ_SET_DATA_NONE) {
 		vfio_send_intx_eventfd(vdev, NULL);
 	} else if (flags & VFIO_IRQ_SET_DATA_BOOL) {
 		uint8_t trigger = *(uint8_t *)data;
