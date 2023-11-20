71f105e45eb725234dc00623293dc47f31c5d481
Amir Goldstein <amir73il@gmail.com>
2023-11-20 12:14:24 +0200

    cachefiles: move kiocb_start_write() after error injection
    We want to move kiocb_start_write() into vfs_iocb_iter_write(), but
    first we need to move it passed cachefiles_inject_write_error() and
    prevent calling kiocb_end_write() if error was injected.
    
    We set the IOCB_WRITE flag after cachefiles_inject_write_error()
    and use it as indication that kiocb_start_write() was called in the
    cleanup/completion handler.
    
    Link: https://lore.kernel.org/r/CAOQ4uxihfJJRxxUhAmOwtD97Lg8PL8RgXw88rH1UfEeP8AtP+w@mail.gmail.com/
    Signed-off-by: Amir Goldstein <amir73il@gmail.com>

--- fs/cachefiles/io.c
+++ fs/cachefiles/io.c
@@ -259,7 +259,8 @@ static void cachefiles_write_complete(struct kiocb *iocb, long ret)
 
 	_enter("%ld", ret);
 
-	kiocb_end_write(iocb);
+	if (iocb->ki_flags & IOCB_WRITE)
+		kiocb_end_write(iocb);
 
 	if (ret < 0)
 		trace_cachefiles_io_error(object, inode, ret,
@@ -305,7 +306,6 @@ int __cachefiles_write(struct cachefiles_object *object,
 	refcount_set(&ki->ki_refcnt, 2);
 	ki->iocb.ki_filp	= file;
 	ki->iocb.ki_pos		= start_pos;
-	ki->iocb.ki_flags	= IOCB_DIRECT | IOCB_WRITE;
 	ki->iocb.ki_ioprio	= get_current_ioprio();
 	ki->object		= object;
 	ki->start		= start_pos;
@@ -319,16 +319,17 @@ int __cachefiles_write(struct cachefiles_object *object,
 		ki->iocb.ki_complete = cachefiles_write_complete;
 	atomic_long_add(ki->b_writing, &cache->b_writing);
 
-	kiocb_start_write(&ki->iocb);
-
 	get_file(ki->iocb.ki_filp);
 	cachefiles_grab_object(object, cachefiles_obj_get_ioreq);
 
 	trace_cachefiles_write(object, file_inode(file), ki->iocb.ki_pos, len);
 	old_nofs = memalloc_nofs_save();
 	ret = cachefiles_inject_write_error();
-	if (ret == 0)
+	if (ret == 0) {
+		ki->iocb.ki_flags = IOCB_DIRECT | IOCB_WRITE;
+		kiocb_start_write(&ki->iocb);
 		ret = vfs_iocb_iter_write(file, &ki->iocb, iter);
+	}
 	memalloc_nofs_restore(old_nofs);
 	switch (ret) {
 	case -EIOCBQUEUED:
