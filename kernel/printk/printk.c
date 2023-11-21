b1b8726ec3f40b66359bb1c63ecaf3da785aa67d
Tony Lindgren <tony@atomide.com>
2023-11-21 13:31:57 +0200

    serial: core: Move console character device handling from printk
    There's no need for console_setup() to try to figure out the console
    character device name for serial ports early on before uart_add_one_port()
    has been called. And for early debug console we have earlycon.
    
    Let's handle the serial port specific commandline options in the serial
    core subsystem and drop the handling from printk. The serial core
    subsystem can just call add_preferred_console() when the serial port is
    getting initialized.
    
    Note that eventually we may want to set up driver specific console quirk
    handling for the serial port device drivers to use. But we need to figure
    out which driver(s) need to call the quirk. So for now, we just move the
    sparc quirk and handle it directly.
    
    Signed-off-by: Tony Lindgren <tony@atomide.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -2438,9 +2438,7 @@ __setup("console_msg_format=", console_msg_format_setup);
  */
 static int __init console_setup(char *str)
 {
-	char buf[sizeof(console_cmdline[0].name) + 4]; /* 4 for "ttyS" */
-	char *s, *options, *brl_options = NULL;
-	int idx;
+	char *brl_options = NULL;
 
 	/*
 	 * Save the console for possible driver subsystem use. Bail out early
@@ -2463,32 +2461,6 @@ static int __init console_setup(char *str)
 	if (_braille_console_setup(&str, &brl_options))
 		return 1;
 
-	/*
-	 * Decode str into name, index, options.
-	 */
-	if (str[0] >= '0' && str[0] <= '9') {
-		strcpy(buf, "ttyS");
-		strncpy(buf + 4, str, sizeof(buf) - 5);
-	} else {
-		strncpy(buf, str, sizeof(buf) - 1);
-	}
-	buf[sizeof(buf) - 1] = 0;
-	options = strchr(str, ',');
-	if (options)
-		*(options++) = 0;
-#ifdef __sparc__
-	if (!strcmp(str, "ttya"))
-		strcpy(buf, "ttyS0");
-	if (!strcmp(str, "ttyb"))
-		strcpy(buf, "ttyS1");
-#endif
-	for (s = buf; *s; s++)
-		if (isdigit(*s) || *s == ',')
-			break;
-	idx = simple_strtoul(s, NULL, 10);
-	*s = 0;
-
-	__add_preferred_console(buf, idx, options, brl_options, true);
 	return 1;
 }
 __setup("console=", console_setup);
