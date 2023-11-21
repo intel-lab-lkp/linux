75ca7138cf88a82bd5604282d2ce42159cb80395
Stefan Wahren <wahrenst@gmx.net>
2023-11-21 17:30:02 +0100

    qca_spi: Fix SPI IRQ handling
    The functions qcaspi_netdev_open/close are responsible of request &
    free of the SPI interrupt, which wasn't the best choice. Currently
    it's possible to trigger a double free of the interrupt by calling
    qcaspi_netdev_close() after qcaspi_netdev_open() has failed.
    So let us split IRQ allocation & enabling, so we can take advantage
    of a device managed IRQ and also fix the issue.
    
    Fixes: 291ab06ecf67 ("net: qualcomm: new Ethernet over SPI driver for QCA7000")
    Signed-off-by: Stefan Wahren <wahrenst@gmx.net>

--- drivers/net/ethernet/qualcomm/qca_spi.c
+++ drivers/net/ethernet/qualcomm/qca_spi.c
@@ -674,7 +674,6 @@ static int
 qcaspi_netdev_open(struct net_device *dev)
 {
 	struct qcaspi *qca = netdev_priv(dev);
-	int ret = 0;
 
 	if (!qca)
 		return -EINVAL;
@@ -684,13 +683,7 @@ qcaspi_netdev_open(struct net_device *dev)
 	qca->sync = QCASPI_SYNC_UNKNOWN;
 	qcafrm_fsm_init_spi(&qca->frm_handle);
 
-	ret = request_irq(qca->spi_dev->irq, qcaspi_intr_handler, 0,
-			  dev->name, qca);
-	if (ret) {
-		netdev_err(dev, "%s: unable to get IRQ %d (irqval=%d).\n",
-			   QCASPI_DRV_NAME, qca->spi_dev->irq, ret);
-		return ret;
-	}
+	enable_irq(qca->spi_dev->irq);
 
 	/* SPI thread takes care of TX queue */
 	kthread_unpark(qca->spi_thread);
@@ -707,7 +700,7 @@ qcaspi_netdev_close(struct net_device *dev)
 	netif_stop_queue(dev);
 
 	qcaspi_write_register(qca, SPI_REG_INTR_ENABLE, 0, wr_verify);
-	free_irq(qca->spi_dev->irq, qca);
+	disable_irq(qca->spi_dev->irq);
 
 	kthread_park(qca->spi_thread);
 	qcaspi_flush_tx_ring(qca);
@@ -976,6 +969,15 @@ qca_spi_probe(struct spi_device *spi)
 	qca->legacy_mode = legacy_mode;
 
 	spi_set_drvdata(spi, qcaspi_devs);
+
+	ret = devm_request_irq(&spi->dev, spi->irq, qcaspi_intr_handler,
+			       IRQF_NO_AUTOEN, qca->net_dev->name, qca);
+	if (ret) {
+		dev_err(&spi->dev, "Unable to get IRQ %d (irqval=%d).\n",
+			spi->irq, ret);
+		free_netdev(qcaspi_devs);
+		return ret;
+	}
 
 	ret = of_get_ethdev_address(spi->dev.of_node, qca->net_dev);
 	if (ret) {
