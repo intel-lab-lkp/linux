8f27ec04441cef4b754aa8e6f8a0ce1d9dca2376
Shengjiu Wang <shengjiu.wang@nxp.com>
2023-11-21 15:37:30 +0800

    media: uapi: Add V4L2_CTRL_CLASS_M2M_AUDIO
    The Audio M2M class includes controls for audio memory-to-memory
    use cases. The controls can be used for audio codecs, audio
    preprocessing, audio postprocessing.
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>

--- drivers/media/v4l2-core/v4l2-ctrls-defs.c
+++ drivers/media/v4l2-core/v4l2-ctrls-defs.c
@@ -1242,6 +1242,9 @@ const char *v4l2_ctrl_get_name(u32 id)
 	case V4L2_CID_COLORIMETRY_CLASS:	return "Colorimetry Controls";
 	case V4L2_CID_COLORIMETRY_HDR10_CLL_INFO:		return "HDR10 Content Light Info";
 	case V4L2_CID_COLORIMETRY_HDR10_MASTERING_DISPLAY:	return "HDR10 Mastering Display";
+
+	/* Audio M2M controls */
+	case V4L2_CID_M2M_AUDIO_CLASS:  return "Audio M2M Controls";
 	default:
 		return NULL;
 	}
@@ -1451,6 +1454,7 @@ void v4l2_ctrl_fill(u32 id, const char **name, enum v4l2_ctrl_type *type,
 	case V4L2_CID_DETECT_CLASS:
 	case V4L2_CID_CODEC_STATELESS_CLASS:
 	case V4L2_CID_COLORIMETRY_CLASS:
+	case V4L2_CID_M2M_AUDIO_CLASS:
 		*type = V4L2_CTRL_TYPE_CTRL_CLASS;
 		/* You can neither read nor write these */
 		*flags |= V4L2_CTRL_FLAG_READ_ONLY | V4L2_CTRL_FLAG_WRITE_ONLY;
