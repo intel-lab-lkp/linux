4c3ef9be53846d218377891e52d54f35b73f000d
Aurelien Aptel <aaptel@nvidia.com>
2023-11-22 13:48:17 +0000

    net/tls,core: export get_netdev_for_sock
    * remove netdev_sk_get_lowest_dev() from net/core
    * move get_netdev_for_sock() from net/tls to net/core
    
    get_netdev_for_sock() is a utility that is used to obtain
    the net_device structure from a connected socket.
    
    Later patches will use this for nvme-tcp DDP and DDP DDGST offloads.
    
    Suggested-by: Christoph Hellwig <hch@lst.de>
    Signed-off-by: Ben Ben-Ishay <benishay@nvidia.com>
    Signed-off-by: Shai Malin <smalin@nvidia.com>
    Signed-off-by: Aurelien Aptel <aaptel@nvidia.com>

--- net/tls/tls_device.c
+++ net/tls/tls_device.c
@@ -119,22 +119,6 @@ static void tls_device_queue_ctx_destruction(struct tls_context *ctx)
 		tls_device_free_ctx(ctx);
 }
 
-/* We assume that the socket is already connected */
-static struct net_device *get_netdev_for_sock(struct sock *sk)
-{
-	struct dst_entry *dst = sk_dst_get(sk);
-	struct net_device *netdev = NULL;
-
-	if (likely(dst)) {
-		netdev = netdev_sk_get_lowest_dev(dst->dev, sk);
-		dev_hold(netdev);
-	}
-
-	dst_release(dst);
-
-	return netdev;
-}
-
 static void destroy_record(struct tls_record_info *record)
 {
 	int i;
