d87a745541278ad56d0a5276c5024624c50016a7
Jiaxun Yang <jiaxun.yang@flygoat.com>
2023-11-23 16:26:22 +0100

    MIPS: Fix set_uncached_handler for ebase in XKPHYS
    ebase may be in XKPHYS if memblock unable to allocate memory
    within KSEG0 physical range.
    
    To map ebase into uncached space we just convert it back to
    physical address and then use platform's TO_UNCAC helper
    to create mapping.
    
    Co-developed-by: Vladimir Kondratiev <vladimir.kondratiev@intel.com>
    Signed-off-by: Vladimir Kondratiev <vladimir.kondratiev@intel.com>
    Co-developed-by: Gregory CLEMENT <gregory.clement@bootlin.com>
    Signed-off-by: Jiaxun Yang <jiaxun.yang@flygoat.com>
    Signed-off-by: Gregory CLEMENT <gregory.clement@bootlin.com>

--- arch/mips/kernel/traps.c
+++ arch/mips/kernel/traps.c
@@ -2346,7 +2346,7 @@ static const char panic_null_cerr[] =
 void set_uncached_handler(unsigned long offset, void *addr,
 	unsigned long size)
 {
-	unsigned long uncached_ebase = CKSEG1ADDR(ebase);
+	unsigned long uncached_ebase = TO_UNCAC(__pa(ebase));
 
 	if (!addr)
 		panic(panic_null_cerr);
