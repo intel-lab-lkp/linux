d626def099bf1bab1e93f195252a9745edfc518f
Dinghao Liu <dinghao.liu@zju.edu.cn>
2023-11-23 15:03:37 +0800

    crypto: ccp - fix memleak in ccp_init_dm_workarea
    When dma_map_single() fails, we should free wa->address to
    prevent potential memleak.
    
    Fixes: 63b945091a07 ("crypto: ccp - CCP device driver and interface support")
    Signed-off-by: Dinghao Liu <dinghao.liu@zju.edu.cn>

--- drivers/crypto/ccp/ccp-ops.c
+++ drivers/crypto/ccp/ccp-ops.c
@@ -179,8 +179,10 @@ static int ccp_init_dm_workarea(struct ccp_dm_workarea *wa,
 
 		wa->dma.address = dma_map_single(wa->dev, wa->address, len,
 						 dir);
-		if (dma_mapping_error(wa->dev, wa->dma.address))
+		if (dma_mapping_error(wa->dev, wa->dma.address)) {
+			kfree(wa->address);
 			return -ENOMEM;
+		}
 
 		wa->dma.length = len;
 	}
