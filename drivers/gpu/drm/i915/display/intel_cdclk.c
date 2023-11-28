62a6a969f3beade23a5da70808337375e59b9552
Ville Syrjälä <ville.syrjala@linux.intel.com>
2023-11-28 13:51:33 +0200

    drm/i915/cdclk: Remove the assumption that cd2x div==2 when using squashing
    Currently we have a hardcoded assumption that the cd2x divider
    is always 2 when squahsing is used. While that is true for all
    current platforms it might not hold in the future. So eliminate
    the assumption and calculate the correct divider from the other
    parameters.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- drivers/gpu/drm/i915/display/intel_cdclk.c
+++ drivers/gpu/drm/i915/display/intel_cdclk.c
@@ -1899,10 +1899,8 @@ static void _bxt_set_cdclk(struct drm_i915_private *dev_priv,
 
 	waveform = cdclk_squash_waveform(dev_priv, cdclk);
 
-	if (waveform)
-		clock = vco / 2;
-	else
-		clock = cdclk;
+	clock = DIV_ROUND_CLOSEST(cdclk * cdclk_squash_len,
+				  cdclk_squash_divider(waveform));
 
 	if (HAS_CDCLK_SQUASH(dev_priv))
 		dg2_cdclk_squash_program(dev_priv, waveform);
