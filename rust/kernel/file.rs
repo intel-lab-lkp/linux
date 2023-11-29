ced5b882914fb606a8b9c191c34bc10454d8c6d1
Alice Ryhl <aliceryhl@google.com>
2023-11-29 13:12:32 +0000

    rust: file: add `DeferredFdCloser`
    To close an fd from kernel space, we could call `ksys_close`. However,
    if we do this to an fd that is held using `fdget`, then we may trigger a
    use-after-free. Introduce a helper that can be used to close an fd even
    if the fd is currently held with `fdget`. This is done by grabbing an
    extra refcount to the file and dropping it in a task work once we return
    to userspace.
    
    This is necessary for Rust Binder because otherwise the user might try
    to have Binder close its fd for /dev/binder, which would cause problems
    as this happens inside an ioctl on /dev/binder, and ioctls hold the fd
    using `fdget`.
    
    Additional motivation can be found in commit 80cd795630d6 ("binder: fix
    use-after-free due to ksys_close() during fdget()") and in the comments
    on `binder_do_fd_close`.
    
    If there is some way to detect whether an fd is currently held with
    `fdget`, then this could be optimized to skip the allocation and task
    work when this is not the case. Another possible optimization would be
    to combine several fds into a single task work, since this is used with
    fd arrays that might hold several fds.
    
    That said, it might not be necessary to optimize it, because Rust Binder
    has two ways to send fds: BINDER_TYPE_FD and BINDER_TYPE_FDA. With
    BINDER_TYPE_FD, it is userspace's responsibility to close the fd, so
    this mechanism is used only by BINDER_TYPE_FDA, but fd arrays are used
    rarely these days.
    
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- rust/kernel/file.rs
+++ rust/kernel/file.rs
@@ -11,7 +11,8 @@ use crate::{
     error::{code::*, Error, Result},
     types::{ARef, AlwaysRefCounted, Opaque},
 };
-use core::{marker::PhantomData, ptr};
+use alloc::boxed::Box;
+use core::{alloc::AllocError, marker::PhantomData, mem, ptr};
 
 /// Flags associated with a [`File`].
 pub mod flags {
@@ -239,6 +240,87 @@ impl Drop for FileDescriptorReservation {
     fn drop(&mut self) {
         // SAFETY: `self.fd` was returned by a previous call to `get_unused_fd_flags`.
         unsafe { bindings::put_unused_fd(self.fd) };
+    }
+}
+
+/// Helper used for closing file descriptors in a way that is safe even if the file is currently
+/// held using `fdget`.
+///
+/// Additional motivation can be found in commit 80cd795630d6 ("binder: fix use-after-free due to
+/// ksys_close() during fdget()") and in the comments on `binder_do_fd_close`.
+pub struct DeferredFdCloser {
+    inner: Box<DeferredFdCloserInner>,
+}
+
+/// SAFETY: This just holds an allocation with no real content, so there's no safety issue with
+/// moving it across threads.
+unsafe impl Send for DeferredFdCloser {}
+unsafe impl Sync for DeferredFdCloser {}
+
+#[repr(C)]
+struct DeferredFdCloserInner {
+    twork: mem::MaybeUninit<bindings::callback_head>,
+    file: *mut bindings::file,
+}
+
+impl DeferredFdCloser {
+    /// Create a new [`DeferredFdCloser`].
+    pub fn new() -> Result<Self, AllocError> {
+        Ok(Self {
+            inner: Box::try_new(DeferredFdCloserInner {
+                twork: mem::MaybeUninit::uninit(),
+                file: core::ptr::null_mut(),
+            })?,
+        })
+    }
+
+    /// Schedule a task work that closes the file descriptor when this task returns to userspace.
+    pub fn close_fd(mut self, fd: u32) {
+        use bindings::task_work_notify_mode_TWA_RESUME as TWA_RESUME;
+
+        let file = unsafe { bindings::close_fd_get_file(fd) };
+        if file.is_null() {
+            // Nothing further to do. The allocation is freed by the destructor of `self.inner`.
+            return;
+        }
+
+        self.inner.file = file;
+
+        // SAFETY: Since `DeferredFdCloserInner` is `#[repr(C)]`, casting the pointers gives a
+        // pointer to the `twork` field.
+        let inner = Box::into_raw(self.inner) as *mut bindings::callback_head;
+
+        // SAFETY: Getting a pointer to current is always safe.
+        let current = unsafe { bindings::get_current() };
+        // SAFETY: The `file` pointer points at a valid file.
+        unsafe { bindings::get_file(file) };
+        // SAFETY: Due to the above `get_file`, even if the current task holds an `fdget` to
+        // this file right now, the refcount will not drop to zero until after it is released
+        // with `fdput`. This is because when using `fdget`, you must always use `fdput` before
+        // returning to userspace, and our task work runs after any `fdget` users have returned
+        // to userspace.
+        //
+        // Note: fl_owner_t is currently a void pointer.
+        unsafe { bindings::filp_close(file, (*current).files as bindings::fl_owner_t) };
+        // SAFETY: The `inner` pointer is compatible with the `do_close_fd` method.
+        unsafe { bindings::init_task_work(inner, Some(Self::do_close_fd)) };
+        // SAFETY: The `inner` pointer points at a valid and fully initialized task work that is
+        // ready to be scheduled.
+        unsafe { bindings::task_work_add(current, inner, TWA_RESUME) };
+    }
+
+    // SAFETY: This function is an implementation detail of `close_fd`, so its safety comments
+    // should be read in extension of that method.
+    unsafe extern "C" fn do_close_fd(inner: *mut bindings::callback_head) {
+        // SAFETY: In `close_fd` we use this method together with a pointer that originates from a
+        // `Box<DeferredFdCloserInner>`, and we have just been given ownership of that allocation.
+        let inner = unsafe { Box::from_raw(inner as *mut DeferredFdCloserInner) };
+        // SAFETY: This drops a refcount we acquired in `close_fd`. Since this callback runs in a
+        // task work after we return to userspace, it is guaranteed that the current thread doesn't
+        // hold this file with `fdget`, as `fdget` must be released before returning to userspace.
+        unsafe { bindings::fput(inner.file) };
+        // Free the allocation.
+        drop(inner);
     }
 }
 
