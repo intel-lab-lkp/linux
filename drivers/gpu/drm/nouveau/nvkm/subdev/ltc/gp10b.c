18361adfbab27b148774357471f376e1d3cf9eea
Jason Gunthorpe <jgg@nvidia.com>
2023-11-28 20:48:04 -0400

    iommu/tegra: Use tegra_dev_iommu_get_stream_id() in the remaining places
    This API was defined to formalize the access to internal iommu details on
    some Tegra SOCs, but a few callers got missed. Add them.
    
    The helper already masks by 0xFFFF so remove this code from the callers.
    
    Suggested-by: Thierry Reding <thierry.reding@gmail.com>
    Signed-off-by: Jason Gunthorpe <jgg@nvidia.com>

--- drivers/gpu/drm/nouveau/nvkm/subdev/ltc/gp10b.c
+++ drivers/gpu/drm/nouveau/nvkm/subdev/ltc/gp10b.c
@@ -28,16 +28,13 @@ static void
 gp10b_ltc_init(struct nvkm_ltc *ltc)
 {
 	struct nvkm_device *device = ltc->subdev.device;
-	struct iommu_fwspec *spec;
+	u32 sid;
 
 	nvkm_wr32(device, 0x17e27c, ltc->ltc_nr);
 	nvkm_wr32(device, 0x17e000, ltc->ltc_nr);
 	nvkm_wr32(device, 0x100800, ltc->ltc_nr);
 
-	spec = dev_iommu_fwspec_get(device->dev);
-	if (spec) {
-		u32 sid = spec->ids[0] & 0xffff;
-
+	if (tegra_dev_iommu_get_stream_id(device->dev, &sid)) {
 		/* stream ID */
 		nvkm_wr32(device, 0x160000, sid << 2);
 	}
