4435b1490e976ea53fd30f35e341804515b43b86
David Laight <David.Laight@ACULAB.COM>
2023-11-29 19:26:02 +0000

    ipv4: Use READ/WRITE_ONCE() for IP local_port_range
    Commit 227b60f5102cd added a seqlock to ensure that the low and high
    port numbers were always updated together.
    This is overkill because the two 16bit port numbers can be held in
    a u32 and read/written in a single instruction.
    
    More recently 91d0b78c5177f added support for finer per-socket limits.
    The user-supplied value is 'high << 16 | low' but they are held
    separately and the socket options protected by the socket lock.
    
    Use a u32 containing 'high << 16 | low' for both the 'net' and 'sk'
    fields and use READ_ONCE()/WRITE_ONCE() to ensure both values are
    always updated together.
    
    Change (the now trival) inet_get_local_port_range() to a static inline
    to optimise the calling code.
    (In particular avoiding returning integers by reference.)
    
    Signed-off-by: David Laight <david.laight@aculab.com>

--- net/ipv4/sysctl_net_ipv4.c
+++ net/ipv4/sysctl_net_ipv4.c
@@ -54,22 +54,18 @@ static void set_local_port_range(struct net *net, int range[2])
 {
 	bool same_parity = !((range[0] ^ range[1]) & 1);
 
-	write_seqlock_bh(&net->ipv4.ip_local_ports.lock);
 	if (same_parity && !net->ipv4.ip_local_ports.warned) {
 		net->ipv4.ip_local_ports.warned = true;
 		pr_err_ratelimited("ip_local_port_range: prefer different parity for start/end values.\n");
 	}
-	net->ipv4.ip_local_ports.range[0] = range[0];
-	net->ipv4.ip_local_ports.range[1] = range[1];
-	write_sequnlock_bh(&net->ipv4.ip_local_ports.lock);
+	WRITE_ONCE(net->ipv4.ip_local_ports.range, range[1] << 16 | range[0]);
 }
 
 /* Validate changes from /proc interface. */
 static int ipv4_local_port_range(struct ctl_table *table, int write,
 				 void *buffer, size_t *lenp, loff_t *ppos)
 {
-	struct net *net =
-		container_of(table->data, struct net, ipv4.ip_local_ports.range);
+	struct net *net = table->data;
 	int ret;
 	int range[2];
 	struct ctl_table tmp = {
@@ -733,8 +729,8 @@ static struct ctl_table ipv4_net_table[] = {
 	},
 	{
 		.procname	= "ip_local_port_range",
-		.maxlen		= sizeof(init_net.ipv4.ip_local_ports.range),
-		.data		= &init_net.ipv4.ip_local_ports.range,
+		.maxlen		= 0,
+		.data		= &init_net,
 		.mode		= 0644,
 		.proc_handler	= ipv4_local_port_range,
 	},
