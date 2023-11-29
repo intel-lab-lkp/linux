e3ebef4e9d77ed2aa4a8e9f8a8940d000bf278f5
Konrad Dybcio <konrad.dybcio@linaro.org>
2023-11-29 19:59:21 +0100

    clk: qcom: Use qcom_branch_set_clk_en()
    Instead of magically poking at the bit0 of branch clocks' CBCR, use
    the newly introduced helper.
    
    Signed-off-by: Konrad Dybcio <konrad.dybcio@linaro.org>

--- drivers/clk/qcom/dispcc-sm8250.c
+++ drivers/clk/qcom/dispcc-sm8250.c
@@ -1365,8 +1365,7 @@ static int disp_cc_sm8250_probe(struct platform_device *pdev)
 	/* Enable clock gating for MDP clocks */
 	regmap_update_bits(regmap, 0x8000, 0x10, 0x10);
 
-	/* DISP_CC_XO_CLK always-on */
-	regmap_update_bits(regmap, 0x605c, BIT(0), BIT(0));
+	qcom_branch_set_clk_en(regmap, 0x605c); /* DISP_CC_XO_CLK */
 
 	ret = qcom_cc_really_probe(pdev, &disp_cc_sm8250_desc, regmap);
 
