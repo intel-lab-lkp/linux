bc94f0255cae23feb0daa8e359c94bfc39b64d0a
Kunwu Chan <chentao@kylinos.cn>
2023-12-04 09:44:55 +0800

    i40e: Use correct buffer size in i40e_dbg_command_read
    The size of "i40e_dbg_command_buf" is 256, the size of "name"
    depends on "IFNAMSIZ", plus a null character and format size,
    the total size is more than 256.
    
    Improve readability and maintainability by replacing a hardcoded string
    allocation and formatting by the use of the kasprintf() helper.
    
    Fixes: 02e9c290814c ("i40e: debugfs interface")
    Signed-off-by: Kunwu Chan <chentao@kylinos.cn>
    Suggested-by: Simon Horman <horms@kernel.org>
    Suggested-by: Alexander Lobakin <aleksander.lobakin@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_debugfs.c
+++ drivers/net/ethernet/intel/i40e/i40e_debugfs.c
@@ -54,23 +54,22 @@ static ssize_t i40e_dbg_command_read(struct file *filp, char __user *buffer,
 {
 	struct i40e_pf *pf = filp->private_data;
 	int bytes_not_copied;
-	int buf_size = 256;
-	char *buf;
+	char *buf = NULL;
 	int len;
 
 	/* don't allow partial reads */
 	if (*ppos != 0)
 		return 0;
-	if (count < buf_size)
-		return -ENOSPC;
 
-	buf = kzalloc(buf_size, GFP_KERNEL);
+	buf = kasprintf(GFP_KERNEL, "%s: %s\n",
+			pf->vsi[pf->lan_vsi]->netdev->name,
+			i40e_dbg_command_buf);
 	if (!buf)
 		return -ENOSPC;
 
-	len = snprintf(buf, buf_size, "%s: %s\n",
-		       pf->vsi[pf->lan_vsi]->netdev->name,
-		       i40e_dbg_command_buf);
+	len = strlen(buf);
+	if (count < len)
+		return -ENOSPC;
 
 	bytes_not_copied = copy_to_user(buffer, buf, len);
 	kfree(buf);
