fd057220f1b63bf8554dbcc91021addd7f0b439e
Long Li <leo.lilong@huawei.com>
2023-12-04 12:39:11 +0800

    xfs: fix perag leak when growfs fails
    During growfs, if new ag in memory has been initialized, however sb_agcount
    has not been updated, if an error occurs at this time it will cause ag
    leaks as follows, these new ags will not been freed during umount because
    of sb_agcount is not been updated.
    
    unreferenced object 0xffff88810be40200 (size 512):
      comm "xfs_growfs", pid 857, jiffies 4294909093
      hex dump (first 32 bytes):
        00 c0 c1 05 81 88 ff ff 04 00 00 00 00 00 00 00  ................
        01 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00  ................
      backtrace (crc 381741e2):
        [<ffffffff8191aef6>] __kmalloc+0x386/0x4f0
        [<ffffffff82553e65>] kmem_alloc+0xb5/0x2f0
        [<ffffffff8238dac5>] xfs_initialize_perag+0xc5/0x810
        [<ffffffff824f679c>] xfs_growfs_data+0x9bc/0xbc0
        [<ffffffff8250b90e>] xfs_file_ioctl+0x5fe/0x14d0
        [<ffffffff81aa5194>] __x64_sys_ioctl+0x144/0x1c0
        [<ffffffff83c3d81f>] do_syscall_64+0x3f/0xe0
        [<ffffffff83e00087>] entry_SYSCALL_64_after_hwframe+0x62/0x6a
    unreferenced object 0xffff88810be40800 (size 512):
      comm "xfs_growfs", pid 857, jiffies 4294909093
      hex dump (first 32 bytes):
        20 00 00 00 00 00 00 00 57 ef be dc 00 00 00 00   .......W.......
        10 08 e4 0b 81 88 ff ff 10 08 e4 0b 81 88 ff ff  ................
      backtrace (crc bde50e2d):
        [<ffffffff8191b43a>] __kmalloc_node+0x3da/0x540
        [<ffffffff81814489>] kvmalloc_node+0x99/0x160
        [<ffffffff8286acff>] bucket_table_alloc.isra.0+0x5f/0x400
        [<ffffffff8286bdc5>] rhashtable_init+0x405/0x760
        [<ffffffff8238dda3>] xfs_initialize_perag+0x3a3/0x810
        [<ffffffff824f679c>] xfs_growfs_data+0x9bc/0xbc0
        [<ffffffff8250b90e>] xfs_file_ioctl+0x5fe/0x14d0
        [<ffffffff81aa5194>] __x64_sys_ioctl+0x144/0x1c0
        [<ffffffff83c3d81f>] do_syscall_64+0x3f/0xe0
        [<ffffffff83e00087>] entry_SYSCALL_64_after_hwframe+0x62/0x6a
    
    Factor out xfs_destroy_perag() from xfs_initialize_perag() for error
    handle. When growfs fails, use xfs_destroy_perag() to destroy newly
    initialized ag in error handle path.
    
    Signed-off-by: Long Li <leo.lilong@huawei.com>

--- fs/xfs/libxfs/xfs_ag.c
+++ fs/xfs/libxfs/xfs_ag.c
@@ -332,6 +332,27 @@ xfs_agino_range(
 	return __xfs_agino_range(mp, xfs_ag_block_count(mp, agno), first, last);
 }
 
+void
+xfs_destroy_perag(
+	xfs_mount_t		*mp,
+	xfs_agnumber_t		agstart,
+	xfs_agnumber_t		agend)
+{
+	xfs_agnumber_t		index;
+	struct xfs_perag	*pag;
+
+	for (index = agstart; index < agend; index++) {
+		spin_lock(&mp->m_perag_lock);
+		pag = radix_tree_delete(&mp->m_perag_tree, index);
+		spin_unlock(&mp->m_perag_lock);
+		if (!pag)
+			break;
+		xfs_buf_hash_destroy(pag);
+		xfs_defer_drain_free(&pag->pag_intents_drain);
+		kmem_free(pag);
+	}
+}
+
 int
 xfs_initialize_perag(
 	struct xfs_mount	*mp,
@@ -431,16 +452,7 @@ out_free_pag:
 	kmem_free(pag);
 out_unwind_new_pags:
 	/* unwind any prior newly initialized pags */
-	for (index = first_initialised; index < agcount; index++) {
-		spin_lock(&mp->m_perag_lock);
-		pag = radix_tree_delete(&mp->m_perag_tree, index);
-		spin_unlock(&mp->m_perag_lock);
-		if (!pag)
-			break;
-		xfs_buf_hash_destroy(pag);
-		xfs_defer_drain_free(&pag->pag_intents_drain);
-		kmem_free(pag);
-	}
+	xfs_destroy_perag(mp, first_initialised, agcount);
 	return error;
 }
 
