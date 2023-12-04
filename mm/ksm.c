fcadc2c667a64c49adf25b7c8c4faba03af9a686
Stefan Roesch <shr@devkernel.io>
2023-12-04 15:49:05 -0800

    mm/ksm: add tracepoint for ksm advisor
    This adds a new tracepoint for the ksm advisor. It reports the last scan
    time, the new setting of the pages_to_scan parameter and the average cpu
    percent usage of the ksmd background thread for the last scan.
    
    Signed-off-by: Stefan Roesch <shr@devkernel.io>

--- mm/ksm.c
+++ mm/ksm.c
@@ -455,6 +455,7 @@ static void scan_time_advisor(void)
 	advisor_ctx.cpu_time = cpu_time;
 
 	ksm_thread_pages_to_scan = pages;
+	trace_ksm_advisor(scan_time, pages, cpu_percent);
 }
 
 static void run_advisor(void)
