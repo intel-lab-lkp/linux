efe1dc56d384b1f68abedf896d8187fee2a67819
Yu Kuai <yukuai3@huawei.com>
2023-12-05 20:39:05 +0800

    nilfs2: use bdev api in nilfs_attach_log_writer()
    Avoid to access bd_inode directly, prepare to remove bd_inode from
    block_devcie.
    
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>

--- fs/nilfs2/segment.c
+++ fs/nilfs2/segment.c
@@ -2824,7 +2824,7 @@ int nilfs_attach_log_writer(struct super_block *sb, struct nilfs_root *root)
 	if (!nilfs->ns_writer)
 		return -ENOMEM;
 
-	inode_attach_wb(nilfs->ns_bdev->bd_inode, NULL);
+	bdev_attach_wb(nilfs->ns_bdev);
 
 	err = nilfs_segctor_start_thread(nilfs->ns_writer);
 	if (unlikely(err))
