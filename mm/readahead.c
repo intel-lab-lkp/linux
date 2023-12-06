4f667c9c26566cd7f5062a9b909950af81646c0b
Matthew Wilcox (Oracle) <willy@infradead.org>
2023-12-06 20:44:42 +0000

    mm: Support order-1 folios in the page cache
    Folios of order 1 have no space to store the deferred list.  This is
    not a problem for the page cache as file-backed folios are never
    placed on the deferred list.  All we need to do is prevent the core
    MM from touching the deferred list for order 1 folios and remove the
    code which prevented us from allocating order 1 folios.
    
    Link: https://lore.kernel.org/linux-mm/90344ea7-4eec-47ee-5996-0c22f42d6a6a@google.com/
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>

--- mm/readahead.c
+++ mm/readahead.c
@@ -513,14 +513,10 @@ void page_cache_ra_order(struct readahead_control *ractl,
 		/* Align with smaller pages if needed */
 		if (index & ((1UL << order) - 1)) {
 			order = __ffs(index);
-			if (order == 1)
-				order = 0;
 		}
 		/* Don't allocate pages past EOF */
-		while (index + (1UL << order) - 1 > limit) {
-			if (--order == 1)
-				order = 0;
-		}
+		while (index + (1UL << order) - 1 > limit)
+			--order;
 		err = ra_alloc_folio(ractl, index, mark, order, gfp);
 		if (err)
 			break;
