9052fc44f5cb14928a1d0e98e25397bec384eee5
Gergo Koteles <soyer@irl.hu>
2023-12-07 01:04:31 +0100

    ALSA: hda/tas2781: reset the amp before component_add
    Prevent race to make the init reliable.
    
    Signed-off-by: Gergo Koteles <soyer@irl.hu>

--- sound/pci/hda/tas2781_hda_i2c.c
+++ sound/pci/hda/tas2781_hda_i2c.c
@@ -800,14 +800,14 @@ static int tas2781_hda_i2c_probe(struct i2c_client *clt)
 
 	pm_runtime_put_autosuspend(tas_hda->dev);
 
+	tas2781_reset(tas_hda->priv);
+
 	ret = component_add(tas_hda->dev, &tas2781_hda_comp_ops);
 	if (ret) {
 		dev_err(tas_hda->dev, "Register component failed: %d\n", ret);
 		pm_runtime_disable(tas_hda->dev);
 		goto err;
 	}
-
-	tas2781_reset(tas_hda->priv);
 err:
 	if (ret)
 		tas2781_hda_remove(&clt->dev);
