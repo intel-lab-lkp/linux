332b74e597b3e1459d5762ab845659eb69ae801a
Luiz Angelo Daros de Luca <luizluca@gmail.com>
2023-12-08 01:41:40 -0300

    net: dsa: realtek: create realtek-common
    Some code can be shared between both interface modules (MDIO and SMI)
    and among variants. Currently, these interface functions are shared:
    
    - realtek_common_lock
    - realtek_common_unlock
    - realtek_common_probe
    - realtek_common_remove
    
    The reset during probe was moved to the last moment before a variant
    detects the switch. This way, we avoid a reset if anything else fails.
    
    Signed-off-by: Luiz Angelo Daros de Luca <luizluca@gmail.com>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>

---
+++ drivers/net/dsa/realtek/realtek-common.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+
+#ifndef _REALTEK_INTERFACE_H
+#define _REALTEK_INTERFACE_H
+
+#include <linux/regmap.h>
+
+void realtek_common_lock(void *ctx);
+void realtek_common_unlock(void *ctx);
+struct realtek_priv *
+realtek_common_probe_pre(struct device *dev, struct regmap_config rc,
+			 struct regmap_config rc_nolock);
+int realtek_common_probe_post(struct realtek_priv *priv);
+void realtek_common_remove(struct realtek_priv *priv);
+
+#endif
