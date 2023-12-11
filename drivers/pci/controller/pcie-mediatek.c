e3adbcd689a5c4a93e8f61245228e9d57715e6d6
qizhong cheng <qizhong.cheng@mediatek.com>
2023-12-11 17:49:23 +0800

    PCI: mediatek: Clear interrupt status before dispatching handler
    We found a failure when used iperf tool for wifi performance testing,
    there are some MSIs received while clearing the interrupt status,
    these MSIs cannot be serviced.
    
    The interrupt status can be cleared even the MSI status still remaining,
    as an edge-triggered interrupts, its interrupt status should be cleared
    before dispatching to the handler of device.
    
    Fixes: 43e6409db64d ("PCI: mediatek: Add MSI support for MT2712 and MT7622")
    Signed-off-by: qizhong cheng <qizhong.cheng@mediatek.com>
    Signed-off-by: Jianjun Wang <jianjun.wang@mediatek.com>
    Reviewed-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
    Cc: stable@vger.kernel.org

--- drivers/pci/controller/pcie-mediatek.c
+++ drivers/pci/controller/pcie-mediatek.c
@@ -617,12 +617,17 @@ static void mtk_pcie_intr_handler(struct irq_desc *desc)
 		if (status & MSI_STATUS){
 			unsigned long imsi_status;
 
+			/*
+			 * The interrupt status can be cleared even the MSI
+			 * status still remaining, hence as an edge-triggered
+			 * interrupts, its interrupt status should be cleared
+			 * before dispatching handler.
+			 */
+			writel(MSI_STATUS, port->base + PCIE_INT_STATUS);
 			while ((imsi_status = readl(port->base + PCIE_IMSI_STATUS))) {
 				for_each_set_bit(bit, &imsi_status, MTK_MSI_IRQS_NUM)
 					generic_handle_domain_irq(port->inner_domain, bit);
 			}
-			/* Clear MSI interrupt status */
-			writel(MSI_STATUS, port->base + PCIE_INT_STATUS);
 		}
 	}
 
