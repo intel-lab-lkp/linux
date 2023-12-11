c5f238a9770b2b936e8d8071cc49ec4f196eaed7
Ronald Monthero <debug.penguin32@gmail.com>
2023-12-11 15:28:49 +1000

    mm/zswap: Improve with alloc_workqueue() call
    Use alloc_workqueue() to create and set finer
    work item attributes instead of create_workqueue()
    which is to be deprecated.
    
    Signed-off-by: Ronald Monthero <debug.penguin32@gmail.com>

--- mm/zswap.c
+++ mm/zswap.c
@@ -1986,7 +1986,8 @@ static int zswap_setup(void)
 		zswap_enabled = false;
 	}
 
-	shrink_wq = create_workqueue("zswap-shrink");
+	shrink_wq = alloc_workqueue("zswap-shrink",
+			WQ_UNBOUND|WQ_MEM_RECLAIM, 0);
 	if (!shrink_wq)
 		goto fallback_fail;
 
