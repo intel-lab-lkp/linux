a0727c7c3f90f3632a250055e5640a2377e54c8b
Michael T. Kloos <michael@michaelkloos.com>
2023-12-10 20:52:25 -0500

    ASoC: Enable built-in microphone on Lenovo ThinkPad P14s Gen 4 (AMD)
    Added System Board to Quirks List using model number prefix.
    Replaced/Removed a duplicate entry in the list.
    Added Yellow Carp Coprocessor Revision number to PCI revision check.
    
    Signed-off-by: Michael T. Kloos <michael@michaelkloos.com>

--- sound/soc/amd/yc/acp6x-mach.c
+++ sound/soc/amd/yc/acp6x-mach.c
@@ -56,7 +56,7 @@ static const struct dmi_system_id yc_acp_quirk_table[] = {
 		.driver_data = &acp6x_card,
 		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "LENOVO"),
-			DMI_MATCH(DMI_PRODUCT_NAME, "21D0"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "21K5"),
 		}
 	},
 	{
