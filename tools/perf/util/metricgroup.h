b3d005ebdcbdc512202d4e8b2bbfc2dc422a6c07
Weilin Wang <weilin.wang@intel.com>
2023-12-12 15:02:19 -0800

    perf stat: Handle taken alone in hardware-grouping
    Add taken alone into consideration when grouping. Only one taken
    alone event is supported per group.
    
    Signed-off-by: Weilin Wang <weilin.wang@intel.com>

--- tools/perf/util/metricgroup.h
+++ tools/perf/util/metricgroup.h
@@ -105,6 +105,7 @@ struct metricgroup__event_info {
 	const char *pmu_name;
 	bool fixed_counter;
 	bool free_counter;
+	bool taken_alone;
 	/** The counters the event allowed to be collected on. */
 	DECLARE_BITMAP(counters, NR_COUNTERS);
 };
@@ -154,6 +155,7 @@ struct metricgroup__group {
 	DECLARE_BITMAP(fixed_counters, NR_COUNTERS);
 	/** Head to the list of event names in this group*/
 	struct list_head event_head;
+	bool taken_alone;
 };
 
 struct metricgroup__group_events {
