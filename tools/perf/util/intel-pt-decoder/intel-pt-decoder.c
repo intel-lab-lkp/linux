46aa860c463db0443be271817d4322b0ab2fb6b7
Yang Guang <yang.guang5@zte.com.cn>
2023-12-12 11:22:56 +0800

    perf intel-pt: replace strlcpy() with strscpy()
    strlcpy() reads the entire source buffer first. This read may exceed
    the destination size limit. This is both inefficient and can lead
    to linear read overflows if a source string is not NUL-terminated.
    No return values were used, so direct replacement is safe.
    
    Signed-off-by: Chen Haonan <chen.haonan2@zte.com.cn>

--- tools/perf/util/intel-pt-decoder/intel-pt-decoder.c
+++ tools/perf/util/intel-pt-decoder/intel-pt-decoder.c
@@ -507,7 +507,7 @@ int intel_pt__strerror(int code, char *buf, size_t buflen)
 {
 	if (code < 1 || code >= INTEL_PT_ERR_MAX)
 		code = INTEL_PT_ERR_UNK;
-	strlcpy(buf, intel_pt_err_msgs[code], buflen);
+	strscpy(buf, intel_pt_err_msgs[code], buflen);
 	return 0;
 }
 
