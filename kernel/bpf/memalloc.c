da1d61cd5d968c55d967820b42b3c6f545654dba
Yonghong Song <yonghong.song@linux.dev>
2023-12-12 14:30:55 -0800

    bpf: Refill only one percpu element in memalloc
    Typically for percpu map element or data structure, once allocated,
    most operations are lookup or in-place update. Deletion are really
    rare. Currently, for percpu data strcture, 4 elements will be
    refilled if the size is <= 256. Let us just do with one element
    for percpu data. For example, for size 256 and 128 cpus, the
    potential saving will be 3 * 256 * 128 * 128 = 12MB.
    
    Signed-off-by: Yonghong Song <yonghong.song@linux.dev>

--- kernel/bpf/memalloc.c
+++ kernel/bpf/memalloc.c
@@ -483,11 +483,15 @@ static void init_refill_work(struct bpf_mem_cache *c)
 
 static void prefill_mem_cache(struct bpf_mem_cache *c, int cpu)
 {
+	int cnt = 1;
+
 	/* To avoid consuming memory assume that 1st run of bpf
 	 * prog won't be doing more than 4 map_update_elem from
 	 * irq disabled region
 	 */
-	alloc_bulk(c, c->unit_size <= 256 ? 4 : 1, cpu_to_node(cpu), false);
+	if (!c->percpu_size && c->unit_size <= 256)
+		cnt = 4;
+	alloc_bulk(c, cnt, cpu_to_node(cpu), false);
 }
 
 static int check_obj_size(struct bpf_mem_cache *c, unsigned int idx)
