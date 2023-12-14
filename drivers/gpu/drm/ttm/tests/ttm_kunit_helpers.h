ff2f66ac0c4105f4624d8090f99ce540d90cdae9
Karolina Stolarek <karolina.stolarek@intel.com>
2023-12-14 15:17:15 +0100

    drm/ttm/tests: Test simple BO creation and validation
    Add tests for ttm_bo_init_reserved() and ttm_bo_validate() that use
    sys manager. Define a simple move function in ttm_device_funcs. Expose
    destroy callback of the buffer object to make testing of
    ttm_bo_init_reserved() behaviour easier.
    
    Signed-off-by: Karolina Stolarek <karolina.stolarek@intel.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Tested-by: Amaranath Somalapuram <Amaranath.Somalapuram@amd.com>

--- drivers/gpu/drm/ttm/tests/ttm_kunit_helpers.h
+++ drivers/gpu/drm/ttm/tests/ttm_kunit_helpers.h
@@ -31,6 +31,7 @@ struct ttm_buffer_object *ttm_bo_kunit_init(struct kunit *test,
 					    size_t size);
 struct ttm_place *ttm_place_kunit_init(struct kunit *test,
 				       uint32_t mem_type, uint32_t flags);
+void dummy_ttm_bo_destroy(struct ttm_buffer_object *bo);
 
 struct ttm_test_devices *ttm_test_devices_basic(struct kunit *test);
 struct ttm_test_devices *ttm_test_devices_all(struct kunit *test);
