bccfb4cf27f830087923c36b7cde4f527b667ca2
Md Sadre Alam <quic_mdalam@quicinc.com>
2023-12-14 17:12:34 +0530

    drivers: bam_dma: Add LOCK & UNLOCK flag support
    Add lock and unlock flag support on command descriptor.
    Once lock set in requester pipe, then the bam controller
    will lock all others pipe and process the request only
    from requester pipe. Unlocking only can be performed from
    the same pipe.
    
    If DMA_PREP_LOCK flag passed in command descriptor then requester
    of this transaction wanted to lock the BAM controller for this
    transaction so BAM driver should set LOCK bit for the HW descriptor.
    
    If DMA_PREP_UNLOCK flag passed in command descriptor then requester
    of this transaction wanted to unlock the BAM controller.so BAM driver
    should set UNLOCK bit for the HW descriptor.
    
    Signed-off-by: Md Sadre Alam <quic_mdalam@quicinc.com>

--- drivers/dma/qcom/bam_dma.c
+++ drivers/dma/qcom/bam_dma.c
@@ -41,6 +41,7 @@
 #include <linux/clk.h>
 #include <linux/dmaengine.h>
 #include <linux/pm_runtime.h>
+#include <linux/dma/qcom_bam_dma.h>
 
 #include "../dmaengine.h"
 #include "../virt-dma.h"
@@ -58,6 +59,8 @@ struct bam_desc_hw {
 #define DESC_FLAG_EOB BIT(13)
 #define DESC_FLAG_NWD BIT(12)
 #define DESC_FLAG_CMD BIT(11)
+#define DESC_FLAG_LOCK BIT(10)
+#define DESC_FLAG_UNLOCK BIT(9)
 
 struct bam_async_desc {
 	struct virt_dma_desc vd;
@@ -686,6 +689,13 @@ static struct dma_async_tx_descriptor *bam_prep_slave_sg(struct dma_chan *chan,
 
 	/* fill in temporary descriptors */
 	desc = async_desc->desc;
+	if (flags & DMA_PREP_CMD) {
+		if (flags & DMA_PREP_LOCK)
+			desc->flags |= cpu_to_le16(DESC_FLAG_LOCK);
+		if (flags & DMA_PREP_UNLOCK)
+			desc->flags |= cpu_to_le16(DESC_FLAG_UNLOCK);
+	}
+
 	for_each_sg(sgl, sg, sg_len, i) {
 		unsigned int remainder = sg_dma_len(sg);
 		unsigned int curr_offset = 0;
