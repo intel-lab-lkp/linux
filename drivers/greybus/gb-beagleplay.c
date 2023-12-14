3f2f23c9fcc6767c1abef4a1147b4be74cabeb1f
Francesco Dolcini <francesco.dolcini@toradex.com>
2023-12-14 18:01:46 +0100

    treewide, serdev: change receive_buf() return type to size_t
    receive_buf() is called from ttyport_receive_buf() that expects values
    ">= 0" from serdev_controller_receive_buf(), change its return type from
    ssize_t to size_t.
    
    Suggested-by: Jiri Slaby <jirislaby@kernel.org>
    Link: https://lore.kernel.org/all/087be419-ec6b-47ad-851a-5e1e3ea5cfcc@kernel.org/
    Signed-off-by: Francesco Dolcini <francesco.dolcini@toradex.com>

--- drivers/greybus/gb-beagleplay.c
+++ drivers/greybus/gb-beagleplay.c
@@ -257,7 +257,7 @@ static void hdlc_rx_frame(struct gb_beagleplay *bg)
 	}
 }
 
-static ssize_t hdlc_rx(struct gb_beagleplay *bg, const u8 *data, size_t count)
+static size_t hdlc_rx(struct gb_beagleplay *bg, const u8 *data, size_t count)
 {
 	size_t i;
 	u8 c;
@@ -317,8 +317,8 @@ static void hdlc_deinit(struct gb_beagleplay *bg)
 	flush_work(&bg->tx_work);
 }
 
-static ssize_t gb_tty_receive(struct serdev_device *sd, const u8 *data,
-			      size_t count)
+static size_t gb_tty_receive(struct serdev_device *sd, const u8 *data,
+			     size_t count)
 {
 	struct gb_beagleplay *bg = serdev_device_get_drvdata(sd);
 
