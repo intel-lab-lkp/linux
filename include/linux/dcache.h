1ddcba75a3a7a7aaf02523a4cafb02d5741d697e
Gabriel Krisman Bertazi <krisman@suse.de>
2023-12-15 16:16:01 -0500

    dcache: Add helper to disable d_revalidate for a specific dentry
    Case-insensitive wants d_compare/d_hash for every dentry in the
    filesystem, while fscrypt needs d_revalidate only for DCACHE_NOKEY_NAME.
    This means we currently can't use sb->s_d_op to set case-insensitive
    hooks in fscrypt+case-insensitive filesystems without paying the cost to
    call d_revalidate for every dentry in the filesystem.
    
    In preparation to doing exactly that, add a way to disable d_revalidate
    later.
    
    Suggested-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Gabriel Krisman Bertazi <krisman@suse.de>

--- include/linux/dcache.h
+++ include/linux/dcache.h
@@ -225,6 +225,7 @@ extern struct dentry * d_instantiate_anon(struct dentry *, struct inode *);
 extern void __d_drop(struct dentry *dentry);
 extern void d_drop(struct dentry *dentry);
 extern void d_delete(struct dentry *);
+extern void d_set_always_valid(struct dentry *dentry);
 extern void d_set_d_op(struct dentry *dentry, const struct dentry_operations *op);
 
 /* allocate/de-allocate */
