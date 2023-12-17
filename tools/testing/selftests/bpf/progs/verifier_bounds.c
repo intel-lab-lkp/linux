7869d7b8a16189f8c9d6116c1fd8be5e2641315d
Menglong Dong <menglong8.dong@gmail.com>
2023-12-17 21:17:16 +0800

    selftests/bpf: add testcase to verifier_bounds.c for JMP_NE
    Add testcase for the logic that the verifier tracks the BPF_JNE for regs.
    The assembly function "reg_not_equal()" that we add is exactly converted
    from the following case:
    
      u32 a = bpf_get_prandom_u32();
      u64 b = 0;
    
      a %= 8;
      /* the "a > 0" here will be optimized to "a != 0" */
      if (a > 0) {
        /* now the range of a should be [1, 7] */
        bpf_skb_store_bytes(skb, 0, &b, a, 0);
      }
    
    Signed-off-by: Menglong Dong <menglong8.dong@gmail.com>

--- tools/testing/selftests/bpf/progs/verifier_bounds.c
+++ tools/testing/selftests/bpf/progs/verifier_bounds.c
@@ -1075,4 +1075,31 @@ l0_%=:	r0 = 0;						\
 	: __clobber_all);
 }
 
+SEC("tc")
+__description("bounds check with JMP_NE for reg edge")
+__success __retval(0)
+__naked void reg_not_equal(void)
+{
+	asm volatile ("					\
+	r6 = r1;					\
+	r1 = 0;						\
+	*(u64*)(r10 - 8) = r1;				\
+	call %[bpf_get_prandom_u32];			\
+	r4 = r0;					\
+	r4 &= 7;					\
+	if r4 == 0 goto l0_%=;				\
+	r1 = r6;					\
+	r2 = 0;						\
+	r3 = r10;					\
+	r3 += -8;					\
+	r5 = 0;						\
+	call %[bpf_skb_store_bytes];			\
+l0_%=:	r0 = 0;						\
+	exit;						\
+"	:
+	: __imm(bpf_get_prandom_u32),
+	  __imm(bpf_skb_store_bytes)
+	: __clobber_all);
+}
+
 char _license[] SEC("license") = "GPL";
