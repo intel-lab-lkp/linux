ac4be3fe5fc92e1a72179ac05acf8ebfba89bbf4
Konrad Dybcio <konrad.dybcio@linaro.org>
2023-12-18 15:40:00 +0100

    thermal: core: Print out thermal zone name on update_temperature error
    Currently, we can encounter an error like this:
    
      thermal thermal_zone48: failed to read out thermal zone (-19)
    
    It's good to know that there's been an issue, but on some occasions
    (like the error happening in the middle of a platform crash), one may
    not be able to look up what kind of thermal zone that is.
    
    Add the TZ name to the error message in order to speed up debugging.
    
    Signed-off-by: Konrad Dybcio <konrad.dybcio@linaro.org>

--- drivers/thermal/thermal_core.c
+++ drivers/thermal/thermal_core.c
@@ -403,8 +403,8 @@ static void update_temperature(struct thermal_zone_device *tz)
 	if (ret) {
 		if (ret != -EAGAIN)
 			dev_warn(&tz->device,
-				 "failed to read out thermal zone (%d)\n",
-				 ret);
+				 "failed to read out thermal zone %s (%d)\n",
+				 tz->type, ret);
 		return;
 	}
 
