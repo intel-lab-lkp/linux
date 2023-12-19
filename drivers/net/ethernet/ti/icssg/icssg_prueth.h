28d7f36fe4f549db7a0ff253b7255f26f58f8dcd
Diogo Ivo <diogo.ivo@siemens.com>
2023-12-19 17:45:42 +0000

    net: ti: icssg-classifier: Add support for SR1.0
    Add the functions to program the SR1.0 packet classifier.
    
    Based on the work of Roger Quadros in TI's 5.10 SDK [1].
    
    [1]: https://git.ti.com/cgit/ti-linux-kernel/ti-linux-kernel/tree/?h=ti-linux-5.10.y
    
    Co-developed-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>
    Signed-off-by: Diogo Ivo <diogo.ivo@siemens.com>

--- drivers/net/ethernet/ti/icssg/icssg_prueth.h
+++ drivers/net/ethernet/ti/icssg/icssg_prueth.h
@@ -277,7 +277,11 @@ extern const struct ethtool_ops icssg_ethtool_ops;
 void icssg_class_set_mac_addr(struct regmap *miig_rt, int slice, u8 *mac);
 void icssg_class_set_host_mac_addr(struct regmap *miig_rt, const u8 *mac);
 void icssg_class_disable(struct regmap *miig_rt, int slice);
-void icssg_class_default(struct regmap *miig_rt, int slice, bool allmulti);
+void icssg_class_default(struct regmap *miig_rt, int slice, bool allmulti,
+			 bool is_sr1);
+void icssg_class_promiscuous_sr1(struct regmap *miig_rt, int slice);
+void icssg_class_add_mcast_sr1(struct regmap *miig_rt, int slice,
+			       struct net_device *ndev);
 void icssg_ft1_set_mac_addr(struct regmap *miig_rt, int slice, u8 *mac_addr);
 
 /* config helpers */
