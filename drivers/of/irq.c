569b99d502432c76cb455d283eb5d1b70c01cdc9
Mark Hasemeyer <markhas@chromium.org>
2023-12-20 16:54:31 -0700

    of: irq: add wake capable bit to of_irq_resource()
    Add wake capability information to the IRQ resource. Wake capability is
    assumed based on conventions provided in the devicetree wakeup-source
    binding documentation. An interrupt is considered wake capable if the
    following are true:
    1. A wakeup-source property exits in the same device node as the
       interrupt.
    2. The IRQ is marked as dedicated by setting its interrupt-name to
       "wakeup".
    
    The wakeup-source documentation states that dedicated interrupts can use
    device specific interrupt names and device drivers are still welcome to
    use their own naming schemes. This API is provided as a helper if one is
    willing to conform to the above conventions.
    
    The ACPI subsystems already provides similar APIs that allow one to
    query the wake capability of an IRQ. This brings closer feature parity
    to the devicetree.
    
    Signed-off-by: Mark Hasemeyer <markhas@chromium.org>

--- drivers/of/irq.c
+++ drivers/of/irq.c
@@ -384,10 +384,38 @@ int of_irq_parse_one(struct device_node *device, int index, struct of_phandle_ar
 EXPORT_SYMBOL_GPL(of_irq_parse_one);
 
 /**
+ * __of_irq_wake_capable - Determine whether a given IRQ index is wake capable
+ *
+ * The IRQ is considered wake capable if the following are true:
+ * 1. wakeup-source property exists
+ * 2. provided IRQ index is labelled as a dedicated wakeirq
+ *
+ * This logic assumes the provided IRQ index is valid.
+ *
+ * @dev: pointer to device tree node
+ * @index: zero-based index of the IRQ
+ * Return: True if provided IRQ index for #dev is wake capable. False otherwise.
+ */
+static bool __of_irq_wake_capable(const struct device_node *dev, int index)
+{
+	int wakeindex;
+
+	if (!of_property_read_bool(dev, "wakeup-source"))
+		return false;
+
+	wakeindex = of_property_match_string(dev, "interrupt-names", "wakeup");
+	return wakeindex >= 0 && wakeindex == index;
+}
+
+/**
  * of_irq_to_resource - Decode a node's IRQ and return it as a resource
  * @dev: pointer to device tree node
- * @index: zero-based index of the irq
+ * @index: zero-based index of the IRQ
  * @r: pointer to resource structure to return result into.
+ *
+ * Return: Linux IRQ number on success, or 0 on the IRQ mapping failure, or
+ * -EPROBE_DEFER if the IRQ domain is not yet created, or error code in case
+ * of any other failure.
  */
 int of_irq_to_resource(struct device_node *dev, int index, struct resource *r)
 {
@@ -411,6 +439,8 @@ int of_irq_to_resource(struct device_node *dev, int index, struct resource *r)
 
 		r->start = r->end = irq;
 		r->flags = IORESOURCE_IRQ | irqd_get_trigger_type(irq_get_irq_data(irq));
+		if (__of_irq_wake_capable(dev, index))
+			r->flags |= IORESOURCE_IRQ_WAKECAPABLE;
 		r->name = name ? name : of_node_full_name(dev);
 	}
 
