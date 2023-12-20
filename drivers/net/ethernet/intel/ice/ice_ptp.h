067974b60c610d9578338938aaa283bd5fcf3e8e
Jacob Keller <jacob.e.keller@intel.com>
2023-12-20 11:43:22 +0100

    ice: factor out ice_ptp_rebuild_owner()
    The ice_ptp_reset() function uses a goto to skip past clock owner
    operations if performing a PF reset or if the device is not the clock
    owner. This is a bit confusing. Factor this out into
    ice_ptp_rebuild_owner() instead.
    
    The ice_ptp_reset() function is called by ice_rebuild() to restore PTP
    functionality after a device reset. Follow the convention set by the
    ice_main.c file and rename this function to ice_ptp_rebuild(), in the
    same way that we have ice_prepare_for_reset() and
    ice_ptp_prepare_for_reset().
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Signed-off-by: Karol Kolacinski <karol.kolacinski@intel.com>
    Reviewed-by: Jacob Keller <jacob.e.keller@intel.com>

--- drivers/net/ethernet/intel/ice/ice_ptp.h
+++ drivers/net/ethernet/intel/ice/ice_ptp.h
@@ -316,6 +316,7 @@ enum ice_tx_tstamp_work ice_ptp_process_ts(struct ice_pf *pf);
 
 u64 ice_ptp_get_rx_hwts(const union ice_32b_rx_flex_desc *rx_desc,
 			const struct ice_pkt_ctx *pkt_ctx);
+void ice_ptp_rebuild(struct ice_pf *pf, enum ice_reset_req reset_type);
 void ice_ptp_prepare_for_reset(struct ice_pf *pf,
 			       enum ice_reset_req reset_type);
 void ice_ptp_init(struct ice_pf *pf);
@@ -364,8 +365,8 @@ static inline void ice_ptp_prepare_for_reset(struct ice_pf *pf) { }
 static inline void
 ice_ptp_rx_hwtstamp(struct ice_rx_ring *rx_ring,
 		    union ice_32b_rx_flex_desc *rx_desc, struct sk_buff *skb) { }
-static inline void ice_ptp_reset(struct ice_pf *pf,
-				 enum ice_reset_req reset_type)
+static inline void ice_ptp_rebuild(struct ice_pf *pf,
+				   enum ice_reset_req reset_type)
 {
 }
 
