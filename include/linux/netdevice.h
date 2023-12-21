fe9aa0a79599e554368d95bf04db3203d0c643ff
Eric Dumazet <edumazet@google.com>
2023-12-21 14:07:47 +0000

    net-device: move gso_partial_features to net_device_read_tx
    dev->gso_partial_features is read from tx fast path for GSO packets.
    
    Move it to appropriate section to avoid a cache line miss.
    
    Fixes: 43a71cd66b9c ("net-device: reorganize net_device fast path variables")
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Cc: Coco Li <lixiaoyan@google.com>
    Cc: David Ahern <dsahern@kernel.org>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2114,6 +2114,7 @@ struct net_device {
 	const struct net_device_ops *netdev_ops;
 	const struct header_ops *header_ops;
 	struct netdev_queue	*_tx;
+	netdev_features_t	gso_partial_features;
 	unsigned int		real_num_tx_queues;
 	unsigned int		gso_max_size;
 	unsigned int		gso_ipv4_max_size;
@@ -2210,7 +2211,6 @@ struct net_device {
 	netdev_features_t	vlan_features;
 	netdev_features_t	hw_enc_features;
 	netdev_features_t	mpls_features;
-	netdev_features_t	gso_partial_features;
 
 	unsigned int		min_mtu;
 	unsigned int		max_mtu;
