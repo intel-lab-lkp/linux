c18edbe42421bb482688f760524e7b5a77e7328c
Vincent Donnefort <vdonnefort@google.com>
2023-12-21 17:35:22 +0000

    ring-buffer: Introducing ring-buffer mapping functions
    In preparation for allowing the user-space to map a ring-buffer, add
    a set of mapping functions:
    
      ring_buffer_{map,unmap}()
      ring_buffer_map_fault()
    
    And controls on the ring-buffer:
    
      ring_buffer_map_get_reader()  /* swap reader and head */
    
    Mapping the ring-buffer also involves:
    
      A unique ID for each subbuf of the ring-buffer, currently they are
      only identified through their in-kernel VA.
    
      A meta-page, where are stored ring-buffer statistics and a
      description for the current reader
    
    The linear mapping exposes the meta-page, and each subbuf of the
    ring-buffer, ordered following their unique ID, assigned during the
    first mapping.
    
    Once mapped, no subbuf can get in or out of the ring-buffer: the buffer
    size will remain unmodified and the splice enabling functions will in
    reality simply memcpy the data instead of swapping subbufs.
    
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

---
+++ include/uapi/linux/trace_mmap.h
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _UAPI_TRACE_MMAP_H_
+#define _UAPI_TRACE_MMAP_H_
+
+#include <linux/types.h>
+
+struct trace_buffer_meta {
+	unsigned long	entries;
+	unsigned long	overrun;
+	unsigned long	read;
+
+	unsigned long	subbufs_touched;
+	unsigned long	subbufs_lost;
+	unsigned long	subbufs_read;
+
+	struct {
+		unsigned long	lost_events;	/* Events lost at the time of the reader swap */
+		__u32		id;		/* Reader subbuf ID from 0 to nr_subbufs - 1 */
+		__u32		read;		/* Number of bytes read on the reader subbuf */
+	} reader;
+
+	__u32		subbuf_size;		/* Size of each subbuf including the header */
+	__u32		nr_subbufs;		/* Number of subbufs in the ring-buffer */
+
+	__u32		meta_page_size;		/* Size of the meta-page */
+	__u32		meta_struct_len;	/* Len of this struct */
+};
+
+#endif /* _UAPI_TRACE_MMAP_H_ */
