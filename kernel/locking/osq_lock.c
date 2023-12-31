ff787c1fd0c13f9b294231952ea62fa62a6cbb6d
David Laight <David.Laight@ACULAB.COM>
2023-12-31 21:51:20 +0000

    locking/osq_lock: Defer clearing node->locked until the slow osq_lock() path.
    Since node->locked cannot be set before the assignment to prev->next
    it is save to clear it in the slow path.
    
    Signed-off-by: David Laight <david.laight@aculab.com>

--- kernel/locking/osq_lock.c
+++ kernel/locking/osq_lock.c
@@ -97,7 +97,6 @@ bool osq_lock(struct optimistic_spin_queue *lock)
 	int curr = encode_cpu(smp_processor_id());
 	int old;
 
-	node->locked = 0;
 	node->next = NULL;
 	node->cpu = curr;
 
@@ -111,6 +110,7 @@ bool osq_lock(struct optimistic_spin_queue *lock)
 	if (old == OSQ_UNLOCKED_VAL)
 		return true;
 
+	node->locked = 0;
 	prev = decode_cpu(old);
 	node->prev = prev;
 
