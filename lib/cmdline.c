a94a2f059a249a8570de0f8b680cd0a20fb7a4da
Qu Wenruo <wqu@suse.com>
2024-01-04 09:57:49 +1030

    kstrtox: introduce a safer version of memparse()
    [BUGS]
    Function memparse() lacks error handling:
    
    - If no valid number string at all
      In that case @retptr would just be updated and return value would be
      zero.
    
    - No overflown detection
      This applies to both the number string part, and the suffixes part.
      And since we have no way to indicate errors, we can get weird results
      like:
    
      	"25E" -> 10376293541461622784 (9E)
    
      This is due to the fact that for "E" suffix, there is only 4 bits
      left, and 25 with 60 bits left shift would lead to overflow.
    
    [CAUSE]
    The root cause is already mentioned in the comments of the function, the
    usage of simple_strtoull() is the source of evil.
    Furthermore the function prototype is no good either, just returning an
    unsigned long long gives us no way to indicate an error.
    
    [FIX]
    Due to the prototype limits, we can not have a drop-in replacement for
    memparse().
    
    This patch can only help by introduce a new helper, memparse_safe(), and
    mark the old memparse() deprecated.
    
    The new memparse_safe() has the following improvement:
    
    - Invalid string detection
      If no number string can be detected at all, -EINVAL would be returned.
    
    - Better overflow detection
      Both the string part and the extra left shift would have overflow
      detection.
      Any overflow would result -ERANGE.
    
    - Safer default suffix selection
      The helper allows the caller to choose the suffixes that they want to
      use.
      But only "KMGTP" are recommended by default since the "E" leaves only
      4 bits before overflow.
      For those callers really know what they are doing, they can still
      manually to include all suffixes.
    
    Due to the prototype change, callers should migrate to the new one and
    change their code and add extra error handling.
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>
    Reviewed-by: David Disseldorp <ddiss@suse.de>

--- lib/cmdline.c
+++ lib/cmdline.c
@@ -139,12 +139,15 @@ char *get_options(const char *str, int nints, int *ints)
 EXPORT_SYMBOL(get_options);
 
 /**
- *	memparse - parse a string with mem suffixes into a number
+ *	memparse - DEPRECATED, parse a string with mem suffixes into a number
  *	@ptr: Where parse begins
  *	@retptr: (output) Optional pointer to next char after parse completes
  *
+ *	There is no way to handle errors, and no overflown detection and string
+ *	sanity checks.
  *	Parses a string into a number.  The number stored at @ptr is
  *	potentially suffixed with K, M, G, T, P, E.
+ *
  */
 
 unsigned long long memparse(const char *ptr, char **retptr)
