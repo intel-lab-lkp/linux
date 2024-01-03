f500a11d664710e5706f64ecb1d9b6525de49296
Kemeng Shi <shikemeng@huaweicloud.com>
2024-01-03 18:28:15 +0800

    ext4: remove unneeded return value of ext4_mb_release_context
    Function ext4_mb_release_context always return 0 and the return value is
    never used. Just remove unneeded return value of ext4_mb_release_context.
    
    Signed-off-by: Kemeng Shi <shikemeng@huaweicloud.com>
    Reviewed-by: Jan Kara <jack@suse.cz>

--- fs/ext4/mballoc.c
+++ fs/ext4/mballoc.c
@@ -5943,7 +5943,7 @@ static void ext4_mb_add_n_trim(struct ext4_allocation_context *ac)
 /*
  * release all resource we used in allocation
  */
-static int ext4_mb_release_context(struct ext4_allocation_context *ac)
+static void ext4_mb_release_context(struct ext4_allocation_context *ac)
 {
 	struct ext4_sb_info *sbi = EXT4_SB(ac->ac_sb);
 	struct ext4_prealloc_space *pa = ac->ac_pa;
@@ -5980,7 +5980,6 @@ static int ext4_mb_release_context(struct ext4_allocation_context *ac)
 	if (ac->ac_flags & EXT4_MB_HINT_GROUP_ALLOC)
 		mutex_unlock(&ac->ac_lg->lg_mutex);
 	ext4_mb_collect_stats(ac);
-	return 0;
 }
 
 static int ext4_mb_discard_preallocations(struct super_block *sb, int needed)
