1134590287b0b2842d31668fa6b01392eb62fc21
Tanzir Hasan <tanzirh@google.com>
2024-01-04 00:13:29 +0000

    x86/syscalls: shrink entry/syscall_64.i via IWYU
    This diff uses an open source tool include-what-you-use (IWYU) to modify
    the include list, changing indirect includes to direct includes. IWYU is
    implemented using the IWYUScripts github repository which is a tool that
    is currently undergoing development. These changes seek to improve build
    times.
    
    This change to entry/syscall_64.c resulted in a preprocessed size of
    entry/syscall_64.i from 64003 lines to 24509 lines (-62%) for the x86
    defconfig.
    
    Suggested-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Tanzir Hasan <tanzirh@google.com>

--- arch/x86/entry/syscall_64.c
+++ arch/x86/entry/syscall_64.c
@@ -1,11 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0
 /* System call table for x86-64. */
 
-#include <linux/linkage.h>
-#include <linux/sys.h>
-#include <linux/cache.h>
-#include <linux/syscalls.h>
 #include <asm/syscall.h>
+
+struct pt_regs;
+typedef long (*sys_call_ptr_t)(const struct pt_regs *);
+extern const sys_call_ptr_t sys_call_table[];
+#if defined(CONFIG_X86_32)
+#define ia32_sys_call_table sys_call_table
+#else
+/*
+ * These may not exist, but still put the prototypes in so we
+ * can use IS_ENABLED().
+ */
+extern const sys_call_ptr_t ia32_sys_call_table[];
+extern const sys_call_ptr_t x32_sys_call_table[];
+#endif
 
 #define __SYSCALL(nr, sym) extern long __x64_##sym(const struct pt_regs *);
 #include <asm/syscalls_64.h>
