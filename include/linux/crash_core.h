20bddc56ac571de17bae4bb8ebb490917bc5393d
Baoquan He <bhe@redhat.com>
2024-01-05 18:33:01 +0800

    kexec_core: move kdump related codes from crash_core.c to kexec_core.c
    In commit 6f991cc363a3 ("crash: move a few code bits to setup support
    of crash hotplug"), some elfcorehdr processing codes were moved to
    kernel/crash_core.c so that they can be shared by kexec_load and
    kexec_file_load interface. And later in commit 247262756121 ("crash: add
    generic infrastructure for crash hotplug support"), generic code for
    crash hotplug support codes were added into kernel/crash_core.c.
    
    In fact, these codes should be put in kernel/kexec_core.c because
    both kexec_load and kexec_file_load need them.
    
    So move them from kernel/crash_core.c to kernel/kexec_core.c. No
    functionality change is introduced.
    
    Signed-off-by: Baoquan He <bhe@redhat.com>

--- include/linux/crash_core.h
+++ include/linux/crash_core.h
@@ -124,30 +124,4 @@ static inline void __init reserve_crashkernel_generic(char *cmdline,
 		bool high)
 {}
 #endif
-
-/* Alignment required for elf header segment */
-#define ELF_CORE_HEADER_ALIGN   4096
-
-struct crash_mem {
-	unsigned int max_nr_ranges;
-	unsigned int nr_ranges;
-	struct range ranges[] __counted_by(max_nr_ranges);
-};
-
-extern int crash_exclude_mem_range(struct crash_mem *mem,
-				   unsigned long long mstart,
-				   unsigned long long mend);
-extern int crash_prepare_elf64_headers(struct crash_mem *mem, int need_kernel_map,
-				       void **addr, unsigned long *sz);
-
-struct kimage;
-struct kexec_segment;
-
-#define KEXEC_CRASH_HP_NONE			0
-#define KEXEC_CRASH_HP_ADD_CPU			1
-#define KEXEC_CRASH_HP_REMOVE_CPU		2
-#define KEXEC_CRASH_HP_ADD_MEMORY		3
-#define KEXEC_CRASH_HP_REMOVE_MEMORY		4
-#define KEXEC_CRASH_HP_INVALID_CPU		-1U
-
 #endif /* LINUX_CRASH_CORE_H */
