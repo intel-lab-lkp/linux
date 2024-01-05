3296b2cb25fd0b42d18a3ff380aad9b5ab62e0c4
Dave Jiang <dave.jiang@intel.com>
2024-01-05 11:03:04 -0700

    cxl: Introduce put_cxl_root() helper
    Add a helper function put_cxl_root() to maintain symmetry for
    find_cxl_root() function instead of relying on open coding of the
    put_device() in order to dereference the 'struct device' that happens via
    get_device() in find_cxl_root().
    
    Suggested-by: Robert Richter <rrichter@amd.com>
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>

--- drivers/cxl/cxl.h
+++ drivers/cxl/cxl.h
@@ -735,6 +735,7 @@ struct cxl_port *devm_cxl_add_port(struct device *host,
 struct cxl_root *devm_cxl_add_root(struct device *host,
 				   const struct cxl_root_ops *ops);
 struct cxl_port *find_cxl_root(struct cxl_port *port);
+void put_cxl_root(struct cxl_root *cxl_root);
 int devm_cxl_enumerate_ports(struct cxl_memdev *cxlmd);
 void cxl_bus_rescan(void);
 void cxl_bus_drain(void);
