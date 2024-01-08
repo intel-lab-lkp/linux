59aea0865f06007a00797c6712d4df342ddf5f7b
Alexander Stein <alexander.stein@ew.tq-group.com>
2024-01-08 16:25:14 +0100

    drm: bridge: dw_hdmi: Set DRM bridge type
    The bridge type was set to default (Unknown). Set proper bridge type.
    With this fixed, debugfs output imx8mp looks proper.
    $ cat /sys/kernel/debug/dri/1/encoder-0/bridges
    bridge[0]: imx_hdmi_pvi_bridge_funcs [imx8mp_hdmi_pvi]
            type: [0] Unknown
            OF: /soc@0/bus@32c00000/display-bridge@32fc4000:fsl,imx8mp-hdmi-pvi
            ops: [0x0]
    bridge[1]: dw_hdmi_bridge_funcs [dw_hdmi]
            type: [11] HDMI-A
            OF: /soc@0/bus@32c00000/hdmi@32fd8000:fsl,imx8mp-hdmi
            ops: [0x7] detect edid hpd
    
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
@@ -3542,6 +3542,7 @@ struct dw_hdmi *dw_hdmi_probe(struct platform_device *pdev,
 	hdmi->bridge.interlace_allowed = true;
 	hdmi->bridge.ddc = hdmi->ddc;
 	hdmi->bridge.of_node = pdev->dev.of_node;
+	hdmi->bridge.type = DRM_MODE_CONNECTOR_HDMIA;
 
 	memset(&pdevinfo, 0, sizeof(pdevinfo));
 	pdevinfo.parent = dev;
