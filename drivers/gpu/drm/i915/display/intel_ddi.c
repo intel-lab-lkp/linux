4708b50e86c1cce9c244f03b297547b7528d6830
Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>
2024-01-08 14:07:25 +0200

    Start separating pipe vs transcoder set logic for bigjoiner during modeset
    Handle only bigjoiner masters in skl_commit_modeset_enables/disables,
    slave crtcs should be handled by master hooks. Same for encoders.
    That way we can also remove a bunch of checks like intel_crtc_is_bigjoiner_slave.
    
    Signed-off-by: Stanislav Lisovskiy <stanislav.lisovskiy@intel.com>

--- drivers/gpu/drm/i915/display/intel_ddi.c
+++ drivers/gpu/drm/i915/display/intel_ddi.c
@@ -3340,8 +3340,7 @@ static void intel_enable_ddi(struct intel_atomic_state *state,
 {
 	drm_WARN_ON(state->base.dev, crtc_state->has_pch_encoder);
 
-	if (!intel_crtc_is_bigjoiner_slave(crtc_state))
-		intel_ddi_enable_transcoder_func(encoder, crtc_state);
+	intel_ddi_enable_transcoder_func(encoder, crtc_state);
 
 	/* Enable/Disable DP2.0 SDP split config before transcoder */
 	intel_audio_sdp_split_update(crtc_state);
