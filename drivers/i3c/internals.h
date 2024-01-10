6af0a6151c23869465345a63d81143a2d69564d0
Frank Li <Frank.Li@nxp.com>
2024-01-10 12:52:20 -0500

    i3c: add API i3c_dev_gettstatus_format1() to get target device status
    I3C standard 5.1.9.3.15 Get Device Status (GETSTATUS):
    Get request for one I3C Target Device to return its current status.
    
    Add API to fetch it with format1.
    
    Signed-off-by: Frank Li <Frank.Li@nxp.com>

--- drivers/i3c/internals.h
+++ drivers/i3c/internals.h
@@ -24,4 +24,5 @@ int i3c_dev_enable_ibi_locked(struct i3c_dev_desc *dev);
 int i3c_dev_request_ibi_locked(struct i3c_dev_desc *dev,
 			       const struct i3c_ibi_setup *req);
 void i3c_dev_free_ibi_locked(struct i3c_dev_desc *dev);
+int i3c_dev_getstatus_format1_locked(struct i3c_dev_desc *dev, u16 *status);
 #endif /* I3C_INTERNAL_H */
