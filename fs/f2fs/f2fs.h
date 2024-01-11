3cc7044f370d3ea514fba3076fed1bd8554ce51b
Chao Yu <chao@kernel.org>
2024-01-11 16:17:43 +0800

    f2fs: reduce expensive checkpoint trigger frequency
    We may trigger high frequent checkpoint for below case:
    1. mkdir /mnt/dir1; set dir1 encrypted
    2. touch /mnt/file1; fsync /mnt/file1
    3. mkdir /mnt/dir2; set dir2 encrypted
    4. touch /mnt/file2; fsync /mnt/file2
    ..
    
    Although, newly created dir and file are not related, due to
    commit bbf156f7afa7 ("f2fs: fix lost xattrs of directories"), we will
    trigger checkpoint whenever fsync() comes after a new encrypted dir
    created.
    
    In order to avoid such condition, let's record an entry including
    directory's ino into global cache when we initialize encryption policy
    in a checkpointed directory, and then only trigger checkpoint() when
    target file's parent has non-persisted encryption policy, for the case
    its parent is not checkpointed, need_do_checkpoint() has cover that
    by verifying it with f2fs_is_checkpointed_node().
    
    Reported-by: Zhiguo Niu <zhiguo.niu@unisoc.com>
    Tested-by: Zhiguo Niu <zhiguo.niu@unisoc.com>
    Reported-by: Yunlei He <heyunlei@hihonor.com>
    Signed-off-by: Chao Yu <chao@kernel.org>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -278,6 +278,7 @@ enum {
 	APPEND_INO,		/* for append ino list */
 	UPDATE_INO,		/* for update ino list */
 	TRANS_DIR_INO,		/* for transactions dir ino list */
+	ENC_DIR_INO,		/* for encrypted dir ino list */
 	FLUSH_INO,		/* for multiple device flushing */
 	MAX_INO_ENTRY,		/* max. list */
 };
@@ -1145,6 +1146,7 @@ enum cp_reason_type {
 	CP_FASTBOOT_MODE,
 	CP_SPEC_LOG_NUM,
 	CP_RECOVER_DIR,
+	CP_ENC_DIR,
 };
 
 enum iostat_type {
--- fs/f2fs/xattr.c
+++ fs/f2fs/xattr.c
@@ -629,6 +629,7 @@ static int __f2fs_setxattr(struct inode *inode, int index,
 			const char *name, const void *value, size_t size,
 			struct page *ipage, int flags)
 {
+	struct f2fs_sb_info *sbi = F2FS_I_SB(inode);
 	struct f2fs_xattr_entry *here, *last;
 	void *base_addr, *last_base_addr;
 	int found, newsize;
@@ -772,8 +773,19 @@ retry:
 	if (index == F2FS_XATTR_INDEX_ENCRYPTION &&
 			!strcmp(name, F2FS_XATTR_NAME_ENCRYPTION_CONTEXT))
 		f2fs_set_encrypted_inode(inode);
-	if (S_ISDIR(inode->i_mode))
-		set_sbi_flag(F2FS_I_SB(inode), SBI_NEED_CP);
+
+	if (S_ISDIR(inode->i_mode)) {
+		/*
+		 * In restrict mode, fsync() always tries triggering checkpoint
+		 * for all metadata consistency, in other mode, it only triggers
+		 * checkpoint when parent's encryption metadata updates.
+		 */
+		if (F2FS_OPTION(sbi).fsync_mode == FSYNC_MODE_STRICT)
+			set_sbi_flag(F2FS_I_SB(inode), SBI_NEED_CP);
+		else if (IS_ENCRYPTED(inode) &&
+			f2fs_is_checkpointed_node(sbi, inode->i_ino))
+			f2fs_add_ino_entry(sbi, inode->i_ino, ENC_DIR_INO);
+	}
 
 same:
 	if (is_inode_flag_set(inode, FI_ACL_MODE)) {
