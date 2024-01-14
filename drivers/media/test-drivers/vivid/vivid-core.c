fd5aef1f3b7b0425f4df3c068580b01eed73a70f
Mauro Carvalho Chehab <mchehab@kernel.org>
2024-01-14 02:27:54 +0100

    This is just a test patch. Please ignore
    This do-nothing patch is just to check if patchwork is properly
    handling patches after its migration to version 3.0.
    
    PLEASE IGNORE.
    
    Signed-off-by: Mauro Carvalho Chehab <mchehab@kernel.org>

--- drivers/media/test-drivers/vivid/vivid-core.c
+++ drivers/media/test-drivers/vivid/vivid-core.c
@@ -5,6 +5,8 @@
  * Copyright 2014 Cisco Systems, Inc. and/or its affiliates. All rights reserved.
  */
 
+/* TEST patch - please ignore */
+
 #include <linux/module.h>
 #include <linux/errno.h>
 #include <linux/kernel.h>
