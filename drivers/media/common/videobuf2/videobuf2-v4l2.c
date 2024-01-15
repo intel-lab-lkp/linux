a77683ca377d14dfa5d11f569b7a97f7a2fdf76a
Benjamin Gaignard <benjamin.gaignard@collabora.com>
2024-01-15 18:08:25 +0100

    media: core: Refactor vb2_ioctl_create_bufs() to always set capabilities flags
    vb2_create_bufs() takes care of setting V4L2_BUF_CAP_SUPPORTS_MAX_NUM_BUFFERS
    flag and vb2_queue max_num_buffers field so call it instead duplicate the
    same code in vb2_ioctl_create_bufs(). Testing p->count after calling
    vb2_create_bufs() is safe since the function also check it.
    
    Fixes: d055a76c0065 ("media: core: Report the maximum possible number of buffers for the queue")
    Signed-off-by: Benjamin Gaignard <benjamin.gaignard@collabora.com>

--- drivers/media/common/videobuf2/videobuf2-v4l2.c
+++ drivers/media/common/videobuf2/videobuf2-v4l2.c
@@ -1029,23 +1029,24 @@ int vb2_ioctl_create_bufs(struct file *file, void *priv,
 	int res = vb2_verify_memory_type(vdev->queue, p->memory,
 			p->format.type);
 
-	p->index = vdev->queue->num_buffers;
-	fill_buf_caps(vdev->queue, &p->capabilities);
-	validate_memory_flags(vdev->queue, p->memory, &p->flags);
-	/*
-	 * If count == 0, then just check if memory and type are valid.
-	 * Any -EBUSY result from vb2_verify_memory_type can be mapped to 0.
-	 */
-	if (p->count == 0)
-		return res != -EBUSY ? res : 0;
-	if (res)
-		return res;
-	if (vb2_queue_is_busy(vdev->queue, file))
-		return -EBUSY;
+	if (vb2_queue_is_busy(vdev->queue, file)) {
+		res = -EBUSY;
+		goto failed;
+	}
 
 	res = vb2_create_bufs(vdev->queue, p);
 	if (res == 0)
 		vdev->queue->owner = file->private_data;
+
+failed:
+	/*
+	 * If count == 0, then just check if memory and type are valid.
+	 * Any -EBUSY result from vb2_verify_memory_type() or
+	 * vb2_queue_is_busy() can be mapped to 0.
+	 */
+	if (p->count == 0)
+		return res != -EBUSY ? res : 0;
+
 	return res;
 }
 EXPORT_SYMBOL_GPL(vb2_ioctl_create_bufs);
