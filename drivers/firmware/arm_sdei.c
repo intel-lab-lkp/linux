258bd26919f66380a62d430acfd513345404b238
Levi Yun <ppbuk5246@gmail.com>
2024-01-18 20:20:54 +0000

    firmware/arm_sdei: Fix invalid argument to unregister private events.
    To unregister private events in device freeze, it should call
    cpuhp_remove_state with sdei_hp_state not sdei_entry_point.
    otherwise, it seems to fail on cpuhp_cb_check.
    
    Signed-off-by: Levi Yun <ppbuk5246@gmail.com>
    Fixes: d2c48b2387eb ("firmware: arm_sdei: Fix sleep from invalid context BUG")
    Reviewed-by: James Morse <james.morse@arm.com>

--- drivers/firmware/arm_sdei.c
+++ drivers/firmware/arm_sdei.c
@@ -763,7 +763,9 @@ static int sdei_device_freeze(struct device *dev)
 	int err;
 
 	/* unregister private events */
-	cpuhp_remove_state(sdei_entry_point);
+	cpuhp_remove_state(sdei_hp_state);
+
+	sdei_hp_state = 0;
 
 	err = sdei_unregister_shared();
 	if (err)
