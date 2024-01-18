7e23e4751a35869e91db9119d66f7cbdc653efcf
Jedrzej Jagielski <jedrzej.jagielski@intel.com>
2024-01-18 14:43:30 +0100

    ixgbe: Convert ret val type from s32 to int
    Currently big amount of the functions returning standard error codes
    are of type s32. Convert them to regular ints as typdefs here are not
    necessary to return standard error codes.
    
    Suggested-by: Jacob Keller <jacob.e.keller@intel.com>
    Reviewed-by: Jacob Keller <jacob.e.keller@intel.com>
    Signed-off-by: Jedrzej Jagielski <jedrzej.jagielski@intel.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_dcb_82598.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_dcb_82598.c
@@ -15,7 +15,7 @@
  *
  * Configure Rx Data Arbiter and credits for each traffic class.
  */
-s32 ixgbe_dcb_config_rx_arbiter_82598(struct ixgbe_hw *hw,
+int ixgbe_dcb_config_rx_arbiter_82598(struct ixgbe_hw *hw,
 					u16 *refill,
 					u16 *max,
 					u8 *prio_type)
@@ -75,7 +75,7 @@ s32 ixgbe_dcb_config_rx_arbiter_82598(struct ixgbe_hw *hw,
  *
  * Configure Tx Descriptor Arbiter and credits for each traffic class.
  */
-s32 ixgbe_dcb_config_tx_desc_arbiter_82598(struct ixgbe_hw *hw,
+int ixgbe_dcb_config_tx_desc_arbiter_82598(struct ixgbe_hw *hw,
 						u16 *refill,
 						u16 *max,
 						u8 *bwg_id,
@@ -124,7 +124,7 @@ s32 ixgbe_dcb_config_tx_desc_arbiter_82598(struct ixgbe_hw *hw,
  *
  * Configure Tx Data Arbiter and credits for each traffic class.
  */
-s32 ixgbe_dcb_config_tx_data_arbiter_82598(struct ixgbe_hw *hw,
+int ixgbe_dcb_config_tx_data_arbiter_82598(struct ixgbe_hw *hw,
 						u16 *refill,
 						u16 *max,
 						u8 *bwg_id,
@@ -171,7 +171,7 @@ s32 ixgbe_dcb_config_tx_data_arbiter_82598(struct ixgbe_hw *hw,
  *
  * Configure Priority Flow Control for each traffic class.
  */
-s32 ixgbe_dcb_config_pfc_82598(struct ixgbe_hw *hw, u8 pfc_en)
+int ixgbe_dcb_config_pfc_82598(struct ixgbe_hw *hw, u8 pfc_en)
 {
 	u32 fcrtl, reg;
 	u8  i;
@@ -224,7 +224,7 @@ s32 ixgbe_dcb_config_pfc_82598(struct ixgbe_hw *hw, u8 pfc_en)
  * Configure queue statistics registers, all queues belonging to same traffic
  * class uses a single set of queue statistics counters.
  */
-static s32 ixgbe_dcb_config_tc_stats_82598(struct ixgbe_hw *hw)
+static int ixgbe_dcb_config_tc_stats_82598(struct ixgbe_hw *hw)
 {
 	u32 reg = 0;
 	u8  i   = 0;
@@ -260,7 +260,7 @@ static s32 ixgbe_dcb_config_tc_stats_82598(struct ixgbe_hw *hw)
  *
  * Configure dcb settings and enable dcb mode.
  */
-s32 ixgbe_dcb_hw_config_82598(struct ixgbe_hw *hw, u8 pfc_en, u16 *refill,
+int ixgbe_dcb_hw_config_82598(struct ixgbe_hw *hw, u8 pfc_en, u16 *refill,
 			      u16 *max, u8 *bwg_id, u8 *prio_type)
 {
 	ixgbe_dcb_config_rx_arbiter_82598(hw, refill, max, prio_type);
