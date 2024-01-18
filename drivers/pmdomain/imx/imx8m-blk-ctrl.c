dc649f0edd77a2cac50d9584917c9916ba9c1d89
Marek Vasut <marex@denx.de>
2024-01-18 20:12:24 +0100

    pmdomain: imx8m-blk-ctrl: imx8mp-blk-ctrl: Error out if domains are missing in DT
    This driver assumes that domain->power_dev is non-NULL in its suspend/resume
    path. The assumption is valid, since all the devices that are being looked up
    here should be described in DT. In case they are not described in DT, beause
    the DT is faulty, suspend/resume attempt would trigger NULL pointer dereference.
    To avoid this failure, check whether the power_dev assignment is not NULL right
    away in probe callback and fail early if it is.
    
    Signed-off-by: Marek Vasut <marex@denx.de>

--- drivers/pmdomain/imx/imx8m-blk-ctrl.c
+++ drivers/pmdomain/imx/imx8m-blk-ctrl.c
@@ -258,7 +258,7 @@ static int imx8m_blk_ctrl_probe(struct platform_device *pdev)
 
 		domain->power_dev =
 			dev_pm_domain_attach_by_name(dev, data->gpc_name);
-		if (IS_ERR(domain->power_dev)) {
+		if (IS_ERR_OR_NULL(domain->power_dev)) {
 			dev_err_probe(dev, PTR_ERR(domain->power_dev),
 				      "failed to attach power domain \"%s\"\n",
 				      data->gpc_name);
