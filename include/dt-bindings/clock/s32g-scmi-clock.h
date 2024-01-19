1464d764a7f4acc03c88452e99602b43caa3a211
Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>
2024-01-19 15:02:28 +0200

    dt-bindings: clock: s32g: add uSDHC clock IDs
    Add the SCMI clock IDs for the uSDHC controller present on
    S32G SoCs.
    
    Signed-off-by: Ciprian Costea <ciprianmarian.costea@nxp.com>
    Signed-off-by: Ghennadi Procopciuc <ghennadi.procopciuc@nxp.com>

---
+++ include/dt-bindings/clock/s32g-scmi-clock.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-3-Clause) */
+/*
+ * Copyright 2020-2024 NXP
+ */
+#ifndef _DT_BINDINGS_SCMI_CLK_S32G_H
+#define _DT_BINDINGS_SCMI_CLK_S32G_H
+
+/* uSDHC */
+#define S32G_SCMI_CLK_USDHC_AHB		31
+#define S32G_SCMI_CLK_USDHC_MODULE	32
+#define S32G_SCMI_CLK_USDHC_CORE	33
+#define S32G_SCMI_CLK_USDHC_MOD32K	34
+
+#endif
