5b2a56f98dbc484331f142601e25b14ff3d0a73f
Yi Sun <yi.sun@unisoc.com>
2024-01-22 19:07:22 +0800

    virtio-blk: Ensure no requests in virtqueues before deleting vqs.
    Ensure no remaining requests in virtqueues before resetting vdev and
    deleting virtqueues. Otherwise these requests will never be completed.
    It may cause the system to become unresponsive. So it is better to place
    blk_mq_quiesce_queue() in front of virtio_reset_device().
    
    Function blk_mq_quiesce_queue() can ensure that requests have become
    in_flight status, but it cannot guarantee that requests have been
    processed by the device. Virtqueues should never be deleted before
    all requests become complete status.
    
    New function blk_mq_tagset_wait_request_completed() ensure that all
    requests in virtqueues become complete status.
    
    Signed-off-by: Yi Sun <yi.sun@unisoc.com>

--- drivers/block/virtio_blk.c
+++ drivers/block/virtio_blk.c
@@ -1593,13 +1593,15 @@ static int virtblk_freeze(struct virtio_device *vdev)
 {
 	struct virtio_blk *vblk = vdev->priv;
 
+	/* Ensure no requests in virtqueues before deleting vqs. */
+	blk_mq_quiesce_queue(vblk->disk->queue);
+	blk_mq_tagset_wait_request_completed(vblk->disk->queue->tag_set);
+
 	/* Ensure we don't receive any more interrupts */
 	virtio_reset_device(vdev);
 
 	/* Make sure no work handler is accessing the device. */
 	flush_work(&vblk->config_work);
-
-	blk_mq_quiesce_queue(vblk->disk->queue);
 
 	vdev->config->del_vqs(vdev);
 	kfree(vblk->vqs);
