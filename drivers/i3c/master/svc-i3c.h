a4a03234a5acb1e981b882a1e24f985ffdc9f6ec
Frank Li <Frank.Li@nxp.com>
2024-01-23 16:25:46 -0500

    i3c: target: add svc target controller support
    Add Silvaco I3C target controller support
    
    Signed-off-by: Frank Li <Frank.Li@nxp.com>

--- drivers/i3c/master/svc-i3c.h
+++ drivers/i3c/master/svc-i3c.h
@@ -8,4 +8,7 @@ void svc_i3c_master_remove(struct platform_device *pdev);
 int svc_i3c_master_runtime_suspend(struct device *dev);
 int svc_i3c_master_runtime_resume(struct device *dev);
 
+int svc_i3c_target_probe(struct platform_device *pdev);
+void svc_i3c_target_remove(struct platform_device *pdev);
+
 #endif
