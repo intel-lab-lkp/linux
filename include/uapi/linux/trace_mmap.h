bc2ee66dcfb05f793cacadaf3f552ca69f185299
Vincent Donnefort <vdonnefort@google.com>
2024-01-23 11:07:53 +0000

    ring-buffer: Introducing ring-buffer mapping functions
    In preparation for allowing the user-space to map a ring-buffer, add
    a set of mapping functions:
    
      ring_buffer_{map,unmap}()
      ring_buffer_map_fault()
    
    And controls on the ring-buffer:
    
      ring_buffer_map_get_reader()  /* swap reader and head */
    
    Mapping the ring-buffer also involves:
    
      A unique ID for each subbuf of the ring-buffer, currently they are
      only identified through their in-kernel VA.
    
      A meta-page, where are stored ring-buffer statistics and a
      description for the current reader
    
    The linear mapping exposes the meta-page, and each subbuf of the
    ring-buffer, ordered following their unique ID, assigned during the
    first mapping.
    
    Once mapped, no subbuf can get in or out of the ring-buffer: the buffer
    size will remain unmodified and the splice enabling functions will in
    reality simply memcpy the data instead of swapping subbufs.
    
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

---
+++ include/uapi/linux/trace_mmap.h
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _TRACE_MMAP_H_
+#define _TRACE_MMAP_H_
+
+#include <linux/types.h>
+
+/**
+ * struct trace_buffer_meta - Ring-buffer Meta-page description
+ * @meta_page_size:	Size of this meta-page.
+ * @meta_struct_len:	Size of this structure.
+ * @subbuf_size:	Size of each subbuf, including the header.
+ * @nr_subbufs:		Number of subbfs in the ring-buffer.
+ * @reader.lost_events:	Number of events lost at the time of the reader swap.
+ * @reader.id:		subbuf ID of the current reader. From 0 to @nr_subbufs - 1
+ * @reader.read:	Number of bytes read on the reader subbuf.
+ * @entries:		Number of entries in the ring-buffer.
+ * @overrun:		Number of entries lost in the ring-buffer.
+ * @read:		Number of entries that have been read.
+ * @subbufs_touched:	Number of subbufs that have been filled.
+ * @subbufs_lost:	Number of subbufs lost to overrun.
+ * @subbufs_read:	Number of subbufs that have been read.
+ */
+struct trace_buffer_meta {
+	__u32		meta_page_size;
+	__u32		meta_struct_len;
+
+	__u32		subbuf_size;
+	__u32		nr_subbufs;
+
+	struct {
+		__u64	lost_events;
+		__u32	id;
+		__u32	read;
+	} reader;
+
+	__u64	entries;
+	__u64	overrun;
+	__u64	read;
+
+	__u64	subbufs_touched;
+	__u64	subbufs_lost;
+};
+
+#endif /* _TRACE_MMAP_H_ */
