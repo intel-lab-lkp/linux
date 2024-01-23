13f82215e1e9b3efb3219dc3c2f6e4ac333ea02d
Vincent Donnefort <vdonnefort@google.com>
2024-01-23 11:07:55 +0000

    tracing: Allow user-space mapping of the ring-buffer
    Currently, user-space extracts data from the ring-buffer via splice,
    which is handy for storage or network sharing. However, due to splice
    limitations, it is imposible to do real-time analysis without a copy.
    
    A solution for that problem is to let the user-space map the ring-buffer
    directly.
    
    The mapping is exposed via the per-CPU file trace_pipe_raw. The first
    element of the mapping is the meta-page. It is followed by each
    subbuffer constituting the ring-buffer, ordered by their unique page ID:
    
      * Meta-page -- include/uapi/linux/trace_mmap.h for a description
      * Subbuf ID 0
      * Subbuf ID 1
         ...
    
    It is therefore easy to translate a subbuf ID into an offset in the
    mapping:
    
      reader_id = meta->reader->id;
      reader_offset = meta->meta_page_size + reader_id * meta->subbuf_size;
    
    When new data is available, the mapper must call a newly introduced ioctl:
    TRACE_MMAP_IOCTL_GET_READER. This will update the Meta-page reader ID to
    point to the next reader containing unread data.
    
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -1175,6 +1175,12 @@ static void tracing_snapshot_instance_cond(struct trace_array *tr,
 		return;
 	}
 
+	if (tr->mapped) {
+		trace_array_puts(tr, "*** BUFFER MEMORY MAPPED ***\n");
+		trace_array_puts(tr, "*** Can not use snapshot (sorry) ***\n");
+		return;
+	}
+
 	local_irq_save(flags);
 	update_max_tr(tr, current, smp_processor_id(), cond_data);
 	local_irq_restore(flags);
@@ -1307,6 +1313,9 @@ static int tracing_arm_snapshot_locked(struct trace_array *tr)
 	lockdep_assert_held(&trace_types_lock);
 
 	if (tr->snapshot == UINT_MAX)
+		return -EBUSY;
+
+	if (tr->mapped)
 		return -EBUSY;
 
 	ret = tracing_alloc_snapshot_instance(tr);
@@ -6520,7 +6529,7 @@ static void tracing_set_nop(struct trace_array *tr)
 {
 	if (tr->current_trace == &nop_trace)
 		return;
-	
+
 	tr->current_trace->enabled--;
 
 	if (tr->current_trace->reset)
@@ -8637,15 +8646,31 @@ out:
 	return ret;
 }
 
-/* An ioctl call with cmd 0 to the ring buffer file will wake up all waiters */
 static long tracing_buffers_ioctl(struct file *file, unsigned int cmd, unsigned long arg)
 {
 	struct ftrace_buffer_info *info = file->private_data;
 	struct trace_iterator *iter = &info->iter;
+	int err;
 
-	if (cmd)
-		return -ENOIOCTLCMD;
+	if (cmd == TRACE_MMAP_IOCTL_GET_READER) {
+		if (!(file->f_flags & O_NONBLOCK)) {
+			err = ring_buffer_wait(iter->array_buffer->buffer,
+					       iter->cpu_file,
+					       iter->tr->buffer_percent);
+			if (err)
+				return err;
+		}
 
+		return ring_buffer_map_get_reader(iter->array_buffer->buffer,
+						  iter->cpu_file);
+	} else if (cmd) {
+		return -ENOTTY;
+	}
+
+	/*
+	 * An ioctl call with cmd 0 to the ring buffer file will wake up all
+	 * waiters
+	 */
 	mutex_lock(&trace_types_lock);
 
 	iter->wait_index++;
@@ -8658,6 +8683,90 @@ static long tracing_buffers_ioctl(struct file *file, unsigned int cmd, unsigned 
 	return 0;
 }
 
+static vm_fault_t tracing_buffers_mmap_fault(struct vm_fault *vmf)
+{
+	struct ftrace_buffer_info *info = vmf->vma->vm_file->private_data;
+	struct trace_iterator *iter = &info->iter;
+	vm_fault_t ret = VM_FAULT_SIGBUS;
+	struct page *page;
+
+	page = ring_buffer_map_fault(iter->array_buffer->buffer, iter->cpu_file,
+				     vmf->pgoff);
+	if (!page)
+		return ret;
+
+	get_page(page);
+	vmf->page = page;
+	vmf->page->mapping = vmf->vma->vm_file->f_mapping;
+	vmf->page->index = vmf->pgoff;
+
+	return 0;
+}
+
+static void tracing_buffers_mmap_close(struct vm_area_struct *vma)
+{
+	struct ftrace_buffer_info *info = vma->vm_file->private_data;
+	struct trace_iterator *iter = &info->iter;
+
+	ring_buffer_unmap(iter->array_buffer->buffer, iter->cpu_file);
+
+#ifdef CONFIG_TRACER_MAX_TRACE
+	mutex_lock(&trace_types_lock);
+	if (!WARN_ON(!iter->tr->mapped))
+		iter->tr->mapped--;
+	mutex_unlock(&trace_types_lock);
+#endif
+}
+
+static void tracing_buffers_mmap_open(struct vm_area_struct *vma)
+{
+	struct ftrace_buffer_info *info = vma->vm_file->private_data;
+	struct trace_iterator *iter = &info->iter;
+
+	WARN_ON(ring_buffer_map(iter->array_buffer->buffer, iter->cpu_file));
+}
+
+static const struct vm_operations_struct tracing_buffers_vmops = {
+	.open		= tracing_buffers_mmap_open,
+	.close		= tracing_buffers_mmap_close,
+	.fault		= tracing_buffers_mmap_fault,
+};
+
+static int tracing_buffers_mmap(struct file *filp, struct vm_area_struct *vma)
+{
+	struct ftrace_buffer_info *info = filp->private_data;
+	struct trace_iterator *iter = &info->iter;
+	int ret = 0;
+
+	if (vma->vm_flags & VM_WRITE)
+		return -EPERM;
+
+	vm_flags_mod(vma, VM_DONTCOPY | VM_DONTDUMP, VM_MAYWRITE);
+	vma->vm_ops = &tracing_buffers_vmops;
+
+#ifdef CONFIG_TRACER_MAX_TRACE
+	mutex_lock(&trace_types_lock);
+	if (iter->tr->snapshot || iter->tr->mapped == INT_MAX) {
+		ret = -EBUSY;
+		goto unlock;
+	}
+
+	/* Wait for update_max_tr() to observe iter->tr->mapped */
+	if (!iter->tr->mapped++)
+		synchronize_rcu();
+#endif
+
+	ret = ring_buffer_map(iter->array_buffer->buffer, iter->cpu_file);
+
+#ifdef CONFIG_TRACER_MAX_TRACE
+	if (ret)
+		iter->tr->mapped--;
+unlock:
+	mutex_unlock(&trace_types_lock);
+#endif
+	return ret;
+}
+
 static const struct file_operations tracing_buffers_fops = {
 	.open		= tracing_buffers_open,
 	.read		= tracing_buffers_read,
@@ -8666,6 +8775,7 @@ static const struct file_operations tracing_buffers_fops = {
 	.splice_read	= tracing_buffers_splice_read,
 	.unlocked_ioctl = tracing_buffers_ioctl,
 	.llseek		= no_llseek,
+	.mmap		= tracing_buffers_mmap,
 };
 
 static ssize_t
