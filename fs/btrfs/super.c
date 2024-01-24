d00fec6ce71566e0ece0bb2b01ace51493594d7c
Josef Bacik <josef@toxicpanda.com>
2024-01-24 12:19:13 -0500

    btrfs: disable encryption on RAID5/6
    The RAID5/6 code will re-arrange bios and submit them through a
    different mechanism.  This is problematic with inline encryption as we
    have to get the bio and csum it after it's been encrypted, and the
    radi5/6 bio's don't have the btrfs_bio embedded, so we have no way to
    get the csums put on disk.
    
    This isn't an unsolvable problem, but would require a bit of reworking.
    Since we discourage users from using this code currently simply don't
    allow encryption on RAID5/6 setups.  If there's sufficient demand in the
    future we can add the support for this.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -679,6 +679,12 @@ bool btrfs_check_options(struct btrfs_fs_info *info, unsigned long *mount_opt,
 	if (btrfs_check_mountopts_zoned(info, mount_opt))
 		ret = false;
 
+	if (btrfs_fs_incompat(info, RAID56) &&
+	    btrfs_raw_test_opt(*mount_opt, TEST_DUMMY_ENCRYPTION)) {
+		btrfs_err(info, "cannot use test_dummy_encryption with RAID5/6");
+		ret = false;
+	}
+
 	if (!test_bit(BTRFS_FS_STATE_REMOUNTING, &info->fs_state)) {
 		if (btrfs_raw_test_opt(*mount_opt, SPACE_CACHE))
 			btrfs_info(info, "disk space caching is enabled");
