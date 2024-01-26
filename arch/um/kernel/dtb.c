bc27c9235e9503319d5ececdc1565093c2c38fd7
Oreoluwa Babatunde <quic_obabatun@quicinc.com>
2024-01-26 15:54:23 -0800

    um: reserved_mem: Switch fdt_init_reserved_mem() to dt_init_reserved_mem()
    Switch from using fdt_init_reserved_mem() to dt_init_reserved_mem() to
    reflect the use of the unflatten devicetree APIs to process the
    reserved memory regions.
    
    Signed-off-by: Oreoluwa Babatunde <quic_obabatun@quicinc.com>

--- arch/um/kernel/dtb.c
+++ arch/um/kernel/dtb.c
@@ -28,7 +28,7 @@ void uml_dtb_init(void)
 
 	early_fdt_scan_reserved_mem();
 	unflatten_device_tree();
-	fdt_init_reserved_mem();
+	dt_init_reserved_mem();
 }
 
 static int __init uml_dtb_setup(char *line, int *add)
