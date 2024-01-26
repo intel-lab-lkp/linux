1578d24135a5b0fb631055ed8145896755e79579
Oreoluwa Babatunde <quic_obabatun@quicinc.com>
2024-01-26 15:53:56 -0800

    of: reserved_mem: Delete the early_init_fdt_scan_reserved_mem() function
    Delete the early_init_fdt_scan_reserved_mem() function definition since
    this function is no longer being used anywhere in the kernel.
    
    Signed-off-by: Oreoluwa Babatunde <quic_obabatun@quicinc.com>

--- include/linux/of_fdt.h
+++ include/linux/of_fdt.h
@@ -62,7 +62,6 @@ extern int early_init_dt_scan_chosen(char *cmdline);
 extern int early_init_dt_scan_memory(void);
 extern void early_init_dt_check_for_usable_mem_range(void);
 extern int early_init_dt_scan_chosen_stdout(void);
-extern void early_init_fdt_scan_reserved_mem(void);
 extern void early_fdt_scan_reserved_mem(void);
 extern void early_init_fdt_reserve_self(void);
 extern void early_init_dt_add_memory_arch(u64 base, u64 size);
@@ -88,7 +87,6 @@ extern void early_get_first_memblock_info(void *, phys_addr_t *);
 #else /* CONFIG_OF_EARLY_FLATTREE */
 static inline void early_init_dt_check_for_usable_mem_range(void) {}
 static inline int early_init_dt_scan_chosen_stdout(void) { return -ENODEV; }
-static inline void early_init_fdt_scan_reserved_mem(void) {}
 static inline void early_fdt_scan_reserved_mem(void) {}
 static inline void early_init_fdt_reserve_self(void) {}
 static inline const char *of_flat_dt_get_machine_name(void) { return NULL; }
