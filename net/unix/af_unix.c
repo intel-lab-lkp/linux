94daa0f9f7a1082db6b472920bf792f641a4b868
Kuniyuki Iwashima <kuniyu@amazon.com>
2024-01-28 02:37:31 -0800

    af_unix: Set sk_peer_pid/sk_peer_cred locklessly for new socket.
    init_peercred() is called in 3 places:
    
      1. socketpair() : both sockets
      2. connect()    : child socket
      3. listen()     : listening socket
    
    The first two need not hold sk_peer_lock because no one can
    touch the socket.
    
    Let's set cred/pid without holding lock for the two cases and
    rename the old init_peercred() to update_peercred() to properly
    reflect the use case.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -685,14 +685,19 @@ static void unix_release_sock(struct sock *sk, int embrion)
 
 static void init_peercred(struct sock *sk)
 {
+	sk->sk_peer_pid = get_pid(task_tgid(current));
+	sk->sk_peer_cred = get_current_cred();
+}
+
+static void update_peercred(struct sock *sk)
+{
 	const struct cred *old_cred;
 	struct pid *old_pid;
 
 	spin_lock(&sk->sk_peer_lock);
 	old_pid = sk->sk_peer_pid;
 	old_cred = sk->sk_peer_cred;
-	sk->sk_peer_pid  = get_pid(task_tgid(current));
-	sk->sk_peer_cred = get_current_cred();
+	init_peercred(sk);
 	spin_unlock(&sk->sk_peer_lock);
 
 	put_pid(old_pid);
@@ -743,7 +748,7 @@ static int unix_listen(struct socket *sock, int backlog)
 	sk->sk_max_ack_backlog	= backlog;
 	sk->sk_state		= TCP_LISTEN;
 	/* set credentials so connect can copy them */
-	init_peercred(sk);
+	update_peercred(sk);
 	err = 0;
 
 out_unlock:
