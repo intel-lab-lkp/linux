45c89a2021ab81b445e8aad347d8815e64796c16
Kefeng Wang <wangkefeng.wang@huawei.com>
2024-01-29 15:09:29 +0800

    mm: remove MIGRATE_SYNC_NO_COPY mode
    Commit 2916ecc0f9d4 ("mm/migrate: new migrate mode MIGRATE_SYNC_NO_COPY")
    introduce a new MIGRATE_SYNC_NO_COPY mode to allow to offload the copy to
    a device DMA engine, which is only used __migrate_device_pages() to decide
    whether or not copy the old page, and the MIGRATE_SYNC_NO_COPY mode only
    set in hmm, as the MIGRATE_SYNC_NO_COPY set is removed by previous cleanup,
    it seems that we could remove the unnecessary MIGRATE_SYNC_NO_COPY.
    
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>

--- mm/migrate.c
+++ mm/migrate.c
@@ -668,10 +668,7 @@ static int __migrate_folio(struct address_space *mapping, struct folio *dst,
 	if (src_private)
 		folio_attach_private(dst, folio_detach_private(src));
 
-	if (mode != MIGRATE_SYNC_NO_COPY)
-		folio_migrate_copy(dst, src);
-	else
-		folio_migrate_flags(dst, src);
+	folio_migrate_copy(dst, src);
 
 	return MIGRATEPAGE_SUCCESS;
 }
@@ -901,7 +898,6 @@ static int fallback_migrate_folio(struct address_space *mapping,
 		/* Only writeback folios in full synchronous migration */
 		switch (mode) {
 		case MIGRATE_SYNC:
-		case MIGRATE_SYNC_NO_COPY:
 			break;
 		default:
 			return -EBUSY;
@@ -1159,7 +1155,6 @@ static int migrate_folio_unmap(new_folio_t get_new_folio,
 		 */
 		switch (mode) {
 		case MIGRATE_SYNC:
-		case MIGRATE_SYNC_NO_COPY:
 			break;
 		default:
 			rc = -EBUSY;
@@ -1370,7 +1365,6 @@ static int unmap_and_move_huge_page(new_folio_t get_new_folio,
 			goto out;
 		switch (mode) {
 		case MIGRATE_SYNC:
-		case MIGRATE_SYNC_NO_COPY:
 			break;
 		default:
 			goto out;
