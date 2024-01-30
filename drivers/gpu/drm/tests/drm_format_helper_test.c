79a65679e1acbb94a814ca39258afc77afda9b3c
Thomas Zimmermann <tzimmermann@suse.de>
2024-01-30 10:53:47 +0100

    drm/format-helper: Use struct drm_pixmap for drm_fb_xrgb8888_to_argb8888()
    Store the source-buffer parameters of drm_fb_xrgb8888_to_argb8888()
    in struct drm_pixmap. Update the function's interface and all of its
    callers.
    
    Callers of drm_fb_xrgb8888_to_argb8888() initialize the pixmap's
    instance from a pre-existing instance of struct drm_framebuffer.
    There's potential to simplify some of that code in a later patch.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>

--- drivers/gpu/drm/tests/drm_format_helper_test.c
+++ drivers/gpu/drm/tests/drm_format_helper_test.c
@@ -937,6 +937,7 @@ static void drm_test_fb_xrgb8888_to_argb8888(struct kunit *test)
 	u32 *buf = NULL;
 	__le32 *xrgb8888 = NULL;
 	struct iosys_map dst, src;
+	struct drm_pixmap src_pix;
 
 	struct drm_framebuffer fb = {
 		.format = drm_format_info(DRM_FORMAT_XRGB8888),
@@ -958,7 +959,9 @@ static void drm_test_fb_xrgb8888_to_argb8888(struct kunit *test)
 	const unsigned int *dst_pitch = (result->dst_pitch == TEST_USE_DEFAULT_PITCH) ?
 		NULL : &result->dst_pitch;
 
-	drm_fb_xrgb8888_to_argb8888(&dst, dst_pitch, &src, &fb, &params->clip, &fmtcnv_state);
+	drm_pixmap_init_from_framebuffer(&src_pix, &fb, &src, &params->clip);
+
+	drm_fb_xrgb8888_to_argb8888(&dst, dst_pitch, &src_pix, &fmtcnv_state);
 	buf = le32buf_to_cpu(test, (__force const __le32 *)buf, dst_size / sizeof(u32));
 	KUNIT_EXPECT_MEMEQ(test, buf, result->expected, dst_size);
 
--- include/drm/drm_format_helper.h
+++ include/drm/drm_format_helper.h
@@ -121,8 +121,8 @@ void drm_fb_xrgb8888_to_rgb888(struct iosys_map *dst, const unsigned int *dst_pi
 			       const struct drm_pixmap *src_pix,
 			       struct drm_format_conv_state *state);
 void drm_fb_xrgb8888_to_argb8888(struct iosys_map *dst, const unsigned int *dst_pitch,
-				 const struct iosys_map *src, const struct drm_framebuffer *fb,
-				 const struct drm_rect *clip, struct drm_format_conv_state *state);
+				 const struct drm_pixmap *src_pix,
+				 struct drm_format_conv_state *state);
 void drm_fb_xrgb8888_to_xrgb2101010(struct iosys_map *dst, const unsigned int *dst_pitch,
 				    const struct iosys_map *src, const struct drm_framebuffer *fb,
 				    const struct drm_rect *clip,
