194df306b5f43f2da2dc1a9039cae2aa2071e7d3
Mario Limonciello <mario.limonciello@amd.com>
2024-01-30 13:26:08 -0600

    drm/nouveau: Use drm_get_acpi_edid() helper
    Rather than inventing a wrapper to acpi_video_get_edid() use the
    one provided by drm. This fixes two problems:
    1. A memory leak that the memory provided by the ACPI call was
       never freed.
    2. Validation of the BIOS provided blob.
    
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- drivers/gpu/drm/nouveau/nouveau_acpi.c
+++ drivers/gpu/drm/nouveau/nouveau_acpi.c
@@ -360,33 +360,6 @@ void nouveau_unregister_dsm_handler(void) {}
 void nouveau_switcheroo_optimus_dsm(void) {}
 #endif
 
-void *
-nouveau_acpi_edid(struct drm_device *dev, struct drm_connector *connector)
-{
-	struct acpi_device *acpidev;
-	int type, ret;
-	void *edid;
-
-	switch (connector->connector_type) {
-	case DRM_MODE_CONNECTOR_LVDS:
-	case DRM_MODE_CONNECTOR_eDP:
-		type = ACPI_VIDEO_DISPLAY_LCD;
-		break;
-	default:
-		return NULL;
-	}
-
-	acpidev = ACPI_COMPANION(dev->dev);
-	if (!acpidev)
-		return NULL;
-
-	ret = acpi_video_get_edid(acpidev, type, -1, &edid);
-	if (ret < 0)
-		return NULL;
-
-	return kmemdup(edid, EDID_LENGTH, GFP_KERNEL);
-}
-
 bool nouveau_acpi_video_backlight_use_native(void)
 {
 	return acpi_video_backlight_use_native();
