c937858182988c2cd5cf346af0e343a0afe35d31
Bastien Curutchet <bastien.curutchet@bootlin.com>
2024-01-30 09:59:34 +0100

    dt-bindings: net: Add TI DP83640
    The TI DP83640 is a PTP PHY. Some of his features can be enabled by
    hardware straps or by registers configuration. Add a device tree
    binding for configuration through registers
    
    Signed-off-by: Bastien Curutchet <bastien.curutchet@bootlin.com>

---
+++ include/dt-bindings/net/ti-dp83640.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
+/*
+ * Device Tree constants for the Texas Instruments DP83640 PHY
+ *
+ * Author: Bastien Curutchet bastien.curutchet@bootlin.com>
+ *
+ * Copyright: 2024 Nanometrics Inc.
+ */
+
+#ifndef _DT_BINDINGS_TI_DP83640_H
+#define _DT_BINDINGS_TI_DP83640_H
+
+/* PHY CTRL bits */
+#define DP83640_PHYCR_LED_CNFG_MODE_1 1
+#define DP83640_PHYCR_LED_CNFG_MODE_2 2
+#define DP83640_PHYCR_LED_CNFG_MODE_3 3
+
+#endif
