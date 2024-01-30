14815b03aa4bdc13e3ab1d195aafbf46e2f93a5e
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2024-01-30 11:52:50 -0500

    ext2: Use dax_is_supported()
    Use dax_is_supported() to validate whether the architecture has
    virtually aliased data caches at mount time. Print an error and disable
    DAX if dax=always is requested as a mount option on an architecture
    which does not support DAX.
    
    This is relevant for architectures which require a dynamic check
    to validate whether they have virtually aliased data caches.
    
    Fixes: d92576f1167c ("dax: does not work correctly with virtual aliasing caches")
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Jan Kara <jack@suse.com>
    Cc: linux-ext4@vger.kernel.org
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: linux-mm@kvack.org
    Cc: linux-arch@vger.kernel.org
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Vishal Verma <vishal.l.verma@intel.com>
    Cc: Dave Jiang <dave.jiang@intel.com>
    Cc: Matthew Wilcox <willy@infradead.org>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Russell King <linux@armlinux.org.uk>
    Cc: nvdimm@lists.linux.dev
    Cc: linux-cxl@vger.kernel.org
    Cc: linux-fsdevel@vger.kernel.org

--- fs/ext2/super.c
+++ fs/ext2/super.c
@@ -955,7 +955,11 @@ static int ext2_fill_super(struct super_block *sb, void *data, int silent)
 	blocksize = BLOCK_SIZE << le32_to_cpu(sbi->s_es->s_log_block_size);
 
 	if (test_opt(sb, DAX)) {
-		if (!sbi->s_daxdev) {
+		if (!dax_is_supported()) {
+			ext2_msg(sb, KERN_ERR,
+				"DAX unsupported by architecture. Turning off DAX.");
+			clear_opt(sbi->s_mount_opt, DAX);
+		} else if (!sbi->s_daxdev) {
 			ext2_msg(sb, KERN_ERR,
 				"DAX unsupported by block device. Turning off DAX.");
 			clear_opt(sbi->s_mount_opt, DAX);
