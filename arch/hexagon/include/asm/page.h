c2948d31f068d982ff113cad911aac5d3219f4c9
Yan Zhao <yan.y.zhao@intel.com>
2024-01-31 14:00:21 +0800

    Hexagon: apply page shift to PFN instead of VA in pfn_to_virt
    Apply the page shift to PFN to get physical address for final VA.
    The macro __va should take physical address instead of PFN as input.
    
    Fixes: d6e81532b10d ("Hexagon: Make pfn accessors statics inlines")
    Signed-off-by: Yan Zhao <yan.y.zhao@intel.com>

--- arch/hexagon/include/asm/page.h
+++ arch/hexagon/include/asm/page.h
@@ -135,7 +135,7 @@ static inline unsigned long virt_to_pfn(const void *kaddr)
 
 static inline void *pfn_to_virt(unsigned long pfn)
 {
-	return (void *)((unsigned long)__va(pfn) << PAGE_SHIFT);
+	return __va(pfn << PAGE_SHIFT);
 }
 
 
