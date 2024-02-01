aa4026e3e8bf82f73d7dbcacdfec27c3f3236c18
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2024-01-31 17:07:44 -0800

    platform/x86/intel/sdsi: Add in-band BIOS lock support
    As per SDSi in-band interface specification, sec titled "BIOS lock for
    in-band provisioning", when IB_LOCK bit is set in control qword, the
    SDSI agent is only allowed to perform the read flow, but not allowed to
    provision license blob or license key. So add check for it in
    sdsi_provision().
    
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
    Signed-off-by: David E. Box <david.e.box@linux.intel.com>

--- drivers/platform/x86/intel/sdsi.c
+++ drivers/platform/x86/intel/sdsi.c
@@ -61,6 +61,7 @@
 #define CTRL_OWNER			GENMASK(5, 4)
 #define CTRL_COMPLETE			BIT(6)
 #define CTRL_READY			BIT(7)
+#define CTRL_INBAND_LOCK		BIT(32)
 #define CTRL_STATUS			GENMASK(15, 8)
 #define CTRL_PACKET_SIZE		GENMASK(31, 16)
 #define CTRL_MSG_SIZE			GENMASK(63, 48)
@@ -331,11 +332,20 @@ static int sdsi_mbox_read(struct sdsi_priv *priv, struct sdsi_mbox_info *info, s
 	return sdsi_mbox_cmd_read(priv, info, data_size);
 }
 
+static bool sdsi_ib_locked(struct sdsi_priv *priv)
+{
+	return !!FIELD_GET(CTRL_INBAND_LOCK, readq(priv->control_addr));
+}
+
 static ssize_t sdsi_provision(struct sdsi_priv *priv, char *buf, size_t count,
 			      enum sdsi_command command)
 {
 	struct sdsi_mbox_info info = {};
 	int ret;
+
+	/* Make sure In-band lock is not set */
+	if (sdsi_ib_locked(priv))
+		return -EPERM;
 
 	if (count > (SDSI_SIZE_WRITE_MSG - SDSI_SIZE_CMD))
 		return -EOVERFLOW;
