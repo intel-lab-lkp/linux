c52118f8220a1ae8f5302f8baa16b05c0631146b
Jani Nikula <jani.nikula@intel.com>
2024-02-02 16:05:31 +0200

    drm/mst: read sideband messaging cap
    Amend drm_dp_read_mst_cap() to return an enum, indicating "none",
    "sideband messaging", or "mst". Modify all call sites to take the new
    return value into account.
    
    Cc: Arun R Murthy <arun.r.murthy@intel.com>
    Cc: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/display/drm_dp_mst_topology.c
+++ drivers/gpu/drm/display/drm_dp_mst_topology.c
@@ -3603,24 +3603,30 @@ fixed20_12 drm_dp_get_vc_payload_bw(const struct drm_dp_mst_topology_mgr *mgr,
 EXPORT_SYMBOL(drm_dp_get_vc_payload_bw);
 
 /**
- * drm_dp_read_mst_cap() - check whether or not a sink supports MST
+ * drm_dp_read_mst_cap() - Read the sink's MST mode capability
  * @aux: The DP AUX channel to use
  * @dpcd: A cached copy of the DPCD capabilities for this sink
  *
- * Returns: %True if the sink supports MST, %false otherwise
+ * Returns: enum drm_dp_mst_mode to indicate MST mode capability
  */
-bool drm_dp_read_mst_cap(struct drm_dp_aux *aux,
-			 const u8 dpcd[DP_RECEIVER_CAP_SIZE])
+enum drm_dp_mst_mode drm_dp_read_mst_cap(struct drm_dp_aux *aux,
+					 const u8 dpcd[DP_RECEIVER_CAP_SIZE])
 {
 	u8 mstm_cap;
 
 	if (dpcd[DP_DPCD_REV] < DP_DPCD_REV_12)
-		return false;
+		return DP_MST_NONE;
 
 	if (drm_dp_dpcd_readb(aux, DP_MSTM_CAP, &mstm_cap) != 1)
-		return false;
+		return DP_MST_NONE;
 
-	return mstm_cap & DP_MST_CAP;
+	if (mstm_cap & DP_MST_CAP)
+		return DP_MST_CAPABLE;
+
+	if (mstm_cap & DP_SINGLE_STREAM_SIDEBAND_MSG)
+		return DP_MST_SIDEBAND_MSG;
+
+	return DP_MST_NONE;
 }
 EXPORT_SYMBOL(drm_dp_read_mst_cap);
 
