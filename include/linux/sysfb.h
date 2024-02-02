ed7093fe029b74d95b8ead552961348bdc6e799d
Thomas Zimmermann <tzimmermann@suse.de>
2024-02-02 12:58:44 +0100

    firmware/sysfb: Set firmware-framebuffer parent device
    Set the firmware framebuffer's parent device, which usually is the
    graphics hardware's physical device. Integrates the framebuffer in
    the Linux device hierarchy and lets Linux handle dependencies among
    devices. For example, the graphics hardware won't be suspended while
    the firmware device is still active.
    
    v2:
    	* detect parent device in sysfb_parent_dev()
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Reviewed-by: Javier Martinez Canillas <javierm@redhat.com>

--- include/linux/sysfb.h
+++ include/linux/sysfb.h
@@ -91,7 +91,8 @@ static inline void sysfb_set_efifb_fwnode(struct platform_device *pd)
 bool sysfb_parse_mode(const struct screen_info *si,
 		      struct simplefb_platform_data *mode);
 struct platform_device *sysfb_create_simplefb(const struct screen_info *si,
-					      const struct simplefb_platform_data *mode);
+					      const struct simplefb_platform_data *mode,
+					      struct device *parent);
 
 #else /* CONFIG_SYSFB_SIMPLE */
 
