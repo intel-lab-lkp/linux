6179490b672abc99f4261839142c9ee4d19da6be
Andrew Lunn <andrew@lunn.ch>
2024-02-03 14:52:49 -0600

    net: dsa: mv88e6xxx: Return -ENODEV when C22/C45 not supported
    MDIO bus drivers can return -ENODEV when they know the bus does not
    have a device at the given address, e.g. because of hardware
    limitation. One such limitation is that the bus does not support C22
    or C45 at all. This is more efficient than returning 0xffff, since it
    immediately stops the probing on the given address, where as further
    reads can be made when 0xffff is returned.
    
    Signed-off-by: Andrew Lunn <andrew@lunn.ch>

--- drivers/net/dsa/mv88e6xxx/chip.c
+++ drivers/net/dsa/mv88e6xxx/chip.c
@@ -3633,7 +3633,7 @@ static int mv88e6xxx_mdio_read(struct mii_bus *bus, int phy, int reg)
 	int err;
 
 	if (!chip->info->ops->phy_read)
-		return -EOPNOTSUPP;
+		return -ENODEV;
 
 	mv88e6xxx_reg_lock(chip);
 	err = chip->info->ops->phy_read(chip, bus, phy, reg, &val);
@@ -3659,7 +3659,7 @@ static int mv88e6xxx_mdio_read_c45(struct mii_bus *bus, int phy, int devad,
 	int err;
 
 	if (!chip->info->ops->phy_read_c45)
-		return 0xffff;
+		return -ENODEV;
 
 	mv88e6xxx_reg_lock(chip);
 	err = chip->info->ops->phy_read_c45(chip, bus, phy, devad, reg, &val);
