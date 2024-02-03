a3fc661c030503f5cb5946ccdea27f3951dd994a
Kuniyuki Iwashima <kuniyu@amazon.com>
2024-02-02 19:00:57 -0800

    af_unix: Remove lock dance in unix_peek_fds().
    In the previous GC implementation, the shape of the inflight graph
    was not expected to change while GC was in progress.
    
    MSG_PEEK was tricky because it could install inflight fd silently
    and transform the graph.
    
    Let's say we peeked a fd, which was a listening socket, and accept()ed
    some embryo sockets from it.  The garbage collection algorithm would
    have been confused as the set of sockets visited in scan_inflight()
    were different within the same GC invocation.
    
    That's why we placed spin_lock(&unix_gc_lock) and spin_unlock() in
    unix_peek_fds() with a fat comment.
    
    In a new implementation, we no longer garbage-collect the socket if
    it exists in another queue, that is, if it has a bridge to another
    SCC.  Also, accept() will require the lock if it has edges.
    
    Thus, we need not do the complicated lock dance.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- net/unix/garbage.c
+++ net/unix/garbage.c
@@ -127,7 +127,7 @@ static void unix_graph_update(struct unix_edge *edge)
 	unix_graph_maybe_cyclic = true;
 }
 
-DEFINE_SPINLOCK(unix_gc_lock);
+static DEFINE_SPINLOCK(unix_gc_lock);
 static LIST_HEAD(unix_unvisited_vertices);
 unsigned int unix_tot_inflight;
 
