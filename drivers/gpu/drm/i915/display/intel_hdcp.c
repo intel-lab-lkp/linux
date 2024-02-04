e3df10f71c1c63208da3d4c5f6b15d53c976faf6
Suraj Kandpal <suraj.kandpal@intel.com>
2024-02-04 13:09:30 +0530

    drm/i915/hdcp: Add new remote capability check shim function
    Create a remote HDCP capability shim function which can read the
    remote monitor HDCP capability when in MST configuration.
    
    --v2
    -Add an assertion to make sure only mst encoder call this remote_cap
    function [Ankit]
    
    Signed-off-by: Suraj Kandpal <suraj.kandpal@intel.com>

--- drivers/gpu/drm/i915/display/intel_hdcp.c
+++ drivers/gpu/drm/i915/display/intel_hdcp.c
@@ -205,6 +205,22 @@ bool intel_hdcp2_capable(struct intel_connector *connector)
 	return capable;
 }
 
+void intel_hdcp_remote_cap(struct intel_connector *connector,
+			   bool *hdcp_capable,
+			   bool *hdcp2_capable)
+{
+	struct intel_hdcp *hdcp = &connector->hdcp;
+
+	/* Remote Sink's capability for HDCP */
+	if (!hdcp->shim->remote_hdcp_cap)
+		return;
+
+	hdcp->shim->remote_hdcp_cap(connector, hdcp_capable, hdcp2_capable);
+
+	if (intel_hdcp2_prerequisite(connector))
+		*hdcp2_capable = false;
+}
+
 static bool intel_hdcp_in_use(struct drm_i915_private *i915,
 			      enum transcoder cpu_transcoder, enum port port)
 {
