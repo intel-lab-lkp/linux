ba9abec5bb867244a203cd48ae26e00b2b40f4d0
Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
2024-02-05 10:33:57 +0100

    gpio: of: assign and read the hog pointer atomically
    The device nodes representing GPIO hogs cannot be deleted without
    unregistering the GPIO chip so there's no need to serialize their access.
    However we must ensure that users can get the right address so write and
    read it atomically.
    
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
    Reviewed-by: Linus Walleij <linus.walleij@linaro.org>

--- drivers/gpio/gpiolib.c
+++ drivers/gpio/gpiolib.c
@@ -2324,7 +2324,7 @@ static bool gpiod_free_commit(struct gpio_desc *desc)
 		clear_bit(FLAG_EDGE_FALLING, &desc->flags);
 		clear_bit(FLAG_IS_HOGGED, &desc->flags);
 #ifdef CONFIG_OF_DYNAMIC
-		desc->hog = NULL;
+		WRITE_ONCE(desc->hog, NULL);
 #endif
 		ret = true;
 	}
