13b7d779df5f72af717163a0a616db21df89622f
Eric Dumazet <edumazet@google.com>
2024-02-09 20:34:21 +0000

    net-sysfs: use dev_addr_sem to remove races in address_show()
    Using dev_base_lock is not preventing from reading garbage.
    
    Use dev_addr_sem instead.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -4031,6 +4031,7 @@ int dev_set_mac_address(struct net_device *dev, struct sockaddr *sa,
 			struct netlink_ext_ack *extack);
 int dev_set_mac_address_user(struct net_device *dev, struct sockaddr *sa,
 			     struct netlink_ext_ack *extack);
+extern struct rw_semaphore dev_addr_sem;
 int dev_get_mac_address(struct sockaddr *sa, struct net *net, char *dev_name);
 int dev_get_port_parent_id(struct net_device *dev,
 			   struct netdev_phys_item_id *ppid, bool recurse);
