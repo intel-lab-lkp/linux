aab9b4037b7a2e1f21d8003cb0f6ab7160dedcc3
Isaku Yamahata <isaku.yamahata@intel.com>
2024-02-09 13:37:39 -0500

    KVM: x86: Add is_vm_type_supported callback
    Allow the backend to specify which VM types are supported.
    
    Based on a patch by Isaku Yamahata <isaku.yamahata@intel.com>.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -4581,11 +4581,18 @@ static int kvm_ioctl_get_supported_hv_cpuid(struct kvm_vcpu *vcpu,
 }
 #endif
 
-static bool kvm_is_vm_type_supported(unsigned long type)
+bool __kvm_is_vm_type_supported(unsigned long type)
 {
 	return type == KVM_X86_DEFAULT_VM ||
 	       (type == KVM_X86_SW_PROTECTED_VM &&
 		IS_ENABLED(CONFIG_KVM_SW_PROTECTED_VM) && tdp_enabled);
+}
+EXPORT_SYMBOL_GPL(__kvm_is_vm_type_supported);
+
+static bool kvm_is_vm_type_supported(unsigned long type)
+{
+	return __kvm_is_vm_type_supported(type) ||
+	       static_call(kvm_x86_is_vm_type_supported)(type);
 }
 
 int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
