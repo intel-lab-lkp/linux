2406a9b736212eb9cb50711bafe11968ee425985
Max Kellermann <max.kellermann@ionos.com>
2024-02-11 13:29:36 +0100

    percpu_counter.h: move declarations to percpu_counter_types.h
    By providing declarations in a lean header, we can reduce header
    dependencies.
    
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- include/linux/flex_proportions.h
+++ include/linux/flex_proportions.h
@@ -8,7 +8,7 @@
 #ifndef _LINUX_FLEX_PROPORTIONS_H
 #define _LINUX_FLEX_PROPORTIONS_H
 
-#include <linux/percpu_counter.h>
+#include <linux/percpu_counter_types.h>
 #include <linux/spinlock_types.h>
 #include <linux/seqlock_types.h>
 #include <linux/gfp_types.h>
