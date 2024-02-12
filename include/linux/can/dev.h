9c658f966d0061473468a452dd1cdbbfbcbf32e6
Max Kellermann <max.kellermann@ionos.com>
2024-02-12 12:54:29 +0100

    workqueue.h: move struct delayed_work to workqueue_types.h
    This allows including workqueue_types.h instead of workqueue.h in some
    headers.
    
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- include/linux/can/dev.h
+++ include/linux/can/dev.h
@@ -22,7 +22,7 @@
 #include <linux/can/skb.h>
 #include <linux/ethtool.h>
 #include <linux/netdevice.h>
-#include <linux/workqueue.h> // for struct delayed_work
+#include <linux/workqueue_types.h> // for struct delayed_work
 
 /*
  * CAN mode
