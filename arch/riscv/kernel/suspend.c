b777fe3ac943cdd3e67feee2775e2f2c13c676bc
Samuel Holland <samuel.holland@sifive.com>
2024-02-12 19:37:35 -0800

    riscv: Save/restore envcfg CSR during CPU suspend
    The value of the [ms]envcfg CSR is lost when entering a nonretentive
    idle state, so the CSR must be rewritten when resuming the CPU.
    
    Cc: <stable@vger.kernel.org> # v6.7+
    Fixes: 43c16d51a19b ("RISC-V: Enable cbo.zero in usermode")
    Signed-off-by: Samuel Holland <samuel.holland@sifive.com>

--- arch/riscv/kernel/suspend.c
+++ arch/riscv/kernel/suspend.c
@@ -15,6 +15,8 @@
 void suspend_save_csrs(struct suspend_context *context)
 {
 	context->scratch = csr_read(CSR_SCRATCH);
+	if (riscv_has_extension_likely(RISCV_ISA_EXT_Sx1p12))
+		context->envcfg = csr_read(CSR_ENVCFG);
 	context->tvec = csr_read(CSR_TVEC);
 	context->ie = csr_read(CSR_IE);
 
@@ -36,6 +38,8 @@ void suspend_save_csrs(struct suspend_context *context)
 void suspend_restore_csrs(struct suspend_context *context)
 {
 	csr_write(CSR_SCRATCH, context->scratch);
+	if (riscv_has_extension_likely(RISCV_ISA_EXT_Sx1p12))
+		csr_write(CSR_ENVCFG, context->envcfg);
 	csr_write(CSR_TVEC, context->tvec);
 	csr_write(CSR_IE, context->ie);
 
