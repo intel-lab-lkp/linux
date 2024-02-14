c3b3b5bc4f31f0e1fa9625f277ee9ebcf23844f5
Alison Schofield <alison.schofield@intel.com>
2024-02-13 23:13:36 -0800

    cxl/test: Replace an illegal CFMWS definition with a useful x3 CFMWS
    cxl/test has a small set (3) of CFMWS's that become available when
    cxl_test is loaded with this module option: interleave_arithmetic=1.
    That gives users of cxl_test access to root decoders that use XOR Math
    for interleave arithmetic rather than the default Modulo.
    
    The existing x4 CFMWS definition is an invalid configuration because
    it repeats Host Bridges in the interleave set. A patch to remove its
    usage in the CXL unit test cxl-xor-region.sh is in flight.
    
    Coincidentally, there is another CFMWS definition queued for addition
    here and a patch to use it in the CXL unit test is also in flight.
    Rather than tear this CFMWS out and adjust the indexes only to follow
    it with another patch to introduce a new CFMWS, do a switch in place.
    
    Transform the illegal x4 CFMWS definition into a useful x3 version.
    
    Fixes: 7a7e6edfca85 ("tools/testing/cxl: Add XOR Math support to cxl_test")
    Signed-off-by: Alison Schofield <alison.schofield@intel.com>

--- tools/testing/cxl/test/cxl.c
+++ tools/testing/cxl/test/cxl.c
@@ -329,14 +329,14 @@ static struct {
 				.length = sizeof(mock_cedt.cfmws8),
 			},
 			.interleave_arithmetic = ACPI_CEDT_CFMWS_ARITHMETIC_XOR,
-			.interleave_ways = 2,
+			.interleave_ways = 8,
 			.granularity = 0,
 			.restrictions = ACPI_CEDT_CFMWS_RESTRICT_TYPE3 |
 					ACPI_CEDT_CFMWS_RESTRICT_PMEM,
 			.qtg_id = 0,
-			.window_size = SZ_256M * 16UL,
+			.window_size = SZ_256M * 12UL,
 		},
-		.target = { 0, 1, 0, 1, },
+		.target = { 0, 1, 2 },
 	},
 	.cxims0 = {
 		.cxims = {
