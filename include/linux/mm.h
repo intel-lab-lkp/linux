8e81209e4b033aeb964331704d550e8273d1a9bc
Max Kellermann <max.kellermann@ionos.com>
2024-02-15 15:56:00 +0100

    linux/mm.h: move is_vmalloc_addr() to mm/vmalloc_addr.h
    Prepare to reduce dependencies in linux/mm.h.
    
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -9,6 +9,7 @@
 #include <linux/mm/page_usage.h>
 #include <linux/mm/page_zone.h>
 #include <linux/mm/pfmemalloc.h>
+#include <linux/mm/vmalloc_addr.h>
 #include <linux/errno.h>
 #include <linux/mmdebug.h>
 #include <linux/gfp.h>
@@ -1066,30 +1067,6 @@ enum {
 
 int region_intersects(resource_size_t offset, size_t size, unsigned long flags,
 		      unsigned long desc);
-
-/* Support for virtually mapped pages */
-struct page *vmalloc_to_page(const void *addr);
-unsigned long vmalloc_to_pfn(const void *addr);
-
-/*
- * Determine if an address is within the vmalloc range
- *
- * On nommu, vmalloc/vfree wrap through kmalloc/kfree directly, so there
- * is no special casing required.
- */
-#ifdef CONFIG_MMU
-extern bool is_vmalloc_addr(const void *x);
-extern int is_vmalloc_or_module_addr(const void *x);
-#else
-static inline bool is_vmalloc_addr(const void *x)
-{
-	return false;
-}
-static inline int is_vmalloc_or_module_addr(const void *x)
-{
-	return 0;
-}
-#endif
 
 /*
  * How many times the entire folio is mapped as a single unit (eg by a
