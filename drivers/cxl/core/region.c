ab05e9f94ef0a903b1223cc3886067432f1b5153
Fabio M. De Francesco <fabio.maria.de.francesco@linux.intel.com>
2024-02-17 11:59:03 +0100

    cxl/region: Use cond_guard() in show_targetN()
    Use cond_guard() in show_target() to not open code an up_read() in an 'out'
    block. If the down_read_interruptible() fails, the statement passed to the
    second argument of cond_guard() returns -EINTR.
    
    Cc: Peter Zijlstra <peterz@infradead.org>
    Suggested-by: Dan Williams <dan.j.williams@intel.com>
    Suggested-by: Ira Weiny <ira.weiny@intel.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Ira Weiny <ira.weiny@intel.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Signed-off-by: Fabio M. De Francesco <fabio.maria.de.francesco@linux.intel.com>

--- drivers/cxl/core/region.c
+++ drivers/cxl/core/region.c
@@ -666,28 +666,20 @@ static size_t show_targetN(struct cxl_region *cxlr, char *buf, int pos)
 {
 	struct cxl_region_params *p = &cxlr->params;
 	struct cxl_endpoint_decoder *cxled;
-	int rc;
 
-	rc = down_read_interruptible(&cxl_region_rwsem);
-	if (rc)
-		return rc;
+	cond_guard(rwsem_read_intr, return -EINTR, &cxl_region_rwsem);
 
 	if (pos >= p->interleave_ways) {
 		dev_dbg(&cxlr->dev, "position %d out of range %d\n", pos,
 			p->interleave_ways);
-		rc = -ENXIO;
-		goto out;
+		return -ENXIO;
 	}
 
 	cxled = p->targets[pos];
 	if (!cxled)
-		rc = sysfs_emit(buf, "\n");
-	else
-		rc = sysfs_emit(buf, "%s\n", dev_name(&cxled->cxld.dev));
-out:
-	up_read(&cxl_region_rwsem);
+		return sysfs_emit(buf, "\n");
 
-	return rc;
+	return sysfs_emit(buf, "%s\n", dev_name(&cxled->cxld.dev));
 }
 
 static int match_free_decoder(struct device *dev, void *data)
