b3acd0ac404257dfd5fcfeea505e0a919a454354
Richard Gobert <richardbgobert@gmail.com>
2024-02-22 21:51:09 +0100

    net: vxlan: enable local address bind for vxlan sockets
    This patch adds support for binding to a local address in vxlan sockets.
    It achieves this by using vxlan_addr union to represent a local address
    to bind to, and copying it to udp_port_cfg in vxlan_create_sock.
    
    Signed-off-by: Richard Gobert <richardbgobert@gmail.com>

--- drivers/net/vxlan/vxlan_core.c
+++ drivers/net/vxlan/vxlan_core.c
@@ -3480,8 +3480,9 @@ static const struct ethtool_ops vxlan_ethtool_ops = {
 	.get_link_ksettings	= vxlan_get_link_ksettings,
 };
 
-static struct socket *vxlan_create_sock(struct net *net, bool ipv6,
-					__be16 port, u32 flags, int ifindex)
+static struct socket *vxlan_create_sock(struct net *net, bool ipv6, __be16 port,
+					u32 flags, int ifindex,
+					union vxlan_addr addr)
 {
 	struct socket *sock;
 	struct udp_port_cfg udp_conf;
@@ -3494,8 +3495,15 @@ static struct socket *vxlan_create_sock(struct net *net, bool ipv6,
 		udp_conf.use_udp6_rx_checksums =
 		    !(flags & VXLAN_F_UDP_ZERO_CSUM6_RX);
 		udp_conf.ipv6_v6only = 1;
+		memcpy(&udp_conf.local_ip6.s6_addr32,
+		       &addr.sin6.sin6_addr.s6_addr32,
+		       sizeof(addr.sin6.sin6_addr.s6_addr32));
 	} else {
 		udp_conf.family = AF_INET;
+		udp_conf.local_ip.s_addr = addr.sin.sin_addr.s_addr;
+		memcpy(&udp_conf.local_ip.s_addr,
+		       &addr.sin.sin_addr.s_addr,
+		       sizeof(addr.sin.sin_addr.s_addr));
 	}
 
 	udp_conf.local_udp_port = port;
@@ -3513,7 +3521,8 @@ static struct socket *vxlan_create_sock(struct net *net, bool ipv6,
 /* Create new listen socket if needed */
 static struct vxlan_sock *vxlan_socket_create(struct net *net, bool ipv6,
 					      __be16 port, u32 flags,
-					      int ifindex)
+					      int ifindex,
+					      union vxlan_addr addr)
 {
 	struct vxlan_net *vn = net_generic(net, vxlan_net_id);
 	struct vxlan_sock *vs;
@@ -3528,7 +3537,7 @@ static struct vxlan_sock *vxlan_socket_create(struct net *net, bool ipv6,
 	for (h = 0; h < VNI_HASH_SIZE; ++h)
 		INIT_HLIST_HEAD(&vs->vni_list[h]);
 
-	sock = vxlan_create_sock(net, ipv6, port, flags, ifindex);
+	sock = vxlan_create_sock(net, ipv6, port, flags, ifindex, addr);
 	if (IS_ERR(sock)) {
 		kfree(vs);
 		return ERR_CAST(sock);
@@ -3592,7 +3601,7 @@ static int __vxlan_sock_add(struct vxlan_dev *vxlan, bool ipv6)
 	if (!vs)
 		vs = vxlan_socket_create(vxlan->net, ipv6,
 					 vxlan->cfg.dst_port, vxlan->cfg.flags,
-					 l3mdev_index);
+					 l3mdev_index, vxlan->cfg.saddr);
 	if (IS_ERR(vs))
 		return PTR_ERR(vs);
 #if IS_ENABLED(CONFIG_IPV6)
