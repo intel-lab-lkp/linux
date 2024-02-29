2c1804182f303f0938097f281e7139216bd6015c
Eric Dumazet <edumazet@google.com>
2024-02-29 09:39:07 +0000

    net: adopt skb_network_offset() and similar helpers
    This is a cleanup patch, making code a bit more concise.
    
    1) Use skb_network_offset(skb) in place of
           (skb_network_header(skb) - skb->data)
    
    2) Use -skb_network_offset(skb) in place of
           (skb->data - skb_network_header(skb))
    
    3) Use skb_transport_offset(skb) in place of
           (skb_transport_header(skb) - skb->data)
    
    4) Use skb_inner_transport_offset(skb) in place of
           (skb_inner_transport_header(skb) - skb->data)
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>

--- net/ipv6/exthdrs.c
+++ net/ipv6/exthdrs.c
@@ -802,7 +802,7 @@ looped_back:
 
 	ip6_route_input(skb);
 	if (skb_dst(skb)->error) {
-		skb_push(skb, skb->data - skb_network_header(skb));
+		skb_push(skb, -skb_network_offset(skb));
 		dst_input(skb);
 		return -1;
 	}
@@ -819,7 +819,7 @@ looped_back:
 		goto looped_back;
 	}
 
-	skb_push(skb, skb->data - skb_network_header(skb));
+	skb_push(skb, -skb_network_offset(skb));
 	dst_input(skb);
 	return -1;
 
