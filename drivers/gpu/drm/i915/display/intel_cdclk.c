cb8758a43c27b7bba04bdab43ec2331eea0e9a2e
Gustavo Sousa <gustavo.sousa@intel.com>
2024-03-04 15:30:24 -0300

    drm/i915: Add mdclk_cdclk_ratio to intel_dbuf_state
    CDCLK programming Xe2LPD always selects the CDCLK PLL as source for the
    MDCLK. Because of that, the ratio between MDCLK and CDCLK is not be
    constant anymore. As such, make sure to have the current ratio available
    in intel_dbuf_state so that it can be used during dbuf programming.
    
    Note that we write-lock the global state instead of serializing to a
    hardware commit because a change in the ratio should be rather handled
    in the CDCLK change sequence, which will need to take care of updating
    the necessary registers in that case. We will implement that in upcoming
    changes.
    
    That said, changes in the MBus joining state should be handled by the
    DBUF/MBUS logic, just like it is already done, but the logic will need
    to know the ratio to properly update the registers.
    
    Signed-off-by: Gustavo Sousa <gustavo.sousa@intel.com>

--- drivers/gpu/drm/i915/display/intel_cdclk.c
+++ drivers/gpu/drm/i915/display/intel_cdclk.c
@@ -39,6 +39,7 @@
 #include "intel_pcode.h"
 #include "intel_psr.h"
 #include "intel_vdsc.h"
+#include "skl_watermark.h"
 #include "vlv_sideband.h"
 
 /**
@@ -1891,6 +1892,22 @@ static u32 xe2lpd_mdclk_source_sel(struct drm_i915_private *i915)
 	return MDCLK_SOURCE_SEL_CD2XCLK;
 }
 
+u8 intel_mdclk_cdclk_ratio(struct drm_i915_private *i915,
+			   const struct intel_cdclk_config *cdclk_config)
+{
+	u32 source_sel = xe2lpd_mdclk_source_sel(i915);
+
+	switch (source_sel) {
+	case MDCLK_SOURCE_SEL_CD2XCLK:
+		return 2;
+	case MDCLK_SOURCE_SEL_CDCLK_PLL:
+		return DIV_ROUND_UP(cdclk_config->vco, cdclk_config->cdclk);
+	default:
+		MISSING_CASE(source_sel);
+		return 2;
+	}
+}
+
 static bool cdclk_compute_crawl_and_squash_midpoint(struct drm_i915_private *i915,
 						    const struct intel_cdclk_config *old_cdclk_config,
 						    const struct intel_cdclk_config *new_cdclk_config,
@@ -3279,6 +3296,15 @@ int intel_modeset_calc_cdclk(struct intel_atomic_state *state)
 
 		drm_dbg_kms(&dev_priv->drm,
 			    "Modeset required for cdclk change\n");
+	}
+
+	if (intel_mdclk_cdclk_ratio(dev_priv, &old_cdclk_state->actual) !=
+	    intel_mdclk_cdclk_ratio(dev_priv, &new_cdclk_state->actual)) {
+		u8 ratio = intel_mdclk_cdclk_ratio(dev_priv, &new_cdclk_state->actual);
+
+		ret = intel_dbuf_state_set_mdclk_cdclk_ratio(state, ratio);
+		if (ret)
+			return ret;
 	}
 
 	drm_dbg_kms(&dev_priv->drm,
