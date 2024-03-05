f5a9a789c7e12130786f86804268aaf88a501561
Martin Tůma <martin.tuma@digiteqautomotive.com>
2024-03-05 14:54:10 +0100

    media: mgb4: Add support for V4L2_CAP_TIMEPERFRAME
    Recent mgb4 firmwares have support for setting a variable framerate independent
    of the signal framerate. Add/fix (the mgb4 driver already did promote
    V4L2_CAP_TIMEPERFRAME, but it didn't work) support for V4L2_CAP_TIMEPERFRAME to
    the driver to enable this feature.
    
    Signed-off-by: Martin Tůma <martin.tuma@digiteqautomotive.com>

--- drivers/media/pci/mgb4/mgb4_sysfs_out.c
+++ drivers/media/pci/mgb4/mgb4_sysfs_out.c
@@ -229,9 +229,12 @@ static ssize_t frame_rate_show(struct device *dev,
 	struct video_device *vdev = to_video_device(dev);
 	struct mgb4_vout_dev *voutdev = video_get_drvdata(vdev);
 	u32 period = mgb4_read_reg(&voutdev->mgbdev->video,
-				   voutdev->config->regs.frame_period);
+				   voutdev->config->regs.frame_limit);
 
-	return sprintf(buf, "%u\n", 125000000 / period);
+	if (!period)
+		return -EIO;
+
+	return sprintf(buf, "%u\n", MGB4_HW_FREQ / period);
 }
 
 /*
@@ -252,7 +255,7 @@ static ssize_t frame_rate_store(struct device *dev,
 		return ret;
 
 	mgb4_write_reg(&voutdev->mgbdev->video,
-		       voutdev->config->regs.frame_period, 125000000 / val);
+		       voutdev->config->regs.frame_limit, MGB4_HW_FREQ / val);
 
 	return count;
 }
