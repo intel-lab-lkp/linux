d4b507179f20f3c5b582b3a634e8de5ad52b0bfb
Eric Dumazet <edumazet@google.com>
2024-03-06 16:00:21 +0000

    net: move dev_tx_weight to net_hotdata
    dev_tx_weight is used in tx fast path.
    
    Move it to net_hotdata for better cache locality.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Acked-by: Soheil Hassas Yeganeh <soheil@google.com>
    Reviewed-by: David Ahern <dsahern@kernel.org>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -4788,7 +4788,6 @@ void dev_fetch_sw_netstats(struct rtnl_link_stats64 *s,
 void dev_get_tstats64(struct net_device *dev, struct rtnl_link_stats64 *s);
 
 extern int		dev_rx_weight;
-extern int		dev_tx_weight;
 
 enum {
 	NESTED_SYNC_IMM_BIT,
