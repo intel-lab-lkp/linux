d20d5fadd5f9dad69c30aa8143649bd499ddc6da
Athira Rajeev <atrajeev@linux.vnet.ibm.com>
2024-03-09 11:21:35 +0530

    tools/erf/util/annotate: Set register_char and memory_ref_char for powerpc
    To identify if the instruction has any memory reference,
    "memory_ref_char" field needs to be set for specific architecture.
    Example memory instruction:
    lwz     r10,0(r9)
    
    Here "(" is the memory_ref_char. Set this as part of arch->objdump
    
    To get register number and access offset from the given instruction,
    arch->objdump.register_char is used. In case of powerpc, the register
    char is "r" (since reg names are r0 to r31). Hence set register_char
    as "r".
    
    Signed-off-by: Athira Rajeev <atrajeev@linux.vnet.ibm.com>

--- tools/perf/util/annotate.c
+++ tools/perf/util/annotate.c
@@ -216,6 +216,11 @@ static struct arch architectures[] = {
 	{
 		.name = "powerpc",
 		.init = powerpc__annotate_init,
+		.objdump =  {
+			.comment_char = '#',
+			.register_char = 'r',
+			.memory_ref_char = '(',
+		},
 	},
 	{
 		.name = "riscv64",
