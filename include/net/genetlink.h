3a601f6e1e273d7ebdfd191df59a97522a7a240e
Jakub Kicinski <kuba@kernel.org>
2024-03-09 10:34:58 -0800

    genetlink: remove linux/genetlink.h
    genetlink.h is a shell of what used to be a combined uAPI
    and kernel header over a decade ago. It has fewer than
    10 lines of code. Merge it into net/genetlink.h.
    In some ways it'd be better to keep the combined header
    under linux/ but it would make looking through git history
    harder.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- include/net/genetlink.h
+++ include/net/genetlink.h
@@ -2,11 +2,18 @@
 #ifndef __NET_GENERIC_NETLINK_H
 #define __NET_GENERIC_NETLINK_H
 
-#include <linux/genetlink.h>
+#include <uapi/linux/genetlink.h>
 #include <net/netlink.h>
 #include <net/net_namespace.h>
 
 #define GENLMSG_DEFAULT_SIZE (NLMSG_DEFAULT_SIZE - GENL_HDRLEN)
+
+/* All generic netlink requests are serialized by a global lock.  */
+extern void genl_lock(void);
+extern void genl_unlock(void);
+
+#define MODULE_ALIAS_GENL_FAMILY(family) \
+ MODULE_ALIAS_NET_PF_PROTO_NAME(PF_NETLINK, NETLINK_GENERIC, "-family-" family)
 
 /* Binding to multicast group requires %CAP_NET_ADMIN */
 #define GENL_MCAST_CAP_NET_ADMIN	BIT(0)
