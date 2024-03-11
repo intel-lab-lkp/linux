6149e3ff1b3a9435d2ff241b09f8adc4224dfbd5
Sui Jingfeng <sui.jingfeng@linux.dev>
2024-03-11 16:56:55 +0800

    drm/bridge: Allow using fwnode API to get the next bridge
    Currently, the various drm bridge drivers relay on OF infrastructures to
    works very well. Yet there are platforms and/or devices absence of OF
    support. Such as virtual display drivers, USB display apapters and ACPI
    based systems etc.
    
    Add fwnode based helpers to fill the niche, this allows part of the drm
    display bridge drivers to work across systems. As the fwnode based API
    has wider coverage than DT and the fwnode graphs are compatible with
    the OF graph, so the provided helpers can be used on all systems in theory.
    Assumed that the system has valid fwnode graphs established before the
    drm bridge driver is probed, and there is a fwnode assigned to the instance
    of specific drm bridge driver.
    
    Signed-off-by: Sui Jingfeng <sui.jingfeng@linux.dev>

--- drivers/gpu/drm/drm_bridge.c
+++ drivers/gpu/drm/drm_bridge.c
@@ -1368,6 +1368,78 @@ struct drm_bridge *of_drm_find_bridge(struct device_node *np)
 EXPORT_SYMBOL(of_drm_find_bridge);
 #endif
 
+/**
+ * drm_bridge_find_by_fwnode - Find the bridge corresponding to the fwnode
+ *
+ * @fwnode: fwnode for which to find the matching drm_bridge
+ *
+ * This function looks up a drm_bridge in the global bridge list, based on
+ * its associated fwnode. Drivers who want to use this function should has
+ * fwnode handle assigned to the fwnode member of the struct drm_bridge
+ * instance.
+ *
+ * Return:
+ * A reference to the drm_bridge if found, return NULL otherwise.
+ */
+struct drm_bridge *drm_bridge_find_by_fwnode(struct fwnode_handle *fwnode)
+{
+	struct drm_bridge *ret = NULL;
+	struct drm_bridge *bridge;
+
+	if (!fwnode)
+		return NULL;
+
+	mutex_lock(&bridge_lock);
+
+	list_for_each_entry(bridge, &bridge_list, list) {
+		if (bridge->fwnode == fwnode) {
+			ret = bridge;
+			break;
+		}
+	}
+
+	mutex_unlock(&bridge_lock);
+
+	return ret;
+}
+EXPORT_SYMBOL_GPL(drm_bridge_find_by_fwnode);
+
+/**
+ * drm_bridge_find_next_bridge_by_fwnode - Find the next bridge by fwnode
+ * @fwnode: fwnode pointer to the current device.
+ * @port: identifier of the port node of the next bridge is connected.
+ *
+ * This function find the next bridge at the current node, it assumed that
+ * that there has valid fwnode graph established.
+ *
+ * Return:
+ * A reference to the drm_bridge if found.
+ * %NULL if not found or the next bridge is not finish probe yet.
+ * A negative error code otherwise.
+ */
+struct drm_bridge *
+drm_bridge_find_next_bridge_by_fwnode(struct fwnode_handle *fwnode, u32 port)
+{
+	struct drm_bridge *bridge;
+	struct fwnode_handle *ep;
+	struct fwnode_handle *remote;
+
+	ep = fwnode_graph_get_endpoint_by_id(fwnode, port, 0, 0);
+	if (!ep)
+		return ERR_PTR(-EINVAL);
+
+	remote = fwnode_graph_get_remote_port_parent(ep);
+	fwnode_handle_put(ep);
+	if (!remote)
+		return ERR_PTR(-ENODEV);
+
+	bridge = drm_bridge_find_by_fwnode(remote);
+	fwnode_handle_put(remote);
+
+	return bridge;
+}
+EXPORT_SYMBOL_GPL(drm_bridge_find_next_bridge_by_fwnode);
+
 MODULE_AUTHOR("Ajay Kumar <ajaykumar.rs@samsung.com>");
 MODULE_DESCRIPTION("DRM bridge infrastructure");
 MODULE_LICENSE("GPL and additional rights");
