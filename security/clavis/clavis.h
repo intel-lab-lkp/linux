6530265be8b34fb2fb177e27b532aeb0d1527be3
Eric Snowberg <eric.snowberg@oracle.com>
2024-03-11 12:11:11 -0400

    clavis: Introduce new LSM called clavis
    Introduce a new LSM called clavis.  The motivation behind this LSM is to
    provide access control for system keys.  The access control list is
    contained within a keyring call .clavis.  During boot if the clavis= boot
    arg is supplied with a key id contained within any of the current system
    keyrings (builtin, secondary, machine, or platform) it shall be used as
    the root of trust for validating anything that is added to the ACL list.
    
    The first restriction introduced with this LSM is the ability to enforce
    key usage.  The kernel already has a notion of tracking key usage.  This
    LSM adds the ability to enforce this usage based on the system owners
    configuration.
    
    Each system key may have one or more uses defined within the ACL list.
    When this LSM is enabled, only the builtin keys are available for loading
    kernel modules and doing a kexec.  Until an entry is added to the .clavis
    keyring, no other system key may be used for any other purpose.
    
    In the future it is envisioned this LSM could be enhanced to provide
    access control for UEFI Secure Boot Advanced Targeting (SBAT).  Using
    the same clavis= boot param and storing the additional contents within
    the new RT UEFI var, SBAT restrictions could be maintained across kexec.
    
    Signed-off-by: Eric Snowberg <eric.snowberg@oracle.com>

--- security/clavis/clavis.h
+++ security/clavis/clavis.h
@@ -1,5 +1,7 @@
 /* SPDX-License-Identifier: GPL-2.0 */
 
+struct key;
+struct public_key_signature;
 struct asymmetric_key_id;
 
 #ifdef CONFIG_EFI
@@ -10,3 +12,5 @@ static inline int __init clavis_efi_param(struct asymmetric_key_id *kid, int len
 	return -EINVAL;
 }
 #endif
+
+int clavis_sig_verify(const struct key *key, const struct public_key_signature *sig);
