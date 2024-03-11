02c496295cd2600639d70d241db52e5735b592f9
Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>
2024-03-11 15:12:37 +0530

    drm/i915/display: Compute vrr_vsync params
    Compute vrr_vsync_start/end, which sets the position
    for hardware to send the Vsync at a fixed position
    relative to the end of the Vblank.
    
    --v2:
    - Updated VSYNC_START/END macros to VRR_VSYNC_START/END. (Ankit)
    - Updated bit fields of VRR_VSYNC_START/END. (Ankit)
    
    --v3:
    - Add PIPE_CONF_CHECK_I(vrr.vsync_start/end).
    - Read/write vrr_vsync params only when we intend to send
    adaptive_sync sdp.
    
    --v4:
    - Use VRR_SYNC_START/END macros correctly.
    
    Signed-off-by: Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -5377,6 +5377,8 @@ intel_pipe_config_compare(const struct intel_crtc_state *current_config,
 		PIPE_CONF_CHECK_I(vrr.flipline);
 		PIPE_CONF_CHECK_I(vrr.pipeline_full);
 		PIPE_CONF_CHECK_I(vrr.guardband);
+		PIPE_CONF_CHECK_I(vrr.vsync_start);
+		PIPE_CONF_CHECK_I(vrr.vsync_end);
 	}
 
 #undef PIPE_CONF_CHECK_X
