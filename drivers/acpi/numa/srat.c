4a3e104c907e729fdb77312ceadcf3a5154aa73e
Robert Richter <rrichter@amd.com>
2024-03-18 22:09:03 +0100

    ACPI/NUMA: Remove architecture dependent remainings
    With the removal of the Itanium architecture [1] the last architecture
    dependent functions:
    
     acpi_numa_slit_init(), acpi_numa_memory_affinity_init()
    
    were removed. Remove its remainings in the header files too an make
    them static.
    
    [1] cf8e8658100d arch: Remove Itanium (IA-64) architecture
    
    Signed-off-by: Robert Richter <rrichter@amd.com>

--- drivers/acpi/numa/srat.c
+++ drivers/acpi/numa/srat.c
@@ -206,15 +206,20 @@ int __init srat_disabled(void)
 	return acpi_numa < 0;
 }
 
-#if defined(CONFIG_X86) || defined(CONFIG_ARM64) || defined(CONFIG_LOONGARCH)
 /*
  * Callback for SLIT parsing.  pxm_to_node() returns NUMA_NO_NODE for
  * I/O localities since SRAT does not list them.  I/O localities are
  * not supported at this point.
  */
-void __init acpi_numa_slit_init(struct acpi_table_slit *slit)
+static int __init acpi_parse_slit(struct acpi_table_header *table)
 {
+	struct acpi_table_slit *slit = (struct acpi_table_slit *)table;
 	int i, j;
+
+	if (!slit_valid(slit)) {
+		pr_info("SLIT table looks invalid. Not used.\n");
+		return -EINVAL;
+	}
 
 	for (i = 0; i < slit->locality_count; i++) {
 		const int from_node = pxm_to_node(i);
@@ -232,18 +237,24 @@ void __init acpi_numa_slit_init(struct acpi_table_slit *slit)
 				slit->entry[slit->locality_count * i + j]);
 		}
 	}
+
+	return 0;
 }
 
-/*
- * Default callback for parsing of the Proximity Domain <-> Memory
- * Area mappings
- */
-int __init
-acpi_numa_memory_affinity_init(struct acpi_srat_mem_affinity *ma)
+static int __initdata parsed_numa_memblks;
+
+static int __init
+acpi_parse_memory_affinity(union acpi_subtable_headers * header,
+			   const unsigned long table_end)
 {
+	struct acpi_srat_mem_affinity *ma;
 	u64 start, end;
 	u32 hotpluggable;
 	int node, pxm;
+
+	ma = (struct acpi_srat_mem_affinity *)header;
+
+	acpi_table_print_srat_entry(&header->common);
 
 	if (srat_disabled())
 		goto out_err;
@@ -290,6 +301,8 @@ acpi_numa_memory_affinity_init(struct acpi_srat_mem_affinity *ma)
 			(unsigned long long)start, (unsigned long long)end - 1);
 
 	max_possible_pfn = max(max_possible_pfn, PFN_UP(end - 1));
+
+	parsed_numa_memblks++;
 
 	return 0;
 out_err_bad_srat:
@@ -446,27 +459,6 @@ static int __init acpi_parse_cfmws(union acpi_subtable_headers *header,
 	(*fake_pxm)++;
 	return 0;
 }
-#else
-static inline void acpi_table_print_cedt(void) {}
-static int __init acpi_parse_cfmws(union acpi_subtable_headers *header,
-				   void *arg, const unsigned long table_end)
-{
-	return 0;
-}
-#endif /* defined(CONFIG_X86) || defined (CONFIG_ARM64) */
-
-static int __init acpi_parse_slit(struct acpi_table_header *table)
-{
-	struct acpi_table_slit *slit = (struct acpi_table_slit *)table;
-
-	if (!slit_valid(slit)) {
-		pr_info("SLIT table looks invalid. Not used.\n");
-		return -EINVAL;
-	}
-	acpi_numa_slit_init(slit);
-
-	return 0;
-}
 
 void __init __weak
 acpi_numa_x2apic_affinity_init(struct acpi_srat_x2apic_cpu_affinity *pa)
@@ -556,24 +548,6 @@ acpi_parse_gi_affinity(union acpi_subtable_headers *header,
 	return 0;
 }
 #endif /* defined(CONFIG_X86) || defined (CONFIG_ARM64) */
-
-static int __initdata parsed_numa_memblks;
-
-static int __init
-acpi_parse_memory_affinity(union acpi_subtable_headers * header,
-			   const unsigned long end)
-{
-	struct acpi_srat_mem_affinity *memory_affinity;
-
-	memory_affinity = (struct acpi_srat_mem_affinity *)header;
-
-	acpi_table_print_srat_entry(&header->common);
-
-	/* let architecture-dependent part to do it */
-	if (!acpi_numa_memory_affinity_init(memory_affinity))
-		parsed_numa_memblks++;
-	return 0;
-}
 
 static int __init acpi_parse_srat(struct acpi_table_header *table)
 {
