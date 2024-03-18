75db3eac6267517f42a22a79d5b09fb986ccdb1d
Luca Coelho <luciano.coelho@intel.com>
2024-03-18 15:37:55 +0200

    drm/i915/display: don't allow DMC wakelock on older hardware
    Only allow running DMC wakelock code if the display version is 20 or
    greater.
    
    Signed-off-by: Luca Coelho <luciano.coelho@intel.com>

--- drivers/gpu/drm/i915/display/intel_dmc_wl.c
+++ drivers/gpu/drm/i915/display/intel_dmc_wl.c
@@ -120,6 +120,9 @@ void intel_dmc_wl_enable(struct drm_i915_private *i915)
 	struct intel_dmc_wl *wl = &i915->display.wl;
 	unsigned long flags;
 
+	if (DISPLAY_VER(i915) < 20)
+		return;
+
 	spin_lock_irqsave(&wl->lock, flags);
 
 	if (wl->enabled)
@@ -142,6 +145,9 @@ void intel_dmc_wl_disable(struct drm_i915_private *i915)
 {
 	struct intel_dmc_wl *wl = &i915->display.wl;
 	unsigned long flags;
+
+	if (DISPLAY_VER(i915) < 20)
+		return;
 
 	flush_delayed_work(&wl->work);
 
@@ -170,6 +176,9 @@ void intel_dmc_wl_get(struct drm_i915_private *i915, i915_reg_t reg)
 {
 	struct intel_dmc_wl *wl = &i915->display.wl;
 	unsigned long flags;
+
+	if (DISPLAY_VER(i915) < 20)
+		return;
 
 	if (!intel_dmc_wl_check_range(reg.reg))
 		return;
@@ -202,6 +211,9 @@ void intel_dmc_wl_put(struct drm_i915_private *i915, i915_reg_t reg)
 {
 	struct intel_dmc_wl *wl = &i915->display.wl;
 	unsigned long flags;
+
+	if (DISPLAY_VER(i915) < 20)
+		return;
 
 	if (!intel_dmc_wl_check_range(reg.reg))
 		return;
