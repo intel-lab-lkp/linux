165c9273640bfff01ffe2b404a319e8180bfbaf2
David Stevens <stevensd@chromium.org>
2024-03-18 18:10:33 +0900

    virtio_balloon: Give the balloon its own wakeup source
    Wakeup sources don't support nesting multiple events, so sharing a
    single object between multiple drivers can result in one driver
    overriding the wakeup event processing period specified by another
    driver. Have the virtio balloon driver use the wakeup source of the
    device it is bound to rather than the wakeup source of the parent
    device, to avoid conflicts with the transport layer.
    
    Note that although the virtio balloon's virtio_device itself isn't what
    actually wakes up the device, it is responsible for processing wakeup
    events. In the same way that EPOLLWAKEUP uses a dedicated wakeup_source
    to prevent suspend when userspace is processing wakeup events, a
    dedicated wakeup_source is necessary when processing wakeup events in a
    higher layer in the kernel.
    
    Fixes: b12fbc3f787e ("virtio_balloon: stay awake while adjusting balloon")
    Signed-off-by: David Stevens <stevensd@chromium.org>

--- drivers/virtio/virtio_balloon.c
+++ drivers/virtio/virtio_balloon.c
@@ -450,7 +450,7 @@ static void start_update_balloon_size(struct virtio_balloon *vb)
 	vb->adjustment_signal_pending = true;
 	if (!vb->adjustment_in_progress) {
 		vb->adjustment_in_progress = true;
-		pm_stay_awake(vb->vdev->dev.parent);
+		pm_stay_awake(&vb->vdev->dev);
 	}
 	spin_unlock_irqrestore(&vb->adjustment_lock, flags);
 
@@ -462,7 +462,7 @@ static void end_update_balloon_size(struct virtio_balloon *vb)
 	spin_lock_irq(&vb->adjustment_lock);
 	if (!vb->adjustment_signal_pending && vb->adjustment_in_progress) {
 		vb->adjustment_in_progress = false;
-		pm_relax(vb->vdev->dev.parent);
+		pm_relax(&vb->vdev->dev);
 	}
 	spin_unlock_irq(&vb->adjustment_lock);
 }
@@ -1028,6 +1028,7 @@ static int virtballoon_probe(struct virtio_device *vdev)
 	}
 
 	spin_lock_init(&vb->adjustment_lock);
+	device_set_wakeup_capable(&vb->vdev->dev, true);
 
 	virtio_device_ready(vdev);
 
