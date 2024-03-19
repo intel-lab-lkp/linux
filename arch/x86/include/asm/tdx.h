2f998560c49bbf4706e0b8a043d26287e8e9b82d
Kai Huang <kai.huang@intel.com>
2024-03-19 01:48:48 +0000

    x86/virt/tdx: Add TDX memory reset notifier to reset other private pages
    TL;DR:
    
    To cover both normal kexec and crash kexec, add a TDX specific memory
    reset notifier to let "in-kernel TDX users" use their own way to convert
    TDX private pages (that they manage respectively) in tdx_reset_memory().
    
    Long version:
    
    On the platforms with TDX "partial write machine check" erratum, during
    kexec, the kernel needs to convert TDX private memory back to normal
    before jumping to the second kernel to avoid the second kernel seeing
    potential machine check.
    
    For now tdx_reset_memory() only resets PAMT pages.  KVM will be the
    first in-kernel TDX user to support running TDX guests, and by then
    other TDX private pages will start to exist.  They need to be covered
    too.
    
    Currently the kernel doesn't have a unified way to tell whether a given
    page is TDX private page or not.  One choice is to add such unified way,
    and there are couple of options to do it:
    
     1) Use a bitmap, or Xarray, etc to track TDX private page for all PFNs;
     2) Use a "software-only" bit in the direct-mapping PTE to mark a given
        page is TDX private page;
     3) Use a new flag in 'struct page' to mark TDX private page;
     4) ... potential other ways.
    
    Option 1) consumes additional memory.  E.g., if using bitmap, the
    overhead is "number of total RAM pages / 8" bytes.
    
    Option 2) would cause splitting large-page mapping to 4K mapping in the
    direct mapping when one page is allocated as TDX private page, and cause
    additional TLB flush etc.  It's not ideal for such use case.
    
    Option 3) apparently contradicts to the effort to reduce the use of the
    flags of 'struct page'.
    
    None of above is ideal.
    
    Therefore, instead of providing a unified way to tell whether a given
    page is TDX private page or not, leave "resetting TDX private pages" to
    the "in-kernel user" of TDX.
    
    This is motivated by the fact that KVM is already maintaining an Xarray
    to track "memory attributes (e.g., private or shared)" for each GFN for
    each guest.  Thus KVM can use its own way to find all TDX private pages
    that it manages and convert them back to normal.
    
    For the normal kexec the reboot notifier could be used, but it doesn't
    cover the cash kexec.
    
    Add a TDX specific memory reset notifier to achieve this.  The in-kernel
    TDX users will need to register their own notifiers to reset TDX private
    pages.  Call these notifiers in tdx_reset_memory() right before
    resetting PAMT pages.
    
    KVM will be the first user of this notifier.  Export the "register" and
    "unregister" APIs for KVM to use.
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -117,12 +117,26 @@ int tdx_cpu_enable(void);
 int tdx_enable(void);
 const char *tdx_dump_mce_info(struct mce *m);
 void tdx_reset_memory(void);
+
+struct notifier_block;
+
+int tdx_register_memory_reset_notifier(struct notifier_block *nb);
+void tdx_unregister_memory_reset_notifier(struct notifier_block *nb);
 #else
 static inline void tdx_init(void) { }
 static inline int tdx_cpu_enable(void) { return -ENODEV; }
 static inline int tdx_enable(void)  { return -ENODEV; }
 static inline const char *tdx_dump_mce_info(struct mce *m) { return NULL; }
 static inline void tdx_reset_memory(void) { }
+
+struct notifier_block;
+
+static inline int tdx_register_memory_reset_notifier(struct notifier_block *nb)
+{
+	return -EOPNOTSUPP;
+}
+static inline void tdx_unregister_memory_reset_notifier(
+		struct notifier_block *nb) { }
 #endif	/* CONFIG_INTEL_TDX_HOST */
 
 #endif /* !__ASSEMBLY__ */
