db03276d8ba92a4b8231d229b09eea8fa90ffea5
Shengjiu Wang <shengjiu.wang@nxp.com>
2024-03-19 15:51:11 +0800

    media: uapi: Add an entity type for audio resampler
    Add and document a media entity type for an audio resampler.
    It is MEDIA_ENT_F_PROC_AUDIO_RESAMPLER.
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@nxp.com>

--- include/uapi/linux/media.h
+++ include/uapi/linux/media.h
@@ -125,6 +125,7 @@ struct media_device_info {
 #define MEDIA_ENT_F_PROC_VIDEO_ENCODER		(MEDIA_ENT_F_BASE + 0x4007)
 #define MEDIA_ENT_F_PROC_VIDEO_DECODER		(MEDIA_ENT_F_BASE + 0x4008)
 #define MEDIA_ENT_F_PROC_VIDEO_ISP		(MEDIA_ENT_F_BASE + 0x4009)
+#define MEDIA_ENT_F_PROC_AUDIO_RESAMPLER	(MEDIA_ENT_F_BASE + 0x400a)
 
 /*
  * Switch and bridge entity functions
