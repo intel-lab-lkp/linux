a80eb4061a443175d7b3cbba09e51555a72fda2e
Kefeng Wang <wangkefeng.wang@huawei.com>
2024-03-21 16:27:31 +0800

    fs: aio: use a folio in aio_setup_ring()
    Use a folio throughout aio_setup_ring() to remove calls to compound_head().
    
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>

--- fs/aio.c
+++ fs/aio.c
@@ -527,17 +527,20 @@ static int aio_setup_ring(struct kioctx *ctx, unsigned int nr_events)
 	}
 
 	for (i = 0; i < nr_pages; i++) {
-		struct page *page;
-		page = find_or_create_page(file->f_mapping,
-					   i, GFP_USER | __GFP_ZERO);
-		if (!page)
-			break;
-		pr_debug("pid(%d) page[%d]->count=%d\n",
-			 current->pid, i, page_count(page));
-		SetPageUptodate(page);
-		unlock_page(page);
+		struct folio *folio;
 
-		ctx->ring_pages[i] = page;
+		folio = __filemap_get_folio(file->f_mapping, i,
+					    FGP_LOCK | FGP_ACCESSED | FGP_CREAT,
+					    GFP_USER | __GFP_ZERO);
+		if (!folio)
+			break;
+
+		pr_debug("pid(%d) [%d] folio->count=%d\n", current->pid, i,
+			 folio_ref_count(folio));
+		folio_mark_uptodate(folio);
+		folio_unlock(folio);
+
+		ctx->ring_pages[i] = &folio->page;
 	}
 	ctx->nr_pages = i;
 
