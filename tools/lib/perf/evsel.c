44714aee819d998fbc14eedf225bc0ede07f1f08
Ben Gainey <ben.gainey@arm.com>
2024-03-22 16:42:35 +0000

    tools/perf: Track where perf_sample_ids need per-thread periods
    perf_sample_ids and related evlist/evsel code are modified to track
    which events combine inherit+inherit_stat+PERF_SAMPLE_READ.
    
    Events with this combination of properties must be handled differently
    when tracking each streams sample period, as their stream-id is now only
    unique per thread, rather than globally unique.
    
    Signed-off-by: Ben Gainey <ben.gainey@arm.com>

--- tools/lib/perf/evsel.c
+++ tools/lib/perf/evsel.c
@@ -537,6 +537,13 @@ void perf_evsel__free_id(struct perf_evsel *evsel)
 	evsel->ids = 0;
 }
 
+bool perf_evsel__attr_has_per_thread_sample_period(struct perf_evsel *evsel)
+{
+	return (evsel->attr.sample_type & PERF_SAMPLE_READ)
+		&& evsel->attr.inherit
+		&& evsel->attr.inherit_stat;
+}
+
 void perf_counts_values__scale(struct perf_counts_values *count,
 			       bool scale, __s8 *pscaled)
 {
