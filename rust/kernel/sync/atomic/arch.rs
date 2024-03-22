2e4c8272ae1e8f706d7f593a00d53a1cdea22bef
Boqun Feng <boqun.feng@gmail.com>
2024-03-22 16:38:37 -0700

    rust: atomic: Add ARM64 fetch_add_relaxed()
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- rust/kernel/sync/atomic/arch.rs
+++ rust/kernel/sync/atomic/arch.rs
@@ -5,5 +5,11 @@
 #[cfg(CONFIG_X86)]
 pub(crate) use x86::*;
 
+#[cfg(CONFIG_ARM64)]
+pub(crate) use arm64::*;
+
 #[cfg(CONFIG_X86)]
 pub(crate) mod x86;
+
+#[cfg(CONFIG_ARM64)]
+pub(crate) mod arm64;
---
+++ rust/kernel/sync/atomic/arch/arm64.rs
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: GPL-2.0
+
+//! ARM64 implementation for atomic and barrier primitives.
+
+use core::arch::asm;
+use core::cell::UnsafeCell;
+
+pub(crate) fn i32_fetch_add_relaxed(v: &UnsafeCell<i32>, i: i32) -> i32 {
+    let mut result;
+    unsafe {
+        asm!(
+            "prfm    pstl1strm, [{v}]",
+            "1: ldxr {result:w}, [{v}]",
+            "add {val:w}, {result:w}, {i:w}",
+            "stxr {tmp:w}, {val:w}, [{v}]",
+            "cbnz {tmp:w}, 1b",
+            result = out(reg) result,
+            tmp = out(reg) _,
+            val = out(reg) _,
+            v = in(reg) v.get(),
+            i = in(reg) i,
+        )
+    }
+
+    result
+}
