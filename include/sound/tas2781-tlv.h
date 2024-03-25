89d54ec00882bb0ca25d98e91a6313854fee7132
Gergo Koteles <soyer@irl.hu>
2024-03-25 22:25:33 +0100

    ALSA: hda/tas2781: remove digital gain kcontrol
    The "Speaker Digital Gain" kcontrol controls the TAS2781_DVC_LVL (0x1A)
    register. Unfortunately the tas2563 does not have DVC_LVL, but has
    INT_MASK0 in 0x1A, which has been misused so far.
    
    Since commit c1947ce61ff4 ("ALSA: hda/realtek: tas2781: enable subwoofer
    volume control") the volume of the tas2781 amplifiers can be controlled
    by the master volume, so this digital gain kcontrol is not needed.
    
    Remove it.
    
    Fixes: 5be27f1e3ec9 ("ALSA: hda/tas2781: Add tas2781 HDA driver")
    CC: stable@vger.kernel.org
    Signed-off-by: Gergo Koteles <soyer@irl.hu>

--- include/sound/tas2781-tlv.h
+++ include/sound/tas2781-tlv.h
@@ -15,7 +15,6 @@
 #ifndef __TAS2781_TLV_H__
 #define __TAS2781_TLV_H__
 
-static const DECLARE_TLV_DB_SCALE(dvc_tlv, -10000, 100, 0);
 static const DECLARE_TLV_DB_SCALE(amp_vol_tlv, 1100, 50, 0);
 
 #endif
