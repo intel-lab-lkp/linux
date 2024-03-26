eab7ff6861a87adc9baada89a2f6b75518af7176
Bob Pearson <rpearsonhpe@gmail.com>
2024-03-26 12:43:21 -0500

    RDMA/rxe: Don't schedule rxe_completer() in rxe_requester()
    Now that rxe_completer() is always called serially after
    rxe_requester() there is no reason to schedule rxe_completer()
    in rxe_requester().
    
    Signed-off-by: Bob Pearson <rpearsonhpe@gmail.com>

--- drivers/infiniband/sw/rxe/rxe_req.c
+++ drivers/infiniband/sw/rxe/rxe_req.c
@@ -631,12 +631,6 @@ static int rxe_do_local_ops(struct rxe_qp *qp, struct rxe_send_wqe *wqe)
 	wqe->status = IB_WC_SUCCESS;
 	qp->req.wqe_index = queue_next_index(qp->sq.queue, qp->req.wqe_index);
 
-	/* There is no ack coming for local work requests
-	 * which can lead to a deadlock. So go ahead and complete
-	 * it now.
-	 */
-	rxe_sched_task(&qp->send_task);
-
 	return 0;
 }
 
@@ -760,7 +754,6 @@ int rxe_requester(struct rxe_qp *qp)
 						       qp->req.wqe_index);
 			wqe->state = wqe_state_done;
 			wqe->status = IB_WC_SUCCESS;
-			rxe_sched_task(&qp->send_task);
 			goto done;
 		}
 		payload = mtu;
