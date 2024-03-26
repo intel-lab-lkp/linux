94c184377567e7da527af4954b584d0f6bccc4a7
Changhuang Liang <changhuang.liang@starfivetech.com>
2024-03-25 20:12:35 -0700

    media: cadence: csi2rx: Add suspend and resume support
    This patch implements suspend and resume operation for Cadence MIPI-CSI2
    RX Controller.
    
    Signed-off-by: Changhuang Liang <changhuang.liang@starfivetech.com>

--- drivers/media/platform/cadence/cdns-csi2rx.c
+++ drivers/media/platform/cadence/cdns-csi2rx.c
@@ -778,6 +778,7 @@ static void csi2rx_remove(struct platform_device *pdev)
 
 static const struct dev_pm_ops csi2rx_pm_ops = {
 	SET_RUNTIME_PM_OPS(csi2rx_suspend, csi2rx_resume, NULL)
+	SET_SYSTEM_SLEEP_PM_OPS(pm_runtime_force_suspend, pm_runtime_force_resume)
 };
 
 static const struct of_device_id csi2rx_of_table[] = {
