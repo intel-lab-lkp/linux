b34a644331006f108534ad3536cb6f36aebb90a5
Mina Almasry <almasrymina@google.com>
2024-03-27 14:45:20 -0700

    net: mirror skb frag ref/unref helpers
    Refactor some of the skb frag ref/unref helpers for improved clarity.
    
    Implement napi_pp_get_page() to be the mirror counterpart of
    napi_pp_put_page().
    
    Implement skb_page_ref() to be the mirror of skb_page_unref().
    
    Improve __skb_frag_ref() to become a mirror counterpart of
    __skb_frag_unref(). Previously unref could handle pp & non-pp pages,
    while the ref could only handle non-pp pages. Now both the ref & unref
    helpers can correctly handle both pp & non-pp pages.
    
    Now that __skb_frag_ref() can handle both pp & non-pp pages, remove
    skb_pp_frag_ref(), and use __skb_frag_ref() instead.  This lets us
    remove pp specific handling from skb_try_coalesce.
    
    Signed-off-by: Mina Almasry <almasrymina@google.com>
    Reviewed-by: Dragos Tatulea <dtatulea@nvidia.com>

--- include/linux/skbuff.h
+++ include/linux/skbuff.h
@@ -3494,15 +3494,29 @@ static inline struct page *skb_frag_page(const skb_frag_t *frag)
 	return netmem_to_page(frag->netmem);
 }
 
+bool napi_pp_get_page(struct page *page);
+
+static inline void skb_page_ref(struct page *page, bool recycle)
+{
+#ifdef CONFIG_PAGE_POOL
+	if (recycle && napi_pp_get_page(page))
+		return;
+#endif
+	get_page(page);
+}
+
 /**
  * __skb_frag_ref - take an addition reference on a paged fragment.
  * @frag: the paged fragment
+ * @recycle: skb->pp_recycle param of the parent skb. False if no parent skb.
  *
- * Takes an additional reference on the paged fragment @frag.
+ * Takes an additional reference on the paged fragment @frag. Obtains the
+ * correct reference count depending on whether skb->pp_recycle is set and
+ * whether the frag is a page pool frag.
  */
-static inline void __skb_frag_ref(skb_frag_t *frag)
+static inline void __skb_frag_ref(skb_frag_t *frag, bool recycle)
 {
-	get_page(skb_frag_page(frag));
+	skb_page_ref(skb_frag_page(frag), recycle);
 }
 
 /**
@@ -3514,7 +3528,7 @@ static inline void __skb_frag_ref(skb_frag_t *frag)
  */
 static inline void skb_frag_ref(struct sk_buff *skb, int f)
 {
-	__skb_frag_ref(&skb_shinfo(skb)->frags[f]);
+	__skb_frag_ref(&skb_shinfo(skb)->frags[f], skb->pp_recycle);
 }
 
 int skb_pp_cow_data(struct page_pool *pool, struct sk_buff **pskb,
