85b05e9b66d6bffbaad3db81c40d877295bf549e
Andy Shevchenko <andy.shevchenko@gmail.com>
2024-03-29 12:55:25 +0200

    pinctrl: aw9523: Remove redundant dependency to OF
    Driver does not dependent on OF, remove it.
    While here, add missing mod_devicetable.h.
    
    Signed-off-by: Andy Shevchenko <andy.shevchenko@gmail.com>

--- drivers/pinctrl/pinctrl-aw9523.c
+++ drivers/pinctrl/pinctrl-aw9523.c
@@ -12,6 +12,7 @@
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/irq.h>
+#include <linux/mod_devicetable.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
 #include <linux/property.h>
