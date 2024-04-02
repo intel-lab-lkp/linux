8e0cd07dbad82397c39a6e6ae06c0692c20bab5d
Jakub Kicinski <kuba@kernel.org>
2024-04-01 18:05:15 -0700

    selftests: net: add scaffolding for Netlink tests in Python
    Add glue code for accessing the YNL library which lives under
    tools/net and YAML spec files from under Documentation/.
    Automatically figure out if tests are run in tree or not.
    Since we'll want to use this library both from net and
    drivers/net test targets make the library a target as well,
    and automatically include it when net or drivers/net are
    included. Making net/lib a target ensures that we end up
    with only one copy of it, and saves us some path guessing.
    
    Add a tiny bit of formatting support to be able to output KTAP
    from the start.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

---
+++ tools/testing/selftests/net/lib/py/__init__.py
@@ -0,0 +1,6 @@
+# SPDX-License-Identifier: GPL-2.0
+
+from .ksft import *
+from .ynl import NlError, YnlFamily, EthtoolFamily, NetdevFamily, RtnlFamily
+from .consts import KSRC
+from .utils import *
