3b60e4a402e6d5ddd492a9d21faabfeb2d5470d0
Alexander Duyck <alexanderduyck@fb.com>
2024-04-03 13:08:58 -0700

    eth: fbnic: implement Tx queue alloc/start/stop/free
    Implement basic management operations for Tx queues.
    Allocate memory for submission and completion rings.
    Learn how to start the queues, stop them, and wait for HW
    to be idle.
    
    We call HW rings "descriptor rings" (stored in ring->desc),
    and SW context rings "buffer rings" (stored in ring->*_buf union).
    
    This is the first patch which actually touches CSRs so add CSR
    helpers.
    
    No actual datapath / packet handling here, yet.
    
    Signed-off-by: Alexander Duyck <alexanderduyck@fb.com>

--- drivers/net/ethernet/meta/fbnic/fbnic_txrx.h
+++ drivers/net/ethernet/meta/fbnic/fbnic_txrx.h
@@ -12,17 +12,30 @@ struct fbnic_net;
 #define FBNIC_MAX_TXQS			128u
 #define FBNIC_MAX_RXQS			128u
 
+#define FBNIC_TXQ_SIZE_DEFAULT		1024
+
 #define FBNIC_RING_F_DISABLED		BIT(0)
 #define FBNIC_RING_F_CTX		BIT(1)
 #define FBNIC_RING_F_STATS		BIT(2)	/* ring's stats may be used */
 
 struct fbnic_ring {
+	/* Pointer to buffer specific info */
+	union {
+		void *buffer;			/* Generic pointer */
+		void **tx_buf;			/* TWQ */
+	};
+
 	u32 __iomem *doorbell;		/* pointer to CSR space for ring */
+	__le64 *desc;			/* descriptor ring memory */
 	u16 size_mask;			/* size of ring in descriptors - 1 */
 	u8 q_idx;			/* logical netdev ring index */
 	u8 flags;			/* ring flags (FBNIC_RING_F_*) */
 
 	u32 head, tail;			/* head/tail of ring */
+
+	/* slow path fields follow */
+	dma_addr_t dma;			/* phys addr of descriptor memory */
+	size_t size;			/* size of descriptor ring in memory */
 };
 
 struct fbnic_q_triad {
@@ -51,5 +64,14 @@ netdev_tx_t fbnic_xmit_frame(struct sk_buff *skb, struct net_device *dev);
 
 int fbnic_alloc_napi_vectors(struct fbnic_net *fbn);
 void fbnic_free_napi_vectors(struct fbnic_net *fbn);
+int fbnic_alloc_resources(struct fbnic_net *fbn);
+void fbnic_free_resources(struct fbnic_net *fbn);
+void fbnic_napi_enable(struct fbnic_net *fbn);
+void fbnic_napi_disable(struct fbnic_net *fbn);
+void fbnic_enable(struct fbnic_net *fbn);
+void fbnic_disable(struct fbnic_net *fbn);
+void fbnic_flush(struct fbnic_net *fbn);
+
+int fbnic_wait_all_queues_idle(struct fbnic_dev *fbd, bool may_fail);
 
 #endif /* _FBNIC_TXRX_H_ */
