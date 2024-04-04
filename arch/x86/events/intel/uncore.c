62e0091718ef884f7170f074d5f39621b1df2183
Kan Liang <kan.liang@linux.intel.com>
2024-04-04 07:17:04 -0700

    perf/x86/intel/uncore: Add Lunar Lake and Arrow Lake support
    The uncore subsystem for Lunar Lake and Arrow Lake are similar to the
    previous Meteor Lake. The uncore PerfMon registers are located at both
    MSR and MMIO space.
    
    The CBOX, ARB, and iMC are kept. There is no difference from the
    Meteor Lake.
    
    The sNCU is moved to the MMIO space.
    
    The HBO is newly added and only be accessed from the MMIO space.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>

--- arch/x86/events/intel/uncore.c
+++ arch/x86/events/intel/uncore.c
@@ -1794,6 +1794,11 @@ static const struct intel_uncore_init_fun mtl_uncore_init __initconst = {
 	.mmio_init = adl_uncore_mmio_init,
 };
 
+static const struct intel_uncore_init_fun lnl_uncore_init __initconst = {
+	.cpu_init = lnl_uncore_cpu_init,
+	.mmio_init = lnl_uncore_mmio_init,
+};
+
 static const struct intel_uncore_init_fun icx_uncore_init __initconst = {
 	.cpu_init = icx_uncore_cpu_init,
 	.pci_init = icx_uncore_pci_init,
@@ -1871,6 +1876,8 @@ static const struct x86_cpu_id intel_uncore_match[] __initconst = {
 	X86_MATCH_INTEL_FAM6_MODEL(RAPTORLAKE_S,	&adl_uncore_init),
 	X86_MATCH_INTEL_FAM6_MODEL(METEORLAKE,		&mtl_uncore_init),
 	X86_MATCH_INTEL_FAM6_MODEL(METEORLAKE_L,	&mtl_uncore_init),
+	X86_MATCH_INTEL_FAM6_MODEL(LUNARLAKE_M,		&lnl_uncore_init),
+	X86_MATCH_INTEL_FAM6_MODEL(ARROWLAKE,		&lnl_uncore_init),
 	X86_MATCH_INTEL_FAM6_MODEL(SAPPHIRERAPIDS_X,	&spr_uncore_init),
 	X86_MATCH_INTEL_FAM6_MODEL(EMERALDRAPIDS_X,	&spr_uncore_init),
 	X86_MATCH_INTEL_FAM6_MODEL(GRANITERAPIDS_X,	&gnr_uncore_init),
