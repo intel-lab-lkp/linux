3620427ee51c94f048eb47cd2de194cdd75b53ab
Ricardo Neri <ricardo.neri-calderon@linux.intel.com>
2024-04-05 18:04:14 -0700

    thermal: intel: intel_tcc: Add model checks for temperature registers
    The register MSR_TEMPERATURE_TARGET is not architectural. Its fields may be
    defined differently for each processor model. TCC_OFFSET is an example of
    such case.
    
    Despite being specified as architectural, the registers IA32_[PACKAGE]_
    THERM_STATUS have become model-specific: in recent processors, the
    digital temperature readout uses bits [23:16] whereas the Intel Software
    Developer's manual specifies bits [22:16].
    
    Create an array of processor models and their bitmasks for TCC_OFFSET and
    the digital temperature readout fields. Do not include recent processors.
    Instead, use the bitmasks of these recent processors as default.
    
    Use these model-specific bitmasks when reading TCC_OFFSET or the
    temperature sensors.
    
    Initialize a model-specific data structure during subsys_initcall() to
    have it ready when thermal drivers are loaded.
    
    Expose the new interfaces get_tcc_offset_mask() and
    intel_tcc_get_temp_mask(). The hwmon/coretemp and the intel_tcc_cooling
    drivers need to use model-specific bitmasks. Include stubs that reflect
    minimum support for !CONFIG_INTEL_TCC.
    
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>

--- include/linux/intel_tcc.h
+++ include/linux/intel_tcc.h
@@ -14,5 +14,13 @@ int intel_tcc_get_tjmax(int cpu);
 int intel_tcc_get_offset(int cpu);
 int intel_tcc_set_offset(int cpu, int offset);
 int intel_tcc_get_temp(int cpu, int *temp, bool pkg);
+#ifdef CONFIG_INTEL_TCC
+u32 get_tcc_offset_mask(void);
+u32 intel_tcc_get_temp_mask(bool pkg);
+#else
+static inline u32 get_tcc_offset_mask(void) { return 0; }
+/* Use the architectural bitmask of the temperature readout. No model checks. */
+static inline u32 intel_tcc_get_temp_mask(bool pkg) { return 0x7f; }
+#endif
 
 #endif /* __INTEL_TCC_H__ */
