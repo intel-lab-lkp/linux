0ca9034b513f6586ce3166c9f35cc28a325e71ac
Danielle Ratson <danieller@nvidia.com>
2024-04-08 15:53:38 +0300

    ethtool: cmis_fw_update: add a layer for supporting firmware update using CDB
    According to the CMIS standard, the firmware update process is done using
    a CDB commands sequence.
    
    Implement a work that will be triggered from the module layer in the
    next patch the will initiate and execute all the CDB commands in order, to
    eventually complete the firmware update process.
    
    This flashing process includes, writing the firmware image, running the new
    firmware image and committing it after testing, so that it will run upon
    reset.
    
    This work will also notify user space about the progress of the firmware
    update process.
    
    signed-off-by: danielle ratson <danieller@nvidia.com>

--- net/ethtool/module_fw.h
+++ net/ethtool/module_fw.h
@@ -9,6 +9,8 @@ void ethnl_module_fw_flash_ntf_complete(struct net_device *dev);
 void ethnl_module_fw_flash_ntf_in_progress(struct net_device *dev, u64 done,
 					   u64 total);
 
+void ethtool_cmis_fw_update(struct work_struct *work);
+
 /**
  * struct ethtool_module_fw_flash_params - module firmware flashing parameters
  * @password: Module password. Only valid when @pass_valid is set.
@@ -17,4 +19,20 @@ void ethnl_module_fw_flash_ntf_in_progress(struct net_device *dev, u64 done,
 struct ethtool_module_fw_flash_params {
 	__be32 password;
 	u8 password_valid:1;
+};
+
+/**
+ * struct ethtool_module_fw_flash - module firmware flashing
+ * @dev: Pointer to the net_device to be flashed.
+ * @dev_tracker: Refcount tracker for @dev.
+ * @params: Module firmware flashing parameters.
+ * @work: The flashing firmware work.
+ * @fw: Firmware to flash.
+ */
+struct ethtool_module_fw_flash {
+	struct net_device *dev;
+	netdevice_tracker dev_tracker;
+	struct ethtool_module_fw_flash_params params;
+	struct work_struct work;
+	const struct firmware *fw;
 };
