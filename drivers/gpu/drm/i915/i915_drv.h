c06dd3428bd86e1cef5373dcdd17bc99bf3a252e
Jani Nikula <jani.nikula@intel.com>
2024-04-09 15:26:45 +0300

    drm/i915: add generic __to_intel_display()
    Add generic __to_intel_display() macro that accepts either struct
    drm_i915_private * or struct intel_display *. This is to be used for
    transitional stuff that eventually needs to be converted to use struct
    intel_display *, and therefore is not part of to_intel_display().
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -357,6 +357,17 @@ struct drm_i915_private {
 	 */
 };
 
+/*
+ * Transitional macro to optionally convert struct drm_i915_private * to struct
+ * intel_display *, also accepting the latter.
+ */
+#define __to_intel_display(p)						\
+	_Generic(p,							\
+		 const struct drm_i915_private *: (&((const struct drm_i915_private *)(p))->display), \
+		 struct drm_i915_private *: (&((struct drm_i915_private *)(p))->display), \
+		 const struct intel_display *: (p),			\
+		 struct intel_display *: (p))
+
 static inline struct drm_i915_private *to_i915(const struct drm_device *dev)
 {
 	return container_of(dev, struct drm_i915_private, drm);
