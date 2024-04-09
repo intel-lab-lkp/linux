6212e8b404e59895268fc7b64bca9fed7c3caec3
Charles Keepax <ckeepax@opensource.cirrus.com>
2024-04-09 14:21:24 +0100

    gpio: swnode: Add ability to specify native chip selects for SPI
    SPI devices can specify a cs-gpios property to enumerate their
    chip selects. Under device tree, a zero entry in this property can
    be used to specify that a particular chip select is using the SPI
    controllers native chip select, for example:
    
            cs-gpios = <&gpio1 0 0>, <0>;
    
    Here the second chip select is native. However, when using swnodes
    there is currently no way to specify a native chip select. The
    proposal here is to register a swnode_gpio_undefined software node,
    that can be specified to allow the indication of a native chip
    select. For example:
    
    static const struct software_node_ref_args device_cs_refs[] = {
    	{
    		.node  = &device_gpiochip_swnode,
    		.nargs = 2,
    		.args  = { 0, GPIO_ACTIVE_LOW },
    	},
    	{
    		.node  = &swnode_gpio_undefined,
    		.nargs = 0,
    	},
    };
    
    Register the swnode as the gpiolib is initialised and
    check in swnode_get_gpio_device if the returned node matches
    swnode_gpio_undefined and return -ENOENT, which matches the behaviour
    of the device tree system when it encounters a 0 phandle.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>

--- drivers/gpio/gpiolib-swnode.c
+++ drivers/gpio/gpiolib-swnode.c
@@ -17,6 +17,8 @@
 #include "gpiolib.h"
 #include "gpiolib-swnode.h"
 
+#define GPIOLIB_SWNODE_UNDEFINED_NAME "swnode-gpio-undefined"
+
 static void swnode_format_propname(const char *con_id, char *propname,
 				   size_t max_size)
 {
@@ -39,6 +41,13 @@ static struct gpio_device *swnode_get_gpio_device(struct fwnode_handle *fwnode)
 	gdev_node = to_software_node(fwnode);
 	if (!gdev_node || !gdev_node->name)
 		return ERR_PTR(-EINVAL);
+
+	/*
+	 * Check for special node that identifies undefined GPIOs, this is
+	 * primarily used as a key for internal chip selects in SPI bindings.
+	 */
+	if (!strcmp(gdev_node->name, GPIOLIB_SWNODE_UNDEFINED_NAME))
+		return ERR_PTR(-ENOENT);
 
 	gdev = gpio_device_find_by_label(gdev_node->name);
 	return gdev ?: ERR_PTR(-EPROBE_DEFER);
@@ -121,3 +130,32 @@ int swnode_gpio_count(const struct fwnode_handle *fwnode, const char *con_id)
 
 	return count ?: -ENOENT;
 }
+
+#if IS_ENABLED(CONFIG_GPIO_SWNODE_UNDEFINED)
+/*
+ * A special node that identifies undefined GPIOs, this is primarily used as
+ * a key for internal chip selects in SPI bindings.
+ */
+const struct software_node swnode_gpio_undefined = {
+	.name = GPIOLIB_SWNODE_UNDEFINED_NAME,
+};
+EXPORT_SYMBOL_NS_GPL(swnode_gpio_undefined, GPIO_SWNODE);
+
+static int __init swnode_gpio_init(void)
+{
+	int ret;
+
+	ret = software_node_register(&swnode_gpio_undefined);
+	if (ret < 0)
+		pr_err("gpiolib: failed to register swnode: %d\n", ret);
+
+	return ret;
+}
+subsys_initcall(swnode_gpio_init);
+
+static void __exit swnode_gpio_cleanup(void)
+{
+	software_node_unregister(&swnode_gpio_undefined);
+}
+__exitcall(swnode_gpio_cleanup);
+#endif
