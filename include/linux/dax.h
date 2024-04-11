a9f32db4e461e542ab9621de3448e80bcf491b83
Alistair Popple <apopple@nvidia.com>
2024-04-11 10:57:26 +1000

    fs/dax: Refactor wait for dax idle page
    A FS DAX page is considered idle when its refcount drops to one. This
    is currently open-coded in all file systems supporting FS DAX. Move
    the idle detection to a common function to make future changes easier.
    
    Signed-off-by: Alistair Popple <apopple@nvidia.com>

--- include/linux/dax.h
+++ include/linux/dax.h
@@ -212,6 +212,17 @@ int dax_zero_range(struct inode *inode, loff_t pos, loff_t len, bool *did_zero,
 int dax_truncate_page(struct inode *inode, loff_t pos, bool *did_zero,
 		const struct iomap_ops *ops);
 
+static inline int dax_wait_page_idle(struct page *page,
+				void (cb)(struct inode *),
+				struct inode *inode)
+{
+	int ret;
+
+	ret = ___wait_var_event(page, page_ref_count(page) == 1,
+				TASK_INTERRUPTIBLE, 0, 0, cb(inode));
+	return ret;
+}
+
 #if IS_ENABLED(CONFIG_DAX)
 int dax_read_lock(void);
 void dax_read_unlock(int id);
