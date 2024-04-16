d6b7185c188b8b2a4d84b11e4130676fb359a202
Tommaso Merciai <tomm.merciai@gmail.com>
2024-04-16 16:19:05 +0200

    media: i2c: alvium: Move V4L2_CID_GAIN to V4L2_CID_ANALOG_GAIN
    Into alvium cameras REG_BCRM_GAIN_RW control the analog gain.
    Let's use the right V4L2_CID_ANALOGUE_GAIN ctrl.
    
    Signed-off-by: Tommaso Merciai <tomm.merciai@gmail.com>

--- drivers/media/i2c/alvium-csi2.c
+++ drivers/media/i2c/alvium-csi2.c
@@ -1993,7 +1993,7 @@ static int alvium_g_volatile_ctrl(struct v4l2_ctrl *ctrl)
 	int val;
 
 	switch (ctrl->id) {
-	case V4L2_CID_GAIN:
+	case V4L2_CID_ANALOGUE_GAIN:
 		val = alvium_get_gain(alvium);
 		if (val < 0)
 			return val;
@@ -2025,7 +2025,7 @@ static int alvium_s_ctrl(struct v4l2_ctrl *ctrl)
 		return 0;
 
 	switch (ctrl->id) {
-	case V4L2_CID_GAIN:
+	case V4L2_CID_ANALOGUE_GAIN:
 		ret = alvium_set_ctrl_gain(alvium, ctrl->val);
 		break;
 	case V4L2_CID_AUTOGAIN:
@@ -2154,7 +2154,7 @@ static int alvium_ctrl_init(struct alvium_dev *alvium)
 
 	if (alvium->avail_ft.gain) {
 		ctrls->gain = v4l2_ctrl_new_std(hdl, ops,
-						V4L2_CID_GAIN,
+						V4L2_CID_ANALOGUE_GAIN,
 						alvium->min_gain,
 						alvium->max_gain,
 						alvium->inc_gain,
