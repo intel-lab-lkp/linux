1605c3a996ad7510a2498ba9cbbc0de45c1fb709
tada keisuke <keisuke1.tada@kioxia.com>
2024-04-18 05:44:23 +0000

    md: add atomic mode switching when removing disk
    This patch depends on patch 08.
    
    To minimize the number of execution of atomic mode switching, not only adding atomic mode switching, but also remove judgment is divided into two stages.
    Latency is minimized because only rdev to be removed is switched to atomic mode, not all rdevs.
    
    Signed-off-by: Keisuke TADA <keisuke1.tada@kioxia.com>
    Signed-off-by: Toshifumi OHTAKE <toshifumi.ootake@kioxia.com>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -9291,8 +9291,11 @@ static bool rdev_removeable(struct md_rdev *rdev)
 		return false;
 
 	/* Fautly rdev is not used, it's safe to remove it. */
-	if (test_bit(Faulty, &rdev->flags))
-		return true;
+	if (nr_pending_is_percpu_mode(rdev) && test_bit(Faulty, &rdev->flags)) {
+		percpu_ref_switch_to_atomic_sync(&rdev->nr_pending);
+		if (nr_pending_is_zero(rdev))
+			return true;
+	}
 
 	/* Journal disk can only be removed if it's faulty. */
 	if (test_bit(Journal, &rdev->flags))
@@ -9345,6 +9348,11 @@ static bool rdev_addable(struct md_rdev *rdev)
 static bool md_spares_need_change(struct mddev *mddev)
 {
 	struct md_rdev *rdev;
+
+	rdev_for_each_rcu(rdev, mddev) {
+		if (test_bit(Faulty, &rdev->flags))
+			percpu_ref_switch_to_atomic_sync(&rdev->nr_pending);
+	}
 
 	rcu_read_lock();
 	rdev_for_each_rcu(rdev, mddev) {
