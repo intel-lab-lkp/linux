7155217ba8566b8d5b8ef0a94a53d6762fe0e9fc
David Wei <dw@davidwei.uk>
2024-04-19 15:08:57 -0700

    net: selftest: add test for netdev netlink queue-get API
    Add a selftest for netdev generic netlink. For now there is only a
    single test that exercises the `queue-get` API.
    
    The test works with netdevsim by default or with a real device by
    setting NETIF.
    
    Signed-off-by: David Wei <dw@davidwei.uk>

--- tools/testing/selftests/drivers/net/lib/py/env.py
+++ tools/testing/selftests/drivers/net/lib/py/env.py
@@ -7,7 +7,7 @@ from lib.py import ip
 from lib.py import NetdevSimDev
 
 class NetDrvEnv:
-    def __init__(self, src_path):
+    def __init__(self, src_path, **kwargs):
         self._ns = None
 
         self.env = os.environ.copy()
@@ -16,11 +16,13 @@ class NetDrvEnv:
         if 'NETIF' in self.env:
             self.dev = ip("link show dev " + self.env['NETIF'], json=True)[0]
         else:
-            self._ns = NetdevSimDev()
+            self._ns = NetdevSimDev(**kwargs)
             self.dev = self._ns.nsims[0].dev
         self.ifindex = self.dev['ifindex']
 
     def __enter__(self):
+        ip(f"link set dev {self.dev['ifname']} up")
+
         return self
 
     def __exit__(self, ex_type, ex_value, ex_tb):
