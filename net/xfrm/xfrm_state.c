860e0abbb7454374f8610486f5bb0148546ae316
Antony Antony <antony.antony@secunet.com>
2024-04-22 00:26:57 +0200

    xfrm: Add Direction to the SA in or out
    This patch introduces the 'dir' attribute, 'in' or 'out', to the
    xfrm_state, SA, enhancing usability by delineating the scope of values
    based on direction. An input SA will restrict values pertinent to input,
    effectively segregating them from output-related values.
    And an output SA will restrict attributes for output. This change aims
    to streamline the configuration process and improve the overall
    consistency of SA attributes during configuration.
    
    This feature sets the groundwork for future patches, including
    the upcoming IP-TFS patch.
    
    Signed-off-by: Antony Antony <antony.antony@secunet.com>
    
    v10->v11:
     - set dir out for acquire state
     - allow output state with ESN to set replay-window 0
     - change XFRMA_SA_DIR using NLA_POLICY_RANGE()
     - replace verify_sa_dir() with refactored code.
    
    v9->v10:
     - add more direction specific validations
    	XFRM_STATE_NOPMTUDISC, XFRM_SA_XFLAG_DONT_ENCAP_DSCP
    	XFRMA_MTIMER_THRESH
     - refactor validations into a fuction.
     - add dir to ALLOCSPI to support strongSwan updating SPI to "in" state
    
    v8->v9:
     - add validation XFRM_STATE_ICMP not allowed on OUT SA.
    
    v7->v8:
     - add extra validation check on replay window and seq
     - XFRM_MSG_UPDSA old and new SA should match "dir"
    
    v6->v7:
     - add replay-window check non-esn 0 and ESN 1.
     - remove :XFRMA_SA_DIR only allowed with HW OFFLOAD
    
    v5->v6:
     - XFRMA_SA_DIR only allowed with HW OFFLOAD
    
    v4->v5:
     - add details to commit message
    
    v3->v4:
     - improve HW OFFLOAD DIR check add the other direction
    
    v2->v3:
     - delete redundant XFRM_SA_DIR_USE
     - use u8 for "dir"
     - fix HW OFFLOAD DIR check
    
    v1->v2:
     - use .strict_start_type in struct nla_policy xfrma_policy
     - delete redundant XFRM_SA_DIR_MAX enum

--- net/xfrm/xfrm_state.c
+++ net/xfrm/xfrm_state.c
@@ -1292,6 +1292,7 @@ found:
 		if (km_query(x, tmpl, pol) == 0) {
 			spin_lock_bh(&net->xfrm.xfrm_state_lock);
 			x->km.state = XFRM_STATE_ACQ;
+			x->dir = XFRM_SA_DIR_OUT;
 			list_add(&x->km.all, &net->xfrm.state_all);
 			XFRM_STATE_INSERT(bydst, &x->bydst,
 					  net->xfrm.state_bydst + h,
@@ -1744,6 +1745,7 @@ static struct xfrm_state *xfrm_state_clone(struct xfrm_state *orig,
 	x->lastused = orig->lastused;
 	x->new_mapping = 0;
 	x->new_mapping_sport = 0;
+	x->dir = orig->dir;
 
 	return x;
 
@@ -1855,6 +1857,9 @@ int xfrm_state_update(struct xfrm_state *x)
 
 	err = -ESRCH;
 	if (!x1)
+		goto out;
+
+	if (x1->dir != x->dir)
 		goto out;
 
 	if (xfrm_state_kern(x1)) {
