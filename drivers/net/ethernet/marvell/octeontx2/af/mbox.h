decbf639d7a251446c9ed86a84b853c0018f63d3
Geetha sowjanya <gakula@marvell.com>
2024-04-22 15:23:57 +0530

    octeontx2-af: Add packet path between representor and VF
    This patch installs tcam rules to stree traffic representors
    and VF when swicthdev mode is set. To support this a HW loopback
    channel is reserved. Through this channel packet are routed
    between representor and VFs. "ESW_CFG" mbox is defined to
    notify AF for installing rules.
    
    Signed-off-by: Geetha sowjanya <gakula@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/af/mbox.h
+++ drivers/net/ethernet/marvell/octeontx2/af/mbox.h
@@ -144,6 +144,7 @@ M(LMTST_TBL_SETUP,	0x00a, lmtst_tbl_setup, lmtst_tbl_setup_req,    \
 M(SET_VF_PERM,		0x00b, set_vf_perm, set_vf_perm, msg_rsp)	\
 M(PTP_GET_CAP,		0x00c, ptp_get_cap, msg_req, ptp_get_cap_rsp)	\
 M(GET_REP_CNT,		0x00d, get_rep_cnt, msg_req, get_rep_cnt_rsp)	\
+M(ESW_CFG,		0x00e, esw_cfg, esw_cfg_req, msg_rsp)	\
 /* CGX mbox IDs (range 0x200 - 0x3FF) */				\
 M(CGX_START_RXTX,	0x200, cgx_start_rxtx, msg_req, msg_rsp)	\
 M(CGX_STOP_RXTX,	0x201, cgx_stop_rxtx, msg_req, msg_rsp)		\
@@ -1529,6 +1530,12 @@ struct get_rep_cnt_rsp {
 	struct mbox_msghdr hdr;
 	u16 rep_cnt;
 	u16 rep_pf_map[64];
+	u64 rsvd;
+};
+
+struct esw_cfg_req {
+	struct mbox_msghdr hdr;
+	u8 ena;
 	u64 rsvd;
 };
 
--- drivers/net/ethernet/marvell/octeontx2/af/rvu_devlink.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_devlink.c
@@ -1464,6 +1464,9 @@ static int rvu_devlink_eswitch_mode_get(struct devlink *devlink, u16 *mode)
 	struct rvu *rvu = rvu_dl->rvu;
 	struct rvu_switch *rswitch;
 
+	if (rvu->rep_mode)
+		return -EOPNOTSUPP;
+
 	rswitch = &rvu->rswitch;
 	*mode = rswitch->mode;
 
@@ -1476,6 +1479,9 @@ static int rvu_devlink_eswitch_mode_set(struct devlink *devlink, u16 mode,
 	struct rvu_devlink *rvu_dl = devlink_priv(devlink);
 	struct rvu *rvu = rvu_dl->rvu;
 	struct rvu_switch *rswitch;
+
+	if (rvu->rep_mode)
+		return -EOPNOTSUPP;
 
 	rswitch = &rvu->rswitch;
 	switch (mode) {
