20fd8295026ef86e72e0678740667a39fbbc178f
Sean Anderson <sean.anderson@linux.dev>
2024-04-23 13:18:55 -0400

    drm: zynqmp_dp: Convert to a hard IRQ
    Now that all of the sleeping work is done outside of the IRQ, we can
    convert it to a hard IRQ.
    
    Signed-off-by: Sean Anderson <sean.anderson@linux.dev>

--- drivers/gpu/drm/xlnx/zynqmp_dp.c
+++ drivers/gpu/drm/xlnx/zynqmp_dp.c
@@ -1786,9 +1786,8 @@ int zynqmp_dp_probe(struct zynqmp_dpsub *dpsub)
 	 * Now that the hardware is initialized and won't generate spurious
 	 * interrupts, request the IRQ.
 	 */
-	ret = devm_request_threaded_irq(dp->dev, dp->irq, NULL,
-					zynqmp_dp_irq_handler, IRQF_ONESHOT,
-					dev_name(dp->dev), dp);
+	ret = devm_request_irq(dp->dev, dp->irq, zynqmp_dp_irq_handler,
+			       IRQF_SHARED, dev_name(dp->dev), dp);
 	if (ret < 0)
 		goto err_phy_exit;
 
