b6bbc89c8b044d25d453d287eeffb782bb06c75d
Aren Moynihan <aren@peacevolution.org>
2024-04-23 18:33:08 -0400

    iio: light: stk3310: log error if reading the chip id fails
    If the chip isn't powered, this call is likely to return an error.
    Without a log here the driver will silently fail to probe. Common errors
    are ENXIO (when the chip isn't powered) and ETIMEDOUT (when the i2c bus
    isn't powered).
    
    Signed-off-by: Aren Moynihan <aren@peacevolution.org>

--- drivers/iio/light/stk3310.c
+++ drivers/iio/light/stk3310.c
@@ -475,7 +475,7 @@ static int stk3310_init(struct iio_dev *indio_dev)
 
 	ret = regmap_read(data->regmap, STK3310_REG_ID, &chipid);
 	if (ret < 0)
-		return ret;
+		return dev_err_probe(&client->dev, ret, "failed to read chip id\n");
 
 	if (chipid != STK3310_CHIP_ID_VAL &&
 	    chipid != STK3311_CHIP_ID_VAL &&
