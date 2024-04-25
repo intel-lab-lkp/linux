5da812bcf33c1b4895b9b08bb30456771d1fc857
Anuj Gupta <anuj20.g@samsung.com>
2024-04-26 00:09:35 +0530

    block: copy bip_max_vcnt vecs instead of bip_vcnt during clone
    If bio_integrity_copy_user is used to process the meta buffer, bip_max_vcnt
    is one greater than bip_vcnt. In this case bip_max_vcnt vecs needs to be
    copied to cloned bip.
    
    Signed-off-by: Anuj Gupta <anuj20.g@samsung.com>
    Signed-off-by: Kanchan Joshi <joshi.k@samsung.com>

--- block/bio-integrity.c
+++ block/bio-integrity.c
@@ -622,12 +622,12 @@ int bio_integrity_clone(struct bio *bio, struct bio *bio_src,
 
 	BUG_ON(bip_src == NULL);
 
-	bip = bio_integrity_alloc(bio, gfp_mask, bip_src->bip_vcnt);
+	bip = bio_integrity_alloc(bio, gfp_mask, bip_src->bip_max_vcnt);
 	if (IS_ERR(bip))
 		return PTR_ERR(bip);
 
 	memcpy(bip->bip_vec, bip_src->bip_vec,
-	       bip_src->bip_vcnt * sizeof(struct bio_vec));
+	       bip_src->bip_max_vcnt * sizeof(struct bio_vec));
 
 	bip->bip_vcnt = bip_src->bip_vcnt;
 	bip->bip_iter = bip_src->bip_iter;
