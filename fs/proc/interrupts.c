cd3a8534db5801435ea8b5f5407f6765e0260d08
Jinjie Ruan <ruanjinjie@huawei.com>
2024-04-28 17:48:47 +0800

    proc: Remove unnecessary interrupts.c include
    The irqnr.h is included in interrupts.h and the fs.h is included in
    proc_fs.h, they are unnecessary included in interrupts.c, so remove it.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- fs/proc/interrupts.c
+++ fs/proc/interrupts.c
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0
-#include <linux/fs.h>
 #include <linux/init.h>
 #include <linux/interrupt.h>
-#include <linux/irqnr.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
 
