16bd026efa42711febc9655d3c4b06be833e9a57
Christoph Hellwig <hch@lst.de>
2024-04-29 08:15:29 +0200

    mm,page_owner: don't remove GFP flags in add_stack_record_to_list
    This loses flags like GFP_NOFS and GFP_NOIO that are important to avoid
    deadlocks as well as GFP_NOLOCKDEP that otherwise generates lockdep false
    positives.
    
    Fixes: 217b2119b9e2 ("mm,page_owner: implement the tracking of the stacks count")
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- mm/page_owner.c
+++ mm/page_owner.c
@@ -168,9 +168,7 @@ static void add_stack_record_to_list(struct stack_record *stack_record,
 	unsigned long flags;
 	struct stack *stack;
 
-	/* Filter gfp_mask the same way stackdepot does, for consistency */
 	gfp_mask &= ~GFP_ZONEMASK;
-	gfp_mask &= (GFP_ATOMIC | GFP_KERNEL);
 	gfp_mask |= __GFP_NOWARN;
 
 	set_current_in_page_owner();
