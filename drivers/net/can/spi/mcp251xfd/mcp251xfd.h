a6cb115397455c1c41a87927bcf1f6489a62e752
Marc Kleine-Budde <mkl@pengutronix.de>
2024-04-29 19:10:58 +0200

    can: mcp251xfd: move mcp251xfd_timestamp_start()/stop() into mcp251xfd_chip_start/stop()
    The mcp251xfd wakes up from Low Power or Sleep Mode when SPI activity
    is detected. To avoid this, make sure that the timestamp worker is
    stopped before shutting down the chip.
    
    Split the starting of the timestamp worker out of
    mcp251xfd_timestamp_init() into the separate function
    mcp251xfd_timestamp_start().
    
    Call mcp251xfd_timestamp_init() before mcp251xfd_chip_start(), move
    mcp251xfd_timestamp_start() to mcp251xfd_chip_start(). In this way,
    mcp251xfd_timestamp_stop() can be called unconditionally by
    mcp251xfd_chip_stop().
    
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/spi/mcp251xfd/mcp251xfd.h
+++ drivers/net/can/spi/mcp251xfd/mcp251xfd.h
@@ -950,6 +950,7 @@ int mcp251xfd_handle_tefif(struct mcp251xfd_priv *priv);
 void mcp251xfd_skb_set_timestamp(const struct mcp251xfd_priv *priv,
 				 struct sk_buff *skb, u32 timestamp);
 void mcp251xfd_timestamp_init(struct mcp251xfd_priv *priv);
+void mcp251xfd_timestamp_start(struct mcp251xfd_priv *priv);
 void mcp251xfd_timestamp_stop(struct mcp251xfd_priv *priv);
 
 netdev_tx_t mcp251xfd_start_xmit(struct sk_buff *skb,
