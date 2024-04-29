a3390efcb3c698959931305973875e6ad5619801
Sean Young <sean@mess.org>
2024-04-29 14:32:31 +0100

    media: mceusb: No need for vendor/product ID in name
    This is available in other places and doesn't belong in the name
    of the rc device.
    
    Signed-off-by: Sean Young <sean@mess.org>

--- drivers/media/rc/mceusb.c
+++ drivers/media/rc/mceusb.c
@@ -494,7 +494,7 @@ struct mceusb_dev {
 	u32 carrier;
 	unsigned char tx_mask;
 
-	char name[128];
+	const char *name;
 	char phys[64];
 	enum mceusb_model_type model;
 
@@ -1591,12 +1591,8 @@ static struct rc_dev *mceusb_init_rc_dev(struct mceusb_dev *ir)
 		goto out;
 	}
 
-	snprintf(ir->name, sizeof(ir->name), "%s (%04x:%04x)",
-		 mceusb_model[ir->model].name ?
-			mceusb_model[ir->model].name :
-			"Media Center Ed. eHome Infrared Remote Transceiver",
-		 le16_to_cpu(ir->usbdev->descriptor.idVendor),
-		 le16_to_cpu(ir->usbdev->descriptor.idProduct));
+	ir->name = mceusb_model[ir->model].name ? :
+		"Media Center Ed. eHome Infrared Remote Transceiver";
 
 	usb_make_path(ir->usbdev, ir->phys, sizeof(ir->phys));
 
