dbab9bb42a5a46a1d148f630cf71712a18e02167
Chengchang Tang <tangchengchang@huawei.com>
2024-04-30 17:28:45 +0800

    RDMA/hns: Support flexible WQE buffer page size
    Currently, driver fixedly allocates 4K pages for userspace WQE buffer
    and results in HW reading WQE with a granularity of 4K even in a 64K
    system. HW has to switch pages every 4K, leading to a loss of performance.
    
    In order to improve performance, add support for userspace to allocate
    flexible WQE buffer page size between 4K to system PAGESIZE.
    
    For old-version userspace driver that does not support this feature,
    the kernel driver will use a fixed 4K pagesize.
    
    Signed-off-by: Chengchang Tang <tangchengchang@huawei.com>
    Signed-off-by: Junxian Huang <huangjunxian6@hisilicon.com>

--- drivers/infiniband/hw/hns/hns_roce_main.c
+++ drivers/infiniband/hw/hns/hns_roce_main.c
@@ -405,6 +405,11 @@ static int hns_roce_alloc_ucontext(struct ib_ucontext *uctx,
 	if (hr_dev->pci_dev->revision >= PCI_REVISION_ID_HIP09)
 		resp.congest_type = hr_dev->caps.cong_cap;
 
+	if (ucmd.config & HNS_ROCE_UCTX_DYN_QP_PGSZ_FLAGS) {
+		context->config |= HNS_ROCE_UCTX_DYN_QP_PGSZ_FLAGS;
+		resp.config |=  HNS_ROCE_RSP_UCTX_DYN_QP_PGSZ_FLAGS;
+	}
+
 	ret = hns_roce_uar_alloc(hr_dev, &context->uar);
 	if (ret)
 		goto error_out;
