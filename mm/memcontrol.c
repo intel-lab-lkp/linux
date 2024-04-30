d68afebfcf7c24076dd7970071b315730ba9fd0f
Shakeel Butt <shakeel.butt@linux.dev>
2024-04-29 23:06:09 -0700

    memcg: cleanup __mod_memcg_lruvec_state
    There are no memcg specific stats for NR_SHMEM_PMDMAPPED and
    NR_FILE_PMDMAPPED. Let's remove them.
    
    Signed-off-by: Shakeel Butt <shakeel.butt@linux.dev>
    Reviewed-by: Yosry Ahmed <yosryahmed@google.com>
    Reviewed-by: Roman Gushchin <roman.gushchin@linux.dev>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -1008,8 +1008,6 @@ static void __mod_memcg_lruvec_state(struct lruvec *lruvec,
 		case NR_ANON_MAPPED:
 		case NR_FILE_MAPPED:
 		case NR_ANON_THPS:
-		case NR_SHMEM_PMDMAPPED:
-		case NR_FILE_PMDMAPPED:
 			if (WARN_ON_ONCE(!in_task()))
 				pr_warn("stat item index: %d\n", idx);
 			break;
