b24e6354b1bfe4949ec3b4bcc56be3025042c5b1
Dr. David Alan Gilbert <linux@treblig.org>
2024-05-02 16:48:23 +0100

    parport: Remove parport_driver.devmodel
    'devmodel' hasn't actually been used since:
      'commit 3275158fa52a ("parport: remove use of devmodel")'
    and everyone now has it set to true and has been fixed up; remove
    the flag.
    
    (There are still comments all over about it)
    
    Signed-off-by: Dr. David Alan Gilbert <linux@treblig.org>

--- drivers/scsi/imm.c
+++ drivers/scsi/imm.c
@@ -1285,7 +1285,6 @@ static struct parport_driver imm_driver = {
 	.name		= "imm",
 	.match_port	= imm_attach,
 	.detach		= imm_detach,
-	.devmodel	= true,
 };
 module_parport_driver(imm_driver);
 
--- drivers/scsi/ppa.c
+++ drivers/scsi/ppa.c
@@ -1157,7 +1157,6 @@ static struct parport_driver ppa_driver = {
 	.name		= "ppa",
 	.match_port	= ppa_attach,
 	.detach		= ppa_detach,
-	.devmodel	= true,
 };
 module_parport_driver(ppa_driver);
 
