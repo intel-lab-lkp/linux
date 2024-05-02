48ba94aa968088dc080bf63846502a526be4383e
Allen Pais <apais@linux.microsoft.com>
2024-05-02 14:59:20 +0000

    fs/coredump: Enable dynamic configuration of max file note size
    Introduce the capability to dynamically configure the maximum file
    note size for ELF core dumps via sysctl. This enhancement removes
    the previous static limit of 4MB, allowing system administrators to
    adjust the size based on system-specific requirements or constraints.
    
    - Remove hardcoded `MAX_FILE_NOTE_SIZE` from `fs/binfmt_elf.c`.
    - Define `max_file_note_size` in `fs/coredump.c` with an initial value
      set to 4MB.
    - Declare `max_file_note_size` as an external variable in
      `include/linux/coredump.h`.
    - Add a new sysctl entry in `kernel/sysctl.c` to manage this setting
      at runtime.
    
    $ sysctl -a | grep max_file_note_size
    kernel.max_file_note_size = 4194304
    
    $ sysctl -n kernel.max_file_note_size
    4194304
    
    $echo 519304 > /proc/sys/kernel/max_file_note_size
    
    $sysctl -n kernel.max_file_note_size
    519304
    
    Why is this being done?
    We have observed that during a crash when there are more than 65k mmaps
    in memory, the existing fixed limit on the size of the ELF notes section
    becomes a bottleneck. The notes section quickly reaches its capacity,
    leading to incomplete memory segment information in the resulting coredump.
    This truncation compromises the utility of the coredumps, as crucial
    information about the memory state at the time of the crash might be
    omitted.
    
    Signed-off-by: Vijay Nag <nagvijay@microsoft.com>
    Signed-off-by: Allen Pais <apais@linux.microsoft.com>

--- fs/coredump.c
+++ fs/coredump.c
@@ -56,10 +56,13 @@
 static bool dump_vma_snapshot(struct coredump_params *cprm);
 static void free_vma_snapshot(struct coredump_params *cprm);
 
+#define MAX_FILE_NOTE_SIZE (4*1024*1024)
+
 static int core_uses_pid;
 static unsigned int core_pipe_limit;
 static char core_pattern[CORENAME_MAX_SIZE] = "core";
 static int core_name_size = CORENAME_MAX_SIZE;
+unsigned int core_file_note_size_max = MAX_FILE_NOTE_SIZE;
 
 struct core_name {
 	char *corename;
@@ -1019,6 +1022,13 @@ static struct ctl_table coredump_sysctls[] = {
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
+	},
+	{
+		.procname       = "core_file_note_size_max",
+		.data           = &core_file_note_size_max,
+		.maxlen         = sizeof(unsigned int),
+		.mode           = 0644,
+		.proc_handler   = proc_douintvec,
 	},
 };
 
