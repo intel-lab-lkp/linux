65edeea3b31b12252f6dca5a553eacc6784d5023
Kuniyuki Iwashima <kuniyu@amazon.com>
2024-05-03 15:31:48 -0700

    af_unix: Move wait_for_unix_gc() to unix_prepare_fpl().
    unix_(dgram|stream)_sendmsg() call wait_for_unix_gc() to trigger GC
    when the number of inflight AF_UNIX sockets is insane.
    
    This does not happen in the sane use case.  If this happened, the
    insane process would continue sending FDs.
    
    We need not impose the duty in the normal sendmsg(), and instead,
    we can trigger GC in unix_prepare_fpl(), which is called when a fd
    of AF_UNIX socket is passed.
    
    Also, this renames wait_for_unix_gc() to __unix_schedule_gc() for the
    following changes.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- net/unix/af_unix.c
+++ net/unix/af_unix.c
@@ -1925,8 +1925,6 @@ static int unix_dgram_sendmsg(struct socket *sock, struct msghdr *msg,
 	if (err < 0)
 		return err;
 
-	wait_for_unix_gc(scm.fp);
-
 	err = -EOPNOTSUPP;
 	if (msg->msg_flags&MSG_OOB)
 		goto out;
@@ -2201,8 +2199,6 @@ static int unix_stream_sendmsg(struct socket *sock, struct msghdr *msg,
 	err = scm_send(sock, msg, &scm, false);
 	if (err < 0)
 		return err;
-
-	wait_for_unix_gc(scm.fp);
 
 	err = -EOPNOTSUPP;
 	if (msg->msg_flags & MSG_OOB) {
