8df6f65d3ced1d65464389c03a0acba6dd1e44c4
Douglas Anderson <dianders@chromium.org>
2024-05-03 14:32:46 -0700

    drm/panel: boe-tv101wum-nl6: Don't call unprepare+disable at shutdown/remove
    It's the responsibility of a correctly written DRM modeset driver to
    call drm_atomic_helper_shutdown() at shutdown time and that should be
    disabling / unpreparing the panel if needed. Panel drivers shouldn't
    be calling these functions themselves.
    
    A recent effort was made to fix as many DRM modeset drivers as
    possible [1] [2] [3] and most drivers are fixed now.
    
    A grep through mainline for compatible strings used by this driver
    indicates that it is used by Mediatek and Qualcomm boards. Both of
    those drivers appear to be correctly calling
    drm_atomic_helper_shutdown() so we can remove the calls.
    
    [1] https://lore.kernel.org/r/20230901234015.566018-1-dianders@chromium.org
    [2] https://lore.kernel.org/r/20230901234202.566951-1-dianders@chromium.org
    [3] https://lore.kernel.org/r/20230921192749.1542462-1-dianders@chromium.org
    
    Cc: Jitao Shi <jitao.shi@mediatek.com>
    Cc: Cong Yang <yangcong5@huaqin.corp-partner.google.com>
    Signed-off-by: Douglas Anderson <dianders@chromium.org>

--- drivers/gpu/drm/panel/panel-boe-tv101wum-nl6.c
+++ drivers/gpu/drm/panel/panel-boe-tv101wum-nl6.c
@@ -1910,20 +1910,10 @@ static int boe_panel_probe(struct mipi_dsi_device *dsi)
 	return ret;
 }
 
-static void boe_panel_shutdown(struct mipi_dsi_device *dsi)
-{
-	struct boe_panel *boe = mipi_dsi_get_drvdata(dsi);
-
-	drm_panel_disable(&boe->base);
-	drm_panel_unprepare(&boe->base);
-}
-
 static void boe_panel_remove(struct mipi_dsi_device *dsi)
 {
 	struct boe_panel *boe = mipi_dsi_get_drvdata(dsi);
 	int ret;
-
-	boe_panel_shutdown(dsi);
 
 	ret = mipi_dsi_detach(dsi);
 	if (ret < 0)
@@ -1972,7 +1962,6 @@ static struct mipi_dsi_driver boe_panel_driver = {
 	},
 	.probe = boe_panel_probe,
 	.remove = boe_panel_remove,
-	.shutdown = boe_panel_shutdown,
 };
 module_mipi_dsi_driver(boe_panel_driver);
 
