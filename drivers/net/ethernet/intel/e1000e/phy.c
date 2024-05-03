2c0cc6e96c9b1798507d6105073a846d4d7cc659
Ricky Wu <en-wei.wu@canonical.com>
2024-05-03 18:18:24 +0800

    e1000e: let the sleep codes run every time
    Originally, the sleep codes being moved forward only
    ran if we met some conditions (e.g. BMSR_LSTATUS bit
    not set in phy_status). Moving these sleep codes forward
    makes the usec_interval take effect every time.
    
    Signed-off-by: Ricky Wu <en-wei.wu@canonical.com>

--- drivers/net/ethernet/intel/e1000e/phy.c
+++ drivers/net/ethernet/intel/e1000e/phy.c
@@ -1777,6 +1777,11 @@ s32 e1000e_phy_has_link_generic(struct e1000_hw *hw, u32 iterations,
 
 	*success = false;
 	for (i = 0; i < iterations; i++) {
+		if (usec_interval >= 1000)
+			msleep(usec_interval / 1000);
+		else
+			udelay(usec_interval);
+
 		/* Some PHYs require the MII_BMSR register to be read
 		 * twice due to the link bit being sticky.  No harm doing
 		 * it across the board.
@@ -1799,10 +1804,6 @@ s32 e1000e_phy_has_link_generic(struct e1000_hw *hw, u32 iterations,
 			*success = true;
 			break;
 		}
-		if (usec_interval >= 1000)
-			msleep(usec_interval / 1000);
-		else
-			udelay(usec_interval);
 	}
 
 	return ret_val;
