605cbaf4c177d3d7f22d8d643f9144754a1f282c
Waiman Long <longman@redhat.com>
2024-05-03 17:59:18 -0400

    locking/qspinlock: Save qspinlock owner CPU into mcs_spinlock
    When examining a contended spinlock in a crash dump, we can trace out the
    list of lock waiter CPUs waiting for the lock by following the linked
    list of mcs_spinlock structures. However, the actual owner of the lock
    is not there making it hard to figure out who the current lock owner is.
    
    Make it easier to figure out this information by saving the lock owner
    CPU into the mcs_spinlock structure of new MCS lock owner, if available,
    when acquiring the lock in the qspinlock slowpath. We can then follow
    the linked list of mcs_spinlock structures to the end to get an encoded
    CPU number of the lock owner, if set.
    
    This owner information is still not available when the lock is acquired
    directly in the fast path or in the pending code path. There is no easy
    way around that.
    
    The additional cost to get the current CPU number in the slowpath should
    be minimal as it should be in a hot cacheline.
    
    Signed-off-by: Waiman Long <longman@redhat.com>

--- kernel/locking/mcs_spinlock.h
+++ kernel/locking/mcs_spinlock.h
@@ -13,11 +13,17 @@
 #ifndef __LINUX_MCS_SPINLOCK_H
 #define __LINUX_MCS_SPINLOCK_H
 
+/*
+ * Save an encoded version of the current MCS lock owner CPU to the
+ * mcs_spinlock structure of the next lock owner.
+ */
+#define MCS_LOCKED	(smp_processor_id() + 1)
+
 #include <asm/mcs_spinlock.h>
 
 struct mcs_spinlock {
 	struct mcs_spinlock *next;
-	int locked; /* 1 if lock acquired */
+	int locked; /* non-zero if lock acquired */
 	int count;  /* nesting count, see qspinlock.c */
 };
 
@@ -42,7 +48,7 @@ do {									\
  * unlocking.
  */
 #define arch_mcs_spin_unlock_contended(l)				\
-	smp_store_release((l), 1)
+	smp_store_release((l), MCS_LOCKED)
 #endif
 
 /*
