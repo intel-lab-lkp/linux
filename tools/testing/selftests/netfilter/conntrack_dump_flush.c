9594acca2efd5d5faa982a247645e50dee5168ce
John Hubbard <jhubbard@nvidia.com>
2024-05-05 14:47:16 -0700

    selftests/netfilter: return a value for several "int" functions
    When building with clang, via:
    
        make LLVM=1 -C tools/testing/selftests
    
    ...clang warns, correctly, that several functions declared with an "int"
    return type are not always returning values in all cases (or at least,
    clang cannot prove that they always return a value).
    
    Fix this by returning 0 for each function. (For these functions,
    non-zero values indicate failure.)
    
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>

--- tools/testing/selftests/netfilter/conntrack_dump_flush.c
+++ tools/testing/selftests/netfilter/conntrack_dump_flush.c
@@ -43,6 +43,7 @@ static int build_cta_tuple_v4(struct nlmsghdr *nlh, int type,
 	mnl_attr_nest_end(nlh, nest_proto);
 
 	mnl_attr_nest_end(nlh, nest);
+	return 0;
 }
 
 static int build_cta_tuple_v6(struct nlmsghdr *nlh, int type,
@@ -71,6 +72,7 @@ static int build_cta_tuple_v6(struct nlmsghdr *nlh, int type,
 	mnl_attr_nest_end(nlh, nest_proto);
 
 	mnl_attr_nest_end(nlh, nest);
+	return 0;
 }
 
 static int build_cta_proto(struct nlmsghdr *nlh)
@@ -90,6 +92,7 @@ static int build_cta_proto(struct nlmsghdr *nlh)
 	mnl_attr_nest_end(nlh, nest_proto);
 
 	mnl_attr_nest_end(nlh, nest);
+	return 0;
 }
 
 static int conntrack_data_insert(struct mnl_socket *sock, struct nlmsghdr *nlh,
@@ -207,6 +210,7 @@ static int conntrack_data_generate_v6(struct mnl_socket *sock,
 static int count_entries(const struct nlmsghdr *nlh, void *data)
 {
 	reply_counter++;
+	return 0;
 }
 
 static int conntracK_count_zone(struct mnl_socket *sock, uint16_t zone)
