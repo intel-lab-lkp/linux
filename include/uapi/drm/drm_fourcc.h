4ee035961ef24c2a3a3b4fcdd9a6dafcbe7e0acc
Juha-Pekka Heikkila <juhapekka.heikkila@gmail.com>
2024-05-06 21:52:36 +0300

    drm/fourcc: define Intel Xe2 related tile4 ccs modifier
    Add Tile4 type ccs modifier to indicate presence of compression on Xe2
    
    Signed-off-by: Juha-Pekka Heikkila <juhapekka.heikkila@gmail.com>

--- include/uapi/drm/drm_fourcc.h
+++ include/uapi/drm/drm_fourcc.h
@@ -703,6 +703,18 @@ extern "C" {
 #define I915_FORMAT_MOD_4_TILED_MTL_RC_CCS_CC fourcc_mod_code(INTEL, 15)
 
 /*
+ * Intel Color Control Surfaces (CCS) for graphics ver. 20 render compression.
+ *
+ * The main surface is Tile 4 and at plane index 0. For semi-planar formats
+ * like NV12, the Y and UV planes are Tile 4 and are located at plane indices
+ * 0 and 1, respectively. The CCS for all planes are stored outside of the
+ * GEM object in a reserved memory area dedicated for the storage of the
+ * CCS data for all compressible GEM objects. The main surface pitch is
+ * required to be a multiple of four Tile 4 widths.
+ */
+#define I915_FORMAT_MOD_4_TILED_XE2_CCS fourcc_mod_code(INTEL, 16)
+
+/*
  * Tiled, NV12MT, grouped in 64 (pixels) x 32 (lines) -sized macroblocks
  *
  * Macroblocks are laid in a Z-shape, and each pixel data is following the
