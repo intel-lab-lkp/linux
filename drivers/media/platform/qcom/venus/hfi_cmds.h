73eced387d72eff2e4c56e0e49f7b06842e603d4
Ricardo Ribalda <ribalda@chromium.org>
2024-05-07 13:18:41 +0000

    media: venus: Refactor hfi_session_empty_buffer_compressed_pkt
    The single element array data[1] is never used. Replace it with a
    padding field of the same size.
    
    This fixes the following cocci warning:
    drivers/media/platform/qcom/venus/hfi_cmds.h:146:5-9: WARNING use flexible-array member instead (https://www.kernel.org/doc/html/latest/process/deprecated.html#zero-length-and-one-element-arrays)
    
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>

--- drivers/media/platform/qcom/venus/hfi_cmds.h
+++ drivers/media/platform/qcom/venus/hfi_cmds.h
@@ -143,7 +143,7 @@ struct hfi_session_empty_buffer_compressed_pkt {
 	u32 input_tag;
 	u32 packet_buffer;
 	u32 extradata_buffer;
-	u32 data[1];
+	u32 padding;
 };
 
 struct hfi_session_empty_buffer_uncompressed_plane0_pkt {
