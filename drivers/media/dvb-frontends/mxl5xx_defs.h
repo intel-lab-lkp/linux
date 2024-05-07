a44b61ab11892425e95bab02109c69fd70148746
Ricardo Ribalda <ribalda@chromium.org>
2024-05-07 13:18:30 +0000

    media: dvb-frontend/mxl5xx: Use flex array for MBIN_SEGMENT_T
    Replace the older style one-element array with a flexible array member.
    There does not seem to be any allocation for this struct in the code, so
    no more code changes are required.
    
    The following cocci warning is fixed:
    drivers/media/dvb-frontends/mxl5xx_defs.h:182:4-8: WARNING use flexible-array member instead (https://www.kernel.org/doc/html/latest/process/deprecated.html#zero-length-and-one-element-arrays)
    
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>

--- drivers/media/dvb-frontends/mxl5xx_defs.h
+++ drivers/media/dvb-frontends/mxl5xx_defs.h
@@ -179,7 +179,7 @@ struct MBIN_SEGMENT_HEADER_T {
 
 struct MBIN_SEGMENT_T {
 	struct MBIN_SEGMENT_HEADER_T header;
-	u8 data[1];
+	u8 data[];
 };
 
 enum MXL_CMD_TYPE_E { MXL_CMD_WRITE = 0, MXL_CMD_READ };
