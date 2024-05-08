92e735dcf24b41cb01d4b3ded1e9623f06df817b
Bitterblue Smith <rtl8821cerfe2@gmail.com>
2024-05-08 13:52:07 +0300

    wifi: rtlwifi: Add rtl8192du/fw.{c,h} and rtl8192du/led.{c,h}
    fw.c contains a function for loading the firmware.
    led.c contains a function for controlling the LED.
    
    Signed-off-by: Bitterblue Smith <rtl8821cerfe2@gmail.com>

---
+++ drivers/net/wireless/realtek/rtlwifi/rtl8192du/fw.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright(c) 2009-2012  Realtek Corporation.*/
+
+#ifndef __RTL92DU_FW_H__
+#define __RTL92DU_FW_H__
+
+int rtl92d_download_fw(struct ieee80211_hw *hw);
+
+#endif
---
+++ drivers/net/wireless/realtek/rtlwifi/rtl8192du/led.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Copyright(c) 2009-2012  Realtek Corporation.*/
+
+#include "../wifi.h"
+#include "led.h"
+
+void rtl92de_led_control(struct ieee80211_hw *hw, enum led_ctl_mode ledaction)
+{
+	/* The hardware has control. */
+}
