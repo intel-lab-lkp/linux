e476794975f75e61c1df49d3810aecc72d84e2b4
Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>
2024-05-09 13:28:29 +0530

    drm/i915: Compute CMRR and calculate vtotal
    Compute Fixed Average Vtotal/CMRR with resepect to
    userspace VRR enablement. Also calculate required
    parameters in case of CMRR is  enabled. During
    intel_vrr_compute_config, CMRR is getting enabled
    based on userspace has enabled Adaptive Sync Vtotal
    mode (Legacy VRR) or not. Make CMRR as small subset of
    FAVT mode, when Panle is running on Fixed refresh rate
    and on VRR framework then only enable CMRR to match with
    actual refresh rate.
    
    --v2:
    - Update is_cmrr_frac_required function return as bool, not int. [Jani]
    - Use signed int math instead of unsigned in cmrr_get_vtotal2. [Jani]
    - Fix typo and usage of camel case in cmrr_get_vtotal. [Jani]
    - Use do_div in cmrr_get_vtotalwhile calculating cmrr_m. [ Jani]
    - Simplify cmrr and vrr compute config in intel_vrr_compute_config. [Jani]
    - Correct valiable name usage in is_cmrr_frac_required. [Ville]
    
    --v3:
    - Removing RFC tag.
    
    --v4:
    - Added edp check to address edp usecase for now. (ville)
    - Updated is_cmrr_fraction_required to more simplified calculation.
    - on longterm goal to be worked upon uapi as suggestion from ville.
    
    --v5:
    - Correct vtotal paramas accuracy and add 2 digit precision.
    - Avoid using DIV_ROUND_UP and improve scanline precision.
    
    --v6:
    - Make CMRR a small subset of FAVT mode.
    
    Signed-off-by: Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>

--- drivers/gpu/drm/i915/display/intel_display_device.h
+++ drivers/gpu/drm/i915/display/intel_display_device.h
@@ -71,6 +71,7 @@ struct drm_printer;
 					  BIT(trans)) != 0)
 #define HAS_VRR(i915)			(DISPLAY_VER(i915) >= 11)
 #define HAS_AS_SDP(i915)		(DISPLAY_VER(i915) >= 13)
+#define HAS_CMRR(i915)			(DISPLAY_VER(i915) >= 20)
 #define INTEL_NUM_PIPES(i915)		(hweight8(DISPLAY_RUNTIME_INFO(i915)->pipe_mask))
 #define I915_HAS_HOTPLUG(i915)		(DISPLAY_INFO(i915)->has_hotplug)
 #define OVERLAY_NEEDS_PHYSICAL(i915)	(DISPLAY_INFO(i915)->overlay_needs_physical)
