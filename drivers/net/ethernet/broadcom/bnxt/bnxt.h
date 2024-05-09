7f1bb8840a6c4e9d9ea533f414fe94d49fd8634b
Manoj Panicker <manoj.panicker2@amd.com>
2024-05-09 11:27:40 -0500

    bnxt_en: Add TPH support in BNXT driver
    As a usage example, this patch implements TPH support in Broadcom BNXT
    device driver by invoking pcie_tph_set_st() function when interrupt
    affinity is changed.
    
    Reviewed-by: Ajit Khaparde <ajit.khaparde@broadcom.com>
    Reviewed-by: Andy Gospodarek <andrew.gospodarek@broadcom.com>
    Reviewed-by: Wei Huang <wei.huang2@amd.com>
    Signed-off-by: Manoj Panicker <manoj.panicker2@amd.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.h
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.h
@@ -1195,6 +1195,10 @@ struct bnxt_irq {
 	u8		have_cpumask:1;
 	char		name[IFNAMSIZ + 2];
 	cpumask_var_t	cpu_mask;
+
+	int		msix_nr;
+	struct bnxt	*bp;
+	struct irq_affinity_notify affinity_notify;
 };
 
 #define HWRM_RING_ALLOC_TX	0x1
