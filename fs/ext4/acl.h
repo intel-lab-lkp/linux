e24f49494c3ce80959708bfb69c8a02059c5d8ce
David Howells <dhowells@redhat.com>
2024-05-10 12:43:25 +0100

    ext4: Don't reduce symlink i_mode by umask if no ACL support
    If CONFIG_EXT4_FS_POSIX_ACL=n then the fallback version of ext4_init_acl()
    will mask off the umask bits from the new inode's i_mode.  This should not
    be done if the inode is a symlink.  If CONFIG_EXT4_FS_POSIX_ACL=y, then we
    go through posix_acl_create() instead which does the right thing with
    symlinks.
    
    However, this is actually unnecessary now as vfs_prepare_mode() has already
    done this where appropriate, so fix this by making the fallback version of
    ext4_init_acl() do nothing.
    
    Fixes: 484fd6c1de13 ("ext4: apply umask if ACL support is disabled")
    Suggested-by: Miklos Szeredi <miklos@szeredi.hu>
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: Max Kellermann <max.kellermann@ionos.com>
    cc: Jan Kara <jack@suse.com>
    cc: Christian Brauner <brauner@kernel.org>
    cc: linux-ext4@vger.kernel.org
    cc: linux-fsdevel@vger.kernel.org

--- fs/ext4/acl.h
+++ fs/ext4/acl.h
@@ -71,7 +71,8 @@ ext4_init_acl(handle_t *handle, struct inode *inode, struct inode *dir)
 	/* usually, the umask is applied by posix_acl_create(), but if
 	   ext4 ACL support is disabled at compile time, we need to do
 	   it here, because posix_acl_create() will never be called */
-	inode->i_mode &= ~current_umask();
+	if (!S_ISLNK(inode->i_mode))
+		inode->i_mode &= ~current_umask();
 
 	return 0;
 }
