8eb245ec5009d71a2cb869fffc0a92b6158a21ec
Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
2024-05-10 13:07:30 +0300

    PCI: Create helper to print TLP Header and Prefix Log
    Add pcie_print_tlp_log() helper to print TLP Header and Prefix Log.
    Print End-End Prefixes only if they are non-zero.
    
    Consolidate the few places which currently print TLP using custom
    formatting.
    
    The first attempt used pr_cont() instead of building a string first but
    it turns out pr_cont() is not compatible with pci_err() and prints on a
    separate line. When I asked about this, Andy Shevchenko suggested
    pr_cont() should not be used in the first place (to eventually get rid
    of it) so pr_cont() is now replaced with building the string first.
    
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -422,6 +422,8 @@ int pcie_read_tlp_log(struct pci_dev *dev, int where, int where2,
 		      unsigned int tlp_len, struct pcie_tlp_log *log);
 unsigned int aer_tlp_log_len(struct pci_dev *dev);
 unsigned int dpc_tlp_log_len(struct pci_dev *dev);
+void pcie_print_tlp_log(const struct pci_dev *dev,
+			const struct pcie_tlp_log *log, const char *pfx);
 #endif	/* CONFIG_PCIEAER */
 
 #ifdef CONFIG_PCIEPORTBUS
