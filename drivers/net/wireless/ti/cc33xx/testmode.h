fac8323bd5d82e07fb428ca0d95a9f894623bae3
Michael Nemanov <Michael.Nemanov@ti.com>
2024-05-12 21:32:46 +0300

    Add testmode.c, testmode.h
    Allows a user-space tools to access FW APIs via CFG80211_TESTMODE
    infrastructure.
    
    Signed-off-by: Michael Nemanov <michael.nemanov@ti.com>

---
+++ drivers/net/wireless/ti/cc33xx/testmode.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+/* Copyright (C) 2022-2024 Texas Instruments Inc.*/
+
+#ifndef __TESTMODE_H__
+#define __TESTMODE_H__
+
+int cc33xx_tm_cmd(struct ieee80211_hw *hw, struct ieee80211_vif *vif,
+		  void *data, int len);
+
+#endif /* __TESTMODE_H__ */
