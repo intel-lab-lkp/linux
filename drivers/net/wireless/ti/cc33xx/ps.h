9dc064c6f39eb07cfcbf6e9228aef6ff1ff7f84d
Michael Nemanov <Michael.Nemanov@ti.com>
2024-05-12 21:32:45 +0300

    Add ps.c, ps.h
    80211 power-save modes are handled automictically by HW but
    can be overridden here.
    
    Signed-off-by: Michael Nemanov <michael.nemanov@ti.com>

---
+++ drivers/net/wireless/ti/cc33xx/ps.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+/* Copyright (C) 2022-2024 Texas Instruments Inc.*/
+
+#ifndef __PS_H__
+#define __PS_H__
+
+#include "acx.h"
+
+int cc33xx_ps_set_mode(struct cc33xx *cc, struct cc33xx_vif *wlvif,
+		       enum cc33xx_cmd_ps_mode_e mode);
+void cc33xx_ps_link_start(struct cc33xx *cc, struct cc33xx_vif *wlvif,
+			  u8 hlid, bool clean_queues);
+
+#endif /* __PS_H__ */
