4a9dffd1bb6f8c8114079f99215a01609347a298
Geliang Tang <tanggeliang@kylinos.cn>
2024-05-15 12:20:21 +0800

    selftests/bpf: Use post_socket_cb in connect_to_fd_opts
    Since the post_socket_cb() callback is added in struct network_helper_opts,
    it's make sense to use it not only in __start_server(), but also in
    connect_to_fd_opts(). Then it can be used to set TCP_CONGESTION sockopt.
    
    Add a post_socket_opts type member cb_opts into struct network_helper_opts,
    then cc can be moved into struct post_socket_opts from network_helper_opts.
    Define a new callback cc_cb() to set TCP_CONGESTION sockopt, and set it to
    post_socket_cb pointer.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- tools/testing/selftests/bpf/network_helpers.h
+++ tools/testing/selftests/bpf/network_helpers.h
@@ -21,16 +21,18 @@ typedef __u16 __sum16;
 #define VIP_NUM 5
 #define MAGIC_BYTES 123
 
-struct post_socket_opts {};
+struct post_socket_opts {
+	const char *cc;
+};
 
 struct network_helper_opts {
-	const char *cc;
 	int timeout_ms;
 	bool must_fail;
 	bool noconnect;
 	int type;
 	int proto;
 	int (*post_socket_cb)(int fd, const struct post_socket_opts *opts);
+	struct post_socket_opts cb_opts;
 };
 
 /* ipv4 test vector */
