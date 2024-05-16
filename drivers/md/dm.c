dd53e5debdde21327c99677de7263ee7e778d63f
Yang Yang <yang.yang@vivo.com>
2024-05-16 16:03:17 +0800

    dm: Avoid sending redundant empty flush bios to the same block device
    If the num_targets is greater than the number of dm_devs in the
    dm_table's devices list, __send_empty_flush() might invoke
    __send_duplicate_bios() multiple times for the same block device.
    This could lead to a substantial decrease in performance when
    num_targets significantly exceeds the number of dm_devs.
    This patch ensure that __send_duplicate_bios() is only called once
    for each dm_dev.
    
    Signed-off-by: Yang Yang <yang.yang@vivo.com>

--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -1567,6 +1567,7 @@ static void __send_empty_flush(struct clone_info *ci)
 {
 	struct dm_table *t = ci->map;
 	struct bio flush_bio;
+	struct dm_dev_internal *dd;
 
 	/*
 	 * Use an on-stack bio for this, it's safe since we don't
@@ -1580,10 +1581,18 @@ static void __send_empty_flush(struct clone_info *ci)
 	ci->sector_count = 0;
 	ci->io->tio.clone.bi_iter.bi_size = 0;
 
-	for (unsigned int i = 0; i < t->num_targets; i++) {
-		struct dm_target *ti = dm_table_get_target(t, i);
+	if (!t->flush_pass_around) {
+		for (unsigned int i = 0; i < t->num_targets; i++) {
+			struct dm_target *ti = dm_table_get_target(t, i);
+			__send_empty_flush_bios(t, ti, ci);
+		}
+	} else {
+		down_read(&t->devices_lock);
 
-		__send_empty_flush_bios(t, ti, ci);
+		list_for_each_entry(dd, dm_table_get_devices(t), list)
+			__send_empty_flush_bios(t, dd->dm_dev->ti, ci);
+
+		up_read(&t->devices_lock);
 	}
 
 	/*
