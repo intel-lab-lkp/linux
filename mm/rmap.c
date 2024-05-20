be2b735f5866e073191701b6c70573069fe13c8a
Byungchul Park <byungchul@sk.com>
2024-05-20 11:17:33 +0900

    mm, migrate: apply luf mechanism to unmapping during migration
    A new mechanism, LUF(Lazy Unmap Flush), defers tlb flush until folios
    that have been unmapped and freed, eventually get allocated again.  It's
    safe for folios that had been mapped read only and were unmapped, since
    the contents of the folios don't change while staying in pcp or buddy
    so we can still read the data through the stale tlb entries.
    
    Applied the mechanism to unmapping during migration.
    
    Signed-off-by: Byungchul Park <byungchul@sk.com>

--- mm/rmap.c
+++ mm/rmap.c
@@ -2600,8 +2600,9 @@ static bool try_to_migrate_one(struct folio *folio, struct vm_area_struct *vma,
  *
  * Tries to remove all the page table entries which are mapping this folio and
  * replace them with special swap entries. Caller must hold the folio lock.
+ * Return true if all the mappings are read-only, otherwise false.
  */
-void try_to_migrate(struct folio *folio, enum ttu_flags flags)
+bool try_to_migrate(struct folio *folio, enum ttu_flags flags)
 {
 	struct rmap_walk_control rwc = {
 		.rmap_one = try_to_migrate_one,
@@ -2620,11 +2621,11 @@ void try_to_migrate(struct folio *folio, enum ttu_flags flags)
 	 */
 	if (WARN_ON_ONCE(flags & ~(TTU_RMAP_LOCKED | TTU_SPLIT_HUGE_PMD |
 					TTU_SYNC | TTU_BATCH_FLUSH)))
-		return;
+		return false;
 
 	if (folio_is_zone_device(folio) &&
 	    (!folio_is_device_private(folio) && !folio_is_device_coherent(folio)))
-		return;
+		return false;
 
 	/*
 	 * During exec, a temporary VMA is setup and later moved.
@@ -2649,6 +2650,8 @@ void try_to_migrate(struct folio *folio, enum ttu_flags flags)
 		fold_ubc(tlb_ubc_luf, tlb_ubc_ro);
 	else
 		fold_ubc(tlb_ubc, tlb_ubc_ro);
+
+	return can_luf;
 }
 
 #ifdef CONFIG_DEVICE_PRIVATE
