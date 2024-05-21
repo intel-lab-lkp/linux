747e55c2ab16384367a37413a7ecd56f43f2cac8
Prabhav Kumar Vaish <pvkumar5749404@gmail.com>
2024-05-22 00:36:24 +0530

    drivers: soc: qcom: Auto cleanup using __free()
    Use automated cleanup to replace of_node_put() in
    qcom_smem_resolve_mem().
    
    Signed-off-by: Prabhav Kumar Vaish <pvkumar5749404@gmail.com>

--- drivers/soc/qcom/smem.c
+++ drivers/soc/qcom/smem.c
@@ -1032,18 +1032,16 @@ static int qcom_smem_resolve_mem(struct qcom_smem *smem, const char *name,
 				 struct smem_region *region)
 {
 	struct device *dev = smem->dev;
-	struct device_node *np;
 	struct resource r;
 	int ret;
+	struct device_node *np __free(device_node) = of_parse_phandle(dev->of_node, name, 0);
 
-	np = of_parse_phandle(dev->of_node, name, 0);
 	if (!np) {
 		dev_err(dev, "No %s specified\n", name);
 		return -EINVAL;
 	}
 
 	ret = of_address_to_resource(np, 0, &r);
-	of_node_put(np);
 	if (ret)
 		return ret;
 
