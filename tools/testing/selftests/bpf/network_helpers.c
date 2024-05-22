d4b6af08ff80f22120ee194d6def4f7287492ed8
Geliang Tang <tanggeliang@kylinos.cn>
2024-05-22 17:23:46 +0800

    selftests/bpf: Add start_server_str helper
    It's a tech debt that start_server() does not take the "opts" argument.
    It's pretty handy to have start_server() as a helper that takes string
    address.
    
    So this patch creates a new helper start_server_str(). Then start_server()
    can be a wrapper of it.
    
    Signed-off-by: Geliang Tang <tanggeliang@kylinos.cn>

--- tools/testing/selftests/bpf/network_helpers.c
+++ tools/testing/selftests/bpf/network_helpers.c
@@ -119,6 +119,21 @@ error_close:
 	return -1;
 }
 
+int start_server_str(int family, const char *addr_str, __u16 port,
+		     const struct network_helper_opts *opts)
+{
+	struct sockaddr_storage addr;
+	socklen_t addrlen;
+
+	if (!opts)
+		opts = &default_opts;
+
+	if (make_sockaddr(family, addr_str, port, &addr, &addrlen))
+		return -1;
+
+	return __start_server((struct sockaddr *)&addr, addrlen, opts);
+}
+
 int start_server(int family, int type, const char *addr_str, __u16 port,
 		 int timeout_ms)
 {
@@ -126,13 +141,8 @@ int start_server(int family, int type, const char *addr_str, __u16 port,
 		.type		= type,
 		.timeout_ms	= timeout_ms,
 	};
-	struct sockaddr_storage addr;
-	socklen_t addrlen;
 
-	if (make_sockaddr(family, addr_str, port, &addr, &addrlen))
-		return -1;
-
-	return __start_server((struct sockaddr *)&addr, addrlen, &opts);
+	return start_server_str(family, addr_str, port, &opts);
 }
 
 static int reuseport_cb(int fd, void *opts)
