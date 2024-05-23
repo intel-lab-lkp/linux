705c523bc915533650ca19218e3aeed96858ca7e
Johannes Thumshirn <johannes.thumshirn@wdc.com>
2024-05-23 17:02:53 +0200

    zonefs: move super block reading from page to folio
    Move reading of the on-disk superblock from page to folios.
    
    Cc: Matthew Wilcox <willy@infradead.org>
    Signed-off-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>

--- fs/zonefs/super.c
+++ fs/zonefs/super.c
@@ -1108,30 +1108,21 @@ cleanup:
 static int zonefs_read_super(struct super_block *sb)
 {
 	struct zonefs_sb_info *sbi = ZONEFS_SB(sb);
+	struct address_space *address_space = sb->s_bdev->bd_mapping;
 	struct zonefs_super *super;
 	u32 crc, stored_crc;
-	struct page *page;
-	struct bio_vec bio_vec;
-	struct bio bio;
+	struct folio *folio;
 	int ret;
 
-	page = alloc_page(GFP_KERNEL);
-	if (!page)
-		return -ENOMEM;
+	folio = read_mapping_folio(address_space, 0, NULL);
+	if (IS_ERR(folio))
+		return PTR_ERR(folio);
 
-	bio_init(&bio, sb->s_bdev, &bio_vec, 1, REQ_OP_READ);
-	bio.bi_iter.bi_sector = 0;
-	__bio_add_page(&bio, page, PAGE_SIZE, 0);
-
-	ret = submit_bio_wait(&bio);
-	if (ret)
-		goto free_page;
-
-	super = page_address(page);
+	super = folio_address(folio);
 
 	ret = -EINVAL;
 	if (le32_to_cpu(super->s_magic) != ZONEFS_MAGIC)
-		goto free_page;
+		goto put_folio;
 
 	stored_crc = le32_to_cpu(super->s_crc);
 	super->s_crc = 0;
@@ -1139,14 +1130,14 @@ static int zonefs_read_super(struct super_block *sb)
 	if (crc != stored_crc) {
 		zonefs_err(sb, "Invalid checksum (Expected 0x%08x, got 0x%08x)",
 			   crc, stored_crc);
-		goto free_page;
+		goto put_folio;
 	}
 
 	sbi->s_features = le64_to_cpu(super->s_features);
 	if (sbi->s_features & ~ZONEFS_F_DEFINED_FEATURES) {
 		zonefs_err(sb, "Unknown features set 0x%llx\n",
 			   sbi->s_features);
-		goto free_page;
+		goto put_folio;
 	}
 
 	if (sbi->s_features & ZONEFS_F_UID) {
@@ -1154,7 +1145,7 @@ static int zonefs_read_super(struct super_block *sb)
 				       le32_to_cpu(super->s_uid));
 		if (!uid_valid(sbi->s_uid)) {
 			zonefs_err(sb, "Invalid UID feature\n");
-			goto free_page;
+			goto put_folio;
 		}
 	}
 
@@ -1163,7 +1154,7 @@ static int zonefs_read_super(struct super_block *sb)
 				       le32_to_cpu(super->s_gid));
 		if (!gid_valid(sbi->s_gid)) {
 			zonefs_err(sb, "Invalid GID feature\n");
-			goto free_page;
+			goto put_folio;
 		}
 	}
 
@@ -1172,14 +1163,14 @@ static int zonefs_read_super(struct super_block *sb)
 
 	if (memchr_inv(super->s_reserved, 0, sizeof(super->s_reserved))) {
 		zonefs_err(sb, "Reserved area is being used\n");
-		goto free_page;
+		goto put_folio;
 	}
 
 	import_uuid(&sbi->s_uuid, super->s_uuid);
 	ret = 0;
 
-free_page:
-	__free_page(page);
+put_folio:
+	folio_put(folio);
 
 	return ret;
 }
