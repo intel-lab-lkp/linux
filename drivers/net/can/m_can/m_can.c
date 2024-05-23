6a82e1e7839245a00cd3edd2fc5fea4c01e565b3
Markus Schneider-Pargmann <msp@baylibre.com>
2024-05-23 09:53:43 +0200

    can: m_can: Map WoL to device_set_wakeup_enable
    In some devices the pins of the m_can module can act as a wakeup source.
    This patch helps do that by connecting the PHY_WAKE WoL option to
    device_set_wakeup_enable. By marking this device as being wakeup
    enabled, this setting can be used by platform code to decide which
    sleep or poweroff mode to use.
    
    Also this prepares the driver for the next patch in which the pinctrl
    settings are changed depending on the desired wakeup source.
    
    Signed-off-by: Markus Schneider-Pargmann <msp@baylibre.com>

--- drivers/net/can/m_can/m_can.c
+++ drivers/net/can/m_can/m_can.c
@@ -2129,6 +2129,26 @@ static int m_can_set_coalesce(struct net_device *dev,
 	return 0;
 }
 
+static void m_can_get_wol(struct net_device *dev, struct ethtool_wolinfo *wol)
+{
+	struct m_can_classdev *cdev = netdev_priv(dev);
+
+	wol->supported = device_can_wakeup(cdev->dev) ? WAKE_PHY : 0;
+	wol->wolopts = device_may_wakeup(cdev->dev) ? WAKE_PHY : 0;
+}
+
+static int m_can_set_wol(struct net_device *dev, struct ethtool_wolinfo *wol)
+{
+	struct m_can_classdev *cdev = netdev_priv(dev);
+
+	if ((wol->wolopts & WAKE_PHY) != wol->wolopts)
+		return -EINVAL;
+
+	device_set_wakeup_enable(cdev->dev, !!wol->wolopts & WAKE_PHY);
+
+	return 0;
+}
+
 static const struct ethtool_ops m_can_ethtool_ops = {
 	.supported_coalesce_params = ETHTOOL_COALESCE_RX_USECS_IRQ |
 		ETHTOOL_COALESCE_RX_MAX_FRAMES_IRQ |
@@ -2142,6 +2162,8 @@ static const struct ethtool_ops m_can_ethtool_ops = {
 
 static const struct ethtool_ops m_can_ethtool_ops_polling = {
 	.get_ts_info = ethtool_op_get_ts_info,
+	.get_wol = m_can_get_wol,
+	.set_wol = m_can_set_wol,
 };
 
 static int register_m_can_dev(struct net_device *dev)
@@ -2265,6 +2287,9 @@ struct m_can_classdev *m_can_class_allocate_dev(struct device *dev,
 		dev_err(dev, "Could not get Message RAM configuration.");
 		goto out;
 	}
+
+	if (dev->of_node && of_property_read_bool(dev->of_node, "wakeup-source"))
+		device_set_wakeup_capable(dev, true);
 
 	/* Get TX FIFO size
 	 * Defines the total amount of echo buffers for loopback
