7f5bf149994fbf566f8f31c1de22d3b4d43c2115
Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>
2024-05-24 15:54:26 +0530

    drm/i915: Update trans_vrr_ctl flag when cmrr is computed
    Add/update trans_vrr_ctl flag when crtc_state->cmrr.enable
    is set, With this commit setting the stage for subsequent
    CMRR enablement.
    
    --v2:
    - Check pipe active state in cmrr enabling. [Jani]
    - Remove usage of bitwise OR on booleans. [Jani]
    - Revert unrelated changes. [Jani]
    - Update intel_vrr_enable, vrr and cmrr enable conditions. [Jani]
    - Simplify whole if-ladder in intel_vrr_enable. [Jani]
    - Revert patch restructuring mistakes in intel_vrr_get_config. [Jani]
    
    --v3:
    - Check pipe active state in cmrr disabling.[Jani]
    - Correct messed up condition in intel_vrr_enable. [Jani]
    
    --v4:
    - Removing RFC tag.
    
    --v5:
    - CMRR handling in co-existatnce of LRR and DRRS.
    
    --v7:
    - Rebase on top of AS SDP merge.
    
    --v8:
    - Remove cmrr_enabling/disabling and update commit message. (Ankit)
    
    Signed-off-by: Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>

--- drivers/gpu/drm/i915/display/intel_vrr.c
+++ drivers/gpu/drm/i915/display/intel_vrr.c
@@ -277,15 +277,20 @@ void intel_vrr_enable(const struct intel_crtc_state *crtc_state)
 
 	intel_de_write(dev_priv, TRANS_PUSH(dev_priv, cpu_transcoder),
 		       TRANS_PUSH_EN);
-
 	if (HAS_AS_SDP(dev_priv))
 		intel_de_write(dev_priv,
 			       TRANS_VRR_VSYNC(dev_priv, cpu_transcoder),
 			       VRR_VSYNC_END(crtc_state->vrr.vsync_end) |
 			       VRR_VSYNC_START(crtc_state->vrr.vsync_start));
 
-	intel_de_write(dev_priv, TRANS_VRR_CTL(dev_priv, cpu_transcoder),
-		       VRR_CTL_VRR_ENABLE | trans_vrr_ctl(crtc_state));
+	if (crtc_state->cmrr.enable) {
+		intel_de_write(dev_priv, TRANS_VRR_CTL(dev_priv, cpu_transcoder),
+			       VRR_CTL_VRR_ENABLE | VRR_CTL_CMRR_ENABLE |
+			       trans_vrr_ctl(crtc_state));
+	} else {
+		intel_de_write(dev_priv, TRANS_VRR_CTL(dev_priv, cpu_transcoder),
+			       VRR_CTL_VRR_ENABLE | trans_vrr_ctl(crtc_state));
+	}
 }
 
 void intel_vrr_disable(const struct intel_crtc_state *old_crtc_state)
