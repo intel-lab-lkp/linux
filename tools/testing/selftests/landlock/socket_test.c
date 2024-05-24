60bc412d35fac8956fb18010fe0e221374c1030a
Mikhail Ivanov <ivanov.mikhail1@huawei-partners.com>
2024-05-24 17:30:07 +0800

    selftests/landlock: Add protocol.socket_access_rights to socket tests
    Add test that checks possibility of adding rule with every possible
    access right.
    
    Signed-off-by: Mikhail Ivanov <ivanov.mikhail1@huawei-partners.com>

--- tools/testing/selftests/landlock/socket_test.c
+++ tools/testing/selftests/landlock/socket_test.c
@@ -178,4 +178,32 @@ TEST_F(protocol, create)
 	ASSERT_EQ(EAFNOSUPPORT, test_socket(&self->unspec_srv0));
 }
 
+TEST_F(protocol, socket_access_rights)
+{
+	const struct landlock_ruleset_attr ruleset_attr = {
+		.handled_access_socket = ACCESS_ALL,
+	};
+	struct landlock_socket_attr protocol = {
+		.family = self->srv0.protocol.family,
+		.type = self->srv0.protocol.type,
+	};
+	int ruleset_fd;
+	__u64 access;
+
+	ruleset_fd =
+		landlock_create_ruleset(&ruleset_attr, sizeof(ruleset_attr), 0);
+	ASSERT_LE(0, ruleset_fd);
+
+	for (access = 1; access <= ACCESS_LAST; access <<= 1) {
+		protocol.allowed_access = access;
+		EXPECT_EQ(0, landlock_add_rule(ruleset_fd, LANDLOCK_RULE_SOCKET,
+					       &protocol, 0))
+		{
+			TH_LOG("Failed to add rule with access 0x%llx: %s",
+			       access, strerror(errno));
+		}
+	}
+	EXPECT_EQ(0, close(ruleset_fd));
+}
+
 TEST_HARNESS_MAIN
