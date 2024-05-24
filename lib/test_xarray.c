318d608215084bc23d88a3c7d41096c0f6146654
Jiapeng Chong <jiapeng.chong@linux.alibaba.com>
2024-05-24 15:16:55 +0800

    XArray tests: Compare pointers to NULL instead of 0
    Avoid pointer type value compared with 0 to make code clear.
    
    /lib/test_xarray.c:973:52-53: WARNING comparing pointer to 0
    
    Reported-by: Abaci Robot <abaci@linux.alibaba.com>
    Closes: https://bugzilla.openanolis.cn/show_bug.cgi?id=9169
    Signed-off-by: Jiapeng Chong <jiapeng.chong@linux.alibaba.com>

--- lib/test_xarray.c
+++ lib/test_xarray.c
@@ -975,7 +975,7 @@ static noinline void check_xa_alloc_1(struct xarray *xa, unsigned int base)
 
 	XA_BUG_ON(xa, xa_alloc(xa, &id, xa_mk_index(10), XA_LIMIT(10, 5),
 				GFP_KERNEL) != -EBUSY);
-	XA_BUG_ON(xa, xa_store_index(xa, 3, GFP_KERNEL) != 0);
+	XA_BUG_ON(xa, NULL != xa_store_index(xa, 3, GFP_KERNEL));
 	XA_BUG_ON(xa, xa_alloc(xa, &id, xa_mk_index(10), XA_LIMIT(10, 5),
 				GFP_KERNEL) != -EBUSY);
 	xa_erase_index(xa, 3);
