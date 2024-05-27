ba91cf4acdf17746839ed02fbd75b7c0b64c58ae
Li RongQing <lirongqing@baidu.com>
2024-05-27 14:19:20 +0800

    dmaengine: idxd: Fix possible Use-After-Free in irq_process_work_list
    I think when the description is freed, it maybe used again because of
    race, then it's next maybe pointer a value that should not be freed
    
    To prevent this, list_for_each_entry_safe should be used.
    
    Signed-off-by: Li RongQing <lirongqing@baidu.com>

--- drivers/dma/idxd/irq.c
+++ drivers/dma/idxd/irq.c
@@ -611,7 +611,7 @@ static void irq_process_work_list(struct idxd_irq_entry *irq_entry)
 
 	spin_unlock(&irq_entry->list_lock);
 
-	list_for_each_entry(desc, &flist, list) {
+	list_for_each_entry_safe(desc, n, &flist, list) {
 		/*
 		 * Check against the original status as ABORT is software defined
 		 * and 0xff, which DSA_COMP_STATUS_MASK can mask out.
