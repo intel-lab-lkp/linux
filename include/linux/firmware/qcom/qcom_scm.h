42adf7cf01771a56295e0d5ecc2565a0a47b81f1
Bartosz Golaszewski <bartosz.golaszewski@linaro.org>
2024-05-27 14:54:59 +0200

    firmware: qcom: qseecom: convert to using the TZ allocator
    Drop the DMA mapping operations from qcom_scm_qseecom_app_send() and
    convert all users of it in the qseecom module to using the TZ allocator
    for creating SCM call buffers. As this is largely a module separate from
    the SCM driver, let's use a separate memory pool. Set the initial size to
    4K and - if we run out - add twice the current amount to the pool.
    
    Signed-off-by: Bartosz Golaszewski <bartosz.golaszewski@linaro.org>

--- include/linux/firmware/qcom/qcom_scm.h
+++ include/linux/firmware/qcom/qcom_scm.h
@@ -118,8 +118,8 @@ bool qcom_scm_lmh_dcvsh_available(void);
 #ifdef CONFIG_QCOM_QSEECOM
 
 int qcom_scm_qseecom_app_get_id(const char *app_name, u32 *app_id);
-int qcom_scm_qseecom_app_send(u32 app_id, dma_addr_t req, size_t req_size,
-			      dma_addr_t rsp, size_t rsp_size);
+int qcom_scm_qseecom_app_send(u32 app_id, void *req, size_t req_size,
+			      void *rsp, size_t rsp_size);
 
 #else /* CONFIG_QCOM_QSEECOM */
 
@@ -129,8 +129,8 @@ static inline int qcom_scm_qseecom_app_get_id(const char *app_name, u32 *app_id)
 }
 
 static inline int qcom_scm_qseecom_app_send(u32 app_id,
-					    dma_addr_t req, size_t req_size,
-					    dma_addr_t rsp, size_t rsp_size)
+					    void *req, size_t req_size,
+					    void *rsp, size_t rsp_size)
 {
 	return -EINVAL;
 }
