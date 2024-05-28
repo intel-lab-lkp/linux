44bff626c045dcc1d836c8785dcdade2b16cc800
kunyu <kunyu@nfschina.com>
2024-05-28 14:45:32 +0800

    sched: core: Remove unnecessary ‘NULL’ values from pending
    pending is assigned first, so it does not need to initialize the
    assignment.
    
    Signed-off-by: kunyu <kunyu@nfschina.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -2777,7 +2777,7 @@ static int affine_move_task(struct rq *rq, struct task_struct *p, struct rq_flag
 	__releases(rq->lock)
 	__releases(p->pi_lock)
 {
-	struct set_affinity_pending my_pending = { }, *pending = NULL;
+	struct set_affinity_pending my_pending = { }, *pending;
 	bool stop_pending, complete = false;
 
 	/* Can the task run on the task's current CPU? If so, we're done */
