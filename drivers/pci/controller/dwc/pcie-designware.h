512673eb5a9d4654577fa053ae0edd96b70ea6de
Niklas Cassel <cassel@kernel.org>
2024-05-28 15:00:37 +0200

    PCI: dwc: ep: Add dw_pcie_ep_deinit_notify()
    Add a DWC specific wrapper function (dw_pcie_ep_deinit_notify()) around
    pci_epc_deinit_notify(), similar to how we have a wrapper function
    (dw_pcie_ep_init_notify()) around pci_epc_init_notify().
    
    This will allow the DWC glue drivers to use the same API layer for init
    and deinit notification.
    
    Signed-off-by: Niklas Cassel <cassel@kernel.org>

--- drivers/pci/controller/dwc/pcie-designware.h
+++ drivers/pci/controller/dwc/pcie-designware.h
@@ -672,6 +672,7 @@ int dw_pcie_ep_init(struct dw_pcie_ep *ep);
 int dw_pcie_ep_init_registers(struct dw_pcie_ep *ep);
 void dw_pcie_ep_init_notify(struct dw_pcie_ep *ep);
 void dw_pcie_ep_deinit(struct dw_pcie_ep *ep);
+void dw_pcie_ep_deinit_notify(struct dw_pcie_ep *ep);
 void dw_pcie_ep_cleanup(struct dw_pcie_ep *ep);
 int dw_pcie_ep_raise_intx_irq(struct dw_pcie_ep *ep, u8 func_no);
 int dw_pcie_ep_raise_msi_irq(struct dw_pcie_ep *ep, u8 func_no,
@@ -703,6 +704,10 @@ static inline void dw_pcie_ep_init_notify(struct dw_pcie_ep *ep)
 }
 
 static inline void dw_pcie_ep_deinit(struct dw_pcie_ep *ep)
+{
+}
+
+static inline void dw_pcie_ep_deinit_notify(struct dw_pcie_ep *ep)
 {
 }
 
