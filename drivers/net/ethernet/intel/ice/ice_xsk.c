f6fe2fc2607dcf804ed219322f119f7159fa7c2e
Larysa Zaremba <larysa.zaremba@intel.com>
2024-05-28 15:14:27 +0200

    ice: move locking outside of ice_qp_ena and ice_qp_dis
    Currently, ice_qp_ena() is called, even if ICE_CFG_BUSY could not be
    acquired by ice_qp_dis(), in such case there is nothing to undo.
    Move locking logic out of these functions, so:
    * we immediately return, if the lock could not be acquired
    * ice_qp_ena() does not operate in an unsafe context
    * ice_qp_ena() does not clear ICE_CFG_BUSY, when it is not held
    
    Fixes: 2d4238f55697 ("ice: Add support for AF_XDP")
    Signed-off-by: Larysa Zaremba <larysa.zaremba@intel.com>

--- drivers/net/ethernet/intel/ice/ice_xsk.c
+++ drivers/net/ethernet/intel/ice/ice_xsk.c
@@ -165,7 +165,6 @@ static int ice_qp_dis(struct ice_vsi *vsi, u16 q_idx)
 	struct ice_q_vector *q_vector;
 	struct ice_tx_ring *tx_ring;
 	struct ice_rx_ring *rx_ring;
-	int timeout = 50;
 	int fail = 0;
 	int err;
 
@@ -175,13 +174,6 @@ static int ice_qp_dis(struct ice_vsi *vsi, u16 q_idx)
 	tx_ring = vsi->tx_rings[q_idx];
 	rx_ring = vsi->rx_rings[q_idx];
 	q_vector = rx_ring->q_vector;
-
-	while (test_and_set_bit(ICE_CFG_BUSY, vsi->state)) {
-		timeout--;
-		if (!timeout)
-			return -EBUSY;
-		usleep_range(1000, 2000);
-	}
 
 	synchronize_net();
 	netif_carrier_off(vsi->netdev);
@@ -257,7 +249,6 @@ static int ice_qp_ena(struct ice_vsi *vsi, u16 q_idx)
 	synchronize_net();
 	netif_tx_start_queue(netdev_get_tx_queue(vsi->netdev, q_idx));
 	netif_carrier_on(vsi->netdev);
-	clear_bit(ICE_CFG_BUSY, vsi->state);
 
 	return fail;
 }
@@ -390,6 +381,14 @@ int ice_xsk_pool_setup(struct ice_vsi *vsi, struct xsk_buff_pool *pool, u16 qid)
 
 	if (if_running) {
 		struct ice_rx_ring *rx_ring = vsi->rx_rings[qid];
+		int timeout = 50;
+
+		while (test_and_set_bit(ICE_CFG_BUSY, vsi->state)) {
+			timeout--;
+			if (!timeout)
+				return -EBUSY;
+			usleep_range(1000, 2000);
+		}
 
 		ret = ice_qp_dis(vsi, qid);
 		if (ret) {
@@ -412,6 +411,7 @@ xsk_pool_if_up:
 			napi_schedule(&vsi->rx_rings[qid]->xdp_ring->q_vector->napi);
 		else if (ret)
 			netdev_err(vsi->netdev, "ice_qp_ena error = %d\n", ret);
+		clear_bit(ICE_CFG_BUSY, vsi->state);
 	}
 
 failure:
