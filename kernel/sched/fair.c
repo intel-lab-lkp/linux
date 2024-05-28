177a3f330825494621980070f028187969a0877d
Qi Zheng <zhengqi.arch@bytedance.com>
2024-05-28 19:30:24 +0800

    sched/numa: return earlier in task_numa_work()
    When a new mm has been allocated, we can return directly after
    initializing the mm->numa_next_scan to avoid redundant calling
    of the time_before().
    
    No functional change intended.
    
    Signed-off-by: Qi Zheng <zhengqi.arch@bytedance.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -3228,6 +3228,7 @@ static void task_numa_work(struct callback_head *work)
 	if (!mm->numa_next_scan) {
 		mm->numa_next_scan = now +
 			msecs_to_jiffies(sysctl_numa_balancing_scan_delay);
+		return;
 	}
 
 	/*
