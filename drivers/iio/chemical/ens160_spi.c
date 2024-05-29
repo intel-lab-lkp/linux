d78456878f9be27de2e77061dfd12b5dae8671fc
Gustavo Silva <gustavograzs@gmail.com>
2024-05-28 21:14:22 -0300

    iio: chemical: ens160: add power management support
    ENS160 supports a deep sleep mode for minimal power consumption.
    Use it to add PM sleep capability to the driver.
    
    Signed-off-by: Gustavo Silva <gustavograzs@gmail.com>

--- drivers/iio/chemical/ens160_spi.c
+++ drivers/iio/chemical/ens160_spi.c
@@ -49,6 +49,7 @@ static struct spi_driver ens160_spi_driver = {
 	.driver = {
 		.name	= "ens160_spi",
 		.of_match_table = ens160_spi_of_match,
+		.pm = pm_sleep_ptr(&ens160_pm_ops),
 	},
 	.probe		= ens160_spi_probe,
 	.id_table	= ens160_spi_id,
