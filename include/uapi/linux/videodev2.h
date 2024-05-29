c31108ea3a51e51538517e46e8f7cfac43e5f258
Daniel Scally <dan.scally@ideasonboard.com>
2024-05-29 16:28:50 +0100

    media: Add MALI_C55_3A_STATS meta format
    Add a new meta format for the Mali-C55 ISP's 3A Statistics along
    with a new descriptor entry.
    
    Acked-by: Nayden Kanchev  <nayden.kanchev@arm.com>
    Co-developed-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>
    Signed-off-by: Daniel Scally <dan.scally@ideasonboard.com>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -841,6 +841,8 @@ struct v4l2_pix_format {
 #define V4L2_META_FMT_RK_ISP1_PARAMS	v4l2_fourcc('R', 'K', '1', 'P') /* Rockchip ISP1 3A Parameters */
 #define V4L2_META_FMT_RK_ISP1_STAT_3A	v4l2_fourcc('R', 'K', '1', 'S') /* Rockchip ISP1 3A Statistics */
 
+#define V4L2_META_FMT_MALI_C55_3A_STATS	v4l2_fourcc('C', '5', '5', 'S') /* ARM Mali-C55 3A Statistics */
+
 #ifdef __KERNEL__
 /*
  * Line-based metadata formats. Remember to update v4l_fill_fmtdesc() when
