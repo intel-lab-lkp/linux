3fd2c3c8e16c9a475ab0cf5c40a8ee4715bdf313
Yajun Deng <yajun.deng@linux.dev>
2024-06-03 20:50:01 +0800

    sched/rt: Use 'back' only if CONFIG_RT_GROUP_SCHED is enabled
    The 'back' member in struct sched_rt_entity only related to
    CONFIG_RT_GROUP_SCHED, so there is no need to init it in dequeue_rt_stack.
    
    Init the 'back' member in init_tg_rt_entry and use it only if
    CONFIG_RT_GROUP_SCHED is enabled.
    
    Signed-off-by: Yajun Deng <yajun.deng@linux.dev>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -232,8 +232,10 @@ void init_tg_rt_entry(struct task_group *tg, struct rt_rq *rt_rq,
 
 	if (!parent)
 		rt_se->rt_rq = &rq->rt;
-	else
+	else {
 		rt_se->rt_rq = parent->my_q;
+		parent->back = rt_se;
+	}
 
 	rt_se->my_q = rt_rq;
 	rt_se->parent = parent;
@@ -1428,16 +1430,19 @@ static void dequeue_rt_stack(struct sched_rt_entity *rt_se, unsigned int flags)
 	struct sched_rt_entity *back = NULL;
 	unsigned int rt_nr_running;
 
-	for_each_sched_rt_entity(rt_se) {
-		rt_se->back = back;
+	for_each_sched_rt_entity(rt_se)
 		back = rt_se;
-	}
 
 	rt_nr_running = rt_rq_of_se(back)->rt_nr_running;
 
-	for (rt_se = back; rt_se; rt_se = rt_se->back) {
+	for (rt_se = back; rt_se; ) {
 		if (on_rt_rq(rt_se))
 			__dequeue_rt_entity(rt_se, flags);
+#ifdef CONFIG_RT_GROUP_SCHED
+		rt_se = rt_se->back;
+#else
+		rt_se = NULL;
+#endif
 	}
 
 	dequeue_top_rt_rq(rt_rq_of_se(back), rt_nr_running);
