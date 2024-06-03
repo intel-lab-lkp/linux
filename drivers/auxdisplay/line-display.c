02ac56a5d09a1ce237c845cfca7ab0d58bbab328
Jeff Johnson <quic_jjohnson@quicinc.com>
2024-06-02 20:02:01 -0700

    auxdisplay: linedisp: add missing MODULE_DESCRIPTION() macro
    make allmodconfig && make W=1 C=1 reports:
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/auxdisplay/line-display.o
    
    Add the missing invocation of the MODULE_DESCRIPTION() macro.
    
    Signed-off-by: Jeff Johnson <quic_jjohnson@quicinc.com>

--- drivers/auxdisplay/line-display.c
+++ drivers/auxdisplay/line-display.c
@@ -388,4 +388,5 @@ void linedisp_unregister(struct linedisp *linedisp)
 }
 EXPORT_SYMBOL_NS_GPL(linedisp_unregister, LINEDISP);
 
+MODULE_DESCRIPTION("Character line display core support");
 MODULE_LICENSE("GPL");
