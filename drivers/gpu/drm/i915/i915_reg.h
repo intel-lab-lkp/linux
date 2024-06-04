98c0c5443ca164a5d37d89ca8a453f67dfa1b384
Jani Nikula <jani.nikula@intel.com>
2024-06-04 18:25:54 +0300

    drm/i915: pass dev_priv explicitly to _PIPEBDSL
    Avoid the implicit dev_priv local variable use, and pass dev_priv
    explicitly to the _PIPEBDSL register macro.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/i915_reg.h
+++ drivers/gpu/drm/i915/i915_reg.h
@@ -2296,7 +2296,7 @@
 #define SWF_ILK(i)	_MMIO(0x4F000 + (i) * 4)
 
 /* Pipe B */
-#define _PIPEBDSL		(DISPLAY_MMIO_BASE(dev_priv) + 0x71000)
+#define _PIPEBDSL(dev_priv)		(DISPLAY_MMIO_BASE(dev_priv) + 0x71000)
 #define _TRANSBCONF		(DISPLAY_MMIO_BASE(dev_priv) + 0x71008)
 #define _PIPEBSTAT		(DISPLAY_MMIO_BASE(dev_priv) + 0x71024)
 #define _PIPEBFRAMEHIGH		0x71040
