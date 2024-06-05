a6bbe951d8c69d752ba6c806d4025133860d9d2d
Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
2024-06-05 15:37:08 -0300

    iwlwifi: mvm: adding check if the thermal firmware is running
    In the dmesg is showing the message "failed to read out thermal zone"
    as if the temperature read is failed by don't find the thermal zone.
    
    After researching and debugging, I see that this specific error is
    occurrenced because the thermal try read the temperature when is started,
    but the firmware is not running yet.
    
    For more legibiliti i change the tt.c for return EAGAIN when this was occurrence.
    After this change, in my computer I compile and install kernel in /boot
    and in my dmesg the message "failed to read out thermal zone" is not show
    any more.
    
    I would like to thanks for Rafael Wysocki <refael.j.wysocki@intel.com> and
    Kalle Valo <kvalo@kernel.org> for your suggestions in my first patch.
    
    Signed-off-by: Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>

--- drivers/net/wireless/intel/iwlwifi/mvm/tt.c
+++ drivers/net/wireless/intel/iwlwifi/mvm/tt.c
@@ -620,8 +620,14 @@ static int iwl_mvm_tzone_get_temp(struct thermal_zone_device *device,
 
 	mutex_lock(&mvm->mutex);
 
-	if (!iwl_mvm_firmware_running(mvm) ||
-	    mvm->fwrt.cur_fw_img != IWL_UCODE_REGULAR) {
+	const int res = iwl_mvm_firmware_running(mvm);
+
+	if (!res) {
+		ret = -EAGAIN;
+		goto out;
+	}
+
+	if (mvm->fwrt.cur_fw_img != IWL_UCODE_REGULAR) {
 		ret = -ENODATA;
 		goto out;
 	}
