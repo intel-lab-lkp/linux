12e1eebfe69af0f7d83e3d090fb0360653dc98ba
Lizhi Xu <lizhi.xu@windriver.com>
2024-06-05 09:23:35 +0800

    fs/ext4: Filesystem without casefold feature cannot be mounted with spihash
    When mounting the ext4 filesystem, if the default hash version is set to
    DX_HASH_SIPHASH but the casefold feature is not set, exit the mounting.
    
    Reported-by: syzbot+340581ba9dceb7e06fb3@syzkaller.appspotmail.com
    Signed-off-by: Lizhi Xu <lizhi.xu@windriver.com>

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -3613,6 +3613,14 @@ int ext4_feature_set_ok(struct super_block *sb, int readonly)
 			 "mounted without CONFIG_UNICODE");
 		return 0;
 	}
+#else
+	if (EXT4_SB(sb)->s_es->s_def_hash_version == DX_HASH_SIPHASH &&
+	    !ext4_has_feature_casefold(sb)) {
+		ext4_msg(sb, KERN_ERR,
+			 "Filesystem without casefold feature cannot be "
+			 "mounted with spihash");
+		return 0;
+	}
 #endif
 
 	if (readonly)
