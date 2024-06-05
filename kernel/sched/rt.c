f2b9437ca7a9372ebfd137140eec0f73de8cd1d0
Yajun Deng <yajun.deng@linux.dev>
2024-06-05 21:35:56 +0800

    sched/rt: Use 'back' only if CONFIG_RT_GROUP_SCHED is enabled
    The 'back' member in struct sched_rt_entity only related to
    CONFIG_RT_GROUP_SCHED, so there is no need to init it in dequeue_rt_stack.
    
    Init the 'back' member in init_tg_rt_entry and use it only if
    CONFIG_RT_GROUP_SCHED is enabled.
    
    Signed-off-by: Yajun Deng <yajun.deng@linux.dev>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -230,10 +230,12 @@ void init_tg_rt_entry(struct task_group *tg, struct rt_rq *rt_rq,
 	if (!rt_se)
 		return;
 
-	if (!parent)
+	if (!parent) {
 		rt_se->rt_rq = &rq->rt;
-	else
+	} else {
 		rt_se->rt_rq = parent->my_q;
+		parent->back = rt_se;
+	}
 
 	rt_se->my_q = rt_rq;
 	rt_se->parent = parent;
@@ -604,6 +606,11 @@ static inline struct rt_bandwidth *sched_rt_bandwidth(struct rt_rq *rt_rq)
 	return &rt_rq->tg->rt_bandwidth;
 }
 
+static struct sched_rt_entity *rt_se_back(struct sched_rt_entity *se)
+{
+	return se->back;
+}
+
 #else /* !CONFIG_RT_GROUP_SCHED */
 
 static inline u64 sched_rt_runtime(struct rt_rq *rt_rq)
@@ -664,6 +671,11 @@ struct rt_rq *sched_rt_period_rt_rq(struct rt_bandwidth *rt_b, int cpu)
 static inline struct rt_bandwidth *sched_rt_bandwidth(struct rt_rq *rt_rq)
 {
 	return &def_rt_bandwidth;
+}
+
+static inline struct sched_rt_entity *rt_se_back(struct sched_rt_entity *se)
+{
+	return NULL;
 }
 
 #endif /* CONFIG_RT_GROUP_SCHED */
@@ -1428,14 +1440,12 @@ static void dequeue_rt_stack(struct sched_rt_entity *rt_se, unsigned int flags)
 	struct sched_rt_entity *back = NULL;
 	unsigned int rt_nr_running;
 
-	for_each_sched_rt_entity(rt_se) {
-		rt_se->back = back;
+	for_each_sched_rt_entity(rt_se)
 		back = rt_se;
-	}
 
 	rt_nr_running = rt_rq_of_se(back)->rt_nr_running;
 
-	for (rt_se = back; rt_se; rt_se = rt_se->back) {
+	for (rt_se = back; rt_se; rt_se = rt_se_back(rt_se)) {
 		if (on_rt_rq(rt_se))
 			__dequeue_rt_entity(rt_se, flags);
 	}
