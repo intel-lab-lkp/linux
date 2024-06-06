c0f46d2311b1b505bf3897af9d3f7eab68a1185c
Aleksandr Mishin <amishin@t-argos.ru>
2024-06-06 13:34:02 +0300

    clk: mediatek: mt8183: Add memory
    No upstream commit exists for this commit.
    
    The issue was introduced with commit c93d059a8045 ("clk: mediatek: mt8183:
    Register 13MHz clock earlier for clocksource")
    
    In case of memory allocation fail in clk_mt8183_top_init_early()
    'top_clk_data' will be set to NULL and later dereferenced without check.
    Fix this bug by adding NULL-return check.
    
    Upstream branch code has been significantly refactored and can't be
    backported directly.
    
    Found by Linux Verification Center (linuxtesting.org) with SVACE.
    
    Signed-off-by: Aleksandr Mishin <amishin@t-argos.ru>

--- drivers/clk/mediatek/clk-mt8183.c
+++ drivers/clk/mediatek/clk-mt8183.c
@@ -1185,6 +1185,11 @@ static void clk_mt8183_top_init_early(struct device_node *node)
 	int i;
 
 	top_clk_data = mtk_alloc_clk_data(CLK_TOP_NR_CLK);
+	if (!top_clk_data) {
+		pr_err("%s(): could not register clock provider: %d\n",
+			__func__, -ENOMEM);
+		return;
+	}
 
 	for (i = 0; i < CLK_TOP_NR_CLK; i++)
 		top_clk_data->hws[i] = ERR_PTR(-EPROBE_DEFER);
