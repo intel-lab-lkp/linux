6ff96c0689016390bdb6f2fce973c3ba211940bd
Piotr Raczynski <piotr.raczynski@intel.com>
2024-06-06 13:24:53 +0200

    ice: allocate devlink for subfunction
    Make devlink allocation function generic to use it for PF and for SF.
    
    Add function for SF devlink port creation. It will be used in next
    patch.
    
    Create header file for subfunction device. Define subfunction device
    structure there as it is needed for devlink allocation and port
    creation.
    
    Reviewed-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>
    Signed-off-by: Piotr Raczynski <piotr.raczynski@intel.com>
    Signed-off-by: Michal Swiatkowski <michal.swiatkowski@linux.intel.com>

---
+++ drivers/net/ethernet/intel/ice/ice_sf_eth.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2024, Intel Corporation. */
+
+#ifndef _ICE_SF_ETH_H_
+#define _ICE_SF_ETH_H_
+
+#include <linux/auxiliary_bus.h>
+#include "ice.h"
+
+struct ice_sf_dev {
+	struct auxiliary_device adev;
+	struct ice_dynamic_port *dyn_port;
+	struct ice_sf_priv *priv;
+};
+
+struct ice_sf_priv {
+	struct ice_sf_dev *dev;
+	struct devlink_port devlink_port;
+};
+
+#endif /* _ICE_SF_ETH_H_ */
