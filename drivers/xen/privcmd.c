9518c6754609647864e1954a6c1d116319634426
Jiqian Chen <Jiqian.Chen@amd.com>
2024-06-07 15:51:09 +0800

    xen/privcmd: Add new syscall to get gsi from dev
    In PVH dom0, it uses the linux local interrupt mechanism,
    when it allocs irq for a gsi, it is dynamic, and follow
    the principle of applying first, distributing first. And
    the irq number is alloced from small to large, but the
    applying gsi number is not, may gsi 38 comes before gsi 28,
    it causes the irq number is not equal with the gsi number.
    And when passthrough a device, QEMU will use device's gsi
    number to do pirq mapping, but the gsi number is got from
    file /sys/bus/pci/devices/<sbdf>/irq, irq!= gsi, so it will
    fail when mapping.
    And in current linux codes, there is no method to get gsi
    for userspace.
    
    For above purpose, record gsi of pcistub devices when init
    pcistub and add a new syscall into privcmd to let userspace
    can get gsi when they have a need.
    
    Signed-off-by: Huang Rui <ray.huang@amd.com>
    Signed-off-by: Jiqian Chen <Jiqian.Chen@amd.com>

--- drivers/xen/privcmd.c
+++ drivers/xen/privcmd.c
@@ -45,6 +45,9 @@
 #include <xen/page.h>
 #include <xen/xen-ops.h>
 #include <xen/balloon.h>
+#ifdef CONFIG_XEN_ACPI
+#include <xen/acpi.h>
+#endif
 
 #include "privcmd.h"
 
@@ -842,6 +845,27 @@ out:
 	return rc;
 }
 
+static long privcmd_ioctl_gsi_from_dev(struct file *file, void __user *udata)
+{
+#ifdef CONFIG_XEN_ACPI
+	struct privcmd_gsi_from_dev kdata;
+
+	if (copy_from_user(&kdata, udata, sizeof(kdata)))
+		return -EFAULT;
+
+	kdata.gsi = pcistub_get_gsi_from_sbdf(kdata.sbdf);
+	if (kdata.gsi == -1)
+		return -EINVAL;
+
+	if (copy_to_user(udata, &kdata, sizeof(kdata)))
+		return -EFAULT;
+
+	return 0;
+#else
+	return -EINVAL;
+#endif
+}
+
 #ifdef CONFIG_XEN_PRIVCMD_EVENTFD
 /* Irqfd support */
 static struct workqueue_struct *irqfd_cleanup_wq;
@@ -1527,6 +1551,10 @@ static long privcmd_ioctl(struct file *file,
 
 	case IOCTL_PRIVCMD_IOEVENTFD:
 		ret = privcmd_ioctl_ioeventfd(file, udata);
+		break;
+
+	case IOCTL_PRIVCMD_GSI_FROM_DEV:
+		ret = privcmd_ioctl_gsi_from_dev(file, udata);
 		break;
 
 	default:
