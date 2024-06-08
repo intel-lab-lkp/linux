4366b3622e5eed1729ac0da6bdc210d6626ceccb
Jeff Johnson <quic_jjohnson@quicinc.com>
2024-06-08 09:31:35 -0700

    mailbox: mtk-cmdq: add missing MODULE_DESCRIPTION() macro
    make allmodconfig && make W=1 C=1 reports:
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/mailbox/mtk-cmdq-mailbox.o
    
    Add the missing invocation of the MODULE_DESCRIPTION() macro.
    
    Signed-off-by: Jeff Johnson <quic_jjohnson@quicinc.com>

--- drivers/mailbox/mtk-cmdq-mailbox.c
+++ drivers/mailbox/mtk-cmdq-mailbox.c
@@ -790,4 +790,5 @@ static void __exit cmdq_drv_exit(void)
 subsys_initcall(cmdq_drv_init);
 module_exit(cmdq_drv_exit);
 
+MODULE_DESCRIPTION("Mediatek Command Queue(CMDQ) driver");
 MODULE_LICENSE("GPL v2");
