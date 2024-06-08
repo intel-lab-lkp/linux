a1f920ebfcac618fc5c31d9da14f360948ef34cd
Marilene A Garcia <marilene.agarcia@gmail.com>
2024-06-08 15:42:10 -0300

    dca: Change the format of kerneldoc parameters to fix warnings
    According to the kernel documentation, the format of the parameter
    descriptions should be "@arg: description".
    
    It also added some missing parameter descriptions and the module
    description.
    
    Signed-off-by: Marilene A Garcia <marilene.agarcia@gmail.com>

--- drivers/dca/dca-core.c
+++ drivers/dca/dca-core.c
@@ -17,6 +17,7 @@
 #define DCA_VERSION "1.12.1"
 
 MODULE_VERSION(DCA_VERSION);
+MODULE_DESCRIPTION("Intel Direct Cache Access Driver");
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Intel Corporation");
 
@@ -165,7 +166,7 @@ static struct dca_provider *dca_find_provider_by_dev(struct device *dev)
 
 /**
  * dca_add_requester - add a dca client to the list
- * @dev - the device that wants dca service
+ * @dev: the device that wants dca service
  */
 int dca_add_requester(struct device *dev)
 {
@@ -220,7 +221,7 @@ EXPORT_SYMBOL_GPL(dca_add_requester);
 
 /**
  * dca_remove_requester - remove a dca client from the list
- * @dev - the device that wants dca service
+ * @dev: the device that wants dca service
  */
 int dca_remove_requester(struct device *dev)
 {
@@ -251,8 +252,8 @@ EXPORT_SYMBOL_GPL(dca_remove_requester);
 
 /**
  * dca_common_get_tag - return the dca tag (serves both new and old api)
- * @dev - the device that wants dca service
- * @cpu - the cpuid as returned by get_cpu()
+ * @dev: the device that wants dca service
+ * @cpu: the cpuid as returned by get_cpu()
  */
 static u8 dca_common_get_tag(struct device *dev, int cpu)
 {
@@ -276,8 +277,8 @@ static u8 dca_common_get_tag(struct device *dev, int cpu)
 /**
  * dca3_get_tag - return the dca tag to the requester device
  *                for the given cpu (new api)
- * @dev - the device that wants dca service
- * @cpu - the cpuid as returned by get_cpu()
+ * @dev: the device that wants dca service
+ * @cpu: the cpuid as returned by get_cpu()
  */
 u8 dca3_get_tag(struct device *dev, int cpu)
 {
@@ -290,7 +291,7 @@ EXPORT_SYMBOL_GPL(dca3_get_tag);
 
 /**
  * dca_get_tag - return the dca tag for the given cpu (old api)
- * @cpu - the cpuid as returned by get_cpu()
+ * @cpu: the cpuid as returned by get_cpu()
  */
 u8 dca_get_tag(int cpu)
 {
@@ -300,8 +301,8 @@ EXPORT_SYMBOL_GPL(dca_get_tag);
 
 /**
  * alloc_dca_provider - get data struct for describing a dca provider
- * @ops - pointer to struct of dca operation function pointers
- * @priv_size - size of extra mem to be added for provider's needs
+ * @ops: pointer to struct of dca operation function pointers
+ * @priv_size: size of extra mem to be added for provider's needs
  */
 struct dca_provider *alloc_dca_provider(const struct dca_ops *ops,
 					int priv_size)
@@ -321,8 +322,7 @@ EXPORT_SYMBOL_GPL(alloc_dca_provider);
 
 /**
  * free_dca_provider - release the dca provider data struct
- * @ops - pointer to struct of dca operation function pointers
- * @priv_size - size of extra mem to be added for provider's needs
+ * @dca: struct created by alloc_dca_provider()
  */
 void free_dca_provider(struct dca_provider *dca)
 {
@@ -332,8 +332,8 @@ EXPORT_SYMBOL_GPL(free_dca_provider);
 
 /**
  * register_dca_provider - register a dca provider
- * @dca - struct created by alloc_dca_provider()
- * @dev - device providing dca services
+ * @dca: struct created by alloc_dca_provider()
+ * @dev: device providing dca services
  */
 int register_dca_provider(struct dca_provider *dca, struct device *dev)
 {
@@ -390,7 +390,8 @@ EXPORT_SYMBOL_GPL(register_dca_provider);
 
 /**
  * unregister_dca_provider - remove a dca provider
- * @dca - struct created by alloc_dca_provider()
+ * @dca: struct created by alloc_dca_provider()
+ * @dev: device providing dca services
  */
 void unregister_dca_provider(struct dca_provider *dca, struct device *dev)
 {
@@ -423,6 +424,7 @@ EXPORT_SYMBOL_GPL(unregister_dca_provider);
 
 /**
  * dca_register_notify - register a client's notifier callback
+ * @nb: notifier block to register
  */
 void dca_register_notify(struct notifier_block *nb)
 {
@@ -432,6 +434,7 @@ EXPORT_SYMBOL_GPL(dca_register_notify);
 
 /**
  * dca_unregister_notify - remove a client's notifier callback
+ * @nb: notifier block to unregister
  */
 void dca_unregister_notify(struct notifier_block *nb)
 {
