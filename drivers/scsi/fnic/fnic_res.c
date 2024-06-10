4648d7ea32d230b3091432f2fda54e451cca6b93
Karan Tilak Kumar <kartilak@cisco.com>
2024-06-10 14:50:56 -0700

    scsi: fnic: Modify fnic interfaces to use FDLS
    Modify fnic driver interfaces to use FDLS and
    supporting functions.
    Refactor code in fnic_probe and fnic_remove.
    Get fnic from shost_priv.
    Add error handling in stats processing functions.
    Modify some print statements.
    Add support to do module unload cleanup.
    Use placeholder functions/modify function declarations
    to not break compilation.
    
    Reviewed-by: Sesidhar Baddela <sebaddel@cisco.com>
    Reviewed-by: Arulprabhu Ponnusamy <arulponn@cisco.com>
    Reviewed-by: Gian Carlo Boffa <gcboffa@cisco.com>
    Signed-off-by: Karan Tilak Kumar <kartilak@cisco.com>

--- drivers/scsi/fnic/fnic_res.c
+++ drivers/scsi/fnic/fnic_res.c
@@ -58,6 +58,11 @@ int fnic_get_vnic_config(struct fnic *fnic)
 	GET_CONFIG(intr_mode);
 	GET_CONFIG(wq_copy_count);
 
+	if ((c->flags & (VFCF_FC_INITIATOR)) == 0) {
+		pr_info("vNIC role not defined (default role: FC Initiator)\n");
+		c->flags |= VFCF_FC_INITIATOR;
+	}
+
 	c->wq_enet_desc_count =
 		min_t(u32, VNIC_FNIC_WQ_DESCS_MAX,
 		      max_t(u32, VNIC_FNIC_WQ_DESCS_MIN,
@@ -137,29 +142,29 @@ int fnic_get_vnic_config(struct fnic *fnic)
 
 	c->wq_copy_count = min_t(u16, FNIC_WQ_COPY_MAX, c->wq_copy_count);
 
-	pr_info("vNIC MAC addr %pM "
+	pr_info("fNIC MAC addr %p "
 		     "wq/wq_copy/rq %d/%d/%d\n",
-		     fnic->ctlr.ctl_src_addr,
+			fnic->data_src_addr,
 		     c->wq_enet_desc_count, c->wq_copy_desc_count,
 		     c->rq_desc_count);
-	pr_info("vNIC node wwn %llx port wwn %llx\n",
+	pr_info("fNIC node wwn 0x%llx port wwn 0x%llx\n",
 		     c->node_wwn, c->port_wwn);
-	pr_info("vNIC ed_tov %d ra_tov %d\n",
+	pr_info("fNIC ed_tov %d ra_tov %d\n",
 		     c->ed_tov, c->ra_tov);
-	pr_info("vNIC mtu %d intr timer %d\n",
+	pr_info("fNIC mtu %d intr timer %d\n",
 		     c->maxdatafieldsize, c->intr_timer);
-	pr_info("vNIC flags 0x%x luns per tgt %d\n",
+	pr_info("fNIC flags 0x%x luns per tgt %d\n",
 		     c->flags, c->luns_per_tgt);
-	pr_info("vNIC flogi_retries %d flogi timeout %d\n",
+	pr_info("fNIC flogi_retries %d flogi timeout %d\n",
 		     c->flogi_retries, c->flogi_timeout);
-	pr_info("vNIC plogi retries %d plogi timeout %d\n",
+	pr_info("fNIC plogi retries %d plogi timeout %d\n",
 		     c->plogi_retries, c->plogi_timeout);
-	pr_info("vNIC io throttle count %d link dn timeout %d\n",
+	pr_info("fNIC io throttle count %d link dn timeout %d\n",
 		     c->io_throttle_count, c->link_down_timeout);
-	pr_info("vNIC port dn io retries %d port dn timeout %d\n",
+	pr_info("fNIC port dn io retries %d port dn timeout %d\n",
 		     c->port_down_io_retries, c->port_down_timeout);
-	pr_info("vNIC wq_copy_count: %d\n", c->wq_copy_count);
-	pr_info("vNIC intr mode: %d\n", c->intr_mode);
+	pr_info("fNIC wq_copy_count: %d\n", c->wq_copy_count);
+	pr_info("fNIC intr mode: %d\n", c->intr_mode);
 
 	return 0;
 }
