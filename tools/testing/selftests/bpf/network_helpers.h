4935b28a588367d8f7ecf013c98732c3364fc19a
YiFei Zhu <zhuyifei@google.com>
2024-06-11 20:42:45 +0000

    selftests/bpf: Move rxq_num helper from xdp_hw_metadata to network_helpers
    This helper may be useful for other AF_XDP tests, such as xsk_hw.
    Moving it out so we don't need to copy-paste that function.
    
    I also changed the function from directly calling error(1, errno, ...)
    to returning an error because I don't think it makes sense for a
    library function to outright kill the process if the function fails.
    
    Signed-off-by: YiFei Zhu <zhuyifei@google.com>

--- tools/testing/selftests/bpf/network_helpers.h
+++ tools/testing/selftests/bpf/network_helpers.h
@@ -72,6 +72,8 @@ int get_socket_local_port(int sock_fd);
 int get_hw_ring_size(char *ifname, struct ethtool_ringparam *ring_param);
 int set_hw_ring_size(char *ifname, struct ethtool_ringparam *ring_param);
 
+int rxq_num(const char *ifname);
+
 struct nstoken;
 /**
  * open_netns() - Switch to specified network namespace by name.
