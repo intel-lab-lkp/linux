7a93f30e35e69a92ad1b122dc59ecc5ddb1333bd
Jeff Johnson <quic_jjohnson@quicinc.com>
2024-06-11 10:52:54 -0700

    USB: serial: add missing MODULE_DESCRIPTION() macros
    With ARCH=x86, make allmodconfig && make W=1 C=1 reports:
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/usb/serial/ch341.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/usb/serial/usb_debug.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/usb/serial/mxuport.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/usb/serial/navman.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/usb/serial/qcaux.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/usb/serial/usb-serial-simple.o
    WARNING: modpost: missing MODULE_DESCRIPTION() in drivers/usb/serial/symbolserial.o
    
    Add the missing invocations of the MODULE_DESCRIPTION() macro.
    
    Signed-off-by: Jeff Johnson <quic_jjohnson@quicinc.com>

--- drivers/usb/serial/ch341.c
+++ drivers/usb/serial/ch341.c
@@ -863,4 +863,5 @@ static struct usb_serial_driver * const serial_drivers[] = {
 
 module_usb_serial_driver(serial_drivers, id_table);
 
+MODULE_DESCRIPTION("Winchiphead CH341 Single Port Serial Driver");
 MODULE_LICENSE("GPL v2");
