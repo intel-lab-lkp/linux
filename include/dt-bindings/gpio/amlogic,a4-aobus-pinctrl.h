33acf55e2c633a95b34b4961c064b9aeb908ec12
Xianwei Zhao <xianwei.zhao@amlogic.com>
2024-06-11 13:10:57 +0800

    dt-bindings: pinctrl: Add support for Amlogic A4 SoCs
    Add the new compatible name for Amlogic A4 pin controller, and add
    a new dt-binding header file which document the detail pin names.
    
    Signed-off-by: Xianwei Zhao <xianwei.zhao@amlogic.com>

---
+++ include/dt-bindings/gpio/amlogic,a4-aobus-pinctrl.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR MIT) */
+/*
+ * Copyright (c) 2024 Amlogic, Inc. All rights reserved.
+ * Author: Xianwei Zhao <xianwei.zhao@amlogic.com>
+ */
+
+#ifndef _DT_BINDINGS_AMLOGIC_A4_AOBUS_PINCTRL_H
+#define _DT_BINDINGS_AMLOGIC_A4_AOBUS_PINCTRL_H
+
+/* GPIOAO */
+#define GPIOAO_0			0
+#define GPIOAO_1			1
+#define GPIOAO_2			2
+#define GPIOAO_3			3
+#define GPIOAO_4			4
+#define GPIOAO_5			5
+#define GPIOAO_6			6
+
+#define GPIO_TEST_N			7
+
+#endif
