086a8e67a7a879c9a993193faa7f8a3d1960b175
Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
2024-06-12 11:56:27 +0300

    resource: Add resource set range and size helpers
    Setting the end address for a resource with a given size lacks a helper
    and is therefore open coded unlike the getter side which has a helper
    for resource size calculation. Also, almost all callsites that
    calculate end address for a resource also set the start address right
    before it like this:
    
    	res->start = start_addr;
    	res->end = res->start + size - 1;
    
    Thus, add resource_set_range(res, start_addr, size) that sets the start
    address and calculates the end address to simplify this often repeated
    fragment. In addition, introduce resource_set_size() for the cases
    where setting the start address of the resource is not necessary but
    note resource_set_range() is preferred.
    
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- include/linux/ioport.h
+++ include/linux/ioport.h
@@ -249,6 +249,38 @@ struct resource *lookup_resource(struct resource *root, resource_size_t start);
 int adjust_resource(struct resource *res, resource_size_t start,
 		    resource_size_t size);
 resource_size_t resource_alignment(struct resource *res);
+
+/**
+ * resource_set_size - Calculates resource end address from size and start address
+ * @res: The resource descriptor
+ * @size: The size of the resource
+ *
+ * Calculates the end address for @res based on @size.
+ *
+ * Note: The start address of @res must be set when calling this function.
+ * Use resource_set_range() if setting both the start address and @size.
+ */
+static inline void resource_set_size(struct resource *res, resource_size_t size)
+{
+	res->end = res->start + size - 1;
+}
+
+/**
+ * resource_set_range - Sets resource start and end addresses
+ * @res: The resource descriptor
+ * @start: The start address for the resource
+ * @size: The size of the resource
+ *
+ * Sets @res start address and calculates the end address based on @size.
+ */
+static inline void resource_set_range(struct resource *res,
+				      resource_size_t start,
+				      resource_size_t size)
+{
+	res->start = start;
+	resource_set_size(res, size);
+}
+
 static inline resource_size_t resource_size(const struct resource *res)
 {
 	return res->end - res->start + 1;
