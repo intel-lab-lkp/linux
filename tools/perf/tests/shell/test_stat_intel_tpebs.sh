e3829e2ca6d7af84725015f1199264d8bdcdc6b3
Weilin Wang <weilin.wang@intel.com>
2024-06-12 23:36:29 -0400

    perf test: Add test for Intel TPEBS counting mode
    Intel TPEBS sampling mode is supported through perf record. The counting mode
    code uses perf record to capture retire_latency value and use it in metric
    calculation. This test checks the counting mode code.
    
    Signed-off-by: Weilin Wang <weilin.wang@intel.com>

---
+++ tools/perf/tests/shell/test_stat_intel_tpebs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# test Intel TPEBS counting mode
+# SPDX-License-Identifier: GPL-2.0
+
+set e
+
+# Use this event for testing because it should exist in all platforms
+e=cache-misses:R
+
+# Without this cmd option, default value or zero is returned
+echo "Testing without --enable-tpebs-recording"
+result=$(perf stat -e "$e" true 2>&1)
+[[ "$result" =~ "$e" ]] || exit 1
+
+# In platforms that do not support TPEBS, it should execute without error.
+echo "Testing with --enable-tpebs-recording"
+result=$(perf stat -e "$e" --enable-tpebs-recording -a sleep 0.01 2>&1)
+[[ "$result" =~ "perf record" && "$result" =~ "$e" ]] || exit 1
