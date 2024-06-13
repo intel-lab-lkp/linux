96f274fff79f2822163e5d2c3f952acf1fefa0f5
Sasha Neftin <sasha.neftin@intel.com>
2024-06-13 08:17:49 +0300

    igc: Remove the internal 'eee_advert' field
    Since the kernel's 'ethtool_keee' structure is in use, the internal
    'eee_advert' field becomes pointless and can be removed.
    
    This patch comes to clean up this redundant code.
    
    Signed-off-by: Sasha Neftin <sasha.neftin@intel.com>

--- drivers/net/ethernet/intel/igc/igc_main.c
+++ drivers/net/ethernet/intel/igc/igc_main.c
@@ -4976,9 +4976,6 @@ void igc_up(struct igc_adapter *adapter)
 	/* start the watchdog. */
 	hw->mac.get_link_status = true;
 	schedule_work(&adapter->watchdog_task);
-
-	adapter->eee_advert = MDIO_EEE_100TX | MDIO_EEE_1000T |
-			      MDIO_EEE_2_5GT;
 }
 
 /**
