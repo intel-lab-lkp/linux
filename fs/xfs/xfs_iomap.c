9b6202fbfd448f34e602e0e09a29e118b077aeeb
Zhang Yi <yi.zhang@huawei.com>
2024-06-13 17:00:27 +0800

    iomap: pass blocksize to iomap_truncate_page()
    iomap_truncate_page() always assumes the block size of the truncating
    inode is i_blocksize(), this is not always true for some filesystems,
    e.g. XFS does extent size alignment for realtime inodes. Drop this
    assumption and pass the block size for zeroing into
    iomap_truncate_page(), allow filesystems to indicate the correct block
    size.
    
    Suggested-by: Dave Chinner <david@fromorbit.com>
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>

--- fs/xfs/xfs_iomap.c
+++ fs/xfs/xfs_iomap.c
@@ -1471,10 +1471,11 @@ xfs_truncate_page(
 	bool			*did_zero)
 {
 	struct inode		*inode = VFS_I(ip);
+	unsigned int		blocksize = i_blocksize(inode);
 
 	if (IS_DAX(inode))
 		return dax_truncate_page(inode, pos, did_zero,
 					&xfs_dax_write_iomap_ops);
-	return iomap_truncate_page(inode, pos, did_zero,
+	return iomap_truncate_page(inode, pos, blocksize, did_zero,
 				   &xfs_buffered_write_iomap_ops);
 }
