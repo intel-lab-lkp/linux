9b895427f1fd1a0259f4d8e98fb4dcbccd3464da
Yunke Cao <yunkec@chromium.org>
2024-06-14 16:37:01 +0900

    media: videobuf2-core: reverse the iteration order in __vb2_buf_dmabuf_put
    Release the planes from num_planes - 1 to 0.
    
    Signed-off-by: Yunke Cao <yunkec@chromium.org>

--- drivers/media/common/videobuf2/videobuf2-core.c
+++ drivers/media/common/videobuf2/videobuf2-core.c
@@ -324,9 +324,9 @@ static void __vb2_plane_dmabuf_put(struct vb2_buffer *vb, struct vb2_plane *p)
  */
 static void __vb2_buf_dmabuf_put(struct vb2_buffer *vb)
 {
-	unsigned int plane;
+	int plane;
 
-	for (plane = 0; plane < vb->num_planes; ++plane)
+	for (plane = vb->num_planes - 1; plane >= 0; --plane)
 		__vb2_plane_dmabuf_put(vb, &vb->planes[plane]);
 }
 
