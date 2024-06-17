93eee8b08bf1012e3a136b778916283a91340ceb
Basavaraj Natikar <Basavaraj.Natikar@amd.com>
2024-06-17 15:33:54 +0530

    dmaengine: ae4dma: Add AMD ae4dma controller driver
    Add support for AMD AE4DMA controller. It performs high-bandwidth
    memory to memory and IO copy operation. Device commands are managed
    via a circular queue of 'descriptors', each of which specifies source
    and destination addresses for copying a single buffer of data.
    
    Reviewed-by: Raju Rangoju <Raju.Rangoju@amd.com>
    Signed-off-by: Basavaraj Natikar <Basavaraj.Natikar@amd.com>

---
+++ drivers/dma/amd/common/amd_dma.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * AMD DMA Driver common
+ *
+ * Copyright (c) 2024, Advanced Micro Devices, Inc.
+ * All Rights Reserved.
+ *
+ * Author: Basavaraj Natikar <Basavaraj.Natikar@amd.com>
+ */
+
+#ifndef AMD_DMA_H
+#define AMD_DMA_H
+
+#include <linux/device.h>
+#include <linux/dmaengine.h>
+#include <linux/dmapool.h>
+#include <linux/list.h>
+#include <linux/mutex.h>
+#include <linux/pci.h>
+#include <linux/spinlock.h>
+#include <linux/wait.h>
+
+#include "../ptdma/ptdma.h"
+#include "../../virt-dma.h"
+
+#endif
