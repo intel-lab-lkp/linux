1a2cba5c1abb841a5e6c3952d057948b2452c16f
Thomas Hellström <thomas.hellstrom@linux.intel.com>
2024-06-18 09:18:13 +0200

    drm/ttm: Provide a generic LRU walker helper
    Provide a generic LRU walker in TTM, in the spirit of drm_gem_lru_scan()
    but building on the restartable TTM LRU functionality.
    
    The LRU walker optionally supports locking objects as part of
    a ww mutex locking transaction, to mimic to some extent the
    current functionality in ttm. However any -EDEADLK return
    is converted to -ENOMEM, so that the driver will need to back
    off and possibly retry without being able to keep the
    ticket.
    
    v3:
    - Move the helper to core ttm.
    - Remove the drm_exec usage from it for now, it will be
      reintroduced later in the series.
    v4:
    - Handle the -EALREADY case if ticketlocking.
    
    Cc: Christian König <christian.koenig@amd.com>
    Cc: Somalapuram Amaranath <Amaranath.Somalapuram@amd.com>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: <dri-devel@lists.freedesktop.org>
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>

--- include/drm/ttm/ttm_bo.h
+++ include/drm/ttm/ttm_bo.h
@@ -190,6 +190,38 @@ struct ttm_operation_ctx {
 	uint64_t bytes_moved;
 };
 
+struct ttm_lru_walk;
+
+/** struct ttm_lru_walk_ops - Operations for a LRU walk. */
+struct ttm_lru_walk_ops {
+	/**
+	 * process_bo - Process this bo.
+	 * @walk: struct ttm_lru_walk describing the walk.
+	 * @bo: A locked and referenced buffer object.
+	 *
+	 * Return: Negative error code on error, Number of processed pages on
+	 * success. 0 also indicates success.
+	 */
+	long (*process_bo)(struct ttm_lru_walk *walk, struct ttm_buffer_object *bo);
+};
+
+/**
+ * struct ttm_lru_walk - Structure describing a LRU walk.
+ */
+struct ttm_lru_walk {
+	/** @ops: Pointer to the ops structure. */
+	const struct ttm_lru_walk_ops *ops;
+	/** @ctx: Pointer to the struct ttm_operation_ctx. */
+	struct ttm_operation_ctx *ctx;
+	/** @ticket: The struct ww_acquire_ctx if any. */
+	struct ww_acquire_ctx *ticket;
+	/** @tryock_only: Only use trylock for locking. */
+	bool trylock_only;
+};
+
+long ttm_lru_walk_for_evict(struct ttm_lru_walk *walk, struct ttm_device *bdev,
+			    struct ttm_resource_manager *man, long target);
+
 /**
  * ttm_bo_get - reference a struct ttm_buffer_object
  *
