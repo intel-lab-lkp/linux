ffe05109c7e8ae555ffabd11cc18ab74b2c17a73
Niklas Cassel <cassel@kernel.org>
2024-06-18 17:35:41 +0200

    ata: libata: Assign print_id at port allocation time
    While the assignment of ap->print_id could have been moved to
    ata_host_alloc(), let's simply move it to ata_port_alloc().
    
    If you allocate a port, you want to give it a unique name that can be used
    for printing.
    
    By moving the ap->print_id assignment to ata_port_alloc(), means that we
    can also remove the ap->print_id assignment from ata_sas_port_alloc().
    
    This will allow a LLD to use the ata_port_*() print functions before
    ata_host_register() has been called.
    
    Signed-off-by: Niklas Cassel <cassel@kernel.org>

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -5463,7 +5463,7 @@ struct ata_port *ata_port_alloc(struct ata_host *host)
 
 	ap->pflags |= ATA_PFLAG_INITIALIZING | ATA_PFLAG_FROZEN;
 	ap->lock = &host->lock;
-	ap->print_id = -1;
+	ap->print_id = atomic_inc_return(&ata_print_id);
 	ap->local_port_no = -1;
 	ap->host = host;
 	ap->dev = host->dev;
@@ -5903,10 +5903,8 @@ int ata_host_register(struct ata_host *host, const struct scsi_host_template *sh
 		WARN_ON(host->ports[i]);
 
 	/* give ports names and add SCSI hosts */
-	for (i = 0; i < host->n_ports; i++) {
-		host->ports[i]->print_id = atomic_inc_return(&ata_print_id);
+	for (i = 0; i < host->n_ports; i++)
 		host->ports[i]->local_port_no = i + 1;
-	}
 
 	/* Create associated sysfs transport objects  */
 	for (i = 0; i < host->n_ports; i++) {
