61b63a350457e85a4f12ee653d57ef15e5ad4f8c
Madadi Vineeth Reddy <vineethr@linux.ibm.com>
2024-06-18 16:59:07 +0530

    perf sched replay: Fix -r/--repeat command line option for infinity
    Currently, the -r/--repeat option accepts values from 0 and complains
    for -1. The help section specifies:
    -r, --repeat <n>      repeat the workload replay N times (-1: infinite)
    
    The -r -1 option raises an error because replay_repeat is defined as
    an unsigned int.
    
    In the current implementation, the workload is repeated n times when
    -r <n> is used, except when n is 0.
    
    When -r is set to 0, the workload is also repeated once. This happens
    because when -r=0, the run_one_test function is not called. (Note that
    mutex unlocking, which is essential for child threads spawned to emulate
    the workload, happens in run_one_test.) However, mutex unlocking is
    still performed in the destroy_tasks function. Thus, -r=0 results in the
    workload running once coincidentally.
    
    To clarify and maintain the existing logic for -r >= 1 (which runs the
    workload the specified number of times) and to fix the issue with infinite
    runs, make -r=0 perform an infinite run.
    
    Signed-off-by: Madadi Vineeth Reddy <vineethr@linux.ibm.com>

--- tools/perf/builtin-sched.c
+++ tools/perf/builtin-sched.c
@@ -3383,8 +3383,12 @@ static int perf_sched__replay(struct perf_sched *sched)
 	sched->thread_funcs_exit = false;
 	create_tasks(sched);
 	printf("------------------------------------------------------------\n");
-	for (i = 0; i < sched->replay_repeat; i++)
+
+	i = 0;
+	while (sched->replay_repeat == 0 || i < sched->replay_repeat) {
 		run_one_test(sched);
+		i++;
+	}
 
 	sched->thread_funcs_exit = true;
 	destroy_tasks(sched);
@@ -3548,7 +3552,7 @@ int cmd_sched(int argc, const char **argv)
 	};
 	const struct option replay_options[] = {
 	OPT_UINTEGER('r', "repeat", &sched.replay_repeat,
-		     "repeat the workload replay N times (-1: infinite)"),
+		     "repeat the workload replay N times (0: infinite)"),
 	OPT_PARENT(sched_options)
 	};
 	const struct option map_options[] = {
