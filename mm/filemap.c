d4e1bebfd2c2b0dd5d4aaa123ee7139164a23a82
Takaya Saeki <takayas@chromium.org>
2024-06-18 09:36:56 +0000

    filemap: add trace events for get_pages, map_pages, and fault
    To allow precise tracking of page caches accessed, add new tracepoints
    that trigger when a process actually accesses them.
    
    The ureadahead program used by ChromeOS traces the disk access of
    programs as they start up at boot up. It uses mincore(2) or the
    'mm_filemap_add_to_page_cache' trace event to accomplish this. It stores
    this information in a "pack" file and on subsequent boots, it will read
    the pack file and call readahead(2) on the information so that disk
    storage can be loaded into RAM before the applications actually need it.
    
    A problem we see is that due to the kernel's readahead algorithm that
    can aggressively pull in more data than needed (to try and accomplish
    the same goal) and this data is also recorded. The end result is that
    the pack file contains a lot of pages on disk that are never actually
    used. Calling readahead(2) on these unused pages can slow down the
    system boot up times.
    
    To solve this, add 3 new trace events, get_pages, map_pages, and fault.
    These will be used to trace the pages are not only pulled in from disk,
    but are actually used by the application. Only those pages will be
    stored in the pack file, and this helps out the performance of boot up.
    
    With the combination of these 3 new trace events and
    mm_filemap_add_to_page_cache, we observed a reduction in the pack file
    by 7.3% - 20% on ChromeOS varying by device.
    
    Signed-off-by: Takaya Saeki <takayas@chromium.org>

--- mm/filemap.c
+++ mm/filemap.c
@@ -2556,6 +2556,7 @@ retry:
 			goto err;
 	}
 
+	trace_mm_filemap_get_pages(mapping, index, last_index);
 	return 0;
 err:
 	if (err < 0)
@@ -3287,6 +3288,8 @@ vm_fault_t filemap_fault(struct vm_fault *vmf)
 	if (unlikely(index >= max_idx))
 		return VM_FAULT_SIGBUS;
 
+	trace_mm_filemap_fault(mapping, index);
+
 	/*
 	 * Do we have something in the page cache already?
 	 */
@@ -3653,6 +3656,7 @@ vm_fault_t filemap_map_pages(struct vm_fault *vmf,
 	} while ((folio = next_uptodate_folio(&xas, mapping, end_pgoff)) != NULL);
 	add_mm_counter(vma->vm_mm, folio_type, rss);
 	pte_unmap_unlock(vmf->pte, vmf->ptl);
+	trace_mm_filemap_map_pages(mapping, start_pgoff, end_pgoff);
 out:
 	rcu_read_unlock();
 
