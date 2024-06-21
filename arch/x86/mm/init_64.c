2b6003399a60282f467170fdbf6f809fc3363367
Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
2024-06-21 19:44:05 +0300

    x86/64/mm: Make SPARSEMEM_VMEMMAP the only memory model
    5-level paging only supports SPARSEMEM_VMEMMAP. CONFIG_X86_5LEVEL is
    being phased out, making 5-level paging support mandatory.
    
    Make CONFIG_SPARSEMEM_VMEMMAP mandatory for x86-64 and eliminate
    any associated conditional statements.
    
    Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>

--- arch/x86/mm/init_64.c
+++ arch/x86/mm/init_64.c
@@ -827,7 +827,6 @@ void __init paging_init(void)
 	zone_sizes_init();
 }
 
-#ifdef CONFIG_SPARSEMEM_VMEMMAP
 #define PAGE_UNUSED 0xFD
 
 /*
@@ -926,7 +925,6 @@ static void __meminit vmemmap_use_new_sub_pmd(unsigned long start, unsigned long
 	if (!IS_ALIGNED(end, PMD_SIZE))
 		unused_pmd_start = end;
 }
-#endif
 
 /*
  * Memory hotplug specific functions
@@ -1125,16 +1123,13 @@ remove_pmd_table(pmd_t *pmd_start, unsigned long addr, unsigned long end,
 				pmd_clear(pmd);
 				spin_unlock(&init_mm.page_table_lock);
 				pages++;
-			}
-#ifdef CONFIG_SPARSEMEM_VMEMMAP
-			else if (vmemmap_pmd_is_unused(addr, next)) {
+			} else if (vmemmap_pmd_is_unused(addr, next)) {
 					free_hugepage_table(pmd_page(*pmd),
 							    altmap);
 					spin_lock(&init_mm.page_table_lock);
 					pmd_clear(pmd);
 					spin_unlock(&init_mm.page_table_lock);
 			}
-#endif
 			continue;
 		}
 
@@ -1482,7 +1477,6 @@ unsigned long memory_block_size_bytes(void)
 	return memory_block_size_probed;
 }
 
-#ifdef CONFIG_SPARSEMEM_VMEMMAP
 /*
  * Initialise the sparsemem vmemmap using huge-pages at the PMD level.
  */
@@ -1631,4 +1625,3 @@ void __meminit vmemmap_populate_print_last(void)
 		node_start = 0;
 	}
 }
-#endif
