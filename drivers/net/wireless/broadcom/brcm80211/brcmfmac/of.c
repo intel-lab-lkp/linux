c07cf3fc77b70702b5e25c944f6789ec2135006f
Alex Bee <knaerzche@gmail.com>
2024-06-22 00:55:58 +0200

    wifi: brcmfmac: of: Support interrupts-extended
    This "new" version of defining external interrupts is around for a very
    long time now and supported and preferred by irq_of_parse_and_map
    respectively of_irq_parse_one.
    
    Support it in brcmfmac as well by checking if either "interrupts" or
    "interrupts-extended" property exists as indication if irq_of_parse_and_map
    should be called.
    
    Signed-off-by: Alex Bee <knaerzche@gmail.com>

--- drivers/net/wireless/broadcom/brcm80211/brcmfmac/of.c
+++ drivers/net/wireless/broadcom/brcm80211/brcmfmac/of.c
@@ -129,7 +129,8 @@ void brcmf_of_probe(struct device *dev, enum brcmf_bus_type bus_type,
 		sdio->drive_strength = val;
 
 	/* make sure there are interrupts defined in the node */
-	if (!of_property_present(np, "interrupts"))
+	if (!of_property_present(np, "interrupts") &&
+	    !of_property_present(np, "interrupts-extended"))
 		return;
 
 	irq = irq_of_parse_and_map(np, 0);
