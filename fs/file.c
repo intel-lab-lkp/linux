ffc9c894d2b6870eb6f3736d5b8c517a2ee1efd8
Yu Ma <yu.ma@intel.com>
2024-06-22 11:49:04 -0400

    fs/file.c: remove sanity_check from alloc_fd()
    alloc_fd() has a sanity check inside to make sure the struct file mapping to the
    allocated fd is NULL. Remove this sanity check since it can be assured by
    exisitng zero initilization and NULL set when recycling fd.
    
    Combined with patch 1 and 2 in series, pts/blogbench-1.1.0 read improved by
    32%, write improved by 17% on Intel ICX 160 cores configuration with v6.10-rc4.
    
    Reviewed-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Yu Ma <yu.ma@intel.com>

--- fs/file.c
+++ fs/file.c
@@ -555,13 +555,6 @@ fastreturn:
 	else
 		__clear_close_on_exec(fd, fdt);
 	error = fd;
-#if 1
-	/* Sanity check */
-	if (rcu_access_pointer(fdt->fd[fd]) != NULL) {
-		printk(KERN_WARNING "alloc_fd: slot %d not NULL!\n", fd);
-		rcu_assign_pointer(fdt->fd[fd], NULL);
-	}
-#endif
 
 out:
 	spin_unlock(&files->file_lock);
