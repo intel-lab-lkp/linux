5325b7bd54d3734b131662a2e19cc32f1df6f7e9
Chao Yu <chao@kernel.org>
2024-06-25 11:13:50 +0800

    f2fs: atomic: fix to truncate pagecache before on-disk metadata truncation
    We should always truncate pagecache while truncating on-disk data.
    
    Fixes: a46bebd502fe ("f2fs: synchronize atomic write aborts")
    Signed-off-by: Chao Yu <chao@kernel.org>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -2185,6 +2185,7 @@ static int f2fs_ioc_start_atomic_write(struct file *filp, bool truncate)
 		clear_inode_flag(fi->cow_inode, FI_INLINE_DATA);
 	} else {
 		/* Reuse the already created COW inode */
+		truncate_setsize(inode, 0);
 		ret = f2fs_do_truncate_blocks(fi->cow_inode, 0, true);
 		if (ret) {
 			f2fs_up_write(&fi->i_gc_rwsem[WRITE]);
