623d0724de78d9c6e04249c1ce8047ce8bf9ea2b
Kan Liang <kan.liang@linux.intel.com>
2024-06-25 11:22:47 -0700

    perf/x86/intel: Rename model-specific pebs_latency_data functions
    The model-specific pebs_latency_data functions of ADL and MTL use the
    "small" as a postfix to indicate the e-core. The postfix is too generic
    for a model-specific function. It cannot provide useful information that
    can directly map it to a specific uarch, which can facilitate the
    development and maintenance.
    Use the abbr of the uarch to rename the model-specific functions.
    
    Suggested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>

--- arch/x86/events/perf_event.h
+++ arch/x86/events/perf_event.h
@@ -1548,9 +1548,9 @@ void intel_pmu_disable_bts(void);
 
 int intel_pmu_drain_bts_buffer(void);
 
-u64 adl_latency_data_small(struct perf_event *event, u64 status);
+u64 adl_latency_data_grt(struct perf_event *event, u64 status);
 
-u64 mtl_latency_data_small(struct perf_event *event, u64 status);
+u64 mtl_latency_data_cmt(struct perf_event *event, u64 status);
 
 extern struct event_constraint intel_core2_pebs_event_constraints[];
 
