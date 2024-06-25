cdb796414464260c066c9260dc59e5d5e79d4d70
Andrii Nakryiko <andrii@kernel.org>
2024-06-24 17:21:34 -0700

    uprobes: grab write mmap lock in unapply_uprobe()
    Given unapply_uprobe() can call remove_breakpoint() which eventually
    calls uprobe_write_opcode(), which can modify a set of memory pages and
    expects mm->mmap_lock held for write, it needs to have writer lock.
    
    Fix this by switching to mmap_write_lock()/mmap_write_unlock().
    
    Fixes: da1816b1caec ("uprobes: Teach handler_chain() to filter out the probed task")
    Signed-off-by: Andrii Nakryiko <andrii@kernel.org>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -1235,7 +1235,7 @@ static int unapply_uprobe(struct uprobe *uprobe, struct mm_struct *mm)
 	struct vm_area_struct *vma;
 	int err = 0;
 
-	mmap_read_lock(mm);
+	mmap_write_lock(mm);
 	for_each_vma(vmi, vma) {
 		unsigned long vaddr;
 		loff_t offset;
@@ -1252,7 +1252,7 @@ static int unapply_uprobe(struct uprobe *uprobe, struct mm_struct *mm)
 		vaddr = offset_to_vaddr(vma, uprobe->offset);
 		err |= remove_breakpoint(uprobe, mm, vaddr);
 	}
-	mmap_read_unlock(mm);
+	mmap_write_unlock(mm);
 
 	return err;
 }
