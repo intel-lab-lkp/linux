2678f5508417c31f2621c30d20ec88d051e9cf08
Amit Cohen <amcohen@nvidia.com>
2024-06-25 15:08:06 +0300

    mlxsw: pci: Allow get page pool info/stats via netlink
    Spectrum ASICs do not have queue per netdevice, so mlxsw driver does not
    have NAPI per netdevice, instead "dummy" netdevice is used. Lately, the
    driver started using page pool for buffers allocations, each Rx queue (RDQ)
    uses a dedicated page pool.
    
    To allow user to query page pool info and statistics, page pool should
    be attached to netdevice. Setting "dummy" netdevice as part of page pool
    parameters allows querying info about specific pool.
    
    Without this patch, "do" commands fail:
    $ ./tools/net/ynl/cli.py --spec Documentation/netlink/specs/netdev.yaml \
    	--do page-pool-get --json '{"id" : "20"}' --output-json
    Netlink error: No such file or directory
    nl_len = 36 (20) nl_flags = 0x100 nl_type = 2
    	error: -2
    
    With this patch, user can query info of specific pool:
    $ ./tools/net/ynl/cli.py --spec Documentation/netlink/specs/netdev.yaml \
    	--do page-pool-get --json '{"id" : "20"}' --output-json
    {"id": 20, "ifindex": 0, "napi-id": 108, "inflight": 3072, "inflight-mem": 12582912}
    
    Note that this behavior works only in case that the devlink instance in the
    initial namespace, in case that the devlink instance is reloaded to
    another namesapce, get command will fail as the dummy netdevice associated with
    the pools belongs to the initial namespace.
    
    $ ip netns add pp_test
    $ devlink dev reload pci/0000:xx:00.0 netns pp_test
    $ ip netns exec pp_test ./tools/net/ynl/cli.py \
    	--spec Documentation/netlink/specs/netdev.yaml \
    	--do page-pool-stats-get --json '{"info" : {"id" : "20"}}' --output-json
    Netlink error: No such file or directory
    nl_len = 36 (20) nl_flags = 0x100 nl_type = 2
    	error: -2
    
    A next patch will allow user use "dump" command also.
    
    Signed-off-by: Amit Cohen <amcohen@nvidia.com>

--- drivers/net/ethernet/mellanox/mlxsw/pci.c
+++ drivers/net/ethernet/mellanox/mlxsw/pci.c
@@ -892,6 +892,7 @@ static int mlxsw_pci_cq_page_pool_init(struct mlxsw_pci_queue *q,
 	pp_params.dev = &mlxsw_pci->pdev->dev;
 	pp_params.napi = &q->u.cq.napi;
 	pp_params.dma_dir = DMA_FROM_DEVICE;
+	pp_params.netdev = q->pci->napi_dev_rx;
 
 	page_pool = page_pool_create(&pp_params);
 	if (IS_ERR(page_pool))
