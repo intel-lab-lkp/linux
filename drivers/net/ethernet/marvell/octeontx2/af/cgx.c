47c8261256ba2766dcdc1125cc00102c295489f5
Suman Ghosh <sumang@marvell.com>
2024-06-25 23:03:43 +0530

    octeontx2-af: Fix klockwork issue in cgx.c
    Variable "cgx_dev" and "lmac" was getting accessed without NULL checks
    which can lead to pointer exception in some erroneous scenarios.
    This patch fixes the same by adding the required NULL checks.
    
    Fixes: 96be2e0da85e ("octeontx2-af: Support for MAC address filters in CGX")
    Signed-off-by: Suman Ghosh <sumang@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/af/cgx.c
+++ drivers/net/ethernet/marvell/octeontx2/af/cgx.c
@@ -465,6 +465,13 @@ u64 cgx_lmac_addr_get(u8 cgx_id, u8 lmac_id)
 	u64 cfg;
 	int id;
 
+	if (!cgx_dev)
+		return 0;
+
+	lmac = lmac_pdata(lmac_id, cgx_dev);
+	if (!lmac)
+		return 0;
+
 	mac_ops = cgx_dev->mac_ops;
 
 	id = get_sequence_id_of_lmac(cgx_dev, lmac_id);
