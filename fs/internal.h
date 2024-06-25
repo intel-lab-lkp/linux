23489f6bfb90603c44f39fd60e3303b1647b5790
Mateusz Guzik <mjguzik@gmail.com>
2024-06-25 17:18:06 +0200

    vfs: support statx(..., NULL, AT_EMPTY_PATH, ...)
    The newly used helper also checks for empty ("") paths.
    
    NULL paths with any flag value other than AT_EMPTY_PATH go the usual
    route and end up with -EFAULT to retain compatibility (Rust is abusing
    calls of the sort to detect availability of statx).
    
    This avoids path lookup code, lockref management, memory allocation and
    in case of NULL path userspace memory access (which can be quite
    expensive with SMAP on x86_64).
    
    Benchmarked with statx(..., AT_EMPTY_PATH, ...) running on Sapphire
    Rapids, with the "" path for the first two cases and NULL for the last
    one.
    
    Results in ops/s:
    stock:     4231237
    pre-check: 5944063 (+40%)
    NULL path: 6601619 (+11%/+56%)
    
    Signed-off-by: Mateusz Guzik <mjguzik@gmail.com>

--- fs/internal.h
+++ fs/internal.h
@@ -247,6 +247,8 @@ extern const struct dentry_operations ns_dentry_operations;
 int getname_statx_lookup_flags(int flags);
 int do_statx(int dfd, struct filename *filename, unsigned int flags,
 	     unsigned int mask, struct statx __user *buffer);
+int do_statx_fd(int fd, unsigned int flags, unsigned int mask,
+		struct statx __user *buffer);
 
 /*
  * fs/splice.c:
