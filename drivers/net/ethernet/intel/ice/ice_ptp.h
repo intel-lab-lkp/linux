e21775aec8cf1679d3890df652b8e4d9ce77be32
Sergey Temerkhanov <sergey.temerkhanov@intel.com>
2024-06-26 14:54:55 +0200

    ice: Use ice_adapter for PTP shared data instead of auxdev
    Use struct ice_adapter to hold shared PTP data and control PTP
    related actions instead of auxbus. This allows significant code
    simplification and faster access to the container fields used in
    the PTP support code.
    
    Move the PTP port list to the ice_adapter container to simplify
    the code and avoid race conditions which could occur due to the
    synchronous nature of the initialization/access and
    certain memory saving can be achieved by moving PTP data into
    the ice_adapter itself.
    
    Reviewed-by: Przemek Kitszel <przemyslaw.kitszel@intel.com>
    Signed-off-by: Sergey Temerkhanov <sergey.temerkhanov@intel.com>

--- drivers/net/ethernet/intel/ice/ice_ptp.h
+++ drivers/net/ethernet/intel/ice/ice_ptp.h
@@ -170,6 +170,7 @@ struct ice_ptp_tx {
  * and determine when the port's PHY offset is valid.
  *
  * @list_member: list member structure of auxiliary device
+ * @list_node: list member structure
  * @tx: Tx timestamp tracking for this port
  * @aux_dev: auxiliary device associated with this port
  * @ov_work: delayed work task for tracking when PHY offset is valid
@@ -180,6 +181,7 @@ struct ice_ptp_tx {
  */
 struct ice_ptp_port {
 	struct list_head list_member;
+	struct list_head list_node;
 	struct ice_ptp_tx tx;
 	struct auxiliary_device aux_dev;
 	struct kthread_delayed_work ov_work;
@@ -205,6 +207,7 @@ enum ice_ptp_tx_interrupt {
  * @ports: list of porst handled by this port owner
  * @lock: protect access to ports list
  */
+
 struct ice_ptp_port_owner {
 	struct auxiliary_driver aux_driver;
 	struct list_head ports;
