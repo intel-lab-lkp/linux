7962bbb72db7c19f033da6161cec2f001d260f02
Daisuke Nojiri <dnojiri@chromium.org>
2024-06-27 16:53:07 -0700

    cros_ec_proto: Consolidate ec_response_get_next_event
    Consolidate struct ec_response_get_next_event_v*.
    
    Let X->Y indicate kernel X sending EC_CMD_GET_NEXT_EVENT to FW Y.
    
    Old->New:
       Existing kernels send a smaller container (e.g.
       ec_response_get_next_data) which may or may not fit the last few
       bytes. The FW copies as many bytes as possible to the container. The
       kernel processes as many leading bytes as it can understand.
    
    New->Old:
       New kernels send a bigger container. Existing FW copies as many bytes
       as it wants, leaving the last few bytes empty. The kernel knows it
       didn't receive full size data from the returned data length.
    
    Signed-off-by: Daisuke Nojiri <dnojiri@chromium.org>

--- drivers/platform/chrome/cros_ec_proto.c
+++ drivers/platform/chrome/cros_ec_proto.c
@@ -686,7 +686,7 @@ EXPORT_SYMBOL(cros_ec_cmd_xfer_status);
 
 static int get_next_event_xfer(struct cros_ec_device *ec_dev,
 			       struct cros_ec_command *msg,
-			       struct ec_response_get_next_event_v1 *event,
+			       struct ec_response_get_next_event *event,
 			       int version, uint32_t size)
 {
 	int ret;
@@ -709,11 +709,11 @@ static int get_next_event(struct cros_ec_device *ec_dev)
 {
 	struct {
 		struct cros_ec_command msg;
-		struct ec_response_get_next_event_v1 event;
+		struct ec_response_get_next_event event;
 	} __packed buf;
 	struct cros_ec_command *msg = &buf.msg;
-	struct ec_response_get_next_event_v1 *event = &buf.event;
-	const int cmd_version = ec_dev->mkbp_event_supported - 1;
+	struct ec_response_get_next_event *event = &buf.event;
+	int cmd_version = ec_dev->mkbp_event_supported - 1;
 
 	memset(msg, 0, sizeof(*msg));
 	if (ec_dev->suspended) {
@@ -721,12 +721,12 @@ static int get_next_event(struct cros_ec_device *ec_dev)
 		return -EHOSTDOWN;
 	}
 
-	if (cmd_version == 0)
-		return get_next_event_xfer(ec_dev, msg, event, 0,
-				  sizeof(struct ec_response_get_next_event));
+	/* The max version we support is v3 */
+	if (cmd_version > 3)
+		cmd_version = 3;
 
 	return get_next_event_xfer(ec_dev, msg, event, cmd_version,
-				sizeof(struct ec_response_get_next_event_v1));
+				   sizeof(*event));
 }
 
 static int get_keyboard_state_event(struct cros_ec_device *ec_dev)
