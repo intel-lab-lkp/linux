690326ded78ed8978ded8c22a83b8ae56ac4e8cf
Xavier <xavier_qy@163.com>
2024-06-28 01:21:55 +0800

    RT SCHED: Optimize the enqueue and dequeue operations for rt_se
    This patch optimizes the enqueue and dequeue of rt_se, the strategy employs
    a bottom-up removal approach. Specifically, when removing an rt_se at a
    certain level, if it is determined that the highest priority of the rq
    associated with that rt_se has not changed, there is no need to continue
    removing rt_se at higher levels. At this point, only the total number
    of removed rt_se needs to be recorded, and the rt_nr_running count of
    higher-level rq should be removed accordingly.
    
    Signed-off-by: Xavier <xavier_qy@163.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2923,6 +2923,7 @@ extern void print_rt_stats(struct seq_file *m, int cpu);
 extern void print_dl_stats(struct seq_file *m, int cpu);
 extern void print_cfs_rq(struct seq_file *m, int cpu, struct cfs_rq *cfs_rq);
 extern void print_rt_rq(struct seq_file *m, int cpu, struct rt_rq *rt_rq);
+extern void print_rt_rq_task(struct seq_file *m, struct rt_rq *rt_rq);
 extern void print_dl_rq(struct seq_file *m, int cpu, struct dl_rq *dl_rq);
 
 extern void resched_latency_warn(int cpu, u64 latency);
