772a553cb4d921ba968ecae263cc94ccf7b222ce
Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
2024-06-28 15:21:37 +0530

    wifi: ath12k: Introduce device group abstraction
    Currently, single device is probed and once firmware is ready, the device
    is registered to mac80211. For multi-link operation, different bands of
    different devices or same device would be part of a single wiphy and for
    this, hardware device group abstraction would be helpful.
    
    Hardware device group abstraction - when there are multiple devices (with
    single radio or dual radio) that are connected by any means of interface
    for communicating between them, then these devices can be combined
    together as a single group using a group id to form a group abstraction
    and register to mac80211.
    
    The grouping information of multiple devices would be based on device tree
    during device probe. If no such information is available then a single
    device will be part of group abstraction and registered to mac80211 else
    multiple devices advertised in device tree are combined and then registered
    to mac80211.
    
    For device group abstraction, a base structure named ath12k_hw_group (ag)
    and the following helpers are introduced:
            ath12k_core_hw_group_alloc()    : allocate ath12k_hw_group (ag)
                                              based on group id and number
                                              of devices that are going to
                                              be part of this group.
            ath12k_core_hw_group_free()     : free ag during deinit.
            ath12k_core_assign_hw_group()   : assign/map the details of group
                                              to ath12k_base (ab).
            ath12k_core_unassign_hw_group() : unassign/unmap the details of ag
                                              in ath12k_base (ab).
            ath12k_core_hw_group_create()   : create the devices which are part
                                              of group (ag).
            ath12k_core_hw_group_destroy()  : cleanup the devices in ag
    
    These helpers are used during device probe and mapping the group to the
    devices involved.
    
    Please find the illustration of how multiple devices might be combined
    together in future based on group id.
    
                    Grouping of multiple devices (in future)
    
    +------------------------------------------------------------------------+
    |  +-------------------------------------+       +-------------------+   |
    |  |   +-----------+ | | +-----------+   |       |   +-----------+   |   |
    |  |   | ar (2GHz) | | | | ar (5GHz) |   |       |   | ar (6GHz) |   |   |
    |  |   +-----------+ | | +-----------+   |       |   +-----------+   |   |
    |  |          ath12k_base (ab)           |       | ath12k_base (ab)  |   |
    |  |         (Dual band device)          |       |                   |   |
    |  +-------------------------------------+       +-------------------+   |
    |                 ath12k_hw_group (ag) based on group id                 |
    +------------------------------------------------------------------------+
    
    In the above representation, two devices are combined into single group
    based on group id.
    
    Add base code changes where single device would be part of a group with an
    invalid group id forming an group abstraction. Multi device grouping will
    be introduced in future.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.0.1-00029-QCAHKSWPL_SILICONZ-1
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0-03427-QCAHMTSWPL_V1.0_V2.0_SILICONZ-1.15378.4
    
    Signed-off-by: Karthikeyan Periyasamy <quic_periyasa@quicinc.com>
    Co-developed-by: Harshitha Prem <quic_hprem@quicinc.com>
    Signed-off-by: Harshitha Prem <quic_hprem@quicinc.com>

--- drivers/net/wireless/ath/ath12k/core.h
+++ drivers/net/wireless/ath/ath12k/core.h
@@ -61,6 +61,10 @@
 #define ATH12K_RECONFIGURE_TIMEOUT_HZ		(10 * HZ)
 #define ATH12K_RECOVER_START_TIMEOUT_HZ		(20 * HZ)
 
+#define ATH12K_MAX_SOCS 3
+#define ATH12K_INVALID_GROUP_ID  0xFF
+#define ATH12K_INVALID_DEVICE_ID 0xFF
+
 enum ath12k_bdf_search {
 	ATH12K_BDF_SEARCH_DEFAULT,
 	ATH12K_BDF_SEARCH_BUS_AND_BOARD,
@@ -740,6 +744,25 @@ struct ath12k_soc_dp_stats {
 	struct ath12k_soc_dp_tx_err_stats tx_err;
 };
 
+/* Holds info on the group of devices that are registered as a single wiphy */
+struct ath12k_hw_group {
+	struct list_head list;
+	u8 id;
+	u8 num_devices;
+	u8 num_probed;
+	struct ath12k_base *ab[ATH12K_MAX_SOCS];
+
+	/* When multiple devices are involved in a group, QMI handshakes would be
+	 * asynchronous between them but the group has to exchange few information
+	 * in QMI/WMI such as partner device details only after all the devices in
+	 * the group are ready.
+	 *
+	 * To synchronize the device group's create, assign, start, stop below
+	 * lock would be helpful
+	 */
+	struct mutex mutex_lock;
+};
+
 /**
  * enum ath12k_link_capable_flags - link capable flags
  *
@@ -944,6 +967,8 @@ struct ath12k_base {
 
 	struct notifier_block panic_nb;
 
+	struct ath12k_hw_group *ag;
+
 	/* must be last */
 	u8 drv_priv[] __aligned(sizeof(void *));
 };
@@ -974,6 +999,7 @@ int ath12k_core_resume_early(struct ath12k_base *ab);
 int ath12k_core_resume(struct ath12k_base *ab);
 int ath12k_core_suspend(struct ath12k_base *ab);
 int ath12k_core_suspend_late(struct ath12k_base *ab);
+void ath12k_core_unassign_hw_group(struct ath12k_base *ab);
 
 const struct firmware *ath12k_core_firmware_request(struct ath12k_base *ab,
 						    const char *filename);
