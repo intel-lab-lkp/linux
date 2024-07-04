210e3d85dcc075d42208fcaad150f7996139a0c0
Liam R. Howlett <Liam.Howlett@Oracle.com>
2024-07-04 14:27:17 -0400

    mm/mmap: Use vms accounted pages in mmap_region()
    Change from nr_pages variable to vms.nr_accounted for the charged pages
    calculation.  This is necessary for a future patch.
    
    This also avoids checking security_vm_enough_memory_mm() if the amount
    of memory won't change.
    
    Signed-off-by: Liam R. Howlett <Liam.Howlett@Oracle.com>
    Cc: Kees Cook <kees@kernel.org>

--- mm/mmap.c
+++ mm/mmap.c
@@ -2980,6 +2980,7 @@ unsigned long mmap_region(struct file *file, unsigned long addr,
 	} else {
 		/* Minimal setup of vms */
 		vms.nr_pages = 0;
+		vms.nr_accounted = 0;
 		next = vma_next(&vmi);
 		prev = vma_prev(&vmi);
 		if (prev)
@@ -2991,9 +2992,10 @@ unsigned long mmap_region(struct file *file, unsigned long addr,
 	 */
 	if (accountable_mapping(file, vm_flags)) {
 		charged = pglen;
-		charged -= nr_accounted;
-		if (security_vm_enough_memory_mm(mm, charged))
+		charged -= vms.nr_accounted;
+		if (charged && security_vm_enough_memory_mm(mm, charged))
 			goto abort_munmap;
+
 		vms.nr_accounted = 0;
 		vm_flags |= VM_ACCOUNT;
 	}
