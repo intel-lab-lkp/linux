4a9bd69658edf02fda6cfb487eb295e67561ae08
Thomas Zimmermann <tzimmermann@suse.de>
2024-07-05 13:47:49 +0200

    drm/mgag200: Add vblank support
    There's no VBLANK interrupt on Matrox chipsets. The workaround that is
    being used here and in other free Matrox drivers is to program <linecomp>
    to the value of <vblkstr> and enable the VLINE interrupt. This triggers
    an interrupt at the time when VBLANK begins.
    
    VLINE uses separate registers for enabling and clearing pending interrupts.
    No extra synchronization between irq handler and the rest of the driver is
    required.
    
    v4:
    - recreate patch on latest upstream
    - use devm_request_irq() for managed cleanup
    - fail if vblanking cannot be initialized
    - rename register constants (Sam, Emil)
    - clear interrupt before registering handler (Ville)
    - move <linecomp> programming into separate commit
    - set <linecomp> to <vblkstr>
    - fix typo in commit message
    
    v3:
    - set <linecomp> to <vdisplay> + 1 to trigger at VBLANK
    - expand comment on linecomp
    
    v2:
    - only signal vblank on CRTC 0
    - use constants for registers and fields
    - set VLINECLR before enabling interrupt
    - test against STATUS and IEN in irq handler
    - coding-style fixes
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    Acked-by: Sam Ravnborg <sam@ravnborg.org>

--- drivers/gpu/drm/mgag200/mgag200_drv.c
+++ drivers/gpu/drm/mgag200/mgag200_drv.c
@@ -18,6 +18,7 @@
 #include <drm/drm_managed.h>
 #include <drm/drm_module.h>
 #include <drm/drm_pciids.h>
+#include <drm/drm_vblank.h>
 
 #include "mgag200_drv.h"
 
@@ -82,6 +83,35 @@ resource_size_t mgag200_probe_vram(void __iomem *mem, resource_size_t size)
 	iowrite16(orig, mem);
 
 	return offset - 65536;
+}
+
+static irqreturn_t mgag200_irq_handler(int irq, void *arg)
+{
+	struct drm_device *dev = arg;
+	struct mga_device *mdev = to_mga_device(dev);
+	struct drm_crtc *crtc;
+	u32 status, ien, iclear;
+
+	status = RREG32(MGAREG_STATUS);
+
+	if (status & MGAREG_STATUS_VLINEPEN) {
+		ien = RREG32(MGAREG_IEN);
+		if (!(ien & MGAREG_IEN_VLINEIEN))
+			goto out;
+
+		crtc = drm_crtc_from_index(dev, 0);
+		if (WARN_ON_ONCE(!crtc))
+			goto out;
+		drm_crtc_handle_vblank(crtc);
+
+		iclear = RREG32(MGAREG_ICLEAR);
+		iclear |= MGAREG_ICLEAR_VLINEICLR;
+		WREG32(MGAREG_ICLEAR, iclear);
+		return IRQ_HANDLED;
+	}
+
+out:
+	return IRQ_NONE;
 }
 
 /*
@@ -167,7 +197,9 @@ int mgag200_device_init(struct mga_device *mdev,
 			const struct mgag200_device_funcs *funcs)
 {
 	struct drm_device *dev = &mdev->base;
+	struct pci_dev *pdev = to_pci_dev(dev->dev);
 	u8 crtcext3, misc;
+	u32 ien, iclear;
 	int ret;
 
 	mdev->info = info;
@@ -191,6 +223,21 @@ int mgag200_device_init(struct mga_device *mdev,
 	WREG8(MGA_MISC_OUT, misc);
 
 	mutex_unlock(&mdev->rmmio_lock);
+
+	ien = RREG32(MGAREG_IEN);
+	ien &= ~(MGAREG_IEN_VLINEIEN);
+	WREG32(MGAREG_IEN, ien);
+
+	iclear = RREG32(MGAREG_ICLEAR);
+	iclear |= MGAREG_ICLEAR_VLINEICLR;
+	WREG32(MGAREG_ICLEAR, iclear);
+
+	ret = devm_request_irq(&pdev->dev, pdev->irq, mgag200_irq_handler, IRQF_SHARED,
+			       dev->driver->name, dev);
+	if (ret) {
+		drm_err(dev, "Failed to acquire interrupt, error %d\n", ret);
+		return ret;
+	}
 
 	return 0;
 }
