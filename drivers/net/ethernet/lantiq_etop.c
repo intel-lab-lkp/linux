588c432ad9c75bcc26c31c9ffc024b00c7e6270b
Aleksander Jan Bajkowski <olek2@wp.pl>
2024-07-07 14:10:37 +0000

    net: ethernet: lantiq_etop: fix double free in detach
    The number of the currently released descriptor is never incremented
    which results in the same skb being released multiple times.
    
    Reported-by: Joe Perches <joe@perches.com>
    Closes: https://lore.kernel.org/all/fc1bf93d92bb5b2f99c6c62745507cc22f3a7b2d.camel@perches.com/
    Signed-off-by: Aleksander Jan Bajkowski <olek2@wp.pl>

--- drivers/net/ethernet/lantiq_etop.c
+++ drivers/net/ethernet/lantiq_etop.c
@@ -217,9 +217,7 @@ ltq_etop_free_channel(struct net_device *dev, struct ltq_etop_chan *ch)
 	if (ch->dma.irq)
 		free_irq(ch->dma.irq, priv);
 	if (IS_RX(ch->idx)) {
-		int desc;
-
-		for (desc = 0; desc < LTQ_DESC_NUM; desc++)
+		for (ch->dma.desc = 0; ch->dma.desc < LTQ_DESC_NUM; ch->dma.desc++)
 			dev_kfree_skb_any(ch->skb[ch->dma.desc]);
 	}
 }
