516d5ae0262a107d2aa6344ec5c2c41b09fe095b
Tengfei Fan <quic_tengfan@quicinc.com>
2024-07-09 22:41:27 +0800

    phy: qcpm-qmp-usb: Add support for QCS9100
    Add support for QCS9100 USB3 PHY.
    QCS9100 is drived from SA8775p. Currently, both the QCS9100 and SA8775p
    platform use non-SCMI resource. In the future, the SA8775p platform will
    move to use SCMI resources and it will have new sa8775p-related device
    tree. Consequently, introduce "qcom,qcs9100-qmp-usb3-uni-phy" to the QMP
    USB device match table.
    
    Signed-off-by: Tengfei Fan <quic_tengfan@quicinc.com>

--- drivers/phy/qualcomm/phy-qcom-qmp-usb.c
+++ drivers/phy/qualcomm/phy-qcom-qmp-usb.c
@@ -2247,6 +2247,9 @@ static const struct of_device_id qmp_usb_of_match_table[] = {
 		.compatible = "qcom,msm8996-qmp-usb3-phy",
 		.data = &msm8996_usb3phy_cfg,
 	}, {
+		.compatible = "qcom,qcs9100-qmp-usb3-uni-phy",
+		.data = &sa8775p_usb3_uniphy_cfg,
+	}, {
 		.compatible = "qcom,qdu1000-qmp-usb3-uni-phy",
 		.data = &qdu1000_usb3_uniphy_cfg,
 	}, {
