fea3681ce6e81dcf44d2b987f0eac343a1d00fce
Liam R. Howlett <Liam.Howlett@oracle.com>
2024-07-17 16:06:50 -0400

    mm/mmap: Introduce abort_munmap_vmas()
    Extract clean up of failed munmap() operations from
    do_vmi_align_munmap().  This simplifies later patches in the series.
    
    It is worth noting that the mas_for_each() loop now has a different
    upper limit.  This should not change the number of vmas visited for
    reattaching to the main vma tree (mm_mt), as all vmas are reattached in
    both scenarios.
    
    Signed-off-by: Liam R. Howlett <Liam.Howlett@oracle.com>
    Reviewed-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Reviewed-by: Suren Baghdasaryan <surenb@google.com>

--- mm/mmap.c
+++ mm/mmap.c
@@ -2587,6 +2587,22 @@ struct vm_area_struct *vma_merge_extend(struct vma_iterator *vmi,
 }
 
 /*
+ * abort_munmap_vmas - Undo any munmap work and free resources
+ *
+ * Reattach any detached vmas and free up the maple tree used to track the vmas.
+ */
+static inline void abort_munmap_vmas(struct ma_state *mas_detach)
+{
+	struct vm_area_struct *vma;
+
+	mas_set(mas_detach, 0);
+	mas_for_each(mas_detach, vma, ULONG_MAX)
+		vma_mark_detached(vma, false);
+
+	__mt_destroy(mas_detach->tree);
+}
+
+/*
  * do_vmi_align_munmap() - munmap the aligned region from @start to @end.
  * @vmi: The vma iterator
  * @vma: The starting vm_area_struct
@@ -2740,11 +2756,7 @@ clear_tree_failed:
 userfaultfd_error:
 munmap_gather_failed:
 end_split_failed:
-	mas_set(&mas_detach, 0);
-	mas_for_each(&mas_detach, next, end)
-		vma_mark_detached(next, false);
-
-	__mt_destroy(&mt_detach);
+	abort_munmap_vmas(&mas_detach);
 start_split_failed:
 map_count_exceeded:
 	validate_mm(mm);
