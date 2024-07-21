87fbb581b807d433d73274d4c5d4e1d1f51e1755
Leo Yan <leo.yan@arm.com>
2024-07-21 21:21:08 +0100

    perf pmu: Directly use evsel's PMU pointer
    Rather than iterating the whole PMU list for finding the associated PMU
    device for an evsel, this commit optimizes to directly use evsel's 'pmu'
    pointer for accessing PMU device.
    
    Signed-off-by: Leo Yan <leo.yan@arm.com>

--- tools/perf/util/pmu.c
+++ tools/perf/util/pmu.c
@@ -1199,7 +1199,7 @@ void perf_pmu__warn_invalid_formats(struct perf_pmu *pmu)
 
 bool evsel__is_aux_event(const struct evsel *evsel)
 {
-	struct perf_pmu *pmu = evsel__find_pmu(evsel);
+	struct perf_pmu *pmu = evsel->pmu;
 
 	return pmu && pmu->auxtrace;
 }
