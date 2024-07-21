526d671c97cc7ad25d8edbde9dc80a54aa23c536
Michael Grzeschik <m.grzeschik@pengutronix.de>
2024-07-22 00:18:46 +0200

    usb: gadget: function: move u_f.h to include/linux/usb/func_utils.h
    We move the func_utils.h header to include/linux/usb to be
    able to compile function drivers outside of the
    drivers/usb/gadget/function directory.
    
    Signed-off-by: Michael Grzeschik <m.grzeschik@pengutronix.de>

--- drivers/usb/gadget/u_f.c
+++ drivers/usb/gadget/u_f.c
@@ -8,7 +8,7 @@
  * Author: Andrzej Pietrasiewicz <andrzejtp2010@gmail.com>
  */
 
-#include "u_f.h"
+#include <linux/usb/func_utils.h>
 #include <linux/usb/ch9.h>
 
 struct usb_request *alloc_ep_req(struct usb_ep *ep, size_t len)
