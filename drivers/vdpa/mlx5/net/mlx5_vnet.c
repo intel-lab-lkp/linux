aa65d07224fb3960b74ec29b2dca203ec1c1a71d
Cindy Lu <lulu@redhat.com>
2024-07-23 13:39:22 +0800

    vdpa/mlx5: Add the support of set mac address
    Add the function to support setting the MAC address.
    For vdpa/mlx5, the function will use mlx5_mpfs_add_mac
    to set the mac address
    
    Tested in ConnectX-6 Dx device
    
    Signed-off-by: Cindy Lu <lulu@redhat.com>

--- drivers/vdpa/mlx5/net/mlx5_vnet.c
+++ drivers/vdpa/mlx5/net/mlx5_vnet.c
@@ -3936,10 +3936,38 @@ static void mlx5_vdpa_dev_del(struct vdpa_mgmt_dev *v_mdev, struct vdpa_device *
 	destroy_workqueue(wq);
 	mgtdev->ndev = NULL;
 }
+static int mlx5_vdpa_set_attr(struct vdpa_mgmt_dev *v_mdev,
+			      struct vdpa_device *dev,
+			      const struct vdpa_dev_set_config *add_config)
+{
+	struct virtio_net_config *config;
+	struct mlx5_core_dev *pfmdev;
+	struct mlx5_vdpa_dev *mvdev;
+	struct mlx5_vdpa_net *ndev;
+	struct mlx5_core_dev *mdev;
+	int err = -EINVAL;
+
+	mvdev = to_mvdev(dev);
+	ndev = to_mlx5_vdpa_ndev(mvdev);
+	mdev = mvdev->mdev;
+	config = &ndev->config;
+
+	down_write(&ndev->reslock);
+	if (add_config->mask & (1 << VDPA_ATTR_DEV_NET_CFG_MACADDR)) {
+		pfmdev = pci_get_drvdata(pci_physfn(mdev->pdev));
+		err = mlx5_mpfs_add_mac(pfmdev, config->mac);
+		if (0 == err)
+			memcpy(config->mac, add_config->net.mac, ETH_ALEN);
+	}
+
+	up_write(&ndev->reslock);
+	return err;
+}
 
 static const struct vdpa_mgmtdev_ops mdev_ops = {
 	.dev_add = mlx5_vdpa_dev_add,
 	.dev_del = mlx5_vdpa_dev_del,
+	.dev_set_attr = mlx5_vdpa_set_attr,
 };
 
 static struct virtio_device_id id_table[] = {
