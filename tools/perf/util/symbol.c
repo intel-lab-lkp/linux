782a7380c0d9eeca9aa25a9f09a29b35e111ed79
Changbin Du <changbin.du@huawei.com>
2024-07-25 10:15:42 +0800

    perf: support specify vdso path in cmdline
    The vdso dumped from process memory (in buildid-cache) lacks debugging
    info. To annotate vdso symbols with source lines we need specify a
    debugging version.
    
    For x86, we can find them from your local build as
    arch/x86/entry/vdso/vdso{32,64}.so.dbg. Or they may reside in
    /lib/modules/<version>/vdso/vdso{32,64}.so on Ubuntu. But notice that
    the buildid has to match.
    
    $ sudo perf record -a
    $ sudo perf report --objdump=llvm-objdump \
      --vdso arch/x86/entry/vdso/vdso64.so.dbg,arch/x86/entry/vdso/vdso32.so.dbg
    
    Samples: 17K of event 'cycles:P', 4000 Hz, Event count (approx.): 1760
    __vdso_clock_gettime  /work/linux-host/arch/x86/entry/vdso/vdso64.so.d
    Percent│       movq    -48(%rbp),%rsi
           │       testq   %rax,%rax
           │     ;               return vread_hvclock();
           │       movq    %rax,%rdx
           │     ;               if (unlikely(!vdso_cycles_ok(cycles)))
           │     ↑ js      eb
           │     ↑ jmp     74
           │     ;               ts->tv_sec = vdso_ts->sec;
      0.02 │147:   leaq    2(%rbx),%rax
           │       shlq    $4, %rax
           │       addq    %r10,%rax
           │     ;               while ((seq = READ_ONCE(vd->seq)) & 1) {
      9.38 │152:   movl    (%r10),%ecx
    
    When doing cross platform analysis, we also need specify the vdso path if
    we are interested in its symbols.
    
    v2: update documentation.
    
    Signed-off-by: Changbin Du <changbin.du@huawei.com>

--- tools/perf/util/symbol.c
+++ tools/perf/util/symbol.c
@@ -19,6 +19,7 @@
 #include "build-id.h"
 #include "cap.h"
 #include "dso.h"
+#include "vdso.h"
 #include "util.h" // lsdir()
 #include "debug.h"
 #include "event.h"
@@ -44,6 +45,7 @@
 
 static int dso__load_kernel_sym(struct dso *dso, struct map *map);
 static int dso__load_guest_kernel_sym(struct dso *dso, struct map *map);
+static int dso__load_vdso_sym(struct dso *dso, struct map *map);
 static bool symbol__is_idle(const char *name);
 
 int vmlinux_path__nr_entries;
@@ -1832,6 +1834,12 @@ int dso__load(struct dso *dso, struct map *map)
 		goto out;
 	}
 
+	if (dso__is_vdso(dso)) {
+		ret = dso__load_vdso_sym(dso, map);
+		if (ret > 0)
+			goto out;
+	}
+
 	dso__set_adjust_symbols(dso, false);
 
 	if (perfmap) {
@@ -2016,12 +2024,14 @@ int dso__load_vmlinux(struct dso *dso, struct map *map,
 		dso__set_binary_type(dso, DSO_BINARY_TYPE__VMLINUX);
 
 	err = dso__load_sym(dso, map, &ss, &ss, 0);
-	symsrc__destroy(&ss);
-
 	if (err > 0) {
 		dso__set_loaded(dso);
 		pr_debug("Using %s for symbols\n", symfs_vmlinux);
+
+		if (symsrc__has_symtab(&ss) && !dso__symsrc_filename(dso))
+			dso__set_symsrc_filename(dso, strdup(symfs_vmlinux));
 	}
+	symsrc__destroy(&ss);
 
 	return err;
 }
@@ -2345,6 +2355,74 @@ static int vmlinux_path__init(struct perf_env *env)
 
 out_fail:
 	vmlinux_path__exit();
+	return -1;
+}
+
+int parse_vdso_pathnames(const struct option *opt __maybe_unused,
+			 const char *arg, int unset __maybe_unused)
+{
+	char *tmp, *tok, *str = strdup(arg);
+	unsigned int i = 0;
+
+	for (tok = strtok_r(str, ",", &tmp); tok && i < ARRAY_SIZE(symbol_conf.vdso_name);
+	     tok = strtok_r(NULL, ",", &tmp)) {
+		symbol_conf.vdso_name[i++] = strdup(tok);
+	}
+
+	free(str);
+	return 0;
+}
+
+static int dso__load_vdso(struct dso *dso, struct map *map,
+			  const char *vdso)
+{
+	int err = -1;
+	struct symsrc ss;
+	char symfs_vdso[PATH_MAX];
+
+	if (vdso[0] == '/')
+		snprintf(symfs_vdso, sizeof(symfs_vdso), "%s", vdso);
+	else
+		symbol__join_symfs(symfs_vdso, vdso);
+
+	if (symsrc__init(&ss, dso, symfs_vdso, DSO_BINARY_TYPE__SYSTEM_PATH_DSO))
+		return -1;
+
+	/*
+	 * dso__load_sym() may copy 'dso' which will result in the copies having
+	 * an incorrect long name unless we set it here first.
+	 */
+	dso__set_long_name(dso, vdso, false);
+	dso__set_binary_type(dso, DSO_BINARY_TYPE__SYSTEM_PATH_DSO);
+
+	err = dso__load_sym(dso, map, &ss, &ss, 0);
+	if (err > 0) {
+		dso__set_loaded(dso);
+		pr_debug("Using %s for %s symbols\n", symfs_vdso, dso__short_name(dso));
+
+		if (symsrc__has_symtab(&ss) && !dso__symsrc_filename(dso))
+			dso__set_symsrc_filename(dso, strdup(symfs_vdso));
+	}
+	symsrc__destroy(&ss);
+
+	return err;
+}
+
+static int dso__load_vdso_sym(struct dso *dso, struct map *map)
+{
+	int ret;
+
+	if (!dso__is_vdso(dso))
+		return -1;
+
+	for (unsigned int i = 0; i < ARRAY_SIZE(symbol_conf.vdso_name); i++) {
+		if (symbol_conf.vdso_name[i] != NULL) {
+			ret = dso__load_vdso(dso, map, symbol_conf.vdso_name[i]);
+			if (ret > 0)
+				return ret;
+		}
+	}
+
 	return -1;
 }
 
--- tools/perf/util/symbol_conf.h
+++ tools/perf/util/symbol_conf.h
@@ -3,6 +3,7 @@
 #define __PERF_SYMBOL_CONF 1
 
 #include <stdbool.h>
+#include <subcmd/parse-options.h>
 
 struct strlist;
 struct intlist;
@@ -56,6 +57,7 @@ struct symbol_conf {
 	const char	*default_guest_vmlinux_name,
 			*default_guest_kallsyms,
 			*default_guest_modules;
+	const char	*vdso_name[2];
 	const char	*guestmount;
 	const char	*dso_list_str,
 			*comm_list_str,
@@ -85,5 +87,8 @@ struct symbol_conf {
 };
 
 extern struct symbol_conf symbol_conf;
+
+int parse_vdso_pathnames(const struct option *opt __maybe_unused,
+			 const char *arg, int unset __maybe_unused);
 
 #endif // __PERF_SYMBOL_CONF
