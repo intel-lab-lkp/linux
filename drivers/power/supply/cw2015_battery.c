180d51febe3b55f87daf2ddcfd5f2612b99b20ef
Chris Morgan <macromorgan@hotmail.com>
2024-07-26 14:49:45 -0500

    power: supply: cw2015: Add support for dual-cell configurations
    The Cellwise cw2015 datasheet reports that it can handle two cells
    in a series configuration. Allow a device tree parameter to note
    this condition so that the driver reports the correct voltage values
    to userspace.
    
    Signed-off-by: Chris Morgan <macromorgan@hotmail.com>

--- drivers/power/supply/cw2015_battery.c
+++ drivers/power/supply/cw2015_battery.c
@@ -77,6 +77,8 @@ struct cw_battery {
 	u32 poll_interval_ms;
 	u8 alert_level;
 
+	bool dual_cell;
+
 	unsigned int read_errors;
 	unsigned int charge_stuck_cnt;
 };
@@ -324,6 +326,9 @@ static int cw_get_voltage(struct cw_battery *cw_bat)
 	 * Negligible error of 0.1%
 	 */
 	voltage_mv = avg * 312 / 1024;
+
+	if (cw_bat->dual_cell)
+		voltage_mv *= 2;
 
 	dev_dbg(cw_bat->dev, "Read voltage: %d mV, raw=0x%04x\n",
 		voltage_mv, reg_val);
@@ -586,6 +591,8 @@ static int cw2015_parse_properties(struct cw_battery *cw_bat)
 		if (ret)
 			return ret;
 	}
+
+	cw_bat->dual_cell = device_property_read_bool(dev, "cellwise,dual-cell");
 
 	ret = device_property_read_u32(dev, "cellwise,monitor-interval-ms",
 				       &cw_bat->poll_interval_ms);
