39249054a2b6493cb0e7a1b69ba798684ed01a81
Barry Song <v-songbaohua@oppo.com>
2024-07-26 21:46:15 +1200

    mm: swap: introduce swapcache_prepare_nr and swapcache_clear_nr for large folios swap-in
    Commit 13ddaf26be32 ("mm/swap: fix race when skipping swapcache") supports
    one entry only, to support large folio swap-in, we need to handle multiple
    swap entries.
    
    To optimize stack usage, we iterate twice in __swap_duplicate_nr(): the
    first time to verify that all entries are valid, and the second time to
    apply the modifications to the entries.
    
    Signed-off-by: Barry Song <v-songbaohua@oppo.com>

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -480,7 +480,7 @@ extern int get_swap_pages(int n, swp_entry_t swp_entries[], int order);
 extern int add_swap_count_continuation(swp_entry_t, gfp_t);
 extern void swap_shmem_alloc(swp_entry_t);
 extern int swap_duplicate(swp_entry_t);
-extern int swapcache_prepare(swp_entry_t);
+extern int swapcache_prepare_nr(swp_entry_t entry, int nr);
 extern void swap_free_nr(swp_entry_t entry, int nr_pages);
 extern void swapcache_free_entries(swp_entry_t *entries, int n);
 extern void free_swap_and_cache_nr(swp_entry_t entry, int nr);
@@ -554,7 +554,7 @@ static inline int swap_duplicate(swp_entry_t swp)
 	return 0;
 }
 
-static inline int swapcache_prepare(swp_entry_t swp)
+static inline int swapcache_prepare_nr(swp_entry_t swp, int nr)
 {
 	return 0;
 }
@@ -610,6 +610,11 @@ static inline void free_swap_and_cache(swp_entry_t entry)
 static inline void swap_free(swp_entry_t entry)
 {
 	swap_free_nr(entry, 1);
+}
+
+static inline int swapcache_prepare(swp_entry_t entry)
+{
+	return swapcache_prepare_nr(entry, 1);
 }
 
 #ifdef CONFIG_MEMCG
