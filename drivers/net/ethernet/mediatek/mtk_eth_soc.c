17372ff6404774a33baf09789085f79e767fe9aa
Elad Yifee <eladwf@gmail.com>
2024-07-29 21:29:55 +0300

    net: ethernet: mtk_eth_soc: use PP exclusively for XDP programs
    PP allocations and XDP code path traversal are unnecessary
    when no XDP program is loaded.
    Prevent that by simply not creating the pool.
    This change boosts driver performance for this use case,
    allowing the CPU to handle about 13% more packets/sec.
    
    Signed-off-by: Elad Yifee <eladwf@gmail.com>

--- drivers/net/ethernet/mediatek/mtk_eth_soc.c
+++ drivers/net/ethernet/mediatek/mtk_eth_soc.c
@@ -2644,7 +2644,7 @@ static int mtk_rx_alloc(struct mtk_eth *eth, int ring_no, int rx_flag)
 	if (!ring->data)
 		return -ENOMEM;
 
-	if (mtk_page_pool_enabled(eth)) {
+	if (mtk_page_pool_enabled(eth) && rcu_access_pointer(eth->prog)) {
 		struct page_pool *pp;
 
 		pp = mtk_create_page_pool(eth, &ring->xdp_q, ring_no,
