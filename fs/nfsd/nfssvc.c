b455a6d4fd4a20f5124e04f01b7fafb29209e0f8
NeilBrown <neilb@suse.de>
2024-07-29 12:18:21 +1000

    sunrpc: allow svc threads to fail initialisation cleanly
    If an svc thread needs to perform some initialisation that might fail,
    it has no good way to handle the failure.
    
    Before the thread can exit it must call svc_exit_thread(), but that
    requires the service mutex to be held.  The thread cannot simply take
    the mutex as that could deadlock if there is a concurrent attempt to
    shut down all threads (which is unlikely, but not impossible).
    
    nfsd currently call svc_exit_thread() unprotected in the unlikely event
    that unshare_fs_struct() fails.
    
    We can clean this up by introducing svc_thread_init_status() by which an
    svc thread can report whether initialisation has succeeded.  If it has,
    it continues normally into the action loop.  If it has not,
    svc_thread_init_status() immediately aborts the thread.
    svc_start_kthread() waits for either of these to happen, and calls
    svc_exit_thread() (under the mutex) if the thread aborted.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- fs/nfsd/nfssvc.c
+++ fs/nfsd/nfssvc.c
@@ -915,11 +915,9 @@ nfsd(void *vrqstp)
 
 	/* At this point, the thread shares current->fs
 	 * with the init process. We need to create files with the
-	 * umask as defined by the client instead of init's umask. */
-	if (unshare_fs_struct() < 0) {
-		printk("Unable to start nfsd thread: out of memory\n");
-		goto out;
-	}
+	 * umask as defined by the client instead of init's umask.
+	 */
+	svc_thread_init_status(rqstp, unshare_fs_struct());
 
 	current->fs->umask = 0;
 
@@ -941,7 +939,6 @@ nfsd(void *vrqstp)
 
 	atomic_dec(&nfsd_th_cnt);
 
-out:
 	/* Release the thread */
 	svc_exit_thread(rqstp);
 	return 0;
