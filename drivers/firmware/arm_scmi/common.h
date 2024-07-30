53da44d1b03bca113e52a14e4556ca12d1e6b1c2
Luke Parkin <luke.parkin@arm.com>
2024-07-30 10:33:39 +0100

    firmware: arm_scmi: Add support for tracking metrics
    Add a new optional config option for tracking, configurable
    	at build.
    Add methods for counting key metrics
    
    Signed-off-by: Luke Parkin <luke.parkin@arm.com>

--- drivers/firmware/arm_scmi/common.h
+++ drivers/firmware/arm_scmi/common.h
@@ -301,6 +301,19 @@ extern const struct scmi_desc scmi_optee_desc;
 
 void scmi_rx_callback(struct scmi_chan_info *cinfo, u32 msg_hdr, void *priv);
 
+enum debug_counters {
+	SCMI_DEBUG_COUNTERS_LAST
+};
+
+#ifdef CONFIG_ARM_SCMI_DEBUG_COUNTERS
+static inline void scmi_inc_count(atomic_t *arr, int stat)
+{
+	atomic_inc(&arr[stat]);
+}
+#else
+static inline void scmi_inc_count(atomic_t *arr, int stat) {}
+#endif
+
 enum scmi_bad_msg {
 	MSG_UNEXPECTED = -1,
 	MSG_INVALID = -2,
