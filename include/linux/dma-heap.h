739977a81b2c28a04f71e231d24761e214f100f4
Huan Yang <link@vivo.com>
2024-07-30 15:57:47 +0800

    dma-buf: heaps: support alloc async read file
    This patch completes the infrastructure for async read. It treats memory
    allocation as the producer and assigns file reading to the heap_fwork_t
    thread as the consumer.
    
    The heap needs to gather each allocated page and, when a certain amount
    (default 128MB) is gathered, it will package and pass it to heap_fwork_t
    to initiate file reading.
    
    This process is completed by the helper functions
    dma_heap_gather_file_page. Ever heap declare a task and pass each page,
    then wait file read done before return dma-buf.
    
    Because the memory allocation and file reading correspond to each other,
    the number of gathers during the prepare process and submit process can
    determine the offset in the file as well as the size to be read.
    
    When a gather page initiates a read, it is packaged into a work and
    passed to the heap_fwork_t thread, containing the offset and size of the
    file being read, the buffer obtained by mapping the gather page to
    vmalloc, and the credentials used during the read.
    
    The buffer for file reading is provided by mapping the gathered pages to
    vmalloc. This means that if direct I/O is used to read a file, the file
    content will be directly transferred to the corresponding memory of the
    dma-buf, without the need for additional CPU copying and intermediate
    buffers.
    
    Although direct I/O requires page aligned, this patch can
    automatically adapt to the file size and use buffer I/O to read the
    unaligned parts.
    
    Note that heap_fwork_t is a single-threaded process, which means that
    the file read work is executed serially. Considering that the default
    I/O amount initiated at a time is 128MB, which is already quite large,
    multiple threads will not help accelerate I/O performance.
    
    So, this is more suit for large size file read into dma-buf.
    
    Signed-off-by: Huan Yang <link@vivo.com>

--- include/linux/dma-heap.h
+++ include/linux/dma-heap.h
@@ -14,6 +14,8 @@
 
 struct dma_heap;
 struct dma_heap_file;
+struct dma_heap_file_task;
+struct dma_heap_file;
 
 /**
  * struct dma_heap_ops - ops to operate on a given heap
@@ -68,5 +70,48 @@ const char *dma_heap_get_name(struct dma_heap *heap);
  * @exp_info:		information needed to register this heap
  */
 struct dma_heap *dma_heap_add(const struct dma_heap_export_info *exp_info);
+
+/**
+ * dma_heap_wait_for_file_read - waits for a file read to complete
+ *
+ * Some users need to call this function before destroying the page to ensure
+ * that all file work has been completed, in order to avoid UAF issues.
+ * Remember, this function does not destroy the data structure corresponding to
+ * the ftask. Before ending the actual processing, you need to call
+ * @dma_heap_end_file_read.
+ *
+ * 0 - success, -EIO - if any file work failed
+ */
+int dma_heap_wait_for_file_read(struct dma_heap_file_task *heap_ftask);
+
+/**
+ * dma_heap_end_file_read - waits for a file read to complete then destroy it
+ * 0 - success, -EIO - if any file work failed
+ */
+int dma_heap_end_file_read(struct dma_heap_file_task *heap_ftask);
+
+/**
+ * dma_heap_alloc_file_read - Declare a task to read file when allocate pages.
+ * @heap_file:		target file to read
+ *
+ * Return NULL if failed, otherwise return a struct pointer.
+ */
+struct dma_heap_file_task *
+dma_heap_declare_file_read(struct dma_heap_file *heap_file);
+
+/**
+ * dma_heap_gather_file_page - gather each allocated page.
+ * @heap_ftask:		prepared and need to commit's work.
+ * @page:		current allocated page. don't care which order.
+ *
+ * This function gather all allocated pages, automatically submit when the
+ * gathering reaches the limit. Submit will package pages, prepare the data
+ * required for reading file, then submit to async read thread.
+ *
+ * 0 - success, nagtive - failed.
+ */
+int dma_heap_gather_file_page(struct dma_heap_file_task *heap_ftask,
+			      struct page *page);
+size_t dma_heap_file_size(struct dma_heap_file *heap_file);
 
 #endif /* _DMA_HEAPS_H */
