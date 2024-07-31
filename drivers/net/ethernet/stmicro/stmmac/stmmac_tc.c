50c1964ec12489a362d427f9e81f7f00be303923
Furong Xu <0x1207@gmail.com>
2024-07-31 18:43:14 +0800

    net: stmmac: support fp parameter of tc-taprio
    tc-taprio can select whether traffic classes are express or preemptible.
    
    After some traffic tests, MAC merge layer statistics are all good.
    
    Local device:
    ethtool --include-statistics --json --show-mm eth1
    [ {
            "ifname": "eth1",
            "pmac-enabled": true,
            "tx-enabled": true,
            "tx-active": true,
            "tx-min-frag-size": 60,
            "rx-min-frag-size": 60,
            "verify-enabled": true,
            "verify-time": 100,
            "max-verify-time": 128,
            "verify-status": "SUCCEEDED",
            "statistics": {
                "MACMergeFrameAssErrorCount": 0,
                "MACMergeFrameSmdErrorCount": 0,
                "MACMergeFrameAssOkCount": 0,
                "MACMergeFragCountRx": 0,
                "MACMergeFragCountTx": 1398,
                "MACMergeHoldCount": 15783
            }
        } ]
    
    Remote device:
    ethtool --include-statistics --json --show-mm eth1
    [ {
            "ifname": "eth1",
            "pmac-enabled": true,
            "tx-enabled": true,
            "tx-active": true,
            "tx-min-frag-size": 60,
            "rx-min-frag-size": 60,
            "verify-enabled": true,
            "verify-time": 100,
            "max-verify-time": 128,
            "verify-status": "SUCCEEDED",
            "statistics": {
                "MACMergeFrameAssErrorCount": 0,
                "MACMergeFrameSmdErrorCount": 0,
                "MACMergeFrameAssOkCount": 1388,
                "MACMergeFragCountRx": 1398,
                "MACMergeFragCountTx": 0,
                "MACMergeHoldCount": 0
            }
        } ]
    
    Tested on DWMAC CORE 5.10a
    
    Signed-off-by: Furong Xu <0x1207@gmail.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_tc.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_tc.c
@@ -943,7 +943,6 @@ static int tc_taprio_configure(struct stmmac_priv *priv,
 	u32 size, wid = priv->dma_cap.estwid, dep = priv->dma_cap.estdep;
 	struct timespec64 time, current_time, qopt_time;
 	ktime_t current_time_ns;
-	bool fpe = false;
 	int i, ret = 0;
 	u64 ctr;
 
@@ -1028,16 +1027,6 @@ static int tc_taprio_configure(struct stmmac_priv *priv,
 
 		switch (qopt->entries[i].command) {
 		case TC_TAPRIO_CMD_SET_GATES:
-			if (fpe)
-				return -EINVAL;
-			break;
-		case TC_TAPRIO_CMD_SET_AND_HOLD:
-			gates |= BIT(0);
-			fpe = true;
-			break;
-		case TC_TAPRIO_CMD_SET_AND_RELEASE:
-			gates &= ~BIT(0);
-			fpe = true;
 			break;
 		default:
 			return -EOPNOTSUPP;
@@ -1068,15 +1057,10 @@ static int tc_taprio_configure(struct stmmac_priv *priv,
 
 	tc_taprio_map_maxsdu_txq(priv, qopt);
 
-	if (fpe && !priv->dma_cap.fpesel) {
+	if (qopt->mqprio.preemptible_tcs && !priv->dma_cap.fpesel) {
 		mutex_unlock(&priv->est_lock);
 		return -EOPNOTSUPP;
 	}
-
-	/* Actual FPE register configuration will be done after FPE handshake
-	 * is success.
-	 */
-	priv->plat->fpe_cfg->enable = fpe;
 
 	ret = stmmac_est_configure(priv, priv, priv->est,
 				   priv->plat->clk_ptp_rate);
@@ -1088,10 +1072,7 @@ static int tc_taprio_configure(struct stmmac_priv *priv,
 
 	netdev_info(priv->dev, "configured EST\n");
 
-	if (fpe) {
-		stmmac_fpe_handshake(priv, true);
-		netdev_info(priv->dev, "start FPE handshake\n");
-	}
+	stmmac_fpe_set_preemptible_tcs(priv, priv->ioaddr, qopt->mqprio.preemptible_tcs);
 
 	return 0;
 
@@ -1109,16 +1090,7 @@ disable:
 		mutex_unlock(&priv->est_lock);
 	}
 
-	priv->plat->fpe_cfg->enable = false;
-	stmmac_fpe_configure(priv, priv->ioaddr,
-			     priv->plat->fpe_cfg,
-			     priv->plat->tx_queues_to_use,
-			     priv->plat->rx_queues_to_use,
-			     false);
-	netdev_info(priv->dev, "disabled FPE\n");
-
-	stmmac_fpe_handshake(priv, false);
-	netdev_info(priv->dev, "stop FPE handshake\n");
+	stmmac_fpe_set_preemptible_tcs(priv, priv->ioaddr, 0);
 
 	return ret;
 }
