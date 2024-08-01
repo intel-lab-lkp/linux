79899ec5b23e303b5637f15981546d2625f4d3af
Pavan Kumar Paluri <papaluri@amd.com>
2024-08-01 15:56:37 -0500

    x86, KVM:SVM: Move sev specific parsing into arch/x86/virt/svm
    Move SEV specific kernel command line option parsing support from
    arch/x86/coco/sev/core.c to arch/x86/virt/svm/cmdline.c so that both
    host and guest related SEV command line options can be supported.
    
    No functional changes intended.
    
    Signed-off-by: Pavan Kumar Paluri <papaluri@amd.com>

---
+++ arch/x86/virt/svm/cmdline.c
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * AMD SVM-SEV command line parsing support
+ *
+ * Copyright (C) 2023 - 2024 Advanced Micro Devices, Inc.
+ *
+ * Author: Michael Roth <michael.roth@amd.com>
+ *
+ */
+
+#include <linux/memblock.h>
+
+#include <asm/sev.h>
+
+struct sev_config sev_cfg;
+
+static int __init init_sev_config(char *str)
+{
+	char *s;
+
+	while ((s = strsep(&str, ","))) {
+		if (!strcmp(s, "debug")) {
+			sev_cfg.debug = true;
+			continue;
+		}
+
+		pr_info("SEV command-line option '%s' was not recognized\n", s);
+	}
+
+	return 1;
+}
+__setup("sev=", init_sev_config);
