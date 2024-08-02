4ee8119a97238eabdf4efe9f1be5bd832943fde3
Lyude Paul <lyude@redhat.com>
2024-08-01 20:10:00 -0400

    rust: Introduce irq module
    This introduces a module for dealing with interrupt-disabled contexts,
    including the ability to enable and disable interrupts
    (with_irqs_disabled()) - along with the ability to annotate functions as
    expecting that IRQs are already disabled on the local CPU.
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>

--- rust/helpers.c
+++ rust/helpers.c
@@ -85,6 +85,28 @@ void rust_helper_spin_unlock(spinlock_t *lock)
 }
 EXPORT_SYMBOL_GPL(rust_helper_spin_unlock);
 
+unsigned long rust_helper_local_irq_save(void)
+{
+	unsigned long flags;
+
+	local_irq_save(flags);
+
+	return flags;
+}
+EXPORT_SYMBOL_GPL(rust_helper_local_irq_save);
+
+void rust_helper_local_irq_restore(unsigned long flags)
+{
+	local_irq_restore(flags);
+}
+EXPORT_SYMBOL_GPL(rust_helper_local_irq_restore);
+
+bool rust_helper_irqs_disabled(void)
+{
+	return irqs_disabled();
+}
+EXPORT_SYMBOL_GPL(rust_helper_irqs_disabled);
+
 void rust_helper_init_wait(struct wait_queue_entry *wq_entry)
 {
 	init_wait(wq_entry);
