aa0f6b5807b2c1c2419d765d25770c6d8aac11a2
Wardenjohn <zhangwarden@gmail.com>
2024-08-05 14:46:56 +0800

    livepatch: Add using attribute to klp_func for using function show
    One system may contains more than one livepatch module. We can see
    which patch is enabled. If some patches applied to one system
    modifing the same function, livepatch will use the function enabled
    on top of the function stack. However, we can not excatly know
    which function of which patch is now enabling.
    
    This patch introduce one sysfs attribute of "using" to klp_func.
    For example, if there are serval patches  make changes to function
    "meminfo_proc_show", the attribute "enabled" of all the patch is 1.
    With this attribute, we can easily know the version enabling belongs
    to which patch.
    
    The "using" is set as three state. 0 is disabled, it means that this
    version of function is not used. 1 is running, it means that this
    version of function is now running. -1 is unknown, it means that
    this version of function is under transition, some task is still
    chaning their running version of this function.
    
    cat /sys/kernel/livepatch/<patch1>/<object1>/<function1,sympos>/using -> 0
    means that the function1 of patch1 is disabled.
    
    cat /sys/kernel/livepatch/<patchN>/<object1>/<function1,sympos>/using -> 1
    means that the function1 of patchN is enabled.
    
    cat /sys/kernel/livepatch/<patchN>/<object1>/<function1,sympos>/using -> -1
    means that the function1 of patchN is under transition and unknown.
    
    Signed-off-by: Wardenjohn <zhangwarden@gmail.com>

--- kernel/livepatch/patch.c
+++ kernel/livepatch/patch.c
@@ -104,7 +104,7 @@ static void notrace klp_ftrace_handler(unsigned long ip,
 			 * original function.
 			 */
 			func = list_entry_rcu(func->stack_node.next,
-					      struct klp_func, stack_node);
+						struct klp_func, stack_node);
 
 			if (&func->stack_node == &ops->func_stack)
 				goto unlock;
@@ -127,6 +127,7 @@ unlock:
 static void klp_unpatch_func(struct klp_func *func)
 {
 	struct klp_ops *ops;
+	struct klp_func *stack_top_func;
 
 	if (WARN_ON(!func->patched))
 		return;
@@ -152,6 +153,10 @@ static void klp_unpatch_func(struct klp_func *func)
 		kfree(ops);
 	} else {
 		list_del_rcu(&func->stack_node);
+		// the previous function is deleted, the stack top is under transition
+		stack_top_func = list_first_entry(&ops->func_stack, struct klp_func,
+							stack_node);
+		stack_top_func->using = -1;
 	}
 
 	func->patched = false;
@@ -160,6 +165,7 @@ static void klp_unpatch_func(struct klp_func *func)
 static int klp_patch_func(struct klp_func *func)
 {
 	struct klp_ops *ops;
+	struct klp_func *stack_top_func;
 	int ret;
 
 	if (WARN_ON(!func->old_func))
@@ -170,6 +176,7 @@ static int klp_patch_func(struct klp_func *func)
 
 	ops = klp_find_ops(func->old_func);
 	if (!ops) {
+		// this function is the first time to be patched
 		unsigned long ftrace_loc;
 
 		ftrace_loc = ftrace_location((unsigned long)func->old_func);
@@ -211,11 +218,16 @@ static int klp_patch_func(struct klp_func *func)
 			goto err;
 		}
 
-
 	} else {
+		// stack_top_func is going to be in transition
+		stack_top_func = list_first_entry(&ops->func_stack, struct klp_func,
+							stack_node);
+		stack_top_func->using = -1;
+		// The new patched function is the one enabling
 		list_add_rcu(&func->stack_node, &ops->func_stack);
-	}
+	}  
 
+	func->using = -1;
 	func->patched = true;
 
 	return 0;
