e87017598b877c9950da23be4cbea8407c2a278a
David Matlack <dmatlack@google.com>
2024-08-05 16:31:08 -0700

    Revert "KVM: x86/mmu: Don't bottom out on leafs when zapping collapsible SPTEs"
    This reverts commit 85f44f8cc07b5f61bef30fe5343d629fd4263230.
    
    Bring back the logic that walks down to leafs when zapping collapsible
    SPTEs. Stepping down to leafs is technically unnecessary when zapping,
    but the leaf SPTE will be used in a subsequent commit to construct a
    huge SPTE and recover the huge mapping in place.
    
    Note, this revert does not revert the function comment changes above
    zap_collapsible_spte_range() and kvm_tdp_mmu_zap_collapsible_sptes()
    since those are still relevant.
    
    Signed-off-by: David Matlack <dmatlack@google.com>

--- arch/x86/kvm/mmu/tdp_mmu.c
+++ arch/x86/kvm/mmu/tdp_mmu.c
@@ -1628,49 +1628,48 @@ static void zap_collapsible_spte_range(struct kvm *kvm,
 
 	rcu_read_lock();
 
-	for_each_tdp_pte_min_level(iter, root, PG_LEVEL_2M, start, end) {
-retry:
+	tdp_root_for_each_pte(iter, root, start, end) {
 		if (tdp_mmu_iter_cond_resched(kvm, &iter, false, true))
 			continue;
 
-		if (iter.level > KVM_MAX_HUGEPAGE_LEVEL ||
-		    !is_shadow_present_pte(iter.old_spte))
-			continue;
-
-		/*
-		 * Don't zap leaf SPTEs, if a leaf SPTE could be replaced with
-		 * a large page size, then its parent would have been zapped
-		 * instead of stepping down.
-		 */
-		if (is_last_spte(iter.old_spte, iter.level))
-			continue;
-
-		/*
-		 * If iter.gfn resides outside of the slot, i.e. the page for
-		 * the current level overlaps but is not contained by the slot,
-		 * then the SPTE can't be made huge.  More importantly, trying
-		 * to query that info from slot->arch.lpage_info will cause an
-		 * out-of-bounds access.
-		 */
-		if (iter.gfn < start || iter.gfn >= end)
+		if (!is_shadow_present_pte(iter.old_spte) ||
+		    !is_last_spte(iter.old_spte, iter.level))
 			continue;
 
 		max_mapping_level = kvm_mmu_max_mapping_level(kvm, slot,
 							      iter.gfn, PG_LEVEL_NUM);
-		if (max_mapping_level < iter.level)
+
+		WARN_ON(max_mapping_level < iter.level);
+
+		/*
+		 * If this page is already mapped at the highest
+		 * viable level, there's nothing more to do.
+		 */
+		if (max_mapping_level == iter.level)
 			continue;
 
+		/*
+		 * The page can be remapped at a higher level, so step
+		 * up to zap the parent SPTE.
+		 */
+		while (max_mapping_level > iter.level)
+			tdp_iter_step_up(&iter);
+
 		/* Note, a successful atomic zap also does a remote TLB flush. */
-		if (tdp_mmu_zap_spte_atomic(kvm, &iter))
-			goto retry;
+		(void)tdp_mmu_zap_spte_atomic(kvm, &iter);
+
+		/*
+		 * If the atomic zap fails, the iter will recurse back into
+		 * the same subtree to retry.
+		 */
 	}
 
 	rcu_read_unlock();
 }
 
 /*
- * Zap non-leaf SPTEs (and free their associated page tables) which could
- * be replaced by huge pages, for GFNs within the slot.
+ * Zap non-leaf SPTEs (and free their associated page tables) which could be
+ * replaced by huge pages, for GFNs within the slot.
  */
 void kvm_tdp_mmu_zap_collapsible_sptes(struct kvm *kvm,
 				       const struct kvm_memory_slot *slot)
