d58de4f958df225c04fd490fe2d76bb4203c9f85
Pedro Falcato <pedro.falcato@gmail.com>
2024-08-06 22:28:08 +0100

    mm: Remove can_modify_mm()
    With no more users in the tree, we can finally remove can_modify_mm().
    
    Signed-off-by: Pedro Falcato <pedro.falcato@gmail.com>

--- mm/internal.h
+++ mm/internal.h
@@ -1515,19 +1515,11 @@ static inline bool can_modify_vma(struct vm_area_struct *vma)
 }
 
 
-bool can_modify_mm(struct mm_struct *mm, unsigned long start,
-		unsigned long end);
 bool can_modify_vma_madv(struct vm_area_struct *vma, int behavior);
 #else
 static inline int can_do_mseal(unsigned long flags)
 {
 	return -EPERM;
-}
-
-static inline bool can_modify_mm(struct mm_struct *mm, unsigned long start,
-		unsigned long end)
-{
-	return true;
 }
 
 static inline bool can_modify_vma_madv(struct vm_area_struct *vma, int behavior)
