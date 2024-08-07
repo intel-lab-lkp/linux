fba53991abd8e118a3063775910bc789a4741901
Florian Westphal <fw@strlen.de>
2024-08-07 21:28:42 +0200

    selftests: netfilter: add test for br_netfilter+conntrack+queue combination
    Trigger cloned skbs leaving softirq protection.
    This triggers splat without the preceeding change
    ("netfilter: nf_queue: drop packets with cloned unconfirmed
     conntracks"):
    
    WARNING: at net/netfilter/nf_conntrack_core.c:1198 __nf_conntrack_confirm..
    
    because local delivery and forwarding will race for confirmation.
    
    Based on a reproducer script from Yi Chen.
    
    Signed-off-by: Florian Westphal <fw@strlen.de>

--- tools/testing/selftests/net/netfilter/lib.sh
+++ tools/testing/selftests/net/netfilter/lib.sh
@@ -8,3 +8,19 @@ checktool (){
 		exit $ksft_skip
 	fi
 }
+
+nf_queue_wait()
+{
+	local procfile="/proc/self/net/netfilter/nfnetlink_queue"
+	local netns id
+
+	netns="$1"
+	id="$2"
+
+	# if this file doesn't exist, nfnetlink_module isn't loaded.
+	# rather than loading it ourselves, wait for kernel module autoload
+	# completion, nfnetlink should do so automatically because nf_queue
+	# helper program, spawned in the background, asked for this functionality.
+	test -f "$procfile" &&
+		ip netns exec "$netns" cat "$procfile" | grep -q "^ *$id "
+}
--- tools/testing/selftests/net/netfilter/nft_queue.sh
+++ tools/testing/selftests/net/netfilter/nft_queue.sh
@@ -190,22 +190,6 @@ EOF
         echo "PASS: $proto: statement with no listener results in packet drop"
 }
 
-nf_queue_wait()
-{
-	local procfile="/proc/self/net/netfilter/nfnetlink_queue"
-	local netns id
-
-	netns="$1"
-	id="$2"
-
-	# if this file doesn't exist, nfnetlink_module isn't loaded.
-	# rather than loading it ourselves, wait for kernel module autoload
-	# completion, nfnetlink should do so automatically because nf_queue
-	# helper program, spawned in the background, asked for this functionality.
-	test -f "$procfile" &&
-		ip netns exec "$netns" cat "$procfile" | grep -q "^ *$id "
-}
-
 test_queue()
 {
 	local expected="$1"
