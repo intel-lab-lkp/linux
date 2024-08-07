40429e097fb55010d98329013d464c4ad1fd0939
Howard Chu <howardchu95@gmail.com>
2024-08-07 23:38:43 +0800

    perf test: Add direct off-cpu dumping test
    Add a simple workload(offcpu.c) to create the scenario for direct
    off-cpu dumping.
    
    Please run this test with 'perf test offcpu'
    
    Suggested-by: Ian Rogers <irogers@google.com>
    Signed-off-by: Howard Chu <howardchu95@gmail.com>

--- tools/perf/tests/builtin-test.c
+++ tools/perf/tests/builtin-test.c
@@ -153,6 +153,7 @@ static struct test_workload *workloads[] = {
 	&workload__brstack,
 	&workload__datasym,
 	&workload__landlock,
+	&workload__offcpu,
 };
 
 static int num_subtests(const struct test_suite *t)
