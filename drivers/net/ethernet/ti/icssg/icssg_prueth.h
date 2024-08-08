cd7f1f4c1d906fffb86faa0690556d9425d5d3b9
Ravi Gunasekaran <r-gunasekaran@ti.com>
2024-08-08 16:38:00 +0530

    net: ti: icssg-prueth: Enable HSR Tx Tag and Rx Tag offload
    Add support to offload HSR Tx Tag Insertion and Rx Tag Removal
    and duplicate discard.
    
    Signed-off-by: Ravi Gunasekaran <r-gunasekaran@ti.com>

--- drivers/net/ethernet/ti/icssg/icssg_prueth.h
+++ drivers/net/ethernet/ti/icssg/icssg_prueth.h
@@ -58,6 +58,7 @@
 #define IEP_DEFAULT_CYCLE_TIME_NS	1000000	/* 1 ms */
 
 #define PRUETH_UNDIRECTED_PKT_DST_TAG	0
+#define PRUETH_UNDIRECTED_PKT_TAG_INS	BIT(30)
 
 /* Firmware status codes */
 #define ICSS_HS_FW_READY 0x55555555
