38fb06cc101c4ba416495127300e264f1dd736f4
Thomas Weißschuh <linux@weissschuh.net>
2024-08-12 22:18:30 +0200

    drm: Add panel backlight quirks
    Panels using a PWM-controlled backlight source without an do not have a
    standard way to communicate their valid PWM ranges.
    On x86 the ranges are read from ACPI through driver-specific tables.
    The built-in ranges are not necessarily correct, or may grow stale if an
    older device can be retrofitted with newer panels.
    
    Add a quirk infrastructure with which the minimum valid backlight value
    can be maintained as part of the kernel.
    
    Signed-off-by: Thomas Weißschuh <linux@weissschuh.net>

--- include/drm/drm_utils.h
+++ include/drm/drm_utils.h
@@ -11,8 +11,11 @@
 #define __DRM_UTILS_H__
 
 #include <linux/types.h>
+#include <drm/drm_edid.h>
 
 int drm_get_panel_orientation_quirk(int width, int height);
+
+int drm_get_panel_min_brightness_quirk(const struct drm_edid *edid);
 
 signed long drm_timeout_abs_to_jiffies(int64_t timeout_nsec);
 
