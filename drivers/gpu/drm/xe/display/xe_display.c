d7ea1c139d3fcaafcd4d14a4900a5ccb24b0173c
Arun R Murthy <arun.r.murthy@intel.com>
2024-08-12 09:32:46 +0530

    drm/xe/display: Change HPD to polling on runtime suspend
    HPD is interrupt based and on runtime suspend change it to polling as
    HPD is not a wakeup event. A worker thread is scheduled for doing this
    polling and it keeps polling for HPD live status on an internval of 10s.
    On runtime resume disable polling and fallback to interrupt mechanism.
    
    v2: move poll_enable() to xe_display(Imre)
    v3: removed un-wanted comments (Jani)
    
    Signed-off-by: Arun R Murthy <arun.r.murthy@intel.com>

--- drivers/gpu/drm/xe/display/xe_display.c
+++ drivers/gpu/drm/xe/display/xe_display.c
@@ -315,6 +315,9 @@ void xe_display_pm_suspend(struct xe_device *xe, bool runtime)
 
 	intel_hpd_cancel_work(xe);
 
+	if (runtime)
+		intel_hpd_poll_enable(xe);
+
 	intel_suspend_encoders(xe);
 
 	intel_opregion_suspend(xe, s2idle ? PCI_D1 : PCI_D3cold);
