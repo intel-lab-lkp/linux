8491ba917db4f664cf88a0332135cd8e7d794ef5
Gaosheng Cui <cuigaosheng1@huawei.com>
2024-08-13 15:01:30 +0800

    omap mmc: Remove unused sx1_mmc_slot_cover_handler() declaration
    The sx1_mmc_slot_cover_handler() has been removed since
    commit 652bcd8f72cc ("omap mmc: Remove broken MMC init code"),
    so remove it.
    
    Signed-off-by: Gaosheng Cui <cuigaosheng1@huawei.com>

--- arch/arm/mach-omap1/board-sx1.h
+++ arch/arm/mach-omap1/board-sx1.h
@@ -40,6 +40,5 @@ int sx1_i2c_write_byte(u8 devaddr, u8 regoffset, u8 value);
 /* MMC prototypes */
 
 extern void sx1_mmc_init(void);
-extern void sx1_mmc_slot_cover_handler(void *arg, int state);
 
 #endif /* __ASM_ARCH_SX1_I2C_CHIPS_H */
