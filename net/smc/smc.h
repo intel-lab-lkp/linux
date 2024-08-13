f9c8f3ffc9576e07133be2b875ba594e890cf2ae
Jeongjun Park <aha310510@gmail.com>
2024-08-13 19:07:22 +0900

    net/smc: prevent NULL pointer dereference in txopt_get
    Since smc_inet6_prot does not initialize ipv6_pinfo_offset, inet6_create()
    copies an incorrect address value, sk + 0 (offset), to inet_sk(sk)->pinet6.
    
    In addition, since inet_sk(sk)->pinet6 and smc_sk(sk)->clcsock practically
    point to the same address, when smc_create_clcsk() stores the newly
    created clcsock in smc_sk(sk)->clcsock, inet_sk(sk)->pinet6 is corrupted
    into clcsock. This causes NULL pointer dereference and various other
    memory corruptions.
    
    To solve this, we need to add a smc6_sock structure for ipv6_pinfo_offset
    initialization and modify the smc_sock structure.
    
    Reported-by: syzbot+f69bfae0a4eb29976e44@syzkaller.appspotmail.com
    Tested-by: syzbot+f69bfae0a4eb29976e44@syzkaller.appspotmail.com
    Fixes: d25a92ccae6b ("net/smc: Introduce IPPROTO_SMC")
    Signed-off-by: Jeongjun Park <aha310510@gmail.com>

--- net/smc/smc.h
+++ net/smc/smc.h
@@ -284,15 +284,6 @@ struct smc_connection {
 
 struct smc_sock {				/* smc sock container */
 	struct sock		sk;
-	struct socket		*clcsock;	/* internal tcp socket */
-	void			(*clcsk_state_change)(struct sock *sk);
-						/* original stat_change fct. */
-	void			(*clcsk_data_ready)(struct sock *sk);
-						/* original data_ready fct. */
-	void			(*clcsk_write_space)(struct sock *sk);
-						/* original write_space fct. */
-	void			(*clcsk_error_report)(struct sock *sk);
-						/* original error_report fct. */
 	struct smc_connection	conn;		/* smc connection */
 	struct smc_sock		*listen_smc;	/* listen parent */
 	struct work_struct	connect_work;	/* handle non-blocking connect*/
@@ -325,6 +316,16 @@ struct smc_sock {				/* smc sock container */
 						/* protects clcsock of a listen
 						 * socket
 						 * */
+	struct socket		*clcsock;	/* internal tcp socket */
+	void			(*clcsk_state_change)(struct sock *sk);
+						/* original stat_change fct. */
+	void			(*clcsk_data_ready)(struct sock *sk);
+						/* original data_ready fct. */
+	void			(*clcsk_write_space)(struct sock *sk);
+						/* original write_space fct. */
+	void			(*clcsk_error_report)(struct sock *sk);
+						/* original error_report fct. */
+
 };
 
 #define smc_sk(ptr) container_of_const(ptr, struct smc_sock, sk)
