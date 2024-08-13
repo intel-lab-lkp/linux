d619f9aa05363c068ea7907c83c51d8d0cf15f58
Jacob Keller <jacob.e.keller@intel.com>
2024-08-13 08:55:06 -0400

    iavf: add support for indirect access to PHC time
    Implement support for reading the PHC time indirectly via the
    VIRTCHNL_OP_1588_PTP_GET_TIME operation.
    
    Based on some simple tests with ftrace, the latency of the indirect
    clock access appears to be about ~110 microseconds. This is due to the
    cost of preparing a message to send over the virtchnl queue.
    
    This is expected, due to the increased jitter caused by sending messages
    over virtchnl. It is not easy to control the precise time that the
    message is sent by the VF, or the time that the message is responded to
    by the PF, or the time that the message sent from the PF is received by
    the VF.
    
    For sending the request, note that many PTP related operations will
    require sending of VIRTCHNL messages. Instead of adding a separate AQ
    flag and storage for each operation, setup a simple queue mechanism for
    queuing up virtchnl messages.
    
    Each message will be converted to a iavf_ptp_aq_cmd structure which ends
    with a flexible array member. A single AQ flag is added for processing
    messages from this queue. In principle this could be extended to handle
    arbitrary virtchnl messages. For now it is kept to PTP-specific as the
    need is primarily for handling PTP-related commands.
    
    Use this to implement .gettimex64 using the indirect method via the
    virtchnl command. The response from the PF is processed and stored into
    the cached_phc_time. A wait queue is used to allow the PTP clock gettime
    request to sleep until the message is sent from the PF.
    
    Signed-off-by: Jacob Keller <jacob.e.keller@intel.com>
    Reviewed-by: Wojciech Drewek <wojciech.drewek@intel.com>
    Reviewed-by: Rahul Rameshbabu <rrameshbabu@nvidia.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: Mateusz Polchlopek <mateusz.polchlopek@intel.com>

--- drivers/net/ethernet/intel/iavf/iavf_ptp.h
+++ drivers/net/ethernet/intel/iavf/iavf_ptp.h
@@ -6,9 +6,13 @@
 
 #include "iavf_types.h"
 
+#define iavf_clock_to_adapter(info)				\
+	container_of_const(info, struct iavf_adapter, ptp.info)
+
 void iavf_ptp_init(struct iavf_adapter *adapter);
 void iavf_ptp_release(struct iavf_adapter *adapter);
 void iavf_ptp_process_caps(struct iavf_adapter *adapter);
 bool iavf_ptp_cap_supported(const struct iavf_adapter *adapter, u32 cap);
+void iavf_virtchnl_send_ptp_cmd(struct iavf_adapter *adapter);
 
 #endif /* _IAVF_PTP_H_ */
