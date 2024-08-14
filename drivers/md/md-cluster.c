7d836b4174c72c849f534149427e5dbca6ae5d81
Yu Kuai <yukuai3@huawei.com>
2024-08-14 15:11:07 +0800

    md/md-bitmap: merge get_bitmap_from_slot() into bitmap_operations
    So that the implementation won't be exposed, and it'll be possible
    to invent a new bitmap by replacing bitmap_operations.
    
    Signed-off-by: Yu Kuai <yukuai3@huawei.com>

--- drivers/md/md-cluster.c
+++ drivers/md/md-cluster.c
@@ -1168,7 +1168,7 @@ static int resize_bitmaps(struct mddev *mddev, sector_t newsize, sector_t oldsiz
 		if (i == md_cluster_ops->slot_number(mddev))
 			continue;
 
-		bitmap = get_bitmap_from_slot(mddev, i);
+		bitmap = mddev->bitmap_ops->get_from_slot(mddev, i);
 		if (IS_ERR(bitmap)) {
 			pr_err("can't get bitmap from slot %d\n", i);
 			bitmap = NULL;
@@ -1234,7 +1234,7 @@ static int cluster_check_sync_size(struct mddev *mddev)
 		if (i == current_slot)
 			continue;
 
-		bitmap = get_bitmap_from_slot(mddev, i);
+		bitmap = mddev->bitmap_ops->get_from_slot(mddev, i);
 		if (IS_ERR(bitmap)) {
 			pr_err("can't get bitmap from slot %d\n", i);
 			return -1;
