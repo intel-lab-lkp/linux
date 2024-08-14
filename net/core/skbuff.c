ce44823fd19f59e2e0785e489926b09a4de8edab
Boris Sukholitko <boris.sukholitko@broadcom.com>
2024-08-14 16:06:14 +0300

    skb: move mac_len adjustment out of skb_vlan_flush
    Let its callers worry about skb headers adjustment.
    
    Signed-off-by: Boris Sukholitko <boris.sukholitko@broadcom.com>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -6240,7 +6240,6 @@ static int skb_vlan_flush(struct sk_buff *skb)
 		return err;
 
 	skb->protocol = skb->vlan_proto;
-	skb->mac_len += VLAN_HLEN;
 
 	skb_postpush_rcsum(skb, skb->data + (2 * ETH_ALEN), VLAN_HLEN);
 	return 0;
@@ -6255,6 +6254,8 @@ int skb_vlan_push(struct sk_buff *skb, __be16 vlan_proto, u16 vlan_tci)
 		int err = skb_vlan_flush(skb);
 		if (err)
 			return err;
+
+		skb->mac_len += VLAN_HLEN;
 	}
 	__vlan_hwaccel_put_tag(skb, vlan_proto, vlan_tci);
 	return 0;
