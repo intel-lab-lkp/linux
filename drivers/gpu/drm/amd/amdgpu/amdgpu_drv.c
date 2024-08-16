bc5e484d5b9255aeec5df92f17335c2a3049ffa7
Thomas Zimmermann <tzimmermann@suse.de>
2024-08-16 14:23:31 +0200

    drm/amdgpu: Run DRM default client setup
    Call drm_client_setup() to run the kernel's default client setup
    for DRM. Set fbdev_probe in struct drm_driver, so that the client
    setup can start the common fbdev client.
    
    The amdgpu driver specifies a preferred color mode depending on
    the available video memory, with a default of 32. Adapt this for
    the new client interface.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: "Christian König" <christian.koenig@amd.com>
    Cc: Xinhui Pan <Xinhui.Pan@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -23,6 +23,7 @@
  */
 
 #include <drm/amdgpu_drm.h>
+#include <drm/drm_client_setup.h>
 #include <drm/drm_drv.h>
 #include <drm/drm_fbdev_ttm.h>
 #include <drm/drm_gem.h>
@@ -2343,9 +2344,9 @@ retry_init:
 	    !list_empty(&adev_to_drm(adev)->mode_config.connector_list)) {
 		/* select 8 bpp console on low vram cards */
 		if (adev->gmc.real_vram_size <= (32*1024*1024))
-			drm_fbdev_ttm_setup(adev_to_drm(adev), 8);
+			drm_client_setup(adev_to_drm(adev), drm_format_info(DRM_FORMAT_C8));
 		else
-			drm_fbdev_ttm_setup(adev_to_drm(adev), 32);
+			drm_client_setup(adev_to_drm(adev), NULL);
 	}
 
 	ret = amdgpu_debugfs_init(adev);
@@ -2957,6 +2958,7 @@ static const struct drm_driver amdgpu_kms_driver = {
 	.num_ioctls = ARRAY_SIZE(amdgpu_ioctls_kms),
 	.dumb_create = amdgpu_mode_dumb_create,
 	.dumb_map_offset = amdgpu_mode_dumb_mmap,
+	DRM_FBDEV_TTM_DRIVER_OPS,
 	.fops = &amdgpu_driver_kms_fops,
 	.release = &amdgpu_driver_release_kms,
 #ifdef CONFIG_PROC_FS
@@ -2983,6 +2985,7 @@ const struct drm_driver amdgpu_partition_driver = {
 	.num_ioctls = ARRAY_SIZE(amdgpu_ioctls_kms),
 	.dumb_create = amdgpu_mode_dumb_create,
 	.dumb_map_offset = amdgpu_mode_dumb_mmap,
+	DRM_FBDEV_TTM_DRIVER_OPS,
 	.fops = &amdgpu_driver_kms_fops,
 	.release = &amdgpu_driver_release_kms,
 
