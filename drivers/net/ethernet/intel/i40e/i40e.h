a0dd857f0e94136a326ada1841482607f2f3939d
Aleksandr Loktionov <aleksandr.loktionov@intel.com>
2024-08-19 11:27:56 +0200

    i40e: Add Energy Efficient Ethernet ability for X710 Base-T/KR/KX cards
    Add "EEE: Enabled/Disabled" to dmesg for supported X710 Base-T/KR/KX cards.
    According to the IEEE standard report the EEE ability and and the
    EEE Link Partner ability. Use the kernel's 'ethtool_keee' structure
    and report EEE link modes.
    
    Example:
    dmesg | grep 'NIC Link is'
    ethtool --show-eee <device>
    
    Before:
    	NIC Link is Up, 10 Gbps Full Duplex, Flow Control: None
    
            Supported EEE link modes:  Not reported
            Advertised EEE link modes:  Not reported
            Link partner advertised EEE link modes:  Not reported
    
    After:
    	NIC Link is Up, 10 Gbps Full Duplex, Flow Control: None, EEE: Enabled
    
            Supported EEE link modes:  100baseT/Full
                                       1000baseT/Full
                                       10000baseT/Full
            Advertised EEE link modes:  100baseT/Full
                                        1000baseT/Full
                                        10000baseT/Full
            Link partner advertised EEE link modes:  100baseT/Full
                                                     1000baseT/Full
                                                     10000baseT/Full
    
    Reviewed-by: Arkadiusz Kubalewski <arkadiusz.kubalewski@intel.com>
    Signed-off-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Reviewed-by: Jacob Keller <jacob.e.keller@intel.com>
    Reviewed-by: Simon Horman <horms@kernel.org>

--- drivers/net/ethernet/intel/i40e/i40e.h
+++ drivers/net/ethernet/intel/i40e/i40e.h
@@ -7,6 +7,7 @@
 #include <linux/pci.h>
 #include <linux/ptp_clock_kernel.h>
 #include <linux/types.h>
+#include <linux/linkmode.h>
 #include <linux/avf/virtchnl.h>
 #include <linux/net/intel/i40e_client.h>
 #include <net/devlink.h>
