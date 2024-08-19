8bc6b3a8e82f8d2803cc59c7255be4f769a7e41c
Uros Bizjak <ubizjak@gmail.com>
2024-08-19 10:33:13 +0200

    x86/boot/64: Strip percpu address space from gdt kernel load
    init_per_cpu_var() returns pointer in the percpu address space while
    rip_rel_ptr() expects pointer in the generic address space, resulting
    in:
    
    asm.h:124:63: error: passing argument 1 of ‘rip_rel_ptr’ from pointer to non-enclosed address space
    
    when strict address space checks are enabled.
    
    Add explicit casts to remove address space of the returned pointer.
    
    Found by GCC's named address space checks.
    
    There were no changes in the resulting object file.
    
    Fixes: 11e36b0f7c21 ("x86/boot/64: Load the final kernel GDT during early boot directly, remove startup_gdt[]")
    Signed-off-by: Uros Bizjak <ubizjak@gmail.com>
    Cc: Brian Gerst <brgerst@gmail.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>

--- arch/x86/kernel/head64.c
+++ arch/x86/kernel/head64.c
@@ -560,9 +560,11 @@ void early_setup_idt(void)
 void __head startup_64_setup_gdt_idt(void)
 {
 	void *handler = NULL;
+	struct desc_struct *gdt =
+		(void *)(__force unsigned long)init_per_cpu_var(gdt_page.gdt);
 
 	struct desc_ptr startup_gdt_descr = {
-		.address = (unsigned long)&RIP_REL_REF(init_per_cpu_var(gdt_page.gdt)),
+		.address = (unsigned long)&RIP_REL_REF(*gdt),
 		.size    = GDT_SIZE - 1,
 	};
 
