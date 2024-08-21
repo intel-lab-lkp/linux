c39c9ce0d392c56b633c330c77a37b3f8d566978
Qi Zheng <zhengqi.arch@bytedance.com>
2024-08-21 16:18:49 +0800

    mm: handle_pte_fault() use pte_offset_map_maywrite_nolock()
    In handle_pte_fault(), we may modify the vmf->pte after acquiring the
    vmf->ptl, so convert it to using pte_offset_map_maywrite_nolock(). But
    since we already do the pte_same() check, so there is no need to get
    pmdval to do pmd_same() check, just pass NULL to pmdvalp parameter.
    
    Signed-off-by: Qi Zheng <zhengqi.arch@bytedance.com>

--- mm/memory.c
+++ mm/memory.c
@@ -5504,9 +5504,14 @@ static vm_fault_t handle_pte_fault(struct vm_fault *vmf)
 		 * pmd by anon khugepaged, since that takes mmap_lock in write
 		 * mode; but shmem or file collapse to THP could still morph
 		 * it into a huge pmd: just retry later if so.
+		 *
+		 * Use the maywrite version to indicate that vmf->pte will be
+		 * modified, but since we will use pte_same() to detect the
+		 * change of the pte entry, there is no need to get pmdval.
 		 */
-		vmf->pte = pte_offset_map_nolock(vmf->vma->vm_mm, vmf->pmd,
-						 vmf->address, &vmf->ptl);
+		vmf->pte = pte_offset_map_maywrite_nolock(vmf->vma->vm_mm,
+							  vmf->pmd, vmf->address,
+							  NULL, &vmf->ptl);
 		if (unlikely(!vmf->pte))
 			return 0;
 		vmf->orig_pte = ptep_get_lockless(vmf->pte);
