e31ac4946c0cb77f352d128585ef23569dc8662c
Tom Herbert <tom@herbertland.com>
2024-08-21 14:22:06 -0700

    flow_dissector: Parse foo-over-udp (FOU)
    Parse FOU by getting the FOU protocol from the matching socket.
    This includes moving "struct fou" and "fou_from_sock" to fou.h
    
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Tom Herbert <tom@herbertland.com>

--- include/net/fou.h
+++ include/net/fou.h
@@ -17,6 +17,22 @@ int __fou_build_header(struct sk_buff *skb, struct ip_tunnel_encap *e,
 int __gue_build_header(struct sk_buff *skb, struct ip_tunnel_encap *e,
 		       u8 *protocol, __be16 *sport, int type);
 
+struct fou {
+	struct socket *sock;
+	u8 protocol;
+	u8 flags;
+	__be16 port;
+	u8 family;
+	u16 type;
+	struct list_head list;
+	struct rcu_head rcu;
+};
+
+static inline struct fou *fou_from_sock(struct sock *sk)
+{
+	return sk->sk_user_data;
+}
+
 int register_fou_bpf(void);
 
 #endif
