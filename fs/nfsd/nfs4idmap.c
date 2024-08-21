b3102a6611f503209cdec846021e462fa5e4da8a
Guoqing Jiang <guoqing.jiang@linux.dev>
2024-08-21 22:03:18 +0800

    nfsd: call cache_put if xdr_reserve_space returns NULL
    If no enough buffer space available, but idmap_lookup have triggered
    lookup_fn which calls cache_get and returns successfully. Then we
    missed to call cache_put here which pairs with cache_get.
    
    Signed-off-by: Guoqing Jiang <guoqing.jiang@linux.dev>

--- fs/nfsd/nfs4idmap.c
+++ fs/nfsd/nfs4idmap.c
@@ -594,8 +594,10 @@ static __be32 idmap_id_to_name(struct xdr_stream *xdr,
 	ret = strlen(item->name);
 	WARN_ON_ONCE(ret > IDMAP_NAMESZ);
 	p = xdr_reserve_space(xdr, ret + 4);
-	if (!p)
+	if (!p) {
+		cache_put(&item->h, nn->idtoname_cache);
 		return nfserr_resource;
+	}
 	p = xdr_encode_opaque(p, item->name, ret);
 	cache_put(&item->h, nn->idtoname_cache);
 	return 0;
