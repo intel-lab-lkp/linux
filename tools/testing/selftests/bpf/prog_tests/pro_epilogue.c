dc335d8f8457a33acead459d7f9a17f9a73b4e21
Martin KaFai Lau <martin.lau@kernel.org>
2024-08-21 16:34:35 -0700

    selftests/bpf: Add tailcall epilogue test
    This patch adds a gen_epilogue test to test a main prog
    using a bpf_tail_call.
    
    A non test_loader test is used. The tailcall target program,
    "test_epilogue_subprog", needs to be used in a struct_ops map
    before it can be loaded. Another struct_ops map is also needed
    to host the actual "test_epilogue_tailcall" struct_ops program
    that does the bpf_tail_call. The earlier test_loader patch
    will attach all struct_ops maps but the bpf_testmod.c does
    not support >1 attached struct_ops.
    
    The earlier patch used the test_loader which has already covered
    checking for the patched pro/epilogue instructions. This is done
    by the __xlated tag.
    
    This patch goes for the regular skel load and syscall test to do
    the tailcall test that can also allow to directly pass the
    the "struct st_ops_args *args" as ctx_in to the
    SEC("syscall") program.
    
    Signed-off-by: Martin KaFai Lau <martin.lau@kernel.org>

--- tools/testing/selftests/bpf/prog_tests/pro_epilogue.c
+++ tools/testing/selftests/bpf/prog_tests/pro_epilogue.c
@@ -4,9 +4,47 @@
 #include <test_progs.h>
 #include "pro_epilogue_subprog.skel.h"
 #include "pro_epilogue_kfunc.skel.h"
+#include "epilogue_tailcall.skel.h"
+
+struct st_ops_args {
+	int a;
+};
+
+static void test_tailcall(void)
+{
+	LIBBPF_OPTS(bpf_test_run_opts, topts);
+	struct epilogue_tailcall *skel;
+	struct st_ops_args args;
+	int err, prog_fd;
+
+	skel = epilogue_tailcall__open_and_load();
+	if (!ASSERT_OK_PTR(skel, "epilogue_tailcall__open_and_load"))
+		return;
+
+	topts.ctx_in = &args;
+	topts.ctx_size_in = sizeof(args);
+
+	skel->links.epilogue_tailcall =
+		bpf_map__attach_struct_ops(skel->maps.epilogue_tailcall);
+	if (!ASSERT_OK_PTR(skel->links.epilogue_tailcall, "attach_struct_ops"))
+		goto done;
+
+	/* tailcall prog + gen_epilogue */
+	memset(&args, 0, sizeof(args));
+	prog_fd = bpf_program__fd(skel->progs.syscall_epilogue_tailcall);
+	err = bpf_prog_test_run_opts(prog_fd, &topts);
+	ASSERT_OK(err, "bpf_prog_test_run_opts");
+	ASSERT_EQ(args.a, 10001, "args.a");
+	ASSERT_EQ(topts.retval, 10001 * 2, "topts.retval");
+
+done:
+	epilogue_tailcall__destroy(skel);
+}
 
 void test_pro_epilogue(void)
 {
 	RUN_TESTS(pro_epilogue_subprog);
 	RUN_TESTS(pro_epilogue_kfunc);
+	if (test__start_subtest("tailcall"))
+		test_tailcall();
 }
