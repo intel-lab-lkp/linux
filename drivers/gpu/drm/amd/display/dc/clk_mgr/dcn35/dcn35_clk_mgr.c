bf5accafe7f0218c505e575187d4e4a23b9cf550
Jinjie Ruan <ruanjinjie@huawei.com>
2024-08-21 14:40:40 +0800

    drm/amd/display: Make dcn35_fpga_funcs static
    The sparse tool complains as follows:
    
    drivers/gpu/drm/amd/amdgpu/../display/dc/clk_mgr/dcn35/dcn35_clk_mgr.c:1071:22: warning:
    	symbol 'dcn35_fpga_funcs' was not declared. Should it be static?
    
    This symbol is not used outside of dcn35_clk_mgr.c, so marks it static.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- drivers/gpu/drm/amd/display/dc/clk_mgr/dcn35/dcn35_clk_mgr.c
+++ drivers/gpu/drm/amd/display/dc/clk_mgr/dcn35/dcn35_clk_mgr.c
@@ -1068,7 +1068,7 @@ static struct clk_mgr_funcs dcn35_funcs = {
 	.is_ips_supported = dcn35_is_ips_supported,
 };
 
-struct clk_mgr_funcs dcn35_fpga_funcs = {
+static struct clk_mgr_funcs dcn35_fpga_funcs = {
 	.get_dp_ref_clk_frequency = dce12_get_dp_ref_freq_khz,
 	.update_clocks = dcn35_update_clocks_fpga,
 	.init_clocks = dcn35_init_clocks_fpga,
