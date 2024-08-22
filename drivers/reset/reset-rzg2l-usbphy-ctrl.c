83a4a03fe48e43fdb01ca0866a0180a3c9315475
Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
2024-08-22 18:27:53 +0300

    reset: rzg2l-usbphy-ctrl: Add support for RZ/G3S
    Add support for RZ/G3S SoC. It needs its own compatible as it uses 2
    reset signals and it cannot work w/o both of them. To be able to
    fully validate this on DT schema, too, the RZ/G3S uses it's own compatible
    w/o a fallback (as if the fallback will be used the RZ/G3S will not work
    anyway).
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>

--- drivers/reset/reset-rzg2l-usbphy-ctrl.c
+++ drivers/reset/reset-rzg2l-usbphy-ctrl.c
@@ -93,6 +93,7 @@ static int rzg2l_usbphy_ctrl_status(struct reset_controller_dev *rcdev,
 
 static const struct of_device_id rzg2l_usbphy_ctrl_match_table[] = {
 	{ .compatible = "renesas,rzg2l-usbphy-ctrl" },
+	{ .compatible = "renesas,r9a08g045-usbphy-ctrl" },
 	{ /* Sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, rzg2l_usbphy_ctrl_match_table);
