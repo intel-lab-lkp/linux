fbbbe5705f7a90863074a348ea0198b0b06f70d1
Jijie Shao <shaojijie@huawei.com>
2024-08-22 17:33:30 +0800

    net: hibmcge: Implement rx_poll function to receive packets
    Implement rx_poll function to read the rx descriptor after
    receiving the rx interrupt. Adjust the skb based on the
    descriptor to complete the reception of the packet.
    
    Signed-off-by: Jijie Shao <shaojijie@huawei.com>

--- drivers/net/ethernet/hisilicon/hibmcge/hbg_irq.c
+++ drivers/net/ethernet/hisilicon/hibmcge/hbg_irq.c
@@ -19,12 +19,18 @@ static void hbg_irq_handle_tx(struct hbg_priv *priv,
 	napi_schedule(&priv->tx_ring.napi);
 }
 
+static void hbg_irq_handle_rx(struct hbg_priv *priv,
+			      struct hbg_irq_info *irq_info)
+{
+	napi_schedule(&priv->rx_ring.napi);
+}
+
 #define HBG_TXRX_IRQ_I(name, mask, handle) {name, mask, false, false, 0, handle}
 #define HBG_ERR_IRQ_I(name, mask, need_print) \
 			{name, mask, true, need_print, 0, hbg_irq_handle_err}
 
 static struct hbg_irq_info hbg_irqs[] = {
-	HBG_TXRX_IRQ_I("RX", HBG_INT_MSK_RX_B, NULL),
+	HBG_TXRX_IRQ_I("RX", HBG_INT_MSK_RX_B, hbg_irq_handle_rx),
 	HBG_TXRX_IRQ_I("TX", HBG_INT_MSK_TX_B, hbg_irq_handle_tx),
 	HBG_ERR_IRQ_I("MAC_MII_FIFO_ERR", HBG_INT_MSK_MAC_MII_FF_ERR_B, true),
 	HBG_ERR_IRQ_I("MAC_PCS_RX_FIFO_ERR", HBG_INT_MSK_MAC_PCS_RXFF_ERR_B, true),
