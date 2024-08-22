65df6f11e64249735c2143a77def646a397ff8d5
Vasiliy Kovalev <kovalev@altlinux.org>
2024-08-22 19:12:18 +0300

    bfs: prevent null pointer dereference in bfs_move_block()
    Detect a failed sb_getblk() call (before copying data)
    so that null pointer dereferences should not happen any more.
    We also decrement (brelse) the bh counter in this case.
    
    Found when launching the reproducer generated by Syzkaller:
    
    KASAN: null-ptr-deref in range [0x0000000000000028-0x000000000000002f]
    Comm: mark_buffer_dir Tainted: G  W  6.1.105-un-def-alt1.kasan #1
    RIP: 0010:bfs_get_block+0x35b/0xdf0 (fs/bfs/file.c:42) [bfs]
    Call Trace:
    <TASK>
     __die_body.cold (arch/x86/kernel/dumpstack.c:478)
     die_addr (arch/x86/kernel/dumpstack.c:462)
     exc_general_protection (arch/x86/kernel/traps.c:787)
     asm_exc_general_protection (./arch/x86/include/asm/idtentry.h:564)
     __getblk_gfp (fs/buffer.c:1335)
     bfs_get_block (fs/bfs/file.c:42) bfs
     bfs_get_block (fs/bfs/file.c:56) bfs
     bfs_get_block (fs/bfs/file.c:125) bfs
     bfs_write_begin (fs/bfs/file.c:66) bfs
     __block_write_begin_int (fs/buffer.c:1991)
     bfs_write_begin (fs/bfs/file.c:66) bfs
     invalidate_bh_lrus_cpu (fs/buffer.c:1955)
     fault_in_readable (mm/gup.c:1898)
     PageHeadHuge (mm/hugetlb.c:2123)
     bfs_write_begin (fs/bfs/file.c:66) bfs
     block_write_begin (fs/buffer.c:2103)
     bfs_write_begin (fs/bfs/file.c:178) bfs
     generic_perform_write (mm/filemap.c:3817)
     generic_file_readonly_mmap (mm/filemap.c:3781)
     new_inode (fs/inode.c:2126)
     generic_write_checks (fs/read_write.c:1687)
     __generic_file_write_iter (mm/filemap.c:3946)
     generic_file_write_iter (./include/linux/fs.h:763 mm/filemap.c:3978)
     vfs_write (./include/linux/fs.h:2265 fs/read_write.c:491)
     kernel_write (fs/read_write.c:565)
     __fget_files (fs/file.c:918)
     ksys_write (fs/read_write.c:638)
    
    Fixes: 1da177e4c3f4 ("Linux-2.6.12-rc2")
    Reported-by: syzbot+d98fd19acd08b36ff422@syzkaller.appspotmail.com
    Link: https://syzkaller.appspot.com/bug?extid=d98fd19acd08b36ff422
    Link: https://syzkaller.appspot.com/text?tag=ReproC&x=16515ba3e80000
    Cc: stable@vger.kernel.org
    Signed-off-by: Vasiliy Kovalev <kovalev@altlinux.org>

--- fs/bfs/file.c
+++ fs/bfs/file.c
@@ -40,6 +40,10 @@ static int bfs_move_block(unsigned long from, unsigned long to,
 	if (!bh)
 		return -EIO;
 	new = sb_getblk(sb, to);
+	if (!new) {
+		brelse(bh);
+		return -EIO;
+	}
 	memcpy(new->b_data, bh->b_data, bh->b_size);
 	mark_buffer_dirty(new);
 	bforget(bh);
