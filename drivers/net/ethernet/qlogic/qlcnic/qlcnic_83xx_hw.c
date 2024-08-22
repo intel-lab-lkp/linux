08297400a3f5aade8a80569cbf663426dfd3b832
Colin Ian King <colin.i.king@gmail.com>
2024-08-22 22:10:28 +0100

    qlcnic: make read-only const array key static
    Don't populate the const read-only array key on the stack at
    run time, instead make it static.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- drivers/net/ethernet/qlogic/qlcnic/qlcnic_83xx_hw.c
+++ drivers/net/ethernet/qlogic/qlcnic/qlcnic_83xx_hw.c
@@ -2045,9 +2045,11 @@ int qlcnic_83xx_config_rss(struct qlcnic_adapter *adapter, int enable)
 	int err;
 	u32 word;
 	struct qlcnic_cmd_args cmd;
-	const u64 key[] = { 0xbeac01fa6a42b73bULL, 0x8030f20c77cb2da3ULL,
-			    0xae7b30b4d0ca2bcbULL, 0x43a38fb04167253dULL,
-			    0x255b0ec26d5a56daULL };
+	static const u64 key[] = {
+		0xbeac01fa6a42b73bULL, 0x8030f20c77cb2da3ULL,
+		0xae7b30b4d0ca2bcbULL, 0x43a38fb04167253dULL,
+		0x255b0ec26d5a56daULL
+	};
 
 	err = qlcnic_alloc_mbx_args(&cmd, adapter, QLCNIC_CMD_CONFIGURE_RSS);
 	if (err)
