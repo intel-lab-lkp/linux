772f818f1df662a9a4c50e9943064318602e6bde
Qi Zheng <zhengqi.arch@bytedance.com>
2024-08-22 15:13:23 +0800

    mm: copy_pte_range() use pte_offset_map_rw_nolock()
    In copy_pte_range(), we may modify the src_pte entry after holding the
    src_ptl, so convert it to using pte_offset_map_rw_nolock(). But since we
    already hold the write lock of mmap_lock, there is no need to get pmdval
    to do pmd_same() check, just pass a dummy variable to it.
    
    Signed-off-by: Qi Zheng <zhengqi.arch@bytedance.com>

--- mm/memory.c
+++ mm/memory.c
@@ -1083,6 +1083,7 @@ copy_pte_range(struct vm_area_struct *dst_vma, struct vm_area_struct *src_vma,
 	struct mm_struct *src_mm = src_vma->vm_mm;
 	pte_t *orig_src_pte, *orig_dst_pte;
 	pte_t *src_pte, *dst_pte;
+	pmd_t dummy_pmdval;
 	pte_t ptent;
 	spinlock_t *src_ptl, *dst_ptl;
 	int progress, max_nr, ret = 0;
@@ -1108,7 +1109,15 @@ again:
 		ret = -ENOMEM;
 		goto out;
 	}
-	src_pte = pte_offset_map_nolock(src_mm, src_pmd, addr, &src_ptl);
+
+	/*
+	 * Use the maywrite version to indicate that dst_pte will be modified,
+	 * but since we already hold the write lock of mmap_lock, there is no
+	 * need to get pmdval to do pmd_same() check, just pass a dummy variable
+	 * to it.
+	 */
+	src_pte = pte_offset_map_rw_nolock(src_mm, src_pmd, addr, &dummy_pmdval,
+					   &src_ptl);
 	if (!src_pte) {
 		pte_unmap_unlock(dst_pte, dst_ptl);
 		/* ret == 0 */
