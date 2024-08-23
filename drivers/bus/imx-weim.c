c3db70d7592916ea835c15e5017224e45e7c3743
Wu Bo <bo.wu@vivo.com>
2024-08-23 04:22:11 -0600

    bus: imx-weim: change to use devm_clk_get_enabled() helpers
    Make the code cleaner and avoid call clk_disable_unprepare()
    
    Signed-off-by: Wu Bo <bo.wu@vivo.com>

--- drivers/bus/imx-weim.c
+++ drivers/bus/imx-weim.c
@@ -282,19 +282,13 @@ static int weim_probe(struct platform_device *pdev)
 	dev_set_drvdata(&pdev->dev, priv);
 
 	/* get the clock */
-	clk = devm_clk_get(&pdev->dev, NULL);
+	clk = devm_clk_get_enabled(&pdev->dev, NULL);
 	if (IS_ERR(clk))
 		return PTR_ERR(clk);
 
-	ret = clk_prepare_enable(clk);
-	if (ret)
-		return ret;
-
 	/* parse the device node */
 	ret = weim_parse_dt(pdev);
-	if (ret)
-		clk_disable_unprepare(clk);
-	else
+	if (!ret)
 		dev_info(&pdev->dev, "Driver registered.\n");
 
 	return ret;
