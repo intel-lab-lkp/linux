b79001638a5dba41fc55a3bbf3bed8455b073e3c
Christopher Bednarz <christopher.n.bednarz@intel.com>
2024-08-23 22:19:11 -0500

    RDMA/irdma: Discover and set up GEN3 hardware register layout
    Discover the hardware register layout for GEN3 devices through an RDMA
    virtual channel operation with the Control Plane (CP). Set up the
    corresponding hardware attributes specific to GEN3 devices.
    
    Signed-off-by: Christopher Bednarz <christopher.n.bednarz@intel.com>
    Signed-off-by: Tatyana Nikolova <tatyana.e.nikolova@intel.com>

--- drivers/infiniband/hw/irdma/ig3rdma_hw.h
+++ drivers/infiniband/hw/irdma/ig3rdma_hw.h
@@ -3,9 +3,27 @@
 #ifndef IG3RDMA_HW_H
 #define IG3RDMA_HW_H
 
+#define IG3_MAX_APFS 1
+#define IG3_MAX_AVFS 0
+
 #define IG3_PF_RDMA_REGION_OFFSET 0xBC00000
 #define IG3_PF_RDMA_REGION_LEN 0x401000
 #define IG3_VF_RDMA_REGION_OFFSET 0x8C00
 #define IG3_VF_RDMA_REGION_LEN 0x8400
 
+enum ig3rdma_device_caps_const {
+	IG3RDMA_MAX_WQ_FRAGMENT_COUNT		= 14,
+	IG3RDMA_MAX_SGE_RD			= 14,
+
+	IG3RDMA_MAX_STATS_COUNT			= 128,
+
+	IG3RDMA_MAX_IRD_SIZE			= 2048,
+	IG3RDMA_MAX_ORD_SIZE			= 2048,
+	IG3RDMA_MIN_WQ_SIZE			= 16 /* WQEs */,
+	IG3RDMA_MAX_INLINE_DATA_SIZE		= 216,
+	IG3RDMA_MAX_PF_PUSH_PAGE_COUNT		= 8192,
+	IG3RDMA_MAX_VF_PUSH_PAGE_COUNT		= 16,
+};
+
+void __iomem *ig3rdma_get_reg_addr(struct irdma_hw *hw, u64 reg_offset);
 #endif /* IG3RDMA_HW_H*/
