93bb882952ed0c3ffe9190349eea1d06d2c929aa
Siddharth Vadapalli <s-vadapalli@ti.com>
2024-08-24 12:51:35 +0530

    PCI: dra7xx: Fix threaded IRQ handler registration
    Commit da87d35a6e51 ("PCI: dra7xx: Use threaded IRQ handler for
    "dra7xx-pcie-main" IRQ") switched from devm_request_irq() to
    devm_request_threaded_irq(). In this process, the "handler" and the
    "thread_fn" parameters were erroneously interchanged, with "NULL" being
    passed as the "handler" and "dra7xx_pcie_irq_handler()" being registered
    as the function to be called in a threaded interrupt context.
    
    Fix this by interchanging the "handler" and "thread_fn" parameters.
    While at it, correct the indentation.
    
    Fixes: da87d35a6e51 ("PCI: dra7xx: Use threaded IRQ handler for "dra7xx-pcie-main" IRQ")
    Cc: <stable@vger.kernel.org>
    Reported-by: Udit Kumar <u-kumar1@ti.com>
    Signed-off-by: Siddharth Vadapalli <s-vadapalli@ti.com>

--- drivers/pci/controller/dwc/pci-dra7xx.c
+++ drivers/pci/controller/dwc/pci-dra7xx.c
@@ -849,8 +849,9 @@ static int dra7xx_pcie_probe(struct platform_device *pdev)
 	}
 	dra7xx->mode = mode;
 
-	ret = devm_request_threaded_irq(dev, irq, NULL, dra7xx_pcie_irq_handler,
-			       IRQF_SHARED, "dra7xx-pcie-main", dra7xx);
+	ret = devm_request_threaded_irq(dev, irq, dra7xx_pcie_irq_handler, NULL,
+					IRQF_SHARED, "dra7xx-pcie-main",
+					dra7xx);
 	if (ret) {
 		dev_err(dev, "failed to request irq\n");
 		goto err_gpio;
