efe4664ec55f271b13cda9720506d46df8defe65
Kanchan Joshi <joshi.k@samsung.com>
2024-08-26 22:36:02 +0530

    fs, block: refactor enum rw_hint
    Rename enum rw_hint to rw_life_hint.
    Change i_write_hint (in inode), bi_write_hint (in bio) and write_hint
    (in request) to use u8 data-type rather than this enum.
    
    This is in preparation to introduce a new write hint type.
    
    Signed-off-by: Kanchan Joshi <joshi.k@samsung.com>

--- include/linux/rw_hint.h
+++ include/linux/rw_hint.h
@@ -7,18 +7,13 @@
 #include <uapi/linux/fcntl.h>
 
 /* Block storage write lifetime hint values. */
-enum rw_hint {
+enum rw_life_hint {
 	WRITE_LIFE_NOT_SET	= RWH_WRITE_LIFE_NOT_SET,
 	WRITE_LIFE_NONE		= RWH_WRITE_LIFE_NONE,
 	WRITE_LIFE_SHORT	= RWH_WRITE_LIFE_SHORT,
 	WRITE_LIFE_MEDIUM	= RWH_WRITE_LIFE_MEDIUM,
 	WRITE_LIFE_LONG		= RWH_WRITE_LIFE_LONG,
 	WRITE_LIFE_EXTREME	= RWH_WRITE_LIFE_EXTREME,
-} __packed;
-
-/* Sparse ignores __packed annotations on enums, hence the #ifndef below. */
-#ifndef __CHECKER__
-static_assert(sizeof(enum rw_hint) == 1);
-#endif
+};
 
 #endif /* _LINUX_RW_HINT_H */
