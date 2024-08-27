cc6e782d99f6b3e51add9e0c23957f5272aa6898
Fedor Pchelkin <pchelkin@ispras.ru>
2024-08-27 18:12:43 +0300

    btrfs: qgroup: add missing extent changeset release
    The extent changeset may have some additional memory dynamically allocated
    for ulist in result of clear_record_extent_bits() execution.
    
    Release it after the local changeset is no longer needed in
    BTRFS_QGROUP_MODE_DISABLED case.
    
    Found by Linux Verification Center (linuxtesting.org) with Syzkaller.
    
    Reported-by: syzbot+81670362c283f3dd889c@syzkaller.appspotmail.com
    Closes: https://lore.kernel.org/lkml/000000000000aa8c0c060ade165e@google.com
    Fixes: af0e2aab3b70 ("btrfs: qgroup: flush reservations during quota disable")
    Cc: stable@vger.kernel.org # 6.10+
    Signed-off-by: Fedor Pchelkin <pchelkin@ispras.ru>

--- fs/btrfs/qgroup.c
+++ fs/btrfs/qgroup.c
@@ -4347,9 +4347,10 @@ static int __btrfs_qgroup_release_data(struct btrfs_inode *inode,
 
 	if (btrfs_qgroup_mode(inode->root->fs_info) == BTRFS_QGROUP_MODE_DISABLED) {
 		extent_changeset_init(&changeset);
-		return clear_record_extent_bits(&inode->io_tree, start,
-						start + len - 1,
-						EXTENT_QGROUP_RESERVED, &changeset);
+		ret = clear_record_extent_bits(&inode->io_tree, start,
+					       start + len - 1,
+					       EXTENT_QGROUP_RESERVED, &changeset);
+		goto out;
 	}
 
 	/* In release case, we shouldn't have @reserved */
