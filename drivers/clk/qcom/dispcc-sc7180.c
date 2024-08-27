7fd8ecda82ea4896a69472f8adc61d92200c9dd8
Stephen Boyd <swboyd@chromium.org>
2024-08-27 16:12:34 -0700

    clk: qcom: dispcc-sc7180: Only park display clks at init
    Amit Pundir reports that audio and USB-C host mode stops working on
    sm8550 if the gcc_usb30_prim_master_clk_src clk is registered and
    clk_rcg2_shared_init() parks it on XO.
    
    Partially revert commit 01a0a6cc8cfd ("clk: qcom: Park shared RCGs upon
    registration") by skipping the parking bit, and make that the default
    for shared RCGs, but keep the part where we cache the config register as
    that's still necessary to figure out the true parent of the clk at
    registration time. Move the logic from clk_rcg2_shared_init() to another
    clk_ops structure for use by the display clks on sc7180 to minimize the
    impact of that commit on other qcom SoCs that can't handle the parking
    part.
    
    Fixes: 01a0a6cc8cfd ("clk: qcom: Park shared RCGs upon registration")
    Cc: Konrad Dybcio <konradybcio@kernel.org>
    Cc: Bjorn Andersson <andersson@kernel.org>
    Cc: Taniya Das <quic_tdas@quicinc.com>
    Cc: Neil Armstrong <neil.armstrong@linaro.org>
    Reported-by: Amit Pundir <amit.pundir@linaro.org>
    Closes: https://lore.kernel.org/CAMi1Hd1KQBE4kKUdAn8E5FV+BiKzuv+8FoyWQrrTHPDoYTuhgA@mail.gmail.com
    Signed-off-by: Stephen Boyd <swboyd@chromium.org>

--- drivers/clk/qcom/dispcc-sc7180.c
+++ drivers/clk/qcom/dispcc-sc7180.c
@@ -154,7 +154,7 @@ static struct clk_rcg2 disp_cc_mdss_ahb_clk_src = {
 		.parent_data = disp_cc_parent_data_4,
 		.num_parents = ARRAY_SIZE(disp_cc_parent_data_4),
 		.flags = CLK_SET_RATE_PARENT,
-		.ops = &clk_rcg2_shared_ops,
+		.ops = &clk_rcg2_shared_init_park_ops,
 	},
 };
 
@@ -263,7 +263,7 @@ static struct clk_rcg2 disp_cc_mdss_mdp_clk_src = {
 		.name = "disp_cc_mdss_mdp_clk_src",
 		.parent_data = disp_cc_parent_data_3,
 		.num_parents = ARRAY_SIZE(disp_cc_parent_data_3),
-		.ops = &clk_rcg2_shared_ops,
+		.ops = &clk_rcg2_shared_init_park_ops,
 	},
 };
 
@@ -291,7 +291,7 @@ static struct clk_rcg2 disp_cc_mdss_rot_clk_src = {
 		.name = "disp_cc_mdss_rot_clk_src",
 		.parent_data = disp_cc_parent_data_3,
 		.num_parents = ARRAY_SIZE(disp_cc_parent_data_3),
-		.ops = &clk_rcg2_shared_ops,
+		.ops = &clk_rcg2_shared_init_park_ops,
 	},
 };
 
@@ -305,7 +305,7 @@ static struct clk_rcg2 disp_cc_mdss_vsync_clk_src = {
 		.name = "disp_cc_mdss_vsync_clk_src",
 		.parent_data = disp_cc_parent_data_0,
 		.num_parents = ARRAY_SIZE(disp_cc_parent_data_0),
-		.ops = &clk_rcg2_shared_ops,
+		.ops = &clk_rcg2_shared_init_park_ops,
 	},
 };
 
