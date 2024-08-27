2263ad16f0de2bc5169def47f0cde8ce7ced6229
Philipp Stanner <pstanner@redhat.com>
2024-08-27 09:45:22 +0200

    drm/sched: Fix UB pointer dereference
    In drm_sched_job_init(), commit 56e449603f0a ("drm/sched: Convert the
    GPU scheduler to variable number of run-queues") implemented a call to
    drm_err(), which uses the job's scheduler pointer as a parameter.
    job->sched, however, is not yet valid as it gets set by
    drm_sched_job_arm(), which is always called after drm_sched_job_init().
    
    Since the scheduler code has no control over how the API-User has
    allocated or set 'job', the pointer's dereference is undefined behavior.
    
    Fix the UB by replacing drm_err() with pr_err().
    
    Cc: <stable@vger.kernel.org>	# 6.7+
    Fixes: 56e449603f0a ("drm/sched: Convert the GPU scheduler to variable number of run-queues")
    Reported-by: Danilo Krummrich <dakr@redhat.com>
    Closes: https://lore.kernel.org/lkml/20231108022716.15250-1-dakr@redhat.com/
    Signed-off-by: Philipp Stanner <pstanner@redhat.com>

--- drivers/gpu/drm/scheduler/sched_main.c
+++ drivers/gpu/drm/scheduler/sched_main.c
@@ -788,7 +788,7 @@ int drm_sched_job_init(struct drm_sched_job *job,
 		 * or worse--a blank screen--leave a trail in the
 		 * logs, so this can be debugged easier.
 		 */
-		drm_err(job->sched, "%s: entity has no rq!\n", __func__);
+		pr_err("*ERROR* %s: entity has no rq!\n", __func__);
 		return -ENOENT;
 	}
 
