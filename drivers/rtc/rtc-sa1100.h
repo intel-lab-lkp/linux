c956dd15d930147a8444a18ba6cfddf86103fea0
Liao Yuanhong <liaoyuanhong@vivo.com>
2024-08-29 15:22:07 +0800

    rtc:rtc-sa1100:Use devm_clk_get_enabled() helpers
    Use devm_clk_get_enabled() instead of clk functions in rtc-sa1100.
    
    Signed-off-by: Liao Yuanhong <liaoyuanhong@vivo.com>

--- drivers/rtc/rtc-sa1100.h
+++ drivers/rtc/rtc-sa1100.h
@@ -4,7 +4,6 @@
 
 #include <linux/kernel.h>
 
-struct clk;
 struct platform_device;
 
 struct sa1100_rtc {
@@ -16,7 +15,6 @@ struct sa1100_rtc {
 	int			irq_1hz;
 	int			irq_alarm;
 	struct rtc_device	*rtc;
-	struct clk		*clk;
 };
 
 int sa1100_rtc_init(struct platform_device *pdev, struct sa1100_rtc *info);
