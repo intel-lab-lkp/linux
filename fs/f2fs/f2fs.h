d81314834f68149ba05ab01676bd5b048da8647e
Daeho Jeong <daehojeong@google.com>
2024-08-29 14:52:40 -0700

    f2fs: do FG_GC when GC boosting is required for zoned devices
    Under low free section count, we need to use FG_GC instead of BG_GC to
    recover free sections.
    
    Signed-off-by: Daeho Jeong <daehojeong@google.com>

--- fs/f2fs/f2fs.h
+++ fs/f2fs/f2fs.h
@@ -1294,6 +1294,7 @@ struct f2fs_gc_control {
 	bool should_migrate_blocks;	/* should migrate blocks */
 	bool err_gc_skipped;		/* return EAGAIN if GC skipped */
 	unsigned int nr_free_secs;	/* # of free sections to do GC */
+	bool one_time;			/* require one time GC in one migration unit */
 };
 
 /*
