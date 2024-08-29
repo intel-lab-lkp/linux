6aec1331571fc45d3f6d1e7a57c58b1b147b8873
Vadim Fedorenko <vadfed@meta.com>
2024-08-29 13:49:22 -0700

    selftests: txtimestamp: add SCM_TS_OPT_ID test
    Extend txtimestamp udp test to run with fixed tskey using
    SCM_TS_OPT_ID control message.
    
    Signed-off-by: Vadim Fedorenko <vadfed@meta.com>

--- tools/testing/selftests/net/txtimestamp.sh
+++ tools/testing/selftests/net/txtimestamp.sh
@@ -39,6 +39,7 @@ run_test_tcpudpraw() {
 
 	run_test_v4v6 ${args}		# tcp
 	run_test_v4v6 ${args} -u	# udp
+	run_test_v4v6 ${args} -u -o 5	# udp with fixed tskey
 	run_test_v4v6 ${args} -r	# raw
 	run_test_v4v6 ${args} -R	# raw (IPPROTO_RAW)
 	run_test_v4v6 ${args} -P	# pf_packet
