6c47bec3ce6bac5ae00ab16bc3f1ccbf5d5c78c1
Kanchana P Sridhar <kanchana.p.sridhar@intel.com>
2024-08-29 14:27:05 -0700

    mm: swap: Count successful mTHP ZSWAP stores in sysfs mTHP zswpout stats.
    Add a new MTHP_STAT_ZSWPOUT entry to the sysfs mTHP stats so that
    per-order mTHP folio ZSWAP stores can be accounted.
    
    If zswap_store() successfully swaps out an mTHP, it will be counted under
    the per-order sysfs "zswpout" stats:
    
    /sys/kernel/mm/transparent_hugepage/hugepages-*kB/stats/zswpout
    
    Other block dev/fs mTHP swap-out events will be counted under
    the existing sysfs "swpout" stats:
    
    /sys/kernel/mm/transparent_hugepage/hugepages-*kB/stats/swpout
    
    Based on changes made in commit 61e751c01466ffef5dc72cb64349454a691c6bfe
    ("mm: cleanup count_mthp_stat() definition"), this patch also moves
    the call to count_mthp_stat() in count_swpout_vm_event() to be outside
    the "ifdef CONFIG_TRANSPARENT_HUGEPAGE".
    
    Signed-off-by: Kanchana P Sridhar <kanchana.p.sridhar@intel.com>

--- mm/page_io.c
+++ mm/page_io.c
@@ -289,6 +289,7 @@ int swap_writepage(struct page *page, struct writeback_control *wbc)
 		swap_zeromap_folio_clear(folio);
 	}
 	if (zswap_store(folio)) {
+		count_mthp_stat(folio_order(folio), MTHP_STAT_ZSWPOUT);
 		folio_unlock(folio);
 		return 0;
 	}
@@ -308,8 +309,8 @@ static inline void count_swpout_vm_event(struct folio *folio)
 		count_memcg_folio_events(folio, THP_SWPOUT, 1);
 		count_vm_event(THP_SWPOUT);
 	}
-	count_mthp_stat(folio_order(folio), MTHP_STAT_SWPOUT);
 #endif
+	count_mthp_stat(folio_order(folio), MTHP_STAT_SWPOUT);
 	count_vm_events(PSWPOUT, folio_nr_pages(folio));
 }
 
