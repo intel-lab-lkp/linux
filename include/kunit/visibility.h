5af26cb9d39dd096e4f0e0bf121678b29bbe4168
Michal Wajdeczko <michal.wajdeczko@intel.com>
2024-08-29 20:13:45 +0200

    kunit: Add macro to conditionally expose expressions to tests
    The VALUE_IF_KUNIT macro will add expression statement only if the
    CONFIG_KUNIT is enabled. Otherwise if CONFIG_KUNIT is not enabled
    it will evaluate always to 0.
    
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>

--- include/kunit/visibility.h
+++ include/kunit/visibility.h
@@ -31,6 +31,25 @@
     #define DECLARE_IF_KUNIT(body...)	body
 
     /**
+     * VALUE_IF_KUNIT - Conditionally evaluate an expression
+     * @expr: the expression to be evaluated conditionally
+     *
+     * This macro evaluates expression statement only if CONFIG_KUNIT is enabled.
+     * Otherwise if CONFIG_KUNIT is not enabled it will evaluate always to 0.
+     *
+     * .. code-block:: C
+     *
+     *     int real_func(int i)
+     *     {
+     *         if (VALUE_IF_KUNIT(i == 0xC0FFE))
+     *             return 0;
+     *
+     *         return i + 1;
+     *     }
+     */
+    #define VALUE_IF_KUNIT(expr...)	expr
+
+    /**
      * VISIBLE_IF_KUNIT - A macro that sets symbols to be static if
      * CONFIG_KUNIT is not enabled. Otherwise if CONFIG_KUNIT is enabled
      * there is no change to the symbol definition.
@@ -46,6 +65,7 @@
 	    EXPORTED_FOR_KUNIT_TESTING)
 #else
     #define DECLARE_IF_KUNIT(body...)
+    #define VALUE_IF_KUNIT(expr...) 0
     #define VISIBLE_IF_KUNIT static
     #define EXPORT_SYMBOL_IF_KUNIT(symbol)
 #endif
