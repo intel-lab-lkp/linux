5f6efc17dac341ffcbf22c811ffc5f3a4b17d98c
Jinjie Ruan <ruanjinjie@huawei.com>
2024-08-30 14:32:26 +0800

    soc: ti: knav_dma: Use dev_err_probe() to simplfy code
    Use the dev_err_probe() helper to simplify error handling
    during probe.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- drivers/soc/ti/knav_dma.c
+++ drivers/soc/ti/knav_dma.c
@@ -708,17 +708,13 @@ static int knav_dma_probe(struct platform_device *pdev)
 	struct device_node *node = pdev->dev.of_node;
 	int ret = 0;
 
-	if (!node) {
-		dev_err(&pdev->dev, "could not find device info\n");
-		return -EINVAL;
-	}
+	if (!node)
+		return dev_err_probe(&pdev->dev, -EINVAL, "could not find device info\n");
 
 	kdev = devm_kzalloc(dev,
 			sizeof(struct knav_dma_pool_device), GFP_KERNEL);
-	if (!kdev) {
-		dev_err(dev, "could not allocate driver mem\n");
-		return -ENOMEM;
-	}
+	if (!kdev)
+		return dev_err_probe(dev, -ENOMEM, "could not allocate driver mem\n");
 
 	kdev->dev = dev;
 	INIT_LIST_HEAD(&kdev->list);
