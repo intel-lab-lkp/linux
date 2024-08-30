569b057796bda5bd1b767b0c35d0184a435dd985
NeilBrown <neilb@suse.de>
2024-08-30 17:03:16 +1000

    nfsd: use clear_and_wake_up_bit()
    nfsd has two places that open-code clear_and_wake_up_bit().  One has the
    required memory barriers.  The other does not.
    
    Change both to use clear_and_wake_up_bit() so with have the barriers
    without the noise.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- fs/nfsd/nfs4state.c
+++ fs/nfsd/nfs4state.c
@@ -3076,8 +3076,7 @@ nfsd4_cb_getattr_release(struct nfsd4_callback *cb)
 			container_of(ncf, struct nfs4_delegation, dl_cb_fattr);
 
 	nfs4_put_stid(&dp->dl_stid);
-	clear_bit(CB_GETATTR_BUSY, &ncf->ncf_cb_flags);
-	wake_up_bit(&ncf->ncf_cb_flags, CB_GETATTR_BUSY);
+	clear_and_wake_up_bit(&ncf->ncf_cb_flags, CB_GETATTR_BUSY);
 }
 
 static const struct nfsd4_callback_ops nfsd4_cb_recall_any_ops = {
