e3a7b501bed0f3674f54f3e02aa13e3d5dc1f16e
Sean Christopherson <seanjc@google.com>
2024-08-29 21:35:58 -0700

    KVM: Add arch hooks for enabling/disabling virtualization
    Add arch hooks that are invoked when KVM enables/disable virtualization.
    x86 will use the hooks to register an "emergency disable" callback, which
    is essentially an x86-specific shutdown notifier that is used when the
    kernel is doing an emergency reboot/shutdown/kexec.
    
    Add comments for the declarations to help arch code understand exactly
    when the callbacks are invoked.  Alternatively, the APIs themselves could
    communicate most of the same info, but kvm_arch_pre_enable_virtualization()
    and kvm_arch_post_disable_virtualization() are a bit cumbersome, and make
    it a bit less obvious that they are intended to be implemented as a pair.
    
    Reviewed-by: Chao Gao <chao.gao@intel.com>
    Reviewed-by: Kai Huang <kai.huang@intel.com>
    Acked-by: Kai Huang <kai.huang@intel.com>
    Tested-by: Farrah Chen <farrah.chen@intel.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -5582,6 +5582,16 @@ static DEFINE_PER_CPU(bool, virtualization_enabled);
 static DEFINE_MUTEX(kvm_usage_lock);
 static int kvm_usage_count;
 
+__weak void kvm_arch_enable_virtualization(void)
+{
+
+}
+
+__weak void kvm_arch_disable_virtualization(void)
+{
+
+}
+
 static int kvm_enable_virtualization_cpu(void)
 {
 	if (__this_cpu_read(virtualization_enabled))
@@ -5681,6 +5691,8 @@ static int kvm_enable_virtualization(void)
 	if (kvm_usage_count++)
 		return 0;
 
+	kvm_arch_enable_virtualization();
+
 	r = cpuhp_setup_state(CPUHP_AP_KVM_ONLINE, "kvm/cpu:online",
 			      kvm_online_cpu, kvm_offline_cpu);
 	if (r)
@@ -5711,6 +5723,7 @@ err_rebooting:
 	unregister_syscore_ops(&kvm_syscore_ops);
 	cpuhp_remove_state(CPUHP_AP_KVM_ONLINE);
 err_cpuhp:
+	kvm_arch_disable_virtualization();
 	--kvm_usage_count;
 	return r;
 }
@@ -5724,6 +5737,7 @@ static void kvm_disable_virtualization(void)
 
 	unregister_syscore_ops(&kvm_syscore_ops);
 	cpuhp_remove_state(CPUHP_AP_KVM_ONLINE);
+	kvm_arch_disable_virtualization();
 }
 
 static int kvm_init_virtualization(void)
