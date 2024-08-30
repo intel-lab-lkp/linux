fae1553cc456f28aa5f97e2fe1db8b8973af3ad6
Gregory Price <gourry@gourry.net>
2024-08-30 09:23:03 -0400

    tpm: do not ignore memblock_reserve return value
    tpm code currently ignores a relevant failure case silently.
    Add an error to make this failure non-silent.
    
    Signed-off-by: Gregory Price <gourry@gourry.net>

--- drivers/firmware/efi/tpm.c
+++ drivers/firmware/efi/tpm.c
@@ -61,7 +61,11 @@ int __init efi_tpm_eventlog_init(void)
 	}
 
 	tbl_size = sizeof(*log_tbl) + log_tbl->size;
-	memblock_reserve(efi.tpm_log, tbl_size);
+	if (memblock_reserve(efi.tpm_log, tbl_size)) {
+		pr_err("TPM Event Log memblock reserve fails 0x%lx - %x\n",
+		       efi.tpm_log, tbl_size);
+		goto out;
+	}
 
 	if (efi.tpm_final_log == EFI_INVALID_TABLE_ADDR) {
 		pr_info("TPM Final Events table not present\n");
