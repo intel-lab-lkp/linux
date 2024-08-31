f6faf7db8aced992f89b51ba8839aefe8dbd8676
Jinjie Ruan <ruanjinjie@huawei.com>
2024-08-31 18:30:47 +0800

    drm/imagination: Use memdup_user() helper
    Switching to memdup_user(), which combines kmalloc() and copy_from_user(),
    and it can simplfy code.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- drivers/gpu/drm/imagination/pvr_context.c
+++ drivers/gpu/drm/imagination/pvr_context.c
@@ -69,27 +69,19 @@ process_static_context_state(struct pvr_device *pvr_dev, const struct pvr_stream
 	void *stream;
 	int err;
 
-	stream = kzalloc(stream_size, GFP_KERNEL);
-	if (!stream)
-		return -ENOMEM;
-
-	if (copy_from_user(stream, u64_to_user_ptr(stream_user_ptr), stream_size)) {
-		err = -EFAULT;
-		goto err_free;
-	}
+	stream = memdup_user(u64_to_user_ptr(stream_user_ptr), stream_size);
+	if (IS_ERR(stream))
+		return PTR_ERR(stream);
 
 	err = pvr_stream_process(pvr_dev, cmd_defs, stream, stream_size, dest);
-	if (err)
-		goto err_free;
+	if (err) {
+		kfree(stream);
+		return err;
+	}
 
 	kfree(stream);
 
 	return 0;
-
-err_free:
-	kfree(stream);
-
-	return err;
 }
 
 static int init_render_fw_objs(struct pvr_context *ctx,
