6bd10bcc75b061aa7de3027729782c09d10120fb
David Wang <00107082@163.com>
2024-09-02 13:49:59 +0800

    PM: add: move warn message out of mutex lock.
    dpm_list_mtx does not protect any data used by
    dev_warn for checking parent's power, move
    dev_warn out of mutex lock block make the
    lock more efficient, especially when the warn
    is triggered. This can happen on some HW when
    resume from suspend with USB camera opened:
     >usb 3-1.1: reset high-speed USB device number 4 using xhci_hcd
     >..
     >ep_81: PM: parent 3-1.1:1.1 should not be sleeping
    
    Signed-off-by: David Wang <00107082@163.com>

--- drivers/base/power/main.c
+++ drivers/base/power/main.c
@@ -134,10 +134,10 @@ void device_pm_add(struct device *dev)
 	pr_debug("Adding info for %s:%s\n",
 		 dev->bus ? dev->bus->name : "No Bus", dev_name(dev));
 	device_pm_check_callbacks(dev);
-	mutex_lock(&dpm_list_mtx);
 	if (dev->parent && dev->parent->power.is_prepared)
 		dev_warn(dev, "parent %s should not be sleeping\n",
 			dev_name(dev->parent));
+	mutex_lock(&dpm_list_mtx);
 	list_add_tail(&dev->power.entry, &dpm_list);
 	dev->power.in_dpm_list = true;
 	mutex_unlock(&dpm_list_mtx);
