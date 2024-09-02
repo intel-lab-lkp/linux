72f5882b7d590a297d6ab440173f0ec83f62a6e1
Christophe Leroy <christophe.leroy@csgroup.eu>
2024-09-02 14:04:42 +0200

    powerpc/vdso: Wire up getrandom() vDSO implementation on PPC64
    Extend getrandom() vDSO implementation to powerpc64.
    
    Tested on QEMU on both ppc64_defconfig and ppc64le_defconfig.
    
    The results are not precise as it is QEMU on an x86 laptop, but
    no need to be precise to see the benefit.
    
    ~ # ./vdso_test_getrandom bench-single
       vdso: 25000000 times in 4.977777162 seconds
       libc: 25000000 times in 75.516749981 seconds
    syscall: 25000000 times in 86.842242014 seconds
    
    ~ # ./vdso_test_getrandom bench-single
       vdso: 25000000 times in 6.473814156 seconds
       libc: 25000000 times in 73.875109463 seconds
    syscall: 25000000 times in 71.805066229 seconds
    
    Signed-off-by: Christophe Leroy <christophe.leroy@csgroup.eu>

--- arch/powerpc/kernel/vdso/vgetrandom.c
+++ arch/powerpc/kernel/vdso/vgetrandom.c
@@ -7,10 +7,8 @@
 #include <linux/time.h>
 #include <linux/types.h>
 
-#ifndef CONFIG_PPC64
 ssize_t __c_kernel_getrandom(void *buffer, size_t len, unsigned int flags, void *opaque_state,
 			     size_t opaque_len, const struct vdso_rng_data *vd)
 {
 	return __cvdso_getrandom_data(vd, buffer, len, flags, opaque_state, opaque_len);
 }
-#endif
