509d90de8a0fe8faa91dfeae877dbb8a4a28ded3
Jinjie Ruan <ruanjinjie@huawei.com>
2024-09-02 19:09:03 +0800

    printk: Use the BITS_PER_LONG macro
    sizeof(unsigned long) * 8 is the number of bits in an unsigned long
    variable, replace it with BITS_PER_LONG macro to make it simpler.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- kernel/printk/printk_ringbuffer.h
+++ kernel/printk/printk_ringbuffer.h
@@ -122,7 +122,7 @@ enum desc_state {
 
 #define _DATA_SIZE(sz_bits)	(1UL << (sz_bits))
 #define _DESCS_COUNT(ct_bits)	(1U << (ct_bits))
-#define DESC_SV_BITS		(sizeof(unsigned long) * 8)
+#define DESC_SV_BITS		BITS_PER_LONG
 #define DESC_FLAGS_SHIFT	(DESC_SV_BITS - 2)
 #define DESC_FLAGS_MASK		(3UL << DESC_FLAGS_SHIFT)
 #define DESC_STATE(sv)		(3UL & (sv >> DESC_FLAGS_SHIFT))
