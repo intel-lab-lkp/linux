db05996bd0d26b3f7052230f7e1531f4ec77e054
Dhruva Gole <d-gole@ti.com>
2024-09-02 15:02:22 +0530

    cpufreq: ti-cpufreq: Make the AM625 efuse_offset 0
    Since the efuse_offset is basically derived from the syscon node, we no
    longer need to use any efuse_offset for AM625. This is in line with how
    the AM62Ax and AM62Px are already doing.
    
    Signed-off-by: Dhruva Gole <d-gole@ti.com>

--- drivers/cpufreq/ti-cpufreq.c
+++ drivers/cpufreq/ti-cpufreq.c
@@ -299,7 +299,7 @@ static struct ti_cpufreq_soc_data am3517_soc_data = {
 
 static struct ti_cpufreq_soc_data am625_soc_data = {
 	.efuse_xlate = am625_efuse_xlate,
-	.efuse_offset = 0x0018,
+	.efuse_offset = 0x0,
 	.efuse_mask = 0x07c0,
 	.efuse_shift = 0x6,
 	.rev_offset = 0x0014,
