abdf12d4c195d1707c021fe562b513fae7603839
Luca Stefani <luca.stefani.ge1@gmail.com>
2024-09-03 09:39:14 +0200

    block: Export blk_alloc_discard_bio
    The fs trim loops over ranges and sends discard requests, some ranges
    can be large so it's all transparently handled by blkdev_issue_discard()
    and processed in smaller chunks.
    
    To support cancellation (or suspend) requests we need to insert checks
    into the the loop, exporting the symbol allows to reimplement
    such loop with the desired behavior.
    
    Signed-off-by: Luca Stefani <luca.stefani.ge1@gmail.com>

--- include/linux/blkdev.h
+++ include/linux/blkdev.h
@@ -1088,6 +1088,8 @@ static inline long nr_blockdev_pages(void)
 
 extern void blk_io_schedule(void);
 
+struct bio *blk_alloc_discard_bio(struct block_device *bdev,
+		sector_t *sector, sector_t *nr_sects, gfp_t gfp_mask);
 int blkdev_issue_discard(struct block_device *bdev, sector_t sector,
 		sector_t nr_sects, gfp_t gfp_mask);
 int __blkdev_issue_discard(struct block_device *bdev, sector_t sector,
