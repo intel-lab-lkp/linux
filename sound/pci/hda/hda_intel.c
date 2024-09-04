06e261ccd0e97025b46d37703493a7bac2bfc8b8
Maxim Mikityanskiy <maxtram95@gmail.com>
2024-09-04 23:39:55 +0300

    ALSA: hda: intel: Fix Optimus when GPU has no sound
    Lenovo IdeaPad Z570 with NVIDIA GeForce Ge 520M doesn't have sound on
    the discrete GPU. snd_hda_intel probes the device and schedules
    azx_probe_continue(), which fails at azx_first_init(). The driver ends
    up probed, but calls azx_free() and stops the chip. However, from the
    runtime PM point of view, the device remains active, because the PCI
    subsystem makes it active on probe, and it's still bound. It prevents
    vga_switcheroo from turning off the DGPU (pci_create_device_link() syncs
    power management for the video and audio devices).
    
    Fix it by forcing the device to the suspended state in azx_free().
    
    Fixes: 07f4f97d7b4b ("vga_switcheroo: Use device link for HDA controller")
    Signed-off-by: Maxim Mikityanskiy <maxtram95@gmail.com>

--- sound/pci/hda/hda_intel.c
+++ sound/pci/hda/hda_intel.c
@@ -1361,8 +1361,20 @@ static void azx_free(struct azx *chip)
 	if (use_vga_switcheroo(hda)) {
 		if (chip->disabled && hda->probe_continued)
 			snd_hda_unlock_devices(&chip->bus);
-		if (hda->vga_switcheroo_registered)
+		if (hda->vga_switcheroo_registered) {
 			vga_switcheroo_unregister_client(chip->pci);
+
+			/* Some GPUs don't have sound, and azx_first_init fails,
+			 * leaving the device probed but non-functional. As long
+			 * as it's probed, the PCI subsystem keeps its runtime
+			 * PM status as active. Force it to suspended (as we
+			 * actually stop the chip) to allow GPU to suspend via
+			 * vga_switcheroo.
+			 */
+			pm_runtime_disable(&pci->dev);
+			pm_runtime_set_suspended(&pci->dev);
+			pm_runtime_enable(&pci->dev);
+		}
 	}
 
 	if (bus->chip_init) {
