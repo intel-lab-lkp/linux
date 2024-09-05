1812310b159111815b381960ef69237af07f61cb
Jason Xing <kernelxing@tencent.com>
2024-09-05 15:17:37 +0800

    rxtimestamp.c: add the test for SOF_TIMESTAMPING_OPT_RX_FILTER
    Test when we use SOF_TIMESTAMPING_OPT_RX_FILTER with software
    or hardware report flag. The expected result is no rx timestamp
    report.
    
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Jason Xing <kernelxing@tencent.com>

--- tools/testing/selftests/net/rxtimestamp.c
+++ tools/testing/selftests/net/rxtimestamp.c
@@ -57,6 +57,7 @@ static struct sof_flag sof_flags[] = {
 	SOF_FLAG(SOF_TIMESTAMPING_SOFTWARE),
 	SOF_FLAG(SOF_TIMESTAMPING_RX_SOFTWARE),
 	SOF_FLAG(SOF_TIMESTAMPING_RX_HARDWARE),
+	SOF_FLAG(SOF_TIMESTAMPING_OPT_RX_FILTER),
 };
 
 static struct socket_type socket_types[] = {
@@ -95,6 +96,16 @@ static struct test_case test_cases[] = {
 	{
 		{ .so_timestamping = SOF_TIMESTAMPING_RX_SOFTWARE
 			| SOF_TIMESTAMPING_RX_HARDWARE },
+		{}
+	},
+	{
+		{ .so_timestamping = SOF_TIMESTAMPING_RAW_HARDWARE
+			| SOF_TIMESTAMPING_OPT_RX_FILTER },
+		{}
+	},
+	{
+		{ .so_timestamping = SOF_TIMESTAMPING_SOFTWARE
+			| SOF_TIMESTAMPING_OPT_RX_FILTER },
 		{}
 	},
 	{
