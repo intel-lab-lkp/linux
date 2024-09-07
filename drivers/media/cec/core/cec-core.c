6c5cf395bf1eeca4e6e30df93f98dbd3d4f1e3e4
Li Zetao <lizetao1@huawei.com>
2024-09-07 11:43:59 +0800

    media: cec: remove redundant null pointer checks in cec_devnode_init()
    Since the debugfs_create_dir() never returns a null pointer, checking
    the return value for a null pointer is redundant. Remove this check
    since debugfs_create_file can handle IS_ERR pointers. At the same time,
    debugfs_create_dir returns ERR_PTR (-ENODEV) by default when
    CONFIG_DEBUG_FS=N, so there is no need for CONFIG_DEBUG_FS macro
    isolation.
    
    Signed-off-by: Li Zetao <lizetao1@huawei.com>

--- drivers/media/cec/core/cec-core.c
+++ drivers/media/cec/core/cec-core.c
@@ -439,13 +439,7 @@ static int __init cec_devnode_init(void)
 		return ret;
 	}
 
-#ifdef CONFIG_DEBUG_FS
 	top_cec_dir = debugfs_create_dir("cec", NULL);
-	if (IS_ERR_OR_NULL(top_cec_dir)) {
-		pr_warn("cec: Failed to create debugfs cec dir\n");
-		top_cec_dir = NULL;
-	}
-#endif
 
 	ret = bus_register(&cec_bus_type);
 	if (ret < 0) {
