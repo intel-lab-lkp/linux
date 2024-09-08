7b3982426bc4835da1eabe69e22d223e04ac3f53
Jonas Karlman <jonas@kwiboo.se>
2024-09-08 13:28:03 +0000

    drm: bridge: dw_hdmi: Disable scrambler feature when not supported
    The scrambler feature can be left enabled when hotplugging from a sink
    and mode that require scrambling to a sink that does not support SCDC or
    scrambling.
    
    Typically a blank screen or 'no signal' message can be observed after
    using a HDMI 2.0 4K@60Hz mode and then hotplugging to a sink that only
    support HDMI 1.4.
    
    Fix this by disabling the scrambler feature when SCDC is not supported.
    
    Fixes: 264fce6cc2c1 ("drm/bridge: dw-hdmi: Add SCDC and TMDS Scrambling support")
    Reported-by: Christopher Obbard <chris.obbard@collabora.com>
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-hdmi.c
@@ -2117,6 +2117,8 @@ static void hdmi_av_composer(struct dw_hdmi *hdmi,
 				    HDMI_MC_SWRSTZ);
 			drm_scdc_set_scrambling(hdmi->curr_conn, 0);
 		}
+	} else if (hdmi->version >= 0x200a) {
+		hdmi_writeb(hdmi, 0, HDMI_FC_SCRAMBLER_CTRL);
 	}
 
 	/* Set up horizontal active pixel width */
