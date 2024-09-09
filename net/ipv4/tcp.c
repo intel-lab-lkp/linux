302459728dd2f8239a9d5af4ae011f6e763ce3fc
Vadim Fedorenko <vadfed@meta.com>
2024-09-09 09:50:45 -0700

    net_tstamp: add SCM_TS_OPT_ID for RAW sockets
    The last type of sockets which supports SOF_TIMESTAMPING_OPT_ID is RAW
    sockets. To add new option this patch converts all callers (direct and
    indirect) of _sock_tx_timestamp to provide sockcm_cookie instead of
    tsflags. And while here fix __sock_tx_timestamp to receive tsflags as
    __u32 instead of __u16.
    
    Signed-off-by: Vadim Fedorenko <vadfed@meta.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -482,7 +482,7 @@ static void tcp_tx_timestamp(struct sock *sk, u16 tsflags)
 		struct skb_shared_info *shinfo = skb_shinfo(skb);
 		struct tcp_skb_cb *tcb = TCP_SKB_CB(skb);
 
-		sock_tx_timestamp(sk, tsflags, &shinfo->tx_flags);
+		sock_tx_timestamp(sk, sockc, &shinfo->tx_flags);
 		if (tsflags & SOF_TIMESTAMPING_TX_ACK)
 			tcb->txstamp_ack = 1;
 		if (tsflags & SOF_TIMESTAMPING_TX_RECORD_MASK)
