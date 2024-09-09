97d1d6b946b5f4d656354ec90365d45b7f4dfcd2
Kai Huang <kai.huang@intel.com>
2024-09-09 20:06:24 +1200

    x86/kexec: do unconditional WBINVD for bare-metal in relocate_kernel()
    Both SME and TDX can leave caches in incoherent state due to memory
    encryption.  During kexec, the caches must be flushed before jumping to
    the second kernel to avoid silent memory corruption to the second kernel.
    
    During kexec, the WBINVD in stop_this_cpu() flushes caches for all
    remote cpus when they are being stopped.  For SME, the WBINVD in
    relocate_kernel() flushes the cache for the last running cpu (which is
    executing the kexec).
    
    Similarly, to support kexec for TDX host, after stopping all remote cpus
    with cache flushed, the kernel needs to flush cache for the last running
    cpu.
    
    Use the existing WBINVD in relocate_kernel() to cover TDX host as well.
    
    However, instead of sprinkling around vendor-specific checks, just do
    unconditional WBINVD to cover both SME and TDX.  Kexec is not a fast path
    so having one additional WBINVD for platforms w/o SME/TDX is acceptable.
    
    But only do WBINVD for bare-metal because TDX guests and SEV-ES/SEV-SNP
    guests will get unexpected (and yet unnecessary) exception (#VE or #VC)
    which the kernel is unable to handle at this stage.
    
    Note commit 93c1800b3799 ("x86/kexec: Fix bug with call depth tracking")
    moved calling 'cc_platform_has(CC_ATTR_HOST_MEM_ENCRYPT)' as an argument
    of relocate_kernel() to an earlier place before load_segments() by
    adding a variable 'host_mem_enc_active'.  The reason was the call to
    cc_platform_has() after load_segments() caused a fault and system crash
    when call depth tracking is active because load_segments() resets GS to
    0 but call depth tracking uses per-CPU variable to operate.
    
    Use !cpu_feature_enabled(X86_FEATURE_HYPERVISOR) to check whether the
    kernel runs on bare-metal.  cpu_feature_enabled() is always inline but
    not a function call, thus it is safe to use it after load_segments()
    when call depth tracking is enabled.  Remove the 'host_mem_enc_active'
    variable and use cpu_feature_enabled() directly as the argument when
    calling relocate_kernel().
    
    Signed-off-by: Kai Huang <kai.huang@intel.com>
    Reviewed-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
    Cc: Tom Lendacky <thomas.lendacky@amd.com>
    Cc: Dave Young <dyoung@redhat.com>
    Cc: David Kaplan <david.kaplan@amd.com>
    Reviewed-by: Tom Lendacky <thomas.lendacky@amd.com>

--- arch/x86/kernel/machine_kexec_64.c
+++ arch/x86/kernel/machine_kexec_64.c
@@ -322,15 +322,8 @@ void machine_kexec_cleanup(struct kimage *image)
 void machine_kexec(struct kimage *image)
 {
 	unsigned long page_list[PAGES_NR];
-	unsigned int host_mem_enc_active;
 	int save_ftrace_enabled;
 	void *control_page;
-
-	/*
-	 * This must be done before load_segments() since if call depth tracking
-	 * is used then GS must be valid to make any function calls.
-	 */
-	host_mem_enc_active = cc_platform_has(CC_ATTR_HOST_MEM_ENCRYPT);
 
 #ifdef CONFIG_KEXEC_JUMP
 	if (image->preserve_context)
@@ -392,7 +385,7 @@ void machine_kexec(struct kimage *image)
 				       (unsigned long)page_list,
 				       image->start,
 				       image->preserve_context,
-				       host_mem_enc_active);
+				       !cpu_feature_enabled(X86_FEATURE_HYPERVISOR));
 
 #ifdef CONFIG_KEXEC_JUMP
 	if (image->preserve_context)
