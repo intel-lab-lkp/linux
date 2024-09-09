aa6b34720eb9ec56c2cd67a50fde89634d2f8b95
Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
2024-09-09 18:19:37 +0100

    drm/sched: Further optimise drm_sched_entity_push_job
    Having removed one re-lock cycle on the entity->lock in a patch titled
    "drm/sched: Optimise drm_sched_entity_push_job", with only a tiny bit
    larger refactoring we can do the same optimisation on the rq->lock.
    (Currently both drm_sched_rq_add_entity() and
    drm_sched_rq_update_fifo_locked() take and release the same lock.)
    
    To achieve this we rename drm_sched_rq_add_entity() to
    drm_sched_rq_add_entity_locked(), making it expect the rq->lock to be
    held, and also add the same expectation to
    drm_sched_rq_update_fifo_locked().
    
    For more stream-lining we also add the run-queue as an explicit parameter
    to drm_sched_rq_remove_fifo_locked() to avoid both callers and callee
    having to dereference entity->rq.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Cc: Christian König <christian.koenig@amd.com>
    Cc: Alex Deucher <alexander.deucher@amd.com>
    Cc: Luben Tuikov <ltuikov89@gmail.com>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: Philipp Stanner <pstanner@redhat.com>

--- drivers/gpu/drm/scheduler/sched_entity.c
+++ drivers/gpu/drm/scheduler/sched_entity.c
@@ -614,11 +614,14 @@ void drm_sched_entity_push_job(struct drm_sched_job *sched_job)
 		sched = rq->sched;
 
 		atomic_inc(sched->score);
-		drm_sched_rq_add_entity(rq, entity);
+
+		spin_lock(&rq->lock);
+		drm_sched_rq_add_entity_locked(rq, entity);
 
 		if (drm_sched_policy == DRM_SCHED_POLICY_FIFO)
-			drm_sched_rq_update_fifo_locked(entity, submit_ts);
+			drm_sched_rq_update_fifo_locked(entity, rq, submit_ts);
 
+		spin_unlock(&rq->lock);
 		spin_unlock(&entity->lock);
 
 		drm_sched_wakeup(sched, entity);
