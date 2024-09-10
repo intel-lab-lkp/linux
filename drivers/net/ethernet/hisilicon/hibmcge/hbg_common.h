cfb6a90fa0fdbb3f7e912d815200cd170f717054
Jijie Shao <shaojijie@huawei.com>
2024-09-10 15:59:35 +0800

    net: hibmcge: Add interrupt supported in this module
    The driver supports four interrupts: TX interrupt, RX interrupt,
    mdio interrupt, and error interrupt.
    
    Actually, the driver does not use the mdio interrupt.
    Therefore, the driver does not request the mdio interrupt.
    
    The error interrupt distinguishes different error information
    by using different masks. To distinguish different errors,
    the statistics count is added for each error.
    
    To ensure the consistency of the code process, masks are added for the
    TX interrupt and RX interrupt.
    
    This patch implements interrupt request, and provides a
    unified entry for the interrupt handler function. However,
    the specific interrupt handler function of each interrupt
    is not implemented currently.
    
    Because of pcim_enable_device(), the interrupt vector
    is already device managed and does not need to be free actively.
    
    Signed-off-by: Jijie Shao <shaojijie@huawei.com>

--- drivers/net/ethernet/hisilicon/hibmcge/hbg_common.h
+++ drivers/net/ethernet/hisilicon/hibmcge/hbg_common.h
@@ -13,6 +13,7 @@
 #define HBG_DEFAULT_MTU_SIZE		1500
 #define HBG_RX_SKIP1			0x00
 #define HBG_RX_SKIP2			0x01
+#define HBG_VECTOR_NUM			4
 
 enum hbg_nic_state {
 	HBG_NIC_STATE_EVENT_HANDLING = 0,
@@ -38,6 +39,22 @@ struct hbg_dev_specs {
 	u32 rx_buf_size;
 };
 
+struct hbg_irq_info {
+	const char *name;
+	u32 mask;
+	bool reenable;
+	bool need_print;
+	u64 count;
+
+	void (*irq_handle)(struct hbg_priv *priv, struct hbg_irq_info *irq_info);
+};
+
+struct hbg_vector {
+	char name[HBG_VECTOR_NUM][32];
+	struct hbg_irq_info *info_array;
+	u32 info_array_len;
+};
+
 struct hbg_mac {
 	struct mii_bus *mdio_bus;
 	struct phy_device *phydev;
@@ -56,6 +73,7 @@ struct hbg_priv {
 	struct hbg_dev_specs dev_specs;
 	unsigned long state;
 	struct hbg_mac mac;
+	struct hbg_vector vectors;
 };
 
 #endif
