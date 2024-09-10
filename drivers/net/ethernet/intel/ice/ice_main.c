dff3e19396b53c695f438cf36a3cb269ac426fe3
Paul Greenwalt <paul.greenwalt@intel.com>
2024-09-10 00:45:04 -0400

    ice: Add E830 checksum offload support
    E830 supports raw receive and generic transmit checksum offloads.
    
    Raw receive checksum support is provided by hardware calculating the
    checksum over the whole packet, regardless of type. The calculated
    checksum is provided to driver in the Rx flex descriptor. Then the driver
    assigns the checksum to skb-->csum and sets skb->ip_summed to
    CHECKSUM_COMPLETE.
    
    Generic transmit checksum support is provided by hardware calculating the
    checksum given two offsets: the start offset to begin checksum calculation,
    and the offset to insert the calculated checksum in the packet. Support is
    advertised to the stack using NETIF_F_HW_CSUM feature.
    
    E830 has the following limitations when both generic transmit checksum
    offload and TCP Segmentation Offload (TSO) are enabled:
    
    1. Inner packet header modification is not supported. This restriction
       includes the inability to alter TCP flags, such as the push flag. As a
       result, this limitation can impact the receiver's ability to coalesce
       packets, potentially degrading network throughput.
    2. The Maximum segment size is limited to 1023 bytes, which prevents
       support of Maximum Transmission Unit (MTU) greater than 1063 bytes.
    
    Therefore NETIF_F_HW_CSUM and NETIF_F_ALL_TSO features are mutually
    exclusive. NETIF_F_HW_CSUM hardware feature support is indicated but is not
    enabled by default. Instead, IP checksums and NETIF_F_ALL_TSO are the
    defaults. Enforcement of mutual exclusivity of NETIF_F_HW_CSUM and
    NETIF_F_ALL_TSO is done in ice_fix_features(). Mutual exclusivity of IP
    checksums and NETIF_F_HW_CSUM is handled by netdev_fix_features().
    
    When NETIF_F_HW_CSUM is requested the provided skb->csum_start and
    skb->csum_offset are passed to hardware in the Tx context descriptor
    generic checksum (GCS) parameters. Hardware calculates the 1's complement
    from skb->csum_start to the end of the packet, and inserts the result in
    the packet at skb->csum_offset.
    
    Co-developed-by: Alice Michael <alice.michael@intel.com>
    Signed-off-by: Alice Michael <alice.michael@intel.com>
    Co-developed-by: Eric Joyner <eric.joyner@intel.com>
    Signed-off-by: Eric Joyner <eric.joyner@intel.com>
    Signed-off-by: Paul Greenwalt <paul.greenwalt@intel.com>

--- drivers/net/ethernet/intel/ice/ice_main.c
+++ drivers/net/ethernet/intel/ice/ice_main.c
@@ -3673,6 +3673,12 @@ void ice_set_netdev_features(struct net_device *netdev)
 	 */
 	netdev->hw_features |= NETIF_F_RXFCS;
 
+	/* Mutual exclusivity for TSO and GCS is enforced by the
+	 * ice_fix_features() ndo callback.
+	 */
+	if (ice_is_feature_supported(pf, ICE_F_GCS))
+		netdev->hw_features |= NETIF_F_HW_CSUM;
+
 	netif_set_tso_max_size(netdev, ICE_MAX_TSO_SIZE);
 }
 
@@ -6246,6 +6252,12 @@ ice_fdb_del(struct ndmsg *ndm, __always_unused struct nlattr *tb[],
  *	These are mutually exclusive as there is currently no way to
  *	enable/disable VLAN filtering based on VLAN ethertype when using VLAN
  *	prune rules.
+ *
+ *	E830 hardware TSO with NETIF_F_HW_CSUM has limitations: inner packet
+ *	header modification is not supported, and Maximum segment size is
+ *	limited to 1023 bytes. Therefore TSO and NETIF_F_HW_CSUM mutual
+ *	exclusivity is enforce, and GCO and IP checksum mutual exclusivity is
+ *	handled by netdev_fix_features().
  */
 static netdev_features_t
 ice_fix_features(struct net_device *netdev, netdev_features_t features)
@@ -6253,6 +6265,7 @@ ice_fix_features(struct net_device *netdev, netdev_features_t features)
 	struct ice_netdev_priv *np = netdev_priv(netdev);
 	netdev_features_t req_vlan_fltr, cur_vlan_fltr;
 	bool cur_ctag, cur_stag, req_ctag, req_stag;
+	netdev_features_t changed;
 
 	cur_vlan_fltr = netdev->features & NETIF_VLAN_FILTERING_FEATURES;
 	cur_ctag = cur_vlan_fltr & NETIF_F_HW_VLAN_CTAG_FILTER;
@@ -6299,6 +6312,30 @@ ice_fix_features(struct net_device *netdev, netdev_features_t features)
 	    !ice_vsi_has_non_zero_vlans(np->vsi)) {
 		netdev_warn(netdev, "Disabling VLAN stripping as FCS/CRC stripping is also disabled and there is no VLAN configured\n");
 		features &= ~NETIF_VLAN_STRIPPING_FEATURES;
+	}
+
+	if (!ice_is_feature_supported(np->vsi->back, ICE_F_GCS) ||
+	    !(features & NETIF_F_HW_CSUM))
+		return features;
+
+	changed = netdev->features ^ features;
+
+	/* HW checksum feature is supported and set, so enforce mutual
+	 * exclusivity of TSO and GCS.
+	 */
+	if (!(features & NETIF_F_ALL_TSO))
+		return features;
+
+	if (changed & NETIF_F_HW_CSUM && changed & NETIF_F_ALL_TSO) {
+		netdev_warn(netdev, "Dropping TSO and HW checksum. TSO and HW checksum are mutually exclusive.\n");
+		features &= ~NETIF_F_HW_CSUM;
+		features &= ~((features & changed) & NETIF_F_ALL_TSO);
+	} else if (changed & NETIF_F_HW_CSUM) {
+		netdev_warn(netdev, "Dropping HW checksum. TSO and HW checksum are mutually exclusive.\n");
+		features &= ~NETIF_F_HW_CSUM;
+	} else {
+		netdev_warn(netdev, "Dropping TSO. TSO and HW checksum are mutually exclusive.\n");
+		features &= ~NETIF_F_ALL_TSO;
 	}
 
 	return features;
