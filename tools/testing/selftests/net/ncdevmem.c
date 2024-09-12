a888f3e6184ea02d86d56cf3dc6b4752b7a0e370
Stanislav Fomichev <sdf@fomichev.me>
2024-09-12 10:12:45 -0700

    selftests: ncdevmem: Remove default arguments
    To make it clear what's required and what's not. Also, some of the
    values don't seem like a good defaults; for example eth1.
    
    Cc: Mina Almasry <almasrymina@google.com>
    Signed-off-by: Stanislav Fomichev <sdf@fomichev.me>

--- tools/testing/selftests/net/ncdevmem.c
+++ tools/testing/selftests/net/ncdevmem.c
@@ -42,30 +42,11 @@
 #define MSG_SOCK_DEVMEM 0x2000000
 #endif
 
-/*
- * tcpdevmem netcat. Works similarly to netcat but does device memory TCP
- * instead of regular TCP. Uses udmabuf to mock a dmabuf provider.
- *
- * Usage:
- *
- *	On server:
- *	ncdevmem -s <server IP> -c <client IP> -f eth1 -l -p 5201 -v 7
- *
- *	On client:
- *	yes $(echo -e \\x01\\x02\\x03\\x04\\x05\\x06) | \
- *		tr \\n \\0 | \
- *		head -c 5G | \
- *		nc <server IP> 5201 -p 5201
- *
- * Note this is compatible with regular netcat. i.e. the sender or receiver can
- * be replaced with regular netcat to test the RX or TX path in isolation.
- */
-
-static char *server_ip = "192.168.1.4";
-static char *port = "5201";
+static char *server_ip;
+static char *port;
 static int start_queue = 8;
 static int num_queues = 8;
-static char *ifname = "eth1";
+static char *ifname;
 static unsigned int ifindex;
 static unsigned int dmabuf_id;
 
@@ -572,6 +553,15 @@ int main(int argc, char *argv[])
 			break;
 		}
 	}
+
+	if (!server_ip)
+		error(1, 0, "Missing -s argument\n");
+
+	if (!port)
+		error(1, 0, "Missing -p argument\n");
+
+	if (!ifname)
+		error(1, 0, "Missing -f argument\n");
 
 	ifindex = if_nametoindex(ifname);
 
