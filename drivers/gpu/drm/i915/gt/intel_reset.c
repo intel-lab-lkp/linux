91c85a04c4c87667ba16730a043af6f1068b1734
Rodrigo Vivi <rodrigo.vivi@intel.com>
2024-09-12 13:25:39 -0400

    drm/i915/irq: Rename suspend/resume funcitons
    Although these functions are used in runtime_pm, they are not
    exclusively used there, so remove the misleading prefix.
    
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/i915/gt/intel_reset.c
+++ drivers/gpu/drm/i915/gt/intel_reset.c
@@ -1224,7 +1224,7 @@ void intel_gt_reset(struct intel_gt *gt,
 	}
 
 	if (INTEL_INFO(gt->i915)->gpu_reset_clobbers_display)
-		intel_runtime_pm_disable_interrupts(gt->i915);
+		intel_irq_suspend(gt->i915);
 
 	if (do_reset(gt, stalled_mask)) {
 		gt_err(gt, "Failed to reset chip\n");
@@ -1232,7 +1232,7 @@ void intel_gt_reset(struct intel_gt *gt,
 	}
 
 	if (INTEL_INFO(gt->i915)->gpu_reset_clobbers_display)
-		intel_runtime_pm_enable_interrupts(gt->i915);
+		intel_irq_resume(gt->i915);
 
 	intel_overlay_reset(gt->i915);
 
