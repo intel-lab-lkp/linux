69812140deb329bca203fe4e42f71b5888024ace
Erni Sri Satya Vennela <ernis@linux.microsoft.com>
2024-09-12 14:27:49 -0700

    Revert "Input: hyperv-keyboard - register as a wakeup source"
    This reverts commit 62238f3aadc9bc56da70100e19ec61b9f8d72a5f.
    
    Remove keyboard as wakeup source since Suspend-to-Idle feature
    is disabled.
    
    Signed-off-by: Erni Sri Satya Vennela <ernis@linux.microsoft.com>

--- drivers/input/serio/hyperv-keyboard.c
+++ drivers/input/serio/hyperv-keyboard.c
@@ -162,15 +162,6 @@ static void hv_kbd_on_receive(struct hv_device *hv_dev,
 			serio_interrupt(kbd_dev->hv_serio, scan_code, 0);
 		}
 		spin_unlock_irqrestore(&kbd_dev->lock, flags);
-
-		/*
-		 * Only trigger a wakeup on key down, otherwise
-		 * "echo freeze > /sys/power/state" can't really enter the
-		 * state because the Enter-UP can trigger a wakeup at once.
-		 */
-		if (!(info & IS_BREAK))
-			pm_wakeup_hard_event(&hv_dev->device);
-
 		break;
 
 	default:
@@ -356,9 +347,6 @@ static int hv_kbd_probe(struct hv_device *hv_dev,
 		goto err_close_vmbus;
 
 	serio_register_port(kbd_dev->hv_serio);
-
-	device_init_wakeup(&hv_dev->device, true);
-
 	return 0;
 
 err_close_vmbus:
