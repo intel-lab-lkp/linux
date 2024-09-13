fea838bdd1f044f202e5a38649262670a5dba3e0
Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>
2024-09-13 17:36:30 +0300

    PCI: Add TLP Prefix reading into pcie_read_tlp_log()
    pcie_read_tlp_log() handles only 4 Header Log DWORDs but TLP Prefix Log
    (PCIe r6.1 secs 7.8.4.12 & 7.9.14.13) may also be present.
    
    Generalize pcie_read_tlp_log() and struct pcie_tlp_log to handle also
    TLP Prefix Log. The relevant registers are formatted identically in AER
    and DPC Capability, but has these variations:
    
    a) The offsets of TLP Prefix Log registers vary.
    b) DPC RP PIO TLP Prefix Log register can be < 4 DWORDs.
    
    Therefore callers must pass the offset of the TLP Prefix Log register
    and the entire length to pcie_read_tlp_log() to be able to read the
    correct number of TLP Prefix DWORDs from the correct offset.
    
    Signed-off-by: Ilpo Järvinen <ilpo.jarvinen@linux.intel.com>

--- drivers/pci/pcie/tlp.c
+++ drivers/pci/pcie/tlp.c
@@ -12,25 +12,64 @@
 #include "../pci.h"
 
 /**
+ * aer_tlp_log_len - Calculates AER Capability TLP Header/Prefix Log length
+ * @dev: PCIe device
+ *
+ * Return: TLP Header/Prefix Log length
+ */
+unsigned int aer_tlp_log_len(struct pci_dev *dev)
+{
+	return 4 + dev->eetlp_prefix_max;
+}
+
+#ifdef CONFIG_PCIE_DPC
+/**
+ * dpc_tlp_log_len - Calculates DPC RP PIO TLP Header/Prefix Log length
+ * @dev: PCIe device
+ *
+ * Return: TLP Header/Prefix Log length
+ */
+unsigned int dpc_tlp_log_len(struct pci_dev *dev)
+{
+	/* Remove ImpSpec Log register from the count */
+	if (dev->dpc_rp_log_size >= 5)
+		return dev->dpc_rp_log_size - 1;
+
+	return dev->dpc_rp_log_size;
+}
+#endif
+
+/**
  * pcie_read_tlp_log - read TLP Header Log
  * @dev: PCIe device
  * @where: PCI Config offset of TLP Header Log
+ * @where2: PCI Config offset of TLP Prefix Log
+ * @tlp_len: TLP Log length (Header Log + TLP Prefix Log in DWORDs)
  * @log: TLP Log structure to fill
  *
  * Fill @log from TLP Header Log registers, e.g., AER or DPC.
  *
  * Return: 0 on success and filled TLP Log structure, <0 on error.
  */
-int pcie_read_tlp_log(struct pci_dev *dev, int where,
-		      struct pcie_tlp_log *log)
+int pcie_read_tlp_log(struct pci_dev *dev, int where, int where2,
+		      unsigned int tlp_len, struct pcie_tlp_log *log)
 {
 	unsigned int i;
-	int ret;
+	int off, ret;
+	u32 *to;
 
 	memset(log, 0, sizeof(*log));
 
-	for (i = 0; i < 4; i++) {
-		ret = pci_read_config_dword(dev, where + i * 4, &log->dw[i]);
+	for (i = 0; i < tlp_len; i++) {
+		if (i < 4) {
+			off = where + i * 4;
+			to = &log->dw[i];
+		} else {
+			off = where2 + (i - 4) * 4;
+			to = &log->prefix[i - 4];
+		}
+
+		ret = pci_read_config_dword(dev, off, to);
 		if (ret)
 			return pcibios_err_to_errno(ret);
 	}
