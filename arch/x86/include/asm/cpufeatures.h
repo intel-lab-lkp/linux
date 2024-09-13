0991df4be9ae2cf7b8571e87731c0df1464f4e42
Jon Kohler <jon@nutanix.com>
2024-09-13 11:42:37 -0700

    x86/bhi: use TSX abort for mitigation on RTM systems
    Introduce ability to mitigate BHI via TSX aborts on systems that
    support RTM feature. The assembly for this mitigation was provided by
    Intel [1], noted as "Listing 3", which starts and immediately aborts a
    TSX transaction and causes the processor to clear the BHB.
    
    Vulnerable systems that do not support RTM or have TSX disabled will
    still use the clear_bhb_loop mitigation by default.
    
    Furthermore, on hardware that supports BHI_DIS_S/X86_FEATURE_BHI_CTRL,
    do not use hardware mitigation when using BHI_MITIGATION_VMEXIT_ONLY,
    as this causes the value of MSR_IA32_SPEC_CTRL to change, inflicting
    measurable KVM overhead.
    
    Example:
    In a typical eIBRS enabled system, such as Intel SPR, the SPEC_CTRL may
    be commonly set to val == 1 to reflect eIBRS enablement; however,
    SPEC_CTRL_BHI_DIS_S causes val == 1025. If the guests that KVM is
    virtualizing do not also set the guest side value == 1025, KVM will
    constantly have to wrmsr toggle the guest vs host value on both entry
    and exit, delaying both.
    
    In fact, if the VMM (such as qemu) does not expose BHI_CTRL + the guest
    kernel does not understand BHI_CTRL, or the VMM does expose it + the
    guest understands BHI_CTRL *but* the guest does not reboot to
    reinitialize SPEC_CTRL, the guest val will never equal 1025, making
    this overhead both painful and unavoidable.
    
    Testing:
    On an Intel SPR 6442Y, using KVM unit tests tscdeadline_immed shows a
    ~17-18% speedup vs the existing default.
    
    [1] https://www.intel.com/content/www/us/en/developer/articles/technical/software-security-guidance/technical-documentation/branch-history-injection.html
    
    Signed-off-by: Jon Kohler <jon@nutanix.com>
    Cc: Chao Gao <chao.gao@intel.com>
    Cc: Daniel Sneddon <daniel.sneddon@linux.intel.com>
    Cc: Pawan Gupta <pawan.kumar.gupta@linux.intel.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -473,6 +473,8 @@
 #define X86_FEATURE_CLEAR_BHB_HW	(21*32+ 3) /* BHI_DIS_S HW control enabled */
 #define X86_FEATURE_CLEAR_BHB_LOOP_ON_VMEXIT (21*32+ 4) /* Clear branch history at vmexit using SW loop */
 #define X86_FEATURE_FAST_CPPC		(21*32 + 5) /* AMD Fast CPPC */
+#define X86_FEATURE_CLEAR_BHB_TSX	(21*32 + 6) /* "" Clear branch history at syscall entry using TSX abort */
+#define X86_FEATURE_CLEAR_BHB_TSX_ON_VMEXIT (21*32 + 7) /* "" Clear branch history at vmexit using TSX abort */
 
 /*
  * BUG word(s)
