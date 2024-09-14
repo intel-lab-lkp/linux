2fb15665ff1f1eddaea958d13920c31a1b88d735
Steven Rostedt <rostedt@goodmis.org>
2024-09-14 17:48:08 -0400

    ftrace: Have calltime be saved in the fgraph storage
    The calltime field in the shadow stack frame is only used by the function
    graph tracer and profiler. But now that there's other users of the function
    graph infrastructure, this adds overhead and wastes space on the shadow
    stack. Move the calltime to the fgraph data storage, where the function
    graph and profiler entry functions will save it in its own graph storage and
    retrieve it in its exit functions.
    
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- include/linux/ftrace.h
+++ include/linux/ftrace.h
@@ -1065,7 +1065,6 @@ void *fgraph_retrieve_parent_data(int idx, int *size_bytes, int depth);
 struct ftrace_ret_stack {
 	unsigned long ret;
 	unsigned long func;
-	unsigned long long calltime;
 #ifdef HAVE_FUNCTION_GRAPH_FP_TEST
 	unsigned long fp;
 #endif
