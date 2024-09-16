8cad77dcdc749dc50d3a3c93d5cc333647c7f7bd
Animesh Manna <animesh.manna@intel.com>
2024-09-16 13:24:05 +0530

    drm/i915/vrr: Split vrr-compute-config in two phases
    As vrr guardband calculation is dependent on modified
    vblank start so better to compute late after all
    vblank adjustement.
    
    v1: Initial version.
    v2: Split in a separate patch from panel-replay workaround. [Ankit]
    v3: Add a function for late vrr related computation. [Ville]
    
    Signed-off-by: Animesh Manna <animesh.manna@intel.com>

--- drivers/gpu/drm/i915/display/intel_vrr.c
+++ drivers/gpu/drm/i915/display/intel_vrr.c
@@ -239,18 +239,26 @@ intel_vrr_compute_config(struct intel_crtc_state *crtc_state,
 			(crtc_state->hw.adjusted_mode.crtc_vtotal -
 			 crtc_state->hw.adjusted_mode.vsync_end);
 	}
+}
 
-	/*
-	 * For XE_LPD+, we use guardband and pipeline override
-	 * is deprecated.
-	 */
-	if (DISPLAY_VER(display) >= 13) {
-		crtc_state->vrr.guardband =
-			crtc_state->vrr.vmin + 1 - adjusted_mode->crtc_vblank_start;
-	} else {
-		crtc_state->vrr.pipeline_full =
-			min(255, crtc_state->vrr.vmin - adjusted_mode->crtc_vblank_start -
-			    crtc_state->framestart_delay - 1);
+void intel_vrr_compute_config_late(struct intel_crtc_state *crtc_state)
+{
+	struct intel_display *display = to_intel_display(crtc_state);
+	struct drm_display_mode *adjusted_mode = &crtc_state->hw.adjusted_mode;
+
+	if (crtc_state->vrr.enable) {
+		/*
+		 * For XE_LPD+, we use guardband and pipeline override
+		 * is deprecated.
+		 */
+		if (DISPLAY_VER(display) >= 13) {
+			crtc_state->vrr.guardband =
+				crtc_state->vrr.vmin + 1 - adjusted_mode->crtc_vblank_start;
+		} else {
+			crtc_state->vrr.pipeline_full =
+				min(255, crtc_state->vrr.vmin - adjusted_mode->crtc_vblank_start -
+				crtc_state->framestart_delay - 1);
+		}
 	}
 }
 
