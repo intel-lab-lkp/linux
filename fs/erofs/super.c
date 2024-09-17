b4881dfc6362480751353c9b0d07c8800162b6c3
Gao Xiang <hsiangkao@linux.alibaba.com>
2024-09-17 13:58:00 +0800

    erofs: ensure regular inodes for file-backed mounts
    Only regular inodes are allowed for file-backed mounts, not directories
    (as seen in the original syzbot case) or special inodes.
    
    Also ensure that .read_folio() is implemented on the underlying fs.
    
    Fixes: fb176750266a ("erofs: add file-backed mount support")
    Reported-by: syzbot+001306cd9c92ce0df23f@syzkaller.appspotmail.com
    Closes: https://lore.kernel.org/r/00000000000011bdde0622498ee3@google.com
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

--- fs/erofs/super.c
+++ fs/erofs/super.c
@@ -714,7 +714,10 @@ static int erofs_fc_get_tree(struct fs_context *fc)
 		if (IS_ERR(sbi->fdev))
 			return PTR_ERR(sbi->fdev);
 
-		return get_tree_nodev(fc, erofs_fc_fill_super);
+		if (S_ISREG(file_inode(sbi->fdev)->i_mode) &&
+		    sbi->fdev->f_mapping->a_ops->read_folio)
+			return get_tree_nodev(fc, erofs_fc_fill_super);
+		fput(sbi->fdev);
 	}
 #endif
 	return ret;
