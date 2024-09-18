33558daef85d92b6e356f072b65e84ad9442ef0d
Dominik Karol Piątkowski <dominik.karol.piatkowski@protonmail.com>
2024-09-18 19:21:03 +0000

    staging: vt6655: s_uGetDataDuration: Rename cbFrameLength parameter
    This patch renames cbFrameLength to frame_length in s_uGetDataDuration
    function in order to avoid using camel case.
    
    Signed-off-by: Dominik Karol Piątkowski <dominik.karol.piatkowski@protonmail.com>

--- drivers/staging/vt6655/rxtx.c
+++ drivers/staging/vt6655/rxtx.c
@@ -231,7 +231,7 @@ unsigned int
 s_uGetDataDuration(
 	struct vnt_private *priv,
 	unsigned char dur_type,
-	unsigned int cbFrameLength,
+	unsigned int frame_length,
 	unsigned char byPktType,
 	unsigned short wRate,
 	bool bNeedAck,
@@ -250,7 +250,7 @@ s_uGetDataDuration(
 	if (uFragIdx == (uMACfragNum - 2))
 		len = cbLastFragmentSize;
 	else
-		len = cbFrameLength;
+		len = frame_length;
 
 	switch (dur_type) {
 	case DATADUR_B:    /* DATADUR_B */
