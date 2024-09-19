4f41463b8fbe890a9b660405e2b9d46e7040d28e
Markus Elfring <elfring@users.sourceforge.net>
2024-09-19 18:05:28 +0200

    net: fman: Use of_node_put(muram_node) call only once in read_dts_node()
    A of_node_put(muram_node) call was immediately used after a return code
    check for a of_address_to_resource() call in this function implementation.
    Thus use such a function call only once instead directly before the check.
    
    This issue was transformed by using the Coccinelle software.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>

--- drivers/net/ethernet/freescale/fman/fman.c
+++ drivers/net/ethernet/freescale/fman/fman.c
@@ -2776,14 +2776,12 @@ static struct fman *read_dts_node(struct platform_device *of_dev)
 
 	err = of_address_to_resource(muram_node, 0,
 				     &fman->dts_params.muram_res);
+	of_node_put(muram_node);
 	if (err) {
-		of_node_put(muram_node);
 		dev_err(&of_dev->dev, "%s: of_address_to_resource() = %d\n",
 			__func__, err);
 		goto fman_free;
 	}
-
-	of_node_put(muram_node);
 
 	err = devm_request_irq(&of_dev->dev, irq, fman_irq, IRQF_SHARED,
 			       "fman", fman);
