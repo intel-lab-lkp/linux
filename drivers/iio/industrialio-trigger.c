868857dc83f16fe64f7b2e492f7eccaacc2f7d26
Vasileios Amoiridis <vassilisamir@gmail.com>
2024-09-21 20:19:39 +0200

    iio: remove iio_validate_own_trigger() completely
    Remove completely the iio_validate_own_trigger() since it is not used
    any more and since the iio_trigger_validate_own_device() can fully
    replace the function.
    
    Signed-off-by: Vasileios Amoiridis <vassilisamir@gmail.com>

--- drivers/iio/industrialio-trigger.c
+++ drivers/iio/industrialio-trigger.c
@@ -719,26 +719,6 @@ bool iio_trigger_using_own(struct iio_dev *indio_dev)
 EXPORT_SYMBOL(iio_trigger_using_own);
 
 /**
- * iio_validate_own_trigger - Check if a trigger and IIO device belong to
- *  the same device
- * @idev: the IIO device to check
- * @trig: the IIO trigger to check
- *
- * This function can be used as the validate_trigger callback for triggers that
- * can only be attached to their own device.
- *
- * Return: 0 if both the trigger and the IIO device belong to the same
- * device, -EINVAL otherwise.
- */
-int iio_validate_own_trigger(struct iio_dev *idev, struct iio_trigger *trig)
-{
-	if (idev->dev.parent != trig->dev.parent)
-		return -EINVAL;
-	return 0;
-}
-EXPORT_SYMBOL_GPL(iio_validate_own_trigger);
-
-/**
  * iio_trigger_validate_own_device - Check if a trigger and IIO device belong to
  *  the same device
  * @trig: The IIO trigger to check
