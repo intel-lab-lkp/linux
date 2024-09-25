f5698379dbc50d85ab7cac3bfe1c5171e8e7d194
Huang Ying <ying.huang@intel.com>
2024-09-25 10:46:43 +0800

    cxl: Rename ACPI_CEDT_CFMWS_RESTRICT_TYPE2/TYPE3
    According to the description of the "Window Restrictions" field of
    "CFMWS Structure" in the CXL spec v3.1 section 9.18.1.3: CXL Fixed
    Memory Window Structure (CFMWS), the bit 0 of "Window Restrictions" is
    formerly known as "CXL Type 2 Memory" and renamed to "Device
    Coherent", while the bit 1 is formerly known as "CXL Type 3 Memory"
    and renamed to "Host-only Coherent".  Because type 3 memory can only
    be host-only coherent before, while it can be host-only coherent or
    device coherent with "Back-Invalidate" now.
    
    To avoid confusing about type 3 memory and host-only coherent in Linux
    kernel, we rename corresponding bit definition from
    ACPI_CEDT_CFMWS_RESTRICT_TYPE2/TYPE3 to
    ACPI_CEDT_CFMWS_RESTRICT_DEVCOH/HOSTONLYCOH.  This makes the kernel
    code consistent with the spec too.
    
    Also rename the corresponding cxl_decoder flags
    CXL_DECODER_F_TYPE2/TYPE3 to CXL_DECODER_F_DEVCOH/HOSTONLYCOH.
    
    No functionality change is expected, because we just rename the flags
    constant definition.
    
    Signed-off-by: "Huang, Ying" <ying.huang@intel.com>
    Suggested-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Cc: Dan Williams <dan.j.williams@intel.com>
    Cc: Davidlohr Bueso <dave@stgolabs.net>
    Cc: Jonathan Cameron <jonathan.cameron@huawei.com>
    Cc: Dave Jiang <dave.jiang@intel.com>
    Cc: Alison Schofield <alison.schofield@intel.com>
    Cc: Vishal Verma <vishal.l.verma@intel.com>
    Cc: Ira Weiny <ira.weiny@intel.com>
    Cc: Alejandro Lucero <alucerop@amd.com>

--- include/acpi/actbl1.h
+++ include/acpi/actbl1.h
@@ -551,11 +551,11 @@ struct acpi_cedt_cfmws_target_element {
 
 /* Values for Restrictions field above */
 
-#define ACPI_CEDT_CFMWS_RESTRICT_TYPE2      (1)
-#define ACPI_CEDT_CFMWS_RESTRICT_TYPE3      (1<<1)
-#define ACPI_CEDT_CFMWS_RESTRICT_VOLATILE   (1<<2)
-#define ACPI_CEDT_CFMWS_RESTRICT_PMEM       (1<<3)
-#define ACPI_CEDT_CFMWS_RESTRICT_FIXED      (1<<4)
+#define ACPI_CEDT_CFMWS_RESTRICT_DEVCOH        (1)
+#define ACPI_CEDT_CFMWS_RESTRICT_HOSTONLYCOH   (1<<1)
+#define ACPI_CEDT_CFMWS_RESTRICT_VOLATILE      (1<<2)
+#define ACPI_CEDT_CFMWS_RESTRICT_PMEM          (1<<3)
+#define ACPI_CEDT_CFMWS_RESTRICT_FIXED         (1<<4)
 
 /* 2: CXL XOR Interleave Math Structure */
 
