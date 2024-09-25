0d11a15c05d56e2219c2051897aad8d8947407ae
liuderong <liuderong@oppo.com>
2024-09-25 10:48:43 +0800

    scsi:ufs:core: Add trace READ(16)/WRITE(16) commands
    For sd_zbc_read_zones, READ(16)/WRITE(16) are mandatory for ZBC disks.
    Currently, when printing the trace:ufshcd_command on zone UFS devices,
    the LBA and SIZE fields appear invalid,
    making it difficult to trace commands.
    So add trace READ(16)/WRITE(16) commands for zone ufs device.
    
    Trace sample:
    ufshcd_command: send_req: 1d84000.ufshc: tag: 31, DB: 0x0,
    size: -1, IS: 0, LBA: 0, opcode: 0x8a (WRITE_16), group_id: 0x0, hwq_id: 7
    ufshcd_command: complete_rsp: 1d84000.ufshc: tag: 31, DB: 0x0,
    size: -1, IS: 0, LBA: 0, opcode: 0x8a (WRITE_16), group_id: 0x0, hwq_id: 7
    
    Signed-off-by: liuderong <liuderong@oppo.com>

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -453,15 +453,19 @@ static void ufshcd_add_command_trace(struct ufs_hba *hba, unsigned int tag,
 
 	opcode = cmd->cmnd[0];
 
-	if (opcode == READ_10 || opcode == WRITE_10) {
+	if (opcode == READ_10 || opcode == READ_16 ||
+		opcode == WRITE_10 || opcode == WRITE_16) {
 		/*
-		 * Currently we only fully trace read(10) and write(10) commands
+		 * Currently we only fully trace the following commands,
+		 * read(10),read(16),write(10), and write(16)
 		 */
 		transfer_len =
 		       be32_to_cpu(lrbp->ucd_req_ptr->sc.exp_data_transfer_len);
 		lba = scsi_get_lba(cmd);
 		if (opcode == WRITE_10)
 			group_id = lrbp->cmd->cmnd[6];
+		if (opcode == WRITE_16)
+			group_id = lrbp->cmd->cmnd[14];
 	} else if (opcode == UNMAP) {
 		/*
 		 * The number of Bytes to be unmapped beginning with the lba.
