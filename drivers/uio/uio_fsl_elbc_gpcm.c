edf3e85b93dad9b23f7878f88b932329b045c578
Charles Han <hanchunchao@inspur.com>
2024-09-29 11:42:23 +0800

    uio: uio_fsl_elbc_gpcm: Add check devm_kasprintf() returned value
    devm_kasprintf() can return a NULL pointer on failure but this
    returned value in uio_fsl_elbc_gpcm_probe() is not checked.
    
    Fixes: d57801c45f53 ("uio: uio_fsl_elbc_gpcm: use device-managed allocators")
    Signed-off-by: Charles Han <hanchunchao@inspur.com>

--- drivers/uio/uio_fsl_elbc_gpcm.c
+++ drivers/uio/uio_fsl_elbc_gpcm.c
@@ -384,6 +384,9 @@ static int uio_fsl_elbc_gpcm_probe(struct platform_device *pdev)
 
 	/* set all UIO data */
 	info->mem[0].name = devm_kasprintf(&pdev->dev, GFP_KERNEL, "%pOFn", node);
+	if (!info->mem[0].name)
+		return -ENOMEM;
+
 	info->mem[0].addr = res.start;
 	info->mem[0].size = resource_size(&res);
 	info->mem[0].memtype = UIO_MEM_PHYS;
