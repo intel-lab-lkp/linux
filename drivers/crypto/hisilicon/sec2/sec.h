b4a4789856ba0c53c5d43377dd1e13e8d73bbd36
Chenghai Huang <huangchenghai2@huawei.com>
2024-09-29 19:26:29 +0800

    crypto: hisilicon/sec2 - fix for aead icv error
    When the AEAD algorithm is used for encryption or decryption,
    the input authentication length varies, the hardware needs to
    obtain the input length to pass the integrity check verification.
    Currently, the driver uses a fixed authentication length,which
    causes decryption failure, so the length configuration is modified.
    In addition, the step of setting the auth length is unnecessary,
    so it was deleted from the setkey function.
    
    Fixes: 2f072d75d1ab ("crypto: hisilicon - Add aead support on SEC2")
    Signed-off-by: Wenkai Lin <linwenkai6@hisilicon.com>
    Signed-off-by: Chenghai Huang <huangchenghai2@huawei.com>

--- drivers/crypto/hisilicon/sec2/sec.h
+++ drivers/crypto/hisilicon/sec2/sec.h
@@ -36,6 +36,7 @@ struct sec_aead_req {
 	dma_addr_t out_mac_dma;
 	u8 *a_ivin;
 	dma_addr_t a_ivin_dma;
+	size_t authsize;
 	struct aead_request *aead_req;
 };
 
@@ -90,7 +91,6 @@ struct sec_auth_ctx {
 	dma_addr_t a_key_dma;
 	u8 *a_key;
 	u8 a_key_len;
-	u8 mac_len;
 	u8 a_alg;
 	bool fallback;
 	struct crypto_shash *hash_tfm;
