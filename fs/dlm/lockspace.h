6231d28c625d7675bbcee0266179027710dee5ec
Alexander Aring <aahringo@redhat.com>
2024-09-30 16:13:47 -0400

    dlm: introduce dlm_find_lockspace_name()
    A DLM lockspace can be either identified by it's unique id or name. Later
    patches will introduce a new netlink api that is using a unique
    lockspace name to identify a lockspace in the lslist. This is mostly
    required for sysfs functionality that is currently solved by a per
    lockspace kobject allocation. The new netlink api cannot simple lookup
    the lockspace by a container_of() call to do whatever sysfs is
    providing so we introduce dlm_find_lockspace_name() to offer such
    functionality.
    
    Signed-off-by: Alexander Aring <aahringo@redhat.com>

--- fs/dlm/lockspace.h
+++ fs/dlm/lockspace.h
@@ -22,6 +22,7 @@
 
 int dlm_lockspace_init(void);
 void dlm_lockspace_exit(void);
+struct dlm_ls *dlm_find_lockspace_name(const char *lsname);
 struct dlm_ls *dlm_find_lockspace_global(uint32_t id);
 struct dlm_ls *dlm_find_lockspace_local(void *id);
 struct dlm_ls *dlm_find_lockspace_device(int minor);
