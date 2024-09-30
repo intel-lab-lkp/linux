ccab93768b9b00ed2113a63ec3dc48cb9d367aea
Julian Vetter <jvetter@kalrayinc.com>
2024-09-30 15:23:20 +0200

    um: Add dummy implementation for IO memcpy/memset
    The um arch is the only architecture that sets the config 'NO_IOMEM',
    yet drivers that use IO memory can be selected. In order to make these
    drivers happy we add a dummy implementation for memcpy_{from,to}io and
    memset_io functions.
    
    Reviewed-by: Yann Sionneau <ysionneau@kalrayinc.com>
    Signed-off-by: Julian Vetter <jvetter@kalrayinc.com>

--- arch/um/include/asm/io.h
+++ arch/um/include/asm/io.h
@@ -21,6 +21,23 @@ static inline void iounmap(void __iomem *addr)
 }
 #endif /* iounmap */
 
+static inline void memset_io(volatile void __iomem *dst, int c, size_t count)
+{
+	memset((void *)dst, c, count);
+}
+
+static inline void memcpy_toio(volatile void __iomem *to, const void *from,
+			       size_t count)
+{
+	memcpy((void *)to, from, count);
+}
+
+static inline void memcpy_fromio(void *to, const volatile void __iomem *from,
+				 size_t count)
+{
+	memcpy(to, (void *)from, count);
+}
+
 #include <asm-generic/io.h>
 
 #endif
