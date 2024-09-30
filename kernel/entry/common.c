797e1443770c30ea5f0ce3b4ea29fee231ac7230
Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
2024-09-30 15:23:50 -0400

    tracing: Declare system call tracepoints with TRACE_EVENT_SYSCALL
    In preparation for allowing system call tracepoints to handle page
    faults, introduce TRACE_EVENT_SYSCALL to declare the sys_enter/sys_exit
    tracepoints.
    
    Emit the static inlines register_trace_syscall_##name for events
    declared with TRACE_EVENT_SYSCALL, allowing source-level validation
    that only probes meant to handle system call entry/exit events are
    registered to them.
    
    Move the common code between __DECLARE_TRACE and __DECLARE_TRACE_SYSCALL
    into __DECLARE_TRACE_COMMON.
    
    This change is not meant to alter the generated code, and only prepares
    the following modifications.
    
    Signed-off-by: Mathieu Desnoyers <mathieu.desnoyers@efficios.com>
    Cc: Michael Jeanson <mjeanson@efficios.com>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Alexei Starovoitov <ast@kernel.org>
    Cc: Yonghong Song <yhs@fb.com>
    Cc: Paul E. McKenney <paulmck@kernel.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Andrii Nakryiko <andrii.nakryiko@gmail.com>
    Cc: bpf@vger.kernel.org
    Cc: Joel Fernandes <joel@joelfernandes.org>

--- kernel/entry/common.c
+++ kernel/entry/common.c
@@ -58,7 +58,7 @@ long syscall_trace_enter(struct pt_regs *regs, long syscall,
 	syscall = syscall_get_nr(current, regs);
 
 	if (unlikely(work & SYSCALL_WORK_SYSCALL_TRACEPOINT)) {
-		trace_sys_enter(regs, syscall);
+		trace_syscall_sys_enter(regs, syscall);
 		/*
 		 * Probes or BPF hooks in the tracepoint may have changed the
 		 * system call number as well.
@@ -166,7 +166,7 @@ static void syscall_exit_work(struct pt_regs *regs, unsigned long work)
 	audit_syscall_exit(regs);
 
 	if (work & SYSCALL_WORK_SYSCALL_TRACEPOINT)
-		trace_sys_exit(regs, syscall_get_return_value(current, regs));
+		trace_syscall_sys_exit(regs, syscall_get_return_value(current, regs));
 
 	step = report_single_step(work);
 	if (step || work & SYSCALL_WORK_SYSCALL_TRACE)
