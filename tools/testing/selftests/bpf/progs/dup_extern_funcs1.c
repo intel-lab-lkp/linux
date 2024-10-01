761e5768ecb7c7a4d2b664e44ad10bdfb182d48c
Eric Long <i@hack3r.moe>
2024-10-01 11:55:22 +0800

    selftests/bpf: test linking with duplicate extern functions
    Previously when multiple BPF object files referencing the same extern
    function (usually kfunc) are statically linked using `bpftool gen
    object`, libbpf tries to get the nonexistent size of BTF_KIND_FUNC_PROTO
    and fails. This test ensures it is fixed.
    
    Signed-off-by: Eric Long <i@hack3r.moe>

---
+++ tools/testing/selftests/bpf/progs/dup_extern_funcs1.c
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0
+
+#include "vmlinux.h"
+#include <bpf/bpf_helpers.h>
+
+char _license[] SEC("license") = "GPL";
+
+void *bpf_cast_to_kern_ctx(void *obj) __ksym;
+
+SEC("tc")
+int handler1(struct __sk_buff *skb)
+{
+	struct sk_buff *skb_kern = bpf_cast_to_kern_ctx(skb);
+
+	if (!skb_kern)
+		return -1;
+
+	return 0;
+}
