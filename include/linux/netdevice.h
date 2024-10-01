cb5dc755af37037699e7e5668796130a8a5f8fb1
Joe Damato <jdamato@fastly.com>
2024-10-01 23:52:34 +0000

    net: napi: Make gro_flush_timeout per-NAPI
    Allow per-NAPI gro_flush_timeout setting.
    
    The existing sysfs parameter is respected; writes to sysfs will write to
    all NAPI structs for the device and the net_device gro_flush_timeout
    field. Reads from sysfs will read from the net_device field.
    
    The ability to set gro_flush_timeout on specific NAPI instances will be
    added in a later commit, via netdev-genl.
    
    Note that idpf has embedded napi_struct in its internals and has
    established some series of asserts that involve the size of napi
    structure. Since this change increases the napi_struct size from 400 to
    416 (according to pahole on my system), I've increased the assertion in
    idpf by 16 bytes. No attention whatsoever was paid to the cacheline
    placement of idpf internals as a result of this change.
    
    Signed-off-by: Joe Damato <jdamato@fastly.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -377,6 +377,7 @@ struct napi_struct {
 	struct list_head	dev_list;
 	struct hlist_node	napi_hash_node;
 	int			irq;
+	unsigned long		gro_flush_timeout;
 	u32			defer_hard_irqs;
 };
 
@@ -2075,7 +2076,6 @@ struct net_device {
 	int			ifindex;
 	unsigned int		real_num_rx_queues;
 	struct netdev_rx_queue	*_rx;
-	unsigned long		gro_flush_timeout;
 	unsigned int		gro_max_size;
 	unsigned int		gro_ipv4_max_size;
 	rx_handler_func_t __rcu	*rx_handler;
@@ -2398,6 +2398,7 @@ struct net_device {
 
 	/** @irq_moder: dim parameters used if IS_ENABLED(CONFIG_DIMLIB). */
 	struct dim_irq_moder	*irq_moder;
+	unsigned long		gro_flush_timeout;
 	u32			napi_defer_hard_irqs;
 
 	u8			priv[] ____cacheline_aligned
