d3831d49ee0d1d0c1e37867c8d1115cff9b506c1
Andrey Ryabinin <arbn@yandex-team.com>
2024-10-02 18:07:16 +0200

    kstate: Add kstate - a mechanism to migrate some kernel state across kexec
    kstate (kernel state) is a mechanism to describe internal kernel state
    (partially), save it into the memory and restore the state after kexec
    in new kernel.
    
    The end goal here and the main use case for this is to be able to
    update host kernel under VMs with VFIO pass-through devices running
    on that host.
    We are pretty far from that end goal yet. This and following patches
    only try to establish some basic infrastructure to describe and migrate
    complex in-kernel states. And as a demonstration, the state of trace buffer
    migrated across kexec to new kernel (in the follow up patches).
    
    States (usually this is some struct) are described by the
    'struct kstate_description' containing the array of individual
    fields descpriptions - 'struct kstate_field'.
    Fields have different types like:
       KS_SIMPLE  - trivial type that just copied by value
    
       KS_POINTER - field contains pointer, it will be dereferenced to copy
         the value during save/restore phases.
    
       KS_STRUCT - contains another struct,
           field->ksd must point to another 'struct kstate_dscription'
    
       KS_CUSTOM - something that requires fit trivial types as above,
                   for this fields the callbacks field->save()/->restore() must
    	       do all job
       KS_ARRAY_OF_POINTER - array of pointers, the size of array determined by the
                             field->count() callback
       KS_END - special flag indicating the end of migration stream data.
    
    kstate_register() call accepts kstate_description along with an instance
    of an object and registers it in the global 'states' list.
    
    During kexec reboot phase this list iterated, and for each instance
    in the list 'struct kstate_entry' formed and saved in the migration stream.
    'kstate_entry' contains information like ID of kstate_description, version
    of it, size of migration data and the data itself.
    
    After the reboot, when the kstate_register() called it parses migration
    stream, finds the appropriate 'kstate_entry' and restores the contents of the
    object.
    
    This is an early RFC, so the code is somewhat hacky and some parts
    of this feature isn't well thought trough yet (like dealing with
    struct changes between old and new kernel, fixed size of migrate stream
    memory, and many more).
    
    Signed-off-by: Andrey Ryabinin <arbn@yandex-team.com>

---
+++ include/linux/kstate.h
@@ -0,0 +1,118 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _KSTATE_H
+#define _KSTATE_H
+
+#include <linux/atomic.h>
+#include <linux/list.h>
+#include <linux/stringify.h>
+
+struct kstate_description;
+enum kstate_flags {
+	KS_SIMPLE = (1 << 0),
+	KS_POINTER = (1 << 1),
+	KS_STRUCT = (1 << 2),
+	KS_CUSTOM = (1 << 3),
+	KS_ARRAY_OF_POINTER = (1 << 4),
+	KS_END = (1UL << 31),
+};
+
+struct kstate_field {
+	const char *name;
+	size_t offset;
+	size_t size;
+	enum kstate_flags flags;
+	const struct kstate_description *ksd;
+	int version_id;
+	int (*restore)(void *mig_stream, void *obj, const struct kstate_field *field);
+	int (*save)(void *mig_stream, void *obj, const struct kstate_field *field);
+	int (*count)(void);
+};
+
+enum kstate_ids {
+	KSTATE_LAST_ID = -1,
+};
+
+struct kstate_description {
+	const char *name;
+	enum kstate_ids id;
+	atomic_t instance_id;
+	int version_id;
+	struct list_head state_list;
+
+	const struct kstate_field *fields;
+};
+
+struct state_entry {
+	u64 id;
+	struct list_head list;
+	struct kstate_description *kstd;
+	void *obj;
+};
+
+static inline bool kstate_get_byte(void **mig_stream)
+{
+	bool ret = **(u8 **)mig_stream;
+	(*mig_stream)++;
+	return ret;
+}
+static inline void *kstate_save_byte(void *mig_stream, u8 val)
+{
+	*(u8 *)mig_stream = val;
+	return mig_stream + sizeof(val);
+}
+
+static inline void *kstate_save_ulong(void *mig_stream, unsigned long val)
+{
+	*(unsigned long *)mig_stream = val;
+	return mig_stream + sizeof(val);
+}
+static inline unsigned long kstate_get_ulong(void **mig_stream)
+{
+	unsigned long ret = **(unsigned long **)mig_stream;
+	(*mig_stream) += sizeof(unsigned long);
+	return ret;
+}
+
+#ifdef CONFIG_KSTATE
+bool is_migrate_kernel(void);
+
+void save_migrate_state(unsigned long mig_stream);
+
+void __kstate_register(struct kstate_description *state,
+		void *obj, struct state_entry *se);
+int kstate_register(struct kstate_description *state, void *obj);
+
+struct kstate_entry;
+void *save_kstate(void *stream, int id, const struct kstate_description *kstate,
+		void *obj);
+void *restore_kstate(struct kstate_entry *ke, int id,
+		const struct kstate_description *kstate, void *obj);
+#else
+
+#define __kstate_register(state, obj, se)
+#define kstate_register(state, obj)
+
+static inline void save_migrate_state(unsigned long mig_stream) { }
+
+#endif
+
+
+#define KSTATE_SIMPLE(_f, _state) {			\
+		.name = (__stringify(_f)),		\
+		.size = sizeof_field(_state, _f),	\
+		.flags = KS_SIMPLE,			\
+		.offset = offsetof(_state, _f),		\
+	}
+
+#define KSTATE_POINTER(_f, _state) {			\
+		.name = (__stringify(_f)),		\
+		.size = sizeof(*(((_state *)0)->_f)),	\
+		.flags = KS_POINTER,			\
+		.offset = offsetof(_state, _f),		\
+	}
+
+#define KSTATE_END_OF_LIST() {	\
+		.flags = KS_END,\
+	}
+
+#endif
