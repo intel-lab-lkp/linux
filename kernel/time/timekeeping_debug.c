3f3e2bbdd28376d0e27f997c327dff702c48e9ad
Jeff Layton <jlayton@kernel.org>
2024-10-02 17:27:17 -0400

    timekeeping: add percpu counter for tracking floor swap events
    The mgtime_floor value is a global variable for tracking the latest
    fine-grained timestamp handed out. Because it's a global, track the
    number of times that a new floor value is assigned.
    
    Add a new percpu counter to the timekeeping code to track the number of
    floor swap events that have occurred. A later patch will add a debugfs
    file to display this counter alongside other stats involving multigrain
    timestamps.
    
    Tested-by: Randy Dunlap <rdunlap@infradead.org> # documentation bits
    Signed-off-by: Jeff Layton <jlayton@kernel.org>

--- kernel/time/timekeeping_debug.c
+++ kernel/time/timekeeping_debug.c
@@ -17,6 +17,9 @@
 
 #define NUM_BINS 32
 
+/* incremented every time mg_floor is updated */
+DEFINE_PER_CPU(unsigned long, timekeeping_mg_floor_swaps);
+
 static unsigned int sleep_time_bin[NUM_BINS] = {0};
 
 static int tk_debug_sleep_time_show(struct seq_file *s, void *data)
@@ -51,5 +54,16 @@ void tk_debug_account_sleep_time(const struct timespec64 *t)
 	sleep_time_bin[bin]++;
 	pm_deferred_pr_dbg("Timekeeping suspended for %lld.%03lu seconds\n",
 			   (s64)t->tv_sec, t->tv_nsec / NSEC_PER_MSEC);
+}
+
+unsigned long timekeeping_get_mg_floor_swaps(void)
+{
+	unsigned long sum = 0;
+	int cpu;
+
+	for_each_possible_cpu(cpu)
+		sum += data_race(per_cpu(timekeeping_mg_floor_swaps, cpu));
+
+	return sum;
 }
 
