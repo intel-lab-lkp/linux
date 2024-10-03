af70b855c1d6ad9b01997432ed7ebc16ce8a5bc7
Harry Wentland <harry.wentland@amd.com>
2024-10-03 16:00:52 -0400

    drm/colorop: Add NEXT property
    We'll construct color pipelines out of drm_colorop by
    chaining them via the NEXT pointer. NEXT will point to
    the next drm_colorop in the pipeline, or by 0 if we're
    at the end of the pipeline.
    
    Signed-off-by: Harry Wentland <harry.wentland@amd.com>

--- drivers/gpu/drm/drm_colorop.c
+++ drivers/gpu/drm/drm_colorop.c
@@ -88,6 +88,7 @@ static int drm_colorop_init(struct drm_device *dev, struct drm_colorop *colorop,
 	colorop->dev = dev;
 	colorop->type = type;
 	colorop->plane = plane;
+	colorop->next = NULL;
 
 	list_add_tail(&colorop->head, &config->colorop_list);
 	colorop->index = config->num_colorop++;
@@ -119,6 +120,16 @@ static int drm_colorop_init(struct drm_device *dev, struct drm_colorop *colorop,
 	drm_object_attach_property(&colorop->base,
 				   colorop->bypass_property,
 				   1);
+
+	/* next */
+	prop = drm_property_create_object(dev, DRM_MODE_PROP_IMMUTABLE | DRM_MODE_PROP_ATOMIC,
+					  "NEXT", DRM_MODE_OBJECT_COLOROP);
+	if (!prop)
+		return -ENOMEM;
+	colorop->next_property = prop;
+	drm_object_attach_property(&colorop->base,
+				   colorop->next_property,
+				   0);
 
 	return ret;
 }
@@ -293,3 +304,22 @@ const char *drm_get_colorop_curve_1d_type_name(enum drm_colorop_curve_1d_type ty
 
 	return colorop_curve_1d_type_names[type];
 }
+
+/**
+ * drm_colorop_set_next_property - sets the next pointer
+ * @colorop: drm colorop
+ * @next: next colorop
+ *
+ * Should be used when constructing the color pipeline
+ */
+void drm_colorop_set_next_property(struct drm_colorop *colorop, struct drm_colorop *next)
+{
+	if (!colorop->next_property)
+		return;
+
+	drm_object_property_set_value(&colorop->base,
+				      colorop->next_property,
+				      next ? next->base.id : 0);
+	colorop->next = next;
+}
+EXPORT_SYMBOL(drm_colorop_set_next_property);
