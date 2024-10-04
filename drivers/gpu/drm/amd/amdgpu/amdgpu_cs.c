d75f3264f099318e77a2c8a63fd58a5e58a1e7d8
Mohammed Anees <pvmohammedanees2003@gmail.com>
2024-10-05 05:29:04 +0530

    drm/amdgpu: prevent BO_HANDLES error from being overwritten
    Before this patch, if multiple BO_HANDLES chunks were submitted,
    the error -EINVAL would be correctly set but could be overwritten
    by the return value from amdgpu_cs_p1_bo_handles(). This patch
    ensures that once an error condition is detected, the function
    returns immediately, avoiding the overwriting of the error code.
    
    Signed-off-by: Mohammed Anees <pvmohammedanees2003@gmail.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_cs.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_cs.c
@@ -266,8 +266,9 @@ static int amdgpu_cs_pass1(struct amdgpu_cs_parser *p,
 			/* Only a single BO list is allowed to simplify handling. */
 			if (p->bo_list)
 				ret = -EINVAL;
+			else
+				ret = amdgpu_cs_p1_bo_handles(p, p->chunks[i].kdata);
 
-			ret = amdgpu_cs_p1_bo_handles(p, p->chunks[i].kdata);
 			if (ret)
 				goto free_partial_kdata;
 			break;
