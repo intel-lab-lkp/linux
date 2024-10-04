4d1d44ffde9efe223dae7b1ae1ec93496590fa7f
Ian Rogers <irogers@google.com>
2024-10-04 16:41:16 -0700

    perf stat: Drop metric-unit if unit is NULL
    Avoid cases like:
    ```
    $ perf stat -a -M topdownl1 -j -I 1000
    ...
    {"interval" : 11.127757275, "counter-value" : "85715898.000000", "unit" : "", "event" : "IDQ.MITE_UOPS", "event-runtime" : 988376123, "pcnt-running" : 100.00, "metric-value" : "0.000000", "metric-unit" : "(null)"}
    ...
    ```
    
    Signed-off-by: Ian Rogers <irogers@google.com>

--- tools/perf/util/stat-display.c
+++ tools/perf/util/stat-display.c
@@ -470,8 +470,9 @@ static void print_metric_json(struct perf_stat_config *config __maybe_unused,
 	struct outstate *os = ctx;
 	FILE *out = os->fh;
 
-	fprintf(out, "\"metric-value\" : \"%f\", ", val);
-	fprintf(out, "\"metric-unit\" : \"%s\"", unit);
+	fprintf(out, "\"metric-value\" : \"%f\"", val);
+	if (unit)
+		fprintf(out, ", \"metric-unit\" : \"%s\"", unit);
 	if (!config->metric_only)
 		fprintf(out, "}");
 }
