d433904aac1e987498a3f2b86275b578ead58247
Gary Guo <gary@garyguo.net>
2024-10-04 16:52:22 +0100

    rust: implement `kernel::sync::Refcount`
    This is a wrapping layer of `include/linux/refcount.h`. Currently only
    the most basic operations (read/set/inc/dec/dec_and_test) are implemented,
    additional methods can be implemented when they are needed.
    
    Currently the kernel refcount has already been used in `Arc`, however it
    calls into FFI directly.
    
    Cc: Will Deacon <will@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Boqun Feng <boqun.feng@gmail.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Signed-off-by: Gary Guo <gary@garyguo.net>

--- rust/helpers/refcount.c
+++ rust/helpers/refcount.c
@@ -8,9 +8,24 @@ refcount_t rust_helper_REFCOUNT_INIT(int n)
 	return (refcount_t)REFCOUNT_INIT(n);
 }
 
+unsigned int rust_helper_refcount_read(refcount_t *r)
+{
+	return refcount_read(r);
+}
+
+void rust_helper_refcount_set(refcount_t *r, int n)
+{
+	refcount_set(r, n);
+}
+
 void rust_helper_refcount_inc(refcount_t *r)
 {
 	refcount_inc(r);
+}
+
+void rust_helper_refcount_dec(refcount_t *r)
+{
+	refcount_dec(r);
 }
 
 bool rust_helper_refcount_dec_and_test(refcount_t *r)
