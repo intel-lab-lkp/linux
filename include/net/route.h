2ab3e60a43a63c886d1edfa011a1cdb3246026c6
Menglong Dong <menglong8.dong@gmail.com>
2024-10-07 15:47:02 +0800

    net: ip: make ip_route_input() return drop reasons
    In this commit, we make ip_route_input() return skb drop reasons that come
    from ip_route_input_noref().
    
    Meanwhile, adjust all the call to it.
    
    Signed-off-by: Menglong Dong <dongml2@chinatelecom.cn>

--- include/net/route.h
+++ include/net/route.h
@@ -209,8 +209,9 @@ int ip_route_use_hint(struct sk_buff *skb, __be32 dst, __be32 src,
 		      u8 tos, struct net_device *devin,
 		      const struct sk_buff *hint);
 
-static inline int ip_route_input(struct sk_buff *skb, __be32 dst, __be32 src,
-				 dscp_t dscp, struct net_device *devin)
+static inline enum skb_drop_reason
+ip_route_input(struct sk_buff *skb, __be32 dst, __be32 src, dscp_t dscp,
+	       struct net_device *devin)
 {
 	enum skb_drop_reason reason;
 
@@ -223,7 +224,7 @@ static inline int ip_route_input(struct sk_buff *skb, __be32 dst, __be32 src,
 	}
 	rcu_read_unlock();
 
-	return reason ? -EINVAL : 0;
+	return reason;
 }
 
 void ipv4_update_pmtu(struct sk_buff *skb, struct net *net, u32 mtu, int oif,
