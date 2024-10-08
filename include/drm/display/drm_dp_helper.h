e1ae1a8f7dbb6fbeaa3109f85d6b3c4db7d11707
Sunil Khatri <sunil.khatri@amd.com>
2024-10-08 14:07:09 +0530

    drm/dp: fix warning for target_rr_divide missing comment
    include/drm/display/drm_dp_helper.h:127: warning:
    Function parameter or struct member 'target_rr_divider'
    not described in 'drm_dp_as_sdp'
    
    Signed-off-by: Sunil Khatri <sunil.khatri@amd.com>
    Cc: Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>
    Cc: Arun R Murthy <arun.r.murthy@intel.com>
    Cc: Suraj Kandpal <suraj.kandpal@intel.com>

--- include/drm/display/drm_dp_helper.h
+++ include/drm/display/drm_dp_helper.h
@@ -112,6 +112,7 @@ struct drm_dp_vsc_sdp {
  * @target_rr: Target Refresh
  * @duration_incr_ms: Successive frame duration increase
  * @duration_decr_ms: Successive frame duration decrease
+ * @target_rr_divider: Refresh rate divider
  * @mode: Adaptive Sync Operation Mode
  */
 struct drm_dp_as_sdp {
