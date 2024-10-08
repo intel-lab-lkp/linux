4e06db53f7e2f80ec5930c7a1e434f033ac467bd
Vinod Govindapillai <vinod.govindapillai@intel.com>
2024-10-08 11:23:20 +0300

    drm/i915/display: create a common function to check validity of ELD
    We would need to check the validity of connector ELD from multiple
    places in the follow up patches. So create a separate function to
    check the validity for ELD.
    
    Signed-off-by: Vinod Govindapillai <vinod.govindapillai@intel.com>

--- drivers/gpu/drm/i915/display/intel_audio.c
+++ drivers/gpu/drm/i915/display/intel_audio.c
@@ -690,13 +690,10 @@ void intel_audio_sdp_split_update(const struct intel_crtc_state *crtc_state)
 			     crtc_state->sdp_split_enable ? AUD_ENABLE_SDP_SPLIT : 0);
 }
 
-bool intel_audio_compute_config(struct intel_crtc_state *crtc_state,
-				struct drm_connector_state *conn_state)
+static bool intel_audio_eld_valid(struct drm_connector_state *conn_state)
 {
 	struct drm_connector *connector = conn_state->connector;
 	struct drm_i915_private *i915 = to_i915(connector->dev);
-	const struct drm_display_mode *adjusted_mode =
-		&crtc_state->hw.adjusted_mode;
 
 	if (!connector->eld[0]) {
 		drm_dbg_kms(&i915->drm,
@@ -704,6 +701,19 @@ bool intel_audio_compute_config(struct intel_crtc_state *crtc_state,
 			    connector->base.id, connector->name);
 		return false;
 	}
+
+	return true;
+}
+
+bool intel_audio_compute_config(struct intel_crtc_state *crtc_state,
+				struct drm_connector_state *conn_state)
+{
+	struct drm_connector *connector = conn_state->connector;
+	const struct drm_display_mode *adjusted_mode =
+		&crtc_state->hw.adjusted_mode;
+
+	if (!intel_audio_eld_valid(conn_state))
+		return false;
 
 	BUILD_BUG_ON(sizeof(crtc_state->eld) != sizeof(connector->eld));
 	memcpy(crtc_state->eld, connector->eld, sizeof(crtc_state->eld));
