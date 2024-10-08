79ec4c67cf7b3bd692c104447fcdec4fb100fc14
Julien Meunier <julien.meunier@nokia.com>
2024-10-08 22:02:25 +0200

    coresight: etm4x: Fix PID tracing when perf is run in an init PID namespace
    The previous implementation limited the tracing capabilities when perf
    was run in the init PID namespace, making it impossible to trace
    applications in non-init PID namespaces.
    
    This update improves the tracing process by verifying the event owner.
    This allows us to determine whether the user has the necessary
    permissions to trace the application.
    
    Cc: stable@vger.kernel.org
    Fixes: aab473867fed ("coresight: etm4x: Don't trace PID for non-root PID namespace")
    Signed-off-by: Julien Meunier <julien.meunier@nokia.com>

--- drivers/hwtracing/coresight/coresight-etm4x-core.c
+++ drivers/hwtracing/coresight/coresight-etm4x-core.c
@@ -693,9 +693,9 @@ static int etm4_parse_event_config(struct coresight_device *csdev,
 		config->cfg |= TRCCONFIGR_TS;
 	}
 
-	/* Only trace contextID when runs in root PID namespace */
+	/* Only trace contextID when the event owner is in root PID namespace */
 	if ((attr->config & BIT(ETM_OPT_CTXTID)) &&
-	    task_is_in_init_pid_ns(current))
+	    task_is_in_init_pid_ns(event->owner))
 		/* bit[6], Context ID tracing bit */
 		config->cfg |= TRCCONFIGR_CID;
 
@@ -709,8 +709,8 @@ static int etm4_parse_event_config(struct coresight_device *csdev,
 			ret = -EINVAL;
 			goto out;
 		}
-		/* Only trace virtual contextID when runs in root PID namespace */
-		if (task_is_in_init_pid_ns(current))
+		/* Only trace virtual contextID when the event owner is in root PID namespace */
+		if (task_is_in_init_pid_ns(event->owner))
 			config->cfg |= TRCCONFIGR_VMID | TRCCONFIGR_VMIDOPT;
 	}
 
