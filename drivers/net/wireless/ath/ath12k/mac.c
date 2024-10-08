894a6bbd80de664b7eb7ca9fb07dacb89f367512
Kang Yang <quic_kangyang@quicinc.com>
2024-10-08 15:35:30 +0800

    wifi: ath12k: properly handling the state variables of monitor mode
    Now monitor mode has some status variables, such as monitor_started,
    monitor_vdev_created. They are not always handled correctly.
    
    For monitor_started, it should be true when driver works in monitor
    mode, and should be false at other times.
    
    For monitor_vdev_id, it should be -1 when driver doesn't work in monitor
    mode.
    
    For monitor_vdev_created, it should be true after monitor vdev is
    created successfully and should be false at other times.
    
    For monitor_conf_enabled, it should be true when mac80211 sets
    IEEE80211_CONF_MONITOR, and should be false at other times.
    
    Handle those state variables according to above descriptions.
    
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0.c5-00481-QCAHMTSWPL_V1.0_V2.0_SILICONZ-3
    
    Signed-off-by: Kang Yang <quic_kangyang@quicinc.com>
    Acked-by: Jeff Johnson <quic_jjohnson@quicinc.com>

--- drivers/net/wireless/ath/ath12k/mac.c
+++ drivers/net/wireless/ath/ath12k/mac.c
@@ -6573,6 +6573,9 @@ static int ath12k_mac_vdev_create(struct ath12k *ar, struct ieee80211_vif *vif)
 			goto err_peer_del;
 		}
 		break;
+	case WMI_VDEV_TYPE_MONITOR:
+		ar->monitor_vdev_created = true;
+		break;
 	default:
 		break;
 	}
@@ -6622,6 +6625,8 @@ err_peer_del:
 err_vdev_del:
 	ath12k_wmi_vdev_delete(ar, arvif->vdev_id);
 	ar->num_created_vdevs--;
+	ar->monitor_vdev_created = false;
+	ar->monitor_vdev_id = -1;
 	arvif->is_created = false;
 	arvif->ar = NULL;
 	ar->allocated_vdev_map &= ~(1LL << arvif->vdev_id);
@@ -9339,6 +9344,11 @@ static void ath12k_mac_setup(struct ath12k *ar)
 
 	INIT_WORK(&ar->wmi_mgmt_tx_work, ath12k_mgmt_over_wmi_tx_work);
 	skb_queue_head_init(&ar->wmi_mgmt_tx_queue);
+
+	ar->monitor_vdev_id = -1;
+	ar->monitor_started = false;
+	ar->monitor_vdev_created = false;
+	ar->monitor_conf_enabled = false;
 }
 
 int ath12k_mac_register(struct ath12k_base *ab)
