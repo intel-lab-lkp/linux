b4eb373a7837618a50695e9e48723ff3338c753a
Sanman Pradhan <sanmanpradhan@meta.com>
2024-10-08 07:32:12 -0700

    eth: fbnic: Add hardware monitoring support via HWMON interface
    This patch adds support for hardware monitoring to the fbnic driver,
    allowing for temperature and voltage sensor data to be exposed to
    userspace via the HWMON interface. The driver registers a HWMON device
    and provides callbacks for reading sensor data, enabling system
    admins to monitor the health and operating conditions of fbnic.
    
    Signed-off-by: Sanman Pradhan <sanmanpradhan@meta.com>

--- drivers/net/ethernet/meta/fbnic/fbnic_pci.c
+++ drivers/net/ethernet/meta/fbnic/fbnic_pci.c
@@ -289,9 +289,11 @@ static int fbnic_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 	fbnic_devlink_register(fbd);
 
+	fbnic_hwmon_register(fbd);
+
 	if (!fbd->dsn) {
 		dev_warn(&pdev->dev, "Reading serial number failed\n");
-		goto init_failure_mode;
+		goto hwmon_unregister;
 	}
 
 	netdev = fbnic_netdev_alloc(fbd);
@@ -310,6 +312,8 @@ static int fbnic_probe(struct pci_dev *pdev, const struct pci_device_id *ent)
 
 ifm_free_netdev:
 	fbnic_netdev_free(fbd);
+hwmon_unregister:
+	fbnic_hwmon_unregister(fbd);
 init_failure_mode:
 	dev_warn(&pdev->dev, "Probe error encountered, entering init failure mode. Normal networking functionality will not be available.\n");
 	 /* Always return 0 even on error so devlink is registered to allow
@@ -345,6 +349,7 @@ static void fbnic_remove(struct pci_dev *pdev)
 		fbnic_netdev_free(fbd);
 	}
 
+	fbnic_hwmon_unregister(fbd);
 	fbnic_devlink_unregister(fbd);
 	fbnic_fw_disable_mbx(fbd);
 	fbnic_free_irqs(fbd);
