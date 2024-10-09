091140181b772cfaa07b7b88be7994ea3f0cc736
Babu Moger <babu.moger@amd.com>
2024-10-09 12:39:47 -0500

    x86/resctrl: Configure mbm_cntr_assign mode if supported
    Configure mbm_cntr_assign on AMD. 'mbm_cntr_assign' mode in AMD is ABMC
    (Assignable Bandwidth Monitoring Counters). It is enabled by default when
    supported on the system.
    
    When the ABMC is updated, it must be updated on all the logical processors
    in the resctrl domain.
    
    Signed-off-by: Babu Moger <babu.moger@amd.com>

--- arch/x86/kernel/cpu/resctrl/monitor.c
+++ arch/x86/kernel/cpu/resctrl/monitor.c
@@ -1268,6 +1268,7 @@ int __init rdt_get_mon_l3_config(struct rdt_resource *r)
 			cpuid_count(0x80000020, 5, &eax, &ebx, &ecx, &edx);
 			r->mon.num_mbm_cntrs = (ebx & GENMASK(15, 0)) + 1;
 			resctrl_file_fflags_init("num_mbm_cntrs", RFTYPE_MON_INFO);
+			hw_res->mbm_cntr_assign_enabled = true;
 		}
 	}
 
