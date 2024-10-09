fe15d14ec2793c4e3088eb5647bd91c5aeda0063
Babu Moger <babu.moger@amd.com>
2024-10-09 12:39:38 -0500

    x86/resctrl: Introduce mbm_cntr_map to track assignable counters at domain
    The MBM counters are allocated globally and assigned to an RMID, event pair
    in a resctrl group. It is tracked by mbm_cntr_free_map. Counters are
    assigned to the domain based on the user input. It needs to be tracked
    at domain level also.
    
    Add the mbm_cntr_map bitmap in struct rdt_mon_domain to keep track of
    assignment at domain level. The global counter at mbm_cntr_free_map can
    be released when assignment at all the domains are cleared.
    
    Signed-off-by: Babu Moger <babu.moger@amd.com>

--- arch/x86/kernel/cpu/resctrl/rdtgroup.c
+++ arch/x86/kernel/cpu/resctrl/rdtgroup.c
@@ -4093,6 +4093,7 @@ static void __init rdtgroup_setup_default(void)
 
 static void domain_destroy_mon_state(struct rdt_mon_domain *d)
 {
+	bitmap_free(d->mbm_cntr_map);
 	bitmap_free(d->rmid_busy_llc);
 	kfree(d->mbm_total);
 	kfree(d->mbm_local);
@@ -4163,6 +4164,15 @@ static int domain_setup_mon_state(struct rdt_resource *r, struct rdt_mon_domain 
 		if (!d->mbm_local) {
 			bitmap_free(d->rmid_busy_llc);
 			kfree(d->mbm_total);
+			return -ENOMEM;
+		}
+	}
+	if (is_mbm_enabled() && r->mon.mbm_cntr_assignable) {
+		d->mbm_cntr_map = bitmap_zalloc(r->mon.num_mbm_cntrs, GFP_KERNEL);
+		if (!d->mbm_cntr_map) {
+			bitmap_free(d->rmid_busy_llc);
+			kfree(d->mbm_total);
+			kfree(d->mbm_local);
 			return -ENOMEM;
 		}
 	}
