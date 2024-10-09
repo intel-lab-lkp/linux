44ff434a221deb7e0e7f17dd9e0b6629b65ba9de
Anastasia Kovaleva <a.kovaleva@yadro.com>
2024-10-09 14:16:52 +0300

    scsi: qla2xxx: Drop starvation counter on success
    Long-lived sessions under high load can accumulate a starvation counter,
    and the current implementation does not allow this counter to be reset
    during an active session.
    
    If HBA sends correct ATIO IOCB, then it has enough resources to process
    commands and we should not call ISP recovery.
    
    Cc: stable@vger.kernel.org
    Fixes: ead038556f64 ("qla2xxx: Add Dual mode support in the driver")
    Signed-off-by: Anastasia Kovaleva <a.kovaleva@yadro.com>
    Reviewed-by: Dmitry Bogdanov <d.bogdanov@yadro.com>

--- drivers/scsi/qla2xxx/qla_target.c
+++ drivers/scsi/qla2xxx/qla_target.c
@@ -6826,6 +6826,12 @@ qlt_24xx_process_atio_queue(struct scsi_qla_host *vha, uint8_t ha_locked)
 			qlt_send_term_exchange(ha->base_qpair, NULL, pkt,
 			    ha_locked, 0);
 		} else {
+			/*
+			 * If we get correct ATIO, then HBA had enough memory
+			 * to proceed without reset.
+			 */
+			WRITE_ONCE(&vha->hw->exch_starvation, 0);
+
 			qlt_24xx_atio_pkt_all_vps(vha,
 			    (struct atio_from_isp *)pkt, ha_locked);
 		}
