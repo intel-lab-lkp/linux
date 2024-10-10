eac2598e8e224d032205dac384feaf60d374cca2
Namhyung Kim <namhyung@kernel.org>
2024-10-09 22:16:20 -0700

    perf test: Fix probe testsuite with a new error message
    On my system, it's constantly failing because of new error message from
    perf probe.  It should update the regex pattern to match the message -
    "A function DIE doesn't have decl_line. Maybe broken DWARF?".
    
      $ sudo head -n 2 /sys/kernel/debug/kprobes/blacklist | cut -f2
      warn_thunk_thunk
      asm_exc_divide_error
    
      $ sudo perf probe warn_thunk_thunk
      A function DIE doesn't have decl_line. Maybe broken DWARF?
      A function DIE doesn't have decl_line. Maybe broken DWARF?
      Probe point 'warn_thunk_thunk' not found.
        Error: Failed to add events.
    
      $ sudo perf probe asm_exc_overflow
      Failed to find scope of probe point.
        Error: Failed to add events.
    
    Cc: Masami Hiramatsu <mhiramat@kernel.org>
    Cc: Michael Petlan <mpetlan@redhat.com>
    Cc: Veronika Molnarova <vmolnaro@redhat.com>
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/tests/shell/base_probe/test_adding_blacklisted.sh
+++ tools/perf/tests/shell/base_probe/test_adding_blacklisted.sh
@@ -42,7 +42,8 @@ REGEX_ERROR_MESSAGE="Error: Failed to add events."
 REGEX_INVALID_ARGUMENT="Failed to write event: Invalid argument"
 REGEX_SYMBOL_FAIL="Failed to find symbol at $RE_ADDRESS"
 REGEX_OUT_SECTION="$BLACKFUNC is out of \.\w+, skip it"
-../common/check_all_lines_matched.pl "$REGEX_SKIP_MESSAGE" "$REGEX_NOT_FOUND_MESSAGE" "$REGEX_ERROR_MESSAGE" "$REGEX_SCOPE_FAIL" "$REGEX_INVALID_ARGUMENT" "$REGEX_SYMBOL_FAIL" "$REGEX_OUT_SECTION" < $LOGS_DIR/adding_blacklisted.err
+REGEX_BROKEN_DWARF="A function DIE doesn\'t have decl_line\. Maybe broken DWARF\?"
+../common/check_all_lines_matched.pl "$REGEX_SKIP_MESSAGE" "$REGEX_NOT_FOUND_MESSAGE" "$REGEX_ERROR_MESSAGE" "$REGEX_SCOPE_FAIL" "$REGEX_INVALID_ARGUMENT" "$REGEX_SYMBOL_FAIL" "$REGEX_OUT_SECTION" "$REGEX_BROKEN_DWARF" < $LOGS_DIR/adding_blacklisted.err
 CHECK_EXIT_CODE=$?
 
 print_results $PERF_EXIT_CODE $CHECK_EXIT_CODE "adding blacklisted function $BLACKFUNC"
