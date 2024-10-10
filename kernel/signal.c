e9f5c0a5149e6e23aa9570341133e842deb84880
Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
2024-10-10 19:15:43 +0100

    pidfd: extend pidfd_get_pid() and de-duplicate pid lookup
    The means by which a pid is determined from a pidfd is duplicated, with
    some callers holding a reference to the (pid)fd, and others explicitly
    pinning the pid.
    
    Introduce __pidfd_get_pid() which abstracts both approaches and provide
    optional output parameters for file->f_flags and the fd (the latter of
    which, if provided, prevents the function from decrementing the fd's
    refernce count).
    
    Additionally, allow the ability to open a pidfd by opening a /proc/<pid>
    directory, utilised by the pidfd_send_signal() system call, providing a
    pidfd_get_pid_proc() helper function to do so.
    
    Doing this allows us to eliminate open-coded pidfd pid lookup and to
    consistently handle this in one place.
    
    This lays the groundwork for a subsequent patch which adds a new sentinel
    pidfd to explicitly reference the current process (i.e. thread group
    leader) without the need for a pidfd.
    
    Signed-off-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>

--- kernel/signal.c
+++ kernel/signal.c
@@ -3875,17 +3875,6 @@ static int copy_siginfo_from_user_any(kernel_siginfo_t *kinfo,
 	return copy_siginfo_from_user(kinfo, info);
 }
 
-static struct pid *pidfd_to_pid(const struct file *file)
-{
-	struct pid *pid;
-
-	pid = pidfd_pid(file);
-	if (!IS_ERR(pid))
-		return pid;
-
-	return tgid_pidfd_to_pid(file);
-}
-
 #define PIDFD_SEND_SIGNAL_FLAGS                            \
 	(PIDFD_SIGNAL_THREAD | PIDFD_SIGNAL_THREAD_GROUP | \
 	 PIDFD_SIGNAL_PROCESS_GROUP)
@@ -3908,10 +3897,11 @@ SYSCALL_DEFINE4(pidfd_send_signal, int, pidfd, int, sig,
 		siginfo_t __user *, info, unsigned int, flags)
 {
 	int ret;
-	struct fd f;
 	struct pid *pid;
 	kernel_siginfo_t kinfo;
 	enum pid_type type;
+	unsigned int f_flags;
+	struct fd f;
 
 	/* Enforce flags be set to 0 until we add an extension. */
 	if (flags & ~PIDFD_SEND_SIGNAL_FLAGS)
@@ -3921,12 +3911,8 @@ SYSCALL_DEFINE4(pidfd_send_signal, int, pidfd, int, sig,
 	if (hweight32(flags & PIDFD_SEND_SIGNAL_FLAGS) > 1)
 		return -EINVAL;
 
-	f = fdget(pidfd);
-	if (!fd_file(f))
-		return -EBADF;
-
 	/* Is this a pidfd? */
-	pid = pidfd_to_pid(fd_file(f));
+	pid = pidfd_to_pid_proc(pidfd, &f_flags, &f);
 	if (IS_ERR(pid)) {
 		ret = PTR_ERR(pid);
 		goto err;
@@ -3939,7 +3925,7 @@ SYSCALL_DEFINE4(pidfd_send_signal, int, pidfd, int, sig,
 	switch (flags) {
 	case 0:
 		/* Infer scope from the type of pidfd. */
-		if (fd_file(f)->f_flags & PIDFD_THREAD)
+		if (f_flags & PIDFD_THREAD)
 			type = PIDTYPE_PID;
 		else
 			type = PIDTYPE_TGID;
