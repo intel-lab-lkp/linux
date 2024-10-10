af45b3696160bbd262f42900e4b2d93281a18b62
Mickaël Salaün <mic@digikod.net>
2024-10-10 17:26:45 +0200

    ipe: Fix inode numbers in audit records
    Use the new inode_get_ino() helper to log the user space's view of
    inode's numbers instead of the private kernel values.
    
    Cc: Fan Wu <wufan@linux.microsoft.com>
    Signed-off-by: Mickaël Salaün <mic@digikod.net>

--- security/ipe/audit.c
+++ security/ipe/audit.c
@@ -150,7 +150,7 @@ void ipe_audit_match(const struct ipe_eval_ctx *const ctx,
 		if (inode) {
 			audit_log_format(ab, " dev=");
 			audit_log_untrustedstring(ab, inode->i_sb->s_id);
-			audit_log_format(ab, " ino=%lu", inode->i_ino);
+			audit_log_format(ab, " ino=%llu", inode_get_ino(inode));
 		} else {
 			audit_log_format(ab, " dev=? ino=?");
 		}
