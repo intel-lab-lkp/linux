db3bdfc6c4b2d6096c4cd766dd2ac1a94b9f83eb
Tatsuya S <tatsuya.s2862@gmail.com>
2024-10-10 22:02:59 +0900

    ftrace: Fix function name for trampoline
    The issue that unrelated function name is shown on stack trace like
    following even though it should be trampoline code address is caused by
    the creation of trampoline code in the area where .init.text section
    of module was freed after module is loaded.
    
    bash-1344    [002] .....    43.644608: <stack trace>
      => (MODULE INIT FUNCTION)
      => vfs_write
      => ksys_write
      => do_syscall_64
      => entry_SYSCALL_64_after_hwframe
    
    To resolve this, when function address of stack trace entry is in
    trampoline, output without looking up symbol name.
    
    Signed-off-by: Tatsuya S <tatsuya.s2862@gmail.com>

--- kernel/trace/trace_output.c
+++ kernel/trace/trace_output.c
@@ -1246,6 +1246,11 @@ static enum print_line_t trace_stack_print(struct trace_iterator *iter,
 			break;
 
 		trace_seq_puts(s, " => ");
+		if (is_ftrace_trampoline((*p) + delta)) {
+			trace_seq_printf(s, "0x%08lx", (*p) + delta);
+			trace_seq_putc(s, '\n');
+			continue;
+		}
 		seq_print_ip_sym(s, (*p) + delta, flags);
 		trace_seq_putc(s, '\n');
 	}
