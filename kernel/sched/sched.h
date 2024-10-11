5b671532bc0825354bb905f3202f2f9674f75184
xw357835 <xw357835@alibaba-inc.com>
2024-10-11 14:11:45 +0800

    sched/eevdf: Reduce the computation frequency of avg_vruntime
    The current code subtracts the value of curr from avg_vruntime and avg_load
    during runtime. Then, every time avg_vruntime() is called, it adds the
    value of curr to the avg_vruntime and avg_load. Afterward, it divides these
    and adds min_vruntime to obtain the actual avg_vruntime.
    
    Analysis of the code indicates that avg_vruntime only changes significantly
    during update_curr(), update_min_vruntime(), and when tasks are enqueued or
    dequeued. Therefore, it is sufficient to recalculate and store avg_vruntime
    only in these specific scenarios. This optimization ensures that accessing
    avg_vruntime() does not necessitate a recalculation each time, thereby
    enhancing the efficiency of the code.
    
    There is no need to subtract curr’s load from avg_load during runtime.
    Instead, we only need to calculate the incremental change and update
    avg_vruntime whenever curr’s time is updated.
    
    To better represent their functions, rename the original avg_vruntime and
    avg_load to tot_vruntime and tot_load, respectively, which more accurately
    describes their roles in the computation.
    
    Signed-off-by: xw357835 <xw357835@alibaba-inc.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -650,8 +650,9 @@ struct cfs_rq {
 	unsigned int		idle_nr_running;   /* SCHED_IDLE */
 	unsigned int		idle_h_nr_running; /* SCHED_IDLE */
 
+	s64			tot_vruntime;
 	s64			avg_vruntime;
-	u64			avg_load;
+	u64			tot_load;
 
 	u64			min_vruntime;
 #ifdef CONFIG_SCHED_CORE
