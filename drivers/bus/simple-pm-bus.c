1113b67eebf248e186ba2a308f8b5f7573e4be7f
Jisheng Zhang <jszhang@kernel.org>
2024-10-14 08:21:29 +0800

    bus: simple-pm-bus: Add CONFIG_ARM_AMBA for amba compatible of_device_id
    I'm trying to run linux on some platforms with small size memory, so
    every byte matters. The of_device_id is 196 Bytes on 32bit platforms,
    add CONFIG_ARM_AMBA macro around the amba bus' of_device_id so that it
    is removed if amba bus isn't used.
    
    Signed-off-by: Jisheng Zhang <jszhang@kernel.org>

--- drivers/bus/simple-pm-bus.c
+++ drivers/bus/simple-pm-bus.c
@@ -121,7 +121,9 @@ static const struct of_device_id simple_pm_bus_of_match[] = {
 	{ .compatible = "simple-bus",	.data = ONLY_BUS },
 	{ .compatible = "simple-mfd",	.data = ONLY_BUS },
 	{ .compatible = "isa",		.data = ONLY_BUS },
+#ifdef CONFIG_ARM_AMBA
 	{ .compatible = "arm,amba-bus",	.data = ONLY_BUS },
+#endif /* CONFIG_ARM_AMBA */
 	{ /* sentinel */ }
 };
 MODULE_DEVICE_TABLE(of, simple_pm_bus_of_match);
