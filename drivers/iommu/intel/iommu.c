b3f8d7057b3ca2b42f9cd097bfb697de413e6a9b
Klaus Jensen <k.jensen@samsung.com>
2024-10-15 23:08:15 +0200

    iommu/vt-d: drop pasid requirement for prq initialization
    PASID support within the IOMMU is not required to enable the Page
    Request Queue, only the PRS capability.
    
    Signed-off-by: Klaus Jensen <k.jensen@samsung.com>
    Reviewed-by: Kevin Tian <kevin.tian@intel.com>
    Signed-off-by: Joel Granados <joel.granados@kernel.org>

--- drivers/iommu/intel/iommu.c
+++ drivers/iommu/intel/iommu.c
@@ -1428,10 +1428,8 @@ static void free_dmar_iommu(struct intel_iommu *iommu)
 	/* free context mapping */
 	free_context_table(iommu);
 
-	if (pasid_supported(iommu)) {
-		if (ecap_prs(iommu->ecap))
-			intel_iommu_finish_prq(iommu);
-	}
+	if (ecap_prs(iommu->ecap))
+		intel_iommu_finish_prq(iommu);
 }
 
 /*
@@ -2352,7 +2350,7 @@ static int __init init_dmars(void)
 
 		iommu_flush_write_buffer(iommu);
 
-		if (pasid_supported(iommu) && ecap_prs(iommu->ecap)) {
+		if (ecap_prs(iommu->ecap)) {
 			/*
 			 * Call dmar_alloc_hwirq() with dmar_global_lock held,
 			 * could cause possible lock race condition.
@@ -2783,7 +2781,7 @@ static int intel_iommu_add(struct dmar_drhd_unit *dmaru)
 	intel_iommu_init_qi(iommu);
 	iommu_flush_write_buffer(iommu);
 
-	if (pasid_supported(iommu) && ecap_prs(iommu->ecap)) {
+	if (ecap_prs(iommu->ecap)) {
 		ret = intel_iommu_enable_prq(iommu);
 		if (ret)
 			goto disable_iommu;
