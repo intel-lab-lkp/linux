e874f683ddb6104fd9abe9105d07f2c3c4df8e09
Qi Zheng <zhengqi.arch@bytedance.com>
2024-10-17 17:47:22 +0800

    mm: zap_install_uffd_wp_if_needed: return whether uffd-wp pte has been re-installed
    In some cases, we'll replace the none pte with an uffd-wp swap special pte
    marker when necessary. Let's expose this information to the caller through
    the return value, so that subsequent commits can use this information to
    detect whether the PTE page is empty.
    
    Signed-off-by: Qi Zheng <zhengqi.arch@bytedance.com>

--- include/linux/mm_inline.h
+++ include/linux/mm_inline.h
@@ -555,8 +555,10 @@ static inline pte_marker copy_pte_marker(
  * pte, and if they see it, they'll fault and serialize at the pgtable lock.
  *
  * This function is a no-op if PTE_MARKER_UFFD_WP is not enabled.
+ *
+ * Returns true if an uffd-wp pte was installed, false otherwise.
  */
-static inline void
+static inline bool
 pte_install_uffd_wp_if_needed(struct vm_area_struct *vma, unsigned long addr,
 			      pte_t *pte, pte_t pteval)
 {
@@ -573,7 +575,7 @@ pte_install_uffd_wp_if_needed(struct vm_area_struct *vma, unsigned long addr,
 	 * with a swap pte.  There's no way of leaking the bit.
 	 */
 	if (vma_is_anonymous(vma) || !userfaultfd_wp(vma))
-		return;
+		return false;
 
 	/* A uffd-wp wr-protected normal pte */
 	if (unlikely(pte_present(pteval) && pte_uffd_wp(pteval)))
@@ -586,10 +588,13 @@ pte_install_uffd_wp_if_needed(struct vm_area_struct *vma, unsigned long addr,
 	if (unlikely(pte_swp_uffd_wp_any(pteval)))
 		arm_uffd_pte = true;
 
-	if (unlikely(arm_uffd_pte))
+	if (unlikely(arm_uffd_pte)) {
 		set_pte_at(vma->vm_mm, addr, pte,
 			   make_pte_marker(PTE_MARKER_UFFD_WP));
+		return true;
+	}
 #endif
+	return false;
 }
 
 static inline bool vma_has_recency(struct vm_area_struct *vma)
