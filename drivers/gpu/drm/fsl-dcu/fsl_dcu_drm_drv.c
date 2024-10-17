d00af319a23b0af56e2bc05ef88963fd2307d1df
Jiapeng Chong <jiapeng.chong@linux.alibaba.com>
2024-10-17 10:24:22 +0800

    drm/fsl-dcu: Remove unnecessary print function dev_err()
    The print function dev_err() is redundant because
    platform_get_irq_byname() already prints an error.
    
    ./drivers/gpu/drm/fsl-dcu/fsl_dcu_drm_drv.c:278:2-9: line 278 is redundant because platform_get_irq() already prints an error.
    
    Reported-by: Abaci Robot <abaci@linux.alibaba.com>
    Closes: https://bugzilla.openanolis.cn/show_bug.cgi?id=11384
    Signed-off-by: Jiapeng Chong <jiapeng.chong@linux.alibaba.com>

--- drivers/gpu/drm/fsl-dcu/fsl_dcu_drm_drv.c
+++ drivers/gpu/drm/fsl-dcu/fsl_dcu_drm_drv.c
@@ -273,11 +273,8 @@ static int fsl_dcu_drm_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	fsl_dev->irq = platform_get_irq(pdev, 0);
-	if (fsl_dev->irq < 0) {
-		dev_err(dev, "failed to get irq\n");
+	if (platform_get_irq(pdev, 0) < 0)
 		return fsl_dev->irq;
-	}
 
 	fsl_dev->regmap = devm_regmap_init_mmio(dev, base,
 			&fsl_dcu_regmap_config);
