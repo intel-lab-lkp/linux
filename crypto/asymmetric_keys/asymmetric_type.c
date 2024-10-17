177068dd2e2cfba7ddc02f7aafd5155e542b12da
Eric Snowberg <eric.snowberg@oracle.com>
2024-10-17 09:55:07 -0600

    keys: Add new verification type (VERIFYING_CLAVIS_SIGNATURE)
    Add a new verification type called VERIFYING_CLAVIS_SIGNATURE.  This new
    usage will be used for validating keys added to the new clavis LSM keyring.
    This will be introduced in a follow-on patch.
    
    Signed-off-by: Eric Snowberg <eric.snowberg@oracle.com>

--- crypto/asymmetric_keys/asymmetric_type.c
+++ crypto/asymmetric_keys/asymmetric_type.c
@@ -25,6 +25,7 @@ const char *const key_being_used_for[NR__KEY_BEING_USED_FOR] = {
 	[VERIFYING_KEY_SIGNATURE]		= "key sig",
 	[VERIFYING_KEY_SELF_SIGNATURE]		= "key self sig",
 	[VERIFYING_UNSPECIFIED_SIGNATURE]	= "unspec sig",
+	[VERIFYING_CLAVIS_SIGNATURE]		= "clavis sig",
 };
 EXPORT_SYMBOL_GPL(key_being_used_for);
 
