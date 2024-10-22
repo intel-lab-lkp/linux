8f105f9f3935beaeed86fdc02baf027c93951ace
Zhang Yi <yi.zhang@huawei.com>
2024-10-22 19:10:56 +0800

    ext4: enable large folio for regular file with iomap buffered I/O path
    Since we have converted the buffered I/O path to iomap for regular
    files, we can enable large folio support as well. This should result in
    significant performance gains for large I/O operations.
    
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>

--- fs/ext4/ialloc.c
+++ fs/ext4/ialloc.c
@@ -1333,8 +1333,10 @@ got:
 		}
 	}
 
-	if (ext4_should_use_buffered_iomap(inode))
+	if (ext4_should_use_buffered_iomap(inode)) {
 		ext4_set_inode_state(inode, EXT4_STATE_BUFFERED_IOMAP);
+		mapping_set_large_folios(inode->i_mapping);
+	}
 
 	ext4_update_inode_fsync_trans(handle, inode, 1);
 
