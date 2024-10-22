075e1353b271a44468b8e537181160bb69146085
Manas <manas18244@iiitd.ac.in>
2024-10-22 20:55:32 +0530

    bcachefs: Add missing btree_bitmap_shift check in bch2_dev_btree_bitmap_marked_sectors
    The syzkaller bug triggers BUG_ON assertions in
    __bch2_dev_btree_bitmap_mark. btree_bitmap_shift is checked in
    bch2_dev_btree_bitmap_marked_sectors prior to this. But only one out of
    two assertions is checked. This patch adds the other as conditional.
    
    Reported-by: syzbot+e8eff054face85d7ea41@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=e8eff054face85d7ea41
    Signed-off-by: Manas <manas18244@iiitd.ac.in>

--- fs/bcachefs/sb-members.h
+++ fs/bcachefs/sb-members.h
@@ -347,12 +347,16 @@ void bch2_dev_errors_reset(struct bch_dev *);
 static inline bool bch2_dev_btree_bitmap_marked_sectors(struct bch_dev *ca, u64 start, unsigned sectors)
 {
 	u64 end = start + sectors;
+	u8 bitmap_shift = ca->mi.btree_bitmap_shift;
 
-	if (end > 64ULL << ca->mi.btree_bitmap_shift)
+	if (bitmap_shift > 57)
+		return true;
+
+	if (end > 64ULL << bitmap_shift)
 		return false;
 
-	for (unsigned bit = start >> ca->mi.btree_bitmap_shift;
-	     (u64) bit << ca->mi.btree_bitmap_shift < end;
+	for (unsigned bit = start >> bitmap_shift;
+	     (u64) bit << bitmap_shift < end;
 	     bit++)
 		if (!(ca->mi.btree_allocated_bitmap & BIT_ULL(bit)))
 			return false;
