204168530561ba730251816d1081451abbb86d46
Jani Nikula <jani.nikula@intel.com>
2024-10-24 19:22:40 +0300

    drm: remove driver date from struct drm_driver and all drivers
    We stopped using the driver initialized date in commit 7fb8af6798e8
    ("drm: deprecate driver date") and (eventually) started returning "0"
    for drm_version ioctl instead.
    
    Finish the job, and remove the unused date member from struct
    drm_driver, its initialization from drivers, along with the common
    DRIVER_DATE macros.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_drv.c
@@ -2998,7 +2998,6 @@ static const struct drm_driver amdgpu_kms_driver = {
 
 	.name = DRIVER_NAME,
 	.desc = DRIVER_DESC,
-	.date = DRIVER_DATE,
 	.major = KMS_DRIVER_MAJOR,
 	.minor = KMS_DRIVER_MINOR,
 	.patchlevel = KMS_DRIVER_PATCHLEVEL,
@@ -3022,7 +3021,6 @@ const struct drm_driver amdgpu_partition_driver = {
 
 	.name = DRIVER_NAME,
 	.desc = DRIVER_DESC,
-	.date = DRIVER_DATE,
 	.major = KMS_DRIVER_MAJOR,
 	.minor = KMS_DRIVER_MINOR,
 	.patchlevel = KMS_DRIVER_PATCHLEVEL,
