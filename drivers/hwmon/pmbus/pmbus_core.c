f3b6b13ee27a90d82431a39a31143c73347418cf
Jerome Brunet <jbrunet@baylibre.com>
2024-10-24 20:10:38 +0200

    hwmon: (pmbus/core) clear faults after setting smbalert mask
    pmbus_write_smbalert_mask() ignores the errors if the chip can't set
    smbalert mask the standard way. It is not necessarily a problem for the irq
    support if the chip is otherwise properly setup but it may leave an
    uncleared fault behind.
    
    pmbus_core will pick the fault on the next register_check(). The register
    check will fails regardless of the actual register support by the chip.
    
    This leads to missing attributes or debugfs entries for chips that should
    provide them.
    
    We cannot rely on register_check() as PMBUS_SMBALERT_MASK may be read-only.
    
    Unconditionally clear the page fault after setting PMBUS_SMBALERT_MASK to
    avoid the problem.
    
    Suggested-by: Guenter Roeck <linux@roeck-us.net>
    Fixes: 221819ca4c36 ("hwmon: (pmbus/core) Add interrupt support")
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

--- drivers/hwmon/pmbus/pmbus_core.c
+++ drivers/hwmon/pmbus/pmbus_core.c
@@ -3346,7 +3346,12 @@ static int pmbus_regulator_notify(struct pmbus_data *data, int page, int event)
 
 static int pmbus_write_smbalert_mask(struct i2c_client *client, u8 page, u8 reg, u8 val)
 {
-	return _pmbus_write_word_data(client, page, PMBUS_SMBALERT_MASK, reg | (val << 8));
+	int ret;
+
+	ret = _pmbus_write_word_data(client, page, PMBUS_SMBALERT_MASK, reg | (val << 8));
+	pmbus_clear_fault_page(client, -1);
+
+	return ret;
 }
 
 static irqreturn_t pmbus_fault_handler(int irq, void *pdata)
