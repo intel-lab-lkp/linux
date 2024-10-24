f8ec478acc05f846216b1e303489d02c0c275a94
Daniel Almeida <daniel.almeida@collabora.com>
2024-10-24 11:20:02 -0300

    rust: irq: add support for request_irq()
    Both regular and threaded versions are supported.
    
    Signed-off-by: Daniel Almeida <daniel.almeida@collabora.com>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -13,6 +13,7 @@
 #include <linux/errname.h>
 #include <linux/ethtool.h>
 #include <linux/firmware.h>
+#include <linux/interrupt.h>
 #include <linux/jiffies.h>
 #include <linux/mdio.h>
 #include <linux/phy.h>
