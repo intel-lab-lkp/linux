23540ebbf6ea717a631fd4f8b56b1a5dc3696fb1
Mario Limonciello <mario.limonciello@amd.com>
2024-10-25 14:30:51 -0500

    ACPI: platform_profile: Add a list to platform profile handler
    In order to prepare for having support for multiple platform handlers
    a list will be needed to iterate over them for various platform
    profile handler calls.
    
    Tested-by: Matthew Schwartz <matthew.schwartz@linux.dev>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- include/linux/platform_profile.h
+++ include/linux/platform_profile.h
@@ -29,6 +29,7 @@ enum platform_profile_option {
 struct platform_profile_handler {
 	const char *name;
 	unsigned long choices[BITS_TO_LONGS(PLATFORM_PROFILE_LAST)];
+	struct list_head list;
 	int (*profile_get)(struct platform_profile_handler *pprof,
 				enum platform_profile_option *profile);
 	int (*profile_set)(struct platform_profile_handler *pprof,
