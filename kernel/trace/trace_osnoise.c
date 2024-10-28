45c6b3f3a88016afb0011dc57485f88ebcde8101
Nam Cao <namcao@linutronix.de>
2024-10-28 08:31:57 +0100

    tracing/osnoise: Switch to use hrtimer_setup()
    There is a newly introduced hrtimer_setup() which will replace
    hrtimer_init(). This new function is similar to the old one, except that it
    also sanity-checks and initializes the timer's callback function.
    
    Switch to use this new function.
    
    Patch was created by using Coccinelle.
    
    Signed-off-by: Nam Cao <namcao@linutronix.de>

--- kernel/trace/trace_osnoise.c
+++ kernel/trace/trace_osnoise.c
@@ -1896,8 +1896,7 @@ static int timerlat_main(void *data)
 	tlat->count = 0;
 	tlat->tracing_thread = false;
 
-	hrtimer_init(&tlat->timer, CLOCK_MONOTONIC, HRTIMER_MODE_ABS_PINNED_HARD);
-	tlat->timer.function = timerlat_irq;
+	hrtimer_setup(&tlat->timer, timerlat_irq, CLOCK_MONOTONIC, HRTIMER_MODE_ABS_PINNED_HARD);
 	tlat->kthread = current;
 	osn_var->pid = current->pid;
 	/*
@@ -2461,8 +2460,7 @@ static int timerlat_fd_open(struct inode *inode, struct file *file)
 	tlat = this_cpu_tmr_var();
 	tlat->count = 0;
 
-	hrtimer_init(&tlat->timer, CLOCK_MONOTONIC, HRTIMER_MODE_ABS_PINNED_HARD);
-	tlat->timer.function = timerlat_irq;
+	hrtimer_setup(&tlat->timer, timerlat_irq, CLOCK_MONOTONIC, HRTIMER_MODE_ABS_PINNED_HARD);
 
 	migrate_enable();
 	return 0;
