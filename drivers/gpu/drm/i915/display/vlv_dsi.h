1a91e3f2728be95ffbcc45a754beb76a97cdda63
Ville Syrjälä <ville.syrjala@linux.intel.com>
2024-10-29 23:52:12 +0200

    drm/i915/cdclk: Extract vlv_dsi_min_cdclk()
    Pull the DSI min cdclk calculation into a helper and hide
    it inside vlv_dsi.c in order to keep most DSI related
    details in one place.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- drivers/gpu/drm/i915/display/vlv_dsi.h
+++ drivers/gpu/drm/i915/display/vlv_dsi.h
@@ -8,19 +8,27 @@
 
 #include <linux/types.h>
 
+#include <drm/drm_mipi_dsi.h>
+
 enum port;
 struct drm_i915_private;
+struct intel_crtc_state;
 struct intel_dsi;
 
 #ifdef I915
 void vlv_dsi_wait_for_fifo_empty(struct intel_dsi *intel_dsi, enum port port);
 enum mipi_dsi_pixel_format pixel_format_from_register_bits(u32 fmt);
+int vlv_dsi_min_cdclk(const struct intel_crtc_state *crtc_state);
 void vlv_dsi_init(struct drm_i915_private *dev_priv);
 #else
 static inline void vlv_dsi_wait_for_fifo_empty(struct intel_dsi *intel_dsi, enum port port)
 {
 }
 static inline enum mipi_dsi_pixel_format pixel_format_from_register_bits(u32 fmt)
+{
+	return 0;
+}
+static inline int vlv_dsi_min_cdclk(const struct intel_crtc_state *crtc_state)
 {
 	return 0;
 }
