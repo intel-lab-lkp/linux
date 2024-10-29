e5501fd974da5484efdf619156a1991f7e292ae6
Babu Moger <babu.moger@amd.com>
2024-10-29 18:21:33 -0500

    x86/resctrl: Add __init attribute for the functions called in resctrl_late_init
    The function resctrl_late_init() has the __init attribute, but some
    functions it calls do not. Add the __init attribute to all the functions
    to maintain consistency throughout the call sequence.
    
    Fixes: 6a445edce657 ("x86/intel_rdt/cqm: Add RDT monitoring initialization")
    Fixes: def10853930a ("x86/intel_rdt: Add two new resources for L2 Code and Data Prioritization (CDP)")
    Fixes: bd334c86b5d7 ("x86/resctrl: Add __init attribute to rdt_get_mon_l3_config()")
    Suggested-by: Reinette Chatre <reinette.chatre@intel.com>
    Signed-off-by: Babu Moger <babu.moger@amd.com>

--- arch/x86/kernel/cpu/resctrl/core.c
+++ arch/x86/kernel/cpu/resctrl/core.c
@@ -275,7 +275,7 @@ static __init bool __rdt_get_mem_config_amd(struct rdt_resource *r)
 	return true;
 }
 
-static void rdt_get_cache_alloc_cfg(int idx, struct rdt_resource *r)
+static __init void rdt_get_cache_alloc_cfg(int idx, struct rdt_resource *r)
 {
 	struct rdt_hw_resource *hw_res = resctrl_to_arch_res(r);
 	union cpuid_0x10_1_eax eax;
@@ -294,7 +294,7 @@ static void rdt_get_cache_alloc_cfg(int idx, struct rdt_resource *r)
 	r->alloc_capable = true;
 }
 
-static void rdt_get_cdp_config(int level)
+static __init void rdt_get_cdp_config(int level)
 {
 	/*
 	 * By default, CDP is disabled. CDP can be enabled by mount parameter
@@ -304,12 +304,12 @@ static void rdt_get_cdp_config(int level)
 	rdt_resources_all[level].r_resctrl.cdp_capable = true;
 }
 
-static void rdt_get_cdp_l3_config(void)
+static __init void rdt_get_cdp_l3_config(void)
 {
 	rdt_get_cdp_config(RDT_RESOURCE_L3);
 }
 
-static void rdt_get_cdp_l2_config(void)
+static __init void rdt_get_cdp_l2_config(void)
 {
 	rdt_get_cdp_config(RDT_RESOURCE_L2);
 }
