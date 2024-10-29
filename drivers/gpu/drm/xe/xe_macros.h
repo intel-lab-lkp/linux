a14abd477bfeb5ec9eb865e66d55b597b8eef78f
Gyeyoung Baek <gye976@gmail.com>
2024-10-29 17:48:58 +0900

    drm/xe: Fix build error for XE_IOCTL_DBG macro
    if CONFIG_DRM_USE_DYNAMIC_DEBUG is set,
    'drm_dbg' function is replaced with '__dynamic_func_call_cls',
    which is replaced with a do while statement.
    so in the previous code, there are the following build errors.
    
    include/linux/dynamic_debug.h:221:58: error: expected expression before ‘do’
      221 | #define __dynamic_func_call_cls(id, cls, fmt, func, ...) do {   \
          |                                                          ^~
    include/linux/dynamic_debug.h:248:9: note: in expansion of macro ‘__dynamic_func_call_cls’
      248 |         __dynamic_func_call_cls(__UNIQUE_ID(ddebug), cls, fmt, func, ##__VA_ARGS__)
          |         ^~~~~~~~~~~~~~~~~~~~~~~
    include/drm/drm_print.h:425:9: note: in expansion of macro ‘_dynamic_func_call_cls’
      425 |         _dynamic_func_call_cls(cat, fmt, __drm_dev_dbg,         \
          |         ^~~~~~~~~~~~~~~~~~~~~~
    include/drm/drm_print.h:504:9: note: in expansion of macro ‘drm_dev_dbg’
      504 |         drm_dev_dbg((drm) ? (drm)->dev : NULL, DRM_UT_DRIVER, fmt, ##__VA_ARGS__)
          |         ^~~~~~~~~~~
    include/drm/drm_print.h:522:33: note: in expansion of macro ‘drm_dbg_driver’
      522 | #define drm_dbg(drm, fmt, ...)  drm_dbg_driver(drm, fmt, ##__VA_ARGS__)
          |                                 ^~~~~~~~~~~~~~
    drivers/gpu/drm/xe/xe_macros.h:14:21: note: in expansion of macro ‘drm_dbg’
       14 |         ((cond) && (drm_dbg(&(xe)->drm, \
          |                     ^~~~~~~
    drivers/gpu/drm/xe/xe_bo.c:2029:13: note: in expansion of macro ‘XE_IOCTL_DBG’
     2029 |         if (XE_IOCTL_DBG(xe, !gem_obj))
    
    the problem is that,
    XE_IOCTL_DBG uses this function for conditional expr.
    
    so I fix the expr to be compatible with the do while statement,
    by referring to "https://gcc.gnu.org/onlinedocs/gcc/Statement-Exprs.html".
    
    v2: I modified this to print when only cond is true.
    
    Signed-off-by: Gyeyoung Baek <gye976@gmail.com>

--- drivers/gpu/drm/xe/xe_macros.h
+++ drivers/gpu/drm/xe/xe_macros.h
@@ -11,8 +11,12 @@
 #define XE_WARN_ON WARN_ON
 
 #define XE_IOCTL_DBG(xe, cond) \
-	((cond) && (drm_dbg(&(xe)->drm, \
-			    "Ioctl argument check failed at %s:%d: %s", \
-			    __FILE__, __LINE__, #cond), 1))
+({ \
+	if ((cond)) \
+		drm_dbg(&(xe)->drm, \
+			"Ioctl argument check failed at %s:%d: %s", \
+			__FILE__, __LINE__, #cond); \
+	(cond); \
+})
 
 #endif
