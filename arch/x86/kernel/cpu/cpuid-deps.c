a9d1c93116ce2e85f17324dac58391bb4028b74d
Sohil Mehta <sohil.mehta@intel.com>
2024-10-30 23:31:18 +0000

    x86/cpufeature: Add feature dependency checks
    Currently, the cpuid-deps[] table is only exercised when a particular
    feature gets explicitly disabled and clear_cpu_cap() is called. However,
    some of these listed dependencies might already be missing during boot.
    These types of errors shouldn't generally happen in production
    environments but they could sometimes sneak through, especially when
    VMs and Kconfigs are in the mix.
    
    Unexpected failures can occur when the kernel tries to use such a
    feature. Rather than debug such scenarios, it would be better to
    disable the feature upfront.
    
    Add a simple boot time check for missing feature dependencies and
    disable any feature whose dependencies are not met.
    
    Signed-off-by: Sohil Mehta <sohil.mehta@intel.com>

--- arch/x86/kernel/cpu/cpuid-deps.c
+++ arch/x86/kernel/cpu/cpuid-deps.c
@@ -146,3 +146,13 @@ void setup_clear_cpu_cap(unsigned int feature)
 {
 	do_clear_cpu_cap(NULL, feature);
 }
+
+void filter_feature_dependencies(struct cpuinfo_x86 *c)
+{
+	const struct cpuid_dep *d;
+
+	for (d = cpuid_deps; d->feature; d++) {
+		if (cpu_has(c, d->feature) && !cpu_has(c, d->depends))
+			do_clear_cpu_cap(c, d->feature);
+	}
+}
