e1c1b5268697c7564975c6168d22ab5808c57876
Javier Carrasco <javier.carrasco.cruz@gmail.com>
2024-10-30 18:10:45 +0100

    drivers: soc: atmel: use automatic cleanup for device_node in atmel_soc_device_init()
    Switch to a more robust approach to automatically release the node when
    it goes out of scope, dropping the need for explicit calls to
    of_node_put().
    
    Signed-off-by: Javier Carrasco <javier.carrasco.cruz@gmail.com>

--- drivers/soc/atmel/soc.c
+++ drivers/soc/atmel/soc.c
@@ -399,15 +399,12 @@ static const struct of_device_id at91_soc_allowed_list[] __initconst = {
 
 static int __init atmel_soc_device_init(void)
 {
-	struct device_node *np = of_find_node_by_path("/");
+	struct device_node *np __free(device_node) = of_find_node_by_path("/");
 
-	if (!of_match_node(at91_soc_allowed_list, np)) {
-		of_node_put(np);
+	if (!of_match_node(at91_soc_allowed_list, np))
 		return 0;
-	}
 
 	at91_soc_init(socs);
-	of_node_put(np);
 
 	return 0;
 }
