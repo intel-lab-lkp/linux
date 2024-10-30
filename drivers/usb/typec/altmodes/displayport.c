e71b6b5eaeaef1024c79eba1a26cef52c1ab91d9
Abhishek Pandit-Subedi <abhishekpandit@chromium.org>
2024-10-30 14:28:34 -0700

    usb: typec: Auto enter control for alternate modes
    Add controls for whether an alternate mode is automatically entered when
    a partner connects. The auto_enter control is only available on ports
    and applies immediately after a partner connects. The default behavior
    is to enable auto enter and drivers must explicitly disable it.
    
    Signed-off-by: Abhishek Pandit-Subedi <abhishekpandit@chromium.org>

--- drivers/usb/typec/altmodes/displayport.c
+++ drivers/usb/typec/altmodes/displayport.c
@@ -767,8 +767,10 @@ int dp_altmode_probe(struct typec_altmode *alt)
 	if (plug)
 		typec_altmode_set_drvdata(plug, dp);
 
-	dp->state = plug ? DP_STATE_ENTER_PRIME : DP_STATE_ENTER;
-	schedule_work(&dp->work);
+	if (port->auto_enter) {
+		dp->state = plug ? DP_STATE_ENTER_PRIME : DP_STATE_ENTER;
+		schedule_work(&dp->work);
+	}
 
 	return 0;
 }
