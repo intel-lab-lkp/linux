7c5a5fdd67a5404e15082aa6aac7a5713e2e00ca
Suraj Sonawane <surajsonawane0215@gmail.com>
2024-10-30 18:01:26 +0530

    fs: hpfs: fix inconsistent indentation in hpfs_write_inode_ea
    Fix the indentation to ensure consistent code style and improve
    readability, and to fix this warning:
    
    fs/hpfs/inode.c:153 hpfs_write_inode_ea() warn: inconsistent indenting
    
    Signed-off-by: Suraj Sonawane <surajsonawane0215@gmail.com>

--- fs/hpfs/inode.c
+++ fs/hpfs/inode.c
@@ -150,7 +150,8 @@ static void hpfs_write_inode_ea(struct inode *i, struct fnode *fnode)
 		   Some unknown structures like ACL may be in fnode,
 		   we'd better not overwrite them
 		hpfs_error(i->i_sb, "fnode %08x has some unknown HPFS386 structures", i->i_ino);
-	} else*/ if (hpfs_sb(i->i_sb)->sb_eas >= 2) {
+	} else*/
+	if (hpfs_sb(i->i_sb)->sb_eas >= 2) {
 		__le32 ea;
 		if (!uid_eq(i->i_uid, hpfs_sb(i->i_sb)->sb_uid) || hpfs_inode->i_ea_uid) {
 			ea = cpu_to_le32(i_uid_read(i));
