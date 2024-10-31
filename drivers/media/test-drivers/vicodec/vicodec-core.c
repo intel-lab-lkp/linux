9e4beef457f5cf6e0c388248b2e12d9755edf03d
Hans Verkuil <hverkuil@xs4all.nl>
2024-10-31 08:49:00 +0100

    media: vicodec: add V4L2_CID_MIN_BUFFERS_FOR_* controls
    Stateful codecs must support the V4L2_CID_MIN_BUFFERS_FOR_OUTPUT
    and V4L2_CID_MIN_BUFFERS_FOR_CAPTURE controls. The vicodec driver
    was missing support for these controls. Add them.
    
    Signed-off-by: Hans Verkuil <hverkuil@xs4all.nl>

--- drivers/media/test-drivers/vicodec/vicodec-core.c
+++ drivers/media/test-drivers/vicodec/vicodec-core.c
@@ -1852,6 +1852,11 @@ static int vicodec_open(struct file *file)
 			  1, 31, 1, 20);
 	v4l2_ctrl_new_std(hdl, &vicodec_ctrl_ops, V4L2_CID_FWHT_P_FRAME_QP,
 			  1, 31, 1, 20);
+	if (!ctx->is_stateless)
+		v4l2_ctrl_new_std(hdl, &vicodec_ctrl_ops, ctx->is_enc ?
+				  V4L2_CID_MIN_BUFFERS_FOR_OUTPUT :
+				  V4L2_CID_MIN_BUFFERS_FOR_CAPTURE,
+				  1, 32, 1, 2);
 	if (ctx->is_enc)
 		v4l2_ctrl_new_std(hdl, &vicodec_ctrl_ops,
 				  V4L2_CID_MIN_BUFFERS_FOR_OUTPUT, 1, 1, 1, 1);
