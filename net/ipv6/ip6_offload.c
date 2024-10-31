9427013ad03d1adf1c2bfd575c87b5666aef68ab
Suraj Sonawane <surajsonawane0215@gmail.com>
2024-10-31 12:21:24 +0530

    net: ipv6: fix inconsistent indentation in ipv6_gro_receive
    Fix the indentation to ensure consistent code style and improve
    readability, and to fix this warning:
    
    net/ipv6/ip6_offload.c:280 ipv6_gro_receive() warn: inconsistent indenting
    
    Signed-off-by: Suraj Sonawane <surajsonawane0215@gmail.com>

--- net/ipv6/ip6_offload.c
+++ net/ipv6/ip6_offload.c
@@ -277,10 +277,10 @@ INDIRECT_CALLABLE_SCOPE struct sk_buff *ipv6_gro_receive(struct list_head *head,
 		 * (nlen != (sizeof(*iph2) + ipv6_exthdrs_len(iph2, &ops)))
 		 * memcmp() alone below is sufficient, right?
 		 */
-		 if ((first_word & htonl(0xF00FFFFF)) ||
-		     !ipv6_addr_equal(&iph->saddr, &iph2->saddr) ||
-		     !ipv6_addr_equal(&iph->daddr, &iph2->daddr) ||
-		     iph->nexthdr != iph2->nexthdr) {
+		if ((first_word & htonl(0xF00FFFFF)) ||
+		    !ipv6_addr_equal(&iph->saddr, &iph2->saddr) ||
+		    !ipv6_addr_equal(&iph->daddr, &iph2->daddr) ||
+		    iph->nexthdr != iph2->nexthdr) {
 not_same_flow:
 			NAPI_GRO_CB(p)->same_flow = 0;
 			continue;
