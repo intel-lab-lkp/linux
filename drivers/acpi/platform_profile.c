60a6f5b79754dd5d3b371dd38d0d18657495888b
Mario Limonciello <mario.limonciello@amd.com>
2024-10-30 23:09:36 -0500

    ACPI: platform_profile: Move sanity check out of the mutex
    The sanity check that the platform handler had choices set doesn't
    need the mutex taken.  Move it to earlier in the registration.
    
    Tested-by: Matthew Schwartz <matthew.schwartz@linux.dev>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- drivers/acpi/platform_profile.c
+++ drivers/acpi/platform_profile.c
@@ -180,6 +180,12 @@ int platform_profile_register(struct platform_profile_handler *pprof)
 {
 	int err;
 
+	/* Sanity check the profile handler */
+	if (!pprof || bitmap_empty(pprof->choices, PLATFORM_PROFILE_LAST) ||
+	    !pprof->profile_set || !pprof->profile_get) {
+		pr_err("platform_profile: handler is invalid\n");
+		return -EINVAL;
+	}
 	if (!pprof->dev) {
 		pr_err("platform_profile: handler device is not set\n");
 		return -EINVAL;
@@ -190,13 +196,6 @@ int platform_profile_register(struct platform_profile_handler *pprof)
 	if (cur_profile) {
 		mutex_unlock(&profile_lock);
 		return -EEXIST;
-	}
-
-	/* Sanity check the profile handler field are set */
-	if (!pprof || bitmap_empty(pprof->choices, PLATFORM_PROFILE_LAST) ||
-		!pprof->profile_set || !pprof->profile_get) {
-		mutex_unlock(&profile_lock);
-		return -EINVAL;
 	}
 
 	err = sysfs_create_group(acpi_kobj, &platform_profile_group);
