ff728424cee8fede9bb45b3f068fe245c5ebc7c1
Mario Limonciello <mario.limonciello@amd.com>
2024-10-30 23:09:44 -0500

    ACPI: platform_profile: Notify change events on register and unregister
    As multiple platform profile handlers may come and go, send a notification
    to userspace each time that a platform profile handler is registered or
    unregistered.
    
    Tested-by: Matthew Schwartz <matthew.schwartz@linux.dev>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- drivers/acpi/platform_profile.c
+++ drivers/acpi/platform_profile.c
@@ -182,6 +182,7 @@ int platform_profile_register(struct platform_profile_handler *pprof)
 	if (err)
 		return err;
 	list_add_tail(&pprof->list, &platform_profile_handler_list);
+	sysfs_notify(acpi_kobj, NULL, "platform_profile");
 
 	cur_profile = pprof;
 	return 0;
@@ -195,6 +196,8 @@ int platform_profile_remove(struct platform_profile_handler *pprof)
 	list_del(&pprof->list);
 
 	cur_profile = NULL;
+
+	sysfs_notify(acpi_kobj, NULL, "platform_profile");
 	if (!platform_profile_is_registered())
 		sysfs_remove_group(acpi_kobj, &platform_profile_group);
 
