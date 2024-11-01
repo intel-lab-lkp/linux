1a2aa27534d8eb11dbc970b44473a40cf0a504bc
Heiner Kallweit <hkallweit1@gmail.com>
2024-11-01 23:11:39 +0100

    i2c: Replace list-based mechanism for handling userspace-created clients
    Similar to the list of auto-detected clients, we can also replace the
    list of userspace-created clients with flagging such client devices.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>

--- include/linux/i2c.h
+++ include/linux/i2c.h
@@ -333,6 +333,7 @@ struct i2c_client {
 #define I2C_CLIENT_HOST_NOTIFY	0x40	/* We want to use I2C host notify */
 #define I2C_CLIENT_WAKE		0x80	/* for board_info; true iff can wake */
 #define I2C_CLIENT_AUTO		0x100	/* for board_info; auto-detected */
+#define I2C_CLIENT_USER		0x200	/* for board_info; userspace-created */
 #define I2C_CLIENT_SCCB		0x9000	/* Use Omnivision SCCB protocol */
 					/* Must match I2C_M_STOP|IGNORE_NAK */
 
