f602f008f1199bf0c055d19db1f477ad0ce7a63e
Yafang Shao <laoar.shao@gmail.com>
2024-11-01 11:17:49 +0800

    sched, psi: Don't account irq time if sched_clock_irqtime is disabled
    sched_clock_irqtime may be disabled due to the clock source. When disabled,
    irq_time_read() won't change over time, so there is nothing to account. We
    can save iterating the whole hierarchy on every tick and context switch.
    
    Signed-off-by: Yafang Shao <laoar.shao@gmail.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>

--- kernel/sched/psi.c
+++ kernel/sched/psi.c
@@ -998,7 +998,8 @@ void psi_account_irqtime(struct rq *rq, struct task_struct *curr, struct task_st
 	s64 delta;
 	u64 irq;
 
-	if (static_branch_likely(&psi_disabled))
+	if (static_branch_likely(&psi_disabled) ||
+	    !static_branch_likely(&sched_clock_irqtime))
 		return;
 
 	if (!curr->pid)
