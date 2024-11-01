06ebf536d45bf76e7398bc927e2015026c688cf0
Shinas Rasheed <srasheed@marvell.com>
2024-11-01 03:34:14 -0700

    octeon_ep: add checks to fix NULL pointer dereferences
    Add Checks to avoid NULL pointer references that might
    happen in rare and corner cases
    
    Fixes: 6a610a46bad1 ("octeon_ep: add support for ndo ops")
    Fixes: 1f2c2d0cee02 ("octeon_ep: add hardware configuration APIs")
    Fixes: 0807dc76f3bf ("octeon_ep: support Octeon CN10K devices")
    Signed-off-by: Shinas Rasheed <srasheed@marvell.com>

--- drivers/net/ethernet/marvell/octeon_ep/octep_main.c
+++ drivers/net/ethernet/marvell/octeon_ep/octep_main.c
@@ -1016,6 +1016,9 @@ static void octep_get_stats64(struct net_device *netdev,
 		struct octep_iq *iq = oct->iq[q];
 		struct octep_oq *oq = oct->oq[q];
 
+		if (!iq || !oq)
+			return;
+
 		tx_packets += iq->stats.instr_completed;
 		tx_bytes += iq->stats.bytes_sent;
 		rx_packets += oq->stats.packets;
