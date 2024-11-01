cac5754943cd77d8583354b6b344f6eb98913e77
Jie Zhang <quic_jiezh@quicinc.com>
2024-11-01 20:40:17 +0530

    drm/msm/a6xx: Add support for Adreno 612
    Add support for Adreno 612 GPU found in SM6150/QCS615 chipsets.
    A612 falls under ADRENO_6XX_GEN1 family and is a cut down version
    of A615 GPU.
    
    A612 has a new IP called Reduced Graphics Management Unit or RGMU
    which is a small state machine which helps to toggle GX GDSC
    (connected to CX rail) to implement IFPC feature. It doesn't support
    any other features of a full fledged GMU like clock control, resource
    voting to rpmh etc. So we need linux clock driver support like other
    gmu-wrapper implementations to control gpu core clock and gpu GX gdsc.
    Since there is no benefit with enabling RGMU at the moment, RGMU is
    entirely skipped in this patch.
    
    Signed-off-by: Jie Zhang <quic_jiezh@quicinc.com>
    Signed-off-by: Akhil P Oommen <quic_akhilpo@quicinc.com>

--- drivers/gpu/drm/msm/adreno/adreno_gpu.h
+++ drivers/gpu/drm/msm/adreno/adreno_gpu.h
@@ -420,6 +420,11 @@ static inline int adreno_is_a610(const struct adreno_gpu *gpu)
 	return adreno_is_revn(gpu, 610);
 }
 
+static inline int adreno_is_a612(const struct adreno_gpu *gpu)
+{
+	return gpu->info->chip_ids[0] == 0x06010200;
+}
+
 static inline int adreno_is_a618(const struct adreno_gpu *gpu)
 {
 	return adreno_is_revn(gpu, 618);
@@ -489,9 +494,9 @@ static inline int adreno_is_a610_family(const struct adreno_gpu *gpu)
 {
 	if (WARN_ON_ONCE(!gpu->info))
 		return false;
-
-	/* TODO: A612 */
-	return adreno_is_a610(gpu) || adreno_is_a702(gpu);
+	return adreno_is_a610(gpu) ||
+	       adreno_is_a702(gpu) ||
+	       adreno_is_a612(gpu);
 }
 
 /* TODO: 615/616 */
