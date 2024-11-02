157b7a8172c193a4745c47819ee2e3e89e2ef85e
Heiner Kallweit <hkallweit1@gmail.com>
2024-11-02 22:46:32 +0100

    drm/sysfs: Drop unused drm_class_device_(un)register
    Only user of these functions was TTM code which dropped sysfs interfaces
    in commit ed89fff97382 ("drm/ttm: drop sysfs directory"). So remove
    these now unused functions.
    
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@linaro.org>
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>

--- include/drm/drm_sysfs.h
+++ include/drm/drm_sysfs.h
@@ -7,9 +7,6 @@ struct device;
 struct drm_connector;
 struct drm_property;
 
-int drm_class_device_register(struct device *dev);
-void drm_class_device_unregister(struct device *dev);
-
 void drm_sysfs_hotplug_event(struct drm_device *dev);
 void drm_sysfs_connector_hotplug_event(struct drm_connector *connector);
 void drm_sysfs_connector_property_event(struct drm_connector *connector,
