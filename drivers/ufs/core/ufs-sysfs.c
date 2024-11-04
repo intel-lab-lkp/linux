690e808ced0baa7884c30fbcaa80d78bf8c5a523
Huan Tang <tanghuan@vivo.com>
2024-11-04 21:46:12 +0800

    ufs: core: Add WB buffer resize support
    Support WB buffer resize function through sysfs, the host can obtain
    resize hint and resize status and enable the resize operation. To
    achieve this goals, three sysfs nodes have been added:
    	1. wb_resize_enable
    	2. wb_resize_hint
    	3. wb_resize_status
    
    The detailed definition of the three nodes can be found in the sysfs
    documentation.
    
    Changelog
    ===
    v4 -> v5:
    	1.For the three new attributes: use words in sysfs instead of numbers
    
    v3 - > v4:
    	1.modify three attributes name and description in document
    	2.add enum wb_resize_en in ufs.h
    	3.modify function name and parameters in ufs-sysfs.c, ufshcd.h, ufshcd.c
    
    v2 - > v3:
    	remove needless code:
    	drivers/ufs/core/ufs-sysfs.c:441:2:
    	index =	ufshcd_wb_get_query_index(hba)
    
    v1 - > v2:
    	remove unused variable "u8 index",
    	drivers/ufs/core/ufs-sysfs.c:419:12: warning: variable 'index'
    	set but not used.
    
    v1
    	https://lore.kernel.org/all/20241025085924.4855-1-tanghuan@vivo.com/
    v2
    	https://lore.kernel.org/all/20241026004423.135-1-tanghuan@vivo.com/
    v3
    	https://lore.kernel.org/all/20241028135205.188-1-tanghuan@vivo.com/
    v4
    	https://lore.kernel.org/all/20241101093318.825-1-tanghuan@vivo.com/
    
    Signed-off-by: Huan Tang <tanghuan@vivo.com>
    Signed-off-by: Lu Hongfei <luhongfei@vivo.com>

--- drivers/ufs/core/ufs-sysfs.c
+++ drivers/ufs/core/ufs-sysfs.c
@@ -411,6 +411,43 @@ static ssize_t wb_flush_threshold_store(struct device *dev,
 	return count;
 }
 
+static const char * const resize_en_mode[] = {
+	[WB_RESIZE_EN_IDLE]		= "idle",
+	[WB_RESIZE_EN_DECREASE]		= "decrease",
+	[WB_RESIZE_EN_INCREASE]		= "increase",
+};
+
+static ssize_t wb_resize_enable_store(struct device *dev,
+				struct device_attribute *attr,
+				const char *buf, size_t count)
+{
+	struct ufs_hba *hba = dev_get_drvdata(dev);
+	int mode;
+	ssize_t res;
+
+	if (!ufshcd_is_wb_allowed(hba) || !hba->dev_info.wb_enabled ||
+		!hba->dev_info.b_presrv_uspc_en)
+		return -EOPNOTSUPP;
+
+	mode = sysfs_match_string(resize_en_mode, buf);
+	if (mode <= 0)
+		return -EINVAL;
+
+	down(&hba->host_sem);
+	if (!ufshcd_is_user_access_allowed(hba)) {
+		res = -EBUSY;
+		goto out;
+	}
+
+	ufshcd_rpm_get_sync(hba);
+	res = ufshcd_wb_set_resize_en(hba, (u32)mode);
+	ufshcd_rpm_put_sync(hba);
+
+out:
+	up(&hba->host_sem);
+	return res < 0 ? res : count;
+}
+
 /**
  * pm_qos_enable_show - sysfs handler to show pm qos enable value
  * @dev: device associated with the UFS controller
@@ -468,6 +505,7 @@ static DEVICE_ATTR_RW(auto_hibern8);
 static DEVICE_ATTR_RW(wb_on);
 static DEVICE_ATTR_RW(enable_wb_buf_flush);
 static DEVICE_ATTR_RW(wb_flush_threshold);
+static DEVICE_ATTR_WO(wb_resize_enable);
 static DEVICE_ATTR_RW(rtc_update_ms);
 static DEVICE_ATTR_RW(pm_qos_enable);
 
@@ -482,6 +520,7 @@ static struct attribute *ufs_sysfs_ufshcd_attrs[] = {
 	&dev_attr_wb_on.attr,
 	&dev_attr_enable_wb_buf_flush.attr,
 	&dev_attr_wb_flush_threshold.attr,
+	&dev_attr_wb_resize_enable.attr,
 	&dev_attr_rtc_update_ms.attr,
 	&dev_attr_pm_qos_enable.attr,
 	NULL
@@ -1476,6 +1515,96 @@ static inline bool ufshcd_is_wb_attrs(enum attr_idn idn)
 		idn <= QUERY_ATTR_IDN_CURR_WB_BUFF_SIZE;
 }
 
+static int wb_read_resize_attrs(struct ufs_hba *hba,
+			enum attr_idn idn, u32 *attr_val)
+{
+	u8 index = 0;
+	int ret;
+
+	if (!ufshcd_is_wb_allowed(hba) || !hba->dev_info.wb_enabled ||
+		!hba->dev_info.b_presrv_uspc_en)
+		return -EOPNOTSUPP;
+
+	down(&hba->host_sem);
+	if (!ufshcd_is_user_access_allowed(hba)) {
+		ret = -EBUSY;
+		goto out;
+	}
+
+	index = ufshcd_wb_get_query_index(hba);
+	ufshcd_rpm_get_sync(hba);
+	ret = ufshcd_query_attr(hba, UPIU_QUERY_OPCODE_READ_ATTR,
+			idn, index, 0, attr_val);
+	ufshcd_rpm_put_sync(hba);
+	if (ret)
+		ret = -EINVAL;
+
+out:
+	up(&hba->host_sem);
+	return ret;
+}
+
+static const char * const resize_hint_info[] = {
+	[WB_RESIZE_HINT_KEEP]		= "keep",
+	[WB_RESIZE_HINT_DECREASE]	= "decrease",
+	[WB_RESIZE_HINT_INCREASE]	= "increase",
+};
+
+static ssize_t wb_resize_hint_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	struct ufs_hba *hba = dev_get_drvdata(dev);
+	int ret;
+	u32 value;
+
+	ret = wb_read_resize_attrs(hba,
+			QUERY_ATTR_IDN_WB_BUF_RESIZE_HINT, &value);
+	if (ret)
+		goto out;
+
+	if (value >= WB_RESIZE_HINT_KEEP &&
+		value <= WB_RESIZE_HINT_INCREASE)
+		sysfs_emit(buf, "%s\n", resize_hint_info[value]);
+	else
+		ret = -EINVAL;
+
+out:
+	return ret;
+}
+
+static DEVICE_ATTR_RO(wb_resize_hint);
+
+static const char * const resize_status[] = {
+	[WB_RESIZE_STATUS_IDLE]		        = "idle",
+	[WB_RESIZE_STATUS_IN_PROGRESS]	    = "in-progress",
+	[WB_RESIZE_STATUS_COMPLETE_SUCCESS]	= "complete-success",
+	[WB_RESIZE_STATUS_GENERAL_FAIL]     = "general-fail",
+};
+
+static ssize_t wb_resize_status_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	struct ufs_hba *hba = dev_get_drvdata(dev);
+	int ret;
+	u32 value;
+
+	ret = wb_read_resize_attrs(hba,
+			QUERY_ATTR_IDN_WB_BUF_RESIZE_STATUS, &value);
+	if (ret)
+		goto out;
+
+	if (value >= WB_RESIZE_STATUS_IDLE &&
+		value <= WB_RESIZE_STATUS_GENERAL_FAIL)
+		sysfs_emit(buf, "%s\n", resize_status[value]);
+	else
+		ret = -EINVAL;
+
+out:
+	return ret;
+}
+
+static DEVICE_ATTR_RO(wb_resize_status);
+
 #define UFS_ATTRIBUTE(_name, _uname)					\
 static ssize_t _name##_show(struct device *dev,				\
 	struct device_attribute *attr, char *buf)			\
@@ -1549,6 +1678,8 @@ static struct attribute *ufs_sysfs_attributes[] = {
 	&dev_attr_wb_avail_buf.attr,
 	&dev_attr_wb_life_time_est.attr,
 	&dev_attr_wb_cur_buf.attr,
+	&dev_attr_wb_resize_hint.attr,
+	&dev_attr_wb_resize_status.attr,
 	NULL,
 };
 
