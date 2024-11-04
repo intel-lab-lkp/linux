6a641266cefd14165426f3bfcddb7a46ee0f89ff
Shuai Xue <xueshuai@linux.alibaba.com>
2024-11-04 09:54:29 +0800

    mm: memory-failure: move return value documentation to function declaration
    Part of return value comments for memory_failure() were originally
    documented at the call site. Move those comments to the function
    declaration to improve code readability and to provide developers with
    immediate access to function usage and return information.
    
    Signed-off-by: Shuai Xue <xueshuai@linux.alibaba.com>
    Reviewed-by: Jarkko Sakkinen <jarkko@kernel.org>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -2209,9 +2209,12 @@ static void kill_procs_now(struct page *p, unsigned long pfn, int flags,
  * Must run in process context (e.g. a work queue) with interrupts
  * enabled and no spinlocks held.
  *
- * Return: 0 for successfully handled the memory error,
- *         -EOPNOTSUPP for hwpoison_filter() filtered the error event,
- *         < 0(except -EOPNOTSUPP) on failure.
+ * Return:
+ *   0             - success,
+ *   -EOPNOTSUPP   - hwpoison_filter() filtered the error event,
+ *   -EHWPOISON    - the page was already poisoned, potentially
+ *                   kill process,
+ *   other negative values - failure.
  */
 int memory_failure(unsigned long pfn, int flags)
 {
