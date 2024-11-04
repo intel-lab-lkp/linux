2b8b9f0b20268beb7bd9ee45c020ce27faaea325
Heiner Kallweit <hkallweit1@gmail.com>
2024-11-04 23:07:20 +0100

    r8169: copy vendor driver 2.5G/5G EEE advertisement constraints
    Vendor driver r8125 doesn't advertise 2.5G EEE on RTL8125A, and r8126
    doesn't advertise 5G EEE. Likely there are compatibility issues,
    therefore do the same in r8169.
    With this change we don't have to disable 2.5G EEE advertisement in
    rtl8125a_config_eee_phy() any longer.
    Note: We don't remove the potentially problematic modes from the
    supported modes, so users can re-enable advertisement of these modes
    if they work fine in their setup.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>

--- drivers/net/ethernet/realtek/r8169_main.c
+++ drivers/net/ethernet/realtek/r8169_main.c
@@ -5316,6 +5316,13 @@ static int r8169_mdio_register(struct rtl8169_private *tp)
 		phy_support_eee(tp->phydev);
 	phy_support_asym_pause(tp->phydev);
 
+	/* mimic behavior of r8125/r8126 vendor drivers */
+	if (tp->mac_version == RTL_GIGA_MAC_VER_61)
+		linkmode_clear_bit(ETHTOOL_LINK_MODE_2500baseT_Full_BIT,
+				   tp->phydev->advertising_eee);
+	linkmode_clear_bit(ETHTOOL_LINK_MODE_5000baseT_Full_BIT,
+			   tp->phydev->advertising_eee);
+
 	/* PHY will be woken up in rtl_open() */
 	phy_suspend(tp->phydev);
 
