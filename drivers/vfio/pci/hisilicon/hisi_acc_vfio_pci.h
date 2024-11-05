2804a9febf34e29dc1403f3a5c928b5be4216e4a
Longfang Liu <liulongfang@huawei.com>
2024-11-05 11:52:53 +0800

    hisi_acc_vfio_pci: register debugfs for hisilicon migration driver
    On the debugfs framework of VFIO, if the CONFIG_VFIO_DEBUGFS macro is
    enabled, the debug function is registered for the live migration driver
    of the HiSilicon accelerator device.
    
    After registering the HiSilicon accelerator device on the debugfs
    framework of live migration of vfio, a directory file "hisi_acc"
    of debugfs is created, and then three debug function files are
    created in this directory:
    
       vfio
        |
        +---<dev_name1>
        |    +---migration
        |        +--state
        |        +--hisi_acc
        |            +--dev_data
        |            +--migf_data
        |            +--cmd_state
        |
        +---<dev_name2>
             +---migration
                 +--state
                 +--hisi_acc
                     +--dev_data
                     +--migf_data
                     +--cmd_state
    
    dev_data file: read device data that needs to be migrated from the
    current device in real time
    migf_data file: read the migration data of the last live migration
    from the current driver.
    cmd_state: used to get the cmd channel state for the device.
    
    +----------------+        +--------------+       +---------------+
    | migration dev  |        |   src  dev   |       |   dst  dev    |
    +-------+--------+        +------+-------+       +-------+-------+
            |                        |                       |
            |                 +------v-------+       +-------v-------+
            |                 |  saving_migf |       | resuming_migf |
      read  |                 |     file     |       |     file      |
            |                 +------+-------+       +-------+-------+
            |                        |          copy         |
            |                        +------------+----------+
            |                                     |
    +-------v--------+                    +-------v--------+
    |   data buffer  |                    |   debug_migf   |
    +-------+--------+                    +-------+--------+
            |                                     |
       cat  |                                 cat |
    +-------v--------+                    +-------v--------+
    |   dev_data     |                    |   migf_data    |
    +----------------+                    +----------------+
    
    When accessing debugfs, user can obtain the most recent status data
    of the device through the "dev_data" file. It can read recent
    complete status data of the device. If the current device is being
    migrated, it will wait for it to complete.
    The data for the last completed migration function will be stored
    in debug_migf. Users can read it via "migf_data".
    
    Signed-off-by: Longfang Liu <liulongfang@huawei.com>
    Reviewed-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>

--- drivers/vfio/pci/hisilicon/hisi_acc_vfio_pci.h
+++ drivers/vfio/pci/hisilicon/hisi_acc_vfio_pci.h
@@ -32,6 +32,7 @@
 #define QM_SQC_VFT_BASE_MASK_V2		GENMASK(15, 0)
 #define QM_SQC_VFT_NUM_SHIFT_V2		45
 #define QM_SQC_VFT_NUM_MASK_V2		GENMASK(9, 0)
+#define QM_MB_CMD_NOT_READY	0xffffffff
 
 /* RW regs */
 #define QM_REGS_MAX_LEN		7
@@ -99,6 +100,12 @@ struct hisi_acc_vf_migration_file {
 struct hisi_acc_vf_core_device {
 	struct vfio_pci_core_device core_device;
 	u8 match_done;
+	/*
+	 * io_base is only valid when dev_opened is true,
+	 * which is protected by open_mutex.
+	 */
+	bool dev_opened;
+	struct mutex open_mutex;
 
 	/* For migration state */
 	struct mutex state_mutex;
@@ -107,9 +114,17 @@ struct hisi_acc_vf_core_device {
 	struct pci_dev *vf_dev;
 	struct hisi_qm *pf_qm;
 	struct hisi_qm vf_qm;
+	/*
+	 * Record whether a driver is added to the acc device in Guest OS.
+	 * The value of QM_VF_STATE is set by the acc device driver.
+	 * The migration driver queries through the QM_VF_STATE register.
+	 */
 	u32 vf_qm_state;
 	int vf_id;
 	struct hisi_acc_vf_migration_file *resuming_migf;
 	struct hisi_acc_vf_migration_file *saving_migf;
+
+	/* An extra buffer for reporting migration data via debugfs */
+	struct hisi_acc_vf_migration_file *debug_migf;
 };
 #endif /* HISI_ACC_VFIO_PCI_H */
