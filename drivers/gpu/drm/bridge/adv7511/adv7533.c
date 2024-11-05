45d92d75872ff860bda35da73e1c46715d78f763
Biju Das <biju.das.jz@bp.renesas.com>
2024-11-05 11:02:28 +0000

    drm: adv7511: Fix out-of-bounds array in clock_div_by_lanes
    Fix out-of-bounds array in adv7511_dsi_config_timing_gen() for
    clock_div_by_lanes[], when dsi lanes = 1.
    
    Fixes: 78fa479d703c ("drm/bridge: adv7533: Use internal timing generator")
    Reported-by: Hien Huynh <hien.huynh.px@renesas.com>
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>

--- drivers/gpu/drm/bridge/adv7511/adv7533.c
+++ drivers/gpu/drm/bridge/adv7511/adv7533.c
@@ -185,6 +185,9 @@ int adv7533_parse_dt(struct device_node *np, struct adv7511 *adv)
 	adv->use_timing_gen = !of_property_read_bool(np,
 						"adi,disable-timing-generator");
 
+	if (adv->use_timing_gen && num_lanes == 1)
+		return -EINVAL;
+
 	/* TODO: Check if these need to be parsed by DT or not */
 	adv->rgb = true;
 	adv->embedded_sync = false;
