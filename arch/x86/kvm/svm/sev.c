bb1df5fabd72b76de42133938b73b95919aaad62
Dionna Glaze <dionnaglaze@google.com>
2024-11-05 01:05:53 +0000

    KVM: SVM: Delay legacy platform initialization on SNP
    When no SEV or SEV-ES guests are active, then the firmware can be
    updated while (SEV-SNP) VM guests are active.
    
    CC: Sean Christopherson <seanjc@google.com>
    CC: Paolo Bonzini <pbonzini@redhat.com>
    CC: Thomas Gleixner <tglx@linutronix.de>
    CC: Ingo Molnar <mingo@redhat.com>
    CC: Borislav Petkov <bp@alien8.de>
    CC: Dave Hansen <dave.hansen@linux.intel.com>
    CC: Ashish Kalra <ashish.kalra@amd.com>
    CC: Tom Lendacky <thomas.lendacky@amd.com>
    CC: John Allen <john.allen@amd.com>
    CC: Herbert Xu <herbert@gondor.apana.org.au>
    CC: "David S. Miller" <davem@davemloft.net>
    CC: Michael Roth <michael.roth@amd.com>
    CC: Luis Chamberlain <mcgrof@kernel.org>
    CC: Russ Weight <russ.weight@linux.dev>
    CC: Danilo Krummrich <dakr@redhat.com>
    CC: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    CC: "Rafael J. Wysocki" <rafael@kernel.org>
    CC: Tianfei zhang <tianfei.zhang@intel.com>
    CC: Alexey Kardashevskiy <aik@amd.com>
    
    Co-developed-by: Ashish Kalra <ashish.kalra@amd.com>
    Signed-off-by: Ashish Kalra <ashish.kalra@amd.com>
    Reviewed-by: Ashish Kalra <ashish.kalra@amd.com>
    Signed-off-by: Dionna Glaze <dionnaglaze@google.com>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -444,7 +444,7 @@ static int __sev_guest_init(struct kvm *kvm, struct kvm_sev_cmd *argp,
 	if (ret)
 		goto e_no_asid;
 
-	init_args.probe = false;
+	init_args.probe = vm_type != KVM_X86_SEV_VM && vm_type != KVM_X86_SEV_ES_VM;
 	ret = sev_platform_init(&init_args);
 	if (ret)
 		goto e_free;
