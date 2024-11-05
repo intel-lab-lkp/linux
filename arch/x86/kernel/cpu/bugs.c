74f3ca558019063047cd67e71b0e6ccf75b66399
David Kaplan <david.kaplan@amd.com>
2024-11-05 15:54:54 -0600

    x86/bugs: Add attack vector controls for srso
    Use attack vector controls to determine if srso mitigation is required.
    
    Signed-off-by: David Kaplan <david.kaplan@amd.com>

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -2868,8 +2868,14 @@ static void __init srso_select_mitigation(void)
 		return;
 
 	/* Default mitigation */
-	if (srso_mitigation == SRSO_MITIGATION_AUTO)
-		srso_mitigation = SRSO_MITIGATION_SAFE_RET;
+	if (srso_mitigation == SRSO_MITIGATION_AUTO) {
+		if (should_mitigate_vuln(X86_BUG_SRSO))
+			srso_mitigation = SRSO_MITIGATION_SAFE_RET;
+		else {
+			srso_mitigation = SRSO_MITIGATION_NONE;
+			return;
+		}
+	}
 
 	if (has_microcode) {
 		/*
