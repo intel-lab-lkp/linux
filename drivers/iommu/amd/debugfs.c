2d86a1223702c8b9555c71cfe75dfd37d7792982
Dheeraj Kumar Srivastava <dheerajkumar.srivastava@amd.com>
2024-11-06 13:16:33 +0530

    iommu/amd: Add debugfs support to dump IOMMU MMIO registers
    Analyzing IOMMU MMIO registers gives a view of what IOMMU is
    configured with on the system and is helpful to debug issues
    with IOMMU.
    
    eg.
    1. To get mmio registers value for iommu<x>
       # echo "0x18" > /sys/kernel/debug/iommu/amd/iommu00/mmio
       # cat /sys/kernel/debug/iommu/amd/iommu00/mmio_dump
    
    Signed-off-by: Dheeraj Kumar Srivastava <dheerajkumar.srivastava@amd.com>

--- drivers/iommu/amd/debugfs.c
+++ drivers/iommu/amd/debugfs.c
@@ -15,6 +15,59 @@
 static struct dentry *amd_iommu_debugfs;
 
 #define	MAX_NAME_LEN	20
+#define	OFS_IN_SZ	8
+
+static int mmio_offset = -1;
+
+static ssize_t iommu_mmio_write(struct file *filp, const char __user *ubuf,
+				size_t cnt, loff_t *ppos)
+{
+	struct seq_file *m = filp->private_data;
+	struct amd_iommu *iommu = m->private;
+	int ret;
+
+	if (cnt > OFS_IN_SZ)
+		return -EINVAL;
+
+	ret = kstrtou32_from_user(ubuf, cnt, 0, &mmio_offset);
+	if (ret)
+		return ret;
+
+	if (mmio_offset > iommu->mmio_phys_end - 4) {
+		mmio_offset = -1;
+		return  -EINVAL;
+	}
+
+	return cnt;
+}
+
+static int iommu_mmio_show(struct seq_file *m, void *unused)
+{
+	if (mmio_offset >= 0)
+		seq_printf(m, "0x%x\n", mmio_offset);
+	else
+		seq_puts(m, "No or invalid input provided\n");
+
+	return 0;
+}
+DEFINE_SHOW_STORE_ATTRIBUTE(iommu_mmio);
+
+static int iommu_mmio_dump_show(struct seq_file *m, void *unused)
+{
+	struct amd_iommu *iommu = m->private;
+	u32 value;
+
+	if (mmio_offset < 0) {
+		seq_puts(m, "Please provide mmio register's offset\n");
+		return 0;
+	}
+
+	value = readl(iommu->mmio_base + mmio_offset);
+	seq_printf(m, "0x%08x\n", value);
+
+	return 0;
+}
+DEFINE_SHOW_ATTRIBUTE(iommu_mmio_dump);
 
 void amd_iommu_debugfs_setup(void)
 {
@@ -26,5 +79,10 @@ void amd_iommu_debugfs_setup(void)
 	for_each_iommu(iommu) {
 		snprintf(name, MAX_NAME_LEN, "iommu%02d", iommu->index);
 		iommu->debugfs = debugfs_create_dir(name, amd_iommu_debugfs);
+
+		debugfs_create_file("mmio", 0644, iommu->debugfs, iommu,
+				    &iommu_mmio_fops);
+		debugfs_create_file("mmio_dump", 0444, iommu->debugfs, iommu,
+				    &iommu_mmio_dump_fops);
 	}
 }
