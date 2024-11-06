5763d0540672a846942f93fde41d44bc9a131639
Suraj Kandpal <suraj.kandpal@intel.com>
2024-11-06 14:30:59 +0530

    drm/i915/psr: Implement WA to help reach PC10
    To reach PC10 when PKG_C_LATENCY is configure we must do the following
    things
    1) Enter PSR1 only when delayed_vblank < 6 lines and DC5 can be entered
    2) Allow PSR2 deep sleep when DC5 can be entered
    3) DC5 can be entered when all transocoder have either PSR1, PSR2 or
    eDP 1.5 PR ALPM enabled and VBI is disabled and flips and pushes are
    not happening.
    
    --v2
    -Add debug prints
    
    --v3
    -use crtc as variable name for intel_crtc [Jani]
    -use encoder as variable name for intel_encoder [Jani]
    -No changes in intel_dp in compute_config_late [Jani]
    
    --v4
    -Remove "check" from naming [Jani]
    -Remove intel_encoder variable which is not necessary in
    compute_config_late
    
    --v5
    -Make readable function names [Jani]
    -No need to duplicate all variables into intel_psr [Jani]
    -Optimize dpkgc_configured function [Jani]
    -Use crtc_state only to decide if wa is needed for PSR1 [Jani]
    
    WA: 22019444797
    Signed-off-by: Suraj Kandpal <suraj.kandpal@intel.com>

--- drivers/gpu/drm/i915/display/intel_ddi.c
+++ drivers/gpu/drm/i915/display/intel_ddi.c
@@ -4367,6 +4367,9 @@ static int intel_ddi_compute_config_late(struct intel_encoder *encoder,
 			port_sync_transcoders & ~BIT(crtc_state->cpu_transcoder);
 	}
 
+	if (intel_encoder_is_dp(encoder))
+		intel_psr_compute_config_late(crtc_state);
+
 	return 0;
 }
 
