7940d4237db3cea617f128ba92b082705598f266
Bernd Schubert <bschubert@ddn.com>
2024-11-07 18:03:53 +0100

    fuse: {uring} Add uring sqe commit and fetch support
    This adds support for fuse request completion through ring SQEs
    (FUSE_URING_REQ_COMMIT_AND_FETCH handling). After committing
    the ring entry it becomes available for new fuse requests.
    Handling of requests through the ring (SQE/CQE handling)
    is complete now.
    
    Fuse request data are copied through the mmaped ring buffer,
    there is no support for any zero copy yet.
    
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>

--- fs/fuse/dev_uring_i.h
+++ fs/fuse/dev_uring_i.h
@@ -19,6 +19,9 @@ enum fuse_ring_req_state {
 	/* The ring request waits for a new fuse request */
 	FRRS_WAIT,
 
+	/* The ring req got assigned a fuse req */
+	FRRS_FUSE_REQ,
+
 	/* request is in or on the way to user space */
 	FRRS_USERSPACE,
 };
@@ -72,6 +75,14 @@ struct fuse_ring_queue {
 	 * to be send to userspace
 	 */
 	struct list_head ent_intermediate_queue;
+
+	/* entries in userspace */
+	struct list_head ent_in_userspace;
+
+	/* fuse requests waiting for an entry slot */
+	struct list_head fuse_req_queue;
+
+	struct fuse_pqueue fpq;
 };
 
 /**
