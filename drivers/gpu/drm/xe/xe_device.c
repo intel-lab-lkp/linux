594593aad13de388594ca24cc2ec2914ec590dff
Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
2024-11-07 11:01:35 +0100

    drm/xe: Defer irq init until after xe_display_init_noaccel
    Technically, I believe this means that xe_display_init_noirq and
    xe_display_init_noaccel can be merged together now.
    
    Signed-off-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20241105121857.17389-4-maarten.lankhorst@linux.intel.com
    Signed-off-by: Maarten Lankhorst,,, <dev@lankhorst.se>

--- drivers/gpu/drm/xe/xe_device.c
+++ drivers/gpu/drm/xe/xe_device.c
@@ -41,7 +41,6 @@
 #include "xe_hw_engine_group.h"
 #include "xe_hwmon.h"
 #include "xe_irq.h"
-#include "xe_memirq.h"
 #include "xe_mmio.h"
 #include "xe_module.h"
 #include "xe_observation.h"
@@ -673,9 +672,6 @@ int xe_device_probe(struct xe_device *xe)
 		err = xe_ggtt_init_early(tile->mem.ggtt);
 		if (err)
 			return err;
-		err = xe_memirq_init(&tile->memirq);
-		if (err)
-			return err;
 	}
 
 	for_each_gt(gt, xe, id) {
@@ -694,10 +690,6 @@ int xe_device_probe(struct xe_device *xe)
 	err = xe_display_init_noirq(xe);
 	if (err)
 		return err;
-
-	err = xe_irq_install(xe);
-	if (err)
-		goto err;
 
 	err = probe_has_flat_ccs(xe);
 	if (err)
@@ -731,6 +723,10 @@ int xe_device_probe(struct xe_device *xe)
 		if (err)
 			goto err;
 	}
+
+	err = xe_irq_install(xe);
+	if (err)
+		goto err;
 
 	for_each_gt(gt, xe, id) {
 		last_gt = id;
