43e52a63401b23e9c87c5bf1da3cbc2094004cf5
Chun-Tse Shao <ctshao@google.com>
2024-11-07 21:59:20 +0000

    perf/arm-smmuv3: Fix lockdep assert in ->event_init()
    Same as
    https://lore.kernel.org/all/20240514180050.182454-1-namhyung@kernel.org/,
    we should skip `for_each_sibling_event()` for group leader since it
    doesn't have the ctx yet.
    
    Fixes: f3c0eba28704 ("perf: Add a few assertions")
    Reported-by: Greg Thelen <gthelen@google.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Robin Murphy <robin.murphy@arm.com>
    Cc: Tuan Phan <tuanphan@os.amperecomputing.com>
    Signed-off-by: Chun-Tse Shao <ctshao@google.com>

--- drivers/perf/arm_smmuv3_pmu.c
+++ drivers/perf/arm_smmuv3_pmu.c
@@ -431,6 +431,11 @@ static int smmu_pmu_event_init(struct perf_event *event)
 			return -EINVAL;
 	}
 
+	hwc->idx = -1;
+
+	if (event->group_leader == event)
+		return 0;
+
 	for_each_sibling_event(sibling, event->group_leader) {
 		if (is_software_event(sibling))
 			continue;
@@ -441,8 +446,6 @@ static int smmu_pmu_event_init(struct perf_event *event)
 		if (++group_num_events > smmu_pmu->num_counters)
 			return -EINVAL;
 	}
-
-	hwc->idx = -1;
 
 	/*
 	 * Ensure all events are on the same cpu so all events are in the
