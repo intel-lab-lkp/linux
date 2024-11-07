521d5e93f9ed51bb37c18fd8a61fdd01295a420e
HONG Yifan <elsk@google.com>
2024-11-07 00:58:22 +0000

    kheaders: prevent `find` from seeing perl temp files
    Symptom:
    
    The command
    
        find ... | xargs ... perl -i
    
    occasionally triggers error messages like the following, with the build
    still succeeding:
    
        Can't open <redacted>/kernel/.tmp_cpio_dir/include/dt-bindings/clock/XXNX4nW9: No such file or directory.
    
    Analysis:
    
    With strace, the root cause has been identified to be `perl -i` creating
    temporary files inside $cpio_dir, which causes `find` to see the
    temporary files and emit the names. `find` is likely implemented with
    readdir. POSIX `readdir` says:
    
        If a file is removed from or added to the directory after the most
        recent call to opendir() or rewinddir(), whether a subsequent call
        to readdir() returns an entry for that file is unspecified.
    
    So if the libc that `find` links against choose to return that entry
    in readdir(), a possible sequence of events is the following:
    
    1. find emits foo.h
    2. xargs executes `perl -i foo.h`
    3. perl (pid=100) creates temporary file `XXXXXXXX`
    4. find sees file `XXXXXXXX` and emit it
    5. PID 100 exits, cleaning up the temporary file `XXXXXXXX`
    6. xargs executes `perl -i XXXXXXXX`
    7. perl (pid=200) tries to read the file, but it doesn't exist any more.
    
    ... triggering the error message.
    
    One can reproduce the bug with the following command (assuming PWD
    contains the list of headers in kheaders.tar.xz)
    
        for i in $(seq 100); do
            find -type f -print0 |
                xargs -0 -P8 -n1 perl -pi -e 'BEGIN {undef $/;}; s/\/\*((?!SPDX).)*?\*\///smg;';
        done
    
    With a `find` linking against musl libc, the error message is emitted
    6/100 times.
    
    The fix:
    
    This change modifies the command so `find` only emits .h names, thereby
    skipping the temporary files. Another possible fix would be to store
    the results of `find` before feeding them into xargs; I didn't take this
    approach because the current approach appears marginally more optimized
    and involves a smaller change.
    
    Signed-off-by: HONG Yifan <elsk@google.com>

--- kernel/gen_kheaders.sh
+++ kernel/gen_kheaders.sh
@@ -84,7 +84,7 @@ for f in $dir_list;
 done | cpio --quiet -pdu $cpio_dir >/dev/null 2>&1
 
 # Remove comments except SDPX lines
-find $cpio_dir -type f -print0 |
+find $cpio_dir -type f -name '*.h' -print0 |
 	xargs -0 -P8 -n1 perl -pi -e 'BEGIN {undef $/;}; s/\/\*((?!SPDX).)*?\*\///smg;'
 
 # Create archive and try to normalize metadata for reproducibility.
