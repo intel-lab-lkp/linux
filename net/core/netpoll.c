90b242125b9503f1aae67f8f57ecab3440063f15
Breno Leitao <leitao@debian.org>
2024-11-07 07:57:07 -0800

    net: netpoll: flush skb pool during cleanup
    The netpoll subsystem maintains a pool of 32 pre-allocated SKBs per
    instance, but these SKBs are not freed when the netpoll user is brought
    down. This leads to memory waste as these buffers remain allocated but
    unused.
    
    Add skb_pool_flush() to properly clean up these SKBs when netconsole is
    terminated, improving memory efficiency.
    
    Signed-off-by: Breno Leitao <leitao@debian.org>

--- net/core/netpoll.c
+++ net/core/netpoll.c
@@ -531,6 +531,22 @@ static int netpoll_parse_ip_addr(const char *str, union inet_addr *addr)
 	return -1;
 }
 
+static void skb_pool_flush(struct netpoll *np)
+{
+	struct sk_buff_head *skb_pool;
+	struct sk_buff *skb;
+	unsigned long flags;
+
+	skb_pool = &np->skb_pool;
+
+	spin_lock_irqsave(&skb_pool->lock, flags);
+	while (skb_pool->qlen > 0) {
+		skb = __skb_dequeue(skb_pool);
+		kfree_skb(skb);
+	}
+	spin_unlock_irqrestore(&skb_pool->lock, flags);
+}
+
 int netpoll_parse_options(struct netpoll *np, char *opt)
 {
 	char *cur=opt, *delim;
@@ -779,10 +795,12 @@ put_noaddr:
 
 	err = __netpoll_setup(np, ndev);
 	if (err)
-		goto put;
+		goto flush;
 	rtnl_unlock();
 	return 0;
 
+flush:
+	skb_pool_flush(np);
 put:
 	DEBUG_NET_WARN_ON_ONCE(np->dev);
 	if (ip_overwritten)
@@ -830,6 +848,8 @@ void __netpoll_cleanup(struct netpoll *np)
 		call_rcu(&npinfo->rcu, rcu_cleanup_netpoll_info);
 	} else
 		RCU_INIT_POINTER(np->dev->npinfo, NULL);
+
+	skb_pool_flush(np);
 }
 EXPORT_SYMBOL_GPL(__netpoll_cleanup);
 
