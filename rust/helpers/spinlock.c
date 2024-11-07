ebdd66d444c43c3ac9dd611386cb9a7b56f590a8
Alistair Francis <alistair23@gmail.com>
2024-11-07 12:08:29 +1000

    rust: helpers: Remove some spinlock helpers
    Now that we support wrap-static-fns we no longer need the custom helpers.
    
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- rust/helpers/spinlock.c
+++ rust/helpers/spinlock.c
@@ -12,13 +12,3 @@ void rust_helper___spin_lock_init(spinlock_t *lock, const char *name,
 	spin_lock_init(lock);
 #endif
 }
-
-void rust_helper_spin_lock(spinlock_t *lock)
-{
-	spin_lock(lock);
-}
-
-void rust_helper_spin_unlock(spinlock_t *lock)
-{
-	spin_unlock(lock);
-}
