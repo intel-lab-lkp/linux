70139555c7b2809159d3b007763fe4e99b592e44
Alistair Francis <alistair23@gmail.com>
2024-11-08 13:10:08 +1000

    rust: helpers: Remove some refcount helpers
    Now that we support wrap-static-fns we no longer need the custom helpers.
    
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- rust/helpers/refcount.c
+++ rust/helpers/refcount.c
@@ -6,13 +6,3 @@ refcount_t rust_helper_REFCOUNT_INIT(int n)
 {
 	return (refcount_t)REFCOUNT_INIT(n);
 }
-
-void rust_helper_refcount_inc(refcount_t *r)
-{
-	refcount_inc(r);
-}
-
-bool rust_helper_refcount_dec_and_test(refcount_t *r)
-{
-	return refcount_dec_and_test(r);
-}
