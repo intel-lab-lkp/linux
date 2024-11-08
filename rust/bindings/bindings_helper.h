51ab67539855f8e44cf3238f1383badcfa82df2b
Alistair Francis <alistair23@gmail.com>
2024-11-08 13:10:06 +1000

    rust: helpers: Remove some page helpers
    Now that we support wrap-static-fns we no longer need the custom helpers.
    
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- rust/bindings/bindings_helper.h
+++ rust/bindings/bindings_helper.h
@@ -13,6 +13,7 @@
 #include <linux/errname.h>
 #include <linux/ethtool.h>
 #include <linux/firmware.h>
+#include <linux/highmem.h>
 #include <linux/jiffies.h>
 #include <linux/mdio.h>
 #include <linux/phy.h>
