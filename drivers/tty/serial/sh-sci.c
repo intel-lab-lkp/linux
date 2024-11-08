86fd15e650026e74398ba27e584a9724d2cbb074
Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
2024-11-08 12:05:07 +0200

    serial: sh-sci: Check if TX data was written to device in .tx_empty()
    On the Renesas RZ/G3S, when doing suspend to RAM, the uart_suspend_port()
    is called. The uart_suspend_port() calls 3 times the
    struct uart_port::ops::tx_empty() before shutting down the port.
    
    According to the documentation, the struct uart_port::ops::tx_empty()
    API tests whether the transmitter FIFO and shifter for the port is
    empty.
    
    The Renesas RZ/G3S SCIFA IP reports the number of data units stored in the
    transmit FIFO through the FDR (FIFO Data Count Register). The data units
    in the FIFOs are written in the shift register and transmitted from there.
    The TEND bit in the Serial Status Register reports if the data was
    transmitted from the shift register.
    
    In the previous code, in the tx_empty() API implemented by the sh-sci
    driver, it is considered that the TX is empty if the hardware reports the
    TEND bit set and the number of data units in the FIFO is zero.
    
    According to the HW manual, the TEND bit has the following meaning:
    
    0: Transmission is in the waiting state or in progress.
    1: Transmission is completed.
    
    It has been noticed that when opening the serial device w/o using it and
    then switch to a power saving mode, the tx_empty() call in the
    uart_port_suspend() function fails, leading to the "Unable to drain
    transmitter" message being printed on the console. This is because the
    TEND=0 if nothing has been transmitted and the FIFOs are empty. As the
    TEND=0 has double meaning (waiting state, in progress) we can't
    determined the scenario described above.
    
    Add a software workaround for this. This sets a variable if any data has
    been sent on the serial console (when using PIO) or if the DMA callback has
    been called (meaning something has been transmitted).
    
    Fixes: 73a19e4c0301 ("serial: sh-sci: Add DMA support.")
    Cc: stable@vger.kernel.org
    Signed-off-by: Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>

--- drivers/tty/serial/sh-sci.c
+++ drivers/tty/serial/sh-sci.c
@@ -157,6 +157,7 @@ struct sci_port {
 
 	bool has_rtscts;
 	bool autorts;
+	bool first_time_tx;
 };
 
 #define SCI_NPORTS CONFIG_SERIAL_SH_SCI_NR_UARTS
@@ -850,6 +851,7 @@ static void sci_transmit_chars(struct uart_port *port)
 {
 	struct tty_port *tport = &port->state->port;
 	unsigned int stopped = uart_tx_stopped(port);
+	struct sci_port *s = to_sci_port(port);
 	unsigned short status;
 	unsigned short ctrl;
 	int count;
@@ -885,6 +887,7 @@ static void sci_transmit_chars(struct uart_port *port)
 		}
 
 		sci_serial_out(port, SCxTDR, c);
+		s->first_time_tx = true;
 
 		port->icount.tx++;
 	} while (--count > 0);
@@ -1240,6 +1243,8 @@ static void sci_dma_tx_complete(void *arg)
 
 	if (kfifo_len(&tport->xmit_fifo) < WAKEUP_CHARS)
 		uart_write_wakeup(port);
+
+	s->first_time_tx = true;
 
 	if (!kfifo_is_empty(&tport->xmit_fifo)) {
 		s->cookie_tx = 0;
@@ -2076,6 +2081,10 @@ static unsigned int sci_tx_empty(struct uart_port *port)
 {
 	unsigned short status = sci_serial_in(port, SCxSR);
 	unsigned short in_tx_fifo = sci_txfill(port);
+	struct sci_port *s = to_sci_port(port);
+
+	if (!s->first_time_tx)
+		return TIOCSER_TEMT;
 
 	return (status & SCxSR_TEND(port)) && !in_tx_fifo ? TIOCSER_TEMT : 0;
 }
@@ -2247,6 +2256,7 @@ static int sci_startup(struct uart_port *port)
 
 	dev_dbg(port->dev, "%s(%d)\n", __func__, port->line);
 
+	s->first_time_tx = false;
 	sci_request_dma(port);
 
 	ret = sci_request_irq(s);
@@ -2267,6 +2277,7 @@ static void sci_shutdown(struct uart_port *port)
 	dev_dbg(port->dev, "%s(%d)\n", __func__, port->line);
 
 	s->autorts = false;
+	s->first_time_tx = false;
 	mctrl_gpio_disable_ms(to_sci_port(port)->gpios);
 
 	uart_port_lock_irqsave(port, &flags);
