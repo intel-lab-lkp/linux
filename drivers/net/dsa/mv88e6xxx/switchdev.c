46cb4eab9c9b44faf7ef8cfc8e88946eb56ecce9
Elliot Ayrey <elliot.ayrey@alliedtelesis.co.nz>
2024-11-08 16:55:45 +1300

    net: dsa: mv88e6xxx: handle member-violations
    Add a handler for servicing member-violations to the mv88e6xxx switch
    driver.
    
    When we receive a member-violation from the hardware first check the
    ATU for the corresponding entry and only service the interrupt if the
    ATU entry has a non-zero DPV and the new port that raised the
    interrupt is not in the DPV.
    
    Servicing this interrupt will send a switchdev notification for the
    new port.
    
    Signed-off-by: Elliot Ayrey <elliot.ayrey@alliedtelesis.co.nz>

--- drivers/net/dsa/mv88e6xxx/switchdev.c
+++ drivers/net/dsa/mv88e6xxx/switchdev.c
@@ -79,5 +79,36 @@ int mv88e6xxx_handle_miss_violation(struct mv88e6xxx_chip *chip, int port,
 				       brport, &info.info, NULL);
 	rtnl_unlock();
 
-	return err;
+	return notifier_to_errno(err);
+}
+
+int mv88e6xxx_handle_member_violation(struct mv88e6xxx_chip *chip, int port,
+				      struct mv88e6xxx_atu_entry *entry, u16 fid)
+{
+	struct switchdev_notifier_fdb_info info = {
+		.addr = entry->mac,
+	};
+	struct net_device *brport;
+	struct dsa_port *dp;
+	u16 vid;
+	int err;
+
+	err = mv88e6xxx_find_vid(chip, fid, &vid);
+	if (err)
+		return err;
+
+	info.vid = vid;
+	dp = dsa_to_port(chip->ds, port);
+
+	rtnl_lock();
+	brport = dsa_port_to_bridge_port(dp);
+	if (!brport) {
+		rtnl_unlock();
+		return -ENODEV;
+	}
+	err = call_switchdev_notifiers(SWITCHDEV_FDB_ADD_TO_BRIDGE,
+				       brport, &info.info, NULL);
+	rtnl_unlock();
+
+	return notifier_to_errno(err);
 }
--- drivers/net/dsa/mv88e6xxx/switchdev.h
+++ drivers/net/dsa/mv88e6xxx/switchdev.h
@@ -15,5 +15,7 @@
 int mv88e6xxx_handle_miss_violation(struct mv88e6xxx_chip *chip, int port,
 				    struct mv88e6xxx_atu_entry *entry,
 				    u16 fid);
+int mv88e6xxx_handle_member_violation(struct mv88e6xxx_chip *chip, int port,
+				      struct mv88e6xxx_atu_entry *entry, u16 fid);
 
 #endif /* _MV88E6XXX_SWITCHDEV_H_ */
