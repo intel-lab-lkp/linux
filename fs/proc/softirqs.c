4435d15a04f8a30f1d6e614d1ee0b8aa6eb85a04
Enlin Mu <enlin.mu@unisoc.com>
2024-11-09 00:22:25 +0800

    proc/softirqs: change softirqs info from possile_cpu to online_cpu
    like /proc/interrupts,/proc/softirqs which shows
    the number of softirq for each online CPU
    
    Signed-off-by: Enlin Mu <enlin.mu@unisoc.com>

--- fs/proc/softirqs.c
+++ fs/proc/softirqs.c
@@ -13,13 +13,13 @@ static int show_softirqs(struct seq_file *p, void *v)
 	int i, j;
 
 	seq_puts(p, "                    ");
-	for_each_possible_cpu(i)
+	for_each_online_cpu(i)
 		seq_printf(p, "CPU%-8d", i);
 	seq_putc(p, '\n');
 
 	for (i = 0; i < NR_SOFTIRQS; i++) {
 		seq_printf(p, "%12s:", softirq_to_name[i]);
-		for_each_possible_cpu(j)
+		for_each_online_cpu(j)
 			seq_put_decimal_ull_width(p, " ", kstat_softirqs_cpu(i, j), 10);
 		seq_putc(p, '\n');
 	}
