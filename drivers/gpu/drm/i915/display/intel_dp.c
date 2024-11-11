db588c70010301e14a76afcf6a7f67a756baa830
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2024-11-11 14:42:16 +0530

    drm/i915/dp: Set FAVT mode in DP SDP with fixed refresh rate
    While running with fixed refresh rate and VRR timing generator set FAVT
    mode (Fixed Vtotal) in DP Adaptive Sync SDP to intimate the panel
    about Fixed refresh rate.
    
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>
    Reviewed-by: Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>

--- drivers/gpu/drm/i915/display/intel_dp.c
+++ drivers/gpu/drm/i915/display/intel_dp.c
@@ -2814,7 +2814,10 @@ static void intel_dp_compute_as_sdp(struct intel_dp *intel_dp,
 		as_sdp->target_rr = drm_mode_vrefresh(adjusted_mode);
 		as_sdp->target_rr_divider = true;
 	} else {
-		as_sdp->mode = DP_AS_SDP_AVT_DYNAMIC_VTOTAL;
+		if (crtc_state->vrr.mode == INTEL_VRRTG_MODE_FIXED_RR)
+			as_sdp->mode = DP_AS_SDP_AVT_FIXED_VTOTAL;
+		else
+			as_sdp->mode = DP_AS_SDP_AVT_DYNAMIC_VTOTAL;
 		as_sdp->vtotal = adjusted_mode->vtotal;
 		as_sdp->target_rr = 0;
 	}
