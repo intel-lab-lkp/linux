0413709883340c25b291422db7d2cf31afe44ce7
Vincent Mailhol <mailhol.vincent@wanadoo.fr>
2024-11-12 01:43:30 +0900

    linux/bits: simplify GENMASK_INPUT_CHECK()
    Because of the shortcut logic of the && operator, below expression:
    
      __builtin_choose_expr(condition, boolean_expression, false)
    
    can be simplified as:
    
      condition && boolean_expression
    
    Applied to GENMASK_INPUT_CHECK(),
    
      __builtin_choose_expr(__is_constexpr((l) > (h)), (l) > (h), 0)
    
    can be replaced by:
    
      __is_constexpr((l) > (h)) && (l) > (h)
    
    Finally, above expression is nearly the same as the expansion of
    statically_true((l) > (h)), except from the use of __is_constexpr()
    instead of __builtin_constant_p().
    
    Introduce _statically_true() which is similar to statically_true()
    but with __is_constexpr(). Apply _statically_true() to simplify
    GENMASK_INPUT_CHECK().
    
    Signed-off-by: Vincent Mailhol <mailhol.vincent@wanadoo.fr>

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -307,6 +307,7 @@ static inline void *offset_to_ptr(const int *off)
  * values to determine that the condition is statically true.
  */
 #define statically_true(x) (__builtin_constant_p(x) && (x))
+#define _statically_true(x) (__is_constexpr(x) && (x))
 
 /*
  * This is needed in functions which generate the stack canary, see
