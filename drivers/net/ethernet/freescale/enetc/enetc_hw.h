53813400da17aa3e267ecff26d5cb21599c83aa0
Wei Fang <wei.fang@nxp.com>
2024-11-12 17:14:43 +0800

    net: enetc: add Rx checksum offload for i.MX95 ENETC
    ENETC rev 4.1 supports TCP and UDP checksum offload for receive, the bit
    108 of the Rx BD will be set if the TCP/UDP checksum is correct. Since
    this capability is not defined in register, the rx_csum bit is added to
    struct enetc_drvdata to indicate whether the device supports Rx checksum
    offload.
    
    Signed-off-by: Wei Fang <wei.fang@nxp.com>
    Reviewed-by: Frank Li <Frank.Li@nxp.com>
    Reviewed-by: Claudiu Manoil <claudiu.manoil@nxp.com>

--- drivers/net/ethernet/freescale/enetc/enetc_hw.h
+++ drivers/net/ethernet/freescale/enetc/enetc_hw.h
@@ -645,6 +645,8 @@ union enetc_rx_bd {
 #define ENETC_RXBD_LSTATUS(flags)	((flags) << 16)
 #define ENETC_RXBD_FLAG_VLAN	BIT(9)
 #define ENETC_RXBD_FLAG_TSTMP	BIT(10)
+/* UDP and TCP checksum offload, for ENETC 4.1 and later */
+#define ENETC_RXBD_FLAG_L4_CSUM_OK	BIT(12)
 #define ENETC_RXBD_FLAG_TPID	GENMASK(1, 0)
 
 #define ENETC_MAC_ADDR_FILT_CNT	8 /* # of supported entries per port */
