0ff5ad39c09de71f06ae927d6d43e80ef14286b6
Shivam Chaudhary <cvam0000@gmail.com>
2024-11-12 20:00:55 +0530

    selftests: tmpfs: Add Test-fail if not run as root
    Add 'ksft_exit_fail_msg()', if  not run as root, with an appropriate
    Warning.
    
    Add 'ksft_print_header()' and 'ksft_set_plan()' to structure test
    outputs more effectively.
    
    Test logs:
    
    Before Change:
    - Without root
     error: unshare, errno 1
    
    - With root
     No, output
    
    After Change:
    - Without root
    TAP version 13
    1..1
    Bail out! Error : Need to run as root# Planned tests != run tests (1 != 0)
    
    - With root
    TAP version 13
    1..1
    
    Signed-off-by: Shivam Chaudhary <cvam0000@gmail.com>

--- tools/testing/selftests/tmpfs/bug-link-o-tmpfile.c
+++ tools/testing/selftests/tmpfs/bug-link-o-tmpfile.c
@@ -23,9 +23,20 @@
 #include <sys/mount.h>
 #include <unistd.h>
 
+#include "../kselftest.h"
+
 int main(void)
 {
 	int fd;
+
+	/* Setting up kselftest framework */
+	ksft_print_header();
+	ksft_set_plan(1);
+
+	/* Check if test is run as root */
+	if (geteuid()) {
+		ksft_exit_fail_msg("Error : Need to run as root");
+	}
 
 	if (unshare(CLONE_NEWNS) == -1) {
 		if (errno == ENOSYS || errno == EPERM) {
