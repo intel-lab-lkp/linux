41592fd1a901c90d4a7c970f71aef4955148ec28
David Gstir <david@sigma-star.at>
2024-11-13 22:27:54 +0100

    KEYS: trusted: dcp: fix improper sg use with CONFIG_VMAP_STACK=y
    With vmalloc stack addresses enabled (CONFIG_VMAP_STACK=y) DCP trusted
    keys can crash during en- and decryption of the blob encryption key via
    the DCP crypto driver. This is caused by improperly using sg_init_one()
    with vmalloc'd stack buffers (plain_key_blob).
    
    Fix this by always using kmalloc() for buffers we give to the DCP crypto
    driver.
    
    Cc: stable@vger.kernel.org # v6.10+
    Fixes: 0e28bf61a5f9 ("KEYS: trusted: dcp: fix leak of blob encryption key")
    Signed-off-by: David Gstir <david@sigma-star.at>

--- security/keys/trusted-keys/trusted_dcp.c
+++ security/keys/trusted-keys/trusted_dcp.c
@@ -200,11 +200,15 @@ static int trusted_dcp_seal(struct trusted_key_payload *p, char *datablob)
 {
 	struct dcp_blob_fmt *b = (struct dcp_blob_fmt *)p->blob;
 	int blen, ret;
-	u8 plain_blob_key[AES_KEYSIZE_128];
+	u8 *plain_blob_key;
 
 	blen = calc_blob_len(p->key_len);
 	if (blen > MAX_BLOB_SIZE)
 		return -E2BIG;
+
+	plain_blob_key = kmalloc(AES_KEYSIZE_128, GFP_KERNEL);
+	if (!plain_blob_key)
+		return -ENOMEM;
 
 	b->fmt_version = DCP_BLOB_VERSION;
 	get_random_bytes(b->nonce, AES_KEYSIZE_128);
@@ -228,7 +232,8 @@ static int trusted_dcp_seal(struct trusted_key_payload *p, char *datablob)
 	ret = 0;
 
 out:
-	memzero_explicit(plain_blob_key, sizeof(plain_blob_key));
+	memzero_explicit(plain_blob_key, AES_KEYSIZE_128);
+	kfree(plain_blob_key);
 
 	return ret;
 }
@@ -237,7 +242,7 @@ static int trusted_dcp_unseal(struct trusted_key_payload *p, char *datablob)
 {
 	struct dcp_blob_fmt *b = (struct dcp_blob_fmt *)p->blob;
 	int blen, ret;
-	u8 plain_blob_key[AES_KEYSIZE_128];
+	u8 *plain_blob_key = NULL;
 
 	if (b->fmt_version != DCP_BLOB_VERSION) {
 		pr_err("DCP blob has bad version: %i, expected %i\n",
@@ -252,6 +257,12 @@ static int trusted_dcp_unseal(struct trusted_key_payload *p, char *datablob)
 		pr_err("DCP blob has bad length: %i != %i\n", blen,
 		       p->blob_len);
 		ret = -EINVAL;
+		goto out;
+	}
+
+	plain_blob_key = kmalloc(AES_KEYSIZE_128, GFP_KERNEL);
+	if (!plain_blob_key) {
+		ret = -ENOMEM;
 		goto out;
 	}
 
@@ -270,7 +281,10 @@ static int trusted_dcp_unseal(struct trusted_key_payload *p, char *datablob)
 
 	ret = 0;
 out:
-	memzero_explicit(plain_blob_key, sizeof(plain_blob_key));
+	if (plain_blob_key) {
+		memzero_explicit(plain_blob_key, AES_KEYSIZE_128);
+		kfree(plain_blob_key);
+	}
 
 	return ret;
 }
