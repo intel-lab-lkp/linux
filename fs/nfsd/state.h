df18cb1d3ac873ade58580bba2e005a5d4b2e7e4
NeilBrown <neilb@suse.de>
2024-11-13 16:38:35 +1100

    nfsd: allocate new session-based DRC slots on demand.
    If a client ever uses the highest available slot for a given session,
    attempt to allocate another slot so there is room for the client to use
    more slots if wanted.  GFP_NOWAIT is used so if there is not plenty of
    free memory, failure is expected - which is what we want.  It also
    allows the allocation while holding a spinlock.
    
    We would expect to stablise with one more slot available than the client
    actually uses.
    
    Now that we grow the slot table on demand we can start with a smaller
    allocation.  Define NFSD_MAX_UNUSED_SLOTS and allocate at most that many
    when session is created.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- fs/nfsd/state.h
+++ fs/nfsd/state.h
@@ -215,6 +215,8 @@ static inline struct nfs4_delegation *delegstateid(struct nfs4_stid *s)
 
 /* Maximum number of slots per session. 160 is useful for long haul TCP */
 #define NFSD_MAX_SLOTS_PER_SESSION     160
+/* Maximum number of slots per session that are allocated by unused */
+#define NFSD_MAX_UNUSED_SLOTS		6
 /* Maximum  session per slot cache size */
 #define NFSD_SLOT_CACHE_SIZE		2048
 /* Maximum number of NFSD_SLOT_CACHE_SIZE slots per session */
