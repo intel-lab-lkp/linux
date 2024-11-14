f878e4d4665217dedd97a3ec51ed2b4f02224580
zhang jiao <zhangjiao2@cmss.chinamobile.com>
2024-11-14 16:40:39 +0800

    tools/thermal: Fix common realloc mistake
    If the 'realloc' fails, the thermal zones pointer is set to NULL. This
    makes all thermal zones references which were previously successfully
    initialized to be lost.
    
    Signed-off-by: zhang jiao <zhangjiao2@cmss.chinamobile.com>

--- tools/thermal/thermometer/thermometer.c
+++ tools/thermal/thermometer/thermometer.c
@@ -259,6 +259,7 @@ static int thermometer_add_tz(const char *path, const char *name, int polling,
 {
 	int fd;
 	char tz_path[PATH_MAX];
+	struct tz *tz;
 
 	sprintf(tz_path, CLASS_THERMAL"/%s/temp", path);
 
@@ -268,12 +269,13 @@ static int thermometer_add_tz(const char *path, const char *name, int polling,
 		return -1;
 	}
 
-	thermometer->tz = realloc(thermometer->tz,
+	tz = realloc(thermometer->tz,
 				  sizeof(*thermometer->tz) * (thermometer->nr_tz + 1));
-	if (!thermometer->tz) {
+	if (!tz) {
 		ERROR("Failed to allocate thermometer->tz\n");
 		return -1;
 	}
+	thermometer->tz = tz;
 
 	thermometer->tz[thermometer->nr_tz].fd_temp = fd;
 	thermometer->tz[thermometer->nr_tz].name = strdup(name);
