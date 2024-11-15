ad8b206e51898744e5684e360358c324e6fc1944
Anand Jain <anand.jain@oracle.com>
2024-11-15 22:54:07 +0800

    btrfs: pr CONFIG_BTRFS_EXPERIMENTAL status
    Commit c9c49e8f157e ("btrfs: split out CONFIG_BTRFS_EXPERIMENTAL from
    CONFIG_BTRFS_DEBUG") introduces a way to enable or disable experimental
    features, print its status during module load, like so:
    
    Btrfs loaded, debug=on, assert=on, zoned=yes, fsverity=yes
    
    Signed-off-by: Anand Jain <anand.jain@oracle.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -2476,6 +2476,11 @@ static int __init btrfs_print_mod_info(void)
 #else
 			", fsverity=no"
 #endif
+#ifdef CONFIG_BTRFS_EXPERIMENTAL
+			", experimental=yes"
+#else
+			", experimental=no"
+#endif
 			;
 	pr_info("Btrfs loaded%s\n", options);
 	return 0;
