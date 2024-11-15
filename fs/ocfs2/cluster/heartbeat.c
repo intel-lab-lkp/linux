9280a84d3eeeb3d5c913fe6b364e49cd55571f30
Daniel Yang <danielyangkang@gmail.com>
2024-11-15 00:00:17 -0800

    ocfs2: replace deprecated simple_strtol with kstrtol
    The function simple_strtol ignores overflows and has an awkward
    interface for error checking. Replace with the recommended kstrtol
    function leads to clearer error checking and safer conversions.
    
    Signed-off-by: Daniel Yang <danielyangkang@gmail.com>

--- fs/ocfs2/cluster/heartbeat.c
+++ fs/ocfs2/cluster/heartbeat.c
@@ -3,6 +3,7 @@
  * Copyright (C) 2004, 2005 Oracle.  All rights reserved.
  */
 
+#include "linux/kstrtox.h"
 #include <linux/kernel.h>
 #include <linux/sched.h>
 #include <linux/jiffies.h>
@@ -1777,8 +1778,8 @@ static ssize_t o2hb_region_dev_store(struct config_item *item,
 	if (o2nm_this_node() == O2NM_MAX_NODES)
 		goto out;
 
-	fd = simple_strtol(p, &p, 0);
-	if (!p || (*p && (*p != '\n')))
+	ret = kstrtol(p, 0, &fd);
+	if (ret < 0)
 		goto out;
 
 	if (fd < 0 || fd >= INT_MAX)
