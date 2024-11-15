318793b71a42bca0a89ca6664e0eeffb85c0c27e
Thomas Hellström <thomas.hellstrom@linux.intel.com>
2024-11-15 16:01:18 +0100

    drm/ttm: Add helpers for shrinking
    Add a number of helpers for shrinking that access core TTM and
    core MM functionality in a way that make them unsuitable for
    driver open-coding.
    
    v11:
    - New patch (split off from previous) and additional helpers.
    v13:
    - Adapt to ttm_backup interface change.
    - Take resource off LRU when backed up.
    
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Reviewed-by: Matthew Brost <matthew.brost@intel.com> #v11

--- include/drm/ttm/ttm_tt.h
+++ include/drm/ttm/ttm_tt.h
@@ -265,6 +265,8 @@ struct ttm_backup_flags {
 long ttm_tt_backup(struct ttm_device *bdev, struct ttm_tt *tt,
 		   const struct ttm_backup_flags flags);
 
+int ttm_tt_setup_backup(struct ttm_tt *tt);
+
 #if IS_ENABLED(CONFIG_AGP)
 #include <linux/agp_backend.h>
 
