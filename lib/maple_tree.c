168645289e916563f78edf15346aacc6f5e46f51
Wei Yang <richard.weiyang@gmail.com>
2024-11-16 01:48:04 +0000

    maple_tree: not possible to be a root node after loop
    Empty tree and single entry tree is handled else whether, so the maple
    tree here must be a tree with nodes.
    
    If the height is 1 and we found the gap, it will jump to *done* since it
    is also a leaf.
    If the height is more than one, and there may be an available range, we
    will descend the tree, which is not root anymore.
    
    If there is no available range, we will set error and return.
    
    This means the check for root node here is not necessary.
    
    Signed-off-by: Wei Yang <richard.weiyang@gmail.com>
    CC: Liam R. Howlett <Liam.Howlett@Oracle.com>
    CC: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    CC: Sidhartha Kumar <sidhartha.kumar@oracle.com>

--- lib/maple_tree.c
+++ lib/maple_tree.c
@@ -4904,7 +4904,7 @@ static inline bool mas_anode_descend(struct ma_state *mas, unsigned long size)
 		if (gap >= size) {
 			if (ma_is_leaf(type)) {
 				found = true;
-				goto done;
+				break;
 			}
 
 			mas->node = mas_slot(mas, slots, offset);
@@ -4921,9 +4921,6 @@ next_slot:
 		}
 	}
 
-	if (mte_is_root(mas->node))
-		found = true;
-done:
 	mas->offset = offset;
 	return found;
 }
