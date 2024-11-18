4c89d70cdb1e73e98940bea73d5e1e72b456d945
baihan li <libaihan@huawei.com>
2024-11-18 22:28:02 +0800

    drm/hisilicon/hibmc: add dp link moduel in hibmc
    Add link training process functions in this moduel.
    
    Signed-off-by: Baihan Li <libaihan@huawei.com>
    Signed-off-by: Yongbang Shi <shiyongbang@huawei.com>

--- drivers/gpu/drm/hisilicon/hibmc/dp/dp_comm.h
+++ drivers/gpu/drm/hisilicon/hibmc/dp/dp_comm.h
@@ -13,11 +13,34 @@
 #include <linux/io.h>
 #include <drm/display/drm_dp_helper.h>
 
+#define HIBMC_DP_LANE_NUM_MAX 2
+
+struct hibmc_link_status {
+	bool clock_recovered;
+	bool channel_equalized;
+};
+
+struct hibmc_link_cap {
+	int rx_dpcd_revision;
+	u8 link_rate;
+	u8 lanes;
+	bool is_tps3;
+	bool is_tps4;
+};
+
+struct hibmc_dp_link {
+	struct hibmc_link_status status;
+	u8 train_set[HIBMC_DP_LANE_NUM_MAX];
+	struct hibmc_link_cap cap;
+};
+
 struct hibmc_dp_dev {
 	struct drm_dp_aux aux;
 	struct drm_device *dev;
 	void __iomem *base;
 	struct mutex lock; /* protects concurrent RW in hibmc_dp_reg_write_field() */
+	struct hibmc_dp_link link;
+	u8 dpcd[DP_RECEIVER_CAP_SIZE];
 };
 
 #define dp_field_modify(reg_value, mask, val) ({		\
@@ -34,5 +57,6 @@ struct hibmc_dp_dev {
 	mutex_unlock(&_dp->lock); })
 
 void hibmc_dp_aux_init(struct hibmc_dp_dev *dp);
+int hibmc_dp_link_training(struct hibmc_dp_dev *dp);
 
 #endif
