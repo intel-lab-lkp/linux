ccfc4ee8d0b7e636ad872600bdd338f31661571a
Jan Petrous (OSS) <jan.petrous@oss.nxp.com>
2024-11-19 16:00:22 +0100

    net: stmmac: platform: Fix PTP clock rate reading
    The stmmac driver supports many vendors SoCs using Synopsys-licensed
    Ethernet controller IP. Most of these vendors reuse the stmmac_platform
    codebase, which has a potential PTP clock initialization issue.
    The PTP clock rate reading might require ungating what is not provided.
    
    Fix the PTP clock initialization by enabling it immediately.
    
    Signed-off-by: Jan Petrous (OSS) <jan.petrous@oss.nxp.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_platform.c
@@ -632,7 +632,7 @@ stmmac_probe_config_dt(struct platform_device *pdev, u8 *mac)
 	clk_prepare_enable(plat->pclk);
 
 	/* Fall-back to main clock in case of no PTP ref is passed */
-	plat->clk_ptp_ref = devm_clk_get(&pdev->dev, "ptp_ref");
+	plat->clk_ptp_ref = devm_clk_get_enabled(&pdev->dev, "ptp_ref");
 	if (IS_ERR(plat->clk_ptp_ref)) {
 		plat->clk_ptp_rate = clk_get_rate(plat->stmmac_clk);
 		plat->clk_ptp_ref = NULL;
