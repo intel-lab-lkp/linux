0a9debda37f927b887c3ea5ba7c00cad1e9b43ba
Mateusz Guzik <mjguzik@gmail.com>
2024-11-23 10:47:28 +0100

    x86/callthunks: s/bcmp/memcmp/
    bcmp() is implemented as a call to memcmp() and callthunks is the only
    consumer.
    
    Use memcmp() directly so that bcmp() can get retired.
    
    Signed-off-by: Mateusz Guzik <mjguzik@gmail.com>

--- arch/x86/kernel/callthunks.c
+++ arch/x86/kernel/callthunks.c
@@ -188,11 +188,11 @@ static void *patch_dest(void *dest, bool direct)
 	apply_relocation(insn_buff, pad, tsize, skl_call_thunk_template, tsize);
 
 	/* Already patched? */
-	if (!bcmp(pad, insn_buff, tsize))
+	if (!memcmp(pad, insn_buff, tsize))
 		return pad;
 
 	/* Ensure there are nops */
-	if (bcmp(pad, nops, tsize)) {
+	if (memcmp(pad, nops, tsize)) {
 		pr_warn_once("Invalid padding area for %pS\n", dest);
 		return NULL;
 	}
@@ -309,7 +309,7 @@ static bool is_callthunk(void *addr)
 	memcpy(insn_buff, skl_call_thunk_template, tmpl_size);
 	apply_relocation(insn_buff, pad, tmpl_size, skl_call_thunk_template, tmpl_size);
 
-	return !bcmp(pad, insn_buff, tmpl_size);
+	return !memcmp(pad, insn_buff, tmpl_size);
 }
 
 int x86_call_depth_emit_accounting(u8 **pprog, void *func, void *ip)
