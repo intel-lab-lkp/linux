218d2d2ceae8a37ba8f819c323158be211bfd329
Mohammad Rafi Shaik <quic_mohs@quicinc.com>
2024-11-26 22:12:56 +0530

    ASoC: dt-bindings: Add bindings for wcd937x static channel mapping
    Add wcd937x static channel mapping values to avoid
    having to use unclear number indices in device trees.
    
    Signed-off-by: Mohammad Rafi Shaik <quic_mohs@quicinc.com>

---
+++ include/dt-bindings/sound/qcom,wcd93xx.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause)
+ * Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef __DT_SOUND_QCOM_WCD93xx_H
+#define __DT_SOUND_QCOM_WCD93xx_H
+
+#define SWRM_CH1 1
+#define SWRM_CH2 2
+#define SWRM_CH3 4
+#define SWRM_CH4 8
+
+#endif
