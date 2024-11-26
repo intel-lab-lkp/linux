6836db69a16a78ee1238f2463bf8d71aef1f875e
Ricardo Ribalda <ribalda@chromium.org>
2024-11-26 16:18:57 +0000

    media: uvcvideo: Make power management granular
    Now that every ioctl takes care of their power management we can remove
    the "global" power management.
    
    Despite its size, this is a relatively big change. We hope that there
    are no size effects of it. If there are some specific devices that
    miss-behave, we can add a small quirk for them.
    
    This patch introduces a behavioral change for the uvc "trigger" button.
    It will not work unless the camera is streaming. We consider that this
    the most common (if not the only) usecase and therefore we do not consider
    it a regression.
    
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>

--- drivers/media/usb/uvc/uvc_v4l2.c
+++ drivers/media/usb/uvc/uvc_v4l2.c
@@ -612,7 +612,6 @@ static int uvc_v4l2_open(struct file *file)
 {
 	struct uvc_streaming *stream;
 	struct uvc_fh *handle;
-	int ret = 0;
 
 	stream = video_drvdata(file);
 	uvc_dbg(stream->dev, CALLS, "%s\n", __func__);
@@ -621,12 +620,6 @@ static int uvc_v4l2_open(struct file *file)
 	handle = kzalloc(sizeof(*handle), GFP_KERNEL);
 	if (!handle)
 		return -ENOMEM;
-
-	ret = uvc_status_get(stream->dev);
-	if (ret) {
-		kfree(handle);
-		return ret;
-	}
 
 	v4l2_fh_init(&handle->vfh, &stream->vdev);
 	v4l2_fh_add(&handle->vfh);
@@ -661,7 +654,6 @@ static int uvc_v4l2_release(struct file *file)
 	kfree(handle);
 	file->private_data = NULL;
 
-	uvc_status_put(stream->dev);
 	return 0;
 }
 
