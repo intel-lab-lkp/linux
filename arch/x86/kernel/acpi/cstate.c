0e5e450d3f1c104e3b4909299c5a9d16c67ed4bb
Patryk Wlazlyn <patryk.wlazlyn@linux.intel.com>
2024-11-27 17:15:16 +0100

    ACPI: processor_idle: Add FFH state handling
    Recent Intel platforms will depend on the idle driver to pass the
    correct hint for playing dead via mwait_play_dead_with_hint(). Expand
    the existing enter_dead interface with handling for FFH states and pass
    the MWAIT hint to the mwait_play_dead code.
    
    Signed-off-by: Patryk Wlazlyn <patryk.wlazlyn@linux.intel.com>
    Suggested-by: Gautham R. Shenoy <gautham.shenoy@amd.com>

--- arch/x86/kernel/acpi/cstate.c
+++ arch/x86/kernel/acpi/cstate.c
@@ -204,6 +204,15 @@ int acpi_processor_ffh_cstate_probe(unsigned int cpu,
 }
 EXPORT_SYMBOL_GPL(acpi_processor_ffh_cstate_probe);
 
+void acpi_processor_ffh_play_dead(struct acpi_processor_cx *cx)
+{
+	unsigned int cpu = smp_processor_id();
+	struct cstate_entry *percpu_entry;
+
+	percpu_entry = per_cpu_ptr(cpu_cstate_entry, cpu);
+	mwait_play_dead_with_hint(percpu_entry->states[cx->index].eax);
+}
+
 void __cpuidle acpi_processor_ffh_cstate_enter(struct acpi_processor_cx *cx)
 {
 	unsigned int cpu = smp_processor_id();
