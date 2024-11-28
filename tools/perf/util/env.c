156635c793f54d7c884e7f2c07d432dc34b88755
Zhongqiu Han <quic_zhonhan@quicinc.com>
2024-11-28 20:54:32 +0800

    perf bpf: Fix two memory leakages when calling perf_env__insert_bpf_prog_info()
    If perf_env__insert_bpf_prog_info() returns false due to a duplicate bpf
    prog info node insertion, the temporary info_node and info_linear memory
    will leak. Add a check to ensure the memory is freed if the function
    returns false.
    
    Fixes: 9c51f8788b5d ("perf env: Avoid recursively taking env->bpf_progs.lock")
    Signed-off-by: Zhongqiu Han <quic_zhonhan@quicinc.com>

--- tools/perf/util/env.c
+++ tools/perf/util/env.c
@@ -24,12 +24,15 @@ struct perf_env perf_env;
 #include "bpf-utils.h"
 #include <bpf/libbpf.h>
 
-void perf_env__insert_bpf_prog_info(struct perf_env *env,
+bool perf_env__insert_bpf_prog_info(struct perf_env *env,
 				    struct bpf_prog_info_node *info_node)
 {
+	bool ret = true;
 	down_write(&env->bpf_progs.lock);
-	__perf_env__insert_bpf_prog_info(env, info_node);
+	if (!__perf_env__insert_bpf_prog_info(env, info_node))
+		ret = false;
 	up_write(&env->bpf_progs.lock);
+	return ret;
 }
 
 bool __perf_env__insert_bpf_prog_info(struct perf_env *env, struct bpf_prog_info_node *info_node)
