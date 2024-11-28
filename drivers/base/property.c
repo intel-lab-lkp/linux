a1bd87afccc3cd29f355b68ea5bcc9984d33d33e
Dmitry Torokhov <dmitry.torokhov@gmail.com>
2024-11-27 21:39:34 -0800

    device property: do not leak child nodes when using NULL/error pointers
    The documentation to various API calls that locate children for a given
    fwnode (such as fwnode_get_next_available_child_node() or
    device_get_next_child_node()) states that the reference to the node
    passed in "child" argument is dropped unconditionally, however the
    change that added checks for the main node to be NULL or error pointer
    broke this promise.
    
    Add missing fwnode_handle_put() calls to restore the documented
    behavior.
    
    Fixes: 002752af7b89 ("device property: Allow error pointer to be passed to fwnode APIs")
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>

--- drivers/base/property.c
+++ drivers/base/property.c
@@ -759,6 +759,12 @@ struct fwnode_handle *
 fwnode_get_next_child_node(const struct fwnode_handle *fwnode,
 			   struct fwnode_handle *child)
 {
+	if (IS_ERR_OR_NULL(fwnode) ||
+	    !fwnode_has_op(fwnode, get_next_child_node)) {
+		fwnode_handle_put(child);
+		return NULL;
+	}
+
 	return fwnode_call_ptr_op(fwnode, get_next_child_node, child);
 }
 EXPORT_SYMBOL_GPL(fwnode_get_next_child_node);
@@ -777,9 +783,6 @@ fwnode_get_next_available_child_node(const struct fwnode_handle *fwnode,
 				     struct fwnode_handle *child)
 {
 	struct fwnode_handle *next_child = child;
-
-	if (IS_ERR_OR_NULL(fwnode))
-		return NULL;
 
 	do {
 		next_child = fwnode_get_next_child_node(fwnode, next_child);
@@ -806,8 +809,10 @@ struct fwnode_handle *device_get_next_child_node(const struct device *dev,
 	const struct fwnode_handle *fwnode = dev_fwnode(dev);
 	struct fwnode_handle *next;
 
-	if (IS_ERR_OR_NULL(fwnode))
+	if (IS_ERR_OR_NULL(fwnode)) {
+		fwnode_handle_put(child);
 		return NULL;
+	}
 
 	/* Try to find a child in primary fwnode */
 	next = fwnode_get_next_child_node(fwnode, child);
