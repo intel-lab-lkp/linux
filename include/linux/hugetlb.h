363d601f3fbb6eb9e848df92924d0dbf7ba4f4f2
Peter Xu <peterx@redhat.com>
2024-12-01 16:22:36 -0500

    mm/hugetlb: Rename avoid_reserve to cow_from_owner
    The old name "avoid_reserve" can be too generic and can be used wrongly in
    the new call sites that want to allocate a hugetlb folio.
    
    It's confusing on two things: (1) whether one can opt-in to avoid global
    reservation, and (2) whether it should take more than one count.
    
    In reality, this flag is only used in an extremely hacky path, in an
    extremely hacky way in hugetlb CoW path only, and always use with 1 saying
    "skip global reservation".  Rename the flag to avoid future abuse of this
    flag, making it a boolean so as to reflect its true representation that
    it's not a counter.  To make it even harder to abuse, add a comment above
    the function to explain it.
    
    Signed-off-by: Peter Xu <peterx@redhat.com>

--- include/linux/hugetlb.h
+++ include/linux/hugetlb.h
@@ -682,7 +682,7 @@ struct huge_bootmem_page {
 
 int isolate_or_dissolve_huge_page(struct page *page, struct list_head *list);
 struct folio *alloc_hugetlb_folio(struct vm_area_struct *vma,
-				unsigned long addr, int avoid_reserve);
+				unsigned long addr, bool cow_from_owner);
 struct folio *alloc_hugetlb_folio_nodemask(struct hstate *h, int preferred_nid,
 				nodemask_t *nmask, gfp_t gfp_mask,
 				bool allow_alloc_fallback);
@@ -1061,7 +1061,7 @@ static inline int isolate_or_dissolve_huge_page(struct page *page,
 
 static inline struct folio *alloc_hugetlb_folio(struct vm_area_struct *vma,
 					   unsigned long addr,
-					   int avoid_reserve)
+					   bool cow_from_owner)
 {
 	return NULL;
 }
