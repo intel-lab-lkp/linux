2b16eae940392e35d8342ea01d728110ade22ea2
Alejandro Lucero <alucerop@amd.com>
2024-12-02 17:12:20 +0000

    cxl: add function for obtaining region range
    A CXL region struct contains the physical address to work with.
    
    Add a function for getting the cxl region range to be used for mapping
    such memory range.
    
    Signed-off-by: Alejandro Lucero <alucerop@amd.com>

--- drivers/cxl/core/region.c
+++ drivers/cxl/core/region.c
@@ -2667,6 +2667,21 @@ err:
 	return ERR_PTR(rc);
 }
 
+int cxl_get_region_range(struct cxl_region *region, struct range *range)
+{
+	if (!region)
+		return -ENODEV;
+
+	if (!region->params.res)
+		return -ENOSPC;
+
+	range->start = region->params.res->start;
+	range->end = region->params.res->end;
+
+	return 0;
+}
+EXPORT_SYMBOL_NS_GPL(cxl_get_region_range, CXL);
+
 static ssize_t __create_region_show(struct cxl_root_decoder *cxlrd, char *buf)
 {
 	return sysfs_emit(buf, "region%u\n", atomic_read(&cxlrd->region_id));
