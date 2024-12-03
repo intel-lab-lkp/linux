b2b94b15abf5dd55bba20939673cdad59ec01585
Arun R Murthy <arun.r.murthy@intel.com>
2024-12-03 11:25:16 +0530

    drm/i915/histogram: histogram interrupt handling
    Upon enabling histogram an interrupt is trigerred after the generation
    of the statistics. This patch registers the histogram interrupt and
    handles the interrupt.
    
    v2: Added intel_crtc backpointer to intel_histogram struct (Jani)
        Removed histogram_wq and instead use dev_priv->unodered_eq (Jani)
    v3: Replaced drm_i915_private with intel_display (Suraj)
        Refactored the histogram read code (Jani)
    v4: Rebased after addressing comments on patch 1
    v5: removed the retry logic and moved to patch7 (Jani)
    
    Signed-off-by: Arun R Murthy <arun.r.murthy@intel.com>

--- drivers/gpu/drm/i915/display/intel_display_irq.c
+++ drivers/gpu/drm/i915/display/intel_display_irq.c
@@ -20,6 +20,7 @@
 #include "intel_fdi_regs.h"
 #include "intel_fifo_underrun.h"
 #include "intel_gmbus.h"
+#include "intel_histogram.h"
 #include "intel_hotplug_irq.h"
 #include "intel_pipe_crc_regs.h"
 #include "intel_pmdemand.h"
@@ -1177,6 +1178,9 @@ void gen8_de_irq_handler(struct drm_i915_private *dev_priv, u32 master_ctl)
 		if (iir & GEN8_PIPE_FIFO_UNDERRUN)
 			intel_cpu_fifo_underrun_irq_handler(dev_priv, pipe);
 
+		if (iir & GEN9_PIPE_HISTOGRAM_EVENT)
+			intel_histogram_irq_handler(display, pipe);
+
 		fault_errors = iir & gen8_de_pipe_fault_mask(dev_priv);
 		if (fault_errors)
 			drm_err_ratelimited(&dev_priv->drm,
@@ -1762,7 +1766,7 @@ void gen8_de_irq_postinstall(struct drm_i915_private *dev_priv)
 	struct intel_uncore *uncore = &dev_priv->uncore;
 
 	u32 de_pipe_masked = gen8_de_pipe_fault_mask(dev_priv) |
-		GEN8_PIPE_CDCLK_CRC_DONE;
+		GEN8_PIPE_CDCLK_CRC_DONE | GEN9_PIPE_HISTOGRAM_EVENT;
 	u32 de_pipe_enables;
 	u32 de_port_masked = gen8_de_port_aux_mask(dev_priv);
 	u32 de_port_enables;
