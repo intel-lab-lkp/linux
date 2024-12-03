8e88b8e261bd3d6f0cb621a0a3a15f7434be6263
Eugene Kobyak <eugene.kobyak@intel.com>
2024-12-03 11:59:03 +0000

    drm/i915: Fix NULL pointer dereference in capture_engine
    When the intel_context structure contains NULL,
    it raises a NULL pointer dereference error in drm_info().
    Fixes: e8a3319c31a1 ("drm/i915: Allow error capture without a request")
    Closes: https://gitlab.freedesktop.org/drm/i915/kernel/-/issues/12309
    Cc: John Harrison <John.C.Harrison@Intel.com>
    Cc: <stable@vger.kernel.org> # v6.3+
    Signed-off-by: Eugene Kobyak <eugene.kobyak@intel.com>

--- drivers/gpu/drm/i915/i915_gpu_error.c
+++ drivers/gpu/drm/i915/i915_gpu_error.c
@@ -1643,9 +1643,12 @@ capture_engine(struct intel_engine_cs *engine,
 		return NULL;
 
 	intel_engine_get_hung_entity(engine, &ce, &rq);
-	if (rq && !i915_request_started(rq))
-		drm_info(&engine->gt->i915->drm, "Got hung context on %s with active request %lld:%lld [0x%04X] not yet started\n",
-			 engine->name, rq->fence.context, rq->fence.seqno, ce->guc_id.id);
+	if (rq && !i915_request_started(rq)) {
+	        char guc_id[11];
+		ce ? snprintf(guc_id, sizeof(guc_id), " [0x%04x] ", ce->guc_id.id) : snprintf(guc_id, sizeof(guc_id), " ");
+	        drm_info(&engine->gt->i915->drm, "Got hung context on %s with active request %lld:%lld%snot yet started\n",
+	                 engine->name, rq->fence.context, rq->fence.seqno, guc_id);
+	}
 
 	if (rq) {
 		capture = intel_engine_coredump_add_request(ee, rq, ATOMIC_MAYFAIL);
