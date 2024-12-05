4e81a737812ccb48589ae648b560f387fde3d199
AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
2024-12-05 12:45:16 +0100

    drm/mediatek: mtk_hdmi_common: Add OP_HDMI, vendor and product strings
    In preparation for adding the HDMI TX v2 driver, and to allow
    a future modernization of the HDMI v1 one, perform changes
    that enable the usage of the HDMI Helpers provided by DRM.
    
    Check if the HDMI driver provides the function pointers to
    hdmi_{clear,write}_infoframe used by the HDMI Helper API and,
    if present, add DRM_BRIDGE_OP_HDMI to the drm_bridge ops,
    enabling the drm API to register the bridge as HDMI and to use
    the HDMI Helper functions.
    
    Moreover, as part of data exclusively used (in the context of
    HDMI bridges) by the helpers, assign the vendor "MediaTek" and
    product "On-Chip HDMI" strings to the drm_bridge struct.
    
    If the hdmi_{write,clear}_infoframe pointers are not assigned,
    vendor and product strings and HDMI helpers will not be used,
    hence this commit brings no functional changes to drivers that
    have not been refactored to use the new helpers.
    
    This also means that, in the current state, there is effectively
    no functional change to mtk_hdmi and its other components.
    
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>

--- drivers/gpu/drm/mediatek/mtk_hdmi_common.c
+++ drivers/gpu/drm/mediatek/mtk_hdmi_common.c
@@ -408,9 +408,16 @@ struct mtk_hdmi *mtk_hdmi_common_probe(struct platform_device *pdev)
 
 	hdmi->bridge.funcs = ver_conf->bridge_funcs;
 	hdmi->bridge.ops = DRM_BRIDGE_OP_DETECT | DRM_BRIDGE_OP_EDID | DRM_BRIDGE_OP_HPD;
+
+	if (ver_conf->bridge_funcs->hdmi_write_infoframe &&
+	    ver_conf->bridge_funcs->hdmi_clear_infoframe)
+		hdmi->bridge.ops |= DRM_BRIDGE_OP_HDMI;
+
 	hdmi->bridge.type = DRM_MODE_CONNECTOR_HDMIA;
 	hdmi->bridge.of_node = pdev->dev.of_node;
 	hdmi->bridge.ddc = hdmi->ddc_adpt;
+	hdmi->bridge.vendor = "MediaTek";
+	hdmi->bridge.product = "On-Chip HDMI";
 
 	ret = devm_drm_bridge_add(dev, &hdmi->bridge);
 	if (ret)
