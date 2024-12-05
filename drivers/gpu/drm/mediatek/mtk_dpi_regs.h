ffde523c58829c9d4957e0d6476ae9e5c58cf31e
AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
2024-12-05 12:45:08 +0100

    drm/mediatek: Add support for MT8195 Digital Parallel Interface
    Add support for the DPI block found in the MT8195 and MT8188 SoCs.
    Inside of the SoC, this block is directly connected to the HDMI IP.
    
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>

--- drivers/gpu/drm/mediatek/mtk_dpi_regs.h
+++ drivers/gpu/drm/mediatek/mtk_dpi_regs.h
@@ -40,6 +40,12 @@
 #define FAKE_DE_LEVEN			BIT(21)
 #define FAKE_DE_RODD			BIT(22)
 #define FAKE_DE_REVEN			BIT(23)
+
+/* DPI_CON: DPI instances */
+#define DPI_OUTPUT_1T1P_EN		BIT(24)
+#define DPI_INPUT_2P_EN			BIT(25)
+
+/* DPI_CON: DPINTF instances */
 #define DPINTF_YUV422_EN		BIT(24)
 #define DPINTF_CSC_ENABLE		BIT(26)
 #define DPINTF_INPUT_2P_EN		BIT(29)
