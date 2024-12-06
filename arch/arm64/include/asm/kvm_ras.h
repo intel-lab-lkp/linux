4810a62e513c0c5e949b653a5dc65c84a0c71426
Jiaqi Yan <jiaqiyan@google.com>
2024-12-06 01:02:44 +0000

    KVM: arm64: SIGBUS VMM for SEA guest abort
    When APEI is unable claim or handles synchronous external abort (SEA)
    today KVM handles SEA for guest by injecting an async SError into the
    guest directly, bypassing VMM, usually results in guest kernel panic.
    
    One major situation of guest SEA is when vCPU consumes uncorrectable
    memory error on the physical memory. Although SError and guest kernel
    panic effectively stops the propagation of corrupted memory, it is not
    easy for VMM and guest to recover from memory error in a more graceful
    manner.
    
    This patch teach KVM to send a SIGBUS BUS_OBJERR to VMM/vCPU, just like
    how core kernel signals SIGBUS BUS_OBJERR to a gernal poison consuming
    userspace thread when APEI is unable to claim the SEA. In addition to
    the benifit that KVM's handling for SEA becomes aligned with core
    kernel's behavior
    - VMM can inject SEA to guest. Compared to SError, the blast radius in
      VM is possible to be limited to only the consuming thread in guest,
      instead of the entire guest kernel (unless the poison consumption is
      from guest kernel).
    - VMM usually tracks the poisoned guest pages. Together with [1], if
      guest consumes again the already poisoned guest pages, VMM can protect
      itself and the host by stopping the consumption at software level, by
      intercepting guest's access to poisoned pages, and again injecting
      SEA to guest.
    
    KVM now handles SEA as follows:
    1. Delegate to APEI/GHES driver to see if SEA can be claimed by them.
    2. If APEI failed to claim the SEA, send current thread (i.e. VMM in EL0)
       a si_code=BUS_OBJERR SIGBUS signal. If the DIMM error's physical
       address is available from FAR_EL2, si_addr will be the DIMM error's
       host virtual address in VMM/vCPU's memory space.
    3. Otherwise bypass VMM and inject async SError to guest.
    
    Tested on a machine running Siryn AmpereOne processor. A dummy application
    in VM allocated some memory buffer. The test used EINJ to inject an
    uncorrectable recoverable memory error at a page in the allocated memory
    buffer. The dummy application then consumed the memory error. Some hack
    was done to make core kernel's memory_failure triggered by poison
    generation to fail, so KVM had to deal with the SEA guest abort due to
    poison consumption. vCPU thread in VMM received SIGBUS BUS_OBJERR with
    valid host virtual address of the poisoned page. VMM then injected a SEA
    into guest using KVM_SET_VCPU_EVENTS with ext_dabt_pending=1. At last
    the dummy application in guest was killed by SIGBUS BUS_OBJERR, while the
    guest survived and continued to run.
    
    [1] https://lpc.events/event/18/contributions/1757/attachments/1442/3073/LPC_%20KVM%20Userfault.pdf
    
    Changelog
    
    RFC v2 -> RFC v1
    - reword commit msg
    - drop unused parameters from kvm_delegate_guest_sea
    - remove KVM_CAP_ARM_SIGBUS_ON_SEA and its opt in code
    - set FnV bit in vcpu's ESR_ELx if host ESR_EL2's FnV is set
    - add documentation for this new SIGBUS feature
    
    Signed-off-by: Jiaqi Yan <jiaqiyan@google.com>

--- arch/arm64/include/asm/kvm_ras.h
+++ arch/arm64/include/asm/kvm_ras.h
@@ -1,5 +1,4 @@
 /* SPDX-License-Identifier: GPL-2.0 */
-/* Copyright (C) 2018 - Arm Ltd */
 
 #ifndef __ARM64_KVM_RAS_H__
 #define __ARM64_KVM_RAS_H__
@@ -11,15 +10,22 @@
 #include <asm/acpi.h>
 
 /*
- * Was this synchronous external abort a RAS notification?
- * Returns '0' for errors handled by some RAS subsystem, or -ENOENT.
+ * For synchrnous external abort taken to KVM at EL2, not on translation
+ * table walk or hardware update of translation table, is FAR_EL2 valid?
  */
-static inline int kvm_handle_guest_sea(phys_addr_t addr, u64 esr)
-{
-	/* apei_claim_sea(NULL) expects to mask interrupts itself */
-	lockdep_assert_irqs_enabled();
+bool kvm_vcpu_sea_far_valid(const struct kvm_vcpu *vcpu);
 
-	return apei_claim_sea(NULL);
-}
+/*
+ * Handle synchronous external abort (SEA) in the following order:
+ * 1. Delegate to APEI/GHES to see if they can claim SEA. If so, all done.
+ * 2. If the SEA is NOT about S2 translation table, send SIGBUS to current
+ *    with BUS_OBJERR and si_addr set to faulting/poisoned host virtual
+ *    address. When accurate HVA is unavailable, si_addr will be 0.
+ * 3. Otherwise, directly inject an async SError to guest.
+ *
+ * Note this applies to both instruction and data abort (ESR_ELx_EC_IABT_*
+ * and ESR_ELx_EC_DABT_*).
+ */
+void kvm_handle_guest_sea(struct kvm_vcpu *vcpu);
 
 #endif /* __ARM64_KVM_RAS_H__ */
