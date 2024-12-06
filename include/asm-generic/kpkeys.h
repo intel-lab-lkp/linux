f7b34d907b12977510cb7bc48dfb34ec561335ba
Kevin Brodsky <kevin.brodsky@arm.com>
2024-12-06 10:10:55 +0000

    mm: Introduce kpkeys
    kpkeys is a simple framework to enable the use of protection keys
    (pkeys) to harden the kernel itself. This patch introduces the basic
    API in <linux/kpkeys.h>: a couple of functions to set and restore
    the pkey register and a macro to define guard objects.
    
    kpkeys introduces a new concept on top of pkeys: the kpkeys level.
    Each level is associated to a set of permissions for the pkeys
    managed by the kpkeys framework. kpkeys_set_level(lvl) sets those
    permissions according to lvl, and returns the original pkey
    register, to be later restored by kpkeys_restore_pkey_reg(). To
    start with, only KPKEYS_LVL_DEFAULT is available, which is meant
    to grant RW access to KPKEYS_PKEY_DEFAULT (i.e. all memory since
    this is the only available pkey for now).
    
    Because each architecture implementing pkeys uses a different
    representation for the pkey register, and may reserve certain pkeys
    for specific uses, support for kpkeys must be explicitly indicated
    by selecting ARCH_HAS_KPKEYS and defining the following functions in
    <asm/kpkeys.h>, in addition to the macros provided in
    <asm-generic/kpkeys.h>:
    
    - arch_kpkeys_set_level()
    - arch_kpkeys_restore_pkey_reg()
    - arch_kpkeys_enabled()
    
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

---
+++ include/asm-generic/kpkeys.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __ASM_GENERIC_KPKEYS_H
+#define __ASM_GENERIC_KPKEYS_H
+
+#ifndef KPKEYS_PKEY_DEFAULT
+#define KPKEYS_PKEY_DEFAULT	0
+#endif
+
+#endif	/* __ASM_GENERIC_KPKEYS_H */
