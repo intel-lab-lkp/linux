0b932f05ed62d9aa73d18e01800bd959c725a14d
Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>
2024-12-06 13:13:30 +0200

    iio: adc: rzg2l_adc: Enable runtime PM autosuspend support
    Enable runtime PM autosuspend support for the rzg2l_adc driver. With this
    change, consecutive conversion requests will no longer cause the device to
    be runtime-enabled/disabled after each request. Instead, the device will
    transition based on the delay configured by the user.
    
    This approach reduces the frequency of hardware register access during
    runtime PM suspend/resume cycles, thereby saving CPU cycles.
    
    Signed-off-by: Claudiu Beznea <claudiu.beznea.uj@bp.renesas.com>

--- drivers/iio/adc/rzg2l_adc.c
+++ drivers/iio/adc/rzg2l_adc.c
@@ -208,7 +208,8 @@ static int rzg2l_adc_conversion(struct iio_dev *indio_dev, struct rzg2l_adc *adc
 	rzg2l_adc_start_stop(adc, false);
 
 rpm_put:
-	pm_runtime_put_sync(dev);
+	pm_runtime_mark_last_busy(dev);
+	pm_runtime_put_autosuspend(dev);
 	return ret;
 }
 
@@ -372,7 +373,8 @@ static int rzg2l_adc_hw_init(struct device *dev, struct rzg2l_adc *adc)
 	rzg2l_adc_writel(adc, RZG2L_ADM(3), reg);
 
 exit_hw_init:
-	pm_runtime_put_sync(dev);
+	pm_runtime_mark_last_busy(dev);
+	pm_runtime_put_autosuspend(dev);
 	return ret;
 }
 
@@ -410,6 +412,8 @@ static int rzg2l_adc_probe(struct platform_device *pdev)
 				     "failed to get/deassert presetn\n");
 	}
 
+	pm_runtime_set_autosuspend_delay(dev, 300);
+	pm_runtime_use_autosuspend(dev);
 	ret = devm_pm_runtime_enable(dev);
 	if (ret)
 		return ret;
