70c4ea934d1557c5a9242c78068d61016372b0c5
Liu Ying <victor.liu@nxp.com>
2024-12-09 11:39:14 +0800

    drm/imx: Add i.MX8qxp Display Controller pixel engine
    i.MX8qxp Display Controller pixel engine consists of all processing
    units that operate in the AXI bus clock domain.  Add drivers for
    ConstFrame, ExtDst, FetchLayer, FetchWarp and LayerBlend units, as
    well as a pixel engine driver, so that two displays with primary
    planes can be supported.  The pixel engine driver and those unit
    drivers are components to be aggregated by a master registered in
    the upcoming DRM driver.
    
    Signed-off-by: Liu Ying <victor.liu@nxp.com>

--- drivers/gpu/drm/imx/dc/dc-drv.h
+++ drivers/gpu/drm/imx/dc/dc-drv.h
@@ -11,14 +11,22 @@
 #include <drm/drm_device.h>
 
 #include "dc-de.h"
+#include "dc-pe.h"
 
 struct dc_drm_device {
 	struct drm_device base;
 	struct dc_de *de[DC_DISPLAYS];
+	struct dc_pe *pe;
 };
 
+extern struct platform_driver dc_cf_driver;
+extern struct platform_driver dc_ed_driver;
 extern struct platform_driver dc_de_driver;
 extern struct platform_driver dc_fg_driver;
+extern struct platform_driver dc_fl_driver;
+extern struct platform_driver dc_fw_driver;
+extern struct platform_driver dc_lb_driver;
+extern struct platform_driver dc_pe_driver;
 extern struct platform_driver dc_tc_driver;
 
 #endif /* __DC_DRV_H__ */
