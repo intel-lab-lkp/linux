e6ba19a61c558257bd898d61ab441a67167c5808
Liu Ying <victor.liu@nxp.com>
2024-12-09 11:39:15 +0800

    drm/imx: Add i.MX8qxp Display Controller interrupt controller
    i.MX8qxp Display Controller has a built-in interrupt controller to support
    Enable/Status/Preset/Clear interrupt bit.  Add driver for it.
    
    Signed-off-by: Liu Ying <victor.liu@nxp.com>

--- drivers/gpu/drm/imx/dc/dc-drv.h
+++ drivers/gpu/drm/imx/dc/dc-drv.h
@@ -25,6 +25,7 @@ extern struct platform_driver dc_de_driver;
 extern struct platform_driver dc_fg_driver;
 extern struct platform_driver dc_fl_driver;
 extern struct platform_driver dc_fw_driver;
+extern struct platform_driver dc_ic_driver;
 extern struct platform_driver dc_lb_driver;
 extern struct platform_driver dc_pe_driver;
 extern struct platform_driver dc_tc_driver;
