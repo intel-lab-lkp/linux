9f66ebee8d346ecc7648aa3d69dc11fd33d0cab6
Ahmed Zaki <ahmed.zaki@intel.com>
2024-12-09 17:26:23 -0700

    bnxt: use napi's irq affinity
    Delete the driver CPU affinity info and use the core's napi config
    instead.
    
    Signed-off-by: Ahmed Zaki <ahmed.zaki@intel.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.c
@@ -11179,14 +11179,8 @@ static void bnxt_free_irq(struct bnxt *bp)
 		int map_idx = bnxt_cp_num_to_irq_num(bp, i);
 
 		irq = &bp->irq_tbl[map_idx];
-		if (irq->requested) {
-			if (irq->have_cpumask) {
-				irq_update_affinity_hint(irq->vector, NULL);
-				free_cpumask_var(irq->cpu_mask);
-				irq->have_cpumask = 0;
-			}
+		if (irq->requested)
 			free_irq(irq->vector, bp->bnapi[i]);
-		}
 
 		irq->requested = 0;
 	}
@@ -11227,23 +11221,9 @@ static int bnxt_request_irq(struct bnxt *bp)
 		if (rc)
 			break;
 
-		netif_napi_set_irq(&bp->bnapi[i]->napi, irq->vector, 0);
+		netif_napi_set_irq(&bp->bnapi[i]->napi,
+				   irq->vector, NAPIF_F_IRQ_AFFINITY);
 		irq->requested = 1;
-
-		if (zalloc_cpumask_var(&irq->cpu_mask, GFP_KERNEL)) {
-			int numa_node = dev_to_node(&bp->pdev->dev);
-
-			irq->have_cpumask = 1;
-			cpumask_set_cpu(cpumask_local_spread(i, numa_node),
-					irq->cpu_mask);
-			rc = irq_update_affinity_hint(irq->vector, irq->cpu_mask);
-			if (rc) {
-				netdev_warn(bp->dev,
-					    "Update affinity hint failed, IRQ = %d\n",
-					    irq->vector);
-				break;
-			}
-		}
 	}
 	return rc;
 }
