38e2784a8f40c0e9d6b58b569985e3d5117179d5
Amal Raj T <amalrajt@meta.com>
2024-12-10 05:34:47 -0800

    serial: Move LF -> CRLF replacement from serial console to kdb
    The current implementation of `poll_put_char` in the serial console driver
    performs LF -> CRLF replacement, which can corrupt binary data. Since kdb
    is the only user of `poll_put_char`, this patch moves the LF -> CRLF
    replacement logic to kdb.
    
    Link: https://lore.kernel.org/linux-debuggers/Zy093jVKPs9gSVx2@telecaster/
    
    Signed-off-by: Amal Raj T <amalrajt@meta.com>

--- drivers/tty/serial/serial_core.c
+++ drivers/tty/serial/serial_core.c
@@ -2126,8 +2126,6 @@ void uart_console_write(struct uart_port *port, const char *s,
 	unsigned int i;
 
 	for (i = 0; i < count; i++, s++) {
-		if (*s == '\n')
-			putchar(port, '\r');
 		putchar(port, *s);
 	}
 }
@@ -2722,8 +2720,6 @@ static void uart_poll_put_char(struct tty_driver *driver, int line, char ch)
 	if (!port)
 		return;
 
-	if (ch == '\n')
-		port->ops->poll_put_char(port, '\r');
 	port->ops->poll_put_char(port, ch);
 	uart_port_deref(port);
 }
