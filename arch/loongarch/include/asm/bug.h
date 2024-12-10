5373c6d0e75e5dbdff4778e99733dde7bf72ab9e
FUJITA Tomonori <fujita.tomonori@gmail.com>
2024-12-10 09:18:00 +0900

    loongarch/bug: Add ARCH_WARN_ASM macro for BUG/WARN asm code sharing with Rust
    Add new ARCH_WARN_ASM macro for BUG/WARN assembly code sharing with
    Rust to avoid the duplication.
    
    No functional changes.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@gmail.com>

--- arch/loongarch/include/asm/bug.h
+++ arch/loongarch/include/asm/bug.h
@@ -21,14 +21,21 @@
 #ifndef CONFIG_GENERIC_BUG
 #define __BUG_ENTRY(flags)
 #else
-#define __BUG_ENTRY(flags) 					\
+
+#define __BUG_ENTRY_START					\
 		.pushsection __bug_table, "aw";			\
 		.align 2;					\
 	10000:	.long 10001f - .;				\
-		_BUGVERBOSE_LOCATION(__FILE__, __LINE__)	\
-		.short flags; 					\
+
+#define __BUG_ENTRY_END						\
 		.popsection;					\
 	10001:
+
+#define __BUG_ENTRY(flags)					\
+		__BUG_ENTRY_START			\
+		_BUGVERBOSE_LOCATION(__FILE__, __LINE__)	\
+		.short flags;					\
+		__BUG_ENTRY_END
 #endif
 
 #define ASM_BUG_FLAGS(flags)					\
@@ -54,6 +61,24 @@ do {								\
 	__BUG_FLAGS(0);						\
 	unreachable();						\
 } while (0)
+
+#ifdef CONFIG_DEBUG_BUGVERBOSE
+#define __BUG_LOCATION_STRING(file, line)		\
+		".long " file "- .;"			\
+		".short " line ";"
+#else
+#define __BUG_LOCATION_STRING(_file, _line)
+#endif
+
+#define __BUG_ENTRY_STRING(file, line, flags)		\
+		__stringify(__BUG_ENTRY_START)		\
+		__BUG_LOCATION_STRING(file, line)	\
+		".short " flags ";"			\
+		__stringify(__BUG_ENTRY_END)
+
+#define ARCH_WARN_ASM(file, line, flags, size)		\
+	__BUG_ENTRY_STRING(file, line, flags)		\
+	__stringify(break BRK_BUG) ";"
 
 #define HAVE_ARCH_BUG
 
