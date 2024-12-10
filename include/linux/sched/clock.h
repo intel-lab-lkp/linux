22477022301c3cd1c59bd5058f3c9ea1c37af5ba
Atharva Tiwari <evepolonium@gmail.com>
2024-12-10 19:09:18 +0530

    x86 tsc: avoid system instability in hibernation
    System instability are seen during resume from hibernation when system is under heavy CPU load. this is caused by the lack of update of sched clock data
    
    Signed-off-by: Atharva Tiwari <evepolonium@gmail.com>

--- include/linux/sched/clock.h
+++ include/linux/sched/clock.h
@@ -40,7 +40,9 @@ static inline void sched_clock_tick(void)
 static inline void clear_sched_clock_stable(void)
 {
 }
-
+static inline void set_sched_clock_stable(void)
+{
+}
 static inline void sched_clock_idle_sleep_event(void)
 {
 }
@@ -65,6 +67,7 @@ static __always_inline u64 local_clock(void)
 }
 #else
 extern int sched_clock_stable(void);
+extern void set_sched_clock_stable(void);
 extern void clear_sched_clock_stable(void);
 
 /*
