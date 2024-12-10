intel-lab-lkp/linux

commit 4095cf872084ecfdfdb0e681f3e9ff9745acfa75

ASoC: amd: yc: Fix for enabling DMIC on acp6x via _DSD entry
Add condition check to register ACP PDM sound card by reading
_WOV acpi entry.

Fixes: 5426f506b584 ("ASoC: amd: Add support for enabling DMIC on acp6x via _DSD")

Signed-off-by: Venkata Prasad Potturu <venkataprasad.potturu@amd.com>
Link: https://patch.msgid.link/20241127112227.227106-1-venkataprasad.potturu@amd.com
Signed-off-by: Mark Brown <broonie@kernel.org>

sound/soc/amd/yc/acp6x-mach.c
@@ -551,8 +551,14 @@ static int acp6x_probe(struct platform_device *pdev)
 	struct acp6x_pdm *machine = NULL;
 	struct snd_soc_card *card;
 	struct acpi_device *adev;
+	acpi_handle handle;
+	acpi_integer dmic_status;
 	int ret;
+	bool is_dmic_enable, wov_en;
 
+	/* IF WOV entry not found, enable dmic based on AcpDmicConnected entry*/
+	is_dmic_enable = false;
+	wov_en = true;
 	/* check the parent device's firmware node has _DSD or not */
 	adev = ACPI_COMPANION(pdev->dev.parent);
 	if (adev) {
@@ -560,8 +566,18 @@ static int acp6x_probe(struct platform_device *pdev)
 
 		if (!acpi_dev_get_property(adev, "AcpDmicConnected", ACPI_TYPE_INTEGER, &obj) &&
 		    obj->integer.value == 1)
-			platform_set_drvdata(pdev, &acp6x_card);
+			is_dmic_enable = true;
 	}
+
+	handle = ACPI_HANDLE(pdev->dev.parent);
+	ret = acpi_evaluate_integer(handle, "_WOV", NULL, &dmic_status);
+	if (!ACPI_FAILURE(ret))
+		wov_en = dmic_status;
+
+	if (is_dmic_enable && wov_en)
+		platform_set_drvdata(pdev, &acp6x_card);
+	else
+		return 0;
 
 	/* check for any DMI overrides */
 	dmi_id = dmi_first_match(yc_acp_quirk_table);

commit 4352292e3421f970a3eda4d368e83f41bfdae28e

ASoC: amd: yc: Fix bug where quirks code was skipped due to early return.
Replaced the early return with DMI quirks code.
This ensures that DMI quirks are executed.

Fixes: 4095cf872084 ("ASoC: amd: yc: Fix for enabling DMIC on acp6x via _DSD entry")
Signed-off-by: Yang Zhao <etoyz688@gmail.com>

sound/soc/amd/yc/acp6x-mach.c
@@ -23,7 +23,7 @@ SND_SOC_DAILINK_DEF(acp6x_pdm,
 
 SND_SOC_DAILINK_DEF(dmic_codec,
 		    DAILINK_COMP_ARRAY(COMP_CODEC("dmic-codec.0",
-						  "dmic-hifi")));
+							      "dmic-hifi")));
 
 SND_SOC_DAILINK_DEF(pdm_platform,
 		    DAILINK_COMP_ARRAY(COMP_PLATFORM("acp_yc_pdm_dma.0")));
@@ -583,13 +583,12 @@ static int acp6x_probe(struct platform_device *pdev)
 
 	if (is_dmic_enable && wov_en)
 		platform_set_drvdata(pdev, &acp6x_card);
-	else
-		return 0;
-
-	/* check for any DMI overrides */
-	dmi_id = dmi_first_match(yc_acp_quirk_table);
-	if (dmi_id)
-		platform_set_drvdata(pdev, dmi_id->driver_data);
+	else {
+		/* check for any DMI overrides */
+		dmi_id = dmi_first_match(yc_acp_quirk_table);
+		if (dmi_id)
+			platform_set_drvdata(pdev, dmi_id->driver_data);
+	}
 
 	card = platform_get_drvdata(pdev);
 	if (!card)
@@ -601,8 +600,8 @@ static int acp6x_probe(struct platform_device *pdev)
 	ret = devm_snd_soc_register_card(&pdev->dev, card);
 	if (ret) {
 		return dev_err_probe(&pdev->dev, ret,
-				"snd_soc_register_card(%s) failed\n",
-				card->name);
+				     "snd_soc_register_card(%s) failed\n",
+				     card->name);
 	}
 	return 0;
 }