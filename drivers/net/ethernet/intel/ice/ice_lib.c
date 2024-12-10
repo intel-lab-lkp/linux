1b9464be1689ec5cbbcdfb061411813b14c2d345
Ahmed Zaki <ahmed.zaki@intel.com>
2024-12-09 17:26:25 -0700

    ice: use napi's irq affinity
    Delete the driver CPU affinity info and use the core's napi config
    instead.
    
    Signed-off-by: Ahmed Zaki <ahmed.zaki@intel.com>

--- drivers/net/ethernet/intel/ice/ice_lib.c
+++ drivers/net/ethernet/intel/ice/ice_lib.c
@@ -2589,12 +2589,6 @@ void ice_vsi_free_irq(struct ice_vsi *vsi)
 		      vsi->q_vectors[i]->num_ring_rx))
 			continue;
 
-		/* clear the affinity notifier in the IRQ descriptor */
-		if (!IS_ENABLED(CONFIG_RFS_ACCEL))
-			irq_set_affinity_notifier(irq_num, NULL);
-
-		/* clear the affinity_hint in the IRQ descriptor */
-		irq_update_affinity_hint(irq_num, NULL);
 		synchronize_irq(irq_num);
 		devm_free_irq(ice_pf_to_dev(pf), irq_num, vsi->q_vectors[i]);
 	}
@@ -2735,7 +2729,8 @@ void ice_vsi_set_napi_queues(struct ice_vsi *vsi)
 	ice_for_each_q_vector(vsi, v_idx) {
 		struct ice_q_vector *q_vector = vsi->q_vectors[v_idx];
 
-		netif_napi_set_irq(&q_vector->napi, q_vector->irq.virq, 0);
+		netif_napi_set_irq(&q_vector->napi,
+				   q_vector->irq.virq, NAPIF_F_IRQ_AFFINITY);
 	}
 }
 
