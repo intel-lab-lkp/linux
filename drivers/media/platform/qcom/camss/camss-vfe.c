a57572c798b19a7df0f1b8fe9a5a538677549211
Depeng Shao <quic_depengs@quicinc.com>
2024-12-11 19:37:38 +0530

    media: qcom: camss: Add support for VFE 780
    Add support for VFE found on SM8550 (Titan 780). This implementation is
    based on the titan 480 implementation. It supports the normal and lite
    VFE.
    
    Co-developed-by: Yongsheng Li <quic_yon@quicinc.com>
    Signed-off-by: Yongsheng Li <quic_yon@quicinc.com>
    Signed-off-by: Depeng Shao <quic_depengs@quicinc.com>
    Reviewed-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>

--- drivers/media/platform/qcom/camss/camss-vfe.c
+++ drivers/media/platform/qcom/camss/camss-vfe.c
@@ -344,6 +344,7 @@ static u32 vfe_src_pad_code(struct vfe_line *line, u32 sink_code,
 	case CAMSS_845:
 	case CAMSS_8250:
 	case CAMSS_8280XP:
+	case CAMSS_8550:
 		switch (sink_code) {
 		case MEDIA_BUS_FMT_YUYV8_1X16:
 		{
@@ -1964,6 +1965,7 @@ static int vfe_bpl_align(struct vfe_device *vfe)
 	case CAMSS_845:
 	case CAMSS_8250:
 	case CAMSS_8280XP:
+	case CAMSS_8550:
 		ret = 16;
 		break;
 	default:
