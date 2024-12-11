2c0bc5244fae024be5af875b488410ebb849b9f1
Ma Ke <make_ruc2021@163.com>
2024-12-11 11:18:29 +0800

    PM / devfreq: Check dev_set_name() return value
    It's possible that dev_set_name() returns -ENOMEM. We could catch and
    handle it by adding dev_set_name() return value check.
    
    Cc: stable@vger.kernel.org
    Fixes: 775fa8c3aa22 ("PM / devfreq: Simplify the sysfs name of devfreq-event device")
    Signed-off-by: Ma Ke <make_ruc2021@163.com>

--- drivers/devfreq/devfreq-event.c
+++ drivers/devfreq/devfreq-event.c
@@ -328,7 +328,10 @@ struct devfreq_event_dev *devfreq_event_add_edev(struct device *dev,
 	edev->dev.class = devfreq_event_class;
 	edev->dev.release = devfreq_event_release_edev;
 
-	dev_set_name(&edev->dev, "event%d", atomic_inc_return(&event_no));
+	ret = dev_set_name(&edev->dev, "event%d", atomic_inc_return(&event_no));
+	if (ret)
+		return ERR_PTR(-ENOMEM);
+
 	ret = device_register(&edev->dev);
 	if (ret < 0) {
 		put_device(&edev->dev);
