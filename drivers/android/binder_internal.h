5c4db9b4f354a53534a38b2ca988ba2ade1e5066
Li Li <dualli@google.com>
2024-12-12 14:41:13 -0800

    binderfs: add new binder devices to binder_devices
    When binderfs is not enabled, the binder driver parses the kernel
    config to create all binder devices. All of the new binder devices
    are stored in the list binder_devices.
    
    When binderfs is enabled, the binder driver creates new binder devices
    dynamically when userspace applications call BINDER_CTL_ADD ioctl. But
    the devices created in this way are not stored in the same list.
    
    This patch fixes that.
    
    Signed-off-by: Li Li <dualli@google.com>

--- drivers/android/binder_internal.h
+++ drivers/android/binder_internal.h
@@ -582,4 +582,12 @@ struct binder_object {
 	};
 };
 
+/**
+ * Add a binder device to binder_devices
+ * @device: the new binder device to add to the global list
+ *
+ * Not reentrant as the list is not protected by any locks
+ */
+void binder_add_device(struct binder_device *device);
+
 #endif /* _LINUX_BINDER_INTERNAL_H */
