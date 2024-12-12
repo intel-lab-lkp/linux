bb23c214cdb6e309ae2d42b3b62f81f83f59a1af
Tudor Ambarus <tudor.ambarus@linaro.org>
2024-12-12 15:43:45 +0000

    dt-bindings: mailbox: add google,gs101-mbox bindings
    Add bindings for the Samsung Exynos Mailbox Controller.
    
    Signed-off-by: Tudor Ambarus <tudor.ambarus@linaro.org>

---
+++ include/dt-bindings/mailbox/google,gs101.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright 2024 Linaro Ltd.
+ *
+ * This header provides constants for the defined mailbox channel types.
+ */
+
+#ifndef _DT_BINDINGS_MAILBOX_GOOGLE_GS101_H
+#define _DT_BINDINGS_MAILBOX_GOOGLE_GS101_H
+
+#define DOORBELL	0
+#define DATA		1
+
+#endif /* _DT_BINDINGS_MAILBOX_GOOGLE_GS101_H */
