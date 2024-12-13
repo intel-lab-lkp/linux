425eda7352779a3d5fc9be8e52b997534e439e87
Biju Das <biju.das.jz@bp.renesas.com>
2024-12-13 17:38:47 +0000

    dt-bindings: pinctrl: renesas: Add alpha-numerical port support for RZ/V2H
    RZ/V2H has ports P0-P9 and PA-PB. Add support for defining alpha-numerical
    ports in DT using RZV2H_* macros. Add PORT_P* macros based on PFC_P_mn
    offset.
    
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>

--- include/dt-bindings/pinctrl/rzg2l-pinctrl.h
+++ include/dt-bindings/pinctrl/rzg2l-pinctrl.h
@@ -11,13 +11,29 @@
 
 #define RZG2L_PINS_PER_PORT	8
 
+/* PORT_Px = Offset address of PFC_P_mn  - 0x20 */
+#define PORT_P0		0
+#define PORT_P1		1
+#define PORT_P2		2
+#define PORT_P3		3
+#define PORT_P4		4
+#define PORT_P5		5
+#define PORT_P6		6
+#define PORT_P7		7
+#define PORT_P8		8
+#define PORT_P9		9
+#define PORT_PA		10
+#define PORT_PB		11
+
 /*
  * Create the pin index from its bank and position numbers and store in
  * the upper 16 bits the alternate function identifier
  */
 #define RZG2L_PORT_PINMUX(b, p, f)	((b) * RZG2L_PINS_PER_PORT + (p) | ((f) << 16))
+#define RZV2H_PORT_PINMUX(b, p, f)	RZG2L_PORT_PINMUX(PORT_P##b, p, f)
 
 /* Convert a port and pin label to its global pin index */
 #define RZG2L_GPIO(port, pin)	((port) * RZG2L_PINS_PER_PORT + (pin))
+#define RZV2H_GPIO(port, pin)	RZG2L_GPIO(PORT_P##port, pin)
 
 #endif /* __DT_BINDINGS_RZG2L_PINCTRL_H */
