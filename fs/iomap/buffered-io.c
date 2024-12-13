1300bedd40139bd653b8325da33ddaa6833b0ddd
Brian Foster <bfoster@redhat.com>
2024-12-13 09:36:09 -0500

    iomap: advance the iter directly on unshare range
    Modify unshare range to advance the iter directly. Replace the local
    pos and length calculations with direct advances and loop based on
    iter state instead.
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>

--- fs/iomap/buffered-io.c
+++ fs/iomap/buffered-io.c
@@ -1268,18 +1268,16 @@ EXPORT_SYMBOL_GPL(iomap_write_delalloc_release);
 static loff_t iomap_unshare_iter(struct iomap_iter *iter)
 {
 	struct iomap *iomap = &iter->iomap;
-	loff_t pos = iter->pos;
-	loff_t length = iomap_length(iter);
-	loff_t written = 0;
 
 	if (!iomap_want_unshare_iter(iter))
-		return length;
+		return iomap_length(iter);
 
 	do {
 		struct folio *folio;
 		int status;
 		size_t offset;
-		size_t bytes = min_t(u64, SIZE_MAX, length);
+		size_t bytes = min_t(u64, SIZE_MAX, iomap_length(iter));
+		loff_t pos = iter->pos;
 		bool ret;
 
 		status = iomap_write_begin(iter, pos, bytes, &folio);
@@ -1299,14 +1297,12 @@ static loff_t iomap_unshare_iter(struct iomap_iter *iter)
 
 		cond_resched();
 
-		pos += bytes;
-		written += bytes;
-		length -= bytes;
+		iomap_iter_advance(iter, bytes);
 
 		balance_dirty_pages_ratelimited(iter->inode->i_mapping);
-	} while (length > 0);
+	} while (iomap_length(iter) > 0);
 
-	return written;
+	return 0;
 }
 
 int
