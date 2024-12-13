acb3bc3ddb8c9a127bf8eae92a57ef9c2186b979
Shimrra Shai <shimrrashai@gmail.com>
2024-12-13 12:08:54 -0600

    arm64: dts: rockchip: add DTs for Firefly ITX-3588J
    Main DTS for the board and Makefile addition.
    
    Signed-off-by: Shimrra Shai <shimrrashai@gmail.com>

---
+++ arch/arm64/boot/dts/rockchip/rockchip-pca9555.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR MIT */
+/*
+ * Bindings for the PCA9555 GPIO extender used on some Rockchip devices, e.g.
+ * Firefly.
+ *
+ * Copyright (c) 2013 MundoReader S.L.
+ * Authors: Heiko Stuebner <heiko@sntech.de>
+ *          Shimrra Shai <shimrrashai@gmail.com>
+ */
+
+#ifndef __RK_PCA9555_H__
+#define __RK_PCA9555_H__
+
+#define PCA_IO0_0          0
+#define PCA_IO0_1          1
+#define PCA_IO0_2          2
+#define PCA_IO0_3          3
+#define PCA_IO0_4          4
+#define PCA_IO0_5          5
+#define PCA_IO0_6          6
+#define PCA_IO0_7          7
+#define PCA_IO1_0          8
+#define PCA_IO1_1          9
+#define PCA_IO1_2          10
+#define PCA_IO1_3          11
+#define PCA_IO1_4          12
+#define PCA_IO1_5          13
+#define PCA_IO1_6          14
+#define PCA_IO1_7          15
+
+#endif
