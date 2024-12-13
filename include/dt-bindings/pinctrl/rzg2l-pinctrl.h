88472439047a525dba7ec414e28d7c1819ed90e2
Biju Das <biju.das.jz@bp.renesas.com>
2024-12-13 17:38:48 +0000

    dt-bindings: pinctrl: renesas: Document RZ/G3E SoC
    Add documentation for the pin controller found on the Renesas RZ/G3E
    (R9A09G047) SoC. The RZ/G3E PFC is similar to the RZ/V2H SoC but has more
    pins(P00-PS3).
    
    Acked-by: Conor Dooley <conor.dooley@microchip.com>
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>

--- include/dt-bindings/pinctrl/rzg2l-pinctrl.h
+++ include/dt-bindings/pinctrl/rzg2l-pinctrl.h
@@ -24,16 +24,35 @@
 #define PORT_P9		9
 #define PORT_PA		10
 #define PORT_PB		11
+#define PORT_PC		12
+#define PORT_PD		13
+#define PORT_PE		14
+#define PORT_PF		15
+#define PORT_PG		16
+#define PORT_PH		17
+#define PORT_PI		18
+#define PORT_PJ		19
+#define PORT_PK		20
+#define PORT_PL		21
+#define PORT_PM		22
+#define PORT_PN		23
+#define PORT_PO		24
+#define PORT_PP		25
+#define PORT_PQ		26
+#define PORT_PR		27
+#define PORT_PS		28
 
 /*
  * Create the pin index from its bank and position numbers and store in
  * the upper 16 bits the alternate function identifier
  */
 #define RZG2L_PORT_PINMUX(b, p, f)	((b) * RZG2L_PINS_PER_PORT + (p) | ((f) << 16))
+#define RZG3E_PORT_PINMUX(b, p, f)	RZG2L_PORT_PINMUX(PORT_P##b, p, f)
 #define RZV2H_PORT_PINMUX(b, p, f)	RZG2L_PORT_PINMUX(PORT_P##b, p, f)
 
 /* Convert a port and pin label to its global pin index */
 #define RZG2L_GPIO(port, pin)	((port) * RZG2L_PINS_PER_PORT + (pin))
+#define RZG3E_GPIO(port, pin)	RZG2L_GPIO(PORT_P##port, pin)
 #define RZV2H_GPIO(port, pin)	RZG2L_GPIO(PORT_P##port, pin)
 
 #endif /* __DT_BINDINGS_RZG2L_PINCTRL_H */
