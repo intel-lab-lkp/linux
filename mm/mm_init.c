6132d2bb074779d75268699b7bbc8c2f40916027
Jiwen Qi <jiwen7.qi@gmail.com>
2024-12-29 18:09:00 +0000

    mm/mm_init: Fix the zone->managed_pages comment to match the code
    The invocation of zone_init_internals(zone, j, nid, zone->present_pages)
    initializes zone->managed_pages to zone->present_pages. zone->present_pages
    is not 0 when the zone is not empty.
    
    Signed-off-by: Jiwen Qi <jiwen7.qi@gmail.com>

--- mm/mm_init.c
+++ mm/mm_init.c
@@ -1567,8 +1567,9 @@ static void __init free_area_init_core(struct pglist_data *pgdat)
 		unsigned long size = zone->spanned_pages;
 
 		/*
-		 * Initialize zone->managed_pages as 0 , it will be reset
-		 * when memblock allocator frees pages into buddy system.
+		 * Initialize zone->managed_pages to zone->present_pages, it
+		 * will be reset when memblock allocator frees pages into
+		 * buddy system.
 		 */
 		zone_init_internals(zone, j, nid, zone->present_pages);
 
