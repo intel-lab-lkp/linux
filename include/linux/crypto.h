71ff37ee140d70c6a6979201333eea808b43ef9a
Harald Freudenberger <freude@linux.ibm.com>
2025-01-02 10:46:15 +0100

    s390/crypto: Enable phmac selftest invocation
    - Add a little helper inline function
        crypto_tfm_alg_get_flags()
      to crypto.h to retrieve the alg flags.
    - Add key preparation code in case of selftest running
      to the phmac setkey function.
    - Add phmac selftest invocation to the crypto testmanager.
    
    Signed-off-by: Harald Freudenberger <freude@linux.ibm.com>

--- include/linux/crypto.h
+++ include/linux/crypto.h
@@ -472,6 +472,11 @@ static inline unsigned int crypto_tfm_alg_alignmask(struct crypto_tfm *tfm)
 	return tfm->__crt_alg->cra_alignmask;
 }
 
+static inline u32 crypto_tfm_alg_get_flags(struct crypto_tfm *tfm)
+{
+	return tfm->__crt_alg->cra_flags;
+}
+
 static inline u32 crypto_tfm_get_flags(struct crypto_tfm *tfm)
 {
 	return tfm->crt_flags;
