1237081151e9b434696a686ad7efa1f45d7dc088
Jiawen Wu <jiawenwu@trustnetic.com>
2025-01-02 18:30:23 +0800

    net: wangxun: Add support for PTP clock
    Implement support for PTP clock on Wangxun NICs.
    
    Signed-off-by: Jiawen Wu <jiawenwu@trustnetic.com>

--- drivers/net/ethernet/wangxun/libwx/wx_lib.h
+++ drivers/net/ethernet/wangxun/libwx/wx_lib.h
@@ -35,5 +35,6 @@ netdev_features_t wx_fix_features(struct net_device *netdev,
 				  netdev_features_t features);
 void wx_set_ring(struct wx *wx, u32 new_tx_count,
 		 u32 new_rx_count, struct wx_ring *temp_ring);
+int wx_ioctl(struct net_device *netdev, struct ifreq *ifr, int cmd);
 
 #endif /* _NGBE_LIB_H_ */
---
+++ drivers/net/ethernet/wangxun/libwx/wx_ptp.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright (c) 2019 - 2025 Beijing WangXun Technology Co., Ltd. */
+
+#ifndef _WX_PTP_H_
+#define _WX_PTP_H_
+
+void wx_ptp_start_cyclecounter(struct wx *wx);
+void wx_ptp_reset(struct wx *wx);
+void wx_ptp_init(struct wx *wx);
+void wx_ptp_suspend(struct wx *wx);
+void wx_ptp_stop(struct wx *wx);
+void wx_ptp_rx_hwtstamp(struct wx *wx, struct sk_buff *skb);
+int wx_ptp_get_ts_config(struct wx *wx, struct ifreq *ifr);
+int wx_ptp_set_ts_config(struct wx *wx, struct ifreq *ifr);
+
+#endif /* _WX_PTP_H_ */
