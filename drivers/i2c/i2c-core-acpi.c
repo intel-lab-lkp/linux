4cdd129c58b2b3957ee87c9a50b813671ccbafb2
Randolph Ha <rha051117@gmail.com>
2025-01-02 23:16:52 -0600

    Force ELAN06FA touchpad I2C bus freq to 100KHz
    Some devices do not define valid bus frequencies for the ELAN06FA
    touchpad in their ACPI table, and some controllers run them at
    400KHz by default. The 06FA touchpad exhibits excessive smoothing
    behaviors when run at 400KHz, so force the bus frequency to 100KHz.
    
    Signed-off-by: Randolph Ha <rha051117@gmail.com>

--- drivers/i2c/i2c-core-acpi.c
+++ drivers/i2c/i2c-core-acpi.c
@@ -355,6 +355,18 @@ static const struct acpi_device_id i2c_acpi_force_400khz_device_ids[] = {
 	{}
 };
 
+static const struct acpi_device_id i2c_acpi_force_100khz_device_ids[] = {
+	/*
+	 * When a 400KHz freq is used on this model of ELAN touchpad instead
+	 * of 100Khz, excessive smoothing (similar to when there is noise in
+	 * the signal) is intermittently applied. As some devices' ACPI
+	 * tables do not specify the 100KHz frequency requirement, it is
+	 * necessary to force the speed to 100KHz.
+	 */
+	{ "ELAN06FA", 0 },
+	{}
+};
+
 static acpi_status i2c_acpi_lookup_speed(acpi_handle handle, u32 level,
 					   void *data, void **return_value)
 {
@@ -372,6 +384,9 @@ static acpi_status i2c_acpi_lookup_speed(acpi_handle handle, u32 level,
 
 	if (acpi_match_device_ids(adev, i2c_acpi_force_400khz_device_ids) == 0)
 		lookup->force_speed = I2C_MAX_FAST_MODE_FREQ;
+
+	if (acpi_match_device_ids(adev, i2c_acpi_force_100khz_device_ids) == 0)
+		lookup->force_speed = I2C_MAX_STANDARD_MODE_FREQ;
 
 	return AE_OK;
 }
