1b3a50a0270056b72432489254c0c4626422de71
Furong Xu <0x1207@gmail.com>
2025-01-06 11:02:25 +0800

    page_pool: check for dma_sync_size earlier
    Setting dma_sync_size to 0 is not illegal, fec_main.c and ravb_main.c
    already did.
    We can save a couple of function calls if check for dma_sync_size earlier.
    
    This is a micro optimization, about 0.6% PPS performance improvement
    has been observed on a single Cortex-A53 CPU core with 64 bytes UDP RX
    traffic test.
    
    Before this patch:
    The average of packets per second is 234026 in one minute.
    
    After this patch:
    The average of packets per second is 235537 in one minute.
    
    Signed-off-by: Furong Xu <0x1207@gmail.com>

--- net/core/page_pool.c
+++ net/core/page_pool.c
@@ -458,7 +458,7 @@ page_pool_dma_sync_for_device(const struct page_pool *pool,
 			      netmem_ref netmem,
 			      u32 dma_sync_size)
 {
-	if (pool->dma_sync && dma_dev_need_sync(pool->p.dev))
+	if (pool->dma_sync && dma_dev_need_sync(pool->p.dev) && dma_sync_size)
 		__page_pool_dma_sync_for_device(pool, netmem, dma_sync_size);
 }
 
