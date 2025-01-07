3b99a86af3b3bc66ccfc9bacf99dfa05e914c7fa
Shiyang Ruan <ruansy.fnst@fujitsu.com>
2025-01-07 18:09:41 +0800

    drivers/base/memory: simplify outputting of valid_zones_show()
    No need to specific position at the first writing to the buf because the
    @len is always 0 at this time.  Use sysfs_emit() instead to simplify it.
    
    Signed-off-by: Shiyang Ruan <ruansy.fnst@fujitsu.com>

--- drivers/base/memory.c
+++ drivers/base/memory.c
@@ -469,19 +469,17 @@ static ssize_t valid_zones_show(struct device *dev,
 		default_zone = mem->zone;
 		if (!default_zone)
 			return sysfs_emit(buf, "%s\n", "none");
-		len += sysfs_emit_at(buf, len, "%s", default_zone->name);
-		goto out;
+		return sysfs_emit(buf, "%s\n", default_zone->name);
 	}
 
 	default_zone = zone_for_pfn_range(MMOP_ONLINE, nid, group,
 					  start_pfn, nr_pages);
 
-	len += sysfs_emit_at(buf, len, "%s", default_zone->name);
+	len += sysfs_emit(buf, "%s", default_zone->name);
 	len += print_allowed_zone(buf, len, nid, group, start_pfn, nr_pages,
 				  MMOP_ONLINE_KERNEL, default_zone);
 	len += print_allowed_zone(buf, len, nid, group, start_pfn, nr_pages,
 				  MMOP_ONLINE_MOVABLE, default_zone);
-out:
 	len += sysfs_emit_at(buf, len, "\n");
 	return len;
 }
