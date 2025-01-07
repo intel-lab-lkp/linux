196e199b084f614ec91e1dc19fe7a19f2fa4e8d1
Christoph Hellwig <hch@lst.de>
2025-01-07 07:30:35 +0100

    block: don't update BLK_FEAT_POLL in __blk_mq_update_nr_hw_queues
    When __blk_mq_update_nr_hw_queues changes the number of tag sets, it
    might have to disable poll queues.  Currently it does so by adjusting
    the BLK_FEAT_POLL, which is a bit against the intent of features that
    describe hardware / driver capabilities, but more importantly causes
    nasty lock order problems with the broadly held freeze when updating the
    number of hardware queues and the limits lock.  Fix this by leaving
    BLK_FEAT_POLL alone, and instead check for the number of poll queues in
    the bio submission and poll handlers.  While this adds extra work to the
    fast path, the variables are in cache lines used by these operations
    anyway, so it should be cheap enough.
    
    Fixes: 8023e144f9d6 ("block: move the poll flag to queue_limits")
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- block/blk-mq.c
+++ block/blk-mq.c
@@ -4320,12 +4320,6 @@ void blk_mq_release(struct request_queue *q)
 	blk_mq_sysfs_deinit(q);
 }
 
-static bool blk_mq_can_poll(struct blk_mq_tag_set *set)
-{
-	return set->nr_maps > HCTX_TYPE_POLL &&
-		set->map[HCTX_TYPE_POLL].nr_queues;
-}
-
 struct request_queue *blk_mq_alloc_queue(struct blk_mq_tag_set *set,
 		struct queue_limits *lim, void *queuedata)
 {
@@ -4336,7 +4330,7 @@ struct request_queue *blk_mq_alloc_queue(struct blk_mq_tag_set *set,
 	if (!lim)
 		lim = &default_lim;
 	lim->features |= BLK_FEAT_IO_STAT | BLK_FEAT_NOWAIT;
-	if (blk_mq_can_poll(set))
+	if (set->nr_maps > HCTX_TYPE_POLL)
 		lim->features |= BLK_FEAT_POLL;
 
 	q = blk_alloc_queue(lim, set->numa_node);
@@ -5024,8 +5018,6 @@ static void __blk_mq_update_nr_hw_queues(struct blk_mq_tag_set *set,
 fallback:
 	blk_mq_update_queue_map(set);
 	list_for_each_entry(q, &set->tag_list, tag_set_list) {
-		struct queue_limits lim;
-
 		blk_mq_realloc_hw_ctxs(set, q);
 
 		if (q->nr_hw_queues != set->nr_hw_queues) {
@@ -5039,13 +5031,6 @@ fallback:
 			set->nr_hw_queues = prev_nr_hw_queues;
 			goto fallback;
 		}
-		lim = queue_limits_start_update(q);
-		if (blk_mq_can_poll(set))
-			lim.features |= BLK_FEAT_POLL;
-		else
-			lim.features &= ~BLK_FEAT_POLL;
-		if (queue_limits_commit_update(q, &lim) < 0)
-			pr_warn("updating the poll flag failed\n");
 		blk_mq_map_swqueue(q);
 	}
 
