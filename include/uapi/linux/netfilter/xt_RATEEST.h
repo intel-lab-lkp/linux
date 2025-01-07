7d315d90ed305e40453242d2181c437154560301
Benjamin Szőke <egyszeregy@freemail.hu>
2025-01-07 03:41:12 +0100

    netfilter: x_tables: Merge xt_RATEEST.h to xt_rateest.h
    Merge xt_RATEEST.h to xt_rateest.h header file.
    
    Signed-off-by: Benjamin Szőke <egyszeregy@freemail.hu>

--- include/uapi/linux/netfilter/xt_RATEEST.h
+++ include/uapi/linux/netfilter/xt_RATEEST.h
@@ -2,16 +2,6 @@
 #ifndef _XT_RATEEST_TARGET_H
 #define _XT_RATEEST_TARGET_H
 
-#include <linux/types.h>
-#include <linux/if.h>
-
-struct xt_rateest_target_info {
-	char			name[IFNAMSIZ];
-	__s8			interval;
-	__u8		ewma_log;
-
-	/* Used internally by the kernel */
-	struct xt_rateest	*est __attribute__((aligned(8)));
-};
+#include <linux/netfilter/xt_rateest.h>
 
 #endif /* _XT_RATEEST_TARGET_H */
