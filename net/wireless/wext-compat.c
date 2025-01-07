605ddcb1f226dcb976af59726d0329e707156fd6
Rameshkumar Sundaram <quic_ramess@quicinc.com>
2025-01-07 23:55:05 +0530

    wifi: cfg80211: set tx power per radio in a wiphy
    A wiphy can have multiple radios(wiphy.n_radio) combined in it
    and each radio is identified by radio index (radio_id).
    When set tx power is done without a valid wdev then it is
    a radio level configuration but currently it is being considered at
    wiphy level since radio identifier is not present in nl80211_set_wiphy().
    
    Pass the radio id that is obtained via NL80211_ATTR_WIPHY_RADIO_INDEX as an
    argument to set_tx_power op to indicate the underlying drivers about the
    radio of the wiphy to which the configuration should be applied.
    
    When NL80211_ATTR_WIPHY_RADIO_INDEX is not passed or the wiphy is a
    non-multi radio wiphy, radio id value of NL80211_WIPHY_RADIO_ID_MAX(255)
    is passed in which case drivers should apply the configuration to all
    radios.
    
    Signed-off-by: Rameshkumar Sundaram <quic_ramess@quicinc.com>

--- net/wireless/wext-compat.c
+++ net/wireless/wext-compat.c
@@ -892,7 +892,8 @@ static int cfg80211_wext_siwtxpower(struct net_device *dev,
 
 	guard(wiphy)(&rdev->wiphy);
 
-	return rdev_set_tx_power(rdev, wdev, type, DBM_TO_MBM(dbm));
+	return rdev_set_tx_power(rdev, wdev, NL80211_WIPHY_RADIO_ID_MAX, type,
+				 DBM_TO_MBM(dbm));
 }
 
 static int cfg80211_wext_giwtxpower(struct net_device *dev,
