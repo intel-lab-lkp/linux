325817f3bcc81992aaa50eebfd3ad2c1a2ca1b80
Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
2025-01-08 18:35:14 +0000

    drm/sched: Remove weak paused submission checks
    There is no need to check the boolean in the work item's prologues since
    the boolean can be set at any later time anyway.
    
    The helper which pauses submission sets it and synchronously cancels the
    work and helpers which queue the work check for the flag so all should be
    good.
    
    Signed-off-by: Tvrtko Ursulin <tvrtko.ursulin@igalia.com>
    Cc: Christian König <christian.koenig@amd.com>
    Cc: Danilo Krummrich <dakr@redhat.com>
    Cc: Matthew Brost <matthew.brost@intel.com>
    Cc: Philipp Stanner <pstanner@redhat.com>

--- drivers/gpu/drm/scheduler/sched_main.c
+++ drivers/gpu/drm/scheduler/sched_main.c
@@ -1153,9 +1153,6 @@ static void drm_sched_free_job_work(struct work_struct *w)
 		container_of(w, struct drm_gpu_scheduler, work_free_job);
 	struct drm_sched_job *job;
 
-	if (READ_ONCE(sched->pause_submit))
-		return;
-
 	job = drm_sched_get_finished_job(sched);
 	if (job)
 		sched->ops->free_job(job);
@@ -1177,9 +1174,6 @@ static void drm_sched_run_job_work(struct work_struct *w)
 	struct dma_fence *fence;
 	struct drm_sched_job *sched_job;
 	int r;
-
-	if (READ_ONCE(sched->pause_submit))
-		return;
 
 	/* Find entity with a ready job */
 	entity = drm_sched_select_entity(sched);
