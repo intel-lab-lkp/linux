cbde9907750227ff478004bee5425de6b77b9a9f
Akhil P Oommen <quic_akhilpo@quicinc.com>
2025-01-09 02:09:58 +0530

    drm/msm/a6xx: Increase HFI response timeout
    When ACD feature is enabled, it triggers some internal calibrations
    which result in a pretty long delay during the first HFI perf vote.
    So, increase the HFI response timeout to match the downstream driver.
    
    Signed-off-by: Akhil P Oommen <quic_akhilpo@quicinc.com>

--- drivers/gpu/drm/msm/adreno/a6xx_hfi.c
+++ drivers/gpu/drm/msm/adreno/a6xx_hfi.c
@@ -108,7 +108,7 @@ static int a6xx_hfi_wait_for_ack(struct a6xx_gmu *gmu, u32 id, u32 seqnum,
 
 	/* Wait for a response */
 	ret = gmu_poll_timeout(gmu, REG_A6XX_GMU_GMU2HOST_INTR_INFO, val,
-		val & A6XX_GMU_GMU2HOST_INTR_INFO_MSGQ, 100, 5000);
+		val & A6XX_GMU_GMU2HOST_INTR_INFO_MSGQ, 100, 1000000);
 
 	if (ret) {
 		DRM_DEV_ERROR(gmu->dev,
@@ -725,7 +725,7 @@ static int a6xx_hfi_enable_acd(struct a6xx_gmu *gmu)
 	}
 
 	/* Send ACD table to GMU */
-	ret = a6xx_hfi_send_msg(gmu, HFI_H2F_MSG_ACD, &msg, sizeof(msg), NULL, 0);
+	ret = a6xx_hfi_send_msg(gmu, HFI_H2F_MSG_ACD, acd_table, sizeof(*acd_table), NULL, 0);
 	if (ret) {
 		DRM_DEV_ERROR(gmu->dev, "Unable to ACD table (%d)\n", ret);
 		return ret;
