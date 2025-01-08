7e8c76cc91e3b56df796d940986d150c7d4e8c6e
Kevin Brodsky <kevin.brodsky@arm.com>
2025-01-08 10:32:44 +0000

    mm: Introduce kpkeys_hardened_pgtables
    kpkeys_hardened_pgtables is a hardening feature based on kpkeys. It
    aims to prevent the corruption of page tables by: 1. mapping all
    page table pages, both kernel and user, with a privileged pkey
    (KPKEYS_PKEY_PGTABLES), and 2. granting write access to that pkey
    only when running at a higher kpkeys level (KPKEYS_LVL_PGTABLES).
    
    The feature is exposed as CONFIG_KPKEYS_HARDENED_PGTABLES; it
    requires explicit architecture opt-in by selecting
    ARCH_HAS_KPKEYS_HARDENED_PGTABLES, since much of the page table
    handling is arch-specific.
    
    This patch introduces an API to modify the PTPs' pkey and switch
    kpkeys level using a guard object. Because this API is going to be
    called from low-level pgtable helpers (setters, allocators), it must
    be inactive on boot and explicitly switched on if and when kpkeys
    become available. A static key is used for that purpose; it is the
    responsibility of each architecture supporting
    kpkeys_hardened_pgtables to call kpkeys_hardened_pgtables_enable()
    as early as possible to switch on that static key. The initial
    kernel page tables are also walked to set their pkey, since they
    have already been allocated at that point.
    
    The definition of the kpkeys_hardened_pgtables guard class does not
    use the static key on the restore path to avoid mismatched
    set/restore pairs. Indeed, enabling the static key itself involves
    modifying page tables, and it is thus possible that the guard object
    is created when the static key appears as false, and destroyed when it
    appears as true. To avoid this situation, we reserve an invalid value
    for the pkey register and use it to disable the restore path.
    
    Signed-off-by: Kevin Brodsky <kevin.brodsky@arm.com>

--- include/linux/kpkeys.h
+++ include/linux/kpkeys.h
@@ -4,11 +4,13 @@
 
 #include <linux/bug.h>
 #include <linux/cleanup.h>
+#include <linux/jump_label.h>
 
 #define KPKEYS_LVL_DEFAULT	0
+#define KPKEYS_LVL_PGTABLES	1
 
 #define KPKEYS_LVL_MIN		KPKEYS_LVL_DEFAULT
-#define KPKEYS_LVL_MAX		KPKEYS_LVL_DEFAULT
+#define KPKEYS_LVL_MAX		KPKEYS_LVL_PGTABLES
 
 #define KPKEYS_GUARD(_name, set_level, restore_pkey_reg)		\
 	__DEFINE_CLASS_IS_CONDITIONAL(_name, false);			\
@@ -62,5 +64,49 @@ static inline bool arch_kpkeys_enabled(void)
 }
 
 #endif /* CONFIG_ARCH_HAS_KPKEYS */
+
+#ifdef CONFIG_KPKEYS_HARDENED_PGTABLES
+
+DECLARE_STATIC_KEY_FALSE(kpkeys_hardened_pgtables_enabled);
+
+/*
+ * Use guard(kpkeys_hardened_pgtables)() to temporarily grant write access
+ * to page tables.
+ */
+KPKEYS_GUARD(kpkeys_hardened_pgtables,
+	     static_branch_unlikely(&kpkeys_hardened_pgtables_enabled) ?
+		     kpkeys_set_level(KPKEYS_LVL_PGTABLES) :
+		     KPKEYS_PKEY_REG_INVAL,
+	     _T != KPKEYS_PKEY_REG_INVAL ?
+		     kpkeys_restore_pkey_reg(_T) :
+		     (void)0)
+
+int kpkeys_protect_pgtable_memory(struct folio *folio);
+int kpkeys_unprotect_pgtable_memory(struct folio *folio);
+
+/*
+ * Enables kpkeys_hardened_pgtables and switches existing kernel page tables to
+ * a privileged pkey (KPKEYS_PKEY_PGTABLES).
+ *
+ * Should be called as early as possible by architecture code, after (k)pkeys
+ * are initialised and before any user task is spawned.
+ */
+void kpkeys_hardened_pgtables_enable(void);
+
+#else /* CONFIG_KPKEYS_HARDENED_PGTABLES */
+
+KPKEYS_GUARD(kpkeys_hardened_pgtables, 0, (void)_T)
+
+static inline int kpkeys_protect_pgtable_memory(struct folio *folio)
+{
+	return 0;
+}
+static inline int kpkeys_unprotect_pgtable_memory(struct folio *folio)
+{
+	return 0;
+}
+static inline void kpkeys_hardened_pgtables_enable(void) {}
+
+#endif /* CONFIG_KPKEYS_HARDENED_PGTABLES */
 
 #endif /* _LINUX_KPKEYS_H */
