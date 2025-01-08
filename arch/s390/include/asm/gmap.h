e7e56d96df8317a7ff7f929bfd03c6378c5c6e38
Claudio Imbrenda <imbrenda@linux.ibm.com>
2025-01-08 19:14:48 +0100

    KVM: s390: move gmap_shadow_pgt_lookup() into kvm
    Move gmap_shadow_pgt_lookup() from mm/gmap.c into kvm/gaccess.c .
    
    Signed-off-by: Claudio Imbrenda <imbrenda@linux.ibm.com>

--- arch/s390/include/asm/gmap.h
+++ arch/s390/include/asm/gmap.h
@@ -129,8 +129,6 @@ int gmap_shadow_sgt(struct gmap *sg, unsigned long saddr, unsigned long sgt,
 		    int fake);
 int gmap_shadow_pgt(struct gmap *sg, unsigned long saddr, unsigned long pgt,
 		    int fake);
-int gmap_shadow_pgt_lookup(struct gmap *sg, unsigned long saddr,
-			   unsigned long *pgt, int *dat_protection, int *fake);
 int gmap_shadow_page(struct gmap *sg, unsigned long saddr, pte_t pte);
 
 void gmap_register_pte_notifier(struct gmap_notifier *);
@@ -145,6 +143,7 @@ int s390_replace_asce(struct gmap *gmap);
 void s390_uv_destroy_pfns(unsigned long count, unsigned long *pfns);
 int __s390_uv_destroy_range(struct mm_struct *mm, unsigned long start,
 			    unsigned long end, bool interruptible);
+unsigned long *gmap_table_walk(struct gmap *gmap, unsigned long gaddr, int level);
 
 /**
  * s390_uv_destroy_range - Destroy a range of pages in the given mm.
