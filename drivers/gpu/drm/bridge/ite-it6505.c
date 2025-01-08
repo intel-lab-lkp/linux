8d20100bc5ad48e0513653f8bebda15017e35717
Hermes Wu <Hermes.wu@ite.com.tw>
2025-01-08 13:45:14 +0800

    drm/bridge: it6505: fix HDCP V match check is not performed correctly
    The loop of V compare is expected to iterate for 5 times
    which compare V array form av[0][] to av[4][].
    It should check loop counter reach the last statement before return true
    
    Fixes: 0989c02c7a5c ("drm/bridge: it6505: fix HDCP CTS compare V matching")
    
    Signed-off-by: Hermes Wu <Hermes.wu@ite.com.tw>

--- drivers/gpu/drm/bridge/ite-it6505.c
+++ drivers/gpu/drm/bridge/ite-it6505.c
@@ -2254,9 +2254,12 @@ static bool it6505_hdcp_part2_ksvlist_check(struct it6505 *it6505)
 			if (bv[i][3] != av[i][0] || bv[i][2] != av[i][1] ||
 			    av[i][1] != av[i][2] || bv[i][0] != av[i][3])
 				break;
-
-			DRM_DEV_DEBUG_DRIVER(dev, "V' all match!! %d, %d", retry, i);
-			return true;
+			if (i == 4) {
+				DRM_DEV_DEBUG_DRIVER(dev,
+						     "V' all match!! %d",
+						     retry);
+				return true;
+			}
 		}
 	}
 
