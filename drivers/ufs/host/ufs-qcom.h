a31a0e5ade924011376398a7efe89d3c335cbf7e
Ram Kumar Dwivedi <quic_rdwivedi@quicinc.com>
2025-01-09 12:33:52 +0530

    scsi: ufs: qcom: Enable UFS Shared ICE Feature
    By default, the UFS controller allocates a fixed number of RX
    and TX engines statically. Consequently, when UFS reads are in
    progress, the TX ICE engines remain idle, and vice versa.
    This leads to inefficient utilization of RX and TX engines.
    
    To address this limitation, enable the UFS shared ICE feature for
    Qualcomm UFS V5.0 and above. This feature utilizes a pool of crypto
    cores for both TX streams (UFS Write – Encryption) and RX streams
    (UFS Read – Decryption). With this approach, crypto cores are
    dynamically allocated to either the RX or TX stream as needed.
    
    Co-developed-by: Naveen Kumar Goud Arepalli <quic_narepall@quicinc.com>
    Signed-off-by: Naveen Kumar Goud Arepalli <quic_narepall@quicinc.com>
    Co-developed-by: Nitin Rawat <quic_nitirawa@quicinc.com>
    Signed-off-by: Nitin Rawat <quic_nitirawa@quicinc.com>
    Signed-off-by: Ram Kumar Dwivedi <quic_rdwivedi@quicinc.com>

--- drivers/ufs/host/ufs-qcom.h
+++ drivers/ufs/host/ufs-qcom.h
@@ -76,6 +76,12 @@ enum {
 	UFS_MEM_CQIS_VS		= 0x8,
 };
 
+/* QCOM UFS host controller Shared ICE registers */
+enum {
+	REG_UFS_MEM_ICE_CONFIG			= 0x260C,
+	REG_UFS_MEM_ICE_NUM_CORE		= 0x2664,
+};
+
 #define UFS_CNTLR_2_x_x_VEN_REGS_OFFSET(x)	(0x000 + x)
 #define UFS_CNTLR_3_x_x_VEN_REGS_OFFSET(x)	(0x400 + x)
 
@@ -110,6 +116,9 @@ enum {
 /* bit definition for UFS_UFS_TEST_BUS_CTRL_n */
 #define TEST_BUS_SUB_SEL_MASK	GENMASK(4, 0)  /* All XXX_SEL fields are 5 bits wide */
 
+/* bit definition for UFS Shared ICE config */
+#define UFS_QCOM_CAP_ICE_CONFIG BIT(0)
+
 #define REG_UFS_CFG2_CGC_EN_ALL (UAWM_HW_CGC_EN | UARM_HW_CGC_EN |\
 				 TXUC_HW_CGC_EN | RXUC_HW_CGC_EN |\
 				 DFC_HW_CGC_EN | TRLUT_HW_CGC_EN |\
@@ -134,6 +143,37 @@ enum {
 #define UNIPRO_CORE_CLK_FREQ_300_MHZ           300
 #define UNIPRO_CORE_CLK_FREQ_201_5_MHZ         202
 #define UNIPRO_CORE_CLK_FREQ_403_MHZ           403
+
+/* ICE allocator type to share AES engines among TX stream and RX stream */
+#define ICE_ALLOCATOR_TYPE 2
+
+/*
+ * Number of cores allocated for RX stream when Read data block received and
+ * Write data block is not in progress
+ */
+#define NUM_RX_R1W0 28
+
+/*
+ * Number of cores allocated for TX stream when Device asked to send write
+ * data block and Read data block is not in progress
+ */
+#define NUM_TX_R0W1 28
+
+/*
+ * Number of cores allocated for RX stream when Read data block received and
+ * Write data block is in progress
+ * OR
+ * Device asked to send write data block and Read data block is in progress
+ */
+#define NUM_RX_R1W1 15
+
+/*
+ * Number of cores allocated for TX stream (UFS write) when Read data block
+ * received and Write data block is in progress
+ * OR
+ * Device asked to send write data block and Read data block is in progress
+ */
+#define NUM_TX_R1W1 13
 
 static inline void
 ufs_qcom_get_controller_revision(struct ufs_hba *hba,
@@ -196,7 +236,7 @@ struct ufs_qcom_host {
 #ifdef CONFIG_SCSI_UFS_CRYPTO
 	struct qcom_ice *ice;
 #endif
-
+	u32 caps;
 	void __iomem *dev_ref_clk_ctrl_mmio;
 	bool is_dev_ref_clk_enabled;
 	struct ufs_hw_version hw_ver;
