2642006d33d3f73c4453d9f44ce86932bb88f938
Andras Szemzo <szemzo.andras@gmail.com>
2025-01-10 13:39:16 +0100

    clk: sunxi-ng: add CCU drivers for V853
    V853 has similar gates/resets like existing Allwinner SOCs. Add support for it's CCU and PRCM CCU.
    
    Signed-off-by: Andras Szemzo <szemzo.andras@gmail.com>

---
+++ drivers/clk/sunxi-ng/ccu-sun8i-v853.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright (C) 2024 Andras Szemzo <szemzo.andras@gmail.com>
+ */
+
+#ifndef _CCU_SUN8I_V853_H_
+#define _CCU_SUN8I_V853_H_
+
+#include <dt-bindings/clock/sun8i-v853-ccu.h>
+#include <dt-bindings/reset/sun8i-v853-ccu.h>
+
+#define CLK_NUMBER		(CLK_FANOUT2 + 1)
+
+#endif /* _CCU_SUN8I_V853_H_ */
