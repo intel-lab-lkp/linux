540ed2675bff22772783c2feae648d69efa8959d
Nancy.Lin <nancy.lin@mediatek.com>
2025-01-10 20:34:05 +0800

    drm/mediatek: add BLENDER support for MT8196
    BLENDER executes the alpha blending function for overlapping
    layers from different sources, which is the primary function
    of the overlapping system.
    
    Signed-off-by: Nancy.Lin <nancy.lin@mediatek.com>
    Signed-off-by: Paul-pl.Chen <paul-pl.chen@mediatek.com>

---
+++ drivers/gpu/drm/mediatek/mtk_disp_blender.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2021 MediaTek Inc.
+ */
+
+#ifndef __MTK_DISP_BLENDER_H__
+#define __MTK_DISP_BLENDER_H__
+
+enum mtk_disp_blender_layer {
+	FIRST_BLENDER,
+	LAST_BLENDER,
+	SINGLE_BLENDER,
+	OTHERS,
+	BLENDER_LAYER_MAX,
+};
+
+#endif // __MTK_DISP_BLENDER_H__
