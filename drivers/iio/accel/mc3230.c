a0cc4164fc7177c80d8eb3554a23478c7f137f3e
Vasiliy Doylov <nekodevelopper@gmail.com>
2025-01-11 23:11:07 +0300

    iio: accel: mc3230: add OF match table
    This will make the driver probe-able via device-tree.
    While the I2C match table may be sufficient, this should extend the cover
    of this driver being probed by other methods.
    
    Signed-off-by: Vasiliy Doylov <nekodevelopper@gmail.com>

--- drivers/iio/accel/mc3230.c
+++ drivers/iio/accel/mc3230.c
@@ -205,10 +205,17 @@ static const struct i2c_device_id mc3230_i2c_id[] = {
 };
 MODULE_DEVICE_TABLE(i2c, mc3230_i2c_id);
 
+static const struct of_device_id mc3230_of_match[] = {
+	{ .compatible = "mcube,mc3230" },
+	{ },
+};
+MODULE_DEVICE_TABLE(of, mc3230_of_match);
+
 static struct i2c_driver mc3230_driver = {
 	.driver = {
 		.name = "mc3230",
 		.pm = pm_sleep_ptr(&mc3230_pm_ops),
+		.of_match_table = mc3230_of_match,
 	},
 	.probe		= mc3230_probe,
 	.remove		= mc3230_remove,
