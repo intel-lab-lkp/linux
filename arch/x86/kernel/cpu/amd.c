b723a71bbf08fa50c15d7764232e2f653f8039e1
Rik van Riel <riel@surriel.com>
2025-01-12 10:53:55 -0500

    x86/mm: enable AMD translation cache extensions
    With AMD TCE (translation cache extensions) only the intermediate mappings
    that cover the address range zapped by INVLPG / INVLPGB get invalidated,
    rather than all intermediate mappings getting zapped at every TLB invalidation.
    
    This can help reduce the TLB miss rate, by keeping more intermediate
    mappings in the cache.
    
    From the AMD manual:
    
    Translation Cache Extension (TCE) Bit. Bit 15, read/write. Setting this bit
    to 1 changes how the INVLPG, INVLPGB, and INVPCID instructions operate on
    TLB entries. When this bit is 0, these instructions remove the target PTE
    from the TLB as well as all upper-level table entries that are cached
    in the TLB, whether or not they are associated with the target PTE.
    When this bit is set, these instructions will remove the target PTE and
    only those upper-level entries that lead to the target PTE in
    the page table hierarchy, leaving unrelated upper-level entries intact.
    
    Signed-off-by: Rik van Riel <riel@surriel.com>

--- arch/x86/kernel/cpu/amd.c
+++ arch/x86/kernel/cpu/amd.c
@@ -1071,6 +1071,9 @@ static void init_amd(struct cpuinfo_x86 *c)
 
 	/* AMD CPUs don't need fencing after x2APIC/TSC_DEADLINE MSR writes. */
 	clear_cpu_cap(c, X86_FEATURE_APIC_MSRS_FENCE);
+
+	if (cpu_feature_enabled(X86_FEATURE_INVLPGB))
+		msr_set_bit(MSR_EFER, _EFER_TCE);
 }
 
 #ifdef CONFIG_X86_32
