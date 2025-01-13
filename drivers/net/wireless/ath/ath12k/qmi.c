bd2c0ba90eab6725e5a56d24b4510923b3cb661a
Lingbo Kong <quic_lingbok@quicinc.com>
2025-01-13 15:48:09 +0800

    wifi: ath12k: Adjust the timing to access ACPI table
    Currently, the timing for accessing the ACPI table is inappropriate.
    
    Due to special ACPI requirements, the ACPI table must be obtained before
    downloading the board data file. Therefore, adjust the timing for accessing
    the ACPI table accordingly.
    
    This patch will not affect QCN9274, because only WCN7850 supports ACPI.
    
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0.c5-00481-QCAHMTSWPL_V1.0_V2.0_SILICONZ-3
    
    Signed-off-by: Lingbo Kong <quic_lingbok@quicinc.com>
    Acked-by: Jeff Johnson <quic_jjohnson@quicinc.com>

--- drivers/net/wireless/ath/ath12k/qmi.c
+++ drivers/net/wireless/ath/ath12k/qmi.c
@@ -2740,6 +2740,11 @@ int ath12k_qmi_request_target_cap(struct ath12k_base *ab)
 	if (r)
 		ath12k_dbg(ab, ATH12K_DBG_QMI, "SMBIOS bdf variant name not set.\n");
 
+	r = ath12k_acpi_start(ab);
+	if (r)
+		/* ACPI is optional so continue in case of an error */
+		ath12k_dbg(ab, ATH12K_DBG_BOOT, "acpi failed: %d\n", r);
+
 out:
 	return ret;
 }
