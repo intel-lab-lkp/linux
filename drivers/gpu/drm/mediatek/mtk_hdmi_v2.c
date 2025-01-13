dc8425a7b9549231e14de09415279c0425768ac3
AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
2025-01-13 15:52:31 +0100

    drm/mediatek: mtk_hdmi_common: Add var to enable interlaced modes
    Add an interlace_allowed bool member to struct mtk_hdmi_ver_conf
    which will be used to signal whether interlaced modes are supported
    by the bridge (in our case, the HDMI IP), and enable it for HDMIv2.
    
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>

--- drivers/gpu/drm/mediatek/mtk_hdmi_v2.c
+++ drivers/gpu/drm/mediatek/mtk_hdmi_v2.c
@@ -1292,6 +1292,7 @@ static const struct mtk_hdmi_ver_conf mtk_hdmi_conf_v2 = {
 	.codec_ops = &mtk_hdmi_v2_audio_codec_ops,
 	.mtk_hdmi_clock_names = mtk_hdmi_v2_clk_names,
 	.num_clocks = MTK_HDMI_V2_CLK_COUNT,
+	.interlace_allowed = true,
 };
 
 static const struct mtk_hdmi_conf mtk_hdmi_conf_mt8188 = {
