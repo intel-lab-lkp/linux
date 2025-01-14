fec7cd6715d79efbf37df4df94eab3ee3b87c17d
Adrián Larumbe <adrian.larumbe@collabora.com>
2025-01-14 17:34:02 +0000

    drm/panthor: Expose size of driver internal BO's over fdinfo
    This will display the sizes of kenrel BO's bound to an open file, which are
    otherwise not exposed to UM through a handle.
    
    The sizes recorded are as follows:
     - Per group: suspend buffer, protm-suspend buffer, syncobjcs
     - Per queue: ringbuffer, profiling slots, firmware interface
     - For all heaps in all heap pools across all VM's bound to an open file,
     record size of all heap chuks, and for each pool the gpu_context BO too.
    
    This does not record the size of FW regions, as these aren't bound to a
    specific open file and remain active through the whole life of the driver.
    
    Reviewed-by: Liviu Dudau <liviu.dudau@arm.com>
    Reviewed-by: Mihail Atanassov <mihail.atanassov@arm.com>
    Reviewed-by: Steven Price <steven.price@arm.com>
    Signed-off-by: Adrián Larumbe <adrian.larumbe@collabora.com>

--- drivers/gpu/drm/panthor/panthor_mmu.c
+++ drivers/gpu/drm/panthor/panthor_mmu.c
@@ -1941,6 +1941,40 @@ struct panthor_heap_pool *panthor_vm_get_heap_pool(struct panthor_vm *vm, bool c
 	return pool;
 }
 
+/**
+ * panthor_vm_heaps_sizes() - Calculate size of all heap chunks across all
+ * heaps over all the heap pools in a VM
+ * @pfile: File.
+ * @stats: Memory stats to be updated.
+ *
+ * Calculate all heap chunk sizes in all heap pools bound to a VM. If the VM
+ * is active, record the size as active as well.
+ */
+void panthor_vm_heaps_sizes(struct panthor_file *pfile, struct drm_memory_stats *stats)
+{
+	struct panthor_vm *vm;
+	unsigned long i;
+
+	if (!pfile->vms)
+		return;
+
+	xa_for_each(&pfile->vms->xa, i, vm) {
+		size_t size;
+
+		mutex_lock(&vm->heaps.lock);
+		if (!vm->heaps.pool) {
+			mutex_unlock(&vm->heaps.lock);
+			continue;
+		}
+		size = panthor_heap_pool_size(vm->heaps.pool);
+		mutex_unlock(&vm->heaps.lock);
+
+		stats->resident += size;
+		if (vm->as.id >= 0)
+			stats->active += size;
+	}
+}
+
 static u64 mair_to_memattr(u64 mair, bool coherent)
 {
 	u64 memattr = 0;
