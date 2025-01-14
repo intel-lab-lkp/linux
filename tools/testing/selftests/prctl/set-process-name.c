bc92434b5f26e0c4492ec84530fd855c5e487516
liuye <liuye@kylinos.cn>
2025-01-14 16:04:10 +0800

    selftests/prctl/set-process-name: Fix Resource leak fptr in check_name
        Exception branch returns without closing fptr.
    Signed-off-by: liuye <liuye@kylinos.cn>

--- tools/testing/selftests/prctl/set-process-name.c
+++ tools/testing/selftests/prctl/set-process-name.c
@@ -66,14 +66,18 @@ int check_name(void)
 		return -EIO;
 
 	fscanf(fptr, "%s", output);
-	if (ferror(fptr))
+	if (ferror(fptr)) {
+		fclose(fptr);
 		return -EIO;
+	}
 
 	int res = prctl(PR_GET_NAME, name, NULL, NULL, NULL);
 
-	if (res < 0)
+	if (res < 0) {
+		fclose(fptr);
 		return -errno;
-
+	}
+	fclose(fptr);
 	return !strcmp(output, name);
 }
 
