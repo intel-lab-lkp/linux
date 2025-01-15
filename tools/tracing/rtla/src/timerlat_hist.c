6839a80324f70e3508d73458964a81bc06d7645e
Costa Shulyupin <costa.shul@redhat.com>
2025-01-15 19:58:30 +0200

    tools/rtla: Add osnoise_trace_is_off()
    The usage of trace_is_off() confusing, which requires a detailed
    explanation.
    
    Let's modify the source code by moving the first
    member, `trace`, of the `osnoise_tool` structure to the second position:
    
     struct osnoise_tool {
    -       struct trace_instance           trace;
            struct osnoise_context          *context;
    +       struct trace_instance           trace;
    
    A correct program would work properly after this change,
    but this one does not.
    
    Then, run the program under gdb to observe the behavior.
    
    gdb -q --args ./rtla osnoise -D -d 2 -T 10000 -q
    
    Program received signal SIGSEGV, Segmentation fault.
    0x000000000040298f in trace_is_off (tool=tool@entry=0x418458, trace=trace@entry=0x8) at src/trace.c:538
    538             if (trace && !tracefs_trace_is_on(trace->inst))
    ...
    
    The program checks if trace, which has a value of 8, is not null,
    and then crashes when it attempts to dereference trace->inst.
    
    It happens because trace_is_off() is called as:
    	trace_is_off(&tool->trace, &record->trace)
    
    Where `record` is NULL. Expression `&record->trace` returns offset of
    member `trace`, which is 8. The original code accidentally works because
    offset of `record->trace` is zero.
    
    Expanded wrong instructions are:
    	record = NULL;
    	if (&record->trace && !tracefs_trace_is_on(record->trace.inst))
    		return 1;
    
    The correct instructions are:
    	record = NULL;
    	if (record && !tracefs_trace_is_on(record->trace.inst))
    		return 1;
    
    Refactor `trace_is_off` to `osnoise_trace_is_off` and move it to
    osnoise.c because it instead of `struct trace_instance` accesses `struct
    osnoise_tool`, which is out of the scope of trace.c.
    
    Signed-off-by: Costa Shulyupin <costa.shul@redhat.com>

--- tools/tracing/rtla/src/timerlat_hist.c
+++ tools/tracing/rtla/src/timerlat_hist.c
@@ -1342,7 +1342,7 @@ int timerlat_hist_main(int argc, char *argv[])
 			goto out_hist;
 		}
 
-		if (trace_is_off(&tool->trace, &record->trace))
+		if (osnoise_trace_is_off(tool, record))
 			break;
 
 		/* is there still any user-threads ? */
@@ -1363,7 +1363,7 @@ int timerlat_hist_main(int argc, char *argv[])
 
 	return_value = 0;
 
-	if (trace_is_off(&tool->trace, &record->trace)) {
+	if (osnoise_trace_is_off(tool, record)) {
 		printf("rtla timerlat hit stop tracing\n");
 
 		if (!params->no_aa)
