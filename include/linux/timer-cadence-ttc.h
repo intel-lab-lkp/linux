6aafd783dd938f5aecd833a2b99cbf455509d916
Mubin Sayyed <mubin.sayyed@amd.com>
2025-01-15 17:05:56 +0530

    pwm: pwm-cadence: Add support for TTC PWM
    Cadence TTC timer can be configured as clocksource/clockevent or PWM
    device.Specific TTC device would be configured as PWM device, if
    pwm-cells property is present in the device tree node.
    
    In case of Zynq, ZynqMP and Versal SoC's, each TTC device has 3
    timers/counters, so maximum 3 PWM channels can be configured for each TTC
    IP instance. Also, output of 0th PWM channel of each TTC device can be
    routed to MIO or EMIO, and output of 2nd and 3rd PWM channel can be
    routed only to EMIO.
    
    Period for given PWM channel is configured through interval timer and
    duty cycle through match counter.
    
    Details for cadence TTC IP can be found in Zynq UltraScale+ TRM.
    
    Signed-off-by: Mubin Sayyed <mubin.sayyed@amd.com>

--- include/linux/timer-cadence-ttc.h
+++ include/linux/timer-cadence-ttc.h
@@ -12,13 +12,14 @@
 #define TTC_CNT_CNTRL_OFFSET            0x0C /* Counter Control Reg, RW */
 #define TTC_COUNT_VAL_OFFSET            0x18 /* Counter Value Reg, RO */
 #define TTC_INTR_VAL_OFFSET             0x24 /* Interval Count Reg, RW */
+#define TTC_MATCH_CNT_VAL_OFFSET        0x30 /* Match Count Reg, RW */
 #define TTC_ISR_OFFSET          0x54 /* Interrupt Status Reg, RO */
 #define TTC_IER_OFFSET          0x60 /* Interrupt Enable Reg, RW */
 
 #define TTC_CNT_CNTRL_DISABLE_MASK      0x1
 
 #define TTC_CLK_CNTRL_CSRC_MASK         (1 << 5)        /* clock source */
-#define TTC_CLK_CNTRL_PSV_MASK          0x1e
+#define TTC_CLK_CNTRL_PSV_MASK		0x1e
 #define TTC_CLK_CNTRL_PSV_SHIFT         1
 
 /*
@@ -32,4 +33,23 @@
 #define CNT_CNTRL_RESET         (1 << 4)
 
 #define MAX_F_ERR 50
+
+#define TTC_PWM_CHANNEL         0x4
+
+#define TTC_CLK_CNTRL_CSRC              BIT(5)
+#define TTC_CLK_CNTRL_PS_EN             BIT(0)
+#define TTC_CNTR_CTRL_DIS               BIT(0)
+#define TTC_CNTR_CTRL_INTR_MODE_EN      BIT(1)
+#define TTC_CNTR_CTRL_MATCH_MODE_EN     BIT(3)
+#define TTC_CNTR_CTRL_RST               BIT(4)
+#define TTC_CNTR_CTRL_WAVE_EN   BIT(5)
+#define TTC_CNTR_CTRL_WAVE_POL  BIT(6)
+#define TTC_CNTR_CTRL_WAVE_POL_SHIFT    6
+#define TTC_CNTR_CTRL_PRESCALE_SHIFT    1
+#define TTC_PWM_MAX_CH	3
+
+#if defined(CONFIG_PWM_CADENCE)
+int ttc_pwm_probe(struct platform_device *pdev);
+void ttc_pwm_remove(struct platform_device *pdev);
+#endif
 
