3ffe913d2a224ba750304ecdbbbcd19a6e938666
Antoine Tenart <atenart@kernel.org>
2025-01-17 11:26:09 +0100

    net-sysfs: move queue attribute groups outside the default groups
    Rx/tx queues embed their own kobject for registering their per-queue
    sysfs files. The issue is they're using the kobject default groups for
    this and entirely rely on the kobject refcounting for releasing their
    sysfs paths.
    
    In order to remove rtnl_trylock calls we need sysfs files not to rely on
    their associated kobject refcounting for their release. Thus we here
    move queues sysfs files from the kobject default groups to their own
    groups which can be removed separately.
    
    Signed-off-by: Antoine Tenart <atenart@kernel.org>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -657,6 +657,7 @@ struct netdev_queue {
 	struct Qdisc __rcu	*qdisc_sleeping;
 #ifdef CONFIG_SYSFS
 	struct kobject		kobj;
+	const struct attribute_group	**groups;
 #endif
 	unsigned long		tx_maxrate;
 	/*
