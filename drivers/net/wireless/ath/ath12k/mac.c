74a3496c76cd2c67bbf66b5e46d624f840f730ad
Aditya Kumar Singh <quic_adisi@quicinc.com>
2025-01-20 15:18:27 +0530

    wifi: ath12k: fix ATH12K_FLAG_REGISTERED flag handling
    Commit a5686ae820fa ("wifi: ath12k: move ATH12K_FLAG_REGISTERED handling to
    ath12k_mac_register()") relocated the setting of the ATH12K_FLAG_REGISTERED
    flag to the ath12k_mac_register() function. However, this function only
    accesses the first device (ab) via ag->ab[0], resulting in the flag being
    set only for the first device in the group. Similarly,
    ath12k_mac_unregister() only unsets the flag for the first device. The flag
    should actually be set for all devices in the group to avoid issues during
    recovery.
    
    Hence, move setting and clearing of this flag in the function
    ath12k_core_hw_group_start() and ath12k_core_hw_group_stop() respectively.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.3.1-00173-QCAHKSWPL_SILICONZ-1
    
    Fixes: a5686ae820fa ("wifi: ath12k: move ATH12K_FLAG_REGISTERED handling to ath12k_mac_register()")
    Acked-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>
    Signed-off-by: Aditya Kumar Singh <quic_adisi@quicinc.com>

--- drivers/net/wireless/ath/ath12k/mac.c
+++ drivers/net/wireless/ath/ath12k/mac.c
@@ -11244,7 +11244,6 @@ void ath12k_mac_mlo_teardown(struct ath12k_hw_group *ag)
 
 int ath12k_mac_register(struct ath12k_hw_group *ag)
 {
-	struct ath12k_base *ab = ag->ab[0];
 	struct ath12k_hw *ah;
 	int i;
 	int ret;
@@ -11256,8 +11255,6 @@ int ath12k_mac_register(struct ath12k_hw_group *ag)
 		if (ret)
 			goto err;
 	}
-
-	set_bit(ATH12K_FLAG_REGISTERED, &ab->dev_flags);
 
 	return 0;
 
@@ -11275,11 +11272,8 @@ err:
 
 void ath12k_mac_unregister(struct ath12k_hw_group *ag)
 {
-	struct ath12k_base *ab = ag->ab[0];
 	struct ath12k_hw *ah;
 	int i;
-
-	clear_bit(ATH12K_FLAG_REGISTERED, &ab->dev_flags);
 
 	for (i = ag->num_hw - 1; i >= 0; i--) {
 		ah = ath12k_ag_to_ah(ag, i);
