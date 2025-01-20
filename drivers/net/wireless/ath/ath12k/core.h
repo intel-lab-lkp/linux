acc71a75e7a4e0e280667034e10b0f2f6f23969f
Aditya Kumar Singh <quic_adisi@quicinc.com>
2025-01-20 15:18:22 +0530

    wifi: ath12k: add reference counting for core attachment to hardware group
    Currently, driver does not manage reference counting for attaching and
    detaching cores to/from hardware groups. This can lead to issues when
    multiple cores are involved. Or with same core, attach/detach is called
    multiple times back to back.
    
    Fix this issue by using reference counting.
    
    With that, it is now ensured that the core is properly attached or detached
    from the hardware group and even back to back calls will not alter the
    count.
    
    Additionally, add some debug logs during the attachment and detachment
    events for better debugging and tracking.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.3.1-00173-QCAHKSWPL_SILICONZ-1
    
    Acked-by: Jeff Johnson <jeff.johnson@oss.qualcomm.com>
    Signed-off-by: Aditya Kumar Singh <quic_adisi@quicinc.com>

--- drivers/net/wireless/ath/ath12k/core.h
+++ drivers/net/wireless/ath/ath12k/core.h
@@ -1052,6 +1052,7 @@ struct ath12k_base {
 
 	struct ath12k_hw_group *ag;
 	struct ath12k_wsi_info wsi_info;
+	bool hw_group_ref;
 
 	/* must be last */
 	u8 drv_priv[] __aligned(sizeof(void *));
@@ -1208,20 +1209,6 @@ static inline void ath12k_ag_set_ah(struct ath12k_hw_group *ag, int idx,
 static inline struct ath12k_hw_group *ath12k_ab_to_ag(struct ath12k_base *ab)
 {
 	return ab->ag;
-}
-
-static inline void ath12k_core_started(struct ath12k_base *ab)
-{
-	lockdep_assert_held(&ab->ag->mutex);
-
-	ab->ag->num_started++;
-}
-
-static inline void ath12k_core_stopped(struct ath12k_base *ab)
-{
-	lockdep_assert_held(&ab->ag->mutex);
-
-	ab->ag->num_started--;
 }
 
 static inline struct ath12k_base *ath12k_ag_to_ab(struct ath12k_hw_group *ag,
