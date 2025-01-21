2fa6982f1cf00ab13af64b07f236606b8681af34
Kunihiko Hayashi <hayashi.kunihiko@socionext.com>
2025-01-21 13:41:37 +0900

    net: stmmac: Limit FIFO size by hardware capability
    Tx/Rx FIFO size is specified by the parameter "{tx,rx}-fifo-depth" from
    stmmac_platform layer.
    
    However, these values are constrained by upper limits determined by the
    capabilities of each hardware feature. There is a risk that the upper
    bits will be truncated due to the calculation, so it's appropriate to
    limit them to the upper limit values and display a warning message.
    
    Fixes: e7877f52fd4a ("stmmac: Read tx-fifo-depth and rx-fifo-depth from the devicetree")
    Signed-off-by: Kunihiko Hayashi <hayashi.kunihiko@socionext.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -7188,6 +7188,19 @@ static int stmmac_hw_init(struct stmmac_priv *priv)
 		priv->plat->tx_queues_to_use = priv->dma_cap.number_tx_queues;
 	}
 
+	if (priv->plat->rx_fifo_size > priv->dma_cap.rx_fifo_size) {
+		dev_warn(priv->device,
+			 "Rx FIFO size exceeds dma capability (%d)\n",
+			 priv->plat->rx_fifo_size);
+		priv->plat->rx_fifo_size = priv->dma_cap.rx_fifo_size;
+	}
+	if (priv->plat->tx_fifo_size > priv->dma_cap.tx_fifo_size) {
+		dev_warn(priv->device,
+			 "Tx FIFO size exceeds dma capability (%d)\n",
+			 priv->plat->tx_fifo_size);
+		priv->plat->tx_fifo_size = priv->dma_cap.tx_fifo_size;
+	}
+
 	priv->hw->vlan_fail_q_en =
 		(priv->plat->flags & STMMAC_FLAG_VLAN_FAIL_Q_EN);
 	priv->hw->vlan_fail_q = priv->plat->vlan_fail_q;
