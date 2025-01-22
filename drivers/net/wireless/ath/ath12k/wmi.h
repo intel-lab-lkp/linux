6bda8c62f0c3a6b72f3c0e91b896412510961739
Sarika Sharma <quic_sarishar@quicinc.com>
2025-01-22 19:04:24 +0530

    wifi: ath12k: add EHT support for TX rate
    Currently, TX rates are not supported for EHT. Hence, add EHT
    handling for TX rates and update the EHT-specific fields in arsta
    accordingly.
    
    Signed-off-by: Sarika Sharma <quic_sarishar@quicinc.com>

--- drivers/net/wireless/ath/ath12k/wmi.h
+++ drivers/net/wireless/ath/ath12k/wmi.h
@@ -1,7 +1,7 @@
 /* SPDX-License-Identifier: BSD-3-Clause-Clear */
 /*
  * Copyright (c) 2018-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #ifndef ATH12K_WMI_H
@@ -4605,6 +4605,7 @@ enum wmi_rate_preamble {
 	WMI_RATE_PREAMBLE_HT,
 	WMI_RATE_PREAMBLE_VHT,
 	WMI_RATE_PREAMBLE_HE,
+	WMI_RATE_PREAMBLE_EHT,
 };
 
 /**
