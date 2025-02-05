81572e8858401e0603483bdb0acbed431686b0ce
Alejandro Lucero <alucerop@amd.com>
2025-02-05 15:19:47 +0000

    sfc: create cxl region
    Use cxl api for creating a region using the endpoint decoder related to
    a DPA range specifying no DAX device should be created.
    
    Signed-off-by: Alejandro Lucero <alucerop@amd.com>
    Reviewed-by: Martin Habets <habetsm.xilinx@gmail.com>
    Acked-by: Edward Cree <ecree.xilinx@gmail.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/net/ethernet/sfc/efx_cxl.c
+++ drivers/net/ethernet/sfc/efx_cxl.c
@@ -129,10 +129,19 @@ int efx_cxl_init(struct efx_probe_data *probe_data)
 		goto err_regs;
 	}
 
+	cxl->efx_region = cxl_create_region(cxl->cxlrd, cxl->cxled, 1, true);
+	if (IS_ERR(cxl->efx_region)) {
+		pci_err(pci_dev, "CXL accel create region failed");
+		rc = PTR_ERR(cxl->efx_region);
+		goto err_region;
+	}
+
 	probe_data->cxl = cxl;
 
 	return 0;
 
+err_region:
+	cxl_dpa_free(probe_data->cxl->cxled);
 err_regs:
 	kfree(probe_data->cxl);
 	return rc;
@@ -142,6 +151,7 @@ err_regs:
 void efx_cxl_exit(struct efx_probe_data *probe_data)
 {
 	if (probe_data->cxl) {
+		cxl_accel_region_detach(probe_data->cxl->cxled);
 		cxl_dpa_free(probe_data->cxl->cxled);
 		cxl_put_root_decoder(probe_data->cxl->cxlrd);
 		kfree(probe_data->cxl);
