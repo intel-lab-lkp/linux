eed15f2384a320be02eef8a57dce4a5d1d8602f0
Alejandro Lucero <alucerop@amd.com>
2025-02-05 15:19:50 +0000

    sfc: support pio mapping based on cxl
    With a device supporting CXL and successfully initialised, use the cxl
    region to map the memory range and use this mapping for PIO buffers.
    
    Signed-off-by: Alejandro Lucero <alucerop@amd.com>
    Acked-by: Edward Cree <ecree.xilinx@gmail.com>

--- drivers/net/ethernet/sfc/efx_cxl.c
+++ drivers/net/ethernet/sfc/efx_cxl.c
@@ -27,6 +27,7 @@ int efx_cxl_init(struct efx_probe_data *probe_data)
 	DECLARE_BITMAP(found, CXL_MAX_CAPS);
 	resource_size_t max_size;
 	struct mds_info sfc_mds_info;
+	struct range range;
 	struct efx_cxl *cxl;
 
 	u16 dvsec;
@@ -136,12 +137,28 @@ int efx_cxl_init(struct efx_probe_data *probe_data)
 		goto err_region;
 	}
 
+	rc = cxl_get_region_range(cxl->efx_region, &range);
+	if (rc) {
+		pci_err(pci_dev, "CXL getting regions params failed");
+		goto err_region_params;
+	}
+
+	cxl->ctpio_cxl = ioremap(range.start, range.end - range.start + 1);
+	if (!cxl->ctpio_cxl) {
+		pci_err(pci_dev, "CXL ioremap region (%pra) pfailed", &range);
+		rc = -ENOMEM;
+		goto err_region_params;
+	}
+
 	probe_data->cxl = cxl;
+	probe_data->cxl_pio_initialised = true;
 
 	return 0;
 
+err_region_params:
+	cxl_accel_region_detach(cxl->cxled);
 err_region:
-	cxl_dpa_free(probe_data->cxl->cxled);
+	cxl_dpa_free(cxl->cxled);
 err_regs:
 	kfree(probe_data->cxl);
 	return rc;
@@ -151,6 +168,7 @@ err_regs:
 void efx_cxl_exit(struct efx_probe_data *probe_data)
 {
 	if (probe_data->cxl) {
+		iounmap(probe_data->cxl->ctpio_cxl);
 		cxl_accel_region_detach(probe_data->cxl->cxled);
 		cxl_dpa_free(probe_data->cxl->cxled);
 		cxl_put_root_decoder(probe_data->cxl->cxlrd);
