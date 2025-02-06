5918dfef71b91c8f285cbe6c6028cf53213db9aa
Michael Riesch <michael.riesch@wolfvision.net>
2025-02-06 10:57:42 +0100

    media: rockchip: add a driver for the rockchip camera interface
    The Rockchip Camera Interface (CIF) is featured in many Rockchip SoCs
    in different variations. For example, the PX30 Video Input Processor (VIP)
    is able to receive video data via the Digital Video Port (DVP, a parallel
    data interface and transfer it into system memory using a double-buffering
    mechanism called ping-pong mode. The RK3568 Video Capture (VICAP) unit,
    on the other hand, features a DVP and a MIPI CSI-2 receiver that can
    receive video data independently (both using the ping-pong scheme).
    The different variants may have additional features, such as scaling
    and/or cropping.
    Finally, the RK3588 VICAP unit constitutes an essential piece of the
    camera interface with one DVP, six MIPI CSI-2 receivers, scale/crop
    units, and different data path multiplexers (to system memory, to ISP,
    ...).
    
    Add a basic media controller centric V4L2 driver for the Rockchip CIF with
     - support for the PX30 VIP
     - support for the RK3568 VICAP DVP
     - abstraction for the ping-pong scheme to allow for future extensions
     - abstraction for the INTERFACE and CROP parts to allow for future
       extensions
    
    [PX30 VIP support v1-v5]
    Co-developed-by: Maxime Chevallier <maxime.chevallier@bootlin.com>
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>
    [PX30 VIP support v6-v13]
    Co-developed-by: Mehdi Djait <mehdi.djait@bootlin.com>
    Signed-off-by: Mehdi Djait <mehdi.djait@bootlin.com>
    [added RK3568 VICAP DVP support]
    [refactored to media controller centric driver, added mplane support]
    Co-developed-by: Gerald Loacker <gerald.loacker@wolfvision.net>
    Signed-off-by: Gerald Loacker <gerald.loacker@wolfvision.net>
    Signed-off-by: Michael Riesch <michael.riesch@wolfvision.net>

---
+++ drivers/media/platform/rockchip/rkcif/rkcif-capture-mipi.c
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0
+/*
+ * Rockchip Camera Interface (CIF) Driver
+ *
+ * Copyright (C) 2018 Rockchip Electronics Co., Ltd.
+ * Copyright (C) 2025 Michael Riesch <michael.riesch@wolfvision.net>
+ */
+
+#include "rkcif-capture-mipi.h"
+#include "rkcif-common.h"
+#include "rkcif-stream.h"
+
+irqreturn_t rkcif_mipi_isr(int irq, void *ctx)
+{
+	return IRQ_NONE;
+}
+
+int rkcif_mipi_register(struct rkcif_device *rkcif)
+{
+	return 0;
+}
+
+void rkcif_mipi_unregister(struct rkcif_device *rkcif)
+{
+}
