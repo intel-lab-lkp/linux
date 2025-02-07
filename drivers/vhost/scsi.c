a0c638f2f68a98c9e27edc3f8e9dfc351235c7e8
Dongli Zhang <dongli.zhang@oracle.com>
2025-02-07 10:41:46 -0800

    vhost-scsi: adjust vhost_scsi_get_desc() to log vring descriptors
    Adjust vhost_scsi_get_desc() to facilitate logging of vring descriptors.
    
    Add new arguments to allow passing the log buffer and length to
    vhost_get_vq_desc().
    
    In addition, reset 'log_num' since vhost_get_vq_desc() may reset it only
    after certain condition checks.
    
    Suggested-by: Joao Martins <joao.m.martins@oracle.com>
    Signed-off-by: Dongli Zhang <dongli.zhang@oracle.com>

--- drivers/vhost/scsi.c
+++ drivers/vhost/scsi.c
@@ -950,13 +950,17 @@ vhost_scsi_send_bad_target(struct vhost_scsi *vs,
 
 static int
 vhost_scsi_get_desc(struct vhost_scsi *vs, struct vhost_virtqueue *vq,
-		    struct vhost_scsi_ctx *vc)
+		    struct vhost_scsi_ctx *vc,
+		    struct vhost_log *log, unsigned int *log_num)
 {
 	int ret = -ENXIO;
 
+	if (likely(log_num))
+		*log_num = 0;
+
 	vc->head = vhost_get_vq_desc(vq, vq->iov,
 				     ARRAY_SIZE(vq->iov), &vc->out, &vc->in,
-				     NULL, NULL);
+				     log, log_num);
 
 	pr_debug("vhost_get_vq_desc: head: %d, out: %u in: %u\n",
 		 vc->head, vc->out, vc->in);
@@ -1086,7 +1090,7 @@ vhost_scsi_handle_vq(struct vhost_scsi *vs, struct vhost_virtqueue *vq)
 	vhost_disable_notify(&vs->dev, vq);
 
 	do {
-		ret = vhost_scsi_get_desc(vs, vq, &vc);
+		ret = vhost_scsi_get_desc(vs, vq, &vc, NULL, NULL);
 		if (ret)
 			goto err;
 
@@ -1411,7 +1415,7 @@ vhost_scsi_ctl_handle_vq(struct vhost_scsi *vs, struct vhost_virtqueue *vq)
 	vhost_disable_notify(&vs->dev, vq);
 
 	do {
-		ret = vhost_scsi_get_desc(vs, vq, &vc);
+		ret = vhost_scsi_get_desc(vs, vq, &vc, NULL, NULL);
 		if (ret)
 			goto err;
 
