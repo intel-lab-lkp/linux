b279d300a9ac29d5e6417e380908d02a7f871fa5
Philipp Stanner <phasta@kernel.org>
2025-02-07 13:36:52 +0100

    drm/sched: Adjust outdated docu for run_job()
    The documentation for drm_sched_backend_ops.run_job() mentions a certain
    function called drm_sched_job_recovery(). This function does not exist.
    What's actually meant is drm_sched_resubmit_jobs(), which is by now also
    deprecated.
    
    Remove the mention of the removed function.
    
    Discourage the behavior of drm_sched_backend_ops.run_job() being called
    multiple times for the same job.
    
    Signed-off-by: Philipp Stanner <phasta@kernel.org>

--- include/drm/gpu_scheduler.h
+++ include/drm/gpu_scheduler.h
@@ -421,14 +421,19 @@ struct drm_sched_backend_ops {
 
 	/**
 	 * @run_job: Called to execute the job once all of the dependencies
-	 * have been resolved. This may be called multiple times, if
-	 * timedout_job() has happened and drm_sched_job_recovery() decides to
-	 * try it again.
+	 * have been resolved.
+	 *
+	 * The deprecated drm_sched_resubmit_jobs() (called from
+	 * drm_sched_backend_ops.timedout_job()) can invoke this again with the
+	 * same parameters. Doing this is strongly discouraged because it
+	 * violates dma_fence rules.
 	 *
 	 * @sched_job: the job to run
 	 *
-	 * Returns: dma_fence the driver must signal once the hardware has
-	 *	completed the job ("hardware fence").
+	 * Returns:
+	 * On success: dma_fence the driver must signal once the hardware has
+	 * completed the job ("hardware fence").
+	 * On failure: NULL or an ERR_PTR.
 	 *
 	 * Note that the scheduler expects to 'inherit' its own reference to
 	 * this fence from the callback. It does not invoke an extra
