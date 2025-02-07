042c9b2d44226b9f24804352ac27a6e48a459682
Lorenzo Bianconi <lorenzo@kernel.org>
2025-02-07 17:26:30 +0100

    net: airoha: Introduce PPE debugfs support
    Similar to PPE support for Mediatek devices, introduce PPE debugfs
    in order to dump binded and unbinded flows.
    
    Signed-off-by: Lorenzo Bianconi <lorenzo@kernel.org>

--- drivers/net/ethernet/airoha/airoha_ppe.c
+++ drivers/net/ethernet/airoha/airoha_ppe.c
@@ -798,6 +798,10 @@ int airoha_ppe_init(struct airoha_eth *eth)
 	if (err)
 		goto error_npu_deinit;
 
+	err = airoha_ppe_debugfs_init(ppe);
+	if (err)
+		goto error_npu_deinit;
+
 	return 0;
 
 error_npu_deinit:
@@ -815,4 +819,5 @@ void airoha_ppe_deinit(struct airoha_eth *eth)
 		airoha_npu_deinit(eth->npu);
 	}
 	rhashtable_destroy(&eth->flow_table);
+	debugfs_remove(eth->ppe->debugfs_dir);
 }
