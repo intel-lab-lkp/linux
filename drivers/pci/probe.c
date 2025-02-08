1ed7f23913c2faccc5b5ec1276493380e9cfd6cf
Terry Bowman <terry.bowman@amd.com>
2025-02-07 18:29:27 -0600

    CXL/PCI: Introduce PCIe helper functions pcie_is_cxl() and pcie_is_cxl_port()
    CXL and AER drivers need the ability to identify CXL devices and CXL port
    devices.
    
    First, add set_pcie_cxl() with logic checking for CXL Flexbus DVSEC
    presence. The CXL Flexbus DVSEC presence is used because it is required
    for all the CXL PCIe devices.[1]
    
    Add boolean 'struct pci_dev::is_cxl' with the purpose to cache the CXL
    Flexbus presence.
    
    Add pcie_is_cxl() as a macro to return 'struct pci_dev::is_cxl'.
    
    Add pcie_is_cxl_port() to check if a device is a CXL Root Port, CXL
    Upstream Switch Port, or CXL Downstream Switch Port. Also, verify the
    CXL Extensions DVSEC for Ports is present.[1]
    
    [1] CXL 3.1 Spec, 8.1.1 PCIe Designated Vendor-Specific Extended
        Capability (DVSEC) ID Assignment, Table 8-2
    
    Signed-off-by: Terry Bowman <terry.bowman@amd.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Fan Ni <fan.ni@samsung.com>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -1676,6 +1676,14 @@ static void set_pcie_thunderbolt(struct pci_dev *dev)
 		dev->is_thunderbolt = 1;
 }
 
+static void set_pcie_cxl(struct pci_dev *dev)
+{
+	u16 dvsec = pci_find_dvsec_capability(dev, PCI_VENDOR_ID_CXL,
+					      PCI_DVSEC_CXL_FLEXBUS);
+	if (dvsec)
+		dev->is_cxl = 1;
+}
+
 static void set_pcie_untrusted(struct pci_dev *dev)
 {
 	struct pci_dev *parent = pci_upstream_bridge(dev);
@@ -2005,6 +2013,8 @@ int pci_setup_device(struct pci_dev *dev)
 
 	/* Need to have dev->cfg_size ready */
 	set_pcie_thunderbolt(dev);
+
+	set_pcie_cxl(dev);
 
 	set_pcie_untrusted(dev);
 
