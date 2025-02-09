32703b946caeed10036b56500e5c2aec0bf6007c
Eric Woudstra <ericwouds@gmail.com>
2025-02-09 12:10:33 +0100

    bridge: Introduce DEV_PATH_BR_VLAN_KEEP_HW for bridge-fastpath
    This patch introduces DEV_PATH_BR_VLAN_KEEP_HW. It is needed in the
    bridge fastpath for switchdevs supporting SWITCHDEV_OBJ_ID_PORT_VLAN.
    
    It is similar to DEV_PATH_BR_VLAN_TAG, with the correcponding bit in
    ingress_vlans set.
    
    In the forward fastpath it is not needed.
    
    Acked-by: Nikolay Aleksandrov <razor@blackwall.org>
    Signed-off-by: Eric Woudstra <ericwouds@gmail.com>

--- net/bridge/br_device.c
+++ net/bridge/br_device.c
@@ -430,6 +430,10 @@ static int br_fill_forward_path(struct net_device_path_ctx *ctx,
 	case DEV_PATH_BR_VLAN_UNTAG:
 		ctx->num_vlans--;
 		break;
+	case DEV_PATH_BR_VLAN_KEEP_HW:
+		if (!src)
+			path->bridge.vlan_mode = DEV_PATH_BR_VLAN_KEEP;
+		break;
 	case DEV_PATH_BR_VLAN_KEEP:
 		break;
 	}
