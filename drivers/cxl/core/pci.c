f82bb873dc46583dfd79d88ad463acc7a0074ff8
Terry Bowman <terry.bowman@amd.com>
2025-02-11 13:24:40 -0600

    cxl/pci: Add trace logging for CXL PCIe Port RAS errors
    The CXL drivers use kernel trace functions for logging Endpoint and
    Restricted CXL host (RCH) Downstream Port RAS errors. Similar functionality
    is required for CXL Root Ports, CXL Downstream Switch Ports, and CXL
    Upstream Switch Ports.
    
    Introduce trace logging functions for both RAS correctable and
    uncorrectable errors specific to CXL PCIe Ports. Additionally, update
    the CXL Port Protocol Error handlers to invoke these new trace functions.
    
    Examples of the output from these changes is below.
    
    Correctable error:
    cxl_port_aer_correctable_error: device=port1 parent=root0 status='Received Error From Physical Layer'
    
    Uncorrectable error:
    cxl_port_aer_uncorrectable_error: device=port1 parent=root0 status: 'Memory Byte Enable Parity Error' first_error: 'Memory Byte Enable Parity Erro'
    
    Signed-off-by: Terry Bowman <terry.bowman@amd.com>
    Reviewed-by: Alejandro Lucero <alucerop@amd.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/cxl/core/pci.c
+++ drivers/cxl/core/pci.c
@@ -671,6 +671,8 @@ static void __cxl_handle_cor_ras(struct device *dev,
 
 	if (is_cxl_memdev(dev))
 		trace_cxl_aer_correctable_error(to_cxl_memdev(dev), status);
+	else if (is_cxl_port(dev))
+		trace_cxl_port_aer_correctable_error(dev, status);
 }
 
 static void cxl_handle_endpoint_cor_ras(struct cxl_dev_state *cxlds)
@@ -730,6 +732,8 @@ static pci_ers_result_t __cxl_handle_ras(struct device *dev, void __iomem *ras_b
 	header_log_copy(ras_base, hl);
 	if (is_cxl_memdev(dev))
 		trace_cxl_aer_uncorrectable_error(to_cxl_memdev(dev), status, fe, hl);
+	else if (is_cxl_port(dev))
+		trace_cxl_port_aer_uncorrectable_error(dev, status, fe, hl);
 
 	writel(status & CXL_RAS_UNCORRECTABLE_STATUS_MASK, addr);
 
