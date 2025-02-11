5c68e1da7cc2330d4c1dd1f15339fdc49ea6f448
Dmitry Antipov <dmantipov@yandex.ru>
2025-02-11 10:39:15 +0300

    wifi: rtw89: rtw8852b{t}: fix TSSI debug timestamps
    Assuming that 'tssi_alimk_time' of 'struct rtw89_tssi_info' is
    in milliseconds, adjust rtw8852b{t}-specific '_tssi_alimentk()'
    to not mix the former with nanoseconds. Compile tested only.
    
    Fixes: 7f18a70d7b4d ("wifi: rtw89: 8852b: rfk: add TSSI")
    Signed-off-by: Dmitry Antipov <dmantipov@yandex.ru>

--- drivers/net/wireless/realtek/rtw89/rtw8852b_rfk.c
+++ drivers/net/wireless/realtek/rtw89/rtw8852b_rfk.c
@@ -3585,7 +3585,7 @@ static void _tssi_alimentk(struct rtw89_dev *rtwdev, enum rtw89_phy_idx phy,
 	u8 ch_idx = _tssi_ch_to_idx(rtwdev, channel);
 	struct rtw8852bx_bb_tssi_bak tssi_bak;
 	s32 aliment_diff, tssi_cw_default;
-	u32 start_time, finish_time;
+	ktime_t start_time;
 	u32 bb_reg_backup[8] = {0};
 	const s16 *power;
 	u8 band;
@@ -3613,7 +3613,7 @@ static void _tssi_alimentk(struct rtw89_dev *rtwdev, enum rtw89_phy_idx phy,
 		return;
 	}
 
-	start_time = ktime_get_ns();
+	start_time = ktime_get();
 
 	if (chan->band_type == RTW89_BAND_2G)
 		power = power_2g;
@@ -3738,11 +3738,10 @@ out:
 	rtw8852bx_bb_restore_tssi(rtwdev, phy, &tssi_bak);
 	rtw8852bx_bb_tx_mode_switch(rtwdev, phy, 0);
 
-	finish_time = ktime_get_ns();
-	tssi_info->tssi_alimk_time += finish_time - start_time;
+	tssi_info->tssi_alimk_time += ktime_ms_delta(ktime_get(), start_time);
 
 	rtw89_debug(rtwdev, RTW89_DBG_RFK,
-		    "[TSSI PA K] %s processing time = %d ms\n", __func__,
+		    "[TSSI PA K] %s processing time = %llu ms\n", __func__,
 		    tssi_info->tssi_alimk_time);
 }
 
