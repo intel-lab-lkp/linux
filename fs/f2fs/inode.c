17d47d47119e59d4ac2ff1c51ddb0ba5b8119da1
Leo Stone <leocstone@gmail.com>
2025-02-11 15:17:25 +0800

    f2fs: add check for deleted inode
    The syzbot reproducer mounts a f2fs image, then tries to unlink an
    existing file. However, the unlinked file already has a link count of 0
    when it is read for the first time in do_read_inode().
    
    Add a check to sanity_check_inode() for i_nlink == 0.
    
    [Chao Yu: rebase the code]
    Reported-by: syzbot+b01a36acd7007e273a83@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=b01a36acd7007e273a83
    Fixes: 39a53e0ce0df ("f2fs: add superblock and major in-memory structure")
    Signed-off-by: Leo Stone <leocstone@gmail.com>
    Signed-off-by: Chao Yu <chao@kernel.org>

--- fs/f2fs/inode.c
+++ fs/f2fs/inode.c
@@ -386,6 +386,12 @@ static bool sanity_check_inode(struct inode *inode, struct page *node_page)
 		}
 	}
 
+	if (inode->i_nlink == 0) {
+		f2fs_warn(sbi, "%s: inode (ino=%lx) has a link count of 0",
+			  __func__, inode->i_ino);
+		return false;
+	}
+
 	return true;
 }
 
