482aaea45c65a1ccae13b10038c0c0d3f4a339ad
Yazen Ghannam <yazen.ghannam@amd.com>
2025-02-13 16:46:01 +0000

    x86/mce: Unify AMD THR handler with MCA Polling
    AMD systems optionally support an MCA thresholding interrupt. The
    interrupt should be used as another signal to trigger MCA polling. This
    is similar to how the Intel Corrected Machine Check interrupt (CMCI) is
    handled.
    
    AMD MCA thresholding is managed using the MCA_MISC registers within an
    MCA bank. The OS will need to modify the hardware error count field in
    order to reset the threshold limit and rearm the interrupt. Management
    of the MCA_MISC register should be done as a follow up to the basic MCA
    polling flow. It should not be the main focus of the interrupt handler.
    
    Furthermore, future systems will have the ability to send an MCA
    thresholding interrupt to the OS even when the OS does not manage the
    feature, i.e. MCA_MISC registers are Read-as-Zero/Locked.
    
    Call the common MCA polling function when handling the MCA thresholding
    interrupt. This will allow the OS to find any valid errors whether or
    not the MCA thresholding feature is OS-managed. Also, this allows the
    common MCA polling options and kernel parameters to apply to AMD
    systems.
    
    Add a callback to the MCA polling function to check and reset any
    threshold blocks that have reached their threshold limit.
    
    Signed-off-by: Yazen Ghannam <yazen.ghannam@amd.com>

--- arch/x86/kernel/cpu/mce/internal.h
+++ arch/x86/kernel/cpu/mce/internal.h
@@ -268,6 +268,7 @@ void mce_prep_record_per_cpu(unsigned int cpu, struct mce *m);
 void mce_threshold_create_device(unsigned int cpu);
 extern bool amd_filter_mce(struct mce *m);
 bool amd_mce_usable_address(struct mce *m);
+void amd_reset_thr_limit(unsigned int bank);
 
 /*
  * If MCA_CONFIG[McaLsbInStatusSupported] is set, extract ErrAddr in bits
@@ -298,6 +299,7 @@ void mce_smca_cpu_init(void);
 static inline void mce_threshold_create_device(unsigned int cpu)	{ }
 static inline bool amd_filter_mce(struct mce *m) { return false; }
 static inline bool amd_mce_usable_address(struct mce *m) { return false; }
+static inline void amd_reset_thr_limit(unsigned int bank) { }
 static inline void smca_extract_err_addr(struct mce *m) { }
 static inline void mce_smca_cpu_init(void) {}
 #endif
