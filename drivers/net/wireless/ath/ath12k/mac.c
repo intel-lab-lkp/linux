95f48fc56f20e76ae4a65bfb7b854287362f40e8
Sarika Sharma <quic_sarishar@quicinc.com>
2025-02-13 22:46:28 +0530

    wifi: cfg80211: add flag to indicate driver supports ML station statistics
    Currently, while filling the link level station statistics, valid_links
    is checked. There might be the case when driver offload link station
    statistics and does not provide per-link statistics.
    
    Hence, add flag WIPHY_FLAG_SUPPORTS_MLO_STA_PER_LINK_STATS in wiphy
    structure to indicate that driver supports per link station statistics.
    Set this flag during hw_register if driver supports per-link station
    statistics and check this flag while filling the station_info structure
    for MLO.
    
    Signed-off-by: Sarika Sharma <quic_sarishar@quicinc.com>

--- drivers/net/wireless/ath/ath12k/mac.c
+++ drivers/net/wireless/ath/ath12k/mac.c
@@ -10942,6 +10942,7 @@ static int ath12k_mac_hw_register(struct ath12k_hw *ah)
 	 * once WIPHY_FLAG_SUPPORTS_MLO is enabled.
 	 */
 	wiphy->flags |= WIPHY_FLAG_DISABLE_WEXT;
+	wiphy->flags |= WIPHY_FLAG_SUPPORTS_MLO_STA_PER_LINK_STATS;
 
 	/* Copy over MLO related capabilities received from
 	 * WMI_SERVICE_READY_EXT2_EVENT if single_chip_mlo_supp is set.
