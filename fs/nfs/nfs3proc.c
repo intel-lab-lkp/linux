cbb1f8cce28f18e7bbc29c11547844bae40ef71c
NeilBrown <neilb@suse.de>
2025-02-14 16:16:42 +1100

    nfs: change mkdir inode_operation to return alternate dentry if needed.
    mkdir now allows a different dentry to be returned which is sometimes
    relevant for nfs.
    
    This patch changes the nfs_rpc_ops mkdir op to return a dentry, and
    passes that back to the caller.
    
    The mkdir nfs_rpc_op may return NULL if the original dentry should be
    used.  This is different from the mkdir inode_operation.
    
    Signed-off-by: NeilBrown <neilb@suse.de>

--- fs/nfs/nfs3proc.c
+++ fs/nfs/nfs3proc.c
@@ -578,7 +578,7 @@ out:
 	return status;
 }
 
-static int
+static struct dentry *
 nfs3_proc_mkdir(struct inode *dir, struct dentry *dentry, struct iattr *sattr)
 {
 	struct posix_acl *default_acl, *acl;
@@ -613,14 +613,15 @@ nfs3_proc_mkdir(struct inode *dir, struct dentry *dentry, struct iattr *sattr)
 
 	status = nfs3_proc_setacls(d_inode(dentry), acl, default_acl);
 
-	dput(d_alias);
 out_release_acls:
 	posix_acl_release(acl);
 	posix_acl_release(default_acl);
 out:
 	nfs3_free_createdata(data);
 	dprintk("NFS reply mkdir: %d\n", status);
-	return status;
+	if (status)
+		return ERR_PTR(status);
+	return dentry;
 }
 
 static int
