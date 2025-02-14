fa3e6315d9e9c2fad97c4fdda109bec2699feb55
Anusha Srivatsa <asrivats@redhat.com>
2025-02-13 19:19:20 -0500

    drm/sti: move to devm_platform_ioremap_resource() usage
    Replace platform_get_resource/_byname + devm_ioremap
    with just devm_platform_ioremap_resource()
    
    Used Coccinelle to do this change. SmPl patch:
    
    @rule@
    identifier res;
    expression ioremap;
    identifier pdev;
    constant mem;
    expression name;
    @@
    -struct resource *res;
    ...
    -res = platform_get_resource_byname(pdev,mem,name);
    <...
    -if (!res) {
    -...
    -}
    ...>
    -ioremap = devm_ioremap(...);
    +ioremap = devm_platform_ioremap_resource_byname(pdev,name);
    
    and
    @rule_2@
    identifier res;
    expression ioremap;
    identifier pdev;
    @@
    -struct resource *res;
    ...
    -res = platform_get_resource(pdev,...);
    <...
    -if (!res) {
    -...
    -}
    ...>
    -ioremap = devm_ioremap(...);
    +ioremap = devm_platform_ioremap_resource(pdev,0);
    
    v2: Fix compilation error.
    
    Cc: Raphael Gallais-Pou <raphael.gallais-pou@foss.st.com>
    Cc: Alain Volmat <alain.volmat@foss.st.com>
    Reviewed-by: Maxime Ripard <mripard@kernel.org>
    Acked-by: Raphael Gallais-Pou <rgallaispou@gmail.com>
    Signed-off-by: Anusha Srivatsa <asrivats@redhat.com>

--- drivers/gpu/drm/sti/sti_hda.c
+++ drivers/gpu/drm/sti/sti_hda.c
@@ -750,14 +750,7 @@ static int sti_hda_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	hda->dev = pdev->dev;
-
-	/* Get resources */
-	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "hda-reg");
-	if (!res) {
-		DRM_ERROR("Invalid hda resource\n");
-		return -ENOMEM;
-	}
-	hda->regs = devm_ioremap(dev, res->start, resource_size(res));
+	hda->regs = devm_platform_ioremap_resource_byname(pdev, "hda-reg");
 	if (!hda->regs)
 		return -ENOMEM;
 
--- drivers/gpu/drm/sti/sti_tvout.c
+++ drivers/gpu/drm/sti/sti_tvout.c
@@ -838,7 +838,6 @@ static int sti_tvout_probe(struct platform_device *pdev)
 	struct device *dev = &pdev->dev;
 	struct device_node *node = dev->of_node;
 	struct sti_tvout *tvout;
-	struct resource *res;
 
 	DRM_INFO("%s\n", __func__);
 
@@ -850,14 +849,7 @@ static int sti_tvout_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	tvout->dev = dev;
-
-	/* get memory resources */
-	res = platform_get_resource_byname(pdev, IORESOURCE_MEM, "tvout-reg");
-	if (!res) {
-		DRM_ERROR("Invalid glue resource\n");
-		return -ENOMEM;
-	}
-	tvout->regs = devm_ioremap(dev, res->start, resource_size(res));
+	tvout->regs = devm_platform_ioremap_resource_byname(pdev, "tvout-reg");
 	if (!tvout->regs)
 		return -ENOMEM;
 
