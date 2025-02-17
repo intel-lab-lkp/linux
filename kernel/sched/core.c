78da2c19736c8dcdb664e62825c009ed268e3b45
Gabriele Monaco <gmonaco@redhat.com>
2025-02-17 14:11:54 +0100

    sched: Add sched tracepoints for RV task model
    Add the following tracepoints:
    * sched_entry(bool preempt, ip)
        Called while entering __schedule
    * sched_exit(bool is_switch, ip)
        Called while exiting __schedule
    * sched_set_state(task, curr_state, state)
        Called when a task changes its state (to and from running)
    
    These tracepoints are useful to describe the Linux task model and are
    adapted from the patches by Daniel Bristot de Oliveira
    (https://bristot.me/linux-task-model/).
    
    Signed-off-by: Gabriele Monaco <gmonaco@redhat.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -491,6 +491,21 @@ sched_core_dequeue(struct rq *rq, struct task_struct *p, int flags) { }
 
 #endif /* CONFIG_SCHED_CORE */
 
+#ifdef TRACEPOINTS_ENABLED
+/* need a wrapper since we may need to trace from modules */
+EXPORT_TRACEPOINT_SYMBOL(sched_set_state_tp);
+
+/*
+ * Do not call this function directly since it won't check if the tp is enabled.
+ * Call the helper macro trace_set_current_state instead.
+ */
+void __do_trace_set_current_state(int state_value)
+{
+	__do_trace_sched_set_state_tp(current, current->__state, state_value);
+}
+EXPORT_SYMBOL(__do_trace_set_current_state);
+#endif
+
 /*
  * Serialization rules:
  *
@@ -5307,6 +5322,12 @@ asmlinkage __visible void schedule_tail(struct task_struct *prev)
 	 */
 
 	finish_task_switch(prev);
+	/*
+	 * This is a special case: the newly created task has just
+	 * switched the context for the fist time. It is returning from
+	 * schedule for the first time in this path.
+	 */
+	trace_sched_exit_tp(true, CALLER_ADDR0);
 	preempt_enable();
 
 	if (current->set_child_tid)
@@ -6650,11 +6671,14 @@ static void __sched notrace __schedule(int sched_mode)
 	 * as a preemption by schedule_debug() and RCU.
 	 */
 	bool preempt = sched_mode > SM_NONE;
+	bool is_switch = false;
 	unsigned long *switch_count;
 	unsigned long prev_state;
 	struct rq_flags rf;
 	struct rq *rq;
 	int cpu;
+
+	trace_sched_entry_tp(preempt, CALLER_ADDR0);
 
 	cpu = smp_processor_id();
 	rq = cpu_rq(cpu);
@@ -6723,7 +6747,8 @@ picked:
 	rq->last_seen_need_resched_ns = 0;
 #endif
 
-	if (likely(prev != next)) {
+	is_switch = prev != next;
+	if (likely(is_switch)) {
 		rq->nr_switches++;
 		/*
 		 * RCU users of rcu_dereference(rq->curr) may not see
@@ -6768,6 +6793,7 @@ picked:
 		__balance_callbacks(rq);
 		raw_spin_rq_unlock_irq(rq);
 	}
+	trace_sched_exit_tp(is_switch, CALLER_ADDR0);
 }
 
 void __noreturn do_task_dead(void)
