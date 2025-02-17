e8a030f709dea383bd9cba469e9eb7fe113f9c67
Subbaraya Sundeep <sbhatta@marvell.com>
2025-02-17 14:22:52 +0530

    octeontx2: Set appropriate PF, VF masks and shifts based on silicon
    Number of RVU PFs on CN20K silicon have increased to 96 from maximum
    of 32 that were supported on earlier silicons. Every RVU PF and VF is
    identified by HW using a 16bit PF_FUNC value. Due to the change in
    Max number of PFs in CN20K, the bit encoding of this PF_FUNC has changed.
    
    This patch handles the change by using helper functions(using silicon
    check) to use PF,VF masks and shifts to support both new silicon CN20K,
    OcteonTx series. These helper functions are used in different modules.
    
    Also moved the NIX AF register offset macros to other files which
    will be posted in coming patches.
    
    Signed-off-by: Subbaraya Sundeep <sbhatta@marvell.com>
    Signed-off-by: Sai Krishna <saikrishnag@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_cn10k.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_cn10k.c
@@ -52,7 +52,7 @@ static int lmtst_map_table_ops(struct rvu *rvu, u32 index, u64 *val,
 #define LMT_MAP_TBL_W1_OFF  8
 static u32 rvu_get_lmtst_tbl_index(struct rvu *rvu, u16 pcifunc)
 {
-	return ((rvu_get_pf(pcifunc) * rvu->hw->total_vfs) +
+	return ((rvu_get_pf(rvu->pdev, pcifunc) * rvu->hw->total_vfs) +
 		(pcifunc & RVU_PFVF_FUNC_MASK)) * LMT_MAPTBL_ENTRY_SIZE;
 }
 
@@ -69,7 +69,7 @@ static int rvu_get_lmtaddr(struct rvu *rvu, u16 pcifunc,
 
 	mutex_lock(&rvu->rsrc_lock);
 	rvu_write64(rvu, BLKADDR_RVUM, RVU_AF_SMMU_ADDR_REQ, iova);
-	pf = rvu_get_pf(pcifunc) & 0x1F;
+	pf = rvu_get_pf(rvu->pdev, pcifunc) & 0x1F;
 	val = BIT_ULL(63) | BIT_ULL(14) | BIT_ULL(13) | pf << 8 |
 	      ((pcifunc & RVU_PFVF_FUNC_MASK) & 0xFF);
 	rvu_write64(rvu, BLKADDR_RVUM, RVU_AF_SMMU_TXN_REQ, val);
