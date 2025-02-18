6731789a4d44e888c8370002ff1bde602e320847
Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>
2025-02-18 09:15:28 +0100

    x86: Minimal SLAB alignment
    Adjust x86 minimal SLAB alignment to match KASAN granularity size. In
    tag-based mode the size changes to 16 bytes so the value needs to be 4.
    
    Signed-off-by: Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>

--- arch/x86/include/asm/kasan.h
+++ arch/x86/include/asm/kasan.h
@@ -36,6 +36,8 @@
 
 #ifdef CONFIG_KASAN_SW_TAGS
 
+#define ARCH_SLAB_MINALIGN (1ULL << KASAN_SHADOW_SCALE_SHIFT)
+
 #define __tag_shifted(tag)		FIELD_PREP(GENMASK_ULL(60, 57), tag)
 #define __tag_reset(addr)		(sign_extend64((u64)(addr), 56))
 #define __tag_get(addr)			((u8)FIELD_GET(GENMASK_ULL(60, 57), (u64)addr))
