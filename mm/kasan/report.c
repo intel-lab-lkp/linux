b68587c9fd13811a046e01b44b9f52e4a99496ae
Samuel Holland <samuel.holland@sifive.com>
2025-02-18 09:15:17 +0100

    kasan: sw_tags: Use arithmetic shift for shadow computation
    Currently, kasan_mem_to_shadow() uses a logical right shift, which turns
    canonical kernel addresses into non-canonical addresses by clearing the
    high KASAN_SHADOW_SCALE_SHIFT bits. The value of KASAN_SHADOW_OFFSET is
    then chosen so that the addition results in a canonical address for the
    shadow memory.
    
    For KASAN_GENERIC, this shift/add combination is ABI with the compiler,
    because KASAN_SHADOW_OFFSET is used in compiler-generated inline tag
    checks[1], which must only attempt to dereference canonical addresses.
    
    However, for KASAN_SW_TAGS we have some freedom to change the algorithm
    without breaking the ABI. Because TBI is enabled for kernel addresses,
    the top bits of shadow memory addresses computed during tag checks are
    irrelevant, and so likewise are the top bits of KASAN_SHADOW_OFFSET.
    This is demonstrated by the fact that LLVM uses a logical right shift
    in the tag check fast path[2] but a sbfx (signed bitfield extract)
    instruction in the slow path[3] without causing any issues.
    
    Using an arithmetic shift in kasan_mem_to_shadow() provides a number of
    benefits:
    
    1) The memory layout is easier to understand. KASAN_SHADOW_OFFSET
    becomes a canonical memory address, and the shifted pointer becomes a
    negative offset, so KASAN_SHADOW_OFFSET == KASAN_SHADOW_END regardless
    of the shift amount or the size of the virtual address space.
    
    2) KASAN_SHADOW_OFFSET becomes a simpler constant, requiring only one
    instruction to load instead of two. Since it must be loaded in each
    function with a tag check, this decreases kernel text size by 0.5%.
    
    3) This shift and the sign extension from kasan_reset_tag() can be
    combined into a single sbfx instruction. When this same algorithm change
    is applied to the compiler, it removes an instruction from each inline
    tag check, further reducing kernel text size by an additional 4.6%.
    
    These benefits extend to other architectures as well. On RISC-V, where
    the baseline ISA does not shifted addition or have an equivalent to the
    sbfx instruction, loading KASAN_SHADOW_OFFSET is reduced from 3 to 2
    instructions, and kasan_mem_to_shadow(kasan_reset_tag(addr)) similarly
    combines two consecutive right shifts.
    
    Due to signed memory-to-shadow mapping kasan_non_canonical_hook() needs
    changes - specifically the first part that tries to deduce if a faulty
    address came from kasan_mem_to_shadow(). Previous value of
    KASAN_SHADOW_OFFSET prevented any overflows when trying to map the
    entire linear address space to shadow memory so the check in
    kasan_non_canonical_hook() could consist of only checking whether the
    address isn't below KASAN_SHADOW_OFFSET.
    
    The signed memory-to-shadow conversion means negative addresses will be
    mapped below KASAN_SHADOW_OFFSET and positive addresses will map above
    KASAN_SHADOW_OFFSET. When looking at the mapping of the entire address
    space there will be an overflow when a big enough positive address will
    be passed to kasan_mem_to_shadow(). Then the question of finding
    addresses that couldn't come from kasan_mem_to_shadow() can be reduced
    to figuring out if the address isn't above the highest overflowed value
    (most positive address possible) AND below the most negative address
    possible.
    
    Link: https://github.com/llvm/llvm-project/blob/llvmorg-20-init/llvm/lib/Transforms/Instrumentation/AddressSanitizer.cpp#L1316 [1]
    Link: https://github.com/llvm/llvm-project/blob/llvmorg-20-init/llvm/lib/Transforms/Instrumentation/HWAddressSanitizer.cpp#L895 [2]
    Link: https://github.com/llvm/llvm-project/blob/llvmorg-20-init/llvm/lib/Target/AArch64/AArch64AsmPrinter.cpp#L669 [3]
    Signed-off-by: Samuel Holland <samuel.holland@sifive.com>
    Signed-off-by: Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>

--- mm/kasan/report.c
+++ mm/kasan/report.c
@@ -645,15 +645,33 @@ void kasan_report_async(void)
  */
 void kasan_non_canonical_hook(unsigned long addr)
 {
+	unsigned long max_shadow_size = BIT(BITS_PER_LONG - KASAN_SHADOW_SCALE_SHIFT);
 	unsigned long orig_addr;
 	const char *bug_type;
 
 	/*
-	 * All addresses that came as a result of the memory-to-shadow mapping
-	 * (even for bogus pointers) must be >= KASAN_SHADOW_OFFSET.
+	 * With the default kasan_mem_to_shadow() algorithm, all addresses
+	 * returned by the memory-to-shadow mapping (even for bogus pointers)
+	 * must be within a certain displacement from KASAN_SHADOW_OFFSET.
+	 *
+	 * For Generic KASAN the displacement is unsigned so the mapping from zero
+	 * to the last kernel address needs checking.
+	 *
+	 * For Software Tag-Based KASAN, the displacement is signed, so
+	 * KASAN_SHADOW_OFFSET is the center of the range. Higher positive
+	 * addresses overflow, so the range that can't be part of
+	 * memory-to-shadow mapping is above the biggest positive address
+	 * mapping and below the lowest possible one.
 	 */
-	if (addr < KASAN_SHADOW_OFFSET)
-		return;
+	if (IS_ENABLED(CONFIG_KASAN_GENERIC)) {
+		if (addr < KASAN_SHADOW_OFFSET ||
+		    addr >= KASAN_SHADOW_OFFSET + max_shadow_size)
+			return;
+	} else {
+		if (addr < KASAN_SHADOW_OFFSET - max_shadow_size / 2 &&
+		    addr >= KASAN_SHADOW_OFFSET + max_shadow_size / 2)
+			return;
+	}
 
 	orig_addr = (unsigned long)kasan_shadow_to_mem((void *)addr);
 
