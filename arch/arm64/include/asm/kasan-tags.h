f4b21694c47637ee14abaf67d87fa951a8422d7f
Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>
2025-02-18 09:15:21 +0100

    kasan: arm64: x86: Make special tags arch specific
    KASAN's tag-based mode defines multiple special tag values. They're
    reserved for:
    - Native kernel value. On arm64 it's 0xFF and it causes an early return
      in the tag checking function.
    - Invalid value. 0xFE marks an area as freed / unallocated. It's also
      the value that is used to initialize regions of shadow memory.
    - Max value. 0xFD is the highest value that can be randomly generated
      for a new tag.
    
    Metadata macro is also defined:
    - Tag width equal to 8.
    
    Tag-based mode on x86 is going to use 4 bit wide tags so all the above
    values need to be changed accordingly.
    
    Make native kernel tag arch specific for x86 and arm64.
    
    Replace hardcoded kernel tag value and tag width with macros in KASAN's
    non-arch specific code.
    
    Signed-off-by: Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>

---
+++ arch/arm64/include/asm/kasan-tags.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef __ASM_KASAN_TAGS_H
+#define __ASM_KASAN_TAGS_H
+
+#define KASAN_TAG_KERNEL	0xFF /* native kernel pointers tag */
+
+#define KASAN_TAG_WIDTH 8
+
+#endif /* ASM_KASAN_TAGS_H */
