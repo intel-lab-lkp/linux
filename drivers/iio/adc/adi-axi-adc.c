1c839ad341bb566c43e4b9750c87b8954c919c28
Nuno Sá <nuno.sa@analog.com>
2025-02-18 10:34:57 +0000

    iio: adc: adi-axi-adc: replace of.h with mod_devicetable.h
    Don't use of.h in order to include mod_devicetable.h. Use it directly as
    there no direct dependency on OF.h
    
    Signed-off-by: Nuno Sá <nuno.sa@analog.com>
    Reviewed-by: David Lechner <dlechner@baylibre.com>
    Link: https://patch.msgid.link/20250218-dev-axi-adc-fix-headers-v1-1-5ddc79221d8c@analog.com
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/iio/adc/adi-axi-adc.c
+++ drivers/iio/adc/adi-axi-adc.c
@@ -14,7 +14,7 @@
 #include <linux/delay.h>
 #include <linux/module.h>
 #include <linux/mutex.h>
-#include <linux/of.h>
+#include <linux/mod_devicetable.h>
 #include <linux/platform_device.h>
 #include <linux/property.h>
 #include <linux/regmap.h>
