ac4e018b366dd307f778e0bb08baea28d7f15f3f
Mario Limonciello <mario.limonciello@amd.com>
2025-02-19 15:02:54 -0600

    cpufreq/amd-pstate-ut: Adjust variable scope for amd_pstate_ut_check_freq()
    The cpudata variable is only needed in the scope of the for loop. Move it
    there.
    
    Reviewed-by: Gautham R. Shenoy <gautham.shenoy@amd.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- drivers/cpufreq/amd-pstate-ut.c
+++ drivers/cpufreq/amd-pstate-ut.c
@@ -186,10 +186,10 @@ static int amd_pstate_ut_check_perf(u32 index)
 static int amd_pstate_ut_check_freq(u32 index)
 {
 	int cpu = 0;
-	struct amd_cpudata *cpudata = NULL;
 
 	for_each_online_cpu(cpu) {
 		struct cpufreq_policy *policy __free(put_cpufreq_policy) = NULL;
+		struct amd_cpudata *cpudata;
 
 		policy = cpufreq_cpu_get(cpu);
 		if (!policy)
