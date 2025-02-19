3d663aef9a90fba911e3ad7706f87f26e1bc8022
Jeongjun Park <aha310510@gmail.com>
2025-02-19 22:29:05 +0900

    ipc: fix to protect IPCS lookups using RCU
    In shm_destroy_orphaned(), we are not performing updates to the IPCS and are
    only calling idr_for_each(), which can be protected by the RCU read-critical
    section.
    
    And if idr_for_each() is not protected by the RCU read-critical section,
    then when radix_tree_node_free() is called to free the struct radix_tree_node
    through call_rcu(), the node will be freed immediately, and when reading the
    next node in radix_tree_for_each_slot(), the memory that has already been
    freed may be read.
    
    Therefore, when calling idr_for_each() in shm_destroy_orphaned(), it should
    be modify to protect it within the RCU read critical section.
    
    Reported-by: syzbot+a2b84e569d06ca3a949c@syzkaller.appspotmail.com
    Fixes: b34a6b1da371 ("ipc: introduce shm_rmid_forced sysctl")
    Signed-off-by: Jeongjun Park <aha310510@gmail.com>

--- ipc/shm.c
+++ ipc/shm.c
@@ -431,8 +431,10 @@ static int shm_try_destroy_orphaned(int id, void *p, void *data)
 void shm_destroy_orphaned(struct ipc_namespace *ns)
 {
 	down_write(&shm_ids(ns).rwsem);
+	rcu_read_lock();
 	if (shm_ids(ns).in_use)
 		idr_for_each(&shm_ids(ns).ipcs_idr, &shm_try_destroy_orphaned, ns);
+	rcu_read_unlock();
 	up_write(&shm_ids(ns).rwsem);
 }
 
