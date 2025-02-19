30ed8036de766cab33b2729c81a4884844a78af2
Lukasz Majewski <lukma@denx.de>
2025-02-19 12:49:36 +0100

    arm: clk: Add ETH switch clock description for vf610 SoC
    The NXP's vf610 soc is equipped with L2 switch IP block from More
    Than IP (MTIP) vendor.
    
    It requires special clock (VF610_CLK_ESW) to be operational.
    
    Signed-off-by: Lukasz Majewski <lukma@denx.de>

--- include/dt-bindings/clock/vf610-clock.h
+++ include/dt-bindings/clock/vf610-clock.h
@@ -197,6 +197,7 @@
 #define VF610_CLK_TCON1			188
 #define VF610_CLK_CAAM			189
 #define VF610_CLK_CRC			190
-#define VF610_CLK_END			191
+#define VF610_CLK_ESW			191
+#define VF610_CLK_END			192
 
 #endif /* __DT_BINDINGS_CLOCK_VF610_H */
