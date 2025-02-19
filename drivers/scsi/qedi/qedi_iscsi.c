f8a0d1f982e6f09f6ba594a8a2b6f2705de5627c
Denis Plotnikov <den-plotnikov@yandex-team.ru>
2025-02-19 18:00:43 +0300

    qedi_iscsi: remove dead code from qedi_process_iscsi_error()
    No functional changes, just dead code removal.
    
    Signed-off-by: Denis Plotnikov <den-plotnikov@yandex-team.ru>

--- drivers/scsi/qedi/qedi_iscsi.c
+++ drivers/scsi/qedi/qedi_iscsi.c
@@ -1649,12 +1649,6 @@ void qedi_process_iscsi_error(struct qedi_endpoint *ep,
 {
 	struct qedi_conn *qedi_conn;
 	struct qedi_ctx *qedi;
-	char warn_notice[] = "iscsi_warning";
-	char error_notice[] = "iscsi_error";
-	char unknown_msg[] = "Unknown error";
-	char *message;
-	int need_recovery = 0;
-	u32 err_mask = 0;
 	char *msg;
 
 	if (!ep)
@@ -1669,25 +1663,14 @@ void qedi_process_iscsi_error(struct qedi_endpoint *ep,
 	QEDI_ERR(&qedi->dbg_ctx, "async event iscsi error:0x%x\n",
 		 data->error_code);
 
-	if (err_mask) {
-		need_recovery = 0;
-		message = warn_notice;
-	} else {
-		need_recovery = 1;
-		message = error_notice;
-	}
-
 	msg = qedi_get_iscsi_error(data->error_code);
-	if (!msg) {
-		need_recovery = 0;
-		msg = unknown_msg;
-	}
 
 	iscsi_conn_printk(KERN_ALERT,
 			  qedi_conn->cls_conn->dd_data,
-			  "qedi: %s - %s\n", message, msg);
+			  "qedi: %s - %s\n", "iscsi_error",
+			  msg ? msg : "Unknown error");
 
-	if (need_recovery)
+	if (msg)
 		qedi_start_conn_recovery(qedi_conn->qedi, qedi_conn);
 }
 
