fbd58b9f8a8f578a3856822c897e4247214be869
Jonathan Cavitt <jonathan.cavitt@intel.com>
2025-02-19 20:28:42 +0000

    drm/xe/xe_gt_pagefault: Migrate pagefault struct to header
    Migrate the pagefault struct from xe_gt_pagefault.c to the
    xe_gt_pagefault.h header file, along with the associated enum values.
    
    Signed-off-by: Jonathan Cavitt <jonathan.cavitt@intel.com>

--- drivers/gpu/drm/xe/xe_gt_pagefault.h
+++ drivers/gpu/drm/xe/xe_gt_pagefault.h
@@ -11,6 +11,34 @@
 struct xe_gt;
 struct xe_guc;
 
+struct pagefault {
+	u64 page_addr;
+	u32 asid;
+	u16 pdata;
+	u8 vfid;
+	u8 access_type;
+	u8 fault_type;
+	u8 fault_level;
+	u8 engine_class;
+	u8 engine_instance;
+	u8 fault_unsuccessful;
+	bool prefetch;
+	bool trva_fault;
+};
+
+enum access_type {
+	ACCESS_TYPE_READ = 0,
+	ACCESS_TYPE_WRITE = 1,
+	ACCESS_TYPE_ATOMIC = 2,
+	ACCESS_TYPE_RESERVED = 3,
+};
+
+enum fault_type {
+	NOT_PRESENT = 0,
+	WRITE_ACCESS_VIOLATION = 1,
+	ATOMIC_ACCESS_VIOLATION = 2,
+};
+
 int xe_gt_pagefault_init(struct xe_gt *gt);
 void xe_gt_pagefault_reset(struct xe_gt *gt);
 int xe_guc_pagefault_handler(struct xe_guc *guc, u32 *msg, u32 len);
