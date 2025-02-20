feb4b8d13d683ea61bd545f69a09d39ba5afee69
Sergio Lopez <slp@redhat.com>
2025-02-20 23:40:32 +0100

    virtio-mmio: read shm region page size
    Use the newly introduced SHM_PAGE_SHIFT register to read the page shift
    for the shared memory region, derive the page size from it and store the
    resulting value into virtio_shm_region.
    
    Reviewed-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- drivers/virtio/virtio_mmio.c
+++ drivers/virtio/virtio_mmio.c
@@ -537,6 +537,7 @@ static bool vm_get_shm_region(struct virtio_device *vdev,
 			      struct virtio_shm_region *region, u8 id)
 {
 	struct virtio_mmio_device *vm_dev = to_virtio_mmio_device(vdev);
+	u8 page_shift = 0;
 	u64 len, addr;
 
 	/* Select the region we're interested in */
@@ -560,7 +561,24 @@ static bool vm_get_shm_region(struct virtio_device *vdev,
 
 	region->addr = addr;
 
-	region->page_size = 4096;
+	/* If supported by the device transport, read the region page size.
+	 * The page_shift variable is initialized to zero above, so if this
+	 * feature isn't supported it will result in a page_size of 4096, a
+	 * default safe value.
+	 */
+	if (__virtio_test_bit(vdev, VIRTIO_F_SHM_PAGE_SIZE)) {
+		page_shift = (u8) readl(vm_dev->base + VIRTIO_MMIO_SHM_PAGE_SHIFT);
+		/* The value derived from page_shift must fit into a 32-bit
+		 * unsigned integer.
+		 */
+		if ((page_shift + 12) >= 32) {
+			dev_err(&vdev->dev, "%s: device returned an invalid page shift: %d\n",
+				__func__, page_shift);
+			return false;
+		}
+	}
+
+	region->page_size = 1U << (page_shift + 12);
 
 	return true;
 }
