8b6954f21417ae4d5831e7fe9e035d368d756fb2
Byungchul Park <byungchul@sk.com>
2025-02-20 14:20:20 +0900

    mm: skip luf tlb flush for luf'd mm that already has been done
    Fault hander performs tlb flush pended by luf when a new pte becomes
    to have write permission, no matter whether tlb flush required has been
    performed or not.
    
    By storing luf generation number, luf_ugen, in struct mm_struct, we can
    skip unnecessary tlb flush.
    
    Signed-off-by: Byungchul Park <byungchul@sk.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1268,6 +1268,7 @@ static struct mm_struct *mm_init(struct mm_struct *mm, struct task_struct *p,
 	memset(&mm->rss_stat, 0, sizeof(mm->rss_stat));
 	spin_lock_init(&mm->page_table_lock);
 	spin_lock_init(&mm->arg_lock);
+	luf_batch_init(&mm->luf_batch);
 	mm_init_cpumask(mm);
 	mm_init_aio(mm);
 	mm_init_owner(mm, p);
