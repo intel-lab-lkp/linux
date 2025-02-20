ca80fc586ce61663cf9d7a077f937b12eb7fbe0f
FUJITA Tomonori <fujita.tomonori@gmail.com>
2025-02-20 16:06:07 +0900

    rust: time: Add wrapper for fsleep() function
    Add a wrapper for fsleep(), flexible sleep functions in
    include/linux/delay.h which typically deals with hardware delays.
    
    The kernel supports several sleep functions to handle various lengths
    of delay. This adds fsleep(), automatically chooses the best sleep
    method based on a duration.
    
    sleep functions including fsleep() belongs to TIMERS, not
    TIMEKEEPING. They are maintained separately. rust/kernel/time.rs is an
    abstraction for TIMEKEEPING. To make Rust abstractions match the C
    side, add rust/kernel/time/delay.rs for this wrapper.
    
    fsleep() can only be used in a nonatomic context. This requirement is
    not checked by these abstractions, but it is intended that klint [1]
    or a similar tool will be used to check it in the future.
    
    Link: https://rust-for-linux.com/klint [1]
    Tested-by: Daniel Almeida <daniel.almeida@collabora.com>
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Fiona Behrens <me@kloenk.dev>
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@gmail.com>

--- rust/kernel/time.rs
+++ rust/kernel/time.rs
@@ -24,6 +24,8 @@
 //! C header: [`include/linux/jiffies.h`](srctree/include/linux/jiffies.h).
 //! C header: [`include/linux/ktime.h`](srctree/include/linux/ktime.h).
 
+pub mod delay;
+
 /// The number of nanoseconds per microsecond.
 pub const NSEC_PER_USEC: i64 = bindings::NSEC_PER_USEC as i64;
 
