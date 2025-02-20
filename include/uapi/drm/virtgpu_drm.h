e383572e3c9b25dce6dc33d464b265dacd8554a1
Sergio Lopez <slp@redhat.com>
2025-02-20 15:28:53 +0100

    drm/virtio: add VIRTGPU_PARAM_HOST_SHM_PAGE_SIZE to params
    Add VIRTGPU_PARAM_HOST_SHM_PAGE_SIZE as a param that can be read with
    VIRTGPU_GETPARAM by userspace applications running in the guest to
    obtain the host's page size and find out the right alignment to be used
    in shared memory allocations.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- include/uapi/drm/virtgpu_drm.h
+++ include/uapi/drm/virtgpu_drm.h
@@ -98,6 +98,7 @@ struct drm_virtgpu_execbuffer {
 #define VIRTGPU_PARAM_CONTEXT_INIT 6 /* DRM_VIRTGPU_CONTEXT_INIT */
 #define VIRTGPU_PARAM_SUPPORTED_CAPSET_IDs 7 /* Bitmask of supported capability set ids */
 #define VIRTGPU_PARAM_EXPLICIT_DEBUG_NAME 8 /* Ability to set debug name from userspace */
+#define VIRTGPU_PARAM_HOST_SHM_PAGE_SIZE 9 /* Host SHM page size, with format PAGE_SIZE >> 12 */
 
 struct drm_virtgpu_getparam {
 	__u64 param;
