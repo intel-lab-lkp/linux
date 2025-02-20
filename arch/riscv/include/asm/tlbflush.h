4a1154e1dbc2d958e216e2c42491b76ba9a95496
Byungchul Park <byungchul@sk.com>
2025-02-20 14:20:04 +0900

    riscv/tlb: add APIs manipulating tlb batch's arch data
    A new mechanism, LUF(Lazy Unmap Flush), defers tlb flush until folios
    that have been unmapped and freed, eventually get allocated again.  It's
    safe for folios that had been mapped read only and were unmapped, since
    the contents of the folios don't change while staying in pcp or buddy
    so we can still read the data through the stale tlb entries.
    
    This is a preparation for the mechanism that needs to recognize
    read-only tlb entries by separating tlb batch arch data into two, one is
    for read-only entries and the other is for writable ones, and merging
    those two when needed.
    
    It also optimizes tlb shootdown by skipping CPUs that have already
    performed tlb flush needed since.  To support it, added APIs
    manipulating arch data for riscv.
    
    Signed-off-by: Byungchul Park <byungchul@sk.com>

--- arch/riscv/include/asm/tlbflush.h
+++ arch/riscv/include/asm/tlbflush.h
@@ -8,6 +8,7 @@
 #define _ASM_RISCV_TLBFLUSH_H
 
 #include <linux/mm_types.h>
+#include <linux/cpumask.h>
 #include <asm/smp.h>
 #include <asm/errata_list.h>
 
@@ -64,6 +65,33 @@ void arch_tlbbatch_add_pending(struct arch_tlbflush_unmap_batch *batch,
 			       unsigned long uaddr);
 void arch_flush_tlb_batched_pending(struct mm_struct *mm);
 void arch_tlbbatch_flush(struct arch_tlbflush_unmap_batch *batch);
+
+static inline void arch_tlbbatch_clear(struct arch_tlbflush_unmap_batch *batch)
+{
+	cpumask_clear(&batch->cpumask);
+
+}
+
+static inline void arch_tlbbatch_fold(struct arch_tlbflush_unmap_batch *bdst,
+		struct arch_tlbflush_unmap_batch *bsrc)
+{
+	cpumask_or(&bdst->cpumask, &bdst->cpumask, &bsrc->cpumask);
+
+}
+
+static inline bool arch_tlbbatch_need_fold(struct arch_tlbflush_unmap_batch *batch,
+		struct mm_struct *mm)
+{
+	return !cpumask_subset(mm_cpumask(mm), &batch->cpumask);
+
+}
+
+static inline bool arch_tlbbatch_done(struct arch_tlbflush_unmap_batch *bdst,
+		struct arch_tlbflush_unmap_batch *bsrc)
+{
+	return !cpumask_andnot(&bdst->cpumask, &bdst->cpumask, &bsrc->cpumask);
+
+}
 
 extern unsigned long tlb_flush_all_threshold;
 #else /* CONFIG_MMU */
