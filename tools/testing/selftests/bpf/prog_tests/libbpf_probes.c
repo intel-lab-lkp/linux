3886cabedd68f734107f1d3760d42675e9091321
Tao Chen <chen.dylane@linux.dev>
2025-02-22 00:33:35 +0800

    selftests/bpf: Add libbpf_probe_bpf_kfunc API selftests
    Add selftests for prog_kfunc feature probing. Thanks for
    Eduard providing the libbpf_probe_func_many test case.
    
     ./test_progs -t libbpf_probe_kfuncs
     #153     libbpf_probe_kfuncs:OK
     Summary: 1/0 PASSED, 0 SKIPPED, 0 FAILED
    
     ./test_progs -t libbpf_probe_kfuncs_many
     #154     libbpf_probe_kfuncs_many:OK
     Summary: 1/0 PASSED, 0 SKIPPED, 0 FAILED
    
    Cc: Tao Chen <dylane.chen@didiglobal.com>
    Reviewed-by: Jiri Olsa <jolsa@kernel.org>
    Co-developed-by: Eduard Zingerman <eddyz87@gmail.com>
    Signed-off-by: Eduard Zingerman <eddyz87@gmail.com>
    Signed-off-by: Tao Chen <chen.dylane@linux.dev>

--- tools/testing/selftests/bpf/prog_tests/libbpf_probes.c
+++ tools/testing/selftests/bpf/prog_tests/libbpf_probes.c
@@ -126,3 +126,176 @@ void test_libbpf_probe_helpers(void)
 		ASSERT_EQ(res, d->supported, buf);
 	}
 }
+
+static int module_btf_fd(char *module)
+{
+	int fd, err;
+	__u32 id = 0, len;
+	struct bpf_btf_info info;
+	char name[64];
+
+	while (true) {
+		err = bpf_btf_get_next_id(id, &id);
+		if (err)
+			return -1;
+
+		fd = bpf_btf_get_fd_by_id(id);
+		if (fd < 0) {
+			if (errno == ENOENT)
+				continue;
+			return -1;
+		}
+		len = sizeof(info);
+		memset(&info, 0, sizeof(info));
+		info.name = ptr_to_u64(name);
+		info.name_len = sizeof(name);
+		err = bpf_btf_get_info_by_fd(fd, &info, &len);
+		if (err) {
+			close(fd);
+			return -1;
+		}
+		/* find target module BTF */
+		if (!strcmp(name, module))
+			break;
+
+		close(fd);
+	}
+
+	return fd;
+}
+
+void test_libbpf_probe_kfuncs(void)
+{
+	int ret, kfunc_id, fd;
+	char *kfunc = "bpf_cpumask_create";
+	struct btf *vmlinux_btf = NULL;
+	struct btf *module_btf = NULL;
+
+	vmlinux_btf = btf__parse("/sys/kernel/btf/vmlinux", NULL);
+	if (!ASSERT_OK_PTR(vmlinux_btf, "btf_parse"))
+		return;
+
+	kfunc_id = btf__find_by_name_kind(vmlinux_btf, kfunc, BTF_KIND_FUNC);
+	if (!ASSERT_GT(kfunc_id, 0, kfunc))
+		goto cleanup;
+
+	/* prog BPF_PROG_TYPE_SYSCALL supports kfunc bpf_cpumask_create */
+	ret = libbpf_probe_bpf_kfunc(BPF_PROG_TYPE_SYSCALL, kfunc_id, -1, NULL);
+	if (!ASSERT_EQ(ret, 1, "kfunc in vmlinux support"))
+		goto cleanup;
+
+	/* prog BPF_PROG_TYPE_KPROBE does not support kfunc bpf_cpumask_create */
+	ret = libbpf_probe_bpf_kfunc(BPF_PROG_TYPE_KPROBE, kfunc_id, -1, NULL);
+	if (!ASSERT_EQ(ret, 0, "kfunc in vmlinux not suuport"))
+		goto cleanup;
+
+	ret = libbpf_probe_bpf_kfunc(BPF_PROG_TYPE_KPROBE, -1, -1, NULL);
+	if (!ASSERT_EQ(ret, 0, "invalid kfunc id:-1"))
+		goto cleanup;
+
+	ret = libbpf_probe_bpf_kfunc(100000, kfunc_id, -1, NULL);
+	if (!ASSERT_ERR(ret, "invalid prog type:100000"))
+		goto cleanup;
+
+	if (!env.has_testmod)
+		goto cleanup;
+
+	module_btf = btf__load_module_btf("bpf_testmod", vmlinux_btf);
+	if (!ASSERT_OK_PTR(module_btf, "load module BTF"))
+		goto cleanup;
+
+	kfunc_id = btf__find_by_name(module_btf, "bpf_kfunc_call_test1");
+	if (!ASSERT_GT(kfunc_id, 0, "func not found"))
+		goto cleanup;
+
+	fd = module_btf_fd("bpf_testmod");
+	if (!ASSERT_GE(fd, 0, "module BTF fd"))
+		goto cleanup;
+
+	/* prog BPF_PROG_TYPE_SYSCALL supports kfunc bpf_kfunc_call_test1 in bpf_testmod */
+	ret = libbpf_probe_bpf_kfunc(BPF_PROG_TYPE_SYSCALL, kfunc_id, fd, NULL);
+	if (!ASSERT_EQ(ret, 1, "kfunc in module BTF support"))
+		goto cleanup_fd;
+
+	/* prog BPF_PROG_TYPE_KPROBE does not support kfunc bpf_kfunc_call_test1
+	 * in bpf_testmod
+	 */
+	ret = libbpf_probe_bpf_kfunc(BPF_PROG_TYPE_KPROBE, kfunc_id, fd, NULL);
+	if (!ASSERT_EQ(ret, 0, "kfunc in module BTF not support"))
+		goto cleanup_fd;
+
+	ret = libbpf_probe_bpf_kfunc(BPF_PROG_TYPE_SYSCALL, -1, fd, NULL);
+	if (!ASSERT_EQ(ret, 0, "invalid kfunc id in module BTF"))
+		goto cleanup_fd;
+
+	ret = libbpf_probe_bpf_kfunc(BPF_PROG_TYPE_SYSCALL, kfunc_id, 100, NULL);
+	ASSERT_EQ(ret, 0, "invalid BTF fd in module BTF");
+
+cleanup_fd:
+	close(fd);
+cleanup:
+	btf__free(vmlinux_btf);
+	btf__free(module_btf);
+}
+
+static const struct {
+	const char *name;
+	int code;
+} program_types[] = {
+#define _T(n) { #n, BPF_PROG_TYPE_##n }
+	_T(KPROBE),
+	_T(XDP),
+	_T(SYSCALL),
+	_T(SCHED_CLS),
+	_T(SCHED_ACT),
+	_T(SK_SKB),
+	_T(SOCKET_FILTER),
+	_T(CGROUP_SKB),
+	_T(LWT_OUT),
+	_T(LWT_IN),
+	_T(LWT_XMIT),
+	_T(LWT_SEG6LOCAL),
+	_T(NETFILTER),
+	_T(CGROUP_SOCK_ADDR),
+	_T(SCHED_ACT)
+#undef _T
+};
+
+void test_libbpf_probe_kfuncs_many(void)
+{
+	int i, kfunc_id, ret, id;
+	const struct btf_type *t;
+	struct btf *btf = NULL;
+	const char *kfunc;
+	const char *tag;
+
+	btf = btf__parse("/sys/kernel/btf/vmlinux", NULL);
+	if (!ASSERT_OK_PTR(btf, "btf_parse"))
+		return;
+	for (id = 0; id < btf__type_cnt(btf); ++id) {
+		t = btf__type_by_id(btf, id);
+		if (!t)
+			continue;
+		if (!btf_is_decl_tag(t))
+			continue;
+		tag = btf__name_by_offset(btf, t->name_off);
+		if (strcmp(tag, "bpf_kfunc") != 0)
+			continue;
+		kfunc_id = t->type;
+		t = btf__type_by_id(btf, kfunc_id);
+		if (!btf_is_func(t))
+			continue;
+		kfunc = btf__name_by_offset(btf, t->name_off);
+		for (i = 0; i < ARRAY_SIZE(program_types); ++i) {
+			ret = libbpf_probe_bpf_kfunc(program_types[i].code,
+						     kfunc_id, -1, NULL);
+			if (ret < 0) {
+				ASSERT_FAIL("kfunc:%s use prog type:%d",
+				      kfunc, program_types[i].code);
+				goto cleanup;
+			}
+		}
+	}
+cleanup:
+	btf__free(btf);
+}
