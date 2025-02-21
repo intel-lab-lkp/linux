a59e2c206e29564c3a05780e39e15f003d55b31c
Pratik R. Sampat <prsampat@amd.com>
2025-02-21 15:01:54 -0600

    KVM: selftests: Add SMT control state helper
    Move the SMT control check out of the hyperv_cpuid selftest so that
    it is generally accessible all selftests. Split the functionality into
    a helper that populates a buffer with SMT control value which other
    helpers can use to ascertain if SMT state is available and active.
    
    Signed-off-by: Pratik R. Sampat <prsampat@amd.com>

--- tools/testing/selftests/kvm/x86/hyperv_cpuid.c
+++ tools/testing/selftests/kvm/x86/hyperv_cpuid.c
@@ -22,25 +22,6 @@ static void guest_code(void)
 {
 }
 
-static bool smt_possible(void)
-{
-	char buf[16];
-	FILE *f;
-	bool res = true;
-
-	f = fopen("/sys/devices/system/cpu/smt/control", "r");
-	if (f) {
-		if (fread(buf, sizeof(*buf), sizeof(buf), f) > 0) {
-			if (!strncmp(buf, "forceoff", 8) ||
-			    !strncmp(buf, "notsupported", 12))
-				res = false;
-		}
-		fclose(f);
-	}
-
-	return res;
-}
-
 static void test_hv_cpuid(struct kvm_vcpu *vcpu, bool evmcs_expected)
 {
 	const bool has_irqchip = !vcpu || vcpu->vm->has_irqchip;
