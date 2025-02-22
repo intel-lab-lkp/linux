78577c552c9e671f45f5112f25191a5fa6a17e68
Baihan Li <libaihan@huawei.com>
2025-02-22 10:50:56 +0800

    drm/hisilicon/hibmc: Add dp serdes cfg in dp process
    Add dp serdes cfg in link training process, and related adapting
    and modificating. Change some init values about training,
    because we want completely to negotiation process, so we start with
    the maximum rate and the electrical characteristic level is 0.
    
    Signed-off-by: Baihan Li <libaihan@huawei.com>
    Signed-off-by: Yongbang Shi <shiyongbang@huawei.com>

--- drivers/gpu/drm/hisilicon/hibmc/dp/dp_serdes.h
+++ drivers/gpu/drm/hisilicon/hibmc/dp/dp_serdes.h
@@ -4,12 +4,15 @@
 #ifndef DP_SERDES_H
 #define DP_SERDES_H
 
+#include "dp_comm.h"
+
 #define HIBMC_DP_HOST_OFFSET		0x10000
 #define HIBMC_DP_LANE0_RATE_OFFSET	0x4
 #define HIBMC_DP_LANE1_RATE_OFFSET	0xc
 #define HIBMC_DP_LANE_STATUS_OFFSET	0x10
 #define HIBMC_DP_PMA_LANE0_OFFSET	0x18
 #define HIBMC_DP_PMA_LANE1_OFFSET	0x1c
+#define HIBMC_DP_HOST_SERDES_CTRL	0x1f001c
 #define HIBMC_DP_PMA_TXDEEMPH		GENMASK(18, 1)
 
 /* dp serdes TX-Deempth Configuration */
@@ -24,6 +27,9 @@
 #define DP_SERDES_VOL2_PRE1		0x4500
 #define DP_SERDES_VOL3_PRE0		0x600
 #define DP_SERDES_BW_8_1		0x3
+#define DP_SERDES_BW_5_4		0x2
+#define DP_SERDES_BW_2_7		0x1
+#define DP_SERDES_BW_1_62		0x0
 
 #define DP_SERDES_DONE			0x3
 
