0e0e7678392a94d0246344fb1e0576a375e0660c
Aleksander Jan Bajkowski <olek2@wp.pl>
2025-02-22 19:38:13 +0100

    net: phy: add LED dimming support
    Some PHYs support LED dimming. The use case is a router that dims LEDs
    at night. PHYs from different manufacturers support a different number of
    brightness levels, so it was necessary to extend the API with the
    led_max_brightness() function. If this function is omitted, a default
    value is used, assuming that only two levels are supported.
    
    Signed-off-by: Aleksander Jan Bajkowski <olek2@wp.pl>
    Reviewed-by: Daniel Golle <daniel@makrotopia.org>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -3368,7 +3368,12 @@ static int of_phy_led(struct phy_device *phydev,
 
 	cdev->hw_control_get_device = phy_led_hw_control_get_device;
 #endif
-	cdev->max_brightness = 1;
+	if (phydev->drv->led_max_brightness)
+		cdev->max_brightness =
+			phydev->drv->led_max_brightness(phydev, index);
+	else
+		cdev->max_brightness = 1;
+
 	init_data.devicename = dev_name(&phydev->mdio.dev);
 	init_data.fwnode = of_fwnode_handle(led);
 	init_data.devname_mandatory = true;
