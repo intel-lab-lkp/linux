5601716f448da12e68b56c8adee2b609605829f7
Sean Christopherson <seanjc@google.com>
2025-02-24 15:55:39 -0800

    KVM: x86: Don't load/put vCPU when unloading its MMU during teardown
    Don't load (and then put) a vCPU when unloading its MMU during VM
    destruction, as nothing in kvm_mmu_unload() accesses vCPU state beyond the
    root page/address of each MMU, i.e. can't possible need to run with the
    vCPU loaded.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -12767,13 +12767,6 @@ out:
 	return ret;
 }
 
-static void kvm_unload_vcpu_mmu(struct kvm_vcpu *vcpu)
-{
-	vcpu_load(vcpu);
-	kvm_mmu_unload(vcpu);
-	vcpu_put(vcpu);
-}
-
 static void kvm_unload_vcpu_mmus(struct kvm *kvm)
 {
 	unsigned long i;
@@ -12781,7 +12774,7 @@ static void kvm_unload_vcpu_mmus(struct kvm *kvm)
 
 	kvm_for_each_vcpu(i, vcpu, kvm) {
 		kvm_clear_async_pf_completion_queue(vcpu);
-		kvm_unload_vcpu_mmu(vcpu);
+		kvm_mmu_unload(vcpu);
 	}
 }
 
