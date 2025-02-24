fd720b129df8ebc33a387af025e8c760508784a9
Keith Busch <kbusch@kernel.org>
2025-02-24 13:31:11 -0800

    io_uring/rw: move fixed buffer import to issue path
    Registered buffers may depend on a linked command, which makes the prep
    path too early to import. Move to the issue path when the node is
    actually needed like all the other users of fixed buffers.
    
    Signed-off-by: Keith Busch <kbusch@kernel.org>

--- io_uring/opdef.c
+++ io_uring/opdef.c
@@ -104,8 +104,8 @@ const struct io_issue_def io_issue_defs[] = {
 		.iopoll			= 1,
 		.iopoll_queue		= 1,
 		.async_size		= sizeof(struct io_async_rw),
-		.prep			= io_prep_read_fixed,
-		.issue			= io_read,
+		.prep			= io_prep_read,
+		.issue			= io_read_fixed,
 	},
 	[IORING_OP_WRITE_FIXED] = {
 		.needs_file		= 1,
@@ -118,8 +118,8 @@ const struct io_issue_def io_issue_defs[] = {
 		.iopoll			= 1,
 		.iopoll_queue		= 1,
 		.async_size		= sizeof(struct io_async_rw),
-		.prep			= io_prep_write_fixed,
-		.issue			= io_write,
+		.prep			= io_prep_write,
+		.issue			= io_write_fixed,
 	},
 	[IORING_OP_POLL_ADD] = {
 		.needs_file		= 1,
