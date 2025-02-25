52cbd88872549ae1fe9cb2a10f02973362fde447
Maxime Chevallier <maxime.chevallier@bootlin.com>
2025-02-25 12:20:39 +0100

    net: phy: sfp: Add support for SMBus module access
    The SFP module's eeprom and internals are accessible through an i2c bus.
    However, all the i2c transfers that are performed are SMBus-style
    transfers for read and write operations.
    
    It is possible that the SFP might be connected to an SMBus-only
    controller, such as the one found in some PHY devices in the VSC85xx
    family.
    
    Introduce a set of sfp read/write ops that are going to be used if the
    i2c bus is only capable of doing smbus byte accesses.
    
    As Single-byte SMBus transaction go against SFF-8472 and breaks the
    atomicity for diagnostics data access, hwmon is disabled in the case
    of SMBus access.
    
    Moreover, as this may cause other instabilities, print a warning at
    probe time to indicate that the setup may be unreliable because of the
    hardware design.
    
    Tested-by: Sean Anderson <sean.anderson@linux.dev>
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>

--- drivers/net/phy/sfp.c
+++ drivers/net/phy/sfp.c
@@ -282,6 +282,7 @@ struct sfp {
 	unsigned int rs_state_mask;
 
 	bool have_a2;
+	bool single_byte_access;
 
 	const struct sfp_quirk *quirk;
 
@@ -690,14 +691,70 @@ static int sfp_i2c_write(struct sfp *sfp, bool a2, u8 dev_addr, void *buf,
 	return ret == ARRAY_SIZE(msgs) ? len : 0;
 }
 
+static int sfp_smbus_read(struct sfp *sfp, bool a2, u8 dev_addr, void *buf,
+			  size_t len)
+{
+	u8 bus_addr = a2 ? 0x51 : 0x50;
+	union i2c_smbus_data smbus_data;
+	u8 *data = buf;
+	int ret;
+
+	while (len) {
+		ret = i2c_smbus_xfer(sfp->i2c, bus_addr, 0,
+				     I2C_SMBUS_READ, dev_addr,
+				     I2C_SMBUS_BYTE_DATA, &smbus_data);
+		if (ret < 0)
+			return ret;
+
+		*data = smbus_data.byte;
+
+		len--;
+		data++;
+		dev_addr++;
+	}
+
+	return data - (u8 *)buf;
+}
+
+static int sfp_smbus_write(struct sfp *sfp, bool a2, u8 dev_addr, void *buf,
+			   size_t len)
+{
+	u8 bus_addr = a2 ? 0x51 : 0x50;
+	union i2c_smbus_data smbus_data;
+	u8 *data = buf;
+	int ret;
+
+	while (len) {
+		smbus_data.byte = *data;
+		ret = i2c_smbus_xfer(sfp->i2c, bus_addr, 0,
+				     I2C_SMBUS_WRITE, dev_addr,
+				     I2C_SMBUS_BYTE_DATA, &smbus_data);
+		if (ret)
+			return ret;
+
+		len--;
+		data++;
+		dev_addr++;
+	}
+
+	return 0;
+}
+
 static int sfp_i2c_configure(struct sfp *sfp, struct i2c_adapter *i2c)
 {
-	if (!i2c_check_functionality(i2c, I2C_FUNC_I2C))
-		return -EINVAL;
-
 	sfp->i2c = i2c;
-	sfp->read = sfp_i2c_read;
-	sfp->write = sfp_i2c_write;
+
+	if (i2c_check_functionality(i2c, I2C_FUNC_I2C)) {
+		sfp->read = sfp_i2c_read;
+		sfp->write = sfp_i2c_write;
+	} else if (i2c_check_functionality(i2c, I2C_FUNC_SMBUS_BYTE_DATA)) {
+		sfp->read = sfp_smbus_read;
+		sfp->write = sfp_smbus_write;
+		sfp->single_byte_access = true;
+	} else {
+		sfp->i2c = NULL;
+		return -EINVAL;
+	}
 
 	return 0;
 }
@@ -1628,7 +1685,8 @@ static void sfp_hwmon_probe(struct work_struct *work)
 
 static int sfp_hwmon_insert(struct sfp *sfp)
 {
-	if (sfp->have_a2 && sfp->id.ext.diagmon & SFP_DIAGMON_DDM) {
+	if (sfp->have_a2 && sfp->id.ext.diagmon & SFP_DIAGMON_DDM &&
+	    !sfp->single_byte_access) {
 		mod_delayed_work(system_wq, &sfp->hwmon_probe, 1);
 		sfp->hwmon_tries = R_PROBE_RETRY_SLOW;
 	}
@@ -3113,6 +3171,15 @@ static int sfp_probe(struct platform_device *pdev)
 	sfp->sfp_bus = sfp_register_socket(sfp->dev, sfp, &sfp_module_ops);
 	if (!sfp->sfp_bus)
 		return -ENOMEM;
+
+	if (sfp->single_byte_access)
+		dev_warn(sfp->dev,
+			 "Please note:\n"
+			 "This SFP cage is accessed via an SMBus only capable of single byte\n"
+			 "transactions. Some features are disabled, other may be unreliable or\n"
+			 "sporadically fail. Use with caution. There is nothing that the kernel\n"
+			 "or community can do to fix it, the kernel will try best efforts. Please\n"
+			 "verify any problems on hardware that supports multi-byte I2C transactions.\n");
 
 	sfp_debugfs_init(sfp);
 
