b339b0bc6eb2c463875c6b0964db774ff8f6c734
Longfang Liu <liulongfang@huawei.com>
2025-02-25 14:27:55 +0800

    hisi_acc_vfio_pci: bugfix cache write-back issue
    At present, cache write-back is placed in the device data
    copy stage after stopping the device operation.
    Writing back to the cache at this stage will cause the data
    obtained by the cache to be written back to be empty.
    
    In order to ensure that the cache data is written back
    successfully, the data needs to be written back into the
    stop device stage.
    
    Fixes: b0eed085903e ("hisi_acc_vfio_pci: Add support for VFIO live migration")
    Signed-off-by: Longfang Liu <liulongfang@huawei.com>
    Reviewed-by: Shameer Kolothum <shameerali.kolothum.thodi@huawei.com>

--- drivers/vfio/pci/hisilicon/hisi_acc_vfio_pci.c
+++ drivers/vfio/pci/hisilicon/hisi_acc_vfio_pci.c
@@ -565,7 +565,6 @@ static int vf_qm_state_save(struct hisi_acc_vf_core_device *hisi_acc_vdev,
 {
 	struct acc_vf_data *vf_data = &migf->vf_data;
 	struct hisi_qm *vf_qm = &hisi_acc_vdev->vf_qm;
-	struct device *dev = &vf_qm->pdev->dev;
 	int ret;
 
 	if (unlikely(qm_wait_dev_not_ready(vf_qm))) {
@@ -578,12 +577,6 @@ static int vf_qm_state_save(struct hisi_acc_vf_core_device *hisi_acc_vdev,
 
 	vf_data->vf_qm_state = QM_READY;
 	hisi_acc_vdev->vf_qm_state = vf_data->vf_qm_state;
-
-	ret = vf_qm_cache_wb(vf_qm);
-	if (ret) {
-		dev_err(dev, "failed to writeback QM Cache!\n");
-		return ret;
-	}
 
 	ret = vf_qm_read_data(vf_qm, vf_data);
 	if (ret)
@@ -1011,6 +1004,13 @@ static int hisi_acc_vf_stop_device(struct hisi_acc_vf_core_device *hisi_acc_vdev
 		dev_err(dev, "failed to check QM INT state!\n");
 		return ret;
 	}
+
+	ret = vf_qm_cache_wb(vf_qm);
+	if (ret) {
+		dev_err(dev, "failed to writeback QM cache!\n");
+		return ret;
+	}
+
 	return 0;
 }
 
