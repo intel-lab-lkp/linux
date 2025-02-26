85f162a0114040f262f430824156ef574f90f8e7
Jakub Kicinski <kuba@kernel.org>
2025-02-26 13:10:02 -0800

    eth: bnxt: maintain tx pkt/byte stats in SW
    Some workloads want to be able to track bandwidth utilization on
    the scale of 10s of msecs. Updating all HW stats at this rate is
    both hard and wasteful of PCIe bandwidth.
    
    Maintain basic Tx pkt/byte counters in software. ethtool -S will still
    show the HW stats, but qstats and rtnl stats will show SW statistics.
    
    We need to take care of TSO and VF representors, record relevant
    state in tx_buf to avoid touching potentially cold skb. Use existing
    holes in the struct (no size change). Note that according to TX_BD_HSIZE
    max header size is 255, so u8 should be enough.
    
    It's not obvious whether VF representor traffic should be counted,
    on one hand it doesn't belong to the local interface. On the other
    it does go thru normal queuing so Qdisc will show it. I opted to
    _not_ count it.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- drivers/net/ethernet/broadcom/bnxt/bnxt.h
+++ drivers/net/ethernet/broadcom/bnxt/bnxt.h
@@ -877,11 +877,14 @@ struct bnxt_sw_tx_bd {
 		struct sk_buff		*skb;
 		struct xdp_frame	*xdpf;
 	};
+	struct page		*page;
 	DEFINE_DMA_UNMAP_ADDR(mapping);
 	DEFINE_DMA_UNMAP_LEN(len);
-	struct page		*page;
+	u16			extra_segs;
+	u8			hdr_size;
 	u8			is_ts_pkt;
 	u8			is_push;
+	u8			is_vfr;
 	u8			action;
 	unsigned short		nr_frags;
 	union {
@@ -1128,6 +1131,10 @@ struct bnxt_rx_sw_stats {
 
 struct bnxt_tx_sw_stats {
 	u64			tx_resets;
+	/* non-ethtool stats follow */
+	u64			tx_packets;
+	u64			tx_bytes;
+	struct u64_stats_sync	syncp;
 };
 
 struct bnxt_cmn_sw_stats {
@@ -1153,6 +1160,8 @@ struct bnxt_total_ring_drv_stats {
 	/* non-ethtool stats follow */
 	u64			rx_total_packets;
 	u64			rx_total_bytes;
+	u64			tx_total_packets;
+	u64			tx_total_bytes;
 };
 
 struct bnxt_stats_mem {
