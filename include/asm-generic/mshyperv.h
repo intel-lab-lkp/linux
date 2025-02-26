f7b64a7b69dcb7c8c4e4318e4c9fcdc7446b2606
Nuno Das Neves <nunodasneves@linux.microsoft.com>
2025-02-26 15:07:57 -0800

    arm64/hyperv: Add some missing functions to arm64
    These non-nested msr and fast hypercall functions are present in x86,
    but they must be available in both architetures for the root partition
    driver code.
    
    Signed-off-by: Nuno Das Neves <nunodasneves@linux.microsoft.com>

--- include/asm-generic/mshyperv.h
+++ include/asm-generic/mshyperv.h
@@ -72,6 +72,8 @@ extern void * __percpu *hyperv_pcpu_output_arg;
 
 extern u64 hv_do_hypercall(u64 control, void *inputaddr, void *outputaddr);
 extern u64 hv_do_fast_hypercall8(u16 control, u64 input8);
+extern u64 hv_do_fast_hypercall16(u16 control, u64 input1, u64 input2);
+
 bool hv_isolation_type_snp(void);
 bool hv_isolation_type_tdx(void);
 
