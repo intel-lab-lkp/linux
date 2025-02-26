edbcbc08f36b7a562c4634450b09f5542f4e4cf3
LeoLiuoc <LeoLiu-oc@zhaoxin.com>
2025-02-26 20:18:36 +0800

    ACPI: APEI: Add new hest_parse_pcie_aer()
    The purpose of the function apei_hest_parse_aer() is used to parse and
    extract register value from HEST PCIe AER structures. This applies to
    all hardware platforms that has a PCI Express AER structure in HEST.
    
    Signed-off-by: LeoLiuoc <LeoLiu-oc@zhaoxin.com>

--- include/acpi/apei.h
+++ include/acpi/apei.h
@@ -23,6 +23,11 @@ enum hest_status {
 	HEST_NOT_FOUND,
 };
 
+struct hest_parse_aer_info {
+	struct pci_dev *pci_dev;
+	void *data;
+};
+
 extern int hest_disable;
 extern int erst_disable;
 #ifdef CONFIG_ACPI_APEI_GHES
@@ -33,12 +38,18 @@ void __init acpi_ghes_init(void);
 static inline void acpi_ghes_init(void) { }
 #endif
 
+typedef int (*apei_hest_func_t)(struct acpi_hest_header *hest_hdr, void *data);
 int apei_hest_parse(apei_hest_func_t func, void *data);
 
 #ifdef CONFIG_ACPI_APEI
 void __init acpi_hest_init(void);
+int hest_parse_pcie_aer(struct acpi_hest_header *hest_hdr, void *data);
 #else
 static inline void acpi_hest_init(void) { }
+static inline int hest_parse_pcie_aer(struct acpi_hest_header *hest_hdr, void *data)
+{
+	return 0;
+}
 #endif
 
 int erst_write(const struct cper_record_header *record);
