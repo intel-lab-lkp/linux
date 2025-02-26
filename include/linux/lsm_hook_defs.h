5f7c4b5a8ec5d2cd0924984861dfae664a2a260d
Thiébaud Weksteen <tweek@google.com>
2025-02-26 11:20:45 -0800

    lsm, selinux: Add setup_report permission to binder
    Introduce a new permission "setup_report" to the "binder" class.
    This persmission controls the ability to set up the binder generic
    netlink driver to report certain binder transactions.
    
    Signed-off-by: Thiébaud Weksteen <tweek@google.com>
    Signed-off-by: Li Li <dualli@google.com>

--- include/linux/lsm_hook_defs.h
+++ include/linux/lsm_hook_defs.h
@@ -33,6 +33,7 @@ LSM_HOOK(int, 0, binder_transfer_binder, const struct cred *from,
 	 const struct cred *to)
 LSM_HOOK(int, 0, binder_transfer_file, const struct cred *from,
 	 const struct cred *to, const struct file *file)
+LSM_HOOK(int, 0, binder_setup_report, const struct cred *to)
 LSM_HOOK(int, 0, ptrace_access_check, struct task_struct *child,
 	 unsigned int mode)
 LSM_HOOK(int, 0, ptrace_traceme, struct task_struct *parent)
