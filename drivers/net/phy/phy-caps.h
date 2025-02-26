be8e1f254c33f10df843c2c786548efe0d35ff3b
Maxime Chevallier <maxime.chevallier@bootlin.com>
2025-02-26 11:09:20 +0100

    net: phy: phy_caps: Introduce phy_caps_valid
    With the link_capabilities array, it's trivial to validate a given mask
    againts a <speed, duplex> tuple. Create a helper for that purpose, and
    use it to replace a phy_settings lookup in phy_check_valid();
    
    Signed-off-by: Maxime Chevallier <maxime.chevallier@bootlin.com>

--- drivers/net/phy/phy-caps.h
+++ drivers/net/phy/phy-caps.h
@@ -44,6 +44,7 @@ void phy_caps_init(void);
 size_t phy_caps_speeds(unsigned int *speeds, size_t size,
 		       unsigned long *linkmodes);
 void phy_caps_linkmode_max_speed(u32 max_speed, unsigned long *linkmodes);
+bool phy_caps_valid(int speed, int duplex, const unsigned long *linkmodes);
 
 
 #endif /* __PHY_CAPS_H */
