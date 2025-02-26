bbb5c46216497c6b82cec95581ae7d411519838f
Daniel Wagner <wagi@kernel.org>
2025-02-26 19:45:53 +0100

    nvmet-fcloop: remove nport from list on last user
    The nport object has an association with the rport and lport object,
    that means we can only remove an nport object from the global nport_list
    after the last user of an rport or lport is gone.
    
    Signed-off-by: Daniel Wagner <wagi@kernel.org>

--- drivers/nvme/target/fcloop.c
+++ drivers/nvme/target/fcloop.c
@@ -1005,6 +1005,11 @@ fcloop_nport_free(struct kref *ref)
 {
 	struct fcloop_nport *nport =
 		container_of(ref, struct fcloop_nport, ref);
+	unsigned long flags;
+
+	spin_lock_irqsave(&fcloop_lock, flags);
+	list_del(&nport->nport_list);
+	spin_unlock_irqrestore(&fcloop_lock, flags);
 
 	kfree(nport);
 }
@@ -1362,8 +1367,6 @@ __unlink_remote_port(struct fcloop_nport *nport)
 	if (rport && nport->tport)
 		nport->tport->remoteport = NULL;
 	nport->rport = NULL;
-
-	list_del(&nport->nport_list);
 
 	return rport;
 }
