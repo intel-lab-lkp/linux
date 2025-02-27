6888c3ee48e95f0cdb57c7db7047735129f10d77
Hector Martin <marcan@marcan.st>
2025-02-27 22:07:45 +1000

    ASoC: tas2770: Add zero-fill and pull-down controls
    Expose the bits that control the behavior of the SDOUT pin when not
    actively transmitting slot data. Zero-fill is useful when there is a
    single amp on the SDOUT bus (e.g. Apple machines with mono speakers or a
    single stereo pair, where L/R are on separate buses).
    
    Pull-down is useful, though not perfect, when multiple amps share a
    bus. It typically takes around 2 bits for the line to transition from
    high to low after going Hi-Z, with the pull-down.
    
    Reviewed-by: Neal Gompa <neal@gompa.dev>
    Signed-off-by: Hector Martin <marcan@marcan.st>
    Signed-off-by: James Calligeros <jcalligeros99@gmail.com>

--- sound/soc/codecs/tas2770.c
+++ sound/soc/codecs/tas2770.c
@@ -632,6 +632,20 @@ static int tas2770_codec_probe(struct snd_soc_component *component)
 			return ret;
 	}
 
+	ret = snd_soc_component_update_bits(component, TAS2770_TDM_CFG_REG4,
+					    TAS2770_TDM_CFG_REG4_TX_FILL,
+					    tas2770->sdout_zfill ? 0 :
+					    TAS2770_TDM_CFG_REG4_TX_FILL);
+	if (ret < 0)
+		return ret;
+
+	ret = snd_soc_component_update_bits(component, TAS2770_DIN_PD,
+					    TAS2770_DIN_PD_SDOUT,
+					    tas2770->sdout_pd ?
+					    TAS2770_DIN_PD_SDOUT : 0);
+	if (ret < 0)
+		return ret;
+
 	return 0;
 }
 
@@ -770,6 +784,9 @@ static int tas2770_parse_dt(struct device *dev, struct tas2770_priv *tas2770)
 
 		tas2770->v_sense_slot = -1;
 	}
+
+	tas2770->sdout_pd = fwnode_property_read_bool(dev->fwnode, "ti,sdout-pull-down");
+	tas2770->sdout_zfill = fwnode_property_read_bool(dev->fwnode, "ti,sdout-zero-fill");
 
 	tas2770->sdz_gpio = devm_gpiod_get_optional(dev, "shutdown", GPIOD_OUT_HIGH);
 	if (IS_ERR(tas2770->sdz_gpio)) {
