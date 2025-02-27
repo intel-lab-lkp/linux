f41d65d2de6e50748496c786b34f70805d117dce
Abel Wu <wuyun.abel@bytedance.com>
2025-02-27 16:58:12 +0800

    sched/fair: Do not let idle entities preempt others
    A task with SCHED_IDLE policy doesn't preempt others by definition, and
    the semantics are intended to be preserved when extending to cgroups
    introduced in commit 304000390f88 ("sched: Cgroup SCHED_IDLE support").
    
    But current implementation allows idle entities to preempt each other
    on wakeup, which seems not behave as expected especially after
    commit faa42d29419d ("sched/fair: Make SCHED_IDLE entity be preempted in strict hierarchy")
    so fix this by explicitly skip wakeup preemption for idle entities.
    
    Fixes: 304000390f88 ("sched: Cgroup SCHED_IDLE support")
    Signed-off-by: Abel Wu <wuyun.abel@bytedance.com>
    Reviewed-by: Vincent Guittot <vincent.guittot@linaro.org>
    Reviewed-by: Josh Don <joshdon@google.com>
    Reviewed-by: Madadi Vineeth Reddy <vineethr@linux.ibm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8795,8 +8795,7 @@ static void check_preempt_wakeup_fair(struct rq *rq, struct task_struct *p, int 
 	pse_is_idle = se_is_idle(pse);
 
 	/*
-	 * Preempt an idle entity in favor of a non-idle entity (and don't preempt
-	 * in the inverse case).
+	 * Preempt an idle entity in favor of a non-idle entity.
 	 */
 	if (cse_is_idle && !pse_is_idle) {
 		/*
@@ -8807,7 +8806,10 @@ static void check_preempt_wakeup_fair(struct rq *rq, struct task_struct *p, int 
 		goto preempt;
 	}
 
-	if (cse_is_idle != pse_is_idle)
+	/*
+	 * IDLE entities do not preempt others.
+	 */
+	if (unlikely(pse_is_idle))
 		return;
 
 	/*
