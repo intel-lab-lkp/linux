b35c2af569bbd60b0cbdeed385ec2358728c49e5
Bence Csókás <csokas.bence@prolan.hu>
2025-02-27 15:40:19 +0100

    counter: microchip-tcb-capture: Add IRQ handling
    Add interrupt servicing to allow userspace to wait for the following events:
    * Change-of-state caused by external trigger
    * Capture of timer value into RA/RB
    * Compare to RC register
    * Overflow
    
    Signed-off-by: Bence Csókás <csokas.bence@prolan.hu>

--- include/uapi/linux/counter/microchip-tcb-capture.h
+++ include/uapi/linux/counter/microchip-tcb-capture.h
@@ -12,11 +12,29 @@
  * Count 0
  * \__  Synapse 0 -- Signal 0 (Channel A, i.e. TIOA)
  * \__  Synapse 1 -- Signal 1 (Channel B, i.e. TIOB)
+ *
+ * It also supports the following events:
+ *
+ * Channel 0:
+ * - CV register changed
+ * - CV overflowed
+ * - RA captured
+ * Channel 1:
+ * - RB captured
+ * Channel 2:
+ * - RC compare triggered
  */
 
 enum counter_mchp_signals {
 	COUNTER_MCHP_SIG_TIOA,
 	COUNTER_MCHP_SIG_TIOB,
+};
+
+enum counter_mchp_event_channels {
+	COUNTER_MCHP_EVCHN_CV = 0,
+	COUNTER_MCHP_EVCHN_RA = 0,
+	COUNTER_MCHP_EVCHN_RB,
+	COUNTER_MCHP_EVCHN_RC,
 };
 
 #endif /* _UAPI_COUNTER_MCHP_TCB_H_ */
