93f42063f9c6226bdf67a8603be670b710622c65
Jonathan Cavitt <jonathan.cavitt@intel.com>
2025-02-28 18:21:19 +0000

    drm/xe/xe_vm: Add per VM pagefault info
    Add additional information to each VM so they can report up to the last
    50 seen pagefaults.  Only failed pagefaults are saved this way, as
    successful pagefaults should recover and not need to be reported to
    userspace.
    
    Signed-off-by: Jonathan Cavitt <jonathan.cavitt@intel.com>
    Suggested-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_vm_types.h
+++ drivers/gpu/drm/xe/xe_vm_types.h
@@ -18,6 +18,7 @@
 #include "xe_range_fence.h"
 
 struct xe_bo;
+struct xe_pagefault;
 struct xe_sync_entry;
 struct xe_user_fence;
 struct xe_vm;
@@ -134,6 +135,13 @@ struct xe_userptr_vma {
 };
 
 struct xe_device;
+
+struct xe_vm_pf_entry {
+	/** @pf: observed pagefault */
+	struct xe_pagefault *pf;
+	/** @list: link into @xe_vm.pfs.list */
+	struct list_head list;
+};
 
 struct xe_vm {
 	/** @gpuvm: base GPUVM used to track VMAs */
@@ -273,6 +281,18 @@ struct xe_vm {
 		/** @capture_once: capture only one error per VM */
 		bool capture_once;
 	} error_capture;
+
+	/**
+	 * @pfs: List of all pagefaults associated with this VM
+	 */
+	struct {
+		/** @lock: lock protecting @bans.list */
+		spinlock_t lock;
+		/** @list: list of xe_exec_queue_ban_entry entries */
+		struct list_head list;
+		/** @len: length of @bans.list */
+		unsigned int len;
+	} pfs;
 
 	/**
 	 * @tlb_flush_seqno: Required TLB flush seqno for the next exec.
