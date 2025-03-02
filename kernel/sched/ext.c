b418aee16e8577a6213c3061b771d72476ef90d4
Andrea Righi <arighi@nvidia.com>
2025-03-02 23:09:03 +0100

    sched_ext: Validate prev_cpu in scx_bpf_select_cpu_dfl()
    If a BPF scheduler provides an invalid CPU (outside the nr_cpu_ids
    range) as prev_cpu to scx_bpf_select_cpu_dfl() it can cause a kernel
    crash.
    
    To prevent this, validate prev_cpu in scx_bpf_select_cpu_dfl() and
    trigger an scx error if an invalid CPU is specified.
    
    Fixes: f0e1a0643a59b ("sched_ext: Implement BPF extensible scheduler class")
    Cc: stable@vger.kernel.org # v6.12+
    Signed-off-by: Andrea Righi <arighi@nvidia.com>

--- kernel/sched/ext.c
+++ kernel/sched/ext.c
@@ -6380,6 +6380,11 @@ static bool check_builtin_idle_enabled(void)
 __bpf_kfunc s32 scx_bpf_select_cpu_dfl(struct task_struct *p, s32 prev_cpu,
 				       u64 wake_flags, bool *is_idle)
 {
+	if (!ops_cpu_valid(prev_cpu, NULL)) {
+		prev_cpu = cpumask_any(p->cpus_ptr);
+		goto prev_cpu;
+	}
+
 	if (!check_builtin_idle_enabled())
 		goto prev_cpu;
 
