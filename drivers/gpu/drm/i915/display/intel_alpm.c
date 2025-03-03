1bb3987429a4b5a1cd41246e3bda6dd76832595b
Animesh Manna <animesh.manna@intel.com>
2025-03-03 14:05:19 +0530

    drm/i915/lobf: Add debug interface for lobf
    Add an interface in debugfs which will help in debugging LOBF
    feature.
    
    v1: Initial version.
    v2:
    - Remove FORCE_EN flag. [Jouni]
    - Change prefix from I915 to INTEL. [Jani]
    - Use u8 instead of bool for lobf-debug flag. [Jani]
    v3:
    - Use intel_connector instead of display. [Jani]
    - Remove edp connector check as it was already present
    in caller function. [Jani]
    - Remove loop of searching edp encoder which is directly
    accessible from intel_connector. [Jani]
    
    Signed-off-by: Animesh Manna <animesh.manna@intel.com>

--- drivers/gpu/drm/i915/display/intel_alpm.c
+++ drivers/gpu/drm/i915/display/intel_alpm.c
@@ -276,6 +276,9 @@ void intel_alpm_lobf_compute_config(struct intel_dp *intel_dp,
 	int waketime_in_lines, first_sdp_position;
 	int context_latency, guardband;
 
+	if (intel_dp->alpm_parameters.lobf_debug & INTEL_LOBF_DEBUG_DISABLE)
+		return;
+
 	if (!intel_dp_is_edp(intel_dp))
 		return;
 
@@ -451,6 +454,32 @@ out:
 
 DEFINE_SHOW_ATTRIBUTE(i915_edp_lobf_info);
 
+static int
+i915_edp_lobf_debug_get(void *data, u64 *val)
+{
+	struct intel_connector *connector = data;
+	struct intel_dp *intel_dp = enc_to_intel_dp(connector->encoder);
+
+	*val = READ_ONCE(intel_dp->alpm_parameters.lobf_debug);
+
+	return 0;
+}
+
+static int
+i915_edp_lobf_debug_set(void *data, u64 val)
+{
+	struct intel_connector *connector = data;
+	struct intel_dp *intel_dp = enc_to_intel_dp(connector->encoder);
+
+	intel_dp->alpm_parameters.lobf_debug = val;
+
+	return 0;
+}
+
+DEFINE_SIMPLE_ATTRIBUTE(i915_edp_lobf_debug_fops,
+			i915_edp_lobf_debug_get, i915_edp_lobf_debug_set,
+			"%llu\n");
+
 void intel_alpm_lobf_debugfs_add(struct intel_connector *connector)
 {
 	struct intel_display *display = to_intel_display(connector);
@@ -459,6 +488,9 @@ void intel_alpm_lobf_debugfs_add(struct intel_connector *connector)
 	if (DISPLAY_VER(display) < 20 ||
 	    connector->base.connector_type != DRM_MODE_CONNECTOR_eDP)
 		return;
+
+	debugfs_create_file("i915_edp_lobf_debug", 0644, root,
+			    connector, &i915_edp_lobf_debug_fops);
 
 	debugfs_create_file("i915_edp_lobf_info", 0444, root,
 			    connector, &i915_edp_lobf_info_fops);
