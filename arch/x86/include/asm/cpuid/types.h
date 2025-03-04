fcd57643d46938143ca1e4a7f8db3ba09179d0fd
Thomas Gleixner <tglx@linutronix.de>
2025-03-04 09:51:42 +0100

    x86/cpu: Consolidate CPUID leaf 0x2 tables
    CPUID leaf 0x2 describes TLBs and caches. So there are two tables with the
    respective decriptor constants in intel.c and cacheinfo.c. The tables
    occupy almost 600 byte and require a loop based lookup for each variant.
    
    Combining them into one table occupies exactly 1k rodata and allows to get
    rid of the loop based lookup by just using the descriptor byte provided by
    CPUID leaf 0x2 as index into the table, which simplifies the code and
    reduces text size.
    
    The conversion of the intel and cacheinfo code is done separately.
    
    [darwi: Actually define struct leaf_0x2_table.
    	Tab-align all of cpuid_0x2_table[] mapping entries.
    	Define needed SZ_* macros at linux/sizes.h instead (parent commit.)
    	Use CACHE_L1_{INST,DATA} as names for L1 cache descriptor types.
    	Set descriptor 0x63 type as TLB_DATA_1G_2M_4M and explain why.
    	Use enums for cache and TLB descriptor types (parent commits.)
    	Start enum types at 1 since type 0 is reserved for unknown descriptors.
    	Ensure that cache and TLB enum type values do not intersect.
    	Add leaf 0x2 table accessor for_each_leaf_0x2_entry() + documentation.]
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Co-developed-by: Ahmed S. Darwish <darwi@linutronix.de>
    Signed-off-by: Ahmed S. Darwish <darwi@linutronix.de>

--- arch/x86/include/asm/cpuid/types.h
+++ arch/x86/include/asm/cpuid/types.h
@@ -10,7 +10,7 @@
 
 /*
  * CPUID(0x2) parsing helpers
- * Check for_each_leaf_0x2_desc() documentation.
+ * Check for_each_leaf_0x2_entry() documentation.
  */
 
 struct leaf_0x2_reg {
@@ -81,21 +81,32 @@ static inline void get_leaf_0x2_regs(union leaf_0x2_regs *regs)
 /*
  * Leaf 0x2 1-byte descriptors' cache types
  * To be used for their mappings at cache_table[]
+ *
+ * Start at 1 since type 0 is reserved for HW byte descriptors which are
+ * not recognized by the kernel; i.e., those without an explicit mapping
+ * entry at cpuid_0x2_table[].
  */
 enum _cache_table_type {
-	CACHE_L1_INST,
+	CACHE_L1_INST	= 1,
 	CACHE_L1_DATA,
 	CACHE_L2,
-	CACHE_L3,
+	CACHE_L3
+	/* Adjust __TLB_TABLE_TYPE_BEGIN before adding more types */
 } __packed;
 static_assert(sizeof(enum _cache_table_type) == 1);
+
+/*
+ * Ensure that leaf 0x2 cache and TLB type values do not intersect,
+ * since they share the same field at struct cpuid_0x2_table.
+ */
+#define __TLB_TABLE_TYPE_BEGIN		(CACHE_L3 + 1)
 
 /*
  * Leaf 0x2 1-byte descriptors' TLB types
  * To be used for their mappings at intel_tlb_table[]
  */
 enum _tlb_table_type {
-	TLB_INST_4K,
+	TLB_INST_4K	= __TLB_TABLE_TYPE_BEGIN,
 	TLB_INST_4M,
 	TLB_INST_2M_4M,
 	TLB_INST_ALL,
@@ -115,5 +126,52 @@ enum _tlb_table_type {
 	STLB_4K_2M,
 } __packed;
 static_assert(sizeof(enum _tlb_table_type) == 1);
+
+/*
+ * Combined table for leaf 0x2 cache and TLB descriptors.
+ */
+struct leaf_0x2_table {
+	union {
+		enum _cache_table_type	c_type;
+		enum _tlb_table_type	t_type;
+	};
+	union {
+		short			c_size;
+		short			entries;
+	};
+};
+
+extern const struct leaf_0x2_table cpuid_0x2_table[256];
+
+/**
+ * for_each_leaf_0x2_entry() - Iterator for parsed leaf 0x2 descriptors
+ * @regs:   Leaf 0x2 register output, as returned by get_leaf_0x2_regs()
+ * @__ptr:  u8 pointer, for macro internal use only
+ * @entry:  Pointer to parsed descriptor information for each iteration
+ *
+ * Loop over the 1-byte descriptors in the passed leaf 0x2 output registers
+ * @regs.  Provide the parsed information for each descriptor through @entry.
+ * To handle cache specific descriptors, switch on @entry->c_type.  For TLB
+ * specific descriptors, switch on @entry->t_type.
+ *
+ * Example usage for cache descriptors::
+ *
+ *	const struct leaf_0x2_table *entry;
+ *	union leaf_0x2_regs regs;
+ *	u8 *ptr;
+ *
+ *	get_leaf_0x2_regs(&regs);
+ *	for_each_leaf_0x2_entry(regs, ptr, entry) {
+ *		switch (entry->c_type) {
+ *			...
+ *		}
+ *	}
+ *
+ */
+#define for_each_leaf_0x2_entry(regs, __ptr, entry)				\
+	/* Skip the first byte as it is not a descriptor */			\
+	for (__ptr = &(regs).desc[1], entry = &cpuid_0x2_table[*__ptr];		\
+	     __ptr < &(regs).desc[16];						\
+	     __ptr++, entry = &cpuid_0x2_table[*__ptr])
 
 #endif /* _ASM_X86_CPUID_TYPES_H */
