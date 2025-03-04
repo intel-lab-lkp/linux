88e8e40f2d101ba0f72a4c7033f1e0ece5fb8966
Liu Shixin <liushixin2@huawei.com>
2025-03-04 16:38:41 +0800

    mm/hugetlb: update nr_huge_pages and surplus_huge_pages together
    In alloc_surplus_hugetlb_folio(), we increase nr_huge_pages and
    surplus_huge_pages separately. In the middle window, if we set
    nr_hugepages to smaller and satisfy count < persistent_huge_pages(h),
    the surplus_huge_pages will be increased by adjust_pool_surplus().
    
    After adding delay in the middle window, we can reproduce the problem
    easily by following step:
    
     1. echo 3 > /proc/sys/vm/nr_overcommit_hugepages
     2. mmap two hugepages. When nr_huge_pages=2 and surplus_huge_pages=1,
        goto step 3.
     3. echo 0 > /proc/sys/vm/nr_huge_pages
    
    Finally, nr_huge_pages is less than surplus_huge_pages.
    
    To fix the problem, call only_alloc_fresh_hugetlb_folio() instead and
    move down __prep_account_new_huge_page() into the hugetlb_lock.
    
    Fixes: 0c397daea1d4 ("mm, hugetlb: further simplify hugetlb allocation API")
    Signed-off-by: Liu Shixin <liushixin2@huawei.com>

--- mm/hugetlb.c
+++ mm/hugetlb.c
@@ -2255,11 +2255,19 @@ static struct folio *alloc_surplus_hugetlb_folio(struct hstate *h,
 		goto out_unlock;
 	spin_unlock_irq(&hugetlb_lock);
 
-	folio = alloc_fresh_hugetlb_folio(h, gfp_mask, nid, nmask);
+	folio = only_alloc_fresh_hugetlb_folio(h, gfp_mask, nid, nmask, NULL);
 	if (!folio)
 		return NULL;
 
+	hugetlb_vmemmap_optimize_folio(h, folio);
+
 	spin_lock_irq(&hugetlb_lock);
+	/*
+	 * Update nr_huge_pages and surplus_huge_pages together,
+	 * otherwise it might confuse persistent_huge_pages() momentarily.
+	 */
+	__prep_account_new_huge_page(h, nid);
+
 	/*
 	 * We could have raced with the pool size change.
 	 * Double check that and simply deallocate the new page
