4ca831f0ff920a5f9ee3d3f3f00adc7213e6ba99
Thorsten Blum <thorsten.blum@linux.dev>
2025-03-04 19:30:57 +0100

    perf/x86: Annotate struct bts_buffer with __counted_by()
    Add the __counted_by() compiler attribute to the flexible array member
    buf to improve access bounds-checking via CONFIG_UBSAN_BOUNDS and
    CONFIG_FORTIFY_SOURCE.
    
    Use struct_size() to calculate the number of bytes to allocate for a new
    bts_buffer. Compared to offsetof(), struct_size() has additional
    compile-time checks (e.g., __must_be_array()).
    
    No functional changes intended.
    
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>

--- arch/x86/events/intel/bts.c
+++ arch/x86/events/intel/bts.c
@@ -58,7 +58,7 @@ struct bts_buffer {
 	local_t		head;
 	unsigned long	end;
 	void		**data_pages;
-	struct bts_phys	buf[];
+	struct bts_phys	buf[] __counted_by(nr_bufs);
 };
 
 static struct pmu bts_pmu;
@@ -101,7 +101,7 @@ bts_buffer_setup_aux(struct perf_event *event, void **pages,
 	if (overwrite && nbuf > 1)
 		return NULL;
 
-	buf = kzalloc_node(offsetof(struct bts_buffer, buf[nbuf]), GFP_KERNEL, node);
+	buf = kzalloc_node(struct_size(buf, buf, nbuf), GFP_KERNEL, node);
 	if (!buf)
 		return NULL;
 
