b7200e1c44f747b56acf69f2d1142b6e50101a28
Dikshita Agarwal <quic_dikshita@quicinc.com>
2025-03-05 16:13:35 +0530

    media: iris: Add handling for no show frames
    Firmware sends the picture type as NO_SHOW for frames which are not
    supposed to be displayed, add handling for the same in driver to drop
    them.
    
    Signed-off-by: Dikshita Agarwal <quic_dikshita@quicinc.com>

--- drivers/media/platform/qcom/iris/iris_hfi_gen2_response.c
+++ drivers/media/platform/qcom/iris/iris_hfi_gen2_response.c
@@ -91,7 +91,9 @@ static int iris_hfi_gen2_get_driver_buffer_flags(struct iris_inst *inst, u32 hfi
 	struct iris_inst_hfi_gen2 *inst_hfi_gen2 = to_iris_inst_hfi_gen2(inst);
 	u32 driver_flags = 0;
 
-	if (inst_hfi_gen2->hfi_frame_info.picture_type & keyframe)
+	if (inst_hfi_gen2->hfi_frame_info.picture_type & HFI_PICTURE_NOSHOW)
+		driver_flags |= V4L2_BUF_FLAG_ERROR;
+	else if (inst_hfi_gen2->hfi_frame_info.picture_type & keyframe)
 		driver_flags |= V4L2_BUF_FLAG_KEYFRAME;
 	else if (inst_hfi_gen2->hfi_frame_info.picture_type & HFI_PICTURE_P)
 		driver_flags |= V4L2_BUF_FLAG_PFRAME;
@@ -656,9 +658,6 @@ static int iris_hfi_gen2_handle_session_property(struct iris_inst *inst,
 						 struct iris_hfi_packet *pkt)
 {
 	struct iris_inst_hfi_gen2 *inst_hfi_gen2 = to_iris_inst_hfi_gen2(inst);
-
-	if (pkt->port != HFI_PORT_BITSTREAM)
-		return 0;
 
 	if (pkt->flags & HFI_FW_FLAGS_INFORMATION)
 		return 0;
