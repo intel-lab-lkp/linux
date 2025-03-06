7b6b515e9203004d3247f54480a10c3cc9a72365
joel granados <joel.granados@kernel.org>
2025-03-06 12:29:46 +0100

    s390: mv s390 sysctls into their own file under arch/s390 dir
    Move s390 sysctls (spin_retry and userprocess_debug) into their own
    files under arch/s390. We create two new sysctl tables
    (2390_{fault,spin}_sysctl_table) which will be initialized with
    arch_initcall placing them after their original place in proc_root_init.
    
    This is part of a greater effort to move ctl tables into their
    respective subsystems which will reduce the merge conflicts in
    kernel/sysctl.c.
    
    Signed-off-by: joel granados <joel.granados@kernel.org>

--- arch/s390/lib/spinlock.c
+++ arch/s390/lib/spinlock.c
@@ -16,6 +16,7 @@
 #include <linux/io.h>
 #include <asm/alternative.h>
 #include <asm/asm.h>
+#include <linux/sysctl.h>
 
 int spin_retry = -1;
 
@@ -36,6 +37,23 @@ static int __init spin_retry_setup(char *str)
 	return 1;
 }
 __setup("spin_retry=", spin_retry_setup);
+
+static const struct ctl_table s390_spin_sysctl_table[] = {
+	{
+		.procname	= "spin_retry",
+		.data		= &spin_retry,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
+};
+
+static int __init init_s390_spin_sysctls(void)
+{
+	register_sysctl_init("kernel", s390_spin_sysctl_table);
+	return 0;
+}
+arch_initcall(init_s390_spin_sysctls);
 
 struct spin_wait {
 	struct spin_wait *next, *prev;
