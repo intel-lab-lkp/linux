a139d29b22ce371cde13d3b97831340a34b66b6c
Jonas Karlman <jonas@kwiboo.se>
2025-03-06 20:38:52 +0000

    net: stmmac: dwmac-rk: Use DELAY_ENABLE macro for RK3328
    Support for Rockchip RK3328 GMAC and addition of the DELAY_ENABLE macro
    was merged in the same merge window. This resulted in RK3328 not being
    converted to use the new DELAY_ENABLE macro.
    
    Change to use the DELAY_ENABLE macro to help disable MAC delay when
    RGMII_ID/RXID/TXID is used.
    
    Fixes: eaf70ad14cbb ("net: stmmac: dwmac-rk: Add handling for RGMII_ID/RXID/TXID")
    Signed-off-by: Jonas Karlman <jonas@kwiboo.se>

--- drivers/net/ethernet/stmicro/stmmac/dwmac-rk.c
+++ drivers/net/ethernet/stmicro/stmmac/dwmac-rk.c
@@ -593,8 +593,7 @@ static void rk3328_set_to_rgmii(struct rk_priv_data *bsp_priv,
 	regmap_write(bsp_priv->grf, RK3328_GRF_MAC_CON1,
 		     RK3328_GMAC_PHY_INTF_SEL_RGMII |
 		     RK3328_GMAC_RMII_MODE_CLR |
-		     RK3328_GMAC_RXCLK_DLY_ENABLE |
-		     RK3328_GMAC_TXCLK_DLY_ENABLE);
+		     DELAY_ENABLE(RK3328, tx_delay, rx_delay));
 
 	regmap_write(bsp_priv->grf, RK3328_GRF_MAC_CON0,
 		     RK3328_GMAC_CLK_RX_DL_CFG(rx_delay) |
