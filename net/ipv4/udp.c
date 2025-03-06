b70e176e9a7fc0fb3bc5be7019f62f6be175e639
Eric Dumazet <edumazet@google.com>
2025-03-06 18:31:01 +0000

    udp: expand SKB_DROP_REASON_UDP_CSUM use
    Use SKB_DROP_REASON_UDP_CSUM in __first_packet_length()
    and udp_read_skb() when dropping a packet because of
    a wrong UDP checksum.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>

--- net/ipv4/udp.c
+++ net/ipv4/udp.c
@@ -1848,7 +1848,7 @@ static struct sk_buff *__first_packet_length(struct sock *sk,
 			atomic_inc(&sk->sk_drops);
 			__skb_unlink(skb, rcvq);
 			*total += skb->truesize;
-			kfree_skb(skb);
+			kfree_skb_reason(skb, SKB_DROP_REASON_UDP_CSUM);
 		} else {
 			udp_skb_csum_unnecessary_set(skb);
 			break;
@@ -2002,7 +2002,7 @@ try_again:
 		__UDP_INC_STATS(net, UDP_MIB_CSUMERRORS, is_udplite);
 		__UDP_INC_STATS(net, UDP_MIB_INERRORS, is_udplite);
 		atomic_inc(&sk->sk_drops);
-		kfree_skb(skb);
+		kfree_skb_reason(skb, SKB_DROP_REASON_UDP_CSUM);
 		goto try_again;
 	}
 
