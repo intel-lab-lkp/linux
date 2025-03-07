d2e45c6481d2dc20c7f5dfad5120767338b994c8
Dan Carpenter <dan.carpenter@linaro.org>
2025-03-07 11:46:41 +0300

    PCI: Fix double free in pci_register_host_bridge()
    Calling put_device(&bus->dev) will call release_pcibus_dev() which will
    free the bus.  It leads to a use after free when we dereference "bus" in
    the cleanup code and the kfree(bus) is a double free.
    
    Fixes: b80b4d4972e6 ("PCI: Fix reference leak in pci_register_host_bridge()")
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -956,6 +956,7 @@ static int pci_register_host_bridge(struct pci_host_bridge *bridge)
 	resource_size_t offset, next_offset;
 	LIST_HEAD(resources);
 	struct resource *res, *next_res;
+	bool bus_registered = false;
 	char addr[64], *fmt;
 	const char *name;
 	int err;
@@ -1019,10 +1020,9 @@ static int pci_register_host_bridge(struct pci_host_bridge *bridge)
 	name = dev_name(&bus->dev);
 
 	err = device_register(&bus->dev);
-	if (err) {
-		put_device(&bus->dev);
+	bus_registered = true;
+	if (err)
 		goto unregister;
-	}
 
 	pcibios_add_bus(bus);
 
@@ -1109,12 +1109,15 @@ static int pci_register_host_bridge(struct pci_host_bridge *bridge)
 unregister:
 	put_device(&bridge->dev);
 	device_del(&bridge->dev);
-
 free:
 #ifdef CONFIG_PCI_DOMAINS_GENERIC
 	pci_bus_release_domain_nr(parent, bus->domain_nr);
 #endif
-	kfree(bus);
+	if (bus_registered)
+		put_device(&bus->dev);
+	else
+		kfree(bus);
+
 	return err;
 }
 
