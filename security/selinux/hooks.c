ed2f5e6da5b367006bb33a7b0fa3d08b48753c7a
Casey Schaufler <casey@schaufler-ca.com>
2025-03-07 10:36:59 -0800

    Audit: Add record for multiple task security contexts
    Create a new audit record AUDIT_MAC_TASK_CONTEXTS.
    An example of the MAC_TASK_CONTEXTS (1423) record is:
    
        type=MAC_TASK_CONTEXTS[1423]
        msg=audit(1600880931.832:113)
        subj_apparmor=unconfined
        subj_smack=_
    
    When an audit event includes a AUDIT_MAC_TASK_CONTEXTS record
    the "subj=" field in other records in the event will be "subj=?".
    An AUDIT_MAC_TASK_CONTEXTS record is supplied when the system has
    multiple security modules that may make access decisions based
    on a subject security context.
    
    Signed-off-by: Casey Schaufler <casey@schaufler-ca.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -7180,6 +7180,7 @@ static int selinux_uring_cmd(struct io_uring_cmd *ioucmd)
 static const struct lsm_id selinux_lsmid = {
 	.name = "selinux",
 	.id = LSM_ID_SELINUX,
+	.subjctx = true,
 };
 
 /*
