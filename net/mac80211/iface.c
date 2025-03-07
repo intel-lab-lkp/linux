29ae56e357fe13f7d2ed3f6f36a6e0b8579ffce4
Muna Sinada <muna.sinada@oss.qualcomm.com>
2025-03-06 16:37:39 -0800

    wifi: mac80211: Create separate links for VLAN interfaces
    Currently, MLD links for an AP_VLAN interface type is not fully
    supported.
    
    Add allocation of separate links for each VLAN interface and copy
    chanctx and chandef of AP bss to VLAN where necessary. Separate
    links are created because for Dynamic VLAN each link will have its own
    default_multicast_key.
    
    Signed-off-by: Muna Sinada <muna.sinada@oss.qualcomm.com>

--- net/mac80211/iface.c
+++ net/mac80211/iface.c
@@ -487,6 +487,9 @@ static void ieee80211_do_stop(struct ieee80211_sub_if_data *sdata, bool going_do
 			break;
 		list_del_rcu(&sdata->u.mntr.list);
 		break;
+	case NL80211_IFTYPE_AP_VLAN:
+		ieee80211_apvlan_link_clear(sdata);
+		break;
 	default:
 		break;
 	}
@@ -1231,7 +1234,7 @@ int ieee80211_do_open(struct wireless_dev *wdev, bool coming_up)
 	struct net_device *dev = wdev->netdev;
 	struct ieee80211_local *local = sdata->local;
 	u64 changed = 0;
-	int res;
+	int link_id, res;
 	u32 hw_reconf_flags = 0;
 
 	lockdep_assert_wiphy(local->hw.wiphy);
@@ -1262,6 +1265,8 @@ int ieee80211_do_open(struct wireless_dev *wdev, bool coming_up)
 
 		sdata->crypto_tx_tailroom_needed_cnt +=
 			master->crypto_tx_tailroom_needed_cnt;
+
+		ieee80211_apvlan_link_setup(sdata);
 
 		break;
 		}
@@ -1319,7 +1324,20 @@ int ieee80211_do_open(struct wireless_dev *wdev, bool coming_up)
 	case NL80211_IFTYPE_AP_VLAN:
 		/* no need to tell driver, but set carrier and chanctx */
 		if (sdata->bss->active) {
-			ieee80211_link_vlan_copy_chanctx(&sdata->deflink);
+			struct ieee80211_link_data *link;
+			unsigned long valid_links = sdata->vif.valid_links;
+
+			if (valid_links) {
+				for_each_set_bit(link_id, &valid_links,
+						 IEEE80211_MLD_MAX_NUM_LINKS) {
+					link = sdata_dereference(sdata->link[link_id],
+								 sdata);
+					ieee80211_link_vlan_copy_chanctx(link);
+				}
+			} else {
+				ieee80211_link_vlan_copy_chanctx(&sdata->deflink);
+			}
+
 			netif_carrier_on(dev);
 			ieee80211_set_vif_encap_ops(sdata);
 		} else {
