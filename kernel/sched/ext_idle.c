f03caa75cf2045328d798d0d12b38c3fe249b35d
Andrea Righi <arighi@nvidia.com>
2025-03-07 21:01:06 +0100

    sched_ext: idle: Introduce scx_bpf_select_cpu_and()
    Provide a new kfunc that can be used to apply the built-in idle CPU
    selection policy to a subset of allowed CPU:
    
    s32 scx_bpf_select_cpu_and(struct task_struct *p,
    			   const struct cpumask *cpus_allowed,
    			   s32 prev_cpu, u64 wake_flags, u64 flags);
    
    This new helper is basically an extension of scx_bpf_select_cpu_dfl().
    However, when an idle CPU can't be found, it returns a negative value
    instead of @prev_cpu, aligning its behavior more closely with
    scx_bpf_pick_idle_cpu().
    
    It also accepts %SCX_PICK_IDLE_* flags, which can be used to enforce
    strict selection to @prev_cpu's node (%SCX_PICK_IDLE_IN_NODE), or to
    request only a full-idle SMT core (%SCX_PICK_IDLE_CORE), while applying
    the built-in selection logic.
    
    With this helper, BPF schedulers can apply the built-in idle CPU
    selection policy restricted to a generic CPU domain.
    
    In the future we can also consider to deprecate scx_bpf_select_cpu_dfl()
    and replace it with scx_bpf_select_cpu_and(), as the latter provides the
    same functionality, with the addition of the allowed domain logic.
    
    Example usage
    =============
    
    Possible usage in ops.select_cpu():
    
    s32 BPF_STRUCT_OPS(foo_select_cpu, struct task_struct *p,
    		  s32 prev_cpu, u64 wake_flags)
    {
    	const struct cpumask *dom = task_domain(p) ?: p->cpus_ptr;
    	s32 cpu;
    
    	cpu = scx_bpf_select_cpu_and(p, dom, prev_cpu, wake_flags, 0);
    	if (cpu >= 0) {
    		scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL, SCX_SLICE_DFL, 0);
    		return cpu;
    	}
    
    	return prev_cpu;
    }
    
    Results
    =======
    
    Load distribution on a 4 sockets / 4 cores per socket system, simulated
    using virtme-ng, running a modified version of scx_bpfland that uses the
    new helper scx_bpf_select_cpu_and() and 0xff00 as allowed domain:
    
     $ vng --cpu 16,sockets=4,cores=4,threads=1
     ...
     $ stress-ng -c 16
     ...
     $ htop
     ...
       0[                         0.0%]   8[||||||||||||||||||||||||100.0%]
       1[                         0.0%]   9[||||||||||||||||||||||||100.0%]
       2[                         0.0%]  10[||||||||||||||||||||||||100.0%]
       3[                         0.0%]  11[||||||||||||||||||||||||100.0%]
       4[                         0.0%]  12[||||||||||||||||||||||||100.0%]
       5[                         0.0%]  13[||||||||||||||||||||||||100.0%]
       6[                         0.0%]  14[||||||||||||||||||||||||100.0%]
       7[                         0.0%]  15[||||||||||||||||||||||||100.0%]
    
    With scx_bpf_select_cpu_dfl() tasks would be distributed evenly across
    all the available CPUs.
    
    Signed-off-by: Andrea Righi <arighi@nvidia.com>

--- kernel/sched/ext_idle.c
+++ kernel/sched/ext_idle.c
@@ -898,6 +898,47 @@ prev_cpu:
 }
 
 /**
+ * scx_bpf_select_cpu_and - Pick an idle CPU usable by task @p,
+ *			    prioritizing those in @cpus_allowed
+ * @p: task_struct to select a CPU for
+ * @cpus_allowed: cpumask of allowed CPUs
+ * @prev_cpu: CPU @p was on previously
+ * @wake_flags: %SCX_WAKE_* flags
+ * @flags: %SCX_PICK_IDLE* flags
+ *
+ * Can only be called from ops.select_cpu() if the built-in CPU selection is
+ * enabled - ops.update_idle() is missing or %SCX_OPS_KEEP_BUILTIN_IDLE is set.
+ * @p, @prev_cpu and @wake_flags match ops.select_cpu().
+ *
+ * Returns the selected idle CPU, which will be automatically awakened upon
+ * returning from ops.select_cpu() and can be used for direct dispatch, or
+ * a negative value if no idle CPU is available.
+ */
+__bpf_kfunc s32 scx_bpf_select_cpu_and(struct task_struct *p,
+				       const struct cpumask *cpus_allowed,
+				       s32 prev_cpu, u64 wake_flags, u64 flags)
+{
+	s32 cpu;
+
+	if (!ops_cpu_valid(prev_cpu, NULL))
+		return -EINVAL;
+
+	if (!check_builtin_idle_enabled())
+		return -EBUSY;
+
+	if (!scx_kf_allowed(SCX_KF_SELECT_CPU))
+		return -EPERM;
+
+#ifdef CONFIG_SMP
+	cpu = scx_select_cpu_dfl(p, cpus_allowed, prev_cpu, wake_flags, flags);
+#else
+	cpu = -EBUSY;
+#endif
+
+	return cpu;
+}
+
+/**
  * scx_bpf_get_idle_cpumask_node - Get a referenced kptr to the
  * idle-tracking per-CPU cpumask of a target NUMA node.
  * @node: target NUMA node
@@ -1205,6 +1246,7 @@ static const struct btf_kfunc_id_set scx_kfunc_set_idle = {
 
 BTF_KFUNCS_START(scx_kfunc_ids_select_cpu)
 BTF_ID_FLAGS(func, scx_bpf_select_cpu_dfl, KF_RCU)
+BTF_ID_FLAGS(func, scx_bpf_select_cpu_and, KF_RCU)
 BTF_KFUNCS_END(scx_kfunc_ids_select_cpu)
 
 static const struct btf_kfunc_id_set scx_kfunc_set_select_cpu = {
