5751acae9dcd0855274b93fbb698c0787f79f275
Chao Yu <chao@kernel.org>
2025-03-07 11:18:38 +0800

    f2fs: disalbe nat_bits by default
    Let's disable nat_bits by default.
    
    Signed-off-by: Chao Yu <chao@kernel.org>

--- fs/f2fs/node.c
+++ fs/f2fs/node.c
@@ -3283,6 +3283,9 @@ static int init_node_manager(struct f2fs_sb_info *sbi)
 	if (!nm_i->nat_bitmap)
 		return -ENOMEM;
 
+	/* disable nat_bits feature by default */
+	disable_nat_bits(sbi, true);
+
 	err = __get_nat_bitmaps(sbi);
 	if (err)
 		return err;
