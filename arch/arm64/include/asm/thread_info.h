4800209503732fdbf324efd476d3de3ae068b03b
Song Liu <song@kernel.org>
2025-03-07 17:27:42 -0800

    arm64: Implement HAVE_LIVEPATCH
    This is largely based on [1] by Suraj Jitindar Singh.
    
    Test coverage:
    
    - Passed manual tests with samples/livepatch.
    - Passed all but test-kprobe.sh in selftests/livepatch.
      test-kprobe.sh is expected to fail, because arm64 doesn't have
      KPROBES_ON_FTRACE.
    - Passed tests with kpatch-build [2]. (This version includes commits that
      are not merged to upstream kpatch yet).
    
    [1] https://lore.kernel.org/all/20210604235930.603-1-surajjs@amazon.com/
    [2] https://github.com/liu-song-6/kpatch/tree/fb-6.13
    Cc: Suraj Jitindar Singh <surajjs@amazon.com>
    Cc: Torsten Duwe <duwe@suse.de>
    Signed-off-by: Song Liu <song@kernel.org>

--- arch/arm64/include/asm/thread_info.h
+++ arch/arm64/include/asm/thread_info.h
@@ -64,6 +64,7 @@ void arch_setup_new_exec(void);
 #define TIF_UPROBE		4	/* uprobe breakpoint or singlestep */
 #define TIF_MTE_ASYNC_FAULT	5	/* MTE Asynchronous Tag Check Fault */
 #define TIF_NOTIFY_SIGNAL	6	/* signal notifications exist */
+#define TIF_PATCH_PENDING	7	/* pending live patching update */
 #define TIF_SYSCALL_TRACE	8	/* syscall trace active */
 #define TIF_SYSCALL_AUDIT	9	/* syscall auditing */
 #define TIF_SYSCALL_TRACEPOINT	10	/* syscall tracepoint for ftrace */
@@ -92,6 +93,7 @@ void arch_setup_new_exec(void);
 #define _TIF_SYSCALL_TRACEPOINT	(1 << TIF_SYSCALL_TRACEPOINT)
 #define _TIF_SECCOMP		(1 << TIF_SECCOMP)
 #define _TIF_SYSCALL_EMU	(1 << TIF_SYSCALL_EMU)
+#define _TIF_PATCH_PENDING	(1 << TIF_PATCH_PENDING)
 #define _TIF_UPROBE		(1 << TIF_UPROBE)
 #define _TIF_SINGLESTEP		(1 << TIF_SINGLESTEP)
 #define _TIF_32BIT		(1 << TIF_32BIT)
@@ -103,7 +105,7 @@ void arch_setup_new_exec(void);
 #define _TIF_WORK_MASK		(_TIF_NEED_RESCHED | _TIF_SIGPENDING | \
 				 _TIF_NOTIFY_RESUME | _TIF_FOREIGN_FPSTATE | \
 				 _TIF_UPROBE | _TIF_MTE_ASYNC_FAULT | \
-				 _TIF_NOTIFY_SIGNAL)
+				 _TIF_NOTIFY_SIGNAL | _TIF_PATCH_PENDING)
 
 #define _TIF_SYSCALL_WORK	(_TIF_SYSCALL_TRACE | _TIF_SYSCALL_AUDIT | \
 				 _TIF_SYSCALL_TRACEPOINT | _TIF_SECCOMP | \
