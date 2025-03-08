a19e19426571d079bc7e1982f6a19f15c00dc332
Blaise Boscaccy <bboscaccy@linux.microsoft.com>
2025-03-07 17:32:35 -0800

    selftests/bpf: Add a kernel flag test for LSM bpf hook
    This test exercises the kernel flag added to security_bpf by
    effectively blocking light-skeletons from loading while allowing
    normal skeletons to function as-is. Since this should work with any
    arbitrary BPF program, an existing program from LSKELS_EXTRA was
    used as a test payload.
    
    Signed-off-by: Blaise Boscaccy <bboscaccy@linux.microsoft.com>

---
+++ tools/testing/selftests/bpf/progs/test_kernel_flag.c
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+ * Copyright (C) 2025 Microsoft Corporation
+ *
+ * Author: Blaise Boscaccy <bboscaccy@linux.microsoft.com>
+ */
+
+#include "vmlinux.h"
+#include <errno.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+char _license[] SEC("license") = "GPL";
+
+__u32 monitored_pid;
+
+SEC("lsm.s/bpf")
+int BPF_PROG(bpf, int cmd, union bpf_attr *attr, unsigned int size, bool kernel)
+{
+	__u32 pid;
+
+	pid = bpf_get_current_pid_tgid() >> 32;
+	if (!kernel || pid != monitored_pid)
+		return 0;
+	else
+		return -EINVAL;
+}
