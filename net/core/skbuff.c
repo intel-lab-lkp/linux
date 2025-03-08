cf113aa91704eea774f631532dfc6a87f3cdd6e3
Mina Almasry <almasrymina@google.com>
2025-03-08 21:40:40 +0000

    net: devmem: Implement TX path
    Augment dmabuf binding to be able to handle TX. Additional to all the RX
    binding, we also create tx_vec needed for the TX path.
    
    Provide API for sendmsg to be able to send dmabufs bound to this device:
    
    - Provide a new dmabuf_tx_cmsg which includes the dmabuf to send from.
    - MSG_ZEROCOPY with SCM_DEVMEM_DMABUF cmsg indicates send from dma-buf.
    
    Devmem is uncopyable, so piggyback off the existing MSG_ZEROCOPY
    implementation, while disabling instances where MSG_ZEROCOPY falls back
    to copying.
    
    We additionally pipe the binding down to the new
    zerocopy_fill_skb_from_devmem which fills a TX skb with net_iov netmems
    instead of the traditional page netmems.
    
    We also special case skb_frag_dma_map to return the dma-address of these
    dmabuf net_iovs instead of attempting to map pages.
    
    The TX path may release the dmabuf in a context where we cannot wait.
    This happens when the user unbinds a TX dmabuf while there are still
    references to its netmems in the TX path. In that case, the netmems will
    be put_netmem'd from a context where we can't unmap the dmabuf, Resolve
    this by making __net_devmem_dmabuf_binding_free schedule_work'd.
    
    Based on work by Stanislav Fomichev <sdf@fomichev.me>. A lot of the meat
    of the implementation came from devmem TCP RFC v1[1], which included the
    TX path, but Stan did all the rebasing on top of netmem/net_iov.
    
    Cc: Stanislav Fomichev <sdf@fomichev.me>
    Signed-off-by: Kaiyuan Zhang <kaiyuanz@google.com>
    Signed-off-by: Mina Almasry <almasrymina@google.com>
    Acked-by: Stanislav Fomichev <sdf@fomichev.me>

--- net/core/skbuff.c
+++ net/core/skbuff.c
@@ -1667,7 +1667,8 @@ void mm_unaccount_pinned_pages(struct mmpin *mmp)
 }
 EXPORT_SYMBOL_GPL(mm_unaccount_pinned_pages);
 
-static struct ubuf_info *msg_zerocopy_alloc(struct sock *sk, size_t size)
+static struct ubuf_info *msg_zerocopy_alloc(struct sock *sk, size_t size,
+					    bool devmem)
 {
 	struct ubuf_info_msgzc *uarg;
 	struct sk_buff *skb;
@@ -1682,7 +1683,7 @@ static struct ubuf_info *msg_zerocopy_alloc(struct sock *sk, size_t size)
 	uarg = (void *)skb->cb;
 	uarg->mmp.user = NULL;
 
-	if (mm_account_pinned_pages(&uarg->mmp, size)) {
+	if (likely(!devmem) && mm_account_pinned_pages(&uarg->mmp, size)) {
 		kfree_skb(skb);
 		return NULL;
 	}
@@ -1705,7 +1706,7 @@ static inline struct sk_buff *skb_from_uarg(struct ubuf_info_msgzc *uarg)
 }
 
 struct ubuf_info *msg_zerocopy_realloc(struct sock *sk, size_t size,
-				       struct ubuf_info *uarg)
+				       struct ubuf_info *uarg, bool devmem)
 {
 	if (uarg) {
 		struct ubuf_info_msgzc *uarg_zc;
@@ -1735,7 +1736,8 @@ struct ubuf_info *msg_zerocopy_realloc(struct sock *sk, size_t size,
 
 		next = (u32)atomic_read(&sk->sk_zckey);
 		if ((u32)(uarg_zc->id + uarg_zc->len) == next) {
-			if (mm_account_pinned_pages(&uarg_zc->mmp, size))
+			if (likely(!devmem) &&
+			    mm_account_pinned_pages(&uarg_zc->mmp, size))
 				return NULL;
 			uarg_zc->len++;
 			uarg_zc->bytelen = bytelen;
@@ -1750,7 +1752,7 @@ struct ubuf_info *msg_zerocopy_realloc(struct sock *sk, size_t size,
 	}
 
 new_alloc:
-	return msg_zerocopy_alloc(sk, size);
+	return msg_zerocopy_alloc(sk, size, devmem);
 }
 EXPORT_SYMBOL_GPL(msg_zerocopy_realloc);
 
@@ -1854,7 +1856,8 @@ EXPORT_SYMBOL_GPL(msg_zerocopy_ubuf_ops);
 
 int skb_zerocopy_iter_stream(struct sock *sk, struct sk_buff *skb,
 			     struct msghdr *msg, int len,
-			     struct ubuf_info *uarg)
+			     struct ubuf_info *uarg,
+			     struct net_devmem_dmabuf_binding *binding)
 {
 	int err, orig_len = skb->len;
 
@@ -1873,7 +1876,8 @@ int skb_zerocopy_iter_stream(struct sock *sk, struct sk_buff *skb,
 			return -EEXIST;
 	}
 
-	err = __zerocopy_sg_from_iter(msg, sk, skb, &msg->msg_iter, len);
+	err = __zerocopy_sg_from_iter(msg, sk, skb, &msg->msg_iter, len,
+				      binding);
 	if (err == -EFAULT || (err == -EMSGSIZE && skb->len == orig_len)) {
 		struct sock *save_sk = skb->sk;
 
