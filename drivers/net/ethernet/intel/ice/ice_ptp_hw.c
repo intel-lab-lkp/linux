55affca98fbf32edd5a59f73cd7bd575b427a54e
Karol Kolacinski <karol.kolacinski@intel.com>
2025-03-10 12:12:54 +0100

    ice: move TSPLL init calls to ice_ptp.c
    Initialize TSPLL after initializing PHC in ice_ptp.c instead of calling
    for each product in PHC init in ice_ptp_hw.c.
    
    Reviewed-by: Michal Kubiak <michal.kubiak@intel.com>
    Reviewed-by: Milena Olech <milena-olech@intel.com>
    Signed-off-by: Karol Kolacinski <karol.kolacinski@intel.com>

--- drivers/net/ethernet/intel/ice/ice_ptp_hw.c
+++ drivers/net/ethernet/intel/ice/ice_ptp_hw.c
@@ -2108,22 +2108,6 @@ static void ice_sb_access_ena_eth56g(struct ice_hw *hw, bool enable)
 }
 
 /**
- * ice_ptp_init_phc_e825 - Perform E825 specific PHC initialization
- * @hw: pointer to HW struct
- *
- * Perform E825-specific PTP hardware clock initialization steps.
- *
- * Return: 0 on success, negative error code otherwise.
- */
-static int ice_ptp_init_phc_e825(struct ice_hw *hw)
-{
-	ice_sb_access_ena_eth56g(hw, true);
-
-	/* Initialize the Clock Generation Unit */
-	return ice_tspll_init(hw);
-}
-
-/**
  * ice_ptp_read_tx_hwtstamp_status_eth56g - Get TX timestamp status
  * @hw: pointer to the HW struct
  * @ts_status: the timestamp mask pointer
@@ -2784,7 +2768,6 @@ static int ice_ptp_set_vernier_wl(struct ice_hw *hw)
  */
 static int ice_ptp_init_phc_e82x(struct ice_hw *hw)
 {
-	int err;
 	u32 val;
 
 	/* Enable reading switch and PHY registers over the sideband queue */
@@ -2793,11 +2776,6 @@ static int ice_ptp_init_phc_e82x(struct ice_hw *hw)
 	val = rd32(hw, PF_SB_REM_DEV_CTL);
 	val |= (PF_SB_REM_DEV_CTL_SWITCH_READ | PF_SB_REM_DEV_CTL_PHY0);
 	wr32(hw, PF_SB_REM_DEV_CTL, val);
-
-	/* Initialize the Clock Generation Unit */
-	err = ice_tspll_init(hw);
-	if (err)
-		return err;
 
 	/* Set window length for all the ports */
 	return ice_ptp_set_vernier_wl(hw);
@@ -5580,7 +5558,7 @@ int ice_ptp_init_phc(struct ice_hw *hw)
 	case ICE_MAC_GENERIC:
 		return ice_ptp_init_phc_e82x(hw);
 	case ICE_MAC_GENERIC_3K_E825:
-		return ice_ptp_init_phc_e825(hw);
+		return 0;
 	default:
 		return -EOPNOTSUPP;
 	}
