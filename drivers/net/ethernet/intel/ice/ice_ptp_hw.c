08a8129b975ed76adbf8345d203d59d28a16605b
Karol Kolacinski <karol.kolacinski@intel.com>
2025-03-10 12:12:46 +0100

    ice: rename TSPLL and CGU functions and definitions
    Rename TSPLL and CGU functions, definitions etc. to match the file name
    and have constistent naming scheme.
    
    Reviewed-by: Michal Kubiak <michal.kubiak@intel.com>
    Reviewed-by: Milena Olech <milena-olech@intel.com>
    Signed-off-by: Karol Kolacinski <karol.kolacinski@intel.com>

--- drivers/net/ethernet/intel/ice/ice_ptp_hw.c
+++ drivers/net/ethernet/intel/ice/ice_ptp_hw.c
@@ -2120,7 +2120,7 @@ static int ice_ptp_init_phc_e825(struct ice_hw *hw)
 	ice_sb_access_ena_eth56g(hw, true);
 
 	/* Initialize the Clock Generation Unit */
-	return ice_init_cgu_e82x(hw);
+	return ice_tspll_init(hw);
 }
 
 /**
@@ -2795,7 +2795,7 @@ static int ice_ptp_init_phc_e82x(struct ice_hw *hw)
 	wr32(hw, PF_SB_REM_DEV_CTL, val);
 
 	/* Initialize the Clock Generation Unit */
-	err = ice_init_cgu_e82x(hw);
+	err = ice_tspll_init(hw);
 	if (err)
 		return err;
 
