dbbb82c786fd64f50b0e78626588192fb6462e3d
Nam Cao <namcao@linutronix.de>
2025-03-11 18:05:08 +0100

    arm64: mm: Add page fault trace points
    Add page fault trace points, which are useful to implement RV monitor which
    watches page faults.
    
    Signed-off-by: Nam Cao <namcao@linutronix.de>

--- arch/arm64/mm/fault.c
+++ arch/arm64/mm/fault.c
@@ -44,6 +44,9 @@
 #include <asm/tlbflush.h>
 #include <asm/traps.h>
 
+#define CREATE_TRACE_POINTS
+#include <trace/events/exceptions.h>
+
 struct fault_info {
 	int	(*fn)(unsigned long far, unsigned long esr,
 		      struct pt_regs *regs);
@@ -558,6 +561,11 @@ static int __kprobes do_page_fault(unsigned long far, unsigned long esr,
 
 	if (kprobe_page_fault(regs, esr))
 		return 0;
+
+	if (user_mode(regs))
+		trace_page_fault_user(addr, regs, esr);
+	else
+		trace_page_fault_kernel(addr, regs, esr);
 
 	/*
 	 * If we're in an interrupt or have no user context, we must not take
