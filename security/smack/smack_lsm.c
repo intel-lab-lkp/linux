25b7441f41bbf7db1a22e4159aa779f0414e33cd
Ryan Lee <ryan.lee@canonical.com>
2025-03-12 14:21:45 -0700

    smack: explicitly skip mediation of O_PATH file descriptors
    Now that O_PATH fds are being passed to the file_open hook,
    unconditionally skip mediation of them to preserve existing behavior.
    
    Signed-off-by: Ryan Lee <ryan.lee@canonical.com>

--- security/smack/smack_lsm.c
+++ security/smack/smack_lsm.c
@@ -2023,6 +2023,10 @@ static int smack_file_open(struct file *file)
 	struct smk_audit_info ad;
 	int rc;
 
+	/* Preserve the behavior of O_PATH fd creation not being mediated */
+	if (file->f_flags & O_PATH)
+		return 0;
+
 	smk_ad_init(&ad, __func__, LSM_AUDIT_DATA_PATH);
 	smk_ad_setfield_u_fs_path(&ad, file->f_path);
 	rc = smk_tskacc(tsp, smk_of_inode(inode), MAY_READ, &ad);
