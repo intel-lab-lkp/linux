3fe2f011bfdef8b9905cb40a4dde822ed7f7c883
Nithyanantham Paramasivam <quic_nithp@quicinc.com>
2025-03-13 07:31:53 +0530

    wifi: cfg80211: Allow monitor creation in NO_VIRTUAL_MONITOR mode with active AP
    Currently, in NO_VIRTUAL_MONITOR mode, when creating an
    AP/STA + monitor, there is a restriction: if the AP/STA is running,
    setting the channel for the monitor is not allowed. For example,
    in a scenario with three supported radios where the AP uses only the
    2 GHz and 5 GHz bands, the 6 GHz band remains available. However,
    due to the restriction that rdev->num_running_ifaces must equal
    rdev->num_running_monitor_ifaces in cfg80211_has_monitors_only(),
    we are unable to create the monitor interface.
    
    cfg80211_set_monitor_channel -> cfg80211_has_monitors_only()
    
    static inline bool cfg80211_has_monitors_only() {
    ...
       return rdev->num_running_ifaces == rdev->num_running_monitor_ifaces
            && rdev->num_running_ifaces > 0;
    }
    
    To address this, introduce the new
    WIPHY_FLAG_SUPPORTS_CONCUR_MONITOR_N_OTHER_VIF
    flag to advertise concurrent support for monitor mode alongside AP/STA
    interfaces in cfg80211. This flag will enable the creation of
    a monitor interface by bypassing the cfg80211_has_monitors_only()
    function. In the mac80211 layer, no additional changes are required.
    It follows the same sequence as standalone monitor creation in
    NO_VIRTUAL_MONITOR mode.
    
    Signed-off-by: Nithyanantham Paramasivam <quic_nithp@quicinc.com>

--- net/wireless/chan.c
+++ net/wireless/chan.c
@@ -1509,7 +1509,9 @@ int cfg80211_set_monitor_channel(struct cfg80211_registered_device *rdev,
 {
 	if (!rdev->ops->set_monitor_channel)
 		return -EOPNOTSUPP;
-	if (!cfg80211_has_monitors_only(rdev))
+	if (!(rdev->wiphy.flags &
+	      WIPHY_FLAG_SUPPORTS_CONCUR_MONITOR_N_OTHER_VIF) &&
+	    !cfg80211_has_monitors_only(rdev))
 		return -EBUSY;
 
 	return rdev_set_monitor_channel(rdev, dev, chandef);
