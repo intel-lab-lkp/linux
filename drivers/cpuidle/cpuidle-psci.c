7453aa46425d47bde83576fabcca9098f9385546
Sudeep Holla <sudeep.holla@arm.com>
2025-03-18 17:01:40 +0000

    cpuidle: psci: Transition to the faux device interface
    The PSCI cpuidle driver does not require the creation of a platform
    device. Originally, this approach was chosen for simplicity when the
    driver was first implemented.
    
    With the introduction of the lightweight faux device interface, we now
    have a more appropriate alternative. Migrate the driver to utilize the
    faux bus, given that the platform device it previously created was not
    a real one anyway. This will simplify the code, reducing its footprint
    while maintaining functionality.
    
    Cc: Lorenzo Pieralisi <lpieralisi@kernel.org>
    Cc: "Rafael J. Wysocki" <rafael@kernel.org>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: linux-pm@vger.kernel.org
    Signed-off-by: Sudeep Holla <sudeep.holla@arm.com>

--- drivers/cpuidle/cpuidle-psci.c
+++ drivers/cpuidle/cpuidle-psci.c
@@ -16,7 +16,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/of.h>
-#include <linux/platform_device.h>
+#include <linux/device/faux.h>
 #include <linux/psci.h>
 #include <linux/pm_domain.h>
 #include <linux/pm_runtime.h>
@@ -404,14 +404,14 @@ deinit:
  * to register cpuidle driver then rollback to cancel all CPUs
  * registration.
  */
-static int psci_cpuidle_probe(struct platform_device *pdev)
+static int psci_cpuidle_probe(struct faux_device *fdev)
 {
 	int cpu, ret;
 	struct cpuidle_driver *drv;
 	struct cpuidle_device *dev;
 
 	for_each_possible_cpu(cpu) {
-		ret = psci_idle_init_cpu(&pdev->dev, cpu);
+		ret = psci_idle_init_cpu(&fdev->dev, cpu);
 		if (ret)
 			goto out_fail;
 	}
@@ -431,28 +431,4 @@ out_fail:
 	return ret;
 }
 
-static struct platform_driver psci_cpuidle_driver = {
-	.probe = psci_cpuidle_probe,
-	.driver = {
-		.name = "psci-cpuidle",
-	},
-};
-
-static int __init psci_idle_init(void)
-{
-	struct platform_device *pdev;
-	int ret;
-
-	ret = platform_driver_register(&psci_cpuidle_driver);
-	if (ret)
-		return ret;
-
-	pdev = platform_device_register_simple("psci-cpuidle", -1, NULL, 0);
-	if (IS_ERR(pdev)) {
-		platform_driver_unregister(&psci_cpuidle_driver);
-		return PTR_ERR(pdev);
-	}
-
-	return 0;
-}
-device_initcall(psci_idle_init);
+module_faux_driver(psci_cpuidle, psci_cpuidle_probe, NULL, true);
