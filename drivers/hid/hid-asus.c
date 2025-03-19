035d0ef2d06d63ae03738b84d6de6ed10d011426
Antheas Kapenekakis <lkml@antheas.dev>
2025-03-19 20:13:14 +0100

    HID: asus: introduce small delay on Asus Z13 RGB init
    The folio keyboard of the Z13 can get stuck in its BIOS mode, where the
    touchpad behaves like a mouse and the keyboard start button is not
    reliable if we perform the initialization too quickly. This mostly
    happens during boot, and can be verified to be caused by hid-asus
    through simple blacklisting. A small delay fixes it.
    
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>

--- drivers/hid/hid-asus.c
+++ drivers/hid/hid-asus.c
@@ -571,6 +571,10 @@ static int asus_kbd_register_leds(struct hid_device *hdev)
 	unsigned char kbd_func;
 	int ret;
 
+	/* Wait a bit before init to prevent locking the keyboard */
+	if (dmi_match(DMI_PRODUCT_FAMILY, "ROG Flow Z13"))
+		msleep(500);
+
 	ret = asus_kbd_init(hdev);
 	if (ret < 0)
 		return ret;
