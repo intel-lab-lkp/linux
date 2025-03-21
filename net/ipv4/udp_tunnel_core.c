e6b9c6550b489165bac7938385aebb12a76e7640
Paolo Abeni <pabeni@redhat.com>
2025-03-21 12:52:55 +0100

    udp_tunnel: avoid inconsistent local variables usage
    In setup_udp_tunnel_sock() 'sk' and 'sock->sk' are alias. The code
    I introduced there uses alternatively both variables, for no good
    reasons.
    
    Stick to 'sk' usage, to be consistent with the prior code.
    
    Signed-off-by: Paolo Abeni <pabeni@redhat.com>

--- net/ipv4/udp_tunnel_core.c
+++ net/ipv4/udp_tunnel_core.c
@@ -90,10 +90,10 @@ void setup_udp_tunnel_sock(struct net *net, struct socket *sock,
 
 	udp_tunnel_encap_enable(sk);
 
-	udp_tunnel_update_gro_rcv(sock->sk, true);
+	udp_tunnel_update_gro_rcv(sk, true);
 
-	if (!sk->sk_dport && !sk->sk_bound_dev_if && sk_saddr_any(sock->sk))
-		udp_tunnel_update_gro_lookup(net, sock->sk, true);
+	if (!sk->sk_dport && !sk->sk_bound_dev_if && sk_saddr_any(sk))
+		udp_tunnel_update_gro_lookup(net, sk, true);
 }
 EXPORT_SYMBOL_GPL(setup_udp_tunnel_sock);
 
