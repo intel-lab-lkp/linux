f60fed3864afea8a951d428beb77c4b1c114c7c6
Danilo Krummrich <dakr@kernel.org>
2025-03-21 15:57:56 +0100

    rust: device: implement bus_type_raw()
    Implement bus_type_raw(), which returns a raw pointer to the device'
    struct bus_type.
    
    This is useful for bus devices, to implement the following trait.
    
    	impl TryFrom<&Device> for &pci::Device
    
    With this a caller can try to get the bus specific device from a generic
    device in a safe way. try_from() will only succeed if the generic
    device' bus type pointer matches the pointer of the bus' type.
    
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Benno Lossin <benno.lossin@proton.me>
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- rust/kernel/device.rs
+++ rust/kernel/device.rs
@@ -65,6 +65,16 @@ impl Device {
         self.0.get()
     }
 
+    /// Returns a raw pointer to the device' bus type.
+    #[expect(unused)]
+    pub(crate) fn bus_type_raw(&self) -> *const bindings::bus_type {
+        // SAFETY:
+        // - By the type invariants, `self.as_raw()` is a valid pointer to a `struct device`.
+        // - `dev->bus` is a pointer to a `const struct bus_type`, which is only ever set at device
+        //    creation.
+        unsafe { (*self.as_raw()).bus }
+    }
+
     /// Convert a raw C `struct device` pointer to a `&'a Device`.
     ///
     /// # Safety
