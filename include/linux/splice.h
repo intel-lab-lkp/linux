6a23a8785805de67717d545e6f8148ed9f27a886
Joe Damato <jdamato@fastly.com>
2025-03-22 20:35:45 +0000

    splice: Move splice_to_socket to net/socket.c
    Eliminate the #ifdef CONFIG_NET from fs/splice.c and move the
    splice_to_socket helper to net/socket.c, where the other splice socket
    helpers live (like sock_splice_read and sock_splice_eof).
    
    Signed-off-by: Joe Damato <jdamato@fastly.com>

--- include/linux/splice.h
+++ include/linux/splice.h
@@ -97,9 +97,6 @@ static inline long splice_copy_file_range(struct file *in, loff_t pos_in,
 
 ssize_t do_tee(struct file *in, struct file *out, size_t len,
 	       unsigned int flags);
-ssize_t splice_to_socket(struct pipe_inode_info *pipe, struct file *out,
-			 loff_t *ppos, size_t len, unsigned int flags);
-
 /*
  * for dynamic pipe sizing
  */
