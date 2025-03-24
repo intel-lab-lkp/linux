f35eb6db5219e426283eab423675603fb4d19bee
P Praneesh <quic_ppranees@quicinc.com>
2025-03-24 11:55:18 +0530

    wifi: ath12k: add monitor interface support on QCN9274
    Currently, the monitor interface is not supported. To support the monitor
    interface, configure the monitor vdev state identifier, configure the HTT
    filter setup, subscribe the mac80211 NO_VIRTUAL_MONITOR feature, remove
    the VIRTUAL_MONITOR handler procedures since align to NO_VIRTUAL_MONITOR
    feature and prevent monitor interface to transmit packet. Therefore, add
    these procedures to add monitor interface support and enable the monitor
    interface support on the QCN9274 platform through the hardware parameter.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.3.1-00173-QCAHKSWPL_SILICONZ-1
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0.c5-00481-QCAHMTSWPL_V1.0_V2.0_SILICONZ-3
    
    Signed-off-by: P Praneesh <quic_ppranees@quicinc.com>
    Signed-off-by: Karthikeyan Periyasamy <quic_periyasa@quicinc.com>

--- drivers/net/wireless/ath/ath12k/hw.c
+++ drivers/net/wireless/ath/ath12k/hw.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 /*
  * Copyright (c) 2018-2021 The Linux Foundation. All rights reserved.
- * Copyright (c) 2021-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2021-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  */
 
 #include <linux/types.h>
@@ -1049,7 +1049,7 @@ static const struct ath12k_hw_params ath12k_hw_params[] = {
 					BIT(NL80211_IFTYPE_AP) |
 					BIT(NL80211_IFTYPE_MESH_POINT) |
 					BIT(NL80211_IFTYPE_AP_VLAN),
-		.supports_monitor = false,
+		.supports_monitor = true,
 
 		.idle_ps = false,
 		.download_calib = true,
