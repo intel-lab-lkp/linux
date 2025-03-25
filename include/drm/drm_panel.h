1266fb956c2d7c09b8ac97d914eba5c073d40b33
Anusha Srivatsa <asrivats@redhat.com>
2025-03-25 13:24:09 -0400

    drm/panel: Add refcount support
    Allocate panel via reference counting.
    Add _get() and _put() helper functions
    to ensure panel allocations are refcounted.
    Avoid use after free by ensuring panel is
    valid and can be usable till the last reference
    is put. This avoids use-after-free
    
    Signed-off-by: Anusha Srivatsa <asrivats@redhat.com>

--- include/drm/drm_panel.h
+++ include/drm/drm_panel.h
@@ -267,6 +267,17 @@ struct drm_panel {
 	 * If true then the panel has been enabled.
 	 */
 	bool enabled;
+
+	/**
+	 * @container: Pointer to the private driver struct embedding this
+	 * @struct drm_panel.
+	 */
+	void *container;
+
+	/**
+	 * @refcount: reference count of users referencing this bridge.
+	 */
+	struct kref refcount;
 };
 
 void *__devm_drm_panel_alloc(struct device *dev, size_t size, size_t offset,
@@ -280,7 +291,10 @@ void *__devm_drm_panel_alloc(struct device *dev, size_t size, size_t offset,
  * @member: the name of the &drm_panel within @type
  * @funcs: callbacks for this panel
  * @connector_type: connector type of the driver
- * The returned refcount is initialised to 1
+ *
+ * The returned refcount is initialised to 1. This  reference will
+ * be automatically dropped via devm (by calling
+ * drm_bridge_put()) when @dev is removed.
  *
  * Returns:
  * Pointer to new panel, or ERR_PTR on failure.
@@ -293,6 +307,9 @@ void *__devm_drm_panel_alloc(struct device *dev, size_t size, size_t offset,
 void drm_panel_init(struct drm_panel *panel, struct device *dev,
 		    const struct drm_panel_funcs *funcs,
 		    int connector_type);
+
+struct drm_panel *drm_panel_get(struct drm_panel *panel);
+struct drm_panel *drm_panel_put(struct drm_panel *panel);
 
 void drm_panel_add(struct drm_panel *panel);
 void drm_panel_remove(struct drm_panel *panel);
