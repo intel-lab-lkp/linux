e2118a4dc822e1228630603bd82e07eae95a46f1
Matti Vaittinen <mazziesaccount@gmail.com>
2025-03-31 11:03:58 +0300

    iio: adc: ti-adc128s052: Support ROHM BD79104
    The ROHM BD79104 ADC has identical SPI communication logic as the
    ti-adc128s052. Eg, SPI transfer should be 16 clk cycles, conversion is
    started when the CS is pulled low, and channel selection is done by
    writing the channel ID after two zero bits. Data is contained in
    big-endian format in the last 12 bits.
    
    The BD79104 has two input voltage pins. Data sheet uses terms "vdd" and
    "iovdd". The "vdd" is used also as an analog reference voltage. Hence
    the driver expects finding these from the device-tree, instead of having
    the "vref" only as TI's driver.
    
    NOTE: The TI's data sheet[1] does show that the TI's IC does actually
    have two voltage inputs as well. Pins are called Va (analog reference)
    and Vd (digital supply pin) - but I keep the existing driver behaviour
    for the TI's IC "as is", because I have no HW to test changes, and
    because I have no real need to touch it.
    
    NOTE II: The BD79104 requires SPI MODE 3.
    
    NOTE III: I used evaluation board "BD79104FV-EVK-001" made by ROHM. With
    this board I had to drop the SPI speed below the 20M which is mentioned
    in the data-sheet [2]. This, however, may be a limitation of the EVK
    board, not the component itself.
    
    [1]: https://www.ti.com/lit/ds/symlink/adc128s052.pdf
    
    [2]:
    https://fscdn.rohm.com/en/products/databook/datasheet/ic/data_converter/dac/bd79104fv-la-e.pdf
    
    Signed-off-by: Matti Vaittinen <mazziesaccount@gmail.com>

--- drivers/iio/adc/ti-adc128s052.c
+++ drivers/iio/adc/ti-adc128s052.c
@@ -21,6 +21,9 @@
 struct adc128_configuration {
 	const struct iio_chan_spec	*channels;
 	u8				num_channels;
+	const char			*refname;
+	int				num_other_regulators;
+	const char * const		(*other_regulators)[];
 };
 
 struct adc128 {
@@ -119,10 +122,28 @@ static const struct iio_chan_spec adc124s021_channels[] = {
 	ADC128_VOLTAGE_CHANNEL(3),
 };
 
+static const char * const bd79104_regulators[] = { "iovdd" };
+
 static const struct adc128_configuration adc128_config[] = {
-	{ adc128s052_channels, ARRAY_SIZE(adc128s052_channels) },
-	{ adc122s021_channels, ARRAY_SIZE(adc122s021_channels) },
-	{ adc124s021_channels, ARRAY_SIZE(adc124s021_channels) },
+	{
+		.channels = adc128s052_channels,
+		.num_channels = ARRAY_SIZE(adc128s052_channels),
+		.refname = "vref",
+	}, {
+		.channels = adc122s021_channels,
+		.num_channels = ARRAY_SIZE(adc122s021_channels),
+		.refname = "vref",
+	}, {
+		.channels = adc124s021_channels,
+		.num_channels = ARRAY_SIZE(adc124s021_channels),
+		.refname = "vref",
+	}, {
+		.channels = adc128s052_channels,
+		.num_channels = ARRAY_SIZE(adc128s052_channels),
+		.refname = "vdd",
+		.other_regulators = &bd79104_regulators,
+		.num_other_regulators = 1,
+	},
 };
 
 static const struct iio_info adc128_info = {
@@ -157,7 +178,7 @@ static int adc128_probe(struct spi_device *spi)
 	indio_dev->channels = config->channels;
 	indio_dev->num_channels = config->num_channels;
 
-	adc->reg = devm_regulator_get(&spi->dev, "vref");
+	adc->reg = devm_regulator_get(&spi->dev, config->refname);
 	if (IS_ERR(adc->reg))
 		return PTR_ERR(adc->reg);
 
@@ -168,6 +189,15 @@ static int adc128_probe(struct spi_device *spi)
 				       adc->reg);
 	if (ret)
 		return ret;
+
+	if (config->num_other_regulators) {
+		ret = devm_regulator_bulk_get_enable(&spi->dev,
+						config->num_other_regulators,
+						*config->other_regulators);
+		if (ret)
+			return dev_err_probe(&spi->dev, ret,
+					     "Failed to enable regulators\n");
+	}
 
 	devm_mutex_init(&spi->dev, &adc->lock);
 
@@ -182,6 +212,7 @@ static const struct of_device_id adc128_of_match[] = {
 	{ .compatible = "ti,adc124s021", .data = &adc128_config[2] },
 	{ .compatible = "ti,adc124s051", .data = &adc128_config[2] },
 	{ .compatible = "ti,adc124s101", .data = &adc128_config[2] },
+	{ .compatible = "rohm,bd79104", .data = &adc128_config[3] },
 	{ },
 };
 MODULE_DEVICE_TABLE(of, adc128_of_match);
@@ -194,6 +225,7 @@ static const struct spi_device_id adc128_id[] = {
 	{ "adc124s021", (kernel_ulong_t)&adc128_config[2] },
 	{ "adc124s051", (kernel_ulong_t)&adc128_config[2] },
 	{ "adc124s101", (kernel_ulong_t)&adc128_config[2] },
+	{ "bd79104", (kernel_ulong_t)&adc128_config[3] },
 	{ }
 };
 MODULE_DEVICE_TABLE(spi, adc128_id);
