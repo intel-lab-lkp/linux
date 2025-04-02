1edcf3a981f65927a51cae8551b5cbfbcf66eb43
Zhang Enpei <zhang.enpei@zte.com.cn>
2025-04-02 19:43:57 +0800

    phy: qualcomm: phy-qcom-ipq4019-usb: Use dev_err_probe()
    Replace the open-code with dev_err_probe() to simplify the code.
    
    Signed-off-by: Zhang Enpei <zhang.enpei@zte.com.cn>
    Signed-off-by: Shao Mingyin <shao.mingyin@zte.com.cn>

--- drivers/phy/qualcomm/phy-qcom-ipq4019-usb.c
+++ drivers/phy/qualcomm/phy-qcom-ipq4019-usb.c
@@ -109,11 +109,9 @@ static int ipq4019_usb_phy_probe(struct platform_device *pdev)
 	}
 
 	phy->por_rst = devm_reset_control_get(phy->dev, "por_rst");
-	if (IS_ERR(phy->por_rst)) {
-		if (PTR_ERR(phy->por_rst) != -EPROBE_DEFER)
-			dev_err(dev, "POR reset is missing\n");
-		return PTR_ERR(phy->por_rst);
-	}
+	if (IS_ERR(phy->por_rst))
+		return dev_err_probe(dev, PTR_ERR(phy->por_rst),
+				     "POR reset is missing\n");
 
 	phy->srif_rst = devm_reset_control_get_optional(phy->dev, "srif_rst");
 	if (IS_ERR(phy->srif_rst))
