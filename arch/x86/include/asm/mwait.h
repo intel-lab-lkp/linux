e0500cc4c4cf8a53420fedd54ddf214b52ab36a6
Andrew Cooper <andrew.cooper3@citrix.com>
2025-04-02 18:24:58 +0100

    x86/idle: Remove MFENCEs for X86_BUG_CLFLUSH_MONITOR
    Commit 7e98b7192046 ("x86, idle: Use static_cpu_has() for CLFLUSH
    workaround, add barriers") adds barriers, justified with:
    
      ... and add memory barriers around it since the documentation is explicit
      that CLFLUSH is only ordered with respect to MFENCE.
    
    This also triggered the same adjustment in commit
    f8e617f45829 ("sched/idle/x86: Optimize unnecessary mwait_idle() resched
    IPIs") during development, although it failed to get the static_cpu_has_bug()
    treatment.
    
    X86_BUG_CLFLUSH_MONITOR (a.k.a the AAI65 errata) is specific to Intel CPUs,
    and the SDM currently states:
    
      Executions of the CLFLUSH instruction are ordered with respect to each
      other and with respect to writes, locked read-modify-write instructions,
      and fence instructions[1].
    
    With footnote 1 reading:
    
      Earlier versions of this manual specified that executions of the CLFLUSH
      instruction were ordered only by the MFENCE instruction.  All processors
      implementing the CLFLUSH instruction also order it relative to the other
      operations enumerated above.
    
    i.e. The SDM was incorrect at the time, and barriers should not have been
    inserted.  Double checking the original AAI65 errata (not available from
    intel.com any more) shows no mention of barriers either.
    
    Note: If this were a general codepath, the MFENCEs would be needed, because
          AMD CPUs of the same vintage do sport otherwise-unordered CLFLUSHs.
    
    Furthermore, use a plain alternative, rather than static_cpu_has_bug() and/or
    no optimisation.  The workaround is a single instruction.
    
    Use an explicit %rax pointer rather than a general memory operand, because
    MONITOR takes the pointer implicitly in the same way.
    
    Link: https://web.archive.org/web/20090219054841/http://download.intel.com/design/xeon/specupdt/32033601.pdf
    Fixes: 7e98b7192046 ("x86, idle: Use static_cpu_has() for CLFLUSH workaround, add barriers")
    Fixes: f8e617f45829 ("sched/idle/x86: Optimize unnecessary mwait_idle() resched IPIs")
    Signed-off-by: Andrew Cooper <andrew.cooper3@citrix.com>

--- arch/x86/include/asm/mwait.h
+++ arch/x86/include/asm/mwait.h
@@ -116,13 +116,12 @@ static __always_inline void __sti_mwait(unsigned long eax, unsigned long ecx)
 static __always_inline void mwait_idle_with_hints(unsigned long eax, unsigned long ecx)
 {
 	if (static_cpu_has_bug(X86_BUG_MONITOR) || !current_set_polling_and_test()) {
-		if (static_cpu_has_bug(X86_BUG_CLFLUSH_MONITOR)) {
-			mb();
-			clflush((void *)&current_thread_info()->flags);
-			mb();
-		}
 
-		__monitor((void *)&current_thread_info()->flags, 0, 0);
+		const void *addr = &current_thread_info()->flags;
+
+		alternative_input("", "clflush (%[addr])", X86_BUG_CLFLUSH_MONITOR,
+				  [addr] "a" (addr));
+		__monitor(addr, 0, 0);
 
 		if (!need_resched()) {
 			if (ecx & 1) {
