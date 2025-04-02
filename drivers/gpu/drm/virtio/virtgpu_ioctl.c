a678cfeae45f202dd2fe1008ce3ce5e7bb13dd4d
Sergio Lopez <slp@redhat.com>
2025-04-02 19:46:01 +0200

    drm/virtio: add VIRTGPU_PARAM_HOST_PAGE_SIZE to params
    Add VIRTGPU_PARAM_HOST_PAGE_SIZE as a param that can be read with
    VIRTGPU_GETPARAM by userspace applications running in the guest to
    obtain the host's page size and find out the right alignment to be used
    in shared memory allocations.
    
    Signed-off-by: Sergio Lopez <slp@redhat.com>

--- drivers/gpu/drm/virtio/virtgpu_ioctl.c
+++ drivers/gpu/drm/virtio/virtgpu_ioctl.c
@@ -117,6 +117,11 @@ static int virtio_gpu_getparam_ioctl(struct drm_device *dev, void *data,
 	case VIRTGPU_PARAM_EXPLICIT_DEBUG_NAME:
 		value = vgdev->has_context_init ? 1 : 0;
 		break;
+	case VIRTGPU_PARAM_HOST_PAGE_SIZE:
+		if (!vgdev->has_host_page_size)
+			return -EINVAL;
+		value = vgdev->host_page_size;
+		break;
 	default:
 		return -EINVAL;
 	}
