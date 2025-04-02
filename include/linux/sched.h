410abe48801fe01afabf9ad2044eb7612c8be89c
Zecheng Li <zecheng@google.com>
2025-04-02 21:29:02 +0000

    sched/fair: Reorder struct sched_entity
    Group the mostly read fields in struct sched_entity to the first
    cacheline when `CONFIG_FAIR_GROUP_SCHED` is set. This moves the
    additional fields from `CONFIG_FAIR_GROUP_SCHED` to the first cache line
    since they are mostly accessed and generally read most. Currently these
    fields related to cfs cgroup scheduling is placed on a separate
    cacheline from hot fields `load`, `on_rq` and `vruntime`. Although
    `depth` is not as hot as other fields, we keep it here to avoid breaking
    the #ifdef boundaries.
    
    Also adds a compile time check when `CONFIG_FAIR_GROUP_SCHED` is set to
    check the placement of the hot fields.
    
    Signed-off-by: Zecheng Li <zecheng@google.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -545,31 +545,9 @@ struct sched_statistics {
 } ____cacheline_aligned;
 
 struct sched_entity {
-	/* For load-balancing: */
-	struct load_weight		load;
-	struct rb_node			run_node;
-	u64				deadline;
-	u64				min_vruntime;
-	u64				min_slice;
-
-	struct list_head		group_node;
-	unsigned char			on_rq;
-	unsigned char			sched_delayed;
-	unsigned char			rel_deadline;
-	unsigned char			custom_slice;
-					/* hole */
-
-	u64				exec_start;
-	u64				sum_exec_runtime;
-	u64				prev_sum_exec_runtime;
-	u64				vruntime;
-	s64				vlag;
-	u64				slice;
-
-	u64				nr_migrations;
-
 #ifdef CONFIG_FAIR_GROUP_SCHED
-	int				depth;
+	/* Group the read most hot fields in sched_entity in a cache line */
+	__cacheline_group_begin_aligned(hot);
 	struct sched_entity		*parent;
 	/* rq on which this entity is (to be) queued: */
 	struct cfs_rq			*cfs_rq;
@@ -577,7 +555,32 @@ struct sched_entity {
 	struct cfs_rq			*my_q;
 	/* cached value of my_q->h_nr_running */
 	unsigned long			runnable_weight;
+	int				depth;
 #endif
+	unsigned char			on_rq;
+	unsigned char			sched_delayed;
+	unsigned char			rel_deadline;
+	unsigned char			custom_slice;
+	/* For load-balancing: */
+	struct load_weight		load;
+	u64				vruntime;
+#ifdef CONFIG_FAIR_GROUP_SCHED
+	__cacheline_group_end_aligned(hot);
+#endif
+	struct rb_node			run_node;
+	u64				deadline;
+	u64				min_vruntime;
+	u64				min_slice;
+
+	struct list_head		group_node;
+
+	u64				exec_start;
+	u64				sum_exec_runtime;
+	u64				prev_sum_exec_runtime;
+	s64				vlag;
+	u64				slice;
+
+	u64				nr_migrations;
 
 #ifdef CONFIG_SMP
 	/*
