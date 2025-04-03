b922667467d4900f646ba9b7ec7f7b149999f716
xu xin <xu.xin16@zte.com.cn>
2025-04-03 13:37:00 +0800

    memcontrol-v1: add ksm_zero_pages in cgroup/memory.ksm_stat
    Users can obtain ksm_zero_pages of a cgroup just by:
    
    / # cat /sys/fs/cgroup/memory.ksm_stat
    ksm_rmap_items 76800
    ksm_zero_pages 0
    
    Current implementation supports cgroup v1 temporarily; cgroup v2
    compatibility is planned for future versions.
    
    Signed-off-by: xu xin <xu.xin16@zte.com.cn>

--- mm/memcontrol-v1.c
+++ mm/memcontrol-v1.c
@@ -11,6 +11,7 @@
 #include <linux/sort.h>
 #include <linux/file.h>
 #include <linux/seq_buf.h>
+#include <linux/ksm.h>
 
 #include "internal.h"
 #include "swap.h"
@@ -1824,6 +1825,7 @@ static int memcg_numa_stat_show(struct seq_file *m, void *v)
 #ifdef CONFIG_KSM
 struct memcg_ksm_stat {
 	unsigned long ksm_rmap_items;
+	long ksm_zero_pages;
 };
 
 static int evaluate_memcg_ksm_stat(struct task_struct *task, void *arg)
@@ -1834,6 +1836,7 @@ static int evaluate_memcg_ksm_stat(struct task_struct *task, void *arg)
 	mm = get_task_mm(task);
 	if (mm) {
 		ksm_stat->ksm_rmap_items += mm->ksm_rmap_items;
+		ksm_stat->ksm_zero_pages += mm_ksm_zero_pages(mm);
 		mmput(mm);
 	}
 
@@ -1847,9 +1850,13 @@ static int memcg_ksm_stat_show(struct seq_file *m, void *v)
 
 	/* Initialization */
 	ksm_stat.ksm_rmap_items = 0;
+	ksm_stat.ksm_zero_pages = 0;
+
 	/* summing all processes'ksm statistic items of this cgroup hierarchy */
 	mem_cgroup_scan_tasks(memcg, evaluate_memcg_ksm_stat, &ksm_stat);
+
 	seq_printf(m, "ksm_rmap_items %lu\n", ksm_stat.ksm_rmap_items);
+	seq_printf(m, "ksm_zero_pages %ld\n", ksm_stat.ksm_zero_pages);
 
 	return 0;
 }
