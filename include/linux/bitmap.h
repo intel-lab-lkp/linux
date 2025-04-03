453a929dd9729d6d9dcb57213395ab7acedec410
Ian Rogers <irogers@google.com>
2025-04-03 09:56:59 -0700

    bitmap: Silence a clang -Wshorten-64-to-32 warning
    The clang warning -Wshorten-64-to-32 can be useful to catch
    inadvertent truncation. In some instances this truncation can lead to
    changing the sign of a result, for example, truncation to return an
    int to fit a sort routine. Silence the warning by making the implicit
    truncation explicit.
    
    Signed-off-by: Ian Rogers <irogers@google.com>

--- include/linux/bitmap.h
+++ include/linux/bitmap.h
@@ -442,7 +442,7 @@ static __always_inline
 unsigned int bitmap_weight(const unsigned long *src, unsigned int nbits)
 {
 	if (small_const_nbits(nbits))
-		return hweight_long(*src & BITMAP_LAST_WORD_MASK(nbits));
+		return (int)hweight_long(*src & BITMAP_LAST_WORD_MASK(nbits));
 	return __bitmap_weight(src, nbits);
 }
 
