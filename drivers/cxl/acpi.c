e81ffaa41642f9a56bc1389565c48d42457e8bda
Li Zhijian <lizhijian@fujitsu.com>
2025-04-07 16:02:17 +0800

    cxl/acpi: Verify CHBS length for CXL2.0
    Per CXL Spec r3.1 Table 9-21, both CXL1.1 and CXL2.0 have defined their
    own length, verify it to avoid an invalid CHBS.
    
    Signed-off-by: Li Zhijian <lizhijian@fujitsu.com>

--- drivers/cxl/acpi.c
+++ drivers/cxl/acpi.c
@@ -11,8 +11,6 @@
 #include "cxlpci.h"
 #include "cxl.h"
 
-#define CXL_RCRB_SIZE	SZ_8K
-
 struct cxl_cxims_data {
 	int nr_maps;
 	u64 xormaps[] __counted_by(nr_maps);
@@ -478,8 +476,10 @@ static int cxl_get_chbs_iter(union acpi_subtable_headers *header, void *arg,
 
 	chbs = (struct acpi_cedt_chbs *) header;
 
-	if (chbs->cxl_version == ACPI_CEDT_CHBS_VERSION_CXL11 &&
-	    chbs->length != CXL_RCRB_SIZE)
+	if ((chbs->cxl_version == ACPI_CEDT_CHBS_VERSION_CXL11 &&
+	    chbs->length != ACPI_CEDT_CHBS_LENGTH_CXL11) ||
+	   (chbs->cxl_version == ACPI_CEDT_CHBS_VERSION_CXL20 &&
+	    chbs->length != ACPI_CEDT_CHBS_LENGTH_CXL20))
 		return 0;
 
 	if (!chbs->base)
