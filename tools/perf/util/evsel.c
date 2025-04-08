0013340d261e5596a6ae75cce5593c3a5f80eeee
Ben Gainey <ben.gainey@arm.com>
2025-04-08 18:15:29 +0100

    tools/perf: Modify event parser to support hf-period term
    parse-events is modified, adding the "hf-period" term which can be used
    to specify the high-frequency sampling period.
    
    Signed-off-by: Ben Gainey <ben.gainey@arm.com>
    Signed-off-by: Mark Barnett <mark.barnett@arm.com>

--- tools/perf/util/evsel.c
+++ tools/perf/util/evsel.c
@@ -180,6 +180,7 @@ static int store_event(struct perf_event_attr *attr, pid_t pid, struct perf_cpu 
 	WRITE_ASS(branch_sample_type, "llu");
 	WRITE_ASS(sample_regs_user,   "llu");
 	WRITE_ASS(sample_stack_user,  PRIu32);
+	WRITE_ASS(hf_sample_period,  PRIu32);
 
 	fclose(file);
 	return 0;
