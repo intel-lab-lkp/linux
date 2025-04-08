6d817640158c9b6657442c14c53b652902487589
Niklas Neronin <niklas.neronin@linux.intel.com>
2025-04-08 14:57:45 +0300

    usb: xhci: improve Interrupt Management register macros
    The Interrupt Management register (IMAN), contains three fields:
     - Bit 0:	Interrupt Pending (IP)
     - Bit 1:	Interrupt Enable (IE)
     - Bits 31:2:	RsvdP (Reserved and Preserved)
    
    Currently, there are multiple macros for both the IP and IE fields.
    Consolidates them into single mask macros for better clarity and
    maintainability.
    
    Comment "THIS IS BUGGY - FIXME - IP IS WRITE 1 TO CLEAR" refers to the
    fact that both macros 'ER_IRQ_ENABLE' and 'ER_IRQ_DISABLE' clear the IP bit
    by writing '0' before modifying the IE bit. However, the IP bit is actually
    cleared by writing '1'. To prevent any regression, this behavior has not
    been altered. Instead, when the IE bit is modified, the IP macro is used
    explicitly to highlight this "quirk".
    
    Signed-off-by: Niklas Neronin <niklas.neronin@linux.intel.com>

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -330,7 +330,9 @@ static int xhci_enable_interrupter(struct xhci_interrupter *ir)
 		return -EINVAL;
 
 	iman = readl(&ir->ir_set->irq_pending);
-	writel(ER_IRQ_ENABLE(iman), &ir->ir_set->irq_pending);
+	iman &= ~IMAN_IP;
+	iman |= IMAN_IE;
+	writel(iman, &ir->ir_set->irq_pending);
 
 	return 0;
 }
@@ -343,7 +345,9 @@ static int xhci_disable_interrupter(struct xhci_interrupter *ir)
 		return -EINVAL;
 
 	iman = readl(&ir->ir_set->irq_pending);
-	writel(ER_IRQ_DISABLE(iman), &ir->ir_set->irq_pending);
+	iman &= ~IMAN_IP;
+	iman &= ~IMAN_IE;
+	writel(iman, &ir->ir_set->irq_pending);
 
 	return 0;
 }
