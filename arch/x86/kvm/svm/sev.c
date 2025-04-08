46d54a0ff566126ba17f82f8cae9505655799fc1
Ketan Chaturvedi <Ketan.Chaturvedi@amd.com>
2025-04-08 15:02:13 +0530

    KVM: SVM: Enable Secure TSC for SNP guests
    Add support for Secure TSC, allowing userspace to configure the Secure TSC
    feature for SNP guests. Use the SNP specification's desired TSC frequency
    parameter during the SNP_LAUNCH_START command to set the mean TSC
    frequency in KHz for Secure TSC enabled guests.
    
    As the frequency needs to be set in the SNP_LAUNCH_START command, userspace
    should set the frequency using the KVM_CAP_SET_TSC_KHZ VM ioctl instead of
    the VCPU ioctl. The desired_tsc_khz defaults to kvm->arch.default_tsc_khz.
    
    Signed-off-by: Ketan Chaturvedi <Ketan.Chaturvedi@amd.com>
    Co-developed-by: Nikunj A Dadhania <nikunj@amd.com>
    Tested-by: Vaishali Thakkar <vaishali.thakkar@suse.com>
    Reviewed-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Nikunj A Dadhania <nikunj@amd.com>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -2205,6 +2205,14 @@ static int snp_launch_start(struct kvm *kvm, struct kvm_sev_cmd *argp)
 
 	start.gctx_paddr = __psp_pa(sev->snp_context);
 	start.policy = params.policy;
+
+	if (snp_secure_tsc_enabled(kvm)) {
+		if (!kvm->arch.default_tsc_khz)
+			return -EINVAL;
+
+		start.desired_tsc_khz = kvm->arch.default_tsc_khz;
+	}
+
 	memcpy(start.gosvw, params.gosvw, sizeof(params.gosvw));
 	rc = __sev_issue_cmd(argp->sev_fd, SEV_CMD_SNP_LAUNCH_START, &start, &argp->error);
 	if (rc) {
@@ -2445,7 +2453,9 @@ static int snp_launch_update_vmsa(struct kvm *kvm, struct kvm_sev_cmd *argp)
 			return ret;
 		}
 
-		svm->vcpu.arch.guest_state_protected = true;
+		vcpu->arch.guest_state_protected = true;
+		vcpu->arch.guest_tsc_protected = snp_secure_tsc_enabled(kvm);
+
 		/*
 		 * SEV-ES (and thus SNP) guest mandates LBR Virtualization to
 		 * be _always_ ON. Enable it only after setting
@@ -3059,6 +3069,9 @@ out:
 	sev_supported_vmsa_features = 0;
 	if (sev_es_debug_swap_enabled)
 		sev_supported_vmsa_features |= SVM_SEV_FEAT_DEBUG_SWAP;
+
+	if (sev_snp_enabled && cpu_feature_enabled(X86_FEATURE_SNP_SECURE_TSC))
+		sev_supported_vmsa_features |= SVM_SEV_FEAT_SECURE_TSC;
 }
 
 void sev_hardware_unsetup(void)
