42280e7c2bd005f82361493d7ac56a7b7770a0e0
Tamir Duberstein <tamird@gmail.com>
2025-04-08 14:52:22 -0400

    rust: remove spurious compiler_builtins dependents
    The dependency on `compiler_builtins.o` was first added in commit
    2f7ab1267dc9 ("Kbuild: add Rust support") to `alloc` which matches the
    standard library[0] but was copied to other targets in:
    - commit ecaa6ddff2fd ("rust: add `build_error` crate")
    - commit d072acda4862 ("rust: use custom FFI integer types")
    - commit 4e1746656839 ("rust: uapi: Add UAPI crate")
    - commit d7659acca7a3 ("rust: add pin-init crate build infrastructure")
    
    The alloc crate was removed in commit 392e34b6bc22 ("kbuild: rust:
    remove the `alloc` crate and `GlobalAlloc`"). As fas as I can tell none
    of the other dependencies are required; it is only required that
    compiler_builtins be linked into the rust-enabled kernel. In the
    standard library, compiler_builtins is a dependency of std[1].
    
    Remove these dependency edges. Add a dependency edge from
    `compiler_builtins` to `core` to `scripts/generate_rust_analyzer.py` to
    match `rust/Makefile`. This has been incorrect since commit 8c4555ccc55c
    ("scripts: add `generate_rust_analyzer.py`")
    
    Link: https://github.com/rust-lang/rust/blob/f820b75feef00654924c9351a2faca8d34818339/library/alloc/Cargo.toml#L19 [0]
    Link: https://github.com/rust-lang/rust/blob/f820b75feef00654924c9351a2faca8d34818339/library/std/Cargo.toml#L21 [1]
    Link: https://rust-for-linux.zulipchat.com/#narrow/channel/288089-General/topic/rust-analyzer.20improvements/near/510200959
    Signed-off-by: Tamir Duberstein <tamird@gmail.com>

--- scripts/generate_rust_analyzer.py
+++ scripts/generate_rust_analyzer.py
@@ -81,7 +81,7 @@ def generate_crates(srctree, objtree, sysroot_src, external_src, cfgs):
     append_crate(
         "compiler_builtins",
         srctree / "rust" / "compiler_builtins.rs",
-        [],
+        ["core"],
     )
 
     append_crate(
@@ -94,7 +94,7 @@ def generate_crates(srctree, objtree, sysroot_src, external_src, cfgs):
     append_crate(
         "build_error",
         srctree / "rust" / "build_error.rs",
-        ["core", "compiler_builtins"],
+        ["core"],
     )
 
     append_crate(
