c0797ff81e336cfcfb1685e7f78a00d30c8b55b4
Karol Kolacinski <karol.kolacinski@intel.com>
2025-04-09 14:25:00 +0200

    ice: use designated initializers for TSPLL consts
    Instead of multiple comments, use designated initializers for TSPLL
    consts.
    
    Adjust ice_tspll_params_e82x fields sizes.
    
    Remove ice_tspll_params_e825 definitions as according to EDS (Electrical
    Design Specification) doc, E825 devices support only 156.25 MHz TSPLL
    frequency for both TCXO and TIME_REF clock source.
    
    Reviewed-by: Michal Kubiak <michal.kubiak@intel.com>
    Reviewed-by: Milena Olech <milena.olech@intel.com>
    Signed-off-by: Karol Kolacinski <karol.kolacinski@intel.com>

--- drivers/net/ethernet/intel/ice/ice_tspll.h
+++ drivers/net/ethernet/intel/ice/ice_tspll.h
@@ -4,38 +4,23 @@
 /**
  * struct ice_tspll_params_e82x
  * @refclk_pre_div: Reference clock pre-divisor
+ * @post_pll_div: Post PLL divisor
  * @feedback_div: Feedback divisor
  * @frac_n_div: Fractional divisor
- * @post_pll_div: Post PLL divisor
  *
  * Clock Generation Unit parameters used to program the PLL based on the
  * selected TIME_REF/TCXO frequency.
  */
 struct ice_tspll_params_e82x {
-	u32 refclk_pre_div;
-	u32 feedback_div;
+	u8 refclk_pre_div;
+	u8 post_pll_div;
+	u8 feedback_div;
 	u32 frac_n_div;
-	u32 post_pll_div;
 };
 
-/**
- * struct ice_tspll_params_e825c
- * @ck_refclkfreq: ck_refclkfreq selection
- * @ndivratio: ndiv ratio that goes directly to the PLL
- * @fbdiv_intgr: TSPLL integer feedback divisor
- * @fbdiv_frac: TSPLL fractional feedback divisor
- * @ref1588_ck_div: clock divisor for tspll ref
- *
- * Clock Generation Unit parameters used to program the PLL based on the
- * selected TIME_REF/TCXO frequency.
- */
-struct ice_tspll_params_e825c {
-	u32 ck_refclkfreq;
-	u32 ndivratio;
-	u32 fbdiv_intgr;
-	u32 fbdiv_frac;
-	u32 ref1588_ck_div;
-};
+#define ICE_TSPLL_CK_REFCLKFREQ_E825		0x1F
+#define ICE_TSPLL_NDIVRATIO_E825		5
+#define ICE_TSPLL_FBDIV_INTGR_E825		256
 
 int ice_tspll_cfg_pps_out_e825c(struct ice_hw *hw, bool enable);
 int ice_tspll_init(struct ice_hw *hw);
