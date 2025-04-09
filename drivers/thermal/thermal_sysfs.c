54d91d8cf46d94dd0a260ace0550260285f4190d
Hsin-Te Yuan <yuanhsinte@chromium.org>
2025-04-09 09:13:11 +0000

    thermal: sysfs: Return ENODATA instead of EAGAIN for reads
    When userspace nonblocking reads temperature via sysfs, EAGAIN error
    returned by thermal driver will confuse user from the usual meaning of
    EAGAIN, the read would block. Change to throw ENODATA instead of EAGAIN
    to userspace. Also, ENODATA more accurately reflects that data is not
    currently available.
    
    Signed-off-by: Hsin-Te Yuan <yuanhsinte@chromium.org>

--- drivers/thermal/thermal_sysfs.c
+++ drivers/thermal/thermal_sysfs.c
@@ -40,8 +40,11 @@ temp_show(struct device *dev, struct device_attribute *attr, char *buf)
 
 	ret = thermal_zone_get_temp(tz, &temperature);
 
-	if (ret)
+	if (ret) {
+		if (ret == -EAGAIN)
+			return -ENODATA;
 		return ret;
+	}
 
 	return sprintf(buf, "%d\n", temperature);
 }
