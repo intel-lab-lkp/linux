37f12ac05414fb6bffdef39d989f055256bee173
Luis Claudio R. Goncalves <lgoncalv@redhat.com>
2025-04-10 09:10:12 -0300

    sched: do not call __put_task_struct() on rt if pi_blocked_on is set
    With PREEMPT_RT enabled, some of the calls to put_task_struct() coming
    from rt_mutex_adjust_prio_chain() could happen in preemptible context and
    with a mutex enqueued. That could lead to this sequence:
    
    	rt_mutex_adjust_prio_chain()
    	  put_task_struct()
    	    __put_task_struct()
    	      sched_ext_free()
    	        spin_lock_irqsave()
    	          rtlock_lock() --->  TRIGGERS
    	                              lockdep_assert(!current->pi_blocked_on);
    
    Fix that by unconditionally resorting to the deferred call to
    __put_task_struct().
    
    v2: (Rostedt) remove the #ifdef from put_task_struct() and create
        tsk_is_pi_blocked_on() in sched.h to make the change cleaner.
    
    v3: (Sebastian and PeterZ) always call the RCU deferred __put_task_struct().
    
    Suggested-by: Crystal Wood <crwood@redhat.com>
    Signed-off-by: Luis Claudio R. Goncalves <lgoncalv@redhat.com>

--- include/linux/sched/task.h
+++ include/linux/sched/task.h
@@ -135,22 +135,12 @@ static inline void put_task_struct(struct task_struct *t)
 		return;
 
 	/*
-	 * In !RT, it is always safe to call __put_task_struct().
-	 * Under RT, we can only call it in preemptible context.
-	 */
-	if (!IS_ENABLED(CONFIG_PREEMPT_RT) || preemptible()) {
-		static DEFINE_WAIT_OVERRIDE_MAP(put_task_map, LD_WAIT_SLEEP);
-
-		lock_map_acquire_try(&put_task_map);
-		__put_task_struct(t);
-		lock_map_release(&put_task_map);
-		return;
-	}
-
-	/*
-	 * under PREEMPT_RT, we can't call put_task_struct
+	 * In !RT, it is always safe to call __put_task_struct(),
+	 * but under PREEMPT_RT, we can't call put_task_struct
 	 * in atomic context because it will indirectly
-	 * acquire sleeping locks.
+	 * acquire sleeping locks. The same is true if the
+	 * current process has a mutex enqueued (blocked on
+	 * a PI chain).
 	 *
 	 * call_rcu() will schedule delayed_put_task_struct_rcu()
 	 * to be called in process context.
