4c40ba84d098739d82188295e5aaa6f99818e75d
Fan Ni <fan.ni@samsung.com>
2025-04-10 11:00:31 -0700

    mm: Introduce free_folio_and_swap_cache() to replace free_page_and_swap_cache()
    The function free_page_and_swap_cache() takes a struct page pointer as
    input parameter, but it will immediately convert it to folio and all
    operations following within use folio instead of page.  It makes more
    sense to pass in folio directly.
    
    Introduce free_folio_and_swap_cache(), which takes folio as input to
    replace free_page_and_swap_cache().  And apply it to all occurrences
    where free_page_and_swap_cache() was used.
    
    Signed-off-by: Fan Ni <fan.ni@samsung.com>

--- include/linux/swap.h
+++ include/linux/swap.h
@@ -450,7 +450,7 @@ static inline unsigned long total_swapcache_pages(void)
 }
 
 void free_swap_cache(struct folio *folio);
-void free_page_and_swap_cache(struct page *);
+void free_folio_and_swap_cache(struct folio *folio);
 void free_pages_and_swap_cache(struct encoded_page **, int);
 /* linux/mm/swapfile.c */
 extern atomic_long_t nr_swap_pages;
@@ -522,8 +522,12 @@ static inline void put_swap_device(struct swap_info_struct *si)
 	do { (val)->freeswap = (val)->totalswap = 0; } while (0)
 /* only sparc can not include linux/pagemap.h in this file
  * so leave put_page and release_pages undeclared... */
-#define free_page_and_swap_cache(page) \
-	put_page(page)
+#define free_folio_and_swap_cache(folio)	\
+	do {					\
+		if (!folio_test_slab(folio))	\
+			folio_put(folio);	\
+	} while (0)
+
 #define free_pages_and_swap_cache(pages, nr) \
 	release_pages((pages), (nr));
 
