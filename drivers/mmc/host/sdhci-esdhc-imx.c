460f4f3e0ecc1960063aa89f3ee4decc8a75835d
Arnd Bergmann <arnd@arndb.de>
2025-04-10 16:09:07 +0200

    mmc: esdhc-imx: convert to modern PM_OPS
    Two newly added functions are unused in configurations without
    power management support:
    
    drivers/mmc/host/sdhci-esdhc-imx.c:1586:13: error: unused function 'sdhc_esdhc_tuning_save' [-Werror,-Wunused-function]
     1586 | static void sdhc_esdhc_tuning_save(struct sdhci_host *host)
          |             ^~~~~~~~~~~~~~~~~~~~~~
    drivers/mmc/host/sdhci-esdhc-imx.c:1608:13: error: unused function 'sdhc_esdhc_tuning_restore' [-Werror,-Wunused-function]
     1608 | static void sdhc_esdhc_tuning_restore(struct sdhci_host *host)
          |             ^~~~~~~~~~~~~~~~~~~~~~~~~
    
    Remove the #ifdef checks and instead use the better macros that
    silently drop the unused functions when PM is disabled.
    
    Fixes: 3d1eea493894 ("mmc: sdhci-esdhc-imx: Save tuning value when card stays powered in suspend")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/mmc/host/sdhci-esdhc-imx.c
+++ drivers/mmc/host/sdhci-esdhc-imx.c
@@ -1877,7 +1877,6 @@ static void sdhci_esdhc_imx_remove(struct platform_device *pdev)
 	sdhci_pltfm_free(pdev);
 }
 
-#ifdef CONFIG_PM_SLEEP
 static int sdhci_esdhc_suspend(struct device *dev)
 {
 	struct sdhci_host *host = dev_get_drvdata(dev);
@@ -1937,9 +1936,7 @@ static int sdhci_esdhc_resume(struct device *dev)
 
 	return ret;
 }
-#endif
 
-#ifdef CONFIG_PM
 static int sdhci_esdhc_runtime_suspend(struct device *dev)
 {
 	struct sdhci_host *host = dev_get_drvdata(dev);
@@ -2019,11 +2016,10 @@ remove_pm_qos_request:
 		cpu_latency_qos_remove_request(&imx_data->pm_qos_req);
 	return err;
 }
-#endif
 
 static const struct dev_pm_ops sdhci_esdhc_pmops = {
-	SET_SYSTEM_SLEEP_PM_OPS(sdhci_esdhc_suspend, sdhci_esdhc_resume)
-	SET_RUNTIME_PM_OPS(sdhci_esdhc_runtime_suspend,
+	SYSTEM_SLEEP_PM_OPS(sdhci_esdhc_suspend, sdhci_esdhc_resume)
+	RUNTIME_PM_OPS(sdhci_esdhc_runtime_suspend,
 				sdhci_esdhc_runtime_resume, NULL)
 };
 
