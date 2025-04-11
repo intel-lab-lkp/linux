d0b643c1f612aabe8353fd94233fc02101afab85
Jan Kara <jack@suse.cz>
2025-04-11 19:38:47 +0200

    writeback: Fix false warning in inode_to_wb()
    inode_to_wb() is used also for filesystems that don't support cgroup
    writeback. For these filesystems inode->i_wb is stable during the
    lifetime of the inode (it points to bdi->wb) and there's no need to hold
    locks protecting the inode->i_wb dereference. Improve the warning in
    inode_to_wb() to not trigger for these filesystems.
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Reviewed-by: Andreas Gruenbacher <agruenba@redhat.com>

--- include/linux/backing-dev.h
+++ include/linux/backing-dev.h
@@ -245,10 +245,11 @@ wb_get_create_current(struct backing_dev_info *bdi, gfp_t gfp)
  * holding either @inode->i_lock, the i_pages lock, or the
  * associated wb's list_lock.
  */
-static inline struct bdi_writeback *inode_to_wb(const struct inode *inode)
+static inline struct bdi_writeback *inode_to_wb(struct inode *inode)
 {
 #ifdef CONFIG_LOCKDEP
 	WARN_ON_ONCE(debug_locks &&
+		     (inode->i_sb->s_iflags & SB_I_CGROUPWB) &&
 		     (!lockdep_is_held(&inode->i_lock) &&
 		      !lockdep_is_held(&inode->i_mapping->i_pages.xa_lock) &&
 		      !lockdep_is_held(&inode->i_wb->list_lock)));
