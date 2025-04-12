e99c7d1a04083a4c05ed2fa9d39e98b7642f3983
Chenyuan Yang <chenyuan0y@gmail.com>
2025-04-12 12:53:56 -0500

    drm/nouveau: Fix null pointer dereference
    The return value of drm_mode_duplicate() is assigned to mode,
    which will lead to a NULL pointer dereference on
    failure of drm_mode_duplicate(). Add a check to avoid npd.
    
    Signed-off-by: Chenyuan Yang <chenyuan0y@gmail.com>
    Fixes: 6ee738610f41 ("drm/nouveau: Add DRM driver for NVIDIA GPUs")

--- drivers/gpu/drm/nouveau/dispnv04/i2c/ch7006_drv.c
+++ drivers/gpu/drm/nouveau/dispnv04/i2c/ch7006_drv.c
@@ -229,6 +229,7 @@ static int ch7006_encoder_get_modes(struct drm_encoder *encoder,
 {
 	struct ch7006_priv *priv = to_ch7006_priv(encoder);
 	const struct ch7006_mode *mode;
+	struct drm_display_mode *drm_mode;
 	int n = 0;
 
 	for (mode = ch7006_modes; mode->mode.clock; mode++) {
@@ -236,9 +237,11 @@ static int ch7006_encoder_get_modes(struct drm_encoder *encoder,
 		    ~mode->valid_norms & 1<<priv->norm)
 			continue;
 
-		drm_mode_probed_add(connector,
-				drm_mode_duplicate(encoder->dev, &mode->mode));
+		drm_mode = drm_mode_duplicate(encoder->dev, &mode->mode);
+		if (!drm_mode)
+			continue;
 
+		drm_mode_probed_add(connector, drm_mode);
 		n++;
 	}
 
