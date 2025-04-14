6c71fc02aeba59f36da34ac2cf360ce3badb5f51
Brahmajit Das <brahmajit.xyz@gmail.com>
2025-04-14 21:46:42 +0530

    ACPI: Fix building with GCC 15
    Since the Linux kernel initializes many non-C-string char arrays with
    literals. While it would be possible to convert initializers from:
       { "BOOP", ... }
    to something like:
       { { 'B', 'O', 'O', 'P' }, ... }
    that is annoying.
    Making -Wunterminated-string-initialization stay silent about char
    arrays marked with nonstring would be much better.
    
    Without the __attribute__((nonstring)) we would get the following build
    error:
    
    ...
    drivers/acpi/acpica/acpredef.h:903:11: error: initializer-string for array of ‘char’ truncates NUL terminator but destination lacks ‘nonstring’ attribute (5 chars into 4 available) [-Werror=unterminated-string-initialization]
      903 |         {{"_S3D", METHOD_0ARGS,
          |           ^~~~~~
    drivers/acpi/acpica/acpredef.h:906:11: error: initializer-string for array of ‘char’ truncates NUL terminator but destination lacks ‘nonstring’ attribute (5 chars into 4 available) [-Werror=unterminated-string-initialization]
      906 |         {{"_S4D", METHOD_0ARGS,
          |           ^~~~~~
    
    and,
    
    ...
    drivers/acpi/acpica/nsrepair2.c:115:10: error: initializer-string for array of ‘char’ truncates NUL terminator but destination lacks ‘nonstring’ attribute (5 chars into 4 available) [-Werror=unterminated-string-initialization]
      115 |         {"_ALR", acpi_ns_repair_ALR},
          |          ^~~~~~
    drivers/acpi/acpica/nsrepair2.c:116:10: error: initializer-string for array of ‘char’ truncates NUL terminator but destination lacks ‘nonstring’ attribute (5 chars into 4 available) [-Werror=unterminated-string-initialization]
      116 |         {"_CID", acpi_ns_repair_CID},
    ...
    
    Upstream GCC has added this commit
    622968990beee7499e951590258363545b4a3b57[0][1] which silences warning
    about truncating NUL char when initializing nonstring arrays.
    
    [0]: https://gcc.gnu.org/cgit/gcc/commit/?id=622968990beee7499e951590258363545b4a3b57
    [1]: https://gcc.gnu.org/cgit/gcc/commit/?id=afb46540d3921e96c4cd7ba8fa2c8b0901759455
    
    Thanks to Jakub Jelinek <jakub@gcc.gnu.org> for the gcc patch.
    
    Signed-off-by: Brahmajit Das <listout@listout.xyz>

--- include/acpi/actbl.h
+++ include/acpi/actbl.h
@@ -66,7 +66,8 @@
  ******************************************************************************/
 
 struct acpi_table_header {
-	char signature[ACPI_NAMESEG_SIZE];	/* ASCII table signature */
+	char signature[ACPI_NAMESEG_SIZE]
+		__attribute__((nonstring)); /* ASCII table signature */
 	u32 length;		/* Length of table in bytes, including this header */
 	u8 revision;		/* ACPI Specification minor version number */
 	u8 checksum;		/* To make sum of entire table == 0 */
