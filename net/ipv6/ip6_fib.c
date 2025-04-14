2133635e6dad2babc0c1b0ea5c64352526dcac3d
Kuniyuki Iwashima <kuniyu@amazon.com>
2025-04-14 11:15:01 -0700

    ipv6: Protect nh->f6i_list with spinlock and flag.
    We will get rid of RTNL from RTM_NEWROUTE and SIOCADDRT.
    
    Then, we may be going to add a route tied to a dying nexthop.
    
    The nexthop itself is not freed during the RCU grace period, but
    if we link a route after __remove_nexthop_fib() is called for the
    nexthop, the route will be leaked.
    
    To avoid the race between IPv6 route addition under RCU vs nexthop
    deletion under RTNL, let's add a dead flag and protect it and
    nh->f6i_list with a spinlock.
    
    __remove_nexthop_fib() acquires the nexthop's spinlock and sets false
    to nh->dead, then calls ip6_del_rt() for the linked route one by one
    without the spinlock because fib6_purge_rt() acquires it later.
    
    While adding an IPv6 route, fib6_add() acquires the nexthop lock and
    checks the dead flag just before inserting the route.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@amazon.com>

--- net/ipv6/ip6_fib.c
+++ net/ipv6/ip6_fib.c
@@ -1048,8 +1048,14 @@ static void fib6_purge_rt(struct fib6_info *rt, struct fib6_node *fn,
 	rt6_flush_exceptions(rt);
 	fib6_drop_pcpu_from(rt, table);
 
-	if (rt->nh && !list_empty(&rt->nh_list))
-		list_del_init(&rt->nh_list);
+	if (rt->nh) {
+		spin_lock(&rt->nh->lock);
+
+		if (!list_empty(&rt->nh_list))
+			list_del_init(&rt->nh_list);
+
+		spin_unlock(&rt->nh->lock);
+	}
 
 	if (refcount_read(&rt->fib6_ref) != 1) {
 		/* This route is used as dummy address holder in some split
@@ -1498,7 +1504,23 @@ int fib6_add(struct fib6_node *root, struct fib6_info *rt,
 	}
 #endif
 
-	err = fib6_add_rt2node(fn, rt, info, extack, &purge_list);
+	if (rt->nh) {
+		spin_lock(&rt->nh->lock);
+
+		if (rt->nh->dead) {
+			NL_SET_ERR_MSG(extack, "Nexthop has been deleted");
+			err = -EINVAL;
+		} else {
+			err = fib6_add_rt2node(fn, rt, info, extack, &purge_list);
+			if (!err)
+				list_add(&rt->nh_list, &rt->nh->f6i_list);
+		}
+
+		spin_unlock(&rt->nh->lock);
+	} else {
+		err = fib6_add_rt2node(fn, rt, info, extack, &purge_list);
+	}
+
 	if (!err) {
 		struct fib6_info *iter, *next;
 
@@ -1508,8 +1530,6 @@ int fib6_add(struct fib6_node *root, struct fib6_info *rt,
 			fib6_info_release(iter);
 		}
 
-		if (rt->nh)
-			list_add(&rt->nh_list, &rt->nh->f6i_list);
 		__fib6_update_sernum_upto_root(rt, fib6_new_sernum(info->nl_net));
 
 		if (rt->fib6_flags & RTF_EXPIRES)
