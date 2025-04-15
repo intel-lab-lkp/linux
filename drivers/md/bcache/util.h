4c3d99bafb0f417e9e9805d6a4829e756b91c481
Robert Pang <robertpang@google.com>
2025-04-15 10:39:08 -0700

    bcache: process fewer btree nodes in incremental GC cycles
    Current incremental GC processes a minimum of 100 btree nodes per cycle,
    followed by a 100ms sleep. For NVMe cache devices, where the average node
    processing time is ~1ms, this leads to front-side I/O latency potentially
    reaching tens or hundreds of milliseconds during GC execution.
    
    This commit resolves this latency issue by reducing the minimum node processing
    count per cycle to 10 and the inter-cycle sleep duration to 10ms. It also
    integrates a check of existing GC statistics to re-scale the number of nodes
    processed per sleep interval when needed, ensuring GC finishes well before the
    next GC is due.
    
    Signed-off-by: Robert Pang <robertpang@google.com>

--- drivers/md/bcache/util.h
+++ drivers/md/bcache/util.h
@@ -305,6 +305,13 @@ static inline unsigned int local_clock_us(void)
 #define NSEC_PER_ms			NSEC_PER_MSEC
 #define NSEC_PER_sec			NSEC_PER_SEC
 
+/*
+ * time_stat_average - return the average duration/frequency stat of a time
+ * stats in the desired unit.
+ */
+#define time_stat_average(stats, stat, units)				\
+	div_u64((stats)->average_ ## stat >> 8, NSEC_PER_ ## units)
+
 #define __print_time_stat(stats, name, stat, units)			\
 	sysfs_print(name ## _ ## stat ## _ ## units,			\
 		    div_u64((stats)->stat >> 8, NSEC_PER_ ## units))
