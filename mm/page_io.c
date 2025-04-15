aa9b78ab32037d443186ba3dfdf58edbf4054353
Muchun Song <songmuchun@bytedance.com>
2025-04-15 10:45:19 +0800

    mm: page_io: prevent memory cgroup release in page_io module
    In the near future, a folio will no longer pin its corresponding
    memory cgroup. To ensure safety, it will only be appropriate to
    hold the rcu read lock or acquire a reference to the memory cgroup
    returned by folio_memcg(), thereby preventing it from being released.
    
    In the current patch, the rcu read lock is employed to safeguard
    against the release of the memory cgroup in swap_writepage() and
    bio_associate_blkg_from_page().
    
    This serves as a preparatory measure for the reparenting of the
    LRU pages.
    
    Signed-off-by: Muchun Song <songmuchun@bytedance.com>

--- mm/page_io.c
+++ mm/page_io.c
@@ -279,10 +279,14 @@ int swap_writeout(struct folio *folio, struct writeback_control *wbc)
 		folio_unlock(folio);
 		return 0;
 	}
+
+	rcu_read_lock();
 	if (!mem_cgroup_zswap_writeback_enabled(folio_memcg(folio))) {
+		rcu_read_unlock();
 		folio_mark_dirty(folio);
 		return AOP_WRITEPAGE_ACTIVATE;
 	}
+	rcu_read_unlock();
 
 	__swap_writepage(folio, wbc);
 	return 0;
@@ -307,11 +311,11 @@ static void bio_associate_blkg_from_page(struct bio *bio, struct folio *folio)
 	struct cgroup_subsys_state *css;
 	struct mem_cgroup *memcg;
 
-	memcg = folio_memcg(folio);
-	if (!memcg)
+	if (!folio_memcg_charged(folio))
 		return;
 
 	rcu_read_lock();
+	memcg = folio_memcg(folio);
 	css = cgroup_e_css(memcg->css.cgroup, &io_cgrp_subsys);
 	bio_associate_blkg_from_css(bio, css);
 	rcu_read_unlock();
