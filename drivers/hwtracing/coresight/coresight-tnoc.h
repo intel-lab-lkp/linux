ca8b5e59c2066b7d38ea043d0cff06f4c8900ea7
Yuanfang Zhang <quic_yuanfang@quicinc.com>
2025-04-15 17:25:40 +0800

    coresight: add coresight Trace Network On Chip driver
    Add a driver to support Coresight device Trace Network On Chip (TNOC),
    which is an integration hierarchy integrating functionalities of TPDA
    and funnels. It aggregates the trace and transports to coresight trace
    bus.
    
    Compared to current configuration, it has the following advantages:
    1. Reduce wires between subsystems.
    2. Continue cleaning the infrastructure.
    3. Reduce Data overhead by transporting raw data from source to target.
    
      +------------------------+                +-------------------------+
      | Video Subsystem        |                |Video Subsystem          |
      |       +-------------+  |                |       +------------+    |
      |       | Video TPDM  |  |                |       | Video TPDM |    |
      |       +-------------+  |                |       +------------+    |
      |            |           |                |              |          |
      |            v           |                |              v          |
      |   +---------------+    |                |        +-----------+    |
      |   | Video funnel  |    |                |        |Video TNOC |    |
      |   +---------------+    |                |        +-----------+    |
      +------------|-----------+                +------------|------------+
                   |                                         |
                   v-----+                                   |
    +--------------------|---------+                         |
    |  Multimedia        v         |                         |
    |  Subsystem   +--------+      |                         |
    |              |  TPDA  |      |                         v
    |              +----|---+      |              +---------------------+
    |                   |          |              |   Aggregator  TNOC  |
    |                   |          |              +----------|----------+
    |                   +--        |                         |
    |                     |        |                         |
    |                     |        |                         |
    |              +------v-----+  |                         |
    |              |  Funnel    |  |                         |
    |              +------------+  |                         |
    +----------------|-------------+                         |
                     |                                       |
                     v                                       v
          +--------------------+                    +------------------+
          |   Coresight Sink   |                    |  Coresight Sink  |
          +--------------------+                    +------------------+
    
           Current Configuration                            TNOC
    
    Signed-off-by: Yuanfang Zhang <quic_yuanfang@quicinc.com>

---
+++ drivers/hwtracing/coresight/coresight-tnoc.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2025 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#define TRACE_NOC_CTRL      0x008
+#define TRACE_NOC_XLD       0x010
+#define TRACE_NOC_FREQVAL   0x018
+#define TRACE_NOC_SYNCR     0x020
+
+/* Enable generation of output ATB traffic.*/
+#define TRACE_NOC_CTRL_PORTEN   BIT(0)
+/* Sets the type of issued ATB FLAG packets.*/
+#define TRACE_NOC_CTRL_FLAGTYPE BIT(7)
+/* Sets the type of issued ATB FREQ packet*/
+#define TRACE_NOC_CTRL_FREQTYPE BIT(8)
+
+#define TRACE_NOC_SYNC_INTERVAL	0xFFFF
+
+/*
+ * struct trace_noc_drvdata - specifics associated to a trace noc component
+ * @base:	memory mapped base address for this component.
+ * @dev:	device node for trace_noc_drvdata.
+ * @csdev:	component vitals needed by the framework.
+ * @spinlock:	only one at a time pls.
+ * @atid:	id for the trace packet.
+ */
+struct trace_noc_drvdata {
+	void __iomem		*base;
+	struct device		*dev;
+	struct coresight_device	*csdev;
+	spinlock_t		spinlock; /* lock for the drvdata. */
+	u32			atid;
+};
