bf9cb7f4f382d3338fcc818b92bb11479b45f47b
Stephen Bates <sbates@raithlin.com>
2025-04-14 18:24:01 -0600

    p2pdma: Whitelist the QEMU host bridge for x86_64
    It is useful to be able to develop and test p2pdma applications in
    virtualized environments. Whitelist the QEMU PCI host bridge emulated
    by the default QEMU system for x86_64.
    
    Signed-off-by: Stephen Bates <sbates@raithlin.com>

--- drivers/pci/p2pdma.c
+++ drivers/pci/p2pdma.c
@@ -451,6 +451,8 @@ static const struct pci_p2pdma_whitelist_entry {
 	{PCI_VENDOR_ID_INTEL,	0x2033, 0},
 	{PCI_VENDOR_ID_INTEL,	0x2020, 0},
 	{PCI_VENDOR_ID_INTEL,	0x09a2, 0},
+	/* QEMU Host Bridge */
+	{PCI_VENDOR_ID_INTEL,	0x29c0, 0},
 	{}
 };
 
