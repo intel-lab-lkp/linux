7fc58d74d510d093f06e1158d008935999da792d
Salah Triki <salah.triki@gmail.com>
2025-04-15 18:41:19 +0100

    smb: server: smb2pdu: check return value of xa_store()
    xa_store() may fail so check its return value and return error code if
    error occurred.
    
    Signed-off-by: Salah Triki <salah.triki@gmail.com>

--- fs/smb/server/smb2pdu.c
+++ fs/smb/server/smb2pdu.c
@@ -1445,7 +1445,7 @@ static int ntlm_authenticate(struct ksmbd_work *work,
 {
 	struct ksmbd_conn *conn = work->conn;
 	struct ksmbd_session *sess = work->sess;
-	struct channel *chann = NULL;
+	struct channel *chann = NULL, *old;
 	struct ksmbd_user *user;
 	u64 prev_id;
 	int sz, rc;
@@ -1557,7 +1557,10 @@ binding_session:
 				return -ENOMEM;
 
 			chann->conn = conn;
-			xa_store(&sess->ksmbd_chann_list, (long)conn, chann, KSMBD_DEFAULT_GFP);
+			old = xa_store(&sess->ksmbd_chann_list, (long)conn, chann,
+					KSMBD_DEFAULT_GFP);
+			if (xa_is_err(old))
+				goto free_channel;
 		}
 	}
 
@@ -1574,6 +1577,10 @@ binding_session:
 		return -ENOENT;
 	}
 	return 0;
+
+free_channel:
+	kfree(chann);
+	return xa_err(old);
 }
 
 #ifdef CONFIG_SMB_SERVER_KERBEROS5
