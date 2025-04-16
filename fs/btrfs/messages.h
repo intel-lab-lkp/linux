3f02e255b1e93a4c9f1d456bcfa977c96364ec44
David Sterba <dsterba@suse.com>
2025-04-16 11:08:10 +0200

    btrfs: add debug build only WARN
    Add conditional WARN() wrapper that's enabled only in debug build. It
    should be used for unexpected condtions that should be noisy.  Use it
    instead of ASSERT(0). As it will not lead to BUG() make sure that
    continuing is still possible, e.g. the error is handled anyway.
    
    Signed-off-by: David Sterba <dsterba@suse.com>

--- fs/btrfs/messages.h
+++ fs/btrfs/messages.h
@@ -204,6 +204,13 @@ static inline void btrfs_assertfail_verbose(const char *str_expr,
 #define VASSERT(expr, fmt, ...)		(void)(expr)
 #endif
 
+#ifdef CONFIG_BTRFS_DEBUG
+/* Verbose warning only under debug build. */
+#define DEBUG_WARN(args...)	WARN(1, KERN_ERR args)
+#else
+#define DEBUG_WARN(...)		do {} while(0)
+#endif
+
 __printf(5, 6)
 __cold
 void __btrfs_handle_fs_error(struct btrfs_fs_info *fs_info, const char *function,
