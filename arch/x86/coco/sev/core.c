bb23be4937af45eb31758b5b7fd4dc34c97a597a
Neeraj Upadhyay <Neeraj.Upadhyay@amd.com>
2025-04-17 14:46:53 +0530

    x86/apic: Initialize Secure AVIC APIC backing page
    With Secure AVIC, the APIC backing page is owned and managed by guest.
    Allocate and initialize APIC backing page for all guest CPUs.
    
    The NPT entry for a vCPU's APIC backing page must always be present
    when the vCPU is running in order for Secure AVIC to function. A
    VMEXIT_BUSY is returned on VMRUN and the vCPU cannot be resumed if
    the NPT entry for the APIC backing page is not present. Notify GPA of
    the vCPU's APIC backing page to the hypervisor by using the
    SVM_VMGEXIT_SECURE_AVIC GHCB protocol event. Before executing VMRUN,
    the hypervisor makes use of this information to make sure the APIC backing
    page is mapped in NPT.
    
    Co-developed-by: Kishon Vijay Abraham I <kvijayab@amd.com>
    Signed-off-by: Kishon Vijay Abraham I <kvijayab@amd.com>
    Signed-off-by: Neeraj Upadhyay <Neeraj.Upadhyay@amd.com>

--- arch/x86/coco/sev/core.c
+++ arch/x86/coco/sev/core.c
@@ -1419,6 +1419,28 @@ static enum es_result vc_handle_msr(struct ghcb *ghcb, struct es_em_ctxt *ctxt)
 	return ret;
 }
 
+enum es_result savic_register_gpa(u64 gpa)
+{
+	struct ghcb_state state;
+	struct es_em_ctxt ctxt;
+	enum es_result res;
+	struct ghcb *ghcb;
+
+	guard(irqsave)();
+
+	ghcb = __sev_get_ghcb(&state);
+	vc_ghcb_invalidate(ghcb);
+
+	ghcb_set_rax(ghcb, SVM_VMGEXIT_SAVIC_SELF_GPA);
+	ghcb_set_rbx(ghcb, gpa);
+	res = sev_es_ghcb_hv_call(ghcb, &ctxt, SVM_VMGEXIT_SAVIC,
+				  SVM_VMGEXIT_SAVIC_REGISTER_GPA, 0);
+
+	__sev_put_ghcb(&state);
+
+	return res;
+}
+
 static void snp_register_per_cpu_ghcb(void)
 {
 	struct sev_es_runtime_data *data;
--- arch/x86/kernel/apic/apic.c
+++ arch/x86/kernel/apic/apic.c
@@ -1502,6 +1502,8 @@ static void setup_local_APIC(void)
 		return;
 	}
 
+	if (apic->setup)
+		apic->setup();
 	/*
 	 * If this comes from kexec/kcrash the APIC might be enabled in
 	 * SPIV. Soft disable it before doing further initialization.
