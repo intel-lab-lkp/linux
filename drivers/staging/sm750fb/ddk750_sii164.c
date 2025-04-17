133390bdb801dc3fbbc4d81bbf29fb93dca1926f
Ruben Wauters <rubenru09@aol.com>
2025-04-17 20:02:50 +0100

    staging: sm750fb: rename gDviCtrlChipName
    Renames gDviCtrlChipName to dvi_controller_chip_name
    This fixes checkpatch.pl's camel case check.
    
    Signed-off-by: Ruben Wauters <rubenru09@aol.com>

--- drivers/staging/sm750fb/ddk750_sii164.c
+++ drivers/staging/sm750fb/ddk750_sii164.c
@@ -14,7 +14,7 @@
 
 #ifdef SII164_FULL_FUNCTIONS
 /* Name of the DVI Controller chip */
-static char *gDviCtrlChipName = "Silicon Image SiI 164";
+static char *dvi_controller_chip_name = "Silicon Image SiI 164";
 #endif
 
 /*
@@ -254,7 +254,7 @@ void sii164_reset_chip(void)
  */
 char *sii164_get_chip_string(void)
 {
-	return gDviCtrlChipName;
+	return dvi_controller_chip_name;
 }
 
 /*
