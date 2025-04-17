59efcfac62214f42145a3df8d3ae4f26521e005f
Ruben Wauters <rubenru09@aol.com>
2025-04-17 20:02:52 +0100

    staging: sm750fb: rename sii164_init_chip params
    Renames sii164_init_chip's params in the header
    file to be identical to the params in the c file.
    
    This also fixes (one of) checkpatch.pl's camel case checks
    
    Signed-off-by: Ruben Wauters <rubenru09@aol.com>

--- drivers/staging/sm750fb/ddk750_sii164.h
+++ drivers/staging/sm750fb/ddk750_sii164.h
@@ -16,16 +16,16 @@ enum sii164_hot_plug_mode {
 };
 
 /* Silicon Image SiI164 chip prototype */
-long sii164_init_chip(unsigned char edgeSelect,
-		      unsigned char busSelect,
-		      unsigned char dualEdgeClkSelect,
-		      unsigned char hsyncEnable,
-		      unsigned char vsyncEnable,
-		      unsigned char deskewEnable,
-		      unsigned char deskewSetting,
-		      unsigned char continuousSyncEnable,
-		      unsigned char pllFilterEnable,
-		      unsigned char pllFilterValue);
+long sii164_init_chip(unsigned char edge_select,
+		      unsigned char bus_select,
+		      unsigned char dual_edge_clk_select,
+		      unsigned char hsync_enable,
+		      unsigned char vsync_enable,
+		      unsigned char deskew_enable,
+		      unsigned char deskew_setting,
+		      unsigned char continuous_sync_enable,
+		      unsigned char pll_filter_enable,
+		      unsigned char pll_filter_value);
 
 unsigned short sii164_get_vendor_id(void);
 unsigned short sii164_get_device_id(void);
