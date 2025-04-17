88e61ecf52e1c92683dfb0778103ecf6e7b5252b
Neeraj Upadhyay <Neeraj.Upadhyay@amd.com>
2025-04-17 14:46:51 +0530

    KVM: x86: Move find_highest_vector() to a common header
    In preparation for using find_highest_vector() in Secure AVIC
    guest APIC driver, move (and rename) find_highest_vector() to
    apic.h.
    
    Signed-off-by: Neeraj Upadhyay <Neeraj.Upadhyay@amd.com>

--- arch/x86/include/asm/apic.h
+++ arch/x86/include/asm/apic.h
@@ -500,6 +500,29 @@ static inline bool is_vector_pending(unsigned int vector)
 	return lapic_vector_set_in_irr(vector) || pi_pending_this_cpu(vector);
 }
 
+#define MAX_APIC_VECTOR			256
+#define APIC_VECTORS_PER_REG		32
+
+static inline int apic_find_highest_vector(void *bitmap)
+{
+	unsigned int regno;
+	unsigned int vec;
+	u32 *reg;
+
+	/*
+	 * The registers in the bitmap are 32-bit wide and 16-byte
+	 * aligned. State of a vector is stored in a single bit.
+	 */
+	for (regno = MAX_APIC_VECTOR / APIC_VECTORS_PER_REG - 1; regno >= 0; regno--) {
+		vec = regno * APIC_VECTORS_PER_REG;
+		reg = bitmap + regno * 16;
+		if (*reg)
+			return __fls(*reg) + vec;
+	}
+
+	return -1;
+}
+
 /*
  * Warm reset vector position:
  */
