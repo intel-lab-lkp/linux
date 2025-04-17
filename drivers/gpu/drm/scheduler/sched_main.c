9a837d6abeea63beb0ec857cf60112be2093b2c6
Sunil Khatri <sunil.khatri@amd.com>
2025-04-17 14:55:14 +0530

    drm/schedular: fix the warning in drm_sched_job_done
    Fix the below warning.
    scheduler/sched_main.c:397: warning: Function parameter or struct member 'result' not described in 'drm_sched_job_done'
    
    Signed-off-by: Sunil Khatri <sunil.khatri@amd.com>

--- drivers/gpu/drm/scheduler/sched_main.c
+++ drivers/gpu/drm/scheduler/sched_main.c
@@ -390,6 +390,7 @@ static void drm_sched_run_free_queue(struct drm_gpu_scheduler *sched)
 /**
  * drm_sched_job_done - complete a job
  * @s_job: pointer to the job which is done
+ * @result: passed dma fence error
  *
  * Finish the job's fence and wake up the worker thread.
  */
