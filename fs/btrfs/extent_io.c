fd1bc91487d8053e95ea57b331d6a8a933685434
Boris Burkov <boris@bur.io>
2025-04-18 11:25:02 -0700

    btrfs: fix broken drop_caches on extent_buffer folios
    The (correct) commit
    e41c81d0d30e ("mm/truncate: Replace page_mapped() call in invalidate_inode_page()")
    replaced the page_mapped(page) check with a refcount check. However,
    this refcount check does not work as expected with drop_caches for
    btrfs's metadata pages.
    
    Btrfs has a per-sb metadata inode with cached pages, and when not in
    active use by btrfs, they have a refcount of 3. One from the initial
    call to alloc_pages, one (nr_pages == 1) from filemap_add_folio, and one
    from folio_attach_private. We would expect such pages to get dropped by
    drop_caches. However, drop_caches calls into mapping_evict_folio via
    mapping_try_invalidate which gets a reference on the folio with
    find_lock_entries(). As a result, these pages have a refcount of 4, and
    fail this check.
    
    For what it's worth, such pages do get reclaimed under memory pressure,
    so I would say that while this behavior is surprising, it is not really
    dangerously broken.
    
    When I asked the mm folks about the expected refcount in this case, I
    was told that the correct thing to do is to donate the refcount from the
    original allocation to the page cache after inserting it.
    https://lore.kernel.org/linux-mm/ZrwhTXKzgDnCK76Z@casper.infradead.org/
    
    Therefore, attempt to fix this by adding a put_folio() to the critical
    spot in alloc_extent_buffer where we are sure that we have really
    allocated and attached new pages. We must also adjust
    folio_detach_private to properly handle being the last reference to the
    folio and not do a UAF after folio_detach_private().
    
    Finally, extent_buffers allocated by clone_extent_buffer() and
    alloc_dummy_extent_buffer() are unmapped, so this transfer of ownership
    from allocation to insertion in the mapping does not apply to them.
    However, we can still folio_put() them safely once they are finished
    being allocated and have called folio_attach_private().
    
    Reviewed-by: Filipe Manana <fdmanana@suse.com>
    Signed-off-by: Boris Burkov <boris@bur.io>

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -2709,6 +2709,7 @@ static bool folio_range_has_eb(struct folio *folio)
 static void detach_extent_buffer_folio(const struct extent_buffer *eb, struct folio *folio)
 {
 	struct btrfs_fs_info *fs_info = eb->fs_info;
+	struct address_space *mapping = folio->mapping;
 	const bool mapped = !test_bit(EXTENT_BUFFER_UNMAPPED, &eb->bflags);
 
 	/*
@@ -2716,11 +2717,11 @@ static void detach_extent_buffer_folio(const struct extent_buffer *eb, struct fo
 	 * be done under the i_private_lock.
 	 */
 	if (mapped)
-		spin_lock(&folio->mapping->i_private_lock);
+		spin_lock(&mapping->i_private_lock);
 
 	if (!folio_test_private(folio)) {
 		if (mapped)
-			spin_unlock(&folio->mapping->i_private_lock);
+			spin_unlock(&mapping->i_private_lock);
 		return;
 	}
 
@@ -2740,7 +2741,7 @@ static void detach_extent_buffer_folio(const struct extent_buffer *eb, struct fo
 			folio_detach_private(folio);
 		}
 		if (mapped)
-			spin_unlock(&folio->mapping->i_private_lock);
+			spin_unlock(&mapping->i_private_lock);
 		return;
 	}
 
@@ -2763,7 +2764,7 @@ static void detach_extent_buffer_folio(const struct extent_buffer *eb, struct fo
 	if (!folio_range_has_eb(folio))
 		btrfs_detach_subpage(fs_info, folio, BTRFS_SUBPAGE_METADATA);
 
-	spin_unlock(&folio->mapping->i_private_lock);
+	spin_unlock(&mapping->i_private_lock);
 }
 
 /* Release all folios attached to the extent buffer */
@@ -2778,9 +2779,6 @@ static void btrfs_release_extent_buffer_folios(const struct extent_buffer *eb)
 			continue;
 
 		detach_extent_buffer_folio(eb, folio);
-
-		/* One for when we allocated the folio. */
-		folio_put(folio);
 	}
 }
 
@@ -2846,6 +2844,7 @@ struct extent_buffer *btrfs_clone_extent_buffer(const struct extent_buffer *src)
 			return NULL;
 		}
 		WARN_ON(folio_test_dirty(folio));
+		folio_put(folio);
 	}
 	copy_extent_buffer_full(new, src);
 	set_extent_buffer_uptodate(new);
@@ -2872,6 +2871,8 @@ struct extent_buffer *alloc_dummy_extent_buffer(struct btrfs_fs_info *fs_info,
 		if (ret < 0)
 			goto out_detach;
 	}
+	for (int i = 0; i < num_extent_folios(eb); i++)
+		folio_put(eb->folios[i]);
 
 	set_extent_buffer_uptodate(eb);
 	btrfs_set_header_nritems(eb, 0);
@@ -3322,8 +3323,15 @@ again:
 	 * btree_release_folio will correctly detect that a page belongs to a
 	 * live buffer and won't free them prematurely.
 	 */
-	for (int i = 0; i < num_extent_folios(eb); i++)
+	for (int i = 0; i < num_extent_folios(eb); i++) {
 		folio_unlock(eb->folios[i]);
+		/*
+		 * A folio that has been added to an address_space mapping
+		 * should not continue holding the refcount from its original
+		 * allocation indefinitely.
+		 */
+		folio_put(eb->folios[i]);
+	}
 	return eb;
 
 out:
