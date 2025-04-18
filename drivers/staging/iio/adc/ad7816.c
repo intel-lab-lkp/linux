26eb08dcc0777806271b909a20a7ac93be0693d4
Gabriel Shahrouzi <gshahrouzi@gmail.com>
2025-04-18 16:47:35 -0400

    staging: iio: adc: ad7816: Allow channel 7 for all devices
    According to the datasheet on page 9 under the channel selection table,
    all devices (AD7816/7/8) are able to use the channel marked as 7. This
    channel is used for diagnostic purposes by routing the internal 1.23V
    bandgap source through the MUX to the input of the ADC.
    
    Modify the channel validation logic to permit channel 7 for all
    supported device types.
    
    Fixes: 7924425db04a ("staging: iio: adc: new driver for AD7816 devices")
    Cc: stable@vger.kernel.org
    Signed-off-by: Gabriel Shahrouzi <gshahrouzi@gmail.com>

--- drivers/staging/iio/adc/ad7816.c
+++ drivers/staging/iio/adc/ad7816.c
@@ -190,11 +190,11 @@ static ssize_t ad7816_store_channel(struct device *dev,
 		dev_err(&chip->spi_dev->dev, "Invalid channel id %lu for %s.\n",
 			data, indio_dev->name);
 		return -EINVAL;
-	} else if (strcmp(indio_dev->name, "ad7818") == 0 && data > 1) {
+	} else if (strcmp(indio_dev->name, "ad7818") == 0 && data > 1 && data != AD7816_CS_MASK) {
 		dev_err(&chip->spi_dev->dev,
 			"Invalid channel id %lu for ad7818.\n", data);
 		return -EINVAL;
-	} else if (strcmp(indio_dev->name, "ad7816") == 0 && data > 0) {
+	} else if (strcmp(indio_dev->name, "ad7816") == 0 && data > 0 && data != AD7816_CS_MASK) {
 		dev_err(&chip->spi_dev->dev,
 			"Invalid channel id %lu for ad7816.\n", data);
 		return -EINVAL;
