e442c2167378b85ee6e1f6745ae426b75d11b3fe
Denis Arefev <arefev@swemel.ru>
2025-04-18 11:31:27 +0300

    drm/amdgpu: check a user-provided number of BOs in list
    The user can set any value to the variable ‘bo_number’, via the ioctl
    command DRM_IOCTL_AMDGPU_BO_LIST. This will affect the arithmetic
    expression ‘in->bo_number * in->bo_info_size’, which is prone to
    overflow. Add a valid value check.
    
    Found by Linux Verification Center (linuxtesting.org) with SVACE.
    
    Fixes: 964d0fbf6301 ("drm/amdgpu: Allow to create BO lists in CS ioctl v3")
    Cc: stable@vger.kernel.org
    Signed-off-by: Denis Arefev <arefev@swemel.ru>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_bo_list.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_bo_list.c
@@ -189,6 +189,9 @@ int amdgpu_bo_create_list_entry_array(struct drm_amdgpu_bo_list_in *in,
 	struct drm_amdgpu_bo_list_entry *info;
 	int r;
 
+	if (!in->bo_number || in->bo_number > USHRT_MAX)
+		return -EINVAL;
+
 	info = kvmalloc_array(in->bo_number, info_size, GFP_KERNEL);
 	if (!info)
 		return -ENOMEM;
