7ac2e8041065910ff41dde4b4d85a18e0bd37e5e
Pavan Kumar Linga <pavan.kumar.linga@intel.com>
2025-04-18 13:49:13 -0700

    idpf: introduce idpf_q_vec_rsrc struct and move vector resources to it
    To group all the vector and queue resources, introduce idpf_q_vec_rsrc
    structure. This helps to reuse the same config path functions by
    other features. For example, PTP implementation can use the
    existing config infrastructure to configure secondary mailbox
    by passing its queue and vector info. It also helps to avoid any
    duplication of code.
    
    Existing queue and vector resources are grouped as default
    resources. This patch moves vector info to the newly introduced
    structure. Following patch moves the queue resources.
    
    While at it, declare the loop iterator for 'num_q_vectors'
    in loop and use the correct type.
    
    Reviewed-by: Anton Nadezhdin <anton.nadezhdin@intel.com>
    Signed-off-by: Pavan Kumar Linga <pavan.kumar.linga@intel.com>

--- drivers/net/ethernet/intel/idpf/idpf.h
+++ drivers/net/ethernet/intel/idpf/idpf.h
@@ -8,6 +8,7 @@
 struct idpf_adapter;
 struct idpf_vport;
 struct idpf_vport_max_q;
+struct idpf_q_vec_rsrc;
 
 #include <net/pkt_sched.h>
 #include <linux/aer.h>
@@ -195,7 +196,8 @@ struct idpf_vport_max_q {
  */
 struct idpf_reg_ops {
 	void (*ctlq_reg_init)(struct idpf_ctlq_create_info *cq);
-	int (*intr_reg_init)(struct idpf_vport *vport);
+	int (*intr_reg_init)(struct idpf_vport *vport,
+			     struct idpf_q_vec_rsrc *rsrc);
 	void (*mb_intr_reg_init)(struct idpf_adapter *adapter);
 	void (*reset_reg_init)(struct idpf_adapter *adapter);
 	void (*trigger_reset)(struct idpf_adapter *adapter,
@@ -266,7 +268,20 @@ struct idpf_tx_tstamp_stats {
 };
 
 /**
+ * struct idpf_q_vec_rsrc - handle for queue and vector resources
+ * @q_vectors: array of queue vectors
+ * @q_vector_idxs: starting index of queue vectors
+ * @num_q_vectors: number of IRQ vectors allocated
+ */
+struct idpf_q_vec_rsrc {
+	struct idpf_q_vector	*q_vectors;
+	u16			*q_vector_idxs;
+	u16			num_q_vectors;
+};
+
+/**
  * struct idpf_vport - Handle for netdevices and queue resources
+ * @dflt_qv_rsrc: contains default queue and vector resources
  * @num_txq: Number of allocated TX queues
  * @num_complq: Number of allocated completion queues
  * @txq_desc_count: TX queue descriptor count
@@ -298,9 +313,6 @@ struct idpf_tx_tstamp_stats {
  * @idx: Software index in adapter vports struct
  * @default_vport: Use this vport if one isn't specified
  * @base_rxd: True if the driver should use base descriptors instead of flex
- * @num_q_vectors: Number of IRQ vectors allocated
- * @q_vectors: Array of queue vectors
- * @q_vector_idxs: Starting index of queue vectors
  * @max_mtu: device given max possible MTU
  * @default_mac_addr: device will give a default MAC to use
  * @rx_itr_profile: RX profiles for Dynamic Interrupt Moderation
@@ -314,6 +326,7 @@ struct idpf_tx_tstamp_stats {
  * @tstamp_stats: Tx timestamping statistics
  */
 struct idpf_vport {
+	struct idpf_q_vec_rsrc dflt_qv_rsrc;
 	u16 num_txq;
 	u16 num_complq;
 	u32 txq_desc_count;
@@ -344,9 +357,6 @@ struct idpf_vport {
 	bool default_vport;
 	bool base_rxd;
 
-	u16 num_q_vectors;
-	struct idpf_q_vector *q_vectors;
-	u16 *q_vector_idxs;
 	u16 max_mtu;
 	u8 default_mac_addr[ETH_ALEN];
 	u16 rx_itr_profile[IDPF_DIM_PROFILE_SLOTS];
