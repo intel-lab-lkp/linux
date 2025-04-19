4c13ff8359f382c3909c0b4f7300fb54db17aaf0
Gabriel Shahrouzi <gshahrouzi@gmail.com>
2025-04-19 09:56:38 -0400

    staging: iio: adc: ad7816: Simplify channel validation using chip_info
    Refactor the channel validation logic within ad7816_store_channel() to
    leverage the max_channels field previously introduced in the
    ad7816_chip_info structure.
    
    Signed-off-by: Gabriel Shahrouzi <gshahrouzi@gmail.com>

--- drivers/staging/iio/adc/ad7816.c
+++ drivers/staging/iio/adc/ad7816.c
@@ -204,17 +204,9 @@ static ssize_t ad7816_store_channel(struct device *dev,
 	if (ret)
 		return ret;
 
-	if (data > AD7816_CS_MAX && data != AD7816_CS_MASK) {
+	if (data > chip->chip_info->max_channels && data != AD7816_CS_MASK) {
 		dev_err(&chip->spi_dev->dev, "Invalid channel id %lu for %s.\n",
-			data, indio_dev->name);
-		return -EINVAL;
-	} else if (strcmp(indio_dev->name, "ad7818") == 0 && data > 1 && data != AD7816_CS_MASK) {
-		dev_err(&chip->spi_dev->dev,
-			"Invalid channel id %lu for ad7818.\n", data);
-		return -EINVAL;
-	} else if (strcmp(indio_dev->name, "ad7816") == 0 && data > 0 && data != AD7816_CS_MASK) {
-		dev_err(&chip->spi_dev->dev,
-			"Invalid channel id %lu for ad7816.\n", data);
+			data, chip->chip_info->name);
 		return -EINVAL;
 	}
 
