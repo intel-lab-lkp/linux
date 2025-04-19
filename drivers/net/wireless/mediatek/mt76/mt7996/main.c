352860c9602730b2c130d3855d0c5faac7c5dcde
sunliming <sunliming@kylinos.cn>
2025-04-19 10:54:17 +0800

    wifi: mt76: mt7996: fix variable dereferenced before check warning
    Fix below smatch warnings:
    drivers/net/wireless/mediatek/mt76/mt7996/main.c:73 mt7996_stop_phy() warn:
    variable dereferenced before check 'phy' (see line 71)
    drivers/net/wireless/mediatek/mt76/mt7996/main.c:360 mt7996_set_monitor() warn:
    variable dereferenced before check 'phy' (see line 358)
    
    Reported-by: kernel test robot <lkp@intel.com>
    Reported-by: Dan Carpenter <error27@gmail.com>
    Closes: https://lore.kernel.org/r/202504101051.1ya4Z4va-lkp@intel.com/
    Signed-off-by: sunliming <sunliming@kylinos.cn>

--- drivers/net/wireless/mediatek/mt76/mt7996/main.c
+++ drivers/net/wireless/mediatek/mt76/mt7996/main.c
@@ -68,10 +68,12 @@ static int mt7996_start(struct ieee80211_hw *hw)
 
 static void mt7996_stop_phy(struct mt7996_phy *phy)
 {
-	struct mt7996_dev *dev = phy->dev;
+	struct mt7996_dev *dev;
 
 	if (!phy || !test_bit(MT76_STATE_RUNNING, &phy->mt76->state))
 		return;
+
+	dev = phy->dev;
 
 	cancel_delayed_work_sync(&phy->mt76->mac_work);
 
@@ -414,10 +416,12 @@ static void mt7996_phy_set_rxfilter(struct mt7996_phy *phy)
 
 static void mt7996_set_monitor(struct mt7996_phy *phy, bool enabled)
 {
-	struct mt7996_dev *dev = phy->dev;
+	struct mt7996_dev *dev;
 
 	if (!phy)
 		return;
+
+	dev = phy->dev;
 
 	if (enabled == !(phy->rxfilter & MT_WF_RFCR_DROP_OTHER_UC))
 		return;
