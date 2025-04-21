7b9ba93788dd604a20901de3807a0d0cf33ae3fd
Uday Shankar <ushankar@purestorage.com>
2025-04-21 17:46:41 -0600

    ublk: mark ublk_queue as const for ublk_register_io_buf
    In the future, we may allow multiple tasks to operate on one ublk_queue
    concurrently. Any writes to ublk_queue in ublk_register_io_buf, which
    has no synchronization, would then become data races. Try to ensure that
    such writes do not exist by marking ublk_queue pointers as const in
    ublk_register_io_buf.
    
    Suggested-by: Ming Lei <ming.lei@redhat.com>
    Signed-off-by: Uday Shankar <ushankar@purestorage.com>
    Reviewed-by: Caleb Sander Mateos <csander@purestorage.com>
    Reviewed-by: Ming Lei <ming.lei@redhat.com>

--- drivers/block/ublk_drv.c
+++ drivers/block/ublk_drv.c
@@ -203,7 +203,7 @@ struct ublk_params_header {
 static void ublk_stop_dev_unlocked(struct ublk_device *ub);
 static void ublk_abort_queue(struct ublk_device *ub, struct ublk_queue *ubq);
 static inline struct request *__ublk_check_and_get_req(struct ublk_device *ub,
-		struct ublk_queue *ubq, int tag, size_t offset);
+		const struct ublk_queue *ubq, int tag, size_t offset);
 static inline unsigned int ublk_req_build_flags(struct request *req);
 static inline struct ublksrv_io_desc *ublk_get_iod(struct ublk_queue *ubq,
 						   int tag);
@@ -1918,7 +1918,7 @@ static void ublk_io_release(void *priv)
 }
 
 static int ublk_register_io_buf(struct io_uring_cmd *cmd,
-				struct ublk_queue *ubq, unsigned int tag,
+				const struct ublk_queue *ubq, unsigned int tag,
 				unsigned int index, unsigned int issue_flags)
 {
 	struct ublk_device *ub = cmd->file->private_data;
@@ -2115,7 +2115,7 @@ static int __ublk_ch_uring_cmd(struct io_uring_cmd *cmd,
 }
 
 static inline struct request *__ublk_check_and_get_req(struct ublk_device *ub,
-		struct ublk_queue *ubq, int tag, size_t offset)
+		const struct ublk_queue *ubq, int tag, size_t offset)
 {
 	struct request *req;
 
