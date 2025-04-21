bf313229e5e39d648b3d36f459a439c0ab998de1
Boon Khai Ng <boon.khai.ng@altera.com>
2025-04-22 00:29:29 +0800

    net: stmmac: Refactor VLAN implementation
    Refactor VLAN implementation by moving common code for DWMAC4 and
    DWXGMAC IPs into a separate VLAN module. VLAN implementation for
    DWMAC4 and DWXGMAC differs only for CSR base address, the descriptor
    for the VLAN ID and VLAN VALID bit field.
    
    The descriptor format for VLAN is not moved to the common code due
    to hardware-specific differences between DWMAC4 and DWXGMAC.
    
    For the DWMAC4 IP, the Receive Normal Descriptor 0 (RDES0) is
    formatted as follows:
        31                                                0
          ------------------------ -----------------------
    RDES0| Inner VLAN TAG [31:16] | Outer VLAN TAG [15:0] |
          ------------------------ -----------------------
    
    For the DWXGMAC IP, the RDES0 format varies based on the
    Tunneled Frame bit (TNP):
    
    a) For Non-Tunneled Frame (TNP=0)
    
        31                                                0
          ------------------------ -----------------------
    RDES0| Inner VLAN TAG [31:16] | Outer VLAN TAG [15:0] |
          ------------------------ -----------------------
    
    b) For Tunneled Frame (TNP=1)
    
         31                   8 7                3 2      0
          --------------------- ------------------ -------
    RDES0| VNID/VSID           | Reserved         | OL2L3 |
          --------------------- ------------------ ------
    
    The logic for handling tunneled frames is not yet implemented
    in the dwxgmac2_wrback_get_rx_vlan_tci() function. Therefore,
    it is prudent to maintain separate functions within their
    respective descriptor driver files
    (dwxgmac2_descs.c and dwmac4_descs.c).
    
    Signed-off-by: Boon Khai Ng <boon.khai.ng@altera.com>
    Reviewed-by: Matthew Gerlach <matthew.gerlach@altera.com>

--- drivers/net/ethernet/stmicro/stmmac/hwif.c
+++ drivers/net/ethernet/stmicro/stmmac/hwif.c
@@ -9,6 +9,7 @@
 #include "stmmac_fpe.h"
 #include "stmmac_ptp.h"
 #include "stmmac_est.h"
+#include "stmmac_vlan.h"
 #include "dwmac4_descs.h"
 #include "dwxgmac2.h"
 
@@ -120,6 +121,7 @@ static const struct stmmac_hwif_entry {
 	const void *tc;
 	const void *mmc;
 	const void *est;
+	const void *vlan;
 	int (*setup)(struct stmmac_priv *priv);
 	int (*quirks)(struct stmmac_priv *priv);
 } stmmac_hw[] = {
@@ -197,6 +199,7 @@ static const struct stmmac_hwif_entry {
 		.desc = &dwmac4_desc_ops,
 		.dma = &dwmac4_dma_ops,
 		.mac = &dwmac410_ops,
+		.vlan = &dwmac_vlan_ops,
 		.hwtimestamp = &stmmac_ptp,
 		.ptp = &stmmac_ptp_clock_ops,
 		.mode = &dwmac4_ring_mode_ops,
@@ -219,6 +222,7 @@ static const struct stmmac_hwif_entry {
 		.desc = &dwmac4_desc_ops,
 		.dma = &dwmac410_dma_ops,
 		.mac = &dwmac410_ops,
+		.vlan = &dwmac_vlan_ops,
 		.hwtimestamp = &stmmac_ptp,
 		.ptp = &stmmac_ptp_clock_ops,
 		.mode = &dwmac4_ring_mode_ops,
@@ -241,6 +245,7 @@ static const struct stmmac_hwif_entry {
 		.desc = &dwmac4_desc_ops,
 		.dma = &dwmac410_dma_ops,
 		.mac = &dwmac510_ops,
+		.vlan = &dwmac_vlan_ops,
 		.hwtimestamp = &stmmac_ptp,
 		.ptp = &stmmac_ptp_clock_ops,
 		.mode = &dwmac4_ring_mode_ops,
@@ -264,6 +269,7 @@ static const struct stmmac_hwif_entry {
 		.desc = &dwxgmac210_desc_ops,
 		.dma = &dwxgmac210_dma_ops,
 		.mac = &dwxgmac210_ops,
+		.vlan = &dwmac_vlan_ops,
 		.hwtimestamp = &stmmac_ptp,
 		.ptp = &stmmac_ptp_clock_ops,
 		.mode = NULL,
@@ -292,6 +298,7 @@ static const struct stmmac_hwif_entry {
 		.mode = NULL,
 		.tc = &dwmac510_tc_ops,
 		.mmc = &dwxgmac_mmc_ops,
+		.vlan = &dwxlgmac2_vlan_ops,
 		.est = &dwmac510_est_ops,
 		.setup = dwxlgmac2_setup,
 		.quirks = stmmac_dwxlgmac_quirks,
@@ -368,6 +375,7 @@ int stmmac_hwif_init(struct stmmac_priv *priv)
 		mac->tc = mac->tc ? : entry->tc;
 		mac->mmc = mac->mmc ? : entry->mmc;
 		mac->est = mac->est ? : entry->est;
+		mac->vlan = mac->vlan ? : entry->vlan;
 
 		priv->hw = mac;
 		priv->fpe_cfg.reg = entry->regs.fpe_reg;
