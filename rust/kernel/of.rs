5b365fcb6aa4485b4f6a31b340e2b6a989853165
Ayush Singh <ayush@beagleboard.org>
2025-04-22 18:02:10 +0530

    rust: kernel: of: Add overlay id abstraction
    Allow applying devicetree overlays from Rust.
    
    The overlay is removed on Drop.
    
    Signed-off-by: Ayush Singh <ayush@beagleboard.org>

--- rust/kernel/of.rs
+++ rust/kernel/of.rs
@@ -80,3 +80,32 @@ impl DeviceNode {
         self.0.get()
     }
 }
+
+/// Devicetree overlay present in livetree.
+///
+/// The overlay is removed on Drop
+pub struct OvcsId(kernel::ffi::c_int);
+
+impl OvcsId {
+    /// Create and apply an overlay changeset to the live tree.
+    pub fn of_overlay_fdt_apply(overlay: &[u8], base: &DeviceNode) -> Result<Self> {
+        let mut ovcs_id: kernel::ffi::c_int = 0;
+
+        crate::error::to_result(unsafe {
+            bindings::of_overlay_fdt_apply(
+                overlay.as_ptr().cast(),
+                overlay.len().try_into().unwrap(),
+                &mut ovcs_id,
+                base.as_raw(),
+            )
+        })?;
+
+        Ok(Self(ovcs_id))
+    }
+}
+
+impl Drop for OvcsId {
+    fn drop(&mut self) {
+        unsafe { bindings::of_overlay_remove(&mut self.0) };
+    }
+}
