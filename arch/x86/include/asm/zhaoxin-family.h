56bc18d4c0264d495ebdf8ad6424c7ce4c8e16cc
SilviaZhao <silviazhao-oc@zhaoxin.com>
2025-04-23 14:35:07 +0800

    x86/cpu/zhaoxin: Introduce macros for Zhaoxin family numbers
    Create zhaoxin-family.h to define Zhaoxin family numbers in one header
    file. So we can use the macros instead of open-coded model numbers in
    other files.
    
    Signed-off-by: SilviaZhao <silviazhao-oc@zhaoxin.com>

---
+++ arch/x86/include/asm/zhaoxin-family.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_X86_ZHAOXIN_FAMILY_H
+#define _ASM_X86_ZHAOXIN_FAMILY_H
+
+/*
+ * The defined symbol names have the following form:
+ *             ZHAOXIN_FAM7{OPTFAMILY}_{MICROARCH}
+ * where:
+ * OPTFAMILY   Describes the family of CPUs that this belongs to. Default
+ *             is assumed to be omitted.
+ * MICROARCH   Is the code name for the micro-architecture for this core.
+ */
+
+
+#define ZHAOXIN_FAM7_WUDAOKOU		0x1B
+#define ZHAOXIN_FAM7_LUJIAZUI		0x3B
+#define ZHAOXIN_FAM7_YONGFENG		0x5B
+
+#endif /* _ASM_X86_ZHAOXIN_FAMILY_H */
