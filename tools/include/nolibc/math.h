993caaf28e048df2be3bf11ee35fdbc4e5ed0a43
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2025-04-23 17:01:36 +0200

    tools/nolibc: add abs() and friends
    This is used in various selftests and will be handy when integrating
    those with nolibc.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

---
+++ tools/include/nolibc/math.h
@@ -0,0 +1,31 @@
+/* SPDX-License-Identifier: LGPL-2.1 OR MIT */
+/*
+ * math definitions for NOLIBC
+ * Copyright (C) 2025 Thomas Weißschuh <thomas.weissschuh@linutronix.de>
+ */
+
+#ifndef _NOLIBC_SYS_MATH_H
+#define _NOLIBC_SYS_MATH_H
+
+static __inline__
+double fabs(double x)
+{
+	return x >= 0 ? x : -x;
+}
+
+static __inline__
+float fabsf(float x)
+{
+	return x >= 0 ? x : -x;
+}
+
+static __inline__
+long double fabsl(long double x)
+{
+	return x >= 0 ? x : -x;
+}
+
+/* make sure to include all global symbols */
+#include "nolibc.h"
+
+#endif /* _NOLIBC_SYS_MATH_H */
