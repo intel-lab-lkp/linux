8e58bd8dc0231e705a5f0081b06a116b623ca16a
Ard Biesheuvel <ardb@kernel.org>
2025-04-23 13:09:50 +0200

    x86/linkage: Add SYM_PI_ALIAS() macro helper to emit symbol aliases
    Startup code that may execute from the early 1:1 mapping of memory will
    be confined into its own address space, and only be permitted to access
    ordinary kernel symbols if this is known to be safe.
    
    Introduce a macro helper PI_ALIAS() that emits a __pi_ prefixed alias
    for a symbol, which allows startup code to access it.
    
    Signed-off-by: Ard Biesheuvel <ardb@kernel.org>

--- arch/x86/include/asm/linkage.h
+++ arch/x86/include/asm/linkage.h
@@ -141,5 +141,11 @@
 #define SYM_FUNC_START_WEAK_NOALIGN(name)		\
 	SYM_START(name, SYM_L_WEAK, SYM_A_NONE)
 
+#ifdef __ASSEMBLER__
+#define SYM_PI_ALIAS(sym)	SYM_ALIAS(__pi_ ## sym, sym, SYM_L_GLOBAL)
+#else
+#define SYM_PI_ALIAS(sym)	extern typeof(sym) __PASTE(__pi_, sym) __alias(sym)
+#endif
+
 #endif /* _ASM_X86_LINKAGE_H */
 
