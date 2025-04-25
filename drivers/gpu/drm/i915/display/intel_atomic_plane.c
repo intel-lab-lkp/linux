c1902861dabb68217b3cf3a123f22e9be232710f
Jocelyn Falempe <jfalempe@redhat.com>
2025-04-25 11:37:52 +0200

    drm/i915/display: Flush the front buffer in panic handler
    On Lunar Lake, if the panic occurs when fbcon is active, the panic
    screen is only partially visible on the screen. Adding this
    intel_frontbuffer_flush() call solves the issue.
    It's probably not safe to do that in the panic handler, but that's
    still better than nothing.
    
    Signed-off-by: Jocelyn Falempe <jfalempe@redhat.com>

--- drivers/gpu/drm/i915/display/intel_atomic_plane.c
+++ drivers/gpu/drm/i915/display/intel_atomic_plane.c
@@ -59,6 +59,7 @@
 #include "intel_fb.h"
 #include "intel_fb_pin.h"
 #include "intel_fbdev.h"
+#include "intel_frontbuffer.h"
 #include "skl_scaler.h"
 #include "skl_universal_plane.h"
 #include "skl_watermark.h"
@@ -1272,8 +1273,14 @@ static void intel_panic_flush(struct drm_plane *plane)
 	drm_clflush_virt_range(panic_map.vaddr, fb->height * fb->pitches[0]);
 
 	/* Don't disable tiling if it's the fbdev framebuffer.*/
-	if (to_intel_framebuffer(fb) == intel_fbdev_framebuffer(display->fbdev.fbdev))
+	if (to_intel_framebuffer(fb) == intel_fbdev_framebuffer(display->fbdev.fbdev)) {
+		struct intel_frontbuffer *front = to_intel_frontbuffer(fb);
+		struct drm_gem_object *obj = intel_fb_bo(fb);
+
+		intel_bo_flush_if_display(obj);
+		intel_frontbuffer_flush(front, ORIGIN_DIRTYFB);
 		return;
+	}
 
 	if (fb->modifier && iplane->disable_tiling)
 		iplane->disable_tiling(iplane);
