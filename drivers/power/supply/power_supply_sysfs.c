d14db12e7ed53eb4368fd28df94b8ec03dc050cd
Armin Wolf <W_Armin@gmx.de>
2025-04-26 01:15:15 +0200

    power: supply: core: Add additional health status values
    Some batteries can signal when an internal fuse was blown. In such a
    case POWER_SUPPLY_HEALTH_DEAD is too vague for userspace applications
    to perform meaningful diagnostics.
    
    Additionally some batteries can also signal when some of their
    internal cells are imbalanced. In such a case returning
    POWER_SUPPLY_HEALTH_UNSPEC_FAILURE is again too vague for userspace
    applications to perform meaningful diagnostics.
    
    Add new health status values for both cases.
    
    Signed-off-by: Armin Wolf <W_Armin@gmx.de>

--- drivers/power/supply/power_supply_sysfs.c
+++ drivers/power/supply/power_supply_sysfs.c
@@ -110,6 +110,8 @@ static const char * const POWER_SUPPLY_HEALTH_TEXT[] = {
 	[POWER_SUPPLY_HEALTH_COOL]		    = "Cool",
 	[POWER_SUPPLY_HEALTH_HOT]		    = "Hot",
 	[POWER_SUPPLY_HEALTH_NO_BATTERY]	    = "No battery",
+	[POWER_SUPPLY_HEALTH_FUSE_BLOWN]	    = "Fuse blown",
+	[POWER_SUPPLY_HEALTH_CELL_IMBALANCED]	    = "Cell imbalanced",
 };
 
 static const char * const POWER_SUPPLY_TECHNOLOGY_TEXT[] = {
