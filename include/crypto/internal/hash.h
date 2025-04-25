50633c49ddea1cc45a3d443475c7308d4bfa3a4e
Herbert Xu <herbert@gondor.apana.org.au>
2025-04-25 11:05:29 +0800

    crypto: api - Add crypto_stack_request_init and initialise flags fully
    On Fri, Apr 25, 2025 at 07:23:49AM +0800, kernel test robot wrote:
    > tree:   https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git master
    > head:   393d0c54cae31317deaa9043320c5fd9454deabc
    > commit: 04bfa4c7d5119ca38f8133bfdae7957a60c8b221 [3678/6092] crypto: hash - Add HASH_REQUEST_ON_STACK
    > config: um-randconfig-r063-20250425 (https://download.01.org/0day-ci/archive/20250425/202504250751.mdy28Ibr-lkp@intel.com/config)
    > compiler: gcc-12 (Debian 12.2.0-14) 12.2.0
    > reproduce (this is a W=1 build): (https://download.01.org/0day-ci/archive/20250425/202504250751.mdy28Ibr-lkp@intel.com/reproduce)
    
    ---8<---
    Add a helper to initialise crypto stack requests and use it for
    ahash and acomp.  Make sure that the flags field is initialised
    fully in the helper to silence false-positive warnings from the
    compiler.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202504250751.mdy28Ibr-lkp@intel.com/
    Signed-off-by: Herbert Xu <herbert@gondor.apana.org.au>

--- include/crypto/internal/hash.h
+++ include/crypto/internal/hash.h
@@ -278,8 +278,7 @@ static inline struct ahash_request *ahash_fbreq_on_stack_init(
 	struct crypto_ahash *tfm = crypto_ahash_reqtfm(old);
 	struct ahash_request *req = (void *)buf;
 
-	ahash_request_set_tfm(req, tfm->fb);
-	req->base.flags = CRYPTO_TFM_REQ_ON_STACK;
+	crypto_stack_request_init(&req->base, crypto_ahash_tfm(tfm->fb));
 	ahash_request_set_callback(req, ahash_request_flags(old), NULL, NULL);
 	req->base.flags &= ~CRYPTO_AHASH_REQ_PRIVATE;
 	req->base.flags |= old->base.flags & CRYPTO_AHASH_REQ_PRIVATE;
--- include/linux/crypto.h
+++ include/linux/crypto.h
@@ -509,5 +509,13 @@ static inline void crypto_request_set_tfm(struct crypto_async_request *req,
 	req->flags &= ~CRYPTO_TFM_REQ_ON_STACK;
 }
 
+static inline void crypto_stack_request_init(struct crypto_async_request *req,
+					     struct crypto_tfm *tfm)
+{
+	req->flags = 0;
+	crypto_request_set_tfm(req, tfm);
+	req->flags |= CRYPTO_TFM_REQ_ON_STACK;
+}
+
 #endif	/* _LINUX_CRYPTO_H */
 
