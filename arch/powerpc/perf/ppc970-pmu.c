f19af7fe179c8470335d78fcb642955be4ae4dc7
Likhitha Korrapati <likhitha@linux.ibm.com>
2025-04-26 09:25:31 +0530

    arch/powerpc/perf : Removing unused macros from ppc970-pmu.c
    The following macros were introduced in ppc970-pmu via
    commit 16b067993dee ("powerpc/perf_counter: Add
    support for PPC970 family")
    
    PM_NONE, PM_LSU1U, MMCR0_PMC2SEL_SH, MMCR_PMCSEL_MSK
    MMCR1_TTM0SEL_SH, MMCR1_TTMSEL_MSK, MMCR1_TD_CP_DBG1SEL_SH
    MMCR1_TD_CP_DBG2SEL_SH, MMCR1_TD_CP_DBG3SEL_SH, MMCR1_PMC4SEL_SH
    MMCR1_PMC5SEL_SH, MMCR1_PMC6SEL_SH, MMCR1_PMC7SEL_SH
    MMCR1_PMC8SEL_SH
    
    Removing the above macros as they are defined but never used.
    
    Co-developed-by: Anjali K <anjalik@linux.ibm.com>
    Signed-off-by: Anjali K <anjalik@linux.ibm.com>
    Signed-off-by: Likhitha Korrapati <likhitha@linux.ibm.com>

--- arch/powerpc/perf/ppc970-pmu.c
+++ arch/powerpc/perf/ppc970-pmu.c
@@ -25,7 +25,6 @@
 #define PM_PMCSEL_MSK	0xf
 
 /* Values in PM_UNIT field */
-#define PM_NONE		0
 #define PM_FPU		1
 #define PM_VPU		2
 #define PM_ISU		3
@@ -33,7 +32,6 @@
 #define PM_IDU		5
 #define PM_STS		6
 #define PM_LSU0		7
-#define PM_LSU1U	8
 #define PM_LSU1L	9
 #define PM_LASTUNIT	9
 
@@ -41,20 +39,13 @@
  * Bits in MMCR0 for PPC970
  */
 #define MMCR0_PMC1SEL_SH	8
-#define MMCR0_PMC2SEL_SH	1
-#define MMCR_PMCSEL_MSK		0x1f
 
 /*
  * Bits in MMCR1 for PPC970
  */
-#define MMCR1_TTM0SEL_SH	62
 #define MMCR1_TTM1SEL_SH	59
 #define MMCR1_TTM3SEL_SH	53
-#define MMCR1_TTMSEL_MSK	3
 #define MMCR1_TD_CP_DBG0SEL_SH	50
-#define MMCR1_TD_CP_DBG1SEL_SH	48
-#define MMCR1_TD_CP_DBG2SEL_SH	46
-#define MMCR1_TD_CP_DBG3SEL_SH	44
 #define MMCR1_PMC1_ADDER_SEL_SH	39
 #define MMCR1_PMC2_ADDER_SEL_SH	38
 #define MMCR1_PMC6_ADDER_SEL_SH	37
@@ -64,11 +55,6 @@
 #define MMCR1_PMC3_ADDER_SEL_SH	33
 #define MMCR1_PMC4_ADDER_SEL_SH	32
 #define MMCR1_PMC3SEL_SH	27
-#define MMCR1_PMC4SEL_SH	22
-#define MMCR1_PMC5SEL_SH	17
-#define MMCR1_PMC6SEL_SH	12
-#define MMCR1_PMC7SEL_SH	7
-#define MMCR1_PMC8SEL_SH	2
 
 static short mmcr1_adder_bits[8] = {
 	MMCR1_PMC1_ADDER_SEL_SH,
