4e223ce12fb947fdd3aa936c53d3599a5a1ce48f
Carlos Bilbao <carlos.bilbao@kernel.org>
2025-04-28 16:59:52 -0500

    x86/panic: Add x86_panic_handler as default post-panic behavior
    Add function x86_panic_handler() as the default behavior for x86 for
    post-panic stage via panic_set_handling(). Instead of busy-wait loop, it
    will halt if there's no console to save CPU cycles.
    
    Signed-off-by: Carlos Bilbao (DigitalOcean) <carlos.bilbao@kernel.org>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -16,6 +16,7 @@
 #include <linux/initrd.h>
 #include <linux/iscsi_ibft.h>
 #include <linux/memblock.h>
+#include <linux/panic.h>
 #include <linux/panic_notifier.h>
 #include <linux/pci.h>
 #include <linux/root_dev.h>
@@ -782,6 +783,15 @@ static void __init x86_report_nx(void)
 	}
 }
 
+
+static void x86_panic_handler(void)
+{
+	if (console_trylock()) {
+		console_unlock();
+		safe_halt();
+	}
+}
+
 /*
  * Determine if we were loaded by an EFI loader.  If so, then we have also been
  * passed the efi memmap, systab, etc., so we should use these data structures
@@ -1262,6 +1272,8 @@ void __init setup_arch(char **cmdline_p)
 #endif
 
 	unwind_init();
+
+	panic_set_handling(x86_panic_handler, 1);
 }
 
 #ifdef CONFIG_X86_32
