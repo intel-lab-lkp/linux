1f24cc213b05bd5a37149329877241c7b8b7a844
Pawel Dembicki <paweldembicki@gmail.com>
2025-04-29 00:13:33 +0200

    hwmon: pmbus: mpq8785: Add support for MPM3695
    Add support for the Monolithic Power Systems MPM3695 device.
    The device is PMBus compliant and shares characteristics with the
    MPM82504.
    
    Tested with device tree based matching.
    
    Signed-off-by: Pawel Dembicki <paweldembicki@gmail.com>

--- drivers/hwmon/pmbus/mpq8785.c
+++ drivers/hwmon/pmbus/mpq8785.c
@@ -8,7 +8,7 @@
 #include <linux/of_device.h>
 #include "pmbus.h"
 
-enum chips { mpq8785, mpm82504 };
+enum chips { mpq8785, mpm82504, mpm3695_10 };
 
 static int mpq8785_identify(struct i2c_client *client,
 			    struct pmbus_driver_info *info)
@@ -60,6 +60,7 @@ static struct pmbus_driver_info mpq8785_info = {
 static const struct i2c_device_id mpq8785_id[] = {
 	{ "mpq8785", mpq8785 },
 	{ "mpm82504", mpm82504 },
+	{ "mpm3695-10", mpm3695_10 },
 	{ },
 };
 MODULE_DEVICE_TABLE(i2c, mpq8785_id);
@@ -67,6 +68,7 @@ MODULE_DEVICE_TABLE(i2c, mpq8785_id);
 static const struct of_device_id __maybe_unused mpq8785_of_match[] = {
 	{ .compatible = "mps,mpq8785", .data = (void *)mpq8785 },
 	{ .compatible = "mps,mpm82504", .data = (void *)mpm82504 },
+	{ .compatible = "mps,mpm3695-10", .data = (void *)mpm3695_10 },
 	{}
 };
 MODULE_DEVICE_TABLE(of, mpq8785_of_match);
@@ -92,6 +94,7 @@ static int mpq8785_probe(struct i2c_client *client)
 
 		break;
 	case mpm82504:
+	case mpm3695_10:
 		info->format[PSC_VOLTAGE_OUT] = direct;
 		info->m[PSC_VOLTAGE_OUT] = 8;
 		info->b[PSC_VOLTAGE_OUT] = 0;
