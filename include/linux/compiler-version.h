42405d1276709482917971dff7d415ee9e902c34
Nicolas Schier <n.schier@avm.de>
2025-04-29 14:59:13 +0200

    randstruct: Rebuild completely if randstruct.seed changes
    Include generated/randstruct_hash.h in linux/compiler-version.h to force
    a complete rebuild if CONFIG_RANDSTRUCT is enabled and randstruct.seed
    changes.
    
    Removal or change of scripts/basic/randstruct.seed leads to a remake of
    generated/randstruct_hash.h.  As linux/compiler-version.h is a
    hard-coded include for every kbuild induced compilation, conditionally
    adding generated/randstruct_hash.h there adds it as build-dependency to
    each object file.
    
    Reported-by: Kees Cook <kees@kernel.org>
    Closes: https://lore.kernel.org/linux-kbuild/202504161928.17A90D9B@keescook/
    Signed-off-by: Nicolas Schier <n.schier@avm.de>

--- include/linux/compiler-version.h
+++ include/linux/compiler-version.h
@@ -6,9 +6,19 @@
 #define __LINUX_COMPILER_VERSION_H
 
 /*
- * This header exists to force full rebuild when the compiler is upgraded.
+ * This header exists to force full rebuild when the compiler is upgraded or
+ * the randstruct is changed.
  *
  * When fixdep scans this, it will find this string "CONFIG_CC_VERSION_TEXT"
  * and add dependency on include/config/CC_VERSION_TEXT, which is touched
  * by Kconfig when the version string from the compiler changes.
  */
+#ifdef CONFIG_RANDSTRUCT
+/*
+ * If CONFIG_RANDSTRUCT is enabled and scripts/basic/randstruct.seed changes,
+ * randstruct_hash.h is updated.  Including it here, makes it a build
+ * dependency for all build objects.
+ */
+#include <generated/randstruct_hash.h>
+#undef RANDSTRUCT_HASHED_SEED
+#endif
