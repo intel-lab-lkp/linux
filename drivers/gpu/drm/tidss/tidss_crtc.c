3659d07563ab149592b3da102be5e9eb8e1180d8
Devarsh Thakkar <devarsht@ti.com>
2025-04-29 20:06:55 +0530

    drm/tidss: Update infrastructure to support K3 DSS cut-down versions
    SoCs like AM62Lx support cut-down version of K3 DSS where although same
    register space is supported as in other K3 DSS supported SoCs such as
    AM65x, AM62x, AM62Ax but some of the resources such as planes and
    corresponding register spaces are truncated.
    
    For e.g. AM62Lx has only single VIDL pipeline supported, so corresponding
    register spaces for other video pipelines need to be skipped.
    
    To add a generic support for future SoCs where one or more video pipelines
    can get truncated from the parent register space, move the video plane
    related information to vid_info struct which will also have a field to
    indicate hardware index of each of the available video planes, so that
    driver only maps and programs those video pipes and skips the unavailable
    ones.
    
    While at it, also change the num_planes field in the features structure to
    num_vid so that all places in code which use vid_info structure are
    highlighted in the code.
    
    Signed-off-by: Devarsh Thakkar <devarsht@ti.com>

--- drivers/gpu/drm/tidss/tidss_crtc.c
+++ drivers/gpu/drm/tidss/tidss_crtc.c
@@ -130,7 +130,7 @@ static void tidss_crtc_position_planes(struct tidss_device *tidss,
 	    !to_tidss_crtc_state(cstate)->plane_pos_changed)
 		return;
 
-	for (layer = 0; layer < tidss->feat->num_planes; layer++) {
+	for (layer = 0; layer < tidss->feat->num_vids ; layer++) {
 		struct drm_plane_state *pstate;
 		struct drm_plane *plane;
 		bool layer_active = false;
@@ -271,9 +271,12 @@ static void tidss_crtc_atomic_disable(struct drm_crtc *crtc,
 	 * another videoport, the DSS will report sync lost issues. Disable all
 	 * the layers here as a work-around.
 	 */
-	for (u32 layer = 0; layer < tidss->feat->num_planes; layer++)
-		dispc_ovr_enable_layer(tidss->dispc, tcrtc->hw_videoport, layer,
-				       false);
+	for (u32 layer = 0; layer < tidss->feat->num_vids; layer++) {
+		u32 hw_id = tidss->feat->vid_info[layer].hw_id;
+
+		dispc_ovr_enable_layer(tidss->dispc, tcrtc->hw_videoport,
+				       hw_id, false);
+	}
 
 	dispc_vp_disable(tidss->dispc, tcrtc->hw_videoport);
 
