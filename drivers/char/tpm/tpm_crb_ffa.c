7bec42b41b2d41df83593e1e47abe65f833e53fa
Stuart Yoder <stuart.yoder@arm.com>
2025-04-29 17:15:18 -0500

    tpm_crb: ffa_tpm: update comment to include 64-bit calling convention
    The comment for DIRECT_REQ and DIRECT_RESP only mentioned AArch32
    and listed 32-bit function IDs.  Update to include 64-bit.
    
    Signed-off-by: Stuart Yoder <stuart.yoder@arm.com>

--- drivers/char/tpm/tpm_crb_ffa.c
+++ drivers/char/tpm/tpm_crb_ffa.c
@@ -38,9 +38,11 @@
  * messages.
  *
  * All requests with FFA_MSG_SEND_DIRECT_REQ and FFA_MSG_SEND_DIRECT_RESP
- * are using the AArch32 SMC calling convention with register usage as
- * defined in FF-A specification:
- * w0:    Function ID (0x8400006F or 0x84000070)
+ * are using the AArch32 or AArch64 SMC calling convention with register usage
+ * as defined in FF-A specification:
+ * w0:    Function ID
+ *          -for 32-bit: 0x8400006F or 0x84000070
+ *          -for 64-bit: 0xC400006F or 0xC4000070
  * w1:    Source/Destination IDs
  * w2:    Reserved (MBZ)
  * w3-w7: Implementation defined, free to be used below
