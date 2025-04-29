695112a1385b3943019f3b0f9acc504acf29ee53
Dev Jain <dev.jain@arm.com>
2025-04-29 10:53:35 +0530

    mm: Batch around can_change_pte_writable()
    In preparation for patch 7, we need to properly batch around
    can_change_pte_writable(). We batch around pte_needs_soft_dirty_wp() by
    the corresponding fpb flag, we batch around the page-anon exclusive check
    using folio_maybe_mapped_shared(); modify_prot_start_ptes() collects the
    dirty and access bits across the batch, therefore batching across
    pte_dirty(): this is correct since the dirty bit on the PTE really
    is just an indication that the folio got written to, so even if
    the PTE is not actually dirty (but one of the PTEs in the batch is),
    the wp-fault optimization can be made.
    
    Signed-off-by: Dev Jain <dev.jain@arm.com>

--- mm/mprotect.c
+++ mm/mprotect.c
@@ -40,8 +40,8 @@
 
 #include "internal.h"
 
-bool can_change_pte_writable(struct vm_area_struct *vma, unsigned long addr,
-			     pte_t pte)
+bool can_change_ptes_writable(struct vm_area_struct *vma, unsigned long addr,
+			      pte_t pte, struct folio *folio, unsigned int nr)
 {
 	struct page *page;
 
@@ -67,8 +67,9 @@ bool can_change_pte_writable(struct vm_area_struct *vma, unsigned long addr,
 		 * write-fault handler similarly would map them writable without
 		 * any additional checks while holding the PT lock.
 		 */
-		page = vm_normal_page(vma, addr, pte);
-		return page && PageAnon(page) && PageAnonExclusive(page);
+		if (!folio)
+			folio = vm_normal_folio(vma, addr, pte);
+		return folio_test_anon(folio) && !folio_maybe_mapped_shared(folio);
 	}
 
 	VM_WARN_ON_ONCE(is_zero_pfn(pte_pfn(pte)) && pte_dirty(pte));
@@ -222,7 +223,7 @@ static long change_pte_range(struct mmu_gather *tlb,
 			 */
 			if ((cp_flags & MM_CP_TRY_CHANGE_WRITABLE) &&
 			    !pte_write(ptent) &&
-			    can_change_pte_writable(vma, addr, ptent))
+			    can_change_ptes_writable(vma, addr, ptent, folio, 1))
 				ptent = pte_mkwrite(ptent, vma);
 
 			ptep_modify_prot_commit(vma, addr, pte, oldpte, ptent);
