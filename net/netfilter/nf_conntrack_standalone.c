07ef868a1f365baeb9053e827e31ce03fac76fd2
Vimal Agrawal <vimal.agrawal@sophos.com>
2025-04-29 13:09:40 +0000

    nf_conntrack: sysctl: expose gc worker scan interval via sysctl
    Default initial gc scan interval of 60 secs is too long for system
    with low number of conntracks causing delay in conntrack deletion.
    It is affecting userspace which are replying on timely arrival of
    conntrack destroy event. So it is better that this is controlled
    through sysctl
    
    Fixes: 2aa192757005 ("netfilter: conntrack: revisit the gc initial rescheduling bias")
    Signed-off-by: Vimal Agrawal <vimal.agrawal@sophos.com>
    Reviewed-by: Anirudh Gupta <anirudh.gupta@sophos.com>

--- net/netfilter/nf_conntrack_standalone.c
+++ net/netfilter/nf_conntrack_standalone.c
@@ -559,6 +559,7 @@ enum nf_ct_sysctl_index {
 #ifdef CONFIG_NF_CONNTRACK_TIMESTAMP
 	NF_SYSCTL_CT_TIMESTAMP,
 #endif
+	NF_SYSCTL_CT_GC_SCAN_INTERVAL_INIT,
 	NF_SYSCTL_CT_PROTO_TIMEOUT_GENERIC,
 	NF_SYSCTL_CT_PROTO_TIMEOUT_TCP_SYN_SENT,
 	NF_SYSCTL_CT_PROTO_TIMEOUT_TCP_SYN_RECV,
@@ -691,6 +692,13 @@ static struct ctl_table nf_ct_sysctl_table[] = {
 		.extra2 	= SYSCTL_ONE,
 	},
 #endif
+	[NF_SYSCTL_CT_GC_SCAN_INTERVAL_INIT] = {
+		.procname	= "nf_conntrack_gc_scan_interval_init",
+		.data		= &nf_conntrack_gc_scan_interval_init,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_jiffies,
+	},
 	[NF_SYSCTL_CT_PROTO_TIMEOUT_GENERIC] = {
 		.procname	= "nf_conntrack_generic_timeout",
 		.maxlen		= sizeof(unsigned int),
