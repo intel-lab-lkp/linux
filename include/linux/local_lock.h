32e1d35bf5050a323c32f1d78c7fc43a6f67637f
Leonardo Bras <leobras@redhat.com>
2025-04-30 04:36:10 -0300

    local_lock: Minor improvements of local_trylock*() documentation
    Fix local_trylock_init() documentation, as it was mentioning the non-try
    helper instead, and use the opportunity to make clear the try_lock*() needs
    to receive a try-enabled variable as parameter.
    
    Signed-off-by: Leonardo Bras <leobras@redhat.com>

--- include/linux/local_lock.h
+++ include/linux/local_lock.h
@@ -52,13 +52,13 @@
 	__local_unlock_irqrestore(lock, flags)
 
 /**
- * local_lock_init - Runtime initialize a lock instance
+ * local_trylock_init - Runtime initialize a try-enabled lock instance
  */
 #define local_trylock_init(lock)	__local_trylock_init(lock)
 
 /**
  * local_trylock - Try to acquire a per CPU local lock
- * @lock:	The lock variable
+ * @lock:	The try-enabled lock variable
  *
  * The function can be used in any context such as NMI or HARDIRQ. Due to
  * locking constrains it will _always_ fail to acquire the lock in NMI or
@@ -69,7 +69,7 @@
 /**
  * local_trylock_irqsave - Try to acquire a per CPU local lock, save and disable
  *			   interrupts if acquired
- * @lock:	The lock variable
+ * @lock:	The try-enabled lock variable
  * @flags:	Storage for interrupt flags
  *
  * The function can be used in any context such as NMI or HARDIRQ. Due to
