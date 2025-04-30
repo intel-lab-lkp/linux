862d2011d6c307f7607e263b5b7c3f1e184a48ef
Grzegorz Jaszczyk <jaszczyk@google.com>
2025-04-30 08:41:37 +0000

    x86/of: add support for reserved memory defined by DT
    The DT reserved-memory nodes can be present in DT as described in
    Documentation/devicetree/bindings/reserved-memory/reserved-memory.yaml.
    Similar to other architecture, which supports DT, there is a need to
    create reserved memory regions for such nodes.
    
    Additionally this patch changes the sequence and calls
    x86_flattree_get_config earlier during memblock setup, just after e820
    memblock setup, so the DT reserved-memory can be properly reflected in
    the memblock before it is used for allocation.
    
    Signed-off-by: Grzegorz Jaszczyk <jaszczyk@google.com>

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -1042,6 +1042,8 @@ void __init setup_arch(char **cmdline_p)
 
 	e820__memblock_setup();
 
+	x86_flattree_get_config();
+
 	/*
 	 * Needs to run after memblock setup because it needs the physical
 	 * memory size.
@@ -1151,8 +1153,6 @@ void __init setup_arch(char **cmdline_p)
 	/* Some platforms need the APIC registered for NUMA configuration */
 	early_acpi_boot_init();
 	x86_init.mpparse.early_parse_smp_cfg();
-
-	x86_flattree_get_config();
 
 	initmem_init();
 	dma_contiguous_reserve(max_pfn_mapped << PAGE_SHIFT);
