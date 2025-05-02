48c67da9bce7682ccdfe8a7ede262fc3dde60add
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2025-05-02 14:40:18 +0200

    selftests: vDSO: vdso_test_getrandom: Always print TAP header
    The TAP specification requires that the output begins with a header line.
    If vgetrandom_init() fails and skips the test, that header line is missing.
    
    Call vgetrandom_init() after ksft_print_header().
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- tools/testing/selftests/vDSO/vdso_test_getrandom.c
+++ tools/testing/selftests/vDSO/vdso_test_getrandom.c
@@ -232,6 +232,7 @@ static void kselftest(void)
 	pid_t child;
 
 	ksft_print_header();
+	vgetrandom_init();
 	ksft_set_plan(2);
 
 	for (size_t i = 0; i < 1000; ++i) {
@@ -285,8 +286,6 @@ static void usage(const char *argv0)
 
 int main(int argc, char *argv[])
 {
-	vgetrandom_init();
-
 	if (argc == 1) {
 		kselftest();
 		return 0;
@@ -296,6 +295,9 @@ int main(int argc, char *argv[])
 		usage(argv[0]);
 		return 1;
 	}
+
+	vgetrandom_init();
+
 	if (!strcmp(argv[1], "bench-single"))
 		bench_single();
 	else if (!strcmp(argv[1], "bench-multi"))
