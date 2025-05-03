9daceaa21ac3228d9a3b583fd796fe2fc6844be7
Yunhong Jiang <yunhong.jiang@linux.intel.com>
2025-05-03 12:15:14 -0700

    x86/hyperv/vtl: Mark the wakeup mailbox page as private
    The current code maps MMIO devices as shared (decrypted) by default in a
    confidential computing VM.
    
    In a TDX environment, secondary CPUs are booted using the Multiprocessor
    Wakeup Structure defined in the ACPI specification. The virtual firmware
    and the operating system function in the guest context, without
    intervention from the VMM. Map the physical memory of the mailbox as
    private. Use the is_private_mmio() callback.
    
    Signed-off-by: Yunhong Jiang <yunhong.jiang@linux.intel.com>
    Signed-off-by: Ricardo Neri <ricardo.neri-calderon@linux.intel.com>

--- arch/x86/hyperv/hv_vtl.c
+++ arch/x86/hyperv/hv_vtl.c
@@ -53,6 +53,18 @@ static void  __noreturn hv_vtl_restart(char __maybe_unused *cmd)
 	hv_vtl_emergency_restart();
 }
 
+static inline bool within_page(u64 addr, u64 start)
+{
+	return addr >= start && addr < (start + PAGE_SIZE);
+}
+
+static bool hv_vtl_is_private_mmio_tdx(u64 addr)
+{
+	u64 mb_addr = get_mp_wakeup_mailbox_paddr();
+
+	return mb_addr && within_page(addr, mb_addr);
+}
+
 void __init hv_vtl_init_platform(void)
 {
 	pr_info("Linux runs in Hyper-V Virtual Trust Level\n");
@@ -60,6 +72,8 @@ void __init hv_vtl_init_platform(void)
 	/* There is no paravisor present if we are here. */
 	if (hv_isolation_type_tdx()) {
 		x86_init.resources.realmode_limit = SZ_4G;
+		x86_platform.hyper.is_private_mmio = hv_vtl_is_private_mmio_tdx;
+
 	} else {
 		x86_platform.realmode_reserve = x86_init_noop;
 		x86_platform.realmode_init = x86_init_noop;
