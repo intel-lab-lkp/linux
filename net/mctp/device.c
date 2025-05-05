3ae0c740bf92a1ec2f8b7775790eb3049b751f29
Matt Johnston <matt@codeconstruct.com.au>
2025-05-05 17:05:12 +0800

    net: mctp: Don't access ifa_index when missing
    In mctp_dump_addrinfo, ifa_index can be used to filter interfaces, but
    only when the struct ifaddrmsg is provided. Otherwise it will be
    comparing to uninitialised memory - reproducible in the syzkaller case from
    dhcpd, or busybox "ip addr show".
    
    The kernel MCTP implementation has always filtered by ifa_index, so
    existing userspace programs expecting to dump MCTP addresses must
    already be passing a valid ifa_index value (either 0 or a real index).
    
    BUG: KMSAN: uninit-value in mctp_dump_addrinfo+0x208/0xac0 net/mctp/device.c:128
     mctp_dump_addrinfo+0x208/0xac0 net/mctp/device.c:128
     rtnl_dump_all+0x3ec/0x5b0 net/core/rtnetlink.c:4380
     rtnl_dumpit+0xd5/0x2f0 net/core/rtnetlink.c:6824
     netlink_dump+0x97b/0x1690 net/netlink/af_netlink.c:2309
    
    Fixes: 583be982d934 ("mctp: Add device handling and netlink interface")
    Reported-by: syzbot+e76d52dadc089b9d197f@syzkaller.appspotmail.com
    Closes: https://lore.kernel.org/all/68135815.050a0220.3a872c.000e.GAE@google.com/
    Reported-by: syzbot+1065a199625a388fce60@syzkaller.appspotmail.com
    Closes: https://lore.kernel.org/all/681357d6.050a0220.14dd7d.000d.GAE@google.com/
    Signed-off-by: Matt Johnston <matt@codeconstruct.com.au>

--- net/mctp/device.c
+++ net/mctp/device.c
@@ -117,11 +117,17 @@ static int mctp_dump_addrinfo(struct sk_buff *skb, struct netlink_callback *cb)
 	struct net_device *dev;
 	struct ifaddrmsg *hdr;
 	struct mctp_dev *mdev;
-	int ifindex, rc;
+	int ifindex = 0, rc;
 
-	hdr = nlmsg_data(cb->nlh);
-	// filter by ifindex if requested
-	ifindex = hdr->ifa_index;
+	/* Filter by ifindex if a header is provided */
+	if (cb->nlh->nlmsg_len >= nlmsg_msg_size(sizeof(*hdr))) {
+		hdr = nlmsg_data(cb->nlh);
+		/* Userspace programs providing AF_MCTP must be expecting ifa_index filter
+		 * behaviour, as will those setting strict_check.
+		 */
+		if (hdr->ifa_family == AF_MCTP || cb->strict_check)
+			ifindex = hdr->ifa_index;
+	}
 
 	rcu_read_lock();
 	for_each_netdev_dump(net, dev, mcb->ifindex) {
