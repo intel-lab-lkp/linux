895754fe276fd356e2715bfafdb988fbd23a1255
Ahmed S. Darwish <darwi@linutronix.de>
2025-05-06 07:04:25 +0200

    x86/cpuid: Introduce scanned CPUID(0x2) API
    Add a new iterator macro, for_each_scanned_leaf_0x2_entry(), for parsing
    scanned CPUID(0x2) entries as 1-byte descriptors.
    
    Unlike the existing for_each_leaf_0x2_entry() macro, which operates on
    directly retrieved CPUID data, the new one takes its input from the
    scanned CPUID data access API.  That is, it is expected to be used as:
    
        const struct leaf_0x2_table *entry;
        const struct cpuid_regs *regs;
        u8 *ptr;
    
        regs = cpudata_cpuid_regs(c, 0x2);	// Scanned CPUID access
        for_each_scanned_leaf_0x2_entry(regs, ptr, entry) {
            ...
        }
    
    which should replace the older method:
    
        const struct leaf_0x2_table *entry;
        union leaf_0x2_regs regs;
        u8 *ptr;
    
        cpuid_get_leaf_0x2_regs(&regs);	// Direct CPUID access
        for_each_leaf_0x2_entry(regs, ptr, entry) {
            ...
        }
    
    In the new macro, assert that the passed 'regs' is the same size as a
    'union leaf_0x2_regs'.  This is necessary since the macro internally
    casts 'regs' to that union in order to iterate over the CPUID(0x2) output
    as a 1-byte array.
    
    A size equivalence assert is used, instead of a typeof() check, to give
    callers the freedom to either pass a 'struct cpuid_regs' pointer or a
    'struct leaf_0x2_0' pointer, both as returned by the scanned CPUID API at
    <asm/cpuid/table_api.h>.  That size comparison matches what other kernel
    CPUID do; e.g. cpuid_leaf() and cpuid_leaf_reg() at <asm/cpuid/api.h>.
    
    Note, put the size equivalence check inside a GNU statement expression,
    ({..}), so that it can be placed inside the macro's loop initialization.
    
    Signed-off-by: Ahmed S. Darwish <darwi@linutronix.de>

--- arch/x86/include/asm/cpuid/leaf_0x2_api.h
+++ arch/x86/include/asm/cpuid/leaf_0x2_api.h
@@ -70,4 +70,39 @@ static inline void cpuid_get_leaf_0x2_regs(union leaf_0x2_regs *regs)
 	     __ptr < &(regs).desc[16] && (entry = &cpuid_0x2_table[*__ptr]);	\
 	     __ptr++)
 
+/**
+ * for_each_scanned_leaf_0x2_entry() - Iterator for parsed CPUID(0x2) descriptors
+ * @regs:   Leaf 0x2 register output, as returned by cpudata_cpuid_regs()
+ * @__ptr:  u8 pointer, for macro internal use only
+ * @entry:  Pointer to parsed descriptor information at each iteration
+ *
+ * Loop over the 1-byte descriptors in the passed CPUID(0x2) output registers
+ * @regs.  Provide the parsed information for each descriptor through @entry.
+ *
+ * To handle cache-specific descriptors, switch on @entry->c_type.  For TLB
+ * descriptors, switch on @entry->t_type.
+ *
+ * Example usage for cache descriptors::
+ *
+ *	const struct leaf_0x2_table *entry;
+ *	struct cpuid_regs *regs;
+ *	u8 *ptr;
+ *
+ *	regs = cpudata_cpuid_regs(c, 0x2);
+ *	if (!regs) {
+ *		// Handle error
+ *	}
+ *
+ *	for_each_scanned_leaf_0x2_entry(regs, ptr, entry) {
+ *		switch (entry->c_type) {
+ *			...
+ *		}
+ *	}
+ */
+#define for_each_scanned_leaf_0x2_entry(regs, __ptr, entry)					\
+	for (({ static_assert(sizeof(*regs) == sizeof(union leaf_0x2_regs)); }),		\
+	     __ptr = &((union leaf_0x2_regs *)(regs))->desc[1];					\
+	     __ptr < &((union leaf_0x2_regs *)(regs))->desc[16] && (entry = &cpuid_0x2_table[*__ptr]);\
+	     __ptr++)
+
 #endif /* _ASM_X86_CPUID_LEAF_0x2_API_H */
