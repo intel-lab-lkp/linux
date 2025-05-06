92bb2d9b68c03e8e5d62b97d0ffe9cb481f7d6a1
Eric Florin <ericflorin@google.com>
2025-05-05 20:30:13 -0700

    staging: sm750fb: rename `regValue` to `reg_value`
    Rename `regValue` to `reg_value` in `write_dpr` to conform with style
    guidelines as reported by checkpatch.pl
    
    CHECK: Avoid CamelCase: <regValue>
    
    Signed-off-by: Eric Florin <ericflorin@google.com>

--- drivers/staging/sm750fb/sm750_accel.c
+++ drivers/staging/sm750fb/sm750_accel.c
@@ -17,9 +17,10 @@
 
 #include "sm750.h"
 #include "sm750_accel.h"
-static inline void write_dpr(struct lynx_accel *accel, int offset, u32 regValue)
+static inline void write_dpr(struct lynx_accel *accel, int offset,
+			     u32 reg_value)
 {
-	writel(regValue, accel->dprBase + offset);
+	writel(reg_value, accel->dprBase + offset);
 }
 
 static inline u32 read_dpr(struct lynx_accel *accel, int offset)
