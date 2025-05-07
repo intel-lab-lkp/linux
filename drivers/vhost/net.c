5e92ab26b0f08820b256acac233eab16e117da6b
Jon Kohler <jon@nutanix.com>
2025-05-07 09:19:35 -0700

    vhost/net: use xdp_get_frame_len()
    Use xdp_get_frame_len helper to ensure xdp frame size is calculated
    correctly in both single buffer and multi buffer configurations.
    
    Signed-off-by: Jon Kohler <jon@nutanix.com>

--- drivers/vhost/net.c
+++ drivers/vhost/net.c
@@ -199,7 +199,7 @@ static int vhost_net_buf_peek_len(void *ptr)
 	if (tun_is_xdp_frame(ptr)) {
 		struct xdp_frame *xdpf = tun_ptr_to_xdp(ptr);
 
-		return xdpf->len;
+		return xdp_get_frame_len(xdpf);
 	}
 
 	return __skb_array_len_with_tag(ptr);
