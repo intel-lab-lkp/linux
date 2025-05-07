afddfc9364a3456d05266955580bc1f279c994e1
Thierry Reding <treding@nvidia.com>
2025-05-07 16:37:58 +0200

    dt-bindings: Add Tegra264 clock and reset definitions
    Signed-off-by: Thierry Reding <treding@nvidia.com>

---
+++ include/dt-bindings/clock/tegra264-clock.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/* Copyright (c) 2022-2023, NVIDIA CORPORATION. All rights reserved. */
+
+#ifndef DT_BINDINGS_CLOCK_TEGRA264_CLOCK_H
+#define DT_BINDINGS_CLOCK_TEGRA264_CLOCK_H
+
+#define TEGRA264_CLK_CLK_S			2U
+
+#endif /* DT_BINDINGS_CLOCK_TEGRA264_CLOCK_H */
