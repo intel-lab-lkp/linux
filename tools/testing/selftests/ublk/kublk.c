b668ff0fffa764d05606ded0f6c1ccfa130be706
Ming Lei <ming.lei@redhat.com>
2025-05-07 12:38:57 +0800

    selftests: ublk: add test for covering UBLK_AUTO_BUF_REG_FALLBACK
    Add test for covering UBLK_AUTO_BUF_REG_FALLBACK:
    
    - pass '--auto_zc_fallback' to null target, which requires both F_AUTO_BUF_REG
    and F_SUPPORT_ZERO_COPY for handling UBLK_AUTO_BUF_REG_FALLBACK
    
    - add ->buf_index() method for returning invalid buffer index to trigger
    UBLK_AUTO_BUF_REG_FALLBACK
    
    - add generic_09 for running the test
    
    - add --auto_zc_fallback test in stress_03/stress_04/stress_05
    
    Signed-off-by: Ming Lei <ming.lei@redhat.com>

--- tools/testing/selftests/ublk/kublk.c
+++ tools/testing/selftests/ublk/kublk.c
@@ -405,7 +405,8 @@ static void ublk_queue_deinit(struct ublk_queue *q)
 		free(q->ios[i].buf_addr);
 }
 
-static int ublk_queue_init(struct ublk_queue *q)
+static int ublk_queue_init(struct ublk_queue *q,
+			   unsigned char auto_zc_fallback)
 {
 	struct ublk_dev *dev = q->dev;
 	int depth = dev->dev_info.queue_depth;
@@ -424,8 +425,11 @@ static int ublk_queue_init(struct ublk_queue *q)
 		q->state |= UBLKSRV_NO_BUF;
 		if (dev->dev_info.flags & UBLK_F_SUPPORT_ZERO_COPY)
 			q->state |= UBLKSRV_ZC;
-		if (dev->dev_info.flags & UBLK_F_AUTO_BUF_REG)
+		if (dev->dev_info.flags & UBLK_F_AUTO_BUF_REG) {
 			q->state |= UBLKSRV_AUTO_BUF_REG;
+			if (auto_zc_fallback)
+				q->state |= UBLKSRV_AUTO_BUF_REG_FALLBACK;
+		}
 	}
 
 	cmd_buf_size = ublk_queue_cmd_buf_sz(q);
@@ -583,8 +587,13 @@ int ublk_queue_io_cmd(struct ublk_queue *q, struct ublk_io *io, unsigned tag)
 		cmd->addr	= 0;
 
 	if (q->state & UBLKSRV_AUTO_BUF_REG) {
-		cmd->auto_buf.index = tag;
+		if (q->tgt_ops->buf_index)
+			cmd->auto_buf.index = q->tgt_ops->buf_index(q, tag);
+		else
+			cmd->auto_buf.index = tag;
 		cmd->auto_buf.flags = 0;
+		if (q->state & UBLKSRV_AUTO_BUF_REG_FALLBACK)
+			cmd->auto_buf.flags |= UBLK_AUTO_BUF_REG_FALLBACK;
 	}
 
 	user_data = build_user_data(tag, _IOC_NR(cmd_op), 0, 0);
@@ -738,6 +747,7 @@ struct ublk_queue_info {
 	struct ublk_queue 	*q;
 	sem_t 			*queue_sem;
 	cpu_set_t 		*affinity;
+	unsigned char 		auto_zc_fallback;
 };
 
 static void *ublk_io_handler_fn(void *data)
@@ -747,7 +757,7 @@ static void *ublk_io_handler_fn(void *data)
 	int dev_id = q->dev->dev_info.dev_id;
 	int ret;
 
-	ret = ublk_queue_init(q);
+	ret = ublk_queue_init(q, info->auto_zc_fallback);
 	if (ret) {
 		ublk_err("ublk dev %d queue %d init queue failed\n",
 				dev_id, q->q_id);
@@ -840,6 +850,7 @@ static int ublk_start_daemon(const struct dev_ctx *ctx, struct ublk_dev *dev)
 		qinfo[i].q = &dev->q[i];
 		qinfo[i].queue_sem = &queue_sem;
 		qinfo[i].affinity = &affinity_buf[i];
+		qinfo[i].auto_zc_fallback = ctx->auto_zc_fallback;
 		pthread_create(&dev->q[i].thread, NULL,
 				ublk_io_handler_fn,
 				&qinfo[i]);
@@ -1255,7 +1266,7 @@ static void __cmd_create_help(char *exe, bool recovery)
 
 	printf("%s %s -t [null|loop|stripe|fault_inject] [-q nr_queues] [-d depth] [-n dev_id]\n",
 			exe, recovery ? "recover" : "add");
-	printf("\t[--foreground] [--quiet] [-z] [--auto_zc] [--debug_mask mask] [-r 0|1 ] [-g]\n");
+	printf("\t[--foreground] [--quiet] [-z] [--auto_zc] [--auto_zc_fallback] [--debug_mask mask] [-r 0|1 ] [-g]\n");
 	printf("\t[-e 0|1 ] [-i 0|1]\n");
 	printf("\t[target options] [backfile1] [backfile2] ...\n");
 	printf("\tdefault: nr_queues=2(max 32), depth=128(max 1024), dev_id=-1(auto allocation)\n");
@@ -1310,7 +1321,8 @@ int main(int argc, char *argv[])
 		{ "recovery_fail_io",	1,	NULL, 'e'},
 		{ "recovery_reissue",	1,	NULL, 'i'},
 		{ "get_data",		1,	NULL, 'g'},
-		{ "auto_zc",		0,	NULL,  0},
+		{ "auto_zc",		0,	NULL,  0 },
+		{ "auto_zc_fallback", 	0,	NULL,  0 },
 		{ 0, 0, 0, 0 }
 	};
 	const struct ublk_tgt_ops *ops = NULL;
@@ -1381,6 +1393,8 @@ int main(int argc, char *argv[])
 				ctx.fg = 1;
 			if (!strcmp(longopts[option_idx].name, "auto_zc"))
 				ctx.flags |= UBLK_F_AUTO_BUF_REG;
+			if (!strcmp(longopts[option_idx].name, "auto_zc_fallback"))
+				ctx.auto_zc_fallback = 1;
 			break;
 		case '?':
 			/*
@@ -1402,6 +1416,16 @@ int main(int argc, char *argv[])
 			optind += 1;
 			break;
 		}
+	}
+
+	/* auto_zc_fallback depends on F_AUTO_BUF_REG & F_SUPPORT_ZERO_COPY */
+	if (ctx.auto_zc_fallback &&
+	    !((ctx.flags & UBLK_F_AUTO_BUF_REG) &&
+		    (ctx.flags & UBLK_F_SUPPORT_ZERO_COPY))) {
+		ublk_err("%s: auto_zc_fallback is set but neither "
+				"F_AUTO_BUF_REG nor F_SUPPORT_ZERO_COPY is enabled\n",
+					__func__);
+		return -EINVAL;
 	}
 
 	i = optind;
