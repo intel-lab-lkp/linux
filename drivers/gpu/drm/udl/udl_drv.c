cdb45fe034d910c340b5ff3253d00d2260d4bff1
Shixiong Ou <oushixiong@kylinos.cn>
2025-05-07 17:47:28 +0800

    drm/udl: use DRM_GEM_SHMEM_SIMPLE_DRIVER_OPS
    Import dmabuf without mapping its sg_table to avoid issues likes:
       udl 2-1.4:1.0: swiotlb buffer is full (sz: 2097152 bytes), total 65536 (slots), used 1 (slots)
    
    Signed-off-by: Shixiong Ou <oushixiong@kylinos.cn>

--- drivers/gpu/drm/udl/udl_drv.c
+++ drivers/gpu/drm/udl/udl_drv.c
@@ -57,7 +57,7 @@ static const struct drm_driver driver = {
 
 	/* GEM hooks */
 	.fops = &udl_driver_fops,
-	DRM_GEM_SHMEM_DRIVER_OPS,
+	DRM_GEM_SHMEM_DRIVER_OPS_FOR_VMAP,
 	DRM_FBDEV_SHMEM_DRIVER_OPS,
 
 	.name = DRIVER_NAME,
