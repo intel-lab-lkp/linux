771db76ed4b71ad822acb36ac41ba725488d2834
Thierry Reding <treding@nvidia.com>
2025-05-07 16:37:59 +0200

    dt-bindings: memory: Add Tegra264 definitions
    This doesn't currently contain any stream ID or memory client ID
    definitions, but they will be added in subsquent patches.
    
    Signed-off-by: Thierry Reding <treding@nvidia.com>

---
+++ include/dt-bindings/memory/tegra264-mc.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: (GPL-2.0 OR MIT) */
+/* Copyright (c) 2024, NVIDIA CORPORATION. All rights reserved. */
+
+#ifndef DT_BINDINGS_MEMORY_TEGRA264_MC_H
+#define DT_BINDINGS_MEMORY_TEGRA264_MC_H
+
+#define TEGRA264_SID(x) ((x) << 8)
+
+/*
+ * memory client IDs
+ */
+
+#endif
