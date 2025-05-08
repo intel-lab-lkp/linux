6294e106d4074623be6f121319e7a39f749dfe82
Jakub Kicinski <kuba@kernel.org>
2025-05-07 19:28:38 -0700

    tools: ynl-gen: auto-indent else
    We auto-indent if statements (increase the indent of the subsequent
    line by 1), do the same thing for else branches without a block.
    There hasn't been any else branches before but we're about to add one.
    
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- tools/net/ynl/pyynl/ynl_gen_c.py
+++ tools/net/ynl/pyynl/ynl_gen_c.py
@@ -1457,6 +1457,7 @@ class CodeWriter:
         if self._silent_block:
             ind += 1
         self._silent_block = line.endswith(')') and CodeWriter._is_cond(line)
+        self._silent_block |= line.strip() == 'else'
         if line[0] == '#':
             ind = 0
         if add_ind:
