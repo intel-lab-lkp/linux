4d4578085704e37c17b91a6b284854bdba485610
Byungchul Park <byungchul@sk.com>
2025-05-09 20:51:17 +0900

    page_pool: rename __page_pool_alloc_pages_slow() to __page_pool_alloc_netmems_slow()
    Now that __page_pool_alloc_pages_slow() is for allocating netmem, not
    struct page, rename it to __page_pool_alloc_netmems_slow() to reflect
    what it does.
    
    Signed-off-by: Byungchul Park <byungchul@sk.com>

--- net/core/page_pool.c
+++ net/core/page_pool.c
@@ -523,7 +523,7 @@ static netmem_ref __page_pool_alloc_large_netmem(struct page_pool *pool,
 }
 
 /* slow path */
-static noinline netmem_ref __page_pool_alloc_pages_slow(struct page_pool *pool,
+static noinline netmem_ref __page_pool_alloc_netmems_slow(struct page_pool *pool,
 							gfp_t gfp)
 {
 	const int bulk = PP_ALLOC_CACHE_REFILL;
@@ -595,7 +595,7 @@ netmem_ref page_pool_alloc_netmems(struct page_pool *pool, gfp_t gfp)
 	if (static_branch_unlikely(&page_pool_mem_providers) && pool->mp_ops)
 		netmem = pool->mp_ops->alloc_netmems(pool, gfp);
 	else
-		netmem = __page_pool_alloc_pages_slow(pool, gfp);
+		netmem = __page_pool_alloc_netmems_slow(pool, gfp);
 	return netmem;
 }
 EXPORT_SYMBOL(page_pool_alloc_netmems);
