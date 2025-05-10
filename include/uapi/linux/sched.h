fa547a38abd1a02bd70477e9c8e9b2ecb1d5060d
Hyunmin Lee <hyunminrlee@gmail.com>
2025-05-11 04:34:29 +0900

    sched: Fix a typo in the comment for CLONE_THREAD
    Signed-off-by: Hyunmin Lee <hyunminrlee@gmail.com>

--- include/uapi/linux/sched.h
+++ include/uapi/linux/sched.h
@@ -16,7 +16,7 @@
 #define CLONE_PTRACE	0x00002000	/* set if we want to let tracing continue on the child too */
 #define CLONE_VFORK	0x00004000	/* set if the parent wants the child to wake it up on mm_release */
 #define CLONE_PARENT	0x00008000	/* set if we want to have the same parent as the cloner */
-#define CLONE_THREAD	0x00010000	/* Same thread group? */
+#define CLONE_THREAD	0x00010000	/* Same thread group */
 #define CLONE_NEWNS	0x00020000	/* New mount namespace group */
 #define CLONE_SYSVSEM	0x00040000	/* share system V SEM_UNDO semantics */
 #define CLONE_SETTLS	0x00080000	/* create a new TLS for the child */
