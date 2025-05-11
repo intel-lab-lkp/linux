b7e4d0b3c3da081192fd0ddacb4721db0efc7a78
Jernej Skrabec <jernej.skrabec@gmail.com>
2025-05-11 22:31:19 +1200

    drm: sun4i: de33: mixer: add Display Engine 3.3 (DE33) support
    The DE33 is a newer version of the Allwinner Display Engine IP block,
    found in the H616, H618, H700 and T507 SoCs. DE2 and DE3 are already
    supported by the mainline driver.
    
    Notable features (from the H616 datasheet and implemented):
    - 4096 x 2048 (4K) output support
    
    Other features (implemented but not in this patchset):
    - AFBC ARM Frame Buffer Compression support
    - YUV pipeline support
    
    The DE2 and DE3 engines have a blender register range within the
    mixer engine register map, whereas the DE33 separates this out into
    a separate display group, and adds a top register map.
    
    The DE33 also appears to remove the global double buffer control
    register, present in the DE2 and DE3.
    
    Extend the mixer to support the DE33.
    
    Signed-off-by: Jernej Skrabec <jernej.skrabec@gmail.com>
    Signed-off-by: Ryan Walklin <ryan@testtoast.com>
    Signed-off-by: Chris Morgan <macromorgan@hotmail.com>

--- drivers/gpu/drm/sun4i/sun8i_mixer.h
+++ drivers/gpu/drm/sun4i/sun8i_mixer.h
@@ -21,6 +21,9 @@
 #define SUN8I_MIXER_GLOBAL_DBUFF		0x8
 #define SUN8I_MIXER_GLOBAL_SIZE			0xc
 
+#define SUN50I_MIXER_GLOBAL_SIZE		0x8
+#define SUN50I_MIXER_GLOBAL_CLK			0xc
+
 #define SUN8I_MIXER_GLOBAL_CTL_RT_EN		BIT(0)
 
 #define SUN8I_MIXER_GLOBAL_DBUFF_ENABLE		BIT(0)
@@ -151,6 +154,12 @@ enum {
 	CCSC_D1_MIXER0_LAYOUT,
 };
 
+enum sun8i_mixer_type {
+	sun8i_mixer_de2,
+	sun8i_mixer_de3,
+	sun8i_mixer_de33,
+};
+
 /**
  * struct sun8i_mixer_cfg - mixer HW configuration
  * @vi_num: number of VI channels
@@ -171,8 +180,9 @@ struct sun8i_mixer_cfg {
 	int		scaler_mask;
 	int		ccsc;
 	unsigned long	mod_rate;
-	unsigned int	is_de3 : 1;
+	unsigned int	de_type;
 	unsigned int	scanline_yuv;
+	unsigned int	map[6];
 };
 
 struct sun8i_mixer {
@@ -184,6 +194,9 @@ struct sun8i_mixer {
 
 	struct clk			*bus_clk;
 	struct clk			*mod_clk;
+
+	struct regmap			*top_regs;
+	struct regmap			*disp_regs;
 };
 
 enum {
@@ -220,13 +233,16 @@ sun8i_blender_base(struct sun8i_mixer *mixer)
 static inline struct regmap *
 sun8i_blender_regmap(struct sun8i_mixer *mixer)
 {
-	return mixer->engine.regs;
+	return mixer->cfg->de_type == sun8i_mixer_de33 ?
+		mixer->disp_regs : mixer->engine.regs;
 }
 
 static inline u32
 sun8i_channel_base(struct sun8i_mixer *mixer, int channel)
 {
-	if (mixer->cfg->is_de3)
+	if (mixer->cfg->de_type == sun8i_mixer_de33)
+		return mixer->cfg->map[channel] * 0x20000 + DE2_CH_SIZE;
+	else if (mixer->cfg->de_type == sun8i_mixer_de3)
 		return DE3_CH_BASE + channel * DE3_CH_SIZE;
 	else
 		return DE2_CH_BASE + channel * DE2_CH_SIZE;
