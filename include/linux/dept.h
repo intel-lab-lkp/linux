abaf6f500e96990944c9b5ce4457c538e48a4097
Byungchul Park <byungchul@sk.com>
2025-05-13 19:07:21 +0900

    dept: make dept aware of lockdep_set_lock_cmp_fn() annotation
    commit eb1cfd09f788e (lockdep: Add lock_set_cmp_fn() annotation) has
    been added to address the issue that lockdep was not able to detect a
    true deadlock like the following:
    
       https://lore.kernel.org/lkml/20220510232633.GA18445@X58A-UD3R/
    
    The approach is only for lockdep but dept should work being aware of it
    because the new annotation is already used to avoid false positive of
    lockdep in the code.
    
    Make dept aware of the new lockdep annotation.
    
    Signed-off-by: Byungchul Park <byungchul@sk.com>

--- include/linux/dept.h
+++ include/linux/dept.h
@@ -136,6 +136,11 @@ struct dept_map {
 	struct dept_key			*keys;
 
 	/*
+	 * keep lockdep map to handle lockdep_set_lock_cmp_fn().
+	 */
+	void				*lockdep_map;
+
+	/*
 	 * subclass that can be set from user
 	 */
 	int				sub_u;
@@ -161,6 +166,7 @@ struct dept_map {
 {									\
 	.name = #n,							\
 	.keys = (struct dept_key *)(k),					\
+	.lockdep_map = NULL,						\
 	.sub_u = 0,							\
 	.map_key = { .classes = { NULL, } },				\
 	.wgen = 0U,							\
@@ -432,6 +438,8 @@ extern void dept_softirqs_on_ip(unsigned long ip);
 extern void dept_hardirqs_on(void);
 extern void dept_softirqs_off(void);
 extern void dept_hardirqs_off(void);
+
+#define dept_set_lockdep_map(m, lockdep_m) ({ (m)->lockdep_map = lockdep_m; })
 #else /* !CONFIG_DEPT */
 struct dept_key { };
 struct dept_map { };
@@ -474,5 +482,7 @@ struct dept_ext_wgen { };
 #define dept_hardirqs_on()				do { } while (0)
 #define dept_softirqs_off()				do { } while (0)
 #define dept_hardirqs_off()				do { } while (0)
+
+#define dept_set_lockdep_map(m, lockdep_m)		do { } while (0)
 #endif
 #endif /* __LINUX_DEPT_H */
