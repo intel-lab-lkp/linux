35971b6ec1c4966e3fabfb3528563a7b6522d1b4
Alejandro Lucero <alucerop@amd.com>
2025-05-14 14:27:40 +0100

    cxl: Add region flag for precluding a device memory to be used for dax
    By definition a type2 cxl device will use the host managed memory for
    specific functionality, therefore it should not be available to other
    uses. However, a dax interface could be just good enough in some cases.
    
    Add a flag to a cxl region for specifically state to not create a dax
    device. Allow a Type2 driver to set that flag at region creation time.
    
    Signed-off-by: Alejandro Lucero <alucerop@amd.com>
    Reviewed-by: Zhi Wang <zhiw@nvidia.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Ben Cheatham <benjamin.cheatham@amd.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>

--- drivers/cxl/core/region.c
+++ drivers/cxl/core/region.c
@@ -3658,12 +3658,14 @@ err:
  * @cxlrd: root decoder to allocate HPA
  * @cxled: endpoint decoder with reserved DPA capacity
  * @ways: interleave ways required
+ * @no_dax: if true no DAX device should be created
  *
  * Returns a fully formed region in the commit state and attached to the
  * cxl_region driver.
  */
 struct cxl_region *cxl_create_region(struct cxl_root_decoder *cxlrd,
-				     struct cxl_endpoint_decoder *cxled, int ways)
+				     struct cxl_endpoint_decoder *cxled, int ways,
+				     bool no_dax)
 {
 	struct cxl_region *cxlr;
 
@@ -3678,6 +3680,9 @@ struct cxl_region *cxl_create_region(struct cxl_root_decoder *cxlrd,
 		drop_region(cxlr);
 		return ERR_PTR(-ENODEV);
 	}
+
+	if (no_dax)
+		set_bit(CXL_REGION_F_NO_DAX, &cxlr->flags);
 
 	return cxlr;
 }
@@ -3841,6 +3846,9 @@ out:
 	rc = devm_add_action_or_reset(&cxlr->dev, shutdown_notifiers, cxlr);
 	if (rc)
 		return rc;
+
+	if (test_bit(CXL_REGION_F_NO_DAX, &cxlr->flags))
+		return 0;
 
 	switch (cxlr->mode) {
 	case CXL_PARTMODE_PMEM:
