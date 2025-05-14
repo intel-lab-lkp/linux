27e9640cf87e44f5e7ffaf11a73edf663791c2b5
Gao Xiang <hsiangkao@linux.alibaba.com>
2025-05-14 19:57:13 +0800

    erofs: refine readahead tracepoint
     - trace_erofs_readpages => trace_erofs_readahead;
    
     - Rename a redundant statement `nrpages = readahead_count(rac);`;
    
     - Move the tracepoint to the beginning of z_erofs_readahead().
    
    Signed-off-by: Gao Xiang <hsiangkao@linux.alibaba.com>

--- include/trace/events/erofs.h
+++ include/trace/events/erofs.h
@@ -113,7 +113,7 @@ TRACE_EVENT(erofs_read_folio,
 		__entry->raw)
 );
 
-TRACE_EVENT(erofs_readpages,
+TRACE_EVENT(erofs_readahead,
 
 	TP_PROTO(struct inode *inode, pgoff_t start, unsigned int nrpage,
 		bool raw),
@@ -138,7 +138,7 @@ TRACE_EVENT(erofs_readpages,
 
 	TP_printk("dev = (%d,%d), nid = %llu, start = %lu nrpage = %u raw = %d",
 		show_dev_nid(__entry),
-		(unsigned long)__entry->start,
+		(unsigned long)__entry->start,`
 		__entry->nrpage,
 		__entry->raw)
 );
