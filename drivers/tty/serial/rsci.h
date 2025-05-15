847f54222a87cf18acacee577eba9a3261f28c12
Thierry Bultel <thierry.bultel.yh@bp.renesas.com>
2025-05-15 16:18:22 +0200

    serial: sh-sci: Add support for RZ/T2H SCI
    Define a new RSCI port type, and the RSCI 32 bits registers set.
    The RZ/T2H SCI has a a fifo, and a quite different set of registers
    from the original SH SCI ones.
    DMA is not supported yet.
    
    Reviewed-by: Wolfram Sang <wsa+renesas@sang-engineering.com>
    Signed-off-by: Thierry Bultel <thierry.bultel.yh@bp.renesas.com>

---
+++ drivers/tty/serial/rsci.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#ifndef __RSCI_H__
+#define __RSCI_H__
+
+#include "sh-sci-common.h"
+
+#ifdef CONFIG_SERIAL_RSCI
+extern struct sci_of_data of_sci_rsci_data;
+#endif
+
+#endif /* __RSCI_H__ */
