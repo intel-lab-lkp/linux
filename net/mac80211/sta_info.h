cebbfa9b02807d334815ee52fe65d9f52e0881a4
Sarika Sharma <quic_sarishar@quicinc.com>
2025-05-15 11:19:00 +0530

    wifi: mac80211: add support to accumulate removed link statistics
    Currently, if a link gets removed in between for a station then
    directly accumulated data will fall down to sum of other active links.
    This will bring inconsistency in station dump statistics.
    
    For instance, let's take Tx packets
    - at t=0-> link-0:2 link-1:3 Tx packets => accumulated = 5
    - at t=1-> link-0:4 link-1:6 Tx packets => accumulated = 10
    let say at t=2, link-0 went down => link-0:0 link-1:7 => accumulated = 7
    Here, suddenly accumulated Tx packets will come down to 7 from 10.
    This is showing inconsistency.
    
    Therefore, store link-0 data when it went down and add to accumulated
    Tx packet = 11.
    
    Hence, store the removed link statistics data in sta structure and
    add it in accumulated statistics for consistency.
    
    Also, initialize accumulatable fields in sinfo structure to 0, to avoid
    any additional packets account for MLO station.
    
    Signed-off-by: Sarika Sharma <quic_sarishar@quicinc.com>

--- net/mac80211/sta_info.h
+++ net/mac80211/sta_info.h
@@ -569,6 +569,35 @@ struct link_sta_info {
 };
 
 /**
+ * struct ieee80211_sta_removed_link_stats - Removed link sta data
+ *
+ * keep required accumulated removed link data for stats
+ *
+ * @rx_packets: accumulated packets (MSDUs & MMPDUs) received from
+ *	this station for removed links
+ * @tx_packets: accumulated packets (MSDUs & MMPDUs) transmitted to
+ *	this station for removed links
+ * @rx_bytes: accumulated bytes (size of MPDUs) received from this
+ *	station for removed links
+ * @tx_bytes: accumulated bytes (size of MPDUs) transmitted to this
+ *	station for removed links
+ * @tx_retries: cumulative retry counts (MPDUs) for removed links
+ * @tx_failed: accumulated number of failed transmissions (MPDUs)
+ *	(retries exceeded, no ACK) for removed links
+ * @rx_dropped_misc: accumulated dropped packets for un-specified reason
+ *	from this station for removed links
+ */
+struct ieee80211_sta_removed_link_stats {
+	u32 rx_packets;
+	u32 tx_packets;
+	u64 rx_bytes;
+	u64 tx_bytes;
+	u32 tx_retries;
+	u32 tx_failed;
+	u32 rx_dropped_misc;
+};
+
+/**
  * struct sta_info - STA information
  *
  * This structure collects information about a station that
@@ -644,6 +673,7 @@ struct link_sta_info {
  *	@deflink address and remaining would be allocated and the address
  *	would be assigned to link[link_id] where link_id is the id assigned
  *	by the AP.
+ * @rem_link_stats: accumulated removed link stats
  */
 struct sta_info {
 	/* General information, mostly static */
@@ -718,6 +748,7 @@ struct sta_info {
 	struct ieee80211_sta_aggregates cur;
 	struct link_sta_info deflink;
 	struct link_sta_info __rcu *link[IEEE80211_MLD_MAX_NUM_LINKS];
+	struct ieee80211_sta_removed_link_stats rem_link_stats;
 
 	/* keep last! */
 	struct ieee80211_sta sta;
@@ -921,6 +952,9 @@ void sta_set_rate_info_tx(struct sta_info *sta,
 			  struct rate_info *rinfo);
 void sta_set_sinfo(struct sta_info *sta, struct station_info *sinfo,
 		   bool tidstats);
+
+void sta_set_accumulated_removed_links_sinfo(struct sta_info *sta,
+					     struct station_info *sinfo);
 
 u32 sta_get_expected_throughput(struct sta_info *sta);
 
