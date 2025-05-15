e29a27c12756fd6ddc1e60bb69481f095371f608
Lyude Paul <lyude@redhat.com>
2025-05-15 17:42:37 -0400

    rust: drm: gem: Simplify use of generics
    Now that my rust skills have been honed, I noticed that there's a lot of
    generics in our gem bindings that don't actually need to be here. Currently
    the hierarchy of traits in our gem bindings looks like this:
    
      * Drivers implement:
        * BaseDriverObject<T: DriverObject> (has the callbacks)
        * DriverObject (has the drm::Driver type)
      * Crate implements:
        * IntoGEMObject for Object<T> where T: DriverObject
          Handles conversion to/from raw object pointers
        * BaseObject for T where T: IntoGEMObject
          Provides methods common to all gem interfaces
    
      Also of note, this leaves us with two different drm::Driver associated
      types:
        * DriverObject::Driver
        * IntoGEMObject::Driver
    
    I'm not entirely sure of the original intent here unfortunately (if anyone
    is, please let me know!), but my guess is that the idea would be that some
    objects can implement IntoGEMObject using a different ::Driver than
    DriverObject - presumably to enable the usage of gem objects from different
    drivers. A reasonable usecase of course.
    
    However - if I'm not mistaken, I don't think that this is actually how
    things would go in practice. Driver implementations are of course
    implemented by their associated drivers, and generally drivers are not
    linked to each-other when building the kernel. Which is to say that even in
    a situation where we would theoretically deal with gem objects from another
    driver, we still wouldn't have access to its drm::driver::Driver
    implementation. It's more likely we would simply want a variant of gem
    objects in such a situation that have no association with a
    drm::driver::Driver type.
    
    Taking that into consideration, we can assume the following:
    * Anything that implements BaseDriverObject will implement DriverObject
      In other words, all BaseDriverObjects indirectly have an associated
      ::Driver type - so the two traits can be combined into one with no
      generics.
    * Not everything that implements IntoGEMObject will have an associated
      ::Driver, and that's OK.
    
    And with this, we now can do quite a bit of cleanup with the use of
    generics here. As such, this commit:
    
    * Removes the generics on BaseDriverObject
    * Moves DriverObject::Driver into BaseDriverObject
    * Removes DriverObject, and renames BaseDriverObject to DriverObject
    * Removes IntoGEMObject::Driver, and require DriverObject be implemented
      for any methods in BaseObject that need an associated driver.
    
    Leaving us with a simpler trait hierarchy that now looks like this:
    
      * Drivers implement: DriverObject
      * Crate implements:
        * IntoGEMObject for Object<T> where T: DriverObject
        * BaseObject for T where T: IntoGEMObject
    
    Which makes the code a lot easier to understand and build on :).
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>

--- rust/kernel/drm/gem/mod.rs
+++ rust/kernel/drm/gem/mod.rs
@@ -15,31 +15,31 @@ use crate::{
 use core::{mem, ops::Deref, ptr::NonNull};
 
 /// GEM object functions, which must be implemented by drivers.
-pub trait BaseDriverObject<T: BaseObject>: Sync + Send + Sized {
+pub trait DriverObject: Sync + Send + Sized {
+    /// Parent `Driver` for this object.
+    type Driver: drm::Driver;
+
     /// Create a new driver data object for a GEM object of a given size.
-    fn new(dev: &drm::Device<T::Driver>, size: usize) -> impl PinInit<Self, Error>;
+    fn new(dev: &drm::Device<Self::Driver>, size: usize) -> impl PinInit<Self, Error>;
 
     /// Open a new handle to an existing object, associated with a File.
     fn open(
-        _obj: &<<T as IntoGEMObject>::Driver as drm::Driver>::Object,
-        _file: &drm::File<<<T as IntoGEMObject>::Driver as drm::Driver>::File>,
+        _obj: &Object<Self>,
+        _file: &drm::File<<Self::Driver as drm::Driver>::File>,
     ) -> Result {
         Ok(())
     }
 
     /// Close a handle to an existing object, associated with a File.
     fn close(
-        _obj: &<<T as IntoGEMObject>::Driver as drm::Driver>::Object,
-        _file: &drm::File<<<T as IntoGEMObject>::Driver as drm::Driver>::File>,
+        _obj: &Object<Self>,
+        _file: &drm::File<<Self::Driver as drm::Driver>::File>,
     ) {
     }
 }
 
 /// Trait that represents a GEM object subtype
 pub trait IntoGEMObject: Sized + super::private::Sealed + AlwaysRefCounted {
-    /// Owning driver for this type
-    type Driver: drm::Driver;
-
     /// Returns a reference to the raw `drm_gem_object` structure, which must be valid as long as
     /// this owning object is valid.
     fn as_raw(&self) -> *mut bindings::drm_gem_object;
@@ -74,25 +74,17 @@ unsafe impl<T: IntoGEMObject> AlwaysRefCounted for T {
     }
 }
 
-/// Trait which must be implemented by drivers using base GEM objects.
-pub trait DriverObject: BaseDriverObject<Object<Self>> {
-    /// Parent `Driver` for this object.
-    type Driver: drm::Driver;
-}
-
-extern "C" fn open_callback<T: BaseDriverObject<U>, U: BaseObject>(
+extern "C" fn open_callback<T: DriverObject>(
     raw_obj: *mut bindings::drm_gem_object,
     raw_file: *mut bindings::drm_file,
 ) -> core::ffi::c_int {
     // SAFETY: `open_callback` is only ever called with a valid pointer to a `struct drm_file`.
     let file = unsafe {
-        drm::File::<<<U as IntoGEMObject>::Driver as drm::Driver>::File>::as_ref(raw_file)
+        drm::File::<<T::Driver as drm::Driver>::File>::as_ref(raw_file)
     };
     // SAFETY: `open_callback` is specified in the AllocOps structure for `Object<T>`, ensuring that
     // `raw_obj` is indeed contained within a `Object<T>`.
-    let obj = unsafe {
-        <<<U as IntoGEMObject>::Driver as drm::Driver>::Object as IntoGEMObject>::as_ref(raw_obj)
-    };
+    let obj = unsafe { Object::<T>::as_ref(raw_obj) };
 
     match T::open(obj, file) {
         Err(e) => e.to_errno(),
@@ -100,26 +92,21 @@ extern "C" fn open_callback<T: BaseDriverObject<U>, U: BaseObject>(
     }
 }
 
-extern "C" fn close_callback<T: BaseDriverObject<U>, U: BaseObject>(
+extern "C" fn close_callback<T: DriverObject>(
     raw_obj: *mut bindings::drm_gem_object,
     raw_file: *mut bindings::drm_file,
 ) {
     // SAFETY: `open_callback` is only ever called with a valid pointer to a `struct drm_file`.
-    let file = unsafe {
-        drm::File::<<<U as IntoGEMObject>::Driver as drm::Driver>::File>::as_ref(raw_file)
-    };
+    let file = unsafe { drm::File::<<T::Driver as drm::Driver>::File>::as_ref(raw_file) };
+
     // SAFETY: `close_callback` is specified in the AllocOps structure for `Object<T>`, ensuring
     // that `raw_obj` is indeed contained within a `Object<T>`.
-    let obj = unsafe {
-        <<<U as IntoGEMObject>::Driver as drm::Driver>::Object as IntoGEMObject>::as_ref(raw_obj)
-    };
+    let obj = unsafe { Object::<T>::as_ref(raw_obj) };
 
     T::close(obj, file);
 }
 
 impl<T: DriverObject> IntoGEMObject for Object<T> {
-    type Driver = T::Driver;
-
     fn as_raw(&self) -> *mut bindings::drm_gem_object {
         self.obj.get()
     }
@@ -143,8 +130,11 @@ pub trait BaseObject: IntoGEMObject {
     /// (or returns an existing one).
     fn create_handle(
         &self,
-        file: &drm::File<<<Self as IntoGEMObject>::Driver as drm::Driver>::File>,
-    ) -> Result<u32> {
+        file: &drm::File<<Self::Driver as drm::Driver>::File>,
+    ) -> Result<u32>
+    where
+        Self: DriverObject
+    {
         let mut handle: u32 = 0;
         // SAFETY: The arguments are all valid per the type invariants.
         to_result(unsafe {
@@ -155,9 +145,12 @@ pub trait BaseObject: IntoGEMObject {
 
     /// Looks up an object by its handle for a given `File`.
     fn lookup_handle(
-        file: &drm::File<<<Self as IntoGEMObject>::Driver as drm::Driver>::File>,
+        file: &drm::File<<Self::Driver as drm::Driver>::File>,
         handle: u32,
-    ) -> Result<ARef<Self>> {
+    ) -> Result<ARef<Self>>
+    where
+        Self: DriverObject
+    {
         // SAFETY: The arguments are all valid per the type invariants.
         let ptr = unsafe { bindings::drm_gem_object_lookup(file.as_raw().cast(), handle) };
         if ptr.is_null() {
@@ -212,8 +205,8 @@ impl<T: DriverObject> Object<T> {
 
     const OBJECT_FUNCS: bindings::drm_gem_object_funcs = bindings::drm_gem_object_funcs {
         free: Some(Self::free_callback),
-        open: Some(open_callback::<T, Object<T>>),
-        close: Some(close_callback::<T, Object<T>>),
+        open: Some(open_callback::<T>),
+        close: Some(close_callback::<T>),
         print_info: None,
         export: None,
         pin: None,
