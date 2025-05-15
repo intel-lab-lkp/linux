91f9cb6eb96be4ee0e7959914cbc8bccc8f25216
Baoquan He <bhe@redhat.com>
2025-05-16 07:39:53 +0800

    ima: add a knob ima= to make IMA be able to be disabled
    Kdump kernel doesn't need IMA functionality, and enabling IMA will cost
    extra memory. It would be very helpful to allow IMA to be disabled for
    kdump kernel.
    
    And Coiby also mentioned that for kdump kernel incorrect ima-policy loaded
    by systemd could cause kdump kernel hang, and it's possible the booting
    process may be stopped by a strict, albeit syntax-correct policy and users
    can't log into the system to fix the policy. In these cases, allowing to
    disable IMA is very helpful too for kdump kernel.
    
    Hence add a knob ima=on|off here to allow people to disable IMA in kdump
    kenrel if needed.
    
    Signed-off-by: Baoquan He <bhe@redhat.com>

--- security/integrity/ima/ima_main.c
+++ security/integrity/ima/ima_main.c
@@ -27,6 +27,7 @@
 #include <linux/fs.h>
 #include <linux/iversion.h>
 #include <linux/evm.h>
+#include <linux/crash_dump.h>
 
 #include "ima.h"
 
@@ -38,10 +39,26 @@ int ima_appraise;
 
 int __ro_after_init ima_hash_algo = HASH_ALGO_SHA1;
 static int hash_setup_done;
+static int ima_disabled;
 
 static struct notifier_block ima_lsm_policy_notifier = {
 	.notifier_call = ima_lsm_policy_change,
 };
+
+static int __init ima_setup(char *str)
+{
+	if (strncmp(str, "off", 3) == 0)
+		ima_disabled = 1;
+	else if (strncmp(str, "on", 2) == 0)
+		ima_disabled = 0;
+	else
+		pr_err("Invalid ima setup option: \"%s\" , please specify ima=on|off.", str);
+
+	return 1;
+}
+__setup("ima=", ima_setup);
+
+
 
 static int __init hash_setup(char *str)
 {
@@ -1185,6 +1202,11 @@ static int ima_kernel_module_request(char *kmod_name)
 static int __init init_ima(void)
 {
 	int error;
+
+	if (ima_disabled && is_kdump_kernel()) {
+		pr_info("IMA functionality is disabled");
+		return 0;
+	}
 
 	ima_appraise_parse_cmdline();
 	ima_init_template_list();
