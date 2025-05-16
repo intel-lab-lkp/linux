6b9c0e1a58dd11eecd7735e1c9ac5e7a44aab239
Rik van Riel <riel@surriel.com>
2025-05-16 12:33:17 -0400

    x86/mm: resize user_pcid_flush_mask for PTI / broadcast TLB flush combination
    Currently no system with AMD INVLPGB support requires the page table
    isolation mitigation. However, people could still enable PTI manually,
    or a vulnerability could be found in the future that makes PTI useful
    on certain AMD CPUs.
    
    Additionally, there are systems that support Intel RAR TLB invalidation,
    where PTI is a useful mitigation.
    
    The combination of PTI and broadcast TLB flush has a problem:
    - invalidate_user_asid() sets a bit corresponding to the process PCID
    - SWITCH_TO_USER_CR3 tests and clears a bit corresponding to the process PCID
    
    However, the PCIDs no longer fit in the range 0-6 when using global ASIDs.
    
    Enlarge user_pcid_flush_mask to fit the PCID numbers that can be present when
    using broadcast TLB flushing. This takes up 256 or 512 bytes per CPU, depending
    on whether or not page table isolation is built into the kernel.
    
    Signed-off-by: Rik van Riel <riel@surriel.com>
    Fixes: c3ed3f5b2550 x86/mm: userspace & pageout flushing using Intel RAR
    Cc: stable@kernel.org

--- arch/x86/include/asm/tlbflush.h
+++ arch/x86/include/asm/tlbflush.h
@@ -23,6 +23,25 @@ void __flush_tlb_all(void);
 #define TLB_FLUSH_ALL	-1UL
 #define TLB_GENERATION_INVALID	0
 
+/*
+ * When enabled, MITIGATION_PAGE_TABLE_ISOLATION consumes a single bit for
+ * user/kernel switches
+ */
+#ifdef CONFIG_MITIGATION_PAGE_TABLE_ISOLATION
+# define PTI_CONSUMED_PCID_BITS	1
+#else
+# define PTI_CONSUMED_PCID_BITS	0
+#endif
+
+#define CR3_AVAIL_PCID_BITS (X86_CR3_PCID_BITS - PTI_CONSUMED_PCID_BITS)
+
+/*
+ * ASIDs are zero-based: 0->MAX_AVAIL_ASID are valid.  -1 below to account
+ * for them being zero-based.  Another -1 is because PCID 0 is reserved for
+ * use by non-PCID-aware users.
+ */
+#define MAX_ASID_AVAILABLE ((1 << CR3_AVAIL_PCID_BITS) - 2)
+
 void cr4_update_irqsoff(unsigned long set, unsigned long clear);
 unsigned long cr4_read_shadow(void);
 
@@ -121,7 +140,11 @@ struct tlb_state {
 	 * the corresponding user PCID needs a flush next time we
 	 * switch to it; see SWITCH_TO_USER_CR3.
 	 */
+#if defined(CONFIG_X86_TLB_BROADCAST_TLB_FLUSH) && defined(CONFIG_MITIGATION_PAGE_TABLE_ISOLATION)
+	unsigned long user_pcid_flush_mask[(1 << CR3_AVAIL_PCID_BITS) / BITS_PER_LONG];
+#else
 	unsigned short user_pcid_flush_mask;
+#endif
 
 	/*
 	 * Access to this CR4 shadow and to H/W CR4 is protected by
--- arch/x86/mm/tlb.c
+++ arch/x86/mm/tlb.c
@@ -91,25 +91,6 @@
  */
 
 /*
- * When enabled, MITIGATION_PAGE_TABLE_ISOLATION consumes a single bit for
- * user/kernel switches
- */
-#ifdef CONFIG_MITIGATION_PAGE_TABLE_ISOLATION
-# define PTI_CONSUMED_PCID_BITS	1
-#else
-# define PTI_CONSUMED_PCID_BITS	0
-#endif
-
-#define CR3_AVAIL_PCID_BITS (X86_CR3_PCID_BITS - PTI_CONSUMED_PCID_BITS)
-
-/*
- * ASIDs are zero-based: 0->MAX_AVAIL_ASID are valid.  -1 below to account
- * for them being zero-based.  Another -1 is because PCID 0 is reserved for
- * use by non-PCID-aware users.
- */
-#define MAX_ASID_AVAILABLE ((1 << CR3_AVAIL_PCID_BITS) - 2)
-
-/*
  * Given @asid, compute kPCID
  */
 static inline u16 kern_pcid(u16 asid)
