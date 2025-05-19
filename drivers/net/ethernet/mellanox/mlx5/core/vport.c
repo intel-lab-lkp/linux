75f4e1170f104d3bc6ae1c1a7c254173be7473d7
Wentao Liang <vulab@iscas.ac.cn>
2025-05-19 11:40:43 +0800

    net: mlx5: vport: Add error handling in mlx5_query_nic_vport_qkey_viol_cntr()
    The function mlx5_query_nic_vport_qkey_viol_cntr() calls the functuion
    mlx5_query_nic_vport_context() but does not check its return value. This
    could lead to undefined behavior if the query fails. A proper
    implementation can be found in mlx5_nic_vport_query_local_lb().
    
    Add error handling for mlx5_query_nic_vport_context(). If it fails, free
    the out buffer via kvfree() and return error code.
    
    Fixes: 9efa75254593 ("net/mlx5_core: Introduce access functions to query vport RoCE fields")
    Cc: stable@vger.kernel.org # v4.5
    Signed-off-by: Wentao Liang <vulab@iscas.ac.cn>

--- drivers/net/ethernet/mellanox/mlx5/core/vport.c
+++ drivers/net/ethernet/mellanox/mlx5/core/vport.c
@@ -518,20 +518,23 @@ int mlx5_query_nic_vport_qkey_viol_cntr(struct mlx5_core_dev *mdev,
 					u16 *qkey_viol_cntr)
 {
 	u32 *out;
-	int outlen = MLX5_ST_SZ_BYTES(query_nic_vport_context_out);
+	int ret, outlen = MLX5_ST_SZ_BYTES(query_nic_vport_context_out);
 
 	out = kvzalloc(outlen, GFP_KERNEL);
 	if (!out)
 		return -ENOMEM;
 
-	mlx5_query_nic_vport_context(mdev, 0, out);
+	ret = mlx5_query_nic_vport_context(mdev, 0, out);
+	if (ret)
+		goto out;
 
 	*qkey_viol_cntr = MLX5_GET(query_nic_vport_context_out, out,
 				   nic_vport_context.qkey_violation_counter);
-
+	ret = 0;
+out:
 	kvfree(out);
 
-	return 0;
+	return ret;
 }
 EXPORT_SYMBOL_GPL(mlx5_query_nic_vport_qkey_viol_cntr);
 
