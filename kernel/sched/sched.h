c9a8c2a3bd5c4902dc5ade774462d7a1c6f7418d
Peter Zijlstra <peterz@infradead.org>
2025-05-20 11:19:43 +0200

    sched: Add ttwu_queue controls
    There are two (soon three) callers of ttwu_queue_wakelist(),
    distinguish them with their own WF_ and add some knobs on.
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2313,6 +2313,8 @@ static inline int task_on_rq_migrating(struct task_struct *p)
 #define WF_CURRENT_CPU		0x40 /* Prefer to move the wakee to the current CPU. */
 #define WF_RQ_SELECTED		0x80 /* ->select_task_rq() was called */
 
+#define WF_ON_CPU		0x0100
+
 #ifdef CONFIG_SMP
 static_assert(WF_EXEC == SD_BALANCE_EXEC);
 static_assert(WF_FORK == SD_BALANCE_FORK);
