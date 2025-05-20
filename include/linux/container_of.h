c796aa0593602a00b6878490805326d016c261e9
Sakari Ailus <sakari.ailus@linux.intel.com>
2025-05-20 13:34:37 +0300

    container_of: Document container_of() is not to be used in new code
    There is a warning in the kerneldoc documentation of container_of() that
    constness of its ptr argument is lost. While this is a faible suggestion
    container_of_const() should be used instead, the vast majority of new code
    still uses container_of():
    
    $ git diff v6.13 v6.14|grep container_of\(|wc -l
    646
    $ git diff v6.13 v6.14|grep container_of_const|wc -l
    9
    
    Make an explicit recommendation to use container_of_const().
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- include/linux/container_of.h
+++ include/linux/container_of.h
@@ -13,7 +13,8 @@
  * @type:	the type of the container struct this is embedded in.
  * @member:	the name of the member within the struct.
  *
- * WARNING: any const qualifier of @ptr is lost.
+ * WARNING: any const qualifier of @ptr is lost. DO NOT USE container_of() IN
+ * NEW CODE.
  */
 #define container_of(ptr, type, member) ({				\
 	void *__mptr = (void *)(ptr);					\
@@ -28,6 +29,8 @@
  * @ptr:		the pointer to the member
  * @type:		the type of the container struct this is embedded in.
  * @member:		the name of the member within the struct.
+ *
+ * Always prefer container_of_const() over container_of() in new code.
  */
 #define container_of_const(ptr, type, member)				\
 	_Generic(ptr,							\
