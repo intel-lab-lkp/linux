26eb28f92874c2b56c83cfe7c596e5aaf420ccec
Vegard Nossum <vegard.nossum@oracle.com>
2025-05-21 14:55:19 +0200

    crypto/testmgr.c: desupport SHA-1 for FIPS 140
    The sunset period of SHA-1 is approaching [1] and FIPS 140 certificates
    have a validity of 5 years. Any distros starting FIPS certification for
    their kernels now would therefore most likely end up on the NIST
    Cryptographic Module Validation Program "historical" list before their
    certification expires.
    
    While SHA-1 is technically still allowed until Dec. 31, 2030, it is
    heavily discouraged by NIST and it makes sense to set .fips_allowed to
    0 now for any crypto algorithms that reference it in order to avoid any
    costly surprises down the line.
    
    [1]: https://www.nist.gov/news-events/news/2022/12/nist-retires-sha-1-cryptographic-algorithm
    
    Acked-by: Stephan Mueller <smueller@chronox.de>
    Cc: Marcus Meissner <meissner@suse.de>
    Cc: Jarod Wilson <jarod@redhat.com>
    Cc: Neil Horman <nhorman@tuxdriver.com>
    Cc: John Haxby <john.haxby@oracle.com>
    Signed-off-by: Vegard Nossum <vegard.nossum@oracle.com>

--- crypto/testmgr.c
+++ crypto/testmgr.c
@@ -4306,7 +4306,6 @@ static const struct alg_test_desc alg_test_descs[] = {
 	}, {
 		.alg = "authenc(hmac(sha1),cbc(aes))",
 		.test = alg_test_aead,
-		.fips_allowed = 1,
 		.suite = {
 			.aead = __VECS(hmac_sha1_aes_cbc_tv_temp)
 		}
@@ -4325,7 +4324,6 @@ static const struct alg_test_desc alg_test_descs[] = {
 	}, {
 		.alg = "authenc(hmac(sha1),ctr(aes))",
 		.test = alg_test_null,
-		.fips_allowed = 1,
 	}, {
 		.alg = "authenc(hmac(sha1),ecb(cipher_null))",
 		.test = alg_test_aead,
@@ -4335,7 +4333,6 @@ static const struct alg_test_desc alg_test_descs[] = {
 	}, {
 		.alg = "authenc(hmac(sha1),rfc3686(ctr(aes)))",
 		.test = alg_test_null,
-		.fips_allowed = 1,
 	}, {
 		.alg = "authenc(hmac(sha224),cbc(des))",
 		.test = alg_test_aead,
@@ -5177,7 +5174,6 @@ static const struct alg_test_desc alg_test_descs[] = {
 	}, {
 		.alg = "hmac(sha1)",
 		.test = alg_test_hash,
-		.fips_allowed = 1,
 		.suite = {
 			.hash = __VECS(hmac_sha1_tv_template)
 		}
@@ -5513,7 +5509,6 @@ static const struct alg_test_desc alg_test_descs[] = {
 	}, {
 		.alg = "sha1",
 		.test = alg_test_hash,
-		.fips_allowed = 1,
 		.suite = {
 			.hash = __VECS(sha1_tv_template)
 		}
