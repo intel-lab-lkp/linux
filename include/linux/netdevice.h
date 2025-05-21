d127b0cc142f2de13018f2a6003717fd547b1cf7
Yajun Deng <yajun.deng@linux.dev>
2025-05-21 22:08:24 +0800

    net: sysfs: Implement is_visible for phys_(port_id, port_name, switch_id)
    phys_port_id_show, phys_port_name_show and phys_switch_id_show would
    return -EOPNOTSUPP if the netdev didn't implement the corresponding
    method.
    
    There is no point in creating these files if they are unsupported.
    
    Put these attributes in netdev_phys_group and implement the is_visible
    method. make phys_(port_id, port_name, switch_id) invisible if the netdev
    dosen't implement the corresponding method.
    
    Signed-off-by: Yajun Deng <yajun.deng@linux.dev>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -2384,7 +2384,7 @@ struct net_device {
 	struct dm_hw_stat_delta __rcu *dm_private;
 #endif
 	struct device		dev;
-	const struct attribute_group *sysfs_groups[4];
+	const struct attribute_group *sysfs_groups[5];
 	const struct attribute_group *sysfs_rx_queue_group;
 
 	const struct rtnl_link_ops *rtnl_link_ops;
