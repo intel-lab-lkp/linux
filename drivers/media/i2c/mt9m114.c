7eb53e4cb41a4aeb98588ae6240a9e1995a5c593
Mathis Foerst <mathis.foerst@mt.com>
2025-05-22 16:35:09 +0200

    media: mt9m114: Allow set_selection while streaming
    The current implementation does not apply changes to the crop-
    configuration of the sensor immediately if the sensor is in
    streaming state. The user has to stop and restart the stream for
    the changes to be applied.
    This can be undesirable e.g. in a calibration usecase where the user
    wants to see the impact of his changes in a live video stream.
    Under the condition that the width & height of the cropped image area
    does not change, the changed cropping configuration can be applied to
    the pixel-array immediately without disturbing the IFP.
    
    Call mt9m114_configure_pa() in mt9m114_pa_set_selection() if the sensor is
    in streaming state and the size of the cropping rectangle didn't change,
    issue a CONFIG_CHANGE to apply the changes immediately.
    
    Signed-off-by: Mathis Foerst <mathis.foerst@mt.com>

--- drivers/media/i2c/mt9m114.c
+++ drivers/media/i2c/mt9m114.c
@@ -1301,11 +1301,14 @@ static int mt9m114_pa_set_selection(struct v4l2_subdev *sd,
 	struct mt9m114 *sensor = pa_to_mt9m114(sd);
 	struct v4l2_mbus_framefmt *format;
 	struct v4l2_rect *crop;
+	struct v4l2_rect old_crop;
+	int ret = 0;
 
 	if (sel->target != V4L2_SEL_TGT_CROP)
 		return -EINVAL;
 
 	crop = v4l2_subdev_state_get_crop(state, sel->pad);
+	old_crop = *crop;
 	format = v4l2_subdev_state_get_format(state, sel->pad);
 
 	/*
@@ -1331,10 +1334,25 @@ static int mt9m114_pa_set_selection(struct v4l2_subdev *sd,
 	format->width = crop->width;
 	format->height = crop->height;
 
-	if (sel->which == V4L2_SUBDEV_FORMAT_ACTIVE)
-		mt9m114_pa_ctrl_update_blanking(sensor, format);
+	if (sel->which != V4L2_SUBDEV_FORMAT_ACTIVE)
+		return ret;
 
-	return 0;
+	mt9m114_pa_ctrl_update_blanking(sensor, format);
+
+	/*
+	 * Apply values immediately if streaming and the selection size did not
+	 * change.
+	 */
+	if (sensor->streaming && old_crop.height == crop->height &&
+	    old_crop.width == crop->width) {
+		ret = mt9m114_configure_pa(sensor, state);
+		if (ret)
+			return ret;
+		// Changing the cropping config requires a CONFIG_CHANGE
+		ret = mt9m114_set_state(sensor,	MT9M114_SYS_STATE_ENTER_CONFIG_CHANGE);
+	}
+
+	return ret;
 }
 
 static const struct v4l2_subdev_pad_ops mt9m114_pa_pad_ops = {
