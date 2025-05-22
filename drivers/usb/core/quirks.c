bf81f179045b4a673db60652e91d633ae4deda5d
Kampalus <kampalus@protonmail.ch>
2025-05-22 20:11:07 +0000

    usb: quirks: disable LPM for Xiaomi fastboot
    Both Xiaomi smartphones and AMD Ryzen systems have sketchy USB setups. When combined, fastboot
    fails to read and write data properly. Disabling Link Power Management works around that.
    
    Despite only being useful for certain setups, let's enable this globally as it does
    not hurt healthy setups.
    
    Signed-off-by: Kampalus <kampalus@protonmail.ch>

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -577,6 +577,9 @@ static const struct usb_device_id usb_quirk_list[] = {
 	/* INTEL VALUE SSD */
 	{ USB_DEVICE(0x8086, 0xf1a5), .driver_info = USB_QUIRK_RESET_RESUME },
 
+	/* Xiaomi smartphones in fastboot mode */
+	{ USB_DEVICE(0x18d1, 0xd00d), .driver_info = USB_QUIRK_NO_LPM },
+
 	{ }  /* terminating entry must be last */
 };
 
