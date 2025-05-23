e2fc929502a7887b18b965ec102c2c0a790fabc7
Ulf Hansson <ulf.hansson@linaro.org>
2025-05-23 15:40:14 +0200

    pmdomain: core: Default to use of_genpd_sync_state() for genpd providers
    Unless the typical platform driver that act as genpd provider, has its own
    ->sync_state() callback implemented let's default to use
    of_genpd_sync_state().
    
    More precisely, while adding a genpd OF provider let's assign the
    ->sync_state() callback, in case the fwnode has a device and its driver
    doesn't have the ->sync_state() set already. In this way the typical
    platform driver doesn't need to assign ->sync_state(), unless it has some
    additional things to manage beyond genpds.
    
    Suggested-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>

--- drivers/pmdomain/core.c
+++ drivers/pmdomain/core.c
@@ -2621,6 +2621,11 @@ static bool genpd_present(const struct generic_pm_domain *genpd)
 	return ret;
 }
 
+static void genpd_sync_state(struct device *dev)
+{
+	return of_genpd_sync_state(dev->of_node);
+}
+
 /**
  * of_genpd_add_provider_simple() - Register a simple PM domain provider
  * @np: Device node pointer associated with the PM domain provider.
@@ -2646,6 +2651,8 @@ int of_genpd_add_provider_simple(struct device_node *np,
 	if (!dev && !genpd_is_no_sync_state(genpd)) {
 		genpd->sync_state = GENPD_SYNC_STATE_SIMPLE;
 		device_set_node(&genpd->dev, fwnode);
+	} else {
+		dev_set_drv_sync_state(dev, genpd_sync_state);
 	}
 
 	put_device(dev);
@@ -2715,6 +2722,8 @@ int of_genpd_add_provider_onecell(struct device_node *np,
 	dev = get_dev_from_fwnode(fwnode);
 	if (!dev)
 		sync_state = true;
+	else
+		dev_set_drv_sync_state(dev, genpd_sync_state);
 
 	put_device(dev);
 
