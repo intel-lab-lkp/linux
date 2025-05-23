06f5ae1114c8b79b33c66a58cc65d1980a6e2c47
Joshua Hay <joshua.a.hay@intel.com>
2025-05-23 12:04:32 -0500

    idpf: implement RDMA vport auxiliary dev create, init, and destroy
    Implement the functions to create, initialize, and destroy an RDMA vport
    auxiliary device. The vport aux dev creation is dependent on the
    core aux device to call idpf_idc_vport_dev_ctrl to signal that it is
    ready for vport aux devices. Implement that core callback to either
    create and initialize the vport aux dev or deinitialize.
    
    Rdma vport aux dev creation is also dependent on the control plane to
    tell us the vport is RDMA enabled. Add a flag in the create vport
    message to signal individual vport RDMA capabilities.
    
    Reviewed-by: Madhu Chittim <madhu.chittim@intel.com>
    Signed-off-by: Joshua Hay <joshua.a.hay@intel.com>
    Signed-off-by: Tatyana Nikolova <tatyana.e.nikolova@intel.com>

--- drivers/net/ethernet/intel/idpf/virtchnl2.h
+++ drivers/net/ethernet/intel/idpf/virtchnl2.h
@@ -563,6 +563,15 @@ struct virtchnl2_queue_reg_chunks {
 VIRTCHNL2_CHECK_STRUCT_LEN(8, virtchnl2_queue_reg_chunks);
 
 /**
+ * enum virtchnl2_vport_flags - Vport flags
+ * @VIRTCHNL2_VPORT_ENABLE_RDMA: RDMA is enabled for this vport
+ */
+enum virtchnl2_vport_flags {
+	/* VIRTCHNL2_VPORT_* bits [0:3] rsvd */
+	VIRTCHNL2_VPORT_ENABLE_RDMA             = BIT(4),
+};
+
+/**
  * struct virtchnl2_create_vport - Create vport config info.
  * @vport_type: See enum virtchnl2_vport_type.
  * @txq_model: See virtchnl2_queue_model.
@@ -580,7 +589,7 @@ VIRTCHNL2_CHECK_STRUCT_LEN(8, virtchnl2_queue_reg_chunks);
  * @max_mtu: Max MTU. CP populates this field on response.
  * @vport_id: Vport id. CP populates this field on response.
  * @default_mac_addr: Default MAC address.
- * @pad: Padding.
+ * @vport_flags: See enum virtchnl2_vport_flags
  * @rx_desc_ids: See VIRTCHNL2_RX_DESC_IDS definitions.
  * @tx_desc_ids: See VIRTCHNL2_TX_DESC_IDS definitions.
  * @pad1: Padding.
@@ -613,7 +622,7 @@ struct virtchnl2_create_vport {
 	__le16 max_mtu;
 	__le32 vport_id;
 	u8 default_mac_addr[ETH_ALEN];
-	__le16 pad;
+	__le16 vport_flags;
 	__le64 rx_desc_ids;
 	__le64 tx_desc_ids;
 	u8 pad1[72];
