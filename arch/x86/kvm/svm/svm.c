a1a263ef362f45c19cd2519de528ae8df5c2405f
Maxim Levitsky <mlevitsk@redhat.com>
2025-05-22 17:59:20 -0700

    KVM: SVM: Add enable_ipiv param, never set IsRunning if disabled
    Let userspace "disable" IPI virtualization for AVIC via the enable_ipiv
    module param, by never setting IsRunning.  SVM doesn't provide a way to
    disable IPI virtualization in hardware, but by ensuring CPUs never see
    IsRunning=1, every IPI in the guest (except for self-IPIs) will generate a
    VM-Exit.
    
    To avoid setting the real IsRunning bit, while still allowing KVM to use
    each vCPU's entry to update GA log entries, simply maintain a shadow of
    the entry, without propagating IsRunning updates to the real table when
    IPI virtualization is disabled.
    
    Providing a way to effectively disable IPI virtualization will allow KVM
    to safely enable AVIC on hardware that is susceptible to erratum #1235,
    which causes hardware to sometimes fail to detect that the IsRunning bit
    has been cleared by software.
    
    Note, the table _must_ be fully populated, as broadcast IPIs skip invalid
    entries, i.e. won't generate VM-Exit if every entry is invalid, and so
    simply pointing the VMCB at a common dummy table won't work.
    
    Alternatively, KVM could allocate a shadow of the entire table, but that'd
    be a waste of 4KiB since the per-vCPU entry doesn't actually consume an
    additional 8 bytes of memory (vCPU structures are large enough that they
    are backed by order-N pages).
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>
    [sean: keep "entry" variables, reuse enable_ipiv, split from erratum]
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -231,6 +231,7 @@ module_param(tsc_scaling, int, 0444);
  */
 static bool avic;
 module_param(avic, bool, 0444);
+module_param(enable_ipiv, bool, 0444);
 
 module_param(enable_device_posted_irqs, bool, 0444);
 
@@ -5594,6 +5595,7 @@ static __init int svm_hardware_setup(void)
 	enable_apicv = avic = avic && avic_hardware_setup();
 
 	if (!enable_apicv) {
+		enable_ipiv = false;
 		svm_x86_ops.vcpu_blocking = NULL;
 		svm_x86_ops.vcpu_unblocking = NULL;
 		svm_x86_ops.vcpu_get_apicv_inhibit_reasons = NULL;
