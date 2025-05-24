e7f556867d1e28b41615464b5901adb1ac5830a5
Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>
2025-05-25 00:23:03 +0530

    PCI: Save and restore root port config space in pcibios_reset_secondary_bus()
    host_bridge::reset_slot() is supposed to reset the PCI root port/slot. Once
    that happens, the config space content would be lost. This was reported by
    Niklas on the dw-rockchip based platform where the MPS setting of the root
    port was lost after the host_bridge::reset_slot() callback. Hence, save the
    config space before calling the host_bridge::reset_slot() callback and
    restore it afterwards.
    
    While at it, make sure that the callback is only called for root ports by
    checking if the bridge is behind the root bus.
    
    Fixes: d5c1e1c25b37 ("PCI/ERR: Add support for resetting the slots in a platform specific way")
    Reported-by: Niklas Cassel <cassel@kernel.org>
    Closes: https://lore.kernel.org/linux-pci/aC9OrPAfpzB_A4K2@ryzen
    Suggested-by: Lukas Wunner <lukas@wunner.de>
    Signed-off-by: Manivannan Sadhasivam <manivannan.sadhasivam@linaro.org>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -4967,10 +4967,19 @@ void __weak pcibios_reset_secondary_bus(struct pci_dev *dev)
 	struct pci_host_bridge *host = pci_find_host_bridge(dev->bus);
 	int ret;
 
-	if (host->reset_slot) {
+	if (pci_is_root_bus(dev->bus) && host->reset_slot) {
+		/*
+		 * Save the config space of the root port before doing the
+		 * reset, since the state could be lost. The device state
+		 * should've been saved by the caller.
+		 */
+		pci_save_state(dev);
 		ret = host->reset_slot(host, dev);
 		if (ret)
 			pci_err(dev, "failed to reset slot: %d\n", ret);
+		else
+			/* Now restore it on success */
+			pci_restore_state(dev);
 
 		return;
 	}
