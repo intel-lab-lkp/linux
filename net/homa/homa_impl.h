90d499b55bb67b3babc809512184c26922ce2ee3
John Ousterhout <ouster@cs.stanford.edu>
2025-05-25 21:28:10 -0700

    net: homa: create homa_pacer.h and homa_pacer.c
    These files provide facilities to pace packet output in order to prevent
    queue buildup in the NIC. This functionality is needed to implement SRPT
    on output, so short messages don't get stuck in long NIC queues. Note: the
    pacer eventually needs to be replaced with a Homa-specific qdisc, which can
    better manage simultaneous transmissions by Homa and TCP. The current
    implementation can coexist with TCP and doesn't harm TCP, but
    Homa's latency suffers when TCP runs concurrently.
    
    Signed-off-by: John Ousterhout <ouster@cs.stanford.edu>

--- net/homa/homa_impl.h
+++ net/homa/homa_impl.h
@@ -387,6 +387,7 @@ extern unsigned int homa_net_id;
 
 int      homa_xmit_control(enum homa_packet_type type, void *contents,
 			   size_t length, struct homa_rpc *rpc);
+void     homa_xmit_data(struct homa_rpc *rpc, bool force);
 
 /**
  * homa_net_from_net() - Return the struct homa_net associated with a particular
