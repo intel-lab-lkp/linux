5e5c6e53e4f2c3b73bd3ba6d134fb93cb459da8a
Jony Lynn <1198715581lxf@gmail.com>
2025-05-27 00:08:42 +0800

    sched/smp: Fix redefinition of flush_smp_call_function_queue()
    The header file kernel/sched/smp.h currently defines
    flush_smp_call_function_queue() unconditionally in the #else branch,
    but lacks appropriate header guards, which causes multiple inclusion
    and a redefinition error during compilation.
    
    This patch adds a proper header guard to prevent multiple inclusion of
    kernel/sched/smp.h and thus avoid the redefinition of
    flush_smp_call_function_queue() when included multiple times in
    different compilation units.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202505262213.QsOaQU24-lkp@intel.com/
    
    Signed-off-by: Jony Lynn <1198715581lxf@gmail.com>

--- kernel/sched/smp.h
+++ kernel/sched/smp.h
@@ -4,6 +4,9 @@
  * and other internal parts of the core kernel:
  */
 
+#ifndef _KERNEL_SCHED_SMP_H
+#define _KERNEL_SCHED_SMP_H
+
 extern void sched_ttwu_pending(void *arg);
 
 extern bool call_function_single_prep_ipi(int cpu);
@@ -12,4 +15,6 @@ extern bool call_function_single_prep_ipi(int cpu);
 extern void flush_smp_call_function_queue(void);
 #else
 static inline void flush_smp_call_function_queue(void) { }
+#endif
+
 #endif
