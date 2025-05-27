12b51f4b8f356c9715e156c97f566fdac8763530
Himal Prasad Ghimiray <himal.prasad.ghimiray@intel.com>
2025-05-27 22:09:58 +0530

    drm/xe/svm: Support DRM_XE_SVM_ATTR_PAT memory attribute
    This attributes sets the pat_index for the svm used vma range, which is
    utilized to ascertain the coherence.
    
    Signed-off-by: Himal Prasad Ghimiray <himal.prasad.ghimiray@intel.com>

--- drivers/gpu/drm/xe/xe_vm_madvise.c
+++ drivers/gpu/drm/xe/xe_vm_madvise.c
@@ -98,7 +98,13 @@ static int madvise_pat_index(struct xe_device *xe, struct xe_vm *vm,
 			     struct xe_vma **vmas, int num_vmas,
 			     struct drm_xe_madvise_ops ops)
 {
-	/* Implementation pending */
+	int i;
+
+	xe_assert(vm->xe, ops.type == DRM_XE_VMA_ATTR_PAT);
+
+	for (i = 0; i < num_vmas; i++)
+		vmas[i]->attr.pat_index = ops.pat_index.val;
+
 	return 0;
 }
 
