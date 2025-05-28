44db64e3709b004c48583c3cecf72d4727359964
Max Staudt <max@enpas.org>
2025-05-28 22:28:15 +0900

    tty: Register device *after* creating the cdev for a tty
    This change makes the tty device file available only after the tty's
    backing character device is ready.
    
    Since 6a7e6f78c235975cc14d4e141fa088afffe7062c, the class device is
    registered before the cdev is created, and userspace may pick it up,
    yet open() will fail because the backing cdev doesn't exist yet.
    Userspace is racing the bottom half of tty_register_device_attr() here,
    specifically the call to tty_cdev_add().
    
    dev_set_uevent_suppress() was used to work around this, but this fails
    on embedded systems that rely on bare devtmpfs rather than udev.
    On such systems, the device file is created as part of device_add(),
    and userspace can pick it up via inotify, irrespective of uevent
    suppression.
    
    So let's undo the existing patch, and create the cdev first, and only
    afterwards register the class device in the kernel's device tree.
    
    However, this restores the original race of the cdev existing before the
    class device is registered, and an attempt to tty_[k]open() the chardev
    between these two steps will lead to tty->dev being assigned NULL by
    alloc_tty_struct().
    
    This will be addressed in a second patch.
    
    Fixes: 6a7e6f78c235 ("tty: close race between device register and open")
    Signed-off-by: Max Staudt <max@enpas.org>
    Cc: <stable@vger.kernel.org>

--- drivers/tty/tty_io.c
+++ drivers/tty/tty_io.c
@@ -3232,6 +3232,7 @@ struct device *tty_register_device_attr(struct tty_driver *driver,
 	struct ktermios *tp;
 	struct device *dev;
 	int retval;
+	bool cdev_added = false;
 
 	if (index >= driver->num) {
 		pr_err("%s: Attempt to register invalid tty line number (%d)\n",
@@ -3243,24 +3244,6 @@ struct device *tty_register_device_attr(struct tty_driver *driver,
 		pty_line_name(driver, index, name);
 	else
 		tty_line_name(driver, index, name);
-
-	dev = kzalloc(sizeof(*dev), GFP_KERNEL);
-	if (!dev)
-		return ERR_PTR(-ENOMEM);
-
-	dev->devt = devt;
-	dev->class = &tty_class;
-	dev->parent = device;
-	dev->release = tty_device_create_release;
-	dev_set_name(dev, "%s", name);
-	dev->groups = attr_grp;
-	dev_set_drvdata(dev, drvdata);
-
-	dev_set_uevent_suppress(dev, 1);
-
-	retval = device_register(dev);
-	if (retval)
-		goto err_put;
 
 	if (!(driver->flags & TTY_DRIVER_DYNAMIC_ALLOC)) {
 		/*
@@ -3275,18 +3258,43 @@ struct device *tty_register_device_attr(struct tty_driver *driver,
 
 		retval = tty_cdev_add(driver, devt, index, 1);
 		if (retval)
-			goto err_del;
+			return ERR_PTR(retval);
+
+		cdev_added = true;
 	}
 
-	dev_set_uevent_suppress(dev, 0);
-	kobject_uevent(&dev->kobj, KOBJ_ADD);
+	dev = kzalloc(sizeof(*dev), GFP_KERNEL);
+	if (!dev) {
+		retval = -ENOMEM;
+		goto err_del_cdev;
+	}
+
+	dev->devt = devt;
+	dev->class = &tty_class;
+	dev->parent = device;
+	dev->release = tty_device_create_release;
+	dev_set_name(dev, "%s", name);
+	dev->groups = attr_grp;
+	dev_set_drvdata(dev, drvdata);
+
+	retval = device_register(dev);
+	if (retval)
+		goto err_put;
 
 	return dev;
 
-err_del:
-	device_del(dev);
 err_put:
+	/*
+	 * device_register() calls device_add(), after which
+	 * we must use put_device() instead of kfree().
+	 */
 	put_device(dev);
+
+err_del_cdev:
+	if (cdev_added) {
+		cdev_del(driver->cdevs[index]);
+		driver->cdevs[index] = NULL;
+	}
 
 	return ERR_PTR(retval);
 }
