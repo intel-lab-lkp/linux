8fefe4beca81fe3bd041098b477b0ebc41cfa06f
Sean Christopherson <seanjc@google.com>
2025-05-29 16:39:54 -0700

    KVM: nSVM: Omit SEV-ES specific passthrough MSRs from L0+L1 bitmap merge
    Don't merge bitmaps on nested VMRUN for MSRs that KVM passes through only
    for SEV-ES guests.  KVM doesn't support nested virtualization for SEV-ES,
    and likely never will.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/svm/nested.c
+++ arch/x86/kvm/svm/nested.c
@@ -184,7 +184,7 @@ void recalc_intercepts(struct vcpu_svm *svm)
 	}
 }
 
-static int nested_svm_msrpm_merge_offsets[9] __ro_after_init;
+static int nested_svm_msrpm_merge_offsets[6] __ro_after_init;
 static int nested_svm_nr_msrpm_merge_offsets __ro_after_init;
 
 int __init nested_svm_init_msrpm_merge_offsets(void)
@@ -209,12 +209,6 @@ int __init nested_svm_init_msrpm_merge_offsets(void)
 		MSR_IA32_LASTBRANCHTOIP,
 		MSR_IA32_LASTINTFROMIP,
 		MSR_IA32_LASTINTTOIP,
-
-		MSR_IA32_XSS,
-		MSR_EFER,
-		MSR_IA32_CR_PAT,
-		MSR_AMD64_SEV_ES_GHCB,
-		MSR_TSC_AUX,
 	};
 	int i, j;
 
