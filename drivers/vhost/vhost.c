3edeea168b6c9f30699116ead6326fdbc1456a09
Cindy Lu <lulu@redhat.com>
2025-05-31 17:47:03 +0800

    vhost: Add a new modparam to allow userspace select kthread
    The vhost now uses vhost_task and workers as a child of the owner thread.
    While this aligns with containerization principles, it confuses some
    legacy userspace applications, therefore, we are reintroducing kthread
    API support.
    
    Add a new module parameter to allow userspace to select behavior
    between using kthread and task.
    
    By default, this parameter is set to true (task mode). This means the
    default behavior remains unchanged by this patch.
    
    Signed-off-by: Cindy Lu <lulu@redhat.com>

--- drivers/vhost/vhost.c
+++ drivers/vhost/vhost.c
@@ -41,6 +41,10 @@ static int max_iotlb_entries = 2048;
 module_param(max_iotlb_entries, int, 0444);
 MODULE_PARM_DESC(max_iotlb_entries,
 	"Maximum number of iotlb entries. (default: 2048)");
+bool inherit_owner_default = true;
+module_param(inherit_owner_default, bool, 0444);
+MODULE_PARM_DESC(inherit_owner_default,
+		 "Set task mode as the default(default: Y)");
 
 enum {
 	VHOST_MEMORY_F_LOG = 0x1,
@@ -552,6 +556,7 @@ void vhost_dev_init(struct vhost_dev *dev,
 	dev->byte_weight = byte_weight;
 	dev->use_worker = use_worker;
 	dev->msg_handler = msg_handler;
+	dev->inherit_owner = inherit_owner_default;
 	init_waitqueue_head(&dev->wait);
 	INIT_LIST_HEAD(&dev->read_list);
 	INIT_LIST_HEAD(&dev->pending_list);
