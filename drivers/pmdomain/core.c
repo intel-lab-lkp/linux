abd6b9f3e04868aea5829b92f3e4c3aeffc4343d
Hiago De Franco <hiago.franco@toradex.com>
2025-06-02 10:19:03 -0300

    pmdomain: core: introduce dev_pm_genpd_is_on
    This helper function returns the current power status of a given generic
    power domain.
    
    As example, remoteproc/imx_rproc.c can now use this function to check
    the power status of the remote core to properly set "attached" or
    "offline" modes.
    
    Suggested-by: Ulf Hansson <ulf.hansson@linaro.org>
    Signed-off-by: Hiago De Franco <hiago.franco@toradex.com>

--- drivers/pmdomain/core.c
+++ drivers/pmdomain/core.c
@@ -759,6 +759,33 @@ int dev_pm_genpd_rpm_always_on(struct device *dev, bool on)
 EXPORT_SYMBOL_GPL(dev_pm_genpd_rpm_always_on);
 
 /**
+ * dev_pm_genpd_is_on - Get device's power status
+ *
+ * @dev: Device to get the current power status
+ *
+ * This function checks whether the generic power domain is on or not by
+ * verifying if genpd_status_on equals GENPD_STATE_ON.
+ *
+ * Return: 'true' if the device's power domain is on, 'false' otherwise.
+ */
+bool dev_pm_genpd_is_on(struct device *dev)
+{
+	struct generic_pm_domain *genpd;
+	bool is_on;
+
+	genpd = dev_to_genpd_safe(dev);
+	if (!genpd)
+		return false;
+
+	genpd_lock(genpd);
+	is_on = genpd_status_on(genpd);
+	genpd_unlock(genpd);
+
+	return is_on;
+}
+EXPORT_SYMBOL_GPL(dev_pm_genpd_is_on);
+
+/**
  * pm_genpd_inc_rejected() - Adjust the rejected/usage counts for an idle-state.
  *
  * @genpd: The PM domain the idle-state belongs to.
