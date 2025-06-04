5e1ff6235cc1cac16fb06080941d14030fafb831
Byungchul Park <byungchul@sk.com>
2025-06-04 11:52:30 +0900

    netmem: introduce netmem alloc APIs to wrap page alloc APIs
    To eliminate the use of struct page in page pool, the page pool code
    should use netmem descriptor and APIs instead.
    
    As part of the work, introduce netmem alloc APIs allowing the code to
    use them rather than the existing APIs for struct page.
    
    Signed-off-by: Byungchul Park <byungchul@sk.com>

--- net/core/netmem_priv.h
+++ net/core/netmem_priv.h
@@ -59,4 +59,18 @@ static inline void netmem_set_dma_index(netmem_ref netmem,
 	magic = netmem_get_pp_magic(netmem) | (id << PP_DMA_INDEX_SHIFT);
 	__netmem_clear_lsb(netmem)->pp_magic = magic;
 }
+
+static inline netmem_ref alloc_netmems_node(int nid, gfp_t gfp_mask,
+					    unsigned int order)
+{
+	return page_to_netmem(alloc_pages_node(nid, gfp_mask, order));
+}
+
+static inline unsigned long alloc_netmems_bulk_node(gfp_t gfp, int nid,
+						    unsigned long nr_netmems,
+						    netmem_ref *netmem_array)
+{
+	return alloc_pages_bulk_node(gfp, nid, nr_netmems,
+			(struct page **)netmem_array);
+}
 #endif
