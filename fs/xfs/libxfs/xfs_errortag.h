2c64bdf9769adcc1dc38f91101d57d161301da9a
Brian Foster <bfoster@redhat.com>
2025-06-05 13:33:57 -0400

    xfs: error tag to force zeroing on debug kernels
    iomap_zero_range() has to cover various corner cases that are
    difficult to test on production kernels because it is used in fairly
    limited use cases. For example, it is currently only used by XFS and
    mostly only in partial block zeroing cases.
    
    While it's possible to test most of these functional cases, we can
    provide more robust test coverage by co-opting fallocate zero range
    to invoke zeroing of the entire range instead of the more efficient
    block punch/allocate sequence. Add an errortag to occasionally
    invoke forced zeroing.
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>

--- fs/xfs/libxfs/xfs_errortag.h
+++ fs/xfs/libxfs/xfs_errortag.h
@@ -65,7 +65,8 @@
 #define XFS_ERRTAG_WRITE_DELAY_MS			43
 #define XFS_ERRTAG_EXCHMAPS_FINISH_ONE			44
 #define XFS_ERRTAG_METAFILE_RESV_CRITICAL		45
-#define XFS_ERRTAG_MAX					46
+#define XFS_ERRTAG_FORCE_ZERO_RANGE			46
+#define XFS_ERRTAG_MAX					47
 
 /*
  * Random factors for above tags, 1 means always, 2 means 1/2 time, etc.
@@ -115,5 +116,6 @@
 #define XFS_RANDOM_WRITE_DELAY_MS			3000
 #define XFS_RANDOM_EXCHMAPS_FINISH_ONE			1
 #define XFS_RANDOM_METAFILE_RESV_CRITICAL		4
+#define XFS_RANDOM_FORCE_ZERO_RANGE			4
 
 #endif /* __XFS_ERRORTAG_H_ */
