4f6db2e5cf6eebe2db87d0a270c197fca22650df
Igor Korotin <igor.korotin.linux@gmail.com>
2025-06-05 17:24:53 +0100

    rust: helpers: Add `is_of_node` helper function
    Add a helper function to determine whether a given device appears to be
    an Open Firmware (OF) node.
    
    This function will be used in Rust driver abstractions to support both
    ACPI and OF matching simultaneously in subsequent patches.
    
    Signed-off-by: Igor Korotin <igor.korotin.linux@gmail.com>

--- rust/helpers/helpers.c
+++ rust/helpers/helpers.c
@@ -21,6 +21,7 @@
 #include "jump_label.c"
 #include "kunit.c"
 #include "mutex.c"
+#include "of.c"
 #include "page.c"
 #include "platform.c"
 #include "pci.c"
---
+++ rust/helpers/of.c
@@ -0,0 +1,6 @@
+#include <linux/of.h>
+
+bool rust_helper_is_of_node(const struct fwnode_handle *fwnode)
+{
+    return is_of_node(fwnode);
+}
