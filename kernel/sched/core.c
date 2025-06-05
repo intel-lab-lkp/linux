58ad62cbf45eebc58ff903c63db95cd0a020b606
luca abeni <luca.abeni@santannapisa.it>
2025-06-05 09:14:10 +0200

    sched/core: Cgroup v2 support
    Make rt_runtime_us and rt_period_us virtual files accessible also to the cgroup
    v2 controller, effectively enabling the RT_GROUP_SCHED mechanism to cgroups v2.
    
    Signed-off-by: luca abeni <luca.abeni@santannapisa.it>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -10130,6 +10130,18 @@ static struct cftype cpu_files[] = {
 		.write = cpu_uclamp_max_write,
 	},
 #endif
+#ifdef CONFIG_RT_GROUP_SCHED
+	{
+		.name = "rt_runtime_us",
+		.read_s64 = cpu_rt_runtime_read,
+		.write_s64 = cpu_rt_runtime_write,
+	},
+	{
+		.name = "rt_period_us",
+		.read_u64 = cpu_rt_period_read_uint,
+		.write_u64 = cpu_rt_period_write_uint,
+	},
+#endif
 	{ }	/* terminate */
 };
 
