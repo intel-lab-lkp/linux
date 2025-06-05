5c354921e6ec575f59fcb2cc1ffe49fa47252620
Quentin Schulz <quentin.schulz@cherry.de>
2025-06-05 17:41:06 +0200

    dt-bindings: mfd: rk806: allow to customize PMIC reset mode
    The RK806 PMIC allows to configure its reset/restart behavior whenever
    the PMIC is reset either programmatically or via some external pins
    (e.g. PWRCTRL or RESETB).
    
    The following modes exist:
     - 0 (RK806_RESTART) restart PMU,
     - 1 (RK806_RESET) reset all power off reset registers and force
       state to switch to ACTIVE mode,
     - 2 (RK806_RESET_NOTIFY) same as RK806_RESET and also pull
       RESETB pin down for 5ms,
    
    For example, some hardware may require a full restart
    (RK806_RESTART mode) in order to function properly as regulators
    are shortly interrupted in this mode.
    
    This is the case for RK3588 Jaguar and RK3588 Tiger which have a
    companion microcontroller running on an independent power supply and
    monitoring the PMIC power rail to know the state of the main system.
    When it detects a restart, it resets its own IPs exposed to the main
    system as if to simulate its own reset. Failing to perform this fake
    reset of the microcontroller may break things (e.g. watchdog not
    automatically disabled, buzzer still running until manually disabled,
    leftover configuration from previous main system state, etc...).
    
    Some other systems may be depending on the power rails to not be
    interrupted even for a small amount of time[1].
    
    This allows to specify how the PMIC should perform on the hardware level
    and may differ between harwdare designs, so a DT property seems
    warranted. I unfortunately do not see how this could be made generic
    enough to make it a non-vendor property.
    
    [1] https://lore.kernel.org/linux-rockchip/2577051.irdbgypaU6@workhorse/
    Signed-off-by: Quentin Schulz <quentin.schulz@cherry.de>

---
+++ include/dt-bindings/mfd/rockchip,rk8xx.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
+/*
+ * Device Tree defines for Rockchip RK8xx PMICs
+ *
+ * Copyright 2025 Cherry Embedded Solutions GmbH
+ *
+ * Author: Quentin Schulz <quentin.schulz@cherry.de>
+ */
+
+#ifndef _DT_BINDINGS_MFD_ROCKCHIP_RK8XX_H
+#define _DT_BINDINGS_MFD_ROCKCHIP_RK8XX_H
+
+#define RK806_RESTART		0
+#define RK806_RESET		1
+#define RK806_RESET_NOTIFY	2
+
+#endif
