9cd35643afaf870a379307c96b2a15adfd71ee7c
Masahiro Yamada <masahiroy@kernel.org>
2025-06-06 13:10:25 +0900

    kbuild: keep .modinfo section in vmlinux.unstripped
    Keep the .modinfo section during linking, but strip it from the final
    vmlinux.
    
    Adjust scripts/mksysmap to exclude modinfo symbols from kallsyms.
    
    This change will allow the next commit to extract the .modinfo section
    from the vmlinux.unstripped intermediate.
    
    Signed-off-by: Masahiro Yamada <masahiroy@kernel.org>

--- include/asm-generic/vmlinux.lds.h
+++ include/asm-generic/vmlinux.lds.h
@@ -830,6 +830,7 @@ defined(CONFIG_AUTOFDO_CLANG) || defined(CONFIG_PROPELLER_CLANG)
 
 /* Required sections not related to debugging. */
 #define ELF_DETAILS							\
+		.modinfo : { *(.modinfo) }				\
 		.comment 0 : { *(.comment) }				\
 		.symtab 0 : { *(.symtab) }				\
 		.strtab 0 : { *(.strtab) }				\
@@ -1043,7 +1044,6 @@ defined(CONFIG_AUTOFDO_CLANG) || defined(CONFIG_PROPELLER_CLANG)
 	*(.discard.*)							\
 	*(.export_symbol)						\
 	*(.no_trim_symbol)						\
-	*(.modinfo)							\
 	/* ld.bfd warns about .gnu.version* even when not emitted */	\
 	*(.gnu.version*)						\
 
