36fac41604814978c67fcb7e69251589350bdb02
Igor Korotin <igor.korotin.linux@gmail.com>
2025-06-06 18:09:05 +0100

    rust: platform: Add ACPI match table support to `Driver` trait
    Extend the `platform::Driver` trait to support ACPI device matching by
    adding the `ACPI_ID_TABLE` constant.
    
    This allows Rust platform drivers to define ACPI match tables alongside
    their existing OF match tables.
    
    These changes mirror the existing OF support and allow Rust platform
    drivers to match devices based on ACPI identifiers.
    
    To avoid breaking compilation, a stub ACPI match table definition is
    added to the Rust sample platform driver. Functional support for ACPI
    matching in the sample driver will be provided in a follow-up patch.
    
    Signed-off-by: Igor Korotin <igor.korotin.linux@gmail.com>

--- rust/kernel/platform.rs
+++ rust/kernel/platform.rs
@@ -38,12 +38,18 @@ unsafe impl<T: Driver + 'static> driver::RegistrationOps for Adapter<T> {
             None => core::ptr::null(),
         };
 
+        let acpi_table = match T::ACPI_ID_TABLE {
+            Some(table) => table.as_ptr(),
+            None => core::ptr::null(),
+        };
+
         // SAFETY: It's safe to set the fields of `struct platform_driver` on initialization.
         unsafe {
             (*pdrv.get()).driver.name = name.as_char_ptr();
             (*pdrv.get()).probe = Some(Self::probe_callback);
             (*pdrv.get()).remove = Some(Self::remove_callback);
             (*pdrv.get()).driver.of_match_table = of_table;
+            (*pdrv.get()).driver.acpi_match_table = acpi_table;
         }
 
         // SAFETY: `pdrv` is guaranteed to be a valid `RegType`.
@@ -97,7 +103,7 @@ impl<T: Driver + 'static> driver::Adapter for Adapter<T> {
     }
 
     fn acpi_id_table() -> Option<acpi::IdTable<Self::IdInfo>> {
-        None
+        T::ACPI_ID_TABLE
     }
 }
 
@@ -128,7 +134,7 @@ macro_rules! module_platform_driver {
 /// # Example
 ///
 ///```
-/// # use kernel::{bindings, c_str, device::Core, of, platform};
+/// # use kernel::{acpi, bindings, c_str, device::Core, of, platform};
 ///
 /// struct MyDriver;
 ///
@@ -144,6 +150,7 @@ macro_rules! module_platform_driver {
 /// impl platform::Driver for MyDriver {
 ///     type IdInfo = ();
 ///     const OF_ID_TABLE: Option<of::IdTable<Self::IdInfo>> = Some(&OF_TABLE);
+///     const ACPI_ID_TABLE: Option<acpi::IdTable<Self::IdInfo>> = Some(&ACPI_TABLE);
 ///
 ///     fn probe(
 ///         _pdev: &platform::Device<Core>,
@@ -163,6 +170,9 @@ pub trait Driver: Send {
 
     /// The table of OF device ids supported by the driver.
     const OF_ID_TABLE: Option<of::IdTable<Self::IdInfo>>;
+
+    /// The table of ACPI device ids supported by the driver.
+    const ACPI_ID_TABLE: Option<acpi::IdTable<Self::IdInfo>>;
 
     /// Platform driver probe.
     ///
