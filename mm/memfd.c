6004223f07a5aeb028ff846b484c92e8e5ffa079
Ye Liu <liuye@kylinos.cn>
2025-06-09 11:18:58 +0800

    mm/memfd: Rename error labels for clarity
    err_name --> err_fd (fd failure case)
    err_fd --> err_file (file failure case)
    
    Signed-off-by: Ye Liu <liuye@kylinos.cn>

--- mm/memfd.c
+++ mm/memfd.c
@@ -475,22 +475,22 @@ SYSCALL_DEFINE2(memfd_create,
 	fd = get_unused_fd_flags((flags & MFD_CLOEXEC) ? O_CLOEXEC : 0);
 	if (fd < 0) {
 		error = fd;
-		goto err_name;
+		goto err_fd;
 	}
 
 	file = alloc_file(name, flags);
 	if (IS_ERR(file)) {
 		error = PTR_ERR(file);
-		goto err_fd;
+		goto err_file;
 	}
 
 	fd_install(fd, file);
 	kfree(name);
 	return fd;
 
-err_fd:
+err_file:
 	put_unused_fd(fd);
-err_name:
+err_fd:
 	kfree(name);
 	return error;
 }
