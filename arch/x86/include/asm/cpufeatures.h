06166251945fed59c95cf4f2f9456bc2cc1b1aeb
Babu Moger <babu.moger@amd.com>
2025-06-11 16:23:26 -0500

    x86/cpufeatures: Add support for L3 Smart Data Cache Injection Allocation Enforcement
    Smart Data Cache Injection (SDCI) is a mechanism that enables direct
    insertion of data from I/O devices into the L3 cache. By directly caching
    data from I/O devices rather than first storing the I/O data in DRAM,
    SDCI reduces demands on DRAM bandwidth and reduces latency to the processor
    consuming the I/O data.
    
    The SDCIAE (SDCI Allocation Enforcement) PQE feature allows system software
    to control the portion of the L3 cache used for SDCI.
    
    When enabled, SDCIAE forces all SDCI lines to be placed into the L3 cache
    partitions identified by the highest-supported L3_MASK_n register, where n
    is the maximum supported CLOSID.
    
    Add CPUID feature bit that can be used to configure SDCIAE.
    
    The feature details are documented in APM listed below [1].
    [1] AMD64 Architecture Programmer's Manual Volume 2: System Programming
    Publication # 24593 Revision 3.41 section 19.4.7 L3 Smart Data Cache
    Injection Allocation Enforcement (SDCIAE)
    
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=206537
    Signed-off-by: Babu Moger <babu.moger@amd.com>

--- arch/x86/include/asm/cpufeatures.h
+++ arch/x86/include/asm/cpufeatures.h
@@ -487,6 +487,7 @@
 #define X86_FEATURE_PREFER_YMM		(21*32+ 8) /* Avoid ZMM registers due to downclocking */
 #define X86_FEATURE_APX			(21*32+ 9) /* Advanced Performance Extensions */
 #define X86_FEATURE_INDIRECT_THUNK_ITS	(21*32+10) /* Use thunk for indirect branches in lower half of cacheline */
+#define X86_FEATURE_SDCIAE		(21*32+11) /* L3 Smart Data Cache Injection Allocation Enforcement */
 
 /*
  * BUG word(s)
