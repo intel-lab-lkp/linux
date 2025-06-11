48e63934badb71772d1abb729551edb6ebbfaed9
Babu Moger <babu.moger@amd.com>
2025-06-11 16:23:29 -0500

    x86/resctrl: Implement "io_alloc" enable/disable handlers
    "io_alloc" enables direct insertion of data from I/O devices into the L3
    cache.
    
    On AMD, "io_alloc" feature is backed by L3 Smart Data Cache Injection
    Allocation Enforcement (SDCIAE). Change SDCIAE state by setting (to enable)
    or clearing (to disable) bit 1 of MSR L3_QOS_EXT_CFG on all logical
    processors within the cache domain.
    
    Introduce architecture-specific handlers to enable and disable the feature.
    
    The SDCIAE feature details are available in APM listed below [1].
    [1] AMD64 Architecture Programmer's Manual Volume 2: System Programming
    Publication # 24593 Revision 3.41 section 19.4.7 L3 Smart Data Cache
    Injection Allocation Enforcement (SDCIAE)
    
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=206537
    Signed-off-by: Babu Moger <babu.moger@amd.com>

--- include/linux/resctrl.h
+++ include/linux/resctrl.h
@@ -531,6 +531,27 @@ void resctrl_arch_reset_rmid_all(struct rdt_resource *r, struct rdt_mon_domain *
  */
 void resctrl_arch_reset_all_ctrls(struct rdt_resource *r);
 
+/**
+ * resctrl_arch_io_alloc_enable() - Enable/disable io_alloc feature.
+ * @r:		The resctrl resource.
+ * @enable:	Enable (true) or disable (false) io_alloc on resource @r.
+ *
+ * This can be called from any CPU.
+ *
+ * Return:
+ * 0 on success, or non-zero on error.
+ */
+int resctrl_arch_io_alloc_enable(struct rdt_resource *r, bool enable);
+
+/**
+ * resctrl_arch_get_io_alloc_enabled() - Get io_alloc feature state.
+ * @r:		The resctrl resource.
+ *
+ * Return:
+ * true if io_alloc is enabled or false if disabled.
+ */
+inline bool resctrl_arch_get_io_alloc_enabled(struct rdt_resource *r);
+
 extern unsigned int resctrl_rmid_realloc_threshold;
 extern unsigned int resctrl_rmid_realloc_limit;
 
