fa9c10ba650ad29cf5be5bcb2063136187bba22a
Krzysztof Karas <krzysztof.karas@intel.com>
2025-06-11 13:33:19 +0000

    drm/i915/selftests: Keep mock file open during unfaultable migrate with fill
    igt_mmap_migrate() tests migration with various parameters.
    In one of the cases, where FILL and UNFAULTABLE flags are set,
    during first stages of this test a mock file is opened in
    igt_mmap_offset(), which results in allocating some data in the
    GPU memory. Then, also in igt_mmap_offset(), the file is closed
    (fput) and the cleanup of that data is scheduled. Right after
    that, the test calls igt_fill_mappable() to fill all available
    GPU memory. At this point, three scenarios are possible
    (N = max size of GPU memory for this test in MiB):
     1) the data cleanup does not fire until the whole test is over,
    so the memory is fully occupied by 1 MiB with that data and
    N - 1 MiB added by igt_fill_mappable(),
     2) the data cleanup fires before igt_fill_mappable() completes,
    so the whole memory is populated with N MiB by
    igt_fill_mappable(),
     3) the data cleanup is performed right after fill is done,
    so only N - 1 MiB are in the GPU memory, preventing the test
    from properly faulting - we'd expect no space left, but an
    object was able to fit in the remaining 1 MiB.
    
    Amend the problem by keeping the mock file open throughout the
    duration of this test and calling fput() from the test itself.
    
    Closes: https://gitlab.freedesktop.org/drm/i915/kernel/-/issues/13929
    Signed-off-by: Krzysztof Karas <krzysztof.karas@intel.com>

--- drivers/gpu/drm/i915/selftests/igt_mmap.c
+++ drivers/gpu/drm/i915/selftests/igt_mmap.c
@@ -9,16 +9,21 @@
 #include "i915_drv.h"
 #include "igt_mmap.h"
 
-unsigned long igt_mmap_offset(struct drm_i915_private *i915,
-			      u64 offset,
-			      unsigned long size,
-			      unsigned long prot,
-			      unsigned long flags)
+unsigned long igt_mmap_offset_with_file(struct drm_i915_private *i915,
+					u64 offset,
+					unsigned long size,
+					unsigned long prot,
+					unsigned long flags,
+					struct file *file)
 {
 	struct drm_vma_offset_node *node;
-	struct file *file;
 	unsigned long addr;
 	int err;
+
+	if (!file) {
+		pr_info("file cannot be NULL\n");
+		return -EINVAL;
+	}
 
 	/* no need to refcount, we own this object */
 	drm_vma_offset_lock_lookup(i915->drm.vma_offset_manager);
@@ -31,22 +36,36 @@ unsigned long igt_mmap_offset(struct drm_i915_private *i915,
 		return -ENOENT;
 	}
 
-	/* Pretend to open("/dev/dri/card0") */
-	file = mock_drm_getfile(i915->drm.primary, O_RDWR);
-	if (IS_ERR(file))
-		return PTR_ERR(file);
-
 	err = drm_vma_node_allow(node, file->private_data);
 	if (err) {
-		addr = err;
-		goto out_file;
+		fput(file);
+		return err;
 	}
 
 	addr = vm_mmap(file, 0, drm_vma_node_size(node) << PAGE_SHIFT,
 		       prot, flags, drm_vma_node_offset_addr(node));
 
 	drm_vma_node_revoke(node, file->private_data);
-out_file:
+
+	return addr;
+}
+
+unsigned long igt_mmap_offset(struct drm_i915_private *i915,
+			      u64 offset,
+			      unsigned long size,
+			      unsigned long prot,
+			      unsigned long flags)
+{
+	struct file *file;
+	unsigned long addr;
+
+	/* Pretend to open("/dev/dri/card0") */
+	file = mock_drm_getfile(i915->drm.primary, O_RDWR);
+	if (IS_ERR(file))
+		return PTR_ERR(file);
+
+	addr = igt_mmap_offset_with_file(i915, offset, size, prot, flags, file);
 	fput(file);
+
 	return addr;
 }
