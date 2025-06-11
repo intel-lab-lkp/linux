ae5363bbabce2183169846032b22201e8932cb8b
Bibo Mao <maobibo@loongson.cn>
2025-06-11 09:46:50 +0800

    LoongArch: KVM: INTC: Add stat information with kernel irqchip
    Move stat information about kernel irqchip from VM to vCPU, since
    all vm exiting events should be vCPU relative. And also add entry
    with structure kvm_vcpu_stats_desc[], so that it can display with
    directory /sys/kernel/debug/kvm.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>

--- arch/loongarch/kvm/intc/eiointc.c
+++ arch/loongarch/kvm/intc/eiointc.c
@@ -311,7 +311,7 @@ static int kvm_eiointc_read(struct kvm_vcpu *vcpu,
 		return -EINVAL;
 	}
 
-	vcpu->kvm->stat.eiointc_read_exits++;
+	vcpu->stat.eiointc_read_exits++;
 	spin_lock_irqsave(&eiointc->lock, flags);
 	switch (len) {
 	case 1:
@@ -682,7 +682,7 @@ static int kvm_eiointc_write(struct kvm_vcpu *vcpu,
 		return -EINVAL;
 	}
 
-	vcpu->kvm->stat.eiointc_write_exits++;
+	vcpu->stat.eiointc_write_exits++;
 	spin_lock_irqsave(&eiointc->lock, flags);
 	switch (len) {
 	case 1:
