4aeea57599f9e2d76d900a438de2d2cf3e25d2c4
Nicolas Frattaroli <nicolas.frattaroli@collabora.com>
2025-06-12 20:56:06 +0200

    media: synopsys: hdmirx: replace macros with bitfield variants
    The era of hand-rolled HIWORD_UPDATE macros is over, at least for those
    drivers that use constant masks.
    
    Replace the UPDATE macro with bitfield.h's FIELD_PREP, to give us
    additional error checking.
    
    Also, replace the HIWORD_UPDATE macro at the same time with bitfield.h's
    new HWORD_UPDATE macro, which also gives us additional error checking.
    
    The UPDATE/HIWORD_UPDATE macros are left as wrappers around the
    bitfield.h macros, in order to not rock the boat too much, and keep the
    changes easy to review.
    
    Signed-off-by: Nicolas Frattaroli <nicolas.frattaroli@collabora.com>

--- drivers/media/platform/synopsys/hdmirx/snps_hdmirx.h
+++ drivers/media/platform/synopsys/hdmirx/snps_hdmirx.h
@@ -8,10 +8,11 @@
 #ifndef DW_HDMIRX_H
 #define DW_HDMIRX_H
 
+#include <linux/bitfield.h>
 #include <linux/bitops.h>
 
-#define UPDATE(x, h, l)		(((x) << (l)) & GENMASK((h), (l)))
-#define HIWORD_UPDATE(v, h, l)	(((v) << (l)) | (GENMASK((h), (l)) << 16))
+#define UPDATE(x, h, l)		(FIELD_PREP(GENMASK((h), (l)), (x)))
+#define HIWORD_UPDATE(v, h, l)	(HWORD_UPDATE(GENMASK((h), (l)), (v)))
 
 /* SYS_GRF */
 #define SYS_GRF_SOC_CON1			0x0304
