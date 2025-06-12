496bc827bab749d61f82466850f8ccb6230e9bda
Ahmed S. Darwish <darwi@linutronix.de>
2025-06-13 01:39:54 +0200

    x86/cpuid: Parse CPUID(0x2)
    Add CPUID(0x2) support to the CPUID parser.
    
    Keep the leaf marked as invalid at the CPUID table if the whole leaf, or
    all of its output registers, were malformed.
    
    Note, the cpuid_leaf_0x2() logic at <cpuid/api.h> will be removed once
    all the CPUID(0x2) call sites are transformed to the new CPUID model API.
    
    References: fe78079ec07f ("x86/cpu: Introduce and use CPUID leaf 0x2 parsing helpers")
    Signed-off-by: Ahmed S. Darwish <darwi@linutronix.de>

--- arch/x86/kernel/cpu/cpuid_parser.c
+++ arch/x86/kernel/cpu/cpuid_parser.c
@@ -27,9 +27,40 @@ static void cpuid_read_generic(const struct cpuid_parse_entry *e, struct cpuid_r
 		cpuid_read_subleaf(e->leaf, e->subleaf + i, output->regs);
 }
 
-/*
- * Leaf-independent parser code:
- */
+static void cpuid_read_0x2(const struct cpuid_parse_entry *e, struct cpuid_read_output *output)
+{
+	union leaf_0x2_regs *regs = (union leaf_0x2_regs *)output->regs;
+	struct leaf_0x2_0 *l = (struct leaf_0x2_0 *)output->regs;
+	int invalid_regs = 0;
+
+	/*
+	 * All Intel CPUs must report an iteration count of 1.  For broken hardware,
+	 * keep the leaf marked as invalid at the CPUID table.
+	 */
+	cpuid_read_subleaf(e->leaf, e->subleaf, l);
+	if (l->iteration_count != 0x01)
+		return;
+
+	/*
+	 * The most significant bit (MSB) of each CPUID(0x2) register must be clear.
+	 * If a register is malformed, replace its 1-byte descriptors with NULL.
+	 */
+	for (int i = 0; i < 4; i++) {
+		if (regs->reg[i].invalid) {
+			regs->regv[i] = 0;
+			invalid_regs++;
+		}
+	}
+
+	/*
+	 * If all of the CPUID(0x2) output registers were malformed, keep the leaf
+	 * marked as invalid at the CPUID table.
+	 */
+	if (invalid_regs == 4)
+		return;
+
+	output->info->nr_entries = 1;
+}
 
 static void cpuid_read_0x80000000(const struct cpuid_parse_entry *e, struct cpuid_read_output *output)
 {
@@ -53,6 +84,10 @@ static void cpuid_read_0x80000000(const struct cpuid_parse_entry *e, struct cpui
 
 	output->info->nr_entries = 1;
 }
+
+/*
+ * Leaf-independent parser code:
+ */
 
 static unsigned int cpuid_range_max_leaf(const struct cpuid_table *t, unsigned int range)
 {
