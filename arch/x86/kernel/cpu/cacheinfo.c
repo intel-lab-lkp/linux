16ce4d67dc4ab6a6efd368c24b640afdca1b2767
Ahmed S. Darwish <darwi@linutronix.de>
2025-06-13 01:40:08 +0200

    x86/cpuid: Use parsed CPUID(0x80000006)
    For cpuid_amd_hygon_has_l3_cache(), use parsed CPUID access instead of a
    direct CPUID query.  The new API offers centralization benefits and
    avoids bit fiddling at call sites.
    
    For testing L3 cache availability, just check if the EDX.l3_assoc output
    is not zero.  Per AMD manuals, an L3 associativity of zero implies the
    absence of an L3 cache on the CPU.
    
    Note, since this function is now using parsed CPUID API, move it under
    the <cpuid/api.h> section: 'Convenience leaf-specific functions (using
    parsed CPUID data)'
    
    Signed-off-by: Ahmed S. Darwish <darwi@linutronix.de>

--- arch/x86/kernel/cpu/cacheinfo.c
+++ arch/x86/kernel/cpu/cacheinfo.c
@@ -210,7 +210,7 @@ static int fill_cpuid4_info(struct cpuinfo_x86 *c, int index, struct _cpuid4_inf
 
 void cacheinfo_amd_init_llc_id(struct cpuinfo_x86 *c, u16 die_id)
 {
-	if (!cpuid_amd_hygon_has_l3_cache())
+	if (!cpuid_amd_hygon_has_l3_cache(c))
 		return;
 
 	if (c->x86 < 0x17) {
@@ -241,7 +241,7 @@ void cacheinfo_amd_init_llc_id(struct cpuinfo_x86 *c, u16 die_id)
 
 void cacheinfo_hygon_init_llc_id(struct cpuinfo_x86 *c)
 {
-	if (!cpuid_amd_hygon_has_l3_cache())
+	if (!cpuid_amd_hygon_has_l3_cache(c))
 		return;
 
 	/*
@@ -257,7 +257,7 @@ void init_amd_cacheinfo(struct cpuinfo_x86 *c)
 
 	ci->num_leaves = boot_cpu_has(X86_FEATURE_TOPOEXT) ?
 		cpuid_subleaf_count(c, 0x8000001d) :
-		cpuid_leaf(c, 0x80000006)->l3_assoc ? 4 : 3;
+		cpuid_amd_hygon_has_l3_cache(c) ? 4 : 3;
 }
 
 void init_hygon_cacheinfo(struct cpuinfo_x86 *c)
