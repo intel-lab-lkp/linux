1484b04cfdd068c510898c5cab039f00ee958979
Alexander Gordeev <agordeev@linux.ibm.com>
2025-06-12 19:36:10 +0200

    mm/debug: Detect wrong arch_enter_lazy_mmu_mode() contexts
    Make default arch_enter|leave|flush_lazy_mmu_mode() callbacks
    complain on enabled preemption to detect wrong contexts. That
    could help to prevent the complicated lazy MMU mode semantics
    misuse, such like one that was solved with commit b9ef323ea168
    ("powerpc/64s: Disable preemption in hash lazy mmu mode").
    
    Skip fully preemptible kernels, since in such case taking the
    page table lock does not disable preemption, so the described
    check would be wrong.
    
    Most platforms do not implement the lazy MMU mode callbacks,
    so to aovid a performance impact allow the complaint when
    CONFIG_DEBUG_VM option is enabled only.
    
    Reviewed-by: Nicholas Piggin <npiggin@gmail.com>
    Signed-off-by: Alexander Gordeev <agordeev@linux.ibm.com>

--- include/linux/pgtable.h
+++ include/linux/pgtable.h
@@ -232,9 +232,23 @@ static inline int pmd_dirty(pmd_t pmd)
  * Nesting is not permitted and the mode cannot be used in interrupt context.
  */
 #ifndef __HAVE_ARCH_ENTER_LAZY_MMU_MODE
-#define arch_enter_lazy_mmu_mode()	do {} while (0)
-#define arch_leave_lazy_mmu_mode()	do {} while (0)
-#define arch_flush_lazy_mmu_mode()	do {} while (0)
+static inline void arch_enter_lazy_mmu_mode(void)
+{
+	if (!IS_ENABLED(CONFIG_PREEMPT_RT))
+		VM_WARN_ON_ONCE(preemptible());
+}
+
+static inline void arch_leave_lazy_mmu_mode(void)
+{
+	if (!IS_ENABLED(CONFIG_PREEMPT_RT))
+		VM_WARN_ON_ONCE(preemptible());
+}
+
+static inline void arch_flush_lazy_mmu_mode(void)
+{
+	if (!IS_ENABLED(CONFIG_PREEMPT_RT))
+		VM_WARN_ON_ONCE(preemptible());
+}
 #endif
 
 #ifndef pte_batch_hint
