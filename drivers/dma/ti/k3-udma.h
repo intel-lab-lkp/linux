99ac48b54f8f50cd52836f36b23626cccfd9e31a
Sai Sree Kartheek Adivi <s-adivi@ti.com>
2025-06-12 12:45:12 +0530

    dmaengine: ti: k3-udma: move resource management functions to k3-udma-common.c
    Move functions responsible for allocation and release of udma
    resources such as channels, rings and flows from k3-udma.c
    to the common k3-udma-common.c file.
    
    The implementation of these functions is largely shared between K3 UDMA
    and K3 UDMA v2. This refactor improves code reuse and maintainability
    across multiple variants.
    
    No functional changes intended.
    
    Signed-off-by: Sai Sree Kartheek Adivi <s-adivi@ti.com>

--- drivers/dma/ti/k3-udma.h
+++ drivers/dma/ti/k3-udma.h
@@ -648,6 +648,31 @@ void udma_dbg_summary_show(struct seq_file *s,
 		struct dma_device *dma_dev);
 #endif /* CONFIG_DEBUG_FS */
 
+int __udma_alloc_gp_rflow_range(struct udma_dev *ud, int from, int cnt);
+int __udma_free_gp_rflow_range(struct udma_dev *ud, int from, int cnt);
+struct udma_rflow *__udma_get_rflow(struct udma_dev *ud, int id);
+void __udma_put_rflow(struct udma_dev *ud, struct udma_rflow *rflow);
+int udma_get_tchan(struct udma_chan *uc);
+int udma_get_rchan(struct udma_chan *uc);
+int udma_get_chan_pair(struct udma_chan *uc);
+int udma_get_rflow(struct udma_chan *uc, int flow_id);
+void udma_put_rchan(struct udma_chan *uc);
+void udma_put_tchan(struct udma_chan *uc);
+void udma_put_rflow(struct udma_chan *uc);
+void udma_free_tx_resources(struct udma_chan *uc);
+void udma_free_rx_resources(struct udma_chan *uc);
+void udma_free_chan_resources(struct dma_chan *chan);
+void bcdma_put_bchan(struct udma_chan *uc);
+void bcdma_free_bchan_resources(struct udma_chan *uc);
+
+#define _UDMA_RESERVE_RESOURCE(res)					\
+struct udma_##res *__udma_reserve_##res(struct udma_dev *ud,	\
+					       enum udma_tp_level tpl,	\
+					       int id);
+_UDMA_RESERVE_RESOURCE(bchan);
+_UDMA_RESERVE_RESOURCE(tchan);
+_UDMA_RESERVE_RESOURCE(rchan);
+
 /* Direct access to UDMA low lever resources for the glue layer */
 int xudma_navss_psil_pair(struct udma_dev *ud, u32 src_thread, u32 dst_thread);
 int xudma_navss_psil_unpair(struct udma_dev *ud, u32 src_thread,
