14b9ccd1f2ed54711234ccfa49ccaf20353ad2e3
Melissa Wen <mwen@igalia.com>
2025-06-13 11:58:15 -0300

    drm/amd/display: add drm_edid to dc_sink
    Add Linux opaque object to dc_sink for storing edid data cross driver,
    drm_edid. Also include the Linux call to free this object, the
    drm_edid_free()
    
    v3:
    - remove uneccessary include (jani)
    
    Signed-off-by: Melissa Wen <mwen@igalia.com>

--- drivers/gpu/drm/amd/display/amdgpu_dm/dc_edid.h
+++ drivers/gpu/drm/amd/display/amdgpu_dm/dc_edid.h
@@ -9,5 +9,6 @@ bool dc_edid_is_same_edid(struct dc_sink *prev_sink,
 			  struct dc_sink *current_sink);
 void dc_edid_copy_edid_to_dc(struct dc_sink *dc_sink,
 			     const void *edid, int len);
+void dc_edid_sink_edid_free(struct dc_sink *sink);
 
 #endif /* __DC_EDID_H__ */
