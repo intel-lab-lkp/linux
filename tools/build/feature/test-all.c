41662a6fc31b42f74ccbf43de47431121d0e82a1
Yuzhuo Jing <yuzhuo@google.com>
2025-06-13 21:41:33 -0700

    tools: Remove libcrypto dependency
    Remove all occurrence of libcrypto in the build system.
    
    Signed-off-by: Yuzhuo Jing <yuzhuo@google.com>
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- tools/build/feature/test-all.c
+++ tools/build/feature/test-all.c
@@ -138,10 +138,6 @@
 # include "test-bpf.c"
 #undef main
 
-#define main main_test_libcrypto
-# include "test-libcrypto.c"
-#undef main
-
 #define main main_test_sdt
 # include "test-sdt.c"
 #undef main
@@ -206,7 +202,6 @@ int main(int argc, char *argv[])
 	main_test_lzma();
 	main_test_get_cpuid();
 	main_test_bpf();
-	main_test_libcrypto();
 	main_test_scandirat();
 	main_test_sched_getcpu();
 	main_test_sdt();
