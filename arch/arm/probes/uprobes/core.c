8491a83787ee59c08049f5d2a7d5dc5160723cac
Yunseong Kim <ysk@kzalloc.com>
2025-06-15 14:11:30 +0000

    arm/probes/uprobes: Remove redundant preempt_disable/enable around kmap_atomic
    The 'kmap_atomic' API implicitly disables preemption for the duration
    of its use. It merely cleans up the code by removing unnecessary
    operations, making it clearer and efficient.
    
    Signed-off-by: Yunseong Kim <ysk@kzalloc.com>

--- arch/arm/probes/uprobes/core.c
+++ arch/arm/probes/uprobes/core.c
@@ -116,15 +116,11 @@ void arch_uprobe_copy_ixol(struct page *page, unsigned long vaddr,
 	void *xol_page_kaddr = kmap_atomic(page);
 	void *dst = xol_page_kaddr + (vaddr & ~PAGE_MASK);
 
-	preempt_disable();
-
 	/* Initialize the slot */
 	memcpy(dst, src, len);
 
 	/* flush caches (dcache/icache) */
 	flush_uprobe_xol_access(page, vaddr, dst, len);
-
-	preempt_enable();
 
 	kunmap_atomic(xol_page_kaddr);
 }
