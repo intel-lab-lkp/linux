b7ae34bf350765a6253b108dad9a8b57e4c672b3
Tao Chen <chen.dylane@linux.dev>
2025-06-15 23:05:14 +0800

    bpf: Add show_fdinfo for kprobe_multi
    Show kprobe_multi link info with fdinfo, the info as follows:
    
    link_type:	kprobe_multi
    link_id:	3
    prog_tag:	e8225cbcc9cdffef
    prog_id:	29
    type:	kprobe_multi
    kprobe_cnt:	8
    missed:	0
    func:	bpf_fentry_test1+0x0/0x20
    cookie:	1
    func:	bpf_fentry_test2+0x0/0x20
    cookie:	7
    func:	bpf_fentry_test3+0x0/0x20
    cookie:	2
    func:	bpf_fentry_test4+0x0/0x20
    cookie:	3
    func:	bpf_fentry_test5+0x0/0x20
    cookie:	4
    func:	bpf_fentry_test6+0x0/0x20
    cookie:	5
    func:	bpf_fentry_test7+0x0/0x20
    cookie:	6
    func:	bpf_fentry_test8+0x0/0x10
    cookie:	8
    
    Signed-off-by: Tao Chen <chen.dylane@linux.dev>

--- kernel/trace/bpf_trace.c
+++ kernel/trace/bpf_trace.c
@@ -2620,10 +2620,43 @@ static int bpf_kprobe_multi_link_fill_link_info(const struct bpf_link *link,
 	return err;
 }
 
+#ifdef CONFIG_PROC_FS
+static void bpf_kprobe_multi_show_fdinfo(const struct bpf_link *link,
+					 struct seq_file *seq)
+{
+	struct bpf_kprobe_multi_link *kmulti_link;
+	char sym[KSYM_NAME_LEN];
+
+	kmulti_link = container_of(link, struct bpf_kprobe_multi_link, link);
+
+	seq_printf(seq,
+		   "type:\t%s\n"
+		   "kprobe_cnt:\t%u\n"
+		   "missed:\t%lu\n",
+		   kmulti_link->flags == BPF_F_KPROBE_MULTI_RETURN ? "kretprobe_multi" :
+					 "kprobe_multi",
+		   kmulti_link->cnt,
+		   kmulti_link->fp.nmissed);
+
+	for (int i = 0; i < kmulti_link->cnt; i++) {
+		sprint_symbol(sym, kmulti_link->addrs[i]);
+		seq_printf(seq,
+			  "func:\t%s\n"
+			  "cookie:\t%llu\n",
+			  sym,
+			  kmulti_link->cookies[i]
+			  );
+	}
+}
+#endif
+
 static const struct bpf_link_ops bpf_kprobe_multi_link_lops = {
 	.release = bpf_kprobe_multi_link_release,
 	.dealloc_deferred = bpf_kprobe_multi_link_dealloc,
 	.fill_link_info = bpf_kprobe_multi_link_fill_link_info,
+#ifdef CONFIG_PROC_FS
+	.show_fdinfo = bpf_kprobe_multi_show_fdinfo,
+#endif
 };
 
 static void bpf_kprobe_multi_cookie_swap(void *a, void *b, int size, const void *priv)
