18a0d5a859d947a51a43516a494cdef452e2b8d6
Thomas Andreatta <thomasandreatta2000@gmail.com>
2025-06-16 01:11:39 +0200

    Staging: media: atomisp: i2c: hardcoded function name
    Checkpatch fix: use func instead of hardcoded function name
    
    Signed-off-by: Thomas Andreatta <thomas.andreatta2000@gmail.com>

--- drivers/staging/media/atomisp/i2c/atomisp-gc0310.c
+++ drivers/staging/media/atomisp/i2c/atomisp-gc0310.c
@@ -584,7 +584,7 @@ static void gc0310_remove(struct i2c_client *client)
 	struct v4l2_subdev *sd = i2c_get_clientdata(client);
 	struct gc0310_device *dev = to_gc0310_sensor(sd);
 
-	dev_dbg(&client->dev, "gc0310_remove...\n");
+	dev_dbg(&client->dev, "%s...\n", __func__);
 
 	v4l2_async_unregister_subdev(sd);
 	media_entity_cleanup(&dev->sd.entity);
