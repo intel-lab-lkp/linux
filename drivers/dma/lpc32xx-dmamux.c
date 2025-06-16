766ae53c295ab89966c67b68927a21cab84ee64d
Charles Han <hanchunchao@inspur.com>
2025-06-16 18:46:39 +0800

    dmaengine: Add NULL check in lpc32xx_dmamux_reserve()
    The function of_find_device_by_node() may return NULL if the device
    node cannot be found or if CONFIG_OF is not defined, dereferencing
    it without NULL check may lead to NULL dereference.
    Add a check to verify whether the return value is NULL.
    
    Fixes: 5d318b595982 ("dmaengine: Add dma router for pl08x in LPC32XX SoC")
    Signed-off-by: Charles Han <hanchunchao@inspur.com>

--- drivers/dma/lpc32xx-dmamux.c
+++ drivers/dma/lpc32xx-dmamux.c
@@ -90,12 +90,19 @@ static void lpc32xx_dmamux_release(struct device *dev, void *route_data)
 static void *lpc32xx_dmamux_reserve(struct of_phandle_args *dma_spec,
 				    struct of_dma *ofdma)
 {
-	struct platform_device *pdev = of_find_device_by_node(ofdma->of_node);
-	struct device *dev = &pdev->dev;
-	struct lpc32xx_dmamux_data *dmamux = platform_get_drvdata(pdev);
+	struct platform_device *pdev;
+	struct device *dev;
+	struct lpc32xx_dmamux_data *dmamux;
 	unsigned long flags;
 	struct lpc32xx_dmamux *mux = NULL;
 	int i;
+
+	pdev = of_find_device_by_node(ofdma->of_node);
+	if (!pdev)
+		return ERR_PTR(-ENODEV);
+
+	dev = &pdev->dev;
+	dmamux = platform_get_drvdata(pdev);
 
 	if (dma_spec->args_count != 3) {
 		dev_err(&pdev->dev, "invalid number of dma mux args\n");
