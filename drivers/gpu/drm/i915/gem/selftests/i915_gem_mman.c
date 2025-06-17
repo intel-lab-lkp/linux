63ce2fbec377436d034832bfd9897e2ce4ec1a42
Krzysztof Karas <krzysztof.karas@intel.com>
2025-06-17 13:19:07 +0000

    drm/i915/selftests: Keep mock file open during unfaultable migrate with fill
    igt_mmap_migrate() tests migration with various parameters.
    In one of the cases, where FILL and UNFAULTABLE flags are set,
    during first stages of this test a mock file is opened in
    igt_mmap_offset(), which results in allocating some data in the
    GPU memory. Then, also in igt_mmap_offset(), the file is closed
    (fput) and the cleanup of that data is scheduled. Right after
    that, the test calls igt_fill_mappable() to fill all available
    GPU memory. At this point, three scenarios are possible
    (N = max size of GPU memory for this test in MiB):
     1) the data cleanup does not fire until the whole test is over,
    so the memory is fully occupied by 1 MiB with that data and
    N - 1 MiB added by igt_fill_mappable(),
     2) the data cleanup fires before igt_fill_mappable() completes,
    so the whole memory is populated with N MiB by
    igt_fill_mappable(),
     3) the data cleanup is performed right after fill is done,
    so only N - 1 MiB are in the GPU memory, preventing the test
    from properly faulting - we'd expect no space left, but an
    object was able to fit in the remaining 1 MiB.
    
    Amend the problem by keeping the mock file open throughout the
    duration of this test and calling fput() from the test itself.
    
    Closes: https://gitlab.freedesktop.org/drm/i915/kernel/-/issues/13929
    Signed-off-by: Krzysztof Karas <krzysztof.karas@intel.com>

--- drivers/gpu/drm/i915/gem/selftests/i915_gem_mman.c
+++ drivers/gpu/drm/i915/gem/selftests/i915_gem_mman.c
@@ -1176,6 +1176,7 @@ static int __igt_mmap_migrate(struct intel_memory_region **placements,
 	struct drm_i915_private *i915 = placements[0]->i915;
 	struct drm_i915_gem_object *obj;
 	struct i915_request *rq = NULL;
+	struct file *mock_file;
 	unsigned long addr;
 	LIST_HEAD(objects);
 	u64 offset;
@@ -1194,14 +1195,19 @@ static int __igt_mmap_migrate(struct intel_memory_region **placements,
 	if (err)
 		goto out_put;
 
+	/* Pretend to open("/dev/dri/card0") */
+	mock_file = mock_drm_getfile(i915->drm.primary, O_RDWR);
+	if (IS_ERR(mock_file))
+		return PTR_ERR(mock_file);
+
 	/*
 	 * This will eventually create a GEM context, due to opening dummy drm
 	 * file, which needs a tiny amount of mappable device memory for the top
 	 * level paging structures(and perhaps scratch), so make sure we
 	 * allocate early, to avoid tears.
 	 */
-	addr = igt_mmap_offset(i915, offset, obj->base.size,
-			       PROT_WRITE, MAP_SHARED);
+	addr = igt_mmap_offset_with_file(i915, offset, obj->base.size,
+					 PROT_WRITE, MAP_SHARED, mock_file);
 	if (IS_ERR_VALUE(addr)) {
 		err = addr;
 		goto out_put;
@@ -1299,6 +1305,7 @@ static int __igt_mmap_migrate(struct intel_memory_region **placements,
 	}
 
 out_put:
+	fput(mock_file);
 	i915_gem_object_put(obj);
 	igt_close_objects(i915, &objects);
 	return err;
