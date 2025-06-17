a5a7aa562d36d7a38cf56ef12acd9bd6140de2c5
Alex Hung <alex.hung@amd.com>
2025-06-16 22:17:24 -0600

    drm/colorop: Add 3D LUT support to color pipeline
    It is to be used to enable HDR by allowing userpace to create and pass
    3D LUTs to kernel and hardware.
    
    new drm_colorop_type: DRM_COLOROP_3D_LUT.
    
    Reviewed-by: Simon Ser <contact@emersion.fr>
    Signed-off-by: Alex Hung <alex.hung@amd.com>
    Reviewed-by: Daniel Stone <daniels@collabora.com>
    Reviewed-by: Melissa Wen <mwen@igalia.com>

--- drivers/gpu/drm/drm_atomic_uapi.c
+++ drivers/gpu/drm/drm_atomic_uapi.c
@@ -718,6 +718,10 @@ static int drm_atomic_color_set_data_property(struct drm_colorop *colorop,
 	case DRM_COLOROP_CTM_3X4:
 		size = sizeof(struct drm_color_ctm_3x4);
 		break;
+	case DRM_COLOROP_3D_LUT:
+		size = colorop->lut_size * colorop->lut_size * colorop->lut_size *
+		       sizeof(struct drm_color_lut_32);
+		break;
 	default:
 		/* should never get here */
 		return -EINVAL;
@@ -743,6 +747,8 @@ static int drm_atomic_colorop_set_property(struct drm_colorop *colorop,
 		state->curve_1d_type = val;
 	} else if (property == colorop->multiplier_property) {
 		state->multiplier = val;
+	} else if (property == colorop->lut3d_interpolation_property) {
+		colorop->lut3d_interpolation = val;
 	} else if (property == colorop->data_property) {
 		return drm_atomic_color_set_data_property(colorop, state,
 							  property, val);
@@ -774,6 +780,8 @@ drm_atomic_colorop_get_property(struct drm_colorop *colorop,
 		*val = state->multiplier;
 	} else if (property == colorop->lut_size_property) {
 		*val = colorop->lut_size;
+	} else if (property == colorop->lut3d_interpolation_property) {
+		*val = colorop->lut3d_interpolation;
 	} else if (property == colorop->data_property) {
 		*val = (state->data) ? state->data->base.id : 0;
 	} else {
