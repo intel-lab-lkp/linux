940d66575c9e72dd3c69e17985f109adc695ade3
Oliver Mangold <oliver.mangold@pm.me>
2025-06-18 12:27:49 +0000

    rust: Split `AlwaysRefCounted` into two traits
    `AlwaysRefCounted` is renamed to `RefCounted`.
    
    `AlwaysRefCounted` will become a marker trait to indicate that it is
    allowed to obtain an `ARef<T>` from a `&T`, which cannot be allowed for
    types which are also Ownable.
    
    Signed-off-by: Oliver Mangold <oliver.mangold@pm.me>
    Suggested-by: Alice Ryhl <aliceryhl@google.com>

--- rust/kernel/block/mq/request.rs
+++ rust/kernel/block/mq/request.rs
@@ -8,7 +8,7 @@ use crate::{
     bindings,
     block::mq::Operations,
     error::Result,
-    types::{ARef, AlwaysRefCounted, Opaque},
+    types::{ARef, AlwaysRefCounted, Opaque, RefCounted},
 };
 use core::{
     marker::PhantomData,
@@ -226,11 +226,10 @@ fn atomic_relaxed_op_unless(target: &AtomicU64, op: impl Fn(u64) -> u64, pred: u
         .is_ok()
 }
 
-// SAFETY: All instances of `Request<T>` are reference counted. This
-// implementation of `AlwaysRefCounted` ensure that increments to the ref count
-// keeps the object alive in memory at least until a matching reference count
-// decrement is executed.
-unsafe impl<T: Operations> AlwaysRefCounted for Request<T> {
+// SAFETY: All instances of `Request<T>` are reference counted. This implementation of `RefCounted`
+// ensure that increments to the ref count keeps the object alive in memory at least until a
+// matching reference count decrement is executed.
+unsafe impl<T: Operations> RefCounted for Request<T> {
     fn inc_ref(&self) {
         let refcount = &self.wrapper_ref().refcount();
 
@@ -260,3 +259,7 @@ unsafe impl<T: Operations> AlwaysRefCounted for Request<T> {
         }
     }
 }
+
+// SAFETY: We currently do not implement `Ownable`, thus it is okay to can obtain an `ARef<Request>`
+// from a `&Request` (but this will change in the future).
+unsafe impl<T: Operations> AlwaysRefCounted for Request<T> {}
