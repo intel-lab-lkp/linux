639b66908aa37ad5fa35e0e0f59c1cd7d5a5e9f2
Mario Limonciello <mario.limonciello@amd.com>
2025-06-19 10:24:58 -0500

    thunderbolt: Fix wake on connect at runtime
    commit 1a760d10ded37 ("thunderbolt: Fix a logic error in wake on connect")
    fixated on the USB4 port sysfs wakeup file not working properly to control
    policy, but it had an unintended side effect that the sysfs file controls
    policy both at runtime and at suspend time. The sysfs file is supposed to
    only control behavior while system is suspended.
    
    Pass whether programming a port for runtime into usb4_switch_set_wake()
    and if runtime then ignore the value in the sysfs file.
    
    Cc: stable@vger.kernel.org
    Reported-by: Alexander Kovacs <Alexander.Kovacs@amd.com>
    Tested-by: Alexander Kovacs <Alexander.Kovacs@amd.com>
    Fixes: 1a760d10ded37 ("thunderbolt: Fix a logic error in wake on connect")
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- drivers/thunderbolt/usb4.c
+++ drivers/thunderbolt/usb4.c
@@ -403,10 +403,11 @@ bool usb4_switch_lane_bonding_possible(struct tb_switch *sw)
  * usb4_switch_set_wake() - Enabled/disable wake
  * @sw: USB4 router
  * @flags: Wakeup flags (%0 to disable)
+ * @runtime: Wake is being programmed during system runtime
  *
  * Enables/disables router to wake up from sleep.
  */
-int usb4_switch_set_wake(struct tb_switch *sw, unsigned int flags)
+int usb4_switch_set_wake(struct tb_switch *sw, unsigned int flags, bool runtime)
 {
 	struct usb4_port *usb4;
 	struct tb_port *port;
@@ -438,13 +439,12 @@ int usb4_switch_set_wake(struct tb_switch *sw, unsigned int flags)
 			val |= PORT_CS_19_WOU4;
 		} else {
 			bool configured = val & PORT_CS_19_PC;
+			bool wakeup = runtime || device_may_wakeup(&usb4->dev);
 			usb4 = port->usb4;
 
-			if (((flags & TB_WAKE_ON_CONNECT) &&
-			      device_may_wakeup(&usb4->dev)) && !configured)
+			if ((flags & TB_WAKE_ON_CONNECT) && wakeup && !configured)
 				val |= PORT_CS_19_WOC;
-			if (((flags & TB_WAKE_ON_DISCONNECT) &&
-			      device_may_wakeup(&usb4->dev)) && configured)
+			if ((flags & TB_WAKE_ON_DISCONNECT) && wakeup && configured)
 				val |= PORT_CS_19_WOD;
 			if ((flags & TB_WAKE_ON_USB4) && configured)
 				val |= PORT_CS_19_WOU4;
