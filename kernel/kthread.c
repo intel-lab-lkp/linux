079de970e3454a15118ebbdda164ca1c44888ac4
Jiazi Li <jqqlijiazi@gmail.com>
2025-06-19 20:00:58 +0800

    kthread: update comment for __to_kthread
    With commit 343f4c49f243 ("kthread: Don't allocate kthread_struct
    for init and umh") and commit 753550eb0ce1 ("fork: Explicitly set
    PF_KTHREAD"), umh task no longer have struct kthread and PF_KTHREAD flag.
    Io_uring worker thread does not have PF_KTHREAD flag, but worker_private
    is not null, so update comment for __to_kthread.
    
    Signed-off-by: Jiazi Li <jqqlijiazi@gmail.com>
    Signed-off-by: mingzhu.wang <mingzhu.wang@transsion.com>

--- kernel/kthread.c
+++ kernel/kthread.c
@@ -88,13 +88,9 @@ static inline struct kthread *to_kthread(struct task_struct *k)
 /*
  * Variant of to_kthread() that doesn't assume @p is a kthread.
  *
- * Per construction; when:
- *
- *   (p->flags & PF_KTHREAD) && p->worker_private
- *
- * the task is both a kthread and struct kthread is persistent. However
- * PF_KTHREAD on it's own is not, kernel_thread() can exec() (See umh.c and
- * begin_new_exec()).
+ * For io_uring worker thread, p->worker_private point to io_worker
+ * struct, but they do not have PF_KTHREAD flag.
+ * Return NULL for such tasks.
  */
 static inline struct kthread *__to_kthread(struct task_struct *p)
 {
