486c939f471fcebae091cb3ab9ca96e6e0fc818b
Alexandre Chartre <alexandre.chartre@oracle.com>
2025-06-19 16:56:44 +0200

    objtool: Create disassembly context
    Create a structure to store information for disassembling functions.
    For now, it is just a wrapper around an objtool file.
    
    Signed-off-by: Alexandre Chartre <alexandre.chartre@oracle.com>

--- tools/objtool/check.c
+++ tools/objtool/check.c
@@ -11,6 +11,7 @@
 #include <objtool/builtin.h>
 #include <objtool/cfi.h>
 #include <objtool/arch.h>
+#include <objtool/disas.h>
 #include <objtool/check.h>
 #include <objtool/special.h>
 #include <objtool/warn.h>
@@ -4591,6 +4592,7 @@ static void free_insns(struct objtool_file *file)
 
 int check(struct objtool_file *file)
 {
+	struct disas_context *disas_ctx;
 	int ret = 0, warnings = 0;
 
 	arch_initial_func_cfi_state(&initial_func_cfi);
@@ -4720,7 +4722,9 @@ out:
 		if (opts.werror && warnings)
 			WARN("%d warning(s) upgraded to errors", warnings);
 		print_args();
-		disas_warned_funcs(file);
+		disas_ctx = disas_context_create(file);
+		disas_warned_funcs(disas_ctx);
+		disas_context_destroy(disas_ctx);
 	}
 
 	return ret;
