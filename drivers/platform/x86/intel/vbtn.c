099b0ca747ce65f95baf8417461787949a6317d4
Xiang Shen <turyshen@gmail.com>
2025-06-20 10:38:49 +1000

    platform/x86: intel-vbtn: Fix code style issues
    Fix checkpatch code style errors:
    
    ERROR: do not use assignment in if condition
    +	if ((ke = sparse_keymap_entry_from_scancode(priv->buttons_dev, event))) {
    
    ERROR: do not use assignment in if condition
    +	} else if ((ke = sparse_keymap_entry_from_scancode(priv->switches_dev, event))) {
    
    Signed-off-by: Xiang Shen <turyshen@gmail.com>

--- drivers/platform/x86/intel/vbtn.c
+++ drivers/platform/x86/intel/vbtn.c
@@ -160,30 +160,34 @@ static void notify_handler(acpi_handle handle, u32 event, void *context)
 
 	guard(mutex)(&priv->mutex);
 
-	if ((ke = sparse_keymap_entry_from_scancode(priv->buttons_dev, event))) {
+	ke = sparse_keymap_entry_from_scancode(priv->buttons_dev, event);
+	if (ke) {
 		if (!priv->has_buttons) {
 			dev_warn(&device->dev, "Warning: received 0x%02x button event on a device without buttons, please report this.\n",
 				 event);
 			return;
 		}
 		input_dev = priv->buttons_dev;
-	} else if ((ke = sparse_keymap_entry_from_scancode(priv->switches_dev, event))) {
-		if (!priv->has_switches) {
-			/* See dual_accel_detect.h for more info */
-			if (priv->dual_accel)
-				return;
-
-			dev_info(&device->dev, "Registering Intel Virtual Switches input-dev after receiving a switch event\n");
-			ret = input_register_device(priv->switches_dev);
-			if (ret)
-				return;
-
-			priv->has_switches = true;
-		}
-		input_dev = priv->switches_dev;
 	} else {
-		dev_dbg(&device->dev, "unknown event index 0x%x\n", event);
-		return;
+		ke = sparse_keymap_entry_from_scancode(priv->switches_dev, event);
+		if (ke) {
+			if (!priv->has_switches) {
+				/* See dual_accel_detect.h for more info */
+				if (priv->dual_accel)
+					return;
+
+				dev_info(&device->dev, "Registering Intel Virtual Switches input-dev after receiving a switch event\n");
+				ret = input_register_device(priv->switches_dev);
+				if (ret)
+					return;
+
+				priv->has_switches = true;
+			}
+			input_dev = priv->switches_dev;
+		} else {
+			dev_dbg(&device->dev, "unknown event index 0x%x\n", event);
+			return;
+		}
 	}
 
 	if (priv->wakeup_mode) {
