63556e038705ca2e3e9aa0ea0c9bf3b262a8836b
Mario Limonciello <mario.limonciello@amd.com>
2025-06-19 21:49:42 -0500

    PCI/VGA: Move check for firmware default out of VGA arbiter
    The x86 specific check for whether a framebuffer belongs to a device
    works for display devices as well as VGA devices.  Callers to
    video_is_primary_device() can benefit from checking non-VGA display
    devices.
    
    Move the x86 specific check into x86 specific code, and adjust VGA
    arbiter to call that code as well. This allows fbcon to find the
    right PCI device on systems that don't have VGA devices.
    
    Suggested-by: Thomas Zimmermann <tzimmermann@suse.de>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>

--- arch/x86/video/video-common.c
+++ arch/x86/video/video-common.c
@@ -9,6 +9,7 @@
 
 #include <linux/module.h>
 #include <linux/pci.h>
+#include <linux/screen_info.h>
 #include <linux/vgaarb.h>
 
 #include <asm/video.h>
@@ -27,12 +28,39 @@ EXPORT_SYMBOL(pgprot_framebuffer);
 
 bool video_is_primary_device(struct device *dev)
 {
+	u64 base = screen_info.lfb_base;
+	u64 size = screen_info.lfb_size;
 	struct pci_dev *pdev;
+	struct resource *r;
+	u64 limit;
 
 	if (!dev_is_pci(dev))
 		return false;
 
 	pdev = to_pci_dev(dev);
+
+	if (!pci_is_display(pdev))
+		return false;
+
+	/* Select the device owning the boot framebuffer if there is one */
+	if (screen_info.capabilities & VIDEO_CAPABILITY_64BIT_BASE)
+		base |= (u64)screen_info.ext_lfb_base << 32;
+
+	limit = base + size;
+
+	/* Does firmware framebuffer belong to us? */
+	pci_dev_for_each_resource(pdev, r) {
+		if (resource_type(r) != IORESOURCE_MEM)
+			continue;
+
+		if (!r->start || !r->end)
+			continue;
+
+		if (base < r->start || limit >= r->end)
+			continue;
+
+		return true;
+	}
 
 	return (pdev == vga_default_device());
 }
