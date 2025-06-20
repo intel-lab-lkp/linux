6929ebb890b1e283ade9dda89b80716298d32bac
Alex Guo <alexguo1023@gmail.com>
2025-06-20 17:22:22 -0400

    media: dvb-usb: dw2102: fix null-ptr-deref in dw3101_i2c_transfer
    msg is controlled by user. If accessing msg.buf without sanity
    check, null pointer deref would happen. We add check on
    msg.len to prevent crash.
    
    Similar commit: commit 0ed554fd769a ("media: dvb-usb: az6027:
    fix null-ptr-deref in az6027_i2c_xfer()")
    
    Signed-off-by: Alex Guo <alexguo1023@gmail.com>

--- drivers/media/usb/dvb-usb/dw2102.c
+++ drivers/media/usb/dvb-usb/dw2102.c
@@ -556,7 +556,10 @@ static int dw3101_i2c_transfer(struct i2c_adapter *adap, struct i2c_msg msg[],
 		}
 		case(DW2102_RC_QUERY): {
 			u8 ibuf[2];
-
+			if (msg[0].len < 2) {
+				ret = -EOPNOTSUPP;
+				goto unlock;
+			}
 			dw210x_op_rw(d->udev, 0xb8, 0, 0,
 				     ibuf, 2, DW210X_READ_MSG);
 			memcpy(msg[0].buf, ibuf, 2);
