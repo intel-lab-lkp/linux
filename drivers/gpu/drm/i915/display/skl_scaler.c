aa098a762511cdcf166d33eb81975b526820af43
Suraj Kandpal <suraj.kandpal@intel.com>
2025-06-20 14:55:34 +0530

    drm/i915/xe3lpd: Prune modes for YUV420
    We only support resolution up to 4k for single pipe when using
    YUV420 format so we prune these modes and restrict the plane size
    at src. This is because pipe scaling will not support YUV420 scaling
    for hwidth > 4096.
    
    --v2
    -Use output format to check [Ville]
    -Add Bspec references
    -Modify commit messge to point to why this is needed
    
    --v3
    -Use a function skl_scaler_mode_valid which is routed throug
    intel_pfit_mode_valid [Ville]
    -Combine the check conditons [Jonathan]
    
    --v4
    -mode_valid functions should return drm_mode_status [Jani]
    
    Bspec: 49247, 50441
    Signed-off-by: Suraj Kandpal <suraj.kandpal@intel.com>
    Reviewed-by: Jonathan Cavitt <jonathan.cavitt@intel.com> #v2

--- drivers/gpu/drm/i915/display/skl_scaler.c
+++ drivers/gpu/drm/i915/display/skl_scaler.c
@@ -134,6 +134,22 @@ static void skl_scaler_max_dst_size(struct intel_crtc *crtc,
 	}
 }
 
+enum drm_mode_status
+skl_scaler_mode_valid(struct intel_display *display,
+		      const struct drm_display_mode *mode,
+		      enum intel_output_format output_format,
+		      int num_joined_pipes)
+{
+	if (num_joined_pipes < 2 && output_format == INTEL_OUTPUT_FORMAT_YCBCR420) {
+		if (DISPLAY_VER(display) >= 14 &&
+		    mode->hdisplay > 4096) {
+			return MODE_NO_420;
+		}
+	}
+
+	return MODE_OK;
+}
+
 static int
 skl_update_scaler(struct intel_crtc_state *crtc_state, bool force_detach,
 		  unsigned int scaler_user, int *scaler_id,
