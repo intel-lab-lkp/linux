56dbcdd1cdb7ba502fa3b0d956f1255112b60154
Samiullah Khawaja <skhawaja@google.com>
2025-06-23 17:53:16 +0000

    Add support to set NAPI threaded for individual NAPI
    A net device has a threaded sysctl that can be used to enable threaded
    NAPI polling on all of the NAPI contexts under that device. Allow
    enabling threaded NAPI polling at individual NAPI level using netlink.
    
    Extend the netlink operation `napi-set` and allow setting the threaded
    attribute of a NAPI. This will enable the threaded polling on a NAPI
    context.
    
    Add a test in `nl_netdev.py` that verifies various cases of threaded
    NAPI being set at NAPI and at device level.
    
    Tested
     ./tools/testing/selftests/net/nl_netdev.py
     TAP version 13
     1..7
     ok 1 nl_netdev.empty_check
     ok 2 nl_netdev.lo_check
     ok 3 nl_netdev.page_pool_check
     ok 4 nl_netdev.napi_list_check
     ok 5 nl_netdev.dev_set_threaded
     ok 6 nl_netdev.napi_set_threaded
     ok 7 nl_netdev.nsim_rxq_reset_down
     # Totals: pass:7 fail:0 xfail:0 xpass:0 skip:0 error:0
    
    Signed-off-by: Samiullah Khawaja <skhawaja@google.com>

--- net/core/dev.h
+++ net/core/dev.h
@@ -315,6 +315,19 @@ static inline void napi_set_irq_suspend_timeout(struct napi_struct *n,
 	WRITE_ONCE(n->irq_suspend_timeout, timeout);
 }
 
+/**
+ * napi_get_threaded - get the NAPI threaded state
+ * @n: napi_struct to get the threaded state from
+ *
+ * Return: the per-NAPI threaded state.
+ */
+static inline bool napi_get_threaded(struct napi_struct *n)
+{
+	return test_bit(NAPI_STATE_THREADED, &n->state);
+}
+
+int napi_set_threaded(struct napi_struct *n, bool threaded);
+
 int rps_cpumask_housekeeping(struct cpumask *mask);
 
 #if defined(CONFIG_DEBUG_NET) && defined(CONFIG_BPF_SYSCALL)
