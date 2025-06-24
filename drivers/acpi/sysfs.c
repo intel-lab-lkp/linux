a2fcd73cb5e1bdcb064bc76f6897f1be41628280
Brahmajit Das <listout@listout.xyz>
2025-06-24 19:51:34 +0530

    ACPI / sysfs: Replace deprecated strcpy() with sysfs_emit
    strcpy() is deprecated; use sysfs_emit() instead. No functional changes
    intended.
    
    Link: https://github.com/KSPP/linux/issues/88
    Signed-off-by: Brahmajit Das <listout@listout.xyz>

--- drivers/acpi/sysfs.c
+++ drivers/acpi/sysfs.c
@@ -181,10 +181,9 @@ static int param_set_trace_method_name(const char *val,
 
 	/* This is a hack.  We can't kmalloc in early boot. */
 	if (is_abs_path)
-		strcpy(trace_method_name, val);
+		sysfs_emit(trace_method_name, "%s", val);
 	else {
-		trace_method_name[0] = '\\';
-		strcpy(trace_method_name+1, val);
+		sysfs_emit(trace_method_name, "\%s", val);
 	}
 
 	/* Restore the original tracer state */
