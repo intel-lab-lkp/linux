46d3ae04d32636834c92a387964f950edbb77a41
Alexey Romanov <romanov.alexey2000@gmail.com>
2025-06-24 16:51:58 +0300

    crypto: amlogic - Drop status field from meson_flow
    This field is used only to check for timeout. But there is more
    convenient way to achive the same goal.
    
    Signed-off-by: Alexey Romanov <romanov.alexey2000@gmail.com>
    Reviewed-by: Neil Armstrong <neil.armstrong@linaro.org>

--- drivers/crypto/amlogic/amlogic-gxl-cipher.c
+++ drivers/crypto/amlogic/amlogic-gxl-cipher.c
@@ -219,13 +219,18 @@ static int meson_cipher(struct skcipher_request *areq)
 	}
 
 	reinit_completion(&mc->chanlist[flow].complete);
-	mc->chanlist[flow].status = 0;
 	meson_dma_start(mc, flow);
-	wait_for_completion_interruptible_timeout(&mc->chanlist[flow].complete,
-						  msecs_to_jiffies(500));
-	if (mc->chanlist[flow].status == 0) {
+
+	err = wait_for_completion_interruptible_timeout(&mc->chanlist[flow].complete,
+							msecs_to_jiffies(500));
+	if (err == 0) {
 		dev_err(mc->dev, "DMA timeout for flow %d\n", flow);
 		err = -EINVAL;
+	} else if (err < 0) {
+		dev_err(mc->dev, "Waiting for DMA completion is failed (%d)\n", err);
+	} else {
+		/* No error */
+		err = 0;
 	}
 
 	dma_unmap_single(mc->dev, phykeyiv, keyivlen, DMA_TO_DEVICE);
