e84dc1c2adf11e1109d767b8c710a902a8b522f5
Alejandro Lucero <alucerop@amd.com>
2025-06-24 15:13:45 +0100

    sfc: get endpoint decoder
    Use cxl api for getting DPA (Device Physical Address) to use through an
    endpoint decoder.
    
    Signed-off-by: Alejandro Lucero <alucerop@amd.com>
    Reviewed-by: Martin Habets <habetsm.xilinx@gmail.com>
    Acked-by: Edward Cree <ecree.xilinx@gmail.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>

--- drivers/net/ethernet/sfc/efx_cxl.c
+++ drivers/net/ethernet/sfc/efx_cxl.c
@@ -22,6 +22,7 @@ int efx_cxl_init(struct efx_probe_data *probe_data)
 {
 	struct efx_nic *efx = &probe_data->efx;
 	struct pci_dev *pci_dev = efx->pci_dev;
+	resource_size_t max_size;
 	struct efx_cxl *cxl;
 	u16 dvsec;
 	int rc;
@@ -86,13 +87,42 @@ int efx_cxl_init(struct efx_probe_data *probe_data)
 		return PTR_ERR(cxl->cxlmd);
 	}
 
+	cxl->endpoint = cxl_acquire_endpoint(cxl->cxlmd);
+	if (IS_ERR(cxl->endpoint))
+		return PTR_ERR(cxl->endpoint);
+
+	cxl->cxlrd = cxl_get_hpa_freespace(cxl->cxlmd, 1,
+					   CXL_DECODER_F_RAM | CXL_DECODER_F_TYPE2,
+					   &max_size);
+
+	if (IS_ERR(cxl->cxlrd)) {
+		pci_err(pci_dev, "cxl_get_hpa_freespace failed\n");
+		rc = PTR_ERR(cxl->cxlrd);
+		goto endpoint_release;
+	}
+
+	if (max_size < EFX_CTPIO_BUFFER_SIZE) {
+		pci_err(pci_dev, "%s: not enough free HPA space %pap < %u\n",
+			__func__, &max_size, EFX_CTPIO_BUFFER_SIZE);
+		rc = -ENOSPC;
+		goto put_root_decoder;
+	}
+
 	probe_data->cxl = cxl;
 
-	return 0;
+	goto endpoint_release;
+
+put_root_decoder:
+	cxl_put_root_decoder(cxl->cxlrd);
+endpoint_release:
+	cxl_release_endpoint(cxl->cxlmd, cxl->endpoint);
+	return rc;
 }
 
 void efx_cxl_exit(struct efx_probe_data *probe_data)
 {
+	if (probe_data->cxl)
+		cxl_put_root_decoder(probe_data->cxl->cxlrd);
 }
 
 MODULE_IMPORT_NS("CXL");
