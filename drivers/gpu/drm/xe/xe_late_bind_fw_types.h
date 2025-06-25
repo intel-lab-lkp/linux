12361207743b73f777f8c0218ecd4d8bd2ea2d96
Badal Nilawar <badal.nilawar@intel.com>
2025-06-25 22:30:09 +0530

    drm/xe/xe_late_bind_fw: Initialize late binding firmware
    Search for late binding firmware binaries and populate the meta data of
    firmware structures.
    
    v2 (Daniele):
     - drm_err if firmware size is more than max pay load size
     - s/request_firmware/firmware_request_nowarn/ as firmware will
       not be available for all possible cards
    v3 (Daniele):
     - init firmware from within xe_late_bind_init, propagate error
     - switch late_bind_fw to array to handle multiple firmware types
    v4 (Daniele):
     - Alloc payload dynamically, fix nits
    
    Signed-off-by: Badal Nilawar <badal.nilawar@intel.com>

--- drivers/gpu/drm/xe/xe_late_bind_fw_types.h
+++ drivers/gpu/drm/xe/xe_late_bind_fw_types.h
@@ -10,6 +10,36 @@
 #include <linux/mutex.h>
 #include <linux/types.h>
 
+#define MAX_PAYLOAD_SIZE SZ_4K
+
+/**
+ * xe_late_bind_fw_id - enum to determine late binding fw index
+ */
+enum xe_late_bind_fw_id {
+	XE_LB_FW_FAN_CONTROL = 0,
+	XE_LB_FW_MAX_ID
+};
+
+/**
+ * struct xe_late_bind_fw
+ */
+struct xe_late_bind_fw {
+	/** @late_bind_fw.valid: to check if fw is valid */
+	bool valid;
+	/** @late_bind_fw.id: firmware index */
+	u32 id;
+	/** @late_bind_fw.blob_path: firmware binary path */
+	char blob_path[PATH_MAX];
+	/** @late_bind_fw.type: firmware type */
+	u32  type;
+	/** @late_bind_fw.flags: firmware flags */
+	u32  flags;
+	/** @late_bind_fw.payload: to store the late binding blob */
+	u8  *payload;
+	/** @late_bind_fw.payload_size: late binding blob payload_size */
+	size_t payload_size;
+};
+
 /**
  * struct xe_late_bind_component - Late Binding services component
  * @mei_dev: device that provide Late Binding service.
@@ -32,6 +62,8 @@ struct xe_late_bind {
 	struct xe_late_bind_component component;
 	/** @late_bind.mutex: protects the component binding and usage */
 	struct mutex mutex;
+	/** @late_bind.late_bind_fw: late binding firmware array */
+	struct xe_late_bind_fw late_bind_fw[XE_LB_FW_MAX_ID];
 };
 
 #endif
