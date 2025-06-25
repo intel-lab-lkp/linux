09d5854ff2c4c2d7975f49a77f58a7d70020822b
Raju Rangoju <Raju.Rangoju@amd.com>
2025-06-25 15:23:15 +0530

    amd-xgbe: do not double read link status
    The link status is latched low so that momentary link drops
    can be detected. Avoid double-reading the status to identify
    short link interruptions, unless the link was already down.
    
    This prevents unnecessary duplicate readings of the link status.
    
    Fixes: 4f3b20bfbb75 ("amd-xgbe: add support for rx-adaptation")
    Signed-off-by: Raju Rangoju <Raju.Rangoju@amd.com>

--- drivers/net/ethernet/amd/xgbe/xgbe-phy-v2.c
+++ drivers/net/ethernet/amd/xgbe/xgbe-phy-v2.c
@@ -2746,8 +2746,7 @@ static bool xgbe_phy_valid_speed(struct xgbe_prv_data *pdata, int speed)
 static int xgbe_phy_link_status(struct xgbe_prv_data *pdata, int *an_restart)
 {
 	struct xgbe_phy_data *phy_data = pdata->phy_data;
-	unsigned int reg;
-	int ret;
+	int reg, ret;
 
 	*an_restart = 0;
 
@@ -2780,13 +2779,23 @@ static int xgbe_phy_link_status(struct xgbe_prv_data *pdata, int *an_restart)
 		if (!phy_data->phydev->link)
 			return 0;
 	}
-
-	/* Link status is latched low, so read once to clear
-	 * and then read again to get current state
+	/* Link status is latched low so that momentary link drops
+	 * can be detected. Do not double-read the status to detect
+	 * short link drops except the link was already down.
 	 */
-	reg = XMDIO_READ(pdata, MDIO_MMD_PCS, MDIO_STAT1);
-	reg = XMDIO_READ(pdata, MDIO_MMD_PCS, MDIO_STAT1);
+	if (!pdata->phy.link) {
+		reg = XMDIO_READ(pdata, MDIO_MMD_PCS, MDIO_STAT1);
+		if (reg < 0)
+			return reg;
 
+		if (reg & MDIO_STAT1_LSTATUS)
+			goto skip_read;
+	}
+
+	reg = XMDIO_READ(pdata, MDIO_MMD_PCS, MDIO_STAT1);
+	if (reg < 0)
+		return reg;
+skip_read:
 	if (pdata->en_rx_adap) {
 		/* if the link is available and adaptation is done,
 		 * declare link up
@@ -2804,9 +2813,7 @@ static int xgbe_phy_link_status(struct xgbe_prv_data *pdata, int *an_restart)
 			xgbe_phy_set_mode(pdata, phy_data->cur_mode);
 		}
 
-		/* check again for the link and adaptation status */
-		reg = XMDIO_READ(pdata, MDIO_MMD_PCS, MDIO_STAT1);
-		if ((reg & MDIO_STAT1_LSTATUS) && pdata->rx_adapt_done)
+		if (pdata->rx_adapt_done)
 			return 1;
 	} else if (reg & MDIO_STAT1_LSTATUS)
 		return 1;
