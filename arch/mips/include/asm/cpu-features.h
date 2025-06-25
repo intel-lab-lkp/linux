9c2696ae6cf791584ab1dfa056e96f06f851f03a
Gregory CLEMENT <gregory.clement@bootlin.com>
2025-06-25 15:27:02 +0200

    MIPS: disable MMID if GINVT is not usable
    If System-level Interconnect (aka Network on Chip) does not support
    the global invalidation, then MMID feature is not usable. Indeed the
    current implementation of MMID relies on the GINV* instruction.
    
    Signed-off-by: Gregory CLEMENT <gregory.clement@bootlin.com>

--- arch/mips/include/asm/cpu-features.h
+++ arch/mips/include/asm/cpu-features.h
@@ -642,9 +642,12 @@
  * We only enable MMID support for configurations which natively support 64 bit
  * atomics because getting good performance from the allocator relies upon
  * efficient atomic64_*() functions.
+ * If System-level Interconnect (aka Network on Chip) does not support
+ * the global invalidation, then we also disable MMID support which
+ * needs to use GINV*
  */
 #ifndef cpu_has_mmid
-# ifdef CONFIG_GENERIC_ATOMIC64
+# if defined(CONFIG_GENERIC_ATOMIC64) || defined(CONFIG_GINVT_UNSUPPORTED_NOC)
 #  define cpu_has_mmid		0
 # else
 #  define cpu_has_mmid		__isa_ge_and_opt(6, MIPS_CPU_MMID)
