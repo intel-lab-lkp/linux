419f83b4a1070c3a943d45fb40eed9f8cb4ac660
Dev Jain <dev.jain@arm.com>
2025-06-25 11:28:04 +0530

    khugepaged: Optimize __collapse_huge_page_copy_succeeded() by PTE batching
    Use PTE batching to optimize __collapse_huge_page_copy_succeeded().
    
    On arm64, suppose khugepaged is scanning a pte-mapped 2MB THP for collapse.
    Then, calling ptep_clear() for every pte will cause a TLB flush for every
    contpte block. Instead, clear_full_ptes() does a
    contpte_try_unfold_partial() which will flush the TLB only for the (if any)
    starting and ending contpte block, if they partially overlap with the range
    khugepaged is looking at.
    
    For all arches, there should be a benefit due to batching atomic operations
    on mapcounts due to folio_remove_rmap_ptes().
    
    No issues were observed with mm-selftests.
    
    Signed-off-by: Dev Jain <dev.jain@arm.com>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -700,12 +700,15 @@ static void __collapse_huge_page_copy_succeeded(pte_t *pte,
 						spinlock_t *ptl,
 						struct list_head *compound_pagelist)
 {
+	unsigned long end = address + HPAGE_PMD_SIZE;
 	struct folio *src, *tmp;
-	pte_t *_pte;
 	pte_t pteval;
+	pte_t *_pte;
+	int nr_ptes;
 
-	for (_pte = pte; _pte < pte + HPAGE_PMD_NR;
-	     _pte++, address += PAGE_SIZE) {
+	for (_pte = pte; _pte < pte + HPAGE_PMD_NR; _pte += nr_ptes,
+	     address += nr_ptes * PAGE_SIZE) {
+		nr_ptes = 1;
 		pteval = ptep_get(_pte);
 		if (pte_none(pteval) || is_zero_pfn(pte_pfn(pteval))) {
 			add_mm_counter(vma->vm_mm, MM_ANONPAGES, 1);
@@ -719,21 +722,33 @@ static void __collapse_huge_page_copy_succeeded(pte_t *pte,
 				ksm_might_unmap_zero_page(vma->vm_mm, pteval);
 			}
 		} else {
+			const fpb_t flags = FPB_IGNORE_DIRTY | FPB_IGNORE_SOFT_DIRTY;
+			int max_nr_ptes;
+
 			struct page *src_page = pte_page(pteval);
 
 			src = page_folio(src_page);
 			if (!folio_test_large(src))
 				release_pte_folio(src);
+
+			max_nr_ptes = (end - address) >> PAGE_SHIFT;
+			if (folio_test_large(src))
+				nr_ptes = folio_pte_batch(src, address, _pte,
+							  pteval, max_nr_ptes,
+							  flags, NULL, NULL, NULL);
+
 			/*
 			 * ptl mostly unnecessary, but preempt has to
 			 * be disabled to update the per-cpu stats
 			 * inside folio_remove_rmap_pte().
 			 */
 			spin_lock(ptl);
-			ptep_clear(vma->vm_mm, address, _pte);
-			folio_remove_rmap_pte(src, src_page, vma);
+			clear_full_ptes(vma->vm_mm, address, _pte, nr_ptes,
+					/* full = */ false);
+			folio_remove_rmap_ptes(src, src_page, nr_ptes, vma);
 			spin_unlock(ptl);
-			free_folio_and_swap_cache(src);
+			free_swap_cache(src);
+			folio_put_refs(src, nr_ptes);
 		}
 	}
 
