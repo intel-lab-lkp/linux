2a1012a77d111e08b214361ba4557b3abf56d61a
Josh Poimboeuf <jpoimboe@kernel.org>
2025-06-25 18:56:14 -0400

    unwind_user/x86: Enable compat mode frame pointer unwinding on x86
    Use ARCH_INIT_USER_COMPAT_FP_FRAME to describe how frame pointers are
    unwound on x86, and implement the hooks needed to add the segment base
    addresses.  Enable HAVE_UNWIND_USER_COMPAT_FP if the system has compat
    mode compiled in.
    
    Signed-off-by: Josh Poimboeuf <jpoimboe@kernel.org>
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- include/linux/unwind_user.h
+++ include/linux/unwind_user.h
@@ -14,6 +14,26 @@
  #define in_compat_mode(regs) false
 #endif
 
+/*
+ * If an architecture needs to initialize the state for a specific
+ * reason, for example, it may need to do something different
+ * in compat mode, it can define arch_unwind_user_init to a
+ * function that will perform this initialization.
+ */
+#ifndef arch_unwind_user_init
+static inline void arch_unwind_user_init(struct unwind_user_state *state, struct pt_regs *reg) {}
+#endif
+
+/*
+ * If an architecture requires some more updates to the state between
+ * stack frames, it can define arch_unwind_user_next to a function
+ * that will update the state between reading stack frames during
+ * the user space stack walk.
+ */
+#ifndef arch_unwind_user_next
+static inline void arch_unwind_user_next(struct unwind_user_state *state) {}
+#endif
+
 int unwind_user_start(struct unwind_user_state *state);
 int unwind_user_next(struct unwind_user_state *state);
 
