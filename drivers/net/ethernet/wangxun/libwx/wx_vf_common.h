5aa6e3e4fb28245d2f652182b762357de463d858
Mengyuan Lou <mengyuanlou@net-swift.com>
2025-06-25 18:20:54 +0800

    net: txgbevf: add phylink check flow
    Add phylink support to wangxun 10/25/40G virtual functions.
    Get link status from pf in mbox, and if it is failed then
    check the vx_status, because vx_status switching is too slow.
    
    Signed-off-by: Mengyuan Lou <mengyuanlou@net-swift.com>

--- drivers/net/ethernet/wangxun/libwx/wx_vf_common.h
+++ drivers/net/ethernet/wangxun/libwx/wx_vf_common.h
@@ -14,7 +14,16 @@ void wx_reset_vf(struct wx *wx);
 void wx_set_rx_mode_vf(struct net_device *netdev);
 void wx_configure_vf(struct wx *wx);
 int wx_set_mac_vf(struct net_device *netdev, void *p);
+void wx_get_mac_link_vf(struct phylink_config *config,
+			struct phylink_link_state *state);
 int wxvf_open(struct net_device *netdev);
 int wxvf_close(struct net_device *netdev);
+void wxvf_mac_config(struct phylink_config *config, unsigned int mode,
+		     const struct phylink_link_state *state);
+void wxvf_mac_link_down(struct phylink_config *config, unsigned int mode,
+			phy_interface_t interface);
+void wxvf_mac_link_up(struct phylink_config *config, struct phy_device *phy,
+		      unsigned int mode, phy_interface_t interface,
+		      int speed, int duplex, bool tx_pause, bool rx_pause);
 
 #endif /* _WX_VF_COMMON_H_ */
