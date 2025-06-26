6750a90b433f3f8857f34bb6ba06f7b436e95ab8
Nikunj A Dadhania <nikunj@amd.com>
2025-06-26 11:31:41 +0530

    x86/sev: Use TSC_FACTOR for Secure TSC frequency calculation
    When using Secure TSC, the GUEST_TSC_FREQ MSR reports a frequency based on
    the nominal P0 frequency, which deviates slightly (typically ~0.2%) from
    the actual mean TSC frequency due to clocking parameters. Over extended VM
    uptime, this discrepancy accumulates, causing clock skew between the
    hypervisor and SEV-SNP VM, leading to early timer interrupts as perceived
    by the guest.
    
    The guest kernel relies on the reported nominal frequency for TSC-based
    timekeeping, while the actual frequency set during SNP_LAUNCH_START may
    differ. This mismatch results in inaccurate time calculations, causing the
    guest to perceive hrtimers as firing earlier than expected.
    
    Utilize the TSC_FACTOR from the SEV firmware's secrets page (see "Secrets
    Page Format" in the SNP Firmware ABI Specification) to calculate the mean
    TSC frequency, ensuring accurate timekeeping and mitigating clock skew in
    SEV-SNP VMs.
    
    Use early_ioremap_encrypted() to map the secrets page as
    ioremap_encrypted() uses kmalloc() which is not available during early TSC
    initialization and causes a panic.
    
    Fixes: 73bbf3b0fbba ("x86/tsc: Init the TSC for Secure TSC guests")
    Cc: stable@vger.kernel.org
    Signed-off-by: Nikunj A Dadhania <nikunj@amd.com>

--- arch/x86/coco/sev/core.c
+++ arch/x86/coco/sev/core.c
@@ -2156,20 +2156,32 @@ void __init snp_secure_tsc_prepare(void)
 
 static unsigned long securetsc_get_tsc_khz(void)
 {
-	return snp_tsc_freq_khz;
+	return (unsigned long)snp_tsc_freq_khz;
 }
 
 void __init snp_secure_tsc_init(void)
 {
+	struct snp_secrets_page *secrets;
 	unsigned long long tsc_freq_mhz;
+	void *mem;
 
 	if (!cc_platform_has(CC_ATTR_GUEST_SNP_SECURE_TSC))
 		return;
 
+	mem = early_memremap_encrypted(sev_secrets_pa, PAGE_SIZE);
+	if (!mem) {
+		pr_err("Unable to get TSC_FACTOR: failed to map the SNP secrets page.\n");
+		sev_es_terminate(SEV_TERM_SET_LINUX, GHCB_TERM_SECURE_TSC);
+	}
+
+	secrets = (__force struct snp_secrets_page *)mem;
+
 	setup_force_cpu_cap(X86_FEATURE_TSC_KNOWN_FREQ);
 	rdmsrq(MSR_AMD64_GUEST_TSC_FREQ, tsc_freq_mhz);
-	snp_tsc_freq_khz = (unsigned long)(tsc_freq_mhz * 1000);
+	snp_tsc_freq_khz = SNP_SCALE_TSC_FREQ(tsc_freq_mhz * 1000, secrets->tsc_factor);
 
 	x86_platform.calibrate_cpu = securetsc_get_tsc_khz;
 	x86_platform.calibrate_tsc = securetsc_get_tsc_khz;
+
+	early_memunmap(mem, PAGE_SIZE);
 }
