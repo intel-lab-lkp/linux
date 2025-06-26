f03c2fd8d55a22af0edc4dbfd22252e6ef45d816
Vladimir Moskovkin <Vladimir.Moskovkin@kaspersky.com>
2025-06-26 16:02:18 +0300

    gpio: kempld: Remove redundant check in kempld_gpio_probe()
    In kempld_gpio_probe() function 'pdata' pointer is compared to NULL.
    But in subsequent call to kempld_gpio_pincount() it is dereferenced inside
    kempld_get_mutex() function. kempld-core module ensures that
    kempld_platform_data is not NULL for corresponding device, see
    kempld_acpi_table[]. So 'if (pdata)' check looks illogical and worsens
    code readability.
    
    Remove this check.
    
    Found by Linux Verification Center (linuxtesting.org) with SVACE.
    
    Signed-off-by: Vladimir Moskovkin <Vladimir.Moskovkin@kaspersky.com>

--- drivers/gpio/gpio-kempld.c
+++ drivers/gpio/gpio-kempld.c
@@ -161,7 +161,7 @@ static int kempld_gpio_probe(struct platform_device *pdev)
 	chip->owner = THIS_MODULE;
 	chip->parent = dev;
 	chip->can_sleep = true;
-	if (pdata && pdata->gpio_base)
+	if (pdata->gpio_base)
 		chip->base = pdata->gpio_base;
 	else
 		chip->base = -1;
