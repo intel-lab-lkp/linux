5d05a453b95350dfebdd0d0902fc1bf43b53f19e
GONG Ruiqi <gongruiqi1@huawei.com>
2025-06-26 10:36:17 +0800

    integrity: Extract secure boot enquiry function out of IMA
    Commit 92ad19559ea9 ("integrity: Do not load MOK and MOKx when secure
    boot be disabled") utilizes arch_ima_get_secureboot() to perform a
    secure boot status check before loading the Machine Owner Key (MOK).
    However, only when CONFIG_IMA_SECURE_AND_OR_TRUSTED_BOOT=y can this
    function be functional, while this config has nothing to do with secure
    boot or MOK loading.
    
    Given that arch_ima_get_secureboot() is just a helper to retrieve info
    about secure boot via EFI and doesn't necessarily be a part of IMA,
    rename it to arch_integrity_get_secureboot(), decouple its functionality
    from IMA and extract it to be a integrity subsystem helper, so that both
    certificate loading and IMA can make use of it.
    
    Compile-tested for powerpc, s390 and x86, all with allmodconfig.
    
    Signed-off-by: GONG Ruiqi <gongruiqi1@huawei.com>

--- arch/s390/kernel/ima_arch.c
+++ arch/s390/kernel/ima_arch.c
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <linux/ima.h>
+#include <linux/integrity.h>
 #include <asm/boot_data.h>
 
-bool arch_ima_get_secureboot(void)
+bool arch_integrity_get_secureboot(void)
 {
 	return ipl_secure_flag;
 }
