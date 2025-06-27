a6e5b7573388a6ba96119b0038ee135be32a34f3
Vitaly Wool <vitaly.wool@konsulko.se>
2025-06-27 20:16:38 +0200

    rust: add support for NUMA ids in allocations
    Add support for specifying NUMA ids in Rust allocators as an Option
    (i. e. providing `None` as nid corresponds to NUMA_NO_NODE). To do
    this, modify ReallocFunc to use the new extended realloc primitives
    from the C side of the kernel (i. e. k[v]realloc_node/vrealloc_node)
    and add the new function alloc_node to the Allocator trait while
    keeping the existing one (alloc) for backward compatibility.
    
    This will allow to specify node to use for allocation of e. g.
    {KV}Box, as well as for future NUMA aware users of the API.
    
    Signed-off-by: Vitaly Wool <vitaly.wool@konsulko.se>

--- rust/helpers/slab.c
+++ rust/helpers/slab.c
@@ -13,3 +13,15 @@ rust_helper_kvrealloc(const void *p, size_t size, gfp_t flags)
 {
 	return kvrealloc(p, size, flags);
 }
+
+void * __must_check __realloc_size(2)
+rust_helper_krealloc_node(const void *objp, size_t new_size, gfp_t flags, int node)
+{
+	return krealloc_node(objp, new_size, flags, node);
+}
+
+void * __must_check __realloc_size(2)
+rust_helper_kvrealloc_node(const void *p, size_t size, gfp_t flags, int node)
+{
+	return kvrealloc_node(p, size, flags, node);
+}
