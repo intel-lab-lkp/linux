29c4fbc7573583c349b84cfe4c06fb3729d732b0
Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>
2025-06-27 16:51:01 +0100

    soundwire: bus: add sdw_slave_get_current_bank helper
    There has been 2 instances of this helper in codec drivers,
    it does not make sense to keep duplicating this part of code.
    
    Lets add a helper sdw_get_current_bank() for codec drivers to use it.
    
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>

--- drivers/soundwire/bus.c
+++ drivers/soundwire/bus.c
@@ -1360,6 +1360,13 @@ int sdw_slave_get_scale_index(struct sdw_slave *slave, u8 *base)
 }
 EXPORT_SYMBOL(sdw_slave_get_scale_index);
 
+int sdw_slave_get_current_bank(struct sdw_slave *sdev)
+{
+	return FIELD_GET(SDW_SCP_STAT_CURR_BANK,
+			 sdw_read(sdev, SDW_SCP_CTRL));
+}
+EXPORT_SYMBOL_GPL(sdw_slave_get_current_bank);
+
 static int sdw_slave_set_frequency(struct sdw_slave *slave)
 {
 	int scale_index;
