923afa08b1e2a65e8828a317d6b1ad04c1a7df5d
Jani Nikula <jani.nikula@intel.com>
2025-06-27 14:36:27 +0300

    drm/i915/vblank: use generic read_poll_timeout() instead of wait_for()
    Prefer generic poll helpers over i915 custom helpers.
    
    The functional change is losing the exponentially growing sleep of
    wait_for(), which used to be 10, 20, 40, ..., 640, and 1280 us.
    
    Use an arbitrary constant 500 us sleep instead. The timeout remains at
    100 ms.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_vblank.c
+++ drivers/gpu/drm/i915/display/intel_vblank.c
@@ -3,6 +3,8 @@
  * Copyright © 2022-2023 Intel Corporation
  */
 
+#include <linux/iopoll.h>
+
 #include <drm/drm_vblank.h>
 
 #include "i915_drv.h"
@@ -492,9 +494,15 @@ static void wait_for_pipe_scanline_moving(struct intel_crtc *crtc, bool state)
 {
 	struct intel_display *display = to_intel_display(crtc);
 	enum pipe pipe = crtc->pipe;
+	bool is_moving;
+	int ret;
 
 	/* Wait for the display line to settle/start moving */
-	if (wait_for(pipe_scanline_is_moving(display, pipe) == state, 100))
+	ret = read_poll_timeout(pipe_scanline_is_moving, is_moving,
+				is_moving == state,
+				500, 100 * 1000, false,
+				display, pipe);
+	if (ret)
 		drm_err(display->drm,
 			"pipe %c scanline %s wait timed out\n",
 			pipe_name(pipe), str_on_off(state));
