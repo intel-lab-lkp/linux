964f351073a87f2c1879d044c6f9281fd28afab6
Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>
2025-06-27 22:36:13 +0300

    drivers: staging: gpib: minor fixes for some C macros.
    Ran checkpatch.pl on drivers/staging/gpib/, found the following:
     - gpio: gpib_bitbang.c: wrapped LINVAL macro w/ parenthesis.
     - hp_82341: hp_82341.c: Used comments instead of "#if 0"
     - tnt4882: tnt4882_gpib.c: Used comments instead of "#if 0"
    
    Signed-off-by: Abdalla Al-Dalleh <abdalla.ahmad@sesame.org.jo>

--- drivers/staging/gpib/tnt4882/tnt4882_gpib.c
+++ drivers/staging/gpib/tnt4882/tnt4882_gpib.c
@@ -1369,14 +1369,14 @@ static struct pci_driver tnt4882_pci_driver = {
 	.probe = &tnt4882_pci_probe
 };
 
-#if 0
 /* unused, will be needed when the driver is turned into a pnp_driver */
-static const struct pnp_device_id tnt4882_pnp_table[] = {
-	{.id = "NICC601"},
-	{.id = ""}
-};
-MODULE_DEVICE_TABLE(pnp, tnt4882_pnp_table);
-#endif
+/*
+ * static const struct pnp_device_id tnt4882_pnp_table[] = {
+ *	{.id = "NICC601"},
+ *	{.id = ""}
+ * };
+ * MODULE_DEVICE_TABLE(pnp, tnt4882_pnp_table);
+ */
 
 #ifdef CONFIG_GPIB_PCMCIA
 static struct gpib_interface ni_pcmcia_interface;
