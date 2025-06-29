04a7d6a7caaee2fca719d3c3f414f4ca52602f18
Rob Clark <robdclark@chromium.org>
2025-06-29 13:12:56 -0700

    drm/msm: drm_gpuvm conversion
    Now that we've realigned deletion and allocation, switch over to using
    drm_gpuvm/drm_gpuva.  This allows us to support multiple VMAs per BO per
    VM, to allow mapping different parts of a single BO at different virtual
    addresses, which is a key requirement for sparse/VM_BIND.
    
    This prepares us for using drm_gpuvm to translate a batch of MAP/
    MAP_NULL/UNMAP operations from userspace into a sequence of map/remap/
    unmap steps for updating the page tables.
    
    Since, unlike our prior vm/vma setup, with drm_gpuvm the vm_bo holds a
    reference to the GEM object.  To prevent reference loops causing us to
    leak all GEM objects, we implicitly tear down the mapping when the GEM
    handle is close or when the obj is unpinned.  Which means the submit
    needs to also hold a reference to the vm_bo, to prevent the VMA from
    being torn down while the submit is in-flight.
    
    Signed-off-by: Rob Clark <robdclark@chromium.org>
    Signed-off-by: Rob Clark <robin.clark@oss.qualcomm.com>
    Tested-by: Antonino Maniscalco <antomani103@gmail.com>
    Reviewed-by: Antonino Maniscalco <antomani103@gmail.com>
    Patchwork: https://patchwork.freedesktop.org/patch/661479/

--- drivers/gpu/drm/msm/msm_gem_submit.c
+++ drivers/gpu/drm/msm/msm_gem_submit.c
@@ -322,7 +322,8 @@ static int submit_pin_objects(struct msm_gem_submit *submit)
 		if (ret)
 			break;
 
-		submit->bos[i].iova = vma->iova;
+		submit->bos[i].vm_bo = drm_gpuvm_bo_get(vma->base.vm_bo);
+		submit->bos[i].iova = vma->base.va.addr;
 	}
 
 	/*
@@ -475,7 +476,11 @@ void msm_submit_retire(struct msm_gem_submit *submit)
 
 	for (i = 0; i < submit->nr_bos; i++) {
 		struct drm_gem_object *obj = submit->bos[i].obj;
+		struct drm_gpuvm_bo *vm_bo = submit->bos[i].vm_bo;
 
+		msm_gem_lock(obj);
+		drm_gpuvm_bo_put(vm_bo);
+		msm_gem_unlock(obj);
 		drm_gem_object_put(obj);
 	}
 }
