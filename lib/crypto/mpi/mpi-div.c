846c42302ca463654eea498f623231d0761bee23
Gu Bowen <gubowen5@huawei.com>
2025-06-30 21:39:31 +0800

    Revert "Revert "lib/mpi: Extend the MPI library""
    This reverts commit fca5cb4dd2b4a9423cb6d112cc71c33899955a1f.
    
    Reintroduce the mpi library based on libgcrypt to support sm2.
    
    Signed-off-by: Gu Bowen <gubowen5@huawei.com>

--- lib/crypto/mpi/mpi-div.c
+++ lib/crypto/mpi/mpi-div.c
@@ -15,6 +15,7 @@
 #include "longlong.h"
 
 int mpi_tdiv_qr(MPI quot, MPI rem, MPI num, MPI den);
+void mpi_fdiv_qr(MPI quot, MPI rem, MPI dividend, MPI divisor);
 
 int mpi_fdiv_r(MPI rem, MPI dividend, MPI divisor)
 {
@@ -44,6 +45,34 @@ free_temp_divisor:
 	mpi_free(temp_divisor);
 
 	return err;
+}
+
+void mpi_fdiv_q(MPI quot, MPI dividend, MPI divisor)
+{
+	MPI tmp = mpi_alloc(mpi_get_nlimbs(quot));
+	mpi_fdiv_qr(quot, tmp, dividend, divisor);
+	mpi_free(tmp);
+}
+
+void mpi_fdiv_qr(MPI quot, MPI rem, MPI dividend, MPI divisor)
+{
+	int divisor_sign = divisor->sign;
+	MPI temp_divisor = NULL;
+
+	if (quot == divisor || rem == divisor) {
+		temp_divisor = mpi_copy(divisor);
+		divisor = temp_divisor;
+	}
+
+	mpi_tdiv_qr(quot, rem, dividend, divisor);
+
+	if ((divisor_sign ^ dividend->sign) && rem->nlimbs) {
+		mpi_sub_ui(quot, quot, 1);
+		mpi_add(rem, rem, divisor);
+	}
+
+	if (temp_divisor)
+		mpi_free(temp_divisor);
 }
 
 /* If den == quot, den needs temporary storage.
