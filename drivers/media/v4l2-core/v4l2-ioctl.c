3c4456e20b0d589607d6462cddd1dd3b691ea440
Ricardo Ribalda <ribalda@chromium.org>
2025-06-30 14:20:32 +0000

    media: core: export v4l2_translate_cmd
    video_translate_cmd() can be useful for drivers to convert between the
    VIDIOC_*32 and VIDIOC_ defines. Let's export it.
    
    Now that the function is exported, use this opportunity to rename the
    function with the v4l2_ prefix, that is less ambiguous than video_
    
    The VIDIOC_*32 defines are not accessible by the drivers, they live in
    v4l2-compat-ioctl32.c.
    
    Signed-off-by: Ricardo Ribalda <ribalda@chromium.org>

--- drivers/media/v4l2-core/v4l2-ioctl.c
+++ drivers/media/v4l2-core/v4l2-ioctl.c
@@ -3245,7 +3245,7 @@ static int check_array_args(unsigned int cmd, void *parg, size_t *array_size,
 	return ret;
 }
 
-static unsigned int video_translate_cmd(unsigned int cmd)
+unsigned int v4l2_translate_cmd(unsigned int cmd)
 {
 #if !defined(CONFIG_64BIT) && defined(CONFIG_COMPAT_32BIT_TIME)
 	switch (cmd) {
@@ -3266,6 +3266,7 @@ static unsigned int video_translate_cmd(unsigned int cmd)
 
 	return cmd;
 }
+EXPORT_SYMBOL(v4l2_translate_cmd);
 
 static int video_get_user(void __user *arg, void *parg,
 			  unsigned int real_cmd, unsigned int cmd,
@@ -3426,7 +3427,7 @@ video_usercopy(struct file *file, unsigned int orig_cmd, unsigned long arg,
 	size_t  array_size = 0;
 	void __user *user_ptr = NULL;
 	void	**kernel_ptr = NULL;
-	unsigned int cmd = video_translate_cmd(orig_cmd);
+	unsigned int cmd = v4l2_translate_cmd(orig_cmd);
 	const size_t ioc_size = _IOC_SIZE(cmd);
 
 	/*  Copy arguments into temp kernel buffer  */
