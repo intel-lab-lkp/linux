ae4b58a8cfa265a361c0807ea1ac6ee481ab6b49
Qu Wenruo <wqu@suse.com>
2025-07-01 15:02:39 +0930

    btrfs: implement remove_bdev super operation callback
    For this callback, btrfs will:
    
    - Go degraded if the fs can still maintain RW operations
      And of course mark the target device as missing.
    
    - Shutdown if the fs can not maintain RW operations
    
    To support the lookup from bdev to a btrfs_device,
    btrfs_dev_lookup_args is enhanced to have a new @devt member.
    If set, we should be able to use that @devt member to uniquely locating a
    btrfs device.
    
    I know the shutdown can be a little overkilled, if one has a RAID1
    metadata and RAID0 data, in that case one can still read data with 50%
    chance to got some good data.
    
    But a filesystem returning -EIO for half of the time is not really
    considered usable.
    Further it can also be as bad as the only device went missing for a single
    device btrfs.
    
    So here we go safe other than sorry when handling missing device.
    
    And the remove_bdev callback will be hidden behind experimental features
    for now, the reasons are:
    
    - There are not enough btrfs specific bdev removal test cases
      The existing test cases are all removing the only device, thus only
      exercises the shutdown branch.
    
    - Not yet determined what's the expected behavior
      Although the current auto-degrade behavior is no worse than the old
      behavior, it may not always be what the end users want.
    
      Before there is a concrete interface, better hide the new feature
      from end users.
    
    Signed-off-by: Qu Wenruo <wqu@suse.com>

--- fs/btrfs/super.c
+++ fs/btrfs/super.c
@@ -2423,6 +2423,48 @@ static long btrfs_free_cached_objects(struct super_block *sb, struct shrink_cont
 	return 0;
 }
 
+#ifdef CONFIG_BTRFS_EXPERIMENTAL
+static void btrfs_remove_bdev(struct super_block *sb, struct block_device *bdev)
+{
+	struct btrfs_fs_info *fs_info = btrfs_sb(sb);
+	struct btrfs_device *device;
+	struct btrfs_dev_lookup_args lookup_args = { .devt = bdev->bd_dev };
+	bool can_rw;
+
+	mutex_lock(&fs_info->fs_devices->device_list_mutex);
+	device = btrfs_find_device(fs_info->fs_devices, &lookup_args);
+	if (!device) {
+		btrfs_warn(fs_info, "unable to find btrfs device for block device '%pg'",
+			   bdev);
+		mutex_unlock(&fs_info->fs_devices->device_list_mutex);
+		return;
+	}
+	set_bit(BTRFS_DEV_STATE_MISSING, &device->dev_state);
+	device->fs_devices->missing_devices++;
+	if (test_and_clear_bit(BTRFS_DEV_STATE_WRITEABLE, &device->dev_state)) {
+		list_del_init(&device->dev_alloc_list);
+		device->fs_devices->rw_devices--;
+	}
+	can_rw = btrfs_check_rw_degradable(fs_info, device);
+	mutex_unlock(&fs_info->fs_devices->device_list_mutex);
+	/*
+	 * Now device is considered missing, btrfs_device_name() won't give a
+	 * meaningful result anymore, so only output the devid.
+	 */
+	if (!can_rw) {
+		btrfs_crit(fs_info,
+		"btrfs device id %llu has gone missing, can not maintain read-write",
+			   device->devid);
+		btrfs_force_shutdown(fs_info);
+		return;
+	}
+	btrfs_warn(fs_info,
+		   "btrfs device id %llu has gone missing, continue as degraded",
+		   device->devid);
+	btrfs_set_opt(fs_info->mount_opt, DEGRADED);
+}
+#endif
+
 static const struct super_operations btrfs_super_ops = {
 	.drop_inode	= btrfs_drop_inode,
 	.evict_inode	= btrfs_evict_inode,
@@ -2438,6 +2480,9 @@ static const struct super_operations btrfs_super_ops = {
 	.unfreeze_fs	= btrfs_unfreeze,
 	.nr_cached_objects = btrfs_nr_cached_objects,
 	.free_cached_objects = btrfs_free_cached_objects,
+#ifdef CONFIG_BTRFS_EXPERIMENTAL
+	.remove_bdev	= btrfs_remove_bdev,
+#endif
 };
 
 static const struct file_operations btrfs_ctl_fops = {
