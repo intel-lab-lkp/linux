b6fcff626b717d4d33f93a4ec2228309a3b71fb9
Dheeraj Kumar Srivastava <dheerajkumar.srivastava@amd.com>
2025-07-02 15:07:57 +0530

    iommu/amd: Refactor AMD IOMMU debugfs initial setup
    Rearrange initial setup of AMD IOMMU debugfs to segregate per IOMMU
    setup and setup which is common for all IOMMUs. This ensures that common
    debugfs paths (introduced in subsequent patches) are created only once
    instead of being created for each IOMMU.
    
    With the change, there is no need to use lock as amd_iommu_debugfs_setup()
    will be called only once during AMD IOMMU initialization. So remove lock
    acquisition in amd_iommu_debugfs_setup().
    
    Signed-off-by: Dheeraj Kumar Srivastava <dheerajkumar.srivastava@amd.com>
    Reviewed-by: Suravee Suthikulpanit <suravee.suthikulpanit@amd.com>
    Reviewed-by: Vasant Hegde <vasant.hegde@amd.com>

--- drivers/iommu/amd/init.c
+++ drivers/iommu/amd/init.c
@@ -3376,7 +3376,6 @@ int amd_iommu_enable_faulting(unsigned int cpu)
  */
 static int __init amd_iommu_init(void)
 {
-	struct amd_iommu *iommu;
 	int ret;
 
 	ret = iommu_go_to_state(IOMMU_INITIALIZED);
@@ -3390,8 +3389,8 @@ static int __init amd_iommu_init(void)
 	}
 #endif
 
-	for_each_iommu(iommu)
-		amd_iommu_debugfs_setup(iommu);
+	if (!ret)
+		amd_iommu_debugfs_setup();
 
 	return ret;
 }
