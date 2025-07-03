cdfff9a71230adeb79699de32af9e22970082839
Badal Nilawar <badal.nilawar@intel.com>
2025-07-04 01:01:01 +0530

    drm/xe/xe_late_bind_fw: Load late binding firmware
    Load late binding firmware
    
    v2:
     - s/EAGAIN/EBUSY/
     - Flush worker in suspend and driver unload (Daniele)
    v3:
     - Use retry interval of 6s, in steps of 200ms, to allow
       other OS components release MEI CL handle (Sasha)
    v4:
     - return -ENODEV if component not added (Daniele)
     - parse and print status returned by csc
    v5:
     - Use payload to check firmware valid (Daniele)
     - Obtain the RPM reference before scheduling the worker to
       ensure the device remains awake until the worker completes
       firmware loading (Rodrigo)
    v6:
     - In case of error donot re-attempt fw download (Daniele)
    
    Signed-off-by: Badal Nilawar <badal.nilawar@intel.com>

--- drivers/gpu/drm/xe/xe_late_bind_fw_types.h
+++ drivers/gpu/drm/xe/xe_late_bind_fw_types.h
@@ -9,6 +9,7 @@
 #include <linux/iosys-map.h>
 #include <linux/mutex.h>
 #include <linux/types.h>
+#include <linux/workqueue.h>
 
 #define XE_LB_MAX_PAYLOAD_SIZE SZ_4K
 
@@ -36,6 +37,8 @@ struct xe_late_bind_fw {
 	const u8  *payload;
 	/** @late_bind_fw.payload_size: late binding blob payload_size */
 	size_t payload_size;
+	/** @late_bind_fw.work: worker to upload latebind blob */
+	struct work_struct work;
 };
 
 /**
@@ -60,6 +63,10 @@ struct xe_late_bind {
 	struct xe_late_bind_component component;
 	/** @late_bind.late_bind_fw: late binding firmware array */
 	struct xe_late_bind_fw late_bind_fw[XE_LB_FW_MAX_ID];
+	/** @late_bind.wq: workqueue to submit request to download late bind blob */
+	struct workqueue_struct *wq;
+	/** @late_bind.component_added: whether the component has been added */
+	bool component_added;
 };
 
 #endif
