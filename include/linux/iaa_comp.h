4d9d295bdce2225d847ea4784279c9c6194eda53
Kanchana P Sridhar <kanchana.p.sridhar@intel.com>
2025-07-03 21:23:08 -0700

    crypto: iaa - Rearchitect the iaa_crypto driver to be usable by zswap and zram.
    This patch rearchitects the iaa_crypto driver to be usable by
    non-crypto_acomp kernel users such as zram. The crypto_acomp interface
    is also preserved for use by zswap. The core driver code is moved under
    a crypto_acomp-agnostic layer that relies only on idxd, dma and
    scatterlist.
    
    Additionally, this patch resolves a race condition triggered when
    IAA wqs and devices are continuously disabled/enabled when workloads are
    using IAA for compression/decompression. This commit, in combination
    with patches 0002 ("crypto: iaa - New architecture for IAA device WQ
    comp/decomp usage & core mapping.) and 0005 (crypto: iaa - iaa_wq uses
    percpu_refs for get/put reference counting.) in this series fix the race
    condition. This has been verified using bisecting.
    
    The newly added include/linux/iaa_comp.h provides the data structures
    and API for use by non-crypto_acomp kernel code such as zram.
    
    This allows kernel users i.e., zswap and zram, to use IAA's hardware
    acceleration for compression/decompression without/with crypto_acomp.
    
    Towards this goal, most of the driver code has been made independent of
    crypto_acomp, by introducing a new "struct iaa_req" data structure, and
    light-weight internal translation routines to/from crypto_acomp, namely,
    acomp_to_iaa() and iaa_to_acomp().
    
    The exception is that the driver defines a "static struct crypto_acomp
    *deflate_crypto_comp" for the software decompress fall-back
    path. Hopefully this shouldn't be an issue for zram because it is
    encapsulated within the iaa_crypto driver.
    
    The acomp_alg .compress() and .decompress() interfaces call into
    iaa_comp_acompress_main() and iaa_comp_adecompress_main(), which are
    wrappers around the core crypto-independent driver functions.
    
    A zram/zcomp backend for iaa_crypto will be submitted as a separate
    patch series, using these interfaces from iaa_comp.h:
    
           int iaa_comp_compress(enum iaa_mode mode, struct iaa_req *req);
    
           int iaa_comp_decompress(enum iaa_mode mode, struct iaa_req *req);
    
    These iaa_crypto interfaces will continue to be available through
    crypto_acomp for use in zswap:
    
           int crypto_acomp_compress(struct acomp_req *req);
           int crypto_acomp_decompress(struct acomp_req *req);
    
    Some other changes introduced by this commit are:
    
    1) iaa_crypto symbol namespace is changed from "IDXD" to
       "CRYPTO_DEV_IAA_CRYPTO".
    
    2) Some constants and data structures are moved to
       include/linux/iaa_comp.h so as to be usable in developing the zram
       iaa_crypto backend.
    
    Fixes: ea7a5cbb4369 ("crypto: iaa - Add Intel IAA Compression Accelerator crypto driver core")
    Signed-off-by: Kanchana P Sridhar <kanchana.p.sridhar@intel.com>

---
+++ include/linux/iaa_comp.h
@@ -0,0 +1,86 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/* Copyright(c) 2021 Intel Corporation. All rights rsvd. */
+
+#ifndef __IAA_COMP_H__
+#define __IAA_COMP_H__
+
+#if IS_ENABLED(CONFIG_CRYPTO_DEV_IAA_CRYPTO)
+
+#include <linux/scatterlist.h>
+
+#define IAA_COMP_MODES_MAX  IAA_MODE_NONE
+
+enum iaa_mode {
+	IAA_MODE_FIXED = 0,
+	IAA_MODE_NONE = 1,
+};
+
+struct iaa_req {
+	struct scatterlist *src;
+	struct scatterlist *dst;
+	unsigned int slen;
+	unsigned int dlen;
+	u32 flags;
+	u32 compression_crc;
+	void *drv_data; /* for driver internal use */
+};
+
+extern bool iaa_comp_enabled(void);
+
+extern enum iaa_mode iaa_comp_get_compressor_mode(const char *compressor_name);
+
+extern bool iaa_comp_mode_is_registered(enum iaa_mode mode);
+
+extern u8 iaa_comp_get_modes(char **iaa_mode_names, enum iaa_mode *iaa_modes);
+
+extern void iaa_comp_put_modes(char **iaa_mode_names, enum iaa_mode *iaa_modes, u8 nr_modes);
+
+extern int iaa_comp_compress(enum iaa_mode mode, struct iaa_req *req);
+
+extern int iaa_comp_decompress(enum iaa_mode mode, struct iaa_req *req);
+
+#else /* CONFIG_CRYPTO_DEV_IAA_CRYPTO */
+
+enum iaa_mode {
+	IAA_MODE_NONE = 1,
+};
+
+struct iaa_req {};
+
+static inline bool iaa_comp_enabled(void)
+{
+	return false;
+}
+
+static inline enum iaa_mode iaa_comp_get_compressor_mode(const char *compressor_name)
+{
+	return IAA_MODE_NONE;
+}
+
+static inline bool iaa_comp_mode_is_registered(enum iaa_mode mode)
+{
+	return false;
+}
+
+static inline u8 iaa_comp_get_modes(char **iaa_mode_names, enum iaa_mode *iaa_modes)
+{
+	return 0;
+}
+
+static inline void iaa_comp_put_modes(char **iaa_mode_names, enum iaa_mode *iaa_modes, u8 nr_modes)
+{
+}
+
+static inline int iaa_comp_compress(enum iaa_mode mode, struct iaa_req *req)
+{
+	return -EINVAL;
+}
+
+static inline int iaa_comp_decompress(enum iaa_mode mode, struct iaa_req *req)
+{
+	return -EINVAL;
+}
+
+#endif /* CONFIG_CRYPTO_DEV_IAA_CRYPTO */
+
+#endif
