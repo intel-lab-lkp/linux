4e34c7210fe9cf82d7c3672fee45fea3db98a496
Markus Theil <theil.markus@gmail.com>
2025-07-04 14:52:31 +0200

    prandom/random32: switch to Xoshiro256++
    The current Linux PRNG is based on LFSR113, which means:
    - needs some warmup rounds to yield better statistical properties
    - seeds/initial states must be of certain structure
    - does not pass L’Ecuyer's BigCrush in TestU01
    
    While of course, there is no clear "best" PRNG, replace with
    Xoshiro256++, which seams to be a sensible replacement, from
    todays point of view:
    - only needs one bit set to 1 in the seed, needs no warmup, when
      seeded with splitmix64.
    - Also has statistical evaluation, like LFSR113.
    - Passes BigCrush in TestU01.
    
    The code got smaller, because some edge cases are ruled out now.
    I kept the test vectors and adapted them to this RNG.
    
    Signed-off-by: Markus Theil <theil.markus@gmail.com>

--- include/linux/prandom.h
+++ include/linux/prandom.h
@@ -14,39 +14,19 @@
 #include <linux/random.h>
 
 struct rnd_state {
-	__u32 s1, s2, s3, s4;
+	__u64 s[4];
 };
 
+/* WARNING: this API MUST NOT be used for cryptographic purposes! */
+u64 prandom_u64_state(struct rnd_state *state);
 /* WARNING: this API MUST NOT be used for cryptographic purposes! */
 u32 prandom_u32_state(struct rnd_state *state);
 /* WARNING: this API MUST NOT be used for cryptographic purposes! */
 void prandom_bytes_state(struct rnd_state *state, void *buf, size_t nbytes);
 void prandom_seed_full_state(struct rnd_state __percpu *pcpu_state);
+void prandom_seed_state(struct rnd_state *state, u64 seed);
 
 #define prandom_init_once(pcpu_state)			\
 	DO_ONCE(prandom_seed_full_state, (pcpu_state))
-
-/*
- * Handle minimum values for seeds
- */
-static inline u32 __seed(u32 x, u32 m)
-{
-	return (x < m) ? x + m : x;
-}
-
-/**
- * prandom_seed_state - set seed for prandom_u32_state().
- * @state: pointer to state structure to receive the seed.
- * @seed: arbitrary 64-bit value to use as a seed.
- */
-static inline void prandom_seed_state(struct rnd_state *state, u64 seed)
-{
-	u32 i = ((seed >> 32) ^ (seed << 10) ^ seed) & 0xffffffffUL;
-
-	state->s1 = __seed(i,   2U);
-	state->s2 = __seed(i,   8U);
-	state->s3 = __seed(i,  16U);
-	state->s4 = __seed(i, 128U);
-}
 
 #endif
