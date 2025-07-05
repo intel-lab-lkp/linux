451e5352572e9baeba53d4d0bbfa86554bfb20a7
Ge Yang <yangge1116@126.com>
2025-07-05 18:16:56 +0800

    efi/tpm: Fix the issue where the CC platforms event log header can't be correctly identified
    Since commit d228814b1913 ("efi/libstub: Add get_event_log() support
    for CC platforms") reuses TPM2 support code for the CC platforms, when
    launching a TDX virtual machine with coco measurement enabled, the
    following error log is generated:
    
    [Firmware Bug]: Failed to parse event in TPM Final Events Log
    
    Call Trace:
    efi_config_parse_tables()
      efi_tpm_eventlog_init()
        tpm2_calc_event_log_size()
          __calc_tpm2_event_size()
    
    The pcr_idx value in the Intel TDX log header is 1, causing the function
    __calc_tpm2_event_size() to fail to recognize the log header, ultimately
    leading to the "Failed to parse event in TPM Final Events Log" error.
    
    According to UEFI Specification 2.10, Section 38.4.1: For TDX, TPM PCR
    0 maps to MRTD, so the log header uses TPM PCR 1 instead. To successfully
    parse the TDX event log header, the check for a pcr_idx value of 0
    must be skipped.
    
    According to Table 6 in Section 10.2.1 of the TCG PC Client
    Specification, the index field does not require the PCR index to be
    fixed at zero. Therefore, skipping the check for a pcr_idx value of
    0 for CC platforms is safe.
    
    Link: https://uefi.org/specs/UEFI/2.10/38_Confidential_Computing.html#intel-trust-domain-extension
    Link: https://trustedcomputinggroup.org/wp-content/uploads/TCG_PCClient_PFP_r1p05_v23_pub.pdf
    Fixes: d228814b1913 ("efi/libstub: Add get_event_log() support for CC platforms")
    Signed-off-by: Ge Yang <yangge1116@126.com>
    Cc: stable@vger.kernel.org

--- drivers/firmware/efi/tpm.c
+++ drivers/firmware/efi/tpm.c
@@ -23,7 +23,8 @@ static int __init tpm2_calc_event_log_size(void *data, int count, void *size_inf
 
 	while (count > 0) {
 		header = data + size;
-		event_size = __calc_tpm2_event_size(header, size_info, true);
+		event_size = __calc_tpm2_event_size(header, size_info, true,
+				     cc_platform_has(CC_ATTR_GUEST_STATE_ENCRYPT));
 		if (event_size == 0)
 			return -1;
 		size += event_size;
