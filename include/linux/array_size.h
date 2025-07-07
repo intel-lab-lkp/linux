cbf35b174e286ceebdfdd1186100a602e82c747c
Alejandro Colomar <alx@kernel.org>
2025-07-07 07:06:13 +0200

    array_size.h: Add ENDOF()
    This macro is useful to calculate the second argument to seprintf(),
    avoiding off-by-one bugs.
    
    Cc: Kees Cook <kees@kernel.org>
    Cc: Christopher Bazley <chris.bazley.wg14@gmail.com>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- include/linux/array_size.h
+++ include/linux/array_size.h
@@ -10,4 +10,10 @@
  */
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]) + __must_be_array(arr))
 
+/**
+ * ENDOF - get a pointer to one past the last element in array @a
+ * @a: array
+ */
+#define ENDOF(a)  (a + ARRAY_SIZE(a))
+
 #endif  /* _LINUX_ARRAY_SIZE_H */
