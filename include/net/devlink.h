98363cc632449ecda14beea7b05c9c38feb441d7
Jedrzej Jagielski <jedrzej.jagielski@intel.com>
2025-07-07 10:58:36 +0200

    devlink: allow driver to freely name interfaces
    Currently when adding devlink port it is prohibited to let
    a driver name an interface on its own. In some scenarios
    it would not be preferable to provide such limitation,
    eg some compatibility purposes.
    
    Add flag skip_phys_port_name_get to devlink_port_attrs struct
    which indicates if devlink should not alter name of interface.
    
    Suggested-by: Jiri Pirko <jiri@resnulli.us>
    Signed-off-by: Jedrzej Jagielski <jedrzej.jagielski@intel.com>

--- include/net/devlink.h
+++ include/net/devlink.h
@@ -78,6 +78,7 @@ struct devlink_port_pci_sf_attrs {
  * @flavour: flavour of the port
  * @split: indicates if this is split port
  * @splittable: indicates if the port can be split.
+ * @skip_phys_port_name_get: if set devlink doesn't alter interface name
  * @lanes: maximum number of lanes the port supports. 0 value is not passed to netlink.
  * @switch_id: if the port is part of switch, this is buffer with ID, otherwise this is NULL
  * @phys: physical port attributes
@@ -87,7 +88,11 @@ struct devlink_port_pci_sf_attrs {
  */
 struct devlink_port_attrs {
 	u8 split:1,
-	   splittable:1;
+	   splittable:1,
+	   skip_phys_port_name_get:1; /* This is for compatibility only,
+				       * newly added driver/port instance
+				       * should never set this.
+				       */
 	u32 lanes;
 	enum devlink_port_flavour flavour;
 	struct netdev_phys_item_id switch_id;
