1df426165ca790ab0771e4f9cdc2ef2a5cad7545
Joanne Koong <joannelkoong@gmail.com>
2025-07-07 16:46:05 -0700

    fuse: use default writeback accounting
    commit 0c58a97f919c ("fuse: remove tmp folio for writebacks and internal
    rb tree") removed temp folios for dirty page writeback. Consequently,
    fuse can now use the default writeback accounting.
    
    With switching fuse to use default writeback accounting, there are some
    added benefits. This updates wb->writeback_inodes tracking as well now
    and updates writeback throughput estimates after writeback completion.
    
    This commit also removes inc_wb_stat() and dec_wb_stat(). These have no
    callers anymore now that fuse does not call them.
    
    Signed-off-by: Joanne Koong <joannelkoong@gmail.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1784,19 +1784,15 @@ static void fuse_writepage_finish(struct fuse_writepage_args *wpa)
 	struct fuse_args_pages *ap = &wpa->ia.ap;
 	struct inode *inode = wpa->inode;
 	struct fuse_inode *fi = get_fuse_inode(inode);
-	struct backing_dev_info *bdi = inode_to_bdi(inode);
 	int i;
 
-	for (i = 0; i < ap->num_folios; i++) {
+	for (i = 0; i < ap->num_folios; i++)
 		/*
 		 * Benchmarks showed that ending writeback within the
 		 * scope of the fi->lock alleviates xarray lock
 		 * contention and noticeably improves performance.
 		 */
 		folio_end_writeback(ap->folios[i]);
-		dec_wb_stat(&bdi->wb, WB_WRITEBACK);
-		wb_writeout_inc(&bdi->wb);
-	}
 
 	wake_up(&fi->page_waitq);
 }
@@ -1982,14 +1978,11 @@ static void fuse_writepage_add_to_bucket(struct fuse_conn *fc,
 static void fuse_writepage_args_page_fill(struct fuse_writepage_args *wpa, struct folio *folio,
 					  uint32_t folio_index)
 {
-	struct inode *inode = folio->mapping->host;
 	struct fuse_args_pages *ap = &wpa->ia.ap;
 
 	ap->folios[folio_index] = folio;
 	ap->descs[folio_index].offset = 0;
 	ap->descs[folio_index].length = folio_size(folio);
-
-	inc_wb_stat(&inode_to_bdi(inode)->wb, WB_WRITEBACK);
 }
 
 static struct fuse_writepage_args *fuse_writepage_args_setup(struct folio *folio,
