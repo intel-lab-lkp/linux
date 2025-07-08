61d46f6afa14f1c3127a9cf53c1da5400a4021a0
Ibrahim Burak Yorulmaz <iburaky.dev@gmail.com>
2025-07-08 02:47:39 +0200

    platform/x86: Add panel orientation quirk for Chuwi MiniBook X
    The Chuwi MiniBook X (CWI558) uses a tablet screen which is oriented
    incorrectly by default. This adds a DMI quirk to rotate the panel into
    the correct orientation.
    
    Signed-off-by: Ibrahim Burak Yorulmaz <iburaky.dev@gmail.com>

--- drivers/gpu/drm/drm_panel_orientation_quirks.c
+++ drivers/gpu/drm/drm_panel_orientation_quirks.c
@@ -282,6 +282,12 @@ static const struct dmi_system_id orientation_data[] = {
 		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "Hi10 pro tablet"),
 		},
 		.driver_data = (void *)&lcd1200x1920_rightside_up,
+	}, {	/* Chuwi MiniBook X (CWI558) */
+		.matches = {
+		  DMI_MATCH(DMI_SYS_VENDOR, "CHUWI"),
+		  DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "MiniBook X"),
+		},
+		.driver_data = (void *)&lcd1200x1920_rightside_up,
 	}, {	/* Dynabook K50 */
 		.matches = {
 		  DMI_EXACT_MATCH(DMI_SYS_VENDOR, "Dynabook Inc."),
