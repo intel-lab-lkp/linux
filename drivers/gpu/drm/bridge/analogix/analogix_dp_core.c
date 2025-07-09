8c125816a5fafeca6a948fc1f8ad9bccec2d033f
Damon Ding <damon.ding@rock-chips.com>
2025-07-09 15:01:33 +0800

    drm/bridge: analogix_dp: Remove redundant &analogix_dp_plat_data.skip_connector
    The &analogix_dp_plat_data.skip_connector related check can be replaced
    by &analogix_dp_plat_data.bridge.
    
    Signed-off-by: Damon Ding <damon.ding@rock-chips.com>

--- drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
+++ drivers/gpu/drm/bridge/analogix/analogix_dp_core.c
@@ -1050,7 +1050,7 @@ static int analogix_dp_bridge_attach(struct drm_bridge *bridge,
 		return -EINVAL;
 	}
 
-	if (!dp->plat_data->skip_connector) {
+	if (!dp->plat_data->bridge) {
 		connector = &dp->connector;
 		connector->polled = DRM_CONNECTOR_POLL_HPD;
 
