74788c2f55aa768b813426d2d0b4a94db68e0a62
Riana Tauro <riana.tauro@intel.com>
2025-07-09 16:50:18 +0530

    drm/xe/doc: Document device wedged and runtime survivability
    Add documentation for vendor specific device wedged recovery method
    and runtime survivability.
    
    v2: fix documentation (Raag)
    
    Signed-off-by: Riana Tauro <riana.tauro@intel.com>

--- drivers/gpu/drm/xe/xe_device.c
+++ drivers/gpu/drm/xe/xe_device.c
@@ -1120,6 +1120,23 @@ static void xe_device_wedged_fini(struct drm_device *drm, void *arg)
 }
 
 /**
+ * DOC: Device Wedging
+ *
+ * Xe driver uses device wedged uevent as documented in Documentation/gpu/drm-uapi.rst.
+ *
+ * When device is in wedged state, every IOCTL will be blocked and GT cannot be
+ * used. Certain critical errors like gt reset failure, firmware failures can cause
+ * the device to be wedged. The default recovery mechanism for a wedged state
+ * is re-probe (unbind + bind)
+ *
+ * However, CSC firmware errors require a firmware flash to restore normal device
+ * operation. Since firmware flash is a vendor-specific action ``WEDGED=vendor-specific``
+ * recovery method along with :ref:`runtime survivability mode <xe-survivability-mode>`
+ * is used to notify userspace. User can then initiate a firmware flash to restore
+ * device to normal operation.
+ */
+
+/*
  * xe_device_set_wedged_method - Set wedged recovery method
  * @xe: xe device instance
  * @method: recovery method to set
