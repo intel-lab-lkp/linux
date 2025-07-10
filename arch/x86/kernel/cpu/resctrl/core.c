ffd35c81636eeb25340c3f152ff26cee0366be9f
Babu Moger <babu.moger@amd.com>
2025-07-10 12:16:16 -0500

    x86/resctrl: Add SDCIAE feature in the command line options
    Add the command line options to enable or disable the new resctrl feature
    L3 Smart Data Cache Injection Allocation Enforcement (SDCIAE).
    
    Signed-off-by: Babu Moger <babu.moger@amd.com>

--- arch/x86/kernel/cpu/resctrl/core.c
+++ arch/x86/kernel/cpu/resctrl/core.c
@@ -707,6 +707,7 @@ enum {
 	RDT_FLAG_MBA,
 	RDT_FLAG_SMBA,
 	RDT_FLAG_BMEC,
+	RDT_FLAG_SDCIAE,
 };
 
 #define RDT_OPT(idx, n, f)	\
@@ -732,6 +733,7 @@ static struct rdt_options rdt_options[]  __ro_after_init = {
 	RDT_OPT(RDT_FLAG_MBA,	    "mba",	X86_FEATURE_MBA),
 	RDT_OPT(RDT_FLAG_SMBA,	    "smba",	X86_FEATURE_SMBA),
 	RDT_OPT(RDT_FLAG_BMEC,	    "bmec",	X86_FEATURE_BMEC),
+	RDT_OPT(RDT_FLAG_SDCIAE,    "sdciae",	X86_FEATURE_SDCIAE),
 };
 #define NUM_RDT_OPTIONS ARRAY_SIZE(rdt_options)
 
