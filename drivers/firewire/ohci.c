07135031fe0a4ad19538d7bdae368725495387ee
Purva Yeshi <purvayeshi550@gmail.com>
2025-07-10 13:09:06 +0530

    firewire: ohci: Initialize payload_bus to avoid uninitialized use warning
    Fix Smatch-detected error:
    drivers/firewire/ohci.c:1514 at_context_queue_packet()
    error: uninitialized symbol 'payload_bus'.
    
    Smatch reports a potential uninitialized use of 'payload_bus' in
    at_context_queue_packet(). If packet->payload_length is zero, the
    variable may not be set before reaching the dma_unmap_single() call,
    which could lead to undefined behavior.
    
    Initialize 'payload_bus' to 0 to ensure it has a defined value in all
    code paths, preventing any uninitialized access.
    
    Signed-off-by: Purva Yeshi <purvayeshi550@gmail.com>

--- drivers/firewire/ohci.c
+++ drivers/firewire/ohci.c
@@ -1386,7 +1386,7 @@ static int at_context_queue_packet(struct context *ctx,
 				   struct fw_packet *packet)
 {
 	struct fw_ohci *ohci = ctx->ohci;
-	dma_addr_t d_bus, payload_bus;
+	dma_addr_t d_bus, payload_bus = 0;
 	struct driver_data *driver_data;
 	struct descriptor *d, *last;
 	__le32 *header;
