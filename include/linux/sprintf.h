39a36bd6c0fa6dd22b5c9a140b109f4d2e520ecb
Alejandro Colomar <alx@kernel.org>
2025-07-10 04:48:56 +0200

    sprintf: Add [V]SPRINTF_END()
    These macros take the end of the array argument implicitly to avoid
    programmer mistakes.  This guarantees that the input is an array, unlike
    
    	snprintf(buf, sizeof(buf), ...);
    
    which is dangerous if the programmer passes a pointer instead of an
    array.
    
    These macros are essentially the same as the 2-argument version of
    strscpy(), but with a formatted string, and returning a pointer to the
    terminating '\0' (or NULL, on error).
    
    Cc: Rasmus Villemoes <linux@rasmusvillemoes.dk>
    Cc: Marco Elver <elver@google.com>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Alejandro Colomar <alx@kernel.org>

--- include/linux/sprintf.h
+++ include/linux/sprintf.h
@@ -4,6 +4,10 @@
 
 #include <linux/compiler_attributes.h>
 #include <linux/types.h>
+#include <linux/array_size.h>
+
+#define SPRINTF_END(a, fmt, ...)  sprintf_end(a, ENDOF(a), fmt, ##__VA_ARGS__)
+#define VSPRINTF_END(a, fmt, ap)  vsprintf_end(a, ENDOF(a), fmt, ap)
 
 int num_to_str(char *buf, int size, unsigned long long num, unsigned int width);
 
