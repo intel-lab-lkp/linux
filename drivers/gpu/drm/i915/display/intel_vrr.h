603552b28b2d1c04e68768e90138d9f74ae2beaf
Jouni Högander <jouni.hogander@intel.com>
2025-07-11 12:08:56 +0300

    drm/i915/vrr: Prepare to Use TRANS_PUSH mechanism for PSR frame change
    On Lunarlake and onwards it is possible to generate PSR "frame change"
    event using TRANS_PUSH mechanism. Implement function to enable this and
    take PSR into account in intel_vrr_send_push.
    
    v4:
      - use rmw when enabling/disabling transcoder
      - set TRANS_PUSH_EN conditionally in intel_vrr_send_push
      - do not call intel_vrr_send_push from intel_psr_trigger_frame_change
      - do not enable using TRANS_PUSH mechanism for PSR "Frame Change"
    v3:
      - use rmw when enabling/disabling
      - keep LNL_TRANS_PUSH_PSR_PR_EN set always on LunarLake and onwards
    v2: use intel_vrr_trans_push_enabled_set_clear instead of rmw
    
    Signed-off-by: Jouni Högander <jouni.hogander@intel.com>

--- drivers/gpu/drm/i915/display/intel_vrr.h
+++ drivers/gpu/drm/i915/display/intel_vrr.h
@@ -30,6 +30,7 @@ void intel_vrr_check_push_sent(struct intel_dsb *dsb,
 			       const struct intel_crtc_state *crtc_state);
 bool intel_vrr_is_push_sent(const struct intel_crtc_state *crtc_state);
 void intel_vrr_disable(const struct intel_crtc_state *old_crtc_state);
+void intel_vrr_psr_frame_change_enable(const struct intel_crtc_state *crtc_state);
 void intel_vrr_get_config(struct intel_crtc_state *crtc_state);
 int intel_vrr_vmax_vtotal(const struct intel_crtc_state *crtc_state);
 int intel_vrr_vmin_vtotal(const struct intel_crtc_state *crtc_state);
