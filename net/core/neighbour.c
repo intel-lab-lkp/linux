ad22f40e206b2c238bc720bf577e8cc9e0f98a7e
Kuniyuki Iwashima <kuniyu@google.com>
2025-07-11 19:06:14 +0000

    neighbour: Drop read_lock_bh(&tbl->lock) in pneigh_dump_table().
    Now pneigh_entry is guaranteed to be alive during the
    RCU critical section even without holding tbl->lock.
    
    Let's drop read_lock_bh(&tbl->lock) and use rcu_dereference()
    to iterate tbl->phash_buckets[] in pneigh_dump_table()
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>

--- net/core/neighbour.c
+++ net/core/neighbour.c
@@ -2801,12 +2801,11 @@ static int pneigh_dump_table(struct neigh_table *tbl, struct sk_buff *skb,
 	if (filter->dev_idx || filter->master_idx)
 		flags |= NLM_F_DUMP_FILTERED;
 
-	read_lock_bh(&tbl->lock);
-
 	for (h = s_h; h <= PNEIGH_HASHMASK; h++) {
 		if (h > s_h)
 			s_idx = 0;
-		for (n = tbl->phash_buckets[h], idx = 0; n; n = n->next) {
+		for (n = rcu_dereference(tbl->phash_buckets[h]), idx = 0; n;
+		     n = rcu_dereference(n->next)) {
 			if (idx < s_idx || pneigh_net(n) != net)
 				goto next;
 			if (neigh_ifindex_filtered(n->dev, filter->dev_idx) ||
@@ -2815,16 +2814,13 @@ static int pneigh_dump_table(struct neigh_table *tbl, struct sk_buff *skb,
 			err = pneigh_fill_info(skb, n, NETLINK_CB(cb->skb).portid,
 					       cb->nlh->nlmsg_seq,
 					       RTM_NEWNEIGH, flags, tbl);
-			if (err < 0) {
-				read_unlock_bh(&tbl->lock);
+			if (err < 0)
 				goto out;
-			}
 		next:
 			idx++;
 		}
 	}
 
-	read_unlock_bh(&tbl->lock);
 out:
 	cb->args[3] = h;
 	cb->args[4] = idx;
