e62735cba7c5048ba8c119101023cd5982f3bc8a
Suchit Karunakaran <suchitkarunakaran@gmail.com>
2025-07-13 20:42:24 +0530

    perf stat: Fix JSON output formatting in iostat_prefix()
    The iostat_prefix() function previously included a TODO noting that its output
    format was incorrect in JSON mode. This patch corrects that by conditionally
    formatting the prefix string based on the output mode specified in
    perf_stat_config. Note, I didn't test it since my system doesn't support
    it.
    
    Signed-off-by: Suchit Karunakaran <suchitkarunakaran@gmail.com>

--- tools/perf/arch/x86/util/iostat.c
+++ tools/perf/arch/x86/util/iostat.c
@@ -403,18 +403,34 @@ void iostat_prefix(struct evlist *evlist,
 	struct iio_root_port *rp = evlist->selected->priv;
 
 	if (rp) {
-		/*
-		 * TODO: This is the incorrect format in JSON mode.
-		 *       See prepare_timestamp()
-		 */
-		if (ts)
-			sprintf(prefix, "%6lu.%09lu%s%04x:%02x%s",
-				ts->tv_sec, ts->tv_nsec,
-				config->csv_sep, rp->domain, rp->bus,
-				config->csv_sep);
-		else
-			sprintf(prefix, "%04x:%02x%s", rp->domain, rp->bus,
-				config->csv_sep);
+		if (config->json_output) {
+			if (ts) {
+				double timestamp = ts->tv_sec + (double)ts->tv_nsec / 1e9;
+				sprintf(prefix, "{\"interval\": %.6f, \"device\": \"%04x:%02x\", ",
+					timestamp, rp->domain, rp->bus);
+			} else {
+				sprintf(prefix, "{\"device\": \"%04x:%02x\", ",
+					rp->domain, rp->bus);
+			}
+		} else if (config->csv_output) {
+			if (ts) {
+				sprintf(prefix, "%lu.%09lu%s%04x:%02x%s",
+					(unsigned long)ts->tv_sec, ts->tv_nsec,
+					config->csv_sep, rp->domain, rp->bus, config->csv_sep);
+			} else {
+				sprintf(prefix, "%04x:%02x%s",
+					rp->domain, rp->bus, config->csv_sep);
+			}
+		} else {
+			if (ts) {
+				sprintf(prefix, "%6lu.%09lu %04x:%02x ",
+					(unsigned long)ts->tv_sec, ts->tv_nsec,
+					rp->domain, rp->bus);
+			} else {
+				sprintf(prefix, "%04x:%02x ",
+					rp->domain, rp->bus);
+			}
+		}
 	}
 }
 
