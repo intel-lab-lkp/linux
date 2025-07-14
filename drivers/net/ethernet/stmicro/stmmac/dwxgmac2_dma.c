591a494bfaccfef7586b178a14406d9862022a92
Rohan G Thomas <rohan.g.thomas@altera.com>
2025-07-14 15:59:18 +0800

    net: stmmac: xgmac: Correct supported speed modes
    Correct supported speed modes as per the XGMAC databook.
    Commit 9cb54af214a7 ("net: stmmac: Fix IP-cores specific
    MAC capabilities") removes support for 10M, 100M and
    1000HD. 1000HD is not supported by XGMAC IP, but it does
    support 10M and 100M FD mode, and it also supports 10M and
    100M HD mode if the HDSEL bit is set in the MAC_HW_FEATURE0
    reg. This commit adds support for 10M and 100M speed modes
    for XGMAC IP.
    
    Fixes: 9cb54af214a7 ("net: stmmac: Fix IP-cores specific MAC capabilities")
    Signed-off-by: Rohan G Thomas <rohan.g.thomas@altera.com>
    Reviewed-by: Matthew Gerlach <matthew.gerlach@altera.com>

--- drivers/net/ethernet/stmicro/stmmac/dwxgmac2_dma.c
+++ drivers/net/ethernet/stmicro/stmmac/dwxgmac2_dma.c
@@ -405,6 +405,7 @@ static int dwxgmac2_get_hw_feature(void __iomem *ioaddr,
 	dma_cap->sma_mdio = (hw_cap & XGMAC_HWFEAT_SMASEL) >> 5;
 	dma_cap->vlhash = (hw_cap & XGMAC_HWFEAT_VLHASH) >> 4;
 	dma_cap->half_duplex = (hw_cap & XGMAC_HWFEAT_HDSEL) >> 3;
+	dma_cap->mbps_10_100 = (hw_cap & XGMAC_HWFEAT_GMIISEL) >> 1;
 	dma_cap->mbps_1000 = (hw_cap & XGMAC_HWFEAT_GMIISEL) >> 1;
 
 	/* MAC HW feature 1 */
