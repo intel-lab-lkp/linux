ff5aff0284cb5d27bd81dc0fce80c0a07ed86928
Brian Foster <bfoster@redhat.com>
2025-07-14 16:41:16 -0400

    filemap: add helper to look up dirty folios in a range
    Add a new filemap_get_folios_dirty() helper to look up existing dirty
    folios in a range and add them to a folio_batch. This is to support
    optimization of certain iomap operations that only care about dirty
    folios in a target range. For example, zero range only zeroes the subset
    of dirty pages over unwritten mappings, seek hole/data may use similar
    logic in the future, etc.
    
    Note that the helper is intended for use under internal fs locks.
    Therefore it trylocks folios in order to filter out clean folios.
    This loosely follows the logic from filemap_range_has_writeback().
    
    Signed-off-by: Brian Foster <bfoster@redhat.com>
    Reviewed-by: Christoph Hellwig <hch@lst.de>

--- include/linux/pagemap.h
+++ include/linux/pagemap.h
@@ -941,6 +941,8 @@ unsigned filemap_get_folios_contig(struct address_space *mapping,
 		pgoff_t *start, pgoff_t end, struct folio_batch *fbatch);
 unsigned filemap_get_folios_tag(struct address_space *mapping, pgoff_t *start,
 		pgoff_t end, xa_mark_t tag, struct folio_batch *fbatch);
+unsigned filemap_get_folios_dirty(struct address_space *mapping,
+		pgoff_t *start, pgoff_t end, struct folio_batch *fbatch);
 
 /*
  * Returns locked page at given index in given cache, creating it if needed.
