7a57d07e232f5dacb8982d296cf974576b5ff733
Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
2025-07-15 14:37:41 +0100

    mm/mseal: Simplify and rename VMA gap check
    The check_mm_seal() function is doing something general - checking whether
    a range contains only VMAs (or rather that it does NOT contain any
    unmapped regions).
    
    So rename this function to range_contains_unmapped().
    
    Additionally simplify the logic, we are simply checking whether the last
    vma->vm_end has either a VMA starting after it or ends before the end
    parameter.
    
    This check is rather dubious, so it is sensible to keep it local to
    mm/mseal.c as at a later stage it may be removed, and we don't want any
    other mm code to perform such a check.
    
    No functional change intended.
    
    Link: https://lkml.kernel.org/r/8efef79c46553a9b8924878c1f4363998c1f49ed.1752586090.git.lorenzo.stoakes@oracle.com
    Signed-off-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Reviewed-by: Liam R. Howlett <Liam.Howlett@oracle.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Cc: Jann Horn <jannh@google.com>
    Cc: Jeff Xu <jeffxu@chromium.org>
    Cc: Pedro Falcato <pfalcato@suse.de>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/mseal.c
+++ mm/mseal.c
@@ -38,34 +38,6 @@ out:
 }
 
 /*
- * Check for do_mseal:
- * 1> start is part of a valid vma.
- * 2> end is part of a valid vma.
- * 3> No gap (unallocated address) between start and end.
- * 4> map is sealable.
- */
-static int check_mm_seal(unsigned long start, unsigned long end)
-{
-	struct vm_area_struct *vma;
-	unsigned long nstart = start;
-	VMA_ITERATOR(vmi, current->mm, start);
-
-	/* going through each vma to check. */
-	for_each_vma_range(vmi, vma, end) {
-		if (vma->vm_start > nstart)
-			/* unallocated memory found. */
-			return -ENOMEM;
-
-		if (vma->vm_end >= end)
-			return 0;
-
-		nstart = vma->vm_end;
-	}
-
-	return -ENOMEM;
-}
-
-/*
  * Apply sealing.
  */
 static int apply_mm_seal(unsigned long start, unsigned long end)
@@ -100,6 +72,24 @@ static int apply_mm_seal(unsigned long start, unsigned long end)
 	}
 
 	return 0;
+}
+
+/* Does the [start, end) range contain any unmapped memory? */
+static bool range_contains_unmapped(struct mm_struct *mm,
+		unsigned long start, unsigned long end)
+{
+	struct vm_area_struct *vma;
+	unsigned long prev_end = start;
+	VMA_ITERATOR(vmi, current->mm, start);
+
+	for_each_vma_range(vmi, vma, end) {
+		if (vma->vm_start > prev_end)
+			return true;
+
+		prev_end = vma->vm_end;
+	}
+
+	return prev_end < end;
 }
 
 /*
@@ -184,14 +174,10 @@ int do_mseal(unsigned long start, size_t len_in, unsigned long flags)
 	if (mmap_write_lock_killable(mm))
 		return -EINTR;
 
-	/*
-	 * First pass, this helps to avoid
-	 * partial sealing in case of error in input address range,
-	 * e.g. ENOMEM error.
-	 */
-	ret = check_mm_seal(start, end);
-	if (ret)
+	if (range_contains_unmapped(mm, start, end)) {
+		ret = -ENOMEM;
 		goto out;
+	}
 
 	/*
 	 * Second pass, this should success, unless there are errors
