09f65ae718266ff6e63ec85f5cb34a381a7a8422
Sean Anderson <sean.anderson@linux.dev>
2025-07-14 21:20:22 -0400

    hwmon: iio: Add min/max support
    Add support for minimum/maximum attributes. Like the _input attribute,
    we just need to call into the IIO API.
    
    Signed-off-by: Sean Anderson <sean.anderson@linux.dev>

--- drivers/hwmon/iio_hwmon.c
+++ drivers/hwmon/iio_hwmon.c
@@ -95,6 +95,54 @@ static ssize_t iio_hwmon_read_val(struct device *dev,
 	return sprintf(buf, "%d\n", result);
 }
 
+static ssize_t iio_hwmon_read_event(struct device *dev,
+				    struct device_attribute *attr,
+				    char *buf)
+{
+	struct sensor_device_attribute_2 *sattr = to_sensor_dev_attr_2(attr);
+	struct iio_hwmon_state *state = dev_get_drvdata(dev);
+	struct iio_channel *chan = &state->channels[sattr->index];
+	int ret, result, scale;
+
+	scale = iio_hwmon_scale(chan);
+	if (scale < 0)
+		return scale;
+
+	ret = iio_read_event_processed_scale(chan, IIO_EV_TYPE_THRESH,
+					     sattr->nr, IIO_EV_INFO_VALUE,
+					     &result, scale);
+	if (ret < 0)
+		return ret;
+
+	return sprintf(buf, "%d\n", result);
+}
+
+static ssize_t iio_hwmon_write_event(struct device *dev,
+				     struct device_attribute *attr,
+				     const char *buf, size_t count)
+{
+	struct sensor_device_attribute_2 *sattr = to_sensor_dev_attr_2(attr);
+	struct iio_hwmon_state *state = dev_get_drvdata(dev);
+	struct iio_channel *chan = &state->channels[sattr->index];
+	int ret, scale, val;
+
+	ret = kstrtoint(buf, 0, &val);
+	if (ret)
+		return ret;
+
+	scale = iio_hwmon_scale(chan);
+	if (scale < 0)
+		return scale;
+
+	ret = iio_write_event_processed_scale(chan, IIO_EV_TYPE_THRESH,
+					      sattr->nr, IIO_EV_INFO_VALUE,
+					      val, scale);
+	if (ret < 0)
+		return ret;
+
+	return count;
+}
+
 static int add_device_attr(struct device *dev, struct iio_hwmon_state *st,
 			   ssize_t (*show)(struct device *dev,
 					   struct device_attribute *attr,
@@ -118,6 +166,40 @@ static int add_device_attr(struct device *dev, struct iio_hwmon_state *st,
 	a->dev_attr.show = show;
 	a->dev_attr.attr.mode = 0444;
 	a->index = i;
+
+	st->attrs[st->num_attrs++] = &a->dev_attr.attr;
+	return 0;
+}
+
+static int add_event_attr(struct device *dev, struct iio_hwmon_state *st,
+			  int i, enum iio_event_direction dir,
+			  const char *fmt, ...)
+{
+	struct sensor_device_attribute_2 *a;
+	umode_t mode;
+	va_list ap;
+
+	mode = iio_event_mode(&st->channels[i], IIO_EV_TYPE_THRESH, dir,
+			      IIO_EV_INFO_VALUE);
+	if (!mode)
+		return 0;
+
+	a = devm_kzalloc(dev, sizeof(*a), GFP_KERNEL);
+	if (!a)
+		return -ENOMEM;
+
+	sysfs_attr_init(&a->dev_attr.attr);
+	va_start(ap, fmt);
+	a->dev_attr.attr.name = devm_kvasprintf(dev, GFP_KERNEL, fmt, ap);
+	va_end(ap);
+	if (!a->dev_attr.attr.name)
+		return -ENOMEM;
+
+	a->dev_attr.show = iio_hwmon_read_event;
+	a->dev_attr.store = iio_hwmon_write_event;
+	a->dev_attr.attr.mode = mode;
+	a->index = i;
+	a->nr = dir;
 
 	st->attrs[st->num_attrs++] = &a->dev_attr.attr;
 	return 0;
@@ -156,7 +238,7 @@ static int iio_hwmon_probe(struct platform_device *pdev)
 		st->num_channels++;
 
 	st->attrs = devm_kcalloc(dev,
-				 2 * st->num_channels + 1, sizeof(*st->attrs),
+				 4 * st->num_channels + 1, sizeof(*st->attrs),
 				 GFP_KERNEL);
 	if (st->attrs == NULL)
 		return -ENOMEM;
@@ -206,6 +288,16 @@ static int iio_hwmon_probe(struct platform_device *pdev)
 			if (ret)
 				return ret;
 		}
+
+		ret = add_event_attr(dev, st, i, IIO_EV_DIR_FALLING,
+				     "%s%d_min", prefix, n);
+		if (ret)
+			return ret;
+
+		ret = add_event_attr(dev, st, i, IIO_EV_DIR_RISING,
+				     "%s%d_max", prefix, n);
+		if (ret)
+			return ret;
 	}
 
 	devm_free_pages(dev, (unsigned long)buf);
