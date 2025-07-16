intel-lab-lkp/linux

commit 21a3afc76a317b9fb214c5afcfd5e1587b2ae9b3

libbpf: Add bpf_stream_printk() macro
Add a convenience macro to print data to the BPF streams. BPF_STDOUT and
BPF_STDERR stream IDs in the vmlinux.h can be passed to the macro to
print to the respective streams.

Acked-by: Andrii Nakryiko <andrii@kernel.org>
Acked-by: Eduard Zingerman <eddyz87@gmail.com>
Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
Link: https://lore.kernel.org/r/20250703204818.925464-10-memxor@gmail.com
Signed-off-by: Alexei Starovoitov <ast@kernel.org>

tools/lib/bpf/bpf_helpers.h
@@ -314,6 +314,22 @@ enum libbpf_tristate {
 			  ___param, sizeof(___param));		\
 })
 
+extern int bpf_stream_vprintk(int stream_id, const char *fmt__str, const void *args,
+			      __u32 len__sz, void *aux__prog) __weak __ksym;
+
+#define bpf_stream_printk(stream_id, fmt, args...)				\
+({										\
+	static const char ___fmt[] = fmt;					\
+	unsigned long long ___param[___bpf_narg(args)];				\
+										\
+	_Pragma("GCC diagnostic push")						\
+	_Pragma("GCC diagnostic ignored \"-Wint-conversion\"")			\
+	___bpf_fill(___param, args);						\
+	_Pragma("GCC diagnostic pop")						\
+										\
+	bpf_stream_vprintk(stream_id, ___fmt, ___param, sizeof(___param), NULL);\
+})
+
 /* Use __bpf_printk when bpf_printk call has 3 or fewer fmt args
  * Otherwise use __bpf_vprintk
  */

commit 7041dbc26006c28adcf7b8e5729a7a756d938802

libbpf: Fix macro redefined
When compiling a program that include <linux/bpf.h> and <bpf/bpf_helpers.h>, (For example: make samples/bpf)
the following warning will be generated:
In file included from tcp_dumpstats_kern.c:7:
samples/bpf/libbpf/include/bpf/bpf_helpers.h:321:9: warning: 'bpf_stream_printk' macro redefined [-Wmacro-redefined]
  321 | #define bpf_stream_printk(stream_id, fmt, args...)                              \
      |         ^
include/linux/bpf.h:3626:9: note: previous definition is here
 3626 | #define bpf_stream_printk(ss, ...) bpf_stream_stage_printk(&ss, __VA_ARGS__)
      |         ^

Therefore, similar to bpf_vprintk,
two underscores are added to distinguish it from bpf_stream_printk in bpf.h.

Fixes: 21a3afc76a31 ("libbpf: Add bpf_stream_printk() macro")
Signed-off-by: Feng Yang <yangfeng@kylinos.cn>

tools/lib/bpf/bpf_helpers.h
@@ -318,7 +318,7 @@ enum libbpf_tristate {
 extern int bpf_stream_vprintk(int stream_id, const char *fmt__str, const void *args,
 			      __u32 len__sz, void *aux__prog) __weak __ksym;
 
-#define bpf_stream_printk(stream_id, fmt, args...)				\
+#define __bpf_stream_printk(stream_id, fmt, args...)				\
 ({										\
 	static const char ___fmt[] = fmt;					\
 	unsigned long long ___param[___bpf_narg(args)];				\

tools/testing/selftests/bpf/progs/stream.c
@@ -29,7 +29,7 @@ int stream_exhaust(void *ctx)
 	/* Use global variable for loop convergence. */
 	size = 0;
 	bpf_repeat(BPF_MAX_LOOPS) {
-		if (bpf_stream_printk(BPF_STDOUT, _STR) == -ENOSPC && size == 99954)
+		if (__bpf_stream_printk(BPF_STDOUT, _STR) == -ENOSPC && size == 99954)
 			return 0;
 		size += sizeof(_STR) - 1;
 	}
@@ -72,7 +72,7 @@ SEC("syscall")
 __success __retval(0)
 int stream_syscall(void *ctx)
 {
-	bpf_stream_printk(BPF_STDOUT, "foo");
+	__bpf_stream_printk(BPF_STDOUT, "foo");
 	return 0;
 }
 