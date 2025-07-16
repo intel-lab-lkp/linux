ae8dce1b8bf82fca960864508f67c43fb0644bff
Dr. David Alan Gilbert <linux@treblig.org>
2025-07-16 14:51:37 +0100

    perf core: Remove perf_event_attrs and perf_event_refresh
    perf_event_refresh() has been unused since the 2015
    commit f63a8daa5812 ("perf: Fix event->ctx locking")
    
    perf_event_attrs() has been unused since the 2017
    commit f91840a32dee ("perf, bpf: Add BPF support to all perf_event types")
    (Note the _ prefix version is still used)
    
    Remove them.
    
    Signed-off-by: Dr. David Alan Gilbert <linux@treblig.org>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -1224,7 +1224,6 @@ extern void perf_event_free_task(struct task_struct *task);
 extern void perf_event_delayed_put(struct task_struct *task);
 extern struct file *perf_event_get(unsigned int fd);
 extern const struct perf_event *perf_get_event(struct file *file);
-extern const struct perf_event_attr *perf_event_attrs(struct perf_event *event);
 extern void perf_event_print_debug(void);
 extern void perf_pmu_disable(struct pmu *pmu);
 extern void perf_pmu_enable(struct pmu *pmu);
@@ -1235,7 +1234,6 @@ extern int perf_event_task_enable(void);
 
 extern void perf_pmu_resched(struct pmu *pmu);
 
-extern int perf_event_refresh(struct perf_event *event, int refresh);
 extern void perf_event_update_userpage(struct perf_event *event);
 extern int perf_event_release_kernel(struct perf_event *event);
 
@@ -1945,10 +1943,6 @@ static inline const struct perf_event *perf_get_event(struct file *file)
 {
 	return ERR_PTR(-EINVAL);
 }
-static inline const struct perf_event_attr *perf_event_attrs(struct perf_event *event)
-{
-	return ERR_PTR(-EINVAL);
-}
 static inline int perf_event_read_local(struct perf_event *event, u64 *value,
 					u64 *enabled, u64 *running)
 {
@@ -1957,10 +1951,6 @@ static inline int perf_event_read_local(struct perf_event *event, u64 *value,
 static inline void perf_event_print_debug(void)				{ }
 static inline int perf_event_task_disable(void)				{ return -EINVAL; }
 static inline int perf_event_task_enable(void)				{ return -EINVAL; }
-static inline int perf_event_refresh(struct perf_event *event, int refresh)
-{
-	return -EINVAL;
-}
 
 static inline void
 perf_sw_event(u32 event_id, u64 nr, struct pt_regs *regs, u64 addr)	{ }
