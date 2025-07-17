0754a0b4553b74314612908dd0fd8be0e3718578
Longfang Liu <liulongfang@huawei.com>
2025-07-17 09:15:02 +0800

    migration: adapt to new migration configuration
    On new platforms greater than QM_HW_V3, the migration region has been
    relocated from the VF to the PF. The driver must also be modified
    accordingly to adapt to the new hardware device.
    
    Utilize the PF's I/O base directly on the new hardware platform,
    and no mmap operation is required. If it is on an old platform,
    the driver needs to be compatible with the old solution.
    
    Signed-off-by: Longfang Liu <liulongfang@huawei.com>

--- drivers/vfio/pci/hisilicon/hisi_acc_vfio_pci.h
+++ drivers/vfio/pci/hisilicon/hisi_acc_vfio_pci.h
@@ -59,6 +59,13 @@
 #define ACC_DEV_MAGIC_V1	0XCDCDCDCDFEEDAACC
 #define ACC_DEV_MAGIC_V2	0xAACCFEEDDECADEDE
 
+#define QM_MIG_REGION_OFFSET		0x180000
+#define QM_MIG_REGION_SIZE		0x2000
+
+#define QM_SUB_VERSION_ID		0x100210
+#define QM_EQC_PF_DW0			0x1c00
+#define QM_AEQC_PF_DW0			0x1c20
+
 struct acc_vf_data {
 #define QM_MATCH_SIZE offsetofend(struct acc_vf_data, qm_rsv_state)
 	/* QM match information */
