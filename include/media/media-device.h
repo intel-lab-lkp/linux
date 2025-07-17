cff1e85e7390dec838ce3752647c2fb104d9cba9
Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
2025-07-17 12:45:27 +0200

    media: mc: Add per-file-handle data support
    The media devnode core associates devnodes with files by storing the
    devnode pointer in the file structure private_data field. In order to
    allow tracking of per-file-handle data introduce a new media devnode
    file handle structure that stores the devnode pointer, and store a
    pointer to that structure in the file private_data field.
    
    Users of the media devnode code (the only existing user being
    media_device) are responsible for managing their own subclass of the
    media_devnode_fh structure.
    
    Signed-off-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    
    Prepare struct media_device_fh to be used for maintaining file handle list
    to avoid shuffling things here and there right after.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Acked-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    Signed-off-by: Jacopo Mondi <jacopo.mondi@ideasonboard.com>

--- include/media/media-device.h
+++ include/media/media-device.h
@@ -108,6 +108,10 @@ struct media_device_ops {
  *		     other operations that stop or start streaming.
  * @request_id: Used to generate unique request IDs
  *
+ * @fh_list:	List of file handles in the media device
+ *		(struct media_device_fh.mdev_list).
+ * @fh_list_lock: Serialise access to fh_list list.
+ *
  * This structure represents an abstract high-level media device. It allows easy
  * access to entities and provides basic media device-level support. The
  * structure can be allocated directly or embedded in a larger structure.
@@ -180,6 +184,9 @@ struct media_device {
 
 	struct mutex req_queue_mutex;
 	atomic_t request_id;
+
+	struct list_head fh_list;
+	spinlock_t fh_list_lock;
 };
 
 /* We don't need to include usb.h here */
