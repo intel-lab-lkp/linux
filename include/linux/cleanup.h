f9d39c1262fb231a9e762e97d1a112f1ddd48e85
H. Peter Anvin <hpa@zytor.com>
2025-07-18 14:32:45 -0700

    include/linux: change "__auto_type" to "auto"
    Replace instances of "__auto_type" with "auto" in include/linux.
    
    Signed-off-by: H. Peter Anvin (Intel) <hpa@zytor.com>

--- include/linux/cleanup.h
+++ include/linux/cleanup.h
@@ -201,8 +201,8 @@
 
 #define __get_and_null(p, nullvalue)   \
 	({                                  \
-		__auto_type __ptr = &(p);   \
-		__auto_type __val = *__ptr; \
+		auto __ptr = &(p);   \
+		auto __val = *__ptr; \
 		*__ptr = nullvalue;         \
 		__val;                      \
 	})
