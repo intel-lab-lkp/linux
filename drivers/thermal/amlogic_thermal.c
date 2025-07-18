85cf1c31e41ee326b314669f6c588cc3f3d9978c
Xianwei Zhao <xianwei.zhao@amlogic.com>
2025-07-18 14:37:42 +0800

    thermal: amlogic: Support C3 thermal controller driver
    Add the thermal controller driver for the C3 SoC family.
    
    The main difference compared to other Amlogic chips lies in the
    offset of the sec_ao base (referred to as u_efuse_off), which is
    handled differently. Everything else remains consistent.
    
    Signed-off-by: Liming Xue <liming.xue@amlogic.com>
    Signed-off-by: Xianwei Zhao <xianwei.zhao@amlogic.com>

--- drivers/thermal/amlogic_thermal.c
+++ drivers/thermal/amlogic_thermal.c
@@ -226,6 +226,12 @@ static const struct amlogic_thermal_data amlogic_thermal_a1_cpu_param = {
 	.regmap_config = &amlogic_thermal_regmap_config_g12a,
 };
 
+static const struct amlogic_thermal_data amlogic_thermal_c3_cpu_param = {
+	.u_efuse_off = 0x108,
+	.calibration_parameters = &amlogic_thermal_g12a,
+	.regmap_config = &amlogic_thermal_regmap_config_g12a,
+};
+
 static const struct of_device_id of_amlogic_thermal_match[] = {
 	{
 		.compatible = "amlogic,g12a-ddr-thermal",
@@ -238,6 +244,10 @@ static const struct of_device_id of_amlogic_thermal_match[] = {
 	{
 		.compatible = "amlogic,a1-cpu-thermal",
 		.data = &amlogic_thermal_a1_cpu_param,
+	},
+	{
+		.compatible = "amlogic,c3-cpu-thermal",
+		.data = &amlogic_thermal_c3_cpu_param,
 	},
 	{ /* sentinel */ }
 };
