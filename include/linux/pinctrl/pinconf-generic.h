f39ac8ed9abb24416d7a178589f629b281c9ac15
Dan Carpenter <dan.carpenter@linaro.org>
2025-07-20 14:38:59 -0500

    pinctrl-scmi: add PIN_CONFIG_INPUT_VALUE
    In SCMI the value of the pin is just another configuration option.  Add
    this as an option in the pin_config_param enum and creating a mapping to
    SCMI_PIN_INPUT_VALUE in pinctrl_scmi_map_pinconf_type()
    
    Since this is an RFC patch, I'm going to comment that I think the SCMI
    pinctrl driver misuses the PIN_CONFIG_OUTPUT enum.  It should be for
    enabling and disabling output on pins which can serve as both input and
    output.  Enabling it is supposed to write a 1 and disabling it is
    supposed to write a 0 but we use that side effect to write 1s and 0s.  I
    did't change this because it would break userspace but I'd like to add a
    PIN_CONFIG_OUTPUT_VALUE enum as well and use that in the GPIO driver.
    But in this patchset I just use PIN_CONFIG_OUTPUT.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

--- include/linux/pinctrl/pinconf-generic.h
+++ include/linux/pinctrl/pinconf-generic.h
@@ -83,6 +83,8 @@ struct pinctrl_map;
  *      schmitt-trigger mode is disabled.
  * @PIN_CONFIG_INPUT_SCHMITT_UV: this will configure an input pin to run in
  *	schmitt-trigger mode. The argument is in uV.
+ * @PIN_CONFIG_INPUT_VALUE: This is used in SCMI to read the value from the
+ *	pin.
  * @PIN_CONFIG_MODE_LOW_POWER: this will configure the pin for low power
  *	operation, if several modes of operation are supported these can be
  *	passed in the argument on a custom form, else just use argument 1
@@ -135,6 +137,7 @@ enum pin_config_param {
 	PIN_CONFIG_INPUT_SCHMITT,
 	PIN_CONFIG_INPUT_SCHMITT_ENABLE,
 	PIN_CONFIG_INPUT_SCHMITT_UV,
+	PIN_CONFIG_INPUT_VALUE,
 	PIN_CONFIG_MODE_LOW_POWER,
 	PIN_CONFIG_MODE_PWM,
 	PIN_CONFIG_OUTPUT,
