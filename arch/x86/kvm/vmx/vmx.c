6cdc36c7e438641cecb14a9e730e164b072e20cb
Colin Ian King <colin.i.king@gmail.com>
2025-07-21 14:47:17 +0100

    KVM: x86: Remove space before \n newline
    There is a extraneous space before a newline in a pr_debug_ratelimited
    message. Remove it.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -3188,7 +3188,7 @@ static void enter_lmode(struct kvm_vcpu *vcpu)
 
 	guest_tr_ar = vmcs_read32(GUEST_TR_AR_BYTES);
 	if ((guest_tr_ar & VMX_AR_TYPE_MASK) != VMX_AR_TYPE_BUSY_64_TSS) {
-		pr_debug_ratelimited("%s: tss fixup for long mode. \n",
+		pr_debug_ratelimited("%s: tss fixup for long mode.\n",
 				     __func__);
 		vmcs_write32(GUEST_TR_AR_BYTES,
 			     (guest_tr_ar & ~VMX_AR_TYPE_MASK)
