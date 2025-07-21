b5ebd4c24f6633a73deb49ba34b6b6bef7f235bc
Aaron Kling <webgeek1234@gmail.com>
2025-07-20 21:15:04 -0500

    phy: tegra: xusb: Add Tegra201B01 Support
    It has slightly different lanes compared to the original Tegra210.
    
    Signed-off-by: Aaron Kling <webgeek1234@gmail.com>

--- drivers/phy/tegra/xusb.c
+++ drivers/phy/tegra/xusb.c
@@ -59,6 +59,10 @@ static const struct of_device_id tegra_xusb_padctl_of_match[] = {
 		.compatible = "nvidia,tegra210-xusb-padctl",
 		.data = &tegra210_xusb_padctl_soc,
 	},
+	{
+		.compatible = "nvidia,tegra210b01-xusb-padctl",
+		.data = &tegra210b01_xusb_padctl_soc,
+	},
 #endif
 #if defined(CONFIG_ARCH_TEGRA_186_SOC)
 	{
