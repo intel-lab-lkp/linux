a9ac3779c7653ceb9aa0eaefd4be492d462373d1
AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>
2025-07-21 09:55:21 +0200

    power: reset: qcom-pon: Migrate to devm_spmi_subdevice_alloc_and_add()
    Some Qualcomm PMICs integrates a Power On device supporting pwrkey
    and resin along with the Android reboot reason action identifier.
    
    Instead of using the parent SPMI device (the main PMIC) as a kind
    of syscon in this driver, register a new SPMI sub-device for PON
    and initialize its own regmap with this sub-device's specific base
    address, retrieved from the devicetree.
    
    This allows to stop manually adding the register base address to
    every R/W call in this driver, as this can be, and is now, handled
    by the regmap API instead.
    
    Signed-off-by: AngeloGioacchino Del Regno <angelogioacchino.delregno@collabora.com>

--- drivers/power/reset/qcom-pon.c
+++ drivers/power/reset/qcom-pon.c
@@ -11,6 +11,7 @@
 #include <linux/reboot.h>
 #include <linux/reboot-mode.h>
 #include <linux/regmap.h>
+#include <linux/spmi.h>
 
 #define PON_SOFT_RB_SPARE		0x8f
 
@@ -22,7 +23,6 @@
 struct qcom_pon {
 	struct device *dev;
 	struct regmap *regmap;
-	u32 baseaddr;
 	struct reboot_mode_driver reboot_mode;
 	long reason_shift;
 };
@@ -35,7 +35,7 @@ static int qcom_pon_reboot_mode_write(struct reboot_mode_driver *reboot,
 	int ret;
 
 	ret = regmap_update_bits(pon->regmap,
-				 pon->baseaddr + PON_SOFT_RB_SPARE,
+				 PON_SOFT_RB_SPARE,
 				 GENMASK(7, pon->reason_shift),
 				 magic << pon->reason_shift);
 	if (ret < 0)
@@ -46,9 +46,20 @@ static int qcom_pon_reboot_mode_write(struct reboot_mode_driver *reboot,
 
 static int qcom_pon_probe(struct platform_device *pdev)
 {
+	struct regmap_config qcom_pon_regmap_config = {
+		.reg_bits = 16,
+		.val_bits = 16,
+		.max_register = 0x100,
+		.fast_io = true
+	};
+	struct spmi_subdevice *sub_sdev;
+	struct spmi_device *sparent;
 	struct qcom_pon *pon;
 	long reason_shift;
 	int error;
+
+	if (!pdev->dev.parent)
+		return -ENODEV;
 
 	pon = devm_kzalloc(&pdev->dev, sizeof(*pon), GFP_KERNEL);
 	if (!pon)
@@ -56,16 +67,24 @@ static int qcom_pon_probe(struct platform_device *pdev)
 
 	pon->dev = &pdev->dev;
 
-	pon->regmap = dev_get_regmap(pdev->dev.parent, NULL);
+	sparent = to_spmi_device(pdev->dev.parent);
+	if (!sparent)
+		return -ENODEV;
+
+	sub_sdev = devm_spmi_subdevice_alloc_and_add(&pdev->dev, sparent);
+	if (IS_ERR(sub_sdev))
+		return PTR_ERR(sub_sdev);
+
+	error = of_property_read_u32(pdev->dev.of_node, "reg",
+				     &qcom_pon_regmap_config.reg_base);
+	if (error)
+		return error;
+
+	pon->regmap = devm_regmap_init_spmi_ext(&sub_sdev->sdev, &qcom_pon_regmap_config);
 	if (!pon->regmap) {
 		dev_err(&pdev->dev, "failed to locate regmap\n");
 		return -ENODEV;
 	}
-
-	error = of_property_read_u32(pdev->dev.of_node, "reg",
-				     &pon->baseaddr);
-	if (error)
-		return error;
 
 	reason_shift = (long)of_device_get_match_data(&pdev->dev);
 
