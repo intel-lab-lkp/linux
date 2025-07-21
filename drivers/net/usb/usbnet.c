3019acaf1ee7862be70ba4e913447b3bf0ecf542
Yi Cong <yicong@kylinos.cn>
2025-07-21 15:10:22 +0800

    usbnet: Set duplex status to unknown in the absence of MII
    CDC device don't use mdio to get link status, duplex is set
    half as default.
    
    Now cdc_ncm can't get duplex, set it UNKNOWN instead of half
    which might actually be in an error state.
    
    Signed-off-by: Yi Cong <yicong@kylinos.cn>

--- drivers/net/usb/usbnet.c
+++ drivers/net/usb/usbnet.c
@@ -1013,6 +1013,11 @@ int usbnet_get_link_ksettings_internal(struct net_device *net,
 	else
 		cmd->base.speed = SPEED_UNKNOWN;
 
+	/* Now we can't get link duplex without MII,
+	 * set it DUPLEX_UNKNOWN instead of default DUPLEX_HALF
+	 */
+	cmd->base.duplex = DUPLEX_UNKNOWN;
+
 	return 0;
 }
 EXPORT_SYMBOL_GPL(usbnet_get_link_ksettings_internal);
