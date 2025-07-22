80ea0ec300ee04cc6101456e04f9043c5203c42a
Seyediman Seyedarab <imandevel@gmail.com>
2025-07-22 09:11:17 -0400

    iommu/vt-d: replace snprintf with scnprintf in dmar_latency_snapshot()
    snprintf returns the number of bytes that would have been written,
    not the number actually written to the buffer. When accumulating
    the byte count with the return value of snprintf, this can cause
    the offset to exceed the actual buffer size if truncation occurs.
    
    The byte count is passed to seq_puts() in latency_show_one() with-
    out checking for truncation.
    
    Replace snprintf with scnprintf, ensuring the buffer offset stays
    within bound.
    
    Signed-off-by: Seyediman Seyedarab <ImanDevel@gmail.com>

--- drivers/iommu/intel/perf.c
+++ drivers/iommu/intel/perf.c
@@ -122,7 +122,7 @@ int dmar_latency_snapshot(struct intel_iommu *iommu, char *str, size_t size)
 	memset(str, 0, size);
 
 	for (i = 0; i < COUNTS_NUM; i++)
-		bytes += snprintf(str + bytes, size - bytes,
+		bytes += scnprintf(str + bytes, size - bytes,
 				  "%s", latency_counter_names[i]);
 
 	spin_lock_irqsave(&latency_lock, flags);
@@ -130,7 +130,7 @@ int dmar_latency_snapshot(struct intel_iommu *iommu, char *str, size_t size)
 		if (!dmar_latency_enabled(iommu, i))
 			continue;
 
-		bytes += snprintf(str + bytes, size - bytes,
+		bytes += scnprintf(str + bytes, size - bytes,
 				  "\n%s", latency_type_names[i]);
 
 		for (j = 0; j < COUNTS_NUM; j++) {
@@ -156,7 +156,7 @@ int dmar_latency_snapshot(struct intel_iommu *iommu, char *str, size_t size)
 				break;
 			}
 
-			bytes += snprintf(str + bytes, size - bytes,
+			bytes += scnprintf(str + bytes, size - bytes,
 					  "%12lld", val);
 		}
 	}
