14cb477e9672943b540684a463957302a649362b
Anindya Sundar Gayen <anindya19@gmail.com>
2025-07-22 23:53:10 +0530

    bcachefs: remove extraneous ; after statements
    There are a couple of statements with two following semicolons, replace
    these with just one semicolon.
    
    Signed-off-by: Anindya Sundar Gayen <anindya19@gmail.com>

--- fs/bcachefs/btree_io.c
+++ fs/bcachefs/btree_io.c
@@ -2020,7 +2020,7 @@ static void btree_node_scrub_work(struct work_struct *work)
 	}
 
 	printbuf_exit(&err);
-	bch2_bkey_buf_exit(&scrub->key, c);;
+	bch2_bkey_buf_exit(&scrub->key, c);
 	btree_bounce_free(c, c->opts.btree_node_size, scrub->used_mempool, scrub->buf);
 	enumerated_ref_put(&scrub->ca->io_ref[READ], BCH_DEV_READ_REF_btree_node_scrub);
 	kfree(scrub);
