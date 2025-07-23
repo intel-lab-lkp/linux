c405dc2e08d8dcb4161b1ddbb5dce9007ee3a53e
Dev Jain <dev.jain@arm.com>
2025-07-23 15:59:05 +0530

    arm64: Enable vmalloc-huge with ptdump
    Our goal is to move towards enabling vmalloc-huge by default on arm64 so
    as to reduce TLB pressure. Therefore, we need a way to analyze the portion
    of block mappings in vmalloc space we can get on a production system; this
    can be done through ptdump, but currently we disable vmalloc-huge if
    CONFIG_PTDUMP_DEBUGFS is on. The reason is that lazy freeing of kernel
    pagetables via vmap_try_huge_pxd() may race with ptdump, so ptdump
    may dereference a bogus address.
    
    To solve this, we need to synchronize ptdump_walk_pgd() with
    pud_free_pmd_page() and pmd_free_pte_page().
    
    Since this race is very unlikely to happen in practice, we do not want to
    penalize other code paths taking the init_mm mmap_lock. Therefore, we use
    static keys. ptdump will enable the static key - upon observing that, the
    vmalloc table freeing path will get patched in with an
    mmap_read_lock/unlock sequence. A code comment explains in detail, how
    a combination of acquire sematics of static_branch_enable() and the
    barriers in __flush_tlb_kernel_pgtable() ensures that ptdump will never
    get a hold on the address of a freed PMD or PTE table.
    
    For an (almost) rigorous proof of correctness, one may look at:
    https://lore.kernel.org/all/e1e87f16-1c48-481b-8f7c-9333ac5d13e7@arm.com/
    
    Reviewed-by: Ryan Roberts <ryan.roberts@arm.com>
    Signed-off-by: Dev Jain <dev.jain@arm.com>

--- arch/arm64/mm/ptdump.c
+++ arch/arm64/mm/ptdump.c
@@ -283,6 +283,14 @@ void note_page_flush(struct ptdump_state *pt_st)
 	note_page(pt_st, 0, -1, pte_val(pte_zero));
 }
 
+static void arm64_ptdump_walk_pgd(struct ptdump_state *st,
+				  struct mm_struct *mm, pgd_t *pgd)
+{
+	static_branch_enable(&arm64_ptdump_lock_key);
+	ptdump_walk_pgd(&st.ptdump, &init_mm, NULL);
+	static_branch_disable(&arm64_ptdump_lock_key);
+}
+
 void ptdump_walk(struct seq_file *s, struct ptdump_info *info)
 {
 	unsigned long end = ~0UL;
@@ -311,7 +319,7 @@ void ptdump_walk(struct seq_file *s, struct ptdump_info *info)
 		}
 	};
 
-	ptdump_walk_pgd(&st.ptdump, info->mm, NULL);
+	arm64_ptdump_walk_pgd(&st.ptdump, info->mm, NULL);
 }
 
 static void __init ptdump_initialize(void)
@@ -353,7 +361,7 @@ bool ptdump_check_wx(void)
 		}
 	};
 
-	ptdump_walk_pgd(&st.ptdump, &init_mm, NULL);
+	arm64_ptdump_walk_pgd(&st.ptdump, info->mm, NULL);
 
 	if (st.wx_pages || st.uxn_pages) {
 		pr_warn("Checked W+X mappings: FAILED, %lu W+X pages found, %lu non-UXN pages found\n",
