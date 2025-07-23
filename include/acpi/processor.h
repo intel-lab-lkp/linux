75621e77853b5b4c9c4bd5cfb7819c2066859cd4
Huisong Li <lihuisong@huawei.com>
2025-07-23 20:10:34 +0800

    ACPI: processor: idle: Fix resource leak and potential concurrent in acpi_processor_power_init()
    There are three kind of issues:
    1> There are two resource leak issues in acpi_processor_power_init:
       a> Don't unregister acpi_idle_driver when do kzalloc failed.
       b> Don't free cpuidle device memory when register cpuidle device failed.
    2> There isn't lock to prevent the global acpi_processor_registered, which
       may lead to concurrent register cpuidle driver.
    3> The cpuidle driver should be registered in advance when all of the CPUs
       have been brought up instead of being in a CPU hotplug callback.
    
    To solve these issues, so add a new function to initialize acpi_idle_driver
    based on the power management information of an available CPU and register
    cpuidle driver in acpi_processor_driver_init().
    
    Signed-off-by: Huisong Li <lihuisong@huawei.com>

--- include/acpi/processor.h
+++ include/acpi/processor.h
@@ -423,6 +423,8 @@ int acpi_processor_power_init(struct acpi_processor *pr);
 int acpi_processor_power_exit(struct acpi_processor *pr);
 int acpi_processor_power_state_has_changed(struct acpi_processor *pr);
 int acpi_processor_hotplug(struct acpi_processor *pr);
+int acpi_processor_register_idle_driver(void);
+void acpi_processor_unregister_idle_driver(void);
 #else
 static inline int acpi_processor_power_init(struct acpi_processor *pr)
 {
@@ -442,6 +444,13 @@ static inline int acpi_processor_power_state_has_changed(struct acpi_processor *
 static inline int acpi_processor_hotplug(struct acpi_processor *pr)
 {
 	return -ENODEV;
+}
+static int acpi_processor_register_idle_driver(void)
+{
+	return -ENODEV;
+}
+static void acpi_processor_unregister_idle_driver(void)
+{
 }
 #endif /* CONFIG_ACPI_PROCESSOR_IDLE */
 
