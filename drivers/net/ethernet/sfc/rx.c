64e6db551f533cabb88a392b197c33f2f05746ea
Chenyuan Yang <chenyuan0y@gmail.com>
2025-07-22 19:32:03 -0500

    sfc: handle NULL returned by xdp_convert_buff_to_frame()
    The xdp_convert_buff_to_frame() function can return NULL when there is
    insufficient headroom in the buffer to store the xdp_frame structure
    or when the driver didn't reserve enough tailroom for skb_shared_info.
    
    Currently, the sfc driver does not check for this NULL return value
    in the XDP_TX case within efx_do_xdp(). While the efx_xdp_tx_buffers()
    function has some defensive checks, passing a NULL xdpf can still lead
    to undefined behavior when the function tries to access xdpf->len and
    xdpf->data.
    
    Fix by adding a proper NULL check in the XDP_TX case. If conversion
    fails, free the RX buffer and increment the bad drops counter, following
    the same pattern used for other XDP error conditions in this driver.
    
    Signed-off-by: Chenyuan Yang <chenyuan0y@gmail.com>
    Fixes: 1b698fa5d8ef ("xdp: Rename convert_to_xdp_frame in xdp_convert_buff_to_frame")

--- drivers/net/ethernet/sfc/rx.c
+++ drivers/net/ethernet/sfc/rx.c
@@ -308,6 +308,12 @@ static bool efx_do_xdp(struct efx_nic *efx, struct efx_channel *channel,
 	case XDP_TX:
 		/* Buffer ownership passes to tx on success. */
 		xdpf = xdp_convert_buff_to_frame(&xdp);
+		if (unlikely(!xdpf)) {
+			efx_free_rx_buffers(rx_queue, rx_buf, 1);
+			channel->n_rx_xdp_bad_drops++;
+			break;
+		}
+
 		err = efx_xdp_tx_buffers(efx, 1, &xdpf, true);
 		if (unlikely(err != 1)) {
 			efx_free_rx_buffers(rx_queue, rx_buf, 1);
