f398c3113dd24ecadc37b70fb49080d75c677d1c
Pankaj Raghav <p.raghav@samsung.com>
2025-07-24 16:49:59 +0200

    mm: add static huge zero folio
    There are many places in the kernel where we need to zeroout larger
    chunks but the maximum segment we can zeroout at a time by ZERO_PAGE
    is limited by PAGE_SIZE.
    
    This is especially annoying in block devices and filesystems where we
    attach multiple ZERO_PAGEs to the bio in different bvecs. With multipage
    bvec support in block layer, it is much more efficient to send out
    larger zero pages as a part of single bvec.
    
    This concern was raised during the review of adding LBS support to
    XFS[1][2].
    
    Usually huge_zero_folio is allocated on demand, and it will be
    deallocated by the shrinker if there are no users of it left. At moment,
    huge_zero_folio infrastructure refcount is tied to the process lifetime
    that created it. This might not work for bio layer as the completions
    can be async and the process that created the huge_zero_folio might no
    longer be alive. And, one of the main point that came during discussion
    is to have something bigger than zero page as a drop-in replacement.
    
    Add a config option STATIC_HUGE_ZERO_FOLIO that will always allocate
    the huge_zero_folio, and it will never drop the reference. This makes
    using the huge_zero_folio without having to pass any mm struct and does
    not tie the lifetime of the zero folio to anything, making it a drop-in
    replacement for ZERO_PAGE.
    
    If STATIC_HUGE_ZERO_FOLIO config option is enabled, then
    mm_get_huge_zero_folio() will simply return this page instead of
    dynamically allocating a new PMD page.
    
    This option can waste memory in small systems or systems with 64k base
    page size. So make it an opt-in and also add an option from individual
    architecture so that we don't enable this feature for larger base page
    size systems.
    
    [1] https://lore.kernel.org/linux-xfs/20231027051847.GA7885@lst.de/
    [2] https://lore.kernel.org/linux-xfs/ZitIK5OnR7ZNY0IG@infradead.org/
    
    Co-developed-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Pankaj Raghav <p.raghav@samsung.com>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -75,6 +75,7 @@ static unsigned long deferred_split_scan(struct shrinker *shrink,
 static bool split_underused_thp = true;
 
 static atomic_t huge_zero_refcount;
+atomic_t huge_zero_folio_is_static __read_mostly;
 struct folio *huge_zero_folio __read_mostly;
 unsigned long huge_zero_pfn __read_mostly = ~0UL;
 unsigned long huge_anon_orders_always __read_mostly;
@@ -265,6 +266,47 @@ void mm_put_huge_zero_folio(struct mm_struct *mm)
 	if (test_bit(MMF_HUGE_ZERO_PAGE, &mm->flags))
 		put_huge_zero_page();
 }
+
+#ifdef CONFIG_STATIC_HUGE_ZERO_FOLIO
+#define FAIL_COUNT_LIMIT 2
+
+struct folio *__get_static_huge_zero_folio(void)
+{
+	static unsigned long fail_count_clear_timer;
+	static atomic_t huge_zero_static_fail_count __read_mostly;
+
+	if (unlikely(!slab_is_available()))
+		return NULL;
+
+	/*
+	 * If we failed to allocate a huge zero folio multiple times,
+	 * just refrain from trying for one minute before retrying to get
+	 * a reference again.
+	 */
+	if (atomic_read(&huge_zero_static_fail_count) > FAIL_COUNT_LIMIT) {
+		if (time_before(jiffies, fail_count_clear_timer))
+			return NULL;
+		atomic_set(&huge_zero_static_fail_count, 0);
+	}
+	/*
+	 * Our raised reference will prevent the shrinker from ever having
+	 * success.
+	 */
+	if (!get_huge_zero_page()) {
+		int count = atomic_inc_return(&huge_zero_static_fail_count);
+
+		if (count > FAIL_COUNT_LIMIT)
+			fail_count_clear_timer = get_jiffies_64() + 60 * HZ;
+
+		return NULL;
+	}
+
+	if (atomic_cmpxchg(&huge_zero_folio_is_static, 0, 1) != 0)
+		put_huge_zero_page();
+
+	return huge_zero_folio;
+}
+#endif /* CONFIG_STATIC_HUGE_ZERO_FOLIO */
 
 static unsigned long shrink_huge_zero_folio_count(struct shrinker *shrink,
 						  struct shrink_control *sc)
