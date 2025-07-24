0d6130e87e813e1174edfc256f7a0d8dde9f1da1
Elena Reshetova <elena.reshetova@intel.com>
2025-07-24 11:02:15 +0300

    x86/sgx: Enable automatic SVN updates for SGX enclaves
    == Background ==
    
    ENCLS[EUPDATESVN] is a new SGX instruction [1] which allows enclave
    attestation to include information about updated microcode SVN without a
    reboot. Before an EUPDATESVN operation can be successful, all SGX memory
    (aka. EPC) must be marked as “unused” in the SGX hardware metadata
    (aka.EPCM). This requirement ensures that no compromised enclave can
    survive the EUPDATESVN procedure and provides an opportunity to generate
    new cryptographic assets.
    
    == Patch Contents ==
    
    Attempt to execute ENCLS[EUPDATESVN] every time the first file descriptor
    is obtained via sgx_(vepc_)open(). In the most common case the microcode
    SVN is already up-to-date, and the operation succeeds without updating SVN.
    
    If it fails with any other error code than SGX_INSUFFICIENT_ENTROPY, this
    is considered unexpected and the *open() returns an error. This should not
    happen in practice.
    
    On contrary, SGX_INSUFFICIENT_ENTROPY might happen due
    to a pressure on the system's DRNG (RDSEED) and therefore the *open() can
    be safely retried to allow normal enclave operation.
    
    [1] Runtime Microcode Updates with Intel Software Guard Extensions,
    https://cdrdv2.intel.com/v1/dl/getContent/648682
    
    Signed-off-by: Elena Reshetova <elena.reshetova@intel.com>

--- arch/x86/kernel/cpu/sgx/main.c
+++ arch/x86/kernel/cpu/sgx/main.c
@@ -921,6 +921,9 @@ EXPORT_SYMBOL_GPL(sgx_set_attribute);
 /* Counter to count the active SGX users */
 static int __maybe_unused sgx_usage_count;
 
+/* Mutex to ensure no concurrent EPC accesses during EUPDATESVN */
+static DEFINE_MUTEX(sgx_svn_lock);
+
 /**
  * sgx_update_svn() - Attempt to call ENCLS[EUPDATESVN].
  * This instruction attempts to update CPUSVN to the
@@ -937,7 +940,7 @@ static int __maybe_unused sgx_usage_count;
  *  entropy in RNG.
  * -EIO: Unexpected error, retries are not advisable.
  */
-static int __maybe_unused sgx_update_svn(void)
+static int sgx_update_svn(void)
 {
 	int ret;
 
@@ -983,12 +986,17 @@ static int __maybe_unused sgx_update_svn(void)
 
 int sgx_inc_usage_count(void)
 {
+	guard(mutex)(&sgx_svn_lock);
+
+	if (sgx_usage_count++ == 0)
+		return sgx_update_svn();
+
 	return 0;
 }
 
 void sgx_dec_usage_count(void)
 {
-	return;
+	sgx_usage_count--;
 }
 
 static int __init sgx_init(void)
