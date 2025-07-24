9b50552c8231439880bcd303df7cdb7440d4ba64
Jimmy Assarsson <extja@kvaser.com>
2025-07-24 11:25:02 +0200

    can: kvaser_usb: Add devlink support
    Add devlink support at device level.
    
    Example output:
      $ devlink dev
      usb/1-1.3:1.0
    
      $ devlink dev info
      usb/1-1.3:1.0:
        driver kvaser_usb
    
    Signed-off-by: Jimmy Assarsson <extja@kvaser.com>

---
+++ drivers/net/can/usb/kvaser_usb/kvaser_usb_devlink.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0
+/* kvaser_usb devlink functions
+ *
+ * Copyright (C) 2025 KVASER AB, Sweden. All rights reserved.
+ */
+
+#include <net/devlink.h>
+
+const struct devlink_ops kvaser_usb_devlink_ops = {
+};
