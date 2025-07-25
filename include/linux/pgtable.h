bd7ffe70c598a1f19b21d13a389428fd8fe6758a
Harry Yoo <harry.yoo@oracle.com>
2025-07-25 10:21:03 +0900

    mm: introduce and use {pgd,p4d}_populate_kernel()
    Introduce and use {pgd,p4d}_populate_kernel() in core MM code when
    populating PGD and P4D entries for the kernel address space.
    These helpers ensure proper synchronization of page tables when
    updating the kernel portion of top-level page tables.
    
    Until now, the kernel has relied on each architecture to handle
    synchronization of top-level page tables in an ad-hoc manner.
    For example, see commit 9b861528a801 ("x86-64, mem: Update all PGDs for
    direct mapping and vmemmap mapping changes").
    
    However, this approach has proven fragile for following reasons:
    
      1) It is easy to forget to perform the necessary page table
         synchronization when introducing new changes.
         For instance, commit 4917f55b4ef9 ("mm/sparse-vmemmap: improve memory
         savings for compound devmaps") overlooked the need to synchronize
         page tables for the vmemmap area.
    
      2) It is also easy to overlook that the vmemmap and direct mapping areas
         must not be accessed before explicit page table synchronization.
         For example, commit 8d400913c231 ("x86/vmemmap: handle unpopulated
         sub-pmd ranges")) caused crashes by accessing the vmemmap area
         before calling sync_global_pgds().
    
    To address this, as suggested by Dave Hansen, introduce _kernel() variants
    of the page table population helpers, which invoke architecture-specific
    hooks to properly synchronize page tables.
    
    They reuse existing infrastructure for vmalloc and ioremap.
    Synchronization requirements are determined by ARCH_PAGE_TABLE_SYNC_MASK,
    and the actual synchronization is performed by arch_sync_kernel_mappings().
    
    This change currently targets only x86_64, so only PGD and P4D level
    helpers are introduced. In theory, PUD and PMD level helpers can be added
    later if needed by other architectures.
    
    Currently this is a no-op, since no architecture sets
    PGTBL_{PGD,P4D}_MODIFIED in ARCH_PAGE_TABLE_SYNC_MASK.
    
    Cc: stable@vger.kernel.org
    Suggested-by: Dave Hansen <dave.hansen@linux.intel.com>
    Signed-off-by: Harry Yoo <harry.yoo@oracle.com>

--- include/linux/pgtable.h
+++ include/linux/pgtable.h
@@ -1332,8 +1332,8 @@ static inline void ptep_modify_prot_commit(struct vm_area_struct *vma,
 
 /*
  * Architectures can set this mask to a combination of PGTBL_P?D_MODIFIED values
- * and let generic vmalloc and ioremap code know when arch_sync_kernel_mappings()
- * needs to be called.
+ * and let generic vmalloc, ioremap and page table update code know when
+ * arch_sync_kernel_mappings() needs to be called.
  */
 #ifndef ARCH_PAGE_TABLE_SYNC_MASK
 #define ARCH_PAGE_TABLE_SYNC_MASK 0
