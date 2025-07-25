0ed0512de9e437830a9a38f62016740bc79a4e7b
Alexander Stein <alexander.stein@ew.tq-group.com>
2025-07-25 07:52:20 +0200

    gpio: stmpe: Allow to compile as a module
    Add the necessary boilerplate to also make this driver modular.
    This follows commit a2b8191ab695a ("pwm: stmpe: Allow to compile as a
    module") which does the same for pwm. Also add OF match table for module
    autoloading.
    
    Signed-off-by: Alexander Stein <alexander.stein@ew.tq-group.com>

--- drivers/gpio/gpio-stmpe.c
+++ drivers/gpio/gpio-stmpe.c
@@ -534,16 +534,19 @@ static int stmpe_gpio_probe(struct platform_device *pdev)
 	return devm_gpiochip_add_data(dev, &stmpe_gpio->chip, stmpe_gpio);
 }
 
+static const struct of_device_id stmpe_gpio_of_matches[] = {
+	{ .compatible = "st,stmpe-gpio", },
+	{ /* sentinel */ }
+};
+MODULE_DEVICE_TABLE(of, stmpe_gpio_of_matches);
+
 static struct platform_driver stmpe_gpio_driver = {
 	.driver = {
-		.suppress_bind_attrs	= true,
-		.name			= "stmpe-gpio",
+		.name = "stmpe-gpio",
+		.of_match_table = stmpe_gpio_of_matches,
 	},
-	.probe		= stmpe_gpio_probe,
 };
+module_platform_driver_probe(stmpe_gpio_driver, stmpe_gpio_probe);
 
-static int __init stmpe_gpio_init(void)
-{
-	return platform_driver_register(&stmpe_gpio_driver);
-}
-subsys_initcall(stmpe_gpio_init);
+MODULE_DESCRIPTION("STMPE expander GPIO");
+MODULE_LICENSE("GPL");
