d6527bc91cdc6fefd07139349943aa86080e90b1
Weitao Wang <WeitaoWang-oc@zhaoxin.com>
2025-07-26 02:51:01 +0800

    usb:xhci:Fix slot_id resource race conflict
    In such a scenario, device-A with slot_id equal to 1 is disconnecting
    while device-B is enumerating, device-B will fail to enumerate in the
    follow sequence.
    
    1.[device-A] send disable slot command
    2.[device-B] send enable slot command
    3.[device-A] disable slot command completed and wakeup waiting thread
    4.[device-B] enable slot command completed with slot_id equal to 1 and
    wakeup waiting thread
    5.[device-B] driver check this slot_id was used by someone(device-A) in
    xhci_alloc_virt_device, this device fails to enumerate as this conflict
    6.[device-A] xhci->devs[slot_id] set to NULL in xhci_free_virt_device
    
    To fix driver's slot_id resources conflict, let the xhci_free_virt_device
    functionm call in the interrupt handler when disable slot command success.
    
    Cc: stable@vger.kernel.org
    Fixes: 7faac1953ed1 ("xhci: avoid race between disable slot command and host runtime suspend")
    Signed-off-by: Weitao Wang <WeitaoWang-oc@zhaoxin.com>

--- drivers/usb/host/xhci-hub.c
+++ drivers/usb/host/xhci-hub.c
@@ -705,10 +705,11 @@ static int xhci_enter_test_mode(struct xhci_hcd *xhci,
 			continue;
 
 		retval = xhci_disable_slot(xhci, i);
-		xhci_free_virt_device(xhci, i);
-		if (retval)
+		if (retval) {
 			xhci_err(xhci, "Failed to disable slot %d, %d. Enter test mode anyway\n",
 				 i, retval);
+			xhci_free_virt_device(xhci, i);
+		}
 	}
 	spin_lock_irqsave(&xhci->lock, *flags);
 	/* Put all ports to the Disable state by clear PP */
