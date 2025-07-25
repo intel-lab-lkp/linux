b5e9795dd6b4099d42ec73fda6e4d7c0a42c39ee
Joshua Hahn <joshua.hahnjy@gmail.com>
2025-07-25 10:35:45 -0700

    mempolicy: Clarify what RECLAIM_ZONE means
    The zone_reclaim_mode API controls reclaim behavior when a node runs out of
    memory. Contrary to its user-facing name, it is internally referred to as
    "node_reclaim_mode". This is slightly confusing but there is not much we can
    do given that it has already been exposed to userspace (since at least 2.6).
    
    However, what we can do is to make sure the internal description of what the
    bits inside zone_reclaim_mode aligns with what it does in practice.
    Setting RECLAIM_ZONE does indeed run shrink_inactive_list, but a more holistic
    description would be to explain that zone reclaim modulates whether page
    allocation (and khugepaged collapsing) prefers reclaiming & attempting to
    allocate locally or should fall back to the next node in the zonelist.
    
    Change the description to clarify what zone reclaim entails.
    
    Signed-off-by: Joshua Hahn <joshua.hahnjy@gmail.com>

--- include/uapi/linux/mempolicy.h
+++ include/uapi/linux/mempolicy.h
@@ -69,7 +69,7 @@ enum {
  * These bit locations are exposed in the vm.zone_reclaim_mode sysctl
  * ABI.  New bits are OK, but existing bits can never change.
  */
-#define RECLAIM_ZONE	(1<<0)	/* Run shrink_inactive_list on the zone */
+#define RECLAIM_ZONE	(1<<0)	/* Prefer reclaiming & allocating locally */
 #define RECLAIM_WRITE	(1<<1)	/* Writeout pages during reclaim */
 #define RECLAIM_UNMAP	(1<<2)	/* Unmap pages during reclaim */
 
