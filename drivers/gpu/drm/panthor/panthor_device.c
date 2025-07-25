bf5a7afaa1099ccdf0b75ef92365fbd47229add2
Lukas Zapolskas <lukas.zapolskas@arm.com>
2025-07-25 15:57:53 +0100

    drm/panthor: Add DEV_QUERY.PERF_INFO handling for Gx10
    This change adds the IOCTL to query data about the performance counter
    setup. Some of this data was available via previous DEV_QUERY calls,
    for instance for GPU info, but exposing it via PERF_INFO
    minimizes the overhead of creating a single session to just the one
    aggregate IOCTL.
    
    Signed-off-by: Lukas Zapolskas <lukas.zapolskas@arm.com>
    Reviewed-by: Adrián Larumbe <adrian.larumbe@collabora.com>

--- drivers/gpu/drm/panthor/panthor_device.c
+++ drivers/gpu/drm/panthor/panthor_device.c
@@ -19,6 +19,7 @@
 #include "panthor_fw.h"
 #include "panthor_gpu.h"
 #include "panthor_mmu.h"
+#include "panthor_perf.h"
 #include "panthor_regs.h"
 #include "panthor_sched.h"
 
@@ -264,6 +265,10 @@ int panthor_device_init(struct panthor_device *ptdev)
 	if (ret)
 		goto err_unplug_fw;
 
+	ret = panthor_perf_init(ptdev);
+	if (ret)
+		goto err_unplug_sched;
+
 	/* ~3 frames */
 	pm_runtime_set_autosuspend_delay(ptdev->base.dev, 50);
 	pm_runtime_use_autosuspend(ptdev->base.dev);
@@ -277,6 +282,8 @@ int panthor_device_init(struct panthor_device *ptdev)
 
 err_disable_autosuspend:
 	pm_runtime_dont_use_autosuspend(ptdev->base.dev);
+
+err_unplug_sched:
 	panthor_sched_unplug(ptdev);
 
 err_unplug_fw:
