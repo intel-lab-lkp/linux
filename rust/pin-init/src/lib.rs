e50fd7ea0280e48cb2077c003fcdf4e277bda4d2
Suchit Karunakaran <suchitkarunakaran@gmail.com>
2025-07-27 22:32:17 +0530

    rust/pin-init: remove workaround for type inference cycle
    The `cast_pin_init` and `cast_init` functions previously used an
    intermediate `let` binding before returning the result expression to work
    around a Rust compiler issue causing type inference cycles. With the
    minimum Rust compiler version for the kernel now at 1.78.0, where this
    issue is fixed, the workaround is no longer needed. This patch removes the
    unnecessary `let` variables and returns the expressions directly.
    
    Signed-off-by: Suchit Karunakaran <suchitkarunakaran@gmail.com>

--- rust/pin-init/src/lib.rs
+++ rust/pin-init/src/lib.rs
@@ -1278,10 +1278,7 @@ pub const unsafe fn init_from_closure<T: ?Sized, E>(
 pub const unsafe fn cast_pin_init<T, U, E>(init: impl PinInit<T, E>) -> impl PinInit<U, E> {
     // SAFETY: initialization delegated to a valid initializer. Cast is valid by function safety
     // requirements.
-    let res = unsafe { pin_init_from_closure(|ptr: *mut U| init.__pinned_init(ptr.cast::<T>())) };
-    // FIXME: remove the let statement once the nightly-MSRV allows it (1.78 otherwise encounters a
-    // cycle when computing the type returned by this function)
-    res
+    unsafe { pin_init_from_closure(|ptr: *mut U| init.__pinned_init(ptr.cast::<T>())) }
 }
 
 /// Changes the to be initialized type.
@@ -1294,10 +1291,7 @@ pub const unsafe fn cast_pin_init<T, U, E>(init: impl PinInit<T, E>) -> impl Pin
 pub const unsafe fn cast_init<T, U, E>(init: impl Init<T, E>) -> impl Init<U, E> {
     // SAFETY: initialization delegated to a valid initializer. Cast is valid by function safety
     // requirements.
-    let res = unsafe { init_from_closure(|ptr: *mut U| init.__init(ptr.cast::<T>())) };
-    // FIXME: remove the let statement once the nightly-MSRV allows it (1.78 otherwise encounters a
-    // cycle when computing the type returned by this function)
-    res
+    unsafe { init_from_closure(|ptr: *mut U| init.__init(ptr.cast::<T>())) }
 }
 
 /// An initializer that leaves the memory uninitialized.
