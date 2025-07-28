7fe1caa6e8b92182a03c794a15665783b18caf10
Jugen Yu <yujugen@huawei.com>
2025-07-28 10:11:51 +0800

    hwspinlock: export structures that affects the KABI
    The member "hwlock" in struct regmap is defined with struct hwspinlock.
    Both regmap_write and regmap_read use struct regmap as a argument, so
    their KABI are affected by the hwspinlock structures.
    
    Signed-off-by: Jugen Yu <yujugen@huawei.com>

--- include/linux/hwspinlock.h
+++ include/linux/hwspinlock.h
@@ -12,6 +12,8 @@
 
 #include <linux/err.h>
 #include <linux/sched.h>
+#include <linux/spinlock.h>
+#include <linux/device.h>
 
 /* hwspinlock mode argument */
 #define HWLOCK_IRQSTATE		0x01 /* Disable interrupts, save state */
@@ -23,7 +25,53 @@ struct device;
 struct device_node;
 struct hwspinlock;
 struct hwspinlock_device;
-struct hwspinlock_ops;
+
+/**
+ * struct hwspinlock_ops - platform-specific hwspinlock handlers
+ *
+ * @trylock: make a single attempt to take the lock. returns 0 on
+ *	     failure and true on success. may _not_ sleep.
+ * @unlock:  release the lock. always succeed. may _not_ sleep.
+ * @bust:    optional, platform-specific bust handler, called by hwspinlock
+ *	     core to bust a specific lock.
+ * @relax:   optional, platform-specific relax handler, called by hwspinlock
+ *	     core while spinning on a lock, between two successive
+ *	     invocations of @trylock. may _not_ sleep.
+ */
+struct hwspinlock_ops {
+	int (*trylock)(struct hwspinlock *lock);
+	void (*unlock)(struct hwspinlock *lock);
+	int (*bust)(struct hwspinlock *lock, unsigned int id);
+	void (*relax)(struct hwspinlock *lock);
+};
+
+/**
+ * struct hwspinlock - this struct represents a single hwspinlock instance
+ * @bank: the hwspinlock_device structure which owns this lock
+ * @lock: initialized and used by hwspinlock core
+ * @priv: private data, owned by the underlying platform-specific hwspinlock drv
+ */
+struct hwspinlock {
+	struct hwspinlock_device *bank;
+	spinlock_t lock;
+	void *priv;
+};
+
+/**
+ * struct hwspinlock_device - a device which usually spans numerous hwspinlocks
+ * @dev: underlying device, will be used to invoke runtime PM api
+ * @ops: platform-specific hwspinlock handlers
+ * @base_id: id index of the first lock in this device
+ * @num_locks: number of locks in this device
+ * @lock: dynamically allocated array of 'struct hwspinlock'
+ */
+struct hwspinlock_device {
+	struct device *dev;
+	const struct hwspinlock_ops *ops;
+	int base_id;
+	int num_locks;
+	struct hwspinlock lock[];
+};
 
 /**
  * struct hwspinlock_pdata - platform data for hwspinlock drivers
