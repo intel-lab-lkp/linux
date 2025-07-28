3aa23f17437bfe1d96e0e233aebfa0422da81a63
Hans Verkuil <hverkuil@kernel.org>
2025-07-28 11:47:02 +0200

    media: update Hans Verkuil's email address
    Replace hverkuil@xs4all.nl by hverkuil@kernel.org.
    
    Signed-off-by: Hans Verkuil <hverkuil@kernel.org>

--- drivers/media/pci/ivtv/ivtv-driver.h
+++ drivers/media/pci/ivtv/ivtv-driver.h
@@ -2,7 +2,7 @@
     ivtv driver internal defines and structures
     Copyright (C) 2003-2004  Kevin Thayer <nufan_wfk at yahoo.com>
     Copyright (C) 2004  Chris Kennedy <c@groovy.org>
-    Copyright (C) 2005-2007  Hans Verkuil <hverkuil@xs4all.nl>
+    Copyright (C) 2005-2007  Hans Verkuil <hverkuil@kernel.org>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
