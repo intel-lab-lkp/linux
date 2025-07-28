ae14b96f7d63fac9e02d86fdb6ccd0129e5dc8d7
Haoran lee <470658536@qq.com>
2025-07-29 00:19:46 +0800

    scripts/mod/modpost: For CentOS 7/old binutils compatibility
    From fcf92a8a85884b383872fb435fef008fc794c3bb Mon Sep 17 00:00:00 2001
    From: houranled <leegit@163.com>
    Date: Mon, 28 Jul 2025 23:54:57 +0800
    Subject: [PATCH] CentOS 7 / old binutils compatibility
    
    Signed-off-by: Haoran Lee <470658536@qq.com>

--- scripts/mod/modpost.c
+++ scripts/mod/modpost.c
@@ -30,6 +30,32 @@
 
 #define MODULE_NS_PREFIX "module:"
 
+/* CentOS 7 / old binutils compatibility */
+#ifndef R_ARM_MOVW_ABS_NC
+#define R_ARM_MOVW_ABS_NC       43
+#endif
+#ifndef R_ARM_MOVT_ABS
+#define R_ARM_MOVT_ABS          44
+#endif
+#ifndef R_ARM_THM_MOVW_ABS_NC
+#define R_ARM_THM_MOVW_ABS_NC   47
+#endif
+#ifndef R_ARM_THM_MOVT_ABS
+#define R_ARM_THM_MOVT_ABS      48
+#endif
+#ifndef R_ARM_CALL
+#define R_ARM_CALL              28
+#endif
+#ifndef R_ARM_JUMP24
+#define R_ARM_JUMP24            29
+#endif
+#ifndef R_ARM_THM_JUMP19
+#define R_ARM_THM_JUMP19        51
+#endif
+#ifndef R_ARM_THM_JUMP24
+#define R_ARM_THM_JUMP24        30
+#endif
+
 static bool module_enabled;
 /* Are we using CONFIG_MODVERSIONS? */
 static bool modversions;
