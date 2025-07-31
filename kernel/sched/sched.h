5613618e3a005c902d2ebd36e96cc67baaf21163
Yuri Andriaccio <yurand2000@gmail.com>
2025-07-31 12:55:24 +0200

    sched/rt: Disable RT_GROUP_SCHED
    Disable the old RT_GROUP_SCHED scheduler. Note that this does not completely
    remove all the RT_GROUP_SCHED functionality, just unhooks it and removes most of
    the relevant functions. Some of the RT_GROUP_SCHED functions are kept because
    they will be adapted for the HCBS scheduling.
    
    Signed-off-by: Yuri Andriaccio <yurand2000@gmail.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -789,7 +789,7 @@ struct scx_rq {
 
 static inline int rt_bandwidth_enabled(void)
 {
-	return sysctl_sched_rt_runtime >= 0;
+	return 0;
 }
 
 /* RT IPI pull logic requires IRQ_WORK */
@@ -829,7 +829,7 @@ struct rt_rq {
 
 static inline bool rt_rq_is_runnable(struct rt_rq *rt_rq)
 {
-	return rt_rq->rt_queued && rt_rq->rt_nr_running;
+	return rt_rq->rt_nr_running;
 }
 
 /* Deadline class' related fields in a runqueue */
@@ -2545,7 +2545,7 @@ static inline bool sched_dl_runnable(struct rq *rq)
 
 static inline bool sched_rt_runnable(struct rq *rq)
 {
-	return rq->rt.rt_queued > 0;
+	return rq->rt.rt_nr_running > 0;
 }
 
 static inline bool sched_fair_runnable(struct rq *rq)
@@ -2655,9 +2655,6 @@ extern void init_sched_fair_class(void);
 extern void resched_curr(struct rq *rq);
 extern void resched_curr_lazy(struct rq *rq);
 extern void resched_cpu(int cpu);
-
-extern void init_rt_bandwidth(struct rt_bandwidth *rt_b, u64 period, u64 runtime);
-extern bool sched_rt_bandwidth_account(struct rt_rq *rt_rq);
 
 extern void init_dl_entity(struct sched_dl_entity *dl_se);
 
