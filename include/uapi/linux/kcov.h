44f6b5c318bf1b523a4468810da84f5921cab2a2
Alexander Potapenko <glider@google.com>
2025-07-31 13:51:37 +0200

    kcov: add ioctl(KCOV_RESET_TRACE)
    Provide a mechanism to reset the coverage for the current task
    without writing directly to the coverage buffer.
    This is slower, but allows the fuzzers to map the coverage buffer
    as read-only, making it harder to corrupt.
    
    Signed-off-by: Alexander Potapenko <glider@google.com>
    Reviewed-by: Dmitry Vyukov <dvyukov@google.com>

--- include/uapi/linux/kcov.h
+++ include/uapi/linux/kcov.h
@@ -23,6 +23,7 @@ struct kcov_remote_arg {
 #define KCOV_DISABLE			_IO('c', 101)
 #define KCOV_REMOTE_ENABLE		_IOW('c', 102, struct kcov_remote_arg)
 #define KCOV_UNIQUE_ENABLE		_IOW('c', 103, unsigned long)
+#define KCOV_RESET_TRACE		_IO('c', 104)
 
 enum {
 	/*
