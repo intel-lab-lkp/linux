6fbe323cf80ce92ae90a5356dac2be740c025c74
Kanchana P Sridhar <kanchana.p.sridhar@intel.com>
2025-07-31 21:36:33 -0700

    crypto: iaa - Disable iaa_verify_compress by default.
    This patch makes it easier for IAA hardware acceleration in the iaa_crypto
    driver to be loaded by default with "iaa_verify_compress" disabled, to
    facilitate performance comparisons with software compressors (which also
    do not run compress verification by default). Earlier, iaa_crypto compress
    verification used to be enabled by default.
    
    The iaa_crypto driver documentation has been updated with this change.
    
    With this patch, if users want to enable compress verification, they can do
    so with these steps:
    
      1) disable all the IAA device/wq bindings that happen at boot time
      2) rmmod iaa_crypto
      3) modprobe iaa_crypto
      4) echo 1 > /sys/bus/dsa/drivers/crypto/verify_compress
      5) re-run initialization of the IAA devices and wqs
    
    Signed-off-by: Kanchana P Sridhar <kanchana.p.sridhar@intel.com>

--- drivers/crypto/intel/iaa/iaa_crypto_main.c
+++ drivers/crypto/intel/iaa/iaa_crypto_main.c
@@ -120,7 +120,7 @@ static bool iaa_distribute_decomps;
 static bool iaa_distribute_comps = true;
 
 /* Verify results of IAA compress or not */
-static bool iaa_verify_compress = true;
+static bool iaa_verify_compress;
 
 /*
  * The iaa crypto driver supports three 'sync' methods determining how
