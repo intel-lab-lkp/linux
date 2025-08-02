a2e9215cf9a6a1b47be39bf47419c3a1b22781f3
Sven Eckelmann <sven@narfation.org>
2025-08-02 12:32:02 +0200

    i2c: rtl9300: Add missing count byte for SMBus Block Write
    The expected on-wire format of an SMBus Block Write is
    
      S Addr Wr [A] Comm [A] Count [A] Data [A] Data [A] ... [A] Data [A] P
    
    Everything starting from the Count byte is provided by the I2C subsystem in
    the array data->block. But the driver was skipping the Count byte
    (data->block[0]) when sending it to the RTL93xx I2C controller.
    
    Only the actual data could be seen on the wire:
    
      S Addr Wr [A] Comm [A] Data [A] Data [A] ... [A] Data [A] P
    
    This wire format is not SMBus Block Write compatible but matches the format
    of an I2C Block Write. Simply adding the count byte to the buffer for the
    I2C controller enough to fix the transmission.
    
    Cc: <stable@vger.kernel.org>
    Fixes: c366be720235 ("i2c: Add driver for the RTL9300 I2C controller")
    Signed-off-by: Sven Eckelmann <sven@narfation.org>

--- drivers/i2c/busses/i2c-rtl9300.c
+++ drivers/i2c/busses/i2c-rtl9300.c
@@ -288,7 +288,7 @@ static int rtl9300_i2c_smbus_xfer(struct i2c_adapter *adap, u16 addr, unsigned s
 		if (ret)
 			goto out_unlock;
 		if (read_write == I2C_SMBUS_WRITE) {
-			ret = rtl9300_i2c_write(i2c, &data->block[1], data->block[0]);
+			ret = rtl9300_i2c_write(i2c, &data->block[0], data->block[0] + 1);
 			if (ret)
 				goto out_unlock;
 		}
