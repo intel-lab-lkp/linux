835d806de212355c81bc22199ff0a58ab5d8b778
Yeoreum Yun <yeoreum.yun@arm.com>
2025-08-04 13:17:20 +0100

    arm64: make the per-task SCTLR2_EL1
    Some of field of SCTLR2 registers should be configurable per task
    not globally -- i.e) FEAT_CPA2 related field and etc.
    
    For future usage of these fields, make the per-task SCTLR2_EL1.
    
    Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>

--- arch/arm64/kernel/process.c
+++ arch/arm64/kernel/process.c
@@ -699,6 +699,11 @@ void update_sctlr_el1(u64 sctlr)
 	isb();
 }
 
+void update_sctlr2_el1(u64 sctlr2)
+{
+	sysreg_clear_set(sctlr2_el1, SCTLR2_USER_MASK, sctlr2);
+}
+
 /*
  * Thread switching.
  */
@@ -737,6 +742,10 @@ struct task_struct *__switch_to(struct task_struct *prev,
 	/* avoid expensive SCTLR_EL1 accesses if no change */
 	if (prev->thread.sctlr_user != next->thread.sctlr_user)
 		update_sctlr_el1(next->thread.sctlr_user);
+
+	if (alternative_has_cap_unlikely(ARM64_HAS_SCTLR2) &&
+	    prev->thread.sctlr2_user != next->thread.sctlr2_user)
+		update_sctlr2_el1(next->thread.sctlr2_user);
 
 	/* the actual thread switch */
 	last = cpu_switch_to(prev, next);
