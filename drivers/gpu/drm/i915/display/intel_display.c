dc60fa79c41bf32f1e39ca4e7595ab133f4ea4fa
Jani Nikula <jani.nikula@intel.com>
2025-08-05 12:18:23 +0300

    drm/i915: log HPLL frequency similar to CZCLK
    With vlv_clock_get_czclk() logging the CZ clock rate when first cached,
    do the same for HPLL VCO.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -151,6 +151,8 @@ int vlv_clock_get_hpll_vco(struct drm_device *drm)
 			CCK_FUSE_HPLL_FREQ_MASK;
 
 		i915->hpll_freq = vco_freq[hpll_freq] * 1000;
+
+		drm_dbg_kms(drm, "HPLL frequency: %d kHz\n", i915->hpll_freq);
 	}
 
 	return i915->hpll_freq;
