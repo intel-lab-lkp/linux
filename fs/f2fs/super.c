24ff37d720c860fc8f8d67b9efb7c56ef4a32a25
Liao Yuanhong <liaoyuanhong@vivo.com>
2025-08-05 21:22:08 +0800

    f2fs: Add bggc_block_io to adjust the priority of BG_GC when issuing IO
    Currently, we have encountered some issues while testing ZUFS. In
    situations near the storage limit (e.g., 50GB remaining), and after
    simulating fragmentation by repeatedly writing and deleting data, we found
    that application installation and startup tests conducted after idling for
    a few minutes take significantly longer several times that of traditional
    UFS. Tracing the operations revealed that the majority of I/Os were issued
    by background GC, which blocks normal I/O operations.
    
    Under normal circumstances, ZUFS indeed requires more background GC and
    employs a more aggressive GC strategy. However, I aim to find a way to
    minimize the impact on regular I/O operations under these near-limit
    conditions. To address this, I have introduced a bggc_block_io feature,
    which controls the prioritization of background GC in the presence of I/Os.
    This switch can be adjusted at the framework level to implement different
    strategies. If set to ALL_IO_PRIOR, all background GC operations will be
    skipped during active I/O issuance. The default option remains consistent
    with the current strategy, ensuring no change in behavior.
    
    Signed-off-by: Liao Yuanhong <liaoyuanhong@vivo.com>

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -4632,6 +4632,7 @@ static int f2fs_scan_devices(struct f2fs_sb_info *sbi)
 #ifdef CONFIG_BLK_DEV_ZONED
 	sbi->max_open_zones = UINT_MAX;
 	sbi->blkzone_alloc_policy = BLKZONE_ALLOC_PRIOR_SEQ;
+	sbi->bggc_block_io = READ_IO_PRIOR;
 #endif
 
 	for (i = 0; i < max_devices; i++) {
