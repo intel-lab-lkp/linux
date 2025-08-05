ec8ac1ec5db42cc689564ecf39b1182e7f8f5632
Steven Rostedt <rostedt@goodmis.org>
2025-08-05 15:26:51 -0400

    tracing: Display some syscall arrays as strings
    Some of the system calls that read a fixed length of memory from the user
    space address are not arrays but strings. Take a bit away from the nb_args
    field in the syscall meta data to use as a flag to denote that the system
    call's user_arg_size is being used as a string. The nb_args should never
    be more than 6, so 7 bits is plenty to hold that number. When the
    user_arg_is_str flag that, when set, will display the data array from the
    user space address as a string and not an array.
    
    This will allow the output to look like this:
    
      sys_sethostname(name: 0x5584310eb2a0 "debian", len: 6)
    
    Signed-off-by: Steven Rostedt (Google) <rostedt@goodmis.org>

--- kernel/trace/trace_syscalls.c
+++ kernel/trace/trace_syscalls.c
@@ -182,13 +182,12 @@ print_syscall_enter(struct trace_iterator *iter, int flags,
 		val = *(int *)ptr;
 
 		ptr = (void *)ent + (val & 0xffff);
+		len = val >> 16;
 
-		if (entry->user_arg_size < 0) {
-			trace_seq_printf(s, " \"%s\"", ptr);
+		if (entry->user_arg_size < 0 || entry->user_arg_is_str) {
+			trace_seq_printf(s, " \"%.*s\"", len, ptr);
 			continue;
 		}
-
-		len = val >> 16;
 
 		val = trace->args[entry->user_arg_size];
 
@@ -250,6 +249,7 @@ print_syscall_exit(struct trace_iterator *iter, int flags,
 static int __init
 __set_enter_print_fmt(struct syscall_metadata *entry, char *buf, int len)
 {
+	bool is_string = entry->user_arg_is_str;
 	int i;
 	int pos = 0;
 
@@ -267,7 +267,7 @@ __set_enter_print_fmt(struct syscall_metadata *entry, char *buf, int len)
 			continue;
 
 		/* Add the format for the user space string or array */
-		if (entry->user_arg_size < 0)
+		if (entry->user_arg_size < 0 || is_string)
 			pos += snprintf(buf + pos, LEN_OR_ZERO, " \\\"%%s\\\"");
 		else
 			pos += snprintf(buf + pos, LEN_OR_ZERO, " (%%s)");
@@ -280,7 +280,7 @@ __set_enter_print_fmt(struct syscall_metadata *entry, char *buf, int len)
 		if (!(BIT(i) & entry->user_mask))
 			continue;
 		/* The user space data for arg has name __<arg>_val */
-		if (entry->user_arg_size < 0) {
+		if (entry->user_arg_size < 0 || is_string) {
 			pos += snprintf(buf + pos, LEN_OR_ZERO, ", __get_str(__%s_val)",
 					entry->args[i]);
 		} else {
@@ -871,6 +871,19 @@ static void check_faultable_syscall(struct trace_event_call *call, int nr)
 	case __NR_pwrite64:
 		sys_data->user_mask = BIT(1);
 		sys_data->user_arg_size = 2;
+		break;
+	/* user arg 0 with size arg at 1 as string */
+	case __NR_setdomainname:
+	case __NR_sethostname:
+		sys_data->user_mask = BIT(0);
+		sys_data->user_arg_size = 1;
+		sys_data->user_arg_is_str = 1;
+		break;
+	/* user arg 4 with size arg at 3 as string */
+	case __NR_kexec_file_load:
+		sys_data->user_mask = BIT(4);
+		sys_data->user_arg_size = 3;
+		sys_data->user_arg_is_str = 1;
 		break;
 	/* user arg at position 0 */
 	case __NR_access:
