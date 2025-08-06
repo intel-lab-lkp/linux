bda426bbda0d5da7ac7e643419fdd75afe47ff86
Liao Yuanhong <liaoyuanhong@vivo.com>
2025-08-06 20:43:41 +0800

    gcc-plugins: Use swap() to simplify code
    Replace the original swapping logic with swap() to improve readability and
    remove temporary variables
    
    Signed-off-by: Liao Yuanhong <liaoyuanhong@vivo.com>

--- scripts/gcc-plugins/randomize_layout_plugin.c
+++ scripts/gcc-plugins/randomize_layout_plugin.c
@@ -199,17 +199,12 @@ static void performance_shuffle(tree *newtree, unsigned long length, ranctx *prn
 
 	/* FIXME: this group shuffle is currently a no-op. */
 	for (i = num_groups - 1; i > 0; i--) {
-		struct partition_group tmp;
 		randnum = ranval(prng_state) % (i + 1);
-		tmp = size_group[i];
-		size_group[i] = size_group[randnum];
-		size_group[randnum] = tmp;
+		swap(size_group[randnum], size_group[i]);
 	}
 
 	for (x = 0; x < num_groups; x++) {
 		for (index = size_group[x].length - 1; index > 0; index--) {
-			tree tmp;
-
 			i = size_group[x].start + index;
 			if (DECL_BIT_FIELD_TYPE(newtree[i]))
 				continue;
@@ -218,9 +213,7 @@ static void performance_shuffle(tree *newtree, unsigned long length, ranctx *prn
 			// we could handle this case differently if desired
 			if (DECL_BIT_FIELD_TYPE(newtree[randnum]))
 				continue;
-			tmp = newtree[i];
-			newtree[i] = newtree[randnum];
-			newtree[randnum] = tmp;
+			swap(newtree[randnum], newtree[i]);
 		}
 	}
 }
@@ -230,11 +223,8 @@ static void full_shuffle(tree *newtree, unsigned long length, ranctx *prng_state
 	unsigned long i, randnum;
 
 	for (i = length - 1; i > 0; i--) {
-		tree tmp;
 		randnum = ranval(prng_state) % (i + 1);
-		tmp = newtree[i];
-		newtree[i] = newtree[randnum];
-		newtree[randnum] = tmp;
+		swap(newtree[randnum], newtree[i]);
 	}
 }
 
