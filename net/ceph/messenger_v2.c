bc1754e1a935fec28ca84527df750c0d7b5d4975
Max Kellermann <max.kellermann@ionos.com>
2025-08-06 11:48:55 +0200

    net/ceph/messenger: add empty check to ceph_con_get_out_msg()
    This moves the list_empty() checks from the two callers (v1 and v2)
    into the base messenger.c library.  Now the v1/v2 specializations do
    not need to know about con->out_queue; that implementation detail is
    now hidden behind the ceph_con_get_out_msg() function.
    
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>
    Reviewed-by: Viacheslav Dubeyko <Slava.Dubeyko@ibm.com>

--- net/ceph/messenger_v2.c
+++ net/ceph/messenger_v2.c
@@ -3261,6 +3261,7 @@ static void finish_message(struct ceph_connection *con)
 
 static int populate_out_iter(struct ceph_connection *con)
 {
+	struct ceph_msg *msg;
 	int ret;
 
 	dout("%s con %p state %d out_state %d\n", __func__, con, con->state,
@@ -3306,8 +3307,7 @@ static int populate_out_iter(struct ceph_connection *con)
 			pr_err("prepare_keepalive2 failed: %d\n", ret);
 			return ret;
 		}
-	} else if (!list_empty(&con->out_queue)) {
-		struct ceph_msg *msg = ceph_con_get_out_msg(con);
+	} else if ((msg = ceph_con_get_out_msg(con)) != NULL) {
 		ret = prepare_message(con, msg);
 		if (ret) {
 			pr_err("prepare_message failed: %d\n", ret);
