89db87b21f9440145e9e94ad7d81a44811ff5cb7
Rosen Penev <rosenp@gmail.com>
2025-08-11 13:34:51 -0700

    wifi: ath10k: add nvmem support for mac address
    device_get_mac_address is a generic way to get the MAC address which
    lacks NVMEM support, which tends to be used on embedded platforms.
    
    In case device_get_mac_address fails, try of_get_mac_address_nvmem and
    handle EPROBE_DEFER to wait for the nvmem driver to initialize.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- drivers/net/wireless/ath/ath10k/core.c
+++ drivers/net/wireless/ath/ath10k/core.c
@@ -11,6 +11,7 @@
 #include <linux/module.h>
 #include <linux/firmware.h>
 #include <linux/of.h>
+#include <linux/of_net.h>
 #include <linux/property.h>
 #include <linux/dmi.h>
 #include <linux/ctype.h>
@@ -3456,7 +3457,11 @@ static int ath10k_core_probe_fw(struct ath10k *ar)
 		ath10k_debug_print_board_info(ar);
 	}
 
-	device_get_mac_address(ar->dev, ar->mac_addr);
+	if (device_get_mac_address(ar->dev, ar->mac_addr)) {
+		ret = of_get_mac_address_nvmem(ar->dev->of_node, ar->mac_addr);
+		if (ret == -EPROBE_DEFER)
+			goto err_free_firmware_files;
+	}
 
 	ret = ath10k_core_init_firmware_features(ar);
 	if (ret) {
