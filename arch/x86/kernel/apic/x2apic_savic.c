4bf9d7aa1fcb86537b657014804619c45e8870f4
Neeraj Upadhyay <Neeraj.Upadhyay@amd.com>
2025-08-11 15:14:34 +0530

    x86/apic: Support LAPIC timer for Secure AVIC
    Secure AVIC requires LAPIC timer to be emulated by the hypervisor.
    KVM already supports emulating LAPIC timer using hrtimers. In order
    to emulate LAPIC timer, APIC_LVTT, APIC_TMICT and APIC_TDCR register
    values need to be propagated to the hypervisor for arming the timer.
    APIC_TMCCT register value has to be read from the hypervisor, which
    is required for calibrating the APIC timer. So, read/write all APIC
    timer registers from/to the hypervisor.
    
    Co-developed-by: Kishon Vijay Abraham I <kvijayab@amd.com>
    Signed-off-by: Kishon Vijay Abraham I <kvijayab@amd.com>
    Reviewed-by: Tianyu Lan <tiala@microsoft.com>
    Signed-off-by: Neeraj Upadhyay <Neeraj.Upadhyay@amd.com>

--- arch/x86/kernel/apic/x2apic_savic.c
+++ arch/x86/kernel/apic/x2apic_savic.c
@@ -66,6 +66,7 @@ static u32 savic_read(u32 reg)
 	case APIC_TMICT:
 	case APIC_TMCCT:
 	case APIC_TDCR:
+		return savic_ghcb_msr_read(reg);
 	case APIC_ID:
 	case APIC_LVR:
 	case APIC_TASKPRI:
@@ -185,10 +186,12 @@ static void savic_write(u32 reg, u32 data)
 
 	switch (reg) {
 	case APIC_LVTT:
-	case APIC_LVT0:
-	case APIC_LVT1:
 	case APIC_TMICT:
 	case APIC_TDCR:
+		savic_ghcb_msr_write(reg, data);
+		break;
+	case APIC_LVT0:
+	case APIC_LVT1:
 	case APIC_TASKPRI:
 	case APIC_EOI:
 	case APIC_SPIV:
