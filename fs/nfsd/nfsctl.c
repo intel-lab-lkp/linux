aa226db3eb259809f21f77af67dcc6198af5764d
Olga Kornievskaia <okorniev@redhat.com>
2025-08-12 15:02:44 -0400

    nfsd: unregister with rpcbind when removing listener
    When a listener is added, a part of creation of transport also registers
    program/port with rpcbind. However, when the listener is removed,
    while transport goes away, rpcbind still has the entry for that
    port/type.
    
    Removal of listeners works by first removing all transports and then
    re-adding the ones that were not removed. In addition to destroying
    all transports, now also call the function that unregisters everything
    with the rpcbind. But we also then need to call the rpcbind setup
    function before adding back new transports.
    
    Fixes: d093c9089260 ("nfsd: fix management of listener transports")
    Signed-off-by: Olga Kornievskaia <okorniev@redhat.com>

--- fs/nfsd/nfsctl.c
+++ fs/nfsd/nfsctl.c
@@ -1997,8 +1997,11 @@ int nfsd_nl_listener_set_doit(struct sk_buff *skb, struct genl_info *info)
 	 * Since we can't delete an arbitrary llist entry, destroy the
 	 * remaining listeners and recreate the list.
 	 */
-	if (delete)
+	if (delete) {
 		svc_xprt_destroy_all(serv, net);
+		svc_rpcb_cleanup(serv, net);
+		svc_bind(serv, net);
+	}
 
 	/* walk list of addrs again, open any that still don't exist */
 	nlmsg_for_each_attr(attr, info->nlhdr, GENL_HDRLEN, rem) {
