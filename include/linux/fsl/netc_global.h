9e5f20af09480818fee23ba4b20a371fb47f2fb1
Wei Fang <wei.fang@nxp.com>
2025-08-12 17:46:23 +0800

    ptp: netc: add NETC V4 Timer PTP driver support
    NETC V4 Timer provides current time with nanosecond resolution, precise
    periodic pulse, pulse on timeout (alarm), and time capture on external
    pulse support. And it supports time synchronization as required for
    IEEE 1588 and IEEE 802.1AS-2020.
    
    Inside NETC, ENETC can capture the timestamp of the sent/received packet
    through the PHC provided by the Timer and record it on the Tx/Rx BD. And
    through the relevant PHC interfaces provided by the driver, the enetc V4
    driver can support PTP time synchronization.
    
    In addition, NETC V4 Timer is similar to the QorIQ 1588 timer, but it is
    not exactly the same. The current ptp-qoriq driver is not compatible with
    NETC V4 Timer, most of the code cannot be reused, see below reasons.
    
    1. The architecture of ptp-qoriq driver makes the register offset fixed,
    however, the offsets of all the high registers and low registers of V4
    are swapped, and V4 also adds some new registers. so extending ptp-qoriq
    to make it compatible with V4 Timer is tantamount to completely rewriting
    ptp-qoriq driver.
    
    2. The usage of some functions is somewhat different from QorIQ timer,
    such as the setting of TCLK_PERIOD and TMR_ADD, the logic of configuring
    PPS, etc., so making the driver compatible with V4 Timer will undoubtedly
    increase the complexity of the code and reduce readability.
    
    3. QorIQ is an expired brand. It is difficult for us to verify whether
    it works stably on the QorIQ platforms if we refactor the driver, and
    this will make maintenance difficult, so refactoring the driver obviously
    does not bring any benefits.
    
    Therefore, add this new driver for NETC V4 Timer. Note that the missing
    features like PEROUT, PPS and EXTTS will be added in subsequent patches.
    
    Signed-off-by: Wei Fang <wei.fang@nxp.com>

--- include/linux/fsl/netc_global.h
+++ include/linux/fsl/netc_global.h
@@ -1,10 +1,11 @@
 /* SPDX-License-Identifier: (GPL-2.0+ OR BSD-3-Clause) */
-/* Copyright 2024 NXP
+/* Copyright 2024-2025 NXP
  */
 #ifndef __NETC_GLOBAL_H
 #define __NETC_GLOBAL_H
 
 #include <linux/io.h>
+#include <linux/pci.h>
 
 static inline u32 netc_read(void __iomem *reg)
 {
@@ -15,5 +16,14 @@ static inline void netc_write(void __iomem *reg, u32 val)
 {
 	iowrite32(val, reg);
 }
+
+#if IS_ENABLED(CONFIG_PTP_NETC_V4_TIMER)
+int netc_timer_get_phc_index(struct pci_dev *timer_pdev);
+#else
+static inline int netc_timer_get_phc_index(struct pci_dev *timer_pdev)
+{
+	return -ENODEV;
+}
+#endif
 
 #endif
