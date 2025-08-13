65126fe505b6cce1138ac3fd7ef5d045c63d1cd5
Emil Tantilov <emil.s.tantilov@intel.com>
2025-08-12 19:42:02 -0700

    idpf: set mac type when adding and removing MAC filters
    On control planes that allow changing the MAC address of the interface,
    the driver must provide a MAC type to avoid errors such as:
    
    idpf 0000:0a:00.0: Transaction failed (op 535)
    idpf 0000:0a:00.0: Received invalid MAC filter payload (op 535) (len 0)
    idpf 0000:0a:00.0: Transaction failed (op 536)
    
    These errors occur during driver load or when changing the MAC via:
    ip link set <iface> address <mac>
    
    Add logic to set the MAC type when sending ADD/DEL (opcodes 535/536) to
    the control plane. Since only one primary MAC is supported per vport, the
    driver only needs to send an ADD opcode when setting it. Remove the old
    address by calling __idpf_del_mac_filter(), which skips the message and
    just clears the entry from the internal list.
    
    Fixes: ce1b75d0635c ("idpf: add ptypes and MAC filter support")
    Reported-by: Jian Liu <jianliu@redhat.com>
    Signed-off-by: Emil Tantilov <emil.s.tantilov@intel.com>

--- drivers/net/ethernet/intel/idpf/idpf_lib.c
+++ drivers/net/ethernet/intel/idpf/idpf_lib.c
@@ -2345,6 +2345,7 @@ static int idpf_set_mac(struct net_device *netdev, void *p)
 	struct idpf_vport_config *vport_config;
 	struct sockaddr *addr = p;
 	struct idpf_vport *vport;
+	u8 old_addr[ETH_ALEN];
 	int err = 0;
 
 	idpf_vport_ctrl_lock(netdev);
@@ -2367,17 +2368,19 @@ static int idpf_set_mac(struct net_device *netdev, void *p)
 	if (ether_addr_equal(netdev->dev_addr, addr->sa_data))
 		goto unlock_mutex;
 
+	ether_addr_copy(old_addr, vport->default_mac_addr);
+	ether_addr_copy(vport->default_mac_addr, addr->sa_data);
 	vport_config = vport->adapter->vport_config[vport->idx];
 	err = idpf_add_mac_filter(vport, np, addr->sa_data, false);
 	if (err) {
 		__idpf_del_mac_filter(vport_config, addr->sa_data);
+		ether_addr_copy(vport->default_mac_addr, netdev->dev_addr);
 		goto unlock_mutex;
 	}
 
-	if (is_valid_ether_addr(vport->default_mac_addr))
-		idpf_del_mac_filter(vport, np, vport->default_mac_addr, false);
+	if (is_valid_ether_addr(old_addr))
+		__idpf_del_mac_filter(vport_config, old_addr);
 
-	ether_addr_copy(vport->default_mac_addr, addr->sa_data);
 	eth_hw_addr_set(netdev, addr->sa_data);
 
 unlock_mutex:
