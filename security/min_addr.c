508c9ecce0d4c5f3c46a928c2159403155c74146
Qianfeng Rong <rongqianfeng@vivo.com>
2025-08-14 22:26:53 +0800

    security: min_addr: use max() to improve code
    Use max() to reduce the code in update_mmap_min_addr() and improve its
    readability.
    
    Signed-off-by: Qianfeng Rong <rongqianfeng@vivo.com>

--- security/min_addr.c
+++ security/min_addr.c
@@ -16,10 +16,7 @@ unsigned long dac_mmap_min_addr = CONFIG_DEFAULT_MMAP_MIN_ADDR;
 static void update_mmap_min_addr(void)
 {
 #ifdef CONFIG_LSM_MMAP_MIN_ADDR
-	if (dac_mmap_min_addr > CONFIG_LSM_MMAP_MIN_ADDR)
-		mmap_min_addr = dac_mmap_min_addr;
-	else
-		mmap_min_addr = CONFIG_LSM_MMAP_MIN_ADDR;
+	mmap_min_addr = max(dac_mmap_min_addr, CONFIG_LSM_MMAP_MIN_ADDR);
 #else
 	mmap_min_addr = dac_mmap_min_addr;
 #endif
