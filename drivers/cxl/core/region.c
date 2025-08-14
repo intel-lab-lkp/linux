f54a74aaccd938a90b7a0db814cb11b126fa6e30
Dave Jiang <dave.jiang@intel.com>
2025-08-14 10:16:49 -0700

    cxl, acpi/hmat: Update CXL access coordinates directly instead of through HMAT
    The current implementation of CXL memory hotplug notifier gets called
    before the HMAT memory hotplug notifier. The CXL driver calculates the
    access coordinates (bandwidth and latency values) for the CXL end to
    end path (i.e. CPU to endpoint). When the CXL region is onlined, the CXL
    memory hotplug notifier writes the access coordinates to the HMAT target
    structs. Then the HMAT memory hotplug notifier is called and it creates
    the access coordinates for the node sysfs attributes.
    
    The original intent of the 'ext_updated' flag in HMAT handling code was to
    stop HMAT memory hotplug callback from clobbering the access coordinates
    after CXL has injected its calculated coordinates and replaced the generic
    target access coordinates provided by the HMAT table in the HMAT target
    structs. However the flag is hacky at best and blocks the updates from
    other CXL regions that are onlined in the same node later on. Remove the
    'ext_updated' flag usage and just update the access coordinates for the
    nodes directly without touching HMAT target data.
    
    The hotplug memory callback ordering is changed. Instead of changing CXL,
    move HMAT back so there's room for the levels rather than have CXL share
    the same level as SLAB_CALLBACK_PRI. The change will resulting in the CXL
    callback to be executed after the HMAT callback.
    
    With the change, the CXL hotplug memory notifier runs after the HMAT
    callback. The HMAT callback will create the node sysfs attributes for
    access coordinates. The CXL callback will write the access coordinates to
    the now created node sysfs attributes directly and will not pollute the
    HMAT target values.
    
    Fixes: debdce20c4f2 ("cxl/region: Deal with numa nodes not enumerated by SRAT")
    Tested-by: Marc Herbert <marc.herbert@linux.intel.com>
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>

--- drivers/cxl/core/region.c
+++ drivers/cxl/core/region.c
@@ -2442,14 +2442,8 @@ static bool cxl_region_update_coordinates(struct cxl_region *cxlr, int nid)
 
 	for (int i = 0; i < ACCESS_COORDINATE_MAX; i++) {
 		if (cxlr->coord[i].read_bandwidth) {
-			rc = 0;
-			if (cxl_need_node_perf_attrs_update(nid))
-				node_set_perf_attrs(nid, &cxlr->coord[i], i);
-			else
-				rc = cxl_update_hmat_access_coordinates(nid, cxlr, i);
-
-			if (rc == 0)
-				cset++;
+			node_update_perf_attrs(nid, &cxlr->coord[i], i);
+			cset++;
 		}
 	}
 
