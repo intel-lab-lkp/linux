f15566798475aeb8251f1cc59caacbcf12b072e2
Anantha Prabhu <anantha.prabhu@broadcom.com>
2025-08-14 16:55:47 +0530

    RDMA/bnxt_re: Update sysfs entries with appropriate data
    Updated the existing sysfs entries with correct data.
    This change is to align the behavior with our OOB driver.
    Added "board_id" sysfs entry which will provide the
    VPD Part number, if exists.
    
    Signed-off-by: Anantha Prabhu <anantha.prabhu@broadcom.com>
    Signed-off-by: Saravanan Vajravel <saravanan.vajravel@broadcom.com>
    Signed-off-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>

--- drivers/infiniband/hw/bnxt_re/bnxt_re.h
+++ drivers/infiniband/hw/bnxt_re/bnxt_re.h
@@ -227,6 +227,8 @@ struct bnxt_re_dev {
 	struct workqueue_struct		*dcb_wq;
 	struct dentry                   *cc_config;
 	struct bnxt_re_dbg_cc_config_params *cc_config_params;
+#define BNXT_VPD_FLD_LEN	32
+	char			board_partno[BNXT_VPD_FLD_LEN];
 };
 
 #define to_bnxt_re_dev(ptr, member)	\
