fcfeaab856537d49c1ca059eab1129dab97d8cb3
Bart Van Assche <bvanassche@acm.org>
2025-08-15 09:00:42 -0700

    ufs: core: Only collect timestamps if the monitoring functionality is enabled
    Every ktime_get() call in the hot path has a measurable impact on IOPS. Hence,
    only collect timestamps if the monitoring functionality is enabled.
    
    See also commit 1d8613a23f3c ("scsi: ufs: core: Introduce HBA performance
    monitor sysfs nodes"; v5.14).
    
    Cc: Can Guo <cang@codeaurora.org>
    Cc: Bean Huo <beanhuo@micron.com>
    Cc: Daejun Park <daejun7.park@samsung.com>
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -2354,10 +2354,12 @@ void ufshcd_send_command(struct ufs_hba *hba, unsigned int task_tag,
 	struct ufshcd_lrb *lrbp = &hba->lrb[task_tag];
 	unsigned long flags;
 
-	lrbp->issue_time_stamp = ktime_get();
-	lrbp->issue_time_stamp_local_clock = local_clock();
-	lrbp->compl_time_stamp = ktime_set(0, 0);
-	lrbp->compl_time_stamp_local_clock = 0;
+	if (hba->monitor.enabled) {
+		lrbp->issue_time_stamp = ktime_get();
+		lrbp->issue_time_stamp_local_clock = local_clock();
+		lrbp->compl_time_stamp = ktime_set(0, 0);
+		lrbp->compl_time_stamp_local_clock = 0;
+	}
 	ufshcd_add_command_trace(hba, task_tag, UFS_CMD_SEND);
 	if (lrbp->cmd)
 		ufshcd_clk_scaling_start_busy(hba);
@@ -5617,8 +5619,10 @@ void ufshcd_compl_one_cqe(struct ufs_hba *hba, int task_tag,
 	enum utp_ocs ocs;
 
 	lrbp = &hba->lrb[task_tag];
-	lrbp->compl_time_stamp = ktime_get();
-	lrbp->compl_time_stamp_local_clock = local_clock();
+	if (hba->monitor.enabled) {
+		lrbp->compl_time_stamp = ktime_get();
+		lrbp->compl_time_stamp_local_clock = local_clock();
+	}
 	cmd = lrbp->cmd;
 	if (cmd) {
 		if (unlikely(ufshcd_should_inform_monitor(hba, lrbp)))
