4330c99ef5e38a04b9f752fdea4330d93b7b2a72
Xichao Zhao <zhao.xichao@vivo.com>
2025-08-15 09:53:55 +0800

    lib/raid6: Clean up code style in recov_avx512.c
    Reduce or add spaces to clean up code style.
    No functional changes here.
    
    Signed-off-by: Xichao Zhao <zhao.xichao@vivo.com>

--- lib/raid6/recov_avx512.c
+++ lib/raid6/recov_avx512.c
@@ -27,8 +27,8 @@ static void raid6_2data_recov_avx512(int disks, size_t bytes, int faila,
 	const u8 *qmul;		/* Q multiplier table (for both) */
 	const u8 x0f = 0x0f;
 
-	p = (u8 *)ptrs[disks-2];
-	q = (u8 *)ptrs[disks-1];
+	p = (u8 *)ptrs[disks - 2];
+	q = (u8 *)ptrs[disks - 1];
 
 	/*
 	 * Compute syndrome with zero for the missing data pages
@@ -38,18 +38,18 @@ static void raid6_2data_recov_avx512(int disks, size_t bytes, int faila,
 
 	dp = (u8 *)ptrs[faila];
 	ptrs[faila] = raid6_get_zero_page();
-	ptrs[disks-2] = dp;
+	ptrs[disks - 2] = dp;
 	dq = (u8 *)ptrs[failb];
 	ptrs[failb] = raid6_get_zero_page();
-	ptrs[disks-1] = dq;
+	ptrs[disks - 1] = dq;
 
 	raid6_call.gen_syndrome(disks, bytes, ptrs);
 
 	/* Restore pointer table */
 	ptrs[faila]   = dp;
 	ptrs[failb]   = dq;
-	ptrs[disks-2] = p;
-	ptrs[disks-1] = q;
+	ptrs[disks - 2] = p;
+	ptrs[disks - 1] = q;
 
 	/* Now, pick the proper data tables */
 	pbmul = raid6_vgfmul[raid6_gfexi[failb-faila]];
@@ -229,8 +229,8 @@ static void raid6_datap_recov_avx512(int disks, size_t bytes, int faila,
 	const u8 *qmul;		/* Q multiplier table */
 	const u8 x0f = 0x0f;
 
-	p = (u8 *)ptrs[disks-2];
-	q = (u8 *)ptrs[disks-1];
+	p = (u8 *)ptrs[disks - 2];
+	q = (u8 *)ptrs[disks - 1];
 
 	/*
 	 * Compute syndrome with zero for the missing data page
@@ -239,13 +239,13 @@ static void raid6_datap_recov_avx512(int disks, size_t bytes, int faila,
 
 	dq = (u8 *)ptrs[faila];
 	ptrs[faila] = raid6_get_zero_page();
-	ptrs[disks-1] = dq;
+	ptrs[disks - 1] = dq;
 
 	raid6_call.gen_syndrome(disks, bytes, ptrs);
 
 	/* Restore pointer table */
 	ptrs[faila]   = dq;
-	ptrs[disks-1] = q;
+	ptrs[disks - 1] = q;
 
 	/* Now, pick the proper data tables */
 	qmul  = raid6_vgfmul[raid6_gfinv[raid6_gfexp[faila]]];
