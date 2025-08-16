5a88a47585bef490be69114ce4203f74dd219cc1
Qianfeng Rong <rongqianfeng@vivo.com>
2025-08-16 22:37:50 +0800

    drm/nouveau: Use vmalloc_array to simplify code
    Use vmalloc_array() instead of vmalloc() to simplify the functions
    nv84_fence_suspend().
    
    vmalloc_array() is also optimized better, resulting in less instructions
    being used.
    
    Signed-off-by: Qianfeng Rong <rongqianfeng@vivo.com>

--- drivers/gpu/drm/nouveau/nv84_fence.c
+++ drivers/gpu/drm/nouveau/nv84_fence.c
@@ -158,7 +158,7 @@ nv84_fence_suspend(struct nouveau_drm *drm)
 	struct nv84_fence_priv *priv = drm->fence;
 	int i;
 
-	priv->suspend = vmalloc(array_size(sizeof(u32), drm->chan_total));
+	priv->suspend = vmalloc_array(drm->chan_total, sizeof(u32));
 	if (priv->suspend) {
 		for (i = 0; i < drm->chan_total; i++)
 			priv->suspend[i] = nouveau_bo_rd32(priv->bo, i*4);
