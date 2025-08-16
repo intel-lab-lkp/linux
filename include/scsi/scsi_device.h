6ee6992da0bf95fbe2210ce26155a730ba1dabe2
Wenchao Hao <haowenchao2@huawei.com>
2025-08-16 19:24:09 +0800

    scsi: scsi_error: Add flags to mark error handle steps has done
    LUN based error handle would mainly do three steps to recover
    commands which are check sense, start unit, and reset lun. It might
    fallback to target/host based error handle which would do these steps
    too.
    
    Target based error handle would reset target, it would also fallback
    to host based error handle.
    
    Add some flags to mark these steps are done to avoid repeating
    these steps.
    
    The flags should be cleared when LUN/target based error handler is
    waked up or when target/host based error handle finished, and set
    when fallback to target/host based error handle.
    
    scsi_eh_get_sense, scsi_eh_stu, scsi_eh_bus_device_reset and
    scsi_eh_target_reset would check these flags before actually action.
    
    Signed-off-by: Wenchao Hao <haowenchao2@huawei.com>
    Co-developed-by: JiangJianJun <jiangjianjun3@h-partners.com>
    Signed-off-by: JiangJianJun <jiangjianjun3@h-partners.com>

--- include/scsi/scsi_device.h
+++ include/scsi/scsi_device.h
@@ -105,6 +105,24 @@ struct scsi_target;
 
 struct scsi_device_eh {
 	/*
+	 * LUN rebased error handle would mainly do three
+	 * steps to recovery commands which are
+	 *   check sense
+	 *   start unit
+	 *   reset lun
+	 * While we would fallback to target or host based error handle
+	 * which would do these steps too. Add flags to mark thes steps
+	 * are done to avoid repeating these steps.
+	 *
+	 * The flags should be cleared when LUN based error handler is
+	 * wakedup or when target/host based error handle finished,
+	 * set when fallback to target or host based error handle.
+	 */
+	unsigned get_sense_done:1;
+	unsigned stu_done:1;
+	unsigned reset_done:1;
+
+	/*
 	 * add scsi command to error handler so it would be handuled by
 	 * driver's error handle strategy
 	 */
@@ -130,6 +148,16 @@ struct scsi_device_eh {
 };
 
 struct scsi_target_eh {
+	/*
+	 * flag to mark target reset is done to avoid repeating
+	 * these steps when fallback to host based error handle
+	 *
+	 * The flag should be cleared when target based error handler
+	 * is * wakedup or when host based error handle finished,
+	 * set when fallback to host based error handle.
+	 */
+	unsigned reset_done:1;
+
 	/*
 	 * add scsi command to error handler so it would be handuled by
 	 * driver's error handle strategy
