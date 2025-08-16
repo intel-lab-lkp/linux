9071e72529aad156a6d8170d4f5a3473cfc8a434
Chenghai Huang <huangchenghai2@huawei.com>
2025-08-16 18:28:30 +0800

    crypto: hisilicon/zip - remove unnecessary validation for high-performance mode configurations
    When configuring the high-performance mode register, there is no
    need to verify whether the register has been successfully
    enabled, as there is no possibility of a write failure for this
    register.
    
    Fixes: a9864bae1806 ("crypto: hisilicon/zip - add zip comp high perf mode configuration")
    Signed-off-by: Chenghai Huang <huangchenghai2@huawei.com>

--- drivers/crypto/hisilicon/zip/zip_main.c
+++ drivers/crypto/hisilicon/zip/zip_main.c
@@ -448,10 +448,9 @@ bool hisi_zip_alg_support(struct hisi_qm *qm, u32 alg)
 	return false;
 }
 
-static int hisi_zip_set_high_perf(struct hisi_qm *qm)
+static void hisi_zip_set_high_perf(struct hisi_qm *qm)
 {
 	u32 val;
-	int ret;
 
 	val = readl_relaxed(qm->io_base + HZIP_HIGH_PERF_OFFSET);
 	if (perf_mode == HZIP_HIGH_COMP_PERF)
@@ -461,13 +460,6 @@ static int hisi_zip_set_high_perf(struct hisi_qm *qm)
 
 	/* Set perf mode */
 	writel(val, qm->io_base + HZIP_HIGH_PERF_OFFSET);
-	ret = readl_relaxed_poll_timeout(qm->io_base + HZIP_HIGH_PERF_OFFSET,
-					 val, val == perf_mode, HZIP_DELAY_1_US,
-					 HZIP_POLL_TIMEOUT_US);
-	if (ret)
-		pci_err(qm->pdev, "failed to set perf mode\n");
-
-	return ret;
 }
 
 static void hisi_zip_open_sva_prefetch(struct hisi_qm *qm)
@@ -580,6 +572,8 @@ static int hisi_zip_set_user_domain_and_cache(struct hisi_qm *qm)
 	writel(SQC_CACHE_ENABLE | CQC_CACHE_ENABLE | SQC_CACHE_WB_ENABLE |
 	       CQC_CACHE_WB_ENABLE | FIELD_PREP(SQC_CACHE_WB_THRD, 1) |
 	       FIELD_PREP(CQC_CACHE_WB_THRD, 1), base + QM_CACHE_CTL);
+
+	hisi_zip_set_high_perf(qm);
 
 	hisi_zip_enable_clock_gate(qm);
 
@@ -1249,10 +1243,6 @@ static int hisi_zip_pf_probe_init(struct hisi_zip *hisi_zip)
 	ctrl->hisi_zip = hisi_zip;
 
 	ret = hisi_zip_set_user_domain_and_cache(qm);
-	if (ret)
-		return ret;
-
-	ret = hisi_zip_set_high_perf(qm);
 	if (ret)
 		return ret;
 
