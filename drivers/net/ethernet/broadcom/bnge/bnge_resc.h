4c6069827978715aab9a9fb7a674a7dcc2a1a3b4
Bhargava Marreddy <bhargava.marreddy@broadcom.com>
2025-08-18 19:47:15 +0000

    bng_en: Register default VNIC
    Allocate the default VNIC with the firmware and configure its RSS,
    HDS, and Jumbo parameters. Add related functions to support VNIC
    configuration for these parameters.
    
    Signed-off-by: Bhargava Marreddy <bhargava.marreddy@broadcom.com>
    Reviewed-by: Vikas Gupta <vikas.gupta@broadcom.com>
    Reviewed-by: Rajashekar Hudumula <rajashekar.hudumula@broadcom.com>

--- drivers/net/ethernet/broadcom/bnge/bnge_resc.h
+++ drivers/net/ethernet/broadcom/bnge/bnge_resc.h
@@ -72,6 +72,7 @@ void bnge_free_irqs(struct bnge_dev *bd);
 int bnge_net_init_dflt_config(struct bnge_dev *bd);
 void bnge_net_uninit_dflt_config(struct bnge_dev *bd);
 void bnge_aux_init_dflt_config(struct bnge_dev *bd);
+u32 bnge_get_rxfh_indir_size(struct bnge_dev *bd);
 
 static inline u32
 bnge_adjust_pow_two(u32 total_ent, u16 ent_per_blk)
