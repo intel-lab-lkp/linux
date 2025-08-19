7e64100aa6b8b2b0342965ecde0f8435532256f6
Mostafa Saleh <smostafa@google.com>
2025-08-19 21:51:46 +0000

    iommu/arm-smmu-v3-kvm: Probe SMMU HW
    Probe SMMU features from the IDR register space, most of
    the logic is common with the kernel.
    
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
@@ -48,6 +48,7 @@ struct arm_smmu_device;
 #define IDR0_S2P			(1 << 0)
 
 #define ARM_SMMU_IDR1			0x4
+#define IDR1_ECMDQ			(1 << 31)
 #define IDR1_TABLES_PRESET		(1 << 30)
 #define IDR1_QUEUES_PRESET		(1 << 29)
 #define IDR1_REL			(1 << 28)
