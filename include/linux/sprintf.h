86b81b01af39fe46785caa434aa22dfd3cfb5f8a
Liao Yuanhong <liaoyuanhong@vivo.com>
2025-08-19 21:13:27 +0800

    lib/vsprintf: remove redundant header files
    The header file <linux/stdarg.h> is already included on line 6. Remove the
    redundant include.
    
    Fixes: 0dec7201788b9 ("sprintf.h requires stdarg.h")
    Signed-off-by: Liao Yuanhong <liaoyuanhong@vivo.com>

--- include/linux/sprintf.h
+++ include/linux/sprintf.h
@@ -5,7 +5,6 @@
 #include <linux/compiler_attributes.h>
 #include <linux/stdarg.h>
 #include <linux/types.h>
-#include <linux/stdarg.h>
 
 int num_to_str(char *buf, int size, unsigned long long num, unsigned int width);
 
