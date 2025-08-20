35e90fb75e39c8b067cacc7dae5dde0c63491829
Yue Haibing <yuehaibing@huawei.com>
2025-08-20 20:32:38 +0800

    sched: Remove unused declaration ia64_set_curr_task()
    This is unused since commit cf8e8658100d ("arch: Remove Itanium (IA-64)
    architecture").
    
    Signed-off-by: Yue Haibing <yuehaibing@huawei.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1912,7 +1912,6 @@ static __always_inline bool is_idle_task(const struct task_struct *p)
 }
 
 extern struct task_struct *curr_task(int cpu);
-extern void ia64_set_curr_task(int cpu, struct task_struct *p);
 
 void yield(void);
 
