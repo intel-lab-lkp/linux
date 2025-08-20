65934d1d8523da9710239e4bfde4218f751900e0
Neill Kapron <nkapron@google.com>
2025-08-20 21:23:01 +0000

    selinux: enable per-file labeling for functionfs
    This patch adds support for genfscon per-file labeling of functionfs
    files as well as support for userspace to apply labels after new
    functionfs endpoints are created.
    
    This allows for separate labels and therefore access control on a
    per-endpoint basis. An example use case would be for the default
    endpoint EP0 used as a restricted control endpoint, and additional
    usb endpoints to be used by other more permissive domains.
    
    It should be noted that if there are multiple functionfs mounts on a
    system, genfs file labels will apply to all mounts, and therefore will not
    likely be as useful as the userspace relabeling portion of this patch -
    the addition to selinux_is_genfs_special_handling().
    
    Signed-off-by: Neill Kapron <nkapron@google.com>

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -474,6 +474,7 @@ static int selinux_is_genfs_special_handling(struct super_block *sb)
 		!strcmp(sb->s_type->name, "debugfs") ||
 		!strcmp(sb->s_type->name, "tracefs") ||
 		!strcmp(sb->s_type->name, "rootfs") ||
+		!strcmp(sb->s_type->name, "functionfs") ||
 		(selinux_policycap_cgroupseclabel() &&
 		 (!strcmp(sb->s_type->name, "cgroup") ||
 		  !strcmp(sb->s_type->name, "cgroup2")));
@@ -741,6 +742,7 @@ static int selinux_set_mnt_opts(struct super_block *sb,
 	    !strcmp(sb->s_type->name, "binder") ||
 	    !strcmp(sb->s_type->name, "bpf") ||
 	    !strcmp(sb->s_type->name, "pstore") ||
+	    !strcmp(sb->s_type->name, "functionfs") ||
 	    !strcmp(sb->s_type->name, "securityfs"))
 		sbsec->flags |= SE_SBGENFS;
 
