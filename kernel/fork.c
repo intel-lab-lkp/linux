137ec3b4d1294e7237b59760e0237fa1c15f242c
Simon Schuster <schuster.simon@siemens-energy.com>
2025-08-21 13:27:37 +0200

    copy_process: Handle architectures where sizeof(unsigned long) < sizeof(u64)
    With the introduction of clone3 in commit 7f192e3cd316 ("fork: add
    clone3") the effective bit width of clone_flags on all architectures was
    increased from 32bit to 64bit. However, the signature of the copy_*
    helper functions (e.g., copy_sighand) used by copy_process was not
    adapted, as such, they potentially truncate the flags on architectures
    such as nios2, where unsigned long is a 32bit unsigned integer type.
    
    This can, for instance, be observed via failures of kernel selftest
    clone3_clear_sighand, which attempts to trigger the conditional
    
    	if (clone_flags & CLONE_CLEAR_SIGHAND)
    
    in function copy_sighand within fork.c that will always fail given:
    
    	unsigned long /* == uint32_t */ clone_flags
    	#define CLONE_CLEAR_SIGHAND 0x100000000ULL
    
    This commit fixes the bug by always passing clone_flags via their
    declared u64 type, invariant of architecture-dependent integer sizes.
    
    Signed-off-by: Simon Schuster <schuster.simon@siemens-energy.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -1510,7 +1510,7 @@ fail_nomem:
 	return NULL;
 }
 
-static int copy_mm(unsigned long clone_flags, struct task_struct *tsk)
+static int copy_mm(u64 clone_flags, struct task_struct *tsk)
 {
 	struct mm_struct *mm, *oldmm;
 
@@ -1548,7 +1548,7 @@ static int copy_mm(unsigned long clone_flags, struct task_struct *tsk)
 	return 0;
 }
 
-static int copy_fs(unsigned long clone_flags, struct task_struct *tsk)
+static int copy_fs(u64 clone_flags, struct task_struct *tsk)
 {
 	struct fs_struct *fs = current->fs;
 	if (clone_flags & CLONE_FS) {
@@ -1569,7 +1569,7 @@ static int copy_fs(unsigned long clone_flags, struct task_struct *tsk)
 	return 0;
 }
 
-static int copy_files(unsigned long clone_flags, struct task_struct *tsk,
+static int copy_files(u64 clone_flags, struct task_struct *tsk,
 		      int no_files)
 {
 	struct files_struct *oldf, *newf;
@@ -1599,7 +1599,7 @@ static int copy_files(unsigned long clone_flags, struct task_struct *tsk,
 	return 0;
 }
 
-static int copy_sighand(unsigned long clone_flags, struct task_struct *tsk)
+static int copy_sighand(u64 clone_flags, struct task_struct *tsk)
 {
 	struct sighand_struct *sig;
 
@@ -1648,7 +1648,7 @@ static void posix_cpu_timers_init_group(struct signal_struct *sig)
 	posix_cputimers_group_init(pct, cpu_limit);
 }
 
-static int copy_signal(unsigned long clone_flags, struct task_struct *tsk)
+static int copy_signal(u64 clone_flags, struct task_struct *tsk)
 {
 	struct signal_struct *sig;
 
