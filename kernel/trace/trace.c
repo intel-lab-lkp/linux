5ea1fc007a044c8215a4db43a310cffe66fee767
Tengda Wu <wutengda@huaweicloud.com>
2025-08-21 02:11:20 +0000

    ftrace: Fix potential warning in trace_printk_seq during ftrace_dump
    When calling ftrace_dump_one() concurrently with reading trace_pipe,
    a WARN_ON_ONCE() in trace_printk_seq() can be triggered due to a race
    condition.
    
    The issue occurs because:
    
    CPU0 (ftrace_dump)                              CPU1 (reader)
    echo z > /proc/sysrq-trigger
    
    !trace_empty(&iter)
    trace_iterator_reset(&iter) <- len = size = 0
                                                    cat /sys/kernel/tracing/trace_pipe
    trace_find_next_entry_inc(&iter)
      __find_next_entry
        ring_buffer_empty_cpu <- all empty
      return NULL
    
    trace_printk_seq(&iter.seq)
      WARN_ON_ONCE(s->seq.len >= s->seq.size)
    
    In the context between trace_empty() and trace_find_next_entry_inc()
    during ftrace_dump, the ring buffer data was consumed by other readers.
    This caused trace_find_next_entry_inc to return NULL, failing to populate
    `iter.seq`. At this point, due to the prior trace_iterator_reset, both
    `iter.seq.len` and `iter.seq.size` were set to 0. Since they are equal,
    the WARN_ON_ONCE condition is triggered.
    
    Add a non-NULL check on the return value of trace_find_next_entry_inc
    prior to invoking trace_printk_seq, ensuring the `iter.seq` is properly
    populated before subsequent operations.
    
    Fixes: d769041f8653 ("ring_buffer: implement new locking")
    Signed-off-by: Tengda Wu <wutengda@huaweicloud.com>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -10617,6 +10617,7 @@ static void ftrace_dump_one(struct trace_array *tr, enum ftrace_dump_mode dump_m
 	 */
 
 	while (!trace_empty(&iter)) {
+		void *ent;
 
 		if (!cnt)
 			printk(KERN_TRACE "---------------------------------\n");
@@ -10625,17 +10626,18 @@ static void ftrace_dump_one(struct trace_array *tr, enum ftrace_dump_mode dump_m
 
 		trace_iterator_reset(&iter);
 		iter.iter_flags |= TRACE_FILE_LAT_FMT;
+		ent = trace_find_next_entry_inc(&iter);
 
-		if (trace_find_next_entry_inc(&iter) != NULL) {
+		if (ent) {
 			int ret;
 
 			ret = print_trace_line(&iter);
 			if (ret != TRACE_TYPE_NO_CONSUME)
 				trace_consume(&iter);
+
+			trace_printk_seq(&iter.seq);
 		}
 		touch_nmi_watchdog();
-
-		trace_printk_seq(&iter.seq);
 	}
 
 	if (!cnt)
