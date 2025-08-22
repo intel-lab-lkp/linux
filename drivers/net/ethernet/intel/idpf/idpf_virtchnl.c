946acc819f75e62e4ac680a205a0598293be6e44
Pavan Kumar Linga <pavan.kumar.linga@intel.com>
2025-08-21 23:32:09 -0700

    idpf: introduce idpf_q_vec_rsrc struct and move vector resources to it
    To group all the vector and queue resources, introduce idpf_q_vec_rsrc
    structure. This helps to reuse the same config path functions by other
    features. For example, PTP implementation can use the existing config
    infrastructure to configure secondary mailbox by passing its queue and
    vector info. It also helps to avoid any duplication of code.
    
    Existing queue and vector resources are grouped as default resources.
    This patch moves vector info to the newly introduced structure.
    Following patch moves the queue resources.
    
    While at it, declare the loop iterator for 'num_q_vectors' in loop and
    use the correct type.
    
    Reviewed-by: Anton Nadezhdin <anton.nadezhdin@intel.com>
    Signed-off-by: Pavan Kumar Linga <pavan.kumar.linga@intel.com>

--- drivers/net/ethernet/intel/idpf/idpf_virtchnl.c
+++ drivers/net/ethernet/intel/idpf/idpf_virtchnl.c
@@ -1949,6 +1949,7 @@ int idpf_send_map_unmap_queue_vector_msg(struct idpf_vport *vport, bool map)
 	struct idpf_vc_xn_params xn_params = {
 		.timeout_ms	= IDPF_VC_XN_DEFAULT_TIMEOUT_MSEC,
 	};
+	struct idpf_q_vec_rsrc *rsrc = &vport->dflt_qv_rsrc;
 	u32 config_sz, chunk_sz, buf_sz;
 	u32 num_msgs, num_chunks, num_q;
 	ssize_t reply_sz;
@@ -1986,7 +1987,7 @@ int idpf_send_map_unmap_queue_vector_msg(struct idpf_vport *vport, bool map)
 				v_idx = vec->v_idx;
 				tx_itr_idx = vec->tx_itr_idx;
 			} else {
-				v_idx = vport->noirq_v_idx;
+				v_idx = rsrc->noirq_v_idx;
 				tx_itr_idx = VIRTCHNL2_ITR_IDX_1;
 			}
 
@@ -2018,7 +2019,7 @@ int idpf_send_map_unmap_queue_vector_msg(struct idpf_vport *vport, bool map)
 			vqv[k].queue_id = cpu_to_le32(rxq->q_id);
 
 			if (idpf_queue_has(NOIRQ, rxq)) {
-				v_idx = vport->noirq_v_idx;
+				v_idx = rsrc->noirq_v_idx;
 				rx_itr_idx = VIRTCHNL2_ITR_IDX_0;
 			} else {
 				v_idx = rxq->q_vector->v_idx;
@@ -2075,7 +2076,7 @@ int idpf_send_map_unmap_queue_vector_msg(struct idpf_vport *vport, bool map)
 
 /**
  * idpf_send_enable_queues_msg - send enable queues virtchnl message
- * @vport: Virtual port private data structure
+ * @vport: virtual port private data structure
  * @chunks: queue ids received over mailbox
  *
  * Will send enable queues virtchnl message.  Returns 0 on success, negative on
@@ -2089,13 +2090,15 @@ int idpf_send_enable_queues_msg(struct idpf_vport *vport,
 
 /**
  * idpf_send_disable_queues_msg - send disable queues virtchnl message
- * @vport: Virtual port private data structure
+ * @vport: virtual port private data structure
+ * @rsrc: pointer to queue and vector resources
  * @chunks: queue ids received over mailbox
  *
  * Will send disable queues virtchnl message.  Returns 0 on success, negative
  * on failure.
  */
 int idpf_send_disable_queues_msg(struct idpf_vport *vport,
+				 struct idpf_q_vec_rsrc *rsrc,
 				 struct idpf_queue_id_reg_info *chunks)
 {
 	int err;
@@ -3199,6 +3202,7 @@ void idpf_vc_core_deinit(struct idpf_adapter *adapter)
 /**
  * idpf_vport_alloc_vec_indexes - Get relative vector indexes
  * @vport: virtual port data struct
+ * @rsrc: pointer to queue and vector resources
  *
  * This function requests the vector information required for the vport and
  * stores the vector indexes received from the 'global vector distribution'
@@ -3206,13 +3210,14 @@ void idpf_vc_core_deinit(struct idpf_adapter *adapter)
  *
  * Return 0 on success, error on failure
  */
-int idpf_vport_alloc_vec_indexes(struct idpf_vport *vport)
+int idpf_vport_alloc_vec_indexes(struct idpf_vport *vport,
+				 struct idpf_q_vec_rsrc *rsrc)
 {
 	struct idpf_vector_info vec_info;
 	int num_alloc_vecs;
 	u32 req;
 
-	vec_info.num_curr_vecs = vport->num_q_vectors;
+	vec_info.num_curr_vecs = rsrc->num_q_vectors;
 	if (vec_info.num_curr_vecs)
 		vec_info.num_curr_vecs += IDPF_RESERVED_VECS;
 
@@ -3225,7 +3230,7 @@ int idpf_vport_alloc_vec_indexes(struct idpf_vport *vport)
 	vec_info.index = vport->idx;
 
 	num_alloc_vecs = idpf_req_rel_vector_indexes(vport->adapter,
-						     vport->q_vector_idxs,
+						     rsrc->q_vector_idxs,
 						     &vec_info);
 	if (num_alloc_vecs <= 0) {
 		dev_err(&vport->adapter->pdev->dev, "Vector distribution failed: %d\n",
@@ -3233,7 +3238,7 @@ int idpf_vport_alloc_vec_indexes(struct idpf_vport *vport)
 		return -EINVAL;
 	}
 
-	vport->num_q_vectors = num_alloc_vecs - IDPF_RESERVED_VECS;
+	rsrc->num_q_vectors = num_alloc_vecs - IDPF_RESERVED_VECS;
 
 	return 0;
 }
@@ -3249,6 +3254,7 @@ int idpf_vport_alloc_vec_indexes(struct idpf_vport *vport)
  */
 int idpf_vport_init(struct idpf_vport *vport, struct idpf_vport_max_q *max_q)
 {
+	struct idpf_q_vec_rsrc *rsrc = &vport->dflt_qv_rsrc;
 	struct idpf_adapter *adapter = vport->adapter;
 	struct virtchnl2_create_vport *vport_msg;
 	struct idpf_vport_config *vport_config;
@@ -3293,7 +3299,7 @@ int idpf_vport_init(struct idpf_vport *vport, struct idpf_vport_max_q *max_q)
 	idpf_vport_init_num_qs(vport, vport_msg);
 	idpf_vport_calc_num_q_desc(vport);
 	idpf_vport_calc_num_q_groups(vport);
-	idpf_vport_alloc_vec_indexes(vport);
+	idpf_vport_alloc_vec_indexes(vport, rsrc);
 
 	vport->crc_enable = adapter->crc_enable;
 
