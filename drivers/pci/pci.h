14a20a6f60a4641784bdf06b7cf289b6749168aa
Hans Zhang <18255117159@163.com>
2025-08-22 23:59:02 +0800

    PCI: Replace msleep(2) with fsleep() for precise delay
    The msleep(2) may sleep longer than intended due to timer granularity.
    According to PCIe r7.0 spec, section 7.5.1.3.13, the minimum Trst is 1ms.
    We double this to 2ms to ensure we meet the requirement. Using fsleep()
    provides a more precise delay.
    
    Signed-off-by: Hans Zhang <18255117159@163.com>

--- drivers/pci/pci.h
+++ drivers/pci/pci.h
@@ -60,6 +60,12 @@ struct pcie_tlp_log;
 #define PCIE_LINK_WAIT_MAX_RETRIES	10
 #define PCIE_LINK_WAIT_SLEEP_MS		90
 
+/*
+ * PCIe r7.0, sec 7.5.1.3.13, requires minimum Trst of 1ms.
+ * Double this to 2ms to ensure we meet the minimum requirement.
+ */
+#define PCI_T_RST_SEC_BUS_DELAY_US	2000	/* 2ms */
+
 /* Message Routing (r[2:0]); PCIe r6.0, sec 2.2.8 */
 #define PCIE_MSG_TYPE_R_RC	0
 #define PCIE_MSG_TYPE_R_ADDR	1
