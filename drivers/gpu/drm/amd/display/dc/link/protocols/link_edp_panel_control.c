8b266436ff7bf5fafc6bfb30328038f94cb577f6
Antheas Kapenekakis <lkml@antheas.dev>
2025-08-24 22:01:59 +0200

    drm: panel-backlight-quirks: Convert brightness quirk to generic structure
    Currently, the brightness quirk is limited to minimum brightness only.
    Refactor it to a structure, so that more quirks can be added in the
    future. Reserve 0 value for "no quirk", and use u16 to allow minimum
    brightness up to 255.
    
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>

--- drivers/gpu/drm/amd/display/dc/link/protocols/link_edp_panel_control.c
+++ drivers/gpu/drm/amd/display/dc/link/protocols/link_edp_panel_control.c
@@ -201,7 +201,7 @@ bool edp_set_backlight_level_nits(struct dc_link *link,
 			return false;
 	} else if (link->backlight_control_type == BACKLIGHT_CONTROL_AMD_AUX) {
 		struct dpcd_source_backlight_set dpcd_backlight_set;
-		*(uint32_t *)&dpcd_backlight_set.backlight_level_millinits = backlight_millinits;
+		*(uint32_t *)&dpcd_backlight_set.backlight_level_millinits = backlight_millinits | 3;
 		*(uint16_t *)&dpcd_backlight_set.backlight_transition_time_ms = (uint16_t)transition_time_in_ms;
 
 		uint8_t backlight_control = isHDR ? 1 : 0;
