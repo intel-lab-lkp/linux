9840858015df73b62b1c253d56e622767a7e8224
Poovendhan Selvaraj <quic_poovendh@quicinc.com>
2025-08-25 17:22:03 +0530

    phy: qcom-qmp-usb-legacy: fix NULL pointer dereference in PM callbacks
    The pm ops are enabled before qmp phy create which causes
    a NULL pointer dereference when accessing qmp->phy->init_count
    in the qmp_usb_runtime_suspend.
    
    So if qmp->phy is NULL, bail out early in suspend / resume callbacks
    to avoid the NULL pointer dereference in qmp_usb_runtime_suspend and
    qmp_usb_runtime_resume.
    
    Cc: stable@vger.kernel.org # v6.6
    Fixes: e464a3180a43 ("phy: qcom-qmp-usb: split off the legacy USB+dp_com support")
    Signed-off-by: Poovendhan Selvaraj <quic_poovendh@quicinc.com>
    Signed-off-by: Kathiravan Thirumoorthy <kathiravan.thirumoorthy@oss.qualcomm.com>

--- drivers/phy/qualcomm/phy-qcom-qmp-usb-legacy.c
+++ drivers/phy/qualcomm/phy-qcom-qmp-usb-legacy.c
@@ -988,7 +988,7 @@ static int __maybe_unused qmp_usb_legacy_runtime_suspend(struct device *dev)
 
 	dev_vdbg(dev, "Suspending QMP phy, mode:%d\n", qmp->mode);
 
-	if (!qmp->phy->init_count) {
+	if (!qmp->phy || !qmp->phy->init_count) {
 		dev_vdbg(dev, "PHY not initialized, bailing out\n");
 		return 0;
 	}
@@ -1009,7 +1009,7 @@ static int __maybe_unused qmp_usb_legacy_runtime_resume(struct device *dev)
 
 	dev_vdbg(dev, "Resuming QMP phy, mode:%d\n", qmp->mode);
 
-	if (!qmp->phy->init_count) {
+	if (!qmp->phy || !qmp->phy->init_count) {
 		dev_vdbg(dev, "PHY not initialized, bailing out\n");
 		return 0;
 	}
