7eef905d4c94dd7d7e35f7d77201818384943192
Fan Gong <gongfan1@huawei.com>
2025-08-26 17:05:48 +0800

    hinic3: Nic_io initialization
    Add nic_io initialization to enable NIC service, initialize function table
    and negotiate activation of NIC features.
    
    Co-developed-by: Xin Guo <guoxin09@huawei.com>
    Signed-off-by: Xin Guo <guoxin09@huawei.com>
    Co-developed-by: Zhu Yikai <zhuyikai1@h-partners.com>
    Signed-off-by: Zhu Yikai <zhuyikai1@h-partners.com>
    Signed-off-by: Fan Gong <gongfan1@huawei.com>

--- drivers/net/ethernet/huawei/hinic3/hinic3_hwdev.c
+++ drivers/net/ethernet/huawei/hinic3/hinic3_hwdev.c
@@ -438,6 +438,18 @@ static void hinic3_uninit_comm_ch(struct hinic3_hwdev *hwdev)
 	free_base_mgmt_channel(hwdev);
 }
 
+static DEFINE_IDA(hinic3_adev_ida);
+
+static int hinic3_adev_idx_alloc(void)
+{
+	return ida_alloc(&hinic3_adev_ida, GFP_KERNEL);
+}
+
+static void hinic3_adev_idx_free(int id)
+{
+	ida_free(&hinic3_adev_ida, id);
+}
+
 static int init_hwdev(struct pci_dev *pdev)
 {
 	struct hinic3_pcidev *pci_adapter = pci_get_drvdata(pdev);
@@ -453,6 +465,7 @@ static int init_hwdev(struct pci_dev *pdev)
 	hwdev->dev = &pci_adapter->pdev->dev;
 	hwdev->func_state = 0;
 	memset(hwdev->features, 0, sizeof(hwdev->features));
+	hwdev->dev_id = hinic3_adev_idx_alloc();
 	spin_lock_init(&hwdev->channel_lock);
 
 	return 0;
@@ -524,6 +537,7 @@ err_free_hwif:
 
 err_free_hwdev:
 	pci_adapter->hwdev = NULL;
+	hinic3_adev_idx_free(hwdev->dev_id);
 	kfree(hwdev);
 
 	return err;
@@ -539,6 +553,7 @@ void hinic3_free_hwdev(struct hinic3_hwdev *hwdev)
 	hinic3_free_cfg_mgmt(hwdev);
 	destroy_workqueue(hwdev->workq);
 	hinic3_free_hwif(hwdev);
+	hinic3_adev_idx_free(hwdev->dev_id);
 	kfree(hwdev);
 }
 
