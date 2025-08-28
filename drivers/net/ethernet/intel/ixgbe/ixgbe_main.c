4991740bca08146db45a4fbb67850ba1435e8412
Kohei Enju <enjuk@amazon.com>
2025-08-29 01:00:49 +0900

    ixgbe: preserve RSS indirection table across admin down/up
    Currently, the RSS indirection table configured by user via ethtool is
    reinitialized to default values during interface resets (e.g., admin
    down/up, MTU change). As for RSS hash key, commit 3dfbfc7ebb95 ("ixgbe:
    Check for RSS key before setting value") made it persistent across
    interface resets.
    
    Adopt the same approach used in igc and igb drivers which reinitializes
    the RSS indirection table only when the queue count changes. Since the
    number of RETA entries can also change in ixgbe, let's make user
    configuration persistent as long as both queue count and the number of
    RETA entries remain unchanged.
    
    Tested on Intel Corporation 82599ES 10-Gigabit SFI/SFP+ Network
    Connection.
    
    Test:
    Set custom indirection table and check the value after interface down/up
    
      # ethtool --set-rxfh-indir ens5 equal 2
      # ethtool --show-rxfh-indir ens5 | head -5
    
      RX flow hash indirection table for ens5 with 12 RX ring(s):
          0:      0     1     0     1     0     1     0     1
          8:      0     1     0     1     0     1     0     1
         16:      0     1     0     1     0     1     0     1
      # ip link set dev ens5 down && ip link set dev ens5 up
    
    Without patch:
      # ethtool --show-rxfh-indir ens5 | head -5
    
      RX flow hash indirection table for ens5 with 12 RX ring(s):
          0:      0     1     2     3     4     5     6     7
          8:      8     9    10    11     0     1     2     3
         16:      4     5     6     7     8     9    10    11
    
    With patch:
      # ethtool --show-rxfh-indir ens5 | head -5
    
      RX flow hash indirection table for ens5 with 12 RX ring(s):
          0:      0     1     0     1     0     1     0     1
          8:      0     1     0     1     0     1     0     1
         16:      0     1     0     1     0     1     0     1
    
    Signed-off-by: Kohei Enju <enjuk@amazon.com>

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -4323,14 +4323,21 @@ static void ixgbe_setup_reta(struct ixgbe_adapter *adapter)
 	/* Fill out hash function seeds */
 	ixgbe_store_key(adapter);
 
-	/* Fill out redirection table */
-	memset(adapter->rss_indir_tbl, 0, sizeof(adapter->rss_indir_tbl));
+	/* Update redirection table in memory on first init, queue count change,
+	 * or reta entries change, otherwise preserve user configurations. Then
+	 * always write to hardware.
+	 */
+	if (adapter->last_rss_i != rss_i ||
+	    adapter->last_reta_entries != reta_entries) {
+		for (i = 0, j = 0; i < reta_entries; i++, j++) {
+			if (j == rss_i)
+				j = 0;
 
-	for (i = 0, j = 0; i < reta_entries; i++, j++) {
-		if (j == rss_i)
-			j = 0;
+			adapter->rss_indir_tbl[i] = j;
+		}
 
-		adapter->rss_indir_tbl[i] = j;
+		adapter->last_rss_i = rss_i;
+		adapter->last_reta_entries = reta_entries;
 	}
 
 	ixgbe_store_reta(adapter);
@@ -4338,8 +4345,9 @@ static void ixgbe_setup_reta(struct ixgbe_adapter *adapter)
 
 static void ixgbe_setup_vfreta(struct ixgbe_adapter *adapter)
 {
-	struct ixgbe_hw *hw = &adapter->hw;
 	u16 rss_i = adapter->ring_feature[RING_F_RSS].indices;
+	struct ixgbe_hw *hw = &adapter->hw;
+	u32 reta_entries = 64;
 	int i, j;
 
 	/* Fill out hash function seeds */
@@ -4352,12 +4360,21 @@ static void ixgbe_setup_vfreta(struct ixgbe_adapter *adapter)
 					*(adapter->rss_key + i));
 	}
 
-	/* Fill out the redirection table */
-	for (i = 0, j = 0; i < 64; i++, j++) {
-		if (j == rss_i)
-			j = 0;
+	/* Update redirection table in memory on first init, queue count change,
+	 * or reta entries change, otherwise preserve user configurations. Then
+	 * always write to hardware.
+	 */
+	if (adapter->last_rss_i != rss_i ||
+	    adapter->last_reta_entries != reta_entries) {
+		for (i = 0, j = 0; i < reta_entries; i++, j++) {
+			if (j == rss_i)
+				j = 0;
 
-		adapter->rss_indir_tbl[i] = j;
+			adapter->rss_indir_tbl[i] = j;
+		}
+
+		adapter->last_rss_i = rss_i;
+		adapter->last_reta_entries = reta_entries;
 	}
 
 	ixgbe_store_vfreta(adapter);
