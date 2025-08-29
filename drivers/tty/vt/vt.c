5b55a5dcd9c6cfaf03d31af4efe6b0bcbf75bae6
Zsolt Kajtar <soci@c64.rulez.org>
2025-08-29 21:49:07 +0200

    tty/vt: Prevent 8th bit corruption with soft cursor
    The attributes of the soft cursor are configurable and one would rightly
    expect that only the attributes are to be affected. But that's not
    guaranteed for a font with 512 glyphs as the 8th bit is in the attribute
    byte. This patch makes sure that really only the attribute bits are
    changed by the cursor and not the glyph's appearance.
    
    Signed-off-by: Zsolt Kajtar <soci@c64.rulez.org>

--- drivers/tty/vt/vt.c
+++ drivers/tty/vt/vt.c
@@ -852,6 +852,7 @@ static void add_softcursor(struct vc_data *vc)
 {
 	int i = scr_readw((u16 *) vc->vc_pos);
 	u32 type = vc->vc_cursor_type;
+	int mask = vc->vc_hi_font_mask | 0xff;
 
 	if (!(type & CUR_SW))
 		return;
@@ -865,6 +866,7 @@ static void add_softcursor(struct vc_data *vc)
 		i ^= CUR_BG;
 	if ((type & CUR_INVERT_FG_BG) && (i & CUR_FG) == ((i & CUR_BG) >> 4))
 		i ^= CUR_FG;
+	i = (i & ~mask) | (softcursor_original & mask);
 	scr_writew(i, (u16 *)vc->vc_pos);
 	if (con_should_update(vc))
 		con_putc(vc, i, vc->state.y, vc->state.x);
