4287721eb77ad21a53d78a8d103a3231485ccfd3
Max Kellermann <max.kellermann@ionos.com>
2025-08-31 11:39:11 +0200

    mm/oom_kill: add `const` to pointer parameter
    For improved const-correctness.
    
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -490,7 +490,8 @@ static bool oom_killer_disabled __read_mostly;
  * task's threads: if one of those is using this mm then this task was also
  * using it.
  */
-bool process_shares_mm(struct task_struct *p, struct mm_struct *mm)
+bool process_shares_mm(const struct task_struct *const p,
+		       const struct mm_struct *const mm)
 {
 	struct task_struct *t;
 
