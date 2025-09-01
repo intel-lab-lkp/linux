02090623227d7c6090b482fb3b77c84ecdc41cc2
Petre Rodan <petre.rodan@subdimension.ro>
2025-09-01 22:47:28 +0300

    iio: accel: BMA220 split original spi driver
    Split original driver from bma220_spi.c into bma220_core.c and bma220.h
    with a minimal number of changes in preparation for the next patches.
    
    Signed-off-by: Petre Rodan <petre.rodan@subdimension.ro>

---
+++ drivers/iio/accel/bma220.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Forward declarations needed by the bma220 sources.
+ *
+ * Copyright 2025 Petre Rodan <petre.rodan@subdimension.ro>
+ */
+
+#ifndef _BMA220_H
+#define _BMA220_H
+
+#include <linux/iio/iio.h>
+
+extern const struct dev_pm_ops bma220_pm_ops;
+
+int bma220_common_probe(struct spi_device *dev);
+
+#endif
