4dc5b37bd0b90ae60a55c59c72ba1043009b1d69
Cristian Ciocaltea <cristian.ciocaltea@collabora.com>
2025-09-02 14:56:34 +0300

    usb: vhci-hcd: Fix block comments
    Address a couple of comment formatting issues indicated by
    checkpatch.pl:
    
      WARNING: Block comments use a trailing */ on a separate line
    
    Signed-off-by: Cristian Ciocaltea <cristian.ciocaltea@collabora.com>

--- drivers/usb/usbip/vhci_hcd.c
+++ drivers/usb/usbip/vhci_hcd.c
@@ -883,8 +883,7 @@ static int vhci_urb_dequeue(struct usb_hcd *hcd, struct urb *urb, int status)
 
 	priv = urb->hcpriv;
 	if (!priv) {
-		/* URB was never linked! or will be soon given back by
-		 * vhci_rx. */
+		/* URB was never linked! or will be soon given back by vhci_rx */
 		spin_unlock_irqrestore(&vhci->lock, flags);
 		return -EIDRM;
 	}
@@ -940,8 +939,7 @@ static int vhci_urb_dequeue(struct usb_hcd *hcd, struct urb *urb, int status)
 
 		unlink->unlink_seqnum = priv->seqnum;
 
-		/* send cmd_unlink and try to cancel the pending URB in the
-		 * peer */
+		/* Send cmd_unlink and try to cancel the pending URB in the peer */
 		list_add_tail(&unlink->list, &vdev->unlink_tx);
 		wake_up(&vdev->waitq_tx);
 
