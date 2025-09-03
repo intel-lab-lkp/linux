ab091fe194bea7337392b393d5ef60e1309a477c
Vivek Kasireddy <vivek.kasireddy@intel.com>
2025-09-03 15:30:56 -0700

    drm/xe/pf: Add a helper function to get a VF's backing object in LMEM
    To properly import a dmabuf that is associated with a VF (or that
    originates in a Guest VM that includes a VF), we need to know where
    in LMEM the VF's allocated regions exist. Therefore, introduce a
    new helper to return the object that backs the VF's regions in LMEM.
    
    v2:
    - Make the helper return the LMEM object instead of the start address.
    
    Signed-off-by: Vivek Kasireddy <vivek.kasireddy@intel.com>

--- drivers/gpu/drm/xe/xe_gt_sriov_pf_config.h
+++ drivers/gpu/drm/xe/xe_gt_sriov_pf_config.h
@@ -31,6 +31,7 @@ int xe_gt_sriov_pf_config_set_fair_dbs(struct xe_gt *gt, unsigned int vfid, unsi
 int xe_gt_sriov_pf_config_bulk_set_dbs(struct xe_gt *gt, unsigned int vfid, unsigned int num_vfs,
 				       u32 num_dbs);
 
+struct xe_bo *xe_gt_sriov_pf_config_get_lmem_obj(struct xe_gt *gt, unsigned int vfid);
 u64 xe_gt_sriov_pf_config_get_lmem(struct xe_gt *gt, unsigned int vfid);
 int xe_gt_sriov_pf_config_set_lmem(struct xe_gt *gt, unsigned int vfid, u64 size);
 int xe_gt_sriov_pf_config_set_fair_lmem(struct xe_gt *gt, unsigned int vfid, unsigned int num_vfs);
