a253c1a08a206f2742e1a58527fd7e86bb34d6af
Yueyang Pan <pyyjason@gmail.com>
2025-09-03 04:16:14 -0700

    mm/show_mem: Add trylock while printing alloc info
    In production, show_mem() can be called concurrently from two
    different entities, for example one from oom_kill_process()
    another from __alloc_pages_slowpath from another kthread. This
    patch adds a spinlock and invokes trylock before printing out the
    kernel alloc info in show_mem(). This way two alloc info won't
    interleave with each other, which then makes parsing easier.
    
    Signed-off-by: Yueyang Pan <pyyjason@gmail.com>

--- mm/show_mem.c
+++ mm/show_mem.c
@@ -421,7 +421,9 @@ void __show_mem(unsigned int filter, nodemask_t *nodemask, int max_zone_idx)
 	printk("%lu pages hwpoisoned\n", atomic_long_read(&num_poisoned_pages));
 #endif
 #ifdef CONFIG_MEM_ALLOC_PROFILING
-	{
+	static DEFINE_SPINLOCK(mem_alloc_profiling_spinlock);
+
+	if (spin_trylock(&mem_alloc_profiling_spinlock)) {
 		struct codetag_bytes tags[10];
 		size_t i, nr;
 
@@ -448,6 +450,7 @@ void __show_mem(unsigned int filter, nodemask_t *nodemask, int max_zone_idx)
 						  ct->lineno, ct->function);
 			}
 		}
+		spin_unlock(&mem_alloc_profiling_spinlock);
 	}
 #endif
 }
