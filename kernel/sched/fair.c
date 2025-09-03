49e3a1d9b7fc7d3350bb3ef40ed3d016d25e986c
Zecheng Li <zecheng@google.com>
2025-09-03 19:45:00 +0000

    sched/fair: Co-locate cfs_rq and sched_entity
    Improve data locality and reduce pointer chasing by allocating struct
    cfs_rq and struct sched_entity together for non-root task groups. This
    is achieved by introducing a new combined struct cfs_rq_with_se that
    holds both objects in a single allocation.
    
    This patch:
    
    - Defines the new struct cfs_rq_with_se.
    
    - Modifies alloc_fair_sched_group() and free_fair_sched_group() to
    allocate and free the new struct as a single unit.
    
    - Modifies the per-CPU pointers in task_group->se and task_group->cfs_rq
    to point to the members in the new combined structure.
    
    Signed-off-by: Zecheng Li <zecheng@google.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -13321,10 +13321,11 @@ void free_fair_sched_group(struct task_group *tg)
 	int i;
 
 	for_each_possible_cpu(i) {
-		if (tg->cfs_rq)
-			kfree(tg->cfs_rq[i]);
-		if (tg->se)
-			kfree(tg->se[i]);
+		if (tg->cfs_rq && tg->cfs_rq[i]) {
+			struct cfs_rq_with_se *combined =
+				container_of(tg->cfs_rq[i], struct cfs_rq_with_se, cfs_rq);
+			kfree(combined);
+		}
 	}
 
 	kfree(tg->cfs_rq);
@@ -13333,6 +13334,7 @@ void free_fair_sched_group(struct task_group *tg)
 
 int alloc_fair_sched_group(struct task_group *tg, struct task_group *parent)
 {
+	struct cfs_rq_with_se *combined;
 	struct sched_entity *se;
 	struct cfs_rq *cfs_rq;
 	int i;
@@ -13349,16 +13351,13 @@ int alloc_fair_sched_group(struct task_group *tg, struct task_group *parent)
 	init_cfs_bandwidth(tg_cfs_bandwidth(tg), tg_cfs_bandwidth(parent));
 
 	for_each_possible_cpu(i) {
-		cfs_rq = kzalloc_node(sizeof(struct cfs_rq),
+		combined = kzalloc_node(sizeof(struct cfs_rq_with_se),
 				      GFP_KERNEL, cpu_to_node(i));
-		if (!cfs_rq)
+		if (!combined)
 			goto err;
 
-		se = kzalloc_node(sizeof(struct sched_entity_stats),
-				  GFP_KERNEL, cpu_to_node(i));
-		if (!se)
-			goto err_free_rq;
-
+		cfs_rq = &combined->cfs_rq;
+		se = &combined->se;
 		init_cfs_rq(cfs_rq);
 		init_tg_cfs_entry(tg, cfs_rq, se, i, parent->se[i]);
 		init_entity_runnable_average(se);
@@ -13366,8 +13365,6 @@ int alloc_fair_sched_group(struct task_group *tg, struct task_group *parent)
 
 	return 1;
 
-err_free_rq:
-	kfree(cfs_rq);
 err:
 	return 0;
 }
