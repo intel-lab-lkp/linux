77669826eead65cc2a7f817462a4eca0683eb7aa
Pooja Katiyar <pooja.katiyar@intel.com>
2025-09-05 21:31:00 -0700

    usb: typec: ucsi: Enable debugfs for message_out data structure
    Add debugfs entry for writing message_out data structure to handle
    UCSI 2.1 and 3.0 commands through debugfs interface.
    
    Users writing to the message_out debugfs file should ensure the input
    data adheres to the following format:
    1. Input must be a non-empty valid hexadecimal string.
    2. Input length of hexadecimal string must not exceed 256 bytes of
       length to be in alignment with the message out data structure size
       as per the UCSI specification v2.1.
    3. If the input string length is odd, then user needs to prepend a
       '0' to the first character for proper hex conversion.
    
    Below are examples of valid hex strings. Note that these values are
    just examples. The exact values depend on specific command use case.
    
    	#echo 1A2B3C4D > message_out
    	#echo 01234567 > message_out
    
    Reviewed-by: Heikki Krogerus <heikki.krogerus@linux.intel.com>
    Signed-off-by: Pooja Katiyar <pooja.katiyar@intel.com>

--- drivers/usb/typec/ucsi/debugfs.c
+++ drivers/usb/typec/ucsi/debugfs.c
@@ -81,6 +81,30 @@ static int ucsi_resp_show(struct seq_file *s, void *not_used)
 }
 DEFINE_SHOW_ATTRIBUTE(ucsi_resp);
 
+static ssize_t ucsi_message_out_write(struct file *file,
+				      const char __user *data, size_t count, loff_t *ppos)
+{
+	struct ucsi *ucsi = file->private_data;
+	int ret;
+
+	char *buf __free(kfree) = memdup_user_nul(data, count);
+	if (IS_ERR(buf))
+		return PTR_ERR(buf);
+
+	ucsi->message_out_size = min(count / 2, UCSI_MAX_MESSAGE_OUT_LENGTH);
+	ret = hex2bin(ucsi->message_out, buf, ucsi->message_out_size);
+	if (ret)
+		return ret;
+
+	return count;
+}
+
+static const struct file_operations ucsi_message_out_fops = {
+	.open = simple_open,
+	.write = ucsi_message_out_write,
+	.llseek = generic_file_llseek,
+};
+
 void ucsi_debugfs_register(struct ucsi *ucsi)
 {
 	ucsi->debugfs = kzalloc(sizeof(*ucsi->debugfs), GFP_KERNEL);
@@ -90,6 +114,8 @@ void ucsi_debugfs_register(struct ucsi *ucsi)
 	ucsi->debugfs->dentry = debugfs_create_dir(dev_name(ucsi->dev), ucsi_debugfs_root);
 	debugfs_create_file("command", 0200, ucsi->debugfs->dentry, ucsi, &ucsi_cmd_fops);
 	debugfs_create_file("response", 0400, ucsi->debugfs->dentry, ucsi, &ucsi_resp_fops);
+	debugfs_create_file("message_out", 0200, ucsi->debugfs->dentry, ucsi,
+			    &ucsi_message_out_fops);
 }
 
 void ucsi_debugfs_unregister(struct ucsi *ucsi)
