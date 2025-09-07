8d7affea8f68e11d623c6c54394f7626e7eab94f
Tianyou Li <tianyou.li@intel.com>
2025-09-07 23:25:10 +0800

    perf tools c2c: Add annotation support to perf c2c report
    Perf c2c report currently specified the code address and source:line
    information in the cacheline browser, while it is lack of annotation
    support like perf report to directly show the disassembly code for
    the particular symbol shared that same cacheline. This patches add
    a key 'a' binding to the cacheline browser which reuse the annotation
    browser to show the disassembly view for easier analysis of cacheline
    contentions. By default, the 'TAB' key navigate to the code address
    where the contentions detected.
    
    Signed-off-by: Tianyou Li <tianyou.li@intel.com>
    Reviewed-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Reviewed-by: Thomas Falcon <thomas.falcon@intel.com>
    Reviewed-by: Jiebin Sun <jiebin.sun@intel.com>
    Reviewed-by: Pan Deng <pan.deng@intel.com>
    Reviewed-by: Zhiguo Zhou <zhiguo.zhou@intel.com>
    Reviewed-by: Wangyang Guo <wangyang.guo@intel.com>

--- tools/perf/ui/browsers/annotate.c
+++ tools/perf/ui/browsers/annotate.c
@@ -592,7 +592,7 @@ static bool annotate_browser__callq(struct annotate_browser *browser,
 	target_ms.map = ms->map;
 	target_ms.sym = dl->ops.target.sym;
 	annotation__unlock(notes);
-	__hist_entry__tui_annotate(browser->he, &target_ms, evsel, hbt);
+	__hist_entry__tui_annotate(browser->he, &target_ms, evsel, hbt, NO_INITIAL_IP);
 	sym_title(ms->sym, ms->map, title, sizeof(title), annotate_opts.percent_type);
 	ui_browser__show_title(&browser->b, title);
 	return true;
@@ -854,6 +854,7 @@ static int annotate_browser__run(struct annotate_browser *browser,
 	const char *help = "Press 'h' for help on key bindings";
 	int delay_secs = hbt ? hbt->refresh : 0;
 	char *br_cntr_text = NULL;
+	u64 init_ip = 0;
 	char title[256];
 	int key;
 
@@ -862,6 +863,13 @@ static int annotate_browser__run(struct annotate_browser *browser,
 		return -1;
 
 	annotate_browser__calc_percent(browser, evsel);
+
+	/* the selection are intentionally even not from the sample percentage */
+	if (browser->entries.rb_node == NULL && browser->selection) {
+		init_ip = sym->start + browser->selection->offset;
+		disasm_rb_tree__insert(browser, browser->selection);
+		browser->curr_hot = rb_last(&browser->entries);
+	}
 
 	if (browser->curr_hot) {
 		annotate_browser__set_rb_top(browser, browser->curr_hot);
@@ -963,6 +971,17 @@ static int annotate_browser__run(struct annotate_browser *browser,
 				ui_helpline__puts(help);
 			annotate__scnprintf_title(hists, title, sizeof(title));
 			annotate_browser__show(&browser->b, title, help);
+			/* Previous RB tree may not valid, need refresh according to new entries*/
+			if (init_ip != 0) {
+				struct disasm_line *dl = find_disasm_line(sym, init_ip, true);
+				browser->curr_hot = NULL;
+				if (dl != NULL) {
+					browser->entries.rb_node = NULL;
+					disasm_rb_tree__insert(browser, &dl->al);
+					browser->curr_hot = rb_last(&browser->entries);
+				}
+				nd = browser->curr_hot;
+			}
 			continue;
 		case 'o':
 			annotate_opts.use_offset = !annotate_opts.use_offset;
@@ -1096,22 +1115,23 @@ out:
 }
 
 int hist_entry__tui_annotate(struct hist_entry *he, struct evsel *evsel,
-			     struct hist_browser_timer *hbt)
+			     struct hist_browser_timer *hbt, u64 init_ip)
 {
 	/* reset abort key so that it can get Ctrl-C as a key */
 	SLang_reset_tty();
 	SLang_init_tty(0, 0, 0);
 	SLtty_set_suspend_state(true);
 
-	return __hist_entry__tui_annotate(he, &he->ms, evsel, hbt);
+	return __hist_entry__tui_annotate(he, &he->ms, evsel, hbt, init_ip);
 }
 
 int __hist_entry__tui_annotate(struct hist_entry *he, struct map_symbol *ms,
 			       struct evsel *evsel,
-			       struct hist_browser_timer *hbt)
+			       struct hist_browser_timer *hbt, u64 init_ip)
 {
 	struct symbol *sym = ms->sym;
 	struct annotation *notes = symbol__annotation(sym);
+	struct disasm_line *dl = NULL;
 	struct annotate_browser browser = {
 		.b = {
 			.refresh = annotate_browser__refresh,
@@ -1161,6 +1181,18 @@ int __hist_entry__tui_annotate(struct hist_entry *he, struct map_symbol *ms,
 		map_symbol__copy(&annotate_he.ms, ms);
 
 		browser.he = &annotate_he;
+	}
+
+	/*
+	 * If init_ip is set, it means that there should be a line
+	 * intentionally selected, not based on the percentages
+	 * which caculated by the event sampling. In this case, we
+	 * convey this information into the browser selection, where
+	 * the selection in other cases should be empty.
+	 */
+	if (init_ip != NO_INITIAL_IP) {
+		dl = find_disasm_line(sym, init_ip, false);
+		browser.selection = &dl->al;
 	}
 
 	ui_helpline__push("Press ESC to exit");
