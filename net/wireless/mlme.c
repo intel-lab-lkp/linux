5333d29475a6494f235ddc441f82b6731a635cec
Janusz Dziedzic <janusz.dziedzic@gmail.com>
2025-09-08 20:07:25 +0200

    wifi: cfg80211: cancel background radar correctly
    Release background radar ownership after cancel
    delayed background cac work.
    
    Signed-off-by: Janusz Dziedzic <janusz.dziedzic@gmail.com>

--- net/wireless/mlme.c
+++ net/wireless/mlme.c
@@ -1306,11 +1306,12 @@ void cfg80211_stop_background_radar_detection(struct wireless_dev *wdev)
 		return;
 
 	rdev_set_radar_background(rdev, NULL);
-	rdev->background_radar_wdev = NULL; /* Release offchain ownership */
 
 	__cfg80211_background_cac_event(rdev, wdev,
 					&rdev->background_radar_chandef,
 					NL80211_RADAR_CAC_ABORTED);
+
+	rdev->background_radar_wdev = NULL; /* Release offchain ownership */
 }
 
 int cfg80211_assoc_ml_reconf(struct cfg80211_registered_device *rdev,
