30e2ef01459172a63843460b15a80f9fe8aa70d8
GuoHan Zhao <zhaoguohan@kylinos.cn>
2025-09-08 13:44:49 +0800

    perf parse-events: Prevent null pointer dereference in __add_event()
    In the error handling path of __add_event(), if evsel__new_idx() fails
    and returns NULL, the subsequent calls to zfree(&evsel->name) and
    zfree(&evsel->metric_id) will cause null pointer dereference.
    
    Extend the goto chain to properly handle the case where evsel allocation
    fails, avoiding unnecessary cleanup operations on a NULL pointer.
    
    Fixes: cd63c2216825 ("perf parse-events: Minor __add_event refactoring")
    Reviewed-by: Markus Elfring <Markus.Elfring@web.de>
    Reviewed-by: Ian Rogers <irogers@google.com>
    Signed-off-by: GuoHan Zhao <zhaoguohan@kylinos.cn>

--- tools/perf/util/parse-events.c
+++ tools/perf/util/parse-events.c
@@ -287,18 +287,18 @@ __add_event(struct list_head *list, int *idx,
 
 	evsel = evsel__new_idx(attr, *idx);
 	if (!evsel)
-		goto out_err;
+		goto out_put_cpus;
 
 	if (name) {
 		evsel->name = strdup(name);
 		if (!evsel->name)
-			goto out_err;
+			goto out_free_evsel;
 	}
 
 	if (metric_id) {
 		evsel->metric_id = strdup(metric_id);
 		if (!evsel->metric_id)
-			goto out_err;
+			goto out_free_evsel;
 	}
 
 	(*idx)++;
@@ -320,12 +320,14 @@ __add_event(struct list_head *list, int *idx,
 		evsel__warn_user_requested_cpus(evsel, user_cpus);
 
 	return evsel;
-out_err:
-	perf_cpu_map__put(cpus);
-	perf_cpu_map__put(pmu_cpus);
+out_free_evsel:
 	zfree(&evsel->name);
 	zfree(&evsel->metric_id);
 	free(evsel);
+out_put_cpus:
+	perf_cpu_map__put(cpus);
+	perf_cpu_map__put(pmu_cpus);
+
 	return NULL;
 }
 
