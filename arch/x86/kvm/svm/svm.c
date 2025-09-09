5576d0fae142e929f659f4a85630c545b1ab6174
Xin Li (Intel) <xin@zytor.com>
2025-09-09 11:28:24 -0700

    x86/reboot: Remove emergency_reboot_disable_virtualization()
    Remove emergency_reboot_disable_virtualization() now that virtualization
    is disabled after the CPU shuts down its local APIC and just before it
    powers off.
    
    Also remove kvm_arch_{enable,disable}_virtualization() as they are no
    longer needed.
    
    Signed-off-by: Xin Li (Intel) <xin@zytor.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -497,13 +497,6 @@ static inline void kvm_cpu_svm_disable(void)
 	}
 }
 
-static void svm_emergency_disable_virtualization_cpu(void)
-{
-	kvm_rebooting = true;
-
-	kvm_cpu_svm_disable();
-}
-
 static void svm_disable_virtualization_cpu(void)
 {
 	/* Make sure we clean up behind us */
@@ -5050,7 +5043,6 @@ static struct kvm_x86_ops svm_x86_ops __initdata = {
 	.hardware_unsetup = svm_hardware_unsetup,
 	.enable_virtualization_cpu = svm_enable_virtualization_cpu,
 	.disable_virtualization_cpu = svm_disable_virtualization_cpu,
-	.emergency_disable_virtualization_cpu = svm_emergency_disable_virtualization_cpu,
 	.has_emulated_msr = svm_has_emulated_msr,
 
 	.vcpu_create = svm_vcpu_create,
