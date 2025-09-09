7d73c06950a66fc56fb7a40ec0e992ee24e0d59a
Dan Carpenter <dan.carpenter@linaro.org>
2025-09-09 13:29:50 +0300

    PM / devfreq: Fix double free in devfreq_event_add_edev()
    The put_device() function calls devfreq_event_release_edev() which frees
    "evdev".  Calling kfree() again is a double free.
    
    Fixes: 430a1845c804 ("PM / devfreq: Fix memory leak in devfreq_event_add_edev()")
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

--- drivers/devfreq/devfreq-event.c
+++ drivers/devfreq/devfreq-event.c
@@ -328,7 +328,6 @@ struct devfreq_event_dev *devfreq_event_add_edev(struct device *dev,
 	ret = device_register(&edev->dev);
 	if (ret < 0) {
 		put_device(&edev->dev);
-		kfree(edev);
 		return ERR_PTR(ret);
 	}
 	dev_set_drvdata(&edev->dev, edev);
