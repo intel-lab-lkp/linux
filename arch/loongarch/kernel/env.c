4ae4851e1d0573468c599a55add984fe0e060ddc
cuitao <cuitao@kylinos.cn>
2025-09-09 15:19:42 +0800

    LoongArch: Add a check for the return value when creating the kobj
    Add a check for the return value of kobject_create_and_add to ensure
    that the kobj allocation succeeds.
    
    Signed-off-by: cuitao <cuitao@kylinos.cn>

--- arch/loongarch/kernel/env.c
+++ arch/loongarch/kernel/env.c
@@ -109,6 +109,10 @@ static int __init boardinfo_init(void)
 	struct kobject *loongson_kobj;
 
 	loongson_kobj = kobject_create_and_add("loongson", firmware_kobj);
+	if (!loongson_kobj) {
+		pr_warn("loongson: Firmware registration failed.\n");
+		return -ENOMEM;
+	}
 
 	return sysfs_create_file(loongson_kobj, &boardinfo_attr.attr);
 }
