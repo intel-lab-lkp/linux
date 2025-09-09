773636f9b4f3c97c7d916f26269beb2716c0b1f2
Miklos Szeredi <mszeredi@redhat.com>
2025-09-09 16:30:47 +0200

    fanotify: add watchdog for permission events
    This is to make it easier to debug issues with AV software, which time and
    again deadlocks with no indication of where the issue comes from, and the
    kernel being blamed for the deadlock.  Then we need to analyze dumps to
    prove that the kernel is not in fact at fault.
    
    The deadlock comes from recursion: handling the event triggers another
    permission event, in some roundabout way, obviously, otherwise it would
    have been found in testing.
    
    With this patch a warning is printed when permission event is received by
    userspace but not answered for more than the timeout specified in
    /proc/sys/fs/fanotify/watchdog_timeout.  The watchdog can be turned off by
    setting the timeout to zero (which is the default).
    
    The timeout is very coarse (T <= t < 2T) but I guess it's good enough for
    the purpose.
    
    Overhead should be minimal.
    
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>

--- fs/notify/fanotify/fanotify.h
+++ fs/notify/fanotify/fanotify.h
@@ -437,11 +437,13 @@ FANOTIFY_ME(struct fanotify_event *event)
 struct fanotify_perm_event {
 	struct fanotify_event fae;
 	struct path path;
-	const loff_t *ppos;		/* optional file range info */
+	const loff_t *ppos;	/* optional file range info */
 	size_t count;
 	u32 response;			/* userspace answer to the event */
 	unsigned short state;		/* state of the event */
+	unsigned short watchdog_cnt;	/* already scanned by watchdog? */
 	int fd;		/* fd we passed to userspace for this event */
+	pid_t recv_pid;	/* pid of task receiving the event */
 	union {
 		struct fanotify_response_info_header hdr;
 		struct fanotify_response_info_audit_rule audit_rule;
