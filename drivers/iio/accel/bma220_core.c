bc56448150d2c4f4dafa7c77fa5bdeb1e4a88727
Petre Rodan <petre.rodan@subdimension.ro>
2025-09-10 10:57:15 +0300

    iio: accel: bma220: add i2c watchdog feature
    Sometimes the sensor gets stuck and enters a condition in which it pulls
    SDA low, thus making the entire i2c bus unusable.
    This problem is mitigated by activating a 1ms watchdog implemented in
    the sensor.
    
    Signed-off-by: Petre Rodan <petre.rodan@subdimension.ro>

--- drivers/iio/accel/bma220_core.c
+++ drivers/iio/accel/bma220_core.c
@@ -10,6 +10,7 @@
 #include <linux/bitfield.h>
 #include <linux/cleanup.h>
 #include <linux/device.h>
+#include <linux/i2c.h>
 #include <linux/kernel.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
@@ -359,6 +360,11 @@ static int bma220_power(struct bma220_data *data, bool up)
 	return -EBUSY;
 }
 
+static int bma220_set_wdt(struct bma220_data *data, const u8 val)
+{
+	return regmap_update_bits(data->regmap, BMA220_REG_WDT, BMA220_WDT_MASK,
+				  FIELD_PREP(BMA220_WDT_MASK, val));
+}
 
 static int bma220_init(struct bma220_data *data)
 {
@@ -389,6 +395,13 @@ static int bma220_init(struct bma220_data *data)
 	ret = bma220_reset(data, true);
 	if (ret)
 		return dev_err_probe(dev, ret, "Failed to soft reset chip\n");
+
+	if (i2c_verify_client(dev)) {
+		ret = bma220_set_wdt(data, BMA220_WDT_1MS);
+		if (ret)
+			return dev_err_probe(dev, ret,
+					     "Failed to set i2c watchdog\n");
+	}
 
 	return 0;
 }
