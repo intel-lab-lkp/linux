e153dfdc88779d58d2fe2d015f9d1098681998cc
Kuniyuki Iwashima <kuniyu@google.com>
2025-09-11 03:05:35 +0000

    tls: Use sk_dst_dev_rcu() in get_netdev_for_sock().
    get_netdev_for_sock() is called during setsockopt(),
    so not under RCU.
    
    Using sk_dst_get(sk)->dev could trigger UAF.
    
    Let's use sk_dst_dev_rcu().
    
    Note that the only ->ndo_sk_get_lower_dev() user is
    bond_sk_get_lower_dev(), which uses RCU.
    
    Fixes: e8f69799810c ("net/tls: Add generic NIC offload infrastructure")
    Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>

--- net/tls/tls_device.c
+++ net/tls/tls_device.c
@@ -123,17 +123,17 @@ static void tls_device_queue_ctx_destruction(struct tls_context *ctx)
 /* We assume that the socket is already connected */
 static struct net_device *get_netdev_for_sock(struct sock *sk)
 {
-	struct dst_entry *dst = sk_dst_get(sk);
-	struct net_device *netdev = NULL;
+	struct net_device *dev, *lowest_dev = NULL;
 
-	if (likely(dst)) {
-		netdev = netdev_sk_get_lowest_dev(dst->dev, sk);
-		dev_hold(netdev);
+	rcu_read_lock();
+	dev = sk_dst_dev_rcu(sk);
+	if (likely(dev)) {
+		lowest_dev = netdev_sk_get_lowest_dev(dev, sk);
+		dev_hold(lowest_dev);
 	}
+	rcu_read_unlock();
 
-	dst_release(dst);
-
-	return netdev;
+	return lowest_dev;
 }
 
 static void destroy_record(struct tls_record_info *record)
