0ff7152209580a001b937e9e29bdd9fd67173fe0
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2025-09-11 08:15:54 +0530

    drm/i915/vrr: Fix seamless_mn drrs for PTL
    With VRR timing generator always on, the fixed refresh rate is achieved
    by setting vrr.flipline and vrr.vmax as the vtotal for the desired mode.
    
    This creates a problem for seamless_mn drrs feature, where user can
    seamlessly set a lower mode on the supporting panels. With VRR timing
    generator, the vrr.flipline and vrr.vmax are set to vtotal, but that
    corresponds to the higher mode.
    
    To fix this, re-compute the vrr timings when seamless_mn drrs is in
    picture. At the same time make sure that the vrr.guardband is set as
    per the highest mode for such panels, so that switching between higher
    to lower mode, does not change the vrr.guardband.
    
    v2: Add a new member `use_highest_mode` to vrr struct to help set the
    vrr timings for highest mode for the seamless_mn drrs case.
    v3:
    -Modify existing function to compute fixed refresh rate timings instead
    of adding a new function. (Mitul)
    -Tweak computation for scaling the vtotal and use DIV_ROUND_UP_ULL.
    -Improve documentation.
    
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>
    Reviewed-by: Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>

--- drivers/gpu/drm/i915/display/intel_dp.h
+++ drivers/gpu/drm/i915/display/intel_dp.h
@@ -217,5 +217,6 @@ void intel_dp_dpcd_set_probe(struct intel_dp *intel_dp, bool force_on_external);
 bool intel_dp_in_hdr_mode(const struct drm_connector_state *conn_state);
 int intel_dp_compute_sdp_latency(const struct intel_crtc_state *crtc_state,
 				 bool assume_all_enabled);
+bool has_seamless_m_n(struct intel_connector *connector);
 
 #endif /* __INTEL_DP_H__ */
