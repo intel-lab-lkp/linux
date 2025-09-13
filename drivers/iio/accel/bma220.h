e51403e66843c16690413a423b45868ad44fd601
Petre Rodan <petre.rodan@subdimension.ro>
2025-09-13 18:39:25 +0300

    iio: accel: bma220: split original driver
    In preparation for the i2c module, move the original code into multiple
    source files without any other functional change.
    
    Create the additional bma220_core module which currently is not
    providing an abstracted bus type (this will change with the regmap
    patch).
    Fix checkpatch warning about GPL v2 license in bma220_spi.c.
    Fix a few includes to make the change functional and prepare for the
    next patches.
    
    Signed-off-by: Petre Rodan <petre.rodan@subdimension.ro>

---
+++ drivers/iio/accel/bma220.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Forward declarations needed by the bma220 sources.
+ *
+ * Copyright 2025 Petre Rodan <petre.rodan@subdimension.ro>
+ */
+
+#ifndef _BMA220_H
+#define _BMA220_H
+
+#include <linux/pm.h>
+
+extern const struct dev_pm_ops bma220_pm_ops;
+struct spi_device;
+
+int bma220_common_probe(struct spi_device *dev);
+
+#endif
