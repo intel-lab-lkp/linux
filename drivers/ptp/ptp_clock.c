4113173a5f394a0a07838aa71739fd1c03d72b28
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2025-09-15 15:42:05 +0100

    ptp: rework ptp_clock_unregister() to disable events
    the ordering of ptp_clock_unregister() is not ideal, as the chardev
    remains published while state is being torn down. There is also no
    cleanup of enabled pin settings, which means enabled events can
    still forward into the core.
    
    Rework the ordering of cleanup in ptp_clock_unregister() so that we
    unpublish the posix clock (and user chardev), disable any pins that
    have events enabled, and then clean up the aux work and PPS source.
    
    This avoids potential use-after-free and races in PTP clock driver
    teardown.
    
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>

--- drivers/ptp/ptp_clock.c
+++ drivers/ptp/ptp_clock.c
@@ -498,8 +498,22 @@ int ptp_clock_unregister(struct ptp_clock *ptp)
 		device_for_each_child(&ptp->dev, NULL, unregister_vclock);
 	}
 
+	/* Get the device to stop posix_clock_unregister() doing the last put
+	 * and freeing the structure(s)
+	 */
+	get_device(&ptp->dev);
+
+	/* Wake up any userspace waiting for an event. */
 	ptp->defunct = 1;
 	wake_up_interruptible(&ptp->tsev_wq);
+
+	/* Tear down the POSIX clock, which removes the user interface. */
+	posix_clock_unregister(&ptp->clock);
+
+	/* Disable any pin functions that the user may have setup, quiescing
+	 * all incoming events.
+	 */
+	ptp_disable_all_pins(ptp);
 
 	if (ptp->kworker) {
 		kthread_cancel_delayed_work_sync(&ptp->aux_work);
@@ -510,7 +524,8 @@ int ptp_clock_unregister(struct ptp_clock *ptp)
 	if (ptp->pps_source)
 		pps_unregister_source(ptp->pps_source);
 
-	posix_clock_unregister(&ptp->clock);
+	/* The final put, normally here, will invoke ptp_clock_release(). */
+	put_device(&ptp->dev);
 
 	return 0;
 }
