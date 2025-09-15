6ad3f889c5d4fc8bf9efdd9568e2f299df481f69
Kyrie Wu <kyrie.wu@mediatek.com>
2025-09-15 11:02:00 +0800

    media: mediatek: jpeg: fix jpeg hw count setting
    Different ICs have different amounts of hardware,
    use a variable to set the amount of hardware.
    
    Fixes: 934e8bccac95 ("mtk-jpegenc: support jpegenc multi-hardware")
    Fixes: 0fa49df4222f ("media: mtk-jpegdec: support jpegdec multi-hardware")
    
    Signed-off-by: Kyrie Wu <kyrie.wu@mediatek.com>

--- drivers/media/platform/mediatek/jpeg/mtk_jpeg_enc_hw.c
+++ drivers/media/platform/mediatek/jpeg/mtk_jpeg_enc_hw.c
@@ -386,6 +386,7 @@ static int mtk_jpegenc_hw_probe(struct platform_device *pdev)
 	master_dev->enc_hw_dev[i] = dev;
 	master_dev->reg_encbase[i] = dev->reg_base;
 	dev->master_dev = master_dev;
+	master_dev->max_hw_count++;
 
 	platform_set_drvdata(pdev, dev);
 	pm_runtime_enable(&pdev->dev);
