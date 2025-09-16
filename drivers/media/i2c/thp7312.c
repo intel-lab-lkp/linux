586918a43fe17ea3c70ac1a663e9dc6adf693d1c
Sakari Ailus <sakari.ailus@linux.intel.com>
2025-09-16 19:01:25 +0300

    media: thp7312: Use fwnode_for_each_child_node() instead
    fwnode_for_each_child_node() is now the same as
    fwnode_for_each_available_child_node() on all backends (OF, ACPI and
    swnode). In order to remove the available variants, switch the uses to
    non-available variants.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- drivers/media/i2c/thp7312.c
+++ drivers/media/i2c/thp7312.c
@@ -2067,7 +2067,7 @@ static int thp7312_parse_dt(struct thp7312_device *thp7312)
 		return -EINVAL;
 	}
 
-	fwnode_for_each_available_child_node(sensors, node) {
+	fwnode_for_each_child_node(sensors, node) {
 		if (fwnode_name_eq(node, "sensor")) {
 			if (!thp7312_sensor_parse_dt(thp7312, node))
 				num_sensors++;
