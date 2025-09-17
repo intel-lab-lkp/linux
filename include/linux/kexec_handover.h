cc68f5600ee6001908b9d559088855e4063036c0
Mike Rapoport (Microsoft) <rppt@kernel.org>
2025-09-17 20:40:31 +0300

    kho: replace kho_preserve_phys() with kho_preserve_pages()
    to make it clear that KHO operates on pages rather than on a random
    physical address.
    
    The kho_preserve_pages() will be also used in upcoming support for
    vmalloc preservation.
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- include/linux/kexec_handover.h
+++ include/linux/kexec_handover.h
@@ -18,6 +18,7 @@ enum kho_event {
 
 struct folio;
 struct notifier_block;
+struct page;
 
 #define DECLARE_KHOSER_PTR(name, type) \
 	union {                        \
@@ -42,7 +43,7 @@ struct kho_serialization;
 bool kho_is_enabled(void);
 
 int kho_preserve_folio(struct folio *folio);
-int kho_preserve_phys(phys_addr_t phys, size_t size);
+int kho_preserve_pages(struct page *page, unsigned int nr_pages);
 struct folio *kho_restore_folio(phys_addr_t phys);
 int kho_add_subtree(struct kho_serialization *ser, const char *name, void *fdt);
 int kho_retrieve_subtree(const char *name, phys_addr_t *phys);
@@ -65,7 +66,7 @@ static inline int kho_preserve_folio(struct folio *folio)
 	return -EOPNOTSUPP;
 }
 
-static inline int kho_preserve_phys(phys_addr_t phys, size_t size)
+static inline int kho_preserve_pages(struct page *page, unsigned int nr_pages)
 {
 	return -EOPNOTSUPP;
 }
