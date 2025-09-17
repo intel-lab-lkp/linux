c40a1a8b75e0d037120a9325308b1175b4ed2a70
Zecheng Li <zecheng@google.com>
2025-09-17 19:58:06 +0000

    perf dwarf-aux: Preserve typedefs in match_var_offset
    Since we are skipping the check_variable, we need to preserve typedefs
    in match_var_offset to match the results by __die_get_real_type. Also
    move the (offset == 0) branch after the is_pointer check to ensure the
    correct type is used, fixing cases where an incorrect pointer type was
    chosen when the access offset was 0.
    
    Signed-off-by: Zecheng Li <zecheng@google.com>

--- tools/perf/util/dwarf-aux.c
+++ tools/perf/util/dwarf-aux.c
@@ -1395,22 +1395,22 @@ static bool match_var_offset(Dwarf_Die *die_mem, struct find_var_data *data,
 	Dwarf_Word size;
 	s64 offset = addr_offset - addr_type;
 
-	if (offset == 0) {
-		/* Update offset relative to the start of the variable */
-		data->offset = 0;
-		return true;
-	}
-
 	if (offset < 0)
 		return false;
 
-	if (die_get_real_type(die_mem, &data->type) == NULL)
+	if (__die_get_real_type(die_mem, &data->type) == NULL)
 		return false;
 
 	if (is_pointer && dwarf_tag(&data->type) == DW_TAG_pointer_type) {
 		/* Get the target type of the pointer */
-		if (die_get_real_type(&data->type, &data->type) == NULL)
+		if (__die_get_real_type(&data->type, &data->type) == NULL)
 			return false;
+	}
+
+	if (offset == 0) {
+		/* Update offset relative to the start of the variable */
+		data->offset = 0;
+		return true;
 	}
 
 	if (dwarf_aggregate_size(&data->type, &size) < 0)
