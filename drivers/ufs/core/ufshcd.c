cb2a686810a26703ceefcc1c127480972398bb1c
Zhongqiu Han <zhongqiu.han@oss.qualcomm.com>
2025-09-17 17:41:43 +0800

    scsi: ufs: core: Fix data race in CPU latency PM QoS request handling
    The cpu_latency_qos_add/remove/update_request interfaces lack internal
    synchronization by design, requiring the caller to ensure thread safety.
    The current implementation relies on the `pm_qos_enabled` flag, which is
    insufficient to prevent concurrent access and cannot serve as a proper
    synchronization mechanism. This has led to data races and list corruption
    issues.
    
    A typical race condition call trace is:
    
    [Thread A]
    ufshcd_pm_qos_exit()
      --> cpu_latency_qos_remove_request()
        --> cpu_latency_qos_apply();
          --> pm_qos_update_target()
            --> plist_del              <--(1) delete plist node
        --> memset(req, 0, sizeof(*req));
      --> hba->pm_qos_enabled = false;
    
    [Thread B]
    ufshcd_devfreq_target
      --> ufshcd_devfreq_scale
        --> ufshcd_scale_clks
          --> ufshcd_pm_qos_update     <--(2) pm_qos_enabled is true
            --> cpu_latency_qos_update_request
              --> pm_qos_update_target
                --> plist_del          <--(3) plist node use-after-free
    
    This patch introduces a dedicated mutex to serialize PM QoS operations,
    preventing data races and ensuring safe access to PM QoS resources,
    including sysfs interface reads.
    
    Fixes: 2777e73fc154 ("scsi: ufs: core: Add CPU latency QoS support for UFS driver")
    Signed-off-by: Zhongqiu Han <zhongqiu.han@oss.qualcomm.com>

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -1047,6 +1047,7 @@ EXPORT_SYMBOL_GPL(ufshcd_is_hba_active);
  */
 void ufshcd_pm_qos_init(struct ufs_hba *hba)
 {
+	guard(mutex)(&hba->pm_qos_mutex);
 
 	if (hba->pm_qos_enabled)
 		return;
@@ -1063,6 +1064,8 @@ void ufshcd_pm_qos_init(struct ufs_hba *hba)
  */
 void ufshcd_pm_qos_exit(struct ufs_hba *hba)
 {
+	guard(mutex)(&hba->pm_qos_mutex);
+
 	if (!hba->pm_qos_enabled)
 		return;
 
@@ -1077,6 +1080,8 @@ void ufshcd_pm_qos_exit(struct ufs_hba *hba)
  */
 static void ufshcd_pm_qos_update(struct ufs_hba *hba, bool on)
 {
+	guard(mutex)(&hba->pm_qos_mutex);
+
 	if (!hba->pm_qos_enabled)
 		return;
 
@@ -10764,6 +10769,10 @@ int ufshcd_init(struct ufs_hba *hba, void __iomem *mmio_base, unsigned int irq)
 	mutex_init(&hba->ee_ctrl_mutex);
 
 	mutex_init(&hba->wb_mutex);
+
+	/* Initialize mutex for PM QoS request synchronization */
+	mutex_init(&hba->pm_qos_mutex);
+
 	init_rwsem(&hba->clk_scaling_lock);
 
 	ufshcd_init_clk_gating(hba);
