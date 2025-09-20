a6c21161351774bca84ebfae4504e270d0441b3d
Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>
2025-09-21 01:11:01 +0530

    firmware: qcom_scm: Introduce PAS context initialization and destroy helper
    When Secure Peripheral Authentication Service (PAS) method runs on a
    SoC where Linux runs at EL2 (Gunyah absence) where reset sequences
    move to EL3 and Linux need to do some extra stuff before calling PAS
    SMC calls like creating SHMbridge. So, PAS SMC call need awareness and
    need handling of things required when Linux run at EL2.
    
    Currently, remoteproc and non-remoteproc subsystems use different
    variants of the MDT loader helper API, primarily due to the handling of
    the metadata context. Remoteproc subsystems retain metadata context
    until authentication and reset is done, while non-remoteproc subsystems
    (e.g., video, graphics, ipa etc.) do not need to retain it and can free
    the context right inside qcom_scm_pas_init() call based on passed context
    parameter as NULL.
    
    So, in an attempt to unify the metadata API process for both remoteproc
    and non-remoteproc subsystems and to make the SMC helper function
    cleaner whether SoC running with Gunyah presence or absence by introducing
    a dedicated PAS context initialization and destroy function. Context
    initialization beforehand would help all SMC function to carry it and do
    the right thing whether SoC is running with Gunyah presence or absence.
    
    Signed-off-by: Mukesh Ojha <mukesh.ojha@oss.qualcomm.com>

--- drivers/firmware/qcom/qcom_scm.c
+++ drivers/firmware/qcom/qcom_scm.c
@@ -559,6 +559,59 @@ static void qcom_scm_set_download_mode(u32 dload_mode)
 }
 
 /**
+ * qcom_scm_pas_ctx_init() - Initialize peripheral authentication service
+ *			     context for a given peripheral and it can be
+ *			     destroyed with qcom_scm_pas_ctx_destroy() to
+ *			     release the context
+ *
+ * @dev:	  PAS firmware device
+ * @pas_id:	  peripheral authentication service id
+ * @mem_phys:	  Subsystem reserve memory start address
+ * @mem_size:	  Subsystem reserve memory size
+ *
+ * Upon successful, returns the PAS context or ERR_PTR() of the error otherwise.
+ */
+void *qcom_scm_pas_ctx_init(struct device *dev, u32 pas_id, phys_addr_t mem_phys,
+			    size_t mem_size)
+{
+	struct qcom_scm_pas_ctx *ctx;
+
+	ctx = kzalloc(sizeof(*ctx), GFP_KERNEL);
+	if (!ctx)
+		return ERR_PTR(-ENOMEM);
+
+	ctx->dev = dev;
+	ctx->pas_id = pas_id;
+	ctx->mem_phys = mem_phys;
+	ctx->mem_size = mem_size;
+
+	ctx->metadata = kzalloc(sizeof(*ctx->metadata), GFP_KERNEL);
+	if (!ctx->metadata) {
+		kfree(ctx);
+		return ERR_PTR(-ENOMEM);
+	}
+
+	return ctx;
+}
+EXPORT_SYMBOL_GPL(qcom_scm_pas_ctx_init);
+
+/**
+ * qcom_scm_pas_ctx_destroy() - release PAS context
+ * @ctx:	PAS context
+ */
+void qcom_scm_pas_ctx_destroy(struct qcom_scm_pas_ctx *ctx)
+{
+	kfree(ctx->metadata);
+	ctx->metadata = NULL;
+	ctx->dev = NULL;
+	ctx->pas_id = 0;
+	ctx->mem_phys = 0;
+	ctx->mem_size = 0;
+	kfree(ctx);
+}
+EXPORT_SYMBOL_GPL(qcom_scm_pas_ctx_destroy);
+
+/**
  * qcom_scm_pas_init_image() - Initialize peripheral authentication service
  *			       state machine for a given peripheral, using the
  *			       metadata
