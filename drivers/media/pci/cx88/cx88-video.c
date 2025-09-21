7ca4d0210c74be94c0dab76f6ce4d5f24b41a1bd
Alex Tran <alex.t.tran@gmail.com>
2025-09-20 17:30:46 -0700

    media: i2c: wm8775: parameterize wm8775_platform_data based on config
    Parameterized wm8775_platform_data struct, removing the single boolean
    for determining device type. wm8775_standard_cfg struct will be used
    for standard devices and wm8775_nova_s_cfg for nova_s devices.
    
    Signed-off-by: Alex Tran <alex.t.tran@gmail.com>

--- drivers/media/pci/cx88/cx88-video.c
+++ drivers/media/pci/cx88/cx88-video.c
@@ -1353,9 +1353,9 @@ static int cx8800_initdev(struct pci_dev *pci_dev,
 		struct v4l2_subdev *sd;
 
 		if (core->boardnr == CX88_BOARD_HAUPPAUGE_NOVASPLUS_S1)
-			core->wm8775_data.is_nova_s = true;
+			core->wm8775_data = wm8775_nova_s_cfg;
 		else
-			core->wm8775_data.is_nova_s = false;
+			core->wm8775_data = wm8775_standard_cfg;
 
 		sd = v4l2_i2c_new_subdev_board(&core->v4l2_dev, &core->i2c_adap,
 					       &wm8775_info, NULL);
