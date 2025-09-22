84545e2722bf81ddd31382346304037465b0c8e1
Sean Christopherson <seanjc@google.com>
2025-09-22 11:47:43 -0700

    KVM: x86: Initialize allow_smaller_maxphyaddr earlier in setup
    Initialize allow_smaller_maxphyaddr during hardware setup as soon as KVM
    knows whether or not TDP will be utilized.  To avoid having to teach KVM's
    emulator all about CET, KVM's upcoming CET virtualization support will be
    mutually exclusive with allow_smaller_maxphyaddr, i.e. will disable SHSTK
    and IBT if allow_smaller_maxphyaddr is enabled.
    
    In general, allow_smaller_maxphyaddr should be initialized as soon as
    possible since it's globally visible while its only input is whether or
    not EPT/NPT is enabled.  I.e. there's effectively zero risk of setting
    allow_smaller_maxphyaddr too early, and substantial risk of setting it
    too late.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -8479,6 +8479,14 @@ __init int vmx_hardware_setup(void)
 		return -EOPNOTSUPP;
 	}
 
+	/*
+	 * Shadow paging doesn't have a (further) performance penalty
+	 * from GUEST_MAXPHYADDR < HOST_MAXPHYADDR so enable it
+	 * by default
+	 */
+	if (!enable_ept)
+		allow_smaller_maxphyaddr = true;
+
 	if (!cpu_has_vmx_ept_ad_bits() || !enable_ept)
 		enable_ept_ad_bits = 0;
 
@@ -8714,14 +8722,6 @@ int __init vmx_init(void)
 	}
 
 	vmx_check_vmcs12_offsets();
-
-	/*
-	 * Shadow paging doesn't have a (further) performance penalty
-	 * from GUEST_MAXPHYADDR < HOST_MAXPHYADDR so enable it
-	 * by default
-	 */
-	if (!enable_ept)
-		allow_smaller_maxphyaddr = true;
 
 	return 0;
 
