6ecc9ff34d6c73f1857286e90e6531a641b5bfb9
Sakari Ailus <sakari.ailus@linux.intel.com>
2025-09-22 15:06:30 +0300

    i2c: ljca: Remove Wentong's e-mail address
    Wentong's e-mail address no longer works, remove it.
    
    Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>

--- drivers/i2c/busses/i2c-ljca.c
+++ drivers/i2c/busses/i2c-ljca.c
@@ -335,7 +335,7 @@ static struct auxiliary_driver ljca_i2c_driver = {
 };
 module_auxiliary_driver(ljca_i2c_driver);
 
-MODULE_AUTHOR("Wentong Wu <wentong.wu@intel.com>");
+MODULE_AUTHOR("Wentong Wu");
 MODULE_AUTHOR("Zhifeng Wang <zhifeng.wang@intel.com>");
 MODULE_AUTHOR("Lixu Zhang <lixu.zhang@intel.com>");
 MODULE_DESCRIPTION("Intel La Jolla Cove Adapter USB-I2C driver");
