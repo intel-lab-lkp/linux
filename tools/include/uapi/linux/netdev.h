5a2496e9a60eb928d64fba525021a0d2cbd07075
Mehdi Ben Hadj Khelifa <mehdi.benhadjkhelifa@gmail.com>
2025-09-23 22:00:14 +0100

    uapi: netdev: Add XDP RX queue index metadata flags
    Added NETDEV_XDP_RX_METADATA_QUEUE_INDEX flag to both netdev.h files
    for the bpf_xdp_metadata_rx_queue_index() function.
    
    Signed-off-by: Mehdi Ben Hadj Khelifa <mehdi.benhadjkhelifa@gmail.com>

--- tools/include/uapi/linux/netdev.h
+++ tools/include/uapi/linux/netdev.h
@@ -46,11 +46,14 @@ enum netdev_xdp_act {
  *   hash via bpf_xdp_metadata_rx_hash().
  * @NETDEV_XDP_RX_METADATA_VLAN_TAG: Device is capable of exposing receive
  *   packet VLAN tag via bpf_xdp_metadata_rx_vlan_tag().
+ * @NETDEV_XDP_RX_METADATA_QUEUE_INDEX: Device is capable of exposing receive HW
+ *   queue index via bpf_xdp_metadata_rx_queue_index().
  */
 enum netdev_xdp_rx_metadata {
 	NETDEV_XDP_RX_METADATA_TIMESTAMP = 1,
 	NETDEV_XDP_RX_METADATA_HASH = 2,
 	NETDEV_XDP_RX_METADATA_VLAN_TAG = 4,
+	NETDEV_XDP_RX_METADATA_QUEUE_INDEX = 8,
 };
 
 /**
