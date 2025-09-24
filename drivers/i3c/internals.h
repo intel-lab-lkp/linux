bfaefdceedc63aa75b7bea337c00953806507ecc
Arnd Bergmann <arnd@arndb.de>
2025-09-24 17:02:53 +0200

    i3c: fix big-endian FIFO transfers
    Short MMIO transfers that are not a multiple of four bytes in size need
    a special case for the final bytes, however the existing implementation
    is not endian-safe and introduces an incorrect byteswap on big-endian
    kernels.
    
    This usually does not cause problems because most systems are
    little-endian and most transfers are multiple of four bytes long, but
    still needs to be fixed to avoid the extra byteswap.
    
    Change the special case for both i3c_writel_fifo() and i3c_readl_fifo()
    to use non-byteswapping writesl() and readsl() with a single element
    instead of the byteswapping writel()/readl() that are meant for individual
    MMIO registers.
    
    The earlier versions in the dw-i3c and i3c-master-cdns had a correct
    implementation, but the generic version that was recently added broke it.
    
    Fixes: 733b439375b4 ("i3c: master: Add inline i3c_readl_fifo() and i3c_writel_fifo()")
    Cc: Manikanta Guntupalli <manikanta.guntupalli@amd.com>
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/i3c/internals.h
+++ drivers/i3c/internals.h
@@ -38,7 +38,7 @@ static inline void i3c_writel_fifo(void __iomem *addr, const void *buf,
 		u32 tmp = 0;
 
 		memcpy(&tmp, buf + (nbytes & ~3), nbytes & 3);
-		writel(tmp, addr);
+		writesl(addr, &buf, 1);
 	}
 }
 
@@ -55,7 +55,7 @@ static inline void i3c_readl_fifo(const void __iomem *addr, void *buf,
 	if (nbytes & 3) {
 		u32 tmp;
 
-		tmp = readl(addr);
+		readsl(addr, &tmp, 1);
 		memcpy(buf + (nbytes & ~3), &tmp, nbytes & 3);
 	}
 }
