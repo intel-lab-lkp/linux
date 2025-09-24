4df7356703cb9763c90dbf2e471640c9ae1c09cb
Brendan Jackman <jackmanb@google.com>
2025-09-24 14:59:36 +0000

    x86/mm/asi: Add CONFIG_MITIGATION_ADDRESS_SPACE_ISOLATION
    This long awkward name is for consistency with
    CONFIG_MITIGATION_PAGE_TABLE_ISOLATION.
    
    In the short term, there isn't much arch code. In the medium term, it
    will mostly be x86 code. So, put the code where it will need to go
    instead of just having to move it soon.
    
    In the long term, it should probably include other archs too, so
    things should be as arch-specific as necessary, but not more so.
    
    Follow the proposal by Mike Rapoport[0]: a generic header includes
    NOP stubs for ASI definitions. If
    CONFIG_MITIGATION_ADDRESS_SPACE_ISOLATION is defined then the asm/ tree
    must have asi.h, and that gets included instead of the stubs.
    
    Signed-off-by: Brendan Jackman <jackmanb@google.com>

---
+++ arch/x86/include/asm/asi.h
@@ -0,0 +1,5 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_X86_ASI_H
+#define _ASM_X86_ASI_H
+
+#endif /* _ASM_X86_ASI_H */
---
+++ include/linux/asi.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _INCLUDE_ASI_H
+#define _INCLUDE_ASI_H
+
+#ifdef CONFIG_MITIGATION_ADDRESS_SPACE_ISOLATION
+#include <asm/asi.h>
+#else
+
+#endif /* CONFIG_MITIGATION_ADDRESS_SPACE_ISOLATION */
+#endif /* _INCLUDE_ASI_H */
