980fe95f7f58c3ad02f1b2c9cf1ecfe94f1bdfd8
Luke Howard <lukeh@padl.com>
2025-09-27 17:07:05 +1000

    net: dsa: mv88e6xxx: add MV88E6XXX_G1_ATU_CTL_MAC_AVB setter
    Add accessors for the MACAVB bit, which controls whether certain ATU bits cause
    the entry to be interpreted as AVB or NRL (non-rate-limiting) entries. This is
    necessary on switches such as the 88E6352 and 88E6240 that support both AVB and
    NRL ATU entries.
    
    Signed-off-by: Luke Howard <lukeh@padl.com>

--- drivers/net/dsa/mv88e6xxx/global1_atu.c
+++ drivers/net/dsa/mv88e6xxx/global1_atu.c
@@ -41,6 +41,23 @@ int mv88e6xxx_g1_atu_set_learn2all(struct mv88e6xxx_chip *chip, bool learn2all)
 	return mv88e6xxx_g1_write(chip, MV88E6XXX_G1_ATU_CTL, val);
 }
 
+int mv88e6xxx_g1_atu_set_mac_avb(struct mv88e6xxx_chip *chip, bool mac_avb)
+{
+	u16 val;
+	int err;
+
+	err = mv88e6xxx_g1_read(chip, MV88E6XXX_G1_ATU_CTL, &val);
+	if (err)
+		return err;
+
+	if (mac_avb)
+		val |= MV88E6XXX_G1_ATU_CTL_MAC_AVB;
+	else
+		val &= ~MV88E6XXX_G1_ATU_CTL_MAC_AVB;
+
+	return mv88e6xxx_g1_write(chip, MV88E6XXX_G1_ATU_CTL, val);
+}
+
 int mv88e6xxx_g1_atu_set_age_time(struct mv88e6xxx_chip *chip,
 				  unsigned int msecs)
 {
