10a231ddf6f43eb544287a7c846a3c4b5183f5c2
Ma Ke <make24@iscas.ac.cn>
2025-09-27 18:57:29 +0800

    i2c: fix reference leak in MP2 PCI device
    In i2c_amd_probe(), amd_mp2_find_device() utilizes
    driver_find_next_device() which internally calls driver_find_device()
    to locate the matching device. driver_find_device() increments the
    reference count of the found device by calling get_device(), but
    amd_mp2_find_device() fails to call put_device() to decrement the
    reference count before returning. This results in a reference count
    leak of the PCI device each time i2c_amd_probe() is executed, which
    may prevent the device from being properly released and cause a memory
    leak.
    
    Found by code review.
    
    Cc: stable@vger.kernel.org
    Fixes: 529766e0a011 ("i2c: Add drivers for the AMD PCIe MP2 I2C controller")
    Signed-off-by: Ma Ke <make24@iscas.ac.cn>

--- drivers/i2c/busses/i2c-amd-mp2-pci.c
+++ drivers/i2c/busses/i2c-amd-mp2-pci.c
@@ -464,7 +464,9 @@ struct amd_mp2_dev *amd_mp2_find_device(void)
 		return NULL;
 
 	pci_dev = to_pci_dev(dev);
-	return (struct amd_mp2_dev *)pci_get_drvdata(pci_dev);
+	mp2_dev = (struct amd_mp2_dev *)pci_get_drvdata(pci_dev);
+	put_device(dev);
+	return mp2_dev;
 }
 EXPORT_SYMBOL_GPL(amd_mp2_find_device);
 
