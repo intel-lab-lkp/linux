ec217637e7f16d237abe70bc70a21b5bcc666dfd
Enzo Matsumiya <ematsumiya@suse.de>
2025-09-29 10:27:58 -0300

    smb: client: actually use cached dirs on readdir
    Currently, even when we have a valid cached dir, cifs_readdir will not
    make use of it for the Find request, and will reopen a separate handle
    in query_dir_first.
    
    Fix this by setting cifsFile->fid to cfid->fid and resetting search info
    parameters.  Also add cifs_search_info->reset_scan to indicate
    SMB2_query_directory_init to include the SMB2_RESTART_SCANS flag.
    
    With this, we use query_dir_next directly instead of query_dir_first.
    
    This patch also keeps the cfid reference all through cifs_readdir().
    To prevent bogus/invalid usage of it, check if cfid is still valid after
    each possible network call (i.e. where possible reconnects may have
    happened).
    
    Signed-off-by: Enzo Matsumiya <ematsumiya@suse.de>

--- fs/smb/client/smb2pdu.c
+++ fs/smb/client/smb2pdu.c
@@ -5418,7 +5418,7 @@ int SMB2_query_directory_init(const unsigned int xid,
 			      struct TCP_Server_Info *server,
 			      struct smb_rqst *rqst,
 			      u64 persistent_fid, u64 volatile_fid,
-			      int index, int info_level)
+			      int index, struct cifs_search_info *search)
 {
 	struct smb2_query_directory_req *req;
 	unsigned char *bufptr;
@@ -5435,7 +5435,7 @@ int SMB2_query_directory_init(const unsigned int xid,
 	if (rc)
 		return rc;
 
-	switch (info_level) {
+	switch (search->info_level) {
 	case SMB_FIND_FILE_DIRECTORY_INFO:
 		req->FileInformationClass = FILE_DIRECTORY_INFORMATION;
 		break;
@@ -5449,14 +5449,15 @@ int SMB2_query_directory_init(const unsigned int xid,
 		req->FileInformationClass = FILE_FULL_DIRECTORY_INFORMATION;
 		break;
 	default:
-		cifs_tcon_dbg(VFS, "info level %u isn't supported\n",
-			info_level);
+		cifs_tcon_dbg(VFS, "info level %u isn't supported\n", search->info_level);
 		return -EINVAL;
 	}
 
 	req->FileIndex = cpu_to_le32(index);
 	req->PersistentFileId = persistent_fid;
 	req->VolatileFileId = volatile_fid;
+	if (search->restart_scan)
+		req->Flags |= SMB2_RESTART_SCANS;
 
 	len = 0x2;
 	bufptr = req->Buffer;
@@ -5603,7 +5604,7 @@ replay_again:
 	rc = SMB2_query_directory_init(xid, tcon, server,
 				       &rqst, persistent_fid,
 				       volatile_fid, index,
-				       srch_inf->info_level);
+				       srch_inf);
 	if (rc)
 		goto qdir_exit;
 
