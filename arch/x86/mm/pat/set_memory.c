2751071e1b86f4fef98070bb19c702b618d15923
syzbot <syzbot+e34177f6091df113ef20@syzkaller.appspotmail.com>
2025-09-29 00:50:58 -0700

    Forwarded:
    For archival purposes, forwarding an incoming command email to
    linux-kernel@vger.kernel.org, syzkaller-bugs@googlegroups.com.
    
    ***
    
    Subject:
    Author: nooraineqbal@gmail.com
    
    #syz test: https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git master
    
    Resending this patch in the existing thread with a '#syz test:' directive
    so syzbot can test it.
    
    From 1bb35c6722b8fb03e9262f6e6530d240629a44df Mon Sep 17 00:00:00 2001
    From: neqbal <nooraineqbal@gmail.com>
    Date: Sun, 28 Sep 2025 03:52:44 +0530
    Subject: [PATCH] x86/mm: Fix off-by-one error in set_memory
    
    Correct end page calculation by subtracting 1 to prevent
    out-of-bounds access.
    
    Reported-by: syzbot+e34177f6091df113ef20@syzkaller.appspotmail.com
    Signed-off-by: neqbal <nooraineqbal@gmail.com>

--- arch/x86/mm/pat/set_memory.c
+++ arch/x86/mm/pat/set_memory.c
@@ -446,7 +446,7 @@ static void cpa_flush(struct cpa_data *cpa, int cache)
 	}
 
 	start = fix_addr(__cpa_addr(cpa, 0));
-	end =   fix_addr(__cpa_addr(cpa, cpa->numpages));
+	end =   fix_addr(__cpa_addr(cpa, cpa->numpages - 1));
 	if (cpa->force_flush_all)
 		end = TLB_FLUSH_ALL;
 
