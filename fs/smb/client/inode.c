70cd95cec92e93b6d53e87c9dbf669b3fc91e24b
Enzo Matsumiya <ematsumiya@suse.de>
2025-09-29 10:28:03 -0300

    smb: client: fix dentry revalidation of cached root
    Don't check root dir dentry in cifs_dentry_needs_reval() as its inode
    was created before the cfid, so the time check will fail and trigger an
    unnecessary revalidation.
    
    Also account for dir_cache_timeout in time comparison, because if we
    have a cached dir, we have a lease for it, and, thus, may assume its
    children are (still) valid.  To confirm that, let the ac*max checks
    go through for granular results.
    
    Signed-off-by: Enzo Matsumiya <ematsumiya@suse.de>

--- fs/smb/client/inode.c
+++ fs/smb/client/inode.c
@@ -2708,10 +2708,28 @@ cifs_dentry_needs_reval(struct dentry *dentry)
 	if (!lookupCacheEnabled)
 		return true;
 
-	cfid = find_cached_dir(tcon->cfids, dentry->d_parent, CFID_LOOKUP_DENTRY);
-	if (cfid) {
-		close_cached_dir(cfid);
-		return false;
+	if (!IS_ROOT(dentry)) {
+		cfid = find_cached_dir(tcon->cfids, dentry->d_parent, CFID_LOOKUP_DENTRY);
+		if (cfid) {
+			/*
+			 * We hold a lease for the cached parent.
+			 * So as long as this child is within cached dir lifetime, we don't need to
+			 * revalidate it.
+			 *
+			 * Since cfid expiration is based on access time, use it for comparison
+			 * instead of creation time.
+			 */
+			if (time_before(cifs_i->time, cfid->atime - dir_cache_timeout * HZ)) {
+				close_cached_dir(cfid);
+				return true;
+			}
+
+			/*
+			 * From cached dir perspective, we're done -- attr caching (ac*max) may
+			 * have different requirements, so let the checks go through.
+			 */
+			close_cached_dir(cfid);
+		}
 	}
 
 	/*
