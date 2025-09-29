23c179928b83af36a6ca313e622c12f3b533a2bf
Łukasz Bartosik <ukaszb@chromium.org>
2025-09-29 09:02:06 +0000

    xhci: dbc: allow to set manufacturer name through sysfs
    Add code which allows to set manufacturer name of a DbC
    device through sysfs.
    
    Signed-off-by: Łukasz Bartosik <ukaszb@chromium.org>

--- drivers/usb/host/xhci-dbgcap.c
+++ drivers/usb/host/xhci-dbgcap.c
@@ -1200,6 +1200,35 @@ static ssize_t dbc_bcdDevice_store(struct device *dev,
 	return size;
 }
 
+static ssize_t dbc_iManufacturer_show(struct device *dev,
+				      struct device_attribute *attr,
+				      char *buf)
+{
+	struct xhci_hcd	*xhci = hcd_to_xhci(dev_get_drvdata(dev));
+	struct xhci_dbc	*dbc = xhci->dbc;
+
+	return sysfs_emit(buf, "%s\n", dbc->str.manufacturer);
+}
+
+static ssize_t dbc_iManufacturer_store(struct device *dev,
+				       struct device_attribute *attr,
+				       const char *buf, size_t size)
+{
+	struct xhci_hcd	*xhci = hcd_to_xhci(dev_get_drvdata(dev));
+	struct xhci_dbc	*dbc = xhci->dbc;
+
+	if (dbc->state != DS_DISABLED)
+		return -EBUSY;
+
+	if (size > USB_MAX_STRING_LEN/2+1)
+		return -E2BIG;
+
+	memcpy(dbc->str.manufacturer, buf, size);
+	/* remove newline and terminate the string */
+	dbc->str.manufacturer[size-1] = '\0';
+
+	return size;
+}
 static ssize_t dbc_iProduct_show(struct device *dev,
 				 struct device_attribute *attr,
 				 char *buf)
@@ -1347,6 +1376,7 @@ static DEVICE_ATTR_RW(dbc);
 static DEVICE_ATTR_RW(dbc_idVendor);
 static DEVICE_ATTR_RW(dbc_idProduct);
 static DEVICE_ATTR_RW(dbc_bcdDevice);
+static DEVICE_ATTR_RW(dbc_iManufacturer);
 static DEVICE_ATTR_RW(dbc_iProduct);
 static DEVICE_ATTR_RW(dbc_iSerial);
 static DEVICE_ATTR_RW(dbc_bInterfaceProtocol);
@@ -1357,6 +1387,7 @@ static struct attribute *dbc_dev_attrs[] = {
 	&dev_attr_dbc_idVendor.attr,
 	&dev_attr_dbc_idProduct.attr,
 	&dev_attr_dbc_bcdDevice.attr,
+	&dev_attr_dbc_iManufacturer.attr,
 	&dev_attr_dbc_iProduct.attr,
 	&dev_attr_dbc_iSerial.attr,
 	&dev_attr_dbc_bInterfaceProtocol.attr,
