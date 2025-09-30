a9cf4fcc80a925d7f9bc5c7c51e3d62c0138be3a
Chuan Liu <chuan.liu@amlogic.com>
2025-09-30 17:37:15 +0800

    dt-bindings: clock: Add Amlogic A4 PLL clock controller
    Add the PLL clock controller dt-bindings for Amlogic A4 SoC family.
    
    Signed-off-by: Chuan Liu <chuan.liu@amlogic.com>

---
+++ include/dt-bindings/clock/amlogic,a4-pll-clkc.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR MIT) */
+/*
+ * Copyright (c) 2025 Amlogic, Inc. All rights reserved.
+ * Author: Chuan Liu <chuan.liu@amlogic.com>
+ */
+
+#ifndef _DT_BINDINGS_CLOCK_AMLOGIC_A4_PLL_CLKC_H
+#define _DT_BINDINGS_CLOCK_AMLOGIC_A4_PLL_CLKC_H
+
+#define CLKID_GP0_PLL_DCO			0
+#define CLKID_GP0_PLL				1
+#define CLKID_HIFI_PLL_DCO			2
+#define CLKID_HIFI_PLL				3
+
+#endif  /* _DT_BINDINGS_CLOCK_AMLOGIC_A4_PLL_CLKC_H */
