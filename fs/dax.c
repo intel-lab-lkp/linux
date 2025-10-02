7e7d73d8a17a679f604543d44a8d98cec71b3620
Jan Kara <jack@suse.cz>
2025-10-02 10:13:12 +0200

    dax: fix assertion in dax_iomap_rw()
    dax_iomap_rw() asserts that inode lock is held when reading from it. The
    assert triggers on erofs as it indeed doesn't hold any locks in this
    case - naturally because there's nothing to race against when the
    filesystem is read-only. Check the locking only if the filesystem is
    actually writeable.
    
    Reported-by: syzbot+47680984f2d4969027ea@syzkaller.appspotmail.com
    Signed-off-by: Jan Kara <jack@suse.cz>

--- fs/dax.c
+++ fs/dax.c
@@ -1752,7 +1752,7 @@ dax_iomap_rw(struct kiocb *iocb, struct iov_iter *iter,
 	if (iov_iter_rw(iter) == WRITE) {
 		lockdep_assert_held_write(&iomi.inode->i_rwsem);
 		iomi.flags |= IOMAP_WRITE;
-	} else {
+	} else if (!IS_RDONLY(iomi.inode)) {
 		lockdep_assert_held(&iomi.inode->i_rwsem);
 	}
 
