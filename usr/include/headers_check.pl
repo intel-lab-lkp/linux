2435cad1964da8ff9933b49873f9727340552acc
Geert Uytterhoeven <geert@linux-m68k.org>
2025-10-06 10:49:28 +0200

    kbuild: uapi: Strip comments before size type check
    On m68k, check_sizetypes in headers_check reports:
    
        ./usr/include/asm/bootinfo-amiga.h:17: found __[us]{8,16,32,64} type without #include <linux/types.h>
    
    This header file does not use any of the Linux-specific integer types,
    but merely refers to them from comments, so this is a false positive.
    As of commit c3a9d74ee413bdb3 ("kbuild: uapi: upgrade check_sizetypes()
    warning to error"), this check was promoted to an error, breaking m68k
    all{mod,yes}config builds.
    
    Fix this by stripping simple comments before looking for Linux-specific
    integer types.
    
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- usr/include/headers_check.pl
+++ usr/include/headers_check.pl
@@ -155,6 +155,9 @@ sub check_sizetypes
 	if (my $included = ($line =~ /^\s*#\s*include\s+[<"](\S+)[>"]/)[0]) {
 		check_include_typesh($included);
 	}
+	# strip comments (single-line and C99 only)
+	$line =~ s@\/\*.*?\*\/@@;
+	$line =~ s@\/\/.*$@@;
 	if ($line =~ m/__[us](8|16|32|64)\b/) {
 		printf STDERR "$filename:$lineno: " .
 		              "found __[us]{8,16,32,64} type " .
