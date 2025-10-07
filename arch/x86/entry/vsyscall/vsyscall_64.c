2874822d83e2bb784670aa8939f925224aecc192
Sohil Mehta <sohil.mehta@intel.com>
2025-10-06 23:51:14 -0700

    x86/vsyscall: Add vsyscall emulation for #GP
    The legacy vsyscall page is mapped at a fixed address in the kernel
    address range 0xffffffffff600000-0xffffffffff601000. Prior to LASS, a
    vsyscall page access from userspace would always generate a #PF. The
    kernel emulates the execute (XONLY) accesses in the #PF handler and
    returns the appropriate values to userspace.
    
    With LASS, these accesses are intercepted before the paging structures
    are traversed triggering a #GP instead of a #PF. However, the #GP
    doesn't provide much information in terms of the error code.
    
    Emulate the vsyscall access without going through complex instruction
    decoding. Use the faulting RIP which is preserved in the user registers
    to determine if the #GP was triggered due to a vsyscall access.
    
    Signed-off-by: Sohil Mehta <sohil.mehta@intel.com>

--- arch/x86/entry/vsyscall/vsyscall_64.c
+++ arch/x86/entry/vsyscall/vsyscall_64.c
@@ -23,7 +23,7 @@
  * soon be no new userspace code that will ever use a vsyscall.
  *
  * The code in this file emulates vsyscalls when notified of a page
- * fault to a vsyscall address.
+ * fault or a general protection fault to a vsyscall address.
  */
 
 #include <linux/kernel.h>
@@ -280,6 +280,18 @@ bool emulate_vsyscall_pf(unsigned long error_code, struct pt_regs *regs,
 		WARN_ON_ONCE(!(error_code & X86_PF_INSTR));
 
 	return __emulate_vsyscall(regs, address);
+}
+
+bool emulate_vsyscall_gp(struct pt_regs *regs)
+{
+	if (!cpu_feature_enabled(X86_FEATURE_LASS))
+		return false;
+
+	/* Emulate only if the RIP points to the vsyscall address */
+	if (!is_vsyscall_vaddr(regs->ip))
+		return false;
+
+	return __emulate_vsyscall(regs, regs->ip);
 }
 
 /*
