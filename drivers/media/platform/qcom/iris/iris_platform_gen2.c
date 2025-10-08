2eb667f0734cc439efbe04caace1fed7d656d561
Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
2025-10-08 07:33:02 +0300

    media: iris: stop encoding PIPE value into fw_caps
    The value of the PIPE property depends on the number of pipes available
    on the platform and is frequently the only difference between several
    fw_caps. In order to reduce duplciation, use num_vpp_pipe from the
    iris_platform_data rather than hardcoding the value into the fw_cap.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>

--- drivers/media/platform/qcom/iris/iris_platform_gen2.c
+++ drivers/media/platform/qcom/iris/iris_platform_gen2.c
@@ -161,9 +161,9 @@ static const struct platform_inst_fw_cap inst_fw_cap_sm8550_dec[] = {
 	{
 		.cap_id = PIPE,
 		.min = PIPE_1,
-		.max = PIPE_4,
+		/* .max is set via platform data */
 		.step_or_mask = 1,
-		.value = PIPE_4,
+		/* .value is set via platform data */
 		.hfi_id = HFI_PROP_PIPE,
 		.set = iris_set_pipe,
 	},
