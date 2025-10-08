bcbc7e1a9b6dc636c13f2cf70047e70db7276984
Svyatoslav Ryhel <clamor95@gmail.com>
2025-10-08 10:30:28 +0300

    staging: media: tegra-video: expand VI and VIP support to Tegra30
    Existing VI and VIP implementation for Tegra20 is fully compatible with
    Tegra30.
    
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>
    Tested-by: Luca Ceresoli <luca.ceresoli@bootlin.com> # Tegra20 VIP
    Reviewed-by: Luca Ceresoli <luca.ceresoli@bootlin.com>

--- drivers/staging/media/tegra-video/video.c
+++ drivers/staging/media/tegra-video/video.c
@@ -123,7 +123,7 @@ static int host1x_video_remove(struct host1x_device *dev)
 }
 
 static const struct of_device_id host1x_video_subdevs[] = {
-#if defined(CONFIG_ARCH_TEGRA_2x_SOC)
+#if defined(CONFIG_ARCH_TEGRA_2x_SOC) || defined(CONFIG_ARCH_TEGRA_3x_SOC)
 	{ .compatible = "nvidia,tegra20-vip", },
 	{ .compatible = "nvidia,tegra20-vi", },
 #endif
