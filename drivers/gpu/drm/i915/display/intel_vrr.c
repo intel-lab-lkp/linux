34fd54f3874469e841244a87633765b82ffa2889
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2025-10-09 14:31:01 +0530

    drm/i915/display: Add vblank_start adjustment logic for always-on VRR TG
    As we move towards using a shorter, optimized guardband, we need to adjust
    how the delayed vblank start is computed.
    
    Adjust the crtc_vblank_start using Vmin Vtotal - guardband only when
    intel_vrr_always_use_vrr_tg() is true.
    
    This also paves way for guardband optimization, by handling the movement of
    the crtc_vblank_start for platforms that have VRR TG always active.
    
    v2: Drop the helper and add the adjustment directly to
    intel_vrr_compute_guardband(). Ville
    
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>

--- drivers/gpu/drm/i915/display/intel_vrr.c
+++ drivers/gpu/drm/i915/display/intel_vrr.c
@@ -436,13 +436,17 @@ intel_vrr_max_guardband(struct intel_crtc_state *crtc_state)
 void intel_vrr_compute_guardband(struct intel_crtc_state *crtc_state)
 {
 	struct intel_display *display = to_intel_display(crtc_state);
-	const struct drm_display_mode *adjusted_mode = &crtc_state->hw.adjusted_mode;
+	struct drm_display_mode *adjusted_mode = &crtc_state->hw.adjusted_mode;
 
 	if (!intel_vrr_possible(crtc_state))
 		return;
 
 	crtc_state->vrr.guardband = min(crtc_state->vrr.vmin - adjusted_mode->crtc_vdisplay,
 					intel_vrr_max_guardband(crtc_state));
+
+	if (intel_vrr_always_use_vrr_tg(display))
+		adjusted_mode->crtc_vblank_start  =
+			crtc_state->vrr.vmin - crtc_state->vrr.guardband;
 
 	if (DISPLAY_VER(display) < 13)
 		crtc_state->vrr.pipeline_full =
