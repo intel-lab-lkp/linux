45579290921db0da9b4f08580ed52931f7fd6413
Faith Ekstrand <faith.ekstrand@collabora.com>
2025-10-10 12:11:38 +0200

    drm/panthor: Add a PANTHOR_BO_SYNC ioctl
    This will be used by the UMD to synchronize CPU-cached mappings when
    the UMD can't do it directly (no usermode cache maintenance instruction
    on Arm32).
    
    v2:
    - Change the flags so they better match the drm_gem_shmem_sync()
      semantics
    
    Signed-off-by: Faith Ekstrand <faith.ekstrand@collabora.com>
    Signed-off-by: Boris Brezillon <boris.brezillon@collabora.com>

--- drivers/gpu/drm/panthor/panthor_gem.h
+++ drivers/gpu/drm/panthor/panthor_gem.h
@@ -147,6 +147,9 @@ panthor_gem_create_with_handle(struct drm_file *file,
 void panthor_gem_bo_set_label(struct drm_gem_object *obj, const char *label);
 void panthor_gem_kernel_bo_set_label(struct panthor_kernel_bo *bo, const char *label);
 
+int panthor_gem_bo_sync(struct drm_gem_object *obj, u32 flags,
+			u64 offset, u64 size);
+
 static inline u64
 panthor_kernel_bo_gpuva(struct panthor_kernel_bo *bo)
 {
