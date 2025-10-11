5105571744c88a4aa1794342f19ea2ecb7506e04
Tim Chen <tim.c.chen@linux.intel.com>
2025-10-11 11:24:44 -0700

    sched/fair: Track LLC-preferred tasks per runqueue
    For each runqueue, track the number of tasks with an LLC preference
    and how many of them are running on their preferred LLC. This mirrors
    nr_numa_running and nr_preferred_running for NUMA balancing, and will
    be used by cache-aware load balancing in later patches.
    
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1100,6 +1100,10 @@ struct rq {
 	unsigned int		nr_preferred_running;
 	unsigned int		numa_migrate_on;
 #endif
+#ifdef CONFIG_SCHED_CACHE
+	unsigned int		nr_pref_llc_running;
+	unsigned int		nr_llc_running;
+#endif
 #ifdef CONFIG_NO_HZ_COMMON
 	unsigned long		last_blocked_load_update_tick;
 	unsigned int		has_blocked_load;
@@ -1953,6 +1957,9 @@ init_numa_balancing(unsigned long clone_flags, struct task_struct *p)
 }
 
 #endif /* !CONFIG_NUMA_BALANCING */
+
+void reset_llc_stats(struct rq *rq);
+int task_llc(const struct task_struct *p);
 
 static inline void
 queue_balance_callback(struct rq *rq,
