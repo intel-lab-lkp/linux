d73b46408bd810a76727e6385683e5249afed3c9
Costa Shulyupin <costa.shul@redhat.com>
2025-10-12 10:11:30 +0300

    tools/rtla: Remove unused function declarations
    Historically four function declarations remain orphaned or duplicated.
    
    Remove them to keep the source clean.
    
    Signed-off-by: Costa Shulyupin <costa.shul@redhat.com>

--- tools/tracing/rtla/src/utils.h
+++ tools/tracing/rtla/src/utils.h
@@ -23,7 +23,6 @@ void err_msg(const char *fmt, ...);
 long parse_seconds_duration(char *val);
 void get_duration(time_t start_time, char *output, int output_size);
 
-int parse_cpu_list(char *cpu_list, char **monitored_cpus);
 long long get_llong_from_str(char *start);
 
 static inline void
