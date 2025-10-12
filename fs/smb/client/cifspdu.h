5cfa2bf5575c035e8bcf7e2dff869b8e07f5e99d
ChenXiaoSong <chenxiaosong@kylinos.cn>
2025-10-12 23:09:09 +0800

    smb: move FILE_SYSTEM_DEVICE_INFO to common/cifspdu.h
    Rename "struct filesystem_device_info" to "FILE_SYSTEM_DEVICE_INFO",
    then move duplicate definitions to common header file.
    
    Signed-off-by: ChenXiaoSong <chenxiaosong@kylinos.cn>

--- fs/smb/client/cifspdu.h
+++ fs/smb/client/cifspdu.h
@@ -1929,11 +1929,6 @@ typedef struct {
 #define FILE_PORTABLE_DEVICE			0x00004000
 #define FILE_DEVICE_ALLOW_APPCONTAINER_TRAVERSAL 0x00020000
 
-typedef struct {
-	__le32 DeviceType;
-	__le32 DeviceCharacteristics;
-} __attribute__((packed)) FILE_SYSTEM_DEVICE_INFO; /* device info level 0x104 */
-
 /* minimum includes first three fields, and empty FS Name */
 #define MIN_FS_ATTR_INFO_SIZE 12
 
