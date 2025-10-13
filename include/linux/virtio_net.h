4de8e03491906d1258109fa1aa5a0994fe62388d
Xuan Zhuo <xuanzhuo@linux.alibaba.com>
2025-10-13 10:06:28 +0800

    virtio-net: correct hdr_len handling for VIRTIO_NET_F_GUEST_HDRLEN
    The commit be50da3e9d4a ("net: virtio_net: implement exact header length
    guest feature") introduces support for the VIRTIO_NET_F_GUEST_HDRLEN
    feature in virtio-net.
    
    This feature requires virtio-net to set hdr_len to the actual header
    length of the packet when transmitting, the number of
    bytes from the start of the packet to the beginning of the
    transport-layer payload.
    
    However, in practice, hdr_len was being set using skb_headlen(skb),
    which is clearly incorrect. This commit fixes that issue.
    
    Fixes: be50da3e9d4a ("net: virtio_net: implement exact header length guest feature")
    Signed-off-by: Xuan Zhuo <xuanzhuo@linux.alibaba.com>

--- include/linux/virtio_net.h
+++ include/linux/virtio_net.h
@@ -217,20 +217,34 @@ static inline int virtio_net_hdr_from_skb(const struct sk_buff *skb,
 
 	if (skb_is_gso(skb)) {
 		struct skb_shared_info *sinfo = skb_shinfo(skb);
+		u16 hdr_len = 0;
 
-		/* This is a hint as to how much should be linear. */
-		hdr->hdr_len = __cpu_to_virtio16(little_endian,
-						 skb_headlen(skb));
+		/* In certain code paths (such as the af_packet.c receive path),
+		 * this function may be called without a transport header.
+		 */
+		if (skb_transport_header_was_set(skb))
+			hdr_len = skb_transport_offset(skb);
+
 		hdr->gso_size = __cpu_to_virtio16(little_endian,
 						  sinfo->gso_size);
-		if (sinfo->gso_type & SKB_GSO_TCPV4)
+		if (sinfo->gso_type & SKB_GSO_TCPV4) {
 			hdr->gso_type = VIRTIO_NET_HDR_GSO_TCPV4;
-		else if (sinfo->gso_type & SKB_GSO_TCPV6)
+			if (hdr_len)
+				hdr_len += tcp_hdrlen(skb);
+		} else if (sinfo->gso_type & SKB_GSO_TCPV6) {
 			hdr->gso_type = VIRTIO_NET_HDR_GSO_TCPV6;
-		else if (sinfo->gso_type & SKB_GSO_UDP_L4)
+			if (hdr_len)
+				hdr_len += tcp_hdrlen(skb);
+		} else if (sinfo->gso_type & SKB_GSO_UDP_L4) {
 			hdr->gso_type = VIRTIO_NET_HDR_GSO_UDP_L4;
-		else
+			if (hdr_len)
+				hdr_len += sizeof(struct udphdr);
+		} else {
 			return -EINVAL;
+		}
+
+		hdr->hdr_len = __cpu_to_virtio16(little_endian, hdr_len);
+
 		if (sinfo->gso_type & SKB_GSO_TCP_ECN)
 			hdr->gso_type |= VIRTIO_NET_HDR_GSO_ECN;
 	} else
