ba831a8fa0fc485a4730f50555277122119f37f5
Antheas Kapenekakis <lkml@antheas.dev>
2025-10-13 22:15:34 +0200

    platform/x86: asus-wmi: add keyboard brightness event handler
    Currenlty, the keyboard brightness control of Asus WMI keyboards is
    handled in the kernel, which leads to the shortcut going from
    brightness 0, to 1, to 2, and 3.
    
    However, for HID keyboards it is exposed as a key and handled by the
    user's desktop environment. For the toggle button, this means that
    brightness control becomes on/off. In addition, in the absence of a
    DE, the keyboard brightness does not work.
    
    Therefore, expose an event handler for the keyboard brightness control
    which can then be used by hid-asus.
    
    Reviewed-by: Luke D. Jones <luke@ljones.dev>
    Tested-by: Luke D. Jones <luke@ljones.dev>
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>

--- include/linux/platform_data/x86/asus-wmi.h
+++ include/linux/platform_data/x86/asus-wmi.h
@@ -168,6 +168,14 @@ struct asus_hid_listener {
 	void (*brightness_set)(struct asus_hid_listener *listener, int brightness);
 };
 
+enum asus_hid_event {
+	ASUS_EV_BRTUP,
+	ASUS_EV_BRTDOWN,
+	ASUS_EV_BRTTOGGLE,
+};
+
+#define ASUS_EV_MAX_BRIGHTNESS 3
+
 #if IS_REACHABLE(CONFIG_ASUS_WMI)
 void set_ally_mcu_hack(enum asus_ally_mcu_hack status);
 void set_ally_mcu_powersave(bool enabled);
@@ -176,6 +184,7 @@ int asus_wmi_evaluate_method(u32 method_id, u32 arg0, u32 arg1, u32 *retval);
 
 int asus_hid_register_listener(struct asus_hid_listener *cdev);
 void asus_hid_unregister_listener(struct asus_hid_listener *cdev);
+int asus_hid_event(enum asus_hid_event event);
 #else
 static inline void set_ally_mcu_hack(enum asus_ally_mcu_hack status)
 {
@@ -199,6 +208,10 @@ static inline int asus_hid_register_listener(struct asus_hid_listener *bdev)
 }
 static inline void asus_hid_unregister_listener(struct asus_hid_listener *bdev)
 {
+}
+static inline int asus_hid_event(enum asus_hid_event event)
+{
+	return -ENODEV;
 }
 #endif
 
