e0e883e9fa8bff500e7e52d68afae6dc297b7b81
David Kaplan <david.kaplan@amd.com>
2025-10-13 09:34:37 -0500

    x86/fpu: Qualify warning in os_xsave
    The warning about using FPU before alternatives are patched is only
    relevant during boot.  Ignore the warning unless the system is booting.
    This suppresses the erroneous warning that can occur during alternative
    re-patching at runtime.
    
    Signed-off-by: David Kaplan <david.kaplan@amd.com>

--- arch/x86/kernel/fpu/xstate.h
+++ arch/x86/kernel/fpu/xstate.h
@@ -220,7 +220,7 @@ static inline void os_xsave(struct fpstate *fpstate)
 	u32 hmask = mask >> 32;
 	int err;
 
-	WARN_ON_FPU(!alternatives_patched);
+	WARN_ON_FPU(!alternatives_patched && system_state == SYSTEM_BOOTING);
 	xfd_validate_state(fpstate, mask, false);
 
 	XSTATE_XSAVE(&fpstate->regs.xsave, lmask, hmask, err);
