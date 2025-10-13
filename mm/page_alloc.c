58d9875fbaaa24a1daa29b20050145eb3022eb87
Kefeng Wang <wangkefeng.wang@huawei.com>
2025-10-13 21:38:49 +0800

    mm: debug_vm_pgtable: add debug_vm_pgtable_free_huge_page()
    Add a new helper to free huge page to be consistency to
    debug_vm_pgtable_alloc_huge_page(), and use HPAGE_PUD_ORDER
    instead of open-code.
    
    Also move the free_contig_range() under CONFIG_ALLOC_CONTIG
    since all caller are built with CONFIG_ALLOC_CONTIG.
    
    Acked-by: David Hildenbrand <david@redhat.com>
    Signed-off-by: Kefeng Wang <wangkefeng.wang@huawei.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -7121,7 +7121,6 @@ struct page *alloc_contig_pages_noprof(unsigned long nr_pages, gfp_t gfp_mask,
 	}
 	return NULL;
 }
-#endif /* CONFIG_CONTIG_ALLOC */
 
 void free_contig_range(unsigned long pfn, unsigned long nr_pages)
 {
@@ -7148,6 +7147,7 @@ void free_contig_range(unsigned long pfn, unsigned long nr_pages)
 	WARN(count != 0, "%lu pages are still in use!\n", count);
 }
 EXPORT_SYMBOL(free_contig_range);
+#endif /* CONFIG_CONTIG_ALLOC */
 
 /*
  * Effectively disable pcplists for the zone by setting the high limit to 0
