0fca1cd5be4f437d74b0b24336a6905cac03e3cc
Karunika Choo <karunika.choo@arm.com>
2025-10-14 10:43:31 +0100

    drm/panthor: Add architecture-specific function operations
    Introduce architecture-specific function pointers to support
    architecture-dependent behaviours. This patch adds the following
    function pointers and updates their usage accordingly:
    
    - soft_reset
    - l2_power_on
    - l2_power_off
    
    Signed-off-by: Karunika Choo <karunika.choo@arm.com>

--- drivers/gpu/drm/panthor/panthor_hw.h
+++ drivers/gpu/drm/panthor/panthor_hw.h
@@ -21,11 +21,34 @@ enum panthor_hw_feature {
 
 
 /**
+ * struct panthor_hw_ops - HW operations that are specific to a GPU
+ */
+struct panthor_hw_ops {
+	/** @soft_reset: Soft reset function pointer */
+	int (*soft_reset)(struct panthor_device *ptdev);
+#define panthor_hw_soft_reset(__ptdev) \
+	((__ptdev)->hw->ops.soft_reset ? (__ptdev)->hw->ops.soft_reset(__ptdev) : 0)
+
+	/** @l2_power_off: L2 power off function pointer */
+	int (*l2_power_off)(struct panthor_device *ptdev);
+#define panthor_hw_l2_power_off(__ptdev) \
+	((__ptdev)->hw->ops.l2_power_off ? (__ptdev)->hw->ops.l2_power_off(__ptdev) : 0)
+
+	/** @l2_power_on: L2 power on function pointer */
+	int (*l2_power_on)(struct panthor_device *ptdev);
+#define panthor_hw_l2_power_on(__ptdev) \
+	((__ptdev)->hw->ops.l2_power_on ? (__ptdev)->hw->ops.l2_power_on(__ptdev) : 0)
+};
+
+/**
  * struct panthor_hw - GPU specific register mapping and functions
  */
 struct panthor_hw {
 	/** @features: Bitmap containing panthor_hw_feature */
 	DECLARE_BITMAP(features, PANTHOR_HW_FEATURES_END);
+
+	/** @ops: Panthor HW specific operations */
+	struct panthor_hw_ops ops;
 };
 
 int panthor_hw_init(struct panthor_device *ptdev);
