612e88f7e85722321bad033403e5be434d31723c
Loïc Molinari <loic.molinari@collabora.com>
2025-10-15 17:30:13 +0200

    drm/gem: Get rid of *_with_mnt helpers
    drm_gem_object_init_with_mnt() and drm_gem_shmem_create_with_mnt() can
    be removed now that the drivers use drm_gem_huge_mnt_create().
    
    Signed-off-by: Loïc Molinari <loic.molinari@collabora.com>
    Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>

--- drivers/gpu/drm/v3d/v3d_bo.c
+++ drivers/gpu/drm/v3d/v3d_bo.c
@@ -151,8 +151,7 @@ struct v3d_bo *v3d_bo_create(struct drm_device *dev, struct drm_file *file_priv,
 	struct v3d_bo *bo;
 	int ret;
 
-	shmem_obj = drm_gem_shmem_create_with_mnt(dev, unaligned_size,
-						  dev->huge_mnt);
+	shmem_obj = drm_gem_shmem_create(dev, unaligned_size);
 	if (IS_ERR(shmem_obj))
 		return ERR_CAST(shmem_obj);
 	bo = to_v3d_bo(&shmem_obj->base);
