52b514f218847683cd597f58e9721aded0d2e4de
Maxim Levitsky <mlevitsk@redhat.com>
2025-10-14 23:32:58 -0400

    KVM: x86: Fix the interaction between SMM and the asynchronous pagefault
    Currently a #SMI can cause KVM to drop an #APF ready event and
    subsequently causes the guest to never resume the task that is waiting
    for it.
    This can result in tasks becoming permanently stuck within the guest.
    
    This happens because KVM flushes the APF queue without notifying the guest
    of completed APF requests when the guest exits to real mode.
    
    And the SMM exit code calls kvm_set_cr0 with CR.PE == 0, which triggers
    this code.
    
    It must be noted that while this flush is reasonable to do for the actual
    real mode entry, it is actually achieves nothing because it is too late to
    flush this queue on SMM exit.
    
    To fix this, avoid doing this flush altogether, and handle the real
    mode entry/exits in the same way KVM already handles the APIC
    enable/disable events:
    
    APF completion events are not injected while APIC is disabled,
    and once APIC is re-enabled, KVM raises the KVM_REQ_APF_READY request
    which causes the first pending #APF ready event to be injected prior
    to entry to the guest mode.
    
    This change also has the side benefit of preserving #APF events if the
    guest temporarily enters real mode - for example, to call firmware -
    although such usage should be extermery rare in modern operating systems.
    
    Signed-off-by: Maxim Levitsky <mlevitsk@redhat.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -1046,6 +1046,13 @@ bool kvm_require_dr(struct kvm_vcpu *vcpu, int dr)
 }
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_require_dr);
 
+static bool kvm_pv_async_pf_enabled(struct kvm_vcpu *vcpu)
+{
+	u64 mask = KVM_ASYNC_PF_ENABLED | KVM_ASYNC_PF_DELIVERY_AS_INT;
+
+	return (vcpu->arch.apf.msr_en_val & mask) == mask;
+}
+
 static inline u64 pdptr_rsvd_bits(struct kvm_vcpu *vcpu)
 {
 	return vcpu->arch.reserved_gpa_bits | rsvd_bits(5, 8) | rsvd_bits(1, 2);
@@ -1138,15 +1145,17 @@ void kvm_post_set_cr0(struct kvm_vcpu *vcpu, unsigned long old_cr0, unsigned lon
 	}
 
 	if ((cr0 ^ old_cr0) & X86_CR0_PG) {
-		kvm_clear_async_pf_completion_queue(vcpu);
-		kvm_async_pf_hash_reset(vcpu);
-
 		/*
 		 * Clearing CR0.PG is defined to flush the TLB from the guest's
 		 * perspective.
 		 */
 		if (!(cr0 & X86_CR0_PG))
 			kvm_make_request(KVM_REQ_TLB_FLUSH_GUEST, vcpu);
+		/*
+		 * Re-check APF completion events, when the guest re-enables paging.
+		 */
+		else if (kvm_pv_async_pf_enabled(vcpu))
+			kvm_make_request(KVM_REQ_APF_READY, vcpu);
 	}
 
 	if ((cr0 ^ old_cr0) & KVM_MMU_CR0_ROLE_BITS)
@@ -3649,13 +3658,6 @@ static int set_msr_mce(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 		return 1;
 	}
 	return 0;
-}
-
-static inline bool kvm_pv_async_pf_enabled(struct kvm_vcpu *vcpu)
-{
-	u64 mask = KVM_ASYNC_PF_ENABLED | KVM_ASYNC_PF_DELIVERY_AS_INT;
-
-	return (vcpu->arch.apf.msr_en_val & mask) == mask;
 }
 
 static int kvm_pv_enable_async_pf(struct kvm_vcpu *vcpu, u64 data)
