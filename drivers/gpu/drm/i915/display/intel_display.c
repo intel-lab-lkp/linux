3ec9571411fba222f403926e7347fae012e6eb4e
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2025-10-15 15:52:36 +0530

    drm/i915/vrr: prep patches for guardband optimization squashed
    This is a squashed patch based on the preparatory series for guardband
    optimization. It contains all the changes in the v7 of the series:
    'Preparatory patches for guardband optimization' [1]
    
    This handles few cases which will need changes when guardband will no
    longer be matched to vblank length.
    - Fix the vblank_start evaluation.
    - Fix PSR wake latency checks wrt to guradband.
    
    NOTE: This patch is not meant for review. Any review related to this
    patch should be done on the original series. In order not to diverge
    the discussion from the main series.
    
    [1] https://patchwork.freedesktop.org/series/155661/#rev7
    
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -2410,11 +2410,11 @@ static int intel_crtc_compute_config(struct intel_atomic_state *state,
 		intel_atomic_get_new_crtc_state(state, crtc);
 	int ret;
 
-	ret = intel_crtc_compute_set_context_latency(state, crtc);
+	ret = intel_dpll_crtc_compute_clock(state, crtc);
 	if (ret)
 		return ret;
 
-	ret = intel_dpll_crtc_compute_clock(state, crtc);
+	ret = intel_crtc_compute_set_context_latency(state, crtc);
 	if (ret)
 		return ret;
 
@@ -2430,6 +2430,8 @@ static int intel_crtc_compute_config(struct intel_atomic_state *state,
 
 	if (crtc_state->has_pch_encoder)
 		return ilk_fdi_compute_config(crtc, crtc_state);
+
+	intel_vrr_compute_guardband(crtc_state);
 
 	return 0;
 }
@@ -4722,8 +4724,6 @@ intel_modeset_pipe_config_late(struct intel_atomic_state *state,
 	struct drm_connector *connector;
 	int i;
 
-	intel_vrr_compute_config_late(crtc_state);
-
 	for_each_new_connector_in_state(&state->base, connector,
 					conn_state, i) {
 		struct intel_encoder *encoder =
@@ -4958,9 +4958,15 @@ static bool allow_vblank_delay_fastset(const struct intel_crtc_state *old_crtc_s
 	 * Allow fastboot to fix up vblank delay (handled via LRR
 	 * codepaths), a bit dodgy as the registers aren't
 	 * double buffered but seems to be working more or less...
+	 *
+	 * Also allow this when the VRR timing generator is always on,
+	 * and optimized guardband is used. In such cases,
+	 * vblank delay may vary even without inherited state, but it's
+	 * still safe as VRR guardband is still same.
 	 */
-	return HAS_LRR(display) && old_crtc_state->inherited &&
-		!intel_crtc_has_type(old_crtc_state, INTEL_OUTPUT_DSI);
+	return HAS_LRR(display) &&
+	       (old_crtc_state->inherited || intel_vrr_always_use_vrr_tg(display)) &&
+	       !intel_crtc_has_type(old_crtc_state, INTEL_OUTPUT_DSI);
 }
 
 bool
