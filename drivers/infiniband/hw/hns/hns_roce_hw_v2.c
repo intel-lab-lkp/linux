1f7c66cd77319031607c0282d9d79c610a433064
Junxian Huang <huangjunxian6@hisilicon.com>
2025-10-16 21:20:22 +0800

    RDMA/hns: Support link state reporting for bond
    The link state of bond depends on the upper device. Adapt current
    link state querying flow and ib_event dispatching flow to report
    correct link state of bond.
    
    Signed-off-by: Junxian Huang <huangjunxian6@hisilicon.com>

--- drivers/infiniband/hw/hns/hns_roce_hw_v2.c
+++ drivers/infiniband/hw/hns/hns_roce_hw_v2.c
@@ -7354,8 +7354,18 @@ static void hns_roce_hw_v2_link_status_change(struct hnae3_handle *handle,
 {
 	struct hns_roce_dev *hr_dev = (struct hns_roce_dev *)handle->priv;
 	struct net_device *netdev = handle->rinfo.netdev;
+	struct hns_roce_bond_group *bond_grp;
+	u8 bus_num;
 
 	if (linkup || !hr_dev)
+		return;
+
+	/* For bond device, the link status depends on the upper netdev,
+	 * and the upper device's link status depends on all the slaves'
+	 * netdev but not only one. So bond device cannot get a correct
+	 * link status from this path.
+	 */
+	if (hns_roce_get_bond_grp(netdev, get_hr_bus_num(hr_dev)))
 		return;
 
 	ib_dispatch_port_state_event(&hr_dev->ib_dev, netdev);
