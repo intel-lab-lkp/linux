def5820329259419524811d03882e879dad7d0d8
Qu Wenruo <wqu@suse.com>
2025-10-16 20:12:38 +1030

    btrfs: scrub: cancel the run if there is a pending signal
    Unlike relocation, scrub never checks pending signals, and even for
    relocation is only explicitly checking for fatal signal (SIGKILL), not
    for regular ones.
    
    Thankfully relocation can still be interrupted by regular signals by
    the usage of wait_on_bit(), which is interruptible by regular signals.
    
    So add the proper signal pending checks for scrub, by checking all
    pending signals, and if we hit one signal cancel the scrub run since the
    signal handling can only be done in the user space.
    
    Reviewed-by: Filipe Manana <fdmanana@suse.com>
    Signed-off-by: Qu Wenruo <wqu@suse.com>

--- fs/btrfs/scrub.c
+++ fs/btrfs/scrub.c
@@ -2098,6 +2098,13 @@ static bool should_cancel_scrub(struct scrub_ctx *sctx)
 	if (fs_info->sb->s_writers.frozen > SB_UNFROZEN ||
 	    freezing(current))
 		return true;
+	/*
+	 * Signal handling is also done in user space, so we have to return
+	 * to user space by canceling the run when there is a pending signal.
+	 * (including non-fatal ones like SIGINT)
+	 */
+	if (signal_pending(current))
+		return true;
 	return false;
 }
 
