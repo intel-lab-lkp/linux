bc5e2bbb613779d9928ff39b0f496841358c5a95
Francesco Lavra <flavra@baylibre.com>
2025-10-16 19:21:22 +0200

    iio: imu: st_lsm6dsx: Decouple sensor ODR from FIFO batch data rate
    The rate at which accelerometer or gyroscope sensor samples are fed
    to the hardware FIFO (batch data rate, or BDR) does not have to
    coincide with the sensor sampling frequency (output data rate, or
    ODR); the only requirement is for the BDR to not be greater than
    the ODR. Having a BDR lower than the ODR is useful in cases where
    an application requires a high sampling rate for accurate detection
    of motion events (e.g. wakeup events), but wants to read sensor
    sample values from the hardware FIFO at a lower data rate (e.g. to
    minimize the amount of I2C or SPI traffic and the rate of periodic
    interrupts).
    To support the above use case, add a sampling_frequency sysfs
    attribute to the buffer directory of st_lsm6dsx IIO devices, which
    controls the BDR for a given sensor independently from the "main"
    sampling_frequency attribute (which controls the ODR); introduce a
    new `hwfifo_odr_mHz` field in struct st_lsm6dsx_sensor to keep
    track of the current BDR value, and use this field instead of the
    `odr` field in the code that deals with the FIFO data rate. In the
    sensor hub driver, make the hwfifo_odr_mHz value always mirror the
    odr value, since there is no separate configuration setting to
    control the BDR for data produced by the sensor hub functionality.
    For backwards compatibility, set the buffer frequency equal to the
    main frequency whenever the latter is updated via sysfs; if
    userspace wants a different buffer frequency, it has to write to
    the relevant sysfs attribute after any writes to the main frequency
    attribute.
    
    Signed-off-by: Francesco Lavra <flavra@baylibre.com>

--- drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_core.c
+++ drivers/iio/imu/st_lsm6dsx/st_lsm6dsx_core.c
@@ -1850,7 +1850,7 @@ static int st_lsm6dsx_write_raw(struct iio_dev *iio_dev,
 		if (val < 0)
 			err = val;
 		else
-			sensor->odr = val;
+			sensor->odr = sensor->hwfifo_odr_mHz = val;
 		break;
 	}
 	default:
@@ -2384,6 +2384,7 @@ static struct iio_dev *st_lsm6dsx_alloc_iiodev(struct st_lsm6dsx_hw *hw,
 	sensor->id = id;
 	sensor->hw = hw;
 	sensor->odr = hw->settings->odr_table[id].odr_avl[0].milli_hz;
+	sensor->hwfifo_odr_mHz = sensor->odr;
 	sensor->gain = hw->settings->fs_table[id].fs_avl[0].gain;
 	sensor->watermark = 1;
 
