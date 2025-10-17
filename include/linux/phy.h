781be37673bdef161bca59b7457acff078de5cef
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2025-10-17 13:04:44 +0100

    net: phy: add phy_may_wakeup()
    Add phy_may_wakeup() which uses the driver model's device_may_wakeup()
    when the PHY driver has marked the device as wakeup capable in the
    driver model, otherwise use phy_drv_wol_enabled().
    
    Replace the sites that used to call phy_drv_wol_enabled() with this
    as checking the driver model will be more efficient than checking the
    WoL state.
    
    Export phy_may_wakeup() so that phylink can use it.
    
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>

--- include/linux/phy.h
+++ include/linux/phy.h
@@ -1391,6 +1391,15 @@ static inline bool phy_can_wakeup(struct phy_device *phydev)
 	return device_can_wakeup(&phydev->mdio.dev);
 }
 
+/**
+ * phy_may_wakeup() - indicate whether PHY has driver model wakeup is enabled
+ * @phydev: The phy_device struct
+ *
+ * Returns: true/false depending on the PHY driver's device_set_wakeup_enabled()
+ * setting.
+ */
+bool phy_may_wakeup(struct phy_device *phydev);
+
 void phy_resolve_aneg_pause(struct phy_device *phydev);
 void phy_resolve_aneg_linkmode(struct phy_device *phydev);
 
