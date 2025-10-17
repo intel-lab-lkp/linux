0c5bc978287d86ad764ad8b5a48e88d27c5ef1ac
XueBing Chen <chenxb_99091@126.com>
2025-10-17 14:41:52 +0800

    sched: Fix code indentation at line 10371
    Fix checkpatch.pl indentation warning by replacing spaces with tab.
    
    Signed-off-by: XueBing Chen <chenxb_99091@126.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -10256,7 +10256,7 @@ const u32 sched_prio_to_wmult[40] = {
 
 void call_trace_sched_update_nr_running(struct rq *rq, int count)
 {
-        trace_sched_update_nr_running_tp(rq, count);
+	trace_sched_update_nr_running_tp(rq, count);
 }
 
 #ifdef CONFIG_SCHED_MM_CID
