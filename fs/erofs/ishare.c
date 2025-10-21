f08d2e379c102ad78a0a610f9797fb72c99f2202
Hongzhen Luo <hongzhen@linux.alibaba.com>
2025-10-21 10:48:15 +0000

    erofs: implement .fadvise for page cache share
    This patch implements the .fadvise interface for page cache share.
    Similar to overlayfs, it drops those clean, unused pages through
    vfs_fadvise().
    
    Signed-off-by: Hongzhen Luo <hongzhen@linux.alibaba.com>
    [hongbo: forward port, minor cleanup]
    Signed-off-by: Hongbo Li <lihongbo22@huawei.com>

--- fs/erofs/ishare.c
+++ fs/erofs/ishare.c
@@ -228,6 +228,16 @@ static int erofs_ishare_mmap(struct file *file, struct vm_area_struct *vma)
 	return generic_file_readonly_mmap(file, vma);
 }
 
+static int erofs_ishare_fadvice(struct file *file, loff_t offset,
+				      loff_t len, int advice)
+{
+	struct file *realfile = file->private_data;
+
+	if (!realfile)
+		return -EINVAL;
+	return vfs_fadvise(realfile, offset, len, advice);
+}
+
 const struct file_operations erofs_ishare_fops = {
 	.open		= erofs_ishare_file_open,
 	.llseek		= generic_file_llseek,
@@ -236,6 +246,7 @@ const struct file_operations erofs_ishare_fops = {
 	.release	= erofs_ishare_file_release,
 	.get_unmapped_area = thp_get_unmapped_area,
 	.splice_read	= filemap_splice_read,
+	.fadvise	= erofs_ishare_fadvice,
 };
 
 void erofs_read_begin(struct erofs_read_ctx *rdctx)
