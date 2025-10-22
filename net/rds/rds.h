954f81acf9828402312111710b90254ae13e968f
Gerd Rausch <gerd.rausch@oracle.com>
2025-10-22 12:17:11 -0700

    net/rds: Use the first lane until RDS_EXTHDR_NPATHS arrives
    Instead of just blocking the sender until "c_npaths" is known
    (it gets updated upon the receipt of a MPRDS PONG message),
    simply use the first lane (cp_index#0).
    
    But just using the first lane isn't enough.
    
    As soon as we enqueue messages on a different lane, we'd run the risk
    of out-of-order delivery of RDS messages.
    
    Earlier messages enqueued on "cp_index == 0" could be delivered later
    than more recent messages enqueued on "cp_index > 0", mostly because of
    possible head of line blocking issues causing the first lane to be
    slower.
    
    To avoid that, we simply take a snapshot of "cp_next_tx_seq" at the
    time we're about to fan-out to more lanes.
    
    Then we delay the transmission of messages enqueued on other lanes
    with "cp_index > 0" until cp_index#0 caught up with the delivery of
    new messages (from "cp_send_queue") as well as in-flight
    messages (from "cp_retrans") that haven't been acknowledged yet
    by the receiver.
    
    We also add a new counter "mprds_catchup_tx0_retries" to keep track
    of how many times "rds_send_xmit" had to suspend activities,
    because it was waiting for the first lane to catch up.
    
    Signed-off-by: Gerd Rausch <gerd.rausch@oracle.com>
    Signed-off-by: Allison Henderson <allison.henderson@oracle.com>

--- net/rds/rds.h
+++ net/rds/rds.h
@@ -171,6 +171,8 @@ struct rds_connection {
 
 	u32			c_my_gen_num;
 	u32			c_peer_gen_num;
+
+	u64			c_cp0_mprds_catchup_tx_seq;
 };
 
 static inline
@@ -748,6 +750,7 @@ struct rds_statistics {
 	uint64_t	s_recv_bytes_added_to_socket;
 	uint64_t	s_recv_bytes_removed_from_socket;
 	uint64_t	s_send_stuck_rm;
+	uint64_t	s_mprds_catchup_tx0_retries;
 };
 
 /* af_rds.c */
