a38ca48a7a7106b39354a66df722da94e1c45acc
Manikandan Muralidharan <manikandan.m@microchip.com>
2025-10-22 14:43:50 +0530

    mfd: atmel-hlcdc: fetch LVDS PLL clock for LVDS display
    The XLCDC IP supports parallel RGB, MIPI DSI and LVDS Display.
    The LCD Generic clock (sys_clk) is used for Parallel RGB and MIPI
    displays, while the LVDS PLL clock (lvds_pll_clk) is used for LVDS
    displays.Since both the clocks cannot co-exist together in the DT
    for a given display, this patch first attempts to retrieve sys_clk
    If that fails,it then tries to acquire lvds_pll_clk.
    
    Signed-off-by: Manikandan Muralidharan <manikandan.m@microchip.com>
    Signed-off-by: Dharma Balasubiramani <dharma.b@microchip.com>

--- drivers/mfd/atmel-hlcdc.c
+++ drivers/mfd/atmel-hlcdc.c
@@ -108,10 +108,22 @@ static int atmel_hlcdc_probe(struct platform_device *pdev)
 		return PTR_ERR(hlcdc->periph_clk);
 	}
 
+	/*
+	 * Retrieve any one of the Main clk required by the LCD to operate:
+	 *
+	 * LCDC Generic Clock (sys_clk) - Parallel RGB and MIPI displays,
+	 * LVDS PLL (lvds_pll_clk)      - LVDS display
+	 */
 	hlcdc->sys_clk = devm_clk_get(dev, "sys_clk");
 	if (IS_ERR(hlcdc->sys_clk)) {
-		dev_err(dev, "failed to get system clock\n");
-		return PTR_ERR(hlcdc->sys_clk);
+		dev_warn(dev,
+			 "failed to get LCDC generic clock, trying for LVDS PLL clock instead\n");
+
+		hlcdc->lvds_pll_clk = devm_clk_get(dev, "lvds_pll_clk");
+		if (IS_ERR(hlcdc->lvds_pll_clk)) {
+			dev_err(dev, "failed to get LVDS PLL clock\n");
+			return PTR_ERR(hlcdc->lvds_pll_clk);
+		}
 	}
 
 	hlcdc->slow_clk = devm_clk_get(dev, "slow_clk");
