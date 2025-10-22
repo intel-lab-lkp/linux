d025827da031436af3c7032b482003c44a5342a0
Dan Carpenter <dan.carpenter@linaro.org>
2025-10-22 14:04:29 +0300

    drm/amd/ras: Fix a NULL vs IS_ERR() bug in ras_process_init()
    The kthread_run() function never returns NULL, it returns error pointers.
    Fix the error checking to match.
    
    Fixes: ea61341b9014 ("drm/amd/ras: Add thread to handle ras events")
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

--- drivers/gpu/drm/amd/ras/rascore/ras_process.c
+++ drivers/gpu/drm/amd/ras/rascore/ras_process.c
@@ -241,9 +241,9 @@ int ras_process_init(struct ras_core_context *ras_core)
 
 	ras_proc->ras_process_thread = kthread_run(ras_process_thread,
 							(void *)ras_core, "ras_process_thread");
-	if (!ras_proc->ras_process_thread) {
+	if (IS_ERR(ras_proc->ras_process_thread)) {
 		RAS_DEV_ERR(ras_core->dev, "Failed to create ras_process_thread.\n");
-		ret =  -ENOMEM;
+		ret = PTR_ERR(ras_proc->ras_process_thread);
 		goto err;
 	}
 
