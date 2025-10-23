ce89d7aa9ae8044cc06bb30ad272f5af111e2269
Kartik Rajput <kkartik@nvidia.com>
2025-10-23 15:00:40 +0530

    rtc: tegra: Use devm_clk_get_enabled() in probe
    Simplify clock management by replacing devm_clk_get() and manual clock
    enable/disable with devm_clk_get_enabled(). This also simplifies the
    error handling logic.
    
    Suggested-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Signed-off-by: Kartik Rajput <kkartik@nvidia.com>

--- drivers/rtc/rtc-tegra.c
+++ drivers/rtc/rtc-tegra.c
@@ -300,13 +300,9 @@ static int tegra_rtc_probe(struct platform_device *pdev)
 	info->rtc->ops = &tegra_rtc_ops;
 	info->rtc->range_max = U32_MAX;
 
-	info->clk = devm_clk_get(&pdev->dev, NULL);
+	info->clk = devm_clk_get_enabled(&pdev->dev, NULL);
 	if (IS_ERR(info->clk))
 		return PTR_ERR(info->clk);
-
-	ret = clk_prepare_enable(info->clk);
-	if (ret < 0)
-		return ret;
 
 	/* set context info */
 	info->pdev = pdev;
@@ -324,22 +320,16 @@ static int tegra_rtc_probe(struct platform_device *pdev)
 	ret = devm_request_irq(&pdev->dev, info->irq, tegra_rtc_irq_handler,
 			       IRQF_TRIGGER_HIGH, dev_name(&pdev->dev),
 			       &pdev->dev);
-	if (ret) {
-		dev_err(&pdev->dev, "failed to request interrupt: %d\n", ret);
-		goto disable_clk;
-	}
+	if (ret)
+		return dev_err_probe(&pdev->dev, ret, "failed to request interrupt\n");
 
 	ret = devm_rtc_register_device(info->rtc);
 	if (ret)
-		goto disable_clk;
+		return ret;
 
 	dev_notice(&pdev->dev, "Tegra internal Real Time Clock\n");
 
 	return 0;
-
-disable_clk:
-	clk_disable_unprepare(info->clk);
-	return ret;
 }
 
 static void tegra_rtc_remove(struct platform_device *pdev)
