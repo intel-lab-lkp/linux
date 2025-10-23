0f061e03b22f7f5c2b5cfe3c01ecd6d32d7d0fe9
Maciej Strozek <mstrozek@opensource.cirrus.com>
2025-10-23 10:27:41 +0100

    ALSA: sound: Increase max size of components field
    The components field of snd_card can run out of space in new systems which
    use many audio devices, hence increase its size to 256 bytes.
    
    Along with this change, bump the CTL protocol version to 2.0.10
    
    Signed-off-by: Maciej Strozek <mstrozek@opensource.cirrus.com>

--- include/uapi/sound/asound.h
+++ include/uapi/sound/asound.h
@@ -1058,7 +1058,7 @@ struct snd_timer_tread {
  *                                                                          *
  ****************************************************************************/
 
-#define SNDRV_CTL_VERSION		SNDRV_PROTOCOL_VERSION(2, 0, 9)
+#define SNDRV_CTL_VERSION		SNDRV_PROTOCOL_VERSION(2, 0, 10)
 
 struct snd_ctl_card_info {
 	int card;			/* card number */
@@ -1069,7 +1069,7 @@ struct snd_ctl_card_info {
 	unsigned char longname[80];	/* name + info text about soundcard */
 	unsigned char reserved_[16];	/* reserved for future (was ID of mixer) */
 	unsigned char mixername[80];	/* visual mixer identification */
-	unsigned char components[128];	/* card components / fine identification, delimited with one space (AC97 etc..) */
+	unsigned char components[256];	/* card components / fine identification, delimited with one space (AC97 etc..) */
 };
 
 typedef int __bitwise snd_ctl_elem_type_t;
