14082a30a72b9f63fc4aed56a69a5e993e67078b
Marek Szyprowski <m.szyprowski@samsung.com>
2025-10-23 20:50:30 +0000

    clocksource/drivers/exynos_mct: Use percpu interrupts only on ARM64
    For some unknown reasons forcing percpu interrupts for local timers
    breaks CPU hotplug for 'little' cores on legacy ARM 32bit Exynos based
    machines (for example Exynos5422-based Odroid-XU3/XU4 boards). Use percpu
    flag only when driver is compiled for newer ARM64 architecture.
    
    Fixes: f3cec54ee3bf ("clocksource/drivers/exynos_mct: Set local timer interrupts as percpu")
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Reviewed-by: Peter Griffin <peter.griffin@linaro.org>
    Reviewed-by: Krzysztof Kozlowski <krzysztof.kozlowski@linaro.org>

--- drivers/clocksource/exynos_mct.c
+++ drivers/clocksource/exynos_mct.c
@@ -597,7 +597,8 @@ static int __init exynos4_timer_interrupts(struct device_node *np,
 			if (request_irq(mct_irq,
 					exynos4_mct_tick_isr,
 					IRQF_TIMER | IRQF_NOBALANCING |
-					IRQF_PERCPU,
+					(IS_ENABLED(CONFIG_ARM64) ?
+					 IRQF_PERCPU : 0),
 					pcpu_mevt->name, pcpu_mevt)) {
 				pr_err("exynos-mct: cannot register IRQ (cpu%d)\n",
 									cpu);
