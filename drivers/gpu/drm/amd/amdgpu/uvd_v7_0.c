085be9fa8f3c047718692d74cfb24f8efbfea88f
Alexander Deucher <alexander.deucher@amd.com>
2025-10-23 14:18:22 -0400

    Revert "drm/amdgpu: More compact UVD 7 IB emission"
    This reverts commit 4400702d23ca58656d9730aac8de45b8909e4917.
    
    Change-Id: Iff9f0f055d335489dac124386e106a690fc6876d

--- drivers/gpu/drm/amd/amdgpu/uvd_v7_0.c
+++ drivers/gpu/drm/amd/amdgpu/uvd_v7_0.c
@@ -222,7 +222,6 @@ static int uvd_v7_0_enc_get_create_msg(struct amdgpu_ring *ring, u32 handle,
 	struct amdgpu_ib *ib;
 	struct dma_fence *f = NULL;
 	uint64_t addr;
-	u32 *ptr;
 	int i, r;
 
 	r = amdgpu_job_alloc_with_ib(ring->adev, NULL, NULL, ib_size_dw * 4,
@@ -231,29 +230,27 @@ static int uvd_v7_0_enc_get_create_msg(struct amdgpu_ring *ring, u32 handle,
 		return r;
 
 	ib = &job->ibs[0];
-	ptr = ib->ptr;
 	addr = amdgpu_bo_gpu_offset(bo);
 
-	*ptr++ = 0x00000018;
-	*ptr++ = 0x00000001; /* session info */
-	*ptr++ = handle;
-	*ptr++ = 0x00000000;
-	*ptr++ = upper_32_bits(addr);
-	*ptr++ = addr;
+	ib->length_dw = 0;
+	ib->ptr[ib->length_dw++] = 0x00000018;
+	ib->ptr[ib->length_dw++] = 0x00000001; /* session info */
+	ib->ptr[ib->length_dw++] = handle;
+	ib->ptr[ib->length_dw++] = 0x00000000;
+	ib->ptr[ib->length_dw++] = upper_32_bits(addr);
+	ib->ptr[ib->length_dw++] = addr;
 
-	*ptr++ = 0x00000014;
-	*ptr++ = 0x00000002; /* task info */
-	*ptr++ = 0x0000001c;
-	*ptr++ = 0x00000000;
-	*ptr++ = 0x00000000;
+	ib->ptr[ib->length_dw++] = 0x00000014;
+	ib->ptr[ib->length_dw++] = 0x00000002; /* task info */
+	ib->ptr[ib->length_dw++] = 0x0000001c;
+	ib->ptr[ib->length_dw++] = 0x00000000;
+	ib->ptr[ib->length_dw++] = 0x00000000;
 
-	*ptr++ = 0x00000008;
-	*ptr++ = 0x08000001; /* op initialize */
-
-	ib->length_dw = ptr - ib->ptr;
+	ib->ptr[ib->length_dw++] = 0x00000008;
+	ib->ptr[ib->length_dw++] = 0x08000001; /* op initialize */
 
 	for (i = ib->length_dw; i < ib_size_dw; ++i)
-		*ptr++ = 0x0;
+		ib->ptr[i] = 0x0;
 
 	r = amdgpu_job_submit_direct(job, ring, &f);
 	if (r)
@@ -288,7 +285,6 @@ static int uvd_v7_0_enc_get_destroy_msg(struct amdgpu_ring *ring, u32 handle,
 	struct amdgpu_ib *ib;
 	struct dma_fence *f = NULL;
 	uint64_t addr;
-	u32 *ptr;
 	int i, r;
 
 	r = amdgpu_job_alloc_with_ib(ring->adev, NULL, NULL, ib_size_dw * 4,
@@ -297,29 +293,27 @@ static int uvd_v7_0_enc_get_destroy_msg(struct amdgpu_ring *ring, u32 handle,
 		return r;
 
 	ib = &job->ibs[0];
-	ptr = ib->ptr;
 	addr = amdgpu_bo_gpu_offset(bo);
 
-	*ptr++ = 0x00000018;
-	*ptr++ = 0x00000001;
-	*ptr++ = handle;
-	*ptr++ = 0x00000000;
-	*ptr++ = upper_32_bits(addr);
-	*ptr++ = addr;
+	ib->length_dw = 0;
+	ib->ptr[ib->length_dw++] = 0x00000018;
+	ib->ptr[ib->length_dw++] = 0x00000001;
+	ib->ptr[ib->length_dw++] = handle;
+	ib->ptr[ib->length_dw++] = 0x00000000;
+	ib->ptr[ib->length_dw++] = upper_32_bits(addr);
+	ib->ptr[ib->length_dw++] = addr;
 
-	*ptr++ = 0x00000014;
-	*ptr++ = 0x00000002;
-	*ptr++ = 0x0000001c;
-	*ptr++ = 0x00000000;
-	*ptr++ = 0x00000000;
+	ib->ptr[ib->length_dw++] = 0x00000014;
+	ib->ptr[ib->length_dw++] = 0x00000002;
+	ib->ptr[ib->length_dw++] = 0x0000001c;
+	ib->ptr[ib->length_dw++] = 0x00000000;
+	ib->ptr[ib->length_dw++] = 0x00000000;
 
-	*ptr++ = 0x00000008;
-	*ptr++ = 0x08000002; /* op close session */
-
-	ib->length_dw = ptr - ib->ptr;
+	ib->ptr[ib->length_dw++] = 0x00000008;
+	ib->ptr[ib->length_dw++] = 0x08000002; /* op close session */
 
 	for (i = ib->length_dw; i < ib_size_dw; ++i)
-		*ptr++ = 0x0;
+		ib->ptr[i] = 0x0;
 
 	r = amdgpu_job_submit_direct(job, ring, &f);
 	if (r)
