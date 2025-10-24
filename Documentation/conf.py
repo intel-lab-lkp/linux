fb28d8eb70b8e35168e0b5fb17072b0f9c410ce4
Jonathan Corbet <corbet@lwn.net>
2025-10-24 14:08:27 -0600

    docs: move kernel-doc to tools/docs
    Move kernel-doc to join the rest of the tools, and update references
    throughout the tree.
    
    Acked-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Jonathan Corbet <corbet@lwn.net>

--- Documentation/conf.py
+++ Documentation/conf.py
@@ -582,7 +582,7 @@ pdf_documents = [
 # kernel-doc extension configuration for running Sphinx directly (e.g. by Read
 # the Docs). In a normal build, these are supplied from the Makefile via command
 # line arguments.
-kerneldoc_bin = "../scripts/kernel-doc.py"
+kerneldoc_bin = "../tools/docs/kernel-doc.py"
 kerneldoc_srctree = ".."
 
 def setup(app):
