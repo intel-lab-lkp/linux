b421c7badabe12de8a2e433cebfa46462a912d27
Matthew Wilcox (Oracle) <willy@infradead.org>
2025-10-24 21:44:28 +0100

    slab: Remove folio references from kfree_nolock()
    In preparation for splitting struct slab from struct page and struct
    folio, remove mentions of struct folio from this function.  Since large
    kmalloc objects are not supported here, we can just use virt_to_slab().
    
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>

--- mm/slub.c
+++ mm/slub.c
@@ -6826,7 +6826,6 @@ EXPORT_SYMBOL(kfree);
  */
 void kfree_nolock(const void *object)
 {
-	struct folio *folio;
 	struct slab *slab;
 	struct kmem_cache *s;
 	void *x = (void *)object;
@@ -6834,13 +6833,12 @@ void kfree_nolock(const void *object)
 	if (unlikely(ZERO_OR_NULL_PTR(object)))
 		return;
 
-	folio = virt_to_folio(object);
-	if (unlikely(!folio_test_slab(folio))) {
+	slab = virt_to_slab(object);
+	if (unlikely(!slab)) {
 		WARN_ONCE(1, "large_kmalloc is not supported by kfree_nolock()");
 		return;
 	}
 
-	slab = folio_slab(folio);
 	s = slab->slab_cache;
 
 	memcg_slab_free_hook(s, slab, &x, 1);
