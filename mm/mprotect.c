6e793f9aef843a778a941d4dff4fd66d53af7a71
Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
2025-10-24 08:41:23 +0100

    mm: introduce is_huge_pmd() and use where appropriate
    The 'swap' PMD case is confusing as only non-present entries (i.e. non-swap
    'swap' entries) are valid at PMD level- currently migration entries and
    device private entries.
    
    We repeatedly perform checks of the form is_swap_pmd() || pmd_trans_huge()
    which is itself confusing - it implies that swap entries at PMD level exist
    and are different from huge entries.
    
    Address this confusion by introduced is_huge_pmd() which checks for either
    case. Sadly due to header dependency issues (huge_mm.h is included very
    early on in headers and cannot really rely on much else) we cannot assert
    here that is_pmd_non_present_folio_entry() is true for the input PMD if
    non-present.
    
    However since these are the only valid, handled cases the function is still
    achieving what it intends to do.
    
    We then replace all instances of is_swap_pmd() || pmd_trans_huge() with
    is_huge_pmd() invocations and adjust logic accordingly to accommodate
    this.
    
    Signed-off-by: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>

--- mm/mprotect.c
+++ mm/mprotect.c
@@ -522,7 +522,7 @@ again:
 			goto next;
 
 		_pmd = pmdp_get_lockless(pmd);
-		if (is_swap_pmd(_pmd) || pmd_trans_huge(_pmd)) {
+		if (is_huge_pmd(_pmd)) {
 			if ((next - addr != HPAGE_PMD_SIZE) ||
 			    pgtable_split_needed(vma, cp_flags)) {
 				__split_huge_pmd(vma, pmd, addr, false);
