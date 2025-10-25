6aa63a4bfdb970f38d237a2f7e6dedf8b147811e
Thomas Hellström <thomas.hellstrom@linux.intel.com>
2025-10-25 14:04:04 +0200

    drm/pagemap_util: Add a utility to assign an owner to a set of interconnected gpus
    The hmm_range_fault() and the migration helpers currently need a common
    "owner" to identify pagemaps and clients with fast interconnect.
    Add a drm_pagemap utility to setup such owners by registering
    drm_pagemaps, in a registry, and for each new drm_pagemap,
    query which existing drm_pagemaps have fast interconnects with the new
    drm_pagemap.
    
    The "owner" scheme is limited in that it is static at drm_pagemap creation.
    Ideally one would want the owner to be adjusted at run-time, but that
    requires changes to hmm. If the proposed scheme becomes too limited,
    we need to revisit.
    
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>

--- include/drm/drm_pagemap_util.h
+++ include/drm/drm_pagemap_util.h
@@ -1,11 +1,57 @@
 /* SPDX-License-Identifier: MIT */
+/*
+ * Copyright © 2025 Intel Corporation
+ */
+
 #ifndef _DRM_PAGEMAP_UTIL_H_
 #define _DRM_PAGEMAP_UTIL_H_
+
+#include <linux/list.h>
+#include <linux/mutex.h>
 
 struct drm_device;
 struct drm_pagemap;
 struct drm_pagemap_cache;
+struct drm_pagemap_owner;
 struct drm_pagemap_shrinker;
+
+/**
+ * struct drm_pagemap_peer - Structure representing a fast interconnect peer
+ * @list: Pointer to a &struct drm_pagemap_owner_list used to keep track of peers
+ * @link: List link for @list's list of peers.
+ * @owner: Pointer to a &struct drm_pagemap_owner, common for a set of peers having
+ * fast interconnects.
+ * @private: Pointer private to the struct embedding this struct.
+ */
+struct drm_pagemap_peer {
+	struct drm_pagemap_owner_list *list;
+	struct list_head link;
+	struct drm_pagemap_owner *owner;
+	void *private;
+};
+
+/**
+ * struct drm_pagemap_owner_list - Keeping track of peers and owners
+ * @peer: List of peers.
+ *
+ * The owner list defines the scope where we identify peers having fast interconnects
+ * and a common owner. Typically a driver has a single global owner list to
+ * keep track of common owners for the driver's pagemaps.
+ */
+struct drm_pagemap_owner_list {
+	/** @lock: Mutex protecting the @peers list. */
+	struct mutex lock;
+	/** @peers: List of peers. */
+	struct list_head peers;
+};
+
+/*
+ * Convenience macro to define an owner list.
+ */
+#define DRM_PAGEMAP_OWNER_LIST_DEFINE(_name)	\
+	struct drm_pagemap_owner_list _name = {	\
+	.lock = __MUTEX_INITIALIZER(_name.lock), \
+	.peers = LIST_HEAD_INIT(_name.peers) }
 
 void drm_pagemap_shrinker_add(struct drm_pagemap *dpagemap);
 
@@ -22,4 +68,11 @@ struct drm_pagemap *drm_pagemap_get_from_cache(struct drm_pagemap_cache *cache);
 void drm_pagemap_cache_set_pagemap(struct drm_pagemap_cache *cache, struct drm_pagemap *dpagemap);
 
 struct drm_pagemap *drm_pagemap_get_from_cache_if_active(struct drm_pagemap_cache *cache);
+
+void drm_pagemap_release_owner(struct drm_pagemap_peer *peer);
+
+int drm_pagemap_acquire_owner(struct drm_pagemap_peer *peer,
+			      struct drm_pagemap_owner_list *owner_list,
+			      bool (*has_interconnect)(struct drm_pagemap_peer *peer1,
+						       struct drm_pagemap_peer *peer2));
 #endif
