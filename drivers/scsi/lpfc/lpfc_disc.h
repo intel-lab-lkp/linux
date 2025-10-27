84edef973f2e9585032472a6a34b9e4a1ec3af7e
Justin Tee <justintee8345@gmail.com>
2025-10-27 16:54:40 -0700

    lpfc: Fix leaked ndlp krefs when in point-to-point topology
    In point-to-point topology, the driver sometimes defers the unsolicited
    FLOGI LS_ACC until it sends its FLOGI to the remote port.  When
    this happens, lpfc neglects to release the ndlp allocated for
    the unsolicited FLOGI.  This patch adds code to release the
    ndlp for the deferred unsolicited FLOGI LS_ACC.
    
    An NLP_FLOGI_DFR_ACC flag is introduced to facilitate identifying an ndlp
    with an expected deferred FLOGI LS_ACC completion.  When lpfc_cmpl_els_rsp
    detects the correct qualifiers, it releases the initial reference on the
    ndlp object.  And when lpfc_cmpl_els_rsp exits, the remaining put for the
    deferred action is executed and the ndlp is released.
    
    Signed-off-by: Justin Tee <justin.tee@broadcom.com>

--- drivers/scsi/lpfc/lpfc_disc.h
+++ drivers/scsi/lpfc/lpfc_disc.h
@@ -208,6 +208,7 @@ enum lpfc_nlp_flag {
 					   NPR list */
 	NLP_RM_DFLT_RPI    = 26,        /* need to remove leftover dflt RPI */
 	NLP_NODEV_REMOVE   = 27,        /* Defer removal till discovery ends */
+	NLP_FLOGI_DFR_ACC  = 28,        /* FLOGI LS_ACC was Deferred */
 	NLP_SC_REQ         = 29,        /* Target requires authentication */
 	NLP_FIRSTBURST     = 30,        /* Target supports FirstBurst */
 	NLP_RPI_REGISTERED = 31         /* nlp_rpi is valid */
