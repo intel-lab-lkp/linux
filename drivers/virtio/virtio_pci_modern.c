4e9a424b49bf7e9f642f255669ed8c543ed39107
Daniel Jurgens <danielj@nvidia.com>
2025-10-27 12:39:46 -0500

    virtio_pci: Remove supported_cap size build assert
    The cap ID list can be more than 64 bits. Remove the build assert. Also
    remove caching of the supported caps, it wasn't used.
    
    Signed-off-by: Daniel Jurgens <danielj@nvidia.com>
    Reviewed-by: Parav Pandit <parav@nvidia.com>

--- drivers/virtio/virtio_pci_modern.c
+++ drivers/virtio/virtio_pci_modern.c
@@ -304,7 +304,6 @@ end:
 
 static void virtio_pci_admin_cmd_cap_init(struct virtio_device *virtio_dev)
 {
-	struct virtio_pci_device *vp_dev = to_vp_device(virtio_dev);
 	struct virtio_admin_cmd_query_cap_id_result *data;
 	struct virtio_admin_cmd cmd = {};
 	struct scatterlist result_sg;
@@ -323,12 +322,7 @@ static void virtio_pci_admin_cmd_cap_init(struct virtio_device *virtio_dev)
 	if (ret)
 		goto end;
 
-	/* Max number of caps fits into a single u64 */
-	BUILD_BUG_ON(sizeof(data->supported_caps) > sizeof(u64));
-
-	vp_dev->admin_vq.supported_caps = le64_to_cpu(data->supported_caps[0]);
-
-	if (!(vp_dev->admin_vq.supported_caps & (1 << VIRTIO_DEV_PARTS_CAP)))
+	if (!(le64_to_cpu(data->supported_caps[0]) & (1 << VIRTIO_DEV_PARTS_CAP)))
 		goto end;
 
 	virtio_pci_admin_cmd_dev_parts_objects_enable(virtio_dev);
