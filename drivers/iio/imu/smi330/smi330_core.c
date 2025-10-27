d698cb077d155232b1ba56a7e5b14d170d3b7efa
Geert Uytterhoeven <geert+renesas@glider.be>
2025-10-27 19:41:44 +0100

    iio: imu: smi330: #undef field_{get,prep}() before definition
    Prepare for the advent of globally available common field_get() and
    field_prep() macros by undefining the symbols before defining local
    variants.  This prevents redefinition warnings from the C preprocessor
    when introducing the common macros later.
    
    Suggested-by: Yury Norov <yury.norov@gmail.com>
    Signed-off-by: Geert Uytterhoeven <geert+renesas@glider.be>
    --
    v5:
      - New.

--- drivers/iio/imu/smi330/smi330_core.c
+++ drivers/iio/imu/smi330/smi330_core.c
@@ -68,7 +68,9 @@
 #define SMI330_SOFT_RESET_DELAY 2000
 
 /* Non-constant mask variant of FIELD_GET() and FIELD_PREP() */
+#undef field_get
 #define field_get(_mask, _reg) (((_reg) & (_mask)) >> (ffs(_mask) - 1))
+#undef field_prep
 #define field_prep(_mask, _val) (((_val) << (ffs(_mask) - 1)) & (_mask))
 
 #define SMI330_ACCEL_CHANNEL(_axis) {					\
