8c731a4c98c26873fe38af73ef654ef590c3e811
Chuan Liu <chuan.liu@amlogic.com>
2025-10-28 17:52:28 +0800

    dt-bindings: clock: Add Amlogic A5 PLL clock controller
    Add the PLL clock controller dt-bindings for the Amlogic A5 SoC
    family.
    
    Signed-off-by: Chuan Liu <chuan.liu@amlogic.com>
    Reviewed-by: Rob Herring (Arm) <robh@kernel.org>
    Signed-off-by: Xianwei Zhao <xianwei.zhao@amlogic.com>

---
+++ include/dt-bindings/clock/amlogic,a5-pll-clkc.h
@@ -0,0 +1,24 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR MIT) */
+/*
+ * Copyright (c) 2024 Amlogic, Inc. All rights reserved.
+ * Author: Chuan Liu <chuan.liu@amlogic.com>
+ */
+
+#ifndef _DT_BINDINGS_CLOCK_AMLOGIC_A5_PLL_CLKC_H
+#define _DT_BINDINGS_CLOCK_AMLOGIC_A5_PLL_CLKC_H
+
+#define CLKID_MPLL_PREDIV			0
+#define CLKID_MPLL0_DIV				1
+#define CLKID_MPLL0				2
+#define CLKID_MPLL1_DIV				3
+#define CLKID_MPLL1				4
+#define CLKID_MPLL2_DIV				5
+#define CLKID_MPLL2				6
+#define CLKID_MPLL3_DIV				7
+#define CLKID_MPLL3				8
+#define CLKID_GP0_PLL_DCO			9
+#define CLKID_GP0_PLL				10
+#define CLKID_HIFI_PLL_DCO			11
+#define CLKID_HIFI_PLL				12
+
+#endif  /* _DT_BINDINGS_CLOCK_AMLOGIC_A5_PLL_CLKC_H */
