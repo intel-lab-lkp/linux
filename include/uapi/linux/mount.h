a04444e5e6a60938304f9b378ab32dedd707d540
Bhavik Sachdev <b.sachdev1904@gmail.com>
2025-10-29 10:47:05 +0530

    statmount: accept fd as a parameter
    Extend `struct mnt_id_req` to take in a fd and introduce STATMOUNT_BY_FD
    flag. When a valid fd is provided and STATMOUNT_BY_FD is set, statmount
    will return mountinfo about the mount the fd is on.
    
    This even works for "unmounted" mounts (mounts that have been umounted
    using umount2(mnt, MNT_DETACH)), if you have access to a file descriptor
    on that mount. These "umounted" mounts will have no mountpoint hence we
    return "[unmounted]" and the mnt_ns_id to be 0.
    
    Co-developed-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Signed-off-by: Pavel Tikhomirov <ptikhomirov@virtuozzo.com>
    Signed-off-by: Bhavik Sachdev <b.sachdev1904@gmail.com>

--- include/uapi/linux/mount.h
+++ include/uapi/linux/mount.h
@@ -197,7 +197,7 @@ struct statmount {
  */
 struct mnt_id_req {
 	__u32 size;
-	__u32 spare;
+	__u32 fd;
 	__u64 mnt_id;
 	__u64 param;
 	__u64 mnt_ns_id;
@@ -231,5 +231,10 @@ struct mnt_id_req {
  */
 #define LSMT_ROOT		0xffffffffffffffff	/* root mount */
 #define LISTMOUNT_REVERSE	(1 << 0) /* List later mounts first */
+
+/*
+ * @flag bits for statmount(2)
+ */
+#define STATMOUNT_BY_FD		0x0000001U /* want mountinfo for given fd */
 
 #endif /* _UAPI_LINUX_MOUNT_H */
