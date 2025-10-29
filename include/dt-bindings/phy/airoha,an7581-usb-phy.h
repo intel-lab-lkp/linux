fd907532ff21d2f17735c31882d1d40204fbc55d
Christian Marangi <ansuelsmth@gmail.com>
2025-10-29 18:37:10 +0100

    dt-bindings: phy: Add documentation for Airoha AN7581 USB PHY
    Add documentation for Airoha AN7581 USB PHY that describe the USB PHY
    for the USB controller.
    
    Airoha AN7581 SoC support a maximum of 2 USB port. The USB 2.0 mode is
    always supported. The USB 3.0 mode is optional and depends on the Serdes
    mode currently configured on the system for the relevant USB port.
    
    The first USB port on the SoC can be both used for USB 3.0 operation or
    Ethernet (HSGMII).
    The second USB port on the SoC can be both used for USB 3.0 operation or
    for an additional PCIe line.
    
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

---
+++ include/dt-bindings/phy/airoha,an7581-usb-phy.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
+
+#ifndef _DT_BINDINGS_AIROHA_AN7581_USB_PHY_H_
+#define _DT_BINDINGS_AIROHA_AN7581_USB_PHY_H_
+
+#define AIROHA_USB2_MONCLK_SEL0                 0
+#define AIROHA_USB2_MONCLK_SEL1                 1
+#define AIROHA_USB2_MONCLK_SEL2                 2
+#define AIROHA_USB2_MONCLK_SEL3                 3
+
+#endif
