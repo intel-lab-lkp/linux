1bef0f0d8bcf9e53337a415d0a99992bd789d09c
Yi Cong <yicong@kylinos.cn>
2025-10-29 11:00:42 +0800

    net: phy: motorcomm: correct the default rx delay config for the rgmii
    According to the dataSheet, rx delay default value is set to 0.
    
    Signed-off-by: Yi Cong <yicong@kylinos.cn>

--- drivers/net/phy/motorcomm.c
+++ drivers/net/phy/motorcomm.c
@@ -890,7 +890,7 @@ static int ytphy_rgmii_clk_delay_config(struct phy_device *phydev)
 	rx_reg = ytphy_get_delay_reg_value(phydev, "rx-internal-delay-ps",
 					   ytphy_rgmii_delays, tb_size,
 					   &rxc_dly_en,
-					   YT8521_RC1R_RGMII_1_950_NS);
+					   YT8521_RC1R_RGMII_0_000_NS);
 	tx_reg = ytphy_get_delay_reg_value(phydev, "tx-internal-delay-ps",
 					   ytphy_rgmii_delays, tb_size, NULL,
 					   YT8521_RC1R_RGMII_1_950_NS);
