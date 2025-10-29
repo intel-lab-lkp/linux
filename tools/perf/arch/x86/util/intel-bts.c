429d25fb38447990679ff982afee79398d0e213b
Ian Rogers <irogers@google.com>
2025-10-28 22:34:00 -0700

    perf arch x86: Sort includes and add missed explicit dependencies
    Fix missing #includes found while cleaning the evsel/evlist header
    files. Sort the remaining header files for consistency with the rest
    of the code.
    
    Signed-off-by: Ian Rogers <irogers@google.com>

--- tools/perf/arch/x86/util/intel-bts.c
+++ tools/perf/arch/x86/util/intel-bts.c
@@ -5,25 +5,27 @@
  */
 
 #include <errno.h>
+
+#include <internal/lib.h> // page_size
 #include <linux/kernel.h>
 #include <linux/types.h>
 #include <linux/bitops.h>
 #include <linux/log2.h>
 #include <linux/zalloc.h>
 
-#include "../../../util/cpumap.h"
-#include "../../../util/event.h"
-#include "../../../util/evsel.h"
-#include "../../../util/evlist.h"
-#include "../../../util/mmap.h"
-#include "../../../util/session.h"
-#include "../../../util/pmus.h"
-#include "../../../util/debug.h"
-#include "../../../util/record.h"
-#include "../../../util/tsc.h"
 #include "../../../util/auxtrace.h"
+#include "../../../util/cpumap.h"
+#include "../../../util/debug.h"
+#include "../../../util/event.h"
+#include "../../../util/evlist.h"
+#include "../../../util/evsel.h"
 #include "../../../util/intel-bts.h"
-#include <internal/lib.h> // page_size
+#include "../../../util/mmap.h"
+#include "../../../util/pmu.h"
+#include "../../../util/pmus.h"
+#include "../../../util/record.h"
+#include "../../../util/session.h"
+#include "../../../util/tsc.h"
 
 #define KiB(x) ((x) * 1024)
 #define MiB(x) ((x) * 1024 * 1024)
