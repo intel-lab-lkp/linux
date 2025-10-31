d31bab01b3574da9e5f10c7da9bd8b183b6d52cf
Zhang Yi <yi.zhang@huawei.com>
2025-10-31 14:29:05 +0800

    ext4: replace ext4_es_insert_extent() when caching on-disk extents
    In ext4, the remaining places for inserting extents into the extent
    status tree within ext4_ext_determine_insert_hole() and
    ext4_map_query_blocks() directly cache on-disk extents. We can use
    ext4_es_cache_extent() instead of ext4_es_insert_extent() in these
    cases. This will help reduce unnecessary increases in extent sequence
    numbers and cache invalidations after supporting IOMAP in the future.
    
    Signed-off-by: Zhang Yi <yi.zhang@huawei.com>

--- fs/ext4/extents.c
+++ fs/ext4/extents.c
@@ -4160,8 +4160,8 @@ again:
 insert_hole:
 	/* Put just found gap into cache to speed up subsequent requests */
 	ext_debug(inode, " -> %u:%u\n", hole_start, len);
-	ext4_es_insert_extent(inode, hole_start, len, ~0,
-			      EXTENT_STATUS_HOLE, false);
+	ext4_es_cache_extent(inode, hole_start, len, ~0,
+			     EXTENT_STATUS_HOLE, true);
 
 	/* Update hole_len to reflect hole size after lblk */
 	if (hole_start != lblk)
