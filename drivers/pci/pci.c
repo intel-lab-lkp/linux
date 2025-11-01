95daba81b91681872846b5297629efcfa3578908
Hans Zhang <18255117159@163.com>
2025-11-02 00:05:35 +0800

    PCI: Add macro for secondary bus reset delay
    Add PCI_T_RST_SEC_BUS_DELAY_MS macro for the secondary bus reset
    delay value according to PCIe r7.0 spec, section 7.5.1.3.13.
    
    Signed-off-by: Hans Zhang <18255117159@163.com>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -4912,11 +4912,8 @@ void pci_reset_secondary_bus(struct pci_dev *dev)
 	ctrl |= PCI_BRIDGE_CTL_BUS_RESET;
 	pci_write_config_word(dev, PCI_BRIDGE_CONTROL, ctrl);
 
-	/*
-	 * PCI spec v3.0 7.6.4.2 requires minimum Trst of 1ms.  Double
-	 * this to 2ms to ensure that we meet the minimum requirement.
-	 */
-	msleep(2);
+	/* Double this to 2ms to ensure that we meet the minimum requirement */
+	msleep(2 * PCI_T_RST_SEC_BUS_DELAY_MS);
 
 	ctrl &= ~PCI_BRIDGE_CTL_BUS_RESET;
 	pci_write_config_word(dev, PCI_BRIDGE_CONTROL, ctrl);
