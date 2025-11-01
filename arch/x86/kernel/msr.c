c8a9377bc1846334805013461018895a87b66d54
Marc Herbert <marc.herbert@linux.intel.com>
2025-11-01 03:10:24 +0000

    x86/msr: add CPU_OUT_OF_SPEC taint name to "unrecognized" pr_warn(msg)
    While restricting access, commit a7e1f67ed29f ("x86/msr: Filter MSR
    writes") also added warning and tainting. But the warning message never
    mentioned tainting. Moreover, this uses the "CPU_OUT_OF_SPEC" flag which
    is not clearly related to MSRs: that flag is overloaded by several,
    fairly different situations, including some much scarier ones. So,
    without an expert around (thank you Dave Hansen), it would have been
    practically impossible to root cause the tainting from just the log file
    at hand.  Fix this by simply appending the CPU_OUT_OF_SPEC flag to the
    warning message.
    
    This readability issue happened when staring at logs involving the
    Intel Memory Latency Checker (among many other things going on in that
    log). The MLC disables hardware prefetch.
    
    Signed-off-by: Marc Herbert <marc.herbert@linux.intel.com>

--- arch/x86/kernel/msr.c
+++ arch/x86/kernel/msr.c
@@ -98,8 +98,8 @@ static int filter_write(u32 reg)
 	if (!__ratelimit(&fw_rs))
 		return 0;
 
-	pr_warn("Write to unrecognized MSR 0x%x by %s (pid: %d).\n",
-	        reg, current->comm, current->pid);
+	pr_warn("Write to unrecognized MSR 0x%x by %s (pid: %d), tainting %s\n",
+		reg, current->comm, current->pid, taint_flags[TAINT_CPU_OUT_OF_SPEC].desc);
 	pr_warn("See https://git.kernel.org/pub/scm/linux/kernel/git/tip/tip.git/about for details.\n");
 
 	return 0;
