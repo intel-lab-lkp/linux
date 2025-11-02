a79fef09741d1af9e060c163d07893dd62d8b611
zntsproj <vacacax16@gmail.com>
2025-11-02 15:14:31 +0300

    watchdog: fix typo 'paramter' -> 'parameter'
    Signed-off-by: zntsproj <vseokaktusah7@gmail.com>

--- drivers/watchdog/diag288_wdt.c
+++ drivers/watchdog/diag288_wdt.c
@@ -9,7 +9,7 @@
  * not recommended because it's only supported on z/VM but not whith LPAR.
  *
  * On LPAR, the watchdog will always trigger a system restart. the module
- * paramter cmd is meaningless here.
+ * parameter cmd is meaningless here.
  *
  *
  * Copyright IBM Corp. 2004, 2013
