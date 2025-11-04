c6d2776e29b97c0e20f3c1621e21a6718e79bbc4
Nuno Sá <nuno.sa@analog.com>
2025-11-04 15:35:08 +0000

    iio: dac: ad5446: Drop duplicated spi_id entry
    AD5600 and AD5541A are compatible so there's no need to have a dedicated
    entry for ID_AD5600.
    
    Signed-off-by: Nuno Sá <nuno.sa@analog.com>

--- drivers/iio/dac/ad5446.c
+++ drivers/iio/dac/ad5446.c
@@ -301,7 +301,6 @@ enum ad5446_supported_spi_device_ids {
 	ID_AD5541A,
 	ID_AD5512A,
 	ID_AD5553,
-	ID_AD5600,
 	ID_AD5601,
 	ID_AD5611,
 	ID_AD5621,
@@ -354,10 +353,6 @@ static const struct ad5446_chip_info ad5446_spi_chip_info[] = {
 	},
 	[ID_AD5553] = {
 		.channel = AD5446_CHANNEL(14, 16, 0),
-		.write = ad5446_write,
-	},
-	[ID_AD5600] = {
-		.channel = AD5446_CHANNEL(16, 16, 0),
 		.write = ad5446_write,
 	},
 	[ID_AD5601] = {
@@ -427,7 +422,7 @@ static const struct spi_device_id ad5446_spi_ids[] = {
 	{"ad5542a", ID_AD5541A}, /* ad5541a and ad5542a are compatible */
 	{"ad5543", ID_AD5541A}, /* ad5541a and ad5543 are compatible */
 	{"ad5553", ID_AD5553},
-	{"ad5600", ID_AD5600},
+	{"ad5600", ID_AD5541A}, /* ad5541a and ad5600 are compatible  */
 	{"ad5601", ID_AD5601},
 	{"ad5611", ID_AD5611},
 	{"ad5621", ID_AD5621},
