7a6a5d2c6719faf403c492c5fe02faa85d88b696
Michał Winiarski <michal.winiarski@intel.com>
2025-11-05 16:10:00 +0100

    drm/xe: Move migration support to device-level struct
    Upcoming changes will allow users to control VF state and obtain its
    migration data with a device-level granularity (not tile/gt).
    Change the data structures to reflect that and move the GT-level
    migration init to happen after device-level init.
    
    Signed-off-by: Michał Winiarski <michal.winiarski@intel.com>
    Reviewed-by: Michal Wajdeczko <michal.wajdeczko@intel.com>

--- drivers/gpu/drm/xe/xe_sriov_pf_types.h
+++ drivers/gpu/drm/xe/xe_sriov_pf_types.h
@@ -9,6 +9,7 @@
 #include <linux/mutex.h>
 #include <linux/types.h>
 
+#include "xe_sriov_pf_migration_types.h"
 #include "xe_sriov_pf_provision_types.h"
 #include "xe_sriov_pf_service_types.h"
 
@@ -43,6 +44,9 @@ struct xe_device_pf {
 
 	/** @provision: device level provisioning data. */
 	struct xe_sriov_pf_provision provision;
+
+	/** @migration: device level migration data. */
+	struct xe_sriov_pf_migration migration;
 
 	/** @service: device level service data. */
 	struct xe_sriov_pf_service service;
