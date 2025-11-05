17c01dffb9fc4e9780780186f0039cba7d139c51
Ville Syrjälä <ville.syrjala@linux.intel.com>
2025-11-05 21:04:27 +0200

    drm/i915/cx0: Undo the unjustified timeout change
    Undo the bogus timeout change to
    intel_cx0_powerdown_change_sequence(). If this was intentional
    then it should have been justifie in the commit message.
    
    This is also now abusing intel_de_wait_custom() in way that
    prevents future conversion to poll_timeout_us().
    
    Fixes: fc9be0a10ca4 ("drm/i915/ltphy: Add a wrapper for LT Phy powerdown change sequence")
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- drivers/gpu/drm/i915/display/intel_cx0_phy.c
+++ drivers/gpu/drm/i915/display/intel_cx0_phy.c
@@ -2830,7 +2830,7 @@ void intel_cx0_powerdown_change_sequence(struct intel_encoder *encoder,
 	/* Update Timeout Value */
 	if (intel_de_wait_custom(display, buf_ctl2_reg,
 				 intel_cx0_get_powerdown_update(lane_mask), 0,
-				 XELPDP_PORT_POWERDOWN_UPDATE_TIMEOUT_US, 2, NULL))
+				 XELPDP_PORT_POWERDOWN_UPDATE_TIMEOUT_US, 0, NULL))
 		drm_warn(display->drm,
 			 "PHY %c failed to bring out of Lane reset after %dus.\n",
 			 phy_name(phy), XELPDP_PORT_RESET_START_TIMEOUT_US);
