f349b9ffeae5a82add18960b0ab79d81406bc64a
Jim Mattson <jmattson@google.com>
2025-11-07 12:11:29 -0800

    KVM: x86: nSVM: Use cached VMCB12 g_pat in VMCB02 when using NPT
    When nested paging is enabled in VMCB12, copy the (cached and
    validated) VMCB12 g_pat to VMCB02.
    
    Fixes: 15038e147247 ("KVM: SVM: obey guest PAT")
    Signed-off-by: Jim Mattson <jmattson@google.com>

--- arch/x86/kvm/svm/nested.c
+++ arch/x86/kvm/svm/nested.c
@@ -605,8 +605,10 @@ void nested_vmcb02_compute_g_pat(struct vcpu_svm *svm)
 	if (!svm->nested.vmcb02.ptr)
 		return;
 
-	/* FIXME: merge g_pat from vmcb01 and vmcb12.  */
-	svm->nested.vmcb02.ptr->save.g_pat = svm->vmcb01.ptr->save.g_pat;
+	if (nested_npt_enabled(svm))
+		svm->nested.vmcb02.ptr->save.g_pat = svm->nested.ctl.g_pat;
+	else
+		svm->nested.vmcb02.ptr->save.g_pat = svm->vmcb01.ptr->save.g_pat;
 }
 
 static void nested_vmcb02_prepare_save(struct vcpu_svm *svm, struct vmcb *vmcb12)
