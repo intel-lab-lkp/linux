2ec984f2a19b03a2cc3b87b1b611262973a5bad1
Junrui Luo <moonafterrain@outlook.com>
2025-11-07 13:16:16 +0800

    net: qede: use scnprintf_append for version string
    Replace snprintf(buf + strlen(buf), left_size, ...) with
    scnprintf_append() for building the firmware version string. This
    simplifies the code.
    
    Signed-off-by: Junrui Luo <moonafterrain@outlook.com>

--- drivers/net/ethernet/qlogic/qede/qede_main.c
+++ drivers/net/ethernet/qlogic/qede/qede_main.c
@@ -1185,7 +1185,6 @@ static void qede_log_probe(struct qede_dev *edev)
 {
 	struct qed_dev_info *p_dev_info = &edev->dev_info.common;
 	u8 buf[QEDE_FW_VER_STR_SIZE];
-	size_t left_size;
 
 	snprintf(buf, QEDE_FW_VER_STR_SIZE,
 		 "Storm FW %d.%d.%d.%d, Management FW %d.%d.%d.%d",
@@ -1200,10 +1199,8 @@ static void qede_log_probe(struct qede_dev *edev)
 		 (p_dev_info->mfw_rev & QED_MFW_VERSION_0_MASK) >>
 		 QED_MFW_VERSION_0_OFFSET);
 
-	left_size = QEDE_FW_VER_STR_SIZE - strlen(buf);
-	if (p_dev_info->mbi_version && left_size)
-		snprintf(buf + strlen(buf), left_size,
-			 " [MBI %d.%d.%d]",
+	if (p_dev_info->mbi_version)
+		scnprintf_append(buf, QEDE_FW_VER_STR_SIZE, " [MBI %d.%d.%d]",
 			 (p_dev_info->mbi_version & QED_MBI_VERSION_2_MASK) >>
 			 QED_MBI_VERSION_2_OFFSET,
 			 (p_dev_info->mbi_version & QED_MBI_VERSION_1_MASK) >>
