18e2fc6adf69803c97a4100535b60e0ca2d34791
Jens Reidel <adrian@mainlining.org>
2025-11-08 23:05:55 +0100

    mips: Use generic endianness macros instead of MIPS-specific ones
    Compiling bpf_skel for mips currently fails because clang --target=bpf
    is invoked and the source files include byteorder.h, which uses the
    MIPS-specific macros to determine the endianness, rather than the generic
    __LITTLE_ENDIAN__ / __BIG_ENDIAN__. Fix this by using the generic
    macros, which are also defined when targeting bpf. This is already done
    similarly for powerpc.
    
    Signed-off-by: Jens Reidel <adrian@mainlining.org>

--- arch/mips/include/uapi/asm/byteorder.h
+++ arch/mips/include/uapi/asm/byteorder.h
@@ -9,12 +9,10 @@
 #ifndef _ASM_BYTEORDER_H
 #define _ASM_BYTEORDER_H
 
-#if defined(__MIPSEB__)
-#include <linux/byteorder/big_endian.h>
-#elif defined(__MIPSEL__)
+#ifdef __LITTLE_ENDIAN__
 #include <linux/byteorder/little_endian.h>
 #else
-# error "MIPS, but neither __MIPSEB__, nor __MIPSEL__???"
+#include <linux/byteorder/big_endian.h>
 #endif
 
 #endif /* _ASM_BYTEORDER_H */
