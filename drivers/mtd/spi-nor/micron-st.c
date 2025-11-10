463d2424eea1c934e4dd076b2d91f67717b2df6f
Haibo Chen <haibo.chen@nxp.com>
2025-11-10 12:02:59 +0800

    mtd: spi-nor: micron-st: enable 8D-8D-8D mode and die erase for mt35xu02gcba
    mt35xu02gcba is similar with mt35xu01gbba and mt35xu512aba, but with
    four dies inside. And it also support 8D-8D-8D mode, but SFDP lack
    SNOR_F_IO_MODE_EN_VOLATILE, so add this fixup flags here.
    
    Link: https://datasheet.octopart.com/MT35XU02GCBA1G12-0AAT-Micron-datasheet-138896808.pdf
    Signed-off-by: Haibo Chen <haibo.chen@nxp.com>

--- drivers/mtd/spi-nor/micron-st.c
+++ drivers/mtd/spi-nor/micron-st.c
@@ -214,6 +214,12 @@ static const struct spi_nor_fixups mt35xu01gbba_fixups = {
 	.late_init = micron_st_nor_two_die_late_init,
 };
 
+static const struct spi_nor_fixups mt35xu02gcba_fixups = {
+	.default_init = mt35xu512aba_default_init,
+	.post_sfdp = mt35xu512aba_post_sfdp_fixup,
+	.late_init = micron_st_nor_four_die_late_init,
+};
+
 static const struct flash_info micron_nor_parts[] = {
 	{
 		.id = SNOR_ID(0x2c, 0x5b, 0x1a),
@@ -237,12 +243,13 @@ static const struct flash_info micron_nor_parts[] = {
 		.fixups = &mt35xu01gbba_fixups,
 	}, {
 		.id = SNOR_ID(0x2c, 0x5b, 0x1c),
-		.name = "mt35xu02g",
+		.name = "mt35xu02gcba",
 		.sector_size = SZ_128K,
 		.size = SZ_256M,
 		.no_sfdp_flags = SECT_4K | SPI_NOR_OCTAL_READ,
 		.mfr_flags = USE_FSR,
-		.fixup_flags = SPI_NOR_4B_OPCODES,
+		.fixup_flags = SPI_NOR_4B_OPCODES | SPI_NOR_IO_MODE_EN_VOLATILE,
+		.fixups = &mt35xu02gcba_fixups,
 	},
 };
 
