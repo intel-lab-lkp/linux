9ce097e7af0fc7e30219fcb890661d4cb935dcb9
Robert Richter <rrichter@amd.com>
2025-11-12 21:31:35 +0100

    cxl: Introduce callback for HPA address ranges translation
    Introduce a callback to translate an endpoint's HPA range to the
    address range of the root port which is the System Physical Address
    (SPA) range used by a region. The callback can be set if a platform
    needs to handle address translation.
    
    The callback is attached to the root port. An endpoint's root port can
    easily be determined in the PCI hierarchy without any CXL specific
    knowledge. This allows the early use of address translation for CXL
    enumeration. Address translation is esp. needed for the detection of
    the root decoders. Thus, the callback is embedded in struct
    cxl_root_ops instead of struct cxl_rd_ops.
    
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Tested-by: Gregory Price <gourry@gourry.net>
    Signed-off-by: Robert Richter <rrichter@amd.com>

--- drivers/cxl/cxl.h
+++ drivers/cxl/cxl.h
@@ -644,6 +644,7 @@ struct cxl_root_ops {
 	int (*qos_class)(struct cxl_root *cxl_root,
 			 struct access_coordinate *coord, int entries,
 			 int *qos_class);
+	int (*translate_hpa_range)(struct cxl_root *cxl_root, void *data);
 };
 
 /**
