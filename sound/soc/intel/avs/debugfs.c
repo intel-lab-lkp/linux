6e07daa63fcbdbeb7a4d181547352aac31c17a97
HariKrishna Sagala <hariconscious@gmail.com>
2025-11-12 23:48:51 +0530

    ASoC: Intel: avs: Fix potential buffer overflow by snprintf()
    snprintf() returns the would-be-filled size when the string overflows
    the given buffer size, hence using this value may result in a buffer
    overflow (although it's unrealistic).
    
    This patch replaces it with a safer version, scnprintf() for papering
    over such a potential issue.
    Link: https://github.com/KSPP/linux/issues/105
    'Fixes: 5a565ba23abe ("ASoC: Intel: avs: Probing and firmware tracing
    over debugfs")'
    
    Signed-off-by: HariKrishna Sagala <hariconscious@gmail.com>

--- sound/soc/intel/avs/debugfs.c
+++ sound/soc/intel/avs/debugfs.c
@@ -119,9 +119,9 @@ static ssize_t probe_points_read(struct file *file, char __user *to, size_t coun
 	}
 
 	for (i = 0; i < num_desc; i++) {
-		ret = snprintf(buf + len, PAGE_SIZE - len,
-			       "Id: %#010x  Purpose: %d  Node id: %#x\n",
-			       desc[i].id.value, desc[i].purpose, desc[i].node_id.val);
+		ret = scnprintf(buf + len, PAGE_SIZE - len,
+					"Id: %#010x  Purpose: %d  Node id: %#x\n",
+					desc[i].id.value, desc[i].purpose, desc[i].node_id.val);
 		if (ret < 0)
 			goto free_desc;
 		len += ret;
