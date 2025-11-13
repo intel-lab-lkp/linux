f6f61533ca33eca35c03f89c4271a919571e3d29
Thorsten Blum <thorsten.blum@linux.dev>
2025-11-13 14:58:31 +0100

    KEYS: encrypted: Replace deprecated strcpy and improve get_derived_key
    Determine 'key_name' before allocating memory for 'derived_buf' to only
    allocate as many bytes as needed. Currently, we potentially allocate one
    more byte than necessary when 'key_name' is "ENC_KEY".
    
    strcpy() is deprecated and uses an additional strlen() internally; use
    memcpy() directly to copy 'key_name' since we already know its length
    and that it is guaranteed to be NUL-terminated.
    
    Also reuse 'key_name_len' when copying 'master_key' instead of calling
    strlen() again.
    
    Link: https://github.com/KSPP/linux/issues/88
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>

--- security/keys/encrypted-keys/encrypted.c
+++ security/keys/encrypted-keys/encrypted.c
@@ -12,6 +12,7 @@
  */
 
 #include <linux/uaccess.h>
+#include <linux/minmax.h>
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/slab.h>
@@ -331,23 +332,18 @@ static int get_derived_key(u8 *derived_key, enum derived_key_type key_type,
 			   const u8 *master_key, size_t master_keylen)
 {
 	u8 *derived_buf;
-	unsigned int derived_buf_len;
+	size_t derived_buf_len;
+	const char *key_name;
+	size_t key_name_len;
 
-	derived_buf_len = strlen("AUTH_KEY") + 1 + master_keylen;
-	if (derived_buf_len < HASH_SIZE)
-		derived_buf_len = HASH_SIZE;
-
+	key_name = key_type ? "AUTH_KEY" : "ENC_KEY";
+	key_name_len = strlen(key_name) + 1;
+	derived_buf_len = max(key_name_len + master_keylen, HASH_SIZE);
 	derived_buf = kzalloc(derived_buf_len, GFP_KERNEL);
 	if (!derived_buf)
 		return -ENOMEM;
-
-	if (key_type)
-		strcpy(derived_buf, "AUTH_KEY");
-	else
-		strcpy(derived_buf, "ENC_KEY");
-
-	memcpy(derived_buf + strlen(derived_buf) + 1, master_key,
-	       master_keylen);
+	memcpy(derived_buf, key_name, key_name_len);
+	memcpy(derived_buf + key_name_len, master_key, master_keylen);
 	sha256(derived_buf, derived_buf_len, derived_key);
 	kfree_sensitive(derived_buf);
 	return 0;
