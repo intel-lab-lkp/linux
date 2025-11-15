d0dcfb06f8e6822810e7f458e8b77a0a6fda3909
Ally Heev <allyheev@gmail.com>
2025-11-15 19:11:27 +0530

    overlayfs: fix uninitialized pointers with free attribute
    Uninitialized pointers with `__free` attribute can cause undefined
    behavior as the memory assigned randomly to the pointer is freed
    automatically when the pointer goes out of scope.
    
    overlayfs doesn't have any bugs related to this as of now, but
    it is better to initialize and assign pointers with `__free` attribute
    in one statement to ensure proper scope-based cleanup
    
    Reported-by: Dan Carpenter <dan.carpenter@linaro.org>
    Closes: https://lore.kernel.org/all/aPiG_F5EBQUjZqsl@stanley.mountain/
    Acked-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Ally Heev <allyheev@gmail.com>

--- fs/overlayfs/params.c
+++ fs/overlayfs/params.c
@@ -448,7 +448,7 @@ static int ovl_parse_layer(struct fs_context *fc, struct fs_parameter *param,
 		err = ovl_do_parse_layer(fc, param->string, &layer_path, layer);
 		break;
 	case fs_value_is_file: {
-		char *buf __free(kfree);
+		char *buf __free(kfree) = NULL;
 		char *layer_name;
 
 		buf = kmalloc(PATH_MAX, GFP_KERNEL_ACCOUNT);
