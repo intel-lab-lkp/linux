612c8cad8272e6ef2792240165d9752d26674962
FUJITA Tomonori <fujita.tomonori@gmail.com>
2025-11-15 14:03:03 +0900

    rust: sync: Add smp_store_release/smp_load_acquire helpers
    Add smp_store_release/smp_load_acquire helpers to support
    store_release/load_acquire for i8/i16.
    
    Both are implemented as a C macro and type information is necessary so
    the size argument is necessary unlike the C version.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@gmail.com>

--- rust/helpers/barrier.c
+++ rust/helpers/barrier.c
@@ -16,3 +16,27 @@ void rust_helper_smp_rmb(void)
 {
 	smp_rmb();
 }
+
+void rust_helper_smp_store_release(void *ptr, u64 val, size_t size)
+{
+	switch (size) {
+	case 1:
+		smp_store_release((u8 *)ptr, (u8)val);
+		break;
+	case 2:
+		smp_store_release((u16 *)ptr, (u16)val);
+		break;
+	}
+}
+
+void rust_helper_smp_load_acquire(void *ptr, void *val, size_t size)
+{
+	switch (size) {
+	case 1:
+		*(u8 *)val = smp_load_acquire((u8 *)ptr);
+		break;
+	case 2:
+		*(u16 *)val = smp_load_acquire((u16 *)ptr);
+		break;
+	}
+}
