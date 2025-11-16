0c0f04c14ec517a41a722f41019f087cdc6f1772
Ma Ke <make24@iscas.ac.cn>
2025-11-16 09:06:13 +0800

    USB: ohci-nxp: Fix error handling in ohci-hcd-nxp driver
    When obtaining the ISP1301 I2C client through the device tree, the
    driver does not release the device reference in the probe failure path
    or in the remove function. This could cause a reference count leak,
    which may prevent the device from being properly unbound or freed,
    leading to resource leakage. Add put_device() to release the reference
    in the probe failure path and in the remove function.
    
    Found by code review.
    
    Cc: stable@vger.kernel.org
    Fixes: 73108aa90cbf ("USB: ohci-nxp: Use isp1301 driver")
    Signed-off-by: Ma Ke <make24@iscas.ac.cn>

--- drivers/usb/host/ohci-nxp.c
+++ drivers/usb/host/ohci-nxp.c
@@ -223,6 +223,8 @@ static int ohci_hcd_nxp_probe(struct platform_device *pdev)
 fail_resource:
 	usb_put_hcd(hcd);
 fail_disable:
+	if (isp1301_i2c_client)
+		put_device(&isp1301_i2c_client->dev);
 	isp1301_i2c_client = NULL;
 	return ret;
 }
@@ -234,6 +236,8 @@ static void ohci_hcd_nxp_remove(struct platform_device *pdev)
 	usb_remove_hcd(hcd);
 	ohci_nxp_stop_hc();
 	usb_put_hcd(hcd);
+	if (isp1301_i2c_client)
+		put_device(&isp1301_i2c_client->dev);
 	isp1301_i2c_client = NULL;
 }
 
