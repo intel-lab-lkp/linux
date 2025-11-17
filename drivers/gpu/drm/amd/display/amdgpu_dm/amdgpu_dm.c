dd69a038019578153e1946a0ce58af9e7fd09836
Werner Sembach <wse@tuxedocomputers.com>
2025-11-17 20:11:45 +0100

    drm/amd/display: Remove unnecessary SIGNAL_TYPE_HDMI_TYPE_A check
    Remove unnecessary SIGNAL_TYPE_HDMI_TYPE_A check that was performed in the
    drm_mode_is_420_only() case, but not in the drm_mode_is_420_also() &&
    force_yuv420_output case.
    
    Without further knowledge if YCbCr 4:2:0 is supported outside of HDMI,
    there is no reason to use RGB when the display
    reports drm_mode_is_420_only() even on a non HDMI connection.
    
    This patch also moves both checks in the same if-case. This  eliminates an
    extra else-if-case.
    
    Signed-off-by: Werner Sembach <wse@tuxedocomputers.com>
    Signed-off-by: Andri Yngvason <andri@yngvason.is>
    Tested-by: Andri Yngvason <andri@yngvason.is>

--- drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
+++ drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -6522,12 +6522,8 @@ static void fill_stream_properties_from_drm_display_mode(
 	timing_out->v_border_top = 0;
 	timing_out->v_border_bottom = 0;
 	/* TODO: un-hardcode */
-	if (drm_mode_is_420_only(info, mode_in)
-			&& stream->signal == SIGNAL_TYPE_HDMI_TYPE_A)
-		timing_out->pixel_encoding = PIXEL_ENCODING_YCBCR420;
-	else if (drm_mode_is_420_also(info, mode_in)
-			&& aconnector
-			&& aconnector->force_yuv420_output)
+	if (drm_mode_is_420_only(info, mode_in) || (drm_mode_is_420_also(info, mode_in) &&
+	    aconnector && aconnector->force_yuv420_output))
 		timing_out->pixel_encoding = PIXEL_ENCODING_YCBCR420;
 	else if ((connector->display_info.color_formats & DRM_COLOR_FORMAT_YCBCR422)
 			&& aconnector
