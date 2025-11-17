edb11c3bd1886d9547b7e7f283941ea8ab1b2035
Pingfan Liu <piliu@redhat.com>
2025-11-17 17:27:30 +0800

    cgroup/cpuset: Introduce cpuset_cpus_allowed_locked()
    cpuset_cpus_allowed() uses a reader lock that is sleepable under RT,
    which means it cannot be called inside raw_spin_lock_t context.
    
    Introduce a new cpuset_cpus_allowed_locked() helper that performs the
    same function as cpuset_cpus_allowed() except that the caller must have
    acquired the cpuset_mutex so that no further locking will be needed.
    
    Suggested-by: Waiman Long <longman@redhat.com>
    Signed-off-by: Pingfan Liu <piliu@redhat.com>
    Cc: Waiman Long <longman@redhat.com>
    Cc: Tejun Heo <tj@kernel.org>
    Cc: Johannes Weiner <hannes@cmpxchg.org>
    Cc: "Michal Koutný" <mkoutny@suse.com>
    Cc: linux-kernel@vger.kernel.org
    To: cgroups@vger.kernel.org

--- include/linux/cpuset.h
+++ include/linux/cpuset.h
@@ -75,6 +75,7 @@ extern void dec_dl_tasks_cs(struct task_struct *task);
 extern void cpuset_lock(void);
 extern void cpuset_unlock(void);
 extern void cpuset_cpus_allowed(struct task_struct *p, struct cpumask *mask);
+extern void cpuset_cpus_allowed_locked(struct task_struct *p, struct cpumask *mask);
 extern bool cpuset_cpus_allowed_fallback(struct task_struct *p);
 extern bool cpuset_cpu_is_isolated(int cpu);
 extern nodemask_t cpuset_mems_allowed(struct task_struct *p);
