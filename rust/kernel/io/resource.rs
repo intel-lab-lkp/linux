7f5a61675c167eb31078abfae1fb409fcabd42b5
Alexandru Radovici <alexandru.radovici@wyliodrin.com>
2025-11-17 18:31:12 +0200

    rust: add a function that returns a reference to the IO Ports Resource
    This patch adds a function to `kernel::io::resource` that returns a static reference
    to a `Resource` that represents the IO Ports.
    
    Without this patch, there does not seem to be any way of getting a
    reference to a `Resource` of the IO Ports. This prevents anyone writing
    a safe driver in Rust that requires registering IO Ports.
    
    Examples are drivers for PS/2 keyboard and mouse or any of the
    QEMU x86 debug console (debugcon) and exit devices (isa-debug-exit).
    
    The QEMU debug devices are one of the simplest drivers that one can
    write and are good examples of the Rust Driver API.
    
    This patch has been inspired by the work at of Daniel Almeida and Fiona
    Behrens on the Asahi Linux branch.
    
    Co-developed-by: Fiona Behrens <me@kloenk.dev>
    Co-developed-by: Daniel Almeida <daniel.almeida@collabora.com>
    Co-developed-by: Alexandru Radovici <alexandru.radovici@wyliodrin.com>
    Signed-off-by: Alexandru Radovici <alexandru.radovici@wyliodrin.com>

--- rust/kernel/io/resource.rs
+++ rust/kernel/io/resource.rs
@@ -22,6 +22,15 @@ use crate::{
 /// `CONFIG_PHYS_ADDR_T_64BIT`, and it can be a u64 even on 32-bit architectures.
 pub type ResourceSize = bindings::phys_addr_t;
 
+#[cfg(CONFIG_HAS_IOPORT)]
+/// Returns a reference to the global `ioport_resource` variable.
+///
+/// This function is available only if `CONFIG_HAS_IOPORT` is enabled.
+pub fn ioport_resource() -> &'static Resource {
+    // SAFETY: `bindings::ioport_resoure` has global lifetime and is of type `Resource`.
+    unsafe { Resource::from_raw(&raw mut bindings::ioport_resource) }
+}
+
 /// A region allocated from a parent [`Resource`].
 ///
 /// # Invariants
