1ee12db773793d51b366b9156b415965ebaa931c
Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>
2025-11-17 11:14:32 +0530

    drm/i915/vrr: Add function to reset DC balance accumulated params
    Add function which resets all accumulated DC Balance parameters
    whenever adaptive mode of VRR goes off. This helps to give a
    fresh start when VRR is re-enabled.
    
    Signed-off-by: Mitul Golani <mitulkumar.ajitkumar.golani@intel.com>

--- drivers/gpu/drm/i915/display/intel_vrr.c
+++ drivers/gpu/drm/i915/display/intel_vrr.c
@@ -624,6 +624,19 @@ void intel_vrr_set_transcoder_timings(const struct intel_crtc_state *crtc_state)
 			       EMP_AS_SDP_DB_TL(crtc_state->vrr.vsync_start));
 }
 
+void
+intel_vrr_dcb_reset(const struct intel_crtc_state *crtc_state,
+		    struct intel_crtc *crtc)
+{
+	struct intel_display *display = to_intel_display(crtc_state);
+	enum pipe pipe = crtc->pipe;
+
+	if (!crtc_state->vrr.dc_balance.enable)
+		return;
+
+	intel_de_write(display, PIPEDMC_DCB_BALANCE_RESET(pipe), 0);
+}
+
 void intel_vrr_send_push(struct intel_dsb *dsb,
 			 const struct intel_crtc_state *crtc_state)
 {
