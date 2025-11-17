3dda9ffb0f0657a3df0105649f5db056ff31fad5
Byungchul Park <byungchul@sk.com>
2025-11-17 14:20:41 +0900

    mm: introduce a new page type for page pool in page type
    Currently, the condition 'page->pp_magic == PP_SIGNATURE' is used to
    determine if a page belongs to a page pool.  However, with the planned
    removal of @pp_magic, we should instead leverage the page_type in struct
    page, such as PGTY_netpp, for this purpose.
    
    Introduce and use the page type APIs e.g. PageNetpp(), __SetPageNetpp(),
    and __ClearPageNetpp() instead, and remove the existing APIs accessing
    @pp_magic e.g. page_pool_page_is_pp(), netmem_or_pp_magic(), and
    netmem_clear_pp_magic().
    
    Plus, add @page_type to struct net_iov at the same offset as struct page
    so as to use the page_type APIs for struct net_iov as well.  While at it,
    reorder @type and @owner in struct net_iov to avoid a hole and
    increasing the struct size.
    
    This work was inspired by the following link:
    
      https://lore.kernel.org/all/582f41c0-2742-4400-9c81-0d46bf4e8314@gmail.com/
    
    While at it, move the sanity check for page pool to on the free path.
    
    Suggested-by: David Hildenbrand <david@redhat.com>
    Co-developed-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Byungchul Park <byungchul@sk.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Acked-by: Zi Yan <ziy@nvidia.com>

--- include/linux/page-flags.h
+++ include/linux/page-flags.h
@@ -934,6 +934,7 @@ enum pagetype {
 	PGTY_zsmalloc		= 0xf6,
 	PGTY_unaccepted		= 0xf7,
 	PGTY_large_kmalloc	= 0xf8,
+	PGTY_netpp		= 0xf9,
 
 	PGTY_mapcount_underflow = 0xff
 };
@@ -1077,6 +1078,11 @@ PAGE_TYPE_OPS(Zsmalloc, zsmalloc, zsmalloc)
  */
 PAGE_TYPE_OPS(Unaccepted, unaccepted, unaccepted)
 FOLIO_TYPE_OPS(large_kmalloc, large_kmalloc)
+
+/*
+ * Marks page_pool allocated pages.
+ */
+PAGE_TYPE_OPS(Netpp, netpp, netpp)
 
 /**
  * PageHuge - Determine if the page belongs to hugetlbfs
