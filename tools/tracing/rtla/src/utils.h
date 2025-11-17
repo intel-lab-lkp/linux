4b03292e7ebb24008746fdbb84afa8071e9cb122
Wander Lairson Costa <wander@redhat.com>
2025-11-17 15:41:15 -0300

    rtla: Use standard exit codes for result enum
    The result enum defines custom values for PASSED, ERROR, and FAILED.
    These values correspond to standard exit codes EXIT_SUCCESS and
    EXIT_FAILURE.
    
    Update the enum to use the standard macros EXIT_SUCCESS and
    EXIT_FAILURE to improve readability and adherence to standard C
    practices.
    
    The FAILED value is implicitly assigned EXIT_FAILURE + 1, so there
    is no need to assign an explicit value.
    
    Signed-off-by: Wander Lairson Costa <wander@redhat.com>

--- tools/tracing/rtla/src/utils.h
+++ tools/tracing/rtla/src/utils.h
@@ -4,6 +4,7 @@
 #include <time.h>
 #include <sched.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 /*
  * '18446744073709551615\0'
@@ -97,7 +98,7 @@ bool strtoi(const char *s, int *res);
 #define ns_to_per(total, part) ((part * 100) / (double)total)
 
 enum result {
-	PASSED = 0, /* same as EXIT_SUCCESS */
-	ERROR = 1,  /* same as EXIT_FAILURE, an error in arguments */
-	FAILED = 2, /* test hit the stop tracing condition */
+	PASSED	= EXIT_SUCCESS,
+	ERROR	= EXIT_FAILURE,
+	FAILED, /* test hit the stop tracing condition */
 };
