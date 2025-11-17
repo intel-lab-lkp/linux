04bf0fac5d658b54fe55707ab78da8ad9c04872e
FUJITA Tomonori <fujita.tomonori@gmail.com>
2025-11-17 09:10:33 +0900

    rust: helpers: Add i8/i16 relaxed atomic helpers
    Add READ_ONCE/WRITE_ONCE based helpers for i8 and i16 types to support
    relaxed atomic operations in Rust.
    
    While relaxed operations could be implemented purely in Rust using
    read_volatile() and write_volatile(), using C's READ_ONCE() and
    WRITE_ONCE() macros ensures complete consistency with the kernel
    memory model.
    
    Signed-off-by: FUJITA Tomonori <fujita.tomonori@gmail.com>

--- rust/helpers/atomic_ext.c
+++ rust/helpers/atomic_ext.c
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <asm/barrier.h>
+#include <asm/rwonce.h>
+
+s8 rust_helper_atomic_i8_load(s8 *ptr)
+{
+	return READ_ONCE(*ptr);
+}
 
 s8 rust_helper_atomic_i8_load_acquire(s8 *ptr)
 {
 	return smp_load_acquire(ptr);
+}
+
+s16 rust_helper_atomic_i16_load(s16 *ptr)
+{
+	return READ_ONCE(*ptr);
 }
 
 s16 rust_helper_atomic_i16_load_acquire(s16 *ptr)
@@ -12,9 +23,19 @@ s16 rust_helper_atomic_i16_load_acquire(s16 *ptr)
 	return smp_load_acquire(ptr);
 }
 
+void rust_helper_atomic_i8_store(s8 *ptr, s8 val)
+{
+	WRITE_ONCE(*ptr, val);
+}
+
 void rust_helper_atomic_i8_store_release(s8 *ptr, s8 val)
 {
 	smp_store_release(ptr, val);
+}
+
+void rust_helper_atomic_i16_store(s16 *ptr, s16 val)
+{
+	WRITE_ONCE(*ptr, val);
 }
 
 void rust_helper_atomic_i16_store_release(s16 *ptr, s16 val)
