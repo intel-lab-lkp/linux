f40fd29fa21eab727dbeb45b2017c0d5d54093fa
Jason A. Donenfeld <Jason@zx2c4.com>
2025-11-18 18:02:39 +0100

    array_size: introduce min_array_size() function decoration
    Clang and recent gcc support warning if they are able to prove that the
    user is passing to a function an array that is too short in size. For
    example:
    
        void blah(unsigned char herp[static 7]);
        static void schma(void)
        {
            unsigned char good[] = { 1, 2, 3, 4, 5, 6, 7 };
            unsigned char bad[] = { 1, 2, 3, 4, 5, 6 };
            blah(good);
            blah(bad);
        }
    
    The notation here, `static 7`, means that it's incorrect to pass
    anything less than 7 elements. This is section 6.7.5.3 of C99:
    
        If the keyword static also appears within the [ and ] of the array
        type derivation, then for each call to the function, the value of
        the corresponding actual argument shall provide access to the first
        element of an array with at least as many elements as specified by
        the size expression.
    
    Here is the output from gcc 15:
    
        zx2c4@thinkpad /tmp $ gcc -c a.c
        a.c: In function ‘schma’:
        a.c:9:9: warning: ‘blah’ accessing 7 bytes in a region of size 6 [-Wstringop-overflow=]
            9 |         blah(bad);
              |         ^~~~~~~~~
        a.c:9:9: note: referencing argument 1 of type ‘unsigned char[7]’
        a.c:2:6: note: in a call to function ‘blah’
            2 | void blah(unsigned char herp[static 7]);
              |      ^~~~
    
    And from clang 21:
    
        zx2c4@thinkpad /tmp $ clang -c a.c
        a.c:9:2: warning: array argument is too small; contains 6 elements, callee requires at least 7
              [-Warray-bounds]
            9 |         blah(bad);
              |         ^    ~~~
        a.c:2:25: note: callee declares array parameter as static here
            2 | void blah(unsigned char herp[static 7]);
              |                         ^   ~~~~~~~~~~
        1 warning generated.
    
    So these are covered by, variously, -Wstringop-overflow and
    -Warray-bounds.
    
    Introduce min_array_size(), so that the above code becomes slightly less
    ugly:
    
        void blah(unsigned char herp[min_array_size(7)]);
    
    Signed-off-by: Jason A. Donenfeld <Jason@zx2c4.com>

--- include/linux/array_size.h
+++ include/linux/array_size.h
@@ -10,4 +10,11 @@
  */
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]) + __must_be_array(arr))
 
+/**
+ * min_array_size - parameter decoration to hint to the compiler that the
+ *                  passed array should have at least @n elements
+ * @n: minimum number of elements, after which the compiler may warn
+ */
+#define min_array_size(n) static n
+
 #endif  /* _LINUX_ARRAY_SIZE_H */
