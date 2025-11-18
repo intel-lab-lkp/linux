0182c893afc275ee5ff480a6603352db5b7a61b2
Hsin-Te Yuan <yuanhsinte@chromium.org>
2025-11-18 15:06:53 +0800

    usb: typec: ucsi: Monitor connector change before getting connector status
    Originally, the notification for connector change will be enabled after
    the first read of the connector status. Therefore, if the event happens
    during this window, it will be missing and make the status unsynced.
    
    Enable the notification for connector change before getting the
    connector status to ensure the status is synced.
    
    Signed-off-by: Hsin-Te Yuan <yuanhsinte@chromium.org>

--- drivers/usb/typec/ucsi/ucsi.c
+++ drivers/usb/typec/ucsi/ucsi.c
@@ -1565,7 +1565,7 @@ static int ucsi_register_port(struct ucsi *ucsi, struct ucsi_connector *con)
 	struct typec_capability *cap = &con->typec_cap;
 	enum typec_accessory *accessory = cap->accessory;
 	enum usb_role u_role = USB_ROLE_NONE;
-	u64 command;
+	u64 command, ntfy;
 	char *name;
 	int ret;
 
@@ -1658,6 +1658,14 @@ static int ucsi_register_port(struct ucsi *ucsi, struct ucsi_connector *con)
 			con->num);
 		goto out;
 	}
+
+	/* Enable the notification for connector change before getting the connector status */
+	ntfy = UCSI_ENABLE_NTFY_CMD_COMPLETE | UCSI_ENABLE_NTFY_ERROR | UCSI_ENABLE_NTFY_CONNECTOR_CHANGE;
+	command = UCSI_SET_NOTIFICATION_ENABLE | ntfy;
+	ret = ucsi_send_command(ucsi, command, NULL, 0);
+	if (ret < 0)
+		dev_warn(ucsi->dev, "con%d: failed to enable the notification for connector change\n",
+			 con->num);
 
 	/* Get the status */
 	ret = ucsi_get_connector_status(con, false);
