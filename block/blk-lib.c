a3c290644062b7e8b3ab2bd30a8380c0bb9cbe7b
Chaitanya Kulkarni <ckulkarnilinux@gmail.com>
2025-11-17 23:42:43 -0800

    block: change __blkdev_issue_discard() return type to void
    __blkdev_issue_discard() always returns 0, making all error checking
    at call sites dead code. The function simply stops allocating bios
    and returns 0.
    
    Discard operations are advisory/optimization, not critical. Some callers
    have dead error checking code expecting wrong return codes such as
    -ENOTSUPP when 0 is only returned.
    
    This patch changes __blkdev_issue_discard() return type to void and
    removes dead error checking code from all call sites:
    
    * Block layer:
      blk-lib.c: Remove return value, update blkdev_issue_discard() caller
    
    * Device mapper:
      dm-thin.c: Change issue_discard() to void, update both callers
      md.c: Simplify conditional to just check for NULL bio
    
    * NVMe target:
      io-cmd-bdev.c: Remove dead error handling and error_slba assignment
    
    * Filesystems:
      f2fs/segment.c: Preserve fault injection
      xfs/xfs_discard.c: Update both xfs_discard_extents() and
      xfs_discard_rtdev_extents() to remove dead error checks
    
    Signed-off-by: Chaitanya Kulkarni <ckulkarnilinux@gmail.com>

--- block/blk-lib.c
+++ block/blk-lib.c
@@ -60,7 +60,7 @@ struct bio *blk_alloc_discard_bio(struct block_device *bdev,
 	return bio;
 }
 
-int __blkdev_issue_discard(struct block_device *bdev, sector_t sector,
+void __blkdev_issue_discard(struct block_device *bdev, sector_t sector,
 		sector_t nr_sects, gfp_t gfp_mask, struct bio **biop)
 {
 	struct bio *bio;
@@ -68,7 +68,6 @@ int __blkdev_issue_discard(struct block_device *bdev, sector_t sector,
 	while ((bio = blk_alloc_discard_bio(bdev, &sector, &nr_sects,
 			gfp_mask)))
 		*biop = bio_chain_and_submit(*biop, bio);
-	return 0;
 }
 EXPORT_SYMBOL(__blkdev_issue_discard);
 
@@ -90,8 +89,8 @@ int blkdev_issue_discard(struct block_device *bdev, sector_t sector,
 	int ret;
 
 	blk_start_plug(&plug);
-	ret = __blkdev_issue_discard(bdev, sector, nr_sects, gfp_mask, &bio);
-	if (!ret && bio) {
+	__blkdev_issue_discard(bdev, sector, nr_sects, gfp_mask, &bio);
+	if (bio) {
 		ret = submit_bio_wait(bio);
 		if (ret == -EOPNOTSUPP)
 			ret = 0;
