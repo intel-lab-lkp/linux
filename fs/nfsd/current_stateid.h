b0f548460eed3e63056deda57c0242be9e9e46b5
NeilBrown <neil@brown.name>
2025-11-19 14:28:56 +1100

    nfsd: discard current_stateid.h
    current_stateid.h no longer contains anything useful.  So remove it.
    
    Reviewed-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: NeilBrown <neil@brown.name>

--- fs/nfsd/current_stateid.h
+++
@@ -1,8 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#ifndef _NFSD4_CURRENT_STATE_H
-#define _NFSD4_CURRENT_STATE_H
-
-#include "state.h"
-#include "xdr4.h"
-
-#endif   /* _NFSD4_CURRENT_STATE_H */
