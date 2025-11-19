7bba4e8aad22bafe6f30202ca3193d718dc71f9f
Luca Ceresoli <luca.ceresoli@bootlin.com>
2025-11-19 14:05:39 +0100

    drm/bridge: imx8qxp-pixel-combiner: use devm_drm_of_find_bridge() to put the next bridge
    This driver obtains a bridge pointer from of_drm_find_bridge() in the probe
    function and stores it until driver removal. of_drm_find_bridge() is
    deprecated. Move to devm_drm_of_find_bridge() which puts the bridge
    reference on remove or on probe failure.
    
    Signed-off-by: Luca Ceresoli <luca.ceresoli@bootlin.com>

--- drivers/gpu/drm/bridge/imx/imx8qxp-pixel-combiner.c
+++ drivers/gpu/drm/bridge/imx/imx8qxp-pixel-combiner.c
@@ -326,7 +326,7 @@ static int imx8qxp_pc_bridge_probe(struct platform_device *pdev)
 			goto free_child;
 		}
 
-		ch->next_bridge = of_drm_find_bridge(remote);
+		ch->next_bridge = devm_drm_of_find_bridge(dev, remote);
 		if (!ch->next_bridge) {
 			of_node_put(remote);
 			ret = -EPROBE_DEFER;
