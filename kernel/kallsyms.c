914e1aac389a721cdec293731b919fe35aaa7503
Eugen Hristev <eugen.hristev@linaro.org>
2025-11-19 17:44:16 +0200

    kallsyms: Annotate static information into meminspect
    Annotate vital static information into meminspect:
     - kallsysms_num_syms
     - kallsyms_relative_base
     - kallsysms_offsets
     - kallsysms_names
     - kallsyms_token_table
     - kallsyms_token_index
     - kallsyms_markers
     - kallsyms_seqs_of_names
    
    Information on these variables is stored into inspection table.
    
    Signed-off-by: Eugen Hristev <eugen.hristev@linaro.org>

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -31,8 +31,18 @@
 #include <linux/kernel.h>
 #include <linux/bsearch.h>
 #include <linux/btf_ids.h>
+#include <linux/meminspect.h>
 
 #include "kallsyms_internal.h"
+
+MEMINSPECT_SIMPLE_ENTRY(kallsyms_num_syms);
+MEMINSPECT_SIMPLE_ENTRY(kallsyms_relative_base);
+MEMINSPECT_AREA_ENTRY(kallsyms_offsets, sizeof(void *));
+MEMINSPECT_AREA_ENTRY(kallsyms_names, sizeof(void *));
+MEMINSPECT_AREA_ENTRY(kallsyms_token_table, sizeof(void *));
+MEMINSPECT_AREA_ENTRY(kallsyms_token_index, sizeof(void *));
+MEMINSPECT_AREA_ENTRY(kallsyms_markers, sizeof(void *));
+MEMINSPECT_AREA_ENTRY(kallsyms_seqs_of_names, sizeof(void *));
 
 /*
  * Expand a compressed symbol data into the resulting uncompressed string,
