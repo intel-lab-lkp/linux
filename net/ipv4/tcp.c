8effa0db2d931c5b8bee8ca98e754f61f0533f7e
Shi Hao <i.shihao.999@gmail.com>
2025-11-19 22:52:39 +0530

    net: ipv4: fix spelling typo in comment
    Replace "splitted" with "split" in comment.
    
    Signed-off-by: Shi Hao <i.shihao.999@gmail.com>

--- net/ipv4/tcp.c
+++ net/ipv4/tcp.c
@@ -1601,7 +1601,7 @@ struct sk_buff *tcp_recv_skb(struct sock *sk, u32 seq, u32 *off)
 			return skb;
 		}
 		/* This looks weird, but this can happen if TCP collapsing
-		 * splitted a fat GRO packet, while we released socket lock
+		 * split a fat GRO packet, while we released socket lock
 		 * in skb_splice_bits()
 		 */
 		tcp_eat_recv_skb(sk, skb);
