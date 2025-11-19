7044f9869e99fc3719f8f9befde2c56cc75e07da
Scott Mayhew <smayhew@redhat.com>
2025-11-19 08:32:31 -0500

    SUNRPC: Check if we need to recalculate slack estimates
    If the incoming GSS verifier is larger than what we previously recorded
    on the gss_auth, that would indicate the GSS cred/context used for that
    RPC is using a different enctype than the one used by the machine
    cred/context, and we should recalculate the slack variables accordingly.
    
    Link: https://bugs.debian.org/1120598
    Signed-off-by: Scott Mayhew <smayhew@redhat.com>

--- net/sunrpc/auth_gss/auth_gss.c
+++ net/sunrpc/auth_gss/auth_gss.c
@@ -1721,6 +1721,14 @@ gss_validate(struct rpc_task *task, struct xdr_stream *xdr)
 	if (maj_stat)
 		goto bad_mic;
 
+	/*
+	 * Normally we only recalculate the slack variables once after
+	 * creating a new gss_auth, but we should also do it if the incoming
+	 * verifier has a larger size than what was previously recorded.
+	 */
+	if (cred->cr_auth->au_verfsize < (XDR_QUADLEN(len) + 2))
+		__set_bit(RPCAUTH_AUTH_UPDATE_SLACK, &cred->cr_auth->au_flags);
+
 	/* We leave it to unwrap to calculate au_rslack. For now we just
 	 * calculate the length of the verifier: */
 	if (test_bit(RPCAUTH_AUTH_UPDATE_SLACK, &cred->cr_auth->au_flags))
