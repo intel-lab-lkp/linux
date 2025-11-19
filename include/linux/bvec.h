215fce9e9be307c96365310e6bd543f7da9d69e2
David Laight <david.laight.linux@gmail.com>
2025-11-19 22:41:27 +0000

    block: bvec.h: use min() instead of min_t()
    min_t(unsigned int, a, b) casts an 'unsigned long' to 'unsigned int'.
    Use min(a, b) instead as it promotes any 'unsigned int' to 'unsigned long'
    and so cannot discard significant bits.
    
    In this case the 'unsigned long' value is small enough that the result
    is ok.
    
    Detected by an extra check added to min_t().
    
    Signed-off-by: David Laight <david.laight.linux@gmail.com>

--- include/linux/bvec.h
+++ include/linux/bvec.h
@@ -219,8 +219,7 @@ static inline void bvec_advance(const struct bio_vec *bvec,
 		bv->bv_page = bvec->bv_page + (bvec->bv_offset >> PAGE_SHIFT);
 		bv->bv_offset = bvec->bv_offset & ~PAGE_MASK;
 	}
-	bv->bv_len = min_t(unsigned int, PAGE_SIZE - bv->bv_offset,
-			   bvec->bv_len - iter_all->done);
+	bv->bv_len = min(PAGE_SIZE - bv->bv_offset, bvec->bv_len - iter_all->done);
 	iter_all->done += bv->bv_len;
 
 	if (iter_all->done == bvec->bv_len) {
