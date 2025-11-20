5981c33ce616317c8956d879a86b4672e89f2cb6
Eric Biggers <ebiggers@kernel.org>
2025-11-19 16:36:52 -0800

    lib/crypto: tests: Add ML-DSA-65 test cases
    Add test vectors and test cases for ML-DSA-65.
    
    (This needs some work.  For one, there are no negative tests yet.)
    
    Signed-off-by: Eric Biggers <ebiggers@kernel.org>

--- lib/crypto/tests/mldsa_kunit.c
+++ lib/crypto/tests/mldsa_kunit.c
@@ -25,6 +25,9 @@ struct mldsa_testvector {
 static const struct mldsa_testvector mldsa_44_testvectors[] = {
 #include "mldsa_pure_rejection_vectors_44.h"
 };
+static const struct mldsa_testvector mldsa_65_testvectors[] = {
+#include "mldsa_pure_rejection_vectors_65.h"
+};
 
 static void do_mldsa_and_assert_success(struct kunit *test, enum mldsa_alg alg,
 					const struct mldsa_testvector *tv)
@@ -45,6 +48,12 @@ static void test_mldsa44(struct kunit *test)
 {
 	test_mldsa(test, MLDSA44, mldsa_44_testvectors,
 		   ARRAY_SIZE(mldsa_44_testvectors));
+}
+
+static void test_mldsa65(struct kunit *test)
+{
+	test_mldsa(test, MLDSA65, mldsa_65_testvectors,
+		   ARRAY_SIZE(mldsa_65_testvectors));
 }
 
 static void benchmark_mldsa(struct kunit *test, enum mldsa_alg alg,
@@ -75,9 +84,16 @@ static void benchmark_mldsa44(struct kunit *test)
 	benchmark_mldsa(test, MLDSA44, &mldsa_44_testvectors[0]);
 }
 
+static void benchmark_mldsa65(struct kunit *test)
+{
+	benchmark_mldsa(test, MLDSA65, &mldsa_65_testvectors[0]);
+}
+
 static struct kunit_case mldsa_kunit_cases[] = {
 	KUNIT_CASE(test_mldsa44),
+	KUNIT_CASE(test_mldsa65),
 	KUNIT_CASE(benchmark_mldsa44),
+	KUNIT_CASE(benchmark_mldsa65),
 	{},
 };
 
