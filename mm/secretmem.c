9545c1e07649ed65bbfb17733dc610bf64f0236e
Christian Brauner <brauner@kernel.org>
2025-11-20 23:32:24 +0100

    secretmem: convert memfd_secret() to FD_PREPARE()
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- mm/secretmem.c
+++ mm/secretmem.c
@@ -224,9 +224,6 @@ err_free_inode:
 
 SYSCALL_DEFINE1(memfd_secret, unsigned int, flags)
 {
-	struct file *file;
-	int fd, err;
-
 	/* make sure local flags do not confict with global fcntl.h */
 	BUILD_BUG_ON(SECRETMEM_FLAGS_MASK & O_CLOEXEC);
 
@@ -238,22 +235,12 @@ SYSCALL_DEFINE1(memfd_secret, unsigned int, flags)
 	if (atomic_read(&secretmem_users) < 0)
 		return -ENFILE;
 
-	fd = get_unused_fd_flags(flags & O_CLOEXEC);
-	if (fd < 0)
-		return fd;
+	FD_PREPARE(fdf, flags & O_CLOEXEC, secretmem_file_create(flags)) {
+		if (fd_prepare_failed(fdf))
+			return fd_prepare_error(fdf);
 
-	file = secretmem_file_create(flags);
-	if (IS_ERR(file)) {
-		err = PTR_ERR(file);
-		goto err_put_fd;
+		return fd_publish(fdf);
 	}
-
-	fd_install(fd, file);
-	return fd;
-
-err_put_fd:
-	put_unused_fd(fd);
-	return err;
 }
 
 static int secretmem_init_fs_context(struct fs_context *fc)
