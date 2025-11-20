5ea1f34112bd905944c59c1ede58a26c3ffcf6ea
Cosmin Ratiu <cratiu@nvidia.com>
2025-11-20 15:09:18 +0200

    devlink: Add parent dev to devlink API
    Upcoming changes to the rate commands need the parent devlink specified.
    This change adds a nested 'parent-dev' attribute to the API and helpers
    to obtain and put a reference to the parent devlink instance in
    info->user_ptr[1].
    
    To avoid deadlocks, the parent devlink is unlocked before obtaining the
    main devlink instance that is the target of the request.
    A reference to the parent is kept until the end of the request to avoid
    it suddenly disappearing.
    
    This means that this reference is of limited use without additional
    protection.
    
    Signed-off-by: Cosmin Ratiu <cratiu@nvidia.com>
    Reviewed-by: Carolina Jubran <cjubran@nvidia.com>
    Reviewed-by: Jiri Pirko <jiri@nvidia.com>
    Signed-off-by: Tariq Toukan <tariqt@nvidia.com>

--- include/uapi/linux/devlink.h
+++ include/uapi/linux/devlink.h
@@ -638,6 +638,8 @@ enum devlink_attr {
 
 	DEVLINK_ATTR_HEALTH_REPORTER_BURST_PERIOD,	/* u64 */
 
+	DEVLINK_ATTR_PARENT_DEV,		/* nested */
+
 	/* Add new attributes above here, update the spec in
 	 * Documentation/netlink/specs/devlink.yaml and re-generate
 	 * net/devlink/netlink_gen.c.
