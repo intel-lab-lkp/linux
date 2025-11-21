7afd52be9d45b22e5c066d47ffbd2808d7c5eaf6
Josef Bacik <josef@toxicpanda.com>
2025-11-21 09:59:22 -0500

    btrfs: remove useless smp_mb in start_transaction
    trans->state is protected by a spin_lock() and there's no mb when we set
    it, simply the spin_unlock(). The correct pairing with that is
    smp_load_acquire(), so replace the smp_mb with the appropriate helper.
    
    Signed-off-by: Josef Bacik <josef@toxicpanda.com>

--- fs/btrfs/transaction.c
+++ fs/btrfs/transaction.c
@@ -739,8 +739,7 @@ again:
 	INIT_LIST_HEAD(&h->new_bgs);
 	btrfs_init_metadata_block_rsv(fs_info, &h->delayed_rsv, BTRFS_BLOCK_RSV_DELOPS);
 
-	smp_mb();
-	if (cur_trans->state >= TRANS_STATE_COMMIT_START &&
+	if (smp_load_acquire(&cur_trans->state) >= TRANS_STATE_COMMIT_START &&
 	    may_wait_transaction(fs_info, type)) {
 		current->journal_info = h;
 		btrfs_commit_transaction(h);
