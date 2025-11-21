41b0d9e59f92d977730031e57612ae0ddbeb7996
Christian Brauner <brauner@kernel.org>
2025-11-21 19:01:11 +0100

    spufs: convert spufs_context_open() to FD_PREPARE()
    Signed-off-by: Christian Brauner <brauner@kernel.org>

--- arch/powerpc/platforms/cell/spufs/inode.c
+++ arch/powerpc/platforms/cell/spufs/inode.c
@@ -268,21 +268,13 @@ spufs_mkdir(struct inode *dir, struct dentry *dentry, unsigned int flags,
 static int spufs_context_open(const struct path *path)
 {
 	int ret;
-	struct file *filp;
 
-	ret = get_unused_fd_flags(0);
-	if (ret < 0)
+	FD_PREPARE(fdf, 0, dentry_open(path, O_RDONLY, current_cred()));
+	ret = ACQUIRE_ERR(fd_prepare, &fdf);
+	if (ret)
 		return ret;
-
-	filp = dentry_open(path, O_RDONLY, current_cred());
-	if (IS_ERR(filp)) {
-		put_unused_fd(ret);
-		return PTR_ERR(filp);
-	}
-
-	filp->f_op = &spufs_context_fops;
-	fd_install(ret, filp);
-	return ret;
+	fd_prepare_file(fdf)->f_op = &spufs_context_fops;
+	return fd_publish(fdf);
 }
 
 static struct spu_context *
