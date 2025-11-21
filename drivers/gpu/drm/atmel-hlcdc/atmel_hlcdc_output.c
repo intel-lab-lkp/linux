524bfe4ade6dbedc715d2f1b097bcb594e7808c6
Ludovic Desroches <ludovic.desroches@microchip.com>
2025-11-21 16:04:37 +0100

    drm/atmel-hlcdc: use devm_drm_of_get_bridge()
    Get rid of drm_of_find_panel_or_bridge() as it is deprecated and use
    devm_drm_of_get_bridge() instead.
    
    Signed-off-by: Ludovic Desroches <ludovic.desroches@microchip.com>
    Reviewed-by: Manikandan Muralidharan <manikandan.m@microchip.com>

--- drivers/gpu/drm/atmel-hlcdc/atmel_hlcdc_output.c
+++ drivers/gpu/drm/atmel-hlcdc/atmel_hlcdc_output.c
@@ -69,16 +69,14 @@ static int atmel_hlcdc_attach_endpoint(struct drm_device *dev, int endpoint)
 {
 	struct atmel_hlcdc_rgb_output *output;
 	struct device_node *ep;
-	struct drm_panel *panel;
 	struct drm_bridge *bridge;
 	struct atmel_hlcdc_dc *dc = dev->dev_private;
 	struct drm_crtc *crtc = dc->crtc;
 	int ret;
 
-	ret = drm_of_find_panel_or_bridge(dev->dev->of_node, 0, endpoint,
-					  &panel, &bridge);
-	if (ret)
-		return ret;
+	bridge = devm_drm_of_get_bridge(dev->dev, dev->dev->of_node, 0, endpoint);
+	if (IS_ERR(bridge))
+		return PTR_ERR(bridge);
 
 	output = drmm_simple_encoder_alloc(dev, struct atmel_hlcdc_rgb_output,
 					   encoder, DRM_MODE_ENCODER_NONE);
@@ -97,22 +95,12 @@ static int atmel_hlcdc_attach_endpoint(struct drm_device *dev, int endpoint)
 	}
 
 
-
-	if (panel) {
-		bridge = drm_panel_bridge_add_typed(panel,
-						    DRM_MODE_CONNECTOR_Unknown);
-		if (IS_ERR(bridge))
-			return PTR_ERR(bridge);
-	}
 	output->encoder.possible_crtcs = drm_crtc_mask(crtc);
 
 	if (bridge) {
 		ret = drm_bridge_attach(&output->encoder, bridge, NULL, 0);
 		if (!ret)
 			return 0;
-
-		if (panel)
-			drm_panel_bridge_remove(bridge);
 	}
 
 	return ret;
