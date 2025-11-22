4bf6ddd0c5743b3a854890674f19e37b8f1e4bf4
Costa Shulyupin <costa.shul@redhat.com>
2025-11-22 20:09:57 +0200

    tools/rtla: Add common_usage()
    The rtla tools (osnoise and timerlat, both hist and top) have
    significant code duplication in their usage() functions. Each tool
    implements its own version of the same help text formatting and option
    descriptions, leading to maintenance overhead and inconsistencies.
    Documentation/tools/rtla/common_options.rst lists 14 common options.
    
    Add common_usage() infrastructure to consolidate help formatting.
    Subsequent patches will extend this to handle other common options.
    
    The refactored output is almost identical to the original, with the
    following changes:
    - add square brackets to specify optionality: `usage: [rtla] ...`
    - remove `-q` from timerlat hist because hist tools don't support it
    - minor spacing
    
    Signed-off-by: Costa Shulyupin <costa.shul@redhat.com>

--- tools/tracing/rtla/src/common.h
+++ tools/tracing/rtla/src/common.h
@@ -156,3 +156,6 @@ bool osnoise_trace_is_off(struct osnoise_tool *tool, struct osnoise_tool *record
 int common_apply_config(struct osnoise_tool *tool, struct common_params *params);
 int top_main_loop(struct osnoise_tool *tool);
 int hist_main_loop(struct osnoise_tool *tool);
+
+void common_usage(const char *tool, const char *mode,
+		  const char *desc, const char * const *start_msgs, const char * const *opt_msgs);
