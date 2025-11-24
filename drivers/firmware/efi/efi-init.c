154b945293b992cd8b7a50acc5184c032deddd0f
Thomas Zimmermann <tzimmermann@suse.de>
2025-11-24 17:40:20 +0100

    efi: Support EDID information
    Add LINUX_EFI_PRIMARY_DISPLAY_TABLE_GUID to the list of config-table
    UUIDs. Read sysfb_primary_display from the entry. The UUID has been
    generated with uuidgen.
    
    Still support LINUX_EFI_SCREEN_INFO_TABLE_GUID as fallback in case an
    older boot loader invokes the kernel.
    
    If CONFIG_FIRMWARE_EDID=n, EDID information is disabled.
    
    Make the Kconfig symbol CONFIG_FIRMWARE_EDID available with EFI. Setting
    the value to 'n' disables EDID support.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>

--- drivers/firmware/efi/efi-init.c
+++ drivers/firmware/efi/efi-init.c
@@ -23,6 +23,7 @@
 
 #include <asm/efi.h>
 
+unsigned long __initdata primary_display_table = EFI_INVALID_TABLE_ADDR;
 unsigned long __initdata screen_info_table = EFI_INVALID_TABLE_ADDR;
 
 static int __init is_memory(efi_memory_desc_t *md)
@@ -67,7 +68,18 @@ EXPORT_SYMBOL_GPL(sysfb_primary_display);
 
 static void __init init_primary_display(void)
 {
-	if (screen_info_table != EFI_INVALID_TABLE_ADDR) {
+	if (primary_display_table != EFI_INVALID_TABLE_ADDR) {
+		struct sysfb_display_info *dpy =
+			early_memremap(primary_display_table, sizeof(*dpy));
+
+		if (!dpy) {
+			pr_err("Could not map primary_display config table\n");
+			return;
+		}
+		sysfb_primary_display = *dpy;
+		memset(dpy, 0, sizeof(*dpy));
+		early_memunmap(dpy, sizeof(*dpy));
+	} else if (screen_info_table != EFI_INVALID_TABLE_ADDR) {
 		struct screen_info *si = early_memremap(screen_info_table, sizeof(*si));
 
 		if (!si) {
