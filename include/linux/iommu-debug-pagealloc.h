cfcf02d4f7d28c0db8361d4b7040c1f5ff092e20
Mostafa Saleh <smostafa@google.com>
2025-11-24 20:08:09 +0000

    drivers/iommu: Add calls for IOMMU_DEBUG_PAGEALLOC
    Add calls for the new iommu debug config IOMMU_DEBUG_PAGEALLOC:
    - iommu_debug_init: Enable the debug mode if configured by the user.
    - iommu_debug_map: Track iommu pages mapped, using physical address.
    - iommu_debug_unmap_begin: Track start of iommu unmap operation, with
      IOVA and size.
    - iommu_debug_unmap_end: Track the end of unmap operation, passing the
      actual unmapped size versus the tracked one at unmap_begin.
    
    We have to do the unmap_begin/end as once pages are unmapped we lose
    the information of the physical address.
    This is racy, but the API is racy by construction as it uses refcounts
    and doesn't attempt to lock/synchronize with the IOMMU API as that will
    be costly, meaning that possibility of false negative exists.
    
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- include/linux/iommu-debug-pagealloc.h
+++ include/linux/iommu-debug-pagealloc.h
@@ -8,9 +8,65 @@
 #ifndef __LINUX_IOMMU_DEBUG_PAGEALLOC_H
 #define __LINUX_IOMMU_DEBUG_PAGEALLOC_H
 
+struct iommu_domain;
+
 #ifdef CONFIG_IOMMU_DEBUG_PAGEALLOC
 
+DECLARE_STATIC_KEY_FALSE(iommu_debug_initialized);
+
 extern struct page_ext_operations page_iommu_debug_ops;
+
+void __iommu_debug_map(struct iommu_domain *domain, phys_addr_t phys,
+		       size_t size);
+void __iommu_debug_unmap_begin(struct iommu_domain *domain,
+			       unsigned long iova, size_t size);
+void __iommu_debug_unmap_end(struct iommu_domain *domain,
+			     unsigned long iova, size_t size, size_t unmapped);
+
+static inline void iommu_debug_map(struct iommu_domain *domain,
+				   phys_addr_t phys, size_t size)
+{
+	if (static_branch_unlikely(&iommu_debug_initialized))
+		__iommu_debug_map(domain, phys, size);
+}
+
+static inline void iommu_debug_unmap_begin(struct iommu_domain *domain,
+					   unsigned long iova, size_t size)
+{
+	if (static_branch_unlikely(&iommu_debug_initialized))
+		__iommu_debug_unmap_begin(domain, iova, size);
+}
+
+static inline void iommu_debug_unmap_end(struct iommu_domain *domain,
+					 unsigned long iova, size_t size,
+					 size_t unmapped)
+{
+	if (static_branch_unlikely(&iommu_debug_initialized))
+		__iommu_debug_unmap_end(domain, iova, size, unmapped);
+}
+
+void iommu_debug_init(void);
+
+#else
+static inline void iommu_debug_map(struct iommu_domain *domain,
+				   phys_addr_t phys, size_t size)
+{
+}
+
+static inline void iommu_debug_unmap_begin(struct iommu_domain *domain,
+					   unsigned long iova, size_t size)
+{
+}
+
+static inline void iommu_debug_unmap_end(struct iommu_domain *domain,
+					 unsigned long iova, size_t size,
+					 size_t unmapped)
+{
+}
+
+static inline void iommu_debug_init(void)
+{
+}
 
 #endif /* CONFIG_IOMMU_DEBUG_PAGEALLOC */
 
