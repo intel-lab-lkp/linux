d9add4ae063e7d45894446be0e45af6cbaf74731
Mike Rapoport (Microsoft) <rppt@kernel.org>
2025-11-25 20:38:37 +0200

    userfaultfd, shmem: use a VMA callback to handle UFFDIO_CONTINUE
    When userspace resolves a page fault in a shmem VMA with UFFDIO_CONTINUE
    it needs to get a folio that already exists in the pagecache backing
    that VMA.
    
    Instead of using shmem_get_folio() for that, add a get_folio() method to
    'struct vm_operations_struct' that will return a folio if it exists in
    the VMA's pagecache at given pgoff.
    
    Implement get_folio() method for shmem and slightly refactor
    userfaultfd's mfill_atomic() and mfill_atomic_pte_continue() to support
    this new API.
    
    Signed-off-by: Mike Rapoport (Microsoft) <rppt@kernel.org>

--- mm/shmem.c
+++ mm/shmem.c
@@ -3263,6 +3263,18 @@ out_unacct_blocks:
 	shmem_inode_unacct_blocks(inode, 1);
 	return ret;
 }
+
+static struct folio *shmem_get_folio_noalloc(struct inode *inode, pgoff_t pgoff)
+{
+	struct folio *folio;
+	int err;
+
+	err = shmem_get_folio(inode, pgoff, 0, &folio, SGP_NOALLOC);
+	if (err)
+		return ERR_PTR(err);
+
+	return folio;
+}
 #endif /* CONFIG_USERFAULTFD */
 
 #ifdef CONFIG_TMPFS
@@ -5295,6 +5307,9 @@ static const struct vm_operations_struct shmem_vm_ops = {
 	.set_policy     = shmem_set_policy,
 	.get_policy     = shmem_get_policy,
 #endif
+#ifdef CONFIG_USERFAULTFD
+	.get_folio	= shmem_get_folio_noalloc,
+#endif
 };
 
 static const struct vm_operations_struct shmem_anon_vm_ops = {
@@ -5303,6 +5318,9 @@ static const struct vm_operations_struct shmem_anon_vm_ops = {
 #ifdef CONFIG_NUMA
 	.set_policy     = shmem_set_policy,
 	.get_policy     = shmem_get_policy,
+#endif
+#ifdef CONFIG_USERFAULTFD
+	.get_folio	= shmem_get_folio_noalloc,
 #endif
 };
 
