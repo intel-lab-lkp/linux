f3c84fb3d2e5fc4030b0f720917371d15409bda1
Dan Carpenter <dan.carpenter@linaro.org>
2025-11-25 16:54:03 +0300

    nvdimm: Prevent integer overflow in ramdax_get_config_data()
    The "cmd->in_offset" variable comes from the user via the __nd_ioctl()
    function.  The problem is that the "cmd->in_offset + cmd->in_length"
    addition could have an integer wrapping issue if cmd->in_offset is close
    to UINT_MAX .  The "cmd->in_length" variable has been capped, but the
    "cmd->in_offset" variable has not.  Both of these variables are type u32.
    
    Fixes: 43bc0aa19a21 ("nvdimm: allow exposing RAM carveouts as NVDIMM DIMM devices")
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

--- drivers/nvdimm/ramdax.c
+++ drivers/nvdimm/ramdax.c
@@ -143,7 +143,7 @@ static int ramdax_get_config_data(struct nvdimm *nvdimm, int buf_len,
 		return -EINVAL;
 	if (struct_size(cmd, out_buf, cmd->in_length) > buf_len)
 		return -EINVAL;
-	if (cmd->in_offset + cmd->in_length > LABEL_AREA_SIZE)
+	if (size_add(cmd->in_offset, cmd->in_length) > LABEL_AREA_SIZE)
 		return -EINVAL;
 
 	memcpy(cmd->out_buf, dimm->label_area + cmd->in_offset, cmd->in_length);
