24f2cc22f6843940419a277adf1ac756a10177a8
Stanislav Kinsburskii <skinsburskii@linux.microsoft.com>
2025-11-26 02:09:28 +0000

    Drivers: hv: Add support for movable memory regions
    Introduce support for movable memory regions in the Hyper-V root partition
    driver to improve memory management flexibility and enable advanced use
    cases such as dynamic memory remapping.
    
    Mirror the address space between the Linux root partition and guest VMs
    using HMM. The root partition owns the memory, while guest VMs act as
    devices with page tables managed via hypercalls. MSHV handles VP intercepts
    by invoking hmm_range_fault() and updating SLAT entries. When memory is
    reclaimed, HMM invalidates the relevant regions, prompting MSHV to clear
    SLAT entries; guest VMs will fault again on access.
    
    Integrate mmu_interval_notifier for movable regions, implement handlers for
    HMM faults and memory invalidation, and update memory region mapping logic
    to support movable regions.
    
    While MMU notifiers are commonly used in virtualization drivers, this
    implementation leverages HMM (Heterogeneous Memory Management) for its
    specialized functionality. HMM provides a framework for mirroring,
    invalidation, and fault handling, reducing boilerplate and improving
    maintainability compared to generic MMU notifiers.
    
    Signed-off-by: Stanislav Kinsburskii <skinsburskii@linux.microsoft.com>

--- drivers/hv/mshv_root.h
+++ drivers/hv/mshv_root.h
@@ -15,6 +15,7 @@
 #include <linux/hashtable.h>
 #include <linux/dev_printk.h>
 #include <linux/build_bug.h>
+#include <linux/mmu_notifier.h>
 #include <uapi/linux/mshv.h>
 
 /*
@@ -70,6 +71,12 @@ do { \
 #define vp_info(v, fmt, ...)	vp_devprintk(info, v, fmt, ##__VA_ARGS__)
 #define vp_dbg(v, fmt, ...)	vp_devprintk(dbg, v, fmt, ##__VA_ARGS__)
 
+enum mshv_region_type {
+	MSHV_REGION_TYPE_MEM_PINNED,
+	MSHV_REGION_TYPE_MEM_MOVABLE,
+	MSHV_REGION_TYPE_MMIO
+};
+
 struct mshv_mem_region {
 	struct hlist_node hnode;
 	struct kref refcount;
@@ -77,11 +84,10 @@ struct mshv_mem_region {
 	u64 start_gfn;
 	u64 start_uaddr;
 	u32 hv_map_flags;
-	struct {
-		u64 range_pinned: 1;
-		u64 reserved:	 63;
-	} flags;
 	struct mshv_partition *partition;
+	enum mshv_region_type type;
+	struct mmu_interval_notifier mni;
+	struct mutex mutex;	/* protects region pages remapping */
 	struct page *pages[];
 };
 
@@ -324,5 +330,8 @@ void mshv_region_invalidate(struct mshv_mem_region *region);
 int mshv_region_pin(struct mshv_mem_region *region);
 void mshv_region_put(struct mshv_mem_region *region);
 int mshv_region_get(struct mshv_mem_region *region);
+bool mshv_region_handle_gfn_fault(struct mshv_mem_region *region, u64 gfn);
+void mshv_region_movable_fini(struct mshv_mem_region *region);
+bool mshv_region_movable_init(struct mshv_mem_region *region);
 
 #endif /* _MSHV_ROOT_H_ */
