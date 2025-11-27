c6d761b75ce6193d7600eee8fd7f433165550f86
Stefan Hajnoczi <stefanha@redhat.com>
2025-11-27 10:54:22 -0500

    nvme: reject invalid pr_read_keys() num_keys values
    The pr_read_keys() interface has a u32 num_keys parameter. The NVMe
    Reservation Report command has a u32 maximum length. Reject num_keys
    values that are too large to fit.
    
    This will become important when pr_read_keys() is exposed to untrusted
    userspace via an <linux/pr.h> ioctl.
    
    Signed-off-by: Stefan Hajnoczi <stefanha@redhat.com>

--- drivers/nvme/host/pr.c
+++ drivers/nvme/host/pr.c
@@ -233,6 +233,10 @@ static int nvme_pr_read_keys(struct block_device *bdev,
 	int ret, i;
 	bool eds;
 
+	/* Check that keys fit into u32 rse_len */
+	if (num_keys > (U32_MAX - sizeof(*rse)) / sizeof(rse->regctl_eds[0]))
+		return -EINVAL;
+
 	/*
 	 * Assume we are using 128-bit host IDs and allocate a buffer large
 	 * enough to get enough keys to fill the return keys buffer.
