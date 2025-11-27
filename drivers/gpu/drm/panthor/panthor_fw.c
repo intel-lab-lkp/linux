8aa6fdfcbc3e17306fd839d4602163ad41bcad12
Ashley Smith <ashley.smith@collabora.com>
2025-11-27 11:44:02 +0000

    drm/panthor: Enable timestamp propagation
    Set the GLB_COUNTER_EN bit to enable coherent propagation of GPU
    timestamp values to shader cores. This is a prerequisite for exposing
    device-coherent timestamp queries.
    
    Bump the version to 1.6 so userspace can detect support.
    
    v2:
    - GLB_COUNTER_EN is not a toggle bit move to panthor_fw_update_reqs
    
    Signed-off-by: Ashley Smith <ashley.smith@collabora.com>

--- drivers/gpu/drm/panthor/panthor_fw.c
+++ drivers/gpu/drm/panthor/panthor_fw.c
@@ -996,7 +996,9 @@ static void panthor_fw_init_global_iface(struct panthor_device *ptdev)
 					 GLB_IDLE_EN |
 					 GLB_IDLE;
 
-	panthor_fw_update_reqs(glb_iface, req, GLB_IDLE_EN, GLB_IDLE_EN);
+	panthor_fw_update_reqs(glb_iface, req,
+			       GLB_IDLE_EN | GLB_COUNTER_EN,
+			       GLB_IDLE_EN | GLB_COUNTER_EN);
 	panthor_fw_toggle_reqs(glb_iface, req, ack,
 			       GLB_CFG_ALLOC_EN |
 			       GLB_CFG_POWEROFF_TIMER |
