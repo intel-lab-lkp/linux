5a9bd9ef94113229c966d3da6f2fd2768fff1968
Loïc Molinari <loic.molinari@collabora.com>
2025-11-28 19:52:50 +0100

    drm/panthor: Introduce huge tmpfs mountpoint option
    Introduce the 'panthor.transparent_hugepage' boolean module parameter
    (false by default). When the parameter is set to true, a new tmpfs
    mountpoint is created and mounted using the 'huge=within_size'
    option. It's then used at GEM object creation instead of the default
    'shm_mnt' mountpoint in order to enable Transparent Hugepage (THP) for
    the object (without having to rely on a system wide parameter).
    
    v3:
    - use huge tmpfs mountpoint in drm_device
    
    v4:
    - fix builds with CONFIG_TRANSPARENT_HUGEPAGE=n
    - clean up mountpoint creation error handling
    - print negative error value
    
    v5:
    - use drm_gem_has_huge_tmp() helper
    - get rid of CONFIG_TRANSPARENT_HUGEPAGE ifdefs
    
    v9:
    - replace drm_gem_has_huge_tmp() by drm_gem_get_huge_tmp()
    
    Signed-off-by: Loïc Molinari <loic.molinari@collabora.com>
    Reviewed-by: Boris Brezillon <boris.brezillon@collabora.com>

--- drivers/gpu/drm/panthor/panthor_drv.c
+++ drivers/gpu/drm/panthor/panthor_drv.c
@@ -1559,6 +1559,7 @@ static const struct file_operations panthor_drm_driver_fops = {
 	.read = drm_read,
 	.llseek = noop_llseek,
 	.mmap = panthor_mmap,
+	.get_unmapped_area = drm_gem_get_unmapped_area,
 	.show_fdinfo = drm_show_fdinfo,
 	.fop_flags = FOP_UNSIGNED_OFFSET,
 };
@@ -1625,6 +1626,12 @@ static const struct drm_driver panthor_drm_driver = {
 	.debugfs_init = panthor_debugfs_init,
 #endif
 };
+
+#ifdef CONFIG_TRANSPARENT_HUGEPAGE
+bool panthor_transparent_hugepage;
+module_param_named(transparent_hugepage, panthor_transparent_hugepage, bool, 0400);
+MODULE_PARM_DESC(transparent_hugepage, "Use a dedicated tmpfs mount point with Transparent Hugepage enabled (false = default)");
+#endif
 
 static int panthor_probe(struct platform_device *pdev)
 {
