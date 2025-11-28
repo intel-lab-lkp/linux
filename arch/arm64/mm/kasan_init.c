679f802c62e45f18f0f62b84a9888e5da6a8259d
Baoquan He <bhe@redhat.com>
2025-11-28 11:33:13 +0800

    arch/arm64: don't initialize kasan if it's disabled
    And also need skip kasan_populate_early_vm_area_shadow() if kasan
    is disabled.
    
    Signed-off-by: Baoquan He <bhe@redhat.com>
    Cc: linux-arm-kernel@lists.infradead.org

--- arch/arm64/mm/kasan_init.c
+++ arch/arm64/mm/kasan_init.c
@@ -384,6 +384,9 @@ void __init kasan_populate_early_vm_area_shadow(void *start, unsigned long size)
 {
 	unsigned long shadow_start, shadow_end;
 
+	if (!kasan_enabled())
+		return;
+
 	if (!is_vmalloc_or_module_addr(start))
 		return;
 
@@ -397,6 +400,9 @@ void __init kasan_populate_early_vm_area_shadow(void *start, unsigned long size)
 
 void __init kasan_init(void)
 {
+	if (kasan_arg_disabled)
+		return;
+
 	kasan_init_shadow();
 	kasan_init_depth();
 	kasan_init_generic();
