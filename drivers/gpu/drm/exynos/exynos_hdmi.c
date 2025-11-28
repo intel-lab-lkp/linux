e090098facebc0023e888fa55afa54b5ab317b7d
Luca Ceresoli <luca.ceresoli@bootlin.com>
2025-11-28 17:50:32 +0100

    drm/exynos: hdmi: use devm_of_drm_get_bridge() to put the next bridge
    This driver obtains a bridge pointer from of_drm_find_bridge() in the probe
    function and stores it until driver removal. of_drm_find_bridge() is
    deprecated. Move to devm_of_drm_get_bridge() which puts the bridge
    reference on remove or on probe failure.
    
    Signed-off-by: Luca Ceresoli <luca.ceresoli@bootlin.com>

--- drivers/gpu/drm/exynos/exynos_hdmi.c
+++ drivers/gpu/drm/exynos/exynos_hdmi.c
@@ -1779,7 +1779,7 @@ static int hdmi_bridge_init(struct hdmi_context *hdata)
 		return -EINVAL;
 	}
 
-	hdata->bridge = of_drm_find_bridge(np);
+	hdata->bridge = devm_of_drm_get_bridge(dev, np);
 	of_node_put(np);
 
 	if (!hdata->bridge)
