94731ccc8408a44086525684769acbad9ad35cee
Vishnu Sankar <vishnuocv@gmail.com>
2025-11-29 09:25:31 +0900

    input: trackpoint - Enable doubletap by default on capable devices
    Enable doubletap functionality by default on TrackPoint devices that
    support it. The feature is detected using firmware ID pattern matching
    (PNP: LEN03xxx) with a deny list of incompatible devices.
    
    This provides immediate doubletap functionality without requiring
    userspace configuration. The hardware is enabled during device
    detection, while event filtering continues to be handled by the
    thinkpad_acpi driver as before.
    
    Signed-off-by: Vishnu Sankar <vishnuocv@gmail.com>
    Suggested-by: Mark Pearson <mpearson-lenovo@squebb.ca>

--- drivers/input/mouse/trackpoint.c
+++ drivers/input/mouse/trackpoint.c
@@ -393,6 +393,48 @@ static int trackpoint_reconnect(struct psmouse *psmouse)
 	return 0;
 }
 
+/* List of known incapable device PNP IDs */
+static const char * const dt_incompatible_devices[] = {
+	"LEN0304",
+	"LEN0306",
+	"LEN0317",
+	"LEN031A",
+	"LEN031B",
+	"LEN031C",
+	"LEN031D",
+};
+
+/*
+ * Checks if it's a doubletap capable device
+ * The PNP ID format is "PNP: LEN030d PNP0f13".
+ */
+static bool is_trackpoint_dt_capable(const char *pnp_id)
+{
+	const char *id_start;
+	char id[8];
+	size_t i;
+
+	if (!strstarts(pnp_id, "PNP: LEN03"))
+		return false;
+
+	/* Points to "LEN03xxxx" */
+	id_start = pnp_id + 5;
+	if (sscanf(id_start, "%7s", id) != 1)
+		return false;
+
+	/* Check if it's in the deny list */
+	for (i = 0; i < ARRAY_SIZE(dt_incompatible_devices); i++) {
+		if (strcmp(id, dt_incompatible_devices[i]) == 0)
+			return false;
+	}
+	return true;
+}
+
+static int trackpoint_set_doubletap(struct ps2dev *ps2dev, bool enable)
+{
+	return trackpoint_write(ps2dev, TP_DOUBLETAP, enable ? TP_DOUBLETAP_ENABLE : TP_DOUBLETAP_DISABLE);
+}
+
 int trackpoint_detect(struct psmouse *psmouse, bool set_properties)
 {
 	struct ps2dev *ps2dev = &psmouse->ps2dev;
@@ -469,6 +511,15 @@ int trackpoint_detect(struct psmouse *psmouse, bool set_properties)
 		     "%s TrackPoint firmware: 0x%02x, buttons: %d/%d\n",
 		     psmouse->vendor, firmware_id,
 		     (button_info & 0xf0) >> 4, button_info & 0x0f);
+
+	/* Enable doubletap by default on capable devices */
+	if (is_trackpoint_dt_capable(ps2dev->serio->firmware_id)) {
+		int error = trackpoint_set_doubletap(ps2dev, true);
+		if (!error)
+			psmouse_info(psmouse, "Doubletap enabled by default!\n");
+		else
+			psmouse_warn(psmouse, "Failed to enable doubletap: %d\n", error);
+	}
 
 	return 0;
 }
