5ad7190dae32083173c94adf8268c88d4c0b7017
hailong.fan <hailong.fan@siengine.com>
2025-12-01 10:57:01 +0800

    net: stmmac: Modify the judgment condition of "tx_avail" from 1 to 2
        Under certain conditions, a WARN_ON will be triggered
        if avail equals 1.
    
        For example, when a VLAN packet is to send,
        stmmac_vlan_insert consumes one unit of space,
        and the data itself consumes another.
        actually requiring 2 units of space in total.
    
    Signed-off-by: hailong.fan <hailong.fan@siengine.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -4665,7 +4665,7 @@ static netdev_tx_t stmmac_xmit(struct sk_buff *skb, struct net_device *dev)
 		}
 	}
 
-	if (unlikely(stmmac_tx_avail(priv, queue) < nfrags + 1)) {
+	if (unlikely(stmmac_tx_avail(priv, queue) < nfrags + 2)) {
 		if (!netif_tx_queue_stopped(netdev_get_tx_queue(dev, queue))) {
 			netif_tx_stop_queue(netdev_get_tx_queue(priv->dev,
 								queue));
