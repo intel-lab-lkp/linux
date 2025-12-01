e43e2ef7b57447049832d61259c65fee0148f7f0
Yuri Andriaccio <yurand2000@gmail.com>
2025-12-01 13:42:00 +0100

    sched/rt: Add debug BUG_ONs for pre-migration code
    Add debug BUG_ONs in rt_queue_push/pull_task(s).
    Can be safely added after all the pre-migration patches.
    
    These are extra asserts which are only useful to debug the kernel code and
    are not meant to be part of the final patchset.
    
    Signed-off-by: Yuri Andriaccio <yurand2000@gmail.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -319,6 +319,9 @@ static inline void rt_queue_push_tasks(struct rt_rq *rt_rq)
 {
 	struct rq *rq = served_rq_of_rt_rq(rt_rq);
 
+	BUG_ON(rt_rq == NULL);
+	BUG_ON(rq != cpu_rq(rq->cpu));
+
 	if (!has_pushable_tasks(rt_rq))
 		return;
 
@@ -328,6 +331,9 @@ static inline void rt_queue_push_tasks(struct rt_rq *rt_rq)
 static inline void rt_queue_pull_task(struct rt_rq *rt_rq)
 {
 	struct rq *rq = served_rq_of_rt_rq(rt_rq);
+
+	BUG_ON(rt_rq == NULL);
+	BUG_ON(rq != cpu_rq(rq->cpu));
 
 	queue_balance_callback(rq, &per_cpu(rt_pull_head, rq->cpu), pull_rt_task);
 }
