3257ad0d02c3fbcf6f22ad335847d434668fa2e4
luca abeni <luca.abeni@santannapisa.it>
2025-12-01 13:41:44 +0100

    sched/deadline: Account rt-cgroups bandwidth in deadline tasks schedulability tests.
    Account the rt-cgroups hierarchy's reserved bandwidth in the
    schedulability test of deadline entities. This mechanism allows to
    completely reserve portion of the rt-bandwidth to rt-cgroups even if
    they do not use all of it.
    
    Account for the rt-cgroups' reserved bandwidth also when changing the
    total dedicated bandwidth for real time tasks.
    
    Co-developed-by: Alessio Balsini <a.balsini@sssup.it>
    Signed-off-by: Alessio Balsini <a.balsini@sssup.it>
    Co-developed-by: Andrea Parri <parri.andrea@gmail.com>
    Signed-off-by: Andrea Parri <parri.andrea@gmail.com>
    Co-developed-by: Yuri Andriaccio <yurand2000@gmail.com>
    Signed-off-by: Yuri Andriaccio <yurand2000@gmail.com>
    Signed-off-by: luca abeni <luca.abeni@santannapisa.it>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -213,8 +213,15 @@ void __dl_add(struct dl_bw *dl_b, u64 tsk_bw, int cpus)
 static inline bool
 __dl_overflow(struct dl_bw *dl_b, unsigned long cap, u64 old_bw, u64 new_bw)
 {
+	u64 dl_groups_root = 0;
+
+#ifdef CONFIG_RT_GROUP_SCHED
+	dl_groups_root = to_ratio(root_task_group.dl_bandwidth.dl_period,
+				  root_task_group.dl_bandwidth.dl_runtime);
+#endif
 	return dl_b->bw != -1 &&
-	       cap_scale(dl_b->bw, cap) < dl_b->total_bw - old_bw + new_bw;
+	       cap_scale(dl_b->bw, cap) < dl_b->total_bw - old_bw + new_bw
+					+ cap_scale(dl_groups_root, cap);
 }
 
 static inline
@@ -3147,9 +3154,15 @@ int sched_dl_global_validate(void)
 	u64 period = global_rt_period();
 	u64 new_bw = to_ratio(period, runtime);
 	u64 cookie = ++dl_cookie;
+	u64 dl_groups_root = 0;
 	struct dl_bw *dl_b;
-	int cpu, cpus, ret = 0;
+	int cpu, cap, cpus, ret = 0;
 	unsigned long flags;
+
+#ifdef CONFIG_RT_GROUP_SCHED
+	dl_groups_root = to_ratio(root_task_group.dl_bandwidth.dl_period,
+				  root_task_group.dl_bandwidth.dl_runtime);
+#endif
 
 	/*
 	 * Here we want to check the bandwidth not being set to some
@@ -3163,10 +3176,12 @@ int sched_dl_global_validate(void)
 			goto next;
 
 		dl_b = dl_bw_of(cpu);
+		cap = dl_bw_capacity(cpu);
 		cpus = dl_bw_cpus(cpu);
 
 		raw_spin_lock_irqsave(&dl_b->lock, flags);
-		if (new_bw * cpus < dl_b->total_bw)
+		if (new_bw * cpus < dl_b->total_bw +
+				    cap_scale(dl_groups_root, cap))
 			ret = -EBUSY;
 		raw_spin_unlock_irqrestore(&dl_b->lock, flags);
 
