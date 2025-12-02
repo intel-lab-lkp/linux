ecf2404708f5d00d31099ac97ee32037ce99e263
Alice Ryhl <aliceryhl@google.com>
2025-12-02 19:37:48 +0000

    rust: mm: add __rust_helper to helpers
    This is needed to inline these helpers into Rust code.
    
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- rust/helpers/vmalloc.c
+++ rust/helpers/vmalloc.c
@@ -2,9 +2,10 @@
 
 #include <linux/vmalloc.h>
 
-void * __must_check __realloc_size(2)
-rust_helper_vrealloc_node_align(const void *p, size_t size, unsigned long align,
-				gfp_t flags, int node)
+__rust_helper void *__must_check __realloc_size(2)
+	rust_helper_vrealloc_node_align(const void *p, size_t size,
+					unsigned long align, gfp_t flags,
+					int node)
 {
 	return vrealloc_node_align(p, size, align, flags, node);
 }
