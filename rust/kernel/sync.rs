36acda5fe7eb15613dbc8e4e8a47b65ef9faec22
Lyude Paul <lyude@redhat.com>
2025-12-02 13:24:21 -0500

    rust: sync: Expose lock::Backend
    Due to the addition of sync::lock::Backend::Context, lock guards can be
    returned with a different Backend than their respective lock. Since we'll
    be adding a trait bound for Backend to GlobalGuard in order to support
    this, users will need to be able to directly refer to Backend so that they
    can use it in trait bounds.
    
    So, let's make this easier for users and expose Backend in sync.
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>

--- rust/kernel/sync.rs
+++ rust/kernel/sync.rs
@@ -29,6 +29,7 @@ pub use lock::mutex::{new_mutex, Mutex, MutexGuard};
 pub use lock::spinlock::{
     new_spinlock, new_spinlock_irq, SpinLock, SpinLockGuard, SpinLockIrq, SpinLockIrqGuard,
 };
+pub use lock::Backend;
 pub use locked_by::LockedBy;
 pub use refcount::Refcount;
 
