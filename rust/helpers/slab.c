23b11249921a9e629530c26c5d56878f6450190b
Alice Ryhl <aliceryhl@google.com>
2025-12-02 19:38:02 +0000

    rust: slab: add __rust_helper to helpers
    This is needed to inline these helpers into Rust code.
    
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- rust/helpers/slab.c
+++ rust/helpers/slab.c
@@ -2,16 +2,18 @@
 
 #include <linux/slab.h>
 
-void * __must_check __realloc_size(2)
-rust_helper_krealloc_node_align(const void *objp, size_t new_size, unsigned long align,
-				gfp_t flags, int node)
+__rust_helper void *__must_check __realloc_size(2)
+	rust_helper_krealloc_node_align(const void *objp, size_t new_size,
+					unsigned long align, gfp_t flags,
+					int node)
 {
 	return krealloc_node_align(objp, new_size, align, flags, node);
 }
 
-void * __must_check __realloc_size(2)
-rust_helper_kvrealloc_node_align(const void *p, size_t size, unsigned long align,
-				 gfp_t flags, int node)
+__rust_helper void *__must_check __realloc_size(2)
+	rust_helper_kvrealloc_node_align(const void *p, size_t size,
+					 unsigned long align, gfp_t flags,
+					 int node)
 {
 	return kvrealloc_node_align(p, size, align, flags, node);
 }
