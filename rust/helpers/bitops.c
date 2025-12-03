7397db79f0bce3ba245b70b7a4330883921fb635
Alice Ryhl <aliceryhl@google.com>
2025-12-03 15:42:06 +0000

    rust: bitops: add helpers for _find_* functions
    On 32-bit ARM, _find_next_bit does not exist because ARM provides that
    function through a #define instead. This means that Rust, which calls
    the underscored version, fails to find it. This triggers errors:
    
    	ld.lld: error: undefined symbol: _find_next_bit
    	>>> referenced by bitmap.rs:459 (rust/kernel/bitmap.rs:459)
    	>>>               rust/kernel.o:(kernel::bitmap::tests::kunit_rust_wrapper_bitmap_borrow) in archive vmlinux.a
    	>>> referenced by bitmap.rs:459 (rust/kernel/bitmap.rs:459)
    	>>>               rust/kernel.o:(kernel::bitmap::tests::kunit_rust_wrapper_bitmap_set_clear_find) in archive vmlinux.a
    	>>> referenced by bitmap.rs:459 (rust/kernel/bitmap.rs:459)
    	>>>               rust/kernel.o:(kernel::bitmap::tests::kunit_rust_wrapper_bitmap_set_clear_find) in archive vmlinux.a
    	>>> referenced 10 more times
    
    	ld.lld: error: undefined symbol: _find_next_zero_bit
    	>>> referenced by bitmap.rs:479 (rust/kernel/bitmap.rs:479)
    	>>>               rust/kernel.o:(kernel::bitmap::tests::kunit_rust_wrapper_bitmap_copy) in archive vmlinux.a
    	>>> referenced by bitmap.rs:479 (rust/kernel/bitmap.rs:479)
    	>>>               rust/kernel.o:(kernel::bitmap::tests::kunit_rust_wrapper_bitmap_set_clear_find) in archive vmlinux.a
    	>>> referenced by bitmap.rs:479 (rust/kernel/bitmap.rs:479)
    	>>>               rust/kernel.o:(kernel::bitmap::tests::kunit_rust_wrapper_owned_bitmap_out_of_bounds) in archive vmlinux.a
    	>>> referenced 6 more times
    
    To fix this, add Rust helpers in this particular case.
    
    Fixes: 6cf93a9ed39e ("rust: add bindings for bitops.h")
    Reported-by: Andreas Hindborg <a.hindborg@kernel.org>
    Closes: https://rust-for-linux.zulipchat.com/#narrow/channel/x/topic/x/near/561677301
    Tested-by: Andreas Hindborg <a.hindborg@kernel.org>
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- rust/helpers/bitops.c
+++ rust/helpers/bitops.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0
 
 #include <linux/bitops.h>
+#include <linux/find.h>
 
 void rust_helper___set_bit(unsigned long nr, unsigned long *addr)
 {
@@ -21,3 +22,34 @@ void rust_helper_clear_bit(unsigned long nr, volatile unsigned long *addr)
 {
 	clear_bit(nr, addr);
 }
+
+/*
+ * Rust normally calls the single-underscore-prefixed version of these
+ * functions, which are not inlined. However, on some platforms, they do not
+ * exist. In those cases, provide a rust helper for the underscored version.
+ */
+#ifdef find_next_zero_bit
+__rust_helper unsigned long
+rust_helper__find_next_zero_bit(const unsigned long *addr, unsigned long size,
+				unsigned long offset)
+{
+	return find_next_zero_bit(addr, size, offset);
+}
+#endif /* find_next_zero_bit */
+
+#ifdef find_next_bit
+__rust_helper unsigned long
+rust_helper__find_next_bit(const unsigned long *addr, unsigned long size,
+			   unsigned long offset)
+{
+	return find_next_bit(addr, size, offset);
+}
+#endif /* find_next_bit */
+
+#ifdef find_last_bit
+__rust_helper unsigned long
+rust_helper__find_last_bit(const unsigned long *addr, unsigned long size)
+{
+	return find_last_bit(addr, size);
+}
+#endif /* find_last_bit */
