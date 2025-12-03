ba60bfe9ec4704257ea37fbfbe668d6f3c5f5c09
John Hubbard <jhubbard@nvidia.com>
2025-12-02 21:59:09 -0800

    gpu: nova-core: Hopper/Blackwell: calculate reserved FB heap size
    Various "reserved" areas of FB (frame buffer: vidmem) have to be
    calculated, because the GSP booting process needs this information.
    
    The calculations are const, so a new const-compatible alignment function
    is also added to num.rs, in order to align the reserved areas.
    
    Signed-off-by: John Hubbard <jhubbard@nvidia.com>

--- drivers/gpu/nova-core/fb.rs
+++ drivers/gpu/nova-core/fb.rs
@@ -153,6 +153,9 @@ pub(crate) struct FbLayout {
     pub(crate) wpr2: FbRange,
     pub(crate) heap: FbRange,
     pub(crate) vf_partition_count: u8,
+    /// Total reserved size (heap + PMU reserved), aligned to 2MB.
+    #[allow(dead_code)]
+    pub(crate) total_reserved_size: u32,
 }
 
 impl FbLayout {
@@ -240,6 +243,16 @@ impl FbLayout {
             FbRange(wpr2.start - HEAP_SIZE..wpr2.start)
         };
 
+        // Calculate reserved sizes. PMU reservation is a subset of the total reserved size.
+        let heap_size = (heap.end - heap.start) as u64;
+        let pmu_reserved_size = u64::from(PMU_RESERVED_SIZE);
+
+        let total_reserved_size = {
+            let total = heap_size + pmu_reserved_size;
+            const RSVD_ALIGN: Alignment = Alignment::new::<SZ_2M>();
+            total.align_up(RSVD_ALIGN).ok_or(EINVAL)?
+        };
+
         Ok(Self {
             fb,
             vga_workspace,
@@ -250,6 +263,11 @@ impl FbLayout {
             wpr2,
             heap,
             vf_partition_count: 0,
+            total_reserved_size: total_reserved_size as u32,
         })
     }
 }
+
+/// PMU reserved size, aligned to 128KB.
+pub(crate) const PMU_RESERVED_SIZE: u32 =
+    crate::num::const_align_up::<SZ_128K>(SZ_8M + SZ_16M + SZ_4K) as u32;
