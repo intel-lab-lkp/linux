2a97a6772c8fae13452533bf372f46379396f23a
Massimiliano Pellizzer <mpellizzer.dev@gmail.com>
2025-12-04 22:09:59 +0100

    btrfs: remove dead assignment in prepare_one_folio()
    In prepare_one_folio(), ret is initialized to 0 at declaration,
    and in an error path we assign ret = 0 before jumping to the
    again label to retry the operation. However, ret is immediately
    overwritten by ret = set_folio_extent_mapped(folio) after the
    again label.
    
    Both assignments are never observed by any code path,
    therefore they can be safely removed.
    
    No functional change.
    
    Signed-off-by: Massimiliano Pellizzer <mpellizzer.dev@gmail.com>

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -859,7 +859,7 @@ static noinline int prepare_one_folio(struct inode *inode, struct folio **folio_
 	fgf_t fgp_flags = (nowait ? FGP_WRITEBEGIN | FGP_NOWAIT : FGP_WRITEBEGIN) |
 			  fgf_set_order(write_bytes);
 	struct folio *folio;
-	int ret = 0;
+	int ret;
 
 again:
 	folio = __filemap_get_folio(inode->i_mapping, index, fgp_flags, mask);
@@ -877,7 +877,6 @@ again:
 		/* The folio is already unlocked. */
 		folio_put(folio);
 		if (!nowait && ret == -EAGAIN) {
-			ret = 0;
 			goto again;
 		}
 		return ret;
