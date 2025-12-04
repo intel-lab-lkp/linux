3f8824ca96ddc7fd31de29322f9a9049b99643f1
Ethan Graham <ethangraham@google.com>
2025-12-04 15:12:48 +0100

    drivers/auxdisplay: add a KFuzzTest for parse_xy()
    Add a KFuzzTest fuzzer for the parse_xy() function, located in a new
    file under /drivers/auxdisplay/tests.
    
    To validate the correctness and effectiveness of this KFuzzTest target,
    a bug was injected into parse_xy() like so:
    
    drivers/auxdisplay/charlcd.c:179
    - s = p;
    + s = p + 1;
    
    Although a simple off-by-one bug, it requires a specific input sequence
    in order to trigger it, thus demonstrating the power of pairing
    KFuzzTest with a coverage-guided fuzzer like syzkaller.
    
    Signed-off-by: Ethan Graham <ethangraham@google.com>
    Signed-off-by: Ethan Graham <ethan.w.s.graham@gmail.com>
    Acked-by: Alexander Potapenko <glider@google.com>

---
+++ drivers/auxdisplay/tests/charlcd_kfuzz.c
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * charlcd KFuzzTest target
+ *
+ * Copyright 2025 Google LLC
+ */
+#include <linux/kfuzztest.h>
+
+struct parse_xy_arg {
+	const char *s;
+};
+
+static bool parse_xy(const char *s, unsigned long *x, unsigned long *y);
+
+FUZZ_TEST(test_parse_xy, struct parse_xy_arg)
+{
+	unsigned long x, y;
+
+	KFUZZTEST_EXPECT_NOT_NULL(parse_xy_arg, s);
+	KFUZZTEST_ANNOTATE_STRING(parse_xy_arg, s);
+	parse_xy(arg->s, &x, &y);
+}
