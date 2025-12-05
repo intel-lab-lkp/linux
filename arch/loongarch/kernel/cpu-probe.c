f550388dc3410fb97daeb3804d94ca4c84ff4433
george <guodongtai@kylinos.cn>
2025-12-05 14:29:05 +0800

    LoongArch: Add SCQ support detection
    Check CPUCFG2_SCQ bit to determin if the CPU supports
    SCQ instrction.
    
    Co-developed-by: Yangyang Lian <lianyangyang@kylinos.cn>
    Signed-off-by: Yangyang Lian <lianyangyang@kylinos.cn>
    Signed-off-by: George Guo <guodongtai@kylinos.cn>

--- arch/loongarch/kernel/cpu-probe.c
+++ arch/loongarch/kernel/cpu-probe.c
@@ -201,6 +201,10 @@ static void cpu_probe_common(struct cpuinfo_loongarch *c)
 		c->options |= LOONGARCH_CPU_PTW;
 		elf_hwcap |= HWCAP_LOONGARCH_PTW;
 	}
+	if (config & CPUCFG2_SCQ)
+		c->options |= LOONGARCH_CPU_SCQ;
+	else
+		pr_warn_once("CPU does NOT support SCQ\n");
 	if (config & CPUCFG2_LSPW) {
 		c->options |= LOONGARCH_CPU_LSPW;
 		elf_hwcap |= HWCAP_LOONGARCH_LSPW;
