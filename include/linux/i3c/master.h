402f4c08913d2def4e15fcd2cf07968f521ca1f9
Adrian Ng Ho Yin <adrianhoyin.ng@altera.com>
2025-12-08 15:11:17 +0800

    i3c: add sysfs attribute for device NACK retry
    Add a `dev_nack_retry` sysfs attribute to allow reading and updating the
    device NACK retry count. A new `dev_nack_retry` field and an optional
    `set_dev_nack_retry()` callback are added to i3c_master_controller.
    The attribute is created only when the callback is implemented.
    
    Updates are applied under the I3C bus maintenance lock to ensure safe
    hardware reconfiguration.
    
    Signed-off-by: Adrian Ng Ho Yin <adrianhoyin.ng@altera.com>

--- include/linux/i3c/master.h
+++ include/linux/i3c/master.h
@@ -461,6 +461,8 @@ struct i3c_bus {
  * @enable_hotjoin: enable hot join event detect.
  * @disable_hotjoin: disable hot join event detect.
  * @set_speed: adjust I3C open drain mode timing.
+ * @set_dev_nack_retry: configure device NACK retry count for the master
+ *                      controller.
  */
 struct i3c_master_controller_ops {
 	int (*bus_init)(struct i3c_master_controller *master);
@@ -490,6 +492,8 @@ struct i3c_master_controller_ops {
 	int (*enable_hotjoin)(struct i3c_master_controller *master);
 	int (*disable_hotjoin)(struct i3c_master_controller *master);
 	int (*set_speed)(struct i3c_master_controller *master, enum i3c_open_drain_speed speed);
+	int (*set_dev_nack_retry)(struct i3c_master_controller *master,
+				  unsigned long dev_nack_retry_cnt);
 };
 
 /**
@@ -509,6 +513,7 @@ struct i3c_master_controller_ops {
  * @boardinfo: board-level information attached to devices connected on the bus
  * @bus: I3C bus exposed by this master
  * @wq: workqueue which can be used by master
+ * @dev_nack_retry: retry count when slave device nack
  *	drivers if they need to postpone operations that need to take place
  *	in a thread context. Typical examples are Hot Join processing which
  *	requires taking the bus lock in maintenance, which in turn, can only
@@ -533,6 +538,7 @@ struct i3c_master_controller {
 	} boardinfo;
 	struct i3c_bus bus;
 	struct workqueue_struct *wq;
+	unsigned int dev_nack_retry;
 };
 
 /**
