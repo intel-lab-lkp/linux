2e5912b4c5eb6fc89a6ff8ead6e4d35a91ec62a6
K Prateek Nayak <kprateek.nayak@amd.com>
2025-12-08 09:26:47 +0000

    sched/fair: Simplify set_cpu_sd_state_*() with guards
    Simplify set_cpu_sd_state_{busy,idle}() using guard(rcu).
    
    Reviewed-by: Gautham R. Shenoy <gautham.shenoy@amd.com>
    Reviewed-by: Shrikanth Hegde <sshegde@linux.ibm.com>
    Signed-off-by: K Prateek Nayak <kprateek.nayak@amd.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12543,16 +12543,14 @@ static void set_cpu_sd_state_busy(int cpu)
 {
 	struct sched_domain *sd;
 
-	rcu_read_lock();
+	guard(rcu)();
+
 	sd = rcu_dereference(per_cpu(sd_llc, cpu));
-
 	if (!sd || !sd->nohz_idle)
-		goto unlock;
-	sd->nohz_idle = 0;
+		return;
 
+	sd->nohz_idle = 0;
 	atomic_inc(&sd->shared->nr_busy_cpus);
-unlock:
-	rcu_read_unlock();
 }
 
 void nohz_balance_exit_idle(struct rq *rq)
@@ -12573,16 +12571,14 @@ static void set_cpu_sd_state_idle(int cpu)
 {
 	struct sched_domain *sd;
 
-	rcu_read_lock();
+	guard(rcu)();
+
 	sd = rcu_dereference(per_cpu(sd_llc, cpu));
-
 	if (!sd || sd->nohz_idle)
-		goto unlock;
-	sd->nohz_idle = 1;
+		return;
 
+	sd->nohz_idle = 1;
 	atomic_dec(&sd->shared->nr_busy_cpus);
-unlock:
-	rcu_read_unlock();
 }
 
 /*
