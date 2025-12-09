c3896b5e95a5afb2519625c632faf3fcbb4820bd
Costa Shulyupin <costa.shul@redhat.com>
2025-12-09 12:00:42 +0200

    tools/rtla: Consolidate -C/--cgroup option parsing
    Each rtla tool duplicates parsing of -C/--cgroup.
    
    Migrate the option parsing from individual tools to the
    common_parse_options().
    
    Signed-off-by: Costa Shulyupin <costa.shul@redhat.com>

--- tools/tracing/rtla/src/timerlat_top.c
+++ tools/tracing/rtla/src/timerlat_top.c
@@ -566,7 +566,6 @@ static struct common_params
 	while (1) {
 		static struct option long_options[] = {
 			{"auto",		required_argument,	0, 'a'},
-			{"cgroup",		optional_argument,	0, 'C'},
 			{"debug",		no_argument,		0, 'D'},
 			{"duration",		required_argument,	0, 'd'},
 			{"event",		required_argument,	0, 'e'},
@@ -600,7 +599,7 @@ static struct common_params
 		if (common_parse_options(argc, argv, &params->common))
 			continue;
 
-		c = getopt_long(argc, argv, "a:C::d:De:hH:i:knp:P:qs:t::T:uU0:1:2:345:6:7:",
+		c = getopt_long(argc, argv, "a:d:De:hH:i:knp:P:qs:t::T:uU0:1:2:345:6:7:",
 				 long_options, NULL);
 
 		/* detect the end of the options. */
@@ -636,10 +635,6 @@ static struct common_params
 
 			/* set aa_only to avoid parsing the trace */
 			params->common.aa_only = 1;
-			break;
-		case 'C':
-			params->common.cgroup = 1;
-			params->common.cgroup_name = optarg;
 			break;
 		case 'D':
 			config_debug = 1;
