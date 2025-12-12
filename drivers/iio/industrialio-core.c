b54974795a48d09c0d0ab18ca598221f8435f59b
Kurt Borja <kuurtb@gmail.com>
2025-12-11 21:45:21 -0500

    iio: core: Match iio_device_claim_*() semantics and implementation
    Implement iio_device_claim_buffer_mode() fully inline with the use of
    __iio_dev_mode_lock(), which takes care of sparse annotations.
    
    To completely match iio_device_claim_direct() semantics, we need to
    also change iio_device_claim_buffer_mode() return semantics to usual
    true/false conditional lock semantics.
    
    Signed-off-by: Kurt Borja <kuurtb@gmail.com>

--- drivers/iio/industrialio-core.c
+++ drivers/iio/industrialio-core.c
@@ -2202,46 +2202,6 @@ void __iio_dev_mode_unlock(struct iio_dev *indio_dev)
 EXPORT_SYMBOL_GPL(__iio_dev_mode_unlock);
 
 /**
- * iio_device_claim_buffer_mode - Keep device in buffer mode
- * @indio_dev:	the iio_dev associated with the device
- *
- * If the device is in buffer mode it is guaranteed to stay
- * that way until iio_device_release_buffer_mode() is called.
- *
- * Use with iio_device_release_buffer_mode().
- *
- * Returns: 0 on success, -EBUSY on failure.
- */
-int iio_device_claim_buffer_mode(struct iio_dev *indio_dev)
-{
-	struct iio_dev_opaque *iio_dev_opaque = to_iio_dev_opaque(indio_dev);
-
-	mutex_lock(&iio_dev_opaque->mlock);
-
-	if (iio_buffer_enabled(indio_dev))
-		return 0;
-
-	mutex_unlock(&iio_dev_opaque->mlock);
-	return -EBUSY;
-}
-EXPORT_SYMBOL_GPL(iio_device_claim_buffer_mode);
-
-/**
- * iio_device_release_buffer_mode - releases claim on buffer mode
- * @indio_dev:	the iio_dev associated with the device
- *
- * Release the claim. Device is no longer guaranteed to stay
- * in buffer mode.
- *
- * Use with iio_device_claim_buffer_mode().
- */
-void iio_device_release_buffer_mode(struct iio_dev *indio_dev)
-{
-	mutex_unlock(&to_iio_dev_opaque(indio_dev)->mlock);
-}
-EXPORT_SYMBOL_GPL(iio_device_release_buffer_mode);
-
-/**
  * iio_device_get_current_mode() - helper function providing read-only access to
  *				   the opaque @currentmode variable
  * @indio_dev:			   IIO device structure for device
