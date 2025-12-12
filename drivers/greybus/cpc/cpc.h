25aec5daf719f3e1c20e787d038892b96c667d53
Damien Riégel <damien.riegel@silabs.com>
2025-12-12 11:13:05 -0500

    greybus: cpc: honour remote's RX window
    The RX window indicates how many reception buffers the peer has
    available for that cport. The other peer must not send more messages
    than that window, or the chances of those messages being lost is very
    high, leading to retransmissions and poor performance.
    
    The RX window is associated with the ack number, and indicates the valid
    range of sequence number the other peer can use:
    
    	Ack		RX window		Valid Sequence Range
    
    	X		0			None
    	X		1			X
    	X		2			X -> X+1
    
    So everytime an ack is received, the driver evaluates if the valid
    sequence range has changed and if so pops message from its holding queue.
    
    As the skb is moved to another queue, it cannot be passed directly to
    the lower layer anymore, instead a clone is passed.
    
    Signed-off-by: Damien Riégel <damien.riegel@silabs.com>

--- drivers/greybus/cpc/cpc.h
+++ drivers/greybus/cpc/cpc.h
@@ -18,6 +18,7 @@
  * @cpc_hd: pointer to the CPC host device this cport belongs to
  * @lock: mutex to synchronize accesses to tcb and other attributes
  * @holding_queue: list of frames queued to be sent
+ * @retx_queue: list of frames sent and waiting for acknowledgment
  * @tcb: Transmission Control Block
  */
 struct cpc_cport {
@@ -27,12 +28,20 @@ struct cpc_cport {
 	struct mutex lock; /* Synchronize access to state variables */
 
 	struct sk_buff_head holding_queue;
+	struct sk_buff_head retx_queue;
 
 	/*
+	 * @send_wnd: send window, maximum number of frames that the remote can accept.
+	 *            TX frames should have a sequence in the range [send_una; send_una + send_wnd]
+	 * @send_nxt: send next, the next sequence number that will be used for transmission
+	 * @send_una: send unacknowledged, the oldest unacknowledged sequence number
 	 * @ack: current acknowledge number
 	 * @seq: current sequence number
 	 */
 	struct {
+		u8 send_wnd;
+		u8 send_nxt;
+		u8 send_una;
 		u8 ack;
 		u8 seq;
 	} tcb;
