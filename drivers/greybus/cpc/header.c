65c78a47883e108bd4322a71e1b7a228de07c906
Damien Riégel <damien.riegel@silabs.com>
2025-12-12 11:13:03 -0500

    greybus: cpc: acknowledge all incoming messages
    Currently, CPC doesn't send messages on its own, it only prepends its
    header to outgoing messages. This can lead to messages not being
    acknowledged, for instance in the case of an SVC Ping
    
    	Host				Device
    
      SVC Ping (seq=X, ack=Y)
    				  SVC Ping Reply (seq=Y, ack=X+1)
    
    The "Ping Reply" is never acknowledged at the CPC level, which can lead
    to retransmissions, or worst the device might think the link is broken
    and do something to recover.
    
    To prevent that scenario, an ack mechanism is implemented in the most
    straightforward manner: send an ACK to all incoming messages. Here, two
    flags need to be added:
     - First, an ACK frame should not be passed to the Greybus layer, so a
       "CONTROL" flag is added. If this flag is set, it means it's a control
       messages and should stay at the CPC level. Currently there is only
       one type of control frame, the standalone ack. Control messages have
       the same format as Greybus operations.
     - Second, ack themselves should not be acked, so to determine if a
       message should be acked or not, a REQUEST_ACK flag is added.
    
    Signed-off-by: Damien Riégel <damien.riegel@silabs.com>

--- drivers/greybus/cpc/header.c
+++ drivers/greybus/cpc/header.c
@@ -3,7 +3,24 @@
  * Copyright (c) 2025, Silicon Laboratories, Inc.
  */
 
+#include <linux/bitfield.h>
+#include <linux/bits.h>
+
 #include "header.h"
+
+#define CPC_HEADER_CONTROL_IS_CONTROL_MASK BIT(7)
+#define CPC_HEADER_CONTROL_REQ_ACK_MASK BIT(6)
+
+/**
+ * cpc_header_is_control() - Identify if this is a control frame.
+ * @hdr: CPC header.
+ *
+ * Return: True if this is a control frame, false if this a Greybus frame.
+ */
+bool cpc_header_is_control(const struct cpc_header *hdr)
+{
+	return hdr->ctrl_flags & CPC_HEADER_CONTROL_IS_CONTROL_MASK;
+}
 
 /**
  * cpc_header_get_seq() - Get the sequence number.
@@ -14,4 +31,28 @@
 u8 cpc_header_get_seq(const struct cpc_header *hdr)
 {
 	return hdr->seq;
+}
+
+/**
+ * cpc_header_get_req_ack() - Get the request acknowledge frame flag.
+ * @hdr: CPC header.
+ *
+ * Return: Request acknowledge frame flag.
+ */
+bool cpc_header_get_req_ack(const struct cpc_header *hdr)
+{
+	return FIELD_GET(CPC_HEADER_CONTROL_REQ_ACK_MASK, hdr->ctrl_flags);
+}
+
+/**
+ * cpc_header_encode_ctrl_flags() - Encode parameters into the control byte.
+ * @control: True if CPC control frame, false if Greybus frame.
+ * @req_ack: Frame flag indicating a request to be acknowledged.
+ *
+ * Return: Encoded control byte.
+ */
+u8 cpc_header_encode_ctrl_flags(bool control, bool req_ack)
+{
+	return FIELD_PREP(CPC_HEADER_CONTROL_IS_CONTROL_MASK, control) |
+	       FIELD_PREP(CPC_HEADER_CONTROL_REQ_ACK_MASK, req_ack);
 }
