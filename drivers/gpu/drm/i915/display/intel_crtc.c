51b2c0d724f24852720ecdf0a2444586e5393bcb
Ville Syrjälä <ville.syrjala@linux.intel.com>
2025-12-12 17:28:38 +0200

    drm/i915: Warn about use_flip==true in MMIO commit codepaths
    Warn if use_flipq==true in intel_pipe_update_{start,end}().
    These functions are part of the MMIO based commit codepath,
    so should never get there with use_flipq==true.
    
    We were also warning about use_dsb==true for the same reason.
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- drivers/gpu/drm/i915/display/intel_crtc.c
+++ drivers/gpu/drm/i915/display/intel_crtc.c
@@ -548,7 +548,7 @@ void intel_pipe_update_start(struct intel_atomic_state *state,
 	struct intel_vblank_evade_ctx evade;
 	int scanline;
 
-	drm_WARN_ON(display->drm, new_crtc_state->use_dsb);
+	drm_WARN_ON(display->drm, new_crtc_state->use_dsb || new_crtc_state->use_flipq);
 
 	intel_psr_lock(new_crtc_state);
 
@@ -687,7 +687,7 @@ void intel_pipe_update_end(struct intel_atomic_state *state,
 	u32 end_vbl_count = intel_crtc_get_vblank_counter(crtc);
 	ktime_t end_vbl_time = ktime_get();
 
-	drm_WARN_ON(display->drm, new_crtc_state->use_dsb);
+	drm_WARN_ON(display->drm, new_crtc_state->use_dsb || new_crtc_state->use_flipq);
 
 	if (new_crtc_state->do_async_flip)
 		goto out;
