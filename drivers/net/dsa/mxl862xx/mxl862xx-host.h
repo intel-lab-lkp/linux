a6266dad7897bab33c787ac9c02d11ceab29cf10
Daniel Golle <daniel@makrotopia.org>
2025-12-14 00:41:34 +0000

    net: dsa: add basic initial driver for MxL862xx switches
    Add very basic DSA driver for MaxLinear's MxL862xx switches.
    
    In contrast to previous MaxLinear switches the MxL862xx has a built-in
    processor that runs a sophisticated firmware based on Zephyr RTOS.
    Interaction between the host and the switch hence is organized using a
    software API of that firmware rather than accessing hardware registers
    directly.
    
    Add descriptions of the most basic firmware API calls to access the
    built-in MDIO bus hosting the 2.5GE PHYs, basic port control as well as
    setting up the CPU port.
    
    Implement a very basic DSA driver using that API which is sufficient to
    get packets flowing between the user ports and the CPU port.
    
    The firmware offers all features one would expect from a modern switch
    hardware, they will be added one by one in follow-up patch series.
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

---
+++ drivers/net/dsa/mxl862xx/mxl862xx-host.h
@@ -0,0 +1,4 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+int mxl862xx_api_wrap(struct mxl862xx_priv *priv, u16 cmd, void *data, u16 size, bool read);
+int mxl862xx_reset(struct mxl862xx_priv *priv);
