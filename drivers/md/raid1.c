2ceb21310e50fded7c26aacf8a1171910de0b3c0
Li Nan <linan122@huawei.com>
2025-12-15 11:04:34 +0800

    md/raid1,raid10: return actual write status in narrow_write_error
    narrow_write_error() currently returns true when setting badblocks fails.
    Instead, return actual status of all retried writes, succeeding only when
    all retried writes complete successfully. This gives upper layers accurate
    information about write outcomes.
    
    When setting badblocks fails, mark the device as faulty and return at once.
    No need to continue processing remaining sections in such cases.
    
    Signed-off-by: Li Nan <linan122@huawei.com>

--- drivers/md/raid1.c
+++ drivers/md/raid1.c
@@ -2541,11 +2541,15 @@ static bool narrow_write_error(struct r1bio *r1_bio, int i)
 		bio_trim(wbio, sector - r1_bio->sector, sectors);
 		wbio->bi_iter.bi_sector += rdev->data_offset;
 
-		if (submit_bio_wait(wbio) < 0)
+		if (submit_bio_wait(wbio)) {
 			/* failure! */
-			ok = rdev_set_badblocks(rdev, sector,
-						sectors, 0)
-				&& ok;
+			ok = false;
+			if (!rdev_set_badblocks(rdev, sector, sectors, 0)) {
+				md_error(mddev, rdev);
+				bio_put(wbio);
+				break;
+			}
+		}
 
 		bio_put(wbio);
 		sect_to_write -= sectors;
@@ -2596,10 +2600,7 @@ static void handle_write_finished(struct r1conf *conf, struct r1bio *r1_bio)
 			 * errors.
 			 */
 			fail = true;
-			if (!narrow_write_error(r1_bio, m))
-				md_error(conf->mddev,
-					 conf->mirrors[m].rdev);
-				/* an I/O failed, we can't clear the bitmap */
+			narrow_write_error(r1_bio, m);
 			rdev_dec_pending(conf->mirrors[m].rdev,
 					 conf->mddev);
 		}
