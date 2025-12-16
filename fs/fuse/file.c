2c962e2bdd909337925fc3b4e98cb4be9bd2e29b
David Laight <david.laight.linux@gmail.com>
2025-12-16 14:16:47 +0000

    fuse: change fuse_wr_pages() to avoid signedness error from min()
    On 32bit builds the 'number of pages required' calculation is signed
    and min() complains because max_pages is unsigned.
    Change the calcualtion that determines the number of pages by adding the
    'offset in page' to 'len' rather than subtracting the end and start pages.
    Although the 64bit value is still signed, the compiler knows it isn't
    negative so min() doesn't complain.
    The generated code is also slightly better.
    
    Forcing the calculation to 32 bits (eg len + (size_t)(pos & ...))
    generates much better code and is probably safe because len should
    be limited to 'INT_MAX - PAGE_SIZE).
    
    Fixes: 0f5bb0cfb0b4 ("fs: use min() or umin() instead of min_t()")
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202512160948.O7QqxHj2-lkp@intel.com/
    Signed-off-by: David Laight <david.laight.linux@gmail.com>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -1323,7 +1323,7 @@ static ssize_t fuse_fill_write_pages(struct fuse_io_args *ia,
 static inline unsigned int fuse_wr_pages(loff_t pos, size_t len,
 				     unsigned int max_pages)
 {
-	return min(((pos + len - 1) >> PAGE_SHIFT) - (pos >> PAGE_SHIFT) + 1,
+	return min(((len + (pos & (PAGE_SIZE - 1)) - 1) >> PAGE_SHIFT) + 1,
 		   max_pages);
 }
 
