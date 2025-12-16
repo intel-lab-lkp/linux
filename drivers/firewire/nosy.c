71053463fc67eb6abc476cd3b67af04fcf254890
Thomas Fourier <fourier.thomas@gmail.com>
2025-12-16 17:54:18 +0100

    firewire: nosy: Fix dma_free_coherent() size
    It looks like the buffer allocated and mapped in add_card() is done
    with size RCV_BUFFER_SIZE which is 16 KB and 4KB.
    
    Fixes: 286468210d83 ("firewire: new driver: nosy - IEEE 1394 traffic sniffer")
    Signed-off-by: Thomas Fourier <fourier.thomas@gmail.com>

--- drivers/firewire/nosy.c
+++ drivers/firewire/nosy.c
@@ -494,6 +494,8 @@ irq_handler(int irq, void *device)
 	return IRQ_HANDLED;
 }
 
+#define RCV_BUFFER_SIZE (16 * 1024)
+
 static void
 remove_card(struct pci_dev *dev)
 {
@@ -517,15 +519,13 @@ remove_card(struct pci_dev *dev)
 			  lynx->rcv_start_pcl, lynx->rcv_start_pcl_bus);
 	dma_free_coherent(&lynx->pci_device->dev, sizeof(struct pcl),
 			  lynx->rcv_pcl, lynx->rcv_pcl_bus);
-	dma_free_coherent(&lynx->pci_device->dev, PAGE_SIZE, lynx->rcv_buffer,
-			  lynx->rcv_buffer_bus);
+	dma_free_coherent(&lynx->pci_device->dev, RCV_BUFFER_SIZE,
+			  lynx->rcv_buffer, lynx->rcv_buffer_bus);
 
 	iounmap(lynx->registers);
 	pci_disable_device(dev);
 	lynx_put(lynx);
 }
-
-#define RCV_BUFFER_SIZE (16 * 1024)
 
 static int
 add_card(struct pci_dev *dev, const struct pci_device_id *unused)
@@ -680,7 +680,7 @@ fail_deallocate_buffers:
 		dma_free_coherent(&lynx->pci_device->dev, sizeof(struct pcl),
 				  lynx->rcv_pcl, lynx->rcv_pcl_bus);
 	if (lynx->rcv_buffer)
-		dma_free_coherent(&lynx->pci_device->dev, PAGE_SIZE,
+		dma_free_coherent(&lynx->pci_device->dev, RCV_BUFFER_SIZE,
 				  lynx->rcv_buffer, lynx->rcv_buffer_bus);
 	iounmap(lynx->registers);
 
