b61028594711ff3d2d1059f2edaf7357861676b2
Uma Shankar <uma.shankar@intel.com>
2025-12-17 11:51:54 +0530

    drm/{i915, xe}: Extract DSPCLK_GATE_D
    Move DSPCLK_GATE_D register definition to common header.
    This allows intel_gmbus.c free of i915_reg.h include.
    
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>

--- drivers/gpu/drm/i915/display/intel_gmbus.c
+++ drivers/gpu/drm/i915/display/intel_gmbus.c
@@ -34,8 +34,8 @@
 
 #include <drm/drm_print.h>
 #include <drm/display/drm_hdcp_helper.h>
+#include <drm/intel/intel_gmd_common_regs.h>
 
-#include "i915_reg.h"
 #include "intel_de.h"
 #include "intel_display_regs.h"
 #include "intel_display_types.h"
