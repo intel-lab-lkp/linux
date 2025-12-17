2f164fa263bf85039341161c0472bf9b6f10999b
Mika Kahola <mika.kahola@intel.com>
2025-12-17 17:19:50 +0200

    drm/i915/cx0: Add a fuzzy check for DP/HDMI clock rates during programming
    Since the clock rate is derived from the PLL divider values it can have
    a +-1kHz difference wrt. the reference rates in the comparison
    
    Signed-off-by: Mika Kahola <mika.kahola@intel.com>

--- drivers/gpu/drm/i915/display/intel_hdmi.c
+++ drivers/gpu/drm/i915/display/intel_hdmi.c
@@ -70,16 +70,14 @@
 
 bool intel_hdmi_is_frl(u32 clock)
 {
-	switch (clock) {
-	case 300000: /* 3 Gbps */
-	case 600000: /* 6 Gbps */
-	case 800000: /* 8 Gbps */
-	case 1000000: /* 10 Gbps */
-	case 1200000: /* 12 Gbps */
-		return true;
-	default:
-		return false;
-	}
+	u32 rates[] = { 300000, 600000, 800000, 1000000, 1200000 };
+	int i;
+
+	for (i = 0; i < ARRAY_SIZE(rates); i++)
+		if (intel_cx0pll_clock_matches(clock, rates[i]))
+			return true;
+
+	return false;
 }
 
 static void
