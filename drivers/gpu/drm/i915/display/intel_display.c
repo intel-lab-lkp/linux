0120d281a9508618ac96e4d5396577032534265f
Uma Shankar <uma.shankar@intel.com>
2025-12-17 11:51:58 +0530

    drm/{i915, xe}: Removed i915_reg.h from intel_display.c
    Move CHICKEN_PIPESL_1 to common header to free intel_display.c
    from including i915_reg.h
    
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>

--- drivers/gpu/drm/i915/display/intel_display.c
+++ drivers/gpu/drm/i915/display/intel_display.c
@@ -45,13 +45,13 @@
 #include <drm/drm_probe_helper.h>
 #include <drm/drm_rect.h>
 #include <drm/drm_vblank.h>
+#include <drm/intel/intel_gmd_common_regs.h>
 
 #include "g4x_dp.h"
 #include "g4x_hdmi.h"
 #include "hsw_ips.h"
 #include "i915_config.h"
 #include "i915_drv.h"
-#include "i915_reg.h"
 #include "i9xx_plane.h"
 #include "i9xx_plane_regs.h"
 #include "i9xx_wm.h"
