201332695b741444a9375d0ad9348c53ab3ed29f
Junrui Luo <moonafterrain@outlook.com>
2025-12-18 15:56:47 +0800

    hwmon: (ibmpex) Fix use-after-free in sysfs removal
    There is a use-after-free vulnerability in the ibmpex driver where
    the driver frees the sensors array while sysfs read operations
    via ibmpex_show_sensor() are still in progress.
    
    Fix this by reordering cleanup operations in ibmpex_bmc_delete().
    
    Reported-by: Yuhao Jiang <danisjiang@gmail.com>
    Reported-by: Junrui Luo <moonafterrain@outlook.com>
    Fixes: 57c7c3a0fdea ("hwmon: IBM power meter driver")
    Cc: stable@vger.kernel.org
    Signed-off-by: Junrui Luo <moonafterrain@outlook.com>

--- drivers/hwmon/ibmpex.c
+++ drivers/hwmon/ibmpex.c
@@ -508,6 +508,7 @@ static void ibmpex_bmc_delete(struct ibmpex_bmc_data *data)
 {
 	int i, j;
 
+	hwmon_device_unregister(data->hwmon_dev);
 	device_remove_file(data->bmc_device,
 			   &sensor_dev_attr_reset_high_low.dev_attr);
 	device_remove_file(data->bmc_device, &dev_attr_name.attr);
@@ -522,7 +523,6 @@ static void ibmpex_bmc_delete(struct ibmpex_bmc_data *data)
 
 	list_del(&data->list);
 	dev_set_drvdata(data->bmc_device, NULL);
-	hwmon_device_unregister(data->hwmon_dev);
 	ipmi_destroy_user(data->user);
 	kfree(data->sensors);
 	kfree(data);
