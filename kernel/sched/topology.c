f48dc06d9439cb5752b7d58f7cbddefd2b0678f0
Fushuai Wang <fushuai.wang@linux.dev>
2025-12-18 10:57:07 +0800

    sched/topology: Convert kzalloc() to kcalloc() in topology.c
    The kzalloc() doesn't check for overflows in size multiplication,
    so use kcalloc() instead.
    
    Signed-off-by: Fushuai Wang <wangfushuai@baidu.com>

--- kernel/sched/topology.c
+++ kernel/sched/topology.c
@@ -2055,7 +2055,7 @@ void sched_init_numa(int offline_node)
 
 	sched_domains_numa_levels = 0;
 
-	masks = kzalloc(sizeof(void *) * nr_levels, GFP_KERNEL);
+	masks = kcalloc(nr_levels, sizeof(void *), GFP_KERNEL);
 	if (!masks)
 		return;
 
@@ -2064,7 +2064,7 @@ void sched_init_numa(int offline_node)
 	 * CPUs of nodes that are that many hops away from us.
 	 */
 	for (i = 0; i < nr_levels; i++) {
-		masks[i] = kzalloc(nr_node_ids * sizeof(void *), GFP_KERNEL);
+		masks[i] = kcalloc(nr_node_ids, sizeof(void *), GFP_KERNEL);
 		if (!masks[i])
 			return;
 
@@ -2096,7 +2096,7 @@ void sched_init_numa(int offline_node)
 	/* Compute default topology size */
 	for (i = 0; sched_domain_topology[i].mask; i++);
 
-	tl = kzalloc((i + nr_levels + 1) *
+	tl = kcalloc(i + nr_levels + 1,
 			sizeof(struct sched_domain_topology_level), GFP_KERNEL);
 	if (!tl)
 		return;
