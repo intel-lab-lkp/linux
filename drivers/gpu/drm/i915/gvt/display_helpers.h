0b1652e857b07f224d57e288326eab5230310b9e
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2025-12-18 13:52:56 +0530

    drm/i915/gvt: Add header to use display offset functions in macros
    Introduce gvt/display_helpers.h to make DISPLAY_MMIO_BASE and
    INTEL_DISPLAY_DEVICE_*_OFFSET macros call exported display functions.
    This lets GVT keep using existing register macros (e.g.,
    TRANSCONF(display, pipe)) while ensuring offset calculations happen
    through functions instead of accessing display internals.
    
    Ideally, we would remove the display headers that define these macros,
    but some macros in GVT still depend on them and have not yet been
    ported. Keeping those headers leads to build conflicts, so as a
    stopgap, we use temporary ifdef/undef blocks to override the macros
    with API-backed versions. These will be removed once all dependent
    macros are ported and the conflicting headers can be safely dropped.
    
    v2:
     - Remove prefix `gvt/` while including the header file. (Jani)
     - Explain the rationale behind temporary ifdef/undefs and plan to drop
       them. (Jani).
    
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>

---
+++ drivers/gpu/drm/i915/gvt/display_helpers.h
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+/*
+ * Copyright © 2025 Intel Corporation
+ */
+
+#ifndef __DISPLAY_HELPERS_H__
+#define __DISPLAY_HELPERS_H__
+
+#include "display/intel_gvt_api.h"
+
+#ifdef DISPLAY_MMIO_BASE
+#undef DISPLAY_MMIO_BASE
+#endif
+#define DISPLAY_MMIO_BASE(display) \
+	intel_display_device_mmio_base((display))
+
+#ifdef INTEL_DISPLAY_DEVICE_PIPE_OFFSET
+#undef INTEL_DISPLAY_DEVICE_PIPE_OFFSET
+#endif
+#define INTEL_DISPLAY_DEVICE_PIPE_OFFSET(display, pipe) \
+	intel_display_device_pipe_offset((display), (pipe))
+
+#ifdef INTEL_DISPLAY_DEVICE_TRANS_OFFSET
+#undef INTEL_DISPLAY_DEVICE_TRANS_OFFSET
+#endif
+#define INTEL_DISPLAY_DEVICE_TRANS_OFFSET(display, trans) \
+	intel_display_device_trans_offset((display), (trans))
+
+#ifdef INTEL_DISPLAY_DEVICE_CURSOR_OFFSET
+#undef INTEL_DISPLAY_DEVICE_CURSOR_OFFSET
+#endif
+#define INTEL_DISPLAY_DEVICE_CURSOR_OFFSET(display, pipe) \
+	intel_display_device_cursor_offset((display), (pipe))
+
+#endif /* __DISPLAY_HELPERS_H__ */
