0610d52fafc20abcbf240fda8767a125c36345ce
Tao Zhang <tao.zhang@oss.qualcomm.com>
2025-12-19 18:04:19 +0800

    coresight: tpda: add sysfs nodes for tpda cross-trigger configuration
    Introduce sysfs nodes to configure cross-trigger parameters for TPDA.
    These registers define the characteristics of cross-trigger packets,
    including generation frequency and flag values.
    
    Signed-off-by: Tao Zhang <tao.zhang@oss.qualcomm.com>
    Reviewed-by: James Clark <james.clark@linaro.org>
    Co-developed-by: Jie Gan <jie.gan@oss.qualcomm.com>
    Signed-off-by: Jie Gan <jie.gan@oss.qualcomm.com>

--- drivers/hwtracing/coresight/coresight-tpda.c
+++ drivers/hwtracing/coresight/coresight-tpda.c
@@ -137,12 +137,32 @@ static int tpda_get_element_size(struct tpda_drvdata *drvdata,
 /* Settings pre enabling port control register */
 static void tpda_enable_pre_port(struct tpda_drvdata *drvdata)
 {
-	u32 val;
+	u32 val = 0;
 
-	val = readl_relaxed(drvdata->base + TPDA_CR);
-	val &= ~TPDA_CR_ATID;
 	val |= FIELD_PREP(TPDA_CR_ATID, drvdata->atid);
+	if (drvdata->trig_async)
+		val |= TPDA_CR_SRIE;
+
+	if (drvdata->trig_flag_ts)
+		val |= TPDA_CR_FLRIE;
+
+	if (drvdata->trig_freq)
+		val |= TPDA_CR_FRIE;
+
+	if (drvdata->freq_ts)
+		val |= TPDA_CR_FREQTS;
+
+	if (drvdata->cmbchan_mode)
+		val |= TPDA_CR_CMBCHANMODE;
+
 	writel_relaxed(val, drvdata->base + TPDA_CR);
+
+	/*
+	 * If FLRIE bit is set, set the master and channel
+	 * id as zero
+	 */
+	if (drvdata->trig_flag_ts)
+		writel_relaxed(0x0, drvdata->base + TPDA_FPID_CR);
 }
 
 static int tpda_enable_port(struct tpda_drvdata *drvdata, int port)
@@ -258,6 +278,116 @@ static const struct coresight_ops tpda_cs_ops = {
 	.link_ops	= &tpda_link_ops,
 };
 
+/* Read cross-trigger register member */
+static ssize_t tpda_trig_sysfs_show(struct device *dev,
+				    struct device_attribute *attr,
+				    char *buf)
+{
+	struct tpda_trig_sysfs_attribute *tpda_attr =
+		container_of(attr, struct tpda_trig_sysfs_attribute, attr);
+	struct tpda_drvdata *drvdata = dev_get_drvdata(dev->parent);
+	unsigned long val;
+
+	guard(spinlock)(&drvdata->spinlock);
+	switch (tpda_attr->mem) {
+	case FLREQ:
+		if (!drvdata->csdev->refcnt)
+			return -EINVAL;
+
+		val = readl_relaxed(drvdata->base + TPDA_CR);
+		/* read global flush request bit only */
+		val &= TPDA_CR_FLREQ;
+		return sysfs_emit(buf, "%lu\n", val);
+	case FREQTS:
+		return sysfs_emit(buf, "%u\n", (unsigned int)drvdata->freq_ts);
+	case FRIE:
+		return sysfs_emit(buf, "%u\n", (unsigned int)drvdata->trig_freq);
+	case FLRIE:
+		return sysfs_emit(buf, "%u\n", (unsigned int)drvdata->trig_flag_ts);
+	case SRIE:
+		return sysfs_emit(buf, "%u\n", (unsigned int)drvdata->trig_async);
+	case CMBCHANMODE:
+		return sysfs_emit(buf, "%u\n", (unsigned int)drvdata->cmbchan_mode);
+
+	}
+	return -EINVAL;
+}
+
+static ssize_t tpda_trig_sysfs_store(struct device *dev,
+				     struct device_attribute *attr,
+				     const char *buf,
+				     size_t size)
+{
+	struct tpda_trig_sysfs_attribute *tpda_attr =
+		container_of(attr, struct tpda_trig_sysfs_attribute, attr);
+	struct tpda_drvdata *drvdata = dev_get_drvdata(dev->parent);
+	ssize_t ret = -EINVAL;
+	unsigned long val;
+
+	if (kstrtoul(buf, 0, &val))
+		return ret;
+
+	guard(spinlock)(&drvdata->spinlock);
+	switch (tpda_attr->mem) {
+	case FLREQ:
+		if (!drvdata->csdev->refcnt || !val)
+			return ret;
+
+		val = readl_relaxed(drvdata->base + TPDA_CR);
+		/* set global flush request bit */
+		val |= TPDA_CR_FLREQ;
+		CS_UNLOCK(drvdata->base);
+		writel_relaxed(val, drvdata->base + TPDA_CR);
+		CS_LOCK(drvdata->base);
+		ret = size;
+		break;
+	case FREQTS:
+		drvdata->freq_ts = !!val;
+		ret = size;
+		break;
+	case FRIE:
+		drvdata->trig_freq = !!val;
+		ret = size;
+		break;
+	case FLRIE:
+		drvdata->trig_flag_ts = !!val;
+		ret = size;
+		break;
+	case SRIE:
+		drvdata->trig_async = !!val;
+		ret = size;
+		break;
+	case CMBCHANMODE:
+		drvdata->cmbchan_mode = !!val;
+		ret = size;
+		break;
+	default:
+		break;
+	}
+
+	return ret;
+}
+
+static struct attribute *tpda_global_cr_attrs[] = {
+	tpda_trig_sysfs_rw(global_flush_req, FLREQ),
+	tpda_trig_sysfs_rw(freq_ts_enable, FREQTS),
+	tpda_trig_sysfs_rw(trig_freq_enable, FRIE),
+	tpda_trig_sysfs_rw(trig_flag_ts_enable, FLRIE),
+	tpda_trig_sysfs_rw(trig_async_enable, SRIE),
+	tpda_trig_sysfs_rw(cmbchan_mode, CMBCHANMODE),
+	NULL,
+};
+
+static struct attribute_group tpda_global_cr_attr_grp = {
+	.attrs	= tpda_global_cr_attrs,
+	.name	= "global_cr",
+};
+
+static const struct attribute_group *tpda_attr_grps[] = {
+	&tpda_global_cr_attr_grp,
+	NULL,
+};
+
 static int tpda_init_default_data(struct tpda_drvdata *drvdata)
 {
 	int atid;
@@ -273,6 +403,7 @@ static int tpda_init_default_data(struct tpda_drvdata *drvdata)
 		return atid;
 
 	drvdata->atid = atid;
+	drvdata->freq_ts = true;
 	return 0;
 }
 
@@ -316,6 +447,7 @@ static int tpda_probe(struct amba_device *adev, const struct amba_id *id)
 	desc.ops = &tpda_cs_ops;
 	desc.pdata = adev->dev.platform_data;
 	desc.dev = &adev->dev;
+	desc.groups = tpda_attr_grps;
 	desc.access = CSDEV_ACCESS_IOMEM(base);
 	drvdata->csdev = coresight_register(&desc);
 	if (IS_ERR(drvdata->csdev))
