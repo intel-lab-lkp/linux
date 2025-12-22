8c958f49dc88aa54a4604a5defe67a4ec6f21e5a
Osose Itua <osose.itua@savoirfairelinux.com>
2025-12-22 17:21:04 -0500

    net: phy: adin: enable configuration of the LP Termination Register
    The ADIN1200/ADIN1300 provide a control bit that selects between normal
    receive termination and the lowest common mode impedance for 100BASE-TX
    operation. This behavior is controlled through the Low Power Termination
    register (B_100_ZPTM_EN_DIMRX).
    
    Bit 0 of this register enables normal termination when set (this is the
    default), and selects the lowest common mode impedance when cleared.
    
    Signed-off-by: Osose Itua <osose.itua@savoirfairelinux.com>

--- drivers/net/phy/adin.c
+++ drivers/net/phy/adin.c
@@ -4,6 +4,7 @@
  *
  * Copyright 2019 Analog Devices Inc.
  */
+#include <cerrno>
 #include <linux/kernel.h>
 #include <linux/bitfield.h>
 #include <linux/delay.h>
@@ -88,6 +89,9 @@
 
 #define ADIN1300_CLOCK_STOP_REG			0x9400
 #define ADIN1300_LPI_WAKE_ERR_CNT_REG		0xa000
+
+#define ADIN1300_B_100_ZPTM_DIMRX		0xB685
+#define ADIN1300_B_100_ZPTM_EN_DIMRX		BIT(0)
 
 #define ADIN1300_CDIAG_RUN			0xba1b
 #define   ADIN1300_CDIAG_RUN_EN			BIT(0)
@@ -522,6 +526,32 @@ static int adin_config_clk_out(struct phy_device *phydev)
 			      ADIN1300_GE_CLK_CFG_MASK, sel);
 }
 
+static int adin_config_zptm100(struct phy_device *phydev)
+{
+	struct device *dev = &phydev->mdio.dev;
+	int reg;
+	int rc;
+
+	if (!(device_property_read_bool(dev, "adi,low-cmode-impedance")))
+		return 0;
+
+	/* set to 0 to configure for lowest common-mode impedance */
+	rc = phy_write_mmd(phydev, MDIO_MMD_VEND1, ADIN1300_B_100_ZPTM_DIMRX, 0x0);
+	if (rc < 0)
+		return rc;
+
+	reg = phy_read_mmd(phydev, MDIO_MMD_VEND1, ADIN1300_B_100_ZPTM_DIMRX);
+	if (reg < 0)
+		return reg;
+
+	if (!(reg & ADIN1300_B_100_ZPTM_EN_DIMRX)) {
+		phydev_err(phydev, "Failed to set lowest common-mode impedance.\n");
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
 static int adin_config_init(struct phy_device *phydev)
 {
 	int rc;
@@ -545,6 +575,10 @@ static int adin_config_init(struct phy_device *phydev)
 		return rc;
 
 	rc = adin_config_clk_out(phydev);
+	if (rc < 0)
+		return rc;
+
+	rc = adin_config_zptm100(phydev);
 	if (rc < 0)
 		return rc;
 
