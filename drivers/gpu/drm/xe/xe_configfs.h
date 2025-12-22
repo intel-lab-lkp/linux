3f1c006402985ff15934a29fae74b0bf5c375e04
Michal Wajdeczko <michal.wajdeczko@intel.com>
2025-12-22 18:50:05 +0100

    drm/xe/configfs: Fix 'parameter name omitted' errors
    On some configs and old compilers we can get following build errors:
    
      ../drivers/gpu/drm/xe/xe_configfs.h: In function 'xe_configfs_get_ctx_restore_mid_bb':
      ../drivers/gpu/drm/xe/xe_configfs.h:40:76: error: parameter name omitted
       static inline u32 xe_configfs_get_ctx_restore_mid_bb(struct pci_dev *pdev, enum xe_engine_class,
                                                                                ^~~~~~~~~~~~~~~~~~~~
      ../drivers/gpu/drm/xe/xe_configfs.h: In function 'xe_configfs_get_ctx_restore_post_bb':
      ../drivers/gpu/drm/xe/xe_configfs.h:42:77: error: parameter name omitted
       static inline u32 xe_configfs_get_ctx_restore_post_bb(struct pci_dev *pdev, enum xe_engine_class,
                                                                                 ^~~~~~~~~~~~~~~~~~~~
    when trying to define our configfs stub functions. Fix that.
    
    Fixes: 7a4756b2fd04 ("drm/xe/lrc: Allow to add user commands mid context switch")
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>

--- drivers/gpu/drm/xe/xe_configfs.h
+++ drivers/gpu/drm/xe/xe_configfs.h
@@ -37,9 +37,9 @@ static inline bool xe_configfs_primary_gt_allowed(struct pci_dev *pdev) { return
 static inline bool xe_configfs_media_gt_allowed(struct pci_dev *pdev) { return true; }
 static inline u64 xe_configfs_get_engines_allowed(struct pci_dev *pdev) { return U64_MAX; }
 static inline bool xe_configfs_get_psmi_enabled(struct pci_dev *pdev) { return false; }
-static inline u32 xe_configfs_get_ctx_restore_mid_bb(struct pci_dev *pdev, enum xe_engine_class,
+static inline u32 xe_configfs_get_ctx_restore_mid_bb(struct pci_dev *pdev, enum xe_engine_class c,
 						     const u32 **cs) { return 0; }
-static inline u32 xe_configfs_get_ctx_restore_post_bb(struct pci_dev *pdev, enum xe_engine_class,
+static inline u32 xe_configfs_get_ctx_restore_post_bb(struct pci_dev *pdev, enum xe_engine_class c,
 						      const u32 **cs) { return 0; }
 static inline unsigned int xe_configfs_get_max_vfs(struct pci_dev *pdev) { return UINT_MAX; }
 #endif
