3a9bed1054557c08223754c64d68ea8e18a89efa
Khaled Saleh <khaled.saleh.req@gmail.com>
2025-12-23 22:37:14 +0200

    trace: make ftrace likely counters atomic
    The correct and incorrect counters in ftrace_likely_update() are
    updated without synchronization and may race on SMP systems.
    
    Convert them to atomic counters to address the existing FIXME
    without changing behavior.
    
    Signed-off-by: Khaled Saleh <khaled.saleh.req@gmail.com>

--- kernel/trace/trace_branch.c
+++ kernel/trace/trace_branch.c
@@ -215,10 +215,11 @@ void ftrace_likely_update(struct ftrace_likely_data *f, int val,
 	trace_likely_condition(f, val, expect);
 
 	/* FIXME: Make this atomic! */
-	if (val == expect)
-		f->data.correct++;
-	else
-		f->data.incorrect++;
+	if (val == expect) {
+		atomic_long_inc(&f->data.correct);
+	} else {
+	atomic_long_inc(&f->data.incorrect);
+	}
 
 	user_access_restore(flags);
 }
