2ce3a9dc33da738f4ef63f4a33641140aec1cb51
Nícolas F. R. A. Prado <nfraprado@collabora.com>
2025-12-23 15:01:37 -0300

    drm/mediatek: Set CRTC color pipeline driver cap
    Now that the driver implements CRTC (post-blend) color pipelines, set
    the driver cap so they can be used from userspace.
    
    Signed-off-by: Nícolas F. R. A. Prado <nfraprado@collabora.com>
    Co-developed-by: Ariel D'Alessandro <ariel.dalessandro@collabora.com>
    Signed-off-by: Ariel D'Alessandro <ariel.dalessandro@collabora.com>
    Reviewed-by: Louis Chauvet <louis.chauvet@bootlin.com>

--- drivers/gpu/drm/mediatek/mtk_drm_drv.c
+++ drivers/gpu/drm/mediatek/mtk_drm_drv.c
@@ -613,7 +613,8 @@ static struct drm_gem_object *mtk_gem_prime_import(struct drm_device *dev,
 }
 
 static const struct drm_driver mtk_drm_driver = {
-	.driver_features = DRIVER_MODESET | DRIVER_GEM | DRIVER_ATOMIC,
+	.driver_features = DRIVER_MODESET | DRIVER_GEM | DRIVER_ATOMIC |
+			   DRIVER_CRTC_COLOR_PIPELINE,
 
 	.dumb_create = mtk_gem_dumb_create,
 	DRM_FBDEV_DMA_DRIVER_OPS,
