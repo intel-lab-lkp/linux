090197e0afa8c61dc5049f92d38299b67acae9cc
Honggang LI <honggangli@163.com>
2025-12-23 11:43:24 +0800

    RDMA/rtrs: client: Fix clt_path::max_pages_per_mr calculation
    If the low two bytes of ib_dev::attrs::max_mr_size are zeros, the `min3`
    function will set clt_path::max_pages_per_mr to zero.
    
    `alloc_path_reqs` will pass zero, which is invalid,  as the third parameter
    to `ib_alloc_mr`.
    
    Fixes: 6a98d71daea1 ("RDMA/rtrs: client: main functionality")
    Signed-off-by: Honggang LI <honggangli@163.com>

--- drivers/infiniband/ulp/rtrs/rtrs-clt.c
+++ drivers/infiniband/ulp/rtrs/rtrs-clt.c
@@ -1465,7 +1465,7 @@ static void query_fast_reg_mode(struct rtrs_clt_path *clt_path)
 	max_pages_per_mr   = ib_dev->attrs.max_mr_size;
 	do_div(max_pages_per_mr, (1ull << mr_page_shift));
 	clt_path->max_pages_per_mr =
-		min3(clt_path->max_pages_per_mr, (u32)max_pages_per_mr,
+		min(min_not_zero(clt_path->max_pages_per_mr, (u32)max_pages_per_mr),
 		     ib_dev->attrs.max_fast_reg_page_list_len);
 	clt_path->clt->max_segments =
 		min(clt_path->max_pages_per_mr, clt_path->clt->max_segments);
