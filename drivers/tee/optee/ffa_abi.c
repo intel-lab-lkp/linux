6fe039553293c0429d0f5c28f79dc7b4c0ec62f2
Aristo Chen <jj251510319013@gmail.com>
2025-12-26 21:19:13 +0800

    tee: optee: expose OS revision via sysfs
    Today the only way to read the OP-TEE OS version is from dmesg/journal
    logs, which can be lost as buffers roll over. Capture the OS revision
    (major/minor/build_id) from secure world for both SMC and FF-A ABIs, store
    it in the OP-TEE driver, and expose a stable userspace readout via
    /sys/class/tee/tee*/revision.
    
    Signed-off-by: Aristo Chen <aristo.chen@canonical.com>

--- drivers/tee/optee/ffa_abi.c
+++ drivers/tee/optee/ffa_abi.c
@@ -776,7 +776,8 @@ static int optee_ffa_reclaim_protmem(struct optee *optee,
  */
 
 static bool optee_ffa_api_is_compatible(struct ffa_device *ffa_dev,
-					const struct ffa_ops *ops)
+					const struct ffa_ops *ops,
+					struct optee_revision *revision)
 {
 	const struct ffa_msg_ops *msg_ops = ops->msg_ops;
 	struct ffa_send_direct_data data = {
@@ -806,6 +807,12 @@ static bool optee_ffa_api_is_compatible(struct ffa_device *ffa_dev,
 		pr_err("Unexpected error %d\n", rc);
 		return false;
 	}
+	if (revision) {
+		revision->os_major = data.data0;
+		revision->os_minor = data.data1;
+		revision->os_build_id = data.data2;
+	}
+
 	if (data.data2)
 		pr_info("revision %lu.%lu (%08lx)",
 			data.data0, data.data1, data.data2);
@@ -898,8 +905,12 @@ static int optee_ffa_open(struct tee_context *ctx)
 	return optee_open(ctx, true);
 }
 
+static int optee_ffa_get_tee_revision(struct tee_device *teedev,
+				      char *buf, size_t len);
+
 static const struct tee_driver_ops optee_ffa_clnt_ops = {
 	.get_version = optee_ffa_get_version,
+	.get_tee_revision = optee_ffa_get_tee_revision,
 	.open = optee_ffa_open,
 	.release = optee_release,
 	.open_session = optee_open_session,
@@ -918,6 +929,7 @@ static const struct tee_desc optee_ffa_clnt_desc = {
 
 static const struct tee_driver_ops optee_ffa_supp_ops = {
 	.get_version = optee_ffa_get_version,
+	.get_tee_revision = optee_ffa_get_tee_revision,
 	.open = optee_ffa_open,
 	.release = optee_release_supp,
 	.supp_recv = optee_supp_recv,
@@ -1034,6 +1046,7 @@ static int optee_ffa_probe(struct ffa_device *ffa_dev)
 {
 	const struct ffa_notifier_ops *notif_ops;
 	const struct ffa_ops *ffa_ops;
+	struct optee_revision revision = { };
 	unsigned int max_notif_value;
 	unsigned int rpc_param_count;
 	struct tee_shm_pool *pool;
@@ -1047,7 +1060,7 @@ static int optee_ffa_probe(struct ffa_device *ffa_dev)
 	ffa_ops = ffa_dev->ops;
 	notif_ops = ffa_ops->notifier_ops;
 
-	if (!optee_ffa_api_is_compatible(ffa_dev, ffa_ops))
+	if (!optee_ffa_api_is_compatible(ffa_dev, ffa_ops, &revision))
 		return -EINVAL;
 
 	if (!optee_ffa_exchange_caps(ffa_dev, ffa_ops, &sec_caps,
@@ -1059,6 +1072,7 @@ static int optee_ffa_probe(struct ffa_device *ffa_dev)
 	optee = kzalloc(sizeof(*optee), GFP_KERNEL);
 	if (!optee)
 		return -ENOMEM;
+	optee->revision = revision;
 
 	pool = optee_ffa_shm_pool_alloc_pages();
 	if (IS_ERR(pool)) {
@@ -1193,4 +1207,11 @@ void optee_ffa_abi_unregister(void)
 {
 	if (IS_REACHABLE(CONFIG_ARM_FFA_TRANSPORT))
 		ffa_unregister(&optee_ffa_driver);
+}
+static int optee_ffa_get_tee_revision(struct tee_device *teedev,
+				      char *buf, size_t len)
+{
+	struct optee *optee = tee_get_drvdata(teedev);
+
+	return optee_get_revision(optee, buf, len);
 }
