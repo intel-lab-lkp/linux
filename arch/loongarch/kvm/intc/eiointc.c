63e1fba0eddbb6a22110d3131c377e3647fe1b8d
Qiang Ma <maqianga@uniontech.com>
2025-12-26 23:07:06 +0800

    LoongArch: KVM: Fix kvm_device leak in kvm_eiointc_destroy
    In kvm_ioctl_create_device(), kvm_device has allocated memory,
    kvm_device->destroy() seems to be supposed to free its kvm_device
    struct, but kvm_eiointc_destroy() is not currently doing this,
    that would lead to a memory leak.
    
    So, fix it.
    
    Signed-off-by: Qiang Ma <maqianga@uniontech.com>

--- arch/loongarch/kvm/intc/eiointc.c
+++ arch/loongarch/kvm/intc/eiointc.c
@@ -679,6 +679,8 @@ static void kvm_eiointc_destroy(struct kvm_device *dev)
 	kvm_io_bus_unregister_dev(kvm, KVM_IOCSR_BUS, &eiointc->device);
 	kvm_io_bus_unregister_dev(kvm, KVM_IOCSR_BUS, &eiointc->device_vext);
 	kfree(eiointc);
+	/* alloc by kvm_ioctl_create_device, free by .destroy */
+	kfree(dev);
 }
 
 static struct kvm_device_ops kvm_eiointc_dev_ops = {
