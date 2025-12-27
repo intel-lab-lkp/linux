0ba65d05bba7bb0aa40c0d5dd9a6c88e80c98fd1
Dmytro Maluka <dmaluka@chromium.org>
2025-12-27 18:57:24 +0100

    iommu/vt-d: Sanitize set bits in pasid_set_bits()
    When setting PASID entry bits via pasid_set_*() helpers, apply the
    specified mask to the specified bits to be set, as a safety measure
    in case the caller accidentally passes a value that exceeds the
    specified mask. Also warn if that happens.
    
    Suggested-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Dmytro Maluka <dmaluka@chromium.org>

--- drivers/iommu/intel/pasid.h
+++ drivers/iommu/intel/pasid.h
@@ -113,8 +113,10 @@ static inline void pasid_set_bits(u64 *ptr, u64 mask, u64 bits)
 {
 	u64 old;
 
+	WARN_ON_ONCE(bits & ~mask);
+
 	old = READ_ONCE(*ptr);
-	WRITE_ONCE(*ptr, (old & ~mask) | bits);
+	WRITE_ONCE(*ptr, (old & ~mask) | (bits & mask));
 }
 
 static inline u64 pasid_get_bits(u64 *ptr)
