a3096aea09928e6c40c14fb5314fb6c33379a65c
Sun Jian <sun.jian.kdev@gmail.com>
2025-12-28 13:49:02 +0800

    staging: rtl8723bs: avoid short msleep duration
    msleep() is documented to sleep for at least the specified
    number of milliseconds, but small values may result in a
    longer delay due to scheduler granularity.
    
    Replace msleep(10) with msleep(20) to match the effective
    sleep duration and silence checkpatch warning.
    
    No functional change intended.
    
    Signed-off-by: Sun Jian <sun.jian.kdev@gmail.com>

--- drivers/staging/rtl8723bs/core/rtw_cmd.c
+++ drivers/staging/rtl8723bs/core/rtw_cmd.c
@@ -215,7 +215,7 @@ void _rtw_free_evt_priv(struct	evt_priv *pevtpriv)
 {
 	_cancel_workitem_sync(&pevtpriv->c2h_wk);
 	while (pevtpriv->c2h_wk_alive)
-		msleep(10);
+		msleep(20);
 
 	while (!rtw_cbuf_empty(pevtpriv->c2h_queue)) {
 		void *c2h = rtw_cbuf_pop(pevtpriv->c2h_queue);
