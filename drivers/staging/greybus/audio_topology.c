837a1b13ce87b1d2d4cd7d7c5440f17e685e0f48
Sun Jian <sun.jian.kdev@gmail.com>
2025-12-30 00:13:46 +0800

    staging: greybus: audio: avoid snprintf truncation warnings
    W=1 reports possible truncation when formatting widget/control names
    with snprintf() and a %s argument. Use a small helper and hide the %s
    pointer from the compiler's truncation analysis via OPTIMIZER_HIDE_VAR(),
    while keeping the existing snprintf() formatting.
    
    No functional change intended.
    
    Signed-off-by: Sun Jian <sun.jian.kdev@gmail.com>

--- drivers/staging/greybus/audio_topology.c
+++ drivers/staging/greybus/audio_topology.c
@@ -1087,7 +1087,8 @@ static int gbaudio_tplg_create_widget(struct gbaudio_module_info *module,
 
 	/* Prefix dev_id to widget control_name */
 	strscpy(temp_name, w->name, sizeof(temp_name));
-	snprintf(w->name, sizeof(w->name), "GB %d %s", module->dev_id, temp_name);
+	scnprintf(w->name, sizeof(w->name), "GB %d ", module->dev_id);
+	strlcat(w->name, temp_name, sizeof(w->name));
 
 	switch (w->type) {
 	case snd_soc_dapm_spk:
@@ -1169,8 +1170,8 @@ static int gbaudio_tplg_process_kcontrols(struct gbaudio_module_info *module,
 		control->id = curr->id;
 		/* Prefix dev_id to widget_name */
 		strscpy(temp_name, curr->name, sizeof(temp_name));
-		snprintf(curr->name, sizeof(curr->name), "GB %d %s", module->dev_id,
-			 temp_name);
+		scnprintf(curr->name, sizeof(curr->name), "GB %d ", module->dev_id);
+		strlcat(curr->name, temp_name, sizeof(curr->name));
 		control->name = curr->name;
 		if (curr->info.type == GB_AUDIO_CTL_ELEM_TYPE_ENUMERATED) {
 			struct gb_audio_enumerated *gbenum =
