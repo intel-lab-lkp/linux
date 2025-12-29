55fb4d569e01b0678f6ffdf9c3fac27a79b5b495
Paul Greenwalt <paul.greenwalt@intel.com>
2025-12-29 03:52:34 -0500

    ice: fix devlink reload call trace
    Commit 4da71a77fc3b ("ice: read internal temperature sensor") introduced
    internal temperature sensor reading via HWMON. ice_hwmon_init() was added
    to ice_init_feature() and ice_hwmon_exit() was added to ice_remove(). As a
    result if devlink reload is used to reinit the device and then the driver
    is removed, a call trace can occur.
    
    BUG: unable to handle page fault for address: ffffffffc0fd4b5d
    Call Trace:
     string+0x48/0xe0
     vsnprintf+0x1f9/0x650
     sprintf+0x62/0x80
     name_show+0x1f/0x30
     dev_attr_show+0x19/0x60
    
    The call trace repeats approximately every 10 minutes when system
    monitoring tools (e.g., sadc) attempt to read the orphaned hwmon sysfs
    attributes that reference freed module memory.
    
    The sequence is:
    1. Driver load, ice_hwmon_init() gets called from ice_init_feature()
    2. Devlink reload down, flow does not call ice_remove()
    3. Devlink reload up, ice_hwmon_init() gets called from
       ice_init_feature() resulting in a second instance
    4. Driver unload, ice_hwmon_exit() called from ice_remove() leaving the
       first hwmon instance orphaned with dangling pointer
    
    Fix this by moving ice_hwmon_exit() from ice_remove() to
    ice_deinit_features() to ensure proper cleanup symmetry with
    ice_hwmon_init().
    
    Fixes: 4da71a77fc3b ("ice: read internal temperature sensor")
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Signed-off-by: Paul Greenwalt <paul.greenwalt@intel.com>
    Reviewed-by: Paul Menzel <pmenzel@molgen.mpg.de>

--- drivers/net/ethernet/intel/ice/ice_main.c
+++ drivers/net/ethernet/intel/ice/ice_main.c
@@ -4849,6 +4849,7 @@ static void ice_deinit_features(struct ice_pf *pf)
 		ice_dpll_deinit(pf);
 	if (pf->eswitch_mode == DEVLINK_ESWITCH_MODE_SWITCHDEV)
 		xa_destroy(&pf->eswitch.reprs);
+	ice_hwmon_exit(pf);
 }
 
 static void ice_init_wakeup(struct ice_pf *pf)
@@ -5453,8 +5454,6 @@ static void ice_remove(struct pci_dev *pdev)
 		set_bit(ICE_VF_RESETS_DISABLED, pf->state);
 		ice_free_vfs(pf);
 	}
-
-	ice_hwmon_exit(pf);
 
 	if (!ice_is_safe_mode(pf))
 		ice_remove_arfs(pf);
