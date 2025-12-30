215c88ff6fe15abcbb55759aec64342da1450a7e
Tommaso Merciai <tommaso.merciai.xr@bp.renesas.com>
2025-12-30 18:09:15 +0100

    media: rzg2l-cru: Skip ICnMC configuration when ICnSVC is used
    When the CRU is configured to use ICnSVC for virtual channel mapping,
    as on the RZ/{G3E, V2H/P} SoC, the ICnMC register must not be
    programmed.
    
    Return early after setting up ICnSVC to avoid overriding the ICnMC
    register, which is not applicable in this mode.
    
    This prevents unintended register programming when ICnSVC is enabled.
    
    Fixes: 3c5ca0a48bb0 ("media: rzg2l-cru: Drop function pointer to configure CSI")
    Cc: stable@vger.kernel.org
    Signed-off-by: Tommaso Merciai <tommaso.merciai.xr@bp.renesas.com>

--- drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
+++ drivers/media/platform/renesas/rzg2l-cru/rzg2l-video.c
@@ -268,6 +268,8 @@ static void rzg2l_cru_csi2_setup(struct rzg2l_cru_dev *cru,
 		rzg2l_cru_write(cru, ICnSVCNUM, csi_vc);
 		rzg2l_cru_write(cru, ICnSVC, ICnSVC_SVC0(0) | ICnSVC_SVC1(1) |
 				ICnSVC_SVC2(2) | ICnSVC_SVC3(3));
+
+		return;
 	}
 
 	icnmc |= rzg2l_cru_read(cru, info->image_conv) & ~ICnMC_INF_MASK;
