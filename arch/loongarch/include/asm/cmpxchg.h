07e1b1f30b688c5463d624195a70b8c884e71c79
George Guo <guodongtai@kylinos.cn>
2025-12-30 09:34:15 +0800

    LoongArch: Add 128-bit atomic cmpxchg support
    Implement 128-bit atomic compare-and-exchange using LoongArch's
    LL.D/SC.Q instructions.
    
    At the same time, fix BPF scheduler test failures (scx_central scx_qmap)
    caused by kmalloc_nolock_noprof returning NULL due to missing
    128-bit atomics. The NULL returns led to -ENOMEM errors during
    scheduler initialization, causing test cases to fail.
    
    Verified by testing with the scx_qmap scheduler (located in
    tools/sched_ext/). Building with `make` and running
    ./tools/sched_ext/build/bin/scx_qmap.
    
    Signed-off-by: George Guo <guodongtai@kylinos.cn>

--- arch/loongarch/include/asm/cmpxchg.h
+++ arch/loongarch/include/asm/cmpxchg.h
@@ -137,6 +137,44 @@ __arch_xchg(volatile void *ptr, unsigned long x, int size)
 	__ret;								\
 })
 
+union __u128_halves {
+	u128 full;
+	struct {
+		u64 low;
+		u64 high;
+	};
+};
+
+#define __cmpxchg128_asm(ptr, old, new)					\
+({									\
+	union __u128_halves __old, __new, __ret;			\
+	volatile u64 *__ptr = (volatile u64 *)(ptr);			\
+									\
+	__old.full = (old);                                             \
+	__new.full = (new);						\
+									\
+	__asm__ __volatile__(						\
+	"1:   ll.d    %0, %3		# 128-bit cmpxchg low	\n"	\
+	__WEAK_LLSC_MB							\
+	"     ld.d    %1, %4		# 128-bit cmpxchg high	\n"	\
+	"     bne     %0, %z5, 2f				\n"	\
+	"     bne     %1, %z6, 2f				\n"	\
+	"     move    $t0, %z7					\n"	\
+	"     move    $t1, %z8					\n"	\
+	"     sc.q    $t0, $t1, %2				\n"	\
+	"     beqz    $t0, 1b					\n"	\
+	"2:							\n"	\
+	__WEAK_LLSC_MB							\
+	: "=&r" (__ret.low), "=&r" (__ret.high)				\
+	: "r" (__ptr),							\
+	  "ZC" (__ptr[0]), "m" (__ptr[1]),				\
+	  "Jr" (__old.low), "Jr" (__old.high),				\
+	  "Jr" (__new.low), "Jr" (__new.high)				\
+	: "t0", "t1", "memory");					\
+									\
+	__ret.full;							\
+})
+
 static inline unsigned int __cmpxchg_small(volatile void *ptr, unsigned int old,
 					   unsigned int new, unsigned int size)
 {
@@ -222,6 +260,15 @@ __cmpxchg(volatile void *ptr, unsigned long old, unsigned long new, unsigned int
 	__res = arch_cmpxchg_local((ptr), (old), (new));		\
 									\
 	__res;								\
+})
+
+/* cmpxchg128 */
+#define system_has_cmpxchg128()		1
+
+#define arch_cmpxchg128(ptr, o, n)					\
+({									\
+	BUILD_BUG_ON(sizeof(*(ptr)) != 16);				\
+	__cmpxchg128_asm(ptr, o, n);					\
 })
 
 #ifdef CONFIG_64BIT
