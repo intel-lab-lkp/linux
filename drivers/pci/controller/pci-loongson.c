ab0e02003b562a4c393709c693e5938ea7410e3a
Ziyao Li <liziyao@uniontech.com>
2026-01-04 18:00:40 +0800

    PCI: loongson: Override PCIe bridge supported speeds for older Loongson 3C6000 series steppings
    Older steppings of the Loongson 3C6000 series incorrectly report the
    supported link speeds on their PCIe bridges (device IDs 3c19, 3c29) as
    only 2.5 GT/s, despite the upstream bus supporting speeds from 2.5 GT/s
    up to 16 GT/s.
    
    As a result, certain PCIe devices would be incorrectly probed as a Gen1-
    only, even if higher link speeds are supported, harming performance and
    prevents dynamic link speed functionality from being enabled in drivers
    such as amdgpu.
    
    Manually override the `supported_speeds` field for affected PCIe bridges
    with those found on the upstream bus to correctly reflect the supported
    link speeds.
    
    This patch was originally found from AOSC OS[1].
    
    Link: https://github.com/AOSC-Tracking/linux/pull/2 #1
    Tested-by: Lain Fearyncess Yang <fsf@live.com>
    Tested-by: Ayden Meng <aydenmeng@yeah.net>
    Signed-off-by: Ayden Meng <aydenmeng@yeah.net>
    Signed-off-by: Mingcong Bai <jeffbai@aosc.io>
    [Xi Ruoyao: Fix falling through logic and add kernel log output.]
    Signed-off-by: Xi Ruoyao <xry111@xry111.site>
    Link: https://github.com/AOSC-Tracking/linux/commit/4392f441363abdf6fa0a0433d73175a17f493454
    [Ziyao Li: move from drivers/pci/quirks.c to drivers/pci/controller/pci-loongson.c]
    Signed-off-by: Ziyao Li <liziyao@uniontech.com>
    Tested-by: Mingcong Bai <jeffbai@aosc.io>

--- drivers/pci/controller/pci-loongson.c
+++ drivers/pci/controller/pci-loongson.c
@@ -176,6 +176,45 @@ static void loongson_pci_msi_quirk(struct pci_dev *dev)
 }
 DECLARE_PCI_FIXUP_FINAL(PCI_VENDOR_ID_LOONGSON, DEV_LS7A_PCIE_PORT5, loongson_pci_msi_quirk);
 
+/*
+ * Older steppings of the Loongson 3C6000 series incorrectly report the
+ * supported link speeds on their PCIe bridges (device IDs 3c19, 3c29) as
+ * only 2.5 GT/s, despite the upstream bus supporting speeds from 2.5 GT/s
+ * up to 16 GT/s.
+ */
+static void quirk_loongson_pci_bridge_supported_speeds(struct pci_dev *pdev)
+{
+	u8 supported_speeds = pdev->supported_speeds;
+
+	switch (pdev->bus->max_bus_speed) {
+	case PCIE_SPEED_16_0GT:
+		supported_speeds |= PCI_EXP_LNKCAP2_SLS_16_0GB;
+		fallthrough;
+	case PCIE_SPEED_8_0GT:
+		supported_speeds |= PCI_EXP_LNKCAP2_SLS_8_0GB;
+		fallthrough;
+	case PCIE_SPEED_5_0GT:
+		supported_speeds |= PCI_EXP_LNKCAP2_SLS_5_0GB;
+		fallthrough;
+	case PCIE_SPEED_2_5GT:
+		supported_speeds |= PCI_EXP_LNKCAP2_SLS_2_5GB;
+		break;
+	default:
+		pci_warn(pdev, "unexpected max bus speed");
+		return;
+	}
+
+	if (supported_speeds != pdev->supported_speeds) {
+		pci_info(pdev, "fixing up supported link speeds: 0x%x => 0x%x",
+			 pdev->supported_speeds, supported_speeds);
+		pdev->supported_speeds = supported_speeds;
+	}
+}
+DECLARE_PCI_FIXUP_HEADER(PCI_VENDOR_ID_LOONGSON, 0x3c19,
+			 quirk_loongson_pci_bridge_supported_speeds);
+DECLARE_PCI_FIXUP_HEADER(PCI_VENDOR_ID_LOONGSON, 0x3c29,
+			 quirk_loongson_pci_bridge_supported_speeds);
+
 static struct loongson_pci *pci_bus_to_loongson_pci(struct pci_bus *bus)
 {
 	struct pci_config_window *cfg;
