13ef8c32719cee8fde690efe86f1144462ce1633
Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>
2026-01-04 22:31:31 +0100

    Input: Realign rest of the HID_UP_BUTTON cases
    The switch statement here had too much indentation.
    
    Signed-off-by: Tomasz Pakuła <tomasz.pakula.oficjalny@gmail.com>

--- drivers/hid/hid-input.c
+++ drivers/hid/hid-input.c
@@ -770,22 +770,22 @@ static void hidinput_configure_usage(struct hid_input *hidinput, struct hid_fiel
 				code = KEY_RESERVED;
 			break;
 		case HID_GD_GAMEPAD:
-				if (code <= 0xf)
-					code += BTN_GAMEPAD;
-				else
-					code += BTN_TRIGGER_HAPPY - 0x10;
-				break;
+			if (code <= 0xf)
+				code += BTN_GAMEPAD;
+			else
+				code += BTN_TRIGGER_HAPPY - 0x10;
+			break;
 		case HID_CP_CONSUMER_CONTROL:
-				if (hidinput_field_in_collection(device, field,
-								 HID_COLLECTION_NAMED_ARRAY,
-								 HID_CP_PROGRAMMABLEBUTTONS)) {
-					if (code <= 0x1d)
-						code += KEY_MACRO1;
-					else
-						code += BTN_TRIGGER_HAPPY - 0x1e;
-					break;
-				}
-				fallthrough;
+			if (hidinput_field_in_collection(device, field,
+							 HID_COLLECTION_NAMED_ARRAY,
+							 HID_CP_PROGRAMMABLEBUTTONS)) {
+				if (code <= 0x1d)
+					code += KEY_MACRO1;
+				else
+					code += BTN_TRIGGER_HAPPY - 0x1e;
+				break;
+			}
+			fallthrough;
 		default:
 			switch (field->physical) {
 			case HID_GD_MOUSE:
