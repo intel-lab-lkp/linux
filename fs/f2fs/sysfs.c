4a19de339362931cb53e8c4b8584229a36fc5687
Chao Yu <chao@kernel.org>
2026-01-04 10:07:17 +0800

    f2fs: sysfs: introduce max_lock_elapsed_time
    This patch add a new sysfs node in /sys/fs/f2fs/<device>/max_lock_elapsed_time.
    
    This is a threshold, once a thread enters critical region that lock covers,
    total elapsed time exceeds this threshold, f2fs will print tracepoint to dump
    information of related context. This sysfs entry can be used to control the
    value of threshold, by default, the value is 500 ms.
    
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/sysfs.c
+++ fs/f2fs/sysfs.c
@@ -1263,6 +1263,7 @@ F2FS_SBI_GENERAL_RW_ATTR(blkzone_alloc_policy);
 F2FS_SBI_GENERAL_RW_ATTR(carve_out);
 F2FS_SBI_GENERAL_RW_ATTR(reserved_pin_section);
 F2FS_SBI_GENERAL_RW_ATTR(bggc_io_aware);
+F2FS_SBI_GENERAL_RW_ATTR(max_lock_elapsed_time);
 
 /* STAT_INFO ATTR */
 #ifdef CONFIG_F2FS_STAT_FS
@@ -1466,6 +1467,7 @@ static struct attribute *f2fs_attrs[] = {
 	ATTR_LIST(reserved_pin_section),
 	ATTR_LIST(allocate_section_hint),
 	ATTR_LIST(allocate_section_policy),
+	ATTR_LIST(max_lock_elapsed_time),
 	NULL,
 };
 ATTRIBUTE_GROUPS(f2fs);
