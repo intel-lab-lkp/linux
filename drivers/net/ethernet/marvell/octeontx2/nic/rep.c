1fceb980851c5fa75891e722c7014da930ad6c5a
Ratheesh Kannoth <rkannoth@marvell.com>
2026-01-06 07:44:43 +0530

    octeontx2-pf: switch: Register for notifier chains.
    Switchdev flow table needs information on 5tuple information
    to mangle/to find egress ports etc. PF driver registers for fdb/fib/netdev
    notifier chains to listen to events. These events are parsed and sent
    to switchdev HW through mbox events.
    
    Signed-off-by: Ratheesh Kannoth <rkannoth@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/rep.c
+++ drivers/net/ethernet/marvell/octeontx2/nic/rep.c
@@ -15,6 +15,7 @@
 #include "cn10k.h"
 #include "otx2_reg.h"
 #include "rep.h"
+#include "switch/sw_nb.h"
 
 #define DRV_NAME	"rvu_rep"
 #define DRV_STRING	"Marvell RVU Representor Driver"
@@ -414,6 +415,11 @@ static int rvu_eswitch_config(struct otx2_nic *priv, u8 ena)
 	memcpy(req->switch_id, attrs.switch_id.id, attrs.switch_id.id_len);
 	otx2_sync_mbox_msg(&priv->mbox);
 	mutex_unlock(&priv->mbox.lock);
+
+#if IS_ENABLED(CONFIG_OCTEONTX_SWITCH)
+	ena ? sw_nb_register() : sw_nb_unregister();
+#endif
+
 	return 0;
 }
 
--- drivers/net/ethernet/marvell/octeontx2/nic/switch/sw_nb.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/switch/sw_nb.h
@@ -7,7 +7,25 @@
 #ifndef SW_NB_H_
 #define SW_NB_H_
 
+enum {
+	OTX2_DEV_UP = 1,
+	OTX2_DEV_DOWN,
+	OTX2_DEV_CHANGE,
+	OTX2_NEIGH_UPDATE,
+	OTX2_FIB_ENTRY_REPLACE,
+	OTX2_FIB_ENTRY_ADD,
+	OTX2_FIB_ENTRY_DEL,
+	OTX2_FIB_ENTRY_APPEND,
+	OTX2_CMD_MAX,
+};
+
 int sw_nb_register(void);
 int sw_nb_unregister(void);
+bool sw_nb_is_valid_dev(struct net_device *netdev);
 
+int otx2_mbox_up_handler_af2pf_fdb_refresh(struct otx2_nic *pf,
+					   struct af2pf_fdb_refresh_req *req,
+					   struct msg_rsp *rsp);
+
+const char *sw_nb_get_cmd2str(int cmd);
 #endif // SW_NB_H__
