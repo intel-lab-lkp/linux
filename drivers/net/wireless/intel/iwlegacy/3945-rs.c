523c679610cf37d3348950d7848b76148d8c45a0
Tuo Li <islituo@gmail.com>
2026-01-07 16:41:49 +0800

    wifi: iwlegacy: 3945-rs: fix possible null-pointer dereferences in il3945_rs_get_rate()
    In this function, il_sta is assigned to rs_sta, and rs_sta is dereferenced
    at several points. If il_sta is NULL, this can lead to null-pointer
    dereferences. To fix this issue, add an early check for il_sta and return
    if it is NULL, consistent with the handling in il3945_rs_tx_status().
    
    Besides, if the STA il data is uninitialized, return early instead of
    setting il_sta to NULL, consistent with the handling in
    il3945_rs_tx_status().
    
    Signed-off-by: Tuo Li <islituo@gmail.com>

--- drivers/net/wireless/intel/iwlegacy/3945-rs.c
+++ drivers/net/wireless/intel/iwlegacy/3945-rs.c
@@ -626,10 +626,15 @@ il3945_rs_get_rate(void *il_r, struct ieee80211_sta *sta, void *il_sta,
 
 	D_RATE("enter\n");
 
+	if (!il_sta) {
+		D_RATE("leave: No STA il data to update!\n");
+		return;
+	}
+
 	/* Treat uninitialized rate scaling data same as non-existing. */
-	if (rs_sta && !rs_sta->il) {
-		D_RATE("Rate scaling information not initialized yet.\n");
-		il_sta = NULL;
+	if (!rs_sta->il) {
+		D_RATE("leave: STA il data uninitialized!\n");
+		return;
 	}
 
 	rate_mask = sta->deflink.supp_rates[sband->band];
