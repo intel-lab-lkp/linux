94f406916e021cc924ddfa2cecedc1f5f9710968
Li Zhe <lizhe.67@bytedance.com>
2026-01-07 19:31:23 +0800

    mm/hugetlb: add pre-zeroed framework
    This patch establishes a pre-zeroing framework by introducing two new
    hugetlb page flags and extends the code at every point where these flags
    may later be required. The roles of the two flags are as follows.
    
    (1) HPG_zeroed – indicates that the huge folio has already been
        zeroed
    (2) HPG_zeroing – marks that the huge folio is currently being zeroed
    
    No functional change, as nothing sets the flags yet.
    
    Co-developed-by: Frank van der Linden <fvdl@google.com>
    Signed-off-by: Frank van der Linden <fvdl@google.com>
    Signed-off-by: Li Zhe <lizhe.67@bytedance.com>

--- fs/hugetlbfs/inode.c
+++ fs/hugetlbfs/inode.c
@@ -828,8 +828,7 @@ static long hugetlbfs_fallocate(struct file *file, int mode, loff_t offset,
 			error = PTR_ERR(folio);
 			goto out;
 		}
-		folio_zero_user(folio, addr);
-		__folio_mark_uptodate(folio);
+		hugetlb_zero_folio(folio, addr);
 		error = hugetlb_add_to_page_cache(folio, mapping, index);
 		if (unlikely(error)) {
 			restore_reserve_on_error(h, &pseudo_vma, addr, folio);
