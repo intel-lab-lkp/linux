05877af052dec44f2ceb3b798617b5739f0663d3
Kari Argillander <kari.argillander@gmail.com>
2026-01-10 17:07:59 +0200

    rust: enable const_refs_to_static feature
    Enable the const_refs_to_static Rust feature to allow taking
    references to static items in const contexts. This is required for
    using ThisModule when constructing static Rust structures.
    
    The Rust support already relies on features available in Rust 1.83, and
    const_refs_to_static has been available since Rust 1.78.
    
    Signed-off-by: Kari Argillander <kari.argillander@gmail.com>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -36,6 +36,7 @@
 #![feature(const_option)]
 #![feature(const_ptr_write)]
 #![feature(const_refs_to_cell)]
+#![feature(const_refs_to_static)]
 //
 // Expected to become stable.
 #![feature(arbitrary_self_types)]
