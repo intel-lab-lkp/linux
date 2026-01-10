121c2525edfbd1c689650f4b9cadd5f54838f58b
Marc Kleine-Budde <mkl@pengutronix.de>
2026-01-10 18:28:53 +0100

    can: esd_usb: esd_usb_read_bulk_callback(): fix URB memory leak
    Fix similar memory leak as in commit 7352e1d5932a ("can: gs_usb:
    gs_usb_receive_bulk_callback(): fix URB memory leak").
    
    In esd_usb_open(), the URBs for USB-in transfers are allocated, added to
    the dev->rx_submitted anchor and submitted. In the complete callback
    esd_usb_read_bulk_callback(), the URBs are processed and resubmitted. In
    esd_usb_close() the URBs are freed by calling
    usb_kill_anchored_urbs(&dev->rx_submitted).
    
    However, this does not take into account that the USB framework unanchors
    the URB before the complete function is called. This means that once an
    in-URB has been completed, it is no longer anchored and is ultimately not
    released in esd_usb_close().
    
    Fix the memory leak by anchoring the URB in the
    esd_usb_read_bulk_callback() to the dev->rx_submitted anchor.
    
    Fixes: 96d8e90382dc ("can: Add driver for esd CAN-USB/2 device")
    Cc: stable@vger.kernel.org
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/usb/esd_usb.c
+++ drivers/net/can/usb/esd_usb.c
@@ -541,6 +541,8 @@ resubmit_urb:
 			  urb->transfer_buffer, ESD_USB_RX_BUFFER_SIZE,
 			  esd_usb_read_bulk_callback, dev);
 
+	usb_anchor_urb(urb, &dev->rx_submitted);
+
 	err = usb_submit_urb(urb, GFP_ATOMIC);
 	if (err == -ENODEV) {
 		for (i = 0; i < dev->net_count; i++) {
