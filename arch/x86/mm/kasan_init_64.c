8b237a482f164134e016356d86efc2f5078fca16
Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>
2026-01-12 17:28:35 +0000

    x86/kasan: make software tag-based kasan available
    Make CONFIG_KASAN_SW_TAGS available for x86 machines if they have
    ADDRESS_MASKING enabled (LAM) as that works similarly to Top-Byte Ignore
    (TBI) that allows the software tag-based mode on arm64 platform.
    
    The value for sw_tags KASAN_SHADOW_OFFSET was calculated by rearranging
    the formulas for KASAN_SHADOW_START and KASAN_SHADOW_END from
    arch/x86/include/asm/kasan.h - the only prerequisites being
    KASAN_SHADOW_SCALE_SHIFT of 4, and KASAN_SHADOW_END equal to the one from
    KASAN generic mode.
    
    Set scale macro based on KASAN mode: in software tag-based mode 16 bytes
    of memory map to one shadow byte and 8 in generic mode.
    
    Disable CONFIG_KASAN_INLINE and CONFIG_KASAN_STACK when
    CONFIG_KASAN_SW_TAGS is enabled on x86 until the appropriate compiler
    support is available.
    
    Link: https://lkml.kernel.org/r/5b46822936bf9bf7e5cf5d1b57f936345c45a140.1768233085.git.m.wieczorretman@pm.me
    Signed-off-by: Maciej Wieczor-Retman <maciej.wieczor-retman@intel.com>
    Cc: Alexander Potapenko <glider@google.com>
    Cc: Andrey Konovalov <andreyknvl@gmail.com>
    Cc: Andrey Ryabinin <ryabinin.a.a@gmail.com>
    Cc: Andy Lutomirski <luto@kernel.org>
    Cc: Anshuman Khandual <anshuman.khandual@arm.com>
    Cc: Ard Biesheuvel <ardb@kernel.org>
    Cc: Axel Rasmussen <axelrasmussen@google.com>
    Cc: Bill Wendling <morbo@google.com>
    Cc: "Borislav Petkov (AMD)" <bp@alien8.de>
    Cc: Brendan Jackman <jackmanb@google.com>
    Cc: Breno Leitao <leitao@debian.org>
    Cc: Brian Gerst <brgerst@gmail.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: David Hildenbrand <david@kernel.org>
    Cc: Dmitriy Vyukov <dvyukov@google.com>
    Cc: Guilherme Giacomo Simoes <trintaeoitogc@gmail.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Jan Kiszka <jan.kiszka@siemens.com>
    Cc: Jeremy Linton <jeremy.linton@arm.com>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Josh Poimboeuf <jpoimboe@kernel.org>
    Cc: Justin Stitt <justinstitt@google.com>
    Cc: Kees Cook <kees@kernel.org>
    Cc: Kieran Bingham <kbingham@kernel.org>
    Cc: Liam Howlett <liam.howlett@oracle.com>
    Cc: Lorenzo Stoakes <lorenzo.stoakes@oracle.com>
    Cc: Marc Rutland <mark.rutland@arm.com>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Mostafa Saleh <smostafa@google.com>
    Cc: Nathan Chancellor <nathan@kernel.org>
    Cc: Omar Sandoval <osandov@fb.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Sami Tolvanen <samitolvanen@google.com>
    Cc: Samuel Holland <samuel.holland@sifive.com>
    Cc: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Thomas Gleixner <tglx@kernel.org>
    Cc: Thomas Huth <thuth@redhat.com>
    Cc: Thomas Lendacky <thomas.lendacky@amd.com>
    Cc: "Uladzislau Rezki (Sony)" <urezki@gmail.com>
    Cc: Uros Bizjak <ubizjak@gmail.com>
    Cc: Vincenzo Frascino <vincenzo.frascino@arm.com>
    Cc: Vlastimil Babka <vbabka@suse.cz>
    Cc: Wei Xu <weixugc@google.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Yeoreum Yun <yeoreum.yun@arm.com>
    Cc: Yuanchu Xie <yuanchu@google.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- arch/x86/mm/kasan_init_64.c
+++ arch/x86/mm/kasan_init_64.c
@@ -465,4 +465,10 @@ void __init kasan_init(void)
 
 	init_task.kasan_depth = 0;
 	kasan_init_generic();
+	pr_info("KernelAddressSanitizer initialized\n");
+
+	if (boot_cpu_has(X86_FEATURE_LAM))
+		kasan_init_sw_tags();
+	else
+		pr_info("KernelAddressSanitizer not initialized (sw-tags): hardware doesn't support LAM\n");
 }
