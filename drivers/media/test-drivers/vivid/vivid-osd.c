0b86416cc52cc233f66d722482fdc763c268b0d3
jempty.liang <imntjempty@163.com>
2026-01-12 05:59:21 +0000

    media: vivid: Simplify fb_vblank init in fb_blank
    Adjust struct fb_vblank declaration scope and use aggregate init
    instead of memset for cleaner FBIOGET_VBLANK handling.
    
    Signed-off-by: jempty.liang <imntjempty@163.com>

--- drivers/media/test-drivers/vivid/vivid-osd.c
+++ drivers/media/test-drivers/vivid/vivid-osd.c
@@ -73,12 +73,10 @@ void vivid_fb_clear(struct vivid_dev *dev)
 static int vivid_fb_ioctl(struct fb_info *info, unsigned cmd, unsigned long arg)
 {
 	struct vivid_dev *dev = (struct vivid_dev *)info->par;
+	struct fb_vblank vblank = {};
 
 	switch (cmd) {
 	case FBIOGET_VBLANK: {
-		struct fb_vblank vblank;
-
-		memset(&vblank, 0, sizeof(vblank));
 		vblank.flags = FB_VBLANK_HAVE_COUNT | FB_VBLANK_HAVE_VCOUNT |
 			FB_VBLANK_HAVE_VSYNC;
 		vblank.count = 0;
