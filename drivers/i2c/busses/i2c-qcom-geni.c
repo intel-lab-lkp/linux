0d8f30945dbfe5007d16dcb81d8ff1e8e6acc84a
Praveen Talari <praveen.talari@oss.qualcomm.com>
2026-01-12 16:17:20 +0530

    i2c: qcom-geni: Use resources helper APIs in runtime PM functions
    To manage GENI serial engine resources during runtime power management,
    drivers currently need to call functions for ICC, clock, and
    SE resource operations in both suspend and resume paths, resulting in
    code duplication across drivers.
    
    The new geni_se_resources_activate() and geni_se_resources_deactivate()
    helper APIs addresses this issue by providing a streamlined method to
    enable or disable all resources based, thereby eliminating redundancy
    across drivers.
    
    Signed-off-by: Praveen Talari <praveen.talari@oss.qualcomm.com>

--- drivers/i2c/busses/i2c-qcom-geni.c
+++ drivers/i2c/busses/i2c-qcom-geni.c
@@ -1160,18 +1160,15 @@ static int __maybe_unused geni_i2c_runtime_suspend(struct device *dev)
 	struct geni_i2c_dev *gi2c = dev_get_drvdata(dev);
 
 	disable_irq(gi2c->irq);
-	ret = geni_se_resources_off(&gi2c->se);
+
+	ret = geni_se_resources_deactivate(&gi2c->se);
 	if (ret) {
 		enable_irq(gi2c->irq);
 		return ret;
-
-	} else {
-		gi2c->suspended = 1;
 	}
 
-	clk_disable_unprepare(gi2c->core_clk);
-
-	return geni_icc_disable(&gi2c->se);
+	gi2c->suspended = 1;
+	return ret;
 }
 
 static int __maybe_unused geni_i2c_runtime_resume(struct device *dev)
@@ -1179,27 +1176,12 @@ static int __maybe_unused geni_i2c_runtime_resume(struct device *dev)
 	int ret;
 	struct geni_i2c_dev *gi2c = dev_get_drvdata(dev);
 
-	ret = geni_icc_enable(&gi2c->se);
+	ret = geni_se_resources_activate(&gi2c->se);
 	if (ret)
 		return ret;
 
-	ret = clk_prepare_enable(gi2c->core_clk);
-	if (ret)
-		goto out_icc_disable;
-
-	ret = geni_se_resources_on(&gi2c->se);
-	if (ret)
-		goto out_clk_disable;
-
 	enable_irq(gi2c->irq);
 	gi2c->suspended = 0;
-
-	return 0;
-
-out_clk_disable:
-	clk_disable_unprepare(gi2c->core_clk);
-out_icc_disable:
-	geni_icc_disable(&gi2c->se);
 
 	return ret;
 }
