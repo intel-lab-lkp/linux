c405b795263cab615f3af90e56417f281f6dad62
Dominik Karol Piątkowski <dominik.karol.piatkowski@protonmail.com>
2026-01-13 19:09:25 +0000

    gpib: pc2: Unify *allocate_private
    Return values for *allocate_private functions as well as calling code in
    gpib driver are all over the place. Unify them by returning -errno if
    something fails, zero otherwise. Use the returned value as early return
    value in case of error.
    
    Signed-off-by: Dominik Karol Piątkowski <dominik.karol.piatkowski@protonmail.com>

--- drivers/gpib/pc2/pc2_gpib.c
+++ drivers/gpib/pc2/pc2_gpib.c
@@ -239,7 +239,7 @@ static int allocate_private(struct gpib_board *board)
 
 	board->private_data = kmalloc(sizeof(struct pc2_priv), GFP_KERNEL);
 	if (!board->private_data)
-		return -1;
+		return -ENOMEM;
 	priv = board->private_data;
 	memset(priv, 0, sizeof(struct pc2_priv));
 	init_nec7210_private(&priv->nec7210_priv);
@@ -257,10 +257,12 @@ static int pc2_generic_attach(struct gpib_board *board, const struct gpib_board_
 {
 	struct pc2_priv *pc2_priv;
 	struct nec7210_priv *nec_priv;
+	int retval;
 
 	board->status = 0;
-	if (allocate_private(board))
-		return -ENOMEM;
+	retval = allocate_private(board);
+	if (retval < 0)
+		return retval;
 	pc2_priv = board->private_data;
 	nec_priv = &pc2_priv->nec7210_priv;
 	nec_priv->read_byte = nec7210_ioport_read_byte;
