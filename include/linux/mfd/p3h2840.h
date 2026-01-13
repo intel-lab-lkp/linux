5daad41e36f172f43399e8b7a44077516f363543
Aman Kumar Pandey <aman.kumarpandey@nxp.com>
2026-01-13 13:45:27 +0200

    mfd: p3h2x4x: Add driver for NXP P3H2x4x i3c hub and on-die regulator
    Add core MFD support for the NXP P3H2x4x (P3H2440/P3H2441/P3H2840/P3H2841)
    family of multiport I3C hub devices. These devices connect to a host via
    I3C/I2C/SMBus and expose multiple downstream target ports.
    
    This patch introduces the MFD driver that registers sub-devices for the
    I3C hub and on-die regulators.
    
    Signed-off-by: Aman Kumar Pandey <aman.kumarpandey@nxp.com>
    Signed-off-by: Vikash Bansal <vikash.bansal@nxp.com>

---
+++ include/linux/mfd/p3h2840.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+/*
+ * Copyright 2025 NXP
+ * This header file contain private Reg address and its bit mapping etc.
+ */
+
+#ifndef P3H2840_H
+#define P3H2840_H
+
+#include <linux/types.h>
+
+/* Reg config for Regmap */
+#define P3H2x4x_REG_BITS					8
+#define P3H2x4x_VAL_BITS					8
+
+struct p3h2x4x_dev {
+	struct i3c_device *i3cdev;
+	struct i2c_client *i2c_client;
+	struct regmap *regmap;
+	bool is_p3h2x4x_in_i3c;
+};
+#endif /* P3H2840_H */
