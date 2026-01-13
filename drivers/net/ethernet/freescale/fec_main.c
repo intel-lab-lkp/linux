9a7f739ea4ee49a9a39b53c72d8ebed75964a63c
Wei Fang <wei.fang@nxp.com>
2026-01-13 11:29:34 +0800

    net: fec: transmit XDP frames in bulk
    Currently, the driver writes the ENET_TDAR register for every XDP frame
    to trigger transmit start. Frequent MMIO writes consume more CPU cycles
    and may reduce XDP TX performance, so transmit XDP frames in bulk.
    
    Signed-off-by: Wei Fang <wei.fang@nxp.com>

--- drivers/net/ethernet/freescale/fec_main.c
+++ drivers/net/ethernet/freescale/fec_main.c
@@ -1994,6 +1994,8 @@ static int fec_enet_rx_queue_xdp(struct fec_enet_private *fep, int queue,
 				rxq->stats[RX_XDP_TX_ERRORS]++;
 				fec_xdp_drop(rxq, &xdp, sync);
 				trace_xdp_exception(ndev, prog, XDP_TX);
+			} else {
+				xdp_res |= FEC_ENET_XDP_TX;
 			}
 			break;
 		default:
@@ -2042,6 +2044,10 @@ rx_processing_done:
 
 	if (xdp_res & FEC_ENET_XDP_REDIR)
 		xdp_do_flush();
+
+	if (xdp_res & FEC_ENET_XDP_TX)
+		/* Trigger transmission start */
+		fec_txq_trigger_xmit(fep, fep->tx_queue[queue]);
 
 	return pkt_received;
 }
@@ -4033,9 +4039,6 @@ static int fec_enet_txq_xmit_frame(struct fec_enet_private *fep,
 
 	txq->bd.cur = bdp;
 
-	/* Trigger transmission start */
-	fec_txq_trigger_xmit(fep, txq);
-
 	return 0;
 }
 
@@ -4086,6 +4089,9 @@ static int fec_enet_xdp_xmit(struct net_device *dev,
 			break;
 		sent_frames++;
 	}
+
+	if (sent_frames)
+		fec_txq_trigger_xmit(fep, txq);
 
 	__netif_tx_unlock(nq);
 
