8e021fb7ea59c61fe0caff45ab6dfc2781d8a78f
Timur Tabi <ttabi@nvidia.com>
2026-01-13 16:54:07 -0600

    gpu: nova-core: create debugfs root when driver loads
    Create the 'nova_core' root debugfs entry when the driver loads.
    The Dir entry is part of the NovaCoreModule, so when the module
    unloads, the NovaCoreModule object is dropped, and that drops the
    _debugfs_root, which automatically deletes the debugfs file.
    
    Signed-off-by: Timur Tabi <ttabi@nvidia.com>

--- drivers/gpu/nova-core/nova_core.rs
+++ drivers/gpu/nova-core/nova_core.rs
@@ -3,6 +3,7 @@
 //! Nova Core GPU Driver
 
 use kernel::{
+    debugfs::{Dir, Directory},
     error::Error,
     pci,
     prelude::*,
@@ -31,12 +32,17 @@ pub(crate) const MODULE_NAME: &kernel::str::CStr = <LocalModule as kernel::Modul
 struct NovaCoreModule {
     #[pin]
     _driver: kernel::driver::Registration<pci::Adapter<driver::NovaCore>>,
+    _debugfs_root: Dir,
 }
 
 impl InPlaceModule for NovaCoreModule {
     fn init(module: &'static kernel::ThisModule) -> impl PinInit<Self, Error> {
+        // Create the debugfs top-level directory.  Each GPU will create a subdirectory.
+        let debugfs_root = Dir::new(kernel::c_str!("nova_core"));
+
         try_pin_init!(Self {
             _driver <- kernel::driver::Registration::new(MODULE_NAME, module),
+            _debugfs_root: debugfs_root,
         })
     }
 }
