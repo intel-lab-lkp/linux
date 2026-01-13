ffd8995a576f73817c53d678ca6e2efed26865a6
Jim Mattson <jmattson@google.com>
2026-01-12 16:29:56 -0800

    KVM: x86: nSVM: Add g_pat to fields copied by svm_copy_vmrun_state()
    The vmcb01 g_pat field holds the value of L1's IA32_PAT MSR. To
    preserve this value through virtual SMM and serialization, add g_pat
    to the fields copied by svm_copy_vmrun_state().
    
    Signed-off-by: Jim Mattson <jmattson@google.com>

--- arch/x86/kvm/svm/nested.c
+++ arch/x86/kvm/svm/nested.c
@@ -1090,6 +1090,7 @@ void svm_copy_vmrun_state(struct vmcb_save_area *to_save,
 	to_save->gdtr = from_save->gdtr;
 	to_save->idtr = from_save->idtr;
 	to_save->rflags = from_save->rflags | X86_EFLAGS_FIXED;
+	to_save->g_pat = from_save->g_pat;
 	to_save->efer = from_save->efer;
 	to_save->cr0 = from_save->cr0;
 	to_save->cr3 = from_save->cr3;
