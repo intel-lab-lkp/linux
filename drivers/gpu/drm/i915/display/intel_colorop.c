322b4bcf8f6831c03c95bf6b9b427ff2d4fe3351
Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>
2026-01-13 15:52:59 +0530

    drm/i915/display: Hook up intel_colorop_destroy
    i915 embeds struct drm_colorop inside struct intel_colorop, so the
    default drm_colorop_destroy() helper cannot be used. Add an
    intel_colorop_destroy() helper that performs common DRM cleanup and
    frees intel_colorop object.
    
    This ensures correct teardown of plane color pipeline objects.
    
    Signed-off-by: Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>
    Reviewed-by: Suraj Kandpal <suraj.kandpal@intel.com>
    Reviewed-by: Uma Shankar <uma.shankar@intel.com>

--- drivers/gpu/drm/i915/display/intel_colorop.c
+++ drivers/gpu/drm/i915/display/intel_colorop.c
@@ -33,3 +33,9 @@ struct intel_colorop *intel_colorop_create(enum intel_color_block id)
 
 	return colorop;
 }
+
+void intel_colorop_destroy(struct drm_colorop *colorop)
+{
+	drm_colorop_cleanup(colorop);
+	kfree(to_intel_colorop(colorop));
+}
