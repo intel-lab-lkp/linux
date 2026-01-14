83334bf1d310d3361a98b9ff36185f0c93d8e1e5
Hou Wenlong <houwenlong.hwl@antgroup.com>
2026-01-14 21:21:54 +0800

    x86/mm: Hide mm_free_global_asid() definition under 'CONFIG_BROADCAST_TLB_FLUSH'
    When 'CONFIG_BROADCAST_TLB_FLUSH' is not enabled, mm_free_global_asid()
    remains a globally visible symbol and generates a useless function call
    to it in destroy_context(). Therefore, hide the mm_free_global_asid()
    definition under 'CONFIG_BROADCAST_TLB_FLUSH' and provide a static
    inline empty version when it is not enabled to remove the function call.
    
    Signed-off-by: Hou Wenlong <houwenlong.hwl@antgroup.com>

--- arch/x86/include/asm/mmu_context.h
+++ arch/x86/include/asm/mmu_context.h
@@ -139,11 +139,6 @@ static inline void mm_reset_untag_mask(struct mm_struct *mm)
 #define enter_lazy_tlb enter_lazy_tlb
 extern void enter_lazy_tlb(struct mm_struct *mm, struct task_struct *tsk);
 
-#define mm_init_global_asid mm_init_global_asid
-extern void mm_init_global_asid(struct mm_struct *mm);
-
-extern void mm_free_global_asid(struct mm_struct *mm);
-
 /*
  * Init a new mm.  Used on mm copies, like at fork()
  * and on mm's that are brand-new, like at execve().
