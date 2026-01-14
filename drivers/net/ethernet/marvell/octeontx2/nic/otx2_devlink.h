afe9254dfbcbddab460baccda64036ff1679c750
Hariprasad Kelam <hkelam@marvell.com>
2026-01-14 12:27:43 +0530

    Octeontx2-pf: Add support for DMAC_FILTER trap
    This patch adds support for DMAC_FILTER trap.
    
    devlink trap show
    pci/0002:02:00.0:
      name otx2_dmac_filter type drop generic false action trap group l2_drops
    
    to get counter
    devlink -s trap show
    pci/0002:02:00.0:
      name otx2_dmac_filter type drop generic false action trap group l2_drops
        stats:
            rx:
              bytes 0 packets 0 dropped 0
    
    Signed-off-by: Hariprasad Kelam <hkelam@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/nic/otx2_devlink.h
+++ drivers/net/ethernet/marvell/octeontx2/nic/otx2_devlink.h
@@ -8,13 +8,39 @@
 #ifndef	OTX2_DEVLINK_H
 #define	OTX2_DEVLINK_H
 
+#define OTX2_TRAP_DROP(_id, _group_id)					\
+	DEVLINK_TRAP_GENERIC(DROP, DROP, _id,				\
+			     DEVLINK_TRAP_GROUP_GENERIC_ID_##_group_id, \
+			     DEVLINK_TRAP_METADATA_TYPE_F_IN_PORT)
+struct otx2_trap {
+	struct devlink_trap trap;
+};
+
+struct otx2_trap_item {
+	enum devlink_trap_action action;
+	void *trap_ctx;
+};
+
+struct otx2_trap_data {
+	struct otx2_devlink *dl;
+	struct otx2_trap_item *trap_items_arr;
+	u32 traps_count;
+};
+
+static const struct devlink_trap_group otx2_trap_groups_arr[] = {
+	/* No policer is associated with following groups (policerid == 0)*/
+	DEVLINK_TRAP_GROUP_GENERIC(L2_DROPS, 0),
+};
+
 struct otx2_devlink {
 	struct devlink *dl;
 	struct otx2_nic *pfvf;
+	struct otx2_trap_data *trap_data;
 };
 
 /* Devlink APIs */
 int otx2_register_dl(struct otx2_nic *pfvf);
 void otx2_unregister_dl(struct otx2_nic *pfvf);
-
+void otx2_devlink_traps_unregister(struct otx2_nic *pfvf);
+int otx2_devlink_traps_register(struct otx2_nic *pfvf);
 #endif /* RVU_DEVLINK_H */
