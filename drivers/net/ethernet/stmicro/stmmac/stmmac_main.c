f8ceb90eb4d7c2d9f0fa23f088c0112f7dd4de48
Tao Wang <tao03.wang@horizon.auto>
2026-01-14 19:00:31 +0800

    net: stmmac: fix transmit queue timed out after resume
    after resume dev_watchdog() message:
    "NETDEV WATCHDOG: CPU: x: transmit queue x timed out xx ms"
    
    The trigging scenario is as follows:
    When the TSO function sets tx_skbuff_dma[tx_q->cur_tx].last_segment = true,
     and the last_segment value is not cleared in stmmac_free_tx_buffer after
     resume, restarting TSO transmission may incorrectly use
    tx_q->tx_skbuff_dma[first_entry].last_segment = true for a new TSO packet.
    
    When the tx queue has timed out, and the emac TX descriptor is as follows:
    eth0: 221 [0x0000000876d10dd0]: 0x73660cbe 0x8 0x42 0xb04416a0
    eth0: 222 [0x0000000876d10de0]: 0x77731d40 0x8 0x16a0 0x90000000
    
    Descriptor 221 is the TSO header, and descriptor 222 is the TSO payload.
    In the tdes3 (0xb04416a0), bit 29 (first descriptor) and bit 28
    (last descriptor) of the TSO packet 221 DMA descriptor cannot both be
    set to 1 simultaneously. Since descriptor 222 is the actual last
    descriptor, failing to set it properly will cause the EMAC DMA to stop
    and hang.
    
    To solve the issue, set last_segment to false in stmmac_free_tx_buffer:
    tx_q->tx_skbuff_dma[i].last_segment = false.  Do not use the last_segment
     default value and set last_segment to false in stmmac_tso_xmit. This
    will prevent similar issues from occurring in the future.
    
    Fixes: c2837423cb54 ("net: stmmac: Rework TX Coalesce logic")
    
    changelog:
    v1 -> v2:
    	- Modify commit message, del empty line, add fixed commit
    	 information.
    
    Signed-off-by: Tao Wang <tao03.wang@horizon.auto>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_main.c
@@ -1653,6 +1653,7 @@ static void stmmac_free_tx_buffer(struct stmmac_priv *priv,
 
 	tx_q->tx_skbuff_dma[i].buf = 0;
 	tx_q->tx_skbuff_dma[i].map_as_page = false;
+	tx_q->tx_skbuff_dma[i].last_segment = false;
 }
 
 /**
@@ -4448,6 +4449,7 @@ static netdev_tx_t stmmac_tso_xmit(struct sk_buff *skb, struct net_device *dev)
 	if (dma_mapping_error(priv->device, des))
 		goto dma_map_err;
 
+	tx_q->tx_skbuff_dma[first_entry].last_segment = false;
 	stmmac_set_desc_addr(priv, first, des);
 	stmmac_tso_allocator(priv, des + proto_hdr_len, pay_len,
 			     (nfrags == 0), queue);
