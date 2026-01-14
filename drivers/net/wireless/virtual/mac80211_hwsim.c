4ce42b61ee8d99b70959368923c1ecd6ae453c5b
Kavita Kavita <kavita.kavita@oss.qualcomm.com>
2026-01-14 16:49:00 +0530

    wifi: mac80211_hwsim: Declare support for EPPKE authentication protocol
    Advertise support for Enhanced Privacy Protection Key Exchange (EPPKE)
    authentication protocol for testing scenarios.
    
    Signed-off-by: Kavita Kavita <kavita.kavita@oss.qualcomm.com>

--- drivers/net/wireless/virtual/mac80211_hwsim.c
+++ drivers/net/wireless/virtual/mac80211_hwsim.c
@@ -5640,6 +5640,10 @@ static int mac80211_hwsim_new_radio(struct genl_info *info,
 	wiphy_ext_feature_set(hw->wiphy,
 			      NL80211_EXT_FEATURE_BSS_COLOR);
 
+	wiphy_ext_feature_set(hw->wiphy, NL80211_EXT_FEATURE_EPPKE);
+	wiphy_ext_feature_set(hw->wiphy,
+			      NL80211_EXT_FEATURE_ASSOC_FRAME_ENCRYPTION);
+
 	hw->wiphy->interface_modes = param->iftypes;
 
 	/* ask mac80211 to reserve space for magic */
