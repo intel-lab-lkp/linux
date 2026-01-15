e73143e53722acada5e8945e2c34e191b1e5ea39
Ryota Sakamoto <sakamo.ryota@gmail.com>
2026-01-16 03:12:14 +0900

    cgroup/rstat: fix missing prototype warning for bpf_rstat_flush()
    Add the prototype to cgroup-internal.h to resolve the Sparse warning.
    
    The function bpf_rstat_flush() is defined as __weak and global in
    kernel/cgroup/rstat.c, but lack of prototype in header file causes warning
    with Sparse (C=1):
    
      kernel/cgroup/rstat.c:342:22: warning: symbol 'bpf_rstat_flush' was not declared. Should it be static?
    
    Signed-off-by: Ryota Sakamoto <sakamo.ryota@gmail.com>

--- kernel/cgroup/cgroup-internal.h
+++ kernel/cgroup/cgroup-internal.h
@@ -277,6 +277,7 @@ int css_rstat_init(struct cgroup_subsys_state *css);
 void css_rstat_exit(struct cgroup_subsys_state *css);
 int ss_rstat_init(struct cgroup_subsys *ss);
 void cgroup_base_stat_cputime_show(struct seq_file *seq);
+void bpf_rstat_flush(struct cgroup *cgrp, struct cgroup *parent, int cpu);
 
 /*
  * namespace.c
