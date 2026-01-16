ce90abc3da774ba732b315eb93a5e01b96b3717c
Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
2026-01-16 09:27:40 +0200

    media: iris: don't specify ubwc_swizzle in the source code
    The UBWC swizzle is specified both in the Iris driver and in the
    platform UBWC config. Use the platform UBWC configuration instead of
    specifying it directly in the source.
    
    Reviewed-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Reviewed-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Reviewed-by: Dikshita Agarwal <dikshita.agarwal@oss.qualcomm.com>
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>

--- drivers/media/platform/qcom/iris/iris_platform_gen2.c
+++ drivers/media/platform/qcom/iris/iris_platform_gen2.c
@@ -633,9 +633,6 @@ static const struct platform_clk_data sm8550_clk_table[] = {
 
 static struct ubwc_config_data ubwc_config_sm8550 = {
 	.max_channels = 8,
-	.bank_swzl_level = 0,
-	.bank_swz2_level = 1,
-	.bank_swz3_level = 1,
 	.bank_spreading = 1,
 };
 
