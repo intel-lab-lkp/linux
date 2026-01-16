be6675e637a4ed55f62b502bd255d3e66c600d39
Ivan Vecera <ivecera@redhat.com>
2026-01-16 19:45:59 +0100

    dt-bindings: dpll: support acting as pin provider
    Enable DPLL devices to act as pin providers for consumers (such as
    Ethernet controllers or PHYs).
    
    Add the '#dpll-pin-cells' property to the generic dpll-device.yaml
    schema and mark it as required. This allows DPLL nodes to define pin
    specifiers for their connected consumers.
    
    Introduce a new header '<dt-bindings/dpll/dpll.h>' to define pin
    direction macros (`DPLL_PIN_INPUT` and `DPLL_PIN_OUTPUT`). These macros
    are intended to be used in the DT pin specifiers (inside 'dpll-pins'
    properties of consumers) to describe the direction of the signal.
    
    Update the 'microchip,zl30731.yaml' examples to include the new
    '#dpll-pin-cells' property.
    
    The core schema definitions for these properties are being added to
    dt-schema in PR #183.
    
    Link: https://github.com/devicetree-org/dt-schema/pull/183
    Signed-off-by: Ivan Vecera <ivecera@redhat.com>

---
+++ include/dt-bindings/dpll/dpll.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: (GPL-2.0 OR BSD-2-Clause) */
+/*
+ * This header provides constants for DPLL bindings.
+ */
+
+#ifndef _DT_BINDINGS_DPLL_DPLL_H
+#define _DT_BINDINGS_DPLL_DPLL_H
+
+/* DPLL pin direction */
+#define DPLL_PIN_INPUT 0
+#define DPLL_PIN_OUTPUT 1
+
+#endif
