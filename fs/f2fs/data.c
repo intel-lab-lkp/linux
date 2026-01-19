e2bd63c9a1e4ebc923bf64754e32f054615ba543
Chao Yu <chao@kernel.org>
2026-01-19 21:32:29 +0800

    f2fs: check skipped write in f2fs_enable_checkpoint()
    This patch introduces sbi->nr_pages[F2FS_SKIPPED_WRITE] to record any
    skipped write during data flush in f2fs_enable_checkpoint().
    
    So in the loop of data flush, if there is any skipped write in previous
    flush, let's retry sync_inode_sb(), otherwise, all dirty data written
    before f2fs_enable_checkpoint() should have been persisted, then break
    the retry loop.
    
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -3495,6 +3495,15 @@ static inline void account_writeback(struct inode *inode, bool inc)
 	f2fs_up_read(&F2FS_I(inode)->i_sem);
 }
 
+static inline void update_skipped_write(struct f2fs_sb_info *sbi,
+						struct writeback_control *wbc)
+{
+	long skipped = wbc->pages_skipped;
+
+	if (skipped && is_sbi_flag_set(sbi, SBI_ENABLE_CHECKPOINT))
+		atomic_add(skipped, &sbi->nr_pages[F2FS_SKIPPED_WRITE]);
+}
+
 static int __f2fs_write_data_pages(struct address_space *mapping,
 						struct writeback_control *wbc,
 						enum iostat_type io_type)
@@ -3559,10 +3568,13 @@ static int __f2fs_write_data_pages(struct address_space *mapping,
 	 */
 
 	f2fs_remove_dirty_inode(inode);
+
+	update_skipped_write(sbi, wbc);
 	return ret;
 
 skip_write:
 	wbc->pages_skipped += get_dirty_pages(inode);
+	update_skipped_write(sbi, wbc);
 	trace_f2fs_writepages(mapping->host, wbc, DATA);
 	return 0;
 }
