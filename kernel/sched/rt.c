5a9bbabd6fbe30a8fec670896a9aecd2f6c14cac
Michal Koutný <mkoutny@suse.com>
2026-01-19 18:41:21 +0100

    sched/rt: Cleanup global RT bandwidth functions
    The commit 5f6bd380c7bdb ("sched/rt: Remove default bandwidth control")
    and followup changes made a few of the functions unnecessary, drop them
    for simplicity.
    
    Signed-off-by: Michal Koutný <mkoutny@suse.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -2807,13 +2807,6 @@ long sched_group_rt_period(struct task_group *tg)
 	return rt_period_us;
 }
 
-#ifdef CONFIG_SYSCTL
-static int sched_rt_global_constraints(void)
-{
-	return 0;
-}
-#endif /* CONFIG_SYSCTL */
-
 int sched_rt_can_attach(struct task_group *tg, struct task_struct *tsk)
 {
 	/* Don't accept real-time tasks when there is no way for them to run */
@@ -2823,14 +2816,6 @@ int sched_rt_can_attach(struct task_group *tg, struct task_struct *tsk)
 	return 1;
 }
 
-#else /* !CONFIG_RT_GROUP_SCHED: */
-
-#ifdef CONFIG_SYSCTL
-static int sched_rt_global_constraints(void)
-{
-	return 0;
-}
-#endif /* CONFIG_SYSCTL */
 #endif /* !CONFIG_RT_GROUP_SCHED */
 
 #ifdef CONFIG_SYSCTL
@@ -2852,10 +2837,6 @@ static int sched_rt_global_validate(void)
 	mutex_unlock(&rt_constraints_mutex);
 #endif
 	return ret;
-}
-
-static void sched_rt_do_global(void)
-{
 }
 
 static int sched_rt_handler(const struct ctl_table *table, int write, void *buffer,
@@ -2881,11 +2862,6 @@ static int sched_rt_handler(const struct ctl_table *table, int write, void *buff
 		if (ret)
 			goto undo;
 
-		ret = sched_rt_global_constraints();
-		if (ret)
-			goto undo;
-
-		sched_rt_do_global();
 		sched_dl_do_global();
 	}
 	if (0) {
