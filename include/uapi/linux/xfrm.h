6c285b3ae19f3604799c3855deb2bc11e05fc396
Antony Antony <antony.antony@secunet.com>
2026-01-19 09:54:55 +0100

    xfrm: add XFRM_MSG_MIGRATE_STATE for single SA migration
    Add a new netlink method to migrate a single xfrm_state.
    Unlike the existing migration mechanism (SA + policy), this
    supports migrating only the SA and allows changing the reqid.
    
    The reqid is invariant in old migration.
    
    Signed-off-by: Antony Antony <antony.antony@secunet.com>

--- include/uapi/linux/xfrm.h
+++ include/uapi/linux/xfrm.h
@@ -227,6 +227,9 @@ enum {
 #define XFRM_MSG_SETDEFAULT XFRM_MSG_SETDEFAULT
 	XFRM_MSG_GETDEFAULT,
 #define XFRM_MSG_GETDEFAULT XFRM_MSG_GETDEFAULT
+
+	XFRM_MSG_MIGRATE_STATE,
+#define XFRM_MSG_MIGRATE_STATE XFRM_MSG_MIGRATE_STATE
 	__XFRM_MSG_MAX
 };
 #define XFRM_MSG_MAX (__XFRM_MSG_MAX - 1)
@@ -505,6 +508,14 @@ struct xfrm_user_migrate {
 	__u32				reqid;
 	__u16				old_family;
 	__u16				new_family;
+};
+
+struct xfrm_user_migrate_state {
+	struct xfrm_usersa_id id;
+	xfrm_address_t new_saddr;
+	xfrm_address_t new_daddr;
+	__u16 new_family;
+	__u32 new_reqid;
 };
 
 struct xfrm_user_mapping {
