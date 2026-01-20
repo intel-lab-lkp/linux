0628b81543b419b24c4071ae903efa0dfb686934
Cosmin Ratiu <cratiu@nvidia.com>
2026-01-20 09:57:55 +0200

    net/mlx5: Store QoS sched nodes in the sh_devlink
    In order to support cross-esw scheduling, the nodes for all devices need
    to be accessible from the same place so that normalization can work.
    Store them in the shared devlink instance and provide an accessor for
    them.
    
    Protecting against concurrent QoS modifications is now done with
    the shd lock. All calls originating from devlink rate already have the
    shd lock held so only the additional entry points into QoS need to
    acquire it.
    
    As part of this change, the E-Switch qos domain concept was removed.
    E-Switch QoS domains were added with the intention of eventually
    implementing shared qos domains to support cross-esw scheduling in the
    previous approach ([1]), but they are no longer necessary in the new
    approach.
    
    [1] https://lore.kernel.org/netdev/20250213180134.323929-1-tariqt@nvidia.com/
    
    Signed-off-by: Cosmin Ratiu <cratiu@nvidia.com>
    Reviewed-by: Carolina Jubran <cjubran@nvidia.com>
    Signed-off-by: Tariq Toukan <tariqt@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/sh_devlink.c
+++ drivers/net/ethernet/mellanox/mlx5/core/sh_devlink.c
@@ -9,6 +9,16 @@
 static const struct devlink_ops mlx5_shd_ops = {
 };
 
+struct mlx5_shd_priv {
+	struct list_head qos_nodes;
+};
+
+static int mlx5_shd_priv_init(struct mlx5_shd_priv *shd_priv)
+{
+	INIT_LIST_HEAD(&shd_priv->qos_nodes);
+	return 0;
+}
+
 int mlx5_shd_init(struct mlx5_core_dev *dev)
 {
 	u8 *vpd_data __free(kfree) = NULL;
@@ -45,10 +55,17 @@ int mlx5_shd_init(struct mlx5_core_dev *dev)
 	*end = '\0';
 
 	/* Get or create shared devlink instance */
-	devlink = devlink_shd_get(sn, &mlx5_shd_ops, 0);
+	devlink = devlink_shd_get(sn, &mlx5_shd_ops,
+				  sizeof(struct mlx5_shd_priv));
 	kfree(sn);
 	if (!devlink)
 		return -ENOMEM;
+
+	err = mlx5_shd_priv_init(devlink_shd_get_priv(devlink));
+	if (err < 0) {
+		devlink_shd_put(devlink);
+		return err;
+	}
 
 	dev->shd = devlink;
 	return 0;
@@ -60,4 +77,15 @@ void mlx5_shd_uninit(struct mlx5_core_dev *dev)
 		return;
 
 	devlink_shd_put(dev->shd);
+}
+
+struct list_head *mlx5_shd_get_qos_nodes(struct mlx5_core_dev *dev)
+{
+	struct mlx5_shd_priv *shd_priv;
+
+	if (!dev->shd)
+		return NULL;
+	devl_assert_locked(dev->shd);
+	shd_priv = devlink_shd_get_priv(dev->shd);
+	return &shd_priv->qos_nodes;
 }
