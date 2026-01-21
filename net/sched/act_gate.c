284553297b91deb94dcbb9b6e109164c56308e9c
Paul Moses <p@1g4.org>
2026-01-21 13:20:35 +0000

    net/sched: act_gate: zero-initialize netlink dump struct
    Zero-initialize the dump struct before selective assignment to avoid
    leaking stack padding in netlink replies. This matches other actions
    (e.g. act_connmark) that zero-init their dump structs.
    
    Fixes: a51c328df310 ("net: qos: introduce a gate control flow action")
    Cc: stable@vger.kernel.org
    Signed-off-by: Paul Moses <p@1g4.org>

--- net/sched/act_gate.c
+++ net/sched/act_gate.c
@@ -499,15 +499,15 @@ static int tcf_gate_dump(struct sk_buff *skb, struct tc_action *a,
 {
 	unsigned char *b = skb_tail_pointer(skb);
 	struct tcf_gate *gact = to_gate(a);
-	struct tc_gate opt = {
-		.index    = gact->tcf_index,
-		.refcnt   = refcount_read(&gact->tcf_refcnt) - ref,
-		.bindcnt  = atomic_read(&gact->tcf_bindcnt) - bind,
-	};
+	struct tc_gate opt = { };
 	struct tcfg_gate_entry *entry;
 	struct tcf_gate_params *p;
 	struct nlattr *entry_list;
 	struct tcf_t t;
+
+	opt.index = gact->tcf_index;
+	opt.refcnt = refcount_read(&gact->tcf_refcnt) - ref;
+	opt.bindcnt = atomic_read(&gact->tcf_bindcnt) - bind;
 
 	spin_lock_bh(&gact->tcf_lock);
 	opt.action = gact->tcf_action;
