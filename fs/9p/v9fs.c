49d44fb369179ed77a0be1591989aac10b99d886
Remi Pommarel <repk@triplefau.lt>
2026-01-21 20:56:09 +0100

    9p: Introduce option for negative dentry cache retention time
    Add support for a new mount option in v9fs that allows users to specify
    the duration for which negative dentries are retained in the cache. The
    retention time can be set in milliseconds using the ndentrytmo option.
    
    For the same consistency reasons, this option should only be used in
    exclusive or read-only mount scenarios, aligning with the cache=loose
    usage.
    
    Signed-off-by: Remi Pommarel <repk@triplefau.lt>

--- fs/9p/v9fs.c
+++ fs/9p/v9fs.c
@@ -39,7 +39,7 @@ enum {
 	 * source if we rejected it as EINVAL */
 	Opt_source,
 	/* Options that take integer arguments */
-	Opt_debug, Opt_dfltuid, Opt_dfltgid, Opt_afid,
+	Opt_debug, Opt_dfltuid, Opt_dfltgid, Opt_afid, Opt_ndentrytmo,
 	/* String options */
 	Opt_uname, Opt_remotename, Opt_cache, Opt_cachetag,
 	/* Options that take no arguments */
@@ -93,6 +93,7 @@ const struct fs_parameter_spec v9fs_param_spec[] = {
 	fsparam_string	("access",	Opt_access),
 	fsparam_flag	("posixacl",	Opt_posixacl),
 	fsparam_u32	("locktimeout",	Opt_locktimeout),
+	fsparam_s32	("ndentrytmo",	Opt_ndentrytmo),
 
 	/* client options */
 	fsparam_u32	("msize",	Opt_msize),
@@ -159,6 +160,8 @@ int v9fs_show_options(struct seq_file *m, struct dentry *root)
 			   from_kgid_munged(&init_user_ns, v9ses->dfltgid));
 	if (v9ses->afid != ~0)
 		seq_printf(m, ",afid=%u", v9ses->afid);
+	if (v9ses->ndentry_timeout != 0)
+		seq_printf(m, ",ndentrytmo=%d", v9ses->ndentry_timeout);
 	if (strcmp(v9ses->uname, V9FS_DEFUSER) != 0)
 		seq_printf(m, ",uname=%s", v9ses->uname);
 	if (strcmp(v9ses->aname, V9FS_DEFANAME) != 0)
@@ -335,6 +338,10 @@ int v9fs_parse_param(struct fs_context *fc, struct fs_parameter *param)
 			return -EINVAL;
 		}
 		session_opts->session_lock_timeout = (long)result.uint_32 * HZ;
+		break;
+
+	case Opt_ndentrytmo:
+		session_opts->ndentry_timeout = result.int_32;
 		break;
 
 	/* Options for client */
