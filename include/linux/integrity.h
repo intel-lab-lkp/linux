cef5e3170d129ed37ab3f8d02599c259235baf0a
Frederick Lawler <fred@cloudflare.com>
2026-01-22 10:49:31 -0600

    ima: Fallback to ctime check for FS without kstat.change_cookie
    Commit 1cf7e834a6fb ("xfs: switch to multigrain timestamps")
    introduced a means to track change detection for an inode
    via ctime updates, opposed to setting kstat.change_cookie to
    an i_version when calling into xfs_vn_getattr().
    
    This introduced a regression for IMA such that an action
    performed on a LOWER inode on a stacked file systems always
    requires a re-evaluation if the LOWER file system does not
    leverage kstat.change_cookie to track inode i_version or lacks
    i_version support all together.
    
    In the case of stacking XFS on XFS, an action on either the LOWER or UPPER
    will require re-evaluation. Stacking TMPFS on XFS for instance, once the
    inode is UPPER is mutated, IMA resumes normal behavior because TMPFS
    leverages generic_fillattr() to update the change cookie.
    
    This is because IMA caches kstat.change_cookie to compare against an
    inode's i_version directly in integrity_inode_attrs_changed(), and thus
    could be out of date depending on how file systems set
    kstat.change_cookie.
    
    To address this, require integrity_inode_attrs_changed() to query
    vfs_getattr_nosec() to compare the cached version against
    kstat.change_cookie directly. This ensures that when updates occur,
    we're accessing the same changed inode version on changes, and fallback
    to compare against kstat.ctime when STATX_CHANGE_COOKIE is missing from
    result mask.
    
    Lastly, because EVM still relies on querying and caching a inode's
    i_version directly, the integrity_inode_attrs_changed() falls back to the
    original inode.i_version != cached comparison.
    
    Link: https://lore.kernel.org/all/aTspr4_h9IU4EyrR@CMGLRV3
    Suggested-by: Jeff Layton <jlayton@kernel.org>
    Signed-off-by: Frederick Lawler <fred@cloudflare.com>

--- include/linux/integrity.h
+++ include/linux/integrity.h
@@ -9,6 +9,8 @@
 
 #include <linux/fs.h>
 #include <linux/iversion.h>
+#include <linux/kernel.h>
+#include <linux/time64.h>
 
 enum integrity_status {
 	INTEGRITY_PASS = 0,
@@ -51,14 +53,39 @@ integrity_inode_attrs_store(struct integrity_inode_attributes *attrs,
 
 /*
  * On stacked filesystems detect whether the inode or its content has changed.
+ *
+ * Must be called in process context.
  */
 static inline bool
 integrity_inode_attrs_changed(const struct integrity_inode_attributes *attrs,
-			      const struct inode *inode)
+			      struct file *file, struct inode *inode)
 {
-	return (inode->i_sb->s_dev != attrs->dev ||
-		inode->i_ino != attrs->ino ||
-		!inode_eq_iversion(inode, attrs->version));
+	struct kstat stat;
+
+	might_sleep();
+
+	if (inode->i_sb->s_dev != attrs->dev || inode->i_ino != attrs->ino)
+		return true;
+
+	/*
+	 * EVM currently relies on backing inode i_version. While IS_I_VERSION
+	 * is not a good indicator of i_version support, this still retains
+	 * the logic such that a re-evaluation should still occur for EVM, and
+	 * only for IMA if vfs_getattr_nosec() fails.
+	 */
+	if (!file || vfs_getattr_nosec(&file->f_path, &stat,
+				       STATX_CHANGE_COOKIE | STATX_CTIME,
+				       AT_STATX_SYNC_AS_STAT))
+		return !IS_I_VERSION(inode) ||
+			!inode_eq_iversion(inode, attrs->version);
+
+	if (stat.result_mask & STATX_CHANGE_COOKIE)
+		return stat.change_cookie != attrs->version;
+
+	if (stat.result_mask & STATX_CTIME)
+		return timespec64_to_ns(&stat.ctime) != (s64)attrs->version;
+
+	return true;
 }
 
 
