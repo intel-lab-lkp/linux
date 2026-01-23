0d42bf274f99f36f0f4c752f53ad5c8a966e2bce
Beata Michalska <beata.michalska@arm.com>
2026-01-23 14:13:08 +0100

    rust: dma: allow drivers to tune max segment size
    Make dma_set_max_seg_size() available to Rust so drivers can perform
    standard DMA setup steps.
    
    Signed-off-by: Beata Michalska <beata.michalska@arm.com>

--- rust/helpers/dma.c
+++ rust/helpers/dma.c
@@ -43,3 +43,8 @@ size_t rust_helper_dma_max_mapping_size(struct device *dev)
 {
 	return dma_max_mapping_size(dev);
 }
+
+void rust_helper_dma_set_max_seg_size(struct device *dev, unsigned int size)
+{
+	dma_set_max_seg_size(dev, size);
+}
