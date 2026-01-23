2a1e3ffecbb00841d4aed11b085665de691663c3
Heiner Kallweit <hkallweit1@gmail.com>
2026-01-23 07:50:20 +0100

    net: phy: make PHY fixup support always built-in
    PHY fixup registration is used from platform code in init phase only.
    Let's move the PHY fixup code from the modular part of phylib to the
    always built-in part of phylib. This allows to annotate the fixup
    registration as __init.
    
    phy_needs_fixup() and phy_scan_fixups() wouldn't have to be moved to
    the built-in part of phylib. But doing so allows to fully factor out
    fixup support into its own source code file, and make struct
    phy_fixup and phy_fixup_list strictly private to phy_fixup.c.
    
    phy_scan_fixups() is used after init phase only, then phy_fixup_list
    is read-only. So we don't need the mutex when accessing the list.
    Also when registering PHY fixups the mutex isn't needed, because
    fixup registration is done sequentially from platform init code.
    Actually there is only one platform with more than one fixup.
    
    Signed-off-by: Heiner Kallweit <hkallweit1@gmail.com>

--- drivers/net/phy/phylib-internal.h
+++ drivers/net/phy/phylib-internal.h
@@ -19,6 +19,7 @@ void of_set_phy_eee_broken(struct phy_device *phydev);
 void of_set_phy_timing_role(struct phy_device *phydev);
 int phy_speed_down_core(struct phy_device *phydev);
 void phy_check_downshift(struct phy_device *phydev);
+int phy_scan_fixups(struct phy_device *phydev);
 
 int genphy_c45_read_eee_adv(struct phy_device *phydev, unsigned long *adv);
 
