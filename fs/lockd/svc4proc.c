7de6abdf51d8604ea78e3b15abd842c57b3aa64b
Chuck Lever <chuck.lever@oracle.com>
2026-01-23 13:52:52 -0500

    lockd: Prepare share helpers for xdrgen conversion
    In order to convert the NLMv4 server-side XDR functions to use
    xdrgen, the internal share helpers need to be decoupled from the
    NLMv3-specific struct nlm_args. NLMv4 procedures will use
    different argument structures once they are converted.
    
    Refactor nlmsvc_share_file() and nlmsvc_unshare_file() to accept
    individual arguments (oh, access, mode) instead of the common
    struct nlm_args. This allows both protocol versions to call these
    helpers without forcing a common argument structure.
    
    While here, add kdoc comments to both functions and fix a comment
    typo in the unshare path.
    
    Signed-off-by: Chuck Lever <chuck.lever@oracle.com>

--- fs/lockd/svc4proc.c
+++ fs/lockd/svc4proc.c
@@ -1062,7 +1062,8 @@ nlm4svc_proc_share(struct svc_rqst *rqstp)
 		return resp->status == nlm_drop_reply ? rpc_drop_reply :rpc_success;
 
 	/* Now try to create the share */
-	resp->status = nlmsvc_share_file(host, file, argp);
+	resp->status = nlmsvc_share_file(host, file, &lock->oh,
+					 argp->fsm_access, argp->fsm_mode);
 
 	dprintk("lockd: SHARE         status %d\n", ntohl(resp->status));
 	nlmsvc_release_lockowner(lock);
@@ -1100,8 +1101,8 @@ nlm4svc_proc_unshare(struct svc_rqst *rqstp)
 	if (resp->status)
 		return resp->status == nlm_drop_reply ? rpc_drop_reply :rpc_success;
 
-	/* Now try to lock the file */
-	resp->status = nlmsvc_unshare_file(host, file, argp);
+	/* Now try to unshare the file */
+	resp->status = nlmsvc_unshare_file(host, file, &lock->oh);
 
 	dprintk("lockd: UNSHARE       status %d\n", ntohl(resp->status));
 	nlmsvc_release_lockowner(lock);
