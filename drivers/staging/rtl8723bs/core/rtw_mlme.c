4681c635014a272ab8f16c658f5871ca56dbb300
Michael Huang <tehsiu.huang@gmail.com>
2026-01-23 01:57:33 -0800

    staging: rtl8723bs: remove redundant else after break
    In _rtw_join_timeout_handler(), an else statement followed a break
    in the previous logic. Removing this else reduces the indentation
    level of the following code and makes the function easier to read,
    while maintaining the original logic by ensuring that disconnect
    indication is still called when roaming fails.
    
    Signed-off-by: Michael Huang <tehsiu.huang@gmail.com>

--- drivers/staging/rtl8723bs/core/rtw_mlme.c
+++ drivers/staging/rtl8723bs/core/rtw_mlme.c
@@ -1559,12 +1559,10 @@ void _rtw_join_timeout_handler(struct timer_list *t)
 				do_join_r = rtw_do_join(adapter);
 				if (do_join_r != _SUCCESS)
 					continue;
-
-				break;
-			} else {
-				rtw_indicate_disconnect(adapter);
 				break;
 			}
+			rtw_indicate_disconnect(adapter);
+			break;
 		}
 
 	} else {
