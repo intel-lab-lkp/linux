5f10afc24ec66cef539c9c585b17684a2a62cd64
Xie Yuanbin <qq570070308@gmail.com>
2026-01-26 00:40:11 +0800

    ARM: entry: use exit_to_user_mode() instead of user_enter_callable()
    According to user_enter_callable()'s comment, the function is obsolete.
    
    Since exit_to_user_mode() is a inline function, add
    arm_exit_to_user_mode_no_work_pending() to call exit_to_user_mode(),
    thereby replacing user_enter_callable() and asm_trace_hardirqs_on().
    
    Also move stackleak_erase_on_task_stack() to
    arm_exit_to_user_mode_no_work_pending().
    
    Signed-off-by: Xie Yuanbin <qq570070308@gmail.com>

---
+++ arch/arm/include/asm/entry-common.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef ARCH_ARM_ENTRY_COMMON_H
+#define ARCH_ARM_ENTRY_COMMON_H
+
+asmlinkage __section(".entry.text")
+void arm_exit_to_user_mode_no_work_pending(void);
+
+#endif
---
+++ arch/arm/kernel/entry.c
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GPL-2.0-only
+#include <linux/linkage.h>
+#include <linux/irq-entry-common.h>
+#include <linux/kstack_erase.h>
+
+/*
+ * arm_exit_to_user_mode_no_work_pending - Fixup state when exiting to user mode
+ *
+ * The calling code satisfies (arch_irqs_disabled() && !lockdep_hardirqs_enabled()).
+ * When the function returns, the state satisfies (arch_irqs_disabled() &&
+ * lockdep_hardirqs_enabled()).
+ */
+asmlinkage __section(".entry.text")
+void arm_exit_to_user_mode_no_work_pending(void)
+{
+	exit_to_user_mode();
+#ifdef CONFIG_KSTACK_ERASE
+	stackleak_erase_on_task_stack();
+#endif
+}
