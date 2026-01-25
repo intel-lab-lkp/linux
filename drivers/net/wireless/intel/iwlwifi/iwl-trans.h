c30d7c4bd15c9a8ae5c97dc3a1b7a0f5849928ce
Chris Bainbridge <chris.bainbridge@gmail.com>
2026-01-25 23:33:34 +0000

    Revert "wifi: iwlwifi: trans: remove STATUS_SUSPENDED"
    This reverts commit e769f6f27ffe41331e00b69a33aa8a34db4dd830.
    
    The removal of STATUS_SUSPENDED (which tracks suspend/resume state)
    resulted in an intermittent race condition on resume. The fault can be
    reproduced by carrying out repeated suspend/resume cycles while passing
    traffic through the NIC. A typical failure looks like:
    
    [  141.093986] iwlwifi 0000:01:00.0: Error sending SCAN_CFG_CMD: time out after 2000ms.
    [  141.094057] iwlwifi 0000:01:00.0: Current CMD queue read_ptr 441 write_ptr 442
    [  141.094864] iwlwifi 0000:01:00.0: Start IWL Error Log Dump:
    [  141.094866] iwlwifi 0000:01:00.0: Transport status: 0x00000042, valid: 6
    [  141.094870] iwlwifi 0000:01:00.0: Loaded firmware version: 89.7f71c7f4.0 ty-a0-gf-a0-89.ucode
    [  141.094873] iwlwifi 0000:01:00.0: 0x01000071 | ADVANCED_SYSASSERT
    ...
    [  141.098401] iwlwifi 0000:01:00.0: iwl_mvm_check_rt_status failed, device is gone during suspend
    
    The kernel then oops due to a null pointer dereference in
    iwl_mvm_realloc_queues_after_restart().
    
    Fixes: e769f6f27ffe ("wifi: iwlwifi: trans: remove STATUS_SUSPENDED")
    Closes: https://yhbt.net/lore/linux-wireless/aTDoDiD55qlUZ0pn@debian.local/
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Chris Bainbridge <chris.bainbridge@gmail.com>

--- drivers/net/wireless/intel/iwlwifi/iwl-trans.h
+++ drivers/net/wireless/intel/iwlwifi/iwl-trans.h
@@ -290,6 +290,8 @@ static inline void iwl_free_rxb(struct iwl_rx_cmd_buffer *r)
  *	the firmware state yet
  * @STATUS_TRANS_RESET_IN_PROGRESS: reset is still in progress, don't
  *	attempt another reset yet
+ * @STATUS_SUSPENDED: device is suspended, don't send commands that
+ *	aren't marked accordingly
  */
 enum iwl_trans_status {
 	STATUS_SYNC_HCMD_ACTIVE,
@@ -303,6 +305,7 @@ enum iwl_trans_status {
 	STATUS_IN_SW_RESET,
 	STATUS_RESET_PENDING,
 	STATUS_TRANS_RESET_IN_PROGRESS,
+	STATUS_SUSPENDED,
 };
 
 static inline int
