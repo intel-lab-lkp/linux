bc7238658b490b2714114993e575742d12174d8f
Xie Yuanbin <qq570070308@gmail.com>
2026-01-26 00:40:12 +0800

    ARM: entry: fix missing statistics of rseq exit
    rseq_syscall_exit_to_user_mode() or rseq_irqentry_exit_to_user_mode() is
    missing on arm when returning to user mode, this leads to missing
    statistics of rseq exit.
    
    Add rseq_irqentry_exit_to_user_mode() to
    arm_exit_to_user_mode_no_work_pending().
    
    Fixes: 5412910487d0 ("rseq: Expose lightweight statistics in debugfs")
    
    Signed-off-by: Xie Yuanbin <qq570070308@gmail.com>

--- arch/arm/kernel/entry.c
+++ arch/arm/kernel/entry.c
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 #include <linux/linkage.h>
+#include <linux/rseq_entry.h>
 #include <linux/irq-entry-common.h>
 #include <linux/kstack_erase.h>
 
@@ -13,6 +14,7 @@
 asmlinkage __section(".entry.text")
 void arm_exit_to_user_mode_no_work_pending(void)
 {
+	rseq_irqentry_exit_to_user_mode();
 	exit_to_user_mode();
 #ifdef CONFIG_KSTACK_ERASE
 	stackleak_erase_on_task_stack();
