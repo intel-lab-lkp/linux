f5111c36f27a4f81d24d5417d5ccd7445b9763bd
Jinjie Ruan <ruanjinjie@huawei.com>
2026-01-26 16:13:34 +0800

    arm64: kexec: Add support for crashkernel CMA reservation
    Commit 35c18f2933c5 ("Add a new optional ",cma" suffix to the
    crashkernel= command line option") and commit ab475510e042 ("kdump:
    implement reserve_crashkernel_cma") added CMA support for kdump
    crashkernel reservation.
    
    Crash kernel memory reservation wastes production resources if too
    large, risks kdump failure if too small, and faces allocation difficulties
    on fragmented systems due to contiguous block constraints. The new
    CMA-based crashkernel reservation scheme splits the "large fixed
    reservation" into a "small fixed region + large CMA dynamic region": the
    CMA memory is available to userspace during normal operation to avoid
    waste, and is reclaimed for kdump upon crash—saving memory while
    improving reliability.
    
    So extend crashkernel CMA reservation support to arm64. The following
    changes are made to enable CMA reservation:
    
    - Parse and obtain the CMA reservation size along with other crashkernel
      parameters.
    - Call reserve_crashkernel_cma() to allocate the CMA region for kdump.
    - Include the CMA-reserved ranges for kdump kernel to use.
    - Exclude the CMA-reserved ranges from the crash kernel memory to
      prevent them from being exported through /proc/vmcore.
    
    Update kernel-parameters.txt to document CMA support for crashkernel on
    arm64 architecture.
    
    Signed-off-by: Jinjie Ruan <ruanjinjie@huawei.com>

--- arch/arm64/kernel/machine_kexec_file.c
+++ arch/arm64/kernel/machine_kexec_file.c
@@ -48,7 +48,7 @@ static int prepare_elf_headers(void **addr, unsigned long *sz)
 	u64 i;
 	phys_addr_t start, end;
 
-	nr_ranges = 2; /* for exclusion of crashkernel region */
+	nr_ranges = 2 + crashk_cma_cnt; /* for exclusion of crashkernel region */
 	for_each_mem_range(i, &start, &end)
 		nr_ranges++;
 
@@ -64,6 +64,12 @@ static int prepare_elf_headers(void **addr, unsigned long *sz)
 		cmem->nr_ranges++;
 	}
 
+	for (i = 0; i < crashk_cma_cnt; i++) {
+		cmem->ranges[cmem->nr_ranges].start = crashk_cma_ranges[i].start;
+		cmem->ranges[cmem->nr_ranges].end = crashk_cma_ranges[i].end;
+		cmem->nr_ranges++;
+	}
+
 	/* Exclude crashkernel region */
 	ret = crash_exclude_mem_range(cmem, crashk_res.start, crashk_res.end);
 	if (ret)
@@ -71,6 +77,13 @@ static int prepare_elf_headers(void **addr, unsigned long *sz)
 
 	if (crashk_low_res.end) {
 		ret = crash_exclude_mem_range(cmem, crashk_low_res.start, crashk_low_res.end);
+		if (ret)
+			goto out;
+	}
+
+	for (i = 0; i < crashk_cma_cnt; ++i) {
+		ret = crash_exclude_mem_range(cmem, crashk_cma_ranges[i].start,
+					      crashk_cma_ranges[i].end);
 		if (ret)
 			goto out;
 	}
