a2b5c94577d13b157320ba5b934314ee6656817a
Chen Ni <nichen@iscas.ac.cn>
2026-01-26 11:55:51 +0800

    phy: marvell: phy-mvebu-cp110-comphy: Remove unnecessary NULL check before clk_disable_unprepare()
    clk_disable_unprepare() already checks NULL by using IS_ERR_OR_NULL.
    Remove unneeded NULL check for clk here.
    
    Signed-off-by: Chen Ni <nichen@iscas.ac.cn>

--- drivers/phy/marvell/phy-mvebu-cp110-comphy.c
+++ drivers/phy/marvell/phy-mvebu-cp110-comphy.c
@@ -984,14 +984,11 @@ dis_mg_domain_clk:
 
 static void mvebu_comphy_disable_unprepare_clks(struct mvebu_comphy_priv *priv)
 {
-	if (priv->axi_clk)
-		clk_disable_unprepare(priv->axi_clk);
+	clk_disable_unprepare(priv->axi_clk);
 
-	if (priv->mg_core_clk)
-		clk_disable_unprepare(priv->mg_core_clk);
+	clk_disable_unprepare(priv->mg_core_clk);
 
-	if (priv->mg_domain_clk)
-		clk_disable_unprepare(priv->mg_domain_clk);
+	clk_disable_unprepare(priv->mg_domain_clk);
 }
 
 static int mvebu_comphy_probe(struct platform_device *pdev)
