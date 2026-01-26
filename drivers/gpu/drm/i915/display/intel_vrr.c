79e67f3616e15ffcf75d4c878efe66d1b7d1aefa
Jouni Högander <jouni.hogander@intel.com>
2026-01-26 09:59:53 +0200

    drm/i915/vrr: Prepare to Use TRANS_PUSH mechanism for PSR frame change
    On Lunarlake and onwards it is possible to generate PSR "frame change"
    event using TRANS_PUSH mechanism. Implement function to enable this and
    take PSR into account in intel_vrr_send_push.
    
    v7:
      - HAS_PSR_FRAME_CHANGE macro moved to separate patch and renamed as
        HAS_PSR_TRANS_PUSH_FRAME_CHANGE
      - use intel_psr_use_trans_push instead of HAS_PSR_FRAME_CHANGE in
        intel_psr_trigger_frame_change
      - moved calling intel_vrr_psr_frame_change_enable away from this patch
    v6:
      - add HAS_PSR_FRAME_CHANGE macro
      - use TRANS_PUSH in instead of TRAN_VRR_CTL
    v5: use intel_psr_use_trans_push for intel_vrr_psr_frame_change_enable
    v4:
      - use rmw when enabling/disabling transcoder
      - set TRANS_PUSH_EN conditionally in intel_vrr_send_push
      - do not call intel_vrr_send_push from intel_psr_trigger_frame_change
      - do not enable using TRANS_PUSH mechanism for PSR "Frame Change"
    v3:
      - use rmw when enabling/disabling
      - keep LNL_TRANS_PUSH_PSR_PR_EN set always on LunarLake and onwards
    v2: use intel_vrr_trans_push_enabled_set_clear instead of rmw
    
    Signed-off-by: Jouni Högander <jouni.hogander@intel.com>

--- drivers/gpu/drm/i915/display/intel_vrr.c
+++ drivers/gpu/drm/i915/display/intel_vrr.c
@@ -698,7 +698,7 @@ void intel_vrr_send_push(struct intel_dsb *dsb,
 	struct intel_display *display = to_intel_display(crtc_state);
 	enum transcoder cpu_transcoder = crtc_state->cpu_transcoder;
 
-	if (!crtc_state->vrr.enable)
+	if (!crtc_state->vrr.enable && !intel_psr_use_trans_push(crtc_state))
 		return;
 
 	if (dsb)
@@ -920,7 +920,8 @@ static void intel_vrr_tg_disable(const struct intel_crtc_state *old_crtc_state)
 				       VRR_STATUS_VRR_EN_LIVE, 1000))
 		drm_err(display->drm, "Timed out waiting for VRR live status to clear\n");
 
-	intel_de_write(display, TRANS_PUSH(display, cpu_transcoder), 0);
+	intel_de_rmw(display, TRANS_PUSH(display, cpu_transcoder),
+		     TRANS_PUSH_EN, 0);
 }
 
 void intel_vrr_enable(const struct intel_crtc_state *crtc_state)
@@ -971,6 +972,15 @@ void intel_vrr_transcoder_disable(const struct intel_crtc_state *old_crtc_state)
 
 	if (intel_vrr_always_use_vrr_tg(display))
 		intel_vrr_tg_disable(old_crtc_state);
+}
+
+void intel_vrr_psr_frame_change_enable(const struct intel_crtc_state *crtc_state)
+{
+	struct intel_display *display = to_intel_display(crtc_state);
+	enum transcoder cpu_transcoder = crtc_state->cpu_transcoder;
+
+	intel_de_write(display, TRANS_PUSH(display, cpu_transcoder),
+		       trans_vrr_push(crtc_state, false));
 }
 
 bool intel_vrr_is_fixed_rr(const struct intel_crtc_state *crtc_state)
