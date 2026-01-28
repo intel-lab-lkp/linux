5a55d36d780f9b1e2123269976b96762a34c8a82
John Madieu <john.madieu.xa@bp.renesas.com>
2026-01-28 14:16:34 +0100

    clk: renesas: rzv2h-cpg: Add support for init_{off|asserted} clocks/resets
    Some peripherals may be left enabled by the bootloader but should be
    explicitly disabled by the kernel to ensure a known initial state.
    This is particularly important for PCIe which requires proper
    initialization sequencing.
    
    Add new macros DEF_MOD_INIT_OFF() and DEF_RST_INIT_ASSERTED() to declare
    module clocks that should be turned off and resets that should be
    asserted during CPG probe if found in the opposite state.
    
    Signed-off-by: John Madieu <john.madieu.xa@bp.renesas.com>

--- drivers/clk/renesas/rzv2h-cpg.h
+++ drivers/clk/renesas/rzv2h-cpg.h
@@ -250,6 +250,7 @@ enum clk_types {
  * @parent: id of parent clock
  * @critical: flag to indicate the clock is critical
  * @no_pm: flag to indicate PM is not supported
+ * @init_off: flag to indicate the clock should be turned off during probe
  * @on_index: control register index
  * @on_bit: ON bit
  * @mon_index: monitor register index
@@ -262,6 +263,7 @@ struct rzv2h_mod_clk {
 	u16 parent;
 	bool critical;
 	bool no_pm;
+	bool init_off;
 	u8 on_index;
 	u8 on_bit;
 	s8 mon_index;
@@ -269,14 +271,15 @@ struct rzv2h_mod_clk {
 	s8 ext_clk_mux_index;
 };
 
-#define DEF_MOD_BASE(_name, _mstop, _parent, _critical, _no_pm, _onindex, \
-		     _onbit, _monindex, _monbit, _ext_clk_mux_index) \
+#define DEF_MOD_BASE(_name, _mstop, _parent, _critical, _no_pm, _init_off, \
+		     _onindex, _onbit, _monindex, _monbit, _ext_clk_mux_index) \
 	{ \
 		.name = (_name), \
 		.mstop_data = (_mstop), \
 		.parent = (_parent), \
 		.critical = (_critical), \
 		.no_pm = (_no_pm), \
+		.init_off = (_init_off), \
 		.on_index = (_onindex), \
 		.on_bit = (_onbit), \
 		.mon_index = (_monindex), \
@@ -285,17 +288,20 @@ struct rzv2h_mod_clk {
 	}
 
 #define DEF_MOD(_name, _parent, _onindex, _onbit, _monindex, _monbit, _mstop) \
-	DEF_MOD_BASE(_name, _mstop, _parent, false, false, _onindex, _onbit, _monindex, _monbit, -1)
+	DEF_MOD_BASE(_name, _mstop, _parent, false, false, false, _onindex, _onbit, _monindex, _monbit, -1)
 
 #define DEF_MOD_CRITICAL(_name, _parent, _onindex, _onbit, _monindex, _monbit, _mstop) \
-	DEF_MOD_BASE(_name, _mstop, _parent, true, false, _onindex, _onbit, _monindex, _monbit, -1)
+	DEF_MOD_BASE(_name, _mstop, _parent, true, false, false, _onindex, _onbit, _monindex, _monbit, -1)
+
+#define DEF_MOD_INIT_OFF(_name, _parent, _onindex, _onbit, _monindex, _monbit, _mstop) \
+	DEF_MOD_BASE(_name, _mstop, _parent, false, false, true, _onindex, _onbit, _monindex, _monbit, -1)
 
 #define DEF_MOD_NO_PM(_name, _parent, _onindex, _onbit, _monindex, _monbit, _mstop) \
-	DEF_MOD_BASE(_name, _mstop, _parent, false, true, _onindex, _onbit, _monindex, _monbit, -1)
+	DEF_MOD_BASE(_name, _mstop, _parent, false, true, false, _onindex, _onbit, _monindex, _monbit, -1)
 
 #define DEF_MOD_MUX_EXTERNAL(_name, _parent, _onindex, _onbit, _monindex, _monbit, _mstop, \
 			     _ext_clk_mux_index) \
-	DEF_MOD_BASE(_name, _mstop, _parent, false, false, _onindex, _onbit, _monindex, _monbit, \
+	DEF_MOD_BASE(_name, _mstop, _parent, false, false, false, _onindex, _onbit, _monindex, _monbit, \
 		     _ext_clk_mux_index)
 
 /**
@@ -305,24 +311,36 @@ struct rzv2h_mod_clk {
  * @reset_bit: reset bit
  * @mon_index: monitor register index
  * @mon_bit: monitor bit
+ * @init_asserted: flag to indicate the reset should be asserted during probe
  */
 struct rzv2h_reset {
 	u8 reset_index;
 	u8 reset_bit;
 	u8 mon_index;
 	u8 mon_bit;
+	bool init_asserted;
 };
 
-#define DEF_RST_BASE(_resindex, _resbit, _monindex, _monbit)	\
+#define DEF_RST_BASE(_resindex, _resbit, _monindex, _monbit, _init_asserted)	\
 	{ \
 		.reset_index = (_resindex), \
 		.reset_bit = (_resbit), \
 		.mon_index = (_monindex), \
 		.mon_bit = (_monbit), \
+		.init_asserted = (_init_asserted), \
 	}
 
 #define DEF_RST(_resindex, _resbit, _monindex, _monbit)	\
-	DEF_RST_BASE(_resindex, _resbit, _monindex, _monbit)
+	DEF_RST_BASE(_resindex, _resbit, _monindex, _monbit, false)
+
+/**
+ * DEF_RST_INIT_ASSERTED - Define a reset that should be asserted during probe
+ *
+ * Use this for peripherals that require their reset to be asserted at boot
+ * to ensure a known initial state before the peripheral driver takes over.
+ */
+#define DEF_RST_INIT_ASSERTED(_reset_index, _reset_bit, _mon_index, _mon_bit) \
+	DEF_RST_BASE(_reset_index, _reset_bit, _mon_index, _mon_bit, true)
 
 /**
  * struct rzv2h_cpg_info - SoC-specific CPG Description
