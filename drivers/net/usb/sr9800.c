2ce281b5fd78f27d865d2020f0016ac7f6083b7c
Ethan Nelson-Moore <enelsonmoore@gmail.com>
2026-01-29 23:37:49 -0800

    net: usb: introduce usbnet_mii_ioctl helper function
    Many USB network drivers use identical code to pass ioctl
    requests on to the MII layer. Reduce code duplication by
    refactoring this code into a helper function.
    
    Reviewed-by: Vadim Fedorenko <vadim.fedorenko@linux.dev> (v1)
    Signed-off-by: Ethan Nelson-Moore <enelsonmoore@gmail.com>

--- drivers/net/usb/sr9800.c
+++ drivers/net/usb/sr9800.c
@@ -478,13 +478,6 @@ static void sr_get_drvinfo(struct net_device *net,
 	strscpy(info->version, DRIVER_VERSION, sizeof(info->version));
 }
 
-static int sr_ioctl(struct net_device *net, struct ifreq *rq, int cmd)
-{
-	struct usbnet *dev = netdev_priv(net);
-
-	return generic_mii_ioctl(&dev->mii, if_mii(rq), cmd, NULL);
-}
-
 static int sr_set_mac_address(struct net_device *net, void *p)
 {
 	struct usbnet *dev = netdev_priv(net);
@@ -677,7 +670,7 @@ static const struct net_device_ops sr9800_netdev_ops = {
 	.ndo_get_stats64	= dev_get_tstats64,
 	.ndo_set_mac_address	= sr_set_mac_address,
 	.ndo_validate_addr	= eth_validate_addr,
-	.ndo_eth_ioctl		= sr_ioctl,
+	.ndo_eth_ioctl		= usbnet_mii_ioctl,
 	.ndo_set_rx_mode        = sr_set_multicast,
 };
 
