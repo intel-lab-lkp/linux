4666f1167793ae4b4a540dabed65c85f5dab2c01
Jordan Niethe <jniethe@nvidia.com>
2026-01-30 22:10:40 +1100

    mm/migrate_device: Make migrate_device_{pfns,range}() take mpfns
    A future change will remove device private pages from the physical
    address space. This will mean that device private pages no longer have a
    pfn.
    
    This causes an issue for migrate_device_{pfns,range}() which take pfn
    parameters. Depending on if the device is MEMORY_DEVICE_PRIVATE or
    MEMORY_DEVICE_COHERENT will effect how that parameter should be
    interpreted.
    
    A MIGRATE_PFN flag will be introduced that distinguishes between mpfns
    that contain a pfn vs an offset into device private memory, we will take
    advantage of that here.
    
    Update migrate_device_{pfns,range}() to take a mpfn instead of pfn.
    
    Update the users of migrate_device_{pfns,range}() to pass in an mpfn.
    
    To support this change, update
    dpagemap_devmem_ops::populate_devmem_pfn() to instead return mpfns and
    rename accordingly.
    
    Signed-off-by: Jordan Niethe <jniethe@nvidia.com>

--- drivers/gpu/drm/xe/xe_svm.c
+++ drivers/gpu/drm/xe/xe_svm.c
@@ -10,6 +10,7 @@
 #include <drm/drm_pagemap.h>
 #include <drm/drm_pagemap_util.h>
 
+#include <linux/migrate.h>
 #include "xe_bo.h"
 #include "xe_exec_queue_types.h"
 #include "xe_gt_stats.h"
@@ -752,8 +753,8 @@ static struct drm_buddy *vram_to_buddy(struct xe_vram_region *vram)
 	return &vram->ttm.mm;
 }
 
-static int xe_svm_populate_devmem_pfn(struct drm_pagemap_devmem *devmem_allocation,
-				      unsigned long npages, unsigned long *pfn)
+static int xe_svm_populate_devmem_mpfn(struct drm_pagemap_devmem *devmem_allocation,
+				       unsigned long npages, unsigned long *pfn)
 {
 	struct xe_bo *bo = to_xe_bo(devmem_allocation);
 	struct ttm_resource *res = bo->ttm.resource;
@@ -769,7 +770,7 @@ static int xe_svm_populate_devmem_pfn(struct drm_pagemap_devmem *devmem_allocati
 		int i;
 
 		for (i = 0; i < drm_buddy_block_size(buddy, block) >> PAGE_SHIFT; ++i)
-			pfn[j++] = block_pfn + i;
+			pfn[j++] = migrate_pfn(block_pfn + i);
 	}
 
 	return 0;
@@ -777,7 +778,7 @@ static int xe_svm_populate_devmem_pfn(struct drm_pagemap_devmem *devmem_allocati
 
 static const struct drm_pagemap_devmem_ops dpagemap_devmem_ops = {
 	.devmem_release = xe_svm_devmem_release,
-	.populate_devmem_pfn = xe_svm_populate_devmem_pfn,
+	.populate_devmem_mpfn = xe_svm_populate_devmem_mpfn,
 	.copy_to_devmem = xe_svm_copy_to_devmem,
 	.copy_to_ram = xe_svm_copy_to_ram,
 };
