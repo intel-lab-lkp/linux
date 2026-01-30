99c375fb2e2d3aebddf779d9f884c3f659340c21
Lukas Herbolt <lukas@herbolt.com>
2026-01-30 16:42:08 +0100

    xfs: Use xarray to track SB UUIDs instead of plain array.
    Removing the plain array to track the UUIDs and switch
    xarray to make more readable.
    
    Signed-off-by: Lukas Herbolt <lukas@herbolt.com>

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -689,7 +689,8 @@ xfs_daddr_to_agbno(struct xfs_mount *mp, xfs_daddr_t d)
 	return (xfs_agblock_t) do_div(ld, mp->m_sb.sb_agblocks);
 }
 
-extern void	xfs_uuid_table_free(void);
+extern void xfs_uuid_table_destroy(void);
+
 uint64_t	xfs_default_resblks(struct xfs_mount *mp,
 			enum xfs_free_counter ctr);
 extern int	xfs_mountfs(xfs_mount_t *mp);
