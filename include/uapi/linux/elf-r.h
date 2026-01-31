e366e52218ce1a76aacd0a035e3f334aa0d37410
Daniel Palmer <daniel@thingy.jp>
2026-01-31 16:44:32 +0900

    elf: Add relocation types used by nolibc
    nolibc based programs are gaining the ability to relocate themselves
    so that they can support PIE without needing a linker or special
    crt from the toolchain.
    
    Add the required relocation types.
    
    Signed-off-by: Daniel Palmer <daniel@thingy.jp>

---
+++ include/uapi/linux/elf-r.h
@@ -0,0 +1,27 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+#ifndef _LINUX_ELF_R_H
+#define _LINUX_ELF_R_H
+
+/* These constants define various ELF relocation types */
+#define R_386_RELATIVE		8
+
+#define R_68K_RELATIVE		22
+
+#define R_AARCH64_RELATIVE	1027
+
+#define R_AMD64_RELATIVE	8
+
+#define R_ARM_RELATIVE		23
+
+#define R_LARCH_RELATIVE	3
+
+#define R_PPC_RELATIVE		22
+
+#define R_RISCV_RELATIVE	3
+
+#define R_SH_RELATIVE		165
+
+#define R_SPARC_NONE		0
+#define R_SPARC_RELATIVE	22
+
+#endif /* _LINUX_ELF_R_H */
