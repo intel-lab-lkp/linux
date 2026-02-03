49ecd8b21223b26fed4015895c237480235e8a70
Isaku Yamahata <isaku.yamahata@intel.com>
2026-02-03 10:17:12 -0800

    KVM: selftests: Add VMCS access test to APIC timer virtualization
    Add test case for VMCS access of nVMX APIC timer virtualization.  It tests
    nVMX emulation of a tertiary processor-based VM executing controls MSR,
    tertiary processor-base VM execution controls, and related VMCS fields by
    L1 vCPU and L2 vCPU.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- tools/testing/selftests/kvm/include/x86/apic.h
+++ tools/testing/selftests/kvm/include/x86/apic.h
@@ -32,6 +32,8 @@
 #define	APIC_SPIV	0xF0
 #define		APIC_SPIV_FOCUS_DISABLED	(1 << 9)
 #define		APIC_SPIV_APIC_ENABLED		(1 << 8)
+#define APIC_ISR	0x100
+#define APIC_ISR_NR	0x8
 #define APIC_IRR	0x200
 #define	APIC_ICR	0x300
 #define	APIC_LVTCMCI	0x2f0
