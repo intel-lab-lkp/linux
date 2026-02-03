d6715ee692acf79b279b9d0a99371f66857a4308
Sun Jian <sun.jian.kdev@gmail.com>
2026-02-03 22:55:11 +0800

    netfilter: amanda: fix RCU pointer typing for nf_nat_amanda_hook
    Sparse reports "incompatible types in comparison expression" error in
    nf_conntrack_amanda.c because nf_nat_amanda_hook is used with
    rcu_dereference() but lacks the proper __rcu annotation.
    
    Fix this by correctly placing the __rcu annotation inside the pointer
    parentheses in both the declaration and definition. This allows the
    standard rcu_dereference() to work correctly.
    
    Suggested-by: Florian Westphal <fw@strlen.de>
    Signed-off-by: Sun Jian <sun.jian.kdev@gmail.com>

--- net/netfilter/nf_conntrack_amanda.c
+++ net/netfilter/nf_conntrack_amanda.c
@@ -37,13 +37,13 @@ MODULE_PARM_DESC(master_timeout, "timeout for the master connection");
 module_param(ts_algo, charp, 0400);
 MODULE_PARM_DESC(ts_algo, "textsearch algorithm to use (default kmp)");
 
-unsigned int (*nf_nat_amanda_hook)(struct sk_buff *skb,
-				   enum ip_conntrack_info ctinfo,
-				   unsigned int protoff,
-				   unsigned int matchoff,
-				   unsigned int matchlen,
-				   struct nf_conntrack_expect *exp)
-				   __read_mostly;
+unsigned int (__rcu *nf_nat_amanda_hook)(struct sk_buff *skb,
+					 enum ip_conntrack_info ctinfo,
+					 unsigned int protoff,
+					 unsigned int matchoff,
+					 unsigned int matchlen,
+					 struct nf_conntrack_expect *exp)
+					 __read_mostly;
 EXPORT_SYMBOL_GPL(nf_nat_amanda_hook);
 
 enum amanda_strings {
@@ -98,7 +98,12 @@ static int amanda_help(struct sk_buff *skb,
 	u_int16_t len;
 	__be16 port;
 	int ret = NF_ACCEPT;
-	typeof(nf_nat_amanda_hook) nf_nat_amanda;
+	unsigned int (*nf_nat_amanda)(struct sk_buff *skb,
+				      enum ip_conntrack_info ctinfo,
+				      unsigned int protoff,
+				      unsigned int matchoff,
+				      unsigned int matchlen,
+				      struct nf_conntrack_expect *exp);
 
 	/* Only look at packets from the Amanda server */
 	if (CTINFO2DIR(ctinfo) == IP_CT_DIR_ORIGINAL)
