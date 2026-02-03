651e2c68f044b5ae5444399ea150c25d032c8162
Isaku Yamahata <isaku.yamahata@intel.com>
2026-02-03 10:16:50 -0800

    KVM: VMX: Update APIC timer virtualization on apicv changed
    VMX APIC timer virtualization requires the virtual-interrupt delivery.
    It's part of KVM apicv, and when apicv state is changed, the
    refresh_apicv_exec_ctrl() callback is invoked.  Export a lapic function to
    update which timer to use for APIC timer emulation and make the VMX backend
    call it.
    
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- arch/x86/kvm/lapic.c
+++ arch/x86/kvm/lapic.c
@@ -1898,6 +1898,12 @@ static void apic_update_apic_virt_timer(struct kvm_lapic *apic)
 		apic_cancel_apic_virt_timer(apic);
 }
 
+void kvm_update_apic_virt_timer(struct kvm_vcpu *vcpu)
+{
+	apic_update_apic_virt_timer(vcpu->arch.apic);
+}
+EXPORT_SYMBOL_GPL(kvm_update_apic_virt_timer);
+
 static void apic_update_lvtt(struct kvm_lapic *apic)
 {
 	u32 timer_mode = kvm_lapic_get_reg(apic, APIC_LVTT) &
