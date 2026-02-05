adea344e1cf4f96933b3d420b5636ad1db16f616
Keith Busch <kbusch@kernel.org>
2026-02-05 13:25:33 -0800

    pci: make reset_subordinate hotplug safe
    Use the slot reset method when resetting the bridge if the bus contains
    hot plug slots. This fixes spurious hot plug events that are triggered
    by the secondary bus reset that bypasses the slot's detection disabling.
    
    Resetting a bridge's subordinate bus can be done like this:
    
      # echo 1 > /sys/bus/pci/devices/0000:50:01.0/reset_subordinate
    
    Prior to this patch, an example kernel message may show something like:
    
      pcieport 0000:50:01.0: pciehp: Slot(40): Link Down
    
    With this change, the pciehp driver ignores the link event during the
    reset, so may show this message instead:
    
      pcieport 0000:50:01.0: pciehp: Slot(40): Link Down/Up ignored
    
    Signed-off-by: Keith Busch <kbusch@kernel.org>

--- drivers/pci/pci.c
+++ drivers/pci/pci.c
@@ -51,6 +51,10 @@ unsigned int pci_pm_d3hot_delay;
 
 static void pci_pme_list_scan(struct work_struct *work);
 
+#define PCI_SLOT_RESET_MASKED true
+#define PCI_SLOT_RESET_UNMASKED false
+static int pci_reset_bridge(struct pci_dev *bridge, bool save);
+
 static LIST_HEAD(pci_pme_list);
 static DEFINE_MUTEX(pci_pme_list_mutex);
 static DECLARE_DELAYED_WORK(pci_pme_work, pci_pme_list_scan);
@@ -5423,29 +5427,7 @@ static int pci_bus_reset(struct pci_bus *bus, bool probe)
  */
 int pci_bus_error_reset(struct pci_dev *bridge)
 {
-	struct pci_bus *bus = bridge->subordinate;
-	struct pci_slot *slot;
-
-	if (!bus)
-		return -ENOTTY;
-
-	mutex_lock(&pci_slot_mutex);
-	if (list_empty(&bus->slots))
-		goto bus_reset;
-
-	list_for_each_entry(slot, &bus->slots, list)
-		if (pci_probe_reset_slot(slot))
-			goto bus_reset;
-
-	list_for_each_entry(slot, &bus->slots, list)
-		if (pci_slot_reset(slot, PCI_RESET_DO_RESET))
-			goto bus_reset;
-
-	mutex_unlock(&pci_slot_mutex);
-	return 0;
-bus_reset:
-	mutex_unlock(&pci_slot_mutex);
-	return pci_bus_reset(bridge->subordinate, PCI_RESET_DO_RESET);
+	return pci_reset_bridge(bridge, PCI_SLOT_RESET_UNMASKED);
 }
 
 /**
@@ -5484,6 +5466,49 @@ static int pci_try_reset_bus(struct pci_bus *bus)
 		rc = -EAGAIN;
 
 	return rc;
+}
+
+static int pci_reset_bridge(struct pci_dev *bridge, bool masked)
+{
+	struct pci_bus *bus = bridge->subordinate;
+	struct pci_slot *slot;
+
+	if (!bus)
+		return -ENOTTY;
+
+	mutex_lock(&pci_slot_mutex);
+	if (list_empty(&bus->slots))
+		goto bus_reset;
+
+	list_for_each_entry(slot, &bus->slots, list)
+		if (pci_probe_reset_slot(slot))
+			goto bus_reset;
+
+	list_for_each_entry(slot, &bus->slots, list) {
+		int ret;
+
+		if (masked)
+			ret = pci_try_reset_slot(slot);
+		else
+			ret = pci_slot_reset(slot, PCI_RESET_DO_RESET);
+
+		if (ret)
+			goto bus_reset;
+	}
+
+	mutex_unlock(&pci_slot_mutex);
+	return 0;
+bus_reset:
+	mutex_unlock(&pci_slot_mutex);
+
+	if (masked)
+		return pci_try_reset_bus(bus);
+	return pci_bus_reset(bridge->subordinate, PCI_RESET_DO_RESET);
+}
+
+int pci_try_reset_bridge(struct pci_dev *bridge)
+{
+	return pci_reset_bridge(bridge, PCI_SLOT_RESET_MASKED);
 }
 
 /**
