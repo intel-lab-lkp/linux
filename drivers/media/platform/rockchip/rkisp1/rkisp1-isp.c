4a0887aab1949a220527cb237591b112925331bb
Isaac Scott <isaac.scott@ideasonboard.com>
2026-02-05 10:32:06 +0000

    media: rkisp1-isp: Set correct data mode for YUV bypass
    The rkisp1 features a 'bypass' mode for RAW and YUV formats. This
    disables all ISP blocks, and makes the rkisp1 display input data from
    the MIPI CSI receiver at the output, unmodified.
    
    To determine whether we can activate bypass, we can detect whether both
    the source and sink formats are YUV. If they are, we must configure the
    ISP to expect a YUV input, interpreting H/VSYNC signals as data
    enable / disable.
    
    Signed-off-by: Isaac Scott <isaac.scott@ideasonboard.com>

--- drivers/media/platform/rockchip/rkisp1/rkisp1-isp.c
+++ drivers/media/platform/rockchip/rkisp1/rkisp1-isp.c
@@ -240,7 +240,9 @@ static int rkisp1_config_isp(struct rkisp1_isp *isp,
 		}
 	} else if (sink_fmt->pixel_enc == V4L2_PIXEL_ENC_YUV) {
 		acq_mult = 2;
-		if (mbus_type == V4L2_MBUS_CSI2_DPHY) {
+		if (src_fmt->pixel_enc == V4L2_PIXEL_ENC_YUV) {
+			isp_ctrl = RKISP1_CIF_ISP_CTRL_ISP_MODE_DATA_MODE;
+		} else if (mbus_type == V4L2_MBUS_CSI2_DPHY) {
 			isp_ctrl = RKISP1_CIF_ISP_CTRL_ISP_MODE_ITU601;
 		} else {
 			if (mbus_type == V4L2_MBUS_BT656)
