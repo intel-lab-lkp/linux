941f6771084febc2d0c61d165b0169a3f81ccb59
Tan Kai Zhe <kaizhetan@yahoo.com>
2026-02-05 23:12:29 +0800

    staging: rtl8723bs: remove unnecessary blank lines
    Adhere to Linux kernel coding style.
    
    Reported by checkpatch:
    
    CHECK: Blank lines aren't necessary before a close brace '}'
    
    Signed-off-by: Tan Kai Zhe <kaizhetan@yahoo.com>

--- drivers/staging/rtl8723bs/core/rtw_cmd.c
+++ drivers/staging/rtl8723bs/core/rtw_cmd.c
@@ -1224,7 +1224,6 @@ u8 traffic_status_watchdog(struct adapter *padapter, u8 from_timer)
 	pmlmepriv->LinkDetectInfo.bHigherBusyTxTraffic = bHigherBusyTxTraffic;
 
 	return bEnterPS;
-
 }
 
 static void dynamic_chk_wk_hdl(struct adapter *padapter)
@@ -1446,7 +1445,6 @@ u8 rtw_dm_ra_mask_wk_cmd(struct adapter *padapter, u8 *psta)
 exit:
 
 	return res;
-
 }
 
 u8 rtw_ps_cmd(struct adapter *padapter)
@@ -1516,9 +1514,7 @@ static void rtw_chk_hi_queue_hdl(struct adapter *padapter)
 		} else {/* re check again */
 			rtw_chk_hi_queue_cmd(padapter);
 		}
-
 	}
-
 }
 
 u8 rtw_chk_hi_queue_cmd(struct adapter *padapter)
@@ -1877,7 +1873,6 @@ void rtw_createbss_cmd_callback(struct adapter *padapter, struct cmd_obj *pcmd)
 
 		spin_unlock_bh(&pmlmepriv->scanned_queue.lock);
 		/*  we will set _FW_LINKED when there is one more sat to join us (rtw_stassoc_event_callback) */
-
 	}
 
 createbss_cmd_fail:
