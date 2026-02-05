cd29c08691fc74b0298624200a9ba819c47c12c9
Li RongQing <lirongqing@baidu.com>
2026-02-05 00:59:33 -0500

    nfp: flower: use GFP_KERNEL in idr_preload
    idr_preload() is called in a sleepable context before acquiring a
    spinlock. Change the allocation flag from GFP_ATOMIC to GFP_KERNEL
    to allow direct reclaim and improve allocation success rate under
    memory pressure.
    
    Signed-off-by: Li RongQing <lirongqing@baidu.com>

--- drivers/net/ethernet/netronome/nfp/flower/main.c
+++ drivers/net/ethernet/netronome/nfp/flower/main.c
@@ -63,7 +63,7 @@ nfp_flower_get_internal_port_id(struct nfp_app *app, struct net_device *netdev)
 	if (id > 0)
 		return id;
 
-	idr_preload(GFP_ATOMIC);
+	idr_preload(GFP_KERNEL);
 	spin_lock_bh(&priv->internal_ports.lock);
 	id = idr_alloc(&priv->internal_ports.port_ids, netdev,
 		       NFP_MIN_INT_PORT_ID, NFP_MAX_INT_PORT_ID, GFP_ATOMIC);
