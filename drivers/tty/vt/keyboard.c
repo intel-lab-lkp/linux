b288c14f9aed83060884b77f7f134dc7cfde61b9
Soham Kute <officialsohamkute@gmail.com>
2026-02-08 00:57:08 +0530

    tty: vt: keyboard: fix general protection fault in k_meta
    syzbot reported a general protection fault in k_meta() caused by
    dereferencing an invalid keyboard pointer when checking META mode.
    
    Add a defensive check to ensure the keyboard pointer is valid before
    calling vc_kbd_mode().
    
    Reported-by: syzbot+03f79366754268a0f20c@syzkaller.appspotmail.com
    
    Signed-off-by: Soham Kute <officialsohamkute@gmail.com>

--- drivers/tty/vt/keyboard.c
+++ drivers/tty/vt/keyboard.c
@@ -880,7 +880,7 @@ static void k_meta(struct vc_data *vc, unsigned char value, char up_flag)
 	if (up_flag)
 		return;
 
-	if (vc_kbd_mode(kbd, VC_META)) {
+	if (kbd && vc_kbd_mode(kbd, VC_META)) {
 		put_queue(vc, '\033');
 		put_queue(vc, value);
 	} else
