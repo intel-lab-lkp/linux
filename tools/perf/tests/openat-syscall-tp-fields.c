45c72ffa66a92b7a261ba96ec1c7c6ac1a3ad99c
Ian Rogers <irogers@google.com>
2026-02-09 09:40:13 -0800

    perf evsel: Refactor evsel__intval to perf_sample__intval
    The evsel argument to evsel__intval, and similar functions, is
    unnecessary as it can be read from the sample. Remove the evsel and
    rename the function to match that the data is coming from the sample.
    
    Signed-off-by: Ian Rogers <irogers@google.com>

--- tools/perf/tests/openat-syscall-tp-fields.c
+++ tools/perf/tests/openat-syscall-tp-fields.c
@@ -118,7 +118,7 @@ static int test__syscall_openat_tp_fields(struct test_suite *test __maybe_unused
 					goto out_delete_evlist;
 				}
 
-				tp_flags = evsel__intval(evsel, &sample, "flags");
+				tp_flags = perf_sample__intval(&sample, "flags");
 				perf_sample__exit(&sample);
 				if (flags != tp_flags) {
 					pr_debug("%s: Expected flags=%#x, got %#x\n",
