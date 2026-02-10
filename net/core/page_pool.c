829c53efe87e024e41c2d2620b9ab3a8a5035212
Byungchul Park <byungchul@sk.com>
2026-02-10 20:47:12 +0900

    mm: introduce a new page type for page pool in page type
    Currently, the condition 'page->pp_magic == PP_SIGNATURE' is used to
    determine if a page belongs to a page pool.  However, with the planned
    removal of @pp_magic, we should instead leverage the page_type in struct
    page, such as PGTY_netpp, for this purpose.
    
    Introduce and use the page type APIs e.g. PageNetpp(), __SetPageNetpp(),
    and __ClearPageNetpp() instead, and remove the existing APIs accessing
    @pp_magic e.g. page_pool_page_is_pp(), netmem_or_pp_magic(), and
    netmem_clear_pp_magic().
    
    Plus, add @page_type to struct net_iov at the same offset as struct page
    so as to use the page_type APIs for struct net_iov as well.  While at it,
    reorder @type and @owner in struct net_iov to avoid a hole and
    increasing the struct size.
    
    This work was inspired by the following link:
    
      https://lore.kernel.org/all/582f41c0-2742-4400-9c81-0d46bf4e8314@gmail.com/
    
    While at it, move the sanity check for page pool to on the free path.
    
    Suggested-by: David Hildenbrand <david@redhat.com>
    Co-developed-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>
    Signed-off-by: Byungchul Park <byungchul@sk.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Acked-by: Zi Yan <ziy@nvidia.com>
    Reviewed-by: Toke Høiland-Jørgensen <toke@redhat.com>

--- net/core/page_pool.c
+++ net/core/page_pool.c
@@ -702,8 +702,18 @@ s32 page_pool_inflight(const struct page_pool *pool, bool strict)
 
 void page_pool_set_pp_info(struct page_pool *pool, netmem_ref netmem)
 {
+	netmem_ref clr_ref;
+
 	netmem_set_pp(netmem, pool);
-	netmem_or_pp_magic(netmem, PP_SIGNATURE);
+
+	/* XXX: Now that the offset of page_type is shared between
+	 * struct page and net_iov, just cast the netmem to struct page
+	 * unconditionally by clearing NET_IOV if any, no matter whether
+	 * it comes from struct net_iov or struct page.  This should be
+	 * adjusted once the offset is no longer shared.
+	 */
+	clr_ref = (netmem_ref)((__force unsigned long)netmem & ~NET_IOV);
+	__SetPageNetpp(__netmem_to_page(clr_ref));
 
 	/* Ensuring all pages have been split into one fragment initially:
 	 * page_pool_set_pp_info() is only called once for every page when it
@@ -718,7 +728,17 @@ void page_pool_set_pp_info(struct page_pool *pool, netmem_ref netmem)
 
 void page_pool_clear_pp_info(netmem_ref netmem)
 {
-	netmem_clear_pp_magic(netmem);
+	netmem_ref clr_ref;
+
+	/* XXX: Now that the offset of page_type is shared between
+	 * struct page and net_iov, just cast the netmem to struct page
+	 * unconditionally by clearing NET_IOV if any, no matter whether
+	 * it comes from struct net_iov or struct page.  This should be
+	 * adjusted once the offset is no longer shared.
+	 */
+	clr_ref = (netmem_ref)((__force unsigned long)netmem & ~NET_IOV);
+	__ClearPageNetpp(__netmem_to_page(clr_ref));
+
 	netmem_set_pp(netmem, NULL);
 }
 
