db0e4645eec65057985f5e6372b57e02fa60b4da
Pavel Begunkov <asml.silence@gmail.com>
2026-02-11 19:04:52 +0000

    io_uring: introduce callback driven main loop
    The io_uring_enter() has a fixed order of execution: it submits
    requests, waits for completions, and returns to the user. Allow to
    optionally replace it with a custom loop driven by a callback called
    loop_step. The basic requirements to the callback is that it should be
    able to submit requests, wait for completions, parse them and repeat.
    Most of the communication including parameter passing can be implemented
    via shared memory.
    
    The callback should return IOU_LOOP_CONTINUE to continue execution or
    IOU_LOOP_STOP to return to the user space. Note that the kernel may
    decide to prematurely terminate it as well, e.g. in case the process was
    signalled or killed.
    
    The hook takes a structure with parameters. It can be used to ask the
    kernel to wait for CQEs by setting cq_wait_idx to the CQE index it wants
    to wait for. Spurious wake ups are possible and even likely, the callback
    is expected to handle it. There will be more parameters in the future
    like timeout.
    
    It can be used with kernel callbacks, for example, as a slow path
    deprecation mechanism overwiting SQEs and emulating the wanted
    behaviour, however it's more useful together with BPF programs
    implemented in following patches.
    
    Note that keeping it separately from the normal io_uring wait loop
    makes things much simpler and cleaner. It keeps it in one place instead
    of spreading a bunch of checks in different places including disabling
    the submission path. It holds the lock by default, which is a better fit
    for BPF synchronisation and the loop execution model. It nicely avoids
    existing quirks like forced wake ups on timeout request completion. And
    it should be easier to implement new features.
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

--- io_uring/io_uring.c
+++ io_uring/io_uring.c
@@ -95,6 +95,7 @@
 #include "eventfd.h"
 #include "wait.h"
 #include "bpf_filter.h"
+#include "loop.h"
 
 #define SQE_COMMON_FLAGS (IOSQE_FIXED_FILE | IOSQE_IO_LINK | \
 			  IOSQE_IO_HARDLINK | IOSQE_ASYNC)
@@ -2581,6 +2582,11 @@ SYSCALL_DEFINE6(io_uring_enter, unsigned int, fd, u32, to_submit,
 	 */
 	if (unlikely(smp_load_acquire(&ctx->flags) & IORING_SETUP_R_DISABLED))
 		goto out;
+
+	if (io_has_loop_ops(ctx)) {
+		ret = io_run_loop(ctx);
+		goto out;
+	}
 
 	/*
 	 * For SQ polling, the thread will do all submissions and completions.
