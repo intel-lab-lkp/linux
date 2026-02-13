a7e03c4b3e1660a380243ef587b9170761281b39
Dave Jiang <dave.jiang@intel.com>
2026-02-13 15:40:38 -0700

    cxl: Fix race of nvdimm_bus object when creating nvdimm objects
    Found issue during running of cxl-translate.sh unit test. Adding a 3s
    sleep right before the test seems to make the issue reproduce fairly
    consistently. The cxl_translate module has dependency on cxl_acpi and
    causes orphaned nvdimm objects to reprobe after cxl_acpi is removed.
    The nvdimm_bus object is registered by the cxl_nvb object when
    cxl_acpi_probe() is called. With the nvdimm_bus object missing,
    __nd_device_register() will trigger NULL pointer dereference when
    accessing the dev->parent that points to &nvdimm_bus->dev.
    
    [  192.884510] BUG: kernel NULL pointer dereference, address: 000000000000006c
    [  192.895383] Hardware name: QEMU Standard PC (Q35 + ICH9, 2009), BIOS edk2-20250812-19.fc42 08/12/2025
    [  192.897721] Workqueue: cxl_port cxl_bus_rescan_queue [cxl_core]
    [  192.899459] RIP: 0010:kobject_get+0xc/0x90
    [  192.924871] Call Trace:
    [  192.925959]  <TASK>
    [  192.926976]  ? pm_runtime_init+0xb9/0xe0
    [  192.929712]  __nd_device_register.part.0+0x4d/0xc0 [libnvdimm]
    [  192.933314]  __nvdimm_create+0x206/0x290 [libnvdimm]
    [  192.936662]  cxl_nvdimm_probe+0x119/0x1d0 [cxl_pmem]
    [  192.940245]  cxl_bus_probe+0x1a/0x60 [cxl_core]
    [  192.943349]  really_probe+0xde/0x380
    
    This patch also relies on the previous change where
    devm_cxl_add_nvdimm_bridge() is called from drivers/cxl/pmem.c instead
    of drivers/cxl/core.c to ensure the dependency of cxl_acpi on cxl_pmem.
    
    1. Set probe_type of cxl_nvb to PROBE_FORCE_SYNCHRONOUS to ensure the
       driver is probed synchronously when add_device() is called.
    2. Add a check in __devm_cxl_add_nvdimm_bridge() to ensure that the
       cxl_nvb driver is attached during cxl_acpi_probe().
    3. Take the cxl_root uport_dev lock and the cxl_nvb->dev lock in
       devm_cxl_add_nvdimm() before checking nvdimm_bus is valid.
    4. Set cxl_nvdimm flag to CXL_NVD_F_INVALIDATED so cxl_nvdimm_probe()
       will exit with -EBUSY.
    
    The removal of cxl_nvdimm devices should prevent any orphaned devices
    from probing once the nvdimm_bus is gone.
    
    Fixes: 21083f51521f ("cxl/pmem: Register 'pmem' / cxl_nvdimm devices")
    Signed-off-by: Dave Jiang <dave.jiang@intel.com>

--- drivers/cxl/pmem.c
+++ drivers/cxl/pmem.c
@@ -143,6 +143,9 @@ static int cxl_nvdimm_probe(struct device *dev)
 	struct nvdimm *nvdimm;
 	int rc;
 
+	if (test_bit(CXL_NVD_F_INVALIDATED, &cxl_nvd->flags))
+		return -EBUSY;
+
 	set_exclusive_cxl_commands(mds, exclusive_cmds);
 	rc = devm_add_action_or_reset(dev, clear_exclusive, mds);
 	if (rc)
@@ -323,8 +326,10 @@ static int detach_nvdimm(struct device *dev, void *data)
 	scoped_guard(device, dev) {
 		if (dev->driver) {
 			cxl_nvd = to_cxl_nvdimm(dev);
-			if (cxl_nvd->cxlmd && cxl_nvd->cxlmd->cxl_nvb == data)
+			if (cxl_nvd->cxlmd && cxl_nvd->cxlmd->cxl_nvb == data) {
 				release = true;
+				set_bit(CXL_NVD_F_INVALIDATED, &cxl_nvd->flags);
+			}
 		}
 	}
 	if (release)
@@ -367,6 +372,7 @@ static struct cxl_driver cxl_nvdimm_bridge_driver = {
 	.probe = cxl_nvdimm_bridge_probe,
 	.id = CXL_DEVICE_NVDIMM_BRIDGE,
 	.drv = {
+		.probe_type = PROBE_FORCE_SYNCHRONOUS,
 		.suppress_bind_attrs = true,
 	},
 };
