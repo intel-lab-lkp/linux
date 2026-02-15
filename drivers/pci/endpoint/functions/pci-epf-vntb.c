dd7c60dea67c177e85e60b0e855a1cf0c555dd17
Koichiro Den <den@valinux.co.jp>
2026-02-16 00:09:11 +0900

    PCI: endpoint: pci-epf-vntb: Fix MSI doorbell IRQ unwind
    epf_ntb_db_bar_init_msi_doorbell() requests ntb->db_count doorbell IRQs
    and then performs additional MSI doorbell setup that may still fail.
    The error path unwinds the requested IRQs, but it uses a loop variable
    that is reused later in the function. When a later step fails, the
    unwind can run with an unexpected index value and leave some IRQs
    requested.
    
    Track the number of successfully requested IRQs separately and use that
    counter for the unwind so all previously requested IRQs are freed on
    failure.
    
    Fixes: dc693d606644 ("PCI: endpoint: pci-epf-vntb: Add MSI doorbell support")
    Signed-off-by: Koichiro Den <den@valinux.co.jp>

--- drivers/pci/endpoint/functions/pci-epf-vntb.c
+++ drivers/pci/endpoint/functions/pci-epf-vntb.c
@@ -523,6 +523,7 @@ static int epf_ntb_db_bar_init_msi_doorbell(struct epf_ntb *ntb,
 					    enum pci_barno barno)
 {
 	struct pci_epf *epf = ntb->epf;
+	unsigned int req;
 	dma_addr_t low, high;
 	struct msi_msg *msg;
 	size_t sz;
@@ -533,14 +534,14 @@ static int epf_ntb_db_bar_init_msi_doorbell(struct epf_ntb *ntb,
 	if (ret)
 		return ret;
 
-	for (i = 0; i < ntb->db_count; i++) {
-		ret = request_irq(epf->db_msg[i].virq, epf_ntb_doorbell_handler,
+	for (req = 0; req < ntb->db_count; req++) {
+		ret = request_irq(epf->db_msg[req].virq, epf_ntb_doorbell_handler,
 				  0, "pci_epf_vntb_db", ntb);
 
 		if (ret) {
 			dev_err(&epf->dev,
 				"Failed to request doorbell IRQ: %d\n",
-				epf->db_msg[i].virq);
+				epf->db_msg[req].virq);
 			goto err_free_irq;
 		}
 	}
@@ -598,8 +599,8 @@ static int epf_ntb_db_bar_init_msi_doorbell(struct epf_ntb *ntb,
 	return 0;
 
 err_free_irq:
-	for (i--; i >= 0; i--)
-		free_irq(epf->db_msg[i].virq, ntb);
+	while (req)
+		free_irq(epf->db_msg[--req].virq, ntb);
 
 	pci_epf_free_doorbell(ntb->epf);
 	return ret;
