0f1f4bee20133324864f8306ee5ba16bc9938623
Miquel Sabaté Solà <mssola@mssola.com>
2026-02-16 01:28:06 +0100

    btrfs: report filesystem shutdown via fserror
    Commit 347b7042fb26 ("Merge patch series "fs: generic file IO error
    reporting"") has introduced a common framework for reporting errors to
    fsnotify in a standard way.
    
    One of the functions being introduced is 'fserror_report_shutdown' that,
    when combined with the experimental support for shutdown in btrfs, it
    means that user-space can also easily detect whenever a btrfs filesystem
    has been marked as shutdown.
    
    Signed-off-by: Miquel Sabaté Solà <mssola@mssola.com>

--- fs/btrfs/fs.h
+++ fs/btrfs/fs.h
@@ -33,6 +33,7 @@
 #include "async-thread.h"
 #include "block-rsv.h"
 #include "messages.h"
+#include <linux/fserror.h>
 
 struct inode;
 struct super_block;
@@ -1199,8 +1200,10 @@ static inline void btrfs_force_shutdown(struct btrfs_fs_info *fs_info)
 	 * So here we only mark the fs error without flipping it RO.
 	 */
 	WRITE_ONCE(fs_info->fs_error, -EIO);
-	if (!test_and_set_bit(BTRFS_FS_STATE_EMERGENCY_SHUTDOWN, &fs_info->fs_state))
+	if (!test_and_set_bit(BTRFS_FS_STATE_EMERGENCY_SHUTDOWN, &fs_info->fs_state)) {
 		btrfs_crit(fs_info, "emergency shutdown");
+		fserror_report_shutdown(fs_info->sb, GFP_KERNEL);
+	}
 }
 
 /*
