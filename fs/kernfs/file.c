8e8cdd1348110bcffc967e238b14f987e8abd311
T.J. Mercier <tjmercier@google.com>
2026-02-17 19:22:31 -0800

    kernfs: send IN_DELETE_SELF and IN_IGNORED on file deletion
    Currently some kernfs files (e.g. cgroup.events, memory.events) support
    inotify watches for IN_MODIFY, but unlike with regular filesystems, they
    do not receive IN_DELETE_SELF or IN_IGNORED events when they are
    removed.
    
    This creates a problem for processes monitoring cgroups. For example, a
    service monitoring memory.events for memory.high breaches needs to know
    when a cgroup is removed to clean up its state. Where it's known that a
    cgroup is removed when all processes die, without IN_DELETE_SELF the
    service must resort to inefficient workarounds such as:
    1.  Periodically scanning procfs to detect process death (wastes CPU and
        is susceptible to PID reuse).
    2.  Placing an additional IN_DELETE watch on the parent directory
        (wastes resources managing double the watches).
    3.  Holding a pidfd for every monitored cgroup (can exhaust file
        descriptors).
    
    This patch enables kernfs to send IN_DELETE_SELF and IN_IGNORED events.
    This allows applications to rely on a single existing watch on the file
    of interest (e.g. memory.events) to receive notifications for both
    modifications and the eventual removal of the file, as well as automatic
    watch descriptor cleanup, simplifying userspace logic and improving
    resource efficiency.
    
    Implementation details:
    The kernfs notification worker is updated to handle file deletion.
    The optimized single call for MODIFY events to both the parent and the
    file is retained, however because CREATE (parent) events remain
    unsupported for kernfs files, support for DELETE (parent) events is not
    added here to retain symmetry. Only support for DELETE_SELF events is
    added.
    
    Signed-off-by: T.J. Mercier <tjmercier@google.com>
    Acked-by: Tejun Heo <tj@kernel.org>

--- fs/kernfs/file.c
+++ fs/kernfs/file.c
@@ -37,8 +37,8 @@ struct kernfs_open_node {
  */
 #define KERNFS_NOTIFY_EOL			((void *)&kernfs_notify_list)
 
-static DEFINE_SPINLOCK(kernfs_notify_lock);
-static struct kernfs_node *kernfs_notify_list = KERNFS_NOTIFY_EOL;
+DEFINE_SPINLOCK(kernfs_notify_lock);
+struct kernfs_node *kernfs_notify_list = KERNFS_NOTIFY_EOL;
 
 static inline struct mutex *kernfs_open_file_mutex_ptr(struct kernfs_node *kn)
 {
@@ -909,7 +909,7 @@ static loff_t kernfs_fop_llseek(struct file *file, loff_t offset, int whence)
 	return ret;
 }
 
-static void kernfs_notify_workfn(struct work_struct *work)
+void kernfs_notify_workfn(struct work_struct *work)
 {
 	struct kernfs_node *kn;
 	struct kernfs_super_info *info;
@@ -935,11 +935,7 @@ repeat:
 	down_read(&root->kernfs_supers_rwsem);
 	down_read(&root->kernfs_rwsem);
 	list_for_each_entry(info, &kernfs_root(kn)->supers, node) {
-		struct kernfs_node *parent;
-		struct inode *p_inode = NULL;
-		const char *kn_name;
 		struct inode *inode;
-		struct qstr name;
 
 		/*
 		 * We want fsnotify_modify() on @kn but as the
@@ -951,23 +947,30 @@ repeat:
 		if (!inode)
 			continue;
 
-		kn_name = kernfs_rcu_name(kn);
-		name = QSTR(kn_name);
-		parent = kernfs_get_parent(kn);
-		if (parent) {
-			p_inode = ilookup(info->sb, kernfs_ino(parent));
-			if (p_inode) {
-				fsnotify(notify_event | FS_EVENT_ON_CHILD,
-					 inode, FSNOTIFY_EVENT_INODE,
-					 p_inode, &name, inode, 0);
-				iput(p_inode);
+		if (notify_event == FS_DELETE) {
+			fsnotify_inoderemove(inode);
+		} else {
+			struct kernfs_node *parent = kernfs_get_parent(kn);
+			struct inode *p_inode = NULL;
+
+			if (parent) {
+				p_inode = ilookup(info->sb, kernfs_ino(parent));
+				if (p_inode) {
+					const char *kn_name = kernfs_rcu_name(kn);
+					struct qstr name = QSTR(kn_name);
+
+					fsnotify(notify_event | FS_EVENT_ON_CHILD,
+						 inode, FSNOTIFY_EVENT_INODE,
+						 p_inode, &name, inode, 0);
+					iput(p_inode);
+				}
+
+				kernfs_put(parent);
 			}
 
-			kernfs_put(parent);
+			if (!p_inode)
+				fsnotify_inode(inode, notify_event);
 		}
-
-		if (!p_inode)
-			fsnotify_inode(inode, notify_event);
 
 		iput(inode);
 	}
