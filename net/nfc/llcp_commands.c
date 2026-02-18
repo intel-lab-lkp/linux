5f5d2238288aa060ff9376a355bc6b51de893943
Sam Swicegood <samswicegood@gmail.com>
2026-02-18 16:45:31 -0500

    nfc: llcp: check skb tailroom before appending TLV
    llcp_add_tlv() appends TLV data to an skb using skb_put_data()
    without first verifying that sufficient tailroom is available.
    
    Add a tailroom check to avoid writing past end of the skb when
    building LLCP PDUs.
    
    Signed-off-by: Sam Swicegood <sam@gib.games>

--- net/nfc/llcp_commands.c
+++ net/nfc/llcp_commands.c
@@ -300,9 +300,10 @@ static struct sk_buff *llcp_add_header(struct sk_buff *pdu,
 static struct sk_buff *llcp_add_tlv(struct sk_buff *pdu, const u8 *tlv,
 				    u8 tlv_length)
 {
-	/* XXX Add an skb length check */
+	if (!pdu || !tlv)
+		return NULL;
 
-	if (tlv == NULL)
+	if (skb_tailroom(pdu) < tlv_length)
 		return NULL;
 
 	skb_put_data(pdu, tlv, tlv_length);
