a91d429361ce9d4e24462559823cd2b42ed4854b
Ethan Tidmore <ethantidmore06@gmail.com>
2026-02-17 19:17:07 -0600

    ipc: Fix error pointer dereference
    The function shm_lock() can return an error pointer and is not checked
    for one. Add check for error pointer.
    
    Detected by Smatch:
    ipc/shm.c:1678 do_shmat() error:
    'shp' dereferencing possible ERR_PTR()
    
    Fixes: 00c2bf85d8feb ("ipc: get rid of ipc_lock_down()")
    Signed-off-by: Ethan Tidmore <ethantidmore06@gmail.com>

--- ipc/shm.c
+++ ipc/shm.c
@@ -1675,12 +1675,15 @@ out_fput:
 out_nattch:
 	down_write(&shm_ids(ns).rwsem);
 	shp = shm_lock(ns, shmid);
-	shp->shm_nattch--;
+	if (!IS_ERR(shp)) {
+		shp->shm_nattch--;
 
-	if (shm_may_destroy(shp))
-		shm_destroy(ns, shp);
-	else
-		shm_unlock(shp);
+		if (shm_may_destroy(shp))
+			shm_destroy(ns, shp);
+		else
+			shm_unlock(shp);
+	}
+
 	up_write(&shm_ids(ns).rwsem);
 	return err;
 
