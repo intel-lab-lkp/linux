b2dcc4b352e9203fedcb40b9096eaac23287e2ce
Oliver Mangold <oliver.mangold@pm.me>
2026-02-20 10:51:11 +0100

    rust: rename `AlwaysRefCounted` to `RefCounted`.
    There are types where it may both be reference counted in some cases and
    owned in others. In such cases, obtaining `ARef<T>` from `&T` would be
    unsound as it allows creation of `ARef<T>` copy from `&Owned<T>`.
    
    Therefore, we split `AlwaysRefCounted` into `RefCounted` (which `ARef<T>`
    would require) and a marker trait to indicate that the type is always
    reference counted (and not `Ownable`) so the `&T` -> `ARef<T>` conversion
    is possible.
    
    - Rename `AlwaysRefCounted` to `RefCounted`.
    - Add a new unsafe trait `AlwaysRefCounted`.
    - Implement the new trait `AlwaysRefCounted` for the newly renamed
      `RefCounted` implementations. This leaves functionality of existing
      implementers of `AlwaysRefCounted` intact.
    
    Suggested-by: Alice Ryhl <aliceryhl@google.com>
    Reviewed-by: Daniel Almeida <daniel.almeida@collabora.com>
    Signed-off-by: Oliver Mangold <oliver.mangold@pm.me>
    [ Andreas: Updated commit message and rebase on rust-6.20-7.0 ]
    Acked-by: Igor Korotin <igor.korotin.linux@gmail.com>
    Acked-by: Danilo Krummrich <dakr@kernel.org>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Reviewed-by: Gary Guo <gary@garyguo.net>
    Co-developed-by: Andreas Hindborg <a.hindborg@kernel.org>
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/usb.rs
+++ rust/kernel/usb.rs
@@ -12,7 +12,8 @@ use crate::{
     error::{from_result, to_result, Result},
     prelude::*,
     str::CStr,
-    types::{AlwaysRefCounted, Opaque},
+    sync::aref::{AlwaysRefCounted, RefCounted},
+    types::Opaque,
     ThisModule,
 };
 use core::{
@@ -365,7 +366,7 @@ impl<Ctx: device::DeviceContext> AsRef<Device> for Interface<Ctx> {
 }
 
 // SAFETY: Instances of `Interface` are always reference-counted.
-unsafe impl AlwaysRefCounted for Interface {
+unsafe impl RefCounted for Interface {
     fn inc_ref(&self) {
         // SAFETY: The invariants of `Interface` guarantee that `self.as_raw()`
         // returns a valid `struct usb_interface` pointer, for which we will
@@ -378,6 +379,10 @@ unsafe impl AlwaysRefCounted for Interface {
         unsafe { bindings::usb_put_intf(obj.cast().as_ptr()) }
     }
 }
+
+// SAFETY: We do not implement `Ownable`, thus it is okay to obtain an `ARef<Interface>` from a
+// `&Interface`.
+unsafe impl AlwaysRefCounted for Interface {}
 
 // SAFETY: A `Interface` is always reference-counted and can be released from any thread.
 unsafe impl Send for Interface {}
@@ -416,7 +421,7 @@ kernel::impl_device_context_deref!(unsafe { Device });
 kernel::impl_device_context_into_aref!(Device);
 
 // SAFETY: Instances of `Device` are always reference-counted.
-unsafe impl AlwaysRefCounted for Device {
+unsafe impl RefCounted for Device {
     fn inc_ref(&self) {
         // SAFETY: The invariants of `Device` guarantee that `self.as_raw()`
         // returns a valid `struct usb_device` pointer, for which we will
@@ -429,6 +434,10 @@ unsafe impl AlwaysRefCounted for Device {
         unsafe { bindings::usb_put_dev(obj.cast().as_ptr()) }
     }
 }
+
+// SAFETY: We do not implement `Ownable`, thus it is okay to obtain an `ARef<Device>` from a
+// `&Device`.
+unsafe impl AlwaysRefCounted for Device {}
 
 impl<Ctx: device::DeviceContext> AsRef<device::Device<Ctx>> for Device<Ctx> {
     fn as_ref(&self) -> &device::Device<Ctx> {
