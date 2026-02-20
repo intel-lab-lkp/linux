50b47253d97667998dce665ddcf0be8b086bbd37
Antoniu Miclaus <antoniu.miclaus@analog.com>
2026-02-20 15:18:47 +0200

    iio: addac: ad74413r: simplify timeout return
    Return -ETIMEDOUT directly instead of assigning it to an intermediate
    variable first.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- drivers/iio/addac/ad74413r.c
+++ drivers/iio/addac/ad74413r.c
@@ -841,10 +841,8 @@ static int _ad74413r_get_single_adc_result(struct ad74413r_state *st,
 
 	ret = wait_for_completion_timeout(&st->adc_data_completion,
 					  msecs_to_jiffies(1000));
-	if (!ret) {
-		ret = -ETIMEDOUT;
-		return ret;
-	}
+	if (!ret)
+		return -ETIMEDOUT;
 
 	ret = regmap_read(st->regmap, AD74413R_REG_ADC_RESULT_X(channel),
 			  &uval);
