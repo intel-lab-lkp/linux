7241d7b3447dd7c5de99119abe8a7b6381d5ded3
Neel Bullywon <neelb2403@gmail.com>
2026-02-20 18:05:55 -0500

    checkpatch: mention fsleep() in MSLEEP and USLEEP_RANGE warnings
    Expand the MSLEEP warning to also reference fsleep() alongside
    msleep(), and expand the USLEEP_RANGE check to reference fsleep()
    alongside usleep_range() and udelay(). fsleep() autoselects the
    best sleep mechanism based on duration, making it a useful
    alternative for developers to consider.
    
    Suggested-by: Andy Shevchenko <andriy.shevchenko@intel.com>
    Signed-off-by: Neel Bullywon <neelb2403@gmail.com>

--- scripts/checkpatch.pl
+++ scripts/checkpatch.pl
@@ -6624,7 +6624,7 @@ sub process {
 			# ignore udelay's < 10, however
 			if (! ($delay < 10) ) {
 				CHK("USLEEP_RANGE",
-				    "usleep_range is preferred over udelay; see function description of usleep_range() and udelay().\n" . $herecurr);
+				    "usleep_range is preferred over udelay; see function description of usleep_range(), udelay(), and fsleep().\n" . $herecurr);
 			}
 			if ($delay > 2000) {
 				WARN("LONG_UDELAY",
@@ -6636,7 +6636,7 @@ sub process {
 		if ($line =~ /\bmsleep\s*\((\d+)\);/) {
 			if ($1 < 20) {
 				WARN("MSLEEP",
-				     "msleep < 20ms can sleep for up to 20ms; see function description of msleep().\n" . $herecurr);
+				     "msleep < 20ms can sleep for up to 20ms; see function description of msleep() and fsleep().\n" . $herecurr);
 			}
 		}
 
