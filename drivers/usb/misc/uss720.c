6c5be159287307eec8b41a5029c6511cacde76e8
Greg Kroah-Hartman <gregkh@linuxfoundation.org>
2026-02-23 08:24:55 +0100

    usb: misc: uss720: properly clean up reference in uss720_probe()
    If get_1284_register() fails, the usb device reference count is
    incorrect and needs to be properly dropped before returning.
    
    Cc: stable <stable@kernel.org>
    Assisted-by: gkh_clanker_2000
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/usb/misc/uss720.c
+++ drivers/usb/misc/uss720.c
@@ -735,8 +735,10 @@ static int uss720_probe(struct usb_interface *intf,
 	 * here. */
 	ret = get_1284_register(pp, 0, &reg, GFP_KERNEL);
 	dev_dbg(&intf->dev, "reg: %7ph\n", priv->reg);
-	if (ret < 0)
+	if (ret < 0) {
+		usb_put_dev(usbdev);
 		return ret;
+	}
 
 	ret = usb_find_last_int_in_endpoint(interface, &epd);
 	if (!ret) {
