8ef77f3c649f1504947a88a649cb9361687be25a
JB Tsai <jb.tsai@mediatek.com>
2026-02-23 15:38:50 +0800

    wifi: mt76: mt7921: refactor regulatory domain handling to regd.[ch]
    Move regd logic to regd.c and regd.h files
    
    Signed-off-by: JB Tsai <jb.tsai@mediatek.com>

---
+++ drivers/net/wireless/mediatek/mt76/mt7921/regd.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: ISC */
+/* Copyright (C) 2025 MediaTek Inc. */
+
+#ifndef __MT7921_REGD_H
+#define __MT7921_REGD_H
+
+#include "mt7921.h"
+
+void mt7921_regd_update(struct mt792x_dev *dev);
+void mt7921_regd_notifier(struct wiphy *wiphy,
+			  struct regulatory_request *request);
+
+#endif
