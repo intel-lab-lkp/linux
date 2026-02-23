a26b8f8fdb175d43534d74f3ac8196ee495cff77
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2026-02-23 19:14:26 +0530

    drm/i915/psr: Add helper to check if PR is with link OFF
    Introduce a helper intel_psr_is_pr_with_link_off() to check if the Panel
    Replay feature needs to be enabled with link off.
    
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>

--- drivers/gpu/drm/i915/display/intel_psr.h
+++ drivers/gpu/drm/i915/display/intel_psr.h
@@ -86,5 +86,6 @@ void intel_psr_compute_config_late(struct intel_dp *intel_dp,
 				   struct intel_crtc_state *crtc_state);
 int intel_psr_min_guardband(struct intel_crtc_state *crtc_state);
 bool intel_psr_use_trans_push(const struct intel_crtc_state *crtc_state);
+bool intel_psr_is_pr_with_link_off(const struct intel_crtc_state *crtc_state);
 
 #endif /* __INTEL_PSR_H__ */
