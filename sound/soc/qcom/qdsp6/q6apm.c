8a8b6232d94eb1d9e07b7d7fd08d4a44ee00ea75
Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>
2026-02-23 18:07:28 +0000

    ASoC: qcom: q6apm: remove child devices when apm is removed
    looks like q6apm driver does not remove the child driver q6apm-dai and
    q6apm-bedais when the this driver is removed.
    
    Fix this by using a manage version of of_platfom_populate.
    
    With this change when the dsp is shutdown all the devices assocated with
    q6apm will now be removed.
    
    Fixes: 5477518b8a0e ("ASoC: qdsp6: audioreach: add q6apm support")
    Signed-off-by: Srinivas Kandagatla <srinivas.kandagatla@oss.qualcomm.com>

--- sound/soc/qcom/qdsp6/q6apm.c
+++ sound/soc/qcom/qdsp6/q6apm.c
@@ -752,7 +752,7 @@ static int apm_probe(gpr_device_t *gdev)
 		return ret;
 	}
 
-	return of_platform_populate(dev->of_node, NULL, NULL, dev);
+	return devm_of_platform_populate(dev);
 }
 
 static void apm_remove(gpr_device_t *gdev)
