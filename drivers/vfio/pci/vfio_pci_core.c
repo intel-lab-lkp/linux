e1b977ab0ee76a7a3ab831b7600b53a8d59f6200
Yishai Hadas <yishaih@nvidia.com>
2026-02-24 10:20:15 +0200

    vfio: Add support for VFIO_DEVICE_FEATURE_MIG_PRECOPY_INFOv2
    Currently, existing VFIO_MIG_GET_PRECOPY_INFO implementations don't
    assign info.flags before copy_to_user().
    
    Because they copy the struct in from userspace first, this effectively
    echoes userspace-provided flags back as output, preventing the field
    from being used to report new reliable data from the drivers.
    
    Add support for a new device feature named
    VFIO_DEVICE_FEATURE_MIG_PRECOPY_INFOv2.
    
    On SET, enables the v2 pre_copy_info behaviour, where the
    vfio_precopy_info.flags is a valid output field.
    
    Signed-off-by: Yishai Hadas <yishaih@nvidia.com>

--- drivers/vfio/pci/vfio_pci_core.c
+++ drivers/vfio/pci/vfio_pci_core.c
@@ -737,6 +737,7 @@ void vfio_pci_core_close_device(struct vfio_device *core_vdev)
 #endif
 	vfio_pci_core_disable(vdev);
 
+	core_vdev->precopy_info_flags_fix = 0;
 	vfio_pci_dma_buf_cleanup(vdev);
 
 	mutex_lock(&vdev->igate);
