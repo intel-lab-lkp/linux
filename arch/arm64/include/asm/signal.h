6cb9a02eb998da24fddeb1f45e4db19bc932ce47
Nirmoy Das <nirmoyd@nvidia.com>
2026-02-24 05:55:03 -0800

    arm64: signal: preserve si_addr for addresses in the VA hole
    When userspace accesses an address in the "hole" between user and kernel
    virtual address space, the kernel delivers SIGSEGV with si_addr set to
    the faulting address. However, untagged_addr() uses sign_extend64() to
    canonicalize the address which corrupts hole addresses making debugging
    difficult as userspace cannot see the actual faulting value.
    
    Fix this by only stripping the TBI top-byte for addresses that fall
    within the valid user range (below TASK_SIZE) after masking. For hole
    addresses, preserve the full original address including any tag bits.
    
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=220750
    Signed-off-by: Nirmoy Das <nirmoyd@nvidia.com>

--- arch/arm64/include/asm/signal.h
+++ arch/arm64/include/asm/signal.h
@@ -3,6 +3,7 @@
 #define __ARM64_ASM_SIGNAL_H
 
 #include <asm/memory.h>
+#include <asm/processor.h>
 #include <uapi/asm/signal.h>
 #include <uapi/asm/siginfo.h>
 
@@ -10,6 +11,8 @@ static inline void __user *arch_untagged_si_addr(void __user *addr,
 						 unsigned long sig,
 						 unsigned long si_code)
 {
+	unsigned long masked;
+
 	/*
 	 * For historical reasons, all bits of the fault address are exposed as
 	 * address bits for watchpoint exceptions. New architectures should
@@ -18,7 +21,16 @@ static inline void __user *arch_untagged_si_addr(void __user *addr,
 	if (sig == SIGTRAP && si_code == TRAP_BRKPT)
 		return addr;
 
-	return untagged_addr(addr);
+	/*
+	 * Strip tag bits only for valid user addresses. For addresses
+	 * in the VA hole, preserve the original value so userspace can
+	 * see the actual faulting address for debugging.
+	 */
+	masked = (unsigned long)addr & ((1UL << 56) - 1);
+	if (masked >= TASK_SIZE)
+		return addr;
+
+	return (void __user *)masked;
 }
 #define arch_untagged_si_addr arch_untagged_si_addr
 
