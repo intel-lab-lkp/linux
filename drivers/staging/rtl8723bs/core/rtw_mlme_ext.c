7c1b98d29a6c53f126924126bed495acd75707c6
Derek Robson <robsonde@gmail.com>
2026-02-24 14:42:22 +1300

    staging: rtl8723bs: fixed use of msleep of less than 20.
    Replaced use of msleep() with usleep_range()
    
    Signed-off-by: Derek Robson <robsonde@gmail.com>

--- drivers/staging/rtl8723bs/core/rtw_mlme_ext.c
+++ drivers/staging/rtl8723bs/core/rtw_mlme_ext.c
@@ -5834,7 +5834,7 @@ u8 chk_bmc_sleepq_hdl(struct adapter *padapter, unsigned char *pbuf)
 		return H2C_SUCCESS;
 
 	if ((pstapriv->tim_bitmap & BIT(0)) && (psta_bmc->sleepq_len > 0)) {
-		msleep(10);/*  10ms, ATIM(HIQ) Windows */
+		usleep_range(10000, 10100);/*  10ms, ATIM(HIQ) Windows */
 
 		/* spin_lock_bh(&psta_bmc->sleep_q.lock); */
 		spin_lock_bh(&pxmitpriv->lock);
--- drivers/staging/rtl8723bs/core/rtw_pwrctrl.c
+++ drivers/staging/rtl8723bs/core/rtw_pwrctrl.c
@@ -420,7 +420,7 @@ s32 LPS_RF_ON_check(struct adapter *padapter, u32 delay_ms)
 			err = -1;
 			break;
 		}
-		msleep(1);
+		usleep_range(1000, 1100);
 	}
 
 	return err;
@@ -571,7 +571,7 @@ void LPS_Leave_check(struct adapter *padapter)
 		if (jiffies_to_msecs(jiffies - start_time) > 100)
 			break;
 
-		msleep(1);
+		usleep_range(1000, 1100);
 	}
 }
 
