89b286c1b8ea8fb09774827ee1a5ab94884a71f6
Russell King (Oracle) <rmk+kernel@armlinux.org.uk>
2026-02-25 11:44:45 +0000

    net: stmmac: add struct stmmac_pcs_info
    We need to describe one more register (offset and field bitmask) to
    the PCS code. Move the existing PCS offset and interrupt enable bits
    to a new struct and pass that in to stmmac_integrated_pcs_init().
    
    Signed-off-by: Russell King (Oracle) <rmk+kernel@armlinux.org.uk>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_pcs.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_pcs.c
@@ -167,8 +167,8 @@ int stmmac_integrated_pcs_get_phy_intf_sel(struct phylink_pcs *pcs,
 	return -EINVAL;
 }
 
-int stmmac_integrated_pcs_init(struct stmmac_priv *priv, unsigned int offset,
-			       u32 int_mask)
+int stmmac_integrated_pcs_init(struct stmmac_priv *priv,
+			       const struct stmmac_pcs_info *pcs_info)
 {
 	struct stmmac_pcs *spcs;
 
@@ -177,8 +177,8 @@ int stmmac_integrated_pcs_init(struct stmmac_priv *priv, unsigned int offset,
 		return -ENOMEM;
 
 	spcs->priv = priv;
-	spcs->base = priv->ioaddr + offset;
-	spcs->int_mask = int_mask;
+	spcs->base = priv->ioaddr + pcs_info->pcs_offset;
+	spcs->int_mask = pcs_info->int_mask;
 	spcs->pcs.ops = &dwmac_integrated_pcs_ops;
 
 	/* If the PCS supports extended status, then it supports BASE-X AN
