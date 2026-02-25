238106e99d54c138434639a25361a0e104d4ec2d
Geraldo Nascimento <geraldogabriel@gmail.com>
2026-02-25 19:27:14 -0300

    PCI: rockchip: limit RK3399 to 2.5 GT/s to avoid data loss
    Shawn Lin from Rockchip has reiterated that there may be danger of
    "catastrophic failure" in using their PCIe with 5.0 GT/s speeds.
    
    This may cause data loss or worse. Warn the user if they make a DT
    change from the default and drive at 2.5 GT/s only, even if the DT
    max-link-speed property is invalid or inexistent.
    
    This change is corroborated by RK3399 official datasheet [1], which
    states maximum link speed for this platform is 2.5 GT/s.
    
    [1] https://opensource.rock-chips.com/images/d/d7/Rockchip_RK3399_Datasheet_V2.1-20200323.pdf
    
    Fixes: 956cd99b35a8 ("PCI: rockchip: Separate common code from RC driver")
    Link: https://lore.kernel.org/all/ffd05070-9879-4468-94e3-b88968b4c21b@rock-chips.com/
    Cc: stable@vger.kernel.org
    Reported-by: Dragan Simic <dsimic@manjaro.org>
    Reported-by: Shawn Lin <shawn.lin@rock-chips.com>
    Reviewed-by: Dragan Simic <dsimic@manjaro.org>
    Signed-off-by: Geraldo Nascimento <geraldogabriel@gmail.com>

--- drivers/pci/controller/pcie-rockchip.c
+++ drivers/pci/controller/pcie-rockchip.c
@@ -66,8 +66,14 @@ int rockchip_pcie_parse_dt(struct rockchip_pcie *rockchip)
 	}
 
 	rockchip->link_gen = of_pci_get_max_link_speed(node);
-	if (rockchip->link_gen < 0 || rockchip->link_gen > 2)
-		rockchip->link_gen = 2;
+	if (rockchip->link_gen < 0 || rockchip->link_gen > 2) {
+		rockchip->link_gen = 1;
+		dev_warn(dev, "invalid max-link-speed, set to 2.5 GT/s\n");
+	}
+	else if (rockchip->link_gen == 2) {
+		rockchip->link_gen = 1;
+		dev_warn(dev, "5.0 GT/s may cause data loss, force 2.5 GT/s\n");
+	}
 
 	for (i = 0; i < ROCKCHIP_NUM_PM_RSTS; i++)
 		rockchip->pm_rsts[i].id = rockchip_pci_pm_rsts[i];
