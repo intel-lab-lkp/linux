983a96f2ce8341642db2dad3190da91c09d70f18
Qinxin Xia <xiaqinxin@huawei.com>
2026-02-25 11:41:25 +0800

    perf: Add --pmu-filter option for filtering PMUs
    This patch adds a new --pmu-filter option to perf-stat command to allow
    filtering events on specific PMUs. This is useful when there are
    multiple PMUs with same type (e.g. hisi_sicl2_cpa0 and hisi_sicl0_cpa0).
    
    perf event:
    [root@localhost tmp]# perf stat -e cpa_cycles
     Performance counter stats for 'system wide':
    
         3,005,509,659      hisi_sicl0_cpa0/cpa_cycles/
         3,005,502,319      hisi_sicl2_cpa0/cpa_cycles/
    
           3.005460970 seconds time elapsed
    
    [root@localhost tmp]# perf stat --pmu-filter hisi_sicl0_cpa0 -e cpa_cycles
     Performance counter stats for 'system wide':
    
         3,003,899,571      cpa_cycles
    
           3.003864470 seconds time elapsed
    
    perf metric:
    [root@localhost tmp]# perf stat -M cpa_p0_avg_bw
     Performance counter stats for 'system wide':
    
        19,417,779,115      hisi_sicl0_cpa0/cpa_cycles/      #     0.00 cpa_p0_avg_bw
                     0      hisi_sicl0_cpa0/cpa_p0_wr_dat/
                     0      hisi_sicl0_cpa0/cpa_p0_rd_dat_64b/
                     0      hisi_sicl0_cpa0/cpa_p0_rd_dat_32b/
        19,417,751,103      hisi_sicl10_cpa0/cpa_cycles/     #     0.00 cpa_p0_avg_bw
                     0      hisi_sicl10_cpa0/cpa_p0_wr_dat/
                     0      hisi_sicl10_cpa0/cpa_p0_rd_dat_64b/
                     0      hisi_sicl10_cpa0/cpa_p0_rd_dat_32b/
        19,417,730,679      hisi_sicl2_cpa0/cpa_cycles/      #     0.31 cpa_p0_avg_bw
            75,635,749      hisi_sicl2_cpa0/cpa_p0_wr_dat/
            18,520,640      hisi_sicl2_cpa0/cpa_p0_rd_dat_64b/
                     0      hisi_sicl2_cpa0/cpa_p0_rd_dat_32b/
        19,417,674,227      hisi_sicl8_cpa0/cpa_cycles/      #     0.00 cpa_p0_avg_bw
                     0      hisi_sicl8_cpa0/cpa_p0_wr_dat/
                     0      hisi_sicl8_cpa0/cpa_p0_rd_dat_64b/
                     0      hisi_sicl8_cpa0/cpa_p0_rd_dat_32b/
    
          19.417734480 seconds time elapsed
    
    [root@localhost tmp]# perf stat --pmu-filter hisi_sicl2_cpa0 -M cpa_p0_avg_bw
     Performance counter stats for 'system wide':
    
         6,234,093,559      cpa_cycles                       #     0.60 cpa_p0_avg_bw
            50,548,465      cpa_p0_wr_dat
             7,552,182      cpa_p0_rd_dat_64b
                     0      cpa_p0_rd_dat_32b
    
           6.234139320 seconds time elapsed
    
    Signed-off-by: Qinxin Xia <xiaqinxin@huawei.com>

--- tools/perf/builtin-stat.c
+++ tools/perf/builtin-stat.c
@@ -1214,6 +1214,28 @@ static int parse_cputype(const struct option *opt,
 	return 0;
 }
 
+static int parse_pmu_filter(const struct option *opt,
+			   const char *str,
+			   int unset __maybe_unused)
+{
+	const struct perf_pmu *pmu;
+	struct evlist *evlist = *(struct evlist **)opt->value;
+
+	if (!list_empty(&evlist->core.entries)) {
+		fprintf(stderr, "Must define pmu-filter before events/metrics\n");
+		return -1;
+	}
+
+	pmu = perf_pmus__pmu_for_pmu_filter(str);
+	if (!pmu) {
+		fprintf(stderr, "--pmu-filter %s is not supported!\n", str);
+		return -1;
+	}
+	parse_events_option_args.pmu_filter = pmu->name;
+
+	return 0;
+}
+
 static int parse_cache_level(const struct option *opt,
 			     const char *str,
 			     int unset __maybe_unused)
@@ -2561,6 +2583,10 @@ int cmd_stat(int argc, const char **argv)
 			"Only enable events on applying cpu with this type "
 			"for hybrid platform (e.g. core or atom)",
 			parse_cputype),
+		OPT_CALLBACK(0, "pmu-filter", &evsel_list, "pmu",
+			"Only enable events on applying pmu with specified "
+			"for multiple pmus with same type(e.g. hisi_sicl2_cpa0 or hisi_sicl0_cpa0)",
+			parse_pmu_filter),
 #ifdef HAVE_LIBPFM
 		OPT_CALLBACK(0, "pfm-events", &evsel_list, "event",
 			"libpfm4 event selector. use 'perf list' to list available events",
