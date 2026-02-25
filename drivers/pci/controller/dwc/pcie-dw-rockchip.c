ece9ae0759cbaa9af0dccc7038b56a9678ef3993
Niklas Cassel <cassel@kernel.org>
2026-02-25 18:03:29 +0100

    PCI: dwc: Disable BARs in common code instead of in each glue driver
    The current EPC core design relies on an EPC driver disabling all BARs by
    default. An EPF driver will then enable the BARs that it wants to enabled.
    
    This design is there because there is no epc->ops->disable_bar().
    (There is a epc->ops->clear_bar(), but that is only to disable a BAR that
    has been enabled using epc->ops->set_bar() first.)
    
    By default, an EPF driver will not be able to get/enable BARs that are
    marked as BAR_RESERVED or BAR_DISABLED (see pci_epc_get_next_free_bar()).
    
    Since the current EPC code design requires an EPC driver to disable all
    BARs by default, let's move this to DWC common code from each glue driver.
    
    BAR_RESERVED BARs are not disabled by default because these BARs are
    hardware backed, and should only be disabled explicitly by an EPF driver
    if absolutely necessary for the EPF driver to function correctly.
    (This is similar to how e.g. NVMe may have vendor specific BARs outside of
    the mandatory BAR0 which contains the NVMe registers.)
    
    Note that there is currently no EPC operation to disable a BAR that has not
    first been programmed using pci_epc_set_bar(). If an EPF driver ever wants
    to disable a BAR marked as BAR_RESERVED, a disable_bar() operation would
    have to be added first.
    
    No functional changes intended.
    
    Tested-by: Manikanta Maddireddy <mmaddireddy@nvidia.com>
    Reviewed-by: Manikanta Maddireddy <mmaddireddy@nvidia.com>
    Signed-off-by: Niklas Cassel <cassel@kernel.org>

--- drivers/pci/controller/dwc/pcie-dw-rockchip.c
+++ drivers/pci/controller/dwc/pcie-dw-rockchip.c
@@ -361,13 +361,9 @@ static void rockchip_pcie_ep_hide_broken_ats_cap_rk3588(struct dw_pcie_ep *ep)
 static void rockchip_pcie_ep_init(struct dw_pcie_ep *ep)
 {
 	struct dw_pcie *pci = to_dw_pcie_from_ep(ep);
-	enum pci_barno bar;
 
 	rockchip_pcie_enable_l0s(pci);
 	rockchip_pcie_ep_hide_broken_ats_cap_rk3588(ep);
-
-	for (bar = 0; bar < PCI_STD_NUM_BARS; bar++)
-		dw_pcie_ep_reset_bar(pci, bar);
 };
 
 static int rockchip_pcie_raise_irq(struct dw_pcie_ep *ep, u8 func_no,
