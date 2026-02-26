ab0903cfc2a2e14ac89051f52b05409901422a81
Ian Rogers <irogers@google.com>
2026-02-25 17:35:31 -0800

    perf header: Add utility to convert feature number to a string
    For logging and debug messages it can be convenient to convert a
    feature number to a name. Add header_feat__name for this and reuse the
    data already within the feat_ops struct.
    
    Signed-off-by: Ian Rogers <irogers@google.com>

--- tools/perf/util/header.c
+++ tools/perf/util/header.c
@@ -3770,6 +3770,13 @@ struct header_print_data {
 	bool full; /* extended list of headers */
 };
 
+const char *header_feat__name(unsigned int id)
+{
+	if (id < HEADER_LAST_FEATURE)
+		return feat_ops[id].name;
+	return "INVALID";
+}
+
 static int perf_file_section__fprintf_info(struct perf_file_section *section,
 					   struct perf_header *ph,
 					   int feat, int fd, void *data)
