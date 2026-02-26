6bec436f99975eb7296ebe6e908910866a297080
Keith Teeple <keithrteeple@gmail.com>
2026-02-25 19:10:19 -0500

    staging: rtl8723bs: split chained assignment in xmit_linux.c
    checkpatch reports that multiple assignments should be avoided.
    Split the chained assignment into two separate statements.
    
    Signed-off-by: Keith Teeple <keithrteeple@gmail.com>

--- drivers/staging/rtl8723bs/os_dep/xmit_linux.c
+++ drivers/staging/rtl8723bs/os_dep/xmit_linux.c
@@ -6,7 +6,6 @@
  ******************************************************************************/
 #include <drv_types.h>
 
-
 uint rtw_remainder_len(struct pkt_file *pfile)
 {
 	return (pfile->buf_len - ((SIZE_PTR)(pfile->cur_addr) - (SIZE_PTR)(pfile->buf_start)));
@@ -15,9 +14,10 @@ uint rtw_remainder_len(struct pkt_file *pfile)
 void _rtw_open_pktfile(struct sk_buff *pktptr, struct pkt_file *pfile)
 {
 	pfile->pkt = pktptr;
-	pfile->cur_addr = pfile->buf_start = pktptr->data;
-	pfile->pkt_len = pfile->buf_len = pktptr->len;
-
+	pfile->buf_start = pktptr->data;
+	pfile->cur_addr = pfile->buf_start;
+	pfile->buf_len = pktptr->len;
+	pfile->pkt_len = pfile->buf_len;
 	pfile->cur_buffer = pfile->buf_start;
 }
 
