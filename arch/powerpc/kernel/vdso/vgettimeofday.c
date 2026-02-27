584a3e5fbbe73fd73d19bf197014967abbd91577
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2026-02-27 07:57:43 +0100

    powerpc/vdso: Respect COMPAT_32BIT_TIME
    If CONFIG_COMPAT_32BIT_TIME is disabled then the vDSO should not
    provide any 32-bit time related functionality. This is the intended
    effect of the kconfig option and also the fallback system calls would
    also not be implemented.
    
    Currently the kconfig option does not affect the gettimeofday() syscall,
    so also keep that in the vDSO.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- arch/powerpc/kernel/vdso/vgettimeofday.c
+++ arch/powerpc/kernel/vdso/vgettimeofday.c
@@ -18,22 +18,24 @@ int __c_kernel_clock_getres(clockid_t clock_id, struct __kernel_timespec *res,
 	return __cvdso_clock_getres_data(vd, clock_id, res);
 }
 #else
+#ifdef CONFIG_COMPAT_32BIT_TIME
 int __c_kernel_clock_gettime(clockid_t clock, struct old_timespec32 *ts,
 			     const struct vdso_time_data *vd)
 {
 	return __cvdso_clock_gettime32_data(vd, clock, ts);
 }
 
-int __c_kernel_clock_gettime64(clockid_t clock, struct __kernel_timespec *ts,
-			       const struct vdso_time_data *vd)
-{
-	return __cvdso_clock_gettime_data(vd, clock, ts);
-}
-
 int __c_kernel_clock_getres(clockid_t clock_id, struct old_timespec32 *res,
 			    const struct vdso_time_data *vd)
 {
 	return __cvdso_clock_getres_time32_data(vd, clock_id, res);
+}
+#endif /* CONFIG_COMPAT_32BIT_TIME */
+
+int __c_kernel_clock_gettime64(clockid_t clock, struct __kernel_timespec *ts,
+			       const struct vdso_time_data *vd)
+{
+	return __cvdso_clock_gettime_data(vd, clock, ts);
 }
 
 int __c_kernel_clock_getres_time64(clockid_t clock_id, struct __kernel_timespec *res,
@@ -49,7 +51,9 @@ int __c_kernel_gettimeofday(struct __kernel_old_timeval *tv, struct timezone *tz
 	return __cvdso_gettimeofday_data(vd, tv, tz);
 }
 
+#if defined(__powerpc64__) || defined(CONFIG_COMPAT_32BIT_TIME)
 __kernel_old_time_t __c_kernel_time(__kernel_old_time_t *time, const struct vdso_time_data *vd)
 {
 	return __cvdso_time_data(vd, time);
 }
+#endif
