a187b83f8055d17b0f02152c2089753cd508a831
Xianwei Zhao <xianwei.zhao@amlogic.com>
2026-02-27 07:20:53 +0000

    dt-bindings: dma: Add Amlogic A9 SoC DMA
    Add documentation describing the Amlogic A9 SoC DMA. And add
    the properties specific values defines into a new include file.
    
    Signed-off-by: Xianwei Zhao <xianwei.zhao@amlogic.com>

---
+++ include/dt-bindings/dma/amlogic-dma.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: (GPL-2.0 OR MIT) */
+
+#ifndef __DT_BINDINGS_DMA_AMLOGIC_DMA_H__
+#define __DT_BINDINGS_DMA_AMLOGIC_DMA_H__
+
+#define AML_DMA_TYPE_TX		0
+#define AML_DMA_TYPE_RX		1
+#endif /* __DT_BINDINGS_DMA_AMLOGIC_DMA_H__ */
