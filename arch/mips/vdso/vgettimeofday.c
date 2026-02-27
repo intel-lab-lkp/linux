790a82ea05bc32a8bb5db3d37c21df26d6805816
Thomas Weißschuh <thomas.weissschuh@linutronix.de>
2026-02-27 07:57:45 +0100

    MIPS: VDSO: Respect COMPAT_32BIT_TIME
    If CONFIG_COMPAT_32BIT_TIME is disabled then the vDSO should not
    provide any 32-bit time related functionality. This is the intended
    effect of the kconfig option and also the fallback system calls would
    also not be implemented.
    
    Currently the kconfig option does not affect the gettimeofday() syscall,
    so also keep that in the vDSO.
    
    Signed-off-by: Thomas Weißschuh <thomas.weissschuh@linutronix.de>

--- arch/mips/vdso/vgettimeofday.c
+++ arch/mips/vdso/vgettimeofday.c
@@ -12,22 +12,25 @@
 #include <vdso/gettime.h>
 
 #if _MIPS_SIM != _MIPS_SIM_ABI64
+
+#ifdef CONFIG_COMPAT_32BIT_TIME
 int __vdso_clock_gettime(clockid_t clock,
 			 struct old_timespec32 *ts)
 {
 	return __cvdso_clock_gettime32(clock, ts);
 }
 
-int __vdso_gettimeofday(struct __kernel_old_timeval *tv,
-			struct timezone *tz)
-{
-	return __cvdso_gettimeofday(tv, tz);
-}
-
 int __vdso_clock_getres(clockid_t clock_id,
 			struct old_timespec32 *res)
 {
 	return __cvdso_clock_getres_time32(clock_id, res);
+}
+#endif /* CONFIG_COMPAT_32BIT_TIME */
+
+int __vdso_gettimeofday(struct __kernel_old_timeval *tv,
+			struct timezone *tz)
+{
+	return __cvdso_gettimeofday(tv, tz);
 }
 
 int __vdso_clock_gettime64(clockid_t clock,
