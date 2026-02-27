af68a8c8669ad16970526d6d83450191a1823765
Maxime Ripard <mripard@kernel.org>
2026-02-27 14:15:44 +0100

    mm: cma: Export cma_alloc and cma_release
    The CMA dma-buf heap uses cma_alloc() and cma_release() to allocate and
    free, respectively, its CMA buffers.
    
    However, these functions are not exported. Since we want to turn the CMA
    heap into a module, let's export them both.
    
    Reviewed-by: T.J. Mercier <tjmercier@google.com>
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- mm/cma.c
+++ mm/cma.c
@@ -951,6 +951,7 @@ struct page *cma_alloc(struct cma *cma, unsigned long count,
 
 	return page;
 }
+EXPORT_SYMBOL_GPL(cma_alloc);
 
 static struct cma_memrange *find_cma_memrange(struct cma *cma,
 		const struct page *pages, unsigned long count)
@@ -1027,6 +1028,7 @@ bool cma_release(struct cma *cma, const struct page *pages,
 
 	return true;
 }
+EXPORT_SYMBOL_GPL(cma_release);
 
 bool cma_release_frozen(struct cma *cma, const struct page *pages,
 		unsigned long count)
