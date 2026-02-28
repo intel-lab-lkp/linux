3a9cd953047cd8c013351588033750a4d55a4aeb
Kevin Cheng <chengkev@google.com>
2026-02-28 03:33:25 +0000

    KVM: SVM: Move STGI and CLGI intercept handling
    Add STGI/CLGI intercept handling to svm_recalc_instruction_intercepts()
    in preparation for making the function EFER-aware. A later patch will
    recalculate instruction intercepts when EFER.SVME is toggled, which is
    needed to inject #UD on STGI/CLGI when the guest clears EFER.SVME.
    
    When clearing the STGI intercept with vgif enabled, request
    KVM_REQ_EVENT if there is a pending GIF-controlled event. This avoids
    breaking NMI/SMI window tracking, as enable_{nmi,smi}_window() sets
    INTERCEPT_STGI to detect when NMIs become unblocked. KVM_REQ_EVENT
    forces kvm_check_and_inject_events() to re-evaluate pending events and
    re-enable the intercept if needed.
    
    Extract the pending GIF event check into a helper function
    svm_has_pending_gif_event() to deduplicate the logic between
    svm_recalc_instruction_intercepts() and svm_set_gif().
    
    Signed-off-by: Kevin Cheng <chengkev@google.com>

--- arch/x86/kvm/svm/svm.c
+++ arch/x86/kvm/svm/svm.c
@@ -1009,6 +1009,14 @@ void svm_write_tsc_multiplier(struct kvm_vcpu *vcpu)
 	preempt_enable();
 }
 
+static bool svm_has_pending_gif_event(struct vcpu_svm *svm)
+{
+	return svm->vcpu.arch.smi_pending ||
+	       svm->vcpu.arch.nmi_pending ||
+	       kvm_cpu_has_injectable_intr(&svm->vcpu) ||
+	       kvm_apic_has_pending_init_or_sipi(&svm->vcpu);
+}
+
 /* Evaluate instruction intercepts that depend on guest CPUID features. */
 static void svm_recalc_instruction_intercepts(struct kvm_vcpu *vcpu)
 {
@@ -1049,6 +1057,20 @@ static void svm_recalc_instruction_intercepts(struct kvm_vcpu *vcpu)
 		if (vls) {
 			svm_clr_intercept(svm, INTERCEPT_VMLOAD);
 			svm_clr_intercept(svm, INTERCEPT_VMSAVE);
+		}
+
+		if (vgif) {
+			/*
+			 * If there is a pending interrupt controlled by GIF, set
+			 * KVM_REQ_EVENT to re-evaluate if the intercept needs to be set
+			 * again to track when GIF is re-enabled (e.g. for NMI
+			 * injection).
+			 */
+			svm_clr_intercept(svm, INTERCEPT_STGI);
+			if (svm_has_pending_gif_event(svm))
+				kvm_make_request(KVM_REQ_EVENT, &svm->vcpu);
+
+			svm_clr_intercept(svm, INTERCEPT_CLGI);
 		}
 	}
 
@@ -2320,10 +2342,7 @@ void svm_set_gif(struct vcpu_svm *svm, bool value)
 			svm_clear_vintr(svm);
 
 		enable_gif(svm);
-		if (svm->vcpu.arch.smi_pending ||
-		    svm->vcpu.arch.nmi_pending ||
-		    kvm_cpu_has_injectable_intr(&svm->vcpu) ||
-		    kvm_apic_has_pending_init_or_sipi(&svm->vcpu))
+		if (svm_has_pending_gif_event(svm))
 			kvm_make_request(KVM_REQ_EVENT, &svm->vcpu);
 	} else {
 		disable_gif(svm);
