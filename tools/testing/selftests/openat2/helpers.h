02641d0306ffa41a4caa5addcc47d7a0db727437
Jori Koolstra <jkoolstra@xs4all.nl>
2026-03-01 20:47:29 +0100

    selftests: get rid of helper header in openat2 tests
    The openat2 tests include a helper header file that defines the
    necessary structs and constants to use openat2(2), such as struct
    open_how. This may result in conflicting definitions when the
    system header openat2.h is present as well.
    
    This commit adds openat2.h generated by 'make headers' to the uapi
    header files in ./tools/include and removes the helper file
    definitions of the openat2 selftests.
    
    Signed-off-by: Jori Koolstra <jkoolstra@xs4all.nl>

--- tools/testing/selftests/openat2/helpers.h
+++ tools/testing/selftests/openat2/helpers.h
@@ -12,6 +12,7 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <linux/types.h>
+#include <linux/openat2.h>
 #include "kselftest.h"
 
 #define ARRAY_LEN(X) (sizeof (X) / sizeof (*(X)))
@@ -24,44 +25,9 @@
 #define SYS_openat2 __NR_openat2
 #endif /* SYS_openat2 */
 
-/*
- * Arguments for how openat2(2) should open the target path. If @resolve is
- * zero, then openat2(2) operates very similarly to openat(2).
- *
- * However, unlike openat(2), unknown bits in @flags result in -EINVAL rather
- * than being silently ignored. @mode must be zero unless one of {O_CREAT,
- * O_TMPFILE} are set.
- *
- * @flags: O_* flags.
- * @mode: O_CREAT/O_TMPFILE file mode.
- * @resolve: RESOLVE_* flags.
- */
-struct open_how {
-	__u64 flags;
-	__u64 mode;
-	__u64 resolve;
-};
-
 #define OPEN_HOW_SIZE_VER0	24 /* sizeof first published struct */
-#define OPEN_HOW_SIZE_LATEST	OPEN_HOW_SIZE_VER0
 
 bool needs_openat2(const struct open_how *how);
-
-#ifndef RESOLVE_IN_ROOT
-/* how->resolve flags for openat2(2). */
-#define RESOLVE_NO_XDEV		0x01 /* Block mount-point crossings
-					(includes bind-mounts). */
-#define RESOLVE_NO_MAGICLINKS	0x02 /* Block traversal through procfs-style
-					"magic-links". */
-#define RESOLVE_NO_SYMLINKS	0x04 /* Block traversal through all symlinks
-					(implies OEXT_NO_MAGICLINKS) */
-#define RESOLVE_BENEATH		0x08 /* Block "lexical" trickery like
-					"..", symlinks, and absolute
-					paths which escape the dirfd. */
-#define RESOLVE_IN_ROOT		0x10 /* Make all jumps to "/" and ".."
-					be scoped inside the dirfd
-					(similar to chroot(2)). */
-#endif /* RESOLVE_IN_ROOT */
 
 #define E_func(func, ...)						      \
 	do {								      \
