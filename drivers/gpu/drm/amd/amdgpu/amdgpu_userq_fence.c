32a799950f85f0c3bf1818b3fc155b4fe2d45d43
Sunil Khatri <sunil.khatri@amd.com>
2026-03-02 18:50:46 +0530

    drm/amdgpu/userq: refcount userqueues to avoid any race conditions
    To avoid race condition and avoid UAF cases, implement kref
    based queues and protect the below operations using xa lock
    a. Getting a queue from xarray
    b. Increment/Decrement it's refcount
    
    Every time some one want to access a queue, always get via
    amdgpu_userq_get to make sure we have locks in place and get
    the object if active.
    
    A userqueue is destroyed on the last refcount is dropped which
    typically would be via IOCTL or during fini.
    
    v2: Add the missing drop in one the condition in the signal ioclt [Alex]
    
    v3: remove the queue from the xarray first in the free queue ioctl path
        [Christian]
    
    - Pass queue to the amdgpu_userq_put directly.
    - make amdgpu_userq_put xa_lock free since we are doing put for each get
      only and final put is done via destroy and we remove the queue from xa
      with lock.
    - use userq_put in fini too so cleanup is done fully.
    
    v4: Use xa_erase directly rather than doing load and erase in free
        ioctl. Also remove some of the error logs which could be exploited
        by the user to flood the logs [Christian]
    
    Signed-off-by: Sunil Khatri <sunil.khatri@amd.com>
    Reviewed-by: Christian König <christian.koenig@amd.com>
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Change-Id: Ie2865bf9947e5d30546febe9fbed42eac095cbbd

--- drivers/gpu/drm/amd/amdgpu/amdgpu_userq_fence.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_userq_fence.c
@@ -470,7 +470,7 @@ int amdgpu_userq_signal_ioctl(struct drm_device *dev, void *data,
 	struct drm_gem_object **gobj_write, **gobj_read;
 	u32 *syncobj_handles, num_syncobj_handles;
 	struct amdgpu_userq_fence *userq_fence;
-	struct amdgpu_usermode_queue *queue;
+	struct amdgpu_usermode_queue *queue = NULL;
 	struct drm_syncobj **syncobj = NULL;
 	struct dma_fence *fence;
 	struct drm_exec exec;
@@ -520,7 +520,7 @@ int amdgpu_userq_signal_ioctl(struct drm_device *dev, void *data,
 		goto put_gobj_read;
 
 	/* Retrieve the user queue */
-	queue = xa_load(&userq_mgr->userq_xa, args->queue_id);
+	queue = amdgpu_userq_get(userq_mgr, args->queue_id);
 	if (!queue) {
 		r = -ENOENT;
 		goto put_gobj_write;
@@ -611,6 +611,9 @@ free_syncobj:
 free_syncobj_handles:
 	kfree(syncobj_handles);
 
+	if (queue)
+		amdgpu_userq_put(queue);
+
 	return r;
 }
 
@@ -625,7 +628,7 @@ int amdgpu_userq_wait_ioctl(struct drm_device *dev, void *data,
 	struct amdgpu_userq_mgr *userq_mgr = &fpriv->userq_mgr;
 	struct drm_gem_object **gobj_write, **gobj_read;
 	u32 *timeline_points, *timeline_handles;
-	struct amdgpu_usermode_queue *waitq;
+	struct amdgpu_usermode_queue *waitq = NULL;
 	u32 *syncobj_handles, num_syncobj;
 	struct dma_fence **fences = NULL;
 	u16 num_points, num_fences = 0;
@@ -861,7 +864,7 @@ int amdgpu_userq_wait_ioctl(struct drm_device *dev, void *data,
 		 */
 		num_fences = dma_fence_dedup_array(fences, num_fences);
 
-		waitq = xa_load(&userq_mgr->userq_xa, wait_info->waitq_id);
+		waitq = amdgpu_userq_get(userq_mgr, wait_info->waitq_id);
 		if (!waitq) {
 			r = -EINVAL;
 			goto free_fences;
@@ -944,6 +947,9 @@ free_timeline_handles:
 	kfree(timeline_handles);
 free_syncobj_handles:
 	kfree(syncobj_handles);
+
+	if (waitq)
+		amdgpu_userq_put(waitq);
 
 	return r;
 }
