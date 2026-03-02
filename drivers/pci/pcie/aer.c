b0ac1583b912ba89395861a17d0e9fa72b9a2df2
Terry Bowman <terry.bowman@amd.com>
2026-03-02 14:36:39 -0600

    PCI/AER: Introduce AER-CXL Kfifo
    CXL virtual hierarchy (VH) RAS handling for CXL Port devices will be added
    soon. This requires a notification mechanism for the AER driver to share
    the AER interrupt with the CXL driver. The notification will be used as an
    indication for the CXL drivers to handle and log the CXL RAS errors.
    
    Note, 'CXL protocol error' terminology will refer to CXL VH and not
    CXL RCH errors unless specifically noted going forward.
    
    Introduce a new file in the AER driver to handle the CXL protocol errors
    named pci/pcie/aer_cxl_vh.c.
    
    Add a kfifo work queue to be used by the AER and CXL drivers. The AER
    driver will be the sole kfifo producer adding work and the cxl_core will be
    the sole kfifo consumer removing work. Add the boilerplate kfifo support.
    Encapsulate the kfifo, RW semaphore, and work pointer in a single structure.
    
    Add CXL work queue handler registration functions in the AER driver. Export
    the functions allowing CXL driver to access. Implement registration
    functions for the CXL driver to assign or clear the work handler function.
    
    Introduce 'struct cxl_proto_err_work_data' to serve as the kfifo work data.
    This will contain a reference to the PCI error source device and the error
    severity. This will be used when the work is dequeued by the cxl_core driver.
    
    Introduce cxl_forward_error() to take a given CXL protocol error and add it
    to a work structure before pushing onto the AER-CXL kfifo. This function
    takes a reference count increment of the PCI device. The kfifo consumer is
    responsible for reference decrementing. If there is an error on adding the
    work then this function must decrement the reference count.
    
    Synchronize accesses to the work function pointer during registration,
    deregistration, enqueue, and dequeue. Further synchronization fixes will
    be added in the following patch.
    
    Signed-off-by: Terry Bowman <terry.bowman@amd.com>
    Reviewed-by: Jonathan Cameron <jonathan.cameron@huawei.com>
    Reviewed-by: Dave Jiang <dave.jiang@intel.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>

--- drivers/pci/pcie/aer.c
+++ drivers/pci/pcie/aer.c
@@ -1152,16 +1152,6 @@ void pci_aer_unmask_internal_errors(struct pci_dev *dev)
  */
 EXPORT_SYMBOL_FOR_MODULES(pci_aer_unmask_internal_errors, "cxl_core");
 
-#ifdef CONFIG_CXL_RAS
-bool is_aer_internal_error(struct aer_err_info *info)
-{
-	if (info->severity == AER_CORRECTABLE)
-		return info->status & PCI_ERR_COR_INTERNAL;
-
-	return info->status & PCI_ERR_UNC_INTN;
-}
-#endif
-
 /**
  * pci_aer_handle_error - handle logging error into an event log
  * @dev: pointer to pci_dev data structure of error source device
@@ -1198,7 +1188,10 @@ static void pci_aer_handle_error(struct pci_dev *dev, struct aer_err_info *info)
 static void handle_error_source(struct pci_dev *dev, struct aer_err_info *info)
 {
 	cxl_rch_handle_error(dev, info);
-	pci_aer_handle_error(dev, info);
+	if (is_cxl_error(dev, info))
+		cxl_forward_error(dev, info);
+	else
+		pci_aer_handle_error(dev, info);
 	pci_dev_put(dev);
 }
 
