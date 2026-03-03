ba20ee19c407b560a695d18649597699a12554eb
Qing Wang <wangqing7171@gmail.com>
2026-03-03 11:40:33 +0800

    f2fs: drop unused parameter 'sbi'
    The 'sbi' parameter in f2fs_in_warm_node_list() is not used within the
    function. Remove it to simplify the function signature and all call sites.
    
    Signed-off-by: Qing Wang <wangqing7171@gmail.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -397,7 +397,7 @@ static void f2fs_write_end_io(struct bio *bio)
 				wq_has_sleeper(&sbi->cp_wait))
 			wake_up(&sbi->cp_wait);
 
-		if (f2fs_in_warm_node_list(sbi, folio))
+		if (f2fs_in_warm_node_list(folio))
 			f2fs_del_fsync_node_entry(sbi, folio);
 		folio_clear_f2fs_gcing(folio);
 		folio_end_writeback(folio);
