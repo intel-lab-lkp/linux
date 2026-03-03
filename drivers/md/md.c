32fb7cc54949df9ca6a34bce96375833d8f4167b
Su Yue <glass.su@suse.com>
2026-03-03 11:37:30 +0800

    md: call md_bitmap_create,destroy in location_store
    If commit
    'md: restore bitmap/location to fix wrong bitmap offset while growing'
    is applied, mdadm will call update_array_info() while growing bitmap from
    none to internal via location_store(). md_bitmap_create() is needed to set
    mddev->bitmap_ops otherwise mddev->bitmap_ops->get_stats() in
    update_array_info() will trigger kernel NULL pointer dereference.
    
    Fixes: fb8cc3b0d9db ("md/md-bitmap: delay registration of bitmap_ops until creating bitmap")
    Signed-off-by: Su Yue <glass.su@suse.com>

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -6455,7 +6455,7 @@ static void md_safemode_timeout(struct timer_list *t)
 
 static int start_dirty_degraded;
 
-static int md_bitmap_create(struct mddev *mddev)
+int md_bitmap_create(struct mddev *mddev)
 {
 	if (mddev->bitmap_id == ID_BITMAP_NONE)
 		return -EINVAL;
@@ -6466,7 +6466,7 @@ static int md_bitmap_create(struct mddev *mddev)
 	return mddev->bitmap_ops->create(mddev);
 }
 
-static void md_bitmap_destroy(struct mddev *mddev)
+void md_bitmap_destroy(struct mddev *mddev)
 {
 	if (!md_bitmap_registered(mddev))
 		return;
