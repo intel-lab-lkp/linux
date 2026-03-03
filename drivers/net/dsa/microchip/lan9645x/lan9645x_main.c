803e1a759dd9ddc2a64a8531279f320b4781c1ae
Jens Emil Schulz Østergaard <jensemil.schulzostergaard@microchip.com>
2026-03-03 13:22:33 +0100

    net: dsa: lan9645x: add mac table integration
    Add MAC table support, and dsa fdb callback integration. The mactable is
    keyed on (vid,mac) and each bucket has 4 slots. A mac table entry
    typically points to a PGID index, the first 9 of which represent a front
    port.
    
    Mac table entries for L2 multicast will use a PGID containing a group
    port mask. For IP multicast entries in the mac table a trick us used,
    where the group port mask is packed into the MAC data, exploiting the
    fact that the top bits are fixed, and that the number of switch ports is
    small enough to fit in the redundant bits.
    
    Therefore, we can avoid using sparse PGID resources for IP multicast
    entries in the mac table.
    
    Reviewed-by: Steen Hegelund <Steen.Hegelund@microchip.com>
    Signed-off-by: Jens Emil Schulz Østergaard <jensemil.schulzostergaard@microchip.com>

--- drivers/net/dsa/microchip/lan9645x/lan9645x_main.c
+++ drivers/net/dsa/microchip/lan9645x/lan9645x_main.c
@@ -78,6 +78,7 @@ static void lan9645x_teardown(struct dsa_switch *ds)
 
 	debugfs_remove_recursive(lan9645x->debugfs_root);
 	lan9645x_npi_port_deinit(lan9645x, lan9645x->npi);
+	lan9645x_mac_deinit(lan9645x);
 }
 
 static int lan9645x_change_mtu(struct dsa_switch *ds, int port, int new_mtu)
@@ -171,8 +172,8 @@ static int lan9645x_setup(struct dsa_switch *ds)
 		return err;
 	}
 
-	mutex_init(&lan9645x->fwd_domain_lock);
 	lan9645x_vlan_init(lan9645x);
+	lan9645x_mac_init(lan9645x);
 
 	/* Link Aggregation Mode: NETDEV_LAG_HASH_L2 */
 	lan_wr(ANA_AGGR_CFG_AC_SMAC_ENA |
@@ -534,6 +535,107 @@ static int lan9645x_port_vlan_del(struct dsa_switch *ds, int port,
 	return 0;
 }
 
+static void lan9645x_port_fast_age(struct dsa_switch *ds, int port)
+{
+	lan9645x_mact_flush(ds->priv, port);
+}
+
+static int lan9645x_fdb_dump(struct dsa_switch *ds, int port,
+			     dsa_fdb_dump_cb_t *cb, void *data)
+{
+	return lan9645x_mact_dsa_dump(ds->priv, port, cb, data);
+}
+
+static int __lan9645x_fdb_add(struct lan9645x *lan9645x, int pgid,
+			      const unsigned char *mac, u16 vid,
+			      struct net_device *bridge)
+{
+	if (!vid)
+		vid = lan9645x_vlan_unaware_pvid(!!bridge);
+
+	return lan9645x_mact_entry_add(lan9645x, pgid, mac, vid);
+}
+
+static struct net_device *lan9645x_db2bridge(struct dsa_db db)
+{
+	switch (db.type) {
+	case DSA_DB_PORT:
+	case DSA_DB_LAG:
+		return NULL;
+	case DSA_DB_BRIDGE:
+		return db.bridge.dev;
+	default:
+		return ERR_PTR(-EOPNOTSUPP);
+	}
+}
+
+static int lan9645x_fdb_add(struct dsa_switch *ds, int port,
+			    const unsigned char *addr, u16 vid,
+			    struct dsa_db db)
+{
+	struct net_device *br = lan9645x_db2bridge(db);
+	struct dsa_port *dp = dsa_to_port(ds, port);
+	struct lan9645x *lan9645x = ds->priv;
+
+	if (IS_ERR(br))
+		return PTR_ERR(br);
+
+	if (dsa_port_is_cpu(dp) && !br &&
+	    dsa_fdb_present_in_other_db(ds, port, addr, vid, db))
+		return 0;
+
+	if (dsa_port_is_cpu(dp)) {
+		/* Trap DSA cpu port */
+		return lan9645x_mact_learn(lan9645x, PGID_CPU, addr,
+					   lan9645x_vlan_unaware_pvid(!!br),
+					   ENTRYTYPE_LOCKED);
+	}
+
+	return __lan9645x_fdb_add(lan9645x, port, addr, vid, br);
+}
+
+static int __lan9645x_fdb_del(struct lan9645x *lan9645x, int port,
+			      const unsigned char *addr, u16 vid,
+			      struct net_device *bridge)
+{
+	int err;
+
+	if (!vid)
+		vid = lan9645x_vlan_unaware_pvid(!!bridge);
+
+	err = lan9645x_mact_entry_del(lan9645x, port, addr, vid);
+	if (err == -ENOENT) {
+		dev_dbg(lan9645x->dev, "fdb not found mac %pM vid %u pgid %u",
+			addr, vid, port);
+		return 0;
+	}
+	return err;
+}
+
+static int lan9645x_fdb_del(struct dsa_switch *ds, int port,
+			    const unsigned char *addr, u16 vid,
+			    struct dsa_db db)
+{
+	struct net_device *br = lan9645x_db2bridge(db);
+	struct dsa_port *dp = dsa_to_port(ds, port);
+	struct lan9645x *lan9645x = ds->priv;
+
+	if (IS_ERR(br))
+		return PTR_ERR(br);
+
+	if (dsa_port_is_cpu(dp) && !br &&
+	    dsa_fdb_present_in_other_db(ds, port, addr, vid, db))
+		return 0;
+
+	if (dsa_port_is_cpu(dp)) {
+		return lan9645x_mact_forget(lan9645x, addr,
+					    lan9645x_vlan_unaware_pvid(!!br),
+					    ENTRYTYPE_LOCKED);
+	}
+
+	return __lan9645x_fdb_del(lan9645x, port, addr, vid, br);
+}
+
 static const struct dsa_switch_ops lan9645x_switch_ops = {
 	.get_tag_protocol		= lan9645x_get_tag_protocol,
 	.connect_tag_protocol		= lan9645x_connect_tag_protocol,
@@ -560,6 +662,12 @@ static const struct dsa_switch_ops lan9645x_switch_ops = {
 	.port_vlan_filtering		= lan9645x_port_vlan_filtering,
 	.port_vlan_add			= lan9645x_port_vlan_add,
 	.port_vlan_del			= lan9645x_port_vlan_del,
+
+	/* MAC table integration */
+	.port_fast_age			= lan9645x_port_fast_age,
+	.port_fdb_dump			= lan9645x_fdb_dump,
+	.port_fdb_add			= lan9645x_fdb_add,
+	.port_fdb_del			= lan9645x_fdb_del,
 };
 
 static int lan9645x_request_target_regmaps(struct lan9645x *lan9645x)
--- drivers/net/dsa/microchip/lan9645x/lan9645x_main.h
+++ drivers/net/dsa/microchip/lan9645x/lan9645x_main.h
@@ -158,6 +158,34 @@ enum lan9645x_vlan_port_tag {
 	LAN9645X_TAG_ALL = 3,
 };
 
+/* MAC table entry types.
+ * ENTRYTYPE_NORMAL is subject to aging.
+ * ENTRYTYPE_LOCKED is not subject to aging.
+ * ENTRYTYPE_MACv4 is not subject to aging. For IPv4 multicast.
+ * ENTRYTYPE_MACv6 is not subject to aging. For IPv6 multicast.
+ */
+enum macaccess_entry_type {
+	ENTRYTYPE_NORMAL = 0,
+	ENTRYTYPE_LOCKED,
+	ENTRYTYPE_MACV4,
+	ENTRYTYPE_MACV6,
+};
+
+struct lan9645x_mact_common {
+	struct lan9645x_mact_key {
+		u16 vid;
+		u8 mac[ETH_ALEN] __aligned(2);
+	} key;
+	u32 row: 11, /* 2048 rows, 4 buckets each */
+	    pgid: 6, /* 0-63 general purpose pgids. */
+	    type: 2;
+};
+
+struct lan9645x_mact_entry {
+	struct lan9645x_mact_common common;
+	struct list_head list;
+};
+
 struct lan9645x {
 	struct device *dev;
 	struct dsa_switch *ds;
@@ -180,6 +208,9 @@ struct lan9645x {
 	u16 bridge_mask; /* Mask for bridged ports */
 	u16 bridge_fwd_mask; /* Mask for forwarding bridged ports */
 	struct mutex fwd_domain_lock; /* lock forwarding configuration */
+	struct list_head mac_entries;
+	struct mutex mact_lock; /* lock access to mact_table */
+	struct mutex mac_entry_lock; /* lock for mac_entries list */
 
 	/* VLAN */
 	u16 vlan_mask[VLAN_N_VID]; /* Port mask per vlan */
@@ -423,5 +454,22 @@ void lan9645x_vlan_cpu_clear_vlan(struct lan9645x *lan9645x, u16 vid);
 void lan9645x_vlan_set_hostmode(struct lan9645x_port *p);
 int lan9645x_port_vlan_prepare(struct lan9645x_port *p, u16 vid, bool pvid,
 			       bool untagged, struct netlink_ext_ack *extack);
+
+/* MAC table: lan9645x_mac.c */
+int lan9645x_mact_flush(struct lan9645x *lan9645x, int port);
+int lan9645x_mact_learn(struct lan9645x *lan9645x, int port,
+			const unsigned char *addr, u16 vid,
+			enum macaccess_entry_type type);
+int lan9645x_mact_forget(struct lan9645x *lan9645x,
+			 const unsigned char mac[ETH_ALEN], unsigned int vid,
+			 enum macaccess_entry_type type);
+void lan9645x_mac_init(struct lan9645x *lan9645x);
+void lan9645x_mac_deinit(struct lan9645x *lan9645x);
+int lan9645x_mact_dsa_dump(struct lan9645x *lan9645x, int port,
+			   dsa_fdb_dump_cb_t *cb, void *data);
+int lan9645x_mact_entry_del(struct lan9645x *lan9645x, int pgid,
+			    const unsigned char *mac, u16 vid);
+int lan9645x_mact_entry_add(struct lan9645x *lan9645x, int pgid,
+			    const unsigned char *mac, u16 vid);
 
 #endif /* __LAN9645X_MAIN_H__ */
