4558c2837a4d734748f5f17784373652c18a29a4
Chris J Arges <carges@cloudflare.com>
2026-03-02 20:43:52 -0600

    selftests: drv-net: xdp: Add rss_hash metadata tests
    This test loads xdp_metadata.bpf which calls bpf_xdp_metadata_rx_hash() on
    incoming packets. The metadata from that packet is then sent to a BPF
    map for validation. It borrows structure from xdp.py, reusing common
    functions.
    
    The test checks the device's xdp-rx-metadata-features via netlink
    before running and skips on devices that do not advertise hash support.
    This can be run on veth devices as well as real hardware.
    
    The test is fairly simple and just verifies that a TCP or UDP packet can be
    identified as an L4 flow. This minimal test also passes if run on a veth
    device.
    
    Signed-off-by: Chris J Arges <carges@cloudflare.com>

--- tools/testing/selftests/drivers/net/hw/lib/py/__init__.py
+++ tools/testing/selftests/drivers/net/hw/lib/py/__init__.py
@@ -24,6 +24,7 @@ try:
     from net.lib.py import bkg, cmd, bpftool, bpftrace, defer, ethtool, \
         fd_read_timeout, ip, rand_port, rand_ports, wait_port_listen, \
         wait_file, tool
+    from net.lib.py import bpf_map_set, bpf_map_dump, bpf_prog_map_ids
     from net.lib.py import KsftSkipEx, KsftFailEx, KsftXfailEx
     from net.lib.py import ksft_disruptive, ksft_exit, ksft_pr, ksft_run, \
         ksft_setup, ksft_variants, KsftNamedVariant
@@ -39,6 +40,7 @@ try:
                "bkg", "cmd", "bpftool", "bpftrace", "defer", "ethtool",
                "fd_read_timeout", "ip", "rand_port", "rand_ports",
                "wait_port_listen", "wait_file", "tool",
+               "bpf_map_set", "bpf_map_dump", "bpf_prog_map_ids",
                "KsftSkipEx", "KsftFailEx", "KsftXfailEx",
                "ksft_disruptive", "ksft_exit", "ksft_pr", "ksft_run",
                "ksft_setup", "ksft_variants", "KsftNamedVariant",
