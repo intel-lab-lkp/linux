cc626624ebd9ae09bec7a16571dd73e1089b75a6
Eduard Bostina <egbostina@gmail.com>
2026-03-04 15:34:26 +0000

    staging: rtl8723bs: Clean up unnecessary parentheses and braces
    Increase code readability and follow Linux kernel coding style by
    removing unnecessary parentheses and braces.
    
    This also fixes the following checkpatch.pl issues:
    WARNING: Unnecessary parentheses - maybe == should be = ?
    WARNING: braces {} are not necessary for any arm of this statement
    
    Signed-off-by: Eduard Bostina <egbostina@gmail.com>

--- drivers/staging/rtl8723bs/core/rtw_recv.c
+++ drivers/staging/rtl8723bs/core/rtw_recv.c
@@ -1339,11 +1339,10 @@ static signed int validate_recv_data_frame(struct adapter *adapter, union recv_f
 
 	}
 
-	if (ret == _FAIL) {
+	if (ret == _FAIL)
 		goto exit;
-	} else if (ret == RTW_RX_HANDLED) {
+	else if (ret == RTW_RX_HANDLED)
 		goto exit;
-	}
 
 
 	if (!psta) {
@@ -1593,7 +1592,7 @@ static signed int wlanhdr_to_ethhdr(union recv_frame *precvframe)
 	eth_type = ntohs(be_tmp); /* pattrib->ether_type */
 	pattrib->eth_type = eth_type;
 
-	if ((check_fwstate(pmlmepriv, WIFI_MP_STATE) == true)) {
+	if (check_fwstate(pmlmepriv, WIFI_MP_STATE) == true) {
 		ptr += rmv_len;
 		*ptr = 0x87;
 		*(ptr + 1) = 0x12;
