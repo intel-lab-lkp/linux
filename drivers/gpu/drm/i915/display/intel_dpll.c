12d7cd85fd13bd420b69778df544a1d7c0f98c03
Mika Kahola <mika.kahola@intel.com>
2026-03-04 13:14:14 +0000

    drm/i915/lt_phy: Add xe3plpd .crtc_get_dpll
    Add .crtc_get_dpll function pointer to support xe3plpd
    platform.
    
    Signed-off-by: Mika Kahola <mika.kahola@intel.com>
    Reviewed-by: Suraj Kandpal <suraj.kandpal@intel.com>

--- drivers/gpu/drm/i915/display/intel_dpll.c
+++ drivers/gpu/drm/i915/display/intel_dpll.c
@@ -1691,6 +1691,7 @@ static int i8xx_crtc_compute_clock(struct intel_atomic_state *state,
 
 static const struct intel_dpll_global_funcs xe3plpd_dpll_funcs = {
 	.crtc_compute_clock = xe3plpd_crtc_compute_clock,
+	.crtc_get_dpll = hsw_crtc_get_dpll,
 };
 
 static const struct intel_dpll_global_funcs mtl_dpll_funcs = {
