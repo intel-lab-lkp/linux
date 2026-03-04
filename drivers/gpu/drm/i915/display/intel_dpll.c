2fdcbcf446b3e8cdef21a70c523e8409d8a06d98
Mika Kahola <mika.kahola@intel.com>
2026-03-04 13:14:13 +0000

    drm/i915/lt_phy: Add xe3plpd .get_freq hook
    Add .get_freq function hook to support dpll framework for xe3plpd
    platform.
    
    Signed-off-by: Mika Kahola <mika.kahola@intel.com>

--- drivers/gpu/drm/i915/display/intel_dpll.c
+++ drivers/gpu/drm/i915/display/intel_dpll.c
@@ -1219,16 +1219,11 @@ static int xe3plpd_crtc_compute_clock(struct intel_atomic_state *state,
 		intel_atomic_get_new_crtc_state(state, crtc);
 	struct intel_encoder *encoder =
 		intel_get_crtc_new_encoder(state, crtc_state);
-	struct intel_display *display = to_intel_display(encoder);
 	int ret;
 
 	ret = intel_lt_phy_pll_calc_state(crtc_state, encoder, &crtc_state->dpll_hw_state);
 	if (ret)
 		return ret;
-
-	/* TODO: Do the readback via intel_compute_shared_dplls() */
-	crtc_state->port_clock =
-			intel_lt_phy_calc_port_clock(display, &crtc_state->dpll_hw_state.ltpll);
 
 	crtc_state->hw.adjusted_mode.crtc_clock = intel_crtc_dotclock(crtc_state);
 
