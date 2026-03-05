64cdfd7c6f58464e03b3d0b2c9c76b69a7d9db56
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2026-03-05 21:21:55 +0100

    ACPI: driver: Do not set acpi_device_class() unnecessarily
    Several core ACPI device drivers set acpi_device_class() for the given
    struct acpi_device to whatever they like, but that value is never used
    unless the driver itself uses it and, sadly, they neglect to clear it on
    remove.  Since the only one of them still using acpi_device_class()
    after previous changes is the button driver, update the others to stop
    setting it in vain.  Also drop the related device class sybmols that
    become redundant.
    
    Since the ACPI button driver continues to use acpi_device_class(), make
    it clear the struct field represented by acpi_device_class() in its
    remove callback.
    
    No intentional functional impact.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>

--- drivers/acpi/acpi_video.c
+++ drivers/acpi/acpi_video.c
@@ -1143,8 +1143,6 @@ static int acpi_video_bus_get_one_device(struct acpi_device *device, void *arg)
 		return -ENOMEM;
 	}
 
-	strscpy(acpi_device_class(device), ACPI_VIDEO_CLASS);
-
 	data->device_id = device_id;
 	data->video = video;
 	data->dev = device;
@@ -2020,7 +2018,6 @@ static int acpi_video_bus_probe(struct platform_device *pdev)
 	platform_set_drvdata(pdev, video);
 
 	video->device = device;
-	strscpy(acpi_device_class(device), ACPI_VIDEO_CLASS);
 	device->driver_data = video;
 
 	acpi_video_bus_find_cap(video);
