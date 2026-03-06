a665d13f78c606e33cdcf55cc0a3b184118a68e8
Yosry Ahmed <yosry@kernel.org>
2026-03-06 21:08:55 +0000

    KVM: SVM: Use maxphyaddr in emulator RAX check for VMRUN/VMLOAD/VMSAVE
    Architecturally, VMRUN/VMLOAD/VMSAVE should generate a #GP if the
    physical address in RAX is not supported. check_svme_pa() hardcodes this
    to checking that bits 63-48 are not set. This is incorrect on HW
    supporting 52 bits of physical address space, so use maxphyaddr instead.
    
    Note that the host's maxphyaddr is used, not the guest, because the
    emulator path for VMLOAD/VMSAVE is generally used when virtual
    VMLOAD/VMSAVE is enabled AND a #NPF is generated. If a #NPF is not
    generated, the CPU will inject a #GP based on the host's maxphyaddr.  So
    this keeps the behavior consistent.
    
    If KVM wants to consistently inject a #GP based on the guest's
    maxphyaddr, it would need to disabled virtual VMLOAD/VMSAVE and
    intercept all VMLOAD/VMSAVE instructions to do the check.
    
    Also, emulating a smaller maxphyaddr for the guest than the host
    generally doesn't work well, so it's not worth handling this.
    
    Fixes: 01de8b09e606 ("KVM: SVM: Add intercept checks for SVM instructions")
    Signed-off-by: Yosry Ahmed <yosry@kernel.org>

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -3887,8 +3887,7 @@ static int check_svme_pa(struct x86_emulate_ctxt *ctxt)
 {
 	u64 rax = reg_read(ctxt, VCPU_REGS_RAX);
 
-	/* Valid physical address? */
-	if (rax & 0xffff000000000000ULL)
+	if (rax & rsvd_bits(kvm_host.maxphyaddr, 63))
 		return emulate_gp(ctxt, 0);
 
 	return check_svme(ctxt);
