28d6c8695900f7eee0c0909bffd2be9948436f2a
Antoniu Miclaus <antoniu.miclaus@analog.com>
2026-03-06 12:24:48 +0200

    iio: frequency: ad9523: use dev_err_probe
    Use dev_err_probe() instead of dev_err() in the probe path to ensure
    proper handling of deferred probing and to simplify error handling.
    
    Signed-off-by: Antoniu Miclaus <antoniu.miclaus@analog.com>

--- drivers/iio/frequency/ad9523.c
+++ drivers/iio/frequency/ad9523.c
@@ -954,10 +954,8 @@ static int ad9523_probe(struct spi_device *spi)
 	struct ad9523_state *st;
 	int ret;
 
-	if (!pdata) {
-		dev_err(dev, "no platform data?\n");
-		return -EINVAL;
-	}
+	if (!pdata)
+		return dev_err_probe(dev, -EINVAL, "no platform data?\n");
 
 	indio_dev = devm_iio_device_alloc(dev, sizeof(*st));
 	if (indio_dev == NULL)
