4251d831b0f2f83397439a4c8cc875eb7e4118dc
Peng Fan <peng.fan@nxp.com>
2026-03-06 14:20:12 +0800

    dt-bindings: clock: Add SCMI clock rounding mode declarations
    The ARM System Control and Management Interface (SCMI) specification
    (DEN0056E) defines the CLOCK_RATE_SET command, which allows clients to
    specify how the platform should round the requested clock rate.
    
    Bits[3:2] define the rounding behaviour:
      - If Bit[3] is set, the platform autonomously selects the closest
        achievable rate, ignoring Bit[2].
      - If Bit[3] is clear, Bit[2] selects the direction:
            0 = round down
            1 = round up
    
    Add dt-bindings definitions for these rounding modes so that device tree
    agent nodes can specify the desired rounding behaviour to the firmware.
    
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

---
+++ include/dt-bindings/clock/scmi.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
+/*
+ * Copyright 2026 NXP
+ */
+
+#ifndef __SCMI_CLOCK_H
+#define __SCMI_CLOCK_H
+
+#define ROUND_DOWN	0
+#define ROUND_UP	1
+#define ROUND_AUTO	2
+
+#endif
