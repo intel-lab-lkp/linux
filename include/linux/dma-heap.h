2cd6ef19cff178e7e9273b35934cbe2e205b11a6
John Stultz <john.stultz@linaro.org>
2026-03-06 11:36:32 +0100

    dma-buf: dma-heap: Keep track of the heap device struct
    Keep track of the heap device struct.
    
    This will be useful for special DMA allocations
    and actions.
    
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Reviewed-by: Maxime Ripard <mripard@kernel.org>
    Signed-off-by: Albert Esteve <aesteve@redhat.com>

--- include/linux/dma-heap.h
+++ include/linux/dma-heap.h
@@ -12,6 +12,7 @@
 #include <linux/types.h>
 
 struct dma_heap;
+struct device;
 
 /**
  * struct dma_heap_ops - ops to operate on a given heap
@@ -43,6 +44,7 @@ struct dma_heap_export_info {
 void *dma_heap_get_drvdata(struct dma_heap *heap);
 
 const char *dma_heap_get_name(struct dma_heap *heap);
+struct device *dma_heap_get_dev(struct dma_heap *heap);
 
 struct dma_heap *dma_heap_add(const struct dma_heap_export_info *exp_info);
 
