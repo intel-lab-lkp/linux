796224b551b7470b2df148709a06b8a2bc757c42
Long Li <longli@microsoft.com>
2026-03-06 17:47:19 -0800

    RDMA/mana_ib: Track QP per ucontext
    Add per-ucontext list tracking for QP objects. Only RAW_PACKET QPs
    are tracked since they persist across reset events. RC, UD and GSI
    QPs are removed and re-added during reset by IB core and do not
    need tracking.
    
    Signed-off-by: Long Li <longli@microsoft.com>

--- drivers/infiniband/hw/mana/mana_ib.h
+++ drivers/infiniband/hw/mana/mana_ib.h
@@ -198,6 +198,7 @@ struct mana_ib_qp {
 
 	refcount_t		refcount;
 	struct completion	free;
+	struct list_head	ucontext_list;
 };
 
 struct mana_ib_ucontext {
@@ -208,6 +209,7 @@ struct mana_ib_ucontext {
 	struct mutex lock;
 	struct list_head pd_list;
 	struct list_head cq_list;
+	struct list_head qp_list;
 	struct list_head wq_list;
 };
 
