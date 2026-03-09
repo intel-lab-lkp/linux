2cd7aa54439b23473ba7ddf3fe998e8ae807c057
Christian Borntraeger <borntraeger@linux.ibm.com>
2026-03-09 12:59:04 +0100

    KVM: s390: log machine checks more aggressively
    KVM will reinject machine checks that happen during guest activity.
    From a host perspective this machine check is no longer visible
    and even for the guest, the guest might decide to only kill a
    userspace program or even ignore the machine check.
    As this can be a disruptive event nevertheless, we should log this
    not only in the VM debug event (that gets lost after guest shutdown)
    but also on the global KVM event as well as syslog.
    Consolidate the logging and log with loglevel 2 and higher.
    
    Signed-off-by: Christian Borntraeger <borntraeger@linux.ibm.com>

--- arch/s390/kvm/interrupt.c
+++ arch/s390/kvm/interrupt.c
@@ -2824,6 +2824,12 @@ void kvm_s390_reinject_machine_check(struct kvm_vcpu *vcpu,
 	int rc;
 
 	mci.val = mcck_info->mcic;
+
+	/* log machine checks being reinjected on all debugs */
+	VCPU_EVENT(vcpu, 2, "guest machine check %lx", mci.val);
+	KVM_EVENT(2, "guest_machine check %lx", mci.val);
+	pr_info("guest_machine check pid %d: %lx", current->pid, mci.val);
+
 	if (mci.sr)
 		cr14 |= CR14_RECOVERY_SUBMASK;
 	if (mci.dg)
