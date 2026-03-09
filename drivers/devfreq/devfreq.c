47dd994b059e04a505b79ea5cf59b310df5180a5
Yaxiong Tian <tianyaxiong@kylinos.cn>
2026-03-09 19:54:55 +0800

    PM / devfreq: Optimize error return value of governor_show()
    When df->governor is NULL, governor_show() returns -EINVAL, which
    confuses users.
    
    To fix this issue, return -ENOENT to indicate that no governor is
    currently set for the device.
    
    Signed-off-by: Yaxiong Tian <tianyaxiong@kylinos.cn>

--- drivers/devfreq/devfreq.c
+++ drivers/devfreq/devfreq.c
@@ -1408,7 +1408,7 @@ static ssize_t governor_show(struct device *dev,
 	struct devfreq *df = to_devfreq(dev);
 
 	if (!df->governor)
-		return -EINVAL;
+		return -ENOENT;
 
 	return sprintf(buf, "%s\n", df->governor->name);
 }
