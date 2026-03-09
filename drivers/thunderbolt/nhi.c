0f189b5cd024b0eb3d5b8e8782e98e795d253033
Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
2026-03-09 11:33:01 +0100

    thunderbolt: Add some more descriptive probe error messages
    Currently there's a lot of silent error-return paths in various places
    where nhi_probe() can fail. Sprinkle some prints to make it clearer
    where the problem is.
    
    Signed-off-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>

--- drivers/thunderbolt/nhi.c
+++ drivers/thunderbolt/nhi.c
@@ -1184,7 +1184,7 @@ int nhi_probe_common(struct tb_nhi *nhi)
 	if (nhi->ops && nhi->ops->init) {
 		res = nhi->ops->init(nhi);
 		if (res)
-			return res;
+			return dev_err_probe(dev, res, "NHI specific init failed\n");
 	}
 
 	tb = nhi_select_cm(nhi);
@@ -1202,7 +1202,7 @@ int nhi_probe_common(struct tb_nhi *nhi)
 		 */
 		tb_domain_put(tb);
 		nhi_shutdown(nhi);
-		return res;
+		return dev_err_probe(dev, res, "tb_domain_add() failed\n");
 	}
 	dev_set_drvdata(dev, tb);
 
