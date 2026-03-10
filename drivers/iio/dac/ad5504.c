ea880661fae1d289f000cae8228900287a3b6fac
Taha Ed-Dafili <0rayn.dev@gmail.com>
2026-03-10 17:48:32 +0000

    iio: dac: ad5504: sort headers alphabetically
    Rearrange the include headers in alphabetical order to follow the
    standard kernel coding style. This is a preparatory cleanup with
    no functional changes.
    
    Suggested-by: Andy Shevchenko <andy@kernel.org>
    Signed-off-by: Taha Ed-Dafili <0rayn.dev@gmail.com>

--- drivers/iio/dac/ad5504.c
+++ drivers/iio/dac/ad5504.c
@@ -5,16 +5,16 @@
  * Copyright 2011 Analog Devices Inc.
  */
 
-#include <linux/interrupt.h>
-#include <linux/fs.h>
-#include <linux/device.h>
-#include <linux/kernel.h>
-#include <linux/spi/spi.h>
-#include <linux/slab.h>
-#include <linux/sysfs.h>
-#include <linux/regulator/consumer.h>
-#include <linux/module.h>
 #include <linux/bitops.h>
+#include <linux/device.h>
+#include <linux/fs.h>
+#include <linux/interrupt.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/regulator/consumer.h>
+#include <linux/slab.h>
+#include <linux/spi/spi.h>
+#include <linux/sysfs.h>
 
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
