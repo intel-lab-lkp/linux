e884b64696f375ad63f2c01607bffaeb10e3c893
Herve Codina (Schneider Electric) <herve.codina@bootlin.com>
2026-03-10 18:32:44 +0100

    dt-bindings: watchdog: renesas,rzn1-wdt: Document the reset line
    Watchdogs available in the RZ/N1 SoC can use their specific hardware
    reset line to reset the system on watchdog timeout.
    
    This line is not documented in the current binding.
    
    Fill this lack and describe this per watchdog reset line.
    
    Signed-off-by: Herve Codina (Schneider Electric) <herve.codina@bootlin.com>

---
+++ include/dt-bindings/watchdog/renesas,rzn1-wdt.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * RZ/N1 watchdog reset lines
+ *
+ * Copyright (C) 2026 Bootlin
+ *
+ * Herve Codina <herve.codina@bootlin.com>
+ */
+
+#ifndef __DT_BINDINGS_RZN1_WDT_H__
+#define __DT_BINDINGS_RZN1_WDT_H__
+
+#define RZN1_WDT_A7_0		0
+#define RZN1_WDT_A7_1		1
+
+#endif /* __DT_BINDINGS_RZN1_WDT_H__ */
