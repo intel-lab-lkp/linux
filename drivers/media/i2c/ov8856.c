0fbceffebfb5363730af20a4a56758a86db0f9dd
Alexander Koskovich <akoskovich@pm.me>
2026-03-10 04:21:34 +0000

    media: i2c: ov8856: parse and register V4L2 device tree properties
    Parse V4L2 device tree properties and register controls for them.
    
    Signed-off-by: Alexander Koskovich <akoskovich@pm.me>

--- drivers/media/i2c/ov8856.c
+++ drivers/media/i2c/ov8856.c
@@ -1887,12 +1887,14 @@ static const struct v4l2_ctrl_ops ov8856_ctrl_ops = {
 
 static int ov8856_init_controls(struct ov8856 *ov8856)
 {
+	struct i2c_client *client = v4l2_get_subdevdata(&ov8856->sd);
+	struct v4l2_fwnode_device_properties props;
 	struct v4l2_ctrl_handler *ctrl_hdlr;
 	s64 exposure_max, h_blank;
 	int ret;
 
 	ctrl_hdlr = &ov8856->ctrl_handler;
-	ret = v4l2_ctrl_handler_init(ctrl_hdlr, 8);
+	ret = v4l2_ctrl_handler_init(ctrl_hdlr, 10);
 	if (ret)
 		return ret;
 
@@ -1953,6 +1955,15 @@ static int ov8856_init_controls(struct ov8856 *ov8856)
 			  V4L2_CID_VFLIP, 0, 1, 1, 0);
 	if (ctrl_hdlr->error)
 		return ctrl_hdlr->error;
+
+	ret = v4l2_fwnode_device_parse(&client->dev, &props);
+	if (ret)
+		return ret;
+
+	ret = v4l2_ctrl_new_fwnode_properties(ctrl_hdlr, &ov8856_ctrl_ops,
+					      &props);
+	if (ret)
+		return ret;
 
 	ov8856->sd.ctrl_handler = ctrl_hdlr;
 
