6ebdbff670f8038fc845c3e939ebc60dd0493a09
Daniel Golle <daniel@makrotopia.org>
2026-03-11 00:49:44 +0000

    net: dsa: mxl862xx: implement bridge offloading
    Add support for bridge offloading on the mxl862xx DSA driver.
    
    Implement joining and leaving bridges as well as add, delete and dump
    operations on isolated FDBs and setting a port's STP state.
    
    The switch supports a maximum of 63 bridges, however, up to 12 may
    be used as "single-port bridges" to isolate standalone ports.
    Allowing up to 48 bridges to be offloaded seems more than enough on
    that hardware, hence that is set as max_num_bridges.
    
    A total of 128 bridge ports are supported in the bridge portmap, and
    virtual bridge ports have to be used eg. for link-aggregation, hence
    potentially exceeding the number of hardware ports.
    
    As there are now more users of the BRIDGEPORT_CONFIG_SET API and the
    state of each port is cached locally, introduce a helper function
    mxl862xx_set_bridge_port(struct dsa_switch *ds, int port) which is
    then also be used to replace the direct calls to the API in
    mxl862xx_setup_cpu_bridge() and mxl862xx_add_single_port_bridge().
    
    Note that there is no convenient way to control flooding on per-port
    level, so the driver is using a 0-rate QoS meter setup as a stopper in
    lack of any better option. This works, but allows a single 64-byte
    packet to pass once after reset. While this limitation doesn't seem to
    be a problem in practice, it has the effect that the
    bridge_vlan_unaware.sh selftest only passes the FDB test the 2nd time
    the test is run after boot (and any subsequent time after that, of
    course).
    
    Signed-off-by: Daniel Golle <daniel@makrotopia.org>

--- drivers/net/dsa/mxl862xx/mxl862xx.h
+++ drivers/net/dsa/mxl862xx/mxl862xx.h
@@ -7,10 +7,62 @@
 #include <net/dsa.h>
 
 #define MXL862XX_MAX_PORTS		17
+#define MXL862XX_DEFAULT_BRIDGE		0
+#define MXL862XX_MAX_BRIDGES		48
+#define MXL862XX_MAX_BRIDGE_PORTS	128
 
+/**
+ * struct mxl862xx_bridge - per-bridge state tracked by the driver
+ * @dsa_bridge_num: DSA framework bridge number, as returned by
+ *                  dsa_switch_bridge_join()
+ * @bridge_id:      firmware FID assigned to this bridge
+ * @portmap:        bitmap of ports belonging to this bridge
+ * @list:           entry in &mxl862xx_priv.bridges
+ */
+struct mxl862xx_bridge {
+	unsigned int dsa_bridge_num;
+	u16 bridge_id;
+	DECLARE_BITMAP(portmap, MXL862XX_MAX_BRIDGE_PORTS);
+	struct list_head list;
+};
+
+/**
+ * struct mxl862xx_port - per-port state tracked by the driver
+ * @fid:         firmware FID for the permanent single-port bridge; kept alive
+ *               for the lifetime of the port so traffic is never forwarded
+ *               while the port is unbridged
+ * @bridge:      bridge this port currently belongs to, or %NULL if unbridged
+ * @portmap:     bitmap of switch port indices that share the current bridge
+ *               with this port (mirrors @bridge->portmap for convenience)
+ * @flood_block: bitmask of firmware meter indices that are currently
+ *               rate-limiting flood traffic on this port (zero-rate meters
+ *               used to block flooding)
+ * @learning:    true when address learning is enabled on this port
+ */
+struct mxl862xx_port {
+	u16 fid;
+	struct mxl862xx_bridge *bridge;
+	DECLARE_BITMAP(portmap, MXL862XX_MAX_BRIDGE_PORTS);
+	unsigned long flood_block;
+	bool learning;
+};
+
+/**
+ * struct mxl862xx_priv - driver private data for an MxL862xx switch
+ * @ds:          pointer to the DSA switch instance
+ * @mdiodev:     MDIO device used to communicate with the switch firmware
+ * @drop_meter:  index of the single shared zero-rate firmware meter used
+ *               to unconditionally drop traffic (used to block flooding)
+ * @ports:       per-port state, indexed by switch port number
+ * @bridges:     list of &struct mxl862xx_bridge instances currently offloaded
+ *               to the hardware
+ */
 struct mxl862xx_priv {
 	struct dsa_switch *ds;
 	struct mdio_device *mdiodev;
+	u16 drop_meter;
+	struct mxl862xx_port ports[MXL862XX_MAX_PORTS];
+	struct list_head bridges;
 };
 
 #endif /* __MXL862XX_H */
