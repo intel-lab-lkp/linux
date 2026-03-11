e7e10f408fa80c70cc40f75c273be5b54be86b57
Marijn Suijten <marijn.suijten@somainline.org>
2026-03-11 23:31:09 +0100

    drm/msm/dsi: Restore widebus calculation for CMDMode panels
    Commit d5e3e440ca73 ("drm/msm/dsi: fix hdisplay calculation when
    programming dsi registers") makes a false and ungrounded statement that
    "Since CMD mode does not use this, we can remove !(msm_host->mode_flags
    & MIPI_DSI_MODE_VIDEO) safely." which isn't the case at all.
    dsi_timing_setup() affects both command mode and video mode panels, and
    by no longer having any path that sets bits_per_pclk = 48 (contrary to
    the updated code-comment) all DSI DSC panels on SM8350 and above (i.e.
    with widebus support) regress thanks to this patch.
    
    The entire reason that video mode was originally omitted from this code
    path is because it was never tested before; any change that enables
    widebus for video mode panels should not regress the command mode path.
    
    Thus add back the path allows 6 bytes or 48 bits to be sent per pclk
    on command mode DSI panels with widebus, restoring the panel on devices
    like the Sony Xperia 1 III and upwards.
    
    Fixes: d5e3e440ca73 ("drm/msm/dsi: fix hdisplay calculation when programming dsi registers")
    Signed-off-by: Marijn Suijten <marijn.suijten@somainline.org>

--- drivers/gpu/drm/msm/dsi/dsi_host.c
+++ drivers/gpu/drm/msm/dsi/dsi_host.c
@@ -1029,10 +1029,14 @@ static void dsi_timing_setup(struct msm_dsi_host *msm_host, bool is_bonded_dsi)
 		 * unused anyway.
 		 */
 		h_total -= hdisplay;
-		if (wide_bus_enabled)
-			bits_per_pclk = mipi_dsi_pixel_format_to_bpp(msm_host->format);
-		else
+		if (wide_bus_enabled) {
+			if (msm_host->mode_flags & MIPI_DSI_MODE_VIDEO)
+				bits_per_pclk = mipi_dsi_pixel_format_to_bpp(msm_host->format);
+			else
+				bits_per_pclk = 48;
+		} else {
 			bits_per_pclk = 24;
+		}
 
 		hdisplay = DIV_ROUND_UP(msm_dsc_get_bytes_per_line(msm_host->dsc) * 8, bits_per_pclk);
 
