3d22c512c2da572eee74012b02b7b37fcc3ab3d5
Ye Bin <yebin10@huawei.com>
2026-03-12 21:12:52 +0800

    ext4: fix possible null-ptr-deref in extents_kunit_exit()
    There's issue as follows:
    KASAN: null-ptr-deref in range [0x00000000000002c0-0x00000000000002c7]
    Tainted: [E]=UNSIGNED_MODULE, [N]=TEST
    RIP: 0010:extents_kunit_exit+0x2e/0xc0 [ext4_test]
    Call Trace:
     <TASK>
     kunit_try_run_case_cleanup+0xbc/0x100 [kunit]
     kunit_generic_run_threadfn_adapter+0x89/0x100 [kunit]
     kthread+0x408/0x540
     ret_from_fork+0xa76/0xdf0
     ret_from_fork_asm+0x1a/0x30
    
    Above issue happens as extents_kunit_init() init testcase failed.
    So test if testcase is inited success.
    
    Fixes: cb1e0c1d1fad ("ext4: kunit tests for extent splitting and conversion")
    Signed-off-by: Ye Bin <yebin10@huawei.com>

--- fs/ext4/extents-test.c
+++ fs/ext4/extents-test.c
@@ -142,8 +142,12 @@ static struct file_system_type ext_fs_type = {
 
 static void extents_kunit_exit(struct kunit *test)
 {
-	struct ext4_sb_info *sbi = k_ctx.k_ei->vfs_inode.i_sb->s_fs_info;
+	struct ext4_sb_info *sbi;
 
+	if (!k_ctx.k_ei)
+		return;
+
+	sbi = k_ctx.k_ei->vfs_inode.i_sb->s_fs_info;
 	deactivate_super(sbi->s_sb);
 	kfree(k_ctx.k_ei);
 	kfree(k_ctx.k_data);
