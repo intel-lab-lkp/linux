f7d26500a1739b25346601669a5560a133978f69
Pablo Neira Ayuso <pablo@netfilter.org>
2026-03-12 11:11:20 +0100

    nf_tables: nft_dynset: fix possible stateful expression memleak in error path
    If cloning the second stateful expression in the element via GFP_ATOMIC
    fails, then the first stateful expression remains in place without being
    released.
    
       unreferenced object (percpu) 0x607b97e9cab8 (size 16):
         comm "softirq", pid 0, jiffies 4294931867
         hex dump (first 16 bytes on cpu 3):
           00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
         backtrace (crc 0):
           pcpu_alloc_noprof+0x453/0xd80
           nft_counter_clone+0x9c/0x190 [nf_tables]
           nft_expr_clone+0x8f/0x1b0 [nf_tables]
           nft_dynset_new+0x2cb/0x5f0 [nf_tables]
           nft_rhash_update+0x236/0x11c0 [nf_tables]
           nft_dynset_eval+0x11f/0x670 [nf_tables]
           nft_do_chain+0x253/0x1700 [nf_tables]
           nft_do_chain_ipv4+0x18d/0x270 [nf_tables]
           nf_hook_slow+0xaa/0x1e0
           ip_local_deliver+0x209/0x330
    
    Pass NULL to nft_set_elem_expr_destroy() given stateful expressions do
    not require context at this stage.
    
    Fixes: 563125a73ac3 ("netfilter: nftables: generalize set extension to support for several expressions")
    Reported-by: Gurpreet Shergill <giki.shergill@proton.me>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nft_dynset.c
+++ net/netfilter/nft_dynset.c
@@ -36,12 +36,17 @@ static int nft_dynset_expr_setup(const struct nft_dynset *priv,
 	for (i = 0; i < priv->num_exprs; i++) {
 		expr = nft_setelem_expr_at(elem_expr, elem_expr->size);
 		if (nft_expr_clone(expr, priv->expr_array[i], GFP_ATOMIC) < 0)
-			return -1;
+			goto err_out;
 
 		elem_expr->size += priv->expr_array[i]->ops->size;
 	}
 
 	return 0;
+err_out:
+	/* Stateful expression do not need context, pass NULL. */
+	nft_set_elem_expr_destroy(NULL, elem_expr);
+
+	return -1;
 }
 
 struct nft_elem_priv *nft_dynset_new(struct nft_set *set,
