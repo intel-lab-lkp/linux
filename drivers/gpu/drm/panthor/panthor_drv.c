a3ba3fb448d5e9bb3da6b6c3834d3832f30faa41
Adrián Larumbe <adrian.larumbe@collabora.com>
2026-03-13 15:09:48 +0000

    drm/panthor: Bump the driver version to 1.8
    Bump the driver version to reflect the new MMU info query ioctl
    parameter and the VM_BIND repeat flag.
    
    Signed-off-by: Adrián Larumbe <adrian.larumbe@collabora.com>

--- drivers/gpu/drm/panthor/panthor_drv.c
+++ drivers/gpu/drm/panthor/panthor_drv.c
@@ -1688,6 +1688,8 @@ static void panthor_debugfs_init(struct drm_minor *minor)
  *       - adds DRM_IOCTL_PANTHOR_BO_SYNC ioctl
  *       - adds DRM_IOCTL_PANTHOR_BO_QUERY_INFO ioctl
  *       - adds drm_panthor_gpu_info::selected_coherency
+ * - 1.8 - adds DRM_PANTHOR_DEV_QUERY_MMU_INFO query
+ *       - adds DRM_PANTHOR_VM_BIND_OP_MAP_REPEAT flag
  */
 static const struct drm_driver panthor_drm_driver = {
 	.driver_features = DRIVER_RENDER | DRIVER_GEM | DRIVER_SYNCOBJ |
@@ -1701,7 +1703,7 @@ static const struct drm_driver panthor_drm_driver = {
 	.name = "panthor",
 	.desc = "Panthor DRM driver",
 	.major = 1,
-	.minor = 7,
+	.minor = 8,
 
 	.gem_create_object = panthor_gem_create_object,
 	.gem_prime_import_sg_table = drm_gem_shmem_prime_import_sg_table,
