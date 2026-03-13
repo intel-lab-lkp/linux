6e0371e07f2e2293935d32022bf4999c8365a98b
Jim Cromie <jim.cromie@gmail.com>
2026-03-13 07:20:28 -0600

    drm_buddy: fix 64-bit truncation in power-of-2 rounding
    The standard roundup_pow_of_two() and rounddown_pow_of_two() macros use
    unsigned long internally, which on 32-bit architectures (like arm32) is
    a 32-bit type.
    
    drm_test_buddy_alloc_exceeds_max_order() uses the on a u64 value,
    where they silently truncate the 10GB allocation, giving unexpected
    success in DRM-CI.  (see below the snip).
    
    Fix this by replacing the standard macros with safe 64-bit
    power-of-two calculations using ilog2().
    
    Signed-off-by: Jim Cromie <jim.cromie@gmail.com>

--- drivers/gpu/buddy.c
+++ drivers/gpu/buddy.c
@@ -1091,7 +1091,7 @@ static int __alloc_contig_try_harder(struct gpu_buddy *mm,
 	u64 modify_size;
 	int err;
 
-	modify_size = rounddown_pow_of_two(size);
+	modify_size = 1ULL << ilog2(size);
 	pages = modify_size >> ilog2(mm->chunk_size);
 	order = fls(pages) - 1;
 	if (order == 0)
@@ -1318,7 +1318,7 @@ int gpu_buddy_alloc_blocks(struct gpu_buddy *mm,
 
 	/* Roundup the size to power of 2 */
 	if (flags & GPU_BUDDY_CONTIGUOUS_ALLOCATION) {
-		size = roundup_pow_of_two(size);
+		size = 1ULL << (ilog2(size - 1) + 1);
 		min_block_size = size;
 		/*
 		 * Normalize the requested size to min_block_size for regular allocations.
