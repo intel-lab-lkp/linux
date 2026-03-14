9b31b33a0f59f5e11d0b953f4eaee76dccbd015d
Rajveer Chaudhari <rajveer.chaudhari.linux@gmail.com>
2026-03-14 22:36:07 +0530

    iio: accel: mma9551: convert to guard(mutex)
    Replace manual mutex_lock/mutex_unlock pair with guard(mutex) in
    mma9551_event_handler(). This ensures the mutex is
    released on all return paths and allows returning directly
    without a goto label.
    
    Signed-off-by: Rajveer Chaudhari <rajveer.chaudhari.linux@gmail.com>

--- drivers/iio/accel/mma9551.c
+++ drivers/iio/accel/mma9551.c
@@ -341,7 +341,7 @@ static irqreturn_t mma9551_event_handler(int irq, void *private)
 	u16 reg;
 	u8 val;
 
-	mutex_lock(&data->mutex);
+	guard(mutex)(&data->mutex);
 
 	for (i = 0; i < 3; i++)
 		if (irq == data->irqs[i]) {
@@ -353,7 +353,7 @@ static irqreturn_t mma9551_event_handler(int irq, void *private)
 		/* IRQ was triggered on 4th line, which we don't use. */
 		dev_warn(&data->client->dev,
 			 "irq triggered on unused line %d\n", data->irqs[3]);
-		goto out;
+		return IRQ_HANDLED;
 	}
 
 	switch (mma_axis) {
@@ -377,16 +377,13 @@ static irqreturn_t mma9551_event_handler(int irq, void *private)
 	if (ret < 0) {
 		dev_err(&data->client->dev,
 			"error %d reading tilt register in IRQ\n", ret);
-		goto out;
+		return IRQ_HANDLED;
 	}
 
 	iio_push_event(indio_dev,
 		       IIO_MOD_EVENT_CODE(IIO_INCLI, 0, (mma_axis + 1),
 					  IIO_EV_TYPE_ROC, IIO_EV_DIR_RISING),
 		       iio_get_time_ns(indio_dev));
-
-out:
-	mutex_unlock(&data->mutex);
 
 	return IRQ_HANDLED;
 }
