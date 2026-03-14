76fa66c31bfd33628f8d0034d93e5003e21dce65
Rajveer Chaudhari <rajveer.chaudhari.linux@gmail.com>
2026-03-14 22:36:06 +0530

    iio: accel: mma9551: sort headers alphabetically
    Signed-off-by: Rajveer Chaudhari <rajveer.chaudhari.linux@gmail.com>

--- drivers/iio/accel/mma9551.c
+++ drivers/iio/accel/mma9551.c
@@ -4,17 +4,21 @@
  * Copyright (c) 2014, Intel Corporation.
  */
 
+#include <linux/cleanup.h>
+#include <linux/delay.h>
 #include <linux/i2c.h>
 #include <linux/interrupt.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
+#include <linux/pm_runtime.h>
 #include <linux/slab.h>
-#include <linux/delay.h>
+
 #include <linux/gpio/consumer.h>
+
+#include <linux/iio/events.h>
 #include <linux/iio/iio.h>
 #include <linux/iio/sysfs.h>
-#include <linux/iio/events.h>
-#include <linux/pm_runtime.h>
+
 #include "mma9551_core.h"
 
 #define MMA9551_GPIO_COUNT		4
