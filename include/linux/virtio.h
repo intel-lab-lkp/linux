7108311d3006fa59b1a130420114497269e07087
Kit Dallege <xaum.io@gmail.com>
2026-03-15 15:57:52 +0100

    virtio: add kernel-doc for map and vmap members
    Document the map and vmap members of struct virtio_device that were
    added without kernel-doc descriptions.
    
    This fixes the following warnings when building with W=1:
    
      include/linux/virtio.h: struct member 'map' not described in 'virtio_device'
      include/linux/virtio.h: struct member 'vmap' not described in 'virtio_device'
    
    Signed-off-by: Kit Dallege <xaum.io@gmail.com>
    Signed-off-by: kovan <xaum.io@gmail.com>

--- include/linux/virtio.h
+++ include/linux/virtio.h
@@ -157,11 +157,13 @@ struct virtio_admin_cmd {
  * @id: the device type identification (used to match it with a driver).
  * @config: the configuration ops for this device.
  * @vringh_config: configuration ops for host vrings.
+ * @map: the mapping ops for this device.
  * @vqs: the list of virtqueues for this device.
  * @features: the 64 lower features supported by both driver and device.
  * @features_array: the full features space supported by both driver and
  *		    device.
  * @priv: private pointer for the driver's use.
+ * @vmap: mapping token passed to the mapping operations.
  * @debugfs_dir: debugfs directory entry.
  * @debugfs_filter_features: features to be filtered set by debugfs.
  */
