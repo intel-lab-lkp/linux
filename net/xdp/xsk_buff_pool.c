1b9c26a1fbe51ab988952efb2ae4e3598c21d07a
Maciej Fijalkowski <maciej.fijalkowski@intel.com>
2026-03-16 18:45:48 +0100

    xsk: validate MTU against usable frame size on bind
    AF_XDP bind currently accepts zero-copy pool configurations without
    verifying that the device MTU fits into the usable frame space provided
    by the UMEM chunk.
    
    This becomes a problem since we started to respect tailroom which is
    subtracted from chunk_size (among with headroom). 2k chunk size might
    not provide enough space for standard 1500 MTU, so let us catch such
    settings at bind time.
    
    This prevents creating an already-invalid setup and complements the
    MTU change restriction for devices with an attached XSK pool.
    
    Signed-off-by: Maciej Fijalkowski <maciej.fijalkowski@intel.com>

--- net/xdp/xsk_buff_pool.c
+++ net/xdp/xsk_buff_pool.c
@@ -157,6 +157,7 @@ static void xp_disable_drv_zc(struct xsk_buff_pool *pool)
 int xp_assign_dev(struct xsk_buff_pool *pool,
 		  struct net_device *netdev, u16 queue_id, u16 flags)
 {
+	bool mbuf = flags & XDP_USE_SG;
 	bool force_zc, force_copy;
 	struct netdev_bpf bpf;
 	int err = 0;
@@ -178,7 +179,7 @@ int xp_assign_dev(struct xsk_buff_pool *pool,
 	if (err)
 		return err;
 
-	if (flags & XDP_USE_SG)
+	if (mbuf)
 		pool->umem->flags |= XDP_UMEM_SG_FLAG;
 
 	if (flags & XDP_USE_NEED_WAKEUP)
@@ -200,9 +201,17 @@ int xp_assign_dev(struct xsk_buff_pool *pool,
 		goto err_unreg_pool;
 	}
 
-	if (netdev->xdp_zc_max_segs == 1 && (flags & XDP_USE_SG)) {
+	if (netdev->xdp_zc_max_segs == 1 && mbuf) {
 		err = -EOPNOTSUPP;
 		goto err_unreg_pool;
+	}
+
+	if (!mbuf) {
+		if (netdev->mtu + netdev->hard_header_len >
+				xsk_pool_get_rx_frame_size(pool)) {
+			err = -EOPNOTSUPP;
+			goto err_unreg_pool;
+		}
 	}
 
 	if (dev_get_min_mp_channel_count(netdev)) {
