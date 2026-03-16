0b126cb2be5a19d0388ec80a42ada31004f3c2ee
Anas Iqbal <mohd.abd.6602@gmail.com>
2026-03-16 15:06:36 +0000

    io_uring: cast id to u64 before shifting in io_allocate_rbuf_ring()
    Smatch warns:
    io_uring/zcrx.c:393 io_allocate_rbuf_ring() warn: should 'id << 16' be a 64 bit type?
    
    The expression 'id << IORING_OFF_PBUF_SHIFT' is evaluated using 32-bit
    arithmetic because id is a u32. This may overflow before being promoted
    to the 64-bit mmap_offset.
    
    Cast id to u64 before shifting to ensure the shift is performed in
    64-bit arithmetic.
    
    Signed-off-by: Anas Iqbal <mohd.abd.6602@gmail.com>

--- io_uring/zcrx.c
+++ io_uring/zcrx.c
@@ -390,7 +390,7 @@ static int io_allocate_rbuf_ring(struct io_ring_ctx *ctx,
 		return -EINVAL;
 
 	mmap_offset = IORING_MAP_OFF_ZCRX_REGION;
-	mmap_offset += id << IORING_OFF_PBUF_SHIFT;
+	mmap_offset += (u64)id << IORING_OFF_PBUF_SHIFT;
 
 	ret = io_create_region(ctx, &ifq->region, rd, mmap_offset);
 	if (ret < 0)
