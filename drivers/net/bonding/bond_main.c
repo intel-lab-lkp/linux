5ad93523d5a38111139a8adae01319bb851d6f5b
Hangbin Liu <liuhangbin@gmail.com>
2026-03-16 12:26:13 +0800

    net: no need to disable LRO specifically
    In commit "net: use ndo_update_offloads to set offload features for
    bonding/bridge/team" we called netdev_change_features() in
    __netdev_upper_dev_link(), which will disable LRO automatically on lower
    device if upper device doesn't have LRO enabled. So we don't need to
    dev_disable_lro() again after netdev_upper_dev_link().
    
    Signed-off-by: Hangbin Liu <liuhangbin@gmail.com>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -2221,9 +2221,6 @@ skip_mac_set:
 	}
 #endif
 
-	if (!(bond_dev->features & NETIF_F_LRO))
-		dev_disable_lro(slave_dev);
-
 	res = netdev_rx_handler_register(slave_dev, bond_handle_frame,
 					 new_slave);
 	if (res) {
