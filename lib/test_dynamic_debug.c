9740f4ef1443b572112946814b43c309a2f0f33b
Philipp Hahn <phahn-oss@avm.de>
2026-03-17 21:39:52 +0100

    dyndbg: Ignore additional arguments from pr_fmt
    pr_fmt can be used to add a common prefix to any output from a module:
    	#define pr_fmt(fmt) KBUILD_MODNAME ".%s " fmt, __func__
    
    But adding additional arguments breaks dynamic debug:
    > error: macro "DEFINE_DYNAMIC_DEBUG_METADATA_CLS" passed 4 arguments, but takes just 3
    > |         pr_debug_ratelimited("%s", "Hello world!");
    > |                                                  ^
    > note: macro "DEFINE_DYNAMIC_DEBUG_METADATA_CLS" defined here
    > | #define DEFINE_DYNAMIC_DEBUG_METADATA_CLS(name, cls, fmt)       \
    > |
    > error: ‘DEFINE_DYNAMIC_DEBUG_METADATA_CLS’ undeclared (first use in this function)
    > |         DEFINE_DYNAMIC_DEBUG_METADATA_CLS(name, _DPRINTK_CLASS_DFLT, fmt)
    > |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    > note: in expansion of macro ‘DEFINE_DYNAMIC_DEBUG_METADATA’
    > |         DEFINE_DYNAMIC_DEBUG_METADATA(descriptor, pr_fmt(fmt));         \
    > |         ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    > note: in expansion of macro ‘pr_debug_ratelimited’
    > |         pr_debug_ratelimited("%s", "Hello world!");
    > |         ^~~~~~~~~~~~~~~~~~~~
    
    Add an additional ', ...' to DEFINE_DYNAMIC_DEBUG_METADATA_CLS to slurp
    any additional argument, which `pr_fmt` might add.
    
    Signed-off-by: Philipp Hahn <phahn-oss@avm.de>
    Acked-by: Jim Cromie <jim.cromie@gmail.com>

--- lib/test_dynamic_debug.c
+++ lib/test_dynamic_debug.c
@@ -6,7 +6,7 @@
  *      Jim Cromie	<jim.cromie@gmail.com>
  */
 
-#define pr_fmt(fmt) "test_dd: " fmt
+#define pr_fmt(fmt) "test_dd: %s " fmt, __func__
 
 #include <linux/module.h>
 
@@ -155,7 +155,7 @@ static int __init test_dynamic_debug_init(void)
 
 static void __exit test_dynamic_debug_exit(void)
 {
-	pr_debug("exited\n");
+	pr_debug_ratelimited("exited\n");
 }
 
 module_init(test_dynamic_debug_init);
