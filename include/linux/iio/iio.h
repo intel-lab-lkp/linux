ba212f8abce1f006da63efd0fbe11868b135bda4
Francesco Lavra <flavra@baylibre.com>
2026-03-17 16:04:01 +0100

    iio: Replace 'sign' field with union in struct iio_scan_type
    This field is used to differentiate between signed and unsigned integers.
    A following commit will extend its use in order to add support for non-
    integer scan elements; therefore, replace it with a union that contains a
    more generic 'format' field. This union will be dropped when all drivers
    are changed to use the format field.
    Opportunistically replace character literals with symbolic constants that
    represent the set of allowed values for the format field.
    
    Signed-off-by: Francesco Lavra <flavra@baylibre.com>

--- include/linux/iio/iio.h
+++ include/linux/iio/iio.h
@@ -176,9 +176,19 @@ struct iio_event_spec {
 	unsigned long mask_shared_by_all;
 };
 
+/*
+ * Format values in scan type
+ * @IIO_SCAN_FORMAT_SIGNED_INT: Signed integer (two's complement).
+ * @IIO_SCAN_FORMAT_UNSIGNED_INT: Unsigned integer.
+ */
+#define IIO_SCAN_FORMAT_SIGNED_INT	's'
+#define IIO_SCAN_FORMAT_UNSIGNED_INT	'u'
+
 /**
  * struct iio_scan_type - specification for channel data format in buffer
- * @sign:		's' or 'u' to specify signed or unsigned
+ * @sign:		Deprecated, use @format instead.
+ * @format:		Data format, can have any of the IIO_SCAN_FORMAT_*
+ *			values.
  * @realbits:		Number of valid bits of data
  * @storagebits:	Realbits + padding
  * @shift:		Shift right by this before masking out realbits.
@@ -189,7 +199,10 @@ struct iio_event_spec {
  * @endianness:		little or big endian
  */
 struct iio_scan_type {
-	char	sign;
+	union {
+		char sign;
+		char format;
+	};
 	u8	realbits;
 	u8	storagebits;
 	u8	shift;
