317007931081aa3e0b0f8002ab207f0bf1354a90
Dibin Moolakadan Subrahmanian <dibin.moolakadan.subrahmanian@intel.com>
2026-03-17 12:07:13 +0530

    drm/i915/dmc: Enable PIPEDMC_ERROR interrupt
    Enable PIPEDMC_ERROR interrupt bit for display version 35+.
    
    Changes in v2:
    - Move PIPEDMC_ERROR interrupt enable to a separate patch (Ville Syrjala)
    Changes in v3:
    - Add Bspec reference (Suraj Kandpal)
    Changes in v4:
    - Add TODO to check the enablement of PIPEDMC_ERROR for PTL (Suraj Kandpal)
    
    Bspec: 70296
    Signed-off-by: Dibin Moolakadan Subrahmanian <dibin.moolakadan.subrahmanian@intel.com>

--- drivers/gpu/drm/i915/display/intel_dmc.c
+++ drivers/gpu/drm/i915/display/intel_dmc.c
@@ -510,7 +510,13 @@ static void pipedmc_clock_gating_wa(struct intel_display *display, bool enable)
 static u32 pipedmc_interrupt_mask(struct intel_display *display)
 {
 	if (DISPLAY_VER(display) >= 35)
-		return PIPEDMC_FLIPQ_PROG_DONE;
+		return PIPEDMC_FLIPQ_PROG_DONE |
+			PIPEDMC_ERROR;
+
+	/*
+	 * TODO: Check if PIPEDMC_ERROR bit enabling causes errors
+	 * on PTL, enable it if validation passes
+	 */
 
 	/*
 	 * FIXME PIPEDMC_ERROR not enabled for now due to LNL pipe B
