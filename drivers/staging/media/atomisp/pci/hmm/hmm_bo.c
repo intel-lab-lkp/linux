71dc50aad741f8fd6e258e31a318027ff9debf09
Zile Xiong <xiongzile99@gmail.com>
2026-03-19 18:35:45 +0800

    staging: media: atomisp: hmm: remove unnecessary casts
    Drop unnecessary casts when accessing vma->vm_private_data.
    
    No functional change.
    
    Signed-off-by: Zile Xiong <xiongzile99@gmail.com>

--- drivers/staging/media/atomisp/pci/hmm/hmm_bo.c
+++ drivers/staging/media/atomisp/pci/hmm/hmm_bo.c
@@ -975,8 +975,7 @@ void hmm_bo_unref(struct hmm_buffer_object *bo)
 
 static void hmm_bo_vm_open(struct vm_area_struct *vma)
 {
-	struct hmm_buffer_object *bo =
-	    (struct hmm_buffer_object *)vma->vm_private_data;
+	struct hmm_buffer_object *bo = vma->vm_private_data;
 
 	check_bo_null_return_void(bo);
 
@@ -993,8 +992,7 @@ static void hmm_bo_vm_open(struct vm_area_struct *vma)
 
 static void hmm_bo_vm_close(struct vm_area_struct *vma)
 {
-	struct hmm_buffer_object *bo =
-	    (struct hmm_buffer_object *)vma->vm_private_data;
+	struct hmm_buffer_object *bo = vma->vm_private_data;
 
 	check_bo_null_return_void(bo);
 
