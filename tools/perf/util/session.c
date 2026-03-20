4c588ac3e1dce3db820aa65e9579f9c2bce9fff7
Ian Rogers <irogers@google.com>
2026-03-20 12:26:04 -0700

    perf sample: Make sure perf_sample__init/exit are used
    The deferred stack trace code wasn't using perf_sample__init/exit. Add
    the deferred stack trace clean up to perf_sample__exit which requires
    proper NULL initialization in perf_sample__init. Make the
    perf_sample__exit robust to being called more than once by using
    zfree. Make the error paths in evsel__parse_sample exit the
    sample. Add a merged_callchain boolean to capture that callchain is
    allocated, deferred_callchain doen't suffice for this. Pack the struct
    variables to avoid padding bytes for this.
    
    Similiarly powerpc_vpadtl_sample wasn't using perf_sample__init/exit,
    use it for consistency and potential issues with uninitialized
    variables.
    
    Signed-off-by: Ian Rogers <irogers@google.com>

--- tools/perf/util/session.c
+++ tools/perf/util/session.c
@@ -1367,14 +1367,18 @@ static int evlist__deliver_deferred_callchain(struct evlist *evlist,
 	list_for_each_entry_safe(de, tmp, &evlist->deferred_samples, list) {
 		struct perf_sample orig_sample;
 
+		perf_sample__init(&orig_sample, /*all=*/false);
 		ret = evlist__parse_sample(evlist, de->event, &orig_sample);
 		if (ret < 0) {
 			pr_err("failed to parse original sample\n");
+			perf_sample__exit(&orig_sample);
 			break;
 		}
 
-		if (sample->tid != orig_sample.tid)
+		if (sample->tid != orig_sample.tid) {
+			perf_sample__exit(&orig_sample);
 			continue;
+		}
 
 		if (event->callchain_deferred.cookie == orig_sample.deferred_cookie)
 			sample__merge_deferred_callchain(&orig_sample, sample);
@@ -1385,9 +1389,7 @@ static int evlist__deliver_deferred_callchain(struct evlist *evlist,
 		ret = evlist__deliver_sample(evlist, tool, de->event,
 					     &orig_sample, evsel, machine);
 
-		if (orig_sample.deferred_callchain)
-			free(orig_sample.callchain);
-
+		perf_sample__exit(&orig_sample);
 		list_del(&de->list);
 		free(de->event);
 		free(de);
@@ -1414,9 +1416,11 @@ static int session__flush_deferred_samples(struct perf_session *session,
 	list_for_each_entry_safe(de, tmp, &evlist->deferred_samples, list) {
 		struct perf_sample sample;
 
+		perf_sample__init(&sample, /*all=*/false);
 		ret = evlist__parse_sample(evlist, de->event, &sample);
 		if (ret < 0) {
 			pr_err("failed to parse original sample\n");
+			perf_sample__exit(&sample);
 			break;
 		}
 
@@ -1424,6 +1428,7 @@ static int session__flush_deferred_samples(struct perf_session *session,
 		ret = evlist__deliver_sample(evlist, tool, de->event,
 					     &sample, evsel, machine);
 
+		perf_sample__exit(&sample);
 		list_del(&de->list);
 		free(de->event);
 		free(de);
