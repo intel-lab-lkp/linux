919c891e2c57ec0499b43e2527f41b0d8f37d9eb
Dave Hansen <dave.hansen@linux.intel.com>
2026-03-20 12:03:33 -0700

    x86/msr: Consolidate rdmsr() definitions
    The paravirt and native code define very similar rdmsr()
    implementations. Move the native one out to common code
    and ensure it can find the paravirt implementation by
    defining raw_read_msr() there.
    
    Remove the now duplicate paravirt rdmsr().
    
    Signed-off-by: Dave Hansen <dave.hansen@linux.intel.com>

--- arch/x86/include/asm/msr.h
+++ arch/x86/include/asm/msr.h
@@ -171,6 +171,9 @@ static inline u64 native_read_pmc(int counter)
 
 #ifdef CONFIG_PARAVIRT_XXL
 #include <asm/paravirt.h>
+
+#define raw_read_msr		paravirt_read_msr
+
 #else
 #include <linux/errno.h>
 
@@ -184,13 +187,6 @@ static inline u64 native_read_pmc(int counter)
  * Note: the rd* operations modify the parameters directly (without using
  * pointer indirection), this allows gcc to optimize better
  */
-
-#define rdmsr(msr, low, high)					\
-do {								\
-	u64 __val = raw_read_msr((msr));			\
-	(void)((low) = (u32)__val);				\
-	(void)((high) = (u32)(__val >> 32));			\
-} while (0)
 
 static inline void wrmsr(u32 msr, u32 low, u32 high)
 {
@@ -232,6 +228,16 @@ static __always_inline u64 rdpmc(int counter)
 }
 
 #endif	/* !CONFIG_PARAVIRT_XXL */
+
+/*
+ * Common paravirt and native helpers:
+ */
+#define rdmsr(msr, low, high)					\
+do {								\
+	u64 __val = raw_read_msr((msr));			\
+	(void)((low) = (u32)__val);				\
+	(void)((high) = (u32)(__val >> 32));			\
+} while (0)
 
 /* Instruction opcode for WRMSRNS supported in binutils >= 2.40 */
 #define ASM_WRMSRNS _ASM_BYTES(0x0f,0x01,0xc6)
