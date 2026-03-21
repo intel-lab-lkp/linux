6df027b32f11a6a67c759039be1ea8e5af6e3a17
Dan Carpenter <dan.carpenter@linaro.org>
2026-03-21 13:26:25 +0300

    nvme: remove bogus check in nvme_pr_read_keys()
    This check for if (rse_len > U32_MAX) is confusing because if
    rse_len is > INT_MAX, that will trigger a WARN() in kvzalloc().
    Fortunately, the caller blkdev_pr_read_keys(), puts a limit on num_keys.
    The number of keys can't be more than PR_KEYS_MAX (65536) and the
    condition is impossible.
    
    Delete the confusing, dead code.
    
    Signed-off-by: Dan Carpenter <dan.carpenter@linaro.org>

--- drivers/nvme/host/pr.c
+++ drivers/nvme/host/pr.c
@@ -239,9 +239,6 @@ static int nvme_pr_read_keys(struct block_device *bdev,
 	 * enough to get enough keys to fill the return keys buffer.
 	 */
 	rse_len = struct_size(rse, regctl_eds, num_keys);
-	if (rse_len > U32_MAX)
-		return -EINVAL;
-
 	rse = kvzalloc(rse_len, GFP_KERNEL);
 	if (!rse)
 		return -ENOMEM;
