0b1605e975cc86bac3fbcb33ebccf4adaefc6c16
Prathima <Prathima.Lk@amd.com>
2026-03-23 16:38:06 +0530

    hwmon/misc: amd-sbi: Move core SBTSI support from hwmon to misc
    Move SBTSI core functionality out of the hwmon-only path and into
    drivers/misc/amd-sbi so it can be reused by non-hwmon consumers.
    
    This split prepares the driver for additional interfaces while keeping
    hwmon support as an optional layer on top of common SBTSI core logic.
    
    Reviewed-by: Akshay Gupta <Akshay.Gupta@amd.com>
    Signed-off-by: Prathima <Prathima.Lk@amd.com>

--- drivers/misc/amd-sbi/sbtsi.c
+++ drivers/misc/amd-sbi/sbtsi.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * sbtsi_temp.c - hwmon driver for a SBI Temperature Sensor Interface (SB-TSI)
+ * sbtsi.c - hwmon driver for a SBI Temperature Sensor Interface (SB-TSI)
  *                compliant AMD SoC temperature device.
  *
  * Copyright (c) 2020, Google Inc.
