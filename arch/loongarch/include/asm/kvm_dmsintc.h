46a3a1b253caf63252b3042bf40ca94975082a6b
Song Gao <gaosong@loongson.cn>
2026-03-24 17:15:21 +0800

    LoongArch: KVM: Add dmsintc inject msi to the dest vcpu
    Implement irqfd deliver msi to vcpu and vcpu dmsintc inject irq.
    Add irqfd choice dmsintc to set msi irq by the msg_addr and
    implement dmsintc set msi irq.
    
    Signed-off-by: Song Gao <gaosong@loongson.cn>

--- arch/loongarch/include/asm/kvm_dmsintc.h
+++ arch/loongarch/include/asm/kvm_dmsintc.h
@@ -11,11 +11,16 @@ struct loongarch_dmsintc {
 	struct kvm *kvm;
 	uint64_t msg_addr_base;
 	uint64_t msg_addr_size;
+	uint32_t cpu_mask;
 };
 
 struct dmsintc_state {
 	atomic64_t  vector_map[4];
 };
 
+void dmsintc_inject_irq(struct kvm_vcpu *vcpu);
+int dmsintc_deliver_msi_to_vcpu(struct kvm *kvm, struct kvm_vcpu *vcpu,
+				u32 vector, int level);
+int kvm_dmsintc_set_msi_irq(struct kvm *kvm, u32 addr, int data, int level);
 int kvm_loongarch_register_dmsintc_device(void);
 #endif
--- arch/loongarch/kvm/intc/dmsintc.c
+++ arch/loongarch/kvm/intc/dmsintc.c
@@ -4,8 +4,81 @@
  */
 
 #include <linux/kvm_host.h>
+#include <asm/kvm_csr.h>
 #include <asm/kvm_dmsintc.h>
 #include <asm/kvm_vcpu.h>
+
+int dmsintc_deliver_msi_to_vcpu(struct kvm *kvm,
+				struct kvm_vcpu *vcpu,
+				u32 vector, int level)
+{
+	struct kvm_interrupt vcpu_irq;
+	struct dmsintc_state *ds;
+
+	if (!level)
+		return 0;
+	if (!vcpu || vector >= 256)
+		return -EINVAL;
+	ds = &vcpu->arch.dmsintc_state;
+	if (!ds)
+		return -ENODEV;
+	set_bit(vector, (unsigned long *)&ds->vector_map);
+	vcpu_irq.irq = INT_AVEC;
+	kvm_vcpu_ioctl_interrupt(vcpu, &vcpu_irq);
+	kvm_vcpu_kick(vcpu);
+	return 0;
+}
+
+int kvm_dmsintc_set_msi_irq(struct kvm *kvm, u32 addr, int data, int level)
+{
+	unsigned int virq, dest;
+	struct kvm_vcpu *vcpu;
+
+	virq = (addr >> AVEC_IRQ_SHIFT) & AVEC_IRQ_MASK;
+	dest = (addr >> AVEC_CPU_SHIFT) & kvm->arch.dmsintc->cpu_mask;
+	if (dest > KVM_MAX_VCPUS)
+		return -EINVAL;
+	vcpu = kvm_get_vcpu_by_cpuid(kvm, dest);
+	if (!vcpu)
+		return -EINVAL;
+	return dmsintc_deliver_msi_to_vcpu(kvm, vcpu, virq, level);
+}
+
+void dmsintc_inject_irq(struct kvm_vcpu *vcpu)
+{
+	struct dmsintc_state *ds = &vcpu->arch.dmsintc_state;
+	unsigned int i;
+	unsigned long temp[4], old;
+
+	if (!ds)
+		return;
+
+	for (i = 0; i < 4; i++) {
+		old = atomic64_read(&(ds->vector_map[i]));
+		if (old)
+			temp[i] = atomic64_xchg(&(ds->vector_map[i]), 0);
+	}
+
+	if (temp[0]) {
+		old = kvm_read_hw_gcsr(LOONGARCH_CSR_ISR0);
+		kvm_write_hw_gcsr(LOONGARCH_CSR_ISR0, temp[0]|old);
+	}
+
+	if (temp[1]) {
+		old = kvm_read_hw_gcsr(LOONGARCH_CSR_ISR1);
+		kvm_write_hw_gcsr(LOONGARCH_CSR_ISR1, temp[1]|old);
+	}
+
+	if (temp[2]) {
+		old = kvm_read_hw_gcsr(LOONGARCH_CSR_ISR2);
+		kvm_write_hw_gcsr(LOONGARCH_CSR_ISR2, temp[2]|old);
+	}
+
+	if (temp[3]) {
+		old = kvm_read_hw_gcsr(LOONGARCH_CSR_ISR3);
+		kvm_write_hw_gcsr(LOONGARCH_CSR_ISR3, temp[3]|old);
+	}
+}
 
 static int kvm_dmsintc_ctrl_access(struct kvm_device *dev,
 				struct kvm_device_attr *attr,
