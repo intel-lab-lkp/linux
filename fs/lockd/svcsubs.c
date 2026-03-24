f4cd272f0b14b784cebd9632758e91c3c5f435c8
NeilBrown <neil@brown.name>
2026-03-24 21:13:35 +1100

    lockd: fix TEST handling when not all permissions are available.
    The F_GETLK fcntl can work with either read access or write access or
    both.  It can query F_RDLCK and F_WRLCK locks in either case.
    
    However lockd currently treats F_GETLK similar to F_SETLK in that read
    access is required to query an F_RDLCK lock and write access is required
    to query a F_WRLCK lock.
    
    This is wrong and can cause problem - e.g.  when qemu accesses a
    read-only (e.g. iso) filesystem image over NFS (though why it queries
    if it can get a write lock - I don't know.  But it does, and this works
    with local filesystems).
    
    So we need TEST requests to be handled differently.  To do this:
    
    - change nlm_do_fopen() to accept O_RDWR as a mode and in that case
      succeed if either a O_RDONLY or O_WRONLY file can be opened.
    - change nlm_lookup_file() to accept a mode argument from caller,
      instead of deducing base on lock time, and pass that on to nlm_do_fopen()
    - change nlm4svc_retrieve_args() and nlmsvc_retrieve_args() to detect
      TEST requests and pass O_RDWR as a mode to nlm_lookup_file, passing
      the same mode as before for other requests.  Also set
       lock->fl.c.flc_file to whichever file is available for TEST requests.
    - change nlmsvc_testlock() to also not calculate the mode, but to use
      whenever was stored in lock->fl.c.flc_file.
    
    Reported-by: Tj <tj.iam.tj@proton.me>
    Link:  https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1128861
    Fixes: 7f024fcd5c97 ("Keep read and write fds with each nlm_file")
    Signed-off-by: NeilBrown <neil@brown.name>

--- fs/lockd/svcsubs.c
+++ fs/lockd/svcsubs.c
@@ -82,18 +82,28 @@ int lock_to_openmode(struct file_lock *lock)
  *
  * We have to make sure we have the right credential to open
  * the file.
+ *
+ * mode can be O_RDONLY(0), O_WRONLY(1) or O_RDWR(2) meaning either
  */
 static __be32 nlm_do_fopen(struct svc_rqst *rqstp,
 			   struct nlm_file *file, int mode)
 {
-	struct file **fp = &file->f_file[mode];
+	struct file **fp;
 	__be32	nfserr;
+	int m;
 
-	if (*fp)
-		return 0;
-	nfserr = nlmsvc_ops->fopen(rqstp, &file->f_handle, fp, mode);
-	if (nfserr)
-		dprintk("lockd: open failed (error %d)\n", nfserr);
+	for (m = O_RDONLY ; m <= O_WRONLY ; m++) {
+		if (mode != O_RDWR && mode != m)
+			continue;
+
+		fp = &file->f_file[m];
+		if (*fp)
+			return 0;
+		nfserr = nlmsvc_ops->fopen(rqstp, &file->f_handle, fp, m);
+		if (!nfserr)
+			return 0;
+	}
+	dprintk("lockd: open failed (error %d)\n", nfserr);
 	return nfserr;
 }
 
@@ -103,17 +113,15 @@ static __be32 nlm_do_fopen(struct svc_rqst *rqstp,
  */
 __be32
 nlm_lookup_file(struct svc_rqst *rqstp, struct nlm_file **result,
-					struct nlm_lock *lock)
+		struct nlm_lock *lock, int mode)
 {
 	struct nlm_file	*file;
 	unsigned int	hash;
 	__be32		nfserr;
-	int		mode;
 
 	nlm_debug_print_fh("nlm_lookup_file", &lock->fh);
 
 	hash = file_hash(&lock->fh);
-	mode = lock_to_openmode(&lock->fl);
 
 	/* Lock file table */
 	mutex_lock(&nlm_file_mutex);
