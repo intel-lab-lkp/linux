e3a2bcd72b9b0945a692041630617b8ba1090ffe
Claudio Imbrenda <imbrenda@linux.ibm.com>
2026-03-25 18:13:10 +0100

    KVM: s390: vsie: Fix guest page tables protection
    When shadowing, the guest page tables are write-protected, in order to
    trap changes and properly unshadow the shadow mapping for the nested
    guest. Already shadowed levels are skipped, so that only the needed
    levels are write protected.
    
    Currently the levels that get write protected are exactly one level too
    deep: the last level (nested guest memory) gets protected in the wrong
    way, and will be protected again correctly a few lines afterwards; most
    importantly, the highest non-shadowed level does *not* get write
    protected.
    
    Moreover, if the nested guest is running in a real address space, there
    are no DAT tables to shadow.
    
    Write protect the correct levels, so that all the levels that need to
    be protected are protected, and avoid double protecting the last level;
    skip attempting to shadow the DAT tables when the nested guest is
    running in a real address space.
    
    Signed-off-by: Claudio Imbrenda <imbrenda@linux.ibm.com>
    Fixes: e38c884df921 ("KVM: s390: Switch to new gmap")
    Tested-by: Christian Borntraeger <borntraeger@linux.ibm.com>
    Reviewed-by: Janosch Frank <frankja@linux.ibm.com>

--- arch/s390/kvm/gaccess.c
+++ arch/s390/kvm/gaccess.c
@@ -1521,13 +1521,20 @@ static int _gaccess_do_shadow(struct kvm_s390_mmu_cache *mc, struct gmap *sg,
 
 	gl = get_level(table, ptep);
 
+	/* In case of a real address space */
+	if (w->level <= LEVEL_MEM) {
+		l = TABLE_TYPE_PAGE_TABLE;
+		hl = TABLE_TYPE_REGION1;
+		goto real_address_space;
+	}
+
 	/*
 	 * Skip levels that are already protected. For each level, protect
 	 * only the page containing the entry, not the whole table.
 	 */
 	for (i = gl ; i >= w->level; i--) {
-		rc = gmap_protect_rmap(mc, sg, entries[i - 1].gfn, gpa_to_gfn(saddr),
-				       entries[i - 1].pfn, i, entries[i - 1].writable);
+		rc = gmap_protect_rmap(mc, sg, entries[i].gfn, gpa_to_gfn(saddr),
+				       entries[i].pfn, i + 1, entries[i].writable);
 		if (rc)
 			return rc;
 		if (!sg->parent)
@@ -1543,6 +1550,7 @@ static int _gaccess_do_shadow(struct kvm_s390_mmu_cache *mc, struct gmap *sg,
 	/* Get the smallest granularity */
 	l = min3(gl, hl, w->level);
 
+real_address_space:
 	flags = DAT_WALK_SPLIT_ALLOC | (uses_skeys(sg->parent) ? DAT_WALK_USES_SKEYS : 0);
 	/* If necessary, create the shadow mapping */
 	if (l < gl) {
