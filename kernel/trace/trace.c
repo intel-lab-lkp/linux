3b5cf3477871a418bb904a8eaf905a8a275f86bd
Masami Hiramatsu (Google) <mhiramat@kernel.org>
2026-03-25 11:25:25 +0900

    ring-buffer: Add persistent ring buffer selftest
    Add a self-destractive test for the persistent ring buffer. This
    will invalidate some sub-buffer pages in the persistent ring buffer
    when kernel gets panic, and check whether the number of detected
    invalid pages and the total entry_bytes are the same as record
    after reboot.
    
    This can ensure the kernel correctly recover partially corrupted
    persistent ring buffer when boot.
    
    The test only runs on the persistent ring buffer whose name is
    "ptracingtest". And user has to fill it up with events before
    kernel panics.
    
    To run the test, enable CONFIG_RING_BUFFER_PERSISTENT_SELFTEST
    and you have to setup the kernel cmdline;
    
     reserve_mem=20M:2M:trace trace_instance=ptracingtest^traceoff@trace
     panic=1
    
    And run following commands after the 1st boot;
    
     cd /sys/kernel/tracing/instances/ptracingtest
     echo 1 > tracing_on
     echo 1 > events/enable
     sleep 3
     echo c > /proc/sysrq-trigger
    
    After panic message, the kernel will reboot and run the verification
    on the persistent ring buffer, e.g.
    
     Ring buffer meta [2] invalid buffer page detected
     Ring buffer meta [2] is from previous boot! (318 pages discarded)
     Ring buffer testing [2] invalid pages: PASSED (318/318)
     Ring buffer testing [2] entry_bytes: PASSED (1300476/1300476)
    
    Signed-off-by: Masami Hiramatsu (Google) <mhiramat@kernel.org>

--- kernel/trace/trace.c
+++ kernel/trace/trace.c
@@ -9366,6 +9366,8 @@ static void setup_trace_scratch(struct trace_array *tr,
 	memset(tscratch, 0, size);
 }
 
+#define TRACE_TEST_PTRACING_NAME	"ptracingtest"
+
 static int
 allocate_trace_buffer(struct trace_array *tr, struct array_buffer *buf, unsigned long size)
 {
@@ -9378,6 +9380,8 @@ allocate_trace_buffer(struct trace_array *tr, struct array_buffer *buf, unsigned
 	buf->tr = tr;
 
 	if (tr->range_addr_start && tr->range_addr_size) {
+		if (!strcmp(tr->name, TRACE_TEST_PTRACING_NAME))
+			rb_flags |= RB_FL_TESTING;
 		/* Add scratch buffer to handle 128 modules */
 		buf->buffer = ring_buffer_alloc_range(size, rb_flags, 0,
 						      tr->range_addr_start,
