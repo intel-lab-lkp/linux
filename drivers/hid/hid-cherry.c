c63dc14a8e9ba983a8a49dee28f2b1a341055768
Aravind Anilraj <aravindanilraj0702@gmail.com>
2026-03-25 12:13:28 -0400

    HID: cherry: Fix switch case formatting
    Fix checkpatch warnings by splitting single-line case
    statements into multiple lines.
    
    No functional changes.
    
    Signed-off-by: Aravind Anilraj <aravindanilraj0702@gmail.com>

--- drivers/hid/hid-cherry.c
+++ drivers/hid/hid-cherry.c
@@ -43,9 +43,15 @@ static int ch_input_mapping(struct hid_device *hdev, struct hid_input *hi,
 		return 0;
 
 	switch (usage->hid & HID_USAGE) {
-	case 0x301: ch_map_key_clear(KEY_PROG1);	break;
-	case 0x302: ch_map_key_clear(KEY_PROG2);	break;
-	case 0x303: ch_map_key_clear(KEY_PROG3);	break;
+	case 0x301:
+		ch_map_key_clear(KEY_PROG1);
+		break;
+	case 0x302:
+		ch_map_key_clear(KEY_PROG2);
+		break;
+	case 0x303:
+		ch_map_key_clear(KEY_PROG3);
+		break;
 	default:
 		return 0;
 	}
