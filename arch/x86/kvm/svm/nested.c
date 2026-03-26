024c8a0fa2541855d52b8003c99b7368f3f8ef08
Paolo Bonzini <pbonzini@redhat.com>
2026-03-26 19:17:19 +0100

    KVM: x86/mmu: add support for GMET to NPT page table walks
    GMET allows page table entries to be created with U=0 in NPT.
    However, when GMET=1 U=0 only affects execution, not reads or
    writes.  Ignore user faults on non-fetch accesses for NPT GMET.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/kvm/svm/nested.c
+++ arch/x86/kvm/svm/nested.c
@@ -95,7 +95,8 @@ static void nested_svm_init_mmu_context(struct kvm_vcpu *vcpu)
 	 */
 	kvm_init_shadow_npt_mmu(vcpu, X86_CR0_PG, svm->vmcb01.ptr->save.cr4,
 				svm->vmcb01.ptr->save.efer,
-				svm->nested.ctl.nested_cr3);
+				svm->nested.ctl.nested_cr3,
+				svm->nested.ctl.nested_ctl);
 	vcpu->arch.mmu->get_guest_pgd     = nested_svm_get_tdp_cr3;
 	vcpu->arch.mmu->get_pdptr         = nested_svm_get_tdp_pdptr;
 	vcpu->arch.mmu->inject_page_fault = nested_svm_inject_npf_exit;
