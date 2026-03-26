14aa72982b41e2ef7810f20c748b53a1cc6e50af
Ville Syrjälä <ville.syrjala@linux.intel.com>
2026-03-27 00:31:31 +0200

    drm/i915/casf: s/casf_enable/enable/
    The 'casf_enable' boolean is already inside a casf specific
    structure, so drop the extra 'casf_' namespace from the bool.
    
    Cc: Nemesa Garg <nemesa.garg@intel.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>

--- drivers/gpu/drm/i915/display/intel_crtc_state_dump.c
+++ drivers/gpu/drm/i915/display/intel_crtc_state_dump.c
@@ -383,7 +383,7 @@ void intel_crtc_state_dump(const struct intel_crtc_state *pipe_config,
 	drm_printf(&p, "sharpness strength: %d, sharpness tap size: %d, sharpness enable: %d\n",
 		   pipe_config->hw.casf_params.strength,
 		   pipe_config->hw.casf_params.win_size,
-		   pipe_config->hw.casf_params.casf_enable);
+		   pipe_config->hw.casf_params.enable);
 
 dump_planes:
 	if (!state)
