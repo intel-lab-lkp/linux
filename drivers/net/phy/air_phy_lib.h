9906af56ac60edfb52bb1638ba50c53320f31b4a
Louis-Alexis Eyraud <louisalexis.eyraud@collabora.com>
2026-03-26 13:04:13 +0100

    net: phy: Add Airoha phy library for shared code
    In preparation of Airoha AN8801R PHY support, split out the interface
    functions that will be common between the already present air_en8811h
    driver and the new one, and put them into a new library named
    air_phy_lib.
    
    Signed-off-by: Louis-Alexis Eyraud <louisalexis.eyraud@collabora.com>

---
+++ drivers/net/phy/air_phy_lib.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright (C) 2026 Airoha Technology Corp.
+ * Copyright (C) 2026 Collabora Ltd.
+ *                    Louis-Alexis Eyraud <louisalexis.eyraud@collabora.com>
+ */
+
+#ifndef __AIR_PHY_LIB_H
+#define __AIR_PHY_LIB_H
+
+int air_phy_read_page(struct phy_device *phydev);
+int air_phy_write_page(struct phy_device *phydev, int page);
+
+#endif /* __AIR_PHY_LIB_H */
