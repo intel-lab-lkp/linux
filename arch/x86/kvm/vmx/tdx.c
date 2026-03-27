2992090ff7ceda7fb0212297ad6e4a15d2f00e3a
Sean Christopherson <seanjc@google.com>
2026-03-27 13:14:20 -0700

    KVM: x86: Move error handling inside free_external_spt()
    Move the logic for TDX’s specific need to leak pages when reclaim
    fails inside the free_external_spt() op, so this can be done in TDX
    specific code and not the generic MMU.
    
    Do this by passing the SP in instead of the external page table
    pointer. This way TDX code can set sp->external_spt to NULL. Since the
    error is now handled internally, change the op to return void. This way
    it also operated like a normal free in that success is guaranteed from
    the callers perspective.
    
    Opportunistically, drop the unused level arg while adjusting the sp arg.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>
    [re-wrote log and massaged op name]
    Signed-off-by: Rick Edgecombe <rick.p.edgecombe@intel.com>

--- arch/x86/kvm/vmx/tdx.c
+++ arch/x86/kvm/vmx/tdx.c
@@ -1765,27 +1765,24 @@ static void tdx_track(struct kvm *kvm)
 	kvm_make_all_cpus_request(kvm, KVM_REQ_OUTSIDE_GUEST_MODE);
 }
 
-static int tdx_sept_free_private_spt(struct kvm *kvm, gfn_t gfn,
-				     enum pg_level level, void *private_spt)
+static void tdx_sept_free_private_spt(struct kvm *kvm, gfn_t gfn,
+					struct kvm_mmu_page *sp)
 {
-	struct kvm_tdx *kvm_tdx = to_kvm_tdx(kvm);
-
 	/*
-	 * free_external_spt() is only called after hkid is freed when TD is
-	 * tearing down.
 	 * KVM doesn't (yet) zap page table pages in mirror page table while
 	 * TD is active, though guest pages mapped in mirror page table could be
 	 * zapped during TD is active, e.g. for shared <-> private conversion
 	 * and slot move/deletion.
+	 *
+	 * In other words, KVM should only free mirror page tables after the
+	 * TD's hkid is freed, when the TD is being torn down.
+	 *
+	 * If the S-EPT PTE can't be removed for any reason, intentionally leak
+	 * the page to prevent the kernel from accessing the encrypted page.
 	 */
-	if (KVM_BUG_ON(is_hkid_assigned(kvm_tdx), kvm))
-		return -EIO;
-
-	/*
-	 * The HKID assigned to this TD was already freed and cache was
-	 * already flushed. We don't have to flush again.
-	 */
-	return tdx_reclaim_page(virt_to_page(private_spt));
+	if (KVM_BUG_ON(is_hkid_assigned(to_kvm_tdx(kvm)), kvm) ||
+	    tdx_reclaim_page(virt_to_page(sp->external_spt)))
+		sp->external_spt = NULL;
 }
 
 static int tdx_sept_remove_private_spte(struct kvm *kvm, gfn_t gfn,
