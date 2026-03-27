29d0231ae555ff953b7565a72cddc4425279cf8b
Ahmed S. Darwish <darwi@linutronix.de>
2026-03-27 03:15:24 +0100

    x86/cpu: Rescan CPUID table after disabling PSN
    On Pentium-III and Transmeta CPUs, disabling the CPUID(0x3) Processor
    Serial Number (PSN) can affect the maximum valid CPUID standard leaf.
    
    Rescan the CPU's CPUID table in that case not to have stale cached data.
    Rescan only the changed CPUID leaves, not to override any of the kernel's
    previously force-set or unset cached CPUID bits.
    
    Rename squash_the_stupid_serial_number() to disable_cpu_serial_number() and
    explain the rationale behind disabling the CPU's PSN.
    
    Signed-off-by: Ahmed S. Darwish <darwi@linutronix.de>

--- arch/x86/kernel/cpu/common.c
+++ arch/x86/kernel/cpu/common.c
@@ -336,14 +336,18 @@ bool cpuid_feature(void)
 	return flag_is_changeable_p(X86_EFLAGS_ID);
 }
 
-static void squash_the_stupid_serial_number(struct cpuinfo_x86 *c)
+/*
+ * For privacy concerns, disable legacy Intel and Transmeta CPUID(0x3)
+ * feature, Processor Serial Number, by default.
+ */
+static void disable_cpu_serial_number(struct cpuinfo_x86 *c)
 {
+	const struct leaf_0x0_0 *l0;
+	unsigned int rescan_from;
 	unsigned long lo, hi;
 
 	if (!cpu_has(c, X86_FEATURE_PN) || !disable_x86_serial_nr)
 		return;
-
-	/* Disable processor serial number: */
 
 	rdmsr(MSR_IA32_BBL_CR_CTL, lo, hi);
 	lo |= 0x200000;
@@ -352,8 +356,18 @@ static void squash_the_stupid_serial_number(struct cpuinfo_x86 *c)
 	pr_notice("CPU serial number disabled.\n");
 	clear_cpu_cap(c, X86_FEATURE_PN);
 
-	/* Disabling the serial number may affect the cpuid level */
-	c->cpuid_level = cpuid_eax(0);
+	/*
+	 * Disabling CPUID(0x3) can change the maximum standard CPUID level
+	 */
+
+	cpuid_refresh_leaf(c, 0x0);
+	l0 = cpuid_leaf(c, 0x0);
+	if (!l0)
+		return;
+
+	rescan_from = min_t(int, l0->max_std_leaf, c->cpuid_level) + 1;
+	cpuid_refresh_range(c, rescan_from, CPUID_BASE_END);
+	c->cpuid_level = l0->max_std_leaf;
 }
 
 static int __init x86_serial_nr_setup(char *s)
@@ -363,7 +377,7 @@ static int __init x86_serial_nr_setup(char *s)
 }
 __setup("serialnumber", x86_serial_nr_setup);
 #else
-static inline void squash_the_stupid_serial_number(struct cpuinfo_x86 *c)
+static inline void disable_cpu_serial_number(struct cpuinfo_x86 *c)
 {
 }
 #endif
@@ -2046,7 +2060,7 @@ static void identify_cpu(struct cpuinfo_x86 *c)
 	bus_lock_init();
 
 	/* Disable the PN if appropriate */
-	squash_the_stupid_serial_number(c);
+	disable_cpu_serial_number(c);
 
 	setup_smep(c);
 	setup_smap(c);
