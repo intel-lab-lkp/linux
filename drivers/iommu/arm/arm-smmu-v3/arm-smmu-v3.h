e49724ad0da774a693140c9f29564ffa3f72a2b8
Qinxin Xia <xiaqinxin@huawei.com>
2026-03-28 18:17:02 +0800

    iommu/arm-smmu-v3: Add basic debugfs framework
    Add basic debugfs framework for ARM SMMUv3 driver.This creates the
    root directory structure and provides capability display functionality.
    
    The debugfs hierarchy is organized as:
    /sys/kernel/debug/iommu/arm_smmu_v3/
    └── smmu<ioaddr>/
        └── capabilities
    
    Signed-off-by: Qinxin Xia <xiaqinxin@huawei.com>

--- drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
+++ drivers/iommu/arm/arm-smmu-v3/arm-smmu-v3.h
@@ -733,6 +733,16 @@ struct arm_smmu_impl_ops {
 			  const struct iommu_user_data *user_data);
 };
 
+#ifdef CONFIG_ARM_SMMU_V3_DEBUGFS
+struct arm_smmu_debugfs {
+	struct dentry			*smmu_dir;
+	/* Reserved for future extensions */
+};
+
+int arm_smmu_debugfs_setup(struct arm_smmu_device *smmu, const char *name);
+void arm_smmu_debugfs_remove(struct arm_smmu_device *smmu);
+#endif
+
 /* An SMMUv3 instance */
 struct arm_smmu_device {
 	struct device			*dev;
@@ -803,6 +813,11 @@ struct arm_smmu_device {
 
 	struct rb_root			streams;
 	struct mutex			streams_mutex;
+
+#ifdef CONFIG_ARM_SMMU_V3_DEBUGFS
+	/* DebugFS Info */
+	struct arm_smmu_debugfs		*debugfs;
+#endif
 };
 
 struct arm_smmu_stream {
