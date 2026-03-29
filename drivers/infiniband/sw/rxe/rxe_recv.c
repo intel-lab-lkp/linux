290fd89260b85cc7f795c104d96081e69720a372
zhenwei pi <zhenwei.pi@linux.dev>
2026-03-29 13:41:55 +0800

    RDMA/rxe: add SENT/RCVD bytes
    There is a lack of sent/received counter in bytes.
    
    Signed-off-by: zhenwei pi <zhenwei.pi@linux.dev>

--- drivers/infiniband/sw/rxe/rxe_recv.c
+++ drivers/infiniband/sw/rxe/rxe_recv.c
@@ -342,6 +342,7 @@ void rxe_rcv(struct sk_buff *skb)
 		goto drop;
 
 	rxe_counter_inc(rxe, RXE_CNT_RCVD_PKTS);
+	rxe_counter_add(rxe, RXE_CNT_RCVD_BYTES, skb->len);
 
 	if (unlikely(bth_qpn(pkt) == IB_MULTICAST_QPN))
 		rxe_rcv_mcast_pkt(rxe, skb);
