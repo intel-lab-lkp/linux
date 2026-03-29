28ce0cf13e99486beeb3412b78e88e5ea4c215e3
Otto Pflüger <otto.pflueger@abscue.de>
2026-03-29 09:27:49 +0200

    regulator: sc2731: Add platform_device_id table
    Make the regulator driver for the SC2731 PMIC probe automatically. Using
    a platform_device_id table instead of DT compatible matching avoids the
    need for a separate compatible property in the "regulators" node, which
    simplifies the DT bindings and makes the parent MFD device responsible
    for selecting the correct regulator driver for the PMIC.
    
    However, this means that the regulator device is not automatically
    associated with the "regulators" node. Tell the regulator core to
    perform device tree lookups using the parent MFD device instead of
    the regulator sub-device and set the .regulators_node member in all
    regulator definitions so that the "regulators" sub-node is used.
    
    Acked-by: Mark Brown <broonie@kernel.org>
    Signed-off-by: Otto Pflüger <otto.pflueger@abscue.de>

--- drivers/regulator/sc2731-regulator.c
+++ drivers/regulator/sc2731-regulator.c
@@ -131,6 +131,7 @@ static const struct regulator_ops sc2731_regu_linear_ops = {
 			  vstep, vmin, vmax) {			\
 	.name			= #_id,				\
 	.of_match		= of_match_ptr(#_id),		\
+	.regulators_node	= of_match_ptr("regulators"),	\
 	.ops			= &sc2731_regu_linear_ops,	\
 	.type			= REGULATOR_VOLTAGE,		\
 	.id			= SC2731_##_id,			\
@@ -226,7 +227,7 @@ static int sc2731_regulator_probe(struct platform_device *pdev)
 		return ret;
 	}
 
-	config.dev = &pdev->dev;
+	config.dev = pdev->dev.parent;
 	config.regmap = regmap;
 
 	for (i = 0; i < ARRAY_SIZE(regulators); i++) {
@@ -242,12 +243,19 @@ static int sc2731_regulator_probe(struct platform_device *pdev)
 	return 0;
 }
 
+static const struct platform_device_id sc2731_regulator_id_table[] = {
+	{ "sc2731-regulator" },
+	{ }
+};
+MODULE_DEVICE_TABLE(platform, sc2731_regulator_id_table);
+
 static struct platform_driver sc2731_regulator_driver = {
 	.driver = {
 		.name = "sc27xx-regulator",
 		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 	},
 	.probe = sc2731_regulator_probe,
+	.id_table = sc2731_regulator_id_table,
 };
 
 module_platform_driver(sc2731_regulator_driver);
