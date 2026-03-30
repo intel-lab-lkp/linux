2fe98bdccca018c1fd530f3be1efd55d3ea49c8d
Thorsten Blum <thorsten.blum@linux.dev>
2026-03-30 16:40:36 +0200

    platform/x86: dell-wmi-sysman: Clean up security buffer helpers
    In calculate_security_buffer(), call strlen() once and use ALIGN() to
    round up to an even size.
    
    In populate_security_buffer(), also avoid recomputing strlen(), rename
    the u32 pointer from 'seclen' to 'seclenp' to avoid confusion with the
    new length variable, and drop the memcpy() guard since calling it with
    size 0 is a no-op and therefore safe.
    
    Use 'const char *' for the read-only source string in both helpers.
    
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>

--- drivers/platform/x86/dell/dell-wmi-sysman/sysman.c
+++ drivers/platform/x86/dell/dell-wmi-sysman/sysman.c
@@ -72,13 +72,9 @@ size_t calculate_string_buffer(const char *str)
  *
  * Currently only supported type is Admin password
  */
-size_t calculate_security_buffer(char *authentication)
+size_t calculate_security_buffer(const char *authentication)
 {
-	if (strlen(authentication) > 0) {
-		return (sizeof(u32) * 2) + strlen(authentication) +
-			strlen(authentication) % 2;
-	}
-	return sizeof(u32) * 2;
+	return sizeof(u32) * 2 + ALIGN(strlen(authentication), 2);
 }
 
 /**
@@ -88,18 +84,18 @@ size_t calculate_security_buffer(char *authentication)
  *
  * Currently only supported type is PLAIN TEXT
  */
-void populate_security_buffer(char *buffer, char *authentication)
+void populate_security_buffer(char *buffer, const char *authentication)
 {
+	size_t seclen = strlen(authentication);
 	char *auth = buffer + sizeof(u32) * 2;
 	u32 *sectype = (u32 *) buffer;
-	u32 *seclen = sectype + 1;
+	u32 *seclenp = sectype + 1;
 
-	*sectype = strlen(authentication) > 0 ? 1 : 0;
-	*seclen = strlen(authentication);
+	*sectype = !!seclen;
+	*seclenp = seclen;
 
 	/* plain text */
-	if (strlen(authentication) > 0)
-		memcpy(auth, authentication, *seclen);
+	memcpy(auth, authentication, seclen);
 }
 
 /**
