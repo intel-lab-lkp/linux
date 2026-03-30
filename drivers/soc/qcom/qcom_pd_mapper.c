a60b642c96deae56e970453d0ce7e3b7399da993
Abel Vesa <abel.vesa@oss.qualcomm.com>
2026-03-30 17:30:56 +0300

    soc: qcom: pd-mapper: Add support for Eliza
    Add support for the Qualcomm Eliza SoC to the protection domain mapper.
    
    It has the same exact protection domain configuration as SM8550 SoC,
    so reuse that.
    
    Reviewed-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
    Signed-off-by: Abel Vesa <abel.vesa@oss.qualcomm.com>

--- drivers/soc/qcom/qcom_pd_mapper.c
+++ drivers/soc/qcom/qcom_pd_mapper.c
@@ -579,6 +579,7 @@ static const struct of_device_id qcom_pdm_domains[] __maybe_unused = {
 	{ .compatible = "qcom,apq8074", .data = NULL, },
 	{ .compatible = "qcom,apq8084", .data = NULL, },
 	{ .compatible = "qcom,apq8096", .data = msm8996_domains, },
+	{ .compatible = "qcom,eliza", .data = sm8550_domains, },
 	{ .compatible = "qcom,glymur", .data = glymur_domains, },
 	{ .compatible = "qcom,kaanapali", .data = kaanapali_domains, },
 	{ .compatible = "qcom,mahua", .data = glymur_domains, },
