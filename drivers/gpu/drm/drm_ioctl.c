aec62f75a2327e1c696a01cf473ac117512a0648
Arun R Murthy <arun.r.murthy@intel.com>
2026-03-31 14:33:59 +0530

    drm: Introduce DRM_CAP_ATOMIC_ERROR_REPORTING
    The new capability informs users that atomic_ioctl() supports
    failure reporting when an error occurs.
    
    Signed-off-by: Arun R Murthy <arun.r.murthy@intel.com>
    Reviewed-by: Suraj Kandpal <suraj.kandpal@intel.com>

--- drivers/gpu/drm/drm_ioctl.c
+++ drivers/gpu/drm/drm_ioctl.c
@@ -304,6 +304,9 @@ static int drm_getcap(struct drm_device *dev, void *data, struct drm_file *file_
 		req->value = drm_core_check_feature(dev, DRIVER_ATOMIC) &&
 			     dev->mode_config.async_page_flip;
 		break;
+	case DRM_CAP_ATOMIC_ERROR_REPORTING:
+		req->value = drm_core_check_feature(dev, DRIVER_ATOMIC) && 1;
+		break;
 	default:
 		return -EINVAL;
 	}
