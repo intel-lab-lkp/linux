c7ec6b50a57867d92a7ca3248af48da0905be623
Derek J. Clark <derekjohn.clark@gmail.com>
2026-03-31 18:11:58 +0000

    platform/x86: lenovo-wmi-other: Zero initialize WMI arguments
    Adds explicit initialization of wmi_method_args_32 declarations with
    zero values to prevent uninitialized data from being sent to the WMI
    device when passed.
    
    Suggested-by: Rong Zhang <i@rong.moe>
    Signed-off-by: Derek J. Clark <derekjohn.clark@gmail.com>

--- drivers/platform/x86/lenovo/wmi-other.c
+++ drivers/platform/x86/lenovo/wmi-other.c
@@ -166,7 +166,7 @@ MODULE_PARM_DESC(relax_fan_constraint,
  */
 static int lwmi_om_fan_get_set(struct lwmi_om_priv *priv, int channel, u32 *val, bool set)
 {
-	struct wmi_method_args_32 args;
+	struct wmi_method_args_32 args = { 0x0, 0x0 };
 	u32 method_id, retval;
 	int err;
 
@@ -772,7 +772,7 @@ static ssize_t attr_current_value_store(struct kobject *kobj,
 					struct tunable_attr_01 *tunable_attr)
 {
 	struct lwmi_om_priv *priv = dev_get_drvdata(tunable_attr->dev);
-	struct wmi_method_args_32 args;
+	struct wmi_method_args_32 args = { 0x0, 0x0 };
 	struct capdata01 capdata;
 	enum thermal_mode mode;
 	u32 attribute_id;
@@ -835,7 +835,7 @@ static ssize_t attr_current_value_show(struct kobject *kobj,
 				       struct tunable_attr_01 *tunable_attr)
 {
 	struct lwmi_om_priv *priv = dev_get_drvdata(tunable_attr->dev);
-	struct wmi_method_args_32 args;
+	struct wmi_method_args_32 args = { 0x0, 0x0 };
 	enum thermal_mode mode;
 	u32 attribute_id;
 	int retval;
