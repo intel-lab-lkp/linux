e5a98949ce2d9efecc10c2b4091699b50c4dbc29
Maciej Fijalkowski <maciej.fijalkowski@intel.com>
2026-03-31 17:02:06 +0200

    xsk: validate MTU against usable frame size on bind
    AF_XDP bind currently accepts zero-copy pool configurations without
    verifying that the device MTU fits into the usable frame space provided
    by the UMEM chunk.
    
    This becomes a problem since we started to respect tailroom which is
    subtracted from chunk_size (among with headroom). 2k chunk size might
    not provide enough space for standard 1500 MTU, so let us catch such
    settings at bind time. Furthermore, validate whether underlying HW will
    be able to satisfy configured MTU wrt XSK's frame size multiplied by
    supported Rx buffer chain length (that is exposed via
    net_device::xdp_zc_max_segs).
    
    Fixes: 24ea50127ecf ("xsk: support mbuf on ZC RX")
    Reviewed-by: Björn Töpel <bjorn@kernel.org>
    Signed-off-by: Maciej Fijalkowski <maciej.fijalkowski@intel.com>

--- net/xdp/xsk_buff_pool.c
+++ net/xdp/xsk_buff_pool.c
@@ -10,6 +10,8 @@
 #include "xdp_umem.h"
 #include "xsk.h"
 
+#define ETH_PAD_LEN (ETH_HLEN + 2 * VLAN_HLEN  + ETH_FCS_LEN)
+
 void xp_add_xsk(struct xsk_buff_pool *pool, struct xdp_sock *xs)
 {
 	if (!xs->tx)
@@ -157,6 +159,9 @@ static void xp_disable_drv_zc(struct xsk_buff_pool *pool)
 int xp_assign_dev(struct xsk_buff_pool *pool,
 		  struct net_device *netdev, u16 queue_id, u16 flags)
 {
+	u32 needed = netdev->mtu + ETH_PAD_LEN;
+	u32 segs = netdev->xdp_zc_max_segs;
+	bool mbuf = flags & XDP_USE_SG;
 	bool force_zc, force_copy;
 	struct netdev_bpf bpf;
 	int err = 0;
@@ -178,7 +183,7 @@ int xp_assign_dev(struct xsk_buff_pool *pool,
 	if (err)
 		return err;
 
-	if (flags & XDP_USE_SG)
+	if (mbuf)
 		pool->umem->flags |= XDP_UMEM_SG_FLAG;
 
 	if (flags & XDP_USE_NEED_WAKEUP)
@@ -200,8 +205,16 @@ int xp_assign_dev(struct xsk_buff_pool *pool,
 		goto err_unreg_pool;
 	}
 
+	if (mbuf) {
+		if (segs == 1) {
+			err = -EOPNOTSUPP;
+			goto err_unreg_pool;
+		}
+	} else {
+		segs = 1;
+	}
 	pool->umem->zc = true;
-	if (netdev->xdp_zc_max_segs == 1 && (flags & XDP_USE_SG)) {
+	if (needed > xsk_pool_get_rx_frame_size(pool) * segs) {
 		err = -EOPNOTSUPP;
 		goto err_unreg_pool;
 	}
