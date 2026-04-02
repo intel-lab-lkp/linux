77e56dfef2e28ba2f48956a2416efe1a0c770d1a
Maxime Ripard <mripard@kernel.org>
2026-04-02 10:23:30 +0200

    drm: Rename struct drm_atomic_state to drm_atomic_commit
    The KMS framework uses two slightly different definitions for the state
    concept. For a given object (plane, CRTC, encoder, etc., so
    drm_$OBJECT_state), the state is the entire state of that object.
    However, at the device level, drm_atomic_state refers to a state update
    for a limited number of objects.
    
    Thus, drm_atomic_state isn't the entire device state, but only the full
    state of some objects in that device. This has been an endless source of
    confusion and thus bugs.
    
    We can rename the drm_atomic_state structure to drm_atomic_commit to
    make it less confusing.
    
    This patch was created using:
    
    rg -l drm_atomic_state | \
    	xargs sed -i 's/drm_atomic_state/drm_atomic_commit/g; s/drm_atomic_commit_helper/drm_atomic_state_helper/g'
    mv drivers/gpu/drm/tests/drm_atomic_state_test.c drivers/gpu/drm/tests/drm_atomic_commit_test.c
    
    Acked-by: Simona Vetter <simona.vetter@ffwll.ch>
    Acked-by: Thomas Zimmermann <tzimmermann@suse.de>
    Acked-by: Jani Nikula <jani.nikula@intel.com>
    Reviewed-by: Luca Ceresoli <luca.ceresoli@bootlin.com>
    Tested-by: Luca Ceresoli <luca.ceresoli@bootlin.com>
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -1590,7 +1590,7 @@ int msm_dp_modeset_init(struct msm_dp *msm_dp_display, struct drm_device *dev,
 }
 
 void msm_dp_bridge_atomic_enable(struct drm_bridge *drm_bridge,
-				 struct drm_atomic_state *state)
+				 struct drm_atomic_commit *state)
 {
 	struct msm_dp_bridge *msm_dp_bridge = to_dp_bridge(drm_bridge);
 	struct msm_dp *dp = msm_dp_bridge->msm_dp_display;
@@ -1651,7 +1651,7 @@ void msm_dp_bridge_atomic_enable(struct drm_bridge *drm_bridge,
 }
 
 void msm_dp_bridge_atomic_disable(struct drm_bridge *drm_bridge,
-				  struct drm_atomic_state *state)
+				  struct drm_atomic_commit *state)
 {
 	struct msm_dp_bridge *msm_dp_bridge = to_dp_bridge(drm_bridge);
 	struct msm_dp *dp = msm_dp_bridge->msm_dp_display;
@@ -1663,7 +1663,7 @@ void msm_dp_bridge_atomic_disable(struct drm_bridge *drm_bridge,
 }
 
 void msm_dp_bridge_atomic_post_disable(struct drm_bridge *drm_bridge,
-				       struct drm_atomic_state *state)
+				       struct drm_atomic_commit *state)
 {
 	struct msm_dp_bridge *msm_dp_bridge = to_dp_bridge(drm_bridge);
 	struct msm_dp *dp = msm_dp_bridge->msm_dp_display;
