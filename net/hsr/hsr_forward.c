996f54c5ed894ecad3787c60d216ca7fc13d6035
Haoze Xie <royenheart@gmail.com>
2026-04-02 22:18:14 +0800

    net: hsr: avoid learning nodes from ordinary PRP SAN traffic
    PRP slave ports accept ordinary SAN traffic and forward it to the
    master device without needing a persistent node entry. Creating one
    node per previously unseen SAN source lets arbitrary source MAC floods
    grow node_db until the prune timer catches up.
    
    Keep the receive path for ordinary PRP SAN traffic, but stop learning a
    new node when the frame is untagged and does not carry a PRP trailer.
    Continue to deliver the frame locally and only keep node state for
    actual HSR/PRP senders or nodes that have already been learned.
    
    Fixes: 451d8123f897 ("net: prp: add packet handling support")
    Reported-by: Yifan Wu <yifanwucs@gmail.com>
    Reported-by: Juefei Pu <tomapufckgml@gmail.com>
    Co-developed-by: Yuan Tan <yuantan098@gmail.com>
    Signed-off-by: Yuan Tan <yuantan098@gmail.com>
    Suggested-by: Xin Liu <bird@lzu.edu.cn>
    Tested-by: Yuqi Xu <xuyuqiabc@gmail.com>
    Signed-off-by: Haoze Xie <royenheart@gmail.com>
    Signed-off-by: Ao Zhou <n05ec@lzu.edu.cn>

--- net/hsr/hsr_forward.c
+++ net/hsr/hsr_forward.c
@@ -403,7 +403,8 @@ static void hsr_deliver_master(struct sk_buff *skb, struct net_device *dev,
 	int res, recv_len;
 
 	was_multicast_frame = (skb->pkt_type == PACKET_MULTICAST);
-	hsr_addr_subst_source(node_src, skb);
+	if (node_src)
+		hsr_addr_subst_source(node_src, skb);
 	skb_pull(skb, ETH_HLEN);
 	recv_len = skb->len;
 	res = netif_rx(skb);
@@ -699,8 +700,12 @@ static int fill_frame_info(struct hsr_frame_info *frame,
 
 	frame->node_src = hsr_get_node(port, n_db, skb,
 				       frame->is_supervision, port->type);
-	if (!frame->node_src)
-		return -1; /* Unknown node and !is_supervision, or no mem */
+	if (IS_ERR(frame->node_src)) {
+		ret = PTR_ERR(frame->node_src);
+		if (ret != -ENOENT)
+			return ret;
+		frame->node_src = NULL;
+	}
 
 	ethhdr = (struct ethhdr *)skb_mac_header(skb);
 	frame->is_vlan = false;
@@ -739,7 +744,8 @@ void hsr_forward_skb(struct sk_buff *skb, struct hsr_port *port)
 	if (fill_frame_info(&frame, skb, port) < 0)
 		goto out_drop;
 
-	hsr_register_frame_in(frame.node_src, port, frame.sequence_nr);
+	if (frame.node_src)
+		hsr_register_frame_in(frame.node_src, port, frame.sequence_nr);
 	hsr_forward_do(&frame);
 	rcu_read_unlock();
 	/* Gets called for ingress frames as well as egress from master port.
