6357fc1b0fe74171796a37f25e57c3e1be4b8eb0
Kim Phillips <kim.phillips@amd.com>
2026-04-02 15:25:56 -0500

    KVM: SEV: Advertise SVM_SEV_FEAT_SNP_ACTIVE
    Allow userspace to set the flag in kvm_sev_init.flags.
    
    KVM still needs to set the flag for backwards compatibility, but
    disallowing SVM_SEV_FEAT_SNP_ACTIVE for an SNP guest is "bizarre."
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Cc: Borislav Petkov (AMD) <bp@alien8.de>
    Link: https://lore.kernel.org/kvm/aaWog_UjW-M3412C@google.com/
    Signed-off-by: Kim Phillips <kim.phillips@amd.com>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -3165,8 +3165,12 @@ out:
 	    cpu_feature_enabled(X86_FEATURE_NO_NESTED_DATA_BP))
 		sev_supported_vmsa_features |= SVM_SEV_FEAT_DEBUG_SWAP;
 
-	if (sev_snp_enabled && tsc_khz && cpu_feature_enabled(X86_FEATURE_SNP_SECURE_TSC))
-		sev_supported_vmsa_features |= SVM_SEV_FEAT_SECURE_TSC;
+	if (sev_snp_enabled) {
+		sev_supported_vmsa_features |= SVM_SEV_FEAT_SNP_ACTIVE;
+
+		if (tsc_khz && cpu_feature_enabled(X86_FEATURE_SNP_SECURE_TSC))
+			sev_supported_vmsa_features |= SVM_SEV_FEAT_SECURE_TSC;
+	}
 }
 
 void sev_hardware_unsetup(void)
