5e6f8465bb2ea4a6c9e343cf4c9b0f85d04432b0
hmtheboy154 <buingoc67@gmail.com>
2026-04-03 16:28:47 +0700

    HID: multitouch: rename MT_CLS_EGALAX_P80H84 to MT_CLS_EGALAX_WIN8
    The Dell Pro Rugged 12 Tablet RA02260 uses an i2c touchscreen from
    eGalax that exposes the same HID_GROUP_MULTITOUCH_WIN_8 as the
    P80H84 and also requires the same quirks to make multitouch work.
    
    Rename the class to MT_CLS_EGALAX_WIN8 for more general use.
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -235,7 +235,7 @@ static void mt_post_parse(struct mt_device *td, struct mt_application *app);
 #define MT_CLS_SMART_TECH			0x0113
 #define MT_CLS_APPLE_TOUCHBAR			0x0114
 #define MT_CLS_YOGABOOK9I			0x0115
-#define MT_CLS_EGALAX_P80H84			0x0116
+#define MT_CLS_EGALAX_WIN8			0x0116
 #define MT_CLS_SIS				0x0457
 
 #define MT_DEFAULT_MAXCONTACT	10
@@ -450,7 +450,7 @@ static const struct mt_class mt_classes[] = {
 			MT_QUIRK_YOGABOOK9I,
 		.export_all_inputs = true
 	},
-	{ .name = MT_CLS_EGALAX_P80H84,
+	{ .name = MT_CLS_EGALAX_WIN8,
 		.quirks = MT_QUIRK_ALWAYS_VALID |
 			MT_QUIRK_IGNORE_DUPLICATES |
 			MT_QUIRK_CONTACT_CNT_ACCURATE,
@@ -2246,7 +2246,7 @@ static const struct hid_device_id mt_devices[] = {
 	{ .driver_data = MT_CLS_EGALAX_SERIAL,
 		MT_USB_DEVICE(USB_VENDOR_ID_DWAV,
 			USB_DEVICE_ID_DWAV_EGALAX_MULTITOUCH_C000) },
-	{ .driver_data = MT_CLS_EGALAX_P80H84,
+	{ .driver_data = MT_CLS_EGALAX_WIN8,
 		HID_DEVICE(HID_BUS_ANY, HID_GROUP_MULTITOUCH_WIN_8,
 			USB_VENDOR_ID_DWAV,
 			USB_DEVICE_ID_DWAV_EGALAX_MULTITOUCH_C002) },
