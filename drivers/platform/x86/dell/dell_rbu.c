7e5361fa6b728cf318afaf6c3ef936d0002e6a72
Fedor Pchelkin <pchelkin@ispras.ru>
2026-04-03 16:42:39 +0300

    platform/x86: dell_rbu: avoid uninit value usage in packet_size_write()
    Ensure the temp value has been properly parsed from the user-provided
    buffer and initialized to be used in later operations.  While at it,
    prefer a convenient kstrtoul() helper.
    
    Found by Linux Verification Center (linuxtesting.org) with Svace static
    analysis tool.
    
    Fixes: ad6ce87e5bd4 ("[PATCH] dell_rbu: changes in packet update mechanism")
    Signed-off-by: Fedor Pchelkin <pchelkin@ispras.ru>

--- drivers/platform/x86/dell/dell_rbu.c
+++ drivers/platform/x86/dell/dell_rbu.c
@@ -619,9 +619,12 @@ static ssize_t packet_size_write(struct file *filp, struct kobject *kobj,
 				 char *buffer, loff_t pos, size_t count)
 {
 	unsigned long temp;
+
+	if (kstrtoul(buffer, 10, &temp))
+		return -EINVAL;
+
 	spin_lock(&rbu_data.lock);
 	packet_empty_list();
-	sscanf(buffer, "%lu", &temp);
 	if (temp < 0xffffffff)
 		rbu_data.packetsize = temp;
 
