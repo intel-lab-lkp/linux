df8632a175660ab0278b7285930227392869fc45
Oskar Ray-Frayssinet <rayfraytech@gmail.com>
2026-04-05 00:50:23 +0200

    tg3: Add PowerEdge R740xd to AER quirk list
    The PowerEdge R740xd is a variant of the R740 that also triggers
    a fatal PCIe AER event during system reboot. Add it to the DMI
    list to apply the same workaround.
    
    Fixes: e0efe83ed325 ("tg3: Disable tg3 PCIe AER on system reboot")
    
    Signed-off-by: Oskar Ray-Frayssinet <rayfraytech@gmail.com>

--- drivers/net/ethernet/broadcom/tg3.c
+++ drivers/net/ethernet/broadcom/tg3.c
@@ -18219,6 +18219,18 @@ static const struct dmi_system_id tg3_restart_aer_quirk_table[] = {
 	{
 		.matches = {
 			DMI_MATCH(DMI_SYS_VENDOR, "Dell Inc."),
+			DMI_MATCH(DMI_PRODUCT_NAME, "PowerEdge R740xd"),
+		},
+	},
+	{
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Dell Inc."),
+			DMI_MATCH(DMI_PRODUCT_NAME, "R740XD vSAN Ready Node"),
+		},
+	},
+	{
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Dell Inc."),
 			DMI_MATCH(DMI_PRODUCT_NAME, "PowerEdge R750"),
 		},
 	},
