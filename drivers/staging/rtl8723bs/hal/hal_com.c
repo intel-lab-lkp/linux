46fd595da5a02a8f13176db3a8d1211577b71eac
Prithvi Tambewagh <activprithvi@gmail.com>
2026-04-05 17:11:28 +0530

    staging: rtl8723bs: move constant to right side of test in comparison
    Move constant from the left side to the right side of the test in a
    comparison, where ==, !=, <=, >=, <, > operators are used, fixing the
    checkpatch warning: Comparisons should place the constant on the right
    side of the test.
    
    Signed-off-by: Prithvi Tambewagh <activprithvi@gmail.com>

--- drivers/staging/rtl8723bs/hal/hal_com.c
+++ drivers/staging/rtl8723bs/hal/hal_com.c
@@ -107,7 +107,7 @@ u8 hal_com_config_channel_plan(
 	pHalData->bDisableSWChannelPlan = false;
 	chnlPlan = def_channel_plan;
 
-	if (0xFF == hw_channel_plan)
+	if (hw_channel_plan == 0xFF)
 		AutoLoadFail = true;
 
 	if (!AutoLoadFail) {
