501ba0ebc2353dfdb29ed80318142f57fe78702a
Haoze Xie <royenheart@gmail.com>
2026-04-06 21:28:16 +0800

    net: l3mdev: Ignore non-L3 uppers in l3mdev_fib_table_rcu
    l3mdev_fib_table_rcu() assumes that any upper device observed for
    an IFF_L3MDEV_SLAVE device is an L3 master and dereferences
    master->l3mdev_ops unconditionally.
    
    VRF slave setup sets IFF_L3MDEV_SLAVE before the upper link is fully
    switched, so readers can transiently observe a non-L3 upper such as a
    bridge and follow a NULL l3mdev_ops pointer. Require the current upper
    to still be an L3 master before consulting its FIB table.
    
    Fixes: fdeea7be88b1 ("net: vrf: Set slave's private flag before linking")
    Reported-by: Yifan Wu <yifanwucs@gmail.com>
    Reported-by: Juefei Pu <tomapufckgml@gmail.com>
    Co-developed-by: Yuan Tan <yuantan098@gmail.com>
    Signed-off-by: Yuan Tan <yuantan098@gmail.com>
    Suggested-by: Xin Liu <bird@lzu.edu.cn>
    Reviewed-by: David Ahern <dsahern@kernel.org>
    Signed-off-by: Haoze Xie <royenheart@gmail.com>
    Signed-off-by: Ao Zhou <n05ec@lzu.edu.cn>

--- net/l3mdev/l3mdev.c
+++ net/l3mdev/l3mdev.c
@@ -177,7 +177,7 @@ u32 l3mdev_fib_table_rcu(const struct net_device *dev)
 		const struct net_device *master;
 
 		master = netdev_master_upper_dev_get_rcu(_dev);
-		if (master &&
+		if (master && netif_is_l3_master(master) &&
 		    master->l3mdev_ops->l3mdev_fib_table)
 			tb_id = master->l3mdev_ops->l3mdev_fib_table(master);
 	}
