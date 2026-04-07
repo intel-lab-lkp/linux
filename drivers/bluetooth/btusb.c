c4d546cc763e94eecd0f2219d00277a9bb479bc4
Chris Lu <chris.lu@mediatek.com>
2026-04-07 14:47:59 +0800

    Bluetooth: btmtk: add event filter to filter specific event
    Due to some Bluetooth hosts have mechanism to detect invalid events,
    causing abnormal behavior. Add a event filter to prevent debugging
    events from being sent to host.
    
    Signed-off-by: Chris Lu <chris.lu@mediatek.com>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -4138,6 +4138,8 @@ static int btusb_probe(struct usb_interface *intf,
 	} else if (id->driver_info & BTUSB_MEDIATEK) {
 		/* Allocate extra space for Mediatek device */
 		priv_size += sizeof(struct btmtk_data);
+
+		data->recv_event = btmtk_recv_event;
 	}
 
 	data->recv_acl = hci_recv_frame;
