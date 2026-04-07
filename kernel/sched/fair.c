a124571ddc4325bc2aabbcbc4ffca62015506738
Shrikanth Hegde <sshegde@linux.ibm.com>
2026-04-08 00:49:40 +0530

    sched/fair: load balance only among preferred CPUs
    Consider only preferred CPUs for load balance.
    
    With this, load balance will end up choosing a preferred CPUs for pull.
    This makes it not fight against the push task mechanism which happens
    at tick. Also, this stops active balance to happen on non-preferred CPU
    pulling the load.
    
    This means there is no load balancing if the task is pinned only to
    non-preferred CPUs. They will continue to run where they were previously
    running before the CPUs was marked as non-preferred.
    
    Signed-off-by: Shrikanth Hegde <sshegde@linux.ibm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -12074,6 +12074,11 @@ static int sched_balance_rq(int this_cpu, struct rq *this_rq,
 
 	cpumask_and(cpus, sched_domain_span(sd), cpu_active_mask);
 
+#ifdef CONFIG_PARAVIRT
+	/* Spread load among preferred CPUs */
+	cpumask_and(cpus, cpus, cpu_preferred_mask);
+#endif
+
 	schedstat_inc(sd->lb_count[idle]);
 
 redo:
