61e8177288122bef5e9a6241edc99e236a6f4ff6
Ye Liu <liuye@kylinos.cn>
2026-04-07 10:07:13 +0800

    mm/memory-failure: use bool for forcekill state
    'forcekill' is used as a boolean flag to control whether
    processes should be forcibly killed. It is only assigned
    from boolean expressions and never used in arithmetic or
    bitmask operations.
    
    Convert it from int to bool.
    
    No functional change intended.
    
    Signed-off-by: Ye Liu <liuye@kylinos.cn>

--- mm/memory-failure.c
+++ mm/memory-failure.c
@@ -459,7 +459,7 @@ void add_to_kill_ksm(struct task_struct *tsk, const struct page *p,
  * Only do anything when FORCEKILL is set, otherwise just free the
  * list (this is used for clean pages which do not need killing)
  */
-static void kill_procs(struct list_head *to_kill, int forcekill,
+static void kill_procs(struct list_head *to_kill, bool forcekill,
 		unsigned long pfn, int flags)
 {
 	struct to_kill *tk, *next;
@@ -1582,7 +1582,7 @@ static bool hwpoison_user_mappings(struct folio *folio, struct page *p,
 {
 	LIST_HEAD(tokill);
 	bool unmap_success;
-	int forcekill;
+	bool forcekill;
 	bool mlocked = folio_test_mlocked(folio);
 
 	/*
