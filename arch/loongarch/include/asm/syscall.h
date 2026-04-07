0b3252236ef9fb56b7716361b80a90a8f8c5d2bf
Tiezhu Yang <yangtiezhu@loongson.cn>
2026-04-07 14:33:50 +0800

    LoongArch: Handle CONFIG_32BIT in syscall_get_arch()
    If CONFIG_32BIT is set, it should return AUDIT_ARCH_LOONGARCH32 instead of
    AUDIT_ARCH_LOONGARCH64 in syscall_get_arch().
    
    Signed-off-by: Tiezhu Yang <yangtiezhu@loongson.cn>

--- arch/loongarch/include/asm/syscall.h
+++ arch/loongarch/include/asm/syscall.h
@@ -78,7 +78,11 @@ static inline void syscall_set_arguments(struct task_struct *task,
 
 static inline int syscall_get_arch(struct task_struct *task)
 {
+#ifdef CONFIG_64BIT
 	return AUDIT_ARCH_LOONGARCH64;
+#else
+	return AUDIT_ARCH_LOONGARCH32;
+#endif
 }
 
 static inline bool arch_syscall_is_vdso_sigreturn(struct pt_regs *regs)
