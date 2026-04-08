d9c15a3cfd4bc8b2cf63450f73365aeb88ec1fdc
Satyanarayana K V P <satyanarayana.k.v.p@intel.com>
2026-04-08 16:05:17 +0000

    drm/xe/tests: Fix pf_set_admin_mode() after sriov.pf.admin_only removal
    Admin-only mode is now derived from xe_device_is_admin_only(), which
    checks whether the device was registered under the admin-only DRM driver
    instance or not.
    
    Update the kunit test to have a stub function to mimic the functionality
    of xe_device_is_admin_only().
    
    Signed-off-by: Satyanarayana K V P <satyanarayana.k.v.p@intel.com>
    Cc: Michal Wajdeczko <michal.wajdeczko@intel.com>

--- drivers/gpu/drm/xe/tests/xe_gt_sriov_pf_config_kunit.c
+++ drivers/gpu/drm/xe/tests/xe_gt_sriov_pf_config_kunit.c
@@ -7,16 +7,34 @@
 #include <kunit/test.h>
 #include <kunit/test-bug.h>
 
+#include "xe_device.h"
 #include "xe_kunit_helpers.h"
 #include "xe_pci_test.h"
 
 #define TEST_MAX_VFS	63
 #define TEST_VRAM	0x7a800000ull	/* random size that works on 32-bit */
 
+static bool xe_device_is_admin_only_stub_enable(const struct xe_device *xe)
+{
+	return true;
+}
+
+static bool xe_device_is_admin_only_stub_disable(const struct xe_device *xe)
+{
+	return false;
+}
+
 static void pf_set_admin_mode(struct xe_device *xe, bool enable)
 {
-	/* should match logic of xe_sriov_pf_admin_only() */
-	xe->sriov.pf.admin_only = enable;
+	if (enable)
+		kunit_activate_static_stub(kunit_get_current_test(),
+					   xe_device_is_admin_only,
+					   xe_device_is_admin_only_stub_enable);
+	else
+		kunit_activate_static_stub(kunit_get_current_test(),
+					   xe_device_is_admin_only,
+					   xe_device_is_admin_only_stub_disable);
+
 	KUNIT_EXPECT_EQ(kunit_get_current_test(), enable, xe_sriov_pf_admin_only(xe));
 }
 
