dab0ef66bb3ff8df99061cfa058bb42031230cb1
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2026-04-08 12:03:19 +0200

    drm/amdgpu: use DRM_SCHED_PRIORITY_KERNEL for all ttm entities
    ttm jobs must have higher priority than any userspace jobs.
    
    Signed-off-by: Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>

--- drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_ttm.c
@@ -2377,7 +2377,7 @@ void amdgpu_ttm_enable_buffer_funcs(struct amdgpu_device *adev)
 		r = amdgpu_ttm_buffer_entity_init(
 			&adev->mman.gtt_mgr,
 			&adev->mman.clear_entities[i],
-			DRM_SCHED_PRIORITY_NORMAL,
+			DRM_SCHED_PRIORITY_KERNEL,
 			adev->mman.buffer_funcs_scheds,
 			adev->mman.num_buffer_funcs_scheds, 1);
 
@@ -2397,7 +2397,7 @@ void amdgpu_ttm_enable_buffer_funcs(struct amdgpu_device *adev)
 		r = amdgpu_ttm_buffer_entity_init(
 			&adev->mman.gtt_mgr,
 			&adev->mman.move_entities[i],
-			DRM_SCHED_PRIORITY_NORMAL,
+			DRM_SCHED_PRIORITY_KERNEL,
 			adev->mman.buffer_funcs_scheds,
 			adev->mman.num_buffer_funcs_scheds, 2);
 
