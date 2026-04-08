877fc19a609bc82a50d146a1ca280503a2d1d483
tas0dev <osjin83@gmail.com>
2026-04-08 19:32:07 +0900

    staging: rtl8723bs: fix casting and alignment spacing in rtl8723b_phycfg.c
    Clean up spacing issues related to type casting and function parameter
    alignment as flagged by checkpatch.pl.
    
    Specifically, this patch:
    - Removes the unnecessary space between a type cast and the variable,
      changing "(u8) value" to "(u8)value".
    - Adjusts the alignment of arguments in function calls to match the
      open parenthesis.
    - Standardizes the formatting of multi-line function parameters for
      better readability.
    
    These changes are stylistic improvements and have no impact on the
    runtime logic of the driver.
    
    Signed-off-by: tas0dev <osjin83@gmail.com>

--- drivers/staging/rtl8723bs/hal/rtl8723b_phycfg.c
+++ drivers/staging/rtl8723bs/hal/rtl8723b_phycfg.c
@@ -395,7 +395,8 @@ int PHY_BBConfig8723B(struct adapter *Adapter)
 
 	PHY_SetRFReg(Adapter, RF_PATH_A, 0x1, 0xfffff, 0x780);
 
-	rtw_write8(Adapter, REG_SYS_FUNC_EN, FEN_PPLL|FEN_PCIEA|FEN_DIO_PCIE|FEN_BB_GLB_RSTn|FEN_BBRSTB);
+	rtw_write8(Adapter, REG_SYS_FUNC_EN,
+		   FEN_PPLL | FEN_PCIEA | FEN_DIO_PCIE | FEN_BB_GLB_RSTn | FEN_BBRSTB);
 
 	rtw_write8(Adapter, REG_AFE_XTAL_CTRL + 1, 0x80);
 
@@ -531,7 +532,7 @@ u8 PHY_GetTxPowerIndex(
 	struct hal_com_data *pHalData = GET_HAL_DATA(padapter);
 	s8 txPower = 0, powerDiffByRate = 0, limit = 0;
 
-	txPower = (s8) PHY_GetTxPowerIndexBase(padapter, RFPath, Rate, BandWidth, Channel);
+	txPower = (s8)PHY_GetTxPowerIndexBase(padapter, RFPath, Rate, BandWidth, Channel);
 	powerDiffByRate = PHY_GetTxPowerByRate(padapter, RF_PATH_A, Rate);
 
 	limit = phy_get_tx_pwr_lmt(
@@ -551,7 +552,7 @@ u8 PHY_GetTxPowerIndex(
 	if (txPower > MAX_POWER_INDEX)
 		txPower = MAX_POWER_INDEX;
 
-	return (u8) txPower;
+	return (u8)txPower;
 }
 
 void PHY_SetTxPowerLevel8723B(struct adapter *Adapter, u8 Channel)
