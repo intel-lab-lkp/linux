a6200bba10303db4d40edb88e41746b9f547e5c4
Thorsten Blum <thorsten.blum@linux.dev>
2026-04-10 00:57:05 +0200

    KEYS: annotate struct user_key_payload with __counted_by
    Add the __counted_by() compiler attribute to the flexible array member
    'data' to improve access bounds-checking via CONFIG_UBSAN_BOUNDS and
    CONFIG_FORTIFY_SOURCE.
    
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>

--- include/keys/user-type.h
+++ include/keys/user-type.h
@@ -27,7 +27,8 @@
 struct user_key_payload {
 	struct rcu_head	rcu;		/* RCU destructor */
 	unsigned short	datalen;	/* length of this data */
-	char		data[] __aligned(__alignof__(u64)); /* actual data */
+	char		data[]		/* actual data */
+			__aligned(8) __counted_by(datalen);
 };
 
 extern struct key_type key_type_user;
