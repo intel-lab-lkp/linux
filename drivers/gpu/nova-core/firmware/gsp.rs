c7b201595f3f746f2f8028bf941074cfd0554093
Timur Tabi <ttabi@nvidia.com>
2026-04-10 15:37:17 -0500

    gpu: nova-core: use correct fwsignature for GA100
    Although GA100 uses the same GSP-RM firmware as Turing, it has a different
    signature specifically for it.
    
    Fixes: 121ea04cd9f2 ("gpu: nova-core: add support for Turing/GA100 fwsignature")
    Signed-off-by: Timur Tabi <ttabi@nvidia.com>

--- drivers/gpu/nova-core/firmware/gsp.rs
+++ drivers/gpu/nova-core/firmware/gsp.rs
@@ -139,7 +139,7 @@ impl GspFirmware {
                         }
                         Architecture::Turing => ".fwsignature_tu10x",
                         // GA100 uses the same firmware as Turing
-                        Architecture::Ampere if chipset == Chipset::GA100 => ".fwsignature_tu10x",
+                        Architecture::Ampere if chipset == Chipset::GA100 => ".fwsignature_ga100",
                         Architecture::Ampere => ".fwsignature_ga10x",
                         Architecture::Ada => ".fwsignature_ad10x",
                     };
