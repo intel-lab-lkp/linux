921b504fb66872c1fbb7c269d2a1f11f60998769
Eliot Courtney <ecourtney@nvidia.com>
2026-04-10 17:38:54 +0900

    gpu: nova-core: vbios: use checked ops and accesses in `FwSecBiosImage::ucode`
    Use checked arithmetic and access for extracting the microcode since the
    offsets are firmware derived.
    
    Fixes: 47c4846e4319 ("gpu: nova-core: vbios: Add support for FWSEC ucode extraction")
    Signed-off-by: Eliot Courtney <ecourtney@nvidia.com>

--- drivers/gpu/nova-core/vbios.rs
+++ drivers/gpu/nova-core/vbios.rs
@@ -1027,16 +1027,21 @@ impl FwSecBiosImage {
 
     /// Get the ucode data as a byte slice
     pub(crate) fn ucode(&self, desc: &FalconUCodeDesc) -> Result<&[u8]> {
-        let falcon_ucode_offset = self.falcon_ucode_offset;
-
         // The ucode data follows the descriptor.
-        let ucode_data_offset = falcon_ucode_offset + desc.size();
-        let size = usize::from_safe_cast(desc.imem_load_size() + desc.dmem_load_size());
+        let data = self
+            .base
+            .data
+            .get(self.falcon_ucode_offset..)
+            .ok_or(ERANGE)?;
+        let size = usize::from_safe_cast(
+            desc.imem_load_size()
+                .checked_add(desc.dmem_load_size())
+                .ok_or(ERANGE)?,
+        );
 
         // Get the data slice, checking bounds in a single operation.
-        self.base
-            .data
-            .get(ucode_data_offset..ucode_data_offset + size)
+        data.get(desc.size()..)
+            .and_then(|data| data.get(..size))
             .ok_or(ERANGE)
             .inspect_err(|_| {
                 dev_err!(
