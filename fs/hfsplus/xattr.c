7f89e972840b84b63607770bb6e908836f5223f2
Colin Ian King <colin.i.king@gmail.com>
2026-04-11 13:53:38 +0100

    hfsplus: remove xattr error message
    When exercising hfsplus with the stress-ng xattr stressor a large
    quantity of somewhat unhelpful error messages "hfsplus: xattr exists
    yet" spam the kernel log. Remove these.
    
    Signed-off-by: Colin Ian King <colin.i.king@gmail.com>

--- fs/hfsplus/xattr.c
+++ fs/hfsplus/xattr.c
@@ -383,7 +383,6 @@ int __hfsplus_setxattr(struct inode *inode, const char *name,
 
 	if (!strcmp_xattr_finder_info(name)) {
 		if (flags & XATTR_CREATE) {
-			pr_err("xattr exists yet\n");
 			err = -EOPNOTSUPP;
 			goto end_setxattr;
 		}
@@ -436,7 +435,6 @@ int __hfsplus_setxattr(struct inode *inode, const char *name,
 
 	if (hfsplus_attr_exists(inode, name)) {
 		if (flags & XATTR_CREATE) {
-			pr_err("xattr exists yet\n");
 			err = -EOPNOTSUPP;
 			goto end_setxattr;
 		}
