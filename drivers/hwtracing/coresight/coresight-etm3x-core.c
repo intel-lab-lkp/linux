8db22e1ad0a112740a45ebd52c826c5b7af34319
Yeoreum Yun <yeoreum.yun@arm.com>
2026-04-13 15:20:02 +0100

    coresight: etm3x: remove redundant call etm4_enable_hw with hotplug
    The cpu_online_mask is set at the CPUHP_BRINGUP_CPU step.
    In other words, if etm4_enable_sysfs() is called between
    CPUHP_BRINGUP_CPU and CPUHP_AP_ARM_CORESIGHT_STARTING,
    etm_enable_hw() may be invoked in etm_enable_sysfs_smp_call()
    and then executed again in etm_starting_cpu().
    
    To remove this redundant call, take the hotplug lock before executing
    etm_enable_sysfs_smp_call().
    
    Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>

--- drivers/hwtracing/coresight/coresight-etm3x-core.c
+++ drivers/hwtracing/coresight/coresight-etm3x-core.c
@@ -522,26 +522,26 @@ static int etm_enable_sysfs(struct coresight_device *csdev, struct coresight_pat
 	struct etm_enable_arg arg = { };
 	int ret;
 
+	arg.drvdata = drvdata;
+	arg.traceid = path->trace_id;
+
+	raw_spin_lock(&drvdata->spinlock);
+	arg.config = drvdata->config;
+	raw_spin_unlock(&drvdata->spinlock);
 
 	/*
 	 * Configure the ETM only if the CPU is online.  If it isn't online
 	 * hw configuration will take place on the local CPU during bring up.
 	 */
-	if (cpu_online(drvdata->cpu)) {
-		arg.drvdata = drvdata;
-		arg.traceid = path->trace_id;
-
-		raw_spin_lock(&drvdata->spinlock);
-		arg.config = drvdata->config;
-		raw_spin_unlock(&drvdata->spinlock);
-
+	cpus_read_lock();
 		ret = smp_call_function_single(drvdata->cpu,
 					       etm_enable_sysfs_smp_call, &arg, 1);
-		if (!ret)
-			ret = arg.rc;
-	} else {
+	cpus_read_unlock();
+
+	if (!ret)
+		ret = arg.rc;
+	else
 		ret = -ENODEV;
-	}
 
 	if (!ret)
 		dev_dbg(&csdev->dev, "ETM tracing enabled\n");
