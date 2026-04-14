9f316c9efbaf2781d47ee2ff535307aee9fb6397
Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>
2026-04-14 17:28:59 +0200

    remoteproc: Introduce optional release_fw operation
    The release_fw operation allows releasing remote processor resources
    configured during rproc_parse_fw() and rproc_load_segments().
    For example, it can release carveouts mapped during these operations.
    
    This operation is invoked in the following cases:
     - When an error occurs during remote processor boot.
     - When an error occurs during remote processor recovery start.
     - After stopping the remote processor.
    
    This operation is required for the remoteproc_tee implementation following
    a stop or upon encountering an error. Since the remoteproc image is loaded
    during resource table parsing, multiple failure scenarios may occur prior
    to remote processor startup, including issues with resource handling and
    carveout allocation.
    
    Signed-off-by: Arnaud Pouliquen <arnaud.pouliquen@foss.st.com>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -1856,6 +1856,8 @@ static int rproc_boot_recovery(struct rproc *rproc)
 
 	/* boot the remote processor up again */
 	ret = rproc_start(rproc, firmware_p);
+	if (ret)
+		rproc_release_fw(rproc);
 
 	release_firmware(firmware_p);
 
@@ -1997,6 +1999,8 @@ int rproc_boot(struct rproc *rproc)
 		}
 
 		ret = rproc_fw_boot(rproc, firmware_p);
+		if (ret)
+			rproc_release_fw(rproc);
 
 		release_firmware(firmware_p);
 	}
@@ -2065,6 +2069,8 @@ int rproc_shutdown(struct rproc *rproc)
 	rproc_unprepare_device(rproc);
 
 	rproc_disable_iommu(rproc);
+
+	rproc_release_fw(rproc);
 
 	/* Free the copy of the resource table */
 	kfree(rproc->cached_table);
--- include/linux/remoteproc.h
+++ include/linux/remoteproc.h
@@ -381,6 +381,10 @@ enum rsc_handling_status {
  * @panic:	optional callback to react to system panic, core will delay
  *		panic at least the returned number of milliseconds
  * @coredump:	  collect firmware dump after the subsystem is shutdown
+ * @release_fw:	Optional function to release resources allocated during
+ *		parse_fw() or load() operations. This function is called after
+ *		stopping the remote processor or in case of an error during the
+ *		boot or recovery sequence.
  */
 struct rproc_ops {
 	int (*prepare)(struct rproc *rproc);
@@ -403,6 +407,7 @@ struct rproc_ops {
 	u64 (*get_boot_addr)(struct rproc *rproc, const struct firmware *fw);
 	unsigned long (*panic)(struct rproc *rproc);
 	void (*coredump)(struct rproc *rproc);
+	void (*release_fw)(struct rproc *rproc);
 };
 
 /**
