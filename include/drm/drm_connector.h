885d8d8d0efba18177cabd370c1c152e090c4a4f
Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>
2026-04-16 02:22:32 +0300

    drm/connector: report IRQ_HPD events to drm_connector_oob_hotplug_event()
    The DisplayPort standard defines a special kind of events called IRQ.
    These events are used to notify DP Source about the events on the Sink
    side. It is extremely important for DP MST handling, where the MST
    events are reported through this IRQ.
    
    In case of the USB-C DP AltMode there is no actual HPD pulse, but the
    events are ported through the bits in the AltMode VDOs.
    
    Extend the drm_connector_oob_hotplug_event() interface and report IRQ
    events to the DisplayPort Sink drivers.
    
    Signed-off-by: Dmitry Baryshkov <dmitry.baryshkov@oss.qualcomm.com>

--- include/drm/drm_connector.h
+++ include/drm/drm_connector.h
@@ -2521,7 +2521,8 @@ drm_connector_is_unregistered(struct drm_connector *connector)
 }
 
 void drm_connector_oob_hotplug_event(struct fwnode_handle *connector_fwnode,
-				     enum drm_connector_status status);
+				     enum drm_connector_status status,
+				     bool irq_hpd);
 const char *drm_get_connector_type_name(unsigned int connector_type);
 const char *drm_get_connector_status_name(enum drm_connector_status status);
 const char *drm_get_subpixel_order_name(enum subpixel_order order);
