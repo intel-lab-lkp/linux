c55a6a9f71c8f7da378ba2456fad245461ab1344
sai madhu <suryasaimadhu369@gmail.com>
2026-04-17 11:33:10 +0000

    drm/mediatek: hdmi: Convert DRM_ERROR() to drm_err()
    The DRM_ERROR() macro is deprecated in favor of drm_err() which
    provides device-specific logging.
    
    Replace DRM_ERROR() with drm_err() in the Mediatek HDMI bridge
    driver and pass the drm_device pointer via bridge->dev.
    
    No functional change intended.
    
    Signed-off-by: sai madhu <suryasaimadhu369@gmail.com>

--- drivers/gpu/drm/mediatek/mtk_hdmi.c
+++ drivers/gpu/drm/mediatek/mtk_hdmi.c
@@ -981,8 +981,9 @@ static int mtk_hdmi_bridge_attach(struct drm_bridge *bridge,
 	int ret;
 
 	if (!(flags & DRM_BRIDGE_ATTACH_NO_CONNECTOR)) {
-		DRM_ERROR("%s: The flag DRM_BRIDGE_ATTACH_NO_CONNECTOR must be supplied\n",
-			  __func__);
+		drm_err(bridge->dev,
+			"%s: The flag DRM_BRIDGE_ATTACH_NO_CONNECTOR must be supplied\n",
+			__func__);
 		return -EINVAL;
 	}
 
