018b2ad143caed5734444092f3c2e69a560b7799
Daniel Palmer <daniel@thingy.jp>
2026-04-17 20:26:21 +0900

    tools/nolibc: fcntl: Add fallocate()
    Add fallocate(). Some special care is needed to put the
    offset and size into the syscall parameters for 32bit
    machines, x32, and mipsn32.
    
    For x32 we can just check if the kernel long size is the
    same as off_t and use the same path as x86_64.
    
    For mipsn32 we override the generic version and provide
    one that does the right thing.
    
    Signed-off-by: Daniel Palmer <daniel@thingy.jp>

--- tools/include/nolibc/fcntl.h
+++ tools/include/nolibc/fcntl.h
@@ -14,6 +14,9 @@
 #include "types.h"
 #include "sys.h"
 
+/* For fallocate() modes */
+#include <linux/falloc.h>
+
 /*
  * int openat(int dirfd, const char *path, int flags[, mode_t mode]);
  */
@@ -64,6 +67,35 @@ int open(const char *path, int flags, ...)
 	}
 
 	return __sysret(_sys_open(path, flags, mode));
+}
+
+/*
+ * int fallocate(int fd, int mode, off_t offset, off_t size);
+ */
+
+#if !defined(_sys_fallocate)
+static __attribute__((unused))
+int _sys_fallocate(int fd, int mode, off_t offset, off_t size)
+{
+	/*
+	 * For 32 bit machines __kernel_long_t will be 4, off_t will be 8
+	 * and we need to split it for 64 machines we can use the values as-is
+	 */
+	const bool offsetsz_two_args = sizeof(__kernel_long_t) != sizeof(off_t);
+
+	if (offsetsz_two_args)
+		return __nolibc_syscall6(__NR_fallocate, fd, mode,
+			__NOLIBC_LLARGPART(offset, 0), __NOLIBC_LLARGPART(offset, 1),
+			__NOLIBC_LLARGPART(size, 0), __NOLIBC_LLARGPART(size, 1));
+	else
+		return __nolibc_syscall4(__NR_fallocate, fd, mode, offset, size);
+}
+#endif
+
+static __attribute__((unused))
+int fallocate(int fd, int mode, off_t offset, off_t size)
+{
+	return __sysret(_sys_fallocate(fd, mode, offset, size));
 }
 
 #endif /* _NOLIBC_FCNTL_H */
