35c68f62e7575763806c7eab03ca132ba143df64
Myeonghun Pak <mhun512@gmail.com>
2026-04-20 15:34:08 +0900

    drm/panel: ili9881c: remove panel on DSI attach failure
    The probe path calls drm_panel_add() before mipi_dsi_attach().
    
    If mipi_dsi_attach() fails, the driver returns without calling
    
    drm_panel_remove(), leaving the panel registered in the global DRM
    panel list. The driver's .remove() callback does not handle this case
    because failed probe does not transition through the normal bound-device
    removal path.
    
    Fix this by unwinding drm_panel_add() when mipi_dsi_attach() fails.
    
    Co-developed-by: Ijae Kim <ae878000@gmail.com>
    Signed-off-by: Ijae Kim <ae878000@gmail.com>
    Co-developed-by: Taegyu Kim <tmk5904@psu.edu>
    Signed-off-by: Taegyu Kim <tmk5904@psu.edu>
    Co-developed-by: Yuho Choi <dbgh9129@gmail.com>
    Signed-off-by: Yuho Choi <dbgh9129@gmail.com>
    Signed-off-by: Myeonghun Pak <mhun512@gmail.com>

--- drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
+++ drivers/gpu/drm/panel/panel-ilitek-ili9881c.c
@@ -2359,7 +2359,13 @@ static int ili9881c_dsi_probe(struct mipi_dsi_device *dsi)
 	dsi->format = MIPI_DSI_FMT_RGB888;
 	dsi->lanes = ctx->desc->lanes;
 
-	return mipi_dsi_attach(dsi);
+	ret = mipi_dsi_attach(dsi);
+	if (ret) {
+		drm_panel_remove(&ctx->panel);
+		return ret;
+	}
+
+	return 0;
 }
 
 static void ili9881c_dsi_remove(struct mipi_dsi_device *dsi)
