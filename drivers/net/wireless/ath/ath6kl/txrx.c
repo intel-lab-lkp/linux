5f2e49b42cb31446a71aeceb40c450ef3b244e38
Tristan Madani <tristan@talencesecurity.com>
2026-04-21 13:50:07 +0000

    wifi: ath6kl: fix OOB access from firmware ADDBA window size
    aggr_recv_addba_req_evt() logs a debug message when the firmware-supplied
    win_sz is outside [AGGR_WIN_SZ_MIN, AGGR_WIN_SZ_MAX] but does not
    return. The out-of-range win_sz is then used in TID_WINDOW_SZ() to
    compute a kzalloc size and stored in rxtid->hold_q_sz, leading to
    zero-size or overflowed allocations and subsequent OOB access.
    
    Return early when win_sz is out of the valid range.
    
    Fixes: bdcd81707973 ("Add ath6kl cleaned up driver")
    Signed-off-by: Tristan Madani <tristan@talencesecurity.com>

--- drivers/net/wireless/ath/ath6kl/txrx.c
+++ drivers/net/wireless/ath/ath6kl/txrx.c
@@ -1723,9 +1723,11 @@ void aggr_recv_addba_req_evt(struct ath6kl_vif *vif, u8 tid_mux, u16 seq_no,
 
 	rxtid = &aggr_conn->rx_tid[tid];
 
-	if (win_sz < AGGR_WIN_SZ_MIN || win_sz > AGGR_WIN_SZ_MAX)
+	if (win_sz < AGGR_WIN_SZ_MIN || win_sz > AGGR_WIN_SZ_MAX) {
 		ath6kl_dbg(ATH6KL_DBG_WLAN_RX, "%s: win_sz %d, tid %d\n",
 			   __func__, win_sz, tid);
+		return;
+	}
 
 	if (rxtid->aggr)
 		aggr_delete_tid_state(aggr_conn, tid);
