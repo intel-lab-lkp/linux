86789bed1f2978c85d57f0921dcfa676ded6c43b
Pablo Neira Ayuso <pablo@netfilter.org>
2026-04-21 20:16:50 +0200

    netfilter: replace skb_try_make_writable() by skb_ensure_writable()
    skb_try_make_writable() only works on clones and uncloned packets might
    have their network header in paged fragments.
    
    Fixes: d32de98ea70f ("netfilter: nft_fwd_netdev: allow to forward packets via neighbour layer")
    Fixes: 7d2086871762 ("netfilter: nf_flow_table: move ipv4 offload hook code to nf_flow_table")
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nft_fwd_netdev.c
+++ net/netfilter/nft_fwd_netdev.c
@@ -111,7 +111,7 @@ static void nft_fwd_neigh_eval(const struct nft_expr *expr,
 			verdict = NFT_BREAK;
 			goto out;
 		}
-		if (skb_try_make_writable(skb, sizeof(*iph))) {
+		if (skb_ensure_writable(skb, sizeof(*iph))) {
 			verdict = NF_DROP;
 			goto out;
 		}
@@ -132,7 +132,7 @@ static void nft_fwd_neigh_eval(const struct nft_expr *expr,
 			verdict = NFT_BREAK;
 			goto out;
 		}
-		if (skb_try_make_writable(skb, sizeof(*ip6h))) {
+		if (skb_ensure_writable(skb, sizeof(*ip6h))) {
 			verdict = NF_DROP;
 			goto out;
 		}
