22437e519d7617739822086af2506f4ede266bc7
Dibin Moolakadan Subrahmanian <dibin.moolakadan.subrahmanian@intel.com>
2026-04-22 21:56:13 +0530

    drm/i915/display: Add DC3CO support check and validate target DC state
    Validate the requested target DC state against allowed_dc_mask in
    intel_display_power_set_target_dc_state() to avoid programming
    unsupported DC states.
    
    Also add intel_display_power_dc3co_supported() helper to query DC3CO
    support from allowed_dc_mask.
    
    Changes in v2:
    - Squash "Add helper to check DC3CO support" patch into this patch
    
    Signed-off-by: Dibin Moolakadan Subrahmanian <dibin.moolakadan.subrahmanian@intel.com>
    Reviewed-by: Uma Shankar <uma.shankar@intel.com>

--- drivers/gpu/drm/i915/display/intel_display_power.h
+++ drivers/gpu/drm/i915/display/intel_display_power.h
@@ -186,6 +186,7 @@ void intel_display_power_resume(struct intel_display *display);
 void intel_display_power_set_target_dc_state(struct intel_display *display,
 					     u32 state);
 u32 intel_display_power_get_current_dc_state(struct intel_display *display);
+bool intel_display_power_dc3co_supported(struct intel_display *display);
 
 bool intel_display_power_is_enabled(struct intel_display *display,
 				    enum intel_display_power_domain domain);
