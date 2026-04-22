a4d1130e2fcd94b0a4b80aa6b4498694d05b0db5
Giorgi Tchankvetadze <giorgitchankvetadze1997@gmail.com>
2026-04-22 16:09:39 +0400

    staging: media: atomisp: gc: Fix function declaration formatting
    Move function return types to the same line as the function names and
    replace leading spaces in parameter lists with tabs.
    
    No functional change.
    
    Signed-off-by: Giorgi Tchankvetadze <giorgitchankvetadze1997@gmail.com>

--- drivers/staging/media/atomisp/pci/isp/kernels/gc/gc_1.0/ia_css_gc.host.c
+++ drivers/staging/media/atomisp/pci/isp/kernels/gc/gc_1.0/ia_css_gc.host.c
@@ -25,11 +25,9 @@ const struct ia_css_ce_config default_ce_config = {
 	255
 };
 
-void
-ia_css_gc_encode(
-    struct sh_css_isp_gc_params *to,
-    const struct ia_css_gc_config *from,
-    unsigned int size)
+void ia_css_gc_encode(struct sh_css_isp_gc_params *to,
+		      const struct ia_css_gc_config *from,
+		      unsigned int size)
 {
 	(void)size;
 	to->gain_k1 =
@@ -40,32 +38,26 @@ ia_css_gc_encode(
 			   IA_CSS_GAMMA_GAIN_K_SHIFT);
 }
 
-void
-ia_css_ce_encode(
-    struct sh_css_isp_ce_params *to,
-    const struct ia_css_ce_config *from,
-    unsigned int size)
+void ia_css_ce_encode(struct sh_css_isp_ce_params *to,
+		      const struct ia_css_ce_config *from,
+		      unsigned int size)
 {
 	(void)size;
 	to->uv_level_min = from->uv_level_min;
 	to->uv_level_max = from->uv_level_max;
 }
 
-void
-ia_css_gc_vamem_encode(
-    struct sh_css_isp_gc_vamem_params *to,
-    const struct ia_css_gamma_table *from,
-    unsigned int size)
+void ia_css_gc_vamem_encode(struct sh_css_isp_gc_vamem_params *to,
+			    const struct ia_css_gamma_table *from,
+			    unsigned int size)
 {
 	(void)size;
 	memcpy(&to->gc,  &from->data, sizeof(to->gc));
 }
 
 #ifndef IA_CSS_NO_DEBUG
-void
-ia_css_gc_dump(
-    const struct sh_css_isp_gc_params *gc,
-    unsigned int level)
+void ia_css_gc_dump(const struct sh_css_isp_gc_params *gc,
+		    unsigned int level)
 {
 	if (!gc) return;
 	ia_css_debug_dtrace(level, "Gamma Correction:\n");
@@ -75,10 +67,8 @@ ia_css_gc_dump(
 			    "gamma_gain_k2", gc->gain_k2);
 }
 
-void
-ia_css_ce_dump(
-    const struct sh_css_isp_ce_params *ce,
-    unsigned int level)
+void ia_css_ce_dump(const struct sh_css_isp_ce_params *ce,
+		    unsigned int level)
 {
 	ia_css_debug_dtrace(level, "Chroma Enhancement:\n");
 	ia_css_debug_dtrace(level, "\t%-32s = %d\n",
@@ -87,20 +77,16 @@ ia_css_ce_dump(
 			    "ce_uv_level_max", ce->uv_level_max);
 }
 
-void
-ia_css_gc_debug_dtrace(
-    const struct ia_css_gc_config *config,
-    unsigned int level)
+void ia_css_gc_debug_dtrace(const struct ia_css_gc_config *config,
+			    unsigned int level)
 {
 	ia_css_debug_dtrace(level,
 			    "config.gain_k1=%d, config.gain_k2=%d\n",
 			    config->gain_k1, config->gain_k2);
 }
 
-void
-ia_css_ce_debug_dtrace(
-    const struct ia_css_ce_config *config,
-    unsigned int level)
+void ia_css_ce_debug_dtrace(const struct ia_css_ce_config *config,
+			    unsigned int level)
 {
 	ia_css_debug_dtrace(level,
 			    "config.uv_level_min=%d, config.uv_level_max=%d\n",
