31dcaf48b613984759efd03acc6da735daa599d1
Pablo Neira Ayuso <pablo@netfilter.org>
2026-04-22 11:44:42 +0200

    netfilter: replace skb_try_make_writable() by skb_ensure_writable()
    skb_try_make_writable() only works on clones and uncloned packets might
    have their network header in paged fragments.
    
    nft_fwd needs to work for the ingress and egress hooks, but the egress
    hook where skb->data points to the mac header, use skb_network_offset()
    to deal include the mac header. The flowtable path only works in the
    ingress hook, it does not need this same handling as nft_fwd does.
    
    Fixes: d32de98ea70f ("netfilter: nft_fwd_netdev: allow to forward packets via neighbour layer")
    Fixes: 7d2086871762 ("netfilter: nf_flow_table: move ipv4 offload hook code to nf_flow_table")
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nft_fwd_netdev.c
+++ net/netfilter/nft_fwd_netdev.c
@@ -98,6 +98,7 @@ static void nft_fwd_neigh_eval(const struct nft_expr *expr,
 	struct nft_fwd_neigh *priv = nft_expr_priv(expr);
 	void *addr = &regs->data[priv->sreg_addr];
 	int oif = regs->data[priv->sreg_dev];
+	int nhoff = skb_network_offset(skb);
 	unsigned int verdict = NF_STOLEN;
 	struct sk_buff *skb = pkt->skb;
 	struct net_device *dev;
@@ -111,7 +112,7 @@ static void nft_fwd_neigh_eval(const struct nft_expr *expr,
 			verdict = NFT_BREAK;
 			goto out;
 		}
-		if (skb_try_make_writable(skb, sizeof(*iph))) {
+		if (skb_ensure_writable(skb, nhoff + sizeof(*iph))) {
 			verdict = NF_DROP;
 			goto out;
 		}
@@ -132,7 +133,7 @@ static void nft_fwd_neigh_eval(const struct nft_expr *expr,
 			verdict = NFT_BREAK;
 			goto out;
 		}
-		if (skb_try_make_writable(skb, sizeof(*ip6h))) {
+		if (skb_ensure_writable(skb, nhoff + sizeof(*ip6h))) {
 			verdict = NF_DROP;
 			goto out;
 		}
