1f403d6e8185e7a3d089e03eedc57a3a175d8714
Rodrigo Alencar <rodrigo.alencar@analog.com>
2026-04-22 15:45:54 +0100

    iio: dac: ad5686: implement new sync() op for the spi bus
    Use of local SPI bus data to manage a collection of SPI transfers and
    flush them to the SPI platform driver with the sync() operation. This
    allows for faster handling of multiple channel DAC writes, avoiding kernel
    overhead per spi_sync() call, which will be helpful when enabling
    triggered buffer support.
    
    Signed-off-by: Rodrigo Alencar <rodrigo.alencar@analog.com>

--- drivers/iio/dac/ad5686.c
+++ drivers/iio/dac/ad5686.c
@@ -508,7 +508,8 @@ EXPORT_SYMBOL_NS_GPL(ad5679r_chip_info, "IIO_AD5686");
 
 int ad5686_probe(struct device *dev,
 		 const struct ad5686_chip_info *chip_info,
-		 const char *name, const struct ad5686_bus_ops *ops)
+		 const char *name, const struct ad5686_bus_ops *ops,
+		 void *bus_data)
 {
 	struct reset_control *rstc;
 	struct iio_dev *indio_dev;
@@ -523,6 +524,7 @@ int ad5686_probe(struct device *dev,
 
 	st->dev = dev;
 	st->ops = ops;
+	st->bus_data = bus_data;
 	st->chip_info = chip_info;
 
 	ret = devm_regulator_get_enable_optional(dev, "vdd");
