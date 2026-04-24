61f76e4692e4bb4a016a69520fffa9491709ca30
Ed Tsai <ed.tsai@mediatek.com>
2026-04-24 14:35:55 +0800

    Revert "scsi: ufs: Use pre-calculated offsets in ufshcd_init_lrb()"
    This reverts commit d5130c5a093257aa4542aaded8034ef116a7624a.
    
    The offsets stored in utp_transfer_req_desc are in double words on
    hosts without UFSHCD_QUIRK_PRDT_BYTE_GRAN, using them directly to
    compute ucd_rsp_dma_addr and ucd_prdt_dma_addr results in incorrect
    DMA addresses. The manual offsetof(struct utp_transfer_cmd_desc, ...)
    calculations always yield correct byte offsets regardless of the quirk
    and should be used instead.
    
    Note that these DMA addresses are only used in ufshcd_print_tr() for
    error logging, so the impact is limited to misleading error logs.
    
    Fixes: d5130c5a0932 ("scsi: ufs: Use pre-calculated offsets in ufshcd_init_lrb()")
    Signed-off-by: Ed Tsai <ed.tsai@mediatek.com>

--- drivers/ufs/core/ufshcd.c
+++ drivers/ufs/core/ufshcd.c
@@ -2971,8 +2971,9 @@ static void ufshcd_init_lrb(struct ufs_hba *hba, struct scsi_cmnd *cmd)
 	struct utp_transfer_req_desc *utrdlp = hba->utrdl_base_addr;
 	dma_addr_t cmd_desc_element_addr =
 		hba->ucdl_dma_addr + i * ufshcd_get_ucd_size(hba);
-	u16 response_offset = le16_to_cpu(utrdlp[i].response_upiu_offset);
-	u16 prdt_offset = le16_to_cpu(utrdlp[i].prd_table_offset);
+	u16 response_offset = offsetof(struct utp_transfer_cmd_desc,
+				       response_upiu);
+	u16 prdt_offset = offsetof(struct utp_transfer_cmd_desc, prd_table);
 	struct ufshcd_lrb *lrb = scsi_cmd_priv(cmd);
 
 	lrb->utr_descriptor_ptr = utrdlp + i;
