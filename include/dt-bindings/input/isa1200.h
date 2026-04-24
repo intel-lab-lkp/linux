0e30a9ac9b4cf5924d48c6554fb1242b70f67fab
Svyatoslav Ryhel <clamor95@gmail.com>
2026-04-24 10:13:04 +0300

    dt-bindings: input: Document Imagis ISA1200 haptic motor driver
    Document the Imagis ISA1200 haptic motor driver, used primarily in mobile
    handheld devices and capable of supporting up to two motors.
    
    The exact datasheet for the ISA1200 is not available; all data was modeled
    based on available downstream kernel sources for various devices and
    fragments of information scattered across the internet.
    
    Signed-off-by: Svyatoslav Ryhel <clamor95@gmail.com>

---
+++ include/dt-bindings/input/isa1200.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
+
+#ifndef _DT_BINDINGS_ISA1200_H
+#define _DT_BINDINGS_ISA1200_H
+
+/* Linked actuator type */
+#define ISA1200_LRA_MODE		0
+#define ISA1200_ERM_MODE		1
+
+/* External clock divider */
+#define ISA1200_CKLDIV_128		0
+#define ISA1200_CKLDIV_256		1
+#define ISA1200_CKLDIV_512		2
+#define ISA1200_CKLDIV_1024		3
+
+#endif
