96e74358cb3dc44542deb016044c47cd04104dc9
Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>
2026-04-24 11:11:27 +0530

    drm/i915/display: Copy color pipeline from plane in the primary joiner pipe
    When copying plane color state in a joiner configuration, use the plane in
    the primary joiner pipe since it carries the pipeline number selected by
    the user-space.
    
    This assumes that all pipes in the joiner are symmetric in their plane
    color capabilities.
    
    Cc: stable@vger.kernel.org # v6.19+
    Fixes: a78f1b6baf4d ("drm/i915/color: Add framework to program CSC")
    Signed-off-by: Chaitanya Kumar Borah <chaitanya.kumar.borah@intel.com>

--- drivers/gpu/drm/i915/display/intel_plane.c
+++ drivers/gpu/drm/i915/display/intel_plane.c
@@ -375,7 +375,7 @@ intel_plane_color_copy_uapi_to_hw_state(struct intel_atomic_state *state,
 	if (!state)
 		return;
 
-	iter_colorop = plane_state->uapi.color_pipeline;
+	iter_colorop = from_plane_state->uapi.color_pipeline;
 
 	while (iter_colorop) {
 		for_each_new_colorop_in_state(&state->base, colorop, new_colorop_state, i) {
