10962a1249bfc91afe4995e20d9d400ab98c8dda
Tomasz Unger <tomasz.unger@yahoo.pl>
2026-04-25 12:15:08 +0200

    staging: media: atomisp: fix typo "commmit" in comments
    Fix a repeated typo "commmit" -> "commit" in generated code comments
    across five files. Found with codespell.
    
    Signed-off-by: Tomasz Unger <tomasz.unger@yahoo.pl>

--- drivers/staging/media/atomisp/pci/ia_css_isp_configs.c
+++ drivers/staging/media/atomisp/pci/ia_css_isp_configs.c
@@ -4,7 +4,7 @@
  * Copyright (c) 2015, Intel Corporation.
  */
 
-/* Generated code: do not edit or commmit. */
+/* Generated code: do not edit or commit. */
 
 #define IA_CSS_INCLUDE_CONFIGURATIONS
 #include "ia_css_pipeline.h"
