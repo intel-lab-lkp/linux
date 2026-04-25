7bc1cc9765c04a78cb40393156a774e8ce8aca8b
Haichen Feng <2806891994@qq.com>
2026-04-25 19:07:21 +0800

    platform/x86: hp-wmi: Add support for Victus 16-r0xxx (8BC2)
    The HP Victus 16-r0xxx (board ID: 8BC2) has the same WMI as other Victus
    S boards, but requires quirks for correctly switching thermal
    profile.
    
    Add the DMI board name to victus_s_thermal_profile_boards[] table and
    map it to omen_v1_thermal_params.
    
    Testing on board 8BC2 confirmed that platform profile is registered
    successfully and fan RPMs are readable and controllable.
    
    Signed-off-by: Haichen Feng <2806891994@qq.com>

--- drivers/platform/x86/hp/hp-wmi.c
+++ drivers/platform/x86/hp/hp-wmi.c
@@ -214,6 +214,10 @@ static const struct dmi_system_id victus_s_thermal_profile_boards[] __initconst 
 		.driver_data = (void *)&omen_v1_thermal_params,
 	},
 	{
+		.matches = {DMI_MATCH(DMI_BOARD_NAME, "8BC2")},
+		.driver_data = (void *)&omen_v1_thermal_params,
+	},
+	{
 		.matches = { DMI_MATCH(DMI_BOARD_NAME, "8BD4") },
 		.driver_data = (void *)&victus_s_thermal_params,
 	},
