fadce49d57a0e711cade19a17b55a3f9e2105eff
Pengyu Luo <mitltlatltl@gmail.com>
2026-04-25 18:52:57 +0800

    media: hi846: fix hi846_write_reg_16 handling
    hi846_write_reg_16() does not clear a positive *err value on success.
    pm_runtime_get_if_in_use() returns a positive value when the device
    is already in use. When hi846_set_ctrl() passes &ret holding this
    positive value) to hi846_write_reg_16(), the function returns with ret
    as is, the positive value propagates back as a return code, which
    callers interpret as an error.
    
    Fix this by resetting *err to 0 only when it is positive.
    
    Fixes: 04fc06f6dc15 ("media: hi846: fix usage of pm_runtime_get_if_in_use()")
    Signed-off-by: Pengyu Luo <mitltlatltl@gmail.com>

--- drivers/media/i2c/hi846.c
+++ drivers/media/i2c/hi846.c
@@ -1270,6 +1270,8 @@ static void hi846_write_reg_16(struct hi846 *hi846, u16 reg, u16 val, int *err)
 
 	if (*err < 0)
 		return;
+	else
+		*err = 0;
 
 	put_unaligned_be16(reg, buf);
 	put_unaligned_be16(val, buf + 2);
