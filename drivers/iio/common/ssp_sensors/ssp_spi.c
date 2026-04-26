1aa8a6489081780fd5f57e3001d2eff4054375f4
Sanjay Chitroda <sanjayembeddedse@gmail.com>
2026-04-26 14:47:10 +0530

    iio: ssp_sensors: reuse embedded RX buffer for SPI transfers
    Avoid allocating a temporary DMA buffer in the interrupt context when
    handling hub-to-AP and AP-to-hub SPI write messages.
    
    Replace the dynamically allocated RX buffer with a fixed-size,
    preallocated buffer embedded in the driver structure and reused for all
    SPI receive operations. This removes memory allocation from the
    IRQ path, simplifies lifetime management.
    
    Signed-off-by: Sanjay Chitroda <sanjayembeddedse@gmail.com>

--- drivers/iio/common/ssp_sensors/ssp_spi.c
+++ drivers/iio/common/ssp_sensors/ssp_spi.c
@@ -339,7 +339,7 @@ static int ssp_parse_dataframe(struct ssp_data *data, char *dataframe, int len)
 /* threaded irq */
 int ssp_irq_msg(struct ssp_data *data)
 {
-	char *buffer;
+	char *buffer = data->rx_buf;
 	u8 msg_type;
 	int ret;
 	u16 length, msg_options;
@@ -383,18 +383,11 @@ int ssp_irq_msg(struct ssp_data *data)
 			 * but the slave should not send such ones - it is to
 			 * check but let's handle this
 			 */
-			buffer = kmalloc(length, GFP_KERNEL | GFP_DMA);
-			if (!buffer) {
-				ret = -ENOMEM;
-				goto _unlock;
-			}
 
 			/* got dead packet so it is always an error */
 			ret = spi_read(data->spi, buffer, length);
 			if (ret >= 0)
 				ret = -EPROTO;
-
-			kfree(buffer);
 
 			dev_err(SSP_DEV, "No match error %x\n",
 				msg_options);
@@ -428,22 +421,13 @@ _unlock:
 		mutex_unlock(&data->pending_lock);
 		break;
 	case SSP_HUB2AP_WRITE:
-		buffer = kzalloc(length, GFP_KERNEL | GFP_DMA);
-		if (!buffer)
-			return -ENOMEM;
-
 		ret = spi_read(data->spi, buffer, length);
 		if (ret < 0) {
 			dev_err(SSP_DEV, "spi read fail\n");
-			kfree(buffer);
 			break;
 		}
 
-		ret = ssp_parse_dataframe(data, buffer, length);
-
-		kfree(buffer);
-		break;
-
+		return ssp_parse_dataframe(data, buffer, length);
 	default:
 		dev_err(SSP_DEV, "unknown msg type\n");
 		return -EPROTO;
