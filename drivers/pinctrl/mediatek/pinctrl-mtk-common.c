c8b837e9a90a49c360812a387871bec6e2f7ce43
Chen-Yu Tsai <wenst@chromium.org>
2026-04-27 14:17:17 +0800

    pinctrl: mediatek: common-v1: Directly modify registers to set GPIO direction
    pinctrl_gpio_direction_input() / pinctrl_gpio_direction_output() take
    the pinctrl mutex. This causes a gpiochip operations to need to sleep.
    Worse yet, the .can_sleep field in the gpiochip is not set. This causes
    the shared GPIO proxy to trip over, as it uses gpiod_cansleep() to check
    whether it can use a spinlock or needs a mutex. In this case, it ends
    up taking a spinlock, then calls pinctrl_gpio_direction_output(), which
    takes a mutex. This causes a huge warning.
    
    Since the Mediatek hardware has separate clear/set registers, there is
    no risk of clobbering other bits like with a read-modify-write pattern.
    Switch to directly setting the GPIO direction register bits to avoid
    the mutex.
    
    Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>

--- drivers/pinctrl/mediatek/pinctrl-mtk-common.c
+++ drivers/pinctrl/mediatek/pinctrl-mtk-common.c
@@ -806,16 +806,24 @@ static const struct pinmux_ops mtk_pmx_ops = {
 	.gpio_request_enable	= mtk_pmx_gpio_request_enable,
 };
 
+static int mtk_gpio_direction_input(struct gpio_chip *chip, unsigned offset)
+{
+	struct mtk_pinctrl *pctl = gpiochip_get_data(chip);
+
+	return mtk_pmx_gpio_set_direction(pctl->pctl_dev, NULL, offset, true);
+}
+
 static int mtk_gpio_direction_output(struct gpio_chip *chip,
 					unsigned offset, int value)
 {
+	struct mtk_pinctrl *pctl = gpiochip_get_data(chip);
 	int ret;
 
 	ret = mtk_gpio_set(chip, offset, value);
 	if (ret)
 		return ret;
 
-	return pinctrl_gpio_direction_output(chip, offset);
+	return mtk_pmx_gpio_set_direction(pctl->pctl_dev, NULL, offset, true);
 }
 
 static int mtk_gpio_get_direction(struct gpio_chip *chip, unsigned offset)
@@ -895,7 +903,7 @@ static const struct gpio_chip mtk_gpio_chip = {
 	.request		= gpiochip_generic_request,
 	.free			= gpiochip_generic_free,
 	.get_direction		= mtk_gpio_get_direction,
-	.direction_input	= pinctrl_gpio_direction_input,
+	.direction_input	= mtk_gpio_direction_input,
 	.direction_output	= mtk_gpio_direction_output,
 	.get			= mtk_gpio_get,
 	.set			= mtk_gpio_set,
