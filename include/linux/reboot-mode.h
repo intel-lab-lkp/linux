22edf2c6a5af1917b55d72011205a66efb11c376
Shivendra Pratap <shivendra.pratap@oss.qualcomm.com>
2026-04-27 23:04:42 +0530

    power: reset: reboot-mode: Add support for 64 bit magic
    Current reboot-mode supports a single 32-bit argument for any
    supported mode. Some reboot-mode based drivers may require
    passing two independent 32-bit arguments during a reboot
    sequence, for uses-cases, where a mode requires an additional
    argument. Such drivers may not be able to use the reboot-mode
    driver. For example, ARM PSCI vendor-specific resets, need two
    arguments for its operation – reset_type and cookie, to complete
    the reset operation. If a driver wants to implement this
    firmware-based reset, it cannot use reboot-mode framework.
    
    Introduce 64-bit magic values in reboot-mode driver to
    accommodate up-to two 32-bit arguments in below format.
    |    Higher 32 bit  |   Lower 32 bit    |
    |	 arg2	    | 	    arg1	|
    
    Update current reboot-mode drivers for 64-bit magic.
    
    Reviewed-by: Sebastian Reichel <sebastian.reichel@collabora.com>
    Signed-off-by: Shivendra Pratap <shivendra.pratap@oss.qualcomm.com>

--- include/linux/reboot-mode.h
+++ include/linux/reboot-mode.h
@@ -2,10 +2,23 @@
 #ifndef __REBOOT_MODE_H__
 #define __REBOOT_MODE_H__
 
+#include <linux/bitfield.h>
+#include <linux/bits.h>
+#include <linux/types.h>
+
+/* Construct 64-bit reboot magic: arg2 in upper 32 bits, arg1 in lower 32 */
+#define REBOOT_MODE_MAGIC(arg1, arg2) \
+	(FIELD_PREP(GENMASK_ULL(31, 0), (arg1)) | \
+	 FIELD_PREP(GENMASK_ULL(63, 32), (arg2)))
+/* Get 32 bit arg1 from 64 bit magic */
+#define REBOOT_MODE_ARG1(magic) FIELD_GET(GENMASK_ULL(31, 0), magic)
+/* Get 32 bit arg2 from 64 bit magic */
+#define REBOOT_MODE_ARG2(magic) FIELD_GET(GENMASK_ULL(63, 32), magic)
+
 struct reboot_mode_driver {
 	struct device *dev;
 	struct list_head head;
-	int (*write)(struct reboot_mode_driver *reboot, unsigned int magic);
+	int (*write)(struct reboot_mode_driver *reboot, u64 magic);
 	struct notifier_block reboot_notifier;
 };
 
