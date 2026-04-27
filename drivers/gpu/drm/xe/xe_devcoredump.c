0e3d5a7cd8938f810ad8cacaf1e3151d00499d9a
Uwe Kleine-König (The Capable Hub) <u.kleine-koenig@baylibre.com>
2026-04-27 18:09:03 +0200

    drm/xe: Don't emit UTS_RELEASE as part of device coredump
    UTS_RELEASE changes quite easily (e.g. uncommitted changes in the source
    tree or new commits). So when checking if a patch introduces changes to the
    resulting binary each usage of UTS_RELEASE is a source of annoyance as
    it introduces changes not caused by the change under test.
    
    It's little helpful to emit the kernel version in the device coredump, so
    drop the kernel version line from it.
    
    Signed-off-by: Uwe Kleine-König (The Capable Hub) <u.kleine-koenig@baylibre.com>

--- drivers/gpu/drm/xe/xe_devcoredump.c
+++ drivers/gpu/drm/xe/xe_devcoredump.c
@@ -101,7 +101,6 @@ static ssize_t __xe_devcoredump_read(char *buffer, ssize_t count,
 
 	drm_puts(&p, "**** Xe Device Coredump ****\n");
 	drm_printf(&p, "Reason: %s\n", ss->reason);
-	drm_puts(&p, "kernel: " UTS_RELEASE "\n");
 	drm_puts(&p, "module: " KBUILD_MODNAME "\n");
 
 	ts = ktime_to_timespec64(ss->snapshot_time);
