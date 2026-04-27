2885ffbf4a15b0a4caff40e5b19b900cc2dd354c
Shivendra Pratap <shivendra.pratap@oss.qualcomm.com>
2026-04-27 23:04:49 +0530

    mfd: psci-mfd: Add psci-reboot-mode child cell
    The PSCI "reboot-mode" node does not define a compatible because it is a
    configuration of boot-states provided by the underlying firmware. With
    the new firmware-node based cells in mfd-core, this node can now be
    exposed as a proper child cell.
    
    Add the psci-reboot-mode child cell to the psci-mfd driver and associate
    the reboot-mode node as its firmware-node.
    
    Signed-off-by: Shivendra Pratap <shivendra.pratap@oss.qualcomm.com>

--- drivers/mfd/psci-mfd.c
+++ drivers/mfd/psci-mfd.c
@@ -7,17 +7,36 @@
 #include <linux/module.h>
 #include <linux/of.h>
 #include <linux/platform_device.h>
+#include <linux/string.h>
+
+#define PSCI_REBOOT_MODE_CELL_IDX 1
 
 static const struct mfd_cell psci_cells[] = {
 	{
 		.name = "psci-cpuidle-domain",
 	},
+	{
+		.name = "psci-reboot-mode",
+	},
 };
 
 static int psci_mfd_probe(struct platform_device *pdev)
 {
-	return devm_mfd_add_devices(&pdev->dev, PLATFORM_DEVID_AUTO, psci_cells,
-				   ARRAY_SIZE(psci_cells), NULL, 0, NULL);
+	struct mfd_cell cells[ARRAY_SIZE(psci_cells)];
+	struct device_node *np = NULL;
+	int ret;
+
+	memcpy(cells, psci_cells, sizeof(cells));
+
+	if (pdev->dev.of_node)
+		np = of_get_child_by_name(pdev->dev.of_node, "reboot-mode");
+	cells[PSCI_REBOOT_MODE_CELL_IDX].fwnode = of_fwnode_handle(np);
+
+	ret = devm_mfd_add_devices(&pdev->dev, PLATFORM_DEVID_AUTO, cells,
+				   ARRAY_SIZE(cells), NULL, 0, NULL);
+	of_node_put(np);
+
+	return ret;
 }
 
 static const struct of_device_id psci_mfd_of_match[] = {
