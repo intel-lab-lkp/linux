3a3c046e8720df05cd176c59c6af2338973f6d56
Furkan Caliskan <frn1furkan10@gmail.com>
2026-04-27 12:20:30 +0300

    sched/deadline: Fix stale comment above yield_task_dl()
    The comment above yield_task_dl() states that bandwidth reclaiming is
    "planned for the future". This is now obsolete as GRUB-PA has been
    implemented.
    
    Update the comment to reflect that yielded bandwidth is now actively
    reclaimed for power saving and scheduling efficiency.
    
    Signed-off-by: Furkan Caliskan <frn1furkan10@gmail.com>

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -2368,14 +2368,11 @@ static bool dequeue_task_dl(struct rq *rq, struct task_struct *p, int flags)
 }
 
 /*
- * Yield task semantic for -deadline tasks is:
+ * Get off from the CPU until our next instance
  *
- *   get off from the CPU until our next instance, with
- *   a new runtime. This is of little use now, since we
- *   don't have a bandwidth reclaiming mechanism. Anyway,
- *   bandwidth reclaiming is planned for the future, and
- *   yield_task_dl will indicate that some spare budget
- *   is available for other task instances to use it.
+ * Yielding indicates that the current instance has finished its work 
+ * early. This allows the remaining budget to be reclaimed by other
+ * tasks or used to reduce CPU frequency via the GRUB-PA mechanism. 
  */
 static void yield_task_dl(struct rq *rq)
 {
