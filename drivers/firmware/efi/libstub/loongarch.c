6d629b57341fbe7f7c52bfc86d6e77333e25650e
WANG Rui <r@hev.cc>
2026-04-28 12:01:58 +0800

    efi/loongarch: Randomize kernel preferred address for KASLR
    Introduce efi_get_kimg_kaslr_address() to compute the preferred
    kernel image address dynamically when CONFIG_RANDOMIZE_BASE is
    enabled. The function derives a random offset using EFI-provided
    randomness combined with the timer value, and constrains it within
    CONFIG_RANDOMIZE_BASE_MAX_OFFSET.
    
    Update EFI_KIMG_PREFERRED_ADDRESS to call this helper so that the
    EFI stub can select a randomized load address when KASLR is active,
    while preserving the original base address behavior when KASLR is
    disabled or nokaslr is specified.
    
    Signed-off-by: WANG Rui <r@hev.cc>

--- drivers/firmware/efi/libstub/loongarch.c
+++ drivers/firmware/efi/libstub/loongarch.c
@@ -38,6 +38,22 @@ static efi_status_t exit_boot_func(struct efi_boot_memmap *map, void *priv)
 	return EFI_SUCCESS;
 }
 
+unsigned long efi_get_kimg_kaslr_address(void)
+{
+	unsigned int random_offset = 0;
+
+#ifdef CONFIG_RANDOMIZE_BASE
+	if (!efi_nokaslr) {
+		efi_get_random_bytes(sizeof(random_offset), (u8 *)&random_offset);
+		random_offset ^= (random_get_entropy() << 16);
+		random_offset &= (CONFIG_RANDOMIZE_BASE_MAX_OFFSET - SZ_64K - 1);
+		random_offset = ALIGN(random_offset, SZ_64K) + SZ_64K;
+	}
+#endif
+
+	return PHYSADDR(VMLINUX_LOAD_ADDRESS) + random_offset;
+}
+
 unsigned long __weak kernel_entry_address(unsigned long kernel_addr,
 		efi_loaded_image_t *image)
 {
