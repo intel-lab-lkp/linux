d2b412684c0ac99c48ebd451b0e5ccbdc8586018
Maxwell Doose <m32285159@gmail.com>
2026-04-28 07:46:42 -0500

    iio: magnetometer: rm3100: Use scoped_guard() in rm3100_get_samp_freq()
    Replace mutex_lock() and mutex_unlock() calls in rm3100_get_samp_freq()
    with the more modern scoped_guard(). This will help modernize the
    driver and bring it up-to-date with modern available macros/functions.
    
    Signed-off-by: Maxwell Doose <m32285159@gmail.com>

--- drivers/iio/magnetometer/rm3100-core.c
+++ drivers/iio/magnetometer/rm3100-core.c
@@ -280,11 +280,12 @@ static int rm3100_get_samp_freq(struct rm3100_data *data, int *val, int *val2)
 	unsigned int tmp;
 	int ret;
 
-	mutex_lock(&data->lock);
-	ret = regmap_read(data->regmap, RM3100_REG_TMRC, &tmp);
-	mutex_unlock(&data->lock);
-	if (ret < 0)
-		return ret;
+	scoped_guard(mutex, &data->lock) {
+		ret = regmap_read(data->regmap, RM3100_REG_TMRC, &tmp);
+
+		if (ret < 0)
+			return ret;
+	}
 	*val = rm3100_samp_rates[tmp - RM3100_TMRC_OFFSET][0];
 	*val2 = rm3100_samp_rates[tmp - RM3100_TMRC_OFFSET][1];
 
