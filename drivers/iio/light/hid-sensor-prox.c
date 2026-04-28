25424e180eaef7beff02d563e1be05dca91349e4
Sanjay Chitroda <sanjayembeddedse@gmail.com>
2026-04-28 12:46:10 +0530

    iio: light: adapt to hid_sensor_remove_trigger() API change
    Update the driver to match the updated hid_sensor_remove_trigger()
    prototype, which no longer requires struct iio_dev.
    
    Signed-off-by: Sanjay Chitroda <sanjayembeddedse@gmail.com>

--- drivers/iio/light/hid-sensor-prox.c
+++ drivers/iio/light/hid-sensor-prox.c
@@ -340,7 +340,7 @@ static int hid_prox_probe(struct platform_device *pdev)
 error_iio_unreg:
 	iio_device_unregister(indio_dev);
 error_remove_trigger:
-	hid_sensor_remove_trigger(indio_dev, &prox_state->common_attributes);
+	hid_sensor_remove_trigger(&prox_state->common_attributes);
 	return ret;
 }
 
@@ -353,7 +353,7 @@ static void hid_prox_remove(struct platform_device *pdev)
 
 	sensor_hub_remove_callback(hsdev, hsdev->usage);
 	iio_device_unregister(indio_dev);
-	hid_sensor_remove_trigger(indio_dev, &prox_state->common_attributes);
+	hid_sensor_remove_trigger(&prox_state->common_attributes);
 }
 
 static const struct platform_device_id hid_prox_ids[] = {
