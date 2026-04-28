cd9616f0c4afd4f45d3662d4ee4fc57b39b8c6c0
Stephen Smalley <stephen.smalley.work@gmail.com>
2026-04-28 15:21:20 -0400

    security,fs,nfs,net: update security_inode_listsecurity() interface
    Update the security_inode_listsecurity() interface to allow
    use of the xattr_list_one() helper and update the hook
    implementations.
    
    Link: https://lore.kernel.org/selinux/20250424152822.2719-1-stephen.smalley.work@gmail.com
    Signed-off-by: Stephen Smalley <stephen.smalley.work@gmail.com>
    [PM: forward porting to bring this patch up to v7.1-rc1+]
    Signed-off-by: Paul Moore <paul@paul-moore.com>

--- fs/xattr.c
+++ fs/xattr.c
@@ -492,9 +492,12 @@ vfs_listxattr(struct dentry *dentry, char *list, size_t size)
 	if (inode->i_op->listxattr) {
 		error = inode->i_op->listxattr(dentry, list, size);
 	} else {
-		error = security_inode_listsecurity(inode, list, size);
-		if (size && error > size)
-			error = -ERANGE;
+		ssize_t remaining = size;
+
+		error = security_inode_listsecurity(inode, &list, &remaining);
+		if (error)
+			return error;
+		error = size - remaining;
 	}
 	return error;
 }
@@ -1452,7 +1455,7 @@ ssize_t simple_xattr_list(struct inode *inode, struct simple_xattrs *xattrs,
 	if (err)
 		return err;
 
-	err = security_inode_listsecurity(inode, buffer, remaining_size);
+	err = security_inode_listsecurity(inode, &buffer, &remaining_size);
 	if (err < 0)
 		return err;
 
