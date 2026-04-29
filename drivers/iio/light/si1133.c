82873cebf184abcc55e8e22717eeeefc115cdfa0
Joshua Crofts <joshua.crofts1@gmail.com>
2026-04-29 17:04:54 +0200

    iio: light: si1133: add local variable for timeout
    Add local variable for timeout to improve readability.
    
    No functional change.
    
    Suggested-by: Jonathan Cameron <jic23@kernel.org>
    Signed-off-by: Joshua Crofts <joshua.crofts1@gmail.com>

--- drivers/iio/light/si1133.c
+++ drivers/iio/light/si1133.c
@@ -391,6 +391,7 @@ static int si1133_cmd_reset_counter(struct si1133_data *data)
 
 static int si1133_command(struct si1133_data *data, u8 cmd)
 {
+	unsigned long timeout = msecs_to_jiffies(SI1133_COMPLETION_TIMEOUT_MS);
 	struct device *dev = &data->client->dev;
 	u32 resp;
 	int err;
@@ -412,8 +413,7 @@ static int si1133_command(struct si1133_data *data, u8 cmd)
 
 	if (cmd == SI1133_CMD_FORCE) {
 		/* wait for irq */
-		if (!wait_for_completion_timeout(&data->completion,
-			msecs_to_jiffies(SI1133_COMPLETION_TIMEOUT_MS)))
+		if (!wait_for_completion_timeout(&data->completion, timeout))
 			return -ETIMEDOUT;
 		err = regmap_read(data->regmap, SI1133_REG_RESPONSE0, &resp);
 		if (err)
