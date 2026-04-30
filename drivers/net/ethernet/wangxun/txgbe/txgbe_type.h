7194c7486965036f40b732b12951a8ebf8870a4b
Jiawen Wu <jiawenwu@trustnetic.com>
2026-04-30 16:25:14 +0800

    net: wangxun: add reinit parameter to wx->do_reset callback
    To implement a simple hardware reset without tearing down the network
    interface state, introduce a boolean 'reinit' parameter to wx->do_reset
    callback.
    
    Signed-off-by: Jiawen Wu <jiawenwu@trustnetic.com>

--- drivers/net/ethernet/wangxun/txgbe/txgbe_type.h
+++ drivers/net/ethernet/wangxun/txgbe/txgbe_type.h
@@ -313,7 +313,7 @@ extern char txgbe_driver_name[];
 void txgbe_down(struct wx *wx);
 void txgbe_up(struct wx *wx);
 int txgbe_setup_tc(struct net_device *dev, u8 tc);
-void txgbe_do_reset(struct net_device *netdev);
+void txgbe_do_reset(struct net_device *netdev, bool reinit);
 
 #define TXGBE_LINK_SPEED_UNKNOWN        0
 #define TXGBE_LINK_SPEED_10GB_FULL      4
