300f6b7c744a365bcd508ea3de6fc1e412103fa3
Lu Yao <yaolu@kylinos.cn>
2026-04-30 14:01:37 +0800

    fbcon: don't suspend/resume when vc is graphics mode
    Don't need to do suspend/resume for fbcon in graphic mode.
    
    Doing this may cause error, eg:
      At the beginning, starting the Xorg with single screen and then an
      external screen was plugged in. After logging out in Xorg, fbdev
      info may using screen which is connected later on for info always
      using first connected connector in list in func 'drm_setup_crtcs_fb'.
      Then, S3 executed, fbcon found that the information did not match
      and do atomic to switch fb. However, Xorg will not re-bind the crtc
      fb but continues doing ioctl. At this time, the fb is incorrect.
    
    Signed-off-by: Lu Yao <yaolu@kylinos.cn>

--- drivers/video/fbdev/core/fbcon.c
+++ drivers/video/fbdev/core/fbcon.c
@@ -2602,8 +2602,9 @@ void fbcon_suspended(struct fb_info *info)
 		return;
 	vc = vc_cons[par->currcon].d;
 
-	/* Clear cursor, restore saved data */
-	fbcon_cursor(vc, false);
+	/* Don't need to clear cursor and restore saved data in graphic mode */
+	if (vc->vc_mode != KD_GRAPHICS)
+		fbcon_cursor(vc, false);
 }
 
 void fbcon_resumed(struct fb_info *info)
@@ -2615,7 +2616,9 @@ void fbcon_resumed(struct fb_info *info)
 		return;
 	vc = vc_cons[par->currcon].d;
 
-	update_screen(vc);
+	/* Graphics mode is managed by userspace */
+	if (vc->vc_mode != KD_GRAPHICS)
+		update_screen(vc);
 }
 
 static void fbcon_modechanged(struct fb_info *info)
