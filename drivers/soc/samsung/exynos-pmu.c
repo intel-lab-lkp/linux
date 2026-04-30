012a43ee4f0412f1dca9098b163adb47267b5131
Alexey Klimov <alexey.klimov@linaro.org>
2026-04-30 02:56:54 +0100

    soc: samsung: exynos-pmu: add Exynos850 CPU hotplug support
    Add cpuhotplug support for Exynos850 platforms. This SoC requires
    its own specific set of writes/updates to PMU and PMU interrupts
    generation block in order to put a CPU or a group of CPUs into
    a different sleep states or prepare these entities for a CPU_OFF
    or wake-up out of idle state or after CPU online.
    Without these writes/updates the CPU(s) wake-up or online fails.
    While at this, also add description of Exynos850 PMU registers.
    
    Signed-off-by: Alexey Klimov <alexey.klimov@linaro.org>

--- drivers/soc/samsung/exynos-pmu.c
+++ drivers/soc/samsung/exynos-pmu.c
@@ -133,6 +133,7 @@ static const struct of_device_id exynos_pmu_of_device_ids[] = {
 		.compatible = "samsung,exynos7-pmu",
 	}, {
 		.compatible = "samsung,exynos850-pmu",
+		.data = &exynos850_pmu_data,
 	},
 	{ /*sentinel*/ },
 };
