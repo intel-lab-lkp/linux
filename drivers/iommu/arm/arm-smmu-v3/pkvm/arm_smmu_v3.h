6c4c7512665a055188f58d0edcdec7e503e8a952
Mostafa Saleh <smostafa@google.com>
2026-05-01 11:19:22 +0000

    iommu/arm-smmu-v3-kvm: Share other queues
    Other queues as PRIQ and EVTQ doesn't need to be shadowed. However, we
    need to make sure they are in a state that disallow them to be donated
    to the hypervisor or guests. So, keep track of those and share them when
    they get enabled.
    
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- drivers/iommu/arm/arm-smmu-v3/pkvm/arm_smmu_v3.h
+++ drivers/iommu/arm/arm-smmu-v3/pkvm/arm_smmu_v3.h
@@ -30,6 +30,8 @@
  * @host_ste_cfg	Host stream table config
  * @host_ste_base	Host stream table base
  * @strtab_cfg		Stream table as seen by HW
+ * @evtq_base		Host evtq base reg
+ * @priq_base		Host priq base reg
  */
 struct hyp_arm_smmu_v3_device {
 	phys_addr_t		mmio_addr;
@@ -52,6 +54,8 @@ struct hyp_arm_smmu_v3_device {
 	u64			host_ste_cfg;
 	u64			host_ste_base;
 	struct arm_smmu_strtab_cfg strtab_cfg;
+	unsigned long		evtq_base;
+	unsigned long		priq_base;
 };
 
 extern size_t kvm_nvhe_sym(kvm_hyp_arm_smmu_v3_count);
