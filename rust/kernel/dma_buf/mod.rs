2b9ef58d22c44ae0d2e6ac5d32d1e34d0fcfc4fb
Alice Ryhl <aliceryhl@google.com>
2026-05-05 13:29:37 +0000

    drm/gpuvm: rust: add drm_gpuvm_exec support
    This adds support for simple usage of drm_gpuvm_exec() for locking all
    of the dma reservations and adding fences to them. For more complex
    usage that does not involve locking all reservations, it is expected
    that the ww-mutex abstractions will be used instead.
    
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- rust/kernel/dma_buf/mod.rs
+++ rust/kernel/dma_buf/mod.rs
@@ -5,3 +5,20 @@
 pub mod dma_fence;
 
 pub use self::dma_fence::Fence;
+
+/// How the fences from a `dma_resv` obj are used.
+///
+/// Please see [the C-side documentation][dma_resv_usage] for more details.
+///
+/// [dma_resv_usage]: https://docs.kernel.org/driver-api/dma-buf.html#c.dma_resv_usage
+#[repr(u32)]
+pub enum DmaResvUsage {
+    /// For in kernel memory management only (e.g. copying, clearing memory).
+    Kernel = bindings::dma_resv_usage_DMA_RESV_USAGE_KERNEL,
+    /// Implicit write synchronization for userspace submissions.
+    Write = bindings::dma_resv_usage_DMA_RESV_USAGE_WRITE,
+    /// Implicit read synchronization for userspace submissions.
+    Read = bindings::dma_resv_usage_DMA_RESV_USAGE_READ,
+    /// No implicit sync (e.g. preemption fences, page table updates, TLB flushes).
+    Bookkeep = bindings::dma_resv_usage_DMA_RESV_USAGE_BOOKKEEP,
+}
