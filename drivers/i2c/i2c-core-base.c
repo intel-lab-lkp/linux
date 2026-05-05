e72a8ec8f3678c4a2d07af1624e6f109e48b1f76
Johan Hovold <johan@kernel.org>
2026-05-05 16:25:41 +0200

    i2c: core: fix adapter probe deferral loop
    Drivers must not probe defer after having registered devices as that
    will trigger a probe loop if the devices bind to a driver (cf. commit
    fbc35b45f9f6 ("Add documentation on meaning of -EPROBE_DEFER")).
    
    Move the recovery initialisation, where the GPIO lookup may fail, before
    registering the adapter to prevent this.
    
    Fixes: 75820314de26 ("i2c: core: add generic I2C GPIO recovery")
    Cc: stable@vger.kernel.org	# 5.9
    Cc: Codrin Ciubotariu <codrin.ciubotariu@microchip.com>
    Signed-off-by: Johan Hovold <johan@kernel.org>

--- drivers/i2c/i2c-core-base.c
+++ drivers/i2c/i2c-core-base.c
@@ -1561,6 +1561,10 @@ static int i2c_register_adapter(struct i2c_adapter *adap)
 	adap->dev.type = &i2c_adapter_type;
 	device_initialize(&adap->dev);
 
+	res = i2c_init_recovery(adap);
+	if (res == -EPROBE_DEFER)
+		goto err_put_adap;
+
 	/*
 	 * This adapter can be used as a parent immediately after device_add(),
 	 * setup runtime-pm (especially ignore-children) before hand.
@@ -1573,18 +1577,13 @@ static int i2c_register_adapter(struct i2c_adapter *adap)
 	res = device_add(&adap->dev);
 	if (res) {
 		pr_err("adapter '%s': can't register device (%d)\n", adap->name, res);
-		put_device(&adap->dev);
-		goto out_list;
+		goto err_put_adap;
 	}
 
 	adap->debugfs = debugfs_create_dir(dev_name(&adap->dev), i2c_debugfs_root);
 
 	res = i2c_setup_smbus_alert(adap);
 	if (res)
-		goto out_reg;
-
-	res = i2c_init_recovery(adap);
-	if (res == -EPROBE_DEFER)
 		goto out_reg;
 
 	dev_dbg(&adap->dev, "adapter [%s] registered\n", adap->name);
@@ -1607,8 +1606,10 @@ static int i2c_register_adapter(struct i2c_adapter *adap)
 out_reg:
 	i2c_deregister_clients(adap);
 	debugfs_remove_recursive(adap->debugfs);
+	device_del(&adap->dev);
+err_put_adap:
 	init_completion(&adap->dev_released);
-	device_unregister(&adap->dev);
+	put_device(&adap->dev);
 	wait_for_completion(&adap->dev_released);
 out_list:
 	mutex_lock(&core_lock);
