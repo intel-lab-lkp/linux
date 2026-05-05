2469f45c460b3fdfa021c2de020b0d8294720aa6
Vishnu Reddy <busanna.reddy@oss.qualcomm.com>
2026-05-05 12:29:31 +0530

    media: iris: Add power sequence for Glymur
    Glymur has a secondary video codec core (vcodec1), equivalent to the
    primary core (vcodec0), but with independent power domains, clocks,
    and reset lines. Reuse the existing code wherever possible and add
    power sequence for vcodec1.
    
    Signed-off-by: Vishnu Reddy <busanna.reddy@oss.qualcomm.com>

--- drivers/media/platform/qcom/iris/iris_platform_common.h
+++ drivers/media/platform/qcom/iris/iris_platform_common.h
@@ -61,6 +61,9 @@ enum platform_clk_type {
 	IRIS_VPP0_HW_CLK,
 	IRIS_VPP1_HW_CLK,
 	IRIS_APV_HW_CLK,
+	IRIS_AXI_VCODEC1_CLK,
+	IRIS_VCODEC1_CLK,
+	IRIS_VCODEC1_FREERUN_CLK,
 };
 
 struct platform_clk_data {
@@ -210,6 +213,7 @@ enum platform_pm_domain_type {
 	IRIS_VPP0_HW_POWER_DOMAIN,
 	IRIS_VPP1_HW_POWER_DOMAIN,
 	IRIS_APV_HW_POWER_DOMAIN,
+	IRIS_VCODEC1_POWER_DOMAIN,
 };
 
 struct platform_pd_data {
