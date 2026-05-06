be7e64689ca071e4c4767d12152b6cfdb4291038
邱晓金 <qiuxiaojin@qs-tech.com>
2026-05-06 08:57:22 +0800

    drm/rockchip: gem: check drm_gem_object_init() return value
    From ab8b1592209438a04c635d4c6c40c398230c1ba7 Mon Sep 17 00:00:00 2001
    From: Qiu Xiaojin <qiuxiaojin@qs-tech.com>
    Date: Wed, 6 May 2026 08:52:45 +0800
    Subject: [PATCH] drm/rockchip: gem: check drm_gem_object_init() return value
    
    rockchip_gem_alloc_object() currently ignores the return value of
    
    drm_gem_object_init(). If initialization fails, the function can
    return an incompletely initialized GEM object.
    
    Handle the failure by freeing the allocated object and returning
    ERR_PTR(ret).
    
    Fixes: 38f993b7c59e ("drm/rockchip: Do not use DMA mapping API if attached to IOMMU domain")
    Signed-off-by: Qiu Xiaojin <qiuxiaojin@qs-tech.com>

--- drivers/gpu/drm/rockchip/rockchip_drm_gem.c
+++ drivers/gpu/drm/rockchip/rockchip_drm_gem.c
@@ -286,6 +286,7 @@ static struct rockchip_gem_object *
 {
 	struct rockchip_gem_object *rk_obj;
 	struct drm_gem_object *obj;
+	int ret;
 
 	size = round_up(size, PAGE_SIZE);
 
@@ -297,7 +298,11 @@ static struct rockchip_gem_object *
 
 	obj->funcs = &rockchip_gem_object_funcs;
 
-	drm_gem_object_init(drm, obj, size);
+	ret = drm_gem_object_init(drm, obj, size);
+	if (ret) {
+		kfree(rk_obj);
+		return ERR_PTR(ret);
+	}
 
 	return rk_obj;
 }
