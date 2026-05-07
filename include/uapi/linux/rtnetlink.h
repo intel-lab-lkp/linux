f04a05aa9e3566c7b2977e51b4209ecb29c0d8fd
Eric Dumazet <edumazet@google.com>
2026-05-07 17:45:47 +0000

    rtnetlink: add RTEXT_FILTER_TERSE_DUMP support
    iproute2 can spend considerable amount of time in ll_init_map()
    or ll_link_get() to dump verbose netdev attributes, contributing
    to RTNL pressure.
    
    Add RTEXT_FILTER_TERSE_DUMP new flag so that rtnl_fill_ifinfo()
    limits its output to:
    
    - struct nlmsghdr
    - IFLA_IFNAME
    - IFLA_PROP_LIST
    
    We can later avoid using RTNL when RTEXT_FILTER_TERSE_DUMP
    is requested, as none of these attributes need RTNL.
    
    Signed-off-by: Eric Dumazet <edumazet@google.com>

--- include/uapi/linux/rtnetlink.h
+++ include/uapi/linux/rtnetlink.h
@@ -840,6 +840,7 @@ enum {
 #define RTEXT_FILTER_CFM_CONFIG	(1 << 5)
 #define RTEXT_FILTER_CFM_STATUS	(1 << 6)
 #define RTEXT_FILTER_MST	(1 << 7)
+#define RTEXT_FILTER_TERSE_DUMP	(1 << 8)
 
 /* End of information exported to user level */
 
