a64d53aaff4a781801a585ace3ce71269f4fb853
Namhyung Kim <namhyung@kernel.org>
2026-05-07 00:26:32 -0700

    perf trace: Update beautifier script for clone flags
    According to the change in the sched.h, update the script to generate
    the flags array like below.  Note that '+1' is needed to detect bitmask
    pattern at index 0.
    
      $ cat tools/perf/trace/beauty/generated/clone_flags_array.c
      static const char *clone_flags[] = {
      	[32 + 1] = "CLEAR_SIGHAND",
      	[33 + 1] = "INTO_CGROUP",
      	[34 + 1] = "AUTOREAP",
      	[35 + 1] = "NNP",
      	[36 + 1] = "PIDFD_AUTOKILL",
      	[37 + 1] = "EMPTY_MNTNS",
      };
    
    This was found by Sashiko during review.
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/trace/beauty/clone.sh
+++ tools/perf/trace/beauty/clone.sh
@@ -10,8 +10,8 @@ fi
 linux_sched=${beauty_uapi_linux_dir}/sched.h
 
 printf "static const char *clone_flags[] = {\n"
-regex='^[[:space:]]*#[[:space:]]*define[[:space:]]+CLONE_([^_]+[[:alnum:]_]+)[[:space:]]+(0x[[:xdigit:]]+)[[:space:]]*.*'
+regex='^[[:space:]]*#[[:space:]]*define[[:space:]]+CLONE_([^_]+[[:alnum:]_]+)[[:space:]]+\(1ULL[[:space:]]*<<[[:space:]]*([[:digit:]]+)\)[[:space:]]*.*'
 grep -E $regex ${linux_sched} | \
 	sed -r "s/$regex/\2 \1/g"	| \
-	xargs printf "\t[ilog2(%s) + 1] = \"%s\",\n"
+	xargs printf "\t[%s + 1] = \"%s\",\n"
 printf "};\n"
