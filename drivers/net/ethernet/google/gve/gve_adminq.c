9f41016385ae5ffef06d56c231f0e19167491b73
Jordan Rhee <jordanrhee@google.com>
2026-05-07 21:13:02 +0000

    gve: skip error logging for retryable AdminQ commands
    AdminQ commands may return -EAGAIN under certain transient conditions.
    These commands are intended to be retried by the driver, so logging
    a formal error to the system log is misleading and creates
    unnecessary noise.
    
    Modify the logging logic to skip the error message when the result
    is -EAGAIN, and move logging to dev_err_ratelimited() to avoid
    spamming the log.
    
    Reviewed-by: Joshua Washington <joshwash@google.com>
    Signed-off-by: Jordan Rhee <jordanrhee@google.com>
    Signed-off-by: Harshitha Ramamurthy <hramamurthy@google.com>

--- drivers/net/ethernet/google/gve/gve_adminq.c
+++ drivers/net/ethernet/google/gve/gve_adminq.c
@@ -416,16 +416,10 @@ static bool gve_adminq_wait_for_cmd(struct gve_priv *priv, u32 prod_cnt)
 
 static int gve_adminq_parse_err(struct gve_priv *priv, u32 status)
 {
-	if (status != GVE_ADMINQ_COMMAND_PASSED &&
-	    status != GVE_ADMINQ_COMMAND_UNSET) {
-		dev_err(&priv->pdev->dev, "AQ command failed with status %d\n", status);
-		priv->adminq_cmd_fail++;
-	}
 	switch (status) {
 	case GVE_ADMINQ_COMMAND_PASSED:
 		return 0;
 	case GVE_ADMINQ_COMMAND_UNSET:
-		dev_err(&priv->pdev->dev, "parse_aq_err: err and status both unset, this should not be possible.\n");
 		return -EINVAL;
 	case GVE_ADMINQ_COMMAND_ERROR_ABORTED:
 	case GVE_ADMINQ_COMMAND_ERROR_CANCELLED:
@@ -452,6 +446,16 @@ static int gve_adminq_parse_err(struct gve_priv *priv, u32 status)
 	default:
 		dev_err(&priv->pdev->dev, "parse_aq_err: unknown status code %d\n", status);
 		return -EINVAL;
+	}
+}
+
+static bool gve_adminq_is_retryable(enum gve_adminq_opcodes opcode)
+{
+	switch (opcode) {
+	case GVE_ADMINQ_REPORT_NIC_TIMESTAMP:
+		return true;
+	default:
+		return false;
 	}
 }
 
@@ -482,9 +486,18 @@ static int gve_adminq_kick_and_wait(struct gve_priv *priv)
 		cmd = &priv->adminq[i & priv->adminq_mask];
 		status = be32_to_cpu(READ_ONCE(cmd->status));
 		err = gve_adminq_parse_err(priv, status);
-		if (err)
+		if (err) {
+			enum gve_adminq_opcodes opcode =
+				be32_to_cpu(READ_ONCE(cmd->opcode));
+			priv->adminq_cmd_fail++;
+			if (!gve_adminq_is_retryable(opcode) || err != -EAGAIN)
+				dev_err_ratelimited(&priv->pdev->dev,
+						    "AQ command %d failed with status %d\n",
+						    opcode, status);
+
 			// Return the first error if we failed.
 			return err;
+		}
 	}
 
 	return 0;
