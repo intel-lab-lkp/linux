646e27f00705ca27eefde7ddfec9cedd9252ab9d
Lizhi Hou <lizhi.hou@amd.com>
2026-05-08 09:36:55 -0700

    accel/amdxdna: Add expandable device heap support
    Introduce an expandable device heap to avoid allocating a large heap
    upfront. Start with a smaller initial heap and grow it on demand.
    Return -EAGAIN when BO allocation fails due to insufficient heap space,
    allowing userspace to trigger heap expansion via a heap BO creation
    IOCTL and retry the allocation.
    
    Manage heap chunks using an xarray. On expansion, register new chunks
    with the firmware via MSG_OP_ADD_HOST_BUFFER.
    
    Since heap shrinking is not supported by the firmware, release all heap
    chunks on device close.
    
    Co-developed-by: Wendy Liang <wendy.liang@amd.com>
    Signed-off-by: Wendy Liang <wendy.liang@amd.com>
    Signed-off-by: Lizhi Hou <lizhi.hou@amd.com>

--- drivers/accel/amdxdna/amdxdna_ctx.c
+++ drivers/accel/amdxdna/amdxdna_ctx.c
@@ -61,16 +61,35 @@ static struct dma_fence *amdxdna_fence_create(struct amdxdna_hwctx *hwctx)
 	return &fence->base;
 }
 
+static void amdxdna_hwctx_release_expanded_heap(struct amdxdna_hwctx *hwctx)
+{
+	struct amdxdna_client *client = hwctx->client;
+	struct amdxdna_gem_obj *heap;
+	unsigned long heap_id;
+
+	mutex_lock(&client->mm_lock);
+	if (hwctx->last_attached_heap) {
+		xa_for_each_range(&client->dev_heap_xa, heap_id, heap, 1,
+				  hwctx->last_attached_heap) {
+			amdxdna_gem_unpin(heap);
+			drm_gem_object_put(to_gobj(heap));
+		}
+	}
+	mutex_unlock(&client->mm_lock);
+}
+
 static void amdxdna_hwctx_destroy_rcu(struct amdxdna_hwctx *hwctx,
 				      struct srcu_struct *ss)
 {
-	struct amdxdna_dev *xdna = hwctx->client->xdna;
+	struct amdxdna_client *client = hwctx->client;
+	struct amdxdna_dev *xdna = client->xdna;
 
 	synchronize_srcu(ss);
 
 	/* At this point, user is not able to submit new commands */
 	xdna->dev_info->ops->hwctx_fini(hwctx);
 
+	amdxdna_hwctx_release_expanded_heap(hwctx);
 	kfree(hwctx->name);
 	kfree(hwctx);
 }
@@ -405,6 +424,68 @@ int amdxdna_hwctx_sync_debug_bo(struct amdxdna_client *client, u32 debug_bo_hdl)
 put_obj:
 	drm_gem_object_put(gobj);
 	return ret;
+}
+
+static int amdxdna_hwctx_expand_heap(struct amdxdna_hwctx *hwctx)
+{
+	struct amdxdna_client *client = hwctx->client;
+	struct amdxdna_dev *xdna = client->xdna;
+	struct amdxdna_gem_obj *heap;
+	unsigned long heap_id, nid;
+	int ret = 0;
+
+	nid = hwctx->last_attached_heap + 1;
+	if (nid == client->dev_heap_nid)
+		goto out;
+
+	if (!xdna->dev_info->ops->hwctx_heap_expand) {
+		ret = -EOPNOTSUPP;
+		goto out;
+	}
+
+	xa_for_each_range(&client->dev_heap_xa, heap_id, heap,
+			  nid, client->dev_heap_nid) {
+		drm_gem_object_get(to_gobj(heap));
+		ret = amdxdna_gem_pin(heap);
+		if (ret) {
+			drm_gem_object_put(to_gobj(heap));
+			break;
+		}
+
+		mutex_unlock(&client->mm_lock);
+		ret = xdna->dev_info->ops->hwctx_heap_expand(hwctx, heap);
+		mutex_lock(&client->mm_lock);
+		if (ret) {
+			amdxdna_gem_unpin(heap);
+			drm_gem_object_put(to_gobj(heap));
+			break;
+		}
+
+		hwctx->last_attached_heap = heap_id;
+	}
+
+out:
+	return ret;
+}
+
+int amdxdna_update_heap(struct amdxdna_client *client, struct amdxdna_hwctx *hwctx)
+{
+	unsigned long hwctx_id;
+	int ret;
+
+	if (hwctx) {
+		guard(mutex)(&client->mm_lock);
+		return amdxdna_hwctx_expand_heap(hwctx);
+	}
+
+	guard(mutex)(&client->mm_lock);
+	amdxdna_for_each_hwctx(client, hwctx_id, hwctx) {
+		ret = amdxdna_hwctx_expand_heap(hwctx);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
 }
 
 static void
