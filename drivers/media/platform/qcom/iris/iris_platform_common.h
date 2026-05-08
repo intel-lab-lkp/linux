f4ffb8d273c8fff434e9b7a19339a2f56fe74dd6
Vishnu Reddy <busanna.reddy@oss.qualcomm.com>
2026-05-09 00:30:01 +0530

    media: iris: Add platform data for glymur
    On glymur platform, the iris core shares most properties with the
    iris core on the SM8550 platform. The major difference is that glymur
    integrates two codec cores (vcodec0 and vcodec1), while SM8550 has only
    one. Add glymur specific platform data, reusing SM8550 definitions
    wherever applicable.
    
    Reviewed-by: Vikash Garodia <vikash.garodia@oss.qualcomm.com>
    Signed-off-by: Vishnu Reddy <busanna.reddy@oss.qualcomm.com>

--- drivers/media/platform/qcom/iris/iris_platform_common.h
+++ drivers/media/platform/qcom/iris/iris_platform_common.h
@@ -30,6 +30,10 @@ struct iris_inst;
 #define DEFAULT_QP				20
 #define BITRATE_DEFAULT			20000000
 
+#define VIDEO_REGION_SECURE_FW_REGION_ID	0
+#define VIDEO_REGION_VM0_SECURE_NP_ID		1
+#define VIDEO_REGION_VM0_NONSECURE_NP_ID	5
+
 enum stage_type {
 	STAGE_1 = 1,
 	STAGE_2 = 2,
@@ -41,6 +45,7 @@ enum pipe_type {
 	PIPE_4 = 4,
 };
 
+extern const struct iris_platform_data glymur_data;
 extern const struct iris_platform_data qcs8300_data;
 extern const struct iris_platform_data sc7280_data;
 extern const struct iris_platform_data sm8250_data;
