3da3cf093f41564db3b459c82ea252aca17e6338
Aaron Tomlin <atomlin@atomlin.com>
2026-05-09 12:48:47 -0400

    mips: sched: Fix CPUMASK_OFFSTACK memory corruption
    This patch addresses a critical memory management flaw.
    
    When CONFIG_CPUMASK_OFFSTACK is enabled, cpumask_var_t is a pointer.
    Consequently, sizeof(new_mask) evaluates to the pointer size, causing
    copy_from_user() to clobber the stack pointer. The subsequent
    alloc_cpumask_var() overwrites this with an uninitialised heap address,
    discarding the user's mask and risking data leaks. Fix this by
    allocating masks first, and using cpumask_size() to copy data directly
    into the allocated buffer.
    
    Additionally, reorder the failure goto labels to ensure task locks and
    memory allocations are cleanly unwound.
    
    Fixes: 295cbf6d63165 ("[MIPS] Move FPU affinity code into separate file.")
    Cc: stable@vger.kernel.org
    Signed-off-by: Aaron Tomlin <atomlin@atomlin.com>

--- arch/mips/kernel/mips-mt-fpaff.c
+++ arch/mips/kernel/mips-mt-fpaff.c
@@ -71,30 +71,11 @@ asmlinkage long mipsmt_sys_sched_setaffinity(pid_t pid, unsigned int len,
 	struct task_struct *p;
 	int retval;
 
-	if (len < sizeof(new_mask))
+	if (len < cpumask_size())
 		return -EINVAL;
 
-	if (copy_from_user(&new_mask, user_mask_ptr, sizeof(new_mask)))
-		return -EFAULT;
-
-	cpus_read_lock();
-	rcu_read_lock();
-
-	p = find_process_by_pid(pid);
-	if (!p) {
-		rcu_read_unlock();
-		cpus_read_unlock();
-		return -ESRCH;
-	}
-
-	/* Prevent p going away */
-	get_task_struct(p);
-	rcu_read_unlock();
-
-	if (!alloc_cpumask_var(&cpus_allowed, GFP_KERNEL)) {
-		retval = -ENOMEM;
-		goto out_put_task;
-	}
+	if (!alloc_cpumask_var(&cpus_allowed, GFP_KERNEL))
+		return -ENOMEM;
 	if (!alloc_cpumask_var(&new_mask, GFP_KERNEL)) {
 		retval = -ENOMEM;
 		goto out_free_cpus_allowed;
@@ -103,6 +84,26 @@ asmlinkage long mipsmt_sys_sched_setaffinity(pid_t pid, unsigned int len,
 		retval = -ENOMEM;
 		goto out_free_new_mask;
 	}
+
+	retval = -EFAULT;
+	if (copy_from_user(new_mask, user_mask_ptr, cpumask_size()))
+		goto out_free_effective_mask;
+
+	cpus_read_lock();
+	rcu_read_lock();
+
+	p = find_process_by_pid(pid);
+	if (!p) {
+		rcu_read_unlock();
+		cpus_read_unlock();
+		retval = -ESRCH;
+		goto out_free_effective_mask;
+	}
+
+	/* Prevent p going away */
+	get_task_struct(p);
+	rcu_read_unlock();
+
 	if (!check_same_owner(p) && !capable(CAP_SYS_NICE)) {
 		retval = -EPERM;
 		goto out_unlock;
@@ -141,14 +142,14 @@ asmlinkage long mipsmt_sys_sched_setaffinity(pid_t pid, unsigned int len,
 		}
 	}
 out_unlock:
+	put_task_struct(p);
+	cpus_read_unlock();
+out_free_effective_mask:
 	free_cpumask_var(effective_mask);
 out_free_new_mask:
 	free_cpumask_var(new_mask);
 out_free_cpus_allowed:
 	free_cpumask_var(cpus_allowed);
-out_put_task:
-	put_task_struct(p);
-	cpus_read_unlock();
 	return retval;
 }
 
