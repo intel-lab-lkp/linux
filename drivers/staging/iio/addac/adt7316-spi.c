79166e05b98af64bfb915b1baee16d68cfa6e1e3
Hungyu Lin <dennylin0707@gmail.com>
2026-05-10 08:10:41 +0000

    staging: iio: addac: adt7316: document SPI interface switching sequence
    The device powers up in I2C mode. Switching to SPI mode
    requires sending a sequence of SPI writes as described in
    the datasheet.
    
    During this sequence, the device may still be in I2C mode,
    so SPI transactions may not be recognized and can fail.
    Such errors are expected and are ignored.
    
    Add a comment to clarify this behavior.
    
    Signed-off-by: Hungyu Lin <dennylin0707@gmail.com>

--- drivers/staging/iio/addac/adt7316-spi.c
+++ drivers/staging/iio/addac/adt7316-spi.c
@@ -106,7 +106,17 @@ static int adt7316_spi_probe(struct spi_device *spi_dev)
 		return -EINVAL;
 	}
 
-	/* switch from default I2C protocol to SPI protocol */
+	/*
+	 * The device powers up in I2C mode. Switching to SPI mode
+	 * requires sending a sequence of SPI writes as described in
+	 * the datasheet ("Serial Interface Selection" section).
+	 *
+	 * During this sequence, the device may still be in I2C mode,
+	 * so SPI transactions may not be recognized and can fail.
+	 * Such errors are expected and are ignored.
+	 *
+	 * TL;DR: Do not change this!
+	 */
 	adt7316_spi_write(spi_dev, 0, 0);
 	adt7316_spi_write(spi_dev, 0, 0);
 	adt7316_spi_write(spi_dev, 0, 0);
