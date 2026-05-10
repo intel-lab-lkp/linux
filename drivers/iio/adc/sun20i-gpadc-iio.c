1f12e08765e0814343c248d0a0d6db8d2ee9d101
Michal Piekos <michal.piekos@mmpsystems.pl>
2026-05-10 14:57:23 +0200

    iio: adc: sun20i-gpadc: add A523 gpadc support
    A523 differs from existing sun20i-gpadc-iio by having two clocks; bus
    clock and module clock.
    
    Change driver to enable all clocks.
    
    Signed-off-by: Michal Piekos <michal.piekos@mmpsystems.pl>

--- drivers/iio/adc/sun20i-gpadc-iio.c
+++ drivers/iio/adc/sun20i-gpadc-iio.c
@@ -180,7 +180,7 @@ static int sun20i_gpadc_probe(struct platform_device *pdev)
 	struct iio_dev *indio_dev;
 	struct sun20i_gpadc_iio *info;
 	struct reset_control *rst;
-	struct clk *clk;
+	struct clk_bulk_data *clks;
 	int irq;
 	int ret;
 
@@ -205,9 +205,9 @@ static int sun20i_gpadc_probe(struct platform_device *pdev)
 	if (IS_ERR(info->regs))
 		return PTR_ERR(info->regs);
 
-	clk = devm_clk_get_enabled(dev, NULL);
-	if (IS_ERR(clk))
-		return dev_err_probe(dev, PTR_ERR(clk), "failed to enable bus clock\n");
+	ret = devm_clk_bulk_get_all_enabled(dev, &clks);
+	if (ret < 0)
+		return dev_err_probe(dev, ret, "failed to enable clocks\n");
 
 	rst = devm_reset_control_get_exclusive(dev, NULL);
 	if (IS_ERR(rst))
