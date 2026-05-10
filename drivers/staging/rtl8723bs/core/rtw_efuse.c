5220b6a4f1041383317f1758dc29c17df82cfb8c
Moksh Panicker <mokshpanicker.7@gmail.com>
2026-05-10 17:14:38 +0000

    staging: rtl8723bs: add braces to else clause in rtw_efuse.c
    Add missing braces to the else clause to satisfy the coding style
    requirement that both arms of an if/else statement use braces.
    
    Signed-off-by: Moksh Panicker <mokshpanicker.7@gmail.com>

--- drivers/staging/rtl8723bs/core/rtw_efuse.c
+++ drivers/staging/rtl8723bs/core/rtw_efuse.c
@@ -73,7 +73,7 @@ u8 rtw_efuse_read_1_byte(struct adapter *Adapter, u16 Address)
 				break;
 		}
 		return rtw_read8(Adapter, EFUSE_CTRL);
-	} else
+	} else {
 		return 0xFF;
 
 } /* rtw_efuse_read_1_byte */
