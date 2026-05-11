e83e12f641d3da496168c8d60610696514f5da01
Bibo Mao <maobibo@loongson.cn>
2026-05-11 14:51:46 +0800

    LoongArch: KVM: Simple interrupt status acquire interface
    When VM is migrating, interrupts status are stored in software CSR estat
    register, also new injected interrupts are cached in
    vcpu::arch::irq_pending.
    
    With interrupt statuc acquire interface, there is expensive vcpu_load()
    and vcpu_put() function call to sync cached vcpu::arch::irq_pending.
    Here new internal API kvm_vcpu_sync_intr() is added to sync cached
    pending irq to software CSR estat register.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>

--- arch/loongarch/kvm/vcpu.c
+++ arch/loongarch/kvm/vcpu.c
@@ -610,18 +610,11 @@ static int _kvm_getcsr(struct kvm_vcpu *vcpu, unsigned int id, u64 *val)
 
 	if (id == LOONGARCH_CSR_ESTAT) {
 		preempt_disable();
-		vcpu_load(vcpu);
-		/*
-		 * Sync pending interrupts into ESTAT so that interrupt
-		 * remains during VM migration stage
-		 */
-		kvm_deliver_intr(vcpu);
-		vcpu->arch.aux_inuse &= ~KVM_LARCH_SWCSR_LATEST;
-		vcpu_put(vcpu);
+		kvm_vcpu_sync_intr(vcpu);
 		preempt_enable();
 
 		/* ESTAT IP0~IP7 get from GINTC */
-		gintc = kvm_read_sw_gcsr(csr, LOONGARCH_CSR_GINTC) & 0xff;
+		gintc = kvm_read_sw_gcsr(csr, LOONGARCH_CSR_GINTC) & KVM_GINTC_IRQ_MASK;
 		*val = kvm_read_sw_gcsr(csr, LOONGARCH_CSR_ESTAT) | (gintc << 2);
 		return 0;
 	}
@@ -1635,6 +1628,15 @@ static int _kvm_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 
 	/* Restore timer state regardless */
 	kvm_restore_timer(vcpu);
+
+	/*
+	 * Restore Root.GINTC from unused Guest.GINTC register
+	 *
+	 * SW state about LOONGARCH_CSR_GINTC is updated with get_csr()
+	 * ioctl command only and without set_csr(). Update HW state
+	 * from changed SW state.
+	 */
+	write_csr_gintc(csr->csrs[LOONGARCH_CSR_GINTC]);
 	kvm_make_request(KVM_REQ_STEAL_UPDATE, vcpu);
 
 	/* Don't bother restoring registers multiple times unless necessary */
@@ -1697,8 +1699,6 @@ static int _kvm_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 		kvm_restore_hw_gcsr(csr, LOONGARCH_CSR_ISR3);
 	}
 
-	/* Restore Root.GINTC from unused Guest.GINTC register */
-	write_csr_gintc(csr->csrs[LOONGARCH_CSR_GINTC]);
 	write_csr_gstat(csr->csrs[LOONGARCH_CSR_GSTAT]);
 
 	/*
