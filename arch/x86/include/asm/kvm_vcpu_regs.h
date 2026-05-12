d1d1534a7ef4694a6a7b079c80e661dd66d04e9c
Chang S. Bae <chang.seok.bae@intel.com>
2026-05-12 01:14:42 +0000

    KVM: VMX: Macrofy GPR swapping in __vmx_vcpu_run()
    Convert the repeated register save/restore sequences into macros to
    simplify the VM entry code. This can also make it easier to extend for
    additional registers.
    
    No functional change intended.
    
    Suggested-by: Paolo Bonzini <pbonzini@redhat.com>
    Cc: x86@kernel.org
    Signed-off-by: Chang S. Bae <chang.seok.bae@intel.com>
    Link: https://lore.kernel.org/6e67df0e-e5f0-43f5-aa86-22e8b01b75d2@redhat.com
    Link: https://patch.msgid.link/20260512011502.53072-2-chang.seok.bae@intel.com/
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- arch/x86/include/asm/kvm_vcpu_regs.h
+++ arch/x86/include/asm/kvm_vcpu_regs.h
@@ -22,4 +22,126 @@
 #define __VCPU_REGS_R15 15
 #endif
 
+#ifdef __ASSEMBLER__
+
+#define REG_NUM_INVALID		100
+
+	.macro R32_NUM opd r32
+	\opd = REG_NUM_INVALID
+	.ifc \r32,%eax
+	\opd = __VCPU_REGS_EAX
+	.endif
+	.ifc \r32,%ecx
+	\opd = __VCPU_REGS_ECX
+	.endif
+	.ifc \r32,%edx
+	\opd = __VCPU_REGS_EDX
+	.endif
+	.ifc \r32,%ebx
+	\opd = __VCPU_REGS_EBX
+	.endif
+	.ifc \r32,%esp
+	\opd = __VCPU_REGS_ESP
+	.endif
+	.ifc \r32,%ebp
+	\opd = __VCPU_REGS_EBP
+	.endif
+	.ifc \r32,%esi
+	\opd = __VCPU_REGS_ESI
+	.endif
+	.ifc \r32,%edi
+	\opd = __VCPU_REGS_EDI
+	.endif
+#ifdef CONFIG_X86_64
+	.ifc \r32,%r8d
+	\opd = 8
+	.endif
+	.ifc \r32,%r9d
+	\opd = 9
+	.endif
+	.ifc \r32,%r10d
+	\opd = 10
+	.endif
+	.ifc \r32,%r11d
+	\opd = 11
+	.endif
+	.ifc \r32,%r12d
+	\opd = 12
+	.endif
+	.ifc \r32,%r13d
+	\opd = 13
+	.endif
+	.ifc \r32,%r14d
+	\opd = 14
+	.endif
+	.ifc \r32,%r15d
+	\opd = 15
+	.endif
+#endif
+	.endm
+
+	.macro R64_NUM opd r64
+	\opd = REG_NUM_INVALID
+#ifdef CONFIG_X86_64
+	.ifc \r64,%rax
+	\opd = __VCPU_REGS_RAX
+	.endif
+	.ifc \r64,%rcx
+	\opd = __VCPU_REGS_RCX
+	.endif
+	.ifc \r64,%rdx
+	\opd = __VCPU_REGS_RDX
+	.endif
+	.ifc \r64,%rbx
+	\opd = __VCPU_REGS_RBX
+	.endif
+	.ifc \r64,%rsp
+	\opd = __VCPU_REGS_RSP
+	.endif
+	.ifc \r64,%rbp
+	\opd = __VCPU_REGS_RBP
+	.endif
+	.ifc \r64,%rsi
+	\opd = __VCPU_REGS_RSI
+	.endif
+	.ifc \r64,%rdi
+	\opd = __VCPU_REGS_RDI
+	.endif
+	.ifc \r64,%r8
+	\opd = 8
+	.endif
+	.ifc \r64,%r9
+	\opd = 9
+	.endif
+	.ifc \r64,%r10
+	\opd = 10
+	.endif
+	.ifc \r64,%r11
+	\opd = 11
+	.endif
+	.ifc \r64,%r12
+	\opd = 12
+	.endif
+	.ifc \r64,%r13
+	\opd = 13
+	.endif
+	.ifc \r64,%r14
+	\opd = 14
+	.endif
+	.ifc \r64,%r15
+	\opd = 15
+	.endif
+#endif
+	.endm
+
+.macro REG_NUM reg_num reg
+#ifdef CONFIG_X86_64
+	R64_NUM \reg_num \reg
+#else
+	R32_NUM \reg_num \reg
+#endif
+.endm
+
+#endif
+
 #endif /* _ASM_X86_KVM_VCPU_REGS_H */
