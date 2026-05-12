3a88275dc49fbe46b5e24fde0cca29a59046d5fd
Harshit Shaw <shawharshit116@gmail.com>
2026-05-12 11:33:20 +0000

    staging: fbtft: convert sysfs attributes to use ATTRIBUTE_GROUPS
    Replace direct device_create_file() calls with the correct
    ATTRIBUTE_GROUPS() macro. Add fbtft_groups as dev_groups in the
    driver structure so the driver core automatically adds and removes
    the sysfs attributes when a device is bound or unbound.
    
    Signed-off-by: Harshit Shaw <shawharshit116@gmail.com>

--- drivers/staging/fbtft/internal.h
+++ drivers/staging/fbtft/internal.h
@@ -4,8 +4,7 @@
 #ifndef __LINUX_FBTFT_INTERNAL_H
 #define __LINUX_FBTFT_INTERNAL_H
 
-void fbtft_sysfs_init(struct fbtft_par *par);
-void fbtft_sysfs_exit(struct fbtft_par *par);
+extern const struct attribute_group *fbtft_groups[];
 void fbtft_expand_debug_value(unsigned long *debug);
 int fbtft_gamma_parse_str(struct fbtft_par *par, u32 *curves,
 			  const char *str, int size);
