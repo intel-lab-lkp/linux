6b4db7b67b42d78f6422337d3ffeb75ea6dbdd7e
Michael Chan <michael.chan@broadcom.com>
2026-05-12 14:20:52 -0700

    bnxt_en: Account for the MPC TX and CP rings
    Modify bnxt_cp_rings_in_use(), bnxt_get_max_func_cp_rings_for_en(),
    and _bnxt_get_max_rings() to account for any TX rings and CP rings
    used by MPCs.  Add a new helper bnxt_total_tx_rings() to include
    MPC TX rings.  Ring reservations will now include the MPC rings.
    
    Reviewed-by: Ajit Khaparde <ajit.khaparde@broadcom.com>
    Reviewed-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Reviewed-by: Andy Gospodarek <andrew.gospodarek@broadcom.com>
    Reviewed-by: Pavan Chebbi <pavan.chebbi@broadcom.com>
    Signed-off-by: Michael Chan <michael.chan@broadcom.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt_mpc.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt_mpc.c
@@ -24,3 +24,24 @@ void bnxt_free_mpc_info(struct bnxt *bp)
 	kfree(bp->mpc_info);
 	bp->mpc_info = NULL;
 }
+
+int bnxt_mpc_tx_rings_in_use(struct bnxt *bp)
+{
+	struct bnxt_mpc_info *mpc = bp->mpc_info;
+	int i, mpc_tx = 0;
+
+	if (!mpc)
+		return 0;
+	for (i = 0; i < BNXT_MPC_TYPE_MAX; i++)
+		mpc_tx += mpc->mpc_ring_count[i];
+	return mpc_tx;
+}
+
+int bnxt_mpc_cp_rings_in_use(struct bnxt *bp)
+{
+	struct bnxt_mpc_info *mpc = bp->mpc_info;
+
+	if (!mpc)
+		return 0;
+	return mpc->mpc_cp_rings;
+}
