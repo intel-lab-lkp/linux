d91836d473178fbdd22e8605876a2b290b4dce86
Ilya Maximets <i.maximets@ovn.org>
2026-05-13 20:35:22 +0200

    openvswitch: vport: remove infrastructure for vport options
    Since removal of tunnel vport types, there aren't any vports that
    support options.  Let's remove the options-related infrastructure.
    
    Can be reinstated if we ever need a new vport type or if we need extra
    options for the existing ones.  The uAPI attribute remains.
    
    Clarification comment is added to highlight that none of the supported
    vports support options at the moment.
    
    Note: It is technically possible that someone has an out-of-tree module
    named vport-type-N that implements a different vport type and they have
    options for this vport type.  However, our message size calculations do
    not account for whatever options such a port would have and so it is
    dangerous to load such a module without modifying the code in the main
    datapath.c, unless the options are smaller than the ones we had for
    vxlan.  A more robust solution would be to have a different version of
    the entire openvswitch module instead, so the use case of a separate
    vport-type-N loaded with the upstream openvswitch module is unlikely.
    At this time we're not aware of anyone doing that.  Alternative to
    immediate removal would be printing out a deprecation warning whenever
    the options setting is attempted.
    
    Signed-off-by: Ilya Maximets <i.maximets@ovn.org>

--- net/openvswitch/datapath.c
+++ net/openvswitch/datapath.c
@@ -1852,7 +1852,6 @@ static int ovs_dp_cmd_new(struct sk_buff *skb, struct genl_info *info)
 	/* Set up our datapath device. */
 	parms.name = nla_data(a[OVS_DP_ATTR_NAME]);
 	parms.type = OVS_VPORT_TYPE_INTERNAL;
-	parms.options = NULL;
 	parms.dp = dp;
 	parms.port_no = OVSP_LOCAL;
 	parms.upcall_portids = a[OVS_DP_ATTR_UPCALL_PID];
@@ -2168,10 +2167,6 @@ static int ovs_vport_cmd_fill_info(struct vport *vport, struct sk_buff *skb,
 	if (ovs_vport_get_upcall_portids(vport, skb))
 		goto nla_put_failure;
 
-	err = ovs_vport_get_options(vport, skb);
-	if (err == -EMSGSIZE)
-		goto error;
-
 	genlmsg_end(skb, ovs_header);
 	return 0;
 
@@ -2179,7 +2174,6 @@ nla_put_failure_unlock:
 	rcu_read_unlock();
 nla_put_failure:
 	err = -EMSGSIZE;
-error:
 	genlmsg_cancel(skb, ovs_header);
 	return err;
 }
@@ -2205,10 +2199,6 @@ static size_t ovs_vport_cmd_msg_size(void)
 
 	/* OVS_VPORT_ATTR_UPCALL_PID */
 	msgsize += nla_total_size(nr_cpu_ids * sizeof(u32));
-
-	/* There are no vports supporting OVS_VPORT_ATTR_OPTIONS, so it is
-	 * not included in the message size calculation.
-	 */
 
 	return msgsize;
 }
@@ -2361,7 +2351,6 @@ restart:
 	}
 
 	parms.name = nla_data(a[OVS_VPORT_ATTR_NAME]);
-	parms.options = a[OVS_VPORT_ATTR_OPTIONS];
 	parms.dp = dp;
 	parms.port_no = port_no;
 	parms.upcall_portids = a[OVS_VPORT_ATTR_UPCALL_PID];
@@ -2421,13 +2410,6 @@ static int ovs_vport_cmd_set(struct sk_buff *skb, struct genl_info *info)
 		err = -EINVAL;
 		goto exit_unlock_free;
 	}
-
-	if (a[OVS_VPORT_ATTR_OPTIONS]) {
-		err = ovs_vport_set_options(vport, a[OVS_VPORT_ATTR_OPTIONS]);
-		if (err)
-			goto exit_unlock_free;
-	}
-
 
 	if (a[OVS_VPORT_ATTR_UPCALL_PID]) {
 		struct nlattr *ids = a[OVS_VPORT_ATTR_UPCALL_PID];
@@ -2602,7 +2584,7 @@ static const struct nla_policy vport_policy[OVS_VPORT_ATTR_MAX + 1] = {
 	[OVS_VPORT_ATTR_PORT_NO] = { .type = NLA_U32 },
 	[OVS_VPORT_ATTR_TYPE] = { .type = NLA_U32 },
 	[OVS_VPORT_ATTR_UPCALL_PID] = { .type = NLA_UNSPEC },
-	[OVS_VPORT_ATTR_OPTIONS] = { .type = NLA_NESTED },
+	[OVS_VPORT_ATTR_OPTIONS] = { .type = NLA_NESTED }, /* Unused. */
 	[OVS_VPORT_ATTR_IFINDEX] = NLA_POLICY_MIN(NLA_S32, 0),
 	[OVS_VPORT_ATTR_NETNSID] = { .type = NLA_S32 },
 	[OVS_VPORT_ATTR_UPCALL_STATS] = { .type = NLA_NESTED },
