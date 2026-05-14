0eff9dd32fcfe38f647cb59ae93346d28806dca9
Frank Li <Frank.Li@nxp.com>
2026-05-14 14:33:53 -0400

    media: v4l2-common: Add helper function media_bus_fmt_to_csi2_(bpp|dt)()
    Add helper function media_bus_fmt_to_csi2_bpp() to get media bus fmt's bpp
    to reduce codes such as
    
    	static const struct imx7_csi_pixfmt pixel_formats[] = {
            {
                    .fourcc = V4L2_PIX_FMT_UYVY,
                    .codes  = IMX_BUS_FMTS(
                            MEDIA_BUS_FMT_UYVY8_2X8,
                            MEDIA_BUS_FMT_UYVY8_1X16
                    ),
                    .yuv    = true,
                    .bpp    = 16,
            },
    	....
    
    .bpp can be removed from pixel_formats with this helper function.
    
    CSI2 data type is defined by MIPI Camera Serial Interface 2 Spec Ver4.1.
    See section 9.4.
    
    Add helper function media_bus_fmt_to_csi2_dt() to convert media bus fmt to
    MIPI defined data type and avoid below duplicated static array in each CSI2
    drivers.
    
    	{
    		.code = MEDIA_BUS_FMT_UYVY8_1X16,
    		.data_type = MIPI_CSI2_DT_YUV422_8B,
    	}
    
    Only add known map for dt type. Need update media_bus_fmt_info when new
    mapping used.
    
    Signed-off-by: Frank Li <Frank.Li@nxp.com>

--- include/media/mipi-csi2.h
+++ include/media/mipi-csi2.h
@@ -8,6 +8,8 @@
 #ifndef _MEDIA_MIPI_CSI2_H
 #define _MEDIA_MIPI_CSI2_H
 
+#include <linux/types.h>
+
 /* Short packet data types */
 #define MIPI_CSI2_DT_FS			0x00
 #define MIPI_CSI2_DT_FE			0x01
@@ -43,5 +45,30 @@
 #define MIPI_CSI2_DT_RAW16		0x2e
 #define MIPI_CSI2_DT_RAW20		0x2f
 #define MIPI_CSI2_DT_USER_DEFINED(n)	(0x30 + (n))	/* 0..7 */
+
+/* Use one undefined value in spec */
+#define MIPI_CSI2_DT_INVALID		0xff
+
+/**
+ * media_bus_fmt_to_csi2_dt - Get MIPI CSI2 data type from media bus format
+ *
+ * @bus_fmt: media bus format identifier (MEDIA_BUS_FMT_*)
+ *
+ * Return: MIPI CSI2 data type MIPI_CSI2_DT_*, MIPI_CSI2_DT_INVALIDATE means
+ * can't get data type from bus_fmt.
+ */
+u32 media_bus_fmt_to_csi2_dt(u32 bus_fmt);
+
+/**
+ * media_bus_fmt_to_csi2_bpp - Get media bus format's bit depth
+ *
+ * @bus_fmt: media bus format identifier (MEDIA_BUS_FMT_*)
+ *
+ * Return: bit depth, -EINVAL if fail to get from bus_fmt.
+ *
+ * Notes: this bpp is suffix from MEDIA_BUS_FMT_*, no pad, not for compressed
+ * data.
+ */
+int media_bus_fmt_to_csi2_bpp(u32 bus_fmt);
 
 #endif /* _MEDIA_MIPI_CSI2_H */
