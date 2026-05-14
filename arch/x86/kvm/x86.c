ea44b5ff79adaa851fdd726e692628d79ba26b26
Sean Christopherson <seanjc@google.com>
2026-05-14 14:53:45 -0700

    KVM: x86: Trace hypercall register *after* truncating values for 32-bit
    When tracing hypercalls, invoke the tracepoint *after* truncating the
    register values for 32-bit guests so as not to record unused garbage (in
    the extremely unlikely scenario that the guest left garbage in a register
    after transitioning from 64-bit mode to 32-bit mode).
    
    Fixes: 229456fc34b1 ("KVM: convert custom marker based tracing to event traces")
    Reviewed-by: Yosry Ahmed <yosry@kernel.org>
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -10430,8 +10430,6 @@ int ____kvm_emulate_hypercall(struct kvm_vcpu *vcpu, int cpl,
 
 	++vcpu->stat.hypercalls;
 
-	trace_kvm_hypercall(nr, a0, a1, a2, a3);
-
 	if (!op_64_bit) {
 		nr &= 0xFFFFFFFF;
 		a0 &= 0xFFFFFFFF;
@@ -10439,6 +10437,8 @@ int ____kvm_emulate_hypercall(struct kvm_vcpu *vcpu, int cpl,
 		a2 &= 0xFFFFFFFF;
 		a3 &= 0xFFFFFFFF;
 	}
+
+	trace_kvm_hypercall(nr, a0, a1, a2, a3);
 
 	if (cpl) {
 		ret = -KVM_EPERM;
