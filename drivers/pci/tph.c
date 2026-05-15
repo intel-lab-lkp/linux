fecb557611c6dac803eb544842a956902e8d0af6
Chengwen Feng <fengchengwen@huawei.com>
2026-05-15 17:40:37 +0800

    PCI/TPH: Export pcie_tph_get_st_modes() for external use
    Export the helper to retrieve supported PCIe TPH steering tag modes so
    that drivers like VFIO can query and expose device capabilities to
    userspace.
    
    Add stub functions for pcie_tph_get_st_table_size() and
    pcie_tph_get_st_table_loc() when !CONFIG_PCIE_TPH.
    
    Add tph_cap validation for pcie_tph_get_st_modes() and
    pcie_tph_get_st_table_loc() to prevent invalid PCI configuration
    space access when TPH is not supported.
    
    Signed-off-by: Chengwen Feng <fengchengwen@huawei.com>
    Acked-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/pci/tph.c
+++ drivers/pci/tph.c
@@ -145,15 +145,27 @@ static void set_ctrl_reg_req_en(struct pci_dev *pdev, u8 req_type)
 	pci_write_config_dword(pdev, pdev->tph_cap + PCI_TPH_CTRL, reg);
 }
 
-static u8 get_st_modes(struct pci_dev *pdev)
+/**
+ * pcie_tph_get_st_modes - Get supported Steering Tag modes
+ * @pdev: PCI device to query
+ *
+ * Return:
+ *  Bitmask of supported ST modes (PCI_TPH_CAP_ST_NS, PCI_TPH_CAP_ST_IV,
+ *                                 PCI_TPH_CAP_ST_DS)
+ */
+u8 pcie_tph_get_st_modes(struct pci_dev *pdev)
 {
 	u32 reg;
+
+	if (!pdev->tph_cap)
+		return 0;
 
 	pci_read_config_dword(pdev, pdev->tph_cap + PCI_TPH_CAP, &reg);
 	reg &= PCI_TPH_CAP_ST_NS | PCI_TPH_CAP_ST_IV | PCI_TPH_CAP_ST_DS;
 
 	return reg;
 }
+EXPORT_SYMBOL(pcie_tph_get_st_modes);
 
 /**
  * pcie_tph_get_st_table_loc - Return the device's ST table location
@@ -167,6 +179,9 @@ static u8 get_st_modes(struct pci_dev *pdev)
 u32 pcie_tph_get_st_table_loc(struct pci_dev *pdev)
 {
 	u32 reg;
+
+	if (!pdev->tph_cap)
+		return PCI_TPH_LOC_NONE;
 
 	pci_read_config_dword(pdev, pdev->tph_cap + PCI_TPH_CAP, &reg);
 
@@ -395,7 +410,7 @@ int pcie_enable_tph(struct pci_dev *pdev, int mode)
 
 	/* Sanitize and check ST mode compatibility */
 	mode &= PCI_TPH_CTRL_MODE_SEL_MASK;
-	dev_modes = get_st_modes(pdev);
+	dev_modes = pcie_tph_get_st_modes(pdev);
 	if (!((1 << mode) & dev_modes))
 		return -EINVAL;
 
