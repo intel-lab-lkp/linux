bf1535b1c1f98dced55621647e029d846addc5a8
Sean Christopherson <seanjc@google.com>
2026-05-15 12:19:19 -0700

    x86/paravirt: Pass sched_clock save/restore helpers during registration
    Pass in a PV clock's save/restore helpers when configuring sched_clock
    instead of relying on each PV clock to manually set the save/restore hooks.
    In addition to bringing sanity to the code, this will allow gracefully
    "rejecting" a PV sched_clock, e.g. when running as a CoCo guest that has
    access to a "secure" TSC.
    
    No functional change intended.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kernel/kvmclock.c
+++ arch/x86/kernel/kvmclock.c
@@ -137,7 +137,9 @@ static void kvm_restore_sched_clock_state(void)
 static inline void kvm_sched_clock_init(bool stable)
 {
 	kvm_sched_clock_offset = kvm_clock_read();
-	__paravirt_set_sched_clock(kvm_sched_clock_read, stable);
+	__paravirt_set_sched_clock(kvm_sched_clock_read, stable,
+				   kvm_save_sched_clock_state,
+				   kvm_restore_sched_clock_state);
 
 	pr_info("kvm-clock: using sched offset of %llu cycles",
 		kvm_sched_clock_offset);
@@ -344,8 +346,6 @@ void __init kvmclock_init(void)
 #ifdef CONFIG_SMP
 	x86_cpuinit.early_percpu_clock_init = kvm_setup_secondary_clock;
 #endif
-	x86_platform.save_sched_clock_state = kvm_save_sched_clock_state;
-	x86_platform.restore_sched_clock_state = kvm_restore_sched_clock_state;
 	kvm_get_preset_lpj();
 
 	/*
