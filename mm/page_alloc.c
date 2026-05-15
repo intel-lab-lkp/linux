b4a12498ed575c187a91e731beca51de7d860c45
Byungchul Park <byungchul@sk.com>
2026-05-15 12:47:01 +0900

    Revert "mm: introduce a new page type for page pool in page type"
    This reverts commit db359fccf212e7fa3136e6edbed6228475646fd7.
    
    Netpp page_type'ed pages might be used in mapping so as to use
    @_mapcount.  However, since @page_type and @_mapcount are union'ed in
    struct page, these two can't be used at the same time.  Revert the
    commit introducing page_type for Netpp for now.
    
    The patch will be retried once @page_type and @_mapcount get allowed to
    be used at the same time.
    
    The revert also includes removal of @page_type initialization part
    introduced by commit 735a309b4bfb9e ("net: add net_iov_init() and use it
    to initialize ->page_type"), which will be restored on the retry.
    
    Reported-by: Dragos Tatulea <dtatulea@nvidia.com>
    Closes: https://lore.kernel.org/all/982b9bc1-0a0a-4fc5-8e3a-3672db2b29a1@nvidia.com
    Signed-off-by: Byungchul Park <byungchul@sk.com>

--- mm/page_alloc.c
+++ mm/page_alloc.c
@@ -1046,6 +1046,7 @@ static inline bool page_expected_state(struct page *page,
 #ifdef CONFIG_MEMCG
 			page->memcg_data |
 #endif
+			page_pool_page_is_pp(page) |
 			(page->flags.f & check_flags)))
 		return false;
 
@@ -1072,6 +1073,8 @@ static const char *page_bad_reason(struct page *page, unsigned long flags)
 	if (unlikely(page->memcg_data))
 		bad_reason = "page still charged to cgroup";
 #endif
+	if (unlikely(page_pool_page_is_pp(page)))
+		bad_reason = "page_pool leak";
 	return bad_reason;
 }
 
@@ -1395,17 +1398,9 @@ static __always_inline bool __free_pages_prepare(struct page *page,
 		mod_mthp_stat(order, MTHP_STAT_NR_ANON, -1);
 		folio->mapping = NULL;
 	}
-	if (unlikely(page_has_type(page))) {
-		/* networking expects to clear its page type before releasing */
-		if (is_check_pages_enabled()) {
-			if (unlikely(PageNetpp(page))) {
-				bad_page(page, "page_pool leak");
-				return false;
-			}
-		}
+	if (unlikely(page_has_type(page)))
 		/* Reset the page_type (which overlays _mapcount) */
 		page->page_type = UINT_MAX;
-	}
 
 	if (is_check_pages_enabled()) {
 		if (free_page_is_bad(page))
