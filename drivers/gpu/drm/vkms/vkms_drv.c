dad5eeec5fed705c1737505d0ffec2f9e21666e7
Thomas Zimmermann <tzimmermann@suse.de>
2026-05-15 13:55:14 +0200

    drm/vkms: Set DRM_VBLANK_FLAG_SIMULATED
    Mark the vblank event on vkms as simulated, so that the WAIT_VBLANK
    ioctl fails with an error. The ioctl should not be supported because
    the output is not synchronized to a display refresh.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>

--- drivers/gpu/drm/vkms/vkms_drv.c
+++ drivers/gpu/drm/vkms/vkms_drv.c
@@ -192,8 +192,8 @@ int vkms_create(struct vkms_config *config)
 		goto out_devres;
 	}
 
-	ret = drm_vblank_init(&vkms_device->drm,
-			      vkms_config_get_num_crtcs(config));
+	ret = drmm_vblank_init(&vkms_device->drm, vkms_config_get_num_crtcs(config),
+			       DRM_VBLANK_FLAG_SIMULATED);
 	if (ret) {
 		DRM_ERROR("Failed to vblank\n");
 		goto out_devres;
