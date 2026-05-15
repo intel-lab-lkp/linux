dc8e7a8a9d7d337d552b477c820de33565d3f329
Chengwen Feng <fengchengwen@huawei.com>
2026-05-15 17:40:38 +0800

    PCI/TPH: Add pcie_tph_enabled_mode() helper
    Add a helper to query enabled TPH mode on a PCI device. This is useful for
    drivers like VFIO-PCI that need to validate TPH state before allowing
    access to steering tag tables.
    
    Signed-off-by: Chengwen Feng <fengchengwen@huawei.com>

--- drivers/pci/tph.c
+++ drivers/pci/tph.c
@@ -451,6 +451,18 @@ int pcie_enable_tph(struct pci_dev *pdev, int mode)
 }
 EXPORT_SYMBOL(pcie_enable_tph);
 
+/**
+ * pcie_tph_enabled_mode - Get current enabled TPH mode
+ * @pdev: PCI device
+ *
+ * Return the enabled TPH mode (IV/DS) or 0 if disabled.
+ */
+int pcie_tph_enabled_mode(struct pci_dev *pdev)
+{
+	return pdev->tph_enabled ? pdev->tph_mode : 0;
+}
+EXPORT_SYMBOL(pcie_tph_enabled_mode);
+
 void pci_restore_tph_state(struct pci_dev *pdev)
 {
 	struct pci_cap_saved_state *save_state;
