63fd32824645ae4ed6eef9f8305cbc671574fdf6
Sanjay Chitroda <sanjayembeddedse@gmail.com>
2026-05-15 23:10:07 +0530

    iio: ssp_sensors: factor out pending list add/remove helpers
    The SSP SPI transfer path manipulates the pending message list in
    multiple places, open-coding the same locking and list operations.
    
    Refactor the pending list add and remove logic into helper functions.
    This reduces duplication and simplifies the transfer flow by
    centralizing the pending list handling.
    
    No functional change intended.
    
    Signed-off-by: Sanjay Chitroda <sanjayembeddedse@gmail.com>
    Suggested-by: Andy Shevchenko <andriy.shevchenko@intel.com>

--- drivers/iio/common/ssp_sensors/ssp_spi.c
+++ drivers/iio/common/ssp_sensors/ssp_spi.c
@@ -174,6 +174,28 @@ static int ssp_check_lines(struct ssp_data *data, bool state)
 	return 0;
 }
 
+static inline void ssp_pending_add(struct ssp_data *data, struct ssp_msg *msg,
+				   bool no_irq)
+{
+	if (no_irq)
+		return;
+
+	mutex_lock(&data->pending_lock);
+	list_add_tail(&msg->list, &data->pending_list);
+	mutex_unlock(&data->pending_lock);
+}
+
+static inline void ssp_pending_del(struct ssp_data *data, struct ssp_msg *msg,
+				   bool no_irq)
+{
+	if (no_irq)
+		return;
+
+	mutex_lock(&data->pending_lock);
+	list_del(&msg->list);
+	mutex_unlock(&data->pending_lock);
+}
+
 static int ssp_do_transfer(struct ssp_data *data, struct ssp_msg *msg,
 			   struct completion *done, int timeout)
 {
@@ -202,35 +224,23 @@ static int ssp_do_transfer(struct ssp_data *data, struct ssp_msg *msg,
 		goto _error_locked;
 	}
 
-	if (!use_no_irq) {
-		mutex_lock(&data->pending_lock);
-		list_add_tail(&msg->list, &data->pending_list);
-		mutex_unlock(&data->pending_lock);
-	}
+	ssp_pending_add(data, msg, use_no_irq);
 
 	status = ssp_check_lines(data, true);
 	if (status < 0) {
-		if (!use_no_irq) {
-			mutex_lock(&data->pending_lock);
-			list_del(&msg->list);
-			mutex_unlock(&data->pending_lock);
-		}
+		ssp_pending_del(data, msg, use_no_irq);
 		goto _error_locked;
 	}
 
 	mutex_unlock(&data->comm_lock);
 
-	if (!use_no_irq && done)
-		if (wait_for_completion_timeout(done,
-						msecs_to_jiffies(timeout)) ==
-		    0) {
-			mutex_lock(&data->pending_lock);
-			list_del(&msg->list);
-			mutex_unlock(&data->pending_lock);
+	if (!use_no_irq && done &&
+	    !wait_for_completion_timeout(done, msecs_to_jiffies(timeout))) {
+		ssp_pending_del(data, msg, use_no_irq);
 
-			data->timeout_cnt++;
-			return -ETIMEDOUT;
-		}
+		data->timeout_cnt++;
+		return -ETIMEDOUT;
+	}
 
 	return 0;
 
