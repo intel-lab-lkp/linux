8a43aef57a5ec8aa350e831bd3f31076441de547
Johannes Thumshirn <johannes.thumshirn@wdc.com>
2026-05-18 11:55:03 +0200

    btrfs: zoned: always set max_active_zones for zoned devices
    When a block device does not report a maximum number of open or active
    zones, btrfs currently assigns BTRFS_DEFAULT_MAX_ACTIVE_ZONES (128) to the
    internal limit, if the device has more than BTRFS_DEFAULT_MAX_ACTIVE_ZONES
    zones.
    
    But if the device has less than BTRFS_DEFAULT_MAX_ACTIVE_ZONES the
    internal max_active_zones limit will stay at 0.
    
    If a device doesn't report a number of max open or active zones and it has
    less than BTRFS_DEFAULT_MAX_ACTIVE_ZONES, set the internal limit to the
    number of zones on the device divided by 4.
    
    Fixes: 04147d8394e8 ("btrfs: zoned: limit active zones to max_open_zones")
    Signed-off-by: Johannes Thumshirn <johannes.thumshirn@wdc.com>

--- fs/btrfs/zoned.c
+++ fs/btrfs/zoned.c
@@ -426,8 +426,10 @@ int btrfs_get_dev_zone_info(struct btrfs_device *device, bool populate_cache)
 
 	max_active_zones = min_not_zero(bdev_max_active_zones(bdev),
 					bdev_max_open_zones(bdev));
-	if (!max_active_zones && zone_info->nr_zones > BTRFS_DEFAULT_MAX_ACTIVE_ZONES)
-		max_active_zones = BTRFS_DEFAULT_MAX_ACTIVE_ZONES;
+	if (!max_active_zones)
+		max_active_zones = min(zone_info->nr_zones / 4,
+				       BTRFS_DEFAULT_MAX_ACTIVE_ZONES);
+
 	if (max_active_zones && max_active_zones < BTRFS_MIN_ACTIVE_ZONES) {
 		btrfs_err(fs_info,
 "zoned: %s: max active zones %u is too small, need at least %u active zones",
