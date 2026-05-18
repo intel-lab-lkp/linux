2f41ccdb4fae63e94a7e51487ffb170b4d017db2
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2026-05-18 10:42:06 +0530

    drm/i915/dp: Allow AS SDP only if v2 is supported
    We do not support AS SDP version 1, so allow AS SDP only if AS SDP v2 is
    supported.
    
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>

--- drivers/gpu/drm/i915/display/intel_dp.c
+++ drivers/gpu/drm/i915/display/intel_dp.c
@@ -3172,7 +3172,7 @@ static void intel_dp_compute_vsc_colorimetry(const struct intel_crtc_state *crtc
 static bool intel_dp_needs_as_sdp(struct intel_dp *intel_dp,
 				  struct intel_crtc_state *crtc_state)
 {
-	if (!intel_dp->as_sdp_supported)
+	if (!intel_dp->as_sdp_v2_supported)
 		return false;
 
 	/*
