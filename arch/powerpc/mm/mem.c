d8ef40fb640e18493c311e9f4057f4121d578afe
David Christensen <drc@linux.ibm.com>
2026-05-19 17:43:52 -0500

    powerpc: remove ehea driver references
    Follow-on cleanup after the removal of the IBM eHEA driver in commit
    XXXXXXXXXXXX ("net: ethernet: ibm: remove the ehea driver").
    
    Remove the CONFIG_IBM_EHEA entry from ppc64_defconfig and the obsolete
    comment in arch/powerpc/mm/mem.c that referenced the driver.
    
    Signed-off-by: David Christensen <drc@linux.ibm.com>

--- arch/powerpc/mm/mem.c
+++ arch/powerpc/mm/mem.c
@@ -370,10 +370,6 @@ int devmem_is_allowed(unsigned long pfn)
 }
 #endif /* CONFIG_STRICT_DEVMEM */
 
-/*
- * This is defined in kernel/resource.c but only powerpc needs to export it, for
- * the EHEA driver. Drop this when drivers/net/ethernet/ibm/ehea is removed.
- */
 EXPORT_SYMBOL_GPL(walk_system_ram_range);
 
 #ifdef CONFIG_EXECMEM
