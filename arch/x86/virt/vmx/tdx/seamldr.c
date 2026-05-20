eb6cd4a9845d8077972be30b5752f14b6fc964be
Chao Gao <chao.gao@intel.com>
2026-05-20 06:38:22 -0700

    x86/virt/seamldr: Do TDX global and per-CPU init after module installation
    After installing a new TDX module, the kernel must re-initialize TDX
    before resuming TDX operations.
    
    This post-update initialization differs from the initial boot-time
    initialization. It only needs TDX global initialization, TDX per-CPU
    initialization, and restoration of TDX state from the handoff data.
    
    tdx_cpu_enable() covers the global and per-CPU initialization. Export it
    and invoke it on all CPUs.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>
    Reviewed-by: Xu Yilun <yilun.xu@linux.intel.com>
    Reviewed-by: Tony Lindgren <tony.lindgren@linux.intel.com>
    Reviewed-by: Kai Huang <kai.huang@intel.com>
    Reviewed-by: Kiryl Shutsemau (Meta) <kas@kernel.org>
    Reviewed-by: Rick Edgecombe <rick.p.edgecombe@intel.com>

--- arch/x86/virt/vmx/tdx/seamldr.c
+++ arch/x86/virt/vmx/tdx/seamldr.c
@@ -188,6 +188,7 @@ enum module_update_state {
 	MODULE_UPDATE_START,
 	MODULE_UPDATE_SHUTDOWN,
 	MODULE_UPDATE_CPU_INSTALL,
+	MODULE_UPDATE_CPU_INIT,
 	MODULE_UPDATE_DONE,
 };
 
@@ -264,6 +265,9 @@ static int do_seamldr_install_module(void *seamldr_params)
 			break;
 		case MODULE_UPDATE_CPU_INSTALL:
 			ret = seamldr_install(seamldr_params);
+			break;
+		case MODULE_UPDATE_CPU_INIT:
+			ret = tdx_cpu_enable();
 			break;
 		default:
 			break;
