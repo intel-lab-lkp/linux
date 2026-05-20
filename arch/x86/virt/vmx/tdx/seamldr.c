398accef0f13e58b3b2ecd137360686e54d21b91
Chao Gao <chao.gao@intel.com>
2026-05-20 06:38:23 -0700

    x86/virt/tdx: Restore TDX module state
    TDX module state was packed as handoff data during module shutdown. After
    per-CPU initialization, the new module can restore TDX module state from
    handoff data to preserve running TDs.
    
    Once the restoration is done, the TDX module update is complete, which
    means the new module is ready to handle requests from the host and guests.
    
    Implement the new TDH.SYS.UPDATE SEAMCALL to restore TDX module state
    and invoke it on one CPU since it only needs to be called once.
    
    For error handling, Intel® Trust Domain Extensions (Intel® TDX)
    Module Base Architecture Specification, Chapter "Restore TDX Module
    State after a TD-Preserving Update" states
    
      If TDH.SYS.UPDATE returns an error, then the host VMM can continue
      with the non-update sequence (TDH.SYS.CONFIG, TDH.SYS.KEY.CONFIG
      etc.). In this case all existing TDs are lost. Alternatively, the host
      VMM can request the P-SEAMLDR to update to another TDX module. If that
      update is successful, existing TDs are preserved.
    
    No error is expected if the new module is fully compatible, and userspace
    should enforce that as part of update image selection.  Given the
    complexity and uncertain value of the recovery paths above, simply
    propagate errors.
    
    Note: the location and the format of handoff data is defined by
    the TDX module. The new module knows where to get handoff data and how
    to parse it. The kernel doesn't need to provide its location, format etc.
    
    Signed-off-by: Chao Gao <chao.gao@intel.com>
    Reviewed-by: Tony Lindgren <tony.lindgren@linux.intel.com>
    Reviewed-by: Kai Huang <kai.huang@intel.com>
    Reviewed-by: Kiryl Shutsemau (Meta) <kas@kernel.org>
    Reviewed-by: Rick Edgecombe <rick.p.edgecombe@intel.com>

--- arch/x86/virt/vmx/tdx/seamldr.c
+++ arch/x86/virt/vmx/tdx/seamldr.c
@@ -189,6 +189,7 @@ enum module_update_state {
 	MODULE_UPDATE_SHUTDOWN,
 	MODULE_UPDATE_CPU_INSTALL,
 	MODULE_UPDATE_CPU_INIT,
+	MODULE_UPDATE_RUN_UPDATE,
 	MODULE_UPDATE_DONE,
 };
 
@@ -268,6 +269,10 @@ static int do_seamldr_install_module(void *seamldr_params)
 			break;
 		case MODULE_UPDATE_CPU_INIT:
 			ret = tdx_cpu_enable();
+			break;
+		case MODULE_UPDATE_RUN_UPDATE:
+			if (is_lead_cpu)
+				ret = tdx_module_run_update();
 			break;
 		default:
 			break;
