0735ab545f0f755916880c8533db83f86253c05a
Mauricio Faria de Oliveira <mfo@igalia.com>
2026-05-20 18:12:06 -0300

    x86/asm, x86/boot: expose inline memcmp
    Move the inline memcmp function currently only available in 'boot/string.c'
    and its users (eg, 'boot/compressed/string.c' and 'purgatory/purgatory.ro')
    into the inline string functions header <asm/string_inline.h> to be reused.
    
    Note that the inline memcmp() returns 0/1, not -1/0/1 as regular memcmp()
    (reported by David Laight <david.laight.linux@gmail.com>), which is not
    caused or changed by this commit; this will be addressed separately.
    
    Signed-off-by: Mauricio Faria de Oliveira <mfo@igalia.com>

--- arch/x86/include/asm/string_inline.h
+++ arch/x86/include/asm/string_inline.h
@@ -23,4 +23,15 @@ static __always_inline void *__inline_memset(void *s, int v, size_t n)
 	return ret;
 }
 
+/* Note: this memcmp() returns 0/1, not -1/0/1 as regular memcmp(). */
+static __always_inline int __inline_memcmp(const void *s1, const void *s2, size_t len)
+{
+	bool diff;
+
+	asm("repe cmpsb"
+	    : "=@ccnz" (diff), "+D" (s1), "+S" (s2), "+c" (len));
+
+	return diff;
+}
+
 #endif /* _ASM_X86_STRING_INLINE_H */
