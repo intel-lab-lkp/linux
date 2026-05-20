e2381aa0b1c7d6b24b20169c807e672e978f7db1
Alexander A. Klimov <grandmaster@al2klimov.de>
2026-05-20 20:03:57 +0200

    scsi: qla2xxx: fix NULL deref, check user input
    qla2x00_dfs_fce_write() did this:
    
        OUTPUT = kstrtoul(INPUT, BASE, 0);
    
    Whenever INPUT was successfully parsed, kstrtoul() wrote its output
    to *(unsigned long*)0. Otherwise, OUTPUT was set to an error value.
    I added proper error handling and call kstrtoul() as expected now:
    
        ERROR = kstrtoul(INPUT, BASE, &OUTPUT);
    
    Fixes: 841df27d619e ("scsi: qla2xxx: Move FCE Trace buffer allocation to user control")
    Signed-off-by: Alexander A. Klimov <grandmaster@al2klimov.de>

--- drivers/scsi/qla2xxx/qla_dfs.c
+++ drivers/scsi/qla2xxx/qla_dfs.c
@@ -510,7 +510,14 @@ qla2x00_dfs_fce_write(struct file *file, const char __user *buffer,
 		return PTR_ERR(buf);
 	}
 
-	enable = kstrtoul(buf, 0, 0);
+	rc = kstrtoul(buf, 0, &enable);
+	if (rc) {
+		ql_dbg(ql_dbg_user, vha, 0xd03d,
+		    "fail to parse user input.");
+		rc = -EINVAL;
+		goto out_free;
+	}
+
 	rc = count;
 
 	mutex_lock(&ha->fce_mutex);
