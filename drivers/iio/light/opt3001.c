fc2ac8e284519c03437bbeada066cce63e0742c5
Joshua Crofts <joshua.crofts1@gmail.com>
2026-05-21 11:55:44 +0200

    iio: light: opt3001: add comment to mutex
    Add comment to mutex per checkpatch.pl report.
    
    No functional change.
    
    Reviewed-by: Maxwell Doose <m32285159@gmail.com>
    Signed-off-by: Joshua Crofts <joshua.crofts1@gmail.com>

--- drivers/iio/light/opt3001.c
+++ drivers/iio/light/opt3001.c
@@ -103,6 +103,7 @@ struct opt3001_chip_info {
 struct opt3001 {
 	struct i2c_client	*client;
 
+	/* Mutex for ensuring one executed command at a time */
 	struct mutex		lock;
 	bool			ok_to_ignore_lock;
 	bool			result_ready;
