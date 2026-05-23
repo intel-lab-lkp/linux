6fb5253c6f19e6fc67113424cb14a8e5f06081bb
Rodrigo Gobbi <rodrigo.gobbi.7@gmail.com>
2026-05-23 11:18:43 -0300

    staging: rtl8723bs: fix unnamed parameters warning detected at checkpatch
    Detected the following WARNING: function definition argument
    'struct adapter *' should also have an identifier name.
    Add explicit names on those definitions.
    
    Signed-off-by: Rodrigo Gobbi <rodrigo.gobbi.7@gmail.com>

--- drivers/staging/rtl8723bs/include/rtw_btcoex.h
+++ drivers/staging/rtl8723bs/include/rtw_btcoex.h
@@ -15,14 +15,14 @@
 #define	PACKET_ARP				2
 #define	PACKET_EAPOL			3
 
-void rtw_btcoex_MediaStatusNotify(struct adapter *, u8 mediaStatus);
-void rtw_btcoex_HaltNotify(struct adapter *);
+void rtw_btcoex_MediaStatusNotify(struct adapter *adapter, u8 mediaStatus);
+void rtw_btcoex_HaltNotify(struct adapter *adapter);
 
 /*  ================================================== */
 /*  Below Functions are called by BT-Coex */
 /*  ================================================== */
-void rtw_btcoex_RejectApAggregatedPacket(struct adapter *, u8 enable);
-void rtw_btcoex_LPS_Enter(struct adapter *);
-void rtw_btcoex_LPS_Leave(struct adapter *);
+void rtw_btcoex_RejectApAggregatedPacket(struct adapter *adapter, u8 enable);
+void rtw_btcoex_LPS_Enter(struct adapter *adapter);
+void rtw_btcoex_LPS_Leave(struct adapter *adapter);
 
 #endif /*  __RTW_BTCOEX_H__ */
