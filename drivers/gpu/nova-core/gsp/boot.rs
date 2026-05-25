c916e5ddb0df2e6fa1e242d3a26438d917e0d82a
Danilo Krummrich <dakr@kernel.org>
2026-05-26 00:58:32 +0200

    gpu: nova-core: gsp: replace ARef<Device> with &'a Device in sequencer
    GspSequencer, GspSeqIter, and GspSequencerParams are already
    lifetime-parameterized; the ARef<Device> is unnecessary -- a plain
    &'a Device reference suffices.
    
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- drivers/gpu/nova-core/gsp/boot.rs
+++ drivers/gpu/nova-core/gsp/boot.rs
@@ -220,7 +220,7 @@ impl super::Gsp {
             libos_dma_handle: libos_handle,
             gsp_falcon,
             sec2_falcon,
-            dev: pdev.as_ref().into(),
+            dev,
             bar,
         };
         GspSequencer::run(&self.cmdq, seq_params)?;
