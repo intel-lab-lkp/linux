e306afe6b099972bc71acaf92ff6fec58481b45c
Matthew Wilcox (Oracle) <willy@infradead.org>
2026-05-25 18:19:13 +0100

    ocfs2: Convert ocfs2_read_blocks to bh_submit()
    Avoid an extra indirect function call by using bh_submit() instead of
    submit_bh().
    
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: ocfs2-devel@lists.linux.dev

--- fs/ocfs2/buffer_head_io.c
+++ fs/ocfs2/buffer_head_io.c
@@ -321,11 +321,10 @@ int ocfs2_read_blocks(struct ocfs2_caching_info *ci, u64 block, int nr,
 				continue;
 			}
 
-			get_bh(bh); /* for end_buffer_read_sync() */
+			get_bh(bh); /* for bh_end_read() */
 			if (validate)
 				set_buffer_needs_validate(bh);
-			bh->b_end_io = end_buffer_read_sync;
-			submit_bh(REQ_OP_READ, bh);
+			bh_submit(bh, REQ_OP_READ, bh_end_read);
 			continue;
 		}
 	}
