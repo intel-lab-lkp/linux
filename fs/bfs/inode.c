4a4156334fea9688f2c88856ec86790d7249aad8
Jan Kara <jack@suse.cz>
2026-05-25 10:58:14 +0200

    bfs: Fix possibly missing inode write on fsync(2)
    Use mmb inode buffer writeout infrastructure to reliably write out
    inode's buffer on fsync(2).
    
    Signed-off-by: Jan Kara <jack@suse.cz>

--- fs/bfs/inode.c
+++ fs/bfs/inode.c
@@ -136,7 +136,6 @@ static int bfs_write_inode(struct inode *inode, struct writeback_control *wbc)
 	unsigned long i_sblock;
 	struct bfs_inode *di;
 	struct buffer_head *bh;
-	int err = 0;
 
 	dprintf("ino=%08x\n", ino);
 
@@ -164,15 +163,10 @@ static int bfs_write_inode(struct inode *inode, struct writeback_control *wbc)
 	di->i_eblock = cpu_to_le32(BFS_I(inode)->i_eblock);
 	di->i_eoffset = cpu_to_le32(i_sblock * BFS_BSIZE + inode->i_size - 1);
 
-	mark_buffer_dirty(bh);
-	if (wbc->sync_mode == WB_SYNC_ALL) {
-		sync_dirty_buffer(bh);
-		if (buffer_req(bh) && !buffer_uptodate(bh))
-			err = -EIO;
-	}
+	mmb_mark_inode_buffer_dirty(bh, &BFS_I(inode)->i_metadata_bhs);
 	brelse(bh);
 	mutex_unlock(&info->bfs_lock);
-	return err;
+	return 0;
 }
 
 static void bfs_evict_inode(struct inode *inode)
