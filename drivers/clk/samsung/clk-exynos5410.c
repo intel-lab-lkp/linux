42ee43e6305a269ba216ec970f2a8ace522d2292
Alexander A. Klimov <grandmaster@al2klimov.de>
2026-05-26 08:13:12 +0200

    clk: samsung: exynos5410: fix refcount leak
    Every value returned from of_clk_get() is supposed to be cleaned up
    via clk_put() once not needed anymore.
    
    Fixes: be95d2c7d918 ("clk: samsung: Add support for EPLL on exynos5410")
    Signed-off-by: Alexander A. Klimov <grandmaster@al2klimov.de>

--- drivers/clk/samsung/clk-exynos5410.c
+++ drivers/clk/samsung/clk-exynos5410.c
@@ -269,8 +269,12 @@ static void __init exynos5410_clk_init(struct device_node *np)
 {
 	struct clk *xxti = of_clk_get(np, 0);
 
-	if (!IS_ERR(xxti) && clk_get_rate(xxti) == 24 * MHZ)
-		exynos5410_plls[epll].rate_table = exynos5410_pll2550x_24mhz_tbl;
+	if (!IS_ERR(xxti)) {
+		if (clk_get_rate(xxti) == 24 * MHZ)
+			exynos5410_plls[epll].rate_table =
+				exynos5410_pll2550x_24mhz_tbl;
+		clk_put(xxti);
+	}
 
 	samsung_cmu_register_one(np, &cmu);
 
