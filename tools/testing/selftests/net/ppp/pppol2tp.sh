7850b80fb2bf34f373e60e9b2ecf5d0db8bc3649
Qingfang Deng <qingfang.deng@linux.dev>
2026-05-26 09:41:47 +0800

    selftests: net: add socat syslog for PPPoL2TP
    As done in pppoe.sh, start socat as the syslog listener. In case the
    test fails, dump its log to see what's going on.
    
    Signed-off-by: Qingfang Deng <qingfang.deng@linux.dev>

--- tools/testing/selftests/net/ppp/pppol2tp.sh
+++ tools/testing/selftests/net/ppp/pppol2tp.sh
@@ -9,10 +9,12 @@ OUTER_IP_SERVER="172.16.1.1"
 OUTER_IP_CLIENT="172.16.1.2"
 
 PPPOL2TP_DIR=$(mktemp -d /tmp/pppol2tp.XXXXXX)
+PPPOL2TP_LOG="$PPPOL2TP_DIR/l2tp.log"
 
 # shellcheck disable=SC2329
 cleanup() {
 	cleanup_all_ns
+	[ -n "$SOCAT_PID" ] && kill_process "$SOCAT_PID"
 	rm -rf "$PPPOL2TP_DIR"
 }
 
@@ -30,6 +32,10 @@ ip -netns "$NS_SERVER" link set "$VETH_SERVER" up
 ip -netns "$NS_CLIENT" link set "$VETH_CLIENT" up
 ip -netns "$NS_SERVER" address add dev "$VETH_SERVER" "$OUTER_IP_SERVER" peer "$OUTER_IP_CLIENT"
 ip -netns "$NS_CLIENT" address add dev "$VETH_CLIENT" "$OUTER_IP_CLIENT" peer "$OUTER_IP_SERVER"
+
+# Start socat as syslog listener
+socat -v -u UNIX-RECV:/dev/log OPEN:/dev/null > "$PPPOL2TP_LOG" 2>&1 &
+SOCAT_PID=$!
 
 # Generate configuration files
 cat > "$PPPOL2TP_DIR/l2tp-server.conf" <<EOF
@@ -79,6 +85,15 @@ echo "c server" > "$PPPOL2TP_DIR/l2tp-client.control"
 ppp_test_connectivity
 
 log_test "PPPoL2TP"
+
+# Dump syslog messages if the test failed
+if [ "$RET" -ne 0 ]; then
+	while read -r _sign _date _time len _from _to
+	do      len=${len##*=}
+		read -n "$len" -r LINE
+		echo "$LINE"
+	done < "$PPPOL2TP_LOG"
+fi
 
 # Recursion test
 RET=0
