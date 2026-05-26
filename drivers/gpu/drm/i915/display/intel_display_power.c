d197f824d1e8aeb128fa3150b1fa3bfda1707c4f
Dibin Moolakadan Subrahmanian <dibin.moolakadan.subrahmanian@intel.com>
2026-05-27 00:48:15 +0530

    drm/i915/display: Switch DC3CO enable from standalone bit to DC level encoding
    On platforms prior to xe3, DC3CO was controlled via a standalone enable
    bit. Starting with xe3, DC3CO is encoded as part of the existing
    DC_STATE_EN_UPTO_DC* field.
    
    No functional change, as DC3CO is not enabled on platforms prior to xe3.
    
    Changes in v2:
    - Update commit header (Uma Shankar)
    
    Signed-off-by: Dibin Moolakadan Subrahmanian <dibin.moolakadan.subrahmanian@intel.com>
    Reviewed-by: Uma Shankar <uma.shankar@intel.com>

--- drivers/gpu/drm/i915/display/intel_display_power.c
+++ drivers/gpu/drm/i915/display/intel_display_power.c
@@ -267,7 +267,7 @@ sanitize_target_dc_state(struct intel_display *display,
 	static const u32 states[] = {
 		DC_STATE_EN_UPTO_DC6,
 		DC_STATE_EN_UPTO_DC5,
-		DC_STATE_EN_DC3CO,
+		DC_STATE_EN_UPTO_DC3CO,
 		DC_STATE_DISABLE,
 	};
 	int i;
@@ -999,10 +999,10 @@ static u32 get_allowed_dc_mask(struct intel_display *display, int enable_dc)
 
 	switch (requested_dc) {
 	case 4:
-		mask |= DC_STATE_EN_DC3CO | DC_STATE_EN_UPTO_DC6;
+		mask |= DC_STATE_EN_UPTO_DC3CO | DC_STATE_EN_UPTO_DC6;
 		break;
 	case 3:
-		mask |= DC_STATE_EN_DC3CO | DC_STATE_EN_UPTO_DC5;
+		mask |= DC_STATE_EN_UPTO_DC3CO | DC_STATE_EN_UPTO_DC5;
 		break;
 	case 2:
 		mask |= DC_STATE_EN_UPTO_DC6;
