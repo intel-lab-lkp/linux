1d44de478a1187eef33667b82b7bdea36e686218
Uwe Kleine-König (The Capable Hub) <u.kleine-koenig@baylibre.com>
2026-05-26 16:17:31 +0200

    video: fm2fb: Use named initializer for zorro_device_id array
    Using named initializers is more explicit and thus easier to parse for a
    human.
    
    While touching this array, drop the explicit zero from the list terminator.
    
    This change doesn't introduce changes to the compiled zorro_device_id
    array.
    
    Signed-off-by: Uwe Kleine-König (The Capable Hub) <u.kleine-koenig@baylibre.com>

--- drivers/video/fbdev/fm2fb.c
+++ drivers/video/fbdev/fm2fb.c
@@ -212,9 +212,9 @@ static int fm2fb_setcolreg(u_int regno, u_int red, u_int green, u_int blue,
 static int fm2fb_probe(struct zorro_dev *z, const struct zorro_device_id *id);
 
 static const struct zorro_device_id fm2fb_devices[] = {
-	{ ZORRO_PROD_BSC_FRAMEMASTER_II },
-	{ ZORRO_PROD_HELFRICH_RAINBOW_II },
-	{ 0 }
+	{ .id = ZORRO_PROD_BSC_FRAMEMASTER_II },
+	{ .id = ZORRO_PROD_HELFRICH_RAINBOW_II },
+	{ }
 };
 MODULE_DEVICE_TABLE(zorro, fm2fb_devices);
 
