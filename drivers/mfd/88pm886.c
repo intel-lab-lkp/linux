9b50caa3134b35678e427c34e3b447a3bae2566c
Duje Mihanović <duje@dujemihanovic.xyz>
2026-05-26 21:14:48 +0200

    mfd: 88pm886: Initialize battery page
    Initialize the PMIC's battery page. The battery page registers are
    shared between Vbus regulator, charger, fuelgauge and camera flash
    blocks, hence the commonization of the page.
    
    Signed-off-by: Duje Mihanović <duje@dujemihanovic.xyz>

--- drivers/mfd/88pm886.c
+++ drivers/mfd/88pm886.c
@@ -16,6 +16,12 @@ static const struct regmap_config pm886_regmap_config = {
 	.max_register = PM886_REG_RTC_SPARE6,
 };
 
+static const struct regmap_config pm886_regmap_battery_config = {
+	.reg_bits = 8,
+	.val_bits = 8,
+	.max_register = PM886_REG_CLS_CONFIG1,
+};
+
 static const struct regmap_irq pm886_regmap_irqs[] = {
 	REGMAP_IRQ_REG(PM886_IRQ_ONKEY, 0, PM886_INT_ENA1_ONKEY),
 };
@@ -88,7 +94,8 @@ static int pm886_probe(struct i2c_client *client)
 	struct regmap_irq_chip_data *irq_data;
 	struct device *dev = &client->dev;
 	struct pm886_chip *chip;
-	struct regmap *regmap;
+	struct regmap *regmap, *regmap_battery;
+	struct i2c_client *battery_page;
 	unsigned int chip_id;
 	int err;
 
@@ -104,6 +111,18 @@ static int pm886_probe(struct i2c_client *client)
 	if (IS_ERR(regmap))
 		return dev_err_probe(dev, PTR_ERR(regmap), "Failed to initialize regmap\n");
 	chip->regmap = regmap;
+
+	battery_page = devm_i2c_new_dummy_device(dev, client->adapter,
+						 client->addr + PM886_PAGE_OFFSET_BATTERY);
+	if (IS_ERR(battery_page))
+		return dev_err_probe(dev, PTR_ERR(battery_page),
+				     "Failed to initialize battery page\n");
+
+	regmap_battery = devm_regmap_init_i2c(battery_page, &pm886_regmap_battery_config);
+	if (IS_ERR(regmap_battery))
+		return dev_err_probe(dev, PTR_ERR(regmap_battery),
+				     "Failed to initialize battery regmap\n");
+	chip->regmap_battery = regmap_battery;
 
 	err = regmap_read(regmap, PM886_REG_ID, &chip_id);
 	if (err)
