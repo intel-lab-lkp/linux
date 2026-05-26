ae59b2cc961e88a61085cb8dab0a0d1adcf6f076
Arnd Bergmann <arnd@arndb.de>
2026-05-26 12:20:38 +0200

    powerpc: export memory encryption helper functions
    The set_memory_encrypted/set_memory_decrypted functions are exported
    on x86 and arm64 but not on powerpc, which leads to a new build failure
    because they are now used in a loadable module:
    
    ERROR: modpost: "set_memory_encrypted" [drivers/dma-buf/heaps/system_heap.ko] undefined!
    ERROR: modpost: "set_memory_decrypted" [drivers/dma-buf/heaps/system_heap.ko] undefined!
    
    Export these the same way we do on the other architectures.
    
    Fixes: fd55edff8a0a ("dma-buf: heaps: system: Turn the heap into a module")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/powerpc/platforms/pseries/svm.c
+++ arch/powerpc/platforms/pseries/svm.c
@@ -6,6 +6,7 @@
  * Author: Anshuman Khandual <khandual@linux.vnet.ibm.com>
  */
 
+#include <linux/export.h>
 #include <linux/mm.h>
 #include <linux/memblock.h>
 #include <linux/mem_encrypt.h>
@@ -50,6 +51,7 @@ int set_memory_encrypted(unsigned long addr, int numpages)
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(set_memory_encrypted);
 
 int set_memory_decrypted(unsigned long addr, int numpages)
 {
@@ -63,6 +65,7 @@ int set_memory_decrypted(unsigned long addr, int numpages)
 
 	return 0;
 }
+EXPORT_SYMBOL_GPL(set_memory_decrypted);
 
 /* There's one dispatch log per CPU. */
 #define NR_DTL_PAGE (DISPATCH_LOG_BYTES * CONFIG_NR_CPUS / PAGE_SIZE)
