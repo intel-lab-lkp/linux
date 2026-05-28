de532a7afbc75a192954163c25543954b980e83a
Claudio Imbrenda <imbrenda@linux.ibm.com>
2026-05-28 13:47:21 +0200

    KVM: s390: Fix _gmap_crstep_xchg_atomic()
    Return false and do not perform the operation if an unshadow event has
    been triggered.
    
    The previous incorrect behaviour cleared the vsie_notif bit without
    returning false, which allowed shadow crstes to be installed without
    the vsie_notif bit.
    
    Signed-off-by: Claudio Imbrenda <imbrenda@linux.ibm.com>
    Fixes: b827ef02f409 ("KVM: s390: Remove non-atomic dat_crstep_xchg()")
    Fixes: a2c17f9270cc ("KVM: s390: New gmap code")

--- arch/s390/kvm/gmap.h
+++ arch/s390/kvm/gmap.h
@@ -273,11 +273,11 @@ static inline bool __must_check _gmap_crstep_xchg_atomic(struct gmap *gmap, unio
 		gmap_unmap_prefix(gmap, gfn, gfn + align);
 	}
 	if (crste_leaf(oldcrste) && crste_needs_unshadow(oldcrste, newcrste)) {
-		newcrste.s.fc1.vsie_notif = 0;
 		if (needs_lock)
 			gmap_handle_vsie_unshadow_event(gmap, gfn);
 		else
 			_gmap_handle_vsie_unshadow_event(gmap, gfn);
+		return false;
 	}
 	if (!oldcrste.s.fc1.d && newcrste.s.fc1.d && !newcrste.s.fc1.s)
 		SetPageDirty(phys_to_page(crste_origin_large(newcrste)));
