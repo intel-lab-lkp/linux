5fe89f78c5a0327e0f73568e1348a053ae73d49b
Pratyush Yadav (Google) <pratyush@kernel.org>
2026-05-27 17:41:46 -0700

    kho: add callback for table pages
    The KHO memory preservation radix tree does not mark the table pages
    themselves as scratch. This is done to avoid a circular dependency where
    preserving a page can lead of allocating other preserved pages. This
    means any walker looking for free ranges of memory outside of scratch
    areas will ignore the table
    
    Add a table callback that is invoked for each table page. The callback
    is given the physical address of the table page.
    
    Signed-off-by: Pratyush Yadav (Google) <pratyush@kernel.org>
    Signed-off-by: Jork Loeser <jloeser@linux.microsoft.com>

--- include/linux/kho_radix_tree.h
+++ include/linux/kho_radix_tree.h
@@ -37,12 +37,15 @@ struct kho_radix_tree {
 /**
  * struct kho_radix_walk_cb - Callbacks for KHO radix tree walk.
  * @key:      Called on each present key in the radix tree.
+ * @table:    Called on each table of the radix tree itself. Receives the
+ *            physical address of the page containing the table.
  *
  * For each callback, a return value of 0 continues the walk and a non-zero
  * return value is directly returned to the caller.
  */
 struct kho_radix_walk_cb {
 	int (*key)(unsigned long key);
+	int (*table)(phys_addr_t phys);
 };
 
 #ifdef CONFIG_KEXEC_HANDOVER
