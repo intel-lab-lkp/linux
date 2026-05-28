2705b5f6dece0d4953a09d0c854781b54a149305
Onish Sharma <neharora23587@gmail.com>
2026-05-28 19:06:26 +0530

    staging: sm750fb: rename pvReg to pv_reg
    Rename pvReg to pv_reg to comply with kernel coding style (checkpatch.pl)
    and improve readability.
    
    Signed-off-by: Onish Sharma <neharora23587@gmail.com>

--- drivers/staging/sm750fb/sm750.h
+++ drivers/staging/sm750fb/sm750.h
@@ -97,7 +97,7 @@ struct sm750_dev {
 	unsigned long vidreg_start;
 	__u32 vidmem_size;
 	__u32 vidreg_size;
-	void __iomem *pvReg;
+	void __iomem *pv_reg;
 	unsigned char __iomem *vmem;
 	/* locks*/
 	spinlock_t slock;
