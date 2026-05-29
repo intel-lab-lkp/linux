50f34fba15f4fd3fdbc2a07a8ef7e5bc8597e729
Jani Nikula <jani.nikula@intel.com>
2026-05-29 14:04:07 +0300

    drm/xe/display: separate d3cold handling from xe_display_pm_runtime_suspend_late()
    Make the special d3cold paths completely separate from the rest of the
    runtime pm calls.
    
    The intel_dmc_wl_flush_release_work() call right after
    xe_display_pm_suspend_late() might be completely redundant, but this
    avoids any functional changes.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>

--- drivers/gpu/drm/xe/display/xe_display.c
+++ drivers/gpu/drm/xe/display/xe_display.c
@@ -394,8 +394,11 @@ void xe_display_pm_runtime_suspend_late(struct xe_device *xe)
 	if (!xe->info.probe_display)
 		return;
 
-	if (xe->d3cold.allowed)
+	if (xe->d3cold.allowed) {
 		xe_display_pm_suspend_late(xe);
+		intel_dmc_wl_flush_release_work(display);
+		return;
+	}
 
 	/*
 	 * If xe_display_pm_suspend_late() is not called, it is likely
