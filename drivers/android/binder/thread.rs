3c82e9c3d7575333c2b6b1c980e42e30ba317186
Carlos Llamas <cmllamas@google.com>
2026-05-29 13:28:40 +0000

    rust_binder: report netlink transactions
    The Android Binder driver supports a netlink API that reports
    transaction *failures* to a userspace daemon. This allows devices to
    monitor processes with many failed transactions so that it can e.g. kill
    misbehaving apps.
    
    One very important thing that this monitors is when many oneway messages
    are sent to a frozen process, so there is special handling to ensure
    this scenario is surfaced over netlink.
    
    Signed-off-by: Carlos Llamas <cmllamas@google.com>
    Co-developed-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>

--- drivers/android/binder/thread.rs
+++ drivers/android/binder/thread.rs
@@ -1246,7 +1246,6 @@ impl Thread {
             self.push_return_work(err.reply);
             if let Some(source) = &err.source {
                 info.errno = source.to_errno();
-                info.reply = err.reply;
 
                 {
                     let mut ee = self.inner.lock().extended_error;
@@ -1261,6 +1260,15 @@ impl Thread {
                     info.to_pid
                 );
             }
+        }
+
+        if info.oneway_spam_suspect {
+            // If this is both a oneway spam suspect and a failure, we report it twice. This is
+            // useful in case the transaction failed with BR_TRANSACTION_PENDING_FROZEN.
+            info.report_netlink(BR_ONEWAY_SPAM_SUSPECT, &self.process.ctx);
+        }
+        if info.reply != 0 {
+            info.report_netlink(info.reply, &self.process.ctx);
         }
 
         Ok(())
@@ -1332,6 +1340,7 @@ impl Thread {
             );
             let reply = Err(BR_FAILED_REPLY);
             orig.from.deliver_reply(reply, &orig);
+            info.reply = BR_FAILED_REPLY;
             err.reply = BR_TRANSACTION_COMPLETE;
             err
         });
