b8e2fe40cfa6cdae139227af03d5c3a785d08f45
Bart Van Assche <bvanassche@acm.org>
2026-05-29 11:03:10 -0700

    ata: libata: Document when host->eh_mutex should be held
    Annotate the following functions with __must_hold(&host->eh_mutex):
     * All ata_port_operations.error_handler() implementations.
     * ata_eh_reset() and ata_eh_recover() because these functions call
       ata_eh_release() and ata_eh_acquire().
     * All callers of ata_eh_reset() and ata_eh_recover().
    
    Enable Clang's context analysis. This will cause the build to fail if
    e.g. a locking bug would be introduced in an error path. This patch
    should not affect the generated assembler code.
    
    Note: although the Linux kernel documentation specifies 22 as minimal
    version for Clang for context analysis support, annotating function
    pointers is a Clang 23 feature. As one can see here, a patch has been
    queued that fixes the kernel documentation:
    https://lore.kernel.org/all/177926568868.711.3058599932884307249.tip-bot2@tip-bot2/
    
    Signed-off-by: Bart Van Assche <bvanassche@acm.org>

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -6820,6 +6820,7 @@ EXPORT_SYMBOL_GPL(ata_ratelimit);
  *	Might sleep.
  */
 void ata_msleep(struct ata_port *ap, unsigned int msecs)
+	__context_unsafe(conditional locking)
 {
 	bool owns_eh = ap && ap->host->eh_owner == current;
 
@@ -6894,6 +6895,7 @@ static unsigned int ata_dummy_qc_issue(struct ata_queued_cmd *qc)
 }
 
 static void ata_dummy_error_handler(struct ata_port *ap)
+	__must_hold(&ap->host->eh_mutex)
 {
 	/* truly dummy */
 }
