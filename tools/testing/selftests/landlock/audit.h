b54b2564f07f01905d34a3bf1689c968aa4f873b
Maximilian Heyne <mheyne@amazon.de>
2026-05-29 20:03:41 +0000

    selftests/landlock: explicitly disable audit
    I'm seeing sporadic selftest failures, such as
    
      #  RUN           scoped_audit.connect_to_child ...
      # scoped_abstract_unix_test.c:314:connect_to_child:Expected 0 (0) == records.access (8)
      # connect_to_child: Test failed
      #          FAIL  scoped_audit.connect_to_child
      not ok 19 scoped_audit.connect_to_child
    
    This seems similar to what commit 3647a4977fb73d ("selftests/landlock:
    Drain stale audit records on init") tried to fix. However, the added
    drain loop is not effective. When setting the AUDIT_STATUS_PID, the
    kauditd_thread is woken up starting to send messages from the hold queue
    to the netlink. Depending on scheduling of this kthread not all messages
    might be send via the netlink in the 1 us interval.
    
    Therefore, instead of trying to drain the queue, let's just disable
    audit when running non-audit tests or more precisely disable it after
    audit-tests. This way we won't generate any new audit message that could
    interfere with the other tests.
    
    The comment saying that on process exit audit will be disabled is wrong.
    The closed file descriptor just causes an auditd_reset(), not a
    disablement. So future messages will be queued in the hold queue.
    
    Cc: stable@vger.kernel.org
    Fixes: 6a500b22971c ("selftests/landlock: Add tests for audit flags and domain IDs")
    Signed-off-by: Maximilian Heyne <mheyne@amazon.de>

--- tools/testing/selftests/landlock/audit.h
+++ tools/testing/selftests/landlock/audit.h
@@ -494,10 +494,9 @@ static int audit_init_filter_exe(struct audit_filter *filter, const char *path)
 static int audit_cleanup(int audit_fd, struct audit_filter *filter)
 {
 	struct audit_filter new_filter;
+	int err;
 
 	if (audit_fd < 0 || !filter) {
-		int err;
-
 		/*
 		 * Simulates audit_init_with_exe_filter() when called from
 		 * FIXTURE_TEARDOWN_PARENT().
@@ -518,12 +517,10 @@ static int audit_cleanup(int audit_fd, struct audit_filter *filter)
 	audit_filter_exe(audit_fd, filter, AUDIT_DEL_RULE);
 	audit_filter_drop(audit_fd, AUDIT_DEL_RULE);
 
-	/*
-	 * Because audit_cleanup() might not be called by the test auditd
-	 * process, it might not be possible to explicitly set it.  Anyway,
-	 * AUDIT_STATUS_ENABLED will implicitly be set to 0 when the auditd
-	 * process will exit.
-	 */
+	err = audit_set_status(audit_fd, AUDIT_STATUS_ENABLED, 0);
+	if (err)
+		return err;
+
 	return close(audit_fd);
 }
 
