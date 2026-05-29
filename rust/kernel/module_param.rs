d527c741f23173f15042c763eccc7f877e2c258d
Andreas Hindborg <a.hindborg@kernel.org>
2026-05-29 10:41:22 +0200

    rust: module_param: return value by copy from `value`
    For `Copy` parameter types it is more ergonomic to retrieve the
    parameter value by copy than through a shared reference. Change
    `ModuleParamAccess::value` to return `T` by copy when `T: Copy`,
    and rename the previous reference-returning accessor to
    `value_ref`. Update the in-tree caller in `rust_minimal`.
    
    Suggested-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Andreas Hindborg <a.hindborg@kernel.org>

--- rust/kernel/module_param.rs
+++ rust/kernel/module_param.rs
@@ -132,9 +132,12 @@ impl<T> ModuleParamAccess<T> {
     }
 
     /// Get a shared reference to the parameter value.
+    ///
+    /// Returns a reference to the value supplied at module load time, or a
+    /// reference to the default value if the parameter has not been set.
     // Note: When sysfs access to parameters are enabled, we have to pass in a
     // held lock guard here.
-    pub fn value(&self) -> &T {
+    pub fn value_ref(&self) -> &T {
         self.value.as_ref().unwrap_or(&self.default)
     }
 
@@ -143,6 +146,16 @@ impl<T> ModuleParamAccess<T> {
     /// NOTE: In most cases it is not safe deref the returned pointer.
     pub const fn as_void_ptr(&self) -> *mut c_void {
         core::ptr::from_ref(self).cast_mut().cast()
+    }
+}
+
+impl<T: Copy> ModuleParamAccess<T> {
+    /// Get a copy of the parameter value.
+    ///
+    /// Returns the value supplied at module load time, or the default value
+    /// if the parameter has not been set.
+    pub fn value(&self) -> T {
+        self.value.copy().unwrap_or(self.default)
     }
 }
 
