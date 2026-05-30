4f0aacdba12e2d70754e435686921b7906dd9688
Philipp Stanner <phasta@kernel.org>
2026-05-30 16:35:11 +0200

    rust: Add dma_fence abstractions
    C's dma_fence's are synchronisation primitives that will be needed by all
    Rust GPU drivers.
    
    The dma_fence framework sets a number of rules, notably:
      - fences must only be signalled once
      - all fences must be signalled at some point
      - fence error codes must only be set before signalling
      - every pointer to a fence must be backed by a reference
    
    All those rules are being addressed by these abstractions.
    
    To cleanly decouple fence issuers and consumers, two types are provided:
      - DriverFence: the only fence type that can be signalled and that
        carries driver-specific data.
      - Fence: the fence type to be shared with other drivers and / or
        userspace. The only type callbacks can be registered on.
        Cannot be signalled.
    
    Hereby, a Fence lives in the same chunk of memory as a DriverFence. Both
    share the refcount of the underlying C dma_fence. Since this
    implementation does not provide a custom dma_fence_backend_ops.release()
    function, the memory is freed by the dma_fence backend once the refcount
    drops to 0.
    
    To create a DriverFence, the user must first allocate a
    DriverFenceAllocation, so that the creation of the DriverFence later on
    can always succeed. Otherwise, deadlocks could occur if fences need to
    be created in a GPU job submission path.
    
    Synchronization is ensured by the dma_fence backend.
    
    All DriverFence's created through this abstraction must be signalled by
    the creator with an error code. In case a DriverFence drops without
    being signalled beforehand, it is signalled with -ECANCELLED as its
    error and a warning is printed. This allows the Rust abstraction to very
    cleanly decouple fence issuer and consumer by relying on the decoupling
    mechanisms in the C backend, which ensures through RCU and the
    'signalled' fence-flag that dma_fence_backend_ops functions cannot
    access the potentially unloaded driver code anymore.
    
    Signalling fences on drop thus grants many advantages. Not signalling
    fences on drop would risk deadlock and does not grant real advantages:
    By definition only the drivers can ensure that a fence always represents
    the hardware's state correctly.
    
    This implementation models a DmaFenceCtx (fence context) object on which
    fences are to be created, thereby ensuring correct sequence numbering
    according to the timeline.
    
    dma_fence supports a variety of callbacks. The mandatory callbacks
    (get_timeline_name() and get_driver_name()) are implemented in this
    patch. For convenience, they store those name parameters in the fence
    context, saving the driver from implementing these two callbacks.
    
    Support for other callbacks (like for hardware signalling) is prepared
    for through the fact that both DriverFence and Fence live in the same
    allocation, allowing for usage of container_of from the callback to
    access the driver-specific data.
    
    Synchronization for backend_ops callbacks is ensured through RCU which
    prevents UAF-bugs should a DriverFence drop while a Fence callback
    is currently operating on the associated driver data.
    
    Add abstractions for dma_fence in Rust.
    
    Signed-off-by: Philipp Stanner <phasta@kernel.org>

---
+++ rust/kernel/dma_buf/mod.rs
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-2.0 OR MIT
+
+//! DMA-buf subsystem abstractions.
+
+pub mod dma_fence;
+
+pub use self::dma_fence::{
+    DriverFence,
+    Fence,
+    FenceCb,
+    FenceCbRegistration,
+    FenceCtx, //
+};
