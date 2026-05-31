263e9960daf2b975d6a85d2b7844c3440df00f87
David Heidelberg <david@ixit.cz>
2026-05-31 15:08:11 +0200

    media: qcom: camss: Prepare CSID for C-PHY support
    Inherit C-PHY information from CSIPHY, so we can configure CSID
    properly.
    
    CSI2_RX_CFG0_PHY_TYPE_SEL must be set to 1, when C-PHY mode is used.
    
    Reviewed-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Acked-by: Cory Keitz <ckeitz@amazon.com>
    Signed-off-by: David Heidelberg <david@ixit.cz>

--- drivers/media/platform/qcom/camss/camss-csid.c
+++ drivers/media/platform/qcom/camss/camss-csid.c
@@ -1291,6 +1291,7 @@ static int csid_link_setup(struct media_entity *entity,
 
 			lane_cfg = &csiphy->cfg.csi2->lane_cfg;
 			csid->phy.lane_cnt = lane_cfg->num_data;
+			csid->phy.cphy = (lane_cfg->phy_cfg == V4L2_MBUS_CSI2_CPHY);
 			csid->phy.lane_assign = csid_get_lane_assign(lane_cfg, lane_cfg->num_data);
 			csid->tpg_linked = false;
 		}
