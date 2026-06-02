b3477bdfaf63f0bbe56a052c59e51a76621feff3
Lyude Paul <lyude@redhat.com>
2026-06-02 13:25:00 -0400

    rust: drm: gem/shmem: Add DmaResvGuard helper
    Just a temporary holdover to make locking/unlocking the dma_resv lock much
    easier.
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>
    Co-authored-by: Alexandre Courbot <acourbot@nvidia.com>
    Signed-off-by: Alexandre Courbot <acourbot@nvidia.com>

--- rust/kernel/drm/gem/shmem.rs
+++ rust/kernel/drm/gem/shmem.rs
@@ -27,7 +27,10 @@ use core::{
         Deref,
         DerefMut, //
     },
-    ptr::NonNull,
+    ptr::{
+        self,
+        NonNull, //
+    },
 };
 use gem::{
     BaseObjectPrivate,
@@ -223,4 +226,30 @@ impl<T: DriverObject> driver::AllocImpl for Object<T> {
         dumb_create: Some(bindings::drm_gem_shmem_dumb_create),
         dumb_map_offset: None,
     };
+}
+
+/// Private helper-type for holding the `dma_resv` object for a GEM shmem object.
+///
+/// When this is dropped, the `dma_resv` lock is dropped as well.
+///
+// TODO: This should be replace with a WwMutex equivalent once we have such bindings in the kernel.
+struct DmaResvGuard<'a, T: DriverObject>(&'a Object<T>);
+
+impl<'a, T: DriverObject> DmaResvGuard<'a, T> {
+    #[inline(always)]
+    #[expect(unused)]
+    fn new(obj: &'a Object<T>) -> Self {
+        // SAFETY: This lock is initialized throughout the lifetime of `object`.
+        unsafe { bindings::dma_resv_lock(obj.raw_dma_resv(), ptr::null_mut()) };
+
+        Self(obj)
+    }
+}
+
+impl<'a, T: DriverObject> Drop for DmaResvGuard<'a, T> {
+    #[inline(always)]
+    fn drop(&mut self) {
+        // SAFETY: We are releasing the lock grabbed during the creation of this object.
+        unsafe { bindings::dma_resv_unlock(self.0.raw_dma_resv()) };
+    }
 }
