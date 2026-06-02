036ee5dd2db2add0fbb5b5f82560d8a0fd675a44
Chengwen Feng <fengchengwen@huawei.com>
2026-06-02 22:40:14 +0800

    PCI/TPH: Fix pcie_tph_get_st_table_loc() field extraction
    pcie_tph_get_st_table_loc() incorrectly uses FIELD_GET(), which shifts the
    field value to bit 0. But the function is designed to return raw
    PCI_TPH_LOC_* values as defined in the function comment.
    
    This causes incorrect ST table location detection. Fix it by using bitwise
    AND with PCI_TPH_CAP_LOC_MASK to return the unshifted field value matching
    the function specification.
    
    This doesn't make a difference to mlx5_st_create(), the lone external
    caller, because it only checks for PCI_TPH_LOC_NONE (0), but will be needed
    for callers that check for PCI_TPH_LOC_CAP or PCI_TPH_LOC_MSIX.
    
    Also add tph_cap validation for pcie_tph_get_st_table_loc() to prevent
    invalid PCI configuration space access when TPH is not supported. Add stub
    functions for pcie_tph_get_st_table_size() and pcie_tph_get_st_table_loc()
    when !CONFIG_PCIE_TPH.
    
    Fixes: d2e8a34876ce ("PCI/TPH: Add Steering Tag support")
    Cc: stable@vger.kernel.org
    Signed-off-by: Chengwen Feng <fengchengwen@huawei.com>
    Reviewed-by: Alex Williamson <alex.williamson@nvidia.com>
    Reviewed-by: Bjorn Helgaas <bhelgaas@google.com>

--- include/linux/pci-tph.h
+++ include/linux/pci-tph.h
@@ -8,6 +8,7 @@
  */
 #ifndef LINUX_PCI_TPH_H
 #define LINUX_PCI_TPH_H
+#include <linux/pci.h>
 
 /*
  * According to the ECN for PCI Firmware Spec, Steering Tag can be different
@@ -41,6 +42,10 @@ static inline int pcie_tph_get_cpu_st(struct pci_dev *dev,
 static inline void pcie_disable_tph(struct pci_dev *pdev) { }
 static inline int pcie_enable_tph(struct pci_dev *pdev, int mode)
 { return -EINVAL; }
+static inline u16 pcie_tph_get_st_table_size(struct pci_dev *pdev)
+{ return 0; }
+static inline u32 pcie_tph_get_st_table_loc(struct pci_dev *pdev)
+{ return PCI_TPH_LOC_NONE; }
 #endif
 
 #endif /* LINUX_PCI_TPH_H */
