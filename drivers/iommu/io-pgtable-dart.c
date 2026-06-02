721abfd333901a3489b207a5e5da96ba80bef6cb
Guanghui Feng <guanghuifeng@linux.alibaba.com>
2026-06-02 18:46:36 +0800

    iommu/io-pgtable-dart: remove deprecated iova_to_phys wrapper
    Remove the iova_to_phys wrapper function and .iova_to_phys assignment
    from DART io-pgtable, as all callers now use iova_to_phys_length.
    
    Signed-off-by: Guanghui Feng <guanghuifeng@linux.alibaba.com>

--- drivers/iommu/io-pgtable-dart.c
+++ drivers/iommu/io-pgtable-dart.c
@@ -335,18 +335,6 @@ static size_t dart_unmap_pages(struct io_pgtable_ops *ops, unsigned long iova,
 
 static phys_addr_t dart_iova_to_phys_length(struct io_pgtable_ops *ops,
 					    unsigned long iova,
-					    size_t *mapped_length);
-
-static phys_addr_t dart_iova_to_phys(struct io_pgtable_ops *ops,
-				     unsigned long iova)
-{
-	phys_addr_t phys = dart_iova_to_phys_length(ops, iova, NULL);
-
-	return (phys == PHYS_ADDR_MAX) ? 0 : phys;
-}
-
-static phys_addr_t dart_iova_to_phys_length(struct io_pgtable_ops *ops,
-					    unsigned long iova,
 					    size_t *mapped_length)
 {
 	struct dart_io_pgtable *data = io_pgtable_ops_to_data(ops);
@@ -416,7 +404,6 @@ dart_alloc_pgtable(struct io_pgtable_cfg *cfg)
 	data->iop.ops = (struct io_pgtable_ops) {
 		.map_pages		= dart_map_pages,
 		.unmap_pages		= dart_unmap_pages,
-		.iova_to_phys		= dart_iova_to_phys,
 		.iova_to_phys_length	= dart_iova_to_phys_length,
 	};
 
