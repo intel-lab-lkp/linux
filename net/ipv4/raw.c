8bf8231f837807f2f4358f701d252b36a52b6908
Runyu Xiao <runyu.xiao@seu.edu.cn>
2026-06-03 00:27:27 +0800

    raw: annotate data-races in raw_v4_match()
    raw_v4_match() is a lockless match helper under sk_for_each_rcu()
    and still reads inet->inet_num, inet->inet_rcv_saddr and
    sk->sk_bound_dev_if with plain loads.
    
    inet_num and sk_bound_dev_if already have WRITE_ONCE() writers, and
    raw_bind() should use WRITE_ONCE() for inet_rcv_saddr as well.
    
    Add matching READ_ONCE()/WRITE_ONCE() annotations.
    
    Signed-off-by: Runyu Xiao <runyu.xiao@seu.edu.cn>

--- net/ipv4/raw.c
+++ net/ipv4/raw.c
@@ -120,11 +120,14 @@ bool raw_v4_match(struct net *net, const struct sock *sk, unsigned short num,
 		  __be32 raddr, __be32 laddr, int dif, int sdif)
 {
 	const struct inet_sock *inet = inet_sk(sk);
+	unsigned short match_num = READ_ONCE(inet->inet_num);
+	__be32 match_rcv_saddr = READ_ONCE(inet->inet_rcv_saddr);
+	int match_bound_dev_if = READ_ONCE(sk->sk_bound_dev_if);
 
-	if (net_eq(sock_net(sk), net) && inet->inet_num == num	&&
+	if (net_eq(sock_net(sk), net) && match_num == num &&
 	    !(inet->inet_daddr && inet->inet_daddr != raddr) 	&&
-	    !(inet->inet_rcv_saddr && inet->inet_rcv_saddr != laddr) &&
-	    raw_sk_bound_dev_eq(net, sk->sk_bound_dev_if, dif, sdif))
+	    !(match_rcv_saddr && match_rcv_saddr != laddr) &&
+	    raw_sk_bound_dev_eq(net, match_bound_dev_if, dif, sdif))
 		return true;
 	return false;
 }
@@ -724,7 +727,8 @@ static int raw_bind(struct sock *sk, struct sockaddr_unsized *uaddr,
 					 chk_addr_ret))
 		goto out;
 
-	inet->inet_rcv_saddr = inet->inet_saddr = addr->sin_addr.s_addr;
+	inet->inet_saddr = addr->sin_addr.s_addr;
+	WRITE_ONCE(inet->inet_rcv_saddr, addr->sin_addr.s_addr);
 	if (chk_addr_ret == RTN_MULTICAST || chk_addr_ret == RTN_BROADCAST)
 		inet->inet_saddr = 0;  /* Use device */
 	sk_dst_reset(sk);
