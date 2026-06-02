64099db218c98925682aa4e6b82f2724af16f69c
David Heidelberg <david@ixit.cz>
2026-06-03 01:30:45 +0200

    media: qcom: camss: Account for C-PHY when calculating link frequency
    Ensure that the link frequency divider correctly accounts for C-PHY
    operation. The divider differs between D-PHY and C-PHY, as described
    in the MIPI CSI-2 specification.
    
    For more details, see:
    https://docs.kernel.org/driver-api/media/tx-rx.html#pixel-rate
    
    Suggested-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Acked-by: Cory Keitz <ckeitz@amazon.com>
    Tested-by: Cory Keitz <ckeitz@amazon.com>
    Reviewed-by: Bryan O'Donoghue <bryan.odonoghue@linaro.org>
    Link: https://docs.kernel.org/driver-api/media/tx-rx.html#pixel-rate
    Signed-off-by: David Heidelberg <david@ixit.cz>

--- drivers/media/platform/qcom/camss/camss-csid.c
+++ drivers/media/platform/qcom/camss/camss-csid.c
@@ -539,15 +539,18 @@ const struct csid_format_info *csid_get_fmt_entry(const struct csid_format_info 
 static int csid_set_clock_rates(struct csid_device *csid)
 {
 	struct device *dev = csid->camss->dev;
+	struct csiphy_device *csiphy = &csid->camss->csiphy[csid->phy.csiphy_id];
+	struct csiphy_lanes_cfg *lane_cfg = &csiphy->cfg.csi2->lane_cfg;
 	const struct csid_format_info *fmt;
+
 	s64 link_freq;
 	int i, j;
 	int ret;
 
 	fmt = csid_get_fmt_entry(csid->res->formats->formats, csid->res->formats->nformats,
 				 csid->fmt[MSM_CSIPHY_PAD_SINK].code);
-	link_freq = camss_get_link_freq(&csid->subdev.entity, fmt->bpp,
-					csid->phy.lane_cnt);
+
+	link_freq = camss_get_link_freq(&csid->subdev.entity, fmt->bpp, lane_cfg);
 	if (link_freq < 0)
 		link_freq = 0;
 
