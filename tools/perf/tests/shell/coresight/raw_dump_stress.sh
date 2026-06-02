1330ab5cf477f5a914299859fb34b1113161baf0
James Clark <james.clark@linaro.org>
2026-06-02 15:26:50 +0100

    perf test cs-etm: Replace memcpy test with raw dump stress test
    Like asm_pure_loop, this memcpy test only checks that 10 of each of a
    few trace packet types occur after recording a lot of trace, which isn't
    more specific than other existing Coresight tests.
    
    Assume it was supposed to be a stress test for dumping and replace it
    with one that doesn't require a custom binary and checks for a specific
    amount of raw output. Don't bother checking for packets because the
    other tests that test decoding will catch issues with malformed data.
    
    This also adds coverage for exit snapshot mode which was missing.
    
    Signed-off-by: James Clark <james.clark@linaro.org>

---
+++ tools/perf/tests/shell/coresight/raw_dump_stress.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -e
+# Coresight raw dump stress (exclusive)
+
+# SPDX-License-Identifier: GPL-2.0
+
+if [ "$(id -u)" != 0 ]; then
+	# Requires root for larger buffer size
+	echo "[Skip] No root permission"
+	exit 2
+fi
+
+# If Coresight is not available, skip the test
+perf list pmu | grep -q cs_etm || exit 2
+
+tmpdir=$(mktemp -d /tmp/__perf_test.coresight_raw_dump_stress.XXXXX)
+
+cleanup() {
+	rm -r "${tmpdir}"
+	trap - EXIT TERM INT
+}
+
+trap_cleanup() {
+	cleanup
+	exit 1
+}
+trap trap_cleanup EXIT TERM INT
+
+# Use exit snapshot to record 2M of trace to make about 80MB of raw dump data.
+echo "Recording..."
+perf record -e cs_etm/timestamp=0/u -m,2M -Se -o "$tmpdir/data" -- \
+	perf test -w brstack 20000 > /dev/null 2>&1
+
+# Test raw dump runs to completion but don't decode because that's too slow for
+# a test
+echo "Dumping raw trace..."
+perf report --dump-raw-trace -i "$tmpdir/data" 2>/dev/null > "$tmpdir/rawdump"
+err=$?
+
+if [ $err -ne 0 ]; then
+	echo "FAIL: perf report failed with error code $err"
+	cleanup
+	exit 1
+fi
+
+size=$(stat -c%s "$tmpdir/rawdump")
+if [ $size -gt $((50 * 1024 * 1024)) ]; then
+	echo "PASS: Raw dump file is larger than 50MB"
+	cleanup
+	exit 0
+fi
+
+echo "FAIL: Got less than 50MB (${size} bytes)"
+cleanup
+exit 1
