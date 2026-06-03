d11228200ced9fe70da53dfceb907ce31efa6cdc
Charles Keepax <ckeepax@opensource.cirrus.com>
2026-06-03 15:44:36 +0100

    mfd: cs42l43: Use new SoundWire enumeration helper
    Now the new wait for SoundWire enumeration helper no longer depends on
    unattach_request it is safe to use from probe time. Update the driver
    to use the new core helper.
    
    Signed-off-by: Charles Keepax <ckeepax@opensource.cirrus.com>

--- drivers/mfd/cs42l43-sdw.c
+++ drivers/mfd/cs42l43-sdw.c
@@ -102,15 +102,12 @@ static int cs42l43_sdw_update_status(struct sdw_slave *sdw, enum sdw_slave_statu
 				CS42L43_INT_STAT_GEN1_MASK);
 
 		cs42l43->attached = true;
-
-		complete(&cs42l43->device_attach);
 		break;
 	case SDW_SLAVE_UNATTACHED:
 		dev_dbg(cs42l43->dev, "Device detach\n");
 
 		cs42l43->attached = false;
 
-		reinit_completion(&cs42l43->device_attach);
 		complete(&cs42l43->device_detach);
 		break;
 	default:
