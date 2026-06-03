81b8b0303658fbc70066542b97d1f9aaa9923823
Xu Rao <raoxu@uniontech.com>
2026-06-03 14:25:35 +0800

    usb: core: config: fix SS companion log for interrupt endpoints
    usb_parse_ss_endpoint_companion() clears bmAttributes when it is
    non-zero for control or interrupt endpoints.
    
    The diagnostic message reports Control for control endpoints and Bulk
    for the other branch. However, the other branch can only be an
    interrupt endpoint because bulk endpoints are handled by the following
    else-if branch.
    
    Report the endpoint type as Interrupt instead of Bulk to avoid
    misleading descriptor diagnostics.
    
    Signed-off-by: Xu Rao <raoxu@uniontech.com>

--- drivers/usb/core/config.c
+++ drivers/usb/core/config.c
@@ -160,7 +160,7 @@ static void usb_parse_ss_endpoint_companion(struct device *ddev, int cfgno,
 		dev_notice(ddev, "%s endpoint with bmAttributes = %d in "
 				"config %d interface %d altsetting %d ep %d: "
 				"setting to zero\n",
-				usb_endpoint_xfer_control(&ep->desc) ? "Control" : "Bulk",
+				usb_endpoint_xfer_control(&ep->desc) ? "Control" : "Interrupt",
 				desc->bmAttributes,
 				cfgno, inum, asnum, ep->desc.bEndpointAddress);
 		ep->ss_ep_comp.bmAttributes = 0;
