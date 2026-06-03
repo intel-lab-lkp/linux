6a046a87c58f7e45db0a3b1f1e07c43a97bb2f24
Reinette Chatre <reinette.chatre@intel.com>
2026-06-02 20:27:29 -0700

    x86,fs/resctrl: Document safe RCU list traversal
    rdt_resource::ctrl_domains and rdt_resource::mon_domains are RCU lists with
    entries added and removed by architecture from CPU hotplug callbacks that
    are run with cpus_write_lock() held. These lists can be traversed safely
    from resctrl fs by either holding cpus_read_lock() or relying on an RCU
    read-side critical section.
    
    resctrl fs traversals of rdt_resource::ctrl_domains and
    rdt_resource::mon_domains are done using list_for_each_entry() with
    cpus_read_lock() held. Similarly, x86 architecture callbacks use
    list_for_each_entry() expecting that resctrl fs makes the call with
    cpus_read_lock() held. Inconsistently, a lockdep_assert_cpus_held() precedes
    the list_for_each_entry() call with varying distance to document this safe
    RCU list traversal.
    
    In preparation for an upcoming traversal of rdt_resource::ctrl_domains that
    needs to be done from RCU read-side critical section there is a requirement
    for developers to always know exactly in which context the list is being
    traversed.
    
    Replace the list_for_each_entry() traversals of RCU list with
    list_for_each_entry_rcu() to document that an RCU list is being traversed
    while making use of the built-in lockdep expression that additionally
    documents that it is cpus_read_lock() that enables the list to be
    traversed from non-RCU protection. Only revert to documenting the
    safety of traversal using a comment when lockdep does not have needed
    visibility in functions called via smp_call*().
    
    The lockdep expression within list_for_each_entry_rcu() depends on
    RCU_EXPERT that is not set in a typical debug kernel so keep the existing
    lockdep_assert_cpus_held() that is active with CONFIG_LOCKDEP=y found in
    typical debug kernel.
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- fs/resctrl/monitor.c
+++ fs/resctrl/monitor.c
@@ -304,7 +304,7 @@ static void add_rmid_to_limbo(struct rmid_entry *entry)
 	idx = resctrl_arch_rmid_idx_encode(entry->closid, entry->rmid);
 
 	entry->busy = 0;
-	list_for_each_entry(d, &r->mon_domains, hdr.list) {
+	list_for_each_entry_rcu(d, &r->mon_domains, hdr.list, lockdep_is_cpus_held()) {
 		/*
 		 * For the first limbo RMID in the domain,
 		 * setup up the limbo worker.
@@ -502,6 +502,11 @@ static int __l3_mon_event_count_sum(struct rdtgroup *rdtgrp, struct rmid_read *r
 	 * all domains fail for any reason.
 	 */
 	ret = -EINVAL;
+	/*
+	 * RCU list being traversed with CPU hotplug lock held. lockdep
+	 * unable to help prove this here since this work is scheduled via
+	 * smp_call*(). Not called from MBM overflow handler.
+	 */
 	list_for_each_entry(d, &rr->r->mon_domains, hdr.list) {
 		if (d->ci_id != rr->ci->id)
 			continue;
@@ -1226,7 +1231,7 @@ static int rdtgroup_assign_cntr_event(struct rdt_l3_mon_domain *d, struct rdtgro
 	int ret = 0;
 
 	if (!d) {
-		list_for_each_entry(d, &r->mon_domains, hdr.list) {
+		list_for_each_entry_rcu(d, &r->mon_domains, hdr.list, lockdep_is_cpus_held()) {
 			int err;
 
 			err = rdtgroup_alloc_assign_cntr(r, d, rdtgrp, mevt);
@@ -1298,7 +1303,7 @@ static void rdtgroup_unassign_cntr_event(struct rdt_l3_mon_domain *d, struct rdt
 	struct rdt_resource *r = resctrl_arch_get_resource(mevt->rid);
 
 	if (!d) {
-		list_for_each_entry(d, &r->mon_domains, hdr.list)
+		list_for_each_entry_rcu(d, &r->mon_domains, hdr.list, lockdep_is_cpus_held())
 			rdtgroup_free_unassign_cntr(r, d, rdtgrp, mevt);
 	} else {
 		rdtgroup_free_unassign_cntr(r, d, rdtgrp, mevt);
@@ -1370,7 +1375,7 @@ static void rdtgroup_update_cntr_event(struct rdt_resource *r, struct rdtgroup *
 	struct rdt_l3_mon_domain *d;
 	int cntr_id;
 
-	list_for_each_entry(d, &r->mon_domains, hdr.list) {
+	list_for_each_entry_rcu(d, &r->mon_domains, hdr.list, lockdep_is_cpus_held()) {
 		cntr_id = mbm_cntr_get(r, d, rdtgrp, evtid);
 		if (cntr_id >= 0)
 			rdtgroup_assign_cntr(r, d, evtid, rdtgrp->mon.rmid,
@@ -1540,7 +1545,7 @@ ssize_t resctrl_mbm_assign_mode_write(struct kernfs_open_file *of, char *buf,
 		/*
 		 * Reset all the non-achitectural RMID state and assignable counters.
 		 */
-		list_for_each_entry(d, &r->mon_domains, hdr.list) {
+		list_for_each_entry_rcu(d, &r->mon_domains, hdr.list, lockdep_is_cpus_held()) {
 			mbm_cntr_free_all(r, d);
 			resctrl_reset_rmid_all(r, d);
 		}
@@ -1563,7 +1568,7 @@ int resctrl_num_mbm_cntrs_show(struct kernfs_open_file *of,
 	cpus_read_lock();
 	mutex_lock(&rdtgroup_mutex);
 
-	list_for_each_entry(dom, &r->mon_domains, hdr.list) {
+	list_for_each_entry_rcu(dom, &r->mon_domains, hdr.list, lockdep_is_cpus_held()) {
 		if (sep)
 			seq_putc(s, ';');
 
@@ -1597,7 +1602,7 @@ int resctrl_available_mbm_cntrs_show(struct kernfs_open_file *of,
 		goto out_unlock;
 	}
 
-	list_for_each_entry(dom, &r->mon_domains, hdr.list) {
+	list_for_each_entry_rcu(dom, &r->mon_domains, hdr.list, lockdep_is_cpus_held()) {
 		if (sep)
 			seq_putc(s, ';');
 
@@ -1647,7 +1652,7 @@ int mbm_L3_assignments_show(struct kernfs_open_file *of, struct seq_file *s, voi
 
 		sep = false;
 		seq_printf(s, "%s:", mevt->name);
-		list_for_each_entry(d, &r->mon_domains, hdr.list) {
+		list_for_each_entry_rcu(d, &r->mon_domains, hdr.list, lockdep_is_cpus_held()) {
 			if (sep)
 				seq_putc(s, ';');
 
@@ -1745,7 +1750,7 @@ next:
 	}
 
 	/* Verify if the dom_id is valid */
-	list_for_each_entry(d, &r->mon_domains, hdr.list) {
+	list_for_each_entry_rcu(d, &r->mon_domains, hdr.list, lockdep_is_cpus_held()) {
 		if (d->hdr.id == dom_id) {
 			ret = rdtgroup_modify_assign_state(dom_str, d, rdtgrp, mevt);
 			if (ret) {
