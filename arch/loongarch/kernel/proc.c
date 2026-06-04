e4b8475bf15a0b0f381b25afccb189b2ca00a301
Markus Elfring <elfring@users.sourceforge.net>
2026-06-04 16:16:43 +0200

    LoongArch: Delete unnecessary braces in show_cpuinfo()
    Do not use curly brackets at one source code place
    where a single statement should be sufficient.
    
    Signed-off-by: Markus Elfring <elfring@users.sourceforge.net>

--- arch/loongarch/kernel/proc.c
+++ arch/loongarch/kernel/proc.c
@@ -96,10 +96,9 @@ static int show_cpuinfo(struct seq_file *m, void *v)
 		seq_puts(m, " lbt_mips");
 
 	seq_printf(m, "\nHardware Watchpoint\t: %s", str_yes_no(cpu_has_watch));
-	if (cpu_has_watch) {
+	if (cpu_has_watch)
 		seq_printf(m, ", iwatch count: %d, dwatch count: %d",
 		      cpu_data[n].watch_ireg_count, cpu_data[n].watch_dreg_count);
-	}
 
 	seq_puts(m, "\n\n");
 
