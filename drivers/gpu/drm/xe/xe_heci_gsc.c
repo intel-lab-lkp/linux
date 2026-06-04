95b1a31b45fc51b3ebadd162b27f551e1bbf275d
Jonathan Cavitt <jonathan.cavitt@intel.com>
2026-06-05 04:25:48 +0800

    drm/{i915, xe}: Refactor generic_handle_irq_safe() error messages
    Currently, all but one uses of generic_handle_irq_safe() report error
    messages using .*_err_ratelimited() error reporting helper functions.
    These helper functions declare their error messages in the following
    form:
    
    "error handling [COMPONENT NAME] irq: %d\n"
    
    .*_err_ratelimited() already logs "error" as a part of the error
    message, so declaring the error is redundant.  Reword it.
    
    The last usage of generic_handle_irq_safe() is in xe_i2c_irq_present(),
    and it currently lacks the same error reporting as in the other cases.
    This is not intentional, so put some logging here.
    
    v2:
    - Use drm_err_ratelimited() correctly (jcavitt)
    
    Suggested-by: Raag Jadav <raag.jadav@intel.com>
    Signed-off-by: Jonathan Cavitt <jonathan.cavitt@intel.com>

--- drivers/gpu/drm/xe/xe_heci_gsc.c
+++ drivers/gpu/drm/xe/xe_heci_gsc.c
@@ -224,7 +224,7 @@ void xe_heci_gsc_irq_handler(struct xe_device *xe, u32 iir)
 
 	ret = generic_handle_irq_safe(xe->heci_gsc.irq);
 	if (ret)
-		drm_err_ratelimited(&xe->drm, "error handling GSC irq: %d\n", ret);
+		drm_err_ratelimited(&xe->drm, "failed to handle GSC irq: %d\n", ret);
 }
 
 void xe_heci_csc_irq_handler(struct xe_device *xe, u32 iir)
@@ -244,5 +244,5 @@ void xe_heci_csc_irq_handler(struct xe_device *xe, u32 iir)
 
 	ret = generic_handle_irq_safe(xe->heci_gsc.irq);
 	if (ret)
-		drm_err_ratelimited(&xe->drm, "error handling GSC irq: %d\n", ret);
+		drm_err_ratelimited(&xe->drm, "failed to handle GSC irq: %d\n", ret);
 }
