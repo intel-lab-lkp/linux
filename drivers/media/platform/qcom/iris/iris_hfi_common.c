6708c91d22b21f8e810cc855abfeef3acc9f564c
Hungyu Lin <dennylin0707@gmail.com>
2026-06-04 08:25:10 +0000

    media: qcom: iris: guard IRQ handler with runtime PM check
    Use pm_runtime_get_if_active() before accessing hardware
    registers in the threaded IRQ handler. Skip interrupt processing
    when the device is not active.
    
    Signed-off-by: Hungyu Lin <dennylin0707@gmail.com>

--- drivers/media/platform/qcom/iris/iris_hfi_common.c
+++ drivers/media/platform/qcom/iris/iris_hfi_common.c
@@ -100,8 +100,13 @@ irqreturn_t iris_hfi_isr(int irq, void *data)
 irqreturn_t iris_hfi_isr_handler(int irq, void *data)
 {
 	struct iris_core *core = data;
+	int ret;
 
 	if (!core)
+		return IRQ_NONE;
+
+	ret = pm_runtime_get_if_active(core->dev);
+	if (ret <= 0)
 		return IRQ_NONE;
 
 	mutex_lock(&core->lock);
@@ -110,6 +115,8 @@ irqreturn_t iris_hfi_isr_handler(int irq, void *data)
 	mutex_unlock(&core->lock);
 
 	core->hfi_response_ops->hfi_response_handler(core);
+
+	pm_runtime_put_autosuspend(core->dev);
 
 	if (!iris_vpu_watchdog(core, core->intr_status))
 		enable_irq(irq);
