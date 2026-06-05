9fb5e76d3583b50a78a553459e8cbd4ba7978abd
Karan Tilak Kumar <kartilak@cisco.com>
2026-06-05 16:45:34 -0700

    scsi: fnic: Send NVMe LS requests through FDLS
    Add the FC frame wrapper for NVMe LS requests and build LS request
    frames from the NVMe-FC transport callback.
    
    Allocate OXIDs, track outstanding LS requests on the target port, arm
    request timers, and register the LS request callback in the NVMe FC
    port template.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Closes: https://lore.kernel.org/oe-kbuild-all/202605280619.pmobiDWp-lkp@intel.com/
    Reviewed-by: Sesidhar Baddela <sebaddel@cisco.com>
    Reviewed-by: Arulprabhu Ponnusamy <arulponn@cisco.com>
    Reviewed-by: Gian Carlo Boffa <gcboffa@cisco.com>
    Reviewed-by: Arun Easi <aeasi@cisco.com>
    Reviewed-by: Hannes Reinecke <hare@kernel.org>
    Reviewed-by: Lee Duncan <lduncan@suse.com>
    Signed-off-by: Karan Tilak Kumar <kartilak@cisco.com>
    Co-developed-by: Hannes Reinecke <hare@kernel.org>

--- drivers/scsi/fnic/fdls_fc.h
+++ drivers/scsi/fnic/fdls_fc.h
@@ -30,6 +30,9 @@
 #include <linux/if_ether.h>
 #include <scsi/fc/fc_encaps.h>
 #include <scsi/fc/fc_fcoe.h>
+#include <linux/nvme.h>
+#include <linux/nvme-fc.h>
+#include <linux/nvme-fc-driver.h>
 
 #define FDLS_MIN_FRAMES	(32)
 #define FDLS_MIN_FRAME_ELEM	(4)
@@ -250,6 +253,11 @@ struct fc_std_logo {
 	struct fc_frame_header fchdr;
 	struct fc_els_logo els;
 } __packed;
+
+struct fc_std_ls_req {
+	struct fc_frame_header fchdr;
+	struct nvmefc_ls_req ls_req;
+};
 
 #define	FNIC_ETH_FCOE_HDRS_OFFSET	\
 	(sizeof(struct ethhdr) + sizeof(struct fcoe_hdr))
