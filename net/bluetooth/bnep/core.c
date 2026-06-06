9565eeb58e542dd840b9e08aaceda1ee7eed797d
David Laight <david.laight.linux@gmail.com>
2026-06-06 21:26:20 +0100

    net/bluetooth/bnep: Use strscpy() to copy device name
    Signed-off-by: David Laight <david.laight.linux@gmail.com>

--- net/bluetooth/bnep/core.c
+++ net/bluetooth/bnep/core.c
@@ -639,7 +639,7 @@ int bnep_add_connection(struct bnep_connadd_req *req, struct socket *sock)
 	}
 
 	up_write(&bnep_session_sem);
-	strcpy(req->device, dev->name);
+	strscpy(req->device, dev->name);
 	return 0;
 
 failed:
@@ -678,7 +678,7 @@ static void __bnep_copy_ci(struct bnep_conninfo *ci, struct bnep_session *s)
 
 	memset(ci, 0, sizeof(*ci));
 	memcpy(ci->dst, s->eh.h_source, ETH_ALEN);
-	strcpy(ci->device, s->dev->name);
+	strscpy(ci->device, s->dev->name);
 	ci->flags = s->flags & valid_flags;
 	ci->state = s->state;
 	ci->role  = s->role;
