d0e821017516a736da7115c7fd111e4b83e66be0
David Laight <david.laight.linux@gmail.com>
2026-06-06 21:26:07 +0100

    drivers/net/bonding: User strscpy() to copy device name
    Signed-off-by: David Laight <david.laight.linux@gmail.com>

--- drivers/net/bonding/bond_main.c
+++ drivers/net/bonding/bond_main.c
@@ -1796,7 +1796,7 @@ static void bond_fill_ifbond(struct bonding *bond, struct ifbond *info)
 
 static void bond_fill_ifslave(struct slave *slave, struct ifslave *info)
 {
-	strcpy(info->slave_name, slave->dev->name);
+	strscpy(info->slave_name, slave->dev->name);
 	info->link = slave->link;
 	info->state = bond_slave_state(slave);
 	info->link_failure_count = slave->link_failure_count;
