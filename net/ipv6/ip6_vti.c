2f11520e0224a69f83e7c57089d6054f9aadec90
David Laight <david.laight.linux@gmail.com>
2026-06-06 21:26:23 +0100

    net/ipv6/ip6_vti: Use strscpy() to copy device name
    Signed-off-by: David Laight <david.laight.linux@gmail.com>

--- net/ipv6/ip6_vti.c
+++ net/ipv6/ip6_vti.c
@@ -186,7 +186,7 @@ static int vti6_tnl_create2(struct net_device *dev)
 	if (err < 0)
 		goto out;
 
-	strcpy(t->parms.name, dev->name);
+	strscpy(t->parms.name, dev->name);
 
 	vti6_tnl_link(ip6n, t);
 
@@ -1170,7 +1170,7 @@ static int __net_init vti6_init_net(struct net *net)
 
 	t = netdev_priv(ip6n->fb_tnl_dev);
 
-	strcpy(t->parms.name, ip6n->fb_tnl_dev->name);
+	strscpy(t->parms.name, ip6n->fb_tnl_dev->name);
 	return 0;
 
 err_register:
