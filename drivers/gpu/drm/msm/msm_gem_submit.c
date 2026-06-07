e016cdb5253e6c211112ab6dabafb45192f3c5ec
Rob Clark <robin.clark@oss.qualcomm.com>
2026-06-07 07:01:57 -0700

    drm/msm: Validate lazy VM is created in GEM_SUBMIT
    Otherwise a GEM_SUBMIT ioctl before any BOs are mapped could cause a
    NPE.
    
    Reported-by: Sashiko <sashiko-bot@kernel.org>
    Fixes: 6a4d287a1ae6 ("drm/msm: Mark VM as unusable on GPU hangs")
    Signed-off-by: Rob Clark <robin.clark@oss.qualcomm.com>

--- drivers/gpu/drm/msm/msm_gem_submit.c
+++ drivers/gpu/drm/msm/msm_gem_submit.c
@@ -572,7 +572,7 @@ int msm_ioctl_gem_submit(struct drm_device *dev, void *data,
 	if (args->pad)
 		return -EINVAL;
 
-	if (to_msm_vm(ctx->vm)->unusable)
+	if (ctx->vm && to_msm_vm(ctx->vm)->unusable)
 		return UERR(EPIPE, dev, "context is unusable");
 
 	/* for now, we just have 3d pipe.. eventually this would need to
