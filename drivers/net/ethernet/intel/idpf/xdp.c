b568992158411026104c7cf41ede86d00fb5d674
Joshua Hay <joshua.a.hay@intel.com>
2026-06-08 13:44:46 -0700

    idpf: do not enable XDP if queue based scheduling is not supported
    The current XDP implementation uses queue based scheduling for its TxQs.
    If the FW does not advertise support for queue based scheduling, do not
    enable XDP. Add the missing capability check at the start of the XDP
    configuration. This will temporarily break XDP while a flow based
    implementation is worked on, as well as while FWs with queue based by
    default are rolled out.
    
    Fixes: 705457e7211f ("idpf: implement XDP_SETUP_PROG in ndo_bpf for splitq")
    Signed-off-by: Joshua Hay <joshua.a.hay@intel.com>
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Reviewed-by: Madhu Chittim <madhu.chittim@intel.com>

--- drivers/net/ethernet/intel/idpf/xdp.c
+++ drivers/net/ethernet/intel/idpf/xdp.c
@@ -510,6 +510,16 @@ int idpf_xdp(struct net_device *dev, struct netdev_bpf *xdp)
 	if (!idpf_is_queue_model_split(vport->dflt_qv_rsrc.txq_model))
 		goto notsupp;
 
+	if (!idpf_is_cap_ena(vport->adapter, IDPF_OTHER_CAPS,
+			     VIRTCHNL2_CAP_SPLITQ_QSCHED)) {
+		struct netlink_ext_ack *extack = xdp->command == XDP_SETUP_PROG ?
+						 xdp->extack : NULL;
+
+		NL_SET_ERR_MSG_MOD(extack,
+				   "XDP is not supported in current Tx scheduling mode");
+		goto notsupp;
+	}
+
 	switch (xdp->command) {
 	case XDP_SETUP_PROG:
 		ret = idpf_xdp_setup_prog(vport, xdp);
