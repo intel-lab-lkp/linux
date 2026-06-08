28f29565accb33270d66832348629622ed69a232
Maxime Ripard <mripard@kernel.org>
2026-06-08 16:37:00 +0200

    drm/bridge: Remove atomic_reset support
    There are no remaining users of the atomic_reset hook. Remove it
    from the core.
    
    Reviewed-by: Laurent Pinchart <laurent.pinchart+renesas@ideasonboard.com>
    Reviewed-by: Thomas Zimmermann <tzimmermann@suse.de>
    Signed-off-by: Maxime Ripard <mripard@kernel.org>

--- drivers/gpu/drm/drm_bridge.c
+++ drivers/gpu/drm/drm_bridge.c
@@ -502,10 +502,7 @@ drm_bridge_atomic_create_priv_state(struct drm_private_obj *obj)
 	struct drm_bridge *bridge = drm_priv_to_bridge(obj);
 	struct drm_bridge_state *state;
 
-	if (bridge->funcs->atomic_create_state)
-		state = bridge->funcs->atomic_create_state(bridge);
-	else
-		state = bridge->funcs->atomic_reset(bridge);
+	state = bridge->funcs->atomic_create_state(bridge);
 	if (IS_ERR(state))
 		return ERR_CAST(state);
 
@@ -520,8 +517,7 @@ static const struct drm_private_state_funcs drm_bridge_priv_state_funcs = {
 
 static bool drm_bridge_is_atomic(struct drm_bridge *bridge)
 {
-	return (bridge->funcs->atomic_create_state ||
-		bridge->funcs->atomic_reset);
+	return bridge->funcs->atomic_create_state != NULL;
 }
 
 /**
