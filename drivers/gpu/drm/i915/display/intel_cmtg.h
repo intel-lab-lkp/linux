c2bb9c6d02df583ee9156dcb5c74517c14e4f2b3
Dibin Moolakadan Subrahmanian <dibin.moolakadan.subrahmanian@intel.com>
2026-06-09 17:32:28 +0530

    drm/i915/cmtg: Modify existing hook to disable CMTG
    Earlier cmtg_disable() used to disable all instances of CMTG
    which cannot handle individual request for specific CMTG instance.
    Introduce cmtg_disable_all() which will disable all cmtg instances
    and cmtg_disable() only disable specific instance.
    
    v2:
    - Use intel_de_rmw to simplify. [Uma]
    
    Signed-off-by: Dibin Moolakadan Subrahmanian <dibin.moolakadan.subrahmanian@intel.com>
    Signed-off-by: Animesh Manna <animesh.manna@intel.com>

--- drivers/gpu/drm/i915/display/intel_cmtg.h
+++ drivers/gpu/drm/i915/display/intel_cmtg.h
@@ -11,6 +11,7 @@
 struct intel_display;
 struct intel_crtc_state;
 
+void intel_cmtg_disable(const struct intel_crtc_state *crtc_state);
 void intel_cmtg_enable_ddi(const struct intel_crtc_state *crtc_state);
 void intel_cmtg_enable_sync(const struct intel_crtc_state *crtc_state);
 void intel_cmtg_set_m_n(const struct intel_crtc_state *crtc_state);
