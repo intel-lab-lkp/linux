25dbc4f1bc653af83ce2bd7030230245b37c5699
Prasad Kumpatla <prasad.kumpatla@oss.qualcomm.com>
2026-06-10 21:15:12 +0530

    ASoC: qcom: qdsp6: add topology-driven Audio IF support
    Add topology parsing and media-format programming for Audio IF
    source and sink modules.
    
    Add the Audio IF module IDs, the required topology tokens, and a
    dedicated topology loader that stores the parsed interface
    configuration in the AudioReach module state. Also add the Audio IF
    media-format path that sends the interface configuration, hardware
    endpoint media format, and frame-duration parameters for Audio IF
    modules.
    
    This keeps the serial-interface configuration topology-driven while
    still allowing the machine driver to provide runtime slot and media
    format settings. The same Audio IF path can then be reused for TDM,
    PCM, and I2S style backends.
    
    The new UAPI tokens (AR_TKN_U32_MODULE_SYNC_SRC=262 through
    AR_TKN_U32_MODULE_INV_EXT_BIT_CLK=276) are added.
    
    MODULE_ID_AUDIO_IF_SINK (0x0700117C) and MODULE_ID_AUDIO_IF_SOURCE
    (0x0700117D) are introduced in this patch.
    
    Signed-off-by: Prasad Kumpatla <prasad.kumpatla@oss.qualcomm.com>

--- sound/soc/qcom/qdsp6/audioreach.h
+++ sound/soc/qcom/qdsp6/audioreach.h
@@ -22,6 +22,8 @@ struct q6apm_graph;
 #define MODULE_ID_PLACEHOLDER_DECODER	0x07001009
 #define MODULE_ID_I2S_SINK		0x0700100A
 #define MODULE_ID_I2S_SOURCE		0x0700100B
+#define MODULE_ID_AUDIO_IF_SINK		0x0700117C
+#define MODULE_ID_AUDIO_IF_SOURCE	0x0700117D
 #define MODULE_ID_SAL			0x07001010
 #define MODULE_ID_MFC			0x07001015
 #define MODULE_ID_DATA_LOGGING		0x0700101A
@@ -544,6 +546,41 @@ struct param_id_i2s_intf_cfg {
 #define PORT_ID_I2S_OUPUT		1
 #define I2S_STACK_SIZE			2048
 
+#define PARAM_ID_AUDIO_IF_INTF_CFG	0x08001B11
+
+#define AUDIO_IF_INTF_MODE_TDM		0x0
+#define AUDIO_IF_INTF_MODE_PCM		0x1
+#define AUDIO_IF_INTF_MODE_I2S		0x2
+
+struct param_id_audio_if_intf_cfg {
+	uint16_t qaif_type;
+	uint16_t intf_idx;
+	uint16_t intf_mode;
+	uint16_t ctrl_data_out_enable;
+	uint32_t active_slot_mask;
+	uint16_t nslots_per_frame;
+	uint16_t slot_width;
+	uint32_t active_lane_mask;
+	uint32_t frame_sync_rate;
+	uint16_t frame_sync_src;
+	uint16_t frame_sync_mode;
+	uint16_t invert_frame_sync_pulse;
+	uint16_t frame_sync_data_delay;
+	uint16_t bit_clk_type;
+	uint8_t inv_int_bit_clk;
+	uint8_t inv_ext_bit_clk;
+} __packed;
+
+#define PARAM_ID_HW_EP_FRAME_DURATION		0x08001B2F
+#define AUDIO_IF_FRAME_DURATION_US		1000
+
+struct param_id_hw_ep_frame_duration {
+	uint32_t frame_duration_in_us;
+	uint32_t allow_frame_duration_normalization;
+	uint32_t min_normalized_frame_dur_us;
+	uint32_t max_normalized_frame_dur_us;
+} __packed;
+
 #define PARAM_ID_DISPLAY_PORT_INTF_CFG		0x08001154
 
 struct param_id_display_port_intf_cfg {
@@ -877,6 +914,28 @@ struct audioreach_module {
 	uint32_t data_format;
 	uint32_t hw_interface_type;
 
+	/* Audio IF module (TDM/PCM/I2S) */
+	/*
+	 * uint32_t fields first to minimise intra-block padding;
+	 * 2 bytes of trailing padding remain after inv_ext_bit_clk
+	 * before the next uint32_t field (interleave_type).
+	 */
+	uint32_t slot_mask;
+	uint32_t active_lane_mask;
+	uint32_t frame_sync_rate;
+	uint16_t qaif_type;
+	uint16_t sync_src;
+	uint16_t ctrl_data_out_enable;
+	uint16_t nslots_per_frame;
+	uint16_t slot_width;
+	uint16_t intf_mode;
+	uint16_t sync_mode;
+	uint16_t ctrl_invert_sync_pulse;
+	uint16_t ctrl_sync_data_delay;
+	uint16_t bit_clk_type;
+	uint8_t inv_int_bit_clk;
+	uint8_t inv_ext_bit_clk;
+
 	/* PCM module specific */
 	uint32_t interleave_type;
 
@@ -907,6 +966,9 @@ struct audioreach_module_config {
 	u32	channel_allocation;
 	u32	sd_line_mask;
 	int	fmt;
+	u32	slot_mask;
+	u16	nslots_per_frame;
+	u16	slot_width;
 	struct snd_codec codec;
 	u8 channel_map[AR_PCM_MAX_NUM_CHANNEL];
 };
