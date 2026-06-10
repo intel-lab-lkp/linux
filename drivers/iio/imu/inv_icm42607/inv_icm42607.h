d851bc6293219d1260d00ab7898d9611dbe51309
Chris Morgan <macromorgan@hotmail.com>
2026-06-10 12:54:52 -0500

    iio: imu: inv_icm42607: Add Gyroscope to icm42607
    Add gyroscope functions to the icm42607 driver.
    
    Signed-off-by: Chris Morgan <macromorgan@hotmail.com>

--- drivers/iio/imu/inv_icm42607/inv_icm42607.h
+++ drivers/iio/imu/inv_icm42607/inv_icm42607.h
@@ -132,6 +132,7 @@ struct inv_icm42607_suspended {
  *  @orientation:	sensor chip orientation relative to main hardware.
  *  @conf:		chip sensors configurations.
  *  @suspended:		suspended sensors configuration.
+ *  @indio_gyro:	gyroscope IIO device.
  *  @indio_accel:	accelerometer IIO device.
  *  @buffer:		data transfer buffer aligned for DMA.
  */
@@ -144,6 +145,7 @@ struct inv_icm42607_state {
 	struct iio_mount_matrix orientation;
 	struct inv_icm42607_conf conf;
 	struct inv_icm42607_suspended suspended;
+	struct iio_dev *indio_gyro;
 	struct iio_dev *indio_accel;
 	__be16 buffer[3] __aligned(IIO_DMA_MINALIGN);
 };
@@ -395,12 +397,18 @@ int inv_icm42607_set_accel_conf(struct inv_icm42607_state *st,
 				struct inv_icm42607_sensor_conf *conf,
 				unsigned int *sleep_ms);
 
+int inv_icm42607_set_gyro_conf(struct inv_icm42607_state *st,
+			       struct inv_icm42607_sensor_conf *conf,
+			       unsigned int *sleep_ms);
+
 int inv_icm42607_set_temp_conf(struct inv_icm42607_state *st, bool enable,
 			       unsigned int *sleep_ms);
 
 int inv_icm42607_core_probe(struct regmap *regmap,
 			    const struct inv_icm42607_hw *hw,
 			    inv_icm42607_bus_setup bus_setup);
+
+struct iio_dev *inv_icm42607_gyro_init(struct inv_icm42607_state *st);
 
 struct iio_dev *inv_icm42607_accel_init(struct inv_icm42607_state *st);
 
