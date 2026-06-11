693a02f3523e0eb086fe4f03fbb8ea1b584fffe5
bui duc phuc <phucduc.bui@gmail.com>
2026-06-11 18:58:51 +0700

    ASoC: Intel: avs: debugfs: Use guard() for spin locks
    Clean up the code using guard() for spin locks.
    Merely code refactoring, and no behavior change.
    
    Signed-off-by: bui duc phuc <phucduc.bui@gmail.com>

--- sound/soc/intel/avs/debugfs.c
+++ sound/soc/intel/avs/debugfs.c
@@ -251,23 +251,21 @@ static int strace_release(struct inode *inode, struct file *file)
 	union avs_notify_msg msg = AVS_NOTIFICATION(LOG_BUFFER_STATUS);
 	struct avs_dev *adev = file->private_data;
 	unsigned long resource_mask;
-	unsigned long flags, i;
+	unsigned long i;
 	u32 num_cores;
 
 	resource_mask = adev->logged_resources;
 	num_cores = adev->hw_cfg.dsp_cores;
 
-	spin_lock_irqsave(&adev->trace_lock, flags);
+	scoped_guard(spinlock_irqsave, &adev->trace_lock) {
+		/* Gather any remaining logs. */
+		for_each_set_bit(i, &resource_mask, num_cores) {
+			msg.log.core = i;
+			avs_dsp_op(adev, log_buffer_status, &msg);
+		}
 
-	/* Gather any remaining logs. */
-	for_each_set_bit(i, &resource_mask, num_cores) {
-		msg.log.core = i;
-		avs_dsp_op(adev, log_buffer_status, &msg);
+		kfifo_free(&adev->trace_fifo);
 	}
-
-	kfifo_free(&adev->trace_fifo);
-
-	spin_unlock_irqrestore(&adev->trace_lock, flags);
 
 	module_put(adev->dev->driver->owner);
 	return 0;
