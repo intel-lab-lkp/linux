026d4e39124de76725959242f6110cf478dc3c46
bui duc phuc <phucduc.bui@gmail.com>
2026-06-11 18:58:56 +0700

    ASoC: Intel: atom: sst_stream: Use guard() for mutex locks
    Clean up the code using guard() for mutex locks.
    Additionally, simplify sst_free_stream() while
    converting the locking code.
    No functional change intended.
    
    Signed-off-by: bui duc phuc <phucduc.bui@gmail.com>

--- sound/soc/intel/atom/sst/sst_stream.c
+++ sound/soc/intel/atom/sst/sst_stream.c
@@ -265,9 +265,8 @@ int sst_pause_stream(struct intel_sst_drv *sst_drv_ctx, int str_id)
 			str_info->status = STREAM_PAUSED;
 		} else if (retval == -SST_ERR_INVALID_STREAM_ID) {
 			retval = -EINVAL;
-			mutex_lock(&sst_drv_ctx->sst_lock);
-			sst_clean_stream(str_info);
-			mutex_unlock(&sst_drv_ctx->sst_lock);
+			scoped_guard(mutex, &sst_drv_ctx->sst_lock)
+				sst_clean_stream(str_info);
 		}
 	} else {
 		retval = -EBADRQC;
@@ -332,9 +331,8 @@ int sst_resume_stream(struct intel_sst_drv *sst_drv_ctx, int str_id)
 			str_info->prev = STREAM_PAUSED;
 		} else if (retval == -SST_ERR_INVALID_STREAM_ID) {
 			retval = -EINVAL;
-			mutex_lock(&sst_drv_ctx->sst_lock);
-			sst_clean_stream(str_info);
-			mutex_unlock(&sst_drv_ctx->sst_lock);
+			scoped_guard(mutex, &sst_drv_ctx->sst_lock)
+				sst_clean_stream(str_info);
 		}
 	} else {
 		retval = -EBADRQC;
@@ -433,39 +431,35 @@ int sst_free_stream(struct intel_sst_drv *sst_drv_ctx, int str_id)
 
 	dev_dbg(sst_drv_ctx->dev, "SST DBG:sst_free_stream for %d\n", str_id);
 
-	mutex_lock(&sst_drv_ctx->sst_lock);
-	if (sst_drv_ctx->sst_state == SST_RESET) {
-		mutex_unlock(&sst_drv_ctx->sst_lock);
-		return -ENODEV;
+	scoped_guard(mutex, &sst_drv_ctx->sst_lock) {
+		if (sst_drv_ctx->sst_state == SST_RESET)
+			return -ENODEV;
 	}
-	mutex_unlock(&sst_drv_ctx->sst_lock);
 	str_info = get_stream_info(sst_drv_ctx, str_id);
 	if (!str_info)
 		return -EINVAL;
 
-	mutex_lock(&str_info->lock);
-	if (str_info->status != STREAM_UN_INIT) {
+	scoped_guard(mutex, &str_info->lock) {
+		if (str_info->status == STREAM_UN_INIT) {
+			dev_dbg(sst_drv_ctx->dev, "SST DBG:BADQRC for stream\n");
+			return -EBADRQC;
+		}
 		str_info->prev =  str_info->status;
 		str_info->status = STREAM_UN_INIT;
-		mutex_unlock(&str_info->lock);
-
-		dev_dbg(sst_drv_ctx->dev, "Free for str %d pipe %#x\n",
-				str_id, str_info->pipe_id);
-		retval = sst_prepare_and_post_msg(sst_drv_ctx, str_info->task_id, IPC_CMD,
-				IPC_IA_FREE_STREAM_MRFLD, str_info->pipe_id, 0,
-				NULL, NULL, true, true, false, true);
-
-		dev_dbg(sst_drv_ctx->dev, "sst: wait for free returned %d\n",
-				retval);
-		mutex_lock(&sst_drv_ctx->sst_lock);
-		sst_clean_stream(str_info);
-		mutex_unlock(&sst_drv_ctx->sst_lock);
-		dev_dbg(sst_drv_ctx->dev, "SST DBG:Stream freed\n");
-	} else {
-		mutex_unlock(&str_info->lock);
-		retval = -EBADRQC;
-		dev_dbg(sst_drv_ctx->dev, "SST DBG:BADQRC for stream\n");
 	}
+
+	dev_dbg(sst_drv_ctx->dev, "Free for str %d pipe %#x\n", str_id, str_info->pipe_id);
+
+	retval = sst_prepare_and_post_msg(sst_drv_ctx, str_info->task_id, IPC_CMD,
+			IPC_IA_FREE_STREAM_MRFLD, str_info->pipe_id, 0,
+			NULL, NULL, true, true, false, true);
+
+	dev_dbg(sst_drv_ctx->dev, "sst: wait for free returned %d\n", retval);
+
+	scoped_guard(mutex, &sst_drv_ctx->sst_lock)
+		sst_clean_stream(str_info);
+
+	dev_dbg(sst_drv_ctx->dev, "SST DBG:Stream freed\n");
 
 	return retval;
 }
