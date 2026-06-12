e73f0bff76165c88972c124eae0bbf93eba61a31
Max Kellermann <max.kellermann@ionos.com>
2026-06-12 19:05:35 +0200

    fs/ceph/caps: make __ceph_remove_cap() static
    It's only used from within caps.c.
    
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- fs/ceph/caps.c
+++ fs/ceph/caps.c
@@ -1119,7 +1119,7 @@ int ceph_is_any_caps(struct inode *inode)
  * caller should hold i_ceph_lock.
  * caller will not hold session s_mutex if called from destroy_inode.
  */
-void __ceph_remove_cap(struct ceph_cap *cap, bool queue_release)
+static void __ceph_remove_cap(struct ceph_cap *cap, bool queue_release)
 {
 	struct ceph_mds_session *session = cap->session;
 	struct ceph_client *cl = session->s_mdsc->fsc->client;
