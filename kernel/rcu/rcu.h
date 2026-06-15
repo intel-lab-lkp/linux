49d3191f5e6e3ab1b63e49685499e8bf427b090a
Vishal Chourasia <vishalc@linux.ibm.com>
2026-06-15 09:55:16 +0530

    cpuhp: Expedite RCU when toggling system-wide SMT mode
    On large idle systems, changing system-wide SMT level using the sysfs
    control interface still takes approximately 40-55 minutes. Changing SMT
    levels is an user triggered operation generally done when systems are
    fairly idle. This causes users to be blocked from doing other subsequent
    work.
    
    Analyzing the collected profile data during SMT level switch, showed
    that CPU hotplug machinery was blocked on synchronize_rcu() calls.
    
    Expedite RCU grace periods for the entire duration of triggered SMT
    switch operation via the sysfs control interface. Individual CPU hotplug
    operations via the online/offline interface are not affected.
    
    On a PPC64 system with 400 CPUs:
    
      SMT8 to SMT1:
        before:  1m14s
        after:   3.2s   (~23x faster)
    
      SMT1 to SMT8:
        before:  2m27s
        after:   2.5s   (~58x faster)
    
    On a large config system with 1920 CPUs, completion time improves from
    ~1 hour to 5-6 minutes.
    
    Tested-by: Samir <samir@linux.ibm.com>
    Reviewed-by: Shrikanth Hegde <sshegde@linux.ibm.com>
    Signed-off-by: Vishal Chourasia <vishalc@linux.ibm.com>

--- kernel/rcu/rcu.h
+++ kernel/rcu/rcu.h
@@ -521,8 +521,6 @@ do {									\
 static inline bool rcu_gp_is_normal(void) { return true; }
 static inline bool rcu_gp_is_expedited(void) { return false; }
 static inline bool rcu_async_should_hurry(void) { return false; }
-static inline void rcu_expedite_gp(void) { }
-static inline void rcu_unexpedite_gp(void) { }
 static inline void rcu_async_hurry(void) { }
 static inline void rcu_async_relax(void) { }
 static inline bool rcu_cpu_online(int cpu) { return true; }
@@ -530,8 +528,6 @@ static inline bool rcu_cpu_online(int cpu) { return true; }
 bool rcu_gp_is_normal(void);     /* Internal RCU use. */
 bool rcu_gp_is_expedited(void);  /* Internal RCU use. */
 bool rcu_async_should_hurry(void);  /* Internal RCU use. */
-void rcu_expedite_gp(void);
-void rcu_unexpedite_gp(void);
 void rcu_async_hurry(void);
 void rcu_async_relax(void);
 void rcupdate_announce_bootup_oddness(void);
