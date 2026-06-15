40aa0a94a4252ff6cd1763325669b83aa95bd7b4
Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>
2026-06-15 16:48:00 +0100

    rtc: rzn1: fix alarm range check truncation on 32-bit systems
    alarm and farest were declared as unsigned long, but
    rtc_tm_to_time64() returns time64_t (s64). On 32-bit systems where
    unsigned long is 32 bits, the assignment silently truncates the upper
    32 bits of the timestamp.
    
    Fix by declaring alarm and farest as time64_t and replacing
    time_after() with a direct signed comparison, which is correct for
    time64_t values that will never realistically overflow.
    
    Signed-off-by: Lad Prabhakar <prabhakar.mahadev-lad.rj@bp.renesas.com>

--- drivers/rtc/rtc-rzn1.c
+++ drivers/rtc/rtc-rzn1.c
@@ -21,6 +21,7 @@
 #include <linux/pm_runtime.h>
 #include <linux/rtc.h>
 #include <linux/spinlock.h>
+#include <linux/time64.h>
 
 #define RZN1_RTC_CTL0 0x00
 #define   RZN1_RTC_CTL0_SLSB_SCMP BIT(4)
@@ -265,8 +266,8 @@ static int rzn1_rtc_set_alarm(struct device *dev, struct rtc_wkalrm *alrm)
 {
 	struct rzn1_rtc *rtc = dev_get_drvdata(dev);
 	struct rtc_time *tm = &alrm->time, tm_now;
-	unsigned long alarm, farest;
 	unsigned int days_ahead, wday;
+	time64_t alarm, farest;
 	int ret;
 
 	ret = rzn1_rtc_read_time(dev, &tm_now);
@@ -276,7 +277,7 @@ static int rzn1_rtc_set_alarm(struct device *dev, struct rtc_wkalrm *alrm)
 	/* We cannot set alarms more than one week ahead */
 	farest = rtc_tm_to_time64(&tm_now) + rtc->rtcdev->alarm_offset_max;
 	alarm = rtc_tm_to_time64(tm);
-	if (time_after(alarm, farest))
+	if (alarm > farest)
 		return -ERANGE;
 
 	/* Convert alarm day into week day */
