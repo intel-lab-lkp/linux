e4cb1cd62ab64813f3458e88a59a9572736ee001
Tony Luck <tony.luck@intel.com>
2026-06-15 11:24:50 -0700

    x86/resctrl: Drop global 'rdt_mon_capable' flag
    File system code calls resctrl_arch_mon_capable() to determine if the
    system supports any monitoring features.
    
    x86 architecture code sets a global flag to support implementation of
    resctrl_arch_mon_capable() but the upcoming change to enumerate AET
    (Application Energy Telemetry) features on each mount introduces a new
    corner case.
    
    Specifically when AET is the only monitoring feature, loading and
    unloading the pmt_telemetry module between resctrl mounts may result
    in monitoring support enabled on some mounts, but not on others.
    
    Replace the global flag with a scan of resources to check if any are
    marked mon_capable.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>

--- arch/x86/include/asm/resctrl.h
+++ arch/x86/include/asm/resctrl.h
@@ -43,7 +43,6 @@ struct resctrl_pqr_state {
 DECLARE_PER_CPU(struct resctrl_pqr_state, pqr_state);
 
 extern bool rdt_alloc_capable;
-extern bool rdt_mon_capable;
 
 DECLARE_STATIC_KEY_FALSE(rdt_enable_key);
 DECLARE_STATIC_KEY_FALSE(rdt_alloc_enable_key);
@@ -66,10 +65,7 @@ static inline void resctrl_arch_disable_alloc(void)
 	static_branch_dec_cpuslocked(&rdt_enable_key);
 }
 
-static inline bool resctrl_arch_mon_capable(void)
-{
-	return rdt_mon_capable;
-}
+bool resctrl_arch_mon_capable(void);
 
 static inline void resctrl_arch_enable_mon(void)
 {
