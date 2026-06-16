13c270c693c8c9d0aa71d40459c780cbbb1dff21
Uwe Kleine-König (The Capable Hub) <u.kleine-koenig@baylibre.com>
2026-06-16 16:31:39 +0200

    extcon: max14577: Use named initializers for the platform_device_id array
    Named initializers are better readable and more robust to changes of the
    struct definition. This robustness is relevant for a planned change to
    struct platform_device_id replacing .driver_data by an anonymous union.
    
    While touching this array drop commas before closing curly braces on the
    same line.
    
    Signed-off-by: Uwe Kleine-König (The Capable Hub) <u.kleine-koenig@baylibre.com>

--- drivers/extcon/extcon-max14577.c
+++ drivers/extcon/extcon-max14577.c
@@ -771,8 +771,8 @@ static int max14577_muic_probe(struct platform_device *pdev)
 }
 
 static const struct platform_device_id max14577_muic_id[] = {
-	{ "max14577-muic", MAXIM_DEVICE_TYPE_MAX14577, },
-	{ "max77836-muic", MAXIM_DEVICE_TYPE_MAX77836, },
+	{ .name = "max14577-muic", .driver_data = MAXIM_DEVICE_TYPE_MAX14577 },
+	{ .name = "max77836-muic", .driver_data = MAXIM_DEVICE_TYPE_MAX77836 },
 	{ }
 };
 MODULE_DEVICE_TABLE(platform, max14577_muic_id);
