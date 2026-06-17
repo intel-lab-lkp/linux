c31f2ee5dbf12340853c4e07e104a44220701afc
Pierre-Eric Pelloux-Prayer <pierre-eric.pelloux-prayer@amd.com>
2026-06-17 15:28:33 +0200

    drm/amdgpu: delay ttm buffer func enablement on xgmi
    When amdgpu_init_minimal_xgmi is used, SDMA engines init
    is delayed so amdgpu_ttm_enable_buffer_funcs must be
    called later.
    
    Without this, the check for num_buffer_funcs_scheds will
    fail and using ttm buffer funcs later will fail.

--- drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
+++ drivers/gpu/drm/amd/amdgpu/amdgpu_device.c
@@ -2478,7 +2478,8 @@ static int amdgpu_device_ip_init(struct amdgpu_device *adev)
 	if (r)
 		goto init_failed;
 
-	amdgpu_ttm_enable_buffer_funcs(adev);
+	if (amdgpu_ip_member_of_hwini(adev, AMD_IP_BLOCK_TYPE_SDMA))
+		amdgpu_ttm_enable_buffer_funcs(adev);
 
 	/* Don't init kfd if whole hive need to be reset during init */
 	if (adev->init_lvl->level != AMDGPU_INIT_LEVEL_MINIMAL_XGMI) {
@@ -5252,8 +5253,6 @@ int amdgpu_device_reinit_after_reset(struct amdgpu_reset_context *reset_context)
 				if (r)
 					goto out;
 
-				amdgpu_ttm_enable_buffer_funcs(tmp_adev);
-
 				r = amdgpu_device_ip_resume_phase3(tmp_adev);
 				if (r)
 					goto out;
@@ -5319,6 +5318,8 @@ out:
 				r = -EAGAIN;
 				goto end;
 			}
+
+			amdgpu_ttm_enable_buffer_funcs(tmp_adev);
 		}
 
 		if (r)
