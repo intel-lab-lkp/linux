437f641bbf7b0cb277c32b553da90a180df7f16c
Shrikanth Hegde <sshegde@linux.ibm.com>
2026-06-17 23:11:28 +0530

    sched/core: Keep tick on non-preferred CPUs until tasks are out
    Enable tick on nohz full CPU when it is marked as non-preferred.
    If there in no CFS running there, disable the tick to save the power.
    
    Steal time handling code will call tick_nohz_dep_set_cpu with
    TICK_DEP_BIT_SCHED for moving the task out of nohz_full CPU fast.
    
    Signed-off-by: Shrikanth Hegde <sshegde@linux.ibm.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1431,6 +1431,10 @@ bool sched_can_stop_tick(struct rq *rq)
 	if (rq->dl.dl_nr_running)
 		return false;
 
+	/* Keep the tick running until CFS tasks are pushed out*/
+	if (!cpu_preferred(rq->cpu) && rq->cfs.h_nr_queued)
+		return false;
+
 	/*
 	 * If there are more than one RR tasks, we need the tick to affect the
 	 * actual RR behaviour.
