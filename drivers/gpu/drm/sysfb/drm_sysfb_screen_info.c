91a384812456a71c6483c0b9704d035d95cea225
Thomas Zimmermann <tzimmermann@suse.de>
2026-06-17 13:27:30 +0200

    drm/sysfb: Return errno code from drm_sysfb_get_visible_size()
    Change the return type of drm_sysfb_get_visible_size() to s64 so
    that it returns a possible errno code from _get_validated_size0().
    Fix callers to handle the errno code.
    
    The currently returned unsigned type converts an errno code to a
    very large size value, which drivers interpret as visible size of
    the system framebuffer. Later efforts to reserve the framebuffer
    resource fail.
    
    The bug has been present since efidrm and vesadrm got merged. It
    was then part of each driver.
    
    Signed-off-by: Thomas Zimmermann <tzimmermann@suse.de>
    Fixes: 32ae90c66fb6 ("drm/sysfb: Add efidrm for EFI displays")
    Fixes: a84eb6abe2b6 ("drm/sysfb: Add vesadrm for VESA displays")
    Cc: Thomas Zimmermann <tzimmermann@suse.de>
    Cc: Javier Martinez Canillas <javierm@redhat.com>
    Cc: dri-devel@lists.freedesktop.org
    Cc: <stable@vger.kernel.org> # v6.16+

--- drivers/gpu/drm/sysfb/drm_sysfb_screen_info.c
+++ drivers/gpu/drm/sysfb/drm_sysfb_screen_info.c
@@ -64,7 +64,7 @@ int drm_sysfb_get_stride_si(struct drm_device *dev, const struct screen_info *si
 }
 EXPORT_SYMBOL(drm_sysfb_get_stride_si);
 
-u64 drm_sysfb_get_visible_size_si(struct drm_device *dev, const struct screen_info *si,
+s64 drm_sysfb_get_visible_size_si(struct drm_device *dev, const struct screen_info *si,
 				  unsigned int height, unsigned int stride, u64 size)
 {
 	u64 vsize = height * stride;
