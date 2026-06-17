ccb796437ebefae5a5792c4790edc50ffb18d5cd
Bibo Mao <maobibo@loongson.cn>
2026-06-17 15:48:23 +0800

    ACPI: LoongArch: Add ACPI_IOVT option
    ACPI_IOVT is acpi table with IOMMU device on LoongArch system, here
    option ACPI_IOVT is added. And empty function acpi_iovt_init() is
    added and called in acpi_arch_init().
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>

--- drivers/acpi/loongarch/init.c
+++ drivers/acpi/loongarch/init.c
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include <linux/acpi.h>
+#include "init.h"
 
 void __init acpi_arch_init(void)
 {
+	if (IS_ENABLED(CONFIG_ACPI_IOVT))
+		acpi_iovt_init();
 }
---
+++ drivers/acpi/loongarch/iovt.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include "init.h"
+
+void __init acpi_iovt_init(void)
+{
+}
