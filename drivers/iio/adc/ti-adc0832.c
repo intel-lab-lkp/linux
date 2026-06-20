90ccaf24d565f7c327fa468aa9a3c855732937c9
Prashant Rahul <prashantrahul23@gmail.com>
2026-06-20 23:10:57 +0530

    iio: adc: ti-adc0832: log buffer setup failure in probe
    Errors are logged when enabling the regulator or adding the cleanup
    action fails, but not when buffer setup fails. Log the error returned by
    the buffer setup path as well.
    
    Signed-off-by: Prashant Rahul <prashantrahul23@gmail.com>

--- drivers/iio/adc/ti-adc0832.c
+++ drivers/iio/adc/ti-adc0832.c
@@ -298,7 +298,7 @@ static int adc0832_probe(struct spi_device *spi)
 	ret = devm_iio_triggered_buffer_setup(&spi->dev, indio_dev, NULL,
 					      adc0832_trigger_handler, NULL);
 	if (ret)
-		return ret;
+		return dev_err_probe(&spi->dev, ret, "iio triggered buffer setup failed\n");
 
 	return devm_iio_device_register(&spi->dev, indio_dev);
 }
