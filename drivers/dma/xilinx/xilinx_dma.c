0a678c660db5af6437250efff6328b4819a73b9a
Suraj Gupta <suraj.gupta2@amd.com>
2026-06-21 02:04:15 +0530

    dmaengine: xilinx_dma: Enable transfer chaining for AXIDMA and MCDMA by removing idle restriction
    Remove the restrictive idle check in xilinx_dma_start_transfer() and
    xilinx_mcdma_start_transfer() that prevented new transfers from being
    queued when the channel was busy.
    Additionally, only update the CURDESC register when the channel is
    running in scatter-gather mode and active list is empty to avoid
    interfering with transfers already in progress. When the active list
    contains transfers, the hardware tail pointer extension mechanism
    handles chaining automatically.
    
    Tested-by: Folker Schwesinger <dev@folker-schwesinger.de>
    Signed-off-by: Suraj Gupta <suraj.gupta2@amd.com>
    Co-developed-by: Srinivas Neeli <srinivas.neeli@amd.com>
    Signed-off-by: Srinivas Neeli <srinivas.neeli@amd.com>

--- drivers/dma/xilinx/xilinx_dma.c
+++ drivers/dma/xilinx/xilinx_dma.c
@@ -1580,9 +1580,6 @@ static void xilinx_dma_start_transfer(struct xilinx_dma_chan *chan)
 		return;
 	}
 
-	if (!chan->idle)
-		return;
-
 	head_desc = list_first_entry(&chan->pending_list,
 				     struct xilinx_dma_tx_descriptor, node);
 	tail_desc = list_last_entry(&chan->pending_list,
@@ -1599,7 +1596,7 @@ static void xilinx_dma_start_transfer(struct xilinx_dma_chan *chan)
 		dma_ctrl_write(chan, XILINX_DMA_REG_DMACR, reg);
 	}
 
-	if (chan->has_sg)
+	if (chan->has_sg && list_empty(&chan->active_list))
 		xilinx_write(chan, XILINX_DMA_REG_CURDESC,
 			     head_desc->async_tx.phys);
 	reg  &= ~XILINX_DMA_CR_DELAY_MAX;
@@ -1660,9 +1657,6 @@ static void xilinx_mcdma_start_transfer(struct xilinx_dma_chan *chan)
 	if (chan->err)
 		return;
 
-	if (!chan->idle)
-		return;
-
 	if (list_empty(&chan->pending_list))
 		return;
 
@@ -1685,8 +1679,9 @@ static void xilinx_mcdma_start_transfer(struct xilinx_dma_chan *chan)
 	dma_ctrl_write(chan, XILINX_MCDMA_CHAN_CR_OFFSET(chan->tdest), reg);
 
 	/* Program current descriptor */
-	xilinx_write(chan, XILINX_MCDMA_CHAN_CDESC_OFFSET(chan->tdest),
-		     head_desc->async_tx.phys);
+	if (chan->has_sg && list_empty(&chan->active_list))
+		xilinx_write(chan, XILINX_MCDMA_CHAN_CDESC_OFFSET(chan->tdest),
+			     head_desc->async_tx.phys);
 
 	/* Program channel enable register */
 	reg = dma_ctrl_read(chan, XILINX_MCDMA_CHEN_OFFSET);
