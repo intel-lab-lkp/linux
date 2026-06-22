9e60704281e8c1eac1c17f601fbe91560b38d73e
Batu Ada Tutkun <batuadatutkun@gmail.com>
2026-06-22 21:49:59 +0300

    staging: media: atomisp: fix indentation to use tabs instead of spaces
    Function parameters in ia_css_cnr.host.c and sp.c were indented
    with spaces instead of tabs, violating the kernel coding style.
    Replace leading spaces with tabs.
    
    Signed-off-by: Batu Ada Tutkun <batuadatutkun@gmail.com>

--- drivers/staging/media/atomisp/pci/hive_isp_css_common/host/sp.c
+++ drivers/staging/media/atomisp/pci/hive_isp_css_common/host/sp.c
@@ -13,8 +13,8 @@
 #include "assert_support.h"
 
 void cnd_sp_irq_enable(
-    const sp_ID_t		ID,
-    const bool		cnd)
+	const sp_ID_t		ID,
+	const bool		cnd)
 {
 	if (cnd) {
 		sp_ctrl_setbit(ID, SP_IRQ_READY_REG, SP_IRQ_READY_BIT);
