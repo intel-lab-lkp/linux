2264a487e7dea570cfe59dc391e671333edcdc53
Liam Zuiderhoek <zuiderhoekl@gmail.com>
2026-06-22 21:46:49 +0200

    GPU: drm: meson: inline call to drm_simple_encoder_init()
    This commit inlines the function drm_simple_encoder_init for the meson
    driver. It replaces the simple encoder init call with drm_encoder_init
    and a custom cleanup struct.
    
    Signed-off-by: Liam Zuiderhoek <zuiderhoekl@gmail.com>

--- drivers/gpu/drm/meson/meson_encoder_hdmi.c
+++ drivers/gpu/drm/meson/meson_encoder_hdmi.c
@@ -44,6 +44,10 @@ struct meson_encoder_hdmi {
 	struct cec_notifier *cec_notifier;
 };
 
+static const struct drm_encoder_funcs drm_simple_encoder_funcs_cleanup = {
+	.destroy = drm_encoder_cleanup,
+};
+
 #define bridge_to_meson_encoder_hdmi(x) \
 	container_of(x, struct meson_encoder_hdmi, bridge)
 
@@ -407,8 +411,9 @@ int meson_encoder_hdmi_probe(struct meson_drm *priv)
 	meson_encoder_hdmi->priv = priv;
 
 	/* Encoder */
-	ret = drm_simple_encoder_init(priv->drm, &meson_encoder_hdmi->encoder,
-				      DRM_MODE_ENCODER_TMDS);
+	ret = drm_encoder_init(priv->drm, &meson_encoder_hdmi->encoder,
+			       &drm_simple_encoder_funcs_cleanup,
+			       DRM_MODE_ENCODER_TMDS, NULL);
 	if (ret) {
 		dev_err_probe(priv->dev, ret, "Failed to init HDMI encoder\n");
 		goto err_put_node;
