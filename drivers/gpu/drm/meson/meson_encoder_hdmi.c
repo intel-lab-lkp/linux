0dbfb372be57f2b41d6dc1818085329564871784
Liam Zuiderhoek <zuiderhoekl@gmail.com>
2026-06-22 20:08:32 +0200

    GPU: drm: meson inline drm_simple_encoder_init()
    Inline the call to drm_simple_encoder_init() for meson,
    as per the TODO list on docs.kernel.org/gpu/todo.html
    
    Signed-off-by: Liam Zuiderhoek <zuiderhoekl@gmail.com>

--- drivers/gpu/drm/meson/meson_encoder_hdmi.c
+++ drivers/gpu/drm/meson/meson_encoder_hdmi.c
@@ -407,8 +407,8 @@ int meson_encoder_hdmi_probe(struct meson_drm *priv)
 	meson_encoder_hdmi->priv = priv;
 
 	/* Encoder */
-	ret = drm_simple_encoder_init(priv->drm, &meson_encoder_hdmi->encoder,
-				      DRM_MODE_ENCODER_TMDS);
+	ret = drm_encoder_init(priv->drm, &meson_encoder_hdmi->encoder,
+			       DRM_MODE_ENCODER_TMDS, NULL);
 	if (ret) {
 		dev_err_probe(priv->dev, ret, "Failed to init HDMI encoder\n");
 		goto err_put_node;
