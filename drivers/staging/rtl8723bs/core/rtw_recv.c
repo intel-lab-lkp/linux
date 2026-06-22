8cef0721ffacb60f594abaff9f6d7628dc30b210
André Moreira <andrem.33333@gmail.com>
2026-06-22 17:02:39 -0300

    staging: rtl8723bs: clean up style and dead code in rtw_recv.c
    Fix checkpatch.pl warnings in rtw_recv.c by removing an explicit '== true'
    comparison with extra parentheses from check_fwstate() and dropping a
    redundant 'else' block after an early return.
    
    Signed-off-by: André Moreira <andrem.33333@gmail.com>

--- drivers/staging/rtl8723bs/core/rtw_recv.c
+++ drivers/staging/rtl8723bs/core/rtw_recv.c
@@ -1597,7 +1597,7 @@ static signed int wlanhdr_to_ethhdr(union recv_frame *precvframe)
 	eth_type = ntohs(be_tmp); /* pattrib->ether_type */
 	pattrib->eth_type = eth_type;
 
-	if ((check_fwstate(pmlmepriv, WIFI_MP_STATE) == true)) {
+	if (check_fwstate(pmlmepriv, WIFI_MP_STATE)) {
 		ptr += rmv_len;
 		*ptr = 0x87;
 		*(ptr + 1) = 0x12;
@@ -1841,8 +1841,7 @@ static int enqueue_reorder_recvframe(struct recv_reorder_ctrl *preorder_ctrl, un
 			/* Duplicate entry is found!! Do not insert current entry. */
 			/* spin_unlock_irqrestore(&ppending_recvframe_queue->lock, irql); */
 			return false;
-		else
-			break;
+		break;
 
 	}
 
