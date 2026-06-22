afc60c2838e74b5465c8d325cd5e09ee47bd7ef4
Joe Simmons-Talbott <joest@redhat.com>
2026-06-22 15:43:04 -0400

    selftests/cgroup: Adjust cpu.max quota based on HZ
    For lower HZ values a quota of 1000us is much lower than the amount
    of microseconds per tick which makes the tests test_cpucg_max and
    test_cpugc_max_nested fail. Use the amount of microseconds per tick
    as the quota value.
    
    Signed-off-by: Joe Simmons-Talbott <joest@redhat.com>

--- tools/testing/selftests/cgroup/test_cpu.c
+++ tools/testing/selftests/cgroup/test_cpu.c
@@ -640,13 +640,37 @@ test_cpucg_nested_weight_underprovisioned(const char *root)
 }
 
 /*
+ * Best effort attempt to get the kernel's HZ value from the config.
+ * Return the HZ value if found otherwise return -1 to indicate failure.
+ */
+static long
+_get_config_hz(void)
+{
+	long hz = -1;
+	FILE *f;
+	char cmd[256] = "zcat /proc/config.gz 2>/dev/null | grep '^CONFIG_HZ='";
+
+	f = popen(cmd, "r");
+
+	if (!f)
+		goto out;
+
+	fscanf(f, "CONFIG_HZ=%ld", &hz);
+
+out:
+	pclose(f);
+	return hz;
+}
+
+/*
  * This test creates a cgroup with some maximum value within a period, and
  * verifies that a process in the cgroup is not overscheduled.
  */
 static int test_cpucg_max(const char *root)
 {
 	int ret = KSFT_FAIL;
-	long quota_usec = 1000;
+	long hz = _get_config_hz();
+	long quota_usec;
 	long default_period_usec = 100000; /* cpu.max's default period */
 	long duration_seconds = 1;
 
@@ -654,6 +678,8 @@ static int test_cpucg_max(const char *root)
 	long usage_usec, n_periods, remainder_usec, expected_usage_usec;
 	char *cpucg;
 	char quota_buf[32];
+
+	quota_usec = hz != -1 ? USEC_PER_SEC / hz : 1000;
 
 	snprintf(quota_buf, sizeof(quota_buf), "%ld", quota_usec);
 
@@ -710,7 +736,8 @@ cleanup:
 static int test_cpucg_max_nested(const char *root)
 {
 	int ret = KSFT_FAIL;
-	long quota_usec = 1000;
+	long quota_usec;
+	long hz = _get_config_hz();
 	long default_period_usec = 100000; /* cpu.max's default period */
 	long duration_seconds = 1;
 
@@ -718,6 +745,8 @@ static int test_cpucg_max_nested(const char *root)
 	long usage_usec, n_periods, remainder_usec, expected_usage_usec;
 	char *parent, *child;
 	char quota_buf[32];
+
+	quota_usec = hz != -1 ? USEC_PER_SEC / hz : 1000;
 
 	snprintf(quota_buf, sizeof(quota_buf), "%ld", quota_usec);
 
