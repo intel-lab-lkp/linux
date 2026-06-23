f1d685132d98d714bc00fc69120dcc4ccd91f2f5
Jingyi Wang <jingyi.wang@oss.qualcomm.com>
2026-06-23 02:05:36 -0700

    remoteproc: core: cancel crash_handler work in rproc_add() error path
    Ensure crash_handler work is cancelled before tearing down rproc resources
    to avoid accessing freed memory.
    
    Signed-off-by: Jingyi Wang <jingyi.wang@oss.qualcomm.com>

--- drivers/remoteproc/remoteproc_core.c
+++ drivers/remoteproc/remoteproc_core.c
@@ -2327,6 +2327,7 @@ int rproc_add(struct rproc *rproc)
 	return 0;
 
 rproc_remove_dev:
+	cancel_work_sync(&rproc->crash_handler);
 	rproc_delete_debug_dir(rproc);
 	device_del(dev);
 rproc_remove_cdev:
