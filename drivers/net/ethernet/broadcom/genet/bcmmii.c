ae84f7cea8471afa3f42725d16b5477f3a27e41e
Pengpeng Hou <pengpeng@iscas.ac.cn>
2026-06-23 14:15:24 +0800

    net: bcmgenet: report MDIO busy timeout
    bcmgenet_mii_wait() waits for the UNIMAC MDIO command busy bit to clear,
    but ignores wait_event_timeout() and always returns success.
    
    Return -ETIMEDOUT when the busy bit remains set for the whole wait
    period so MDIO callers see the failed transaction instead of continuing
    as if the bus were idle.
    
    Signed-off-by: Pengpeng Hou <pengpeng@iscas.ac.cn>

--- drivers/net/ethernet/broadcom/genet/bcmmii.c
+++ drivers/net/ethernet/broadcom/genet/bcmmii.c
@@ -439,10 +439,12 @@ static int bcmgenet_mii_wait(void *wait_func_data)
 {
 	struct bcmgenet_priv *priv = wait_func_data;
 
-	wait_event_timeout(priv->wq,
-			   !(bcmgenet_umac_readl(priv, UMAC_MDIO_CMD)
-			   & MDIO_START_BUSY),
-			   HZ / 100);
+	if (!wait_event_timeout(priv->wq,
+				!(bcmgenet_umac_readl(priv, UMAC_MDIO_CMD)
+				& MDIO_START_BUSY),
+				HZ / 100))
+		return -ETIMEDOUT;
+
 	return 0;
 }
 
