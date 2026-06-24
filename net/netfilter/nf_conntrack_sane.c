a1266eb7ecf15ffb70e649777504f2edc582737e
Carlos Grillet <carlos@carlosgrillet.me>
2026-06-24 20:40:31 +0200

    netfilter: nf_conntrack_sane: replace u_int16_t with u16
    Use preferred kernel integer type u16 instead of the POSIX u_int16_t
    variant.
    
    No functional change.
    
    Signed-off-by: Carlos Grillet <carlos@carlosgrillet.me>

--- net/netfilter/nf_conntrack_sane.c
+++ net/netfilter/nf_conntrack_sane.c
@@ -35,7 +35,7 @@ MODULE_DESCRIPTION("SANE connection tracking helper");
 MODULE_ALIAS_NFCT_HELPER(HELPER_NAME);
 
 #define MAX_PORTS 8
-static u_int16_t ports[MAX_PORTS];
+static u16 ports[MAX_PORTS];
 static unsigned int ports_c;
 module_param_array(ports, ushort, &ports_c, 0400);
 
