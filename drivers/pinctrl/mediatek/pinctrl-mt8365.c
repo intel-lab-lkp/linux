4123894726f257cde3d5de8be7c9e6cb252681c1
Justin Yeh <justin.yeh@mediatek.com>
2026-06-26 09:44:15 +0800

    pinctrl: mediatek: mt8365: Enable module build support
    Add MODULE_LICENSE("GPL") macro and change Kconfig option from
    bool to tristate to allow building as a loadable kernel module.
    
    This is required for Android GKI + vendor_dlkm deployments where
    vendor-specific drivers must be kept separate from the GKI vmlinux.
    
    Fixes: e94d8b6fb83a ("pinctrl: mediatek: add support for mt8365 SoC")
    Signed-off-by: Justin Yeh <justin.yeh@mediatek.com>

--- drivers/pinctrl/mediatek/pinctrl-mt8365.c
+++ drivers/pinctrl/mediatek/pinctrl-mt8365.c
@@ -494,4 +494,5 @@ static int __init mtk_pinctrl_init(void)
 arch_initcall(mtk_pinctrl_init);
 
 MODULE_DESCRIPTION("MediaTek MT8365 Pinctrl Driver");
+MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Zhiyong Tao <zhiyong.tao@mediatek.com>");
