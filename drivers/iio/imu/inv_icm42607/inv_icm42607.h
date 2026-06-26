e6fbf96b19ee266ffdd9b5e5d019da439651ac58
Chris Morgan <macromorgan@hotmail.com>
2026-06-26 11:12:28 -0500

    iio: imu: inv_icm42607: Add Gyroscope to icm42607
    Add gyroscope functions to the icm42607 driver.
    
    Signed-off-by: Chris Morgan <macromorgan@hotmail.com>

--- drivers/iio/imu/inv_icm42607/inv_icm42607.h
+++ drivers/iio/imu/inv_icm42607/inv_icm42607.h
@@ -142,6 +142,7 @@ struct inv_icm42607_suspended {
  *  @lock:		lock for serializing multiple registers access.
  *  @map:		regmap pointer.
  *  @indio_accel:	accelerometer IIO device.
+ *  @indio_gyro:	gyroscope IIO device.
  *  @vddio_supply:	I/O voltage regulator for the chip.
  *  @vddio_en:		I/O voltage status for runtime PM.
  *  @suspended:		suspended sensors configuration.
@@ -153,6 +154,7 @@ struct inv_icm42607_state {
 	struct mutex lock;
 	struct regmap *map;
 	struct iio_dev *indio_accel;
+	struct iio_dev *indio_gyro;
 	struct regulator *vddio_supply;
 	bool vddio_en;
 	struct inv_icm42607_suspended suspended;
@@ -413,6 +415,8 @@ int inv_icm42607_read_sensor(struct iio_dev *indio_dev,
 int inv_icm42607_core_probe(struct regmap *regmap,
 			    const struct inv_icm42607_hw *hw,
 			    inv_icm42607_bus_setup bus_setup);
+
+struct iio_dev *inv_icm42607_gyro_init(struct inv_icm42607_state *st);
 
 struct iio_dev *inv_icm42607_accel_init(struct inv_icm42607_state *st);
 
