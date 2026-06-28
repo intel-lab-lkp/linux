a0c03dd260b9d89e94599124c2363bb32b865c06
Yousef Alhouseen <alhouseenyousef@gmail.com>
2026-06-28 11:39:29 +0200

    net: usb: rtl8150: handle link status read failures
    set_carrier() ignores the result of the USB control transfer and tests
    the stack variable supplied as its receive buffer. If the device rejects
    or aborts the request, that variable remains uninitialized and the driver
    chooses an arbitrary carrier state.
    
    Report carrier down when the link status cannot be read.
    
    Fixes: 1da177e4c3f4 ("Linux-2.6.12-rc2")
    Reported-by: syzbot+9db6c624635564ad813c@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=9db6c624635564ad813c
    Cc: stable@vger.kernel.org
    Signed-off-by: Yousef Alhouseen <alhouseenyousef@gmail.com>

--- drivers/net/usb/rtl8150.c
+++ drivers/net/usb/rtl8150.c
@@ -732,7 +732,11 @@ static void set_carrier(struct net_device *netdev)
 	rtl8150_t *dev = netdev_priv(netdev);
 	short tmp;
 
-	get_registers(dev, CSCR, 2, &tmp);
+	if (get_registers(dev, CSCR, 2, &tmp)) {
+		netif_carrier_off(netdev);
+		return;
+	}
+
 	if (tmp & CSCR_LINK_STATUS)
 		netif_carrier_on(netdev);
 	else
