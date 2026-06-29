0cef7860b8055c4fb8c7f4b5c4ea0c63e110458b
Juergen Gross <jgross@suse.com>
2026-06-29 08:39:39 +0200

    x86/msr: Switch users of native_wrmsr() to native_wrmsrq()
    Switch all users of native_wrmsr() to native_wrmsrq() in order to
    prepare removal of native_wrmsr().
    
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- arch/x86/coco/sev/internal.h
+++ arch/x86/coco/sev/internal.h
@@ -80,12 +80,7 @@ static inline u64 sev_es_rd_ghcb_msr(void)
 
 static __always_inline void sev_es_wr_ghcb_msr(u64 val)
 {
-	u32 low, high;
-
-	low  = (u32)(val);
-	high = (u32)(val >> 32);
-
-	native_wrmsr(MSR_AMD64_SEV_ES_GHCB, low, high);
+	native_wrmsrq(MSR_AMD64_SEV_ES_GHCB, val);
 }
 
 enum es_result __vc_handle_msr(struct ghcb *ghcb, struct es_em_ctxt *ctxt, bool write);
