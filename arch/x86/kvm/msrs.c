d15c87c1413bdc4566ec672eb58cfe1137dd834e
Yosry Ahmed <yosry@kernel.org>
2026-06-30 23:47:14 +0000

    KVM: x86: Move supported EFER bits to kvm_caps
    Supported EFER bits naturally fits into kvm_caps because it needs to be
    recomputed during vendor init (e.g. to account for EFER.SVME being
    allowed/disallowed based on nested being enabled/disabled).
    
    Move efer_supported_bits into kvm_caps as supported_efer_bits (for
    naming consistency), and reinitialize it at the beginning of
    kvm_setup_efer_caps(), removing the need to clear unsupported bits in
    vendor code (e.g. EFER.SVME).
    
    As the bitmask is now globally visible as part of kvm_caps, there's
    little use for helpers to enable/disable specific bits, so drop them and
    open-code updates to kvm_caps.supported_efer_bits.
    
    No functional change intended.
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Yosry Ahmed <yosry@kernel.org>

--- arch/x86/kvm/msrs.c
+++ arch/x86/kvm/msrs.c
@@ -19,9 +19,6 @@ bool __read_mostly report_ignored_msrs = true;
 module_param(report_ignored_msrs, bool, 0644);
 EXPORT_SYMBOL_FOR_KVM_INTERNAL(report_ignored_msrs);
 
-/* Enable syscall by default because its emulated by KVM */
-static u64 __read_mostly efer_supported_bits = ((u64)EFER_SCE);
-
 #define MAX_IO_MSRS 256
 
 struct msr_bitmap_range {
@@ -606,7 +603,7 @@ static bool __kvm_valid_efer(struct kvm_vcpu *vcpu, u64 efer)
 }
 bool kvm_valid_efer(struct kvm_vcpu *vcpu, u64 efer)
 {
-	if (!(efer & efer_supported_bits))
+	if (!(efer & kvm_caps.supported_efer_bits))
 		return false;
 
 	return __kvm_valid_efer(vcpu, efer);
@@ -619,7 +616,7 @@ static int set_efer(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 	u64 efer = msr_info->data;
 	int r;
 
-	if (!(efer & efer_supported_bits))
+	if (!(efer & kvm_caps.supported_efer_bits))
 		return 1;
 
 	if (!msr_info->host_initiated) {
@@ -649,18 +646,6 @@ static int set_efer(struct kvm_vcpu *vcpu, struct msr_data *msr_info)
 
 	return 0;
 }
-
-void kvm_enable_efer_bits(u64 mask)
-{
-	efer_supported_bits |= mask;
-}
-EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_enable_efer_bits);
-
-void kvm_disable_efer_bits(u64 mask)
-{
-	efer_supported_bits &= ~mask;
-}
-EXPORT_SYMBOL_FOR_KVM_INTERNAL(kvm_disable_efer_bits);
 
 bool kvm_msr_allowed(struct kvm_vcpu *vcpu, u32 index, u32 type)
 {
