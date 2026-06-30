7865b51fabb44146d40b9fd8dbd7dbf06e07e8a1
Paul Handrigan <paulha@opensource.cirrus.com>
2026-06-30 16:55:47 +0100

    dt-bindings: clock: cs2600: Add support for the CS2600
    Add device tree schema for the Cirrus Logic CS2600 clock generator.
    
    The majority of the schema is typical clock, power and I2C
    properties.
    
    Passes dt_binding_check:
    make dt_binding_check DT_SCHEMA_FILES=clock/cirrus,cs2600.yaml
      SCHEMA  Documentation/devicetree/bindings/processed-schema.json
      CHKDT   ./Documentation/devicetree/bindings
      LINT    ./Documentation/devicetree/bindings
      STYLE   ./Documentation/devicetree/bindings
      DTEX    Documentation/devicetree/bindings/clock/cirrus,cs2600.example.dts
      DTC [C] Documentation/devicetree/bindings/clock/cirrus,cs2600.example.dtb
    
    Signed-off-by: Paul Handrigan <paulha@opensource.cirrus.com>
    Co-developed-by: Richard Fitzgerald <rf@opensource.cirrus.com>
    Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>

---
+++ include/dt-bindings/clock/cirrus,cs2600-clock.h
@@ -0,0 +1,18 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause*/
+/*
+ * CS2600 clock driver
+ *
+ * Copyright (C) 2023-2025 Cirrus Logic, Inc. and
+ *                         Cirrus Logic International Semiconductor Ltd.
+ */
+
+#ifndef __DT_BINDINGS_CLOCK_CS2600_H
+#define __DT_BINDINGS_CLOCK_CS2600_H
+
+/* Clock output indexes */
+#define CS2600_PLL_OUT			0
+#define CS2600_CLK_OUT			1
+#define CS2600_BCLK_OUT			2
+#define CS2600_FSYNC_OUT		3
+
+#endif /* __DT_BINDINGS_CLOCK_CS2600_H */
