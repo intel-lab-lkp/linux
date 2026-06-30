b20934bcce9371824f5009a0d3a4020b2021790d
Adrian Ng Ho Yin <adrian.ho.yin.ng@altera.com>
2026-06-30 06:20:25 -0700

    i3c: master: dw: Report actual GET CCC payload length on success
    On successful GET CCC transfers, set dests[0].payload.actual_len from
    RESPONSE_PORT_DATA_LEN so the I3C core receives the number of bytes
    actually read. Add actual_len to struct i3c_ccc_cmd_payload to keep
    the requested buffer size in len.
    
    Signed-off-by: Adrian Ng Ho Yin <adrian.ho.yin.ng@altera.com>
    Signed-off-by: Tze Yee Ng <tze.yee.ng@altera.com>

--- drivers/i3c/master/dw-i3c-master.c
+++ drivers/i3c/master/dw-i3c-master.c
@@ -742,7 +742,10 @@ static int dw_i3c_ccc_set(struct dw_i3c_master *master,
 		dw_i3c_master_dequeue_xfer(master, xfer);
 
 	ret = xfer->ret;
-	if (xfer->cmds[0].error == RESPONSE_ERROR_IBA_NACK)
+	cmd = &xfer->cmds[0];
+	if (!ret)
+		ccc->dests[0].payload.actual_len = cmd->rx_len;
+	if (cmd->error == RESPONSE_ERROR_IBA_NACK)
 		ccc->err = I3C_ERROR_M2;
 
 	return ret;
