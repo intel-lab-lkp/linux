07c3a404934d34fd069f9c7fa1112606aee96703
Shrikanth Hegde <sshegde@linux.ibm.com>
2026-07-01 19:46:43 +0530

    virt: Introduce steal monitor driver
    Introduce a new driver in virt named steal_monitor. This driver
    will compute the steal time and drive the policy decisions of preferred
    CPU state.
    
    More on it can be found in the Documentation/driver-api/steal-monitor.rst
    Introduce the skeleton code first.
    
    There is no new kconfig. It depends on CONFIG_PREFERRED_CPU.
    - If CONFIG_PREFERRED_CPU=y, it gets compiled as a module. It is not
      loaded by default.
    - If CONFIG_PREFERRED_CPU=n, module isn't compiled.
    
    File layout of the driver is designed with having arch specific
    files in the future.
    
    - sm_core.c - contains main driver code. This includes the periodic
      work function and take action on steal time.
    - defaults.c - contains the default implementation defined with __weak
      symbols.
    - sm_core.h - header file which includes data structure.
    
    Signed-off-by: Shrikanth Hegde <sshegde@linux.ibm.com>

---
+++ drivers/virt/steal_monitor/sm_core.c
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Steal time Monitor.
+ *
+ * Periodically compute steal time. Based on the thresholds either
+ * reduce/increase the preferred CPUs which can be made use
+ * by the workload to avoid vCPU preemption to an extent possible.
+ *
+ * Available as module with CONFIG_PREFERRED_CPU=y
+ *
+ * Copyright (C) 2026 IBM
+ * Author: Shrikanth Hegde <sshegde@linux.ibm.com>
+ */
+
+#include "sm_core.h"
+
+static int __init steal_monitor_init(void)
+{
+	pr_info("steal_monitor is enabled\n");
+	return 0;
+}
+
+static void __exit steal_monitor_exit(void)
+{
+	pr_info("steal_monitor is disabled\n");
+}
+
+module_init(steal_monitor_init);
+module_exit(steal_monitor_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("IBM Corporation");
+MODULE_DESCRIPTION("Virtualization Steal Time Monitor");
---
+++ drivers/virt/steal_monitor/sm_core.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef __VIRT_STEAL_CORE_H
+#define __VIRT_STEAL_CORE_H
+
+#include <linux/types.h>
+
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+#endif /* __VIRT_STEAL_CORE_H */
