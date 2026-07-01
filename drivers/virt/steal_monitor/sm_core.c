97934c72b1ccfebc6f8befa48c4c1aa6c0df8d7a
Shrikanth Hegde <sshegde@linux.ibm.com>
2026-07-01 19:46:46 +0530

    virt/steal_monitor: Add control knobs for handling steal values
    These are the knobs to control the steal_monitor.
    
    interval_ms:
    How often steal monitor checks for steal time.
    (Default: 1000 i.e 1 second)
    
    This controls how fast steal monitor driver reacts to changes to
    the contention of physical CPUs. Since it does fair amount of
    work, setting too low will have overheads. If set to 0, on next
    work it will be set to default.
    
    low_threshold:
    lower threshold value in percentage * 100.
    (Default: 200, i.e 2% steal is considered as low threshold)
    
    This determines what values should be considered as nil/no steal values.
    When steal monitor see steal time is below or equal to this value, it
    will increase the preferred CPUs by 1 core. Having value as zero
    might cause too much oscillations.
    
    high_threshold:
    higher threshold value in percentage * 100
    (Default: 500, i.e 5% steal is considered as high threshold)
    
    This determines what values should be considered as high steal values.
    When steal monitor sees steal time is higher than this value, it will
    reduce the preferred CPUs by 1 core.
    
    Also available at: Documentation/driver-api/steal-monitor.rst
    
    Signed-off-by: Shrikanth Hegde <sshegde@linux.ibm.com>

--- drivers/virt/steal_monitor/sm_core.c
+++ drivers/virt/steal_monitor/sm_core.c
@@ -14,7 +14,23 @@
 
 #include "sm_core.h"
 
-struct steal_monitor sm_core_ctx;
+struct steal_monitor sm_core_ctx = {
+	.interval_ms = 1000,	/* 1 second */
+	.high_threshold = 500,	/* 5% */
+	.low_threshold = 200,	/* 2% */
+};
+
+module_param_named(interval_ms, sm_core_ctx.interval_ms, uint, 0644);
+MODULE_PARM_DESC(interval_ms,
+		 "Sampling frequency for steal values in milliseconds (default: 1000)");
+
+module_param_named(high_threshold, sm_core_ctx.high_threshold, uint, 0644);
+MODULE_PARM_DESC(high_threshold,
+		 "High steal threshold (default: 500 i.e 5%)");
+
+module_param_named(low_threshold, sm_core_ctx.low_threshold, uint, 0644);
+MODULE_PARM_DESC(low_threshold,
+		 "Low steal threshold (default: 200 i.e 2%)");
 
 static int __init steal_monitor_init(void)
 {
