4dff4145cc9c70833f18e67c842fe4f5e73d8e48
Aaron Lewis <aaronlewis@google.com>
2026-07-01 20:33:09 +0000

    vfio: selftests: Add memfd test to vfio_dma_mapping_perf_test
    Add a second test to "vfio_dma_mapping_perf_test" to evaluate memfd
    latencies. A key motivator for adding this test is to demonstrate that
    the IOMMU_IOAS_MAP_FILE ioctl is significantly faster than other methods
    of mapping DMA regions. While this performance difference is not fully
    apparent with the test's current capabilities, it will become evident
    as more features are introduced later in the series.
    
    To support this, add IOMMU_IOAS_MAP_FILE ioctl support to the VFIO
    selftest library via iommufd_map_file().
    
    Signed-off-by: Aaron Lewis <aaronlewis@google.com>

--- tools/testing/selftests/vfio/vfio_dma_mapping_perf_test.c
+++ tools/testing/selftests/vfio/vfio_dma_mapping_perf_test.c
@@ -117,6 +117,124 @@ TEST_F(vfio_dma_mapping_perf_test, dma_map_unmap)
 	printf("Completed munmap() in %.2lfms\n", timer_elapsed_ms(start));
 }
 
+FIXTURE(vfio_dma_mapping_perf_memfd_test) {
+	struct iommu *iommu;
+	struct vfio_pci_device *device;
+	struct iova_allocator *iova_allocator;
+};
+
+FIXTURE_VARIANT(vfio_dma_mapping_perf_memfd_test) {
+	const char *iommu_mode;
+	int mmap_flags;
+	int memfd_flags;
+};
+
+#define FIXTURE_VARIANT_ADD_IOMMU_MODE(_iommu_mode, _name, _mmap_flags, _memfd_flags) \
+FIXTURE_VARIANT_ADD(vfio_dma_mapping_perf_memfd_test, _iommu_mode ## _ ## _name) {    \
+	.iommu_mode = #_iommu_mode,						      \
+	.mmap_flags = MAP_SHARED | (_mmap_flags),				      \
+	.memfd_flags = MAP_SHARED | (_memfd_flags),				      \
+}
+
+FIXTURE_VARIANT_ADD_ALL_IOMMU_MODES(memfd, 0, 0);
+FIXTURE_VARIANT_ADD_ALL_IOMMU_MODES(memfd_hugetlb_2mb,
+				    MAP_HUGETLB | MAP_HUGE_2MB,
+				    MFD_HUGETLB | MFD_HUGE_2MB);
+FIXTURE_VARIANT_ADD_ALL_IOMMU_MODES(memfd_hugetlb_1gb,
+				    MAP_HUGETLB | MAP_HUGE_1GB,
+				    MFD_HUGETLB | MFD_HUGE_1GB);
+
+#undef FIXTURE_VARIANT_ADD_IOMMU_MODE
+
+FIXTURE_SETUP(vfio_dma_mapping_perf_memfd_test)
+{
+	self->iommu = iommu_init(variant->iommu_mode);
+	self->device = vfio_pci_device_init(device_bdf, self->iommu);
+	self->iova_allocator = iova_allocator_init(self->iommu);
+}
+
+FIXTURE_TEARDOWN(vfio_dma_mapping_perf_memfd_test)
+{
+	iova_allocator_cleanup(self->iova_allocator);
+	vfio_pci_device_cleanup(self->device);
+	iommu_cleanup(self->iommu);
+}
+
+static void *setup_memfd(int *fd, u64 size, int mmap_flags, int mfd_flags)
+{
+	void *buf = MAP_FAILED;
+	struct timespec start;
+
+	timer_start(&start);
+	*fd = memfd_create("vfio_dma_mapping_perf_memfd_test", mfd_flags);
+	printf("Completed memfd_create() in %.2lfms\n", timer_elapsed_ms(start));
+	if (*fd <= 0)
+		return MAP_FAILED;
+
+	if (ftruncate(*fd, size))
+		goto out;
+
+	timer_start(&start);
+	buf = mmap(NULL, size, PROT_READ | PROT_WRITE, mmap_flags, *fd, 0);
+	printf("Completed mmap() for memfd in %.2lfms\n", timer_elapsed_ms(start));
+
+out:
+	if (buf == MAP_FAILED)
+		close(*fd);
+
+	return buf;
+}
+
+static void teardown_memfd(int fd, u64 size, void *vaddr)
+{
+	struct timespec start;
+
+	if (vaddr != MAP_FAILED) {
+		timer_start(&start);
+		munmap(vaddr, size);
+		printf("Completed munmap() in %.2lfms\n", timer_elapsed_ms(start));
+	}
+
+	if (fd != -1) {
+		timer_start(&start);
+		close(fd);
+		printf("Completed close() in %.2lfms\n", timer_elapsed_ms(start));
+	}
+}
+
+TEST_F(vfio_dma_mapping_perf_memfd_test, dma_map_unmap_from_file)
+{
+	const u64 size = SZ_1G;
+	struct dma_region region;
+	struct timespec start;
+	u64 unmapped;
+	int rc, fd;
+
+	region.vaddr = setup_memfd(&fd, size, variant->mmap_flags, variant->memfd_flags);
+	ASSERT_NE(region.vaddr, MAP_FAILED);
+
+	region.iova = iova_allocator_alloc(self->iova_allocator, size);
+	region.size = size;
+
+	timer_start(&start);
+	if (strcmp(variant->iommu_mode, MODE_IOMMUFD) == 0) {
+		iommufd_map_file(self->iommu, &region, fd);
+	} else {
+		iommu_map(self->iommu, &region);
+	}
+	printf("Mapped HVA %p (size %luG) at IOVA 0x%lx in %.2lfms\n",
+	       region.vaddr, size / SZ_1G, region.iova, timer_elapsed_ms(start));
+	ASSERT_EQ(region.iova, to_iova(self->device, region.vaddr));
+
+	timer_start(&start);
+	rc = __iommu_unmap(self->iommu, &region, &unmapped);
+	printf("Unmapped IOVA 0x%lx in %.2lfms\n", region.iova, timer_elapsed_ms(start));
+	ASSERT_EQ(rc, 0);
+	ASSERT_EQ(unmapped, region.size);
+
+	teardown_memfd(fd, size, region.vaddr);
+}
+
 int main(int argc, char *argv[])
 {
 	device_bdf = vfio_selftests_get_bdf(&argc, argv);
