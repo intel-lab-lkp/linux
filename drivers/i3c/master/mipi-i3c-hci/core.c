7ae02439c2b5294a7056bc41e5d50b59b7945d21
Adrian Hunter <adrian.hunter@intel.com>
2026-07-01 23:05:51 +0300

    i3c: mipi-i3c-hci: Factor out i3c_hci_sysdev()
    The MIPI I3C HCI driver needs to identify the underlying system device
    used for DMA mapping and PM operations. The logic for determining that
    device is currently embedded in the DMA implementation.
    
    Factor this code out into i3c_hci_sysdev() so it can be shared by other
    parts of the driver and keep the device-selection logic in one place.
    
    Signed-off-by: Adrian Hunter <adrian.hunter@intel.com>

--- drivers/i3c/master/mipi-i3c-hci/core.c
+++ drivers/i3c/master/mipi-i3c-hci/core.c
@@ -15,6 +15,7 @@
 #include <linux/interrupt.h>
 #include <linux/iopoll.h>
 #include <linux/module.h>
+#include <linux/pci.h>
 #include <linux/platform_data/mipi-i3c-hci.h>
 #include <linux/platform_device.h>
 #include <linux/pm_runtime.h>
@@ -115,6 +116,17 @@
 static inline struct i3c_hci *to_i3c_hci(struct i3c_master_controller *m)
 {
 	return container_of(m, struct i3c_hci, master);
+}
+
+/*
+ * Determine the device that does PM / DMA and has IOMMU setup done for it in
+ * case of enabled IOMMU (for use with the DMA API).
+ * Such device is either "mipi-i3c-hci" platform device (OF/ACPI enumeration)
+ * parent or grandparent (PCI enumeration).
+ */
+struct device *i3c_hci_sysdev(struct device *dev)
+{
+	return dev->parent && dev_is_pci(dev->parent) ? dev->parent : dev;
 }
 
 static void i3c_hci_set_master_dyn_addr(struct i3c_hci *hci)
