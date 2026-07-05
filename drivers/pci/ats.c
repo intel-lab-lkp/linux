b877bdc8bbb67b2d8b59641c73f9d40ef98d47c4
Dan Williams <djbw@kernel.org>
2026-07-05 15:08:13 -0700

    PCI, device core: Move "untrusted" concept to DEVICE_TRUST_ADVERSARY
    An "adversary" device is one that the kernel is allowed to operate, but
    with extra safety / paranoia added. It aims to keep the device bounded in
    both address space and time (no batched invalidation) for DMA accesses. The
    concept of adversarial devices already exists in the PCI core. The
    categorization is applied to externally attached PCI devices by default
    (like Thunderbolt attached devices). Unlike DEVICE_TRUST_NONE that says "do
    not allow driver bind", DEVICE_TRUST_ADVERSARY says "allow driver bind as
    long as the driver and/or IOMMU are taking precautions." Uplevel this trust
    mechanism from a PCI device "untrusted" boolean flag to the core device
    trust level.
    
    The rationale for dev->bus_trust separate from dev->p->trust is to give
    buses the ability to establish a trust level before device_add(). Then,
    after device_add(), the dependency on 'struct device_private' mandates that
    the device-core own and coordinate changes to the device's trust level.
    
    There are implications of the bus expressing less than full trust in a
    device and a module later expressing higher levels of trust. For PCI, the
    "adversary" / pci_untrusted() determination is made prior to initial
    iommu_probe_device(). The default IOMMU domain determination is latched
    from bus's specified trust value. Later changes to the trust level, for
    example by "modprobe $module trust=auto" skip that IOMMU probing. The
    result is that moving a device to DEVICE_TRUST_AUTO may lift translation
    blocking (PCI_ACS_TB), but not change the default IOMMU domain from
    IOMMU_DOMAIN_DMA.
    
    The default domain can be validated / changed via iommu_group sysfs. The
    longer term goal is to dynamically adjust iommu domain at the next
    $bus_dma_configure() event, but that needs more surgery and consideration
    for multi-device iommu groups.
    
    Cc: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Cc: "Rafael J. Wysocki" <rafael@kernel.org>
    Cc: Danilo Krummrich <dakr@kernel.org>
    Cc: Will Deacon <will@kernel.org>
    Cc: Robin Murphy <robin.murphy@arm.com>
    Cc: Jason Gunthorpe <jgg@ziepe.ca>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Alexey Kardashevskiy <aik@amd.com>
    Cc: Xu Yilun <yilun.xu@linux.intel.com>
    Cc: "Aneesh Kumar K.V" <aneesh.kumar@kernel.org>
    Cc: Dexuan Cui <decui@microsoft.com>
    Signed-off-by: Dan Williams <djbw@kernel.org>

--- drivers/pci/ats.c
+++ drivers/pci/ats.c
@@ -43,7 +43,7 @@ bool pci_ats_supported(struct pci_dev *dev)
 	if (!dev->ats_cap)
 		return false;
 
-	return (dev->untrusted == 0);
+	return !pci_untrusted(dev);
 }
 EXPORT_SYMBOL_GPL(pci_ats_supported);
 
