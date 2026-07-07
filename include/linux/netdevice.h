30669bdf58b68cb3a9e209529acfb5e59fd6e682
Eric Woudstra <ericwouds@gmail.com>
2026-07-07 11:10:40 +0200

    net: core: dev: Add dev_fill_bridge_path()
    New function dev_fill_bridge_path(), similar to dev_fill_forward_path().
    It handles starting from a bridge port instead of the bridge master.
    The structures ctx and nft_forward_info need to be already filled in with
    the (vlan) encaps.
    
    Reviewed-by: Nikolay Aleksandrov <razor@blackwall.org>
    Signed-off-by: Eric Woudstra <ericwouds@gmail.com>

--- include/linux/netdevice.h
+++ include/linux/netdevice.h
@@ -3420,6 +3420,8 @@ void dev_remove_offload(struct packet_offload *po);
 
 int dev_get_iflink(const struct net_device *dev);
 int dev_fill_metadata_dst(struct net_device *dev, struct sk_buff *skb);
+int dev_fill_bridge_path(struct net_device_path_ctx *ctx,
+			 struct net_device_path_stack *stack);
 int dev_fill_forward_path(const struct net_device *dev, const u8 *daddr,
 			  struct net_device_path_stack *stack);
 struct net_device *dev_get_by_name(struct net *net, const char *name);
