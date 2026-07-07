bad5d3996ddfec8eeec0ed4cc119116480c6b5d5
Wandun Chen <chenwandun@lixiang.com>
2026-07-07 20:59:22 +0800

    mm/migrate: do not migrate folios mapped into VM_LOCKED VMAs under compaction
    When compact_unevictable_allowed=0, unevictable pages should not be
    migrated. However, mlock_folio_batch in the mlock[all] syscall introduces
    a race, mlock_folio() sets PG_mlocked immediately but defers PG_unevictable
    to mlock_folio_batch(), causing pages that are about to become unevictable
    to be migrated, which violates the intent of compact_unevictable_allowed,
    and causes spike latency in RT kernels [1].
    
    In order to fix this, migration is forbidden for pages mapped into VMAs
    marked with VM_LOCKED. In addition, two early-return paths are introduced,
    filter out mlocked pages, return early to avoid unnecessary operations.
    
    Fixes: 90d07210ab55 ("mm: mlock: use folios and a folio batch internally")
    Reported-by: Alexander Krabler <Alexander.Krabler@kuka.com>
    Closes: https://lore.kernel.org/all/DU0PR01MB10385345F7153F334100981888259A@DU0PR01MB10385.eurprd01.prod.exchangelabs.com/ [1]
    Suggested-by: Vlastimil Babka <vbabka@suse.cz>
    Signed-off-by: Wandun Chen <chenwandun@lixiang.com>
    Link: https://lore.kernel.org/linux-rt-users/33275585-f2db-4779-89f0-3ae24b455a67@suse.cz/#t

--- mm/migrate.c
+++ mm/migrate.c
@@ -1207,7 +1207,7 @@ static void migrate_folio_done(struct folio *src,
 static int migrate_folio_unmap(new_folio_t get_new_folio,
 		free_folio_t put_new_folio, unsigned long private,
 		struct folio *src, struct folio **dstp, enum migrate_mode mode,
-		struct list_head *ret)
+		struct list_head *ret, enum migrate_reason reason)
 {
 	struct folio *dst;
 	int rc = -EAGAIN;
@@ -1215,6 +1215,7 @@ static int migrate_folio_unmap(new_folio_t get_new_folio,
 	struct anon_vma *anon_vma = NULL;
 	bool locked = false;
 	bool dst_locked = false;
+	enum ttu_flags ttu = 0;
 
 	dst = get_new_folio(src, private);
 	if (!dst)
@@ -1254,8 +1255,14 @@ static int migrate_folio_unmap(new_folio_t get_new_folio,
 		folio_lock(src);
 	}
 	locked = true;
-	if (folio_test_mlocked(src))
+	if (folio_test_mlocked(src)) {
 		old_folio_state |= FOLIO_WAS_MLOCKED;
+
+		if (reason == MR_COMPACTION && !compaction_allow_unevictable()) {
+			rc = -EBUSY;
+			goto out;
+		}
+	}
 
 	if (folio_test_writeback(src)) {
 		/*
@@ -1329,7 +1336,14 @@ static int migrate_folio_unmap(new_folio_t get_new_folio,
 		/* Establish migration ptes */
 		VM_BUG_ON_FOLIO(folio_test_anon(src) &&
 			       !folio_test_ksm(src) && !anon_vma, src);
-		try_to_migrate(src, mode == MIGRATE_ASYNC ? TTU_BATCH_FLUSH : 0);
+
+		if (mode == MIGRATE_ASYNC)
+			ttu |= TTU_BATCH_FLUSH;
+
+		if (reason == MR_COMPACTION && !compaction_allow_unevictable())
+			ttu |= TTU_RESPECT_MLOCK;
+
+		try_to_migrate(src, ttu);
 		old_folio_state |= FOLIO_WAS_MAPPED;
 	}
 
@@ -1910,7 +1924,8 @@ static int migrate_pages_batch(struct list_head *from,
 			}
 
 			rc = migrate_folio_unmap(get_new_folio, put_new_folio,
-					private, folio, &dst, mode, ret_folios);
+					private, folio, &dst, mode, ret_folios,
+					reason);
 			/*
 			 * The rules are:
 			 *	0: folio will be put on unmap_folios list,
