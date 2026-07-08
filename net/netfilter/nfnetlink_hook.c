58ab2fbb2cce7b896933e7c0c53f8d1e26c514bd
Phil Sutter <phil@nwl.cc>
2026-07-08 18:19:38 +0200

    netfilter: nfnetlink_hook: Deref hook entry using READ_ONCE()
    Writer (nf_remove_net_hook) assigns to the field value using
    WRITE_ONCE(), appropriately call READ_ONCE() to make sure reader
    (nfnl_hook_dump) sees either the old or new value, not both.
    
    Fixes: b010e2a4a9ac ("netfilter: nfnetlink_hook: Dump nat type chains")
    Signed-off-by: Phil Sutter <phil@nwl.cc>

--- net/netfilter/nfnetlink_hook.c
+++ net/netfilter/nfnetlink_hook.c
@@ -390,10 +390,12 @@ static int nfnl_hook_dump(struct sk_buff *nlskb,
 	ops = nf_hook_entries_get_hook_ops(e);
 
 	for (; i < e->num_hook_entries; i++) {
-		if (ops[i]->hook_ops_type == NF_HOOK_OP_NAT)
-			err = nfnl_hook_dump_nat(nlskb, cb, ops[i], family);
+		struct nf_hook_ops *cur = READ_ONCE(ops[i]);
+
+		if (cur->hook_ops_type == NF_HOOK_OP_NAT)
+			err = nfnl_hook_dump_nat(nlskb, cb, cur, family);
 		else
-			err = nfnl_hook_dump_one(nlskb, ctx, ops[i],
+			err = nfnl_hook_dump_one(nlskb, ctx, cur,
 						 ops[i]->priority, family,
 						 cb->nlh->nlmsg_seq);
 		if (err)
