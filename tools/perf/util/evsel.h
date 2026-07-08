904ff93c6ce343f3a49aaa6906ce1325e67ea4ea
Namhyung Kim <namhyung@kernel.org>
2026-07-08 16:31:03 -0700

    perf evsel: Arrange some fields that should be cloned
    In the evsel, there's an internal struct to put fields need copy when
    the evsel is cloned.  This is purely to make it easier track those
    fields even if it sometimes failed to do so. :)
    
    Signed-off-by: Namhyung Kim <namhyung@kernel.org>

--- tools/perf/util/evsel.h
+++ tools/perf/util/evsel.h
@@ -70,6 +70,10 @@ struct evsel {
 		const char		*unit;
 		struct cgroup		*cgrp;
 		const char		*metric_id;
+
+		/* The PMU the event is from. Used for missing_features, PMU name, etc. */
+		struct perf_pmu		*pmu;
+
 		/*
 		 * This point to the first evsel with the same name, intended to store the
 		 * aggregated counts in aggregation mode.
@@ -90,6 +94,8 @@ struct evsel {
 		bool			skippable;
 		bool			retire_lat;
 		bool			dont_regroup;
+		bool			default_metricgroup; /* A member of the Default metricgroup */
+		bool			default_show_events; /* If a default group member, show the event */
 		int			bpf_fd;
 		struct bpf_object	*bpf_obj;
 		struct list_head	config_terms;
@@ -121,8 +127,6 @@ struct evsel {
 	bool			cmdline_group_boundary;
 	bool			reset_group;
 	bool			needs_auxtrace_mmap;
-	bool			default_metricgroup; /* A member of the Default metricgroup */
-	bool			default_show_events; /* If a default group member, show the event */
 	bool			needs_uniquify;
 	bool			fallenback_eacces;
 	bool			fallenback_eopnotsupp;
@@ -177,9 +181,6 @@ struct evsel {
 	};
 	unsigned long		open_flags;
 	int			precise_ip_original;
-
-	/* The PMU the event is from. Used for missing_features, PMU name, etc. */
-	struct perf_pmu		*pmu;
 
 	/* For tool events */
 	/* Beginning time subtracted when the counter is read. */
