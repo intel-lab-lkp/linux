60d33442c25aa7ed586ba9e79d6f5467593f595a
James Clark <james.clark@linaro.org>
2026-07-09 16:25:21 +0100

    coresight: tmc-etr: Prevent per-thread events from sharing a sink
    Sinks are potentially shared between multiple ETMs, and even with
    Coresight being an exclusive PMU, multiple per-thread events in parallel
    are supported. If both of the events are owned by the same PID then the
    sink becomes shared resulting in trace from the wrong thread appearing
    in a per-thread buffer and WARNs in the driver being hit:
    
      $ perf test -w named_threads 2 10 &
      $ perf record -e cs_etm//u --per-thread --pid 984
    
      WARNING: drivers/hwtracing/coresight/coresight-tmc-etr.c:1654 at tmc_update_etr_buffer+0x328/0x348 [coresight_tmc], CPU#1: perf/686
      [...]
      Call trace:
       tmc_update_etr_buffer+0x328/0x348 [coresight_tmc] (P)
       etm_event_stop+0x1c8/0x260 [coresight]
       etm_event_del+0x20/0x38 [coresight]
       event_sched_out+0xc4/0x1c8
       group_sched_out+0x5c/0x170
       __pmu_ctx_sched_out+0xe8/0x148
       ctx_sched_out+0x12c/0x178
       perf_event_exit_task_context+0xb0/0x3a8
       perf_event_exit_task+0xa4/0x138
       do_exit+0x1cc/0x808
       do_group_exit+0x7c/0xb0
       get_signal+0x628/0x678
       arch_do_signal_or_restart+0x98/0x1b28
       exit_to_user_mode_loop+0x90/0x188
       el0_svc+0x1cc/0x260
       el0t_64_sync_handler+0x78/0x130
       el0t_64_sync+0x198/0x1a0
    
    Fix it by returning -EBUSY if a second per-thread event tries to use the
    same sink. Users will only be able to use per-thread mode reliably with
    multiple threads on a system that has a dedicated sink per ETM. This
    limitation hasn't changed since this fix, it just makes the behavior
    more consistent.
    
    Fixes: 8d03cfd16a72 ("coresight: tmc-etr: Add support for CPU-wide trace scenarios")
    Signed-off-by: James Clark <james.clark@linaro.org>

--- drivers/hwtracing/coresight/coresight-core.c
+++ drivers/hwtracing/coresight/coresight-core.c
@@ -2246,6 +2246,37 @@ int coresight_get_enable_clocks(struct device *dev, struct clk **pclk,
 }
 EXPORT_SYMBOL_GPL(coresight_get_enable_clocks);
 
+/*
+ * Returns true if a sink that's potentially connected to multiple ETMs can be
+ * used in parallel with another event.
+ */
+bool coresight_sink_can_share(struct perf_event *existing,
+			      struct perf_event *new)
+{
+	/*
+	 * Inheritance in per-CPU mode means an event might not have an owner,
+	 * but the parent will.
+	 */
+	struct perf_event *existing_root = existing->parent ?: existing;
+	struct perf_event *new_root = new->parent ?: new;
+
+	/*
+	 * In per-thread mode only one event can use a sink, otherwise a sink
+	 * shared by multiple ETMs could have trace from two different threads
+	 * in it from two different events.
+	 */
+	if (existing->cpu == -1)
+		return existing == new;
+
+	/*
+	 * In per-CPU mode allow the sink to be shared across events, userspace
+	 * expects data from multiple CPUs in a shared sink. The only
+	 * restriction is the events must have the same owner.
+	 */
+	return existing_root->owner == new_root->owner;
+}
+EXPORT_SYMBOL_GPL(coresight_sink_can_share);
+
 MODULE_LICENSE("GPL v2");
 MODULE_AUTHOR("Pratik Patel <pratikp@codeaurora.org>");
 MODULE_AUTHOR("Mathieu Poirier <mathieu.poirier@linaro.org>");
