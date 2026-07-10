e4cd1db49dda39ec706ad35e35da37d55fffa30e
Phil Sutter <phil@nwl.cc>
2026-07-10 17:14:11 +0200

    netfilter: nfnetlink_hook: Fix for concurrent NAT hooks dump and change
    If hooks are added or deleted, the nf_hook_entries buffer is reallocated
    and the RCU-protected pointer updated. If a dump requires multiple parts
    (exceeding sk_buff space) the second invocation may see a different
    nf_hook_entries pointer than the first one. Detect this just like the
    outer nfnl_hook_dump function does, by storing the pointer value in
    context. If it changes, signal user space it must restart.
    
    As with the stored loop counter, that stored pointer value must be
    manually zeroed upon successful loop completion since this inner
    function may run multiple times for different nf_hook_ops but with same
    context.
    
    Fixes: b010e2a4a9ac ("netfilter: nfnetlink_hook: Dump nat type chains")
    Signed-off-by: Phil Sutter <phil@nwl.cc>

--- net/netfilter/nfnetlink_hook.c
+++ net/netfilter/nfnetlink_hook.c
@@ -55,6 +55,7 @@ static int nf_netlink_dump_start_rcu(struct sock *nlsk, struct sk_buff *skb,
 struct nfnl_dump_hook_data {
 	char devname[IFNAMSIZ];
 	unsigned long headv;
+	unsigned long natv;
 	u8 hook;
 };
 
@@ -348,6 +349,15 @@ static int nfnl_hook_dump_nat(struct sk_buff *nlskb,
 	unsigned int i = cb->args[1];
 	int err = 0;
 
+	if (!ctx->natv)
+		ctx->natv = (unsigned long)e;
+
+	if ((e && i >= e->num_hook_entries) ||
+	    ctx->natv != (unsigned long)e) {
+		cb->seq++;
+		return -EINTR;
+	}
+
 	if (!e)
 		return 0;
 
@@ -362,8 +372,10 @@ static int nfnl_hook_dump_nat(struct sk_buff *nlskb,
 			break;
 	}
 
-	if (!err)
+	if (!err) {
+		ctx->natv = 0;
 		i = 0;
+	}
 	cb->args[1] = i;
 	return err;
 }
