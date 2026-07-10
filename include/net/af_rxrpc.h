ddd6e56539447f49097e9e30d170b592d8901486
David Howells <dhowells@redhat.com>
2026-07-10 20:22:18 +0100

    rxrpc: Remove OOB challenge/response code
    Currently, when a CHALLENGE packet comes in, it's queued in an OOB queue on
    the AF_RXRPC socket that generated one of the calls on that connection for
    the application (which might be in userspace) to service.  The application
    then picks up the CHALLENGE and requests a RESPONSE packet be generated,
    allowing the app to include app-specific data in it if appropriate.  There
    is, however, no actual limit on the capacity of the CHALLENGE queue, and
    this could be abused remotely - and also getting the OOB mechanism right
    has proven tricky.
    
    Further, by analogy with other AFS codebases, it's not actually necessary
    to generate the application data in response to the CHALLENGE.  The reason
    I did this was to set the encryption on the app-data to be the same as that
    specified in the CHALLENGE as the server must be able to handle that.
    However, it's sufficient to use the encoding type set in the token that is
    going to be sent to the server; presumably the kerberos server knows that
    the fileserver can handle that type - otherwise why tell the client to use
    it?
    
    This is the final part of the fix.  It removes all the OOB communication
    stuff along with the UAPI elements related to them.
    
    Fixes: 5800b1cf3fd8 ("rxrpc: Allow CHALLENGEs to the passed to the app for a RESPONSE")
    Link: https://sashiko.dev/#/patchset/20260624163819.3017002-1-dhowells%40redhat.com
    Signed-off-by: David Howells <dhowells@redhat.com>
    cc: Marc Dionne <marc.dionne@auristor.com>
    cc: Jeffrey Altman <jaltman@auristor.com>
    cc: Eric Dumazet <edumazet@google.com>
    cc: "David S. Miller" <davem@davemloft.net>
    cc: Jakub Kicinski <kuba@kernel.org>
    cc: Paolo Abeni <pabeni@redhat.com>
    cc: Simon Horman <horms@kernel.org>
    cc: Jarkko Sakkinen <jarkko@kernel.org>
    cc: linux-afs@lists.infradead.org
    cc: keyrings@vger.kernel.org
    cc: stable@kernel.org

--- include/net/af_rxrpc.h
+++ include/net/af_rxrpc.h
@@ -92,24 +92,6 @@ bool rxrpc_kernel_check_life(const struct socket *, const struct rxrpc_call *);
 
 int rxrpc_sock_set_min_security_level(struct sock *sk, unsigned int val);
 int rxrpc_sock_set_security_keyring(struct sock *, struct key *);
-int rxrpc_sock_set_manage_response(struct sock *sk, bool set);
-
-enum rxrpc_oob_type rxrpc_kernel_query_oob(struct sk_buff *oob,
-					   struct rxrpc_peer **_peer,
-					   unsigned long *_peer_appdata);
-struct sk_buff *rxrpc_kernel_dequeue_oob(struct socket *sock,
-					 enum rxrpc_oob_type *_type);
-void rxrpc_kernel_free_oob(struct sk_buff *oob);
-void rxrpc_kernel_query_challenge(struct sk_buff *challenge,
-				  struct rxrpc_peer **_peer,
-				  unsigned long *_peer_appdata,
-				  u16 *_service_id, u8 *_security_index);
-int rxrpc_kernel_reject_challenge(struct sk_buff *challenge, u32 abort_code,
-				  int error, enum rxrpc_abort_reason why);
-int rxkad_kernel_respond_to_challenge(struct sk_buff *challenge);
-u32 rxgk_kernel_query_challenge(struct sk_buff *challenge);
-int rxgk_kernel_respond_to_challenge(struct sk_buff *challenge,
-				     struct krb5_buffer *appdata);
 void rxrpc_kernel_query_key(const struct key *key, u8 *_security_index,
 			    u32 *_krb5_enctype);
 u8 rxrpc_kernel_query_call_security(struct rxrpc_call *call,
