17435907971bc5a03600d0bb82c2be782ae9d802
Jiebin Sun <jiebin.sun@intel.com>
2026-07-10 16:42:36 +0800

    perf c2c: add function view type definitions and helpers
    Add the foundational type definitions and helper functions for the
    function view browser:
    
    - perf_c2c_ext: extended context holding function-grouped histograms
    - c2c_function_browser: browser state wrapping hist_browser
    - symbol_name_equal(): symbol comparison by name
    - hist_entry__iaddr(): instruction address extraction helper
    - c2c_hitm_count(): inline accessor for a c2c_stats HITM total
    
    Signed-off-by: Jiebin Sun <jiebin.sun@intel.com>
    Cc: Adrian Hunter <adrian.hunter@intel.com>
    Cc: Alexander Shishkin <alexander.shishkin@linux.intel.com>
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: James Clark <james.clark@linaro.org>
    Cc: Jiri Olsa <jolsa@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Falcon <thomas.falcon@intel.com>
    Reviewed-by: Tianyou Li <tianyou.li@intel.com>
    Reviewed-by: Wangyang Guo <wangyang.guo@intel.com>

--- tools/perf/ui/browsers/c2c-function.c
+++ tools/perf/ui/browsers/c2c-function.c
@@ -37,6 +37,35 @@
 #include "../../c2c.h"
 #include "hists.h"
 
+struct perf_c2c_ext {
+	struct c2c_hists	function_hists;
+	/* Cached across all level-1 entries; 0 means "not yet computed". */
+	u64			total_cycles;
+};
+
+static struct perf_c2c_ext c2c_ext __maybe_unused;
+
+struct c2c_function_browser {
+	struct hist_browser	hb;
+};
+
+static __maybe_unused inline u64 c2c_hitm_count(const struct c2c_stats *stats)
+{
+	return stats->tot_hitm;
+}
+
+static __maybe_unused inline bool symbol_name_equal(struct symbol *a, struct symbol *b)
+{
+	return a && b && arch__compare_symbol_names(a->name, b->name) == 0;
+}
+
+static __maybe_unused inline u64 hist_entry__iaddr(struct hist_entry *he)
+{
+	if (he->mem_info)
+		return mem_info__iaddr(he->mem_info)->addr;
+	return he->ip;
+}
+
 int perf_c2c__browse_function_view(struct hists *hists __maybe_unused)
 {
 	ui__warning("C2C function view is not implemented yet.\n");
