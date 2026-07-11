3d0980213b0e0fc061f3280516c84945212ca874
Miguel Ojeda <ojeda@kernel.org>
2026-07-11 20:02:31 +0200

    rust: firmware: avoid UB in example by passing parameter
    The `Firmware` example crafts an `ARef<Device>` using a null pointer,
    which breaks the safety requirements of `Device::get_device()`.
    
    Instead, pass an `ARef` via a parameter, which is simpler, avoids UB
    and removes an `unsafe` block.
    
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- rust/kernel/firmware.rs
+++ rust/kernel/firmware.rs
@@ -51,12 +51,9 @@ impl FwFunc {
 /// # Examples
 ///
 /// ```no_run
-/// # use kernel::{device::Device, firmware::Firmware};
+/// # use kernel::{device::Device, firmware::Firmware, sync::aref::ARef};
 ///
-/// # fn no_run() -> Result<(), Error> {
-/// # // SAFETY: *NOT* safe, just for the example to get an `ARef<Device>` instance
-/// # let dev = unsafe { Device::get_device(core::ptr::null_mut()) };
-///
+/// # fn no_run(dev: ARef<Device>) -> Result<(), Error> {
 /// let fw = Firmware::request(c"path/to/firmware.bin", &dev)?;
 /// let blob = fw.data();
 ///
