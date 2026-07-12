da93f7d3acc3b1de6751cf4ec3ecc4d56385ad27
Miles Krause <mileskrause5200@gmail.com>
2026-07-11 22:47:12 -0400

    selftests/futex: Give circular-list nodes static storage
    The registered robust-list head has static storage but points at three
    automatic lock nodes. GCC warns about storing the address of a local
    variable in the static head when building the selftest with -Wall.
    
    Give the nodes static storage as well. This keeps every object in the
    circular list alive through child exit without changing the list topology.
    
    Signed-off-by: Miles Krause <mileskrause5200@gmail.com>

--- tools/testing/selftests/futex/functional/robust_list.c
+++ tools/testing/selftests/futex/functional/robust_list.c
@@ -523,7 +523,7 @@ TEST(test_robust_list_multiple_elements)
 static int child_circular_list(void *arg)
 {
 	static struct robust_list_head head;
-	struct lock_struct a, b, c;
+	static struct lock_struct a, b, c;
 	int ret;
 
 	ret = set_list(&head);
