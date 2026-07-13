12124e87f31133531e208aa28f04bed330e418ed
Guopeng Zhang <zhangguopeng@kylinos.cn>
2026-07-13 16:50:53 +0800

    mm: memcontrol: update state_local when flushing NMI stats
    flush_nmi_stats() updates state[] for kmem and slab counters but leaves
    the corresponding state_local[] counters unchanged.  Local kmem and slab
    statistics therefore miss updates collected through the NMI-safe atomic
    path.
    
    Update state_local[] together with state[].
    
    Link: https://lore.kernel.org/20260713085053.2916813-1-guopeng.zhang@linux.dev
    Fixes: 940b01fc8dc1 ("memcg: nmi safe memcg stats for specific archs")
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Acked-by: Tao Cui <cuitao@kylinos.cn>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Cc: Alexandre Ghiti <alex@ghiti.fr>
    Cc: Michal Hocko <mhocko@kernel.org>
    Cc: Muchun Song <muchun.song@linux.dev>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Shakeel Butt <shakeel.butt@linux.dev>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Cc: <stable@vger.kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4442,6 +4442,7 @@ static void flush_nmi_stats(struct mem_cgroup *memcg, struct mem_cgroup *parent,
 		int index = memcg_stats_index(MEMCG_KMEM);
 
 		memcg->vmstats->state[index] += kmem;
+		memcg->vmstats->state_local[index] += kmem;
 		if (parent)
 			parent->vmstats->state_pending[index] += kmem;
 	}
@@ -4459,9 +4460,11 @@ static void flush_nmi_stats(struct mem_cgroup *memcg, struct mem_cgroup *parent,
 			int index = memcg_stats_index(NR_SLAB_RECLAIMABLE_B);
 
 			lstats->state[index] += slab;
+			lstats->state_local[index] += slab;
 			if (plstats)
 				plstats->state_pending[index] += slab;
 			memcg->vmstats->state[index] += slab;
+			memcg->vmstats->state_local[index] += slab;
 			if (parent)
 				parent->vmstats->state_pending[index] += slab;
 		}
@@ -4470,9 +4473,11 @@ static void flush_nmi_stats(struct mem_cgroup *memcg, struct mem_cgroup *parent,
 			int index = memcg_stats_index(NR_SLAB_UNRECLAIMABLE_B);
 
 			lstats->state[index] += slab;
+			lstats->state_local[index] += slab;
 			if (plstats)
 				plstats->state_pending[index] += slab;
 			memcg->vmstats->state[index] += slab;
+			memcg->vmstats->state_local[index] += slab;
 			if (parent)
 				parent->vmstats->state_pending[index] += slab;
 		}
