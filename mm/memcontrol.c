5630512fa1078ba71adaeb24ca7624acd6800faf
Guopeng Zhang <zhangguopeng@kylinos.cn>
2026-07-13 17:00:10 +0800

    mm: memcontrol: drop unused cpu argument from flush_nmi_stats
    flush_nmi_stats() does not use its cpu argument.  Remove it from the
    function and its !CONFIG_MEMCG_NMI_SAFETY_REQUIRES_ATOMIC stub.  The
    caller still uses cpu for the subsequent per-CPU rstat flush.
    
    No functional change.
    
    Link: https://lore.kernel.org/20260713090010.2991906-1-guopeng.zhang@linux.dev
    Signed-off-by: Guopeng Zhang <zhangguopeng@kylinos.cn>
    Acked-by: Michal Hocko <mhocko@suse.com>
    Acked-by: Johannes Weiner <hannes@cmpxchg.org>
    Reviewed-by: Muchun Song <muchun.song@linux.dev>
    Cc: Roman Gushchin <roman.gushchin@linux.dev>
    Cc: Shakeel Butt <shakeel.butt@linux.dev>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4432,8 +4432,7 @@ static void mem_cgroup_stat_aggregate(struct aggregate_control *ac)
 }
 
 #ifdef CONFIG_MEMCG_NMI_SAFETY_REQUIRES_ATOMIC
-static void flush_nmi_stats(struct mem_cgroup *memcg, struct mem_cgroup *parent,
-			    int cpu)
+static void flush_nmi_stats(struct mem_cgroup *memcg, struct mem_cgroup *parent)
 {
 	int nid;
 
@@ -4484,8 +4483,7 @@ static void flush_nmi_stats(struct mem_cgroup *memcg, struct mem_cgroup *parent,
 	}
 }
 #else
-static void flush_nmi_stats(struct mem_cgroup *memcg, struct mem_cgroup *parent,
-			    int cpu)
+static void flush_nmi_stats(struct mem_cgroup *memcg, struct mem_cgroup *parent)
 {}
 #endif
 
@@ -4497,7 +4495,7 @@ static void mem_cgroup_css_rstat_flush(struct cgroup_subsys_state *css, int cpu)
 	struct aggregate_control ac;
 	int nid;
 
-	flush_nmi_stats(memcg, parent, cpu);
+	flush_nmi_stats(memcg, parent);
 
 	statc = per_cpu_ptr(memcg->vmstats_percpu, cpu);
 
