03dee1a4c456a61321c3f606534addffd24f4c3c
Michael Chapman <mike@very.puzzling.org>
2026-07-16 15:01:17 +1000

    media: dvb-usb: dib0700: fix null-ptr-deref in dib0700_disconnect
    dib0700_disconnect() may be called before driver-specific state has been
    attached to the USB device.
    
    Signed-off-by: Michael Chapman <mike@very.puzzling.org>

--- drivers/media/usb/dvb-usb/dib0700_core.c
+++ drivers/media/usb/dvb-usb/dib0700_core.c
@@ -911,8 +911,14 @@ static int dib0700_probe(struct usb_interface *intf,
 static void dib0700_disconnect(struct usb_interface *intf)
 {
 	struct dvb_usb_device *d = usb_get_intfdata(intf);
-	struct dib0700_state *st = d->priv;
+	struct dib0700_state *st;
 	struct i2c_client *client;
+
+	if (!d)
+		goto exit;
+	st = d->priv;
+	if (!st)
+		goto exit;
 
 	/* remove I2C client for tuner */
 	client = st->i2c_client_tuner;
@@ -928,6 +934,7 @@ static void dib0700_disconnect(struct usb_interface *intf)
 		i2c_unregister_device(client);
 	}
 
+exit:
 	dvb_usb_device_exit(intf);
 }
 
