df62a9252fd9b5f6f7b66368d0e7a519fd26237f
Rob Clark <robin.clark@oss.qualcomm.com>
2026-07-16 13:15:31 -0700

    drm/msm: Validate lazy VM in GEM_NEW
    Otherwise creating a _NO_SHARE BO before any BOs are mapped could cause
    a NPE.
    
    Reported-by: Sashiko <sashiko-bot@kernel.org>
    Fixes: b58e12a66e47 ("drm/msm: Add _NO_SHARE flag")
    Signed-off-by: Rob Clark <robin.clark@oss.qualcomm.com>

--- drivers/gpu/drm/msm/msm_gem.c
+++ drivers/gpu/drm/msm/msm_gem.c
@@ -1146,8 +1146,16 @@ int msm_gem_new_handle(struct drm_device *dev, struct drm_file *file,
 		msm_gem_object_set_name(obj, "%s", name);
 
 	if (flags & MSM_BO_NO_SHARE) {
+		struct msm_drm_private *priv = dev->dev_private;
 		struct msm_context *ctx = file->driver_priv;
-		struct drm_gem_object *r_obj = drm_gpuvm_resv_obj(ctx->vm);
+		struct drm_gpuvm *vm = msm_context_vm(dev, ctx);
+
+		if (!priv->gpu || !vm) {
+			ret = UERR(EINVAL, dev, "not supported with shared VM");
+			goto out_put;
+		}
+
+		struct drm_gem_object *r_obj = drm_gpuvm_resv_obj(vm);
 
 		drm_gem_object_get(r_obj);
 
@@ -1157,6 +1165,7 @@ int msm_gem_new_handle(struct drm_device *dev, struct drm_file *file,
 	ret = drm_gem_handle_create(file, obj, handle);
 
 	/* drop reference from allocate - handle holds it now */
+out_put:
 	drm_gem_object_put(obj);
 
 	return ret;
