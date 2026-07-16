02019179198b5d40ebf0e01fb2a649f94576dc9e
Jan Kara <jack@suse.cz>
2026-07-16 16:55:42 +0200

    fat: Replace fat_sync_inode() with sync_inode_metadata()
    Use generic sync_inode_metadata() instead of fat_sync_inode() for
    persisting inode metadata changes for DIRSYNC inodes. This slightly
    simplifies code and also addresses a theoretical race where
    fat_sync_inode() could return before all metadata buffers associated
    with the inode were properly written out when racing with fsync(2).
    
    Signed-off-by: Jan Kara <jack@suse.cz>
    Link: https://patch.msgid.link/20260716145527.3580300-41-jack@suse.cz
    Signed-off-by: Christian Brauner (Amutable) <brauner@kernel.org>

--- fs/fat/dir.c
+++ fs/fat/dir.c
@@ -1102,10 +1102,9 @@ int fat_remove_entries(struct inode *dir, struct fat_slot_info *sinfo)
 
 	fat_truncate_time(dir, NULL, FAT_UPDATE_ATIME | FAT_UPDATE_CMTIME);
 	err = 0;
+	mark_inode_dirty(dir);
 	if (IS_DIRSYNC(dir))
-		err = fat_sync_inode(dir);
-	else
-		mark_inode_dirty(dir);
+		err = sync_inode_metadata(dir, 1);
 
 	return err;
 }
