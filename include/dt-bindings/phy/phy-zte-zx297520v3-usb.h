0cbf73a97d852cba386c83e634a393e2ca1ae02e
Stefan Dösinger <stefandoesinger@gmail.com>
2026-07-17 00:35:37 +0300

    dt-bindings: phy: Add zx297520v3 USB phy documentation
    This binding will be used as a subnode of topcrm, which will be added in
    the next patch.
    
    Why is this a child node and not #phy-cells added to topcrm itself,
    like clocks and resets as writing-bindings.rst suggests? Because
    of_phy_provider_register checks if the of_node the PHY provider is added
    to belongs to the device or is a child of the device, so I can't put the
    PHY driver into an MFD (or AUX) child without its own node.
    
    Signed-off-by: Stefan Dösinger <stefandoesinger@gmail.com>

---
+++ include/dt-bindings/phy/phy-zte-zx297520v3-usb.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR BSD-2-Clause) */
+/*
+ * Copyright (C) Stefan Dösinger.
+ */
+
+#ifndef __DT_BINDINGS_PHY_USB_ZX297520V3_H
+#define __DT_BINDINGS_PHY_USB_ZX297520V3_H
+
+#define ZX297520V3_USB_PHY	0
+#define ZX297520V3_HSIC_PHY	1
+
+#endif /* __DT_BINDINGS_PHY_USB_ZX297520V3_H */
