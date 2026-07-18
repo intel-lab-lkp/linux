cf8b418989fbaacfb9c6e1f92fb4788c443948e2
Tarang Raval <tarang.raval@siliconsignals.io>
2026-07-19 01:39:04 +0530

    media: i2c: os05b10: Rename vmax variable in VBLANK control
    The VBLANK control programs the sensor VTS register. Rename the local
    variable from vmax to vts to match the register meaning and improve
    readability.
    
    Signed-off-by: Tarang Raval <tarang.raval@siliconsignals.io>

--- drivers/media/i2c/os05b10.c
+++ drivers/media/i2c/os05b10.c
@@ -750,7 +750,7 @@ static int os05b10_set_ctrl(struct v4l2_ctrl *ctrl)
 	const struct os05b10_mode *mode;
 	struct v4l2_mbus_framefmt *fmt;
 	unsigned int num_modes;
-	int vmax, ret;
+	int vts, ret;
 
 	state = v4l2_subdev_get_locked_active_state(&os05b10->sd);
 	fmt = v4l2_subdev_state_get_format(state, 0);
@@ -779,8 +779,8 @@ static int os05b10_set_ctrl(struct v4l2_ctrl *ctrl)
 
 	switch (ctrl->id) {
 	case V4L2_CID_VBLANK:
-		vmax = fmt->height + ctrl->val;
-		ret = cci_write(os05b10->cci, OS05B10_REG_VTS, vmax, NULL);
+		vts = fmt->height + ctrl->val;
+		ret = cci_write(os05b10->cci, OS05B10_REG_VTS, vts, NULL);
 		break;
 	case V4L2_CID_ANALOGUE_GAIN:
 		ret = cci_write(os05b10->cci, OS05B10_REG_ANALOG_GAIN,
