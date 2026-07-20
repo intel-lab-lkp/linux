f116facd0ce793295ab01549e9cff5c9a47cb681
Thorsten Blum <thorsten.blum@linux.dev>
2026-07-20 12:33:00 +0200

    x86/serial: Use generic BASE_BAUD in asm/serial.h
    Include asm-generic/serial.h and use the generic BASE_BAUD definition
    instead of redefining it. Consolidate the comments accordingly.
    
    Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>

--- include/asm-generic/serial.h
+++ include/asm-generic/serial.h
@@ -3,12 +3,11 @@
 #define __ASM_GENERIC_SERIAL_H
 
 /*
- * This should not be an architecture specific #define, oh well.
+ * This should not be an architecture-specific definition.
  *
- * Traditionally, it just describes i8250 and related serial ports
- * that have this clock rate.
+ * Traditionally, it describes i8250 and related serial ports using
+ * a 1.8432 MHz UART clock.
  */
-
 #define BASE_BAUD (1843200 / 16)
 
 #endif /* __ASM_GENERIC_SERIAL_H */
