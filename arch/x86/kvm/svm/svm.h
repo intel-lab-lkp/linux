8552fa806d44a23509187759f910ded561b3d2e8
Shivansh Dhiman <shivansh.dhiman@amd.com>
2026-07-21 05:05:56 +0000

    KVM: SVM: Refactor svm_update_lbrv()
    Rewrite the enable_lbrv computation in svm_update_lbrv() as a series of
    'if' statements. Rename nested_vmcb12_has_lbrv() to nested_lbrv_enabled(),
    expose it, and use it instead of open-coding the nested LBRV check.
    
    No functional change intended.
    
    Suggested-by: Yosry Ahmed <yosry@kernel.org>
    Signed-off-by: Shivansh Dhiman <shivansh.dhiman@amd.com>
    Reviewed-by: Yosry Ahmed <yosry@kernel.org>

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -899,6 +899,7 @@ void nested_copy_vmcb_control_to_cache(struct vcpu_svm *svm,
 void nested_copy_vmcb_save_to_cache(struct vcpu_svm *svm,
 				    struct vmcb_save_area *save);
 void nested_sync_control_from_vmcb02(struct vcpu_svm *svm);
+bool nested_lbrv_enabled(struct kvm_vcpu *vcpu);
 void svm_switch_vmcb(struct vcpu_svm *svm, struct kvm_vmcb_info *target_vmcb);
 
 
