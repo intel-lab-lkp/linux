8e4f2aa3534cbe444fc2fa554fc3dec5262947c5
Yonatan Nachum <ynachum@amazon.com>
2026-07-21 07:00:26 +0000

    RDMA/efa: Expose 64-bit send WR ID support to userspace
    Currently EFA WRs support 16-bit request ID, this requires EFA to manage
    a translation table to translate the IB WR ID from 64-bits to 16-bits
    and translating it back on CQ completion.
    Expose a new device capability to handle 64-bit request ID for SQ WRs
    allowing userspace to directly post the 64-bit ID to the device.
    
    Reviewed-by: Michael Margolin <mrgolin@amazon.com>
    Reviewed-by: Tom Sela <tomsela@amazon.com>
    Signed-off-by: Yonatan Nachum <ynachum@amazon.com>

--- drivers/infiniband/hw/efa/efa_verbs.c
+++ drivers/infiniband/hw/efa/efa_verbs.c
@@ -265,6 +265,9 @@ int efa_query_device(struct ib_device *ibdev,
 		if (EFA_DEV_CAP(dev, UNSOLICITED_WRITE_RECV))
 			resp.device_caps |= EFA_QUERY_DEVICE_CAPS_UNSOLICITED_WRITE_RECV;
 
+		if (EFA_DEV_CAP(dev, SQ_64_BIT_REQ_ID))
+			resp.device_caps |= EFA_QUERY_DEVICE_CAPS_SQ_64_BIT_REQ_ID;
+
 		if (dev->neqs)
 			resp.device_caps |= EFA_QUERY_DEVICE_CAPS_CQ_NOTIFICATIONS;
 
