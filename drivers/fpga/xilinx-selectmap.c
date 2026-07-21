3fb102e7a1e7a9ab28c632664eb4c06dc9c8727e
Heiko Schocher <hs@nabladev.com>
2026-07-21 13:42:52 +0200

    driver: fpga: xilinx-selectmap: add csi and rdwr support
    Add csi_b and rdwr pin support for the xilinx,selectmap driver.
    
    In current driver the pins are configured from DTS and set to
    GPIOD_OUT_HIGH. This works in case you have one FPGA.
    
    Extend this to really implement csi_b and rdwr pin function in
    driver, so it works with more than one FPGA.
    
    Tested on AM625 based board with 2 FPGAs connected to GPMC.
    
    Signed-off-by: Heiko Schocher <hs@nabladev.com>

--- drivers/fpga/xilinx-selectmap.c
+++ drivers/fpga/xilinx-selectmap.c
@@ -19,6 +19,8 @@
 struct xilinx_selectmap_conf {
 	struct xilinx_fpga_core core;
 	void __iomem *base;
+	struct gpio_desc *csi_b;
+	struct gpio_desc *rdwr_b;
 };
 
 #define to_xilinx_selectmap_conf(obj) \
@@ -30,8 +32,14 @@ static int xilinx_selectmap_write(struct xilinx_fpga_core *core,
 	struct xilinx_selectmap_conf *conf = to_xilinx_selectmap_conf(core);
 	size_t i;
 
+	gpiod_set_value(conf->csi_b, GPIOD_OUT_HIGH);
+	gpiod_set_value(conf->rdwr_b, GPIOD_OUT_HIGH);
+
 	for (i = 0; i < count; ++i)
 		writeb(buf[i], conf->base);
+
+	gpiod_set_value(conf->rdwr_b, GPIOD_OUT_LOW);
+	gpiod_set_value(conf->csi_b, GPIOD_OUT_LOW);
 
 	return 0;
 }
@@ -39,7 +47,6 @@ static int xilinx_selectmap_write(struct xilinx_fpga_core *core,
 static int xilinx_selectmap_probe(struct platform_device *pdev)
 {
 	struct xilinx_selectmap_conf *conf;
-	struct gpio_desc *gpio;
 	void __iomem *base;
 
 	conf = devm_kzalloc(&pdev->dev, sizeof(*conf), GFP_KERNEL);
@@ -56,15 +63,17 @@ static int xilinx_selectmap_probe(struct platform_device *pdev)
 	conf->base = base;
 
 	/* CSI_B is active low */
-	gpio = devm_gpiod_get_optional(&pdev->dev, "csi", GPIOD_OUT_HIGH);
-	if (IS_ERR(gpio))
-		return dev_err_probe(&pdev->dev, PTR_ERR(gpio),
+	conf->csi_b = devm_gpiod_get_optional(&pdev->dev, "csi",
+					      GPIOD_OUT_LOW);
+	if (IS_ERR(conf->csi_b))
+		return dev_err_probe(&pdev->dev, PTR_ERR(conf->csi_b),
 				     "Failed to get CSI_B gpio\n");
 
 	/* RDWR_B is active low */
-	gpio = devm_gpiod_get_optional(&pdev->dev, "rdwr", GPIOD_OUT_HIGH);
-	if (IS_ERR(gpio))
-		return dev_err_probe(&pdev->dev, PTR_ERR(gpio),
+	conf->rdwr_b = devm_gpiod_get_optional(&pdev->dev, "rdwr",
+					       GPIOD_OUT_LOW);
+	if (IS_ERR(conf->rdwr_b))
+		return dev_err_probe(&pdev->dev, PTR_ERR(conf->rdwr_b),
 				     "Failed to get RDWR_B gpio\n");
 
 	return xilinx_core_probe(&conf->core);
