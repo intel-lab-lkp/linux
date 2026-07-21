7cbf12fc6bf65a997418bb8f23685b8dd023a64b
Naveen N Rao (AMD) <naveen@kernel.org>
2026-07-21 13:33:36 +0530

    x86/apic: Introduce a variable to track the number of EILVT registers
    Future AMD processors will be increasing the number of EILVT registers.
    Rather than hardcoding the maximum EILVT register count and using that
    everywhere, introduce a variable to track the EILVT register count.
    
    The number of EILVT registers (the actual count) is exposed through the
    extended APIC Feature Register (APIC_EFEAT) bits 23:16. Use this to
    initialize the count and fall back to the current default
    (APIC_EILVT_NR_AMD_10H) if the count is not available.
    
    Signed-off-by: Naveen N Rao (AMD) <naveen@kernel.org>
    Tested-by: Manali Shukla <manali.shukla@amd.com>

--- arch/x86/kernel/apic/apic.c
+++ arch/x86/kernel/apic/apic.c
@@ -342,6 +342,7 @@ static void __setup_APIC_LVTT(unsigned int clocks, int oneshot, int irqen)
  */
 
 static atomic_t eilvt_offsets[APIC_EILVT_NR_MAX];
+unsigned int apic_eilvt_count __ro_after_init;
 
 static inline int eilvt_entry_is_changeable(unsigned int old, unsigned int new)
 {
@@ -409,6 +410,15 @@ int setup_APIC_eilvt(u8 offset, u8 vector, u8 msg_type, u8 mask)
 	return 0;
 }
 EXPORT_SYMBOL_GPL(setup_APIC_eilvt);
+
+static __init void init_eilvt(void)
+{
+	if (cpu_feature_enabled(X86_FEATURE_EXTAPIC))
+		apic_eilvt_count = APIC_EFEAT_XLC(apic_read(APIC_EFEAT));
+
+	if (!apic_eilvt_count && boot_cpu_data.x86_vendor == X86_VENDOR_AMD)
+		apic_eilvt_count = APIC_EILVT_NR_AMD_10H;
+}
 
 /*
  * Program the next event, relative to now
@@ -2345,6 +2355,7 @@ static void __init apic_bsp_setup(bool upmode)
 	if (upmode)
 		apic_bsp_up_setup();
 	setup_local_APIC();
+	init_eilvt();
 
 	enable_IO_APIC();
 	end_local_APIC_setup();
