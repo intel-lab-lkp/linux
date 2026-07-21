ac1ff2cd33063aa6099cac40762302901df4163b
Bjorn Helgaas <helgaas@kernel.org>
2026-07-21 10:49:02 -0500

    regmap: Clarify regmap_update_bits_base() 'async' kernel-doc
    The kernel-doc for the regmap_update_bits_base() 'async' parameter didn't
    actually say what it does.
    
    Reword it to clarify the read-modify-write mechanics: when 'async' is true,
    the write is queued asynchronously, but the read will still be synchronous
    unless the device uses a register cache.
    
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>

--- drivers/base/regmap/regmap.c
+++ drivers/base/regmap/regmap.c
@@ -3296,11 +3296,9 @@ static int _regmap_update_bits(struct regmap *map, unsigned int reg,
  * Perform a read/modify/write cycle on a register map with change, async, force
  * options.
  *
- * If async is true:
- *
- * With most buses the read must be done synchronously so this is most useful
- * for devices with a cache which do not need to interact with the hardware to
- * determine the current register value.
+ * If async is true, queue an asynchronous write. However, reads are
+ * synchronous on most buses, so they will still block unless the device
+ * uses a register cache.
  *
  * Returns zero for success, a negative number on error.
  */
