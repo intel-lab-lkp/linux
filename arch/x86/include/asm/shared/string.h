0abd9e679060401114fc25a112f55d5489a8a8a6
Mauricio Faria de Oliveira <mfo@igalia.com>
2026-07-21 12:56:42 -0300

    x86/asm, x86/boot: expose inline memcmp
    Move the inline memcmp function currently only available in 'boot/string.c'
    into the shared string function header <asm/shared/string.h> to be reused.
    
    This is not done through <asm/string.h> to avoid pulling unnecessary code
    in 'boot/string.c' that causes build errors in 'boot/compressed/string.c'
    and 'purgatory/purgatory.ro'.
    
    Signed-off-by: Mauricio Faria de Oliveira <mfo@igalia.com>

---
+++ arch/x86/include/asm/shared/string.h
@@ -0,0 +1,20 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _ASM_X86_SHARED_STRING_H
+#define _ASM_X86_SHARED_STRING_H
+
+/*
+ * This inline memcmp() returns 0 (equal) or 1 (not equal).
+ * The regular memcmp() returns <0 (less than), 0 (equal), or >0 (greater than)
+ * to indicate ordering as well.
+ */
+static __always_inline int __inline_memcmp(const void *s1, const void *s2, size_t len)
+{
+	bool diff;
+
+	asm("repe cmpsb"
+	    : "=@ccnz" (diff), "+D" (s1), "+S" (s2), "+c" (len));
+
+	return diff;
+}
+
+#endif /* _ASM_X86_SHARED_STRING_H */
