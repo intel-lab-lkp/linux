e7398bce8b943d0509bf743aaafc4c02ee6bc439
Danilo Krummrich <dakr@kernel.org>
2026-07-21 17:06:45 +0200

    rust: fs: add iminor() helper
    Add a Rust helper for iminor(), which extracts the minor number from a
    struct inode. This is a static inline function in the C headers, so
    bindgen does not generate a binding for it.
    
    Signed-off-by: Danilo Krummrich <dakr@kernel.org>

--- rust/helpers/fs.c
+++ rust/helpers/fs.c
@@ -10,3 +10,8 @@ __rust_helper struct file *rust_helper_get_file(struct file *f)
 {
 	return get_file(f);
 }
+
+__rust_helper unsigned int rust_helper_iminor(const struct inode *inode)
+{
+	return iminor(inode);
+}
