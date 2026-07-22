ca14dcbf24b310d30df095a6fa7f06381c1e961a
Eli Billauer <eli.billauer@gmail.com>
2026-07-22 12:23:01 +0200

    char: xillybus: Improve control of execution flow with mutexes
    This commit addresses two issues by using mutexes:
    
    (1) Add a mutex to protect the fifo_buf_order global variable. The
        purpose of this variable is avoid repeated failed calls to
        __get_free_pages() for allocating FIFO memory, when the chunk
        size was too big. However, if two drivers are initialized at the
        same time, fifo_init() may run in parallel, and fifo_buf_order
        may be reduced too much. This is a far-fetched scenario, now
        completely prevented by fifo_buf_order_mutex.
    
    (2) setup_channels() acquires process_in_mutex to prevent
        process_bulk_in() from accessing the xillyusb_dev struct. With
        correctly working hardware, process_bulk_in() is never called while
        setup_channels() runs, because the device has no reason to send data
        in that phase. The mutex ensures that process_bulk_in() does not
        touch the members that setup_channels() alters.
    
        There is no similar protection for data flow in the other direction,
        because during the setup process, the only outbound data is the
        BULK endpoint used for commands, and it remains untouched after its
        initial setup.
    
    Signed-off-by: Eli Billauer <eli.billauer@gmail.com>

--- drivers/char/xillybus/xillyusb.c
+++ drivers/char/xillybus/xillyusb.c
@@ -50,6 +50,7 @@ MODULE_LICENSE("GPL v2");
 static const char xillyname[] = "xillyusb";
 
 static unsigned int fifo_buf_order;
+static DEFINE_MUTEX(fifo_buf_order_mutex);
 static struct workqueue_struct *wakeup_wq;
 
 #define USB_VENDOR_ID_XILINX		0x03fd
@@ -374,6 +375,8 @@ static int fifo_init(struct xillyfifo *fifo,
 	int i;
 
 	unsigned int log2_fifo_buf_size;
+
+	guard(mutex)(&fifo_buf_order_mutex);
 
 retry:
 	log2_fifo_buf_size = fifo_buf_order + PAGE_SHIFT;
@@ -1942,6 +1945,9 @@ static int setup_channels(struct xillyusb_dev *xdev,
 	struct usb_device *udev = xdev->udev;
 	struct xillyusb_channel *chan, *new_channels;
 	int i;
+
+	/* Don't let process_bulk_in() run while we change the channels */
+	guard(mutex)(&xdev->process_in_mutex);
 
 	chan = kzalloc_objs(*chan, num_channels);
 	if (!chan)
