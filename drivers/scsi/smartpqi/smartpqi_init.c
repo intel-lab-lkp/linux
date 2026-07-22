9c538567554f860501aba9bd47027e380eea75f7
David Strahan <David.Strahan@microchip.com>
2026-07-22 15:23:24 -0500

    scsi: smartpqi: add new pci device-ids
    All PCI ID entries in Hex.
    
    Add PCI IDs for Hurray Data controllers:
                                                VID  / DID  / SVID / SDID
                                                ----   ----   ----   ----
                                                9005 / 028f / 207d / 4246
                                                9005 / 028f / 207d / 4256
                                                9005 / 028f / 207d / 4356
                                                9005 / 028f / 207d / 4940
                                                9005 / 028f / 207d / 4a46
    
    Add PCI IDs for ZTE controllers:
                                                VID  / DID  / SVID / SDID
                                                ----   ----   ----   ----
                                                9005 / 028f / 1cf2 / 5451
                                                9005 / 028f / 1cf2 / 5452
                                                9005 / 028f / 1cf2 / 5453
    
    Add PCI ID for Ramaxel controller:
                                                VID  / DID  / SVID / SDID
                                                ----   ----   ----   ----
                                                9005 / 028f / 1f3f / 0670
    
    Signed-off-by: David Strahan <david.strahan@microchip.com>
    Reviewed-by: Don Brace <don.brace@microchip.com>

--- drivers/scsi/smartpqi/smartpqi_init.c
+++ drivers/scsi/smartpqi/smartpqi_init.c
@@ -10154,7 +10154,27 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x207d, 0x4246)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x207d, 0x4256)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x207d, 0x4356)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       0x207d, 0x4840)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x207d, 0x4940)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x207d, 0x4a46)
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
@@ -10342,6 +10362,18 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1cf2, 0x5451)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1cf2, 0x5452)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1cf2, 0x5453)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       0x1cf2, 0x54da)
 	},
 	{
@@ -10379,6 +10411,10 @@ static const struct pci_device_id pqi_pci_id_table[] = {
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
 			       0x1f3f, 0x0610)
+	},
+	{
+		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
+			       0x1f3f, 0x0670)
 	},
 	{
 		PCI_DEVICE_SUB(PCI_VENDOR_ID_ADAPTEC2, 0x028f,
