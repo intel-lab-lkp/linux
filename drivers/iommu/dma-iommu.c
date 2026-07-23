35c3ffb4dbeb586d8cb7e4ca55cfab739d4a191b
Krzysztof Karas <krzysztof.karas@intel.com>
2026-07-23 10:25:39 +0000

    iommu/dma: Catch scatterlist length overflows
    It is possible, when a very large mapping uses only one
    scatterlist, that padding overflows scatterlist's length field.
    This results in:
     1) silently wrapping the value
     2) smaller than desired mappings produced by iommu_map_sg
     3) leaving mapped bytes in memory (no iommu_unmap)
    
    Address this issue by adding overflow detection for scatterlist
    length field.
    
    Fixes: 809eac54cdd6 ("iommu/dma: Implement scatterlist segment merging")
    Signed-off-by: Krzysztof Karas <krzysztof.karas@intel.com>
    Reviewed-by: Robin Murphy <robin.murphy@arm.com>

--- drivers/iommu/dma-iommu.c
+++ drivers/iommu/dma-iommu.c
@@ -21,6 +21,7 @@
 #include <linux/iommu-dma.h>
 #include <linux/iova.h>
 #include <linux/irq.h>
+#include <linux/limits.h>
 #include <linux/list_sort.h>
 #include <linux/memremap.h>
 #include <linux/mm.h>
@@ -1477,6 +1478,12 @@ int iommu_dma_map_sg(struct device *dev, struct scatterlist *sg, int nents,
 		sg_dma_len(s) = s_length;
 		s->offset -= s_iova_off;
 		s_length = iova_align(iovad, s_length + s_iova_off);
+		if ((s_length & UINT_MAX) == 0) {
+			/* Prevent possible wrapping upon assignment to s->length. */
+			ret = -EOVERFLOW;
+			goto out_restore_sg;
+		}
+
 		s->length = s_length;
 
 		/*
@@ -1493,7 +1500,18 @@ int iommu_dma_map_sg(struct device *dev, struct scatterlist *sg, int nents,
 		 *   time through here (i.e. before it has a meaningful value).
 		 */
 		if (pad_len && pad_len < s_length - 1) {
-			prev->length += pad_len;
+			unsigned int new_prev_len;
+			/*
+			 * For large mappings spanning multiple GBs we
+			 * may not be able to fit all needed padding into
+			 * sg->length.
+			 */
+			if (check_add_overflow(prev->length, pad_len, &new_prev_len)) {
+				ret = -EOVERFLOW;
+				goto out_restore_sg;
+			}
+
+			prev->length = new_prev_len;
 			iova_len += pad_len;
 		}
 
