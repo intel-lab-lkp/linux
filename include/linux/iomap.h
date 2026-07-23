ac6916ed841d2264f6fcdfadcb8b455baa97804b
Christoph Hellwig <hch@lst.de>
2026-07-23 16:49:31 +0200

    block,iomap: fix protection information verification with initial bvec offset
    When reconstructing a bvec_iter from an ioend for protection information
    verification, iomap currently ignores the offset into the initial
    bio_vec.
    
    This can't happen for buffered I/O an direct I/O to user addresses, but
    is exercised by split on O_DIRECT file descriptors or when using the loop
    driver.
    
    Fortunately the only file system PI user (XFS) currently always bounce
    buffers, so this can't actually be triggered yet.  But we'll want to make
    the bounce buffering conditional soon, for which this needs to be fixed.
    
    Store the initial offset in struct iomap_ioend, and pass a
    pre-constructed bvec_iter to fs_bio_integrity_verify.  For the
    synchronous read case the fix is even simpler as this path can
    simply stash away the original bvec_iter.
    
    Fixes: 0bde8a12b554 ("block: add fs_bio_integrity helpers")
    Signed-off-by: Christoph Hellwig <hch@lst.de>

--- include/linux/iomap.h
+++ include/linux/iomap.h
@@ -424,6 +424,7 @@ sector_t iomap_bmap(struct address_space *mapping, sector_t bno,
 struct iomap_ioend {
 	struct list_head	io_list;	/* next ioend in chain */
 	u16			io_flags;	/* IOMAP_IOEND_* */
+	u32			io_bvec_offset;	/* offset into first bvec */
 	struct inode		*io_inode;	/* file being written to */
 	size_t			io_size;	/* size of the extent */
 	atomic_t		io_remaining;	/* completetion defer count */
@@ -439,6 +440,13 @@ struct iomap_ioend {
 static inline struct iomap_ioend *iomap_ioend_from_bio(struct bio *bio)
 {
 	return container_of(bio, struct iomap_ioend, io_bio);
+}
+
+#define BVEC_ITER_IOEND(_ioend)				\
+{							\
+	.bi_sector	= (_ioend)->io_sector,		\
+	.bi_size	= (_ioend)->io_size,		\
+	.bi_bvec_done	= (_ioend)->io_bvec_offset,	\
 }
 
 struct iomap_writeback_ops {
