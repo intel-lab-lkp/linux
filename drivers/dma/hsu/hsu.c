c5e9d3d1cf61ec2e9fd7b3e558d76cb775cba9ab
Allen Pais <allen.lkml@gmail.com>
2026-07-27 13:39:03 -0700

    dmaengine: hsu: kill vchan BH on teardown
    Use dma_chan_kill_bh() for virt-dma cleanup.
    
    Signed-off-by: Allen Pais <allen.lkml@gmail.com>

--- drivers/dma/hsu/hsu.c
+++ drivers/dma/hsu/hsu.c
@@ -500,7 +500,7 @@ int hsu_dma_remove(struct hsu_dma_chip *chip)
 	for (i = 0; i < hsu->nr_channels; i++) {
 		struct hsu_dma_chan *hsuc = &hsu->chan[i];
 
-		tasklet_kill(&hsuc->vchan.task);
+		dma_chan_kill_bh(&hsuc->vchan.chan);
 	}
 
 	return 0;
