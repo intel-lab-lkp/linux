be870c6d569c49208b04f84c787b47d38f7d8cbe
Allen Pais <allen.lkml@gmail.com>
2026-07-27 13:39:05 -0700

    dmaengine: pxa_dma: kill vchan BH on teardown
    Use dma_chan_kill_bh() for virt-dma cleanup.
    
    Signed-off-by: Allen Pais <allen.lkml@gmail.com>

--- drivers/dma/pxa_dma.c
+++ drivers/dma/pxa_dma.c
@@ -1215,7 +1215,7 @@ static void pxad_free_channels(struct dma_device *dmadev)
 	list_for_each_entry_safe(c, cn, &dmadev->channels,
 				 vc.chan.device_node) {
 		list_del(&c->vc.chan.device_node);
-		tasklet_kill(&c->vc.task);
+		dma_chan_kill_bh(&c->vc.chan);
 	}
 }
 
