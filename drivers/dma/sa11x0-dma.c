7337f5df9dbb333b51d8b0d8b82c5a78ad45c4c9
Allen Pais <allen.lkml@gmail.com>
2026-07-27 13:28:58 -0700

    dmaengine: sa11x0-dma: kill vchan BH on remove
    Use dma_chan_kill_bh() when tearing down virt-dma channels.
    
    Signed-off-by: Allen Pais <allen.lkml@gmail.com>

--- drivers/dma/sa11x0-dma.c
+++ drivers/dma/sa11x0-dma.c
@@ -891,7 +891,7 @@ static void sa11x0_dma_free_channels(struct dma_device *dmadev)
 
 	list_for_each_entry_safe(c, cn, &dmadev->channels, vc.chan.device_node) {
 		list_del(&c->vc.chan.device_node);
-		tasklet_kill(&c->vc.task);
+		dma_chan_kill_bh(&c->vc.chan);
 		kfree(c);
 	}
 }
