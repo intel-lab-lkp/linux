37af96c91189cddb149af502ebc5a5fb5130edd7
Julian Braha <julianbraha@gmail.com>
2026-07-29 21:38:45 +0100

    tee: remove TZMEM_MODE_GENERIC
    'select' does not work on config options in a 'choice', so currently it is
    possible to enable QCOMTEE without QCOM_TZMEM_MODE_SHMBRIDGE, even though
    this is needed at runtime.
    
    There are no users of the generic allocator option,
    QCOM_TZMEM_MODE_GENERIC, so let's remove it. Then, we can remove the
    containing choice..endchoice, which allows the 'select' to work as
    intended.
    
    Suggested-by: Arnd Bergmann <arnd@arndb.de>
    Signed-off-by: Julian Braha <julianbraha@gmail.com>

--- drivers/firmware/qcom/qcom_tzmem.c
+++ drivers/firmware/qcom/qcom_tzmem.c
@@ -49,25 +49,6 @@ static struct device *qcom_tzmem_dev;
 static RADIX_TREE(qcom_tzmem_chunks, GFP_ATOMIC);
 static DEFINE_SPINLOCK(qcom_tzmem_chunks_lock);
 
-#if IS_ENABLED(CONFIG_QCOM_TZMEM_MODE_GENERIC)
-
-static int qcom_tzmem_init(void)
-{
-	return 0;
-}
-
-static int qcom_tzmem_init_area(struct qcom_tzmem_area *area)
-{
-	return 0;
-}
-
-static void qcom_tzmem_cleanup_area(struct qcom_tzmem_area *area)
-{
-
-}
-
-#elif IS_ENABLED(CONFIG_QCOM_TZMEM_MODE_SHMBRIDGE)
-
 #include <linux/firmware/qcom/qcom_scm.h>
 #include <linux/of.h>
 
@@ -188,8 +169,6 @@ static void qcom_tzmem_cleanup_area(struct qcom_tzmem_area *area)
 	qcom_tzmem_shm_bridge_delete(*handle);
 	kfree(handle);
 }
-
-#endif /* CONFIG_QCOM_TZMEM_MODE_SHMBRIDGE */
 
 static int qcom_tzmem_pool_add_memory(struct qcom_tzmem_pool *pool,
 				      size_t size, gfp_t gfp)
