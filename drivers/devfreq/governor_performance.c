a479639d1f3ea95df525b1b07d6e24bc7f6e4446
Jie Zhan <zhanjie9@hisilicon.com>
2026-07-29 18:23:48 +0800

    devfreq: Refcount governor modules while in use
    A governor module can be inserted or removed dynamically when built as a
    kernel module.  'devfreq->governor' would become NULL if the governor
    module is removed when it's in use.
    
    Add a refcount mechanism for governor modules to prevent the governor
    module from being removed (except for force unload):
    1. Add an optional 'owner' member to struct devfreq_governor so the devfreq
       core can identify the module that holds the governor code.
    2. Get and put a refcount of the governor module when starting and stopping
       the governor.
    
    The new 'owner' field is optional:
    - Common governor modules (performance, powersave, simple_ondemand,
      userspace, passive) set 'owner' to THIS_MODULE.
    - Governors that are bundled into a device driver module must leave 'owner'
      NULL.  The device's lifetime already pins that module, and setting
      'owner' would create a self-reference that blocks the driver from being
      unloaded.
    
    As a result, a non-forced rmmod of an in-use stand-alone governor now
    fails with -EBUSY, e.g.:
    
      $ cat governor
      performance
      $ rmmod governor_performance
      rmmod: ERROR: Module governor_performance is in use
    
    Force unloads (rmmod -f, if configured) can't be blocked.
    
    Signed-off-by: Jie Zhan <zhanjie9@hisilicon.com>

--- drivers/devfreq/governor_performance.c
+++ drivers/devfreq/governor_performance.c
@@ -37,6 +37,7 @@ static int devfreq_performance_handler(struct devfreq *devfreq,
 
 static struct devfreq_governor devfreq_performance = {
 	.name = DEVFREQ_GOV_PERFORMANCE,
+	.owner = THIS_MODULE,
 	.get_target_freq = devfreq_performance_func,
 	.event_handler = devfreq_performance_handler,
 };
--- drivers/devfreq/governor_powersave.c
+++ drivers/devfreq/governor_powersave.c
@@ -37,6 +37,7 @@ static int devfreq_powersave_handler(struct devfreq *devfreq,
 
 static struct devfreq_governor devfreq_powersave = {
 	.name = DEVFREQ_GOV_POWERSAVE,
+	.owner = THIS_MODULE,
 	.get_target_freq = devfreq_powersave_func,
 	.event_handler = devfreq_powersave_handler,
 };
