47922acde8ddec1d69bda9e63fe726f3b89e6d1c
Anshuman Khandual <anshuman.khandual@arm.com>
2026-07-29 17:54:39 +0530

    arm64/mm: Convert READ_ONCE() as p4dp_get() while accessing P4D
    Convert all READ_ONCE() based P4D accesses as p4dp_get() instead which will
    support both D64 and D128 translation regime going forward. That is because
    READ_ONCE() would need 128 bit single copy atomic guarantees, while reading
    128 bit page table entries which is currently not supported on arm64. Build
    fails for READ_ONCE() while accessing beyond 64 bits.
    
    Load Pair/Store Pair (ldp/stp) are only single copy atomic if FEAT_LSE128
    is supported (which is required when FEAT_D128 is supported). Currently 128
    bit pgtables is a compile time decision - so we could have chosen to extend
    READ_ONCE()/WRITE_ONCE() to allow 128 bit for this configuration. But then
    it's a general purpose API and we were concerned that other users might
    eventually creep in that expect 128 and then fail to compile in the other
    configs.
    
    But worse, we are considering eventually making D128 a boot time option, at
    which point we'd have to make READ_ONCE() always allow 128 bit at compile
    time but then it might silently tear at runtime.
    
    So our preference is to standardize on these existing helpers, which we can
    override in arm64 to give the 128 bit single copy guarantee when required.
    
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will@kernel.org>
    Cc: Ryan Roberts <ryan.roberts@arm.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: linux-arm-kernel@lists.infradead.org
    Cc: linux-kernel@vger.kernel.org
    Cc: kasan-dev@googlegroups.com
    Signed-off-by: Anshuman Khandual <anshuman.khandual@arm.com>

--- arch/arm64/mm/mmu.c
+++ arch/arm64/mm/mmu.c
@@ -368,7 +368,7 @@ static int alloc_init_pud(p4d_t *p4dp, unsigned long addr, unsigned long end,
 {
 	int ret = 0;
 	unsigned long next;
-	p4d_t p4d = READ_ONCE(*p4dp);
+	p4d_t p4d = p4dp_get(p4dp);
 	pud_t *pudp;
 
 	if (p4d_none(p4d)) {
@@ -458,7 +458,7 @@ static int alloc_init_p4d(pgd_t *pgdp, unsigned long addr, unsigned long end,
 	}
 
 	do {
-		p4d_t old_p4d = READ_ONCE(*p4dp);
+		p4d_t old_p4d = p4dp_get(p4dp);
 
 		next = p4d_addr_end(addr, end);
 
@@ -468,7 +468,7 @@ static int alloc_init_p4d(pgd_t *pgdp, unsigned long addr, unsigned long end,
 			goto out;
 
 		VM_WARN_ON_ONCE(p4d_val(old_p4d) != 0 &&
-				p4d_val(old_p4d) != READ_ONCE(p4d_val(*p4dp)));
+				p4d_val(old_p4d) != (p4d_val(p4dp_get(p4dp))));
 
 		phys += next - addr;
 	} while (p4dp++, addr = next, addr != end);
@@ -1567,7 +1567,7 @@ static void unmap_hotplug_p4d_range(pgd_t *pgdp, unsigned long addr,
 	do {
 		next = p4d_addr_end(addr, end);
 		p4dp = p4d_offset(pgdp, addr);
-		p4d = READ_ONCE(*p4dp);
+		p4d = p4dp_get(p4dp);
 		if (p4d_none(p4d))
 			continue;
 
@@ -1733,7 +1733,7 @@ static void free_empty_p4d_table(pgd_t *pgdp, unsigned long addr,
 	do {
 		next = p4d_addr_end(addr, end);
 		p4dp = p4d_offset(pgdp, addr);
-		p4d = READ_ONCE(*p4dp);
+		p4d = p4dp_get(p4dp);
 		if (p4d_none(p4d))
 			continue;
 
@@ -1754,7 +1754,7 @@ static void free_empty_p4d_table(pgd_t *pgdp, unsigned long addr,
 	 */
 	p4dp = p4d_offset(pgdp, 0UL);
 	for (i = 0; i < PTRS_PER_P4D; i++) {
-		if (!p4d_none(READ_ONCE(p4dp[i])))
+		if (!p4d_none(p4dp_get(p4dp + i)))
 			return;
 	}
 
