912005c90adce3ddc448793e2434aaca312b9bab
Coiby Xu <coiby.xu@gmail.com>
2026-07-29 11:36:47 +0800

    crash_dump: Improve readability of config_keys_restore_store
    config_keys_restore_store currently doesn't validate the user input
    before restoring dm-crypt keys. Although it's not necessary for the case
    of vmcore dumping, it's better to do it for the sake of consistency and
    code readability. Also check the return code of
    restore_dm_crypt_keys_to_thread_keyring.
    
    Fixes: 62f17d9df692 ("crash_dump: retrieve dm crypt keys in kdump kernel")
    Signed-off-by: Coiby Xu <coiby.xu@gmail.com>

--- kernel/crash_dump_dm_crypt.c
+++ kernel/crash_dump_dm_crypt.c
@@ -370,11 +370,18 @@ static ssize_t config_keys_restore_show(struct config_item *item, char *page)
 static ssize_t config_keys_restore_store(struct config_item *item,
 					  const char *page, size_t count)
 {
-	if (!restore)
-		restore_dm_crypt_keys_to_thread_keyring();
+	bool val;
 
-	if (kstrtobool(page, &restore))
+	if (kstrtobool(page, &val))
 		return -EINVAL;
+
+	if (val) {
+		if (restore) {
+			pr_warn("dm-crypt keys already restored!\n");
+			return count;
+		} else if (!restore_dm_crypt_keys_to_thread_keyring())
+			restore = true;
+	}
 
 	return count;
 }
