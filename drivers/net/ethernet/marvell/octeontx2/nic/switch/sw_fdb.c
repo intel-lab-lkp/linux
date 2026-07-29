00ae34fc69c780fab4862a5f78ccd33db1ae3633
Ratheesh Kannoth <rkannoth@marvell.com>
2026-07-29 10:20:53 +0530

    octeontx2-pf: switch: Add pf files hierarchy
    Adds CONFIG_OCTEONTX_SWITCH, links stub switch objects into the PF
    module, and introduces empty sw_* init/deinit and notifier hooks for
    later patches.
    
    Signed-off-by: Ratheesh Kannoth <rkannoth@marvell.com>

---
+++ drivers/net/ethernet/marvell/octeontx2/nic/switch/sw_fdb.c
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0
+/* Marvell RVU switch driver
+ *
+ * Copyright (C) 2026 Marvell.
+ *
+ */
+#include "sw_fdb.h"
+
+#if IS_ENABLED(CONFIG_OCTEONTX_SWITCH)
+int sw_fdb_init(void)
+{
+	return 0;
+}
+
+void sw_fdb_deinit(void)
+{
+}
+
+#endif
