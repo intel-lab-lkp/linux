45f5f603aab7c4be93d1132ad3596bc9cb079b4c
Tina Zhang <zhang_wei@open-hieco.net>
2026-07-30 20:08:06 +0800

    KVM: nSVM: Track hardware-provided instruction bytes
    DecodeAssists provides instruction bytes for nested page faults and
    intercepted page faults caused by data accesses.  When reflecting such an
    exit to L1, KVM needs to distinguish bytes produced by the current hardware
    VM-Exit from stale VMCB02 state.
    
    Clear the VMCB02 instruction-byte fields before each nested run and track
    whether the nested VM-Exit being reflected originated from hardware.
    Preserve that state when a hardware #PF is processed by KVM and then
    reflected to L1.
    
    A subsequent change will use this state to propagate hardware-provided
    instruction bytes to VMCB12.
    
    Signed-off-by: Tina Zhang <zhang_wei@open-hieco.net>

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -242,6 +242,12 @@ struct svm_nested_state {
 	 * on its side.
 	 */
 	bool force_msr_bitmap_recalc;
+
+	/*
+	 * True if VMCB02 contains DecodeAssist instruction bytes from the
+	 * hardware VM-Exit currently being reflected to L1.
+	 */
+	bool vmcb02_insn_bytes_fresh;
 };
 
 struct vcpu_sev_es_state {
@@ -887,7 +893,7 @@ static inline void nested_svm_simple_vmexit(struct vcpu_svm *svm, u32 exit_code)
 	nested_svm_vmexit(svm);
 }
 
-int nested_svm_exit_handled(struct vcpu_svm *svm);
+int nested_svm_exit_handled(struct vcpu_svm *svm, bool vmcb02_insn_bytes_fresh);
 int nested_svm_check_permissions(struct kvm_vcpu *vcpu);
 int nested_svm_check_cached_vmcb12(struct kvm_vcpu *vcpu);
 int nested_svm_check_exception(struct vcpu_svm *svm, unsigned nr,
