b4d936287758e91574ab5a4cefaa08cfeb61192e
Mete Durlu <meted@linux.ibm.com>
2026-07-30 11:23:29 +0200

    sparc: Implement arch_do_panic
    Implement sparc specific arch_do_panic() instead of using sparc specific
    ifdef sections in vpanic() code.
    Reorder arch specific panic handling, sparc's Stop-A messages are now
    printed after "end Kernel panic" marker.
    
    Signed-off-by: Mete Durlu <meted@linux.ibm.com>

--- arch/sparc/kernel/setup.c
+++ arch/sparc/kernel/setup.c
@@ -36,6 +36,14 @@ static const struct ctl_table sparc_sysctl_table[] = {
 #endif
 };
 
+void arch_do_panic(void)
+{
+	extern int stop_a_enabled;
+	/* Make sure the user can actually press Stop-A (L1-A) */
+	stop_a_enabled = 1;
+	pr_emerg("Press Stop-A (L1-A) from sun keyboard or send break\n"
+		 "twice on console to return to the boot prom\n");
+}
 
 static int __init init_sparc_sysctls(void)
 {
