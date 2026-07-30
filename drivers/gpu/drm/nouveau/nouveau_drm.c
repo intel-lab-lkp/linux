1eb4613b7f1ec7245cbc4e7be58455ee6c91d00d
Lyude Paul <lyude@redhat.com>
2026-07-30 15:51:07 -0400

    drm/nouveau: Fix drm_driver struct/nouveau.atomic parameter handling
    The way we handled the nouveau.atomic module parameter before was fairly
    broken, and had a number of issues:
    
    - It was only ever actually parsed in the case of PCI devices.
    - When nouveau.atomic was enabled, it would add the cap for atomic
      modesetting to the global driver_pci structure. This meant that if one
      GPU on a system supported atomic and another didn't, it would still get
      enabled for both.
    
    Looking into this exposed further silliness in the way that we actually
    handle the drm_driver struct. We have one global structure for platform
    devices, and another for PCI devices - both of which are literally
    identical.
    
    So before we start preparing to enable atomic modesetting by default, let's
    fix this. Instead of sharing driver_pci and driver_platform, we move over
    to dynamically allocating a drm_driver structure with devm to use, which
    copies the contents of driver_stub. We then convert driver_stub to a const,
    and move the handling of the nouveau.atomic module parameter into
    nouveau_drm_device_new(), and conditionally add the atomic modesetting
    capability to the dynamically allocated drm_driver struct.
    
    Doing this is also preferable, as the next step for enabling atomic
    modesetting by default will be ensuring that we don't enable it for legacy
    devices that still don't support it. This requires only checking the atomic
    modesetting module parameter after the NVIF device is ready, as this allows
    us to check the GPU family that nouveau is running on.
    
    Signed-off-by: Lyude Paul <lyude@redhat.com>

--- drivers/gpu/drm/nouveau/nouveau_drm.c
+++ drivers/gpu/drm/nouveau/nouveau_drm.c
@@ -111,9 +111,7 @@ MODULE_PARM_DESC(runpm, "disable (0), force enable (1), optimus only default (-1
 static int nouveau_runtime_pm = -1;
 module_param_named(runpm, nouveau_runtime_pm, int, 0400);
 
-static struct drm_driver driver_stub;
-static struct drm_driver driver_pci;
-static struct drm_driver driver_platform;
+static const struct drm_driver driver_stub;
 
 #ifdef CONFIG_DEBUG_FS
 struct dentry *nouveau_debugfs_root;
@@ -726,8 +724,7 @@ nouveau_drm_device_del(struct nouveau_drm *drm)
 }
 
 static struct nouveau_drm *
-nouveau_drm_device_new(const struct drm_driver *drm_driver, struct device *parent,
-		       struct nvkm_device *device)
+nouveau_drm_device_new(struct device *parent, struct nvkm_device *device)
 {
 	static const struct nvif_mclass
 	mmus[] = {
@@ -737,11 +734,17 @@ nouveau_drm_device_new(const struct drm_driver *drm_driver, struct device *paren
 		{}
 	};
 	struct nouveau_drm *drm;
+	struct drm_driver *drm_driver;
 	int ret;
 
 	drm = devm_kzalloc(parent, sizeof(*drm), GFP_KERNEL);
 	if (!drm)
 		return ERR_PTR(-ENOMEM);
+
+	drm_driver = devm_kzalloc(parent, sizeof(*drm_driver), GFP_KERNEL);
+	if (!drm_driver)
+		return ERR_PTR(-ENOMEM);
+	*drm_driver = driver_stub;
 
 	drm->nvkm = device;
 
@@ -766,6 +769,9 @@ nouveau_drm_device_new(const struct drm_driver *drm_driver, struct device *paren
 		NV_ERROR(drm, "Device allocation failed: %d\n", ret);
 		goto done;
 	}
+
+	if (nouveau_atomic)
+		drm_driver->driver_features |= DRIVER_ATOMIC;
 
 	ret = nvif_device_map(&drm->device);
 	if (ret) {
@@ -870,16 +876,13 @@ static int nouveau_drm_probe(struct pci_dev *pdev,
 		return ret;
 
 	/* Remove conflicting drivers (vesafb, efifb etc). */
-	ret = aperture_remove_conflicting_pci_devices(pdev, driver_pci.name);
+	ret = aperture_remove_conflicting_pci_devices(pdev, driver_stub.name);
 	if (ret)
 		goto fail_nvkm;
 
 	pci_set_master(pdev);
 
-	if (nouveau_atomic)
-		driver_pci.driver_features |= DRIVER_ATOMIC;
-
-	drm = nouveau_drm_device_new(&driver_pci, &pdev->dev, device);
+	drm = nouveau_drm_device_new(&pdev->dev, device);
 	if (IS_ERR(drm)) {
 		ret = PTR_ERR(drm);
 		goto fail_nvkm;
@@ -1356,7 +1359,7 @@ nouveau_driver_fops = {
 	.fop_flags = FOP_UNSIGNED_OFFSET,
 };
 
-static struct drm_driver
+static const struct drm_driver
 driver_stub = {
 	.driver_features = DRIVER_GEM |
 			   DRIVER_SYNCOBJ | DRIVER_SYNCOBJ_TIMELINE |
@@ -1453,7 +1456,7 @@ nouveau_platform_device_create(const struct nvkm_device_tegra_func *func,
 	if (err)
 		goto err_free;
 
-	drm = nouveau_drm_device_new(&driver_platform, &pdev->dev, *pdevice);
+	drm = nouveau_drm_device_new(&pdev->dev, *pdevice);
 	if (IS_ERR(drm)) {
 		err = PTR_ERR(drm);
 		goto err_free;
@@ -1477,9 +1480,6 @@ static int __init
 nouveau_drm_init(void)
 {
 	int ret;
-
-	driver_pci = driver_stub;
-	driver_platform = driver_stub;
 
 	nouveau_display_options();
 
