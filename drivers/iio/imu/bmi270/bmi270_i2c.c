134dd2adc4f391f16e9689c4582872d264fe56a6
Philip Mueller <philm@manjaro.org>
2026-07-31 21:53:25 +0200

    iio: imu: bmi270: fix typo found in dc757dc
    Fixes a typo found in dc757dc.
    
    Fixes: dc757dc1572d ("iio: imu: bmi270: Match PNP ID found on newer GPD firmware")
    Cc: stable@vger.kernel.org # 6.18+
    Signed-off-by: Philip Mueller <philm@manjaro.org>

--- drivers/iio/imu/bmi270/bmi270_i2c.c
+++ drivers/iio/imu/bmi270/bmi270_i2c.c
@@ -42,7 +42,7 @@ static const struct acpi_device_id bmi270_acpi_match[] = {
 	/* GPD Win Mini, Aya Neo AIR Pro, OXP Mini Pro, etc. */
 	{ "BMI0160",  (kernel_ulong_t)&bmi260_chip_info },
 	{ "BOSC0160", (kernel_ulong_t)&bmi260_chip_info },
-	/* GPD Win Max 2 2023(sincice BIOS v0.40), etc. */
+	/* GPD Win Max 2 2023 (since BIOS v0.40), etc. */
 	{ "BMI0260",  (kernel_ulong_t)&bmi260_chip_info },
 	/* OrangePi Neo 2026 (since BIOS BCPH01_V1.20), etc. */
 	{ "BOSC0260", (kernel_ulong_t)&bmi260_chip_info },
