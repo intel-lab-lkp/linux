8fe5961cd328e9f9df585368d90324420259187a
Ali Nasrollahi <a.nasrolahi01@gmail.com>
2026-07-31 21:03:10 +0330

    staging: media: atomisp: remove unnecessary parentheses
    Remove unnecessary parentheses around comparison expressions in ov2722.c
    to be alinged with kernel coding style. This patch is only cosmetic and
    does not change the logic.
    
    Signed-off-by: Ali Nasrollahi <A.Nasrolahi01@gmail.com>

--- drivers/staging/media/atomisp/i2c/atomisp-ov2722.c
+++ drivers/staging/media/atomisp/i2c/atomisp-ov2722.c
@@ -731,7 +731,7 @@ static int ov2722_detect(struct i2c_client *client)
 			OV2722_SC_CMMN_CHIP_ID_L, &low);
 	id = (high << 8) | low;
 
-	if ((id != OV2722_ID) && (id != OV2720_ID)) {
+	if (id != OV2722_ID && id != OV2720_ID) {
 		dev_err(&client->dev, "sensor ID error\n");
 		return -ENODEV;
 	}
