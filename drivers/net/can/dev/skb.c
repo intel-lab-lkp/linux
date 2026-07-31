7da631e3cda5855ae6e73f228217e5ba44e7bf70
Cunhao Lu <1579567540@qq.com>
2026-07-31 17:45:00 +0800

    can: skb: make echo skb freeing safe in any IRQ context
    can_put_echo_skb() can be called with hardware interrupts disabled. Its
    direct drop paths use kfree_skb(), while can_create_echo_skb() uses
    kfree_skb() when cloning fails and consume_skb() after a successful clone.
    None of these helpers is safe in every IRQ context.
    
    Use dev_kfree_skb_any() for all drop paths and dev_consume_skb_any() when
    consuming a successfully cloned skb. This preserves the respective skb drop
    and consumed semantics regardless of the caller IRQ context.
    
    Signed-off-by: Cunhao Lu <1579567540@qq.com>
    Link: https://patch.msgid.link/tencent_E84809CF236D0137885E7E4E4D58340B3208@qq.com
    Signed-off-by: Marc Kleine-Budde <mkl@pengutronix.de>

--- drivers/net/can/dev/skb.c
+++ drivers/net/can/dev/skb.c
@@ -63,7 +63,7 @@ int can_put_echo_skb(struct sk_buff *skb, struct net_device *dev,
 	    (skb->protocol != htons(ETH_P_CAN) &&
 	     skb->protocol != htons(ETH_P_CANFD) &&
 	     skb->protocol != htons(ETH_P_CANXL))) {
-		kfree_skb(skb);
+		dev_kfree_skb_any(skb);
 		return 0;
 	}
 
@@ -91,7 +91,7 @@ int can_put_echo_skb(struct sk_buff *skb, struct net_device *dev,
 	} else {
 		/* locking problem with netif_stop_queue() ?? */
 		netdev_err(dev, "%s: BUG! echo_skb %d is occupied!\n", __func__, idx);
-		kfree_skb(skb);
+		dev_kfree_skb_any(skb);
 		return -EBUSY;
 	}
 
