47ca69e8f9dc5569236dc20a5f735d54565ecf16
Jonas Haugk <j.haugk@gmx.de>
2026-08-02 23:03:18 +0200

    hwmon: dell-smm-hwmon: Add fan control for OptiPlex Micro 7020
    Add the Dell OptiPlex Micro 7020 to the fan control whitelist using
    the 0x34a3/0x35a3 SMM command pair.
    
    Manual fan control was tested successfully with PWM values 64, 128,
    and 255. The fan speed changed accordingly. Restoring automatic BIOS
    fan control through pwm1_enable also worked correctly.
    
    Tested on a Dell OptiPlex Micro 7020.
    
    Signed-off-by: Jonas Haugk <j.haugk@gmx.de>

--- drivers/hwmon/dell-smm-hwmon.c
+++ drivers/hwmon/dell-smm-hwmon.c
@@ -1655,6 +1655,15 @@ static const struct dmi_system_id i8k_whitelist_fan_control[] __initconst = {
 		},
 		.driver_data = (void *)&i8k_fan_control_data[I8K_FAN_30A3_31A3],
 	},
+	{
+		.ident = "Dell OptiPlex Micro 7020",
+		.matches = {
+			DMI_MATCH(DMI_SYS_VENDOR, "Dell Inc."),
+			DMI_EXACT_MATCH(DMI_PRODUCT_NAME,
+					"OptiPlex Micro 7020"),
+		},
+		.driver_data = (void *)&i8k_fan_control_data[I8K_FAN_34A3_35A3],
+	},
 	{ }
 };
 
