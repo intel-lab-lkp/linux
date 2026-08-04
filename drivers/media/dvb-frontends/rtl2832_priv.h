237e713884116b7613da2a98c6fab3dd715d6abb
Link Mauve <linkmauve@linkmauve.fr>
2026-08-04 13:46:21 +0200

    media: rtl2832: make regmap_config static const
    This struct was previously carried over in the rtl2832_dev struct, even
    though it was used only once at probe-time, using purely static data.
    
    This reduces the size of the rtl2832_dev struct by 192 B, by putting it
    in the .rodata section instead.
    
    Signed-off-by: Link Mauve <linkmauve@linkmauve.fr>

--- drivers/media/dvb-frontends/rtl2832_priv.h
+++ drivers/media/dvb-frontends/rtl2832_priv.h
@@ -20,7 +20,6 @@
 struct rtl2832_dev {
 	struct rtl2832_platform_data *pdata;
 	struct i2c_client *client;
-	struct regmap_config regmap_config;
 	struct regmap *regmap;
 	struct i2c_mux_core *muxc;
 	struct dvb_frontend fe;
