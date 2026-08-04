828253d9880bdb98b10fc288379bf382dbb7aae4
Esben Haabendal <esben@geanix.com>
2026-08-04 19:02:15 +0200

    iio: light: ltr501: Power down chip if request irq fails
    In case devm_request_threaded_irq() failed, we were leaving the chip
    powered instead of powering it down again. Power down the chip switching
    ALS and PS modes to standby.
    
    The dev_err() message was causing duplicated error messages, as
    devm_request_threaded_irq() calls dev_err_probe() on failure.
    
    Fixes: 7ac702b3144b ("iio: ltr501: Add interrupt support")
    Cc: stable@vger.kernel.org
    Signed-off-by: Esben Haabendal <esben@geanix.com>

--- drivers/iio/light/ltr501.c
+++ drivers/iio/light/ltr501.c
@@ -1537,11 +1537,8 @@ static int ltr501_probe(struct i2c_client *client)
 						IRQF_ONESHOT,
 						"ltr501_thresh_event",
 						indio_dev);
-		if (ret) {
-			dev_err(&client->dev, "request irq (%d) failed\n",
-				client->irq);
-			return ret;
-		}
+		if (ret)
+			goto powerdown_on_error;
 	} else {
 		indio_dev->info = data->chip_info->info_no_irq;
 	}
