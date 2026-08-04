c25d15139304e799fe1d9927541176ba6711618e
Cosmin Ratiu <cratiu@nvidia.com>
2026-08-04 11:35:30 +0300

    net/mlx5e: psp: Handle HW-decapsulated RX PSP packets
    mlx5e_psp_offload_handle_rx_skb() handles RX PSP packets.
    
    Add an additional flow tag marker and helpers for decapsulated PSP
    packets and extend the handler to construct the PSP skb extension from
    the CQE fields (SPI in ft_metadata and PSP version in flow_tag).
    
    Signed-off-by: Cosmin Ratiu <cratiu@nvidia.com>
    Reviewed-by: Dragos Tatulea <dtatulea@nvidia.com>
    Signed-off-by: Tariq Toukan <tariqt@nvidia.com>

--- drivers/net/ethernet/mellanox/mlx5/core/en_accel/flow_tag.h
+++ drivers/net/ethernet/mellanox/mlx5/core/en_accel/flow_tag.h
@@ -16,8 +16,10 @@
  *              1 = IPsec
  *              2 = MACsec
  *              3 = PSP (HW decrypted, PSP header present)
- *              4-7 = reserved
- *   [20:16] = reserved
+ *              4 = PSP decap (HW decrypted & decapsulated)
+ *              5-7 = reserved
+ *   [20:17] = PSP version (4 bits, valid when proto == PSP decap)
+ *   16      = reserved
  *   [15:0]  = used by other subsystems (e.g. TC).
  */
 #define MLX5E_ACCEL_FLOW_TAG_PROTO_MASK		GENMASK(23, 21)
@@ -25,6 +27,10 @@
 #define MLX5E_ACCEL_FLOW_TAG_PROTO_IPSEC	(1 << 21)
 #define MLX5E_ACCEL_FLOW_TAG_PROTO_MACSEC	(2 << 21)
 #define MLX5E_ACCEL_FLOW_TAG_PROTO_PSP		(3 << 21)
+#define MLX5E_ACCEL_FLOW_TAG_PROTO_PSP_DECAP	(4 << 21)
+
+#define MLX5E_ACCEL_FLOW_TAG_PSP_VER_SHIFT	17
+#define MLX5E_ACCEL_FLOW_TAG_PSP_VER_MASK	(0xF << MLX5E_ACCEL_FLOW_TAG_PSP_VER_SHIFT)
 
 static inline u32 mlx5e_accel_flow_tag(struct mlx5_cqe64 *cqe)
 {
