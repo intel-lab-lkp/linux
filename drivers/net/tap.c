445d589b9baa7ec5a14aabb9c7fb814d824b95f7
Rongguang Wei <weirongguang@kylinos.cn>
2026-08-05 15:21:21 +0800

    tap: fix incorrect variable used for USO check in set_offload()
    The USO features in set_offload() incorrectly uses feature_mask
    instead of arg argument.
    
    TUN_F_USO4 and TUN_F_USO6 belong to the TUN_F_* flag, while feature_mask
    belong to NETIF_F_* values (NETIF_F_HW_CSUM, NETIF_F_TSO, etc.) and
    will never contain any TUN_F_* bits.
    
    Fixes: 399e0827642f ("driver/net/tun: Added features for USO.")
    Signed-off-by: Rongguang Wei <weirongguang@kylinos.cn>

--- drivers/net/tap.c
+++ drivers/net/tap.c
@@ -972,7 +972,7 @@ static int set_offload(struct tap_queue *q, unsigned long arg)
 	 * user-space will not receive TSO frames.
 	 */
 	if (feature_mask & (NETIF_F_TSO | NETIF_F_TSO6) ||
-	    (feature_mask & (TUN_F_USO4 | TUN_F_USO6)) == (TUN_F_USO4 | TUN_F_USO6))
+	    (arg & (TUN_F_USO4 | TUN_F_USO6)) == (TUN_F_USO4 | TUN_F_USO6))
 		features |= RX_OFFLOADS;
 	else
 		features &= ~RX_OFFLOADS;
