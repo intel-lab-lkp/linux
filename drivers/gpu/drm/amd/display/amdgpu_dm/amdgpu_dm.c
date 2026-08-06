intel-lab-lkp/linux

commit 0e691bc73026fb6e58772ca9b2fb3c5ad2044344

drm: Remove use of drm_mode_object
This patch removes the drm_mode_object prints, evaluation and use from
drm_display_mode objects used in drm files. It removes dependency from
drm_mode_object.

Signed-off-by: Shayenne Moura <shayenneluzmoura@gmail.com>
Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
Link: https://patchwork.freedesktop.org/patch/msgid/785896b0a551464d0b780a55411707300802d6b6.1547214023.git.shayenneluzmoura@gmail.com

drivers/gpu/drm/drm_crtc_helper.c
@@ -391,9 +391,8 @@ bool drm_crtc_helper_set_mode(struct drm_crtc *crtc,
 		if (!encoder_funcs)
 			continue;
 
-		DRM_DEBUG_KMS("[ENCODER:%d:%s] set [MODE:%d:%s]\n",
-			encoder->base.id, encoder->name,
-			mode->base.id, mode->name);
+		DRM_DEBUG_KMS("[ENCODER:%d:%s] set [MODE:%s]\n",
+			encoder->base.id, encoder->name, mode->name);
 		if (encoder_funcs->mode_set)
 			encoder_funcs->mode_set(encoder, mode, adjusted_mode);
 

drivers/gpu/drm/drm_modes.c
@@ -71,11 +71,6 @@ struct drm_display_mode *drm_mode_create(struct drm_device *dev)
 	if (!nmode)
 		return NULL;
 
-	if (drm_mode_object_add(dev, &nmode->base, DRM_MODE_OBJECT_MODE)) {
-		kfree(nmode);
-		return NULL;
-	}
-
 	return nmode;
 }
 EXPORT_SYMBOL(drm_mode_create);
@@ -91,8 +86,6 @@ void drm_mode_destroy(struct drm_device *dev, struct drm_display_mode *mode)
 {
 	if (!mode)
 		return;
-
-	drm_mode_object_unregister(dev, &mode->base);
 
 	kfree(mode);
 }
@@ -911,11 +904,9 @@ EXPORT_SYMBOL(drm_mode_set_crtcinfo);
  */
 void drm_mode_copy(struct drm_display_mode *dst, const struct drm_display_mode *src)
 {
-	int id = dst->base.id;
 	struct list_head head = dst->head;
 
 	*dst = *src;
-	dst->base.id = id;
 	dst->head = head;
 }
 EXPORT_SYMBOL(drm_mode_copy);

commit f3706bdc5b3f21bb6907af6ac3b3c3ff5aa9661f

gpu/drm: Remove unused *dev parameter from drm_mode_destroy function
Get rid of unused 'struct drm_device *dev' parameter from drm_mode_destroy()
prototype and definition.
Keep drm_mode_destroy() comment up to date - remove unnecessary *dev record.
Also, fix all drm_mode_destroy() calls where *dev argument is used.

Fixes: 0e691bc73026 ("drm: Remove use of drm_mode_object")
Signed-off-by: Jan Niedziolka <j.niedziolka@samsung.com>

drivers/gpu/drm/amd/display/amdgpu_dm/amdgpu_dm.c
@@ -8562,7 +8562,7 @@ enum drm_mode_status amdgpu_dm_connector_mode_valid(struct drm_connector *connec
 	stream = create_validate_stream_for_sink(connector, test_mode,
 						 to_dm_connector_state(connector->state),
 						 NULL);
-	drm_mode_destroy(connector->dev, test_mode);
+	drm_mode_destroy(test_mode);
 	if (stream) {
 		dc_stream_release(stream);
 		result = MODE_OK;
@@ -9217,7 +9217,7 @@ static uint add_fs_modes(struct amdgpu_dm_connector *aconnector)
 			drm_mode_probed_add(&aconnector->base, new_mode);
 			new_modes_count += 1;
 		} else
-			drm_mode_destroy(aconnector->base.dev, new_mode);
+			drm_mode_destroy(new_mode);
 	}
  out:
 	return new_modes_count;

drivers/gpu/drm/bridge/cadence/cdns-mhdp8546-core.c
@@ -1913,7 +1913,7 @@ cdns_mhdp_bridge_atomic_destroy_state(struct drm_bridge *bridge,
 	cdns_mhdp_state = to_cdns_mhdp_bridge_state(state);
 
 	if (cdns_mhdp_state->current_mode) {
-		drm_mode_destroy(bridge->dev, cdns_mhdp_state->current_mode);
+		drm_mode_destroy(cdns_mhdp_state->current_mode);
 		cdns_mhdp_state->current_mode = NULL;
 	}
 

drivers/gpu/drm/drm_client_modeset.c
@@ -79,7 +79,7 @@ static void drm_client_modeset_release(struct drm_client_dev *client)
 	drm_client_for_each_modeset(modeset, client) {
 		int i;
 
-		drm_mode_destroy(client->dev, modeset->mode);
+		drm_mode_destroy(modeset->mode);
 		modeset->mode = NULL;
 		modeset->fb = NULL;
 
@@ -272,7 +272,7 @@ static void mode_replace(struct drm_device *dev,
 			 const struct drm_display_mode **dst,
 			 const struct drm_display_mode *src)
 {
-	drm_mode_destroy(dev, (struct drm_display_mode *)*dst);
+	drm_mode_destroy((struct drm_display_mode *)*dst);
 
 	*dst = src ? drm_mode_duplicate(dev, src) : NULL;
 }
@@ -367,7 +367,7 @@ static bool drm_client_target_cloned(struct drm_device *dev,
 		if (!modes[i])
 			can_clone = false;
 	}
-	drm_mode_destroy(dev, dmt_mode);
+	drm_mode_destroy(dmt_mode);
 
 	if (can_clone) {
 		drm_dbg_kms(dev, "can clone using 1024x768\n");
@@ -913,7 +913,7 @@ int drm_client_modeset_probe(struct drm_client_dev *client, unsigned int width, 
 				break;
 			}
 
-			drm_mode_destroy(dev, modeset->mode);
+			drm_mode_destroy(modeset->mode);
 			modeset->mode = drm_mode_duplicate(dev, mode);
 			if (!modeset->mode) {
 				ret = -ENOMEM;

drivers/gpu/drm/drm_connector.c
@@ -706,7 +706,7 @@ static void drm_mode_remove(struct drm_connector *connector,
 			    struct drm_display_mode *mode)
 {
 	list_del(&mode->head);
-	drm_mode_destroy(connector->dev, mode);
+	drm_mode_destroy(mode);
 }
 
 /**

drivers/gpu/drm/drm_crtc.c
@@ -897,7 +897,7 @@ out:
 		}
 	}
 	kfree(connector_set);
-	drm_mode_destroy(dev, mode);
+	drm_mode_destroy(mode);
 
 	/* In case we need to retry... */
 	connector_set = NULL;

drivers/gpu/drm/drm_crtc_helper.c
@@ -421,7 +421,7 @@ bool drm_crtc_helper_set_mode(struct drm_crtc *crtc,
 
 	/* FIXME: add subpixel order */
 done:
-	drm_mode_destroy(dev, adjusted_mode);
+	drm_mode_destroy(adjusted_mode);
 	if (!ret) {
 		crtc->enabled = saved_enabled;
 		drm_mode_copy(&crtc->mode, &saved_mode);

drivers/gpu/drm/drm_edid.c
@@ -3361,7 +3361,7 @@ drm_gtf2_mode(struct drm_device *dev,
 		return NULL;
 
 	if (drm_mode_hsync(mode) > drm_gtf2_hbreak(drm_edid)) {
-		drm_mode_destroy(dev, mode);
+		drm_mode_destroy(mode);
 		mode = drm_gtf_mode_complex(dev, hsize, vsize,
 					    vrefresh_rate, 0, 0,
 					    drm_gtf2_m(drm_edid),
@@ -3777,7 +3777,7 @@ static int drm_gtf_modes_for_range(struct drm_connector *connector,
 		drm_mode_fixup_1366x768(newmode);
 		if (!mode_in_range(newmode, drm_edid, timing) ||
 		    !valid_inferred_mode(connector, newmode)) {
-			drm_mode_destroy(dev, newmode);
+			drm_mode_destroy(newmode);
 			continue;
 		}
 
@@ -3806,7 +3806,7 @@ static int drm_gtf2_modes_for_range(struct drm_connector *connector,
 		drm_mode_fixup_1366x768(newmode);
 		if (!mode_in_range(newmode, drm_edid, timing) ||
 		    !valid_inferred_mode(connector, newmode)) {
-			drm_mode_destroy(dev, newmode);
+			drm_mode_destroy(newmode);
 			continue;
 		}
 
@@ -3836,7 +3836,7 @@ static int drm_cvt_modes_for_range(struct drm_connector *connector,
 		drm_mode_fixup_1366x768(newmode);
 		if (!mode_in_range(newmode, drm_edid, timing) ||
 		    !valid_inferred_mode(connector, newmode)) {
-			drm_mode_destroy(dev, newmode);
+			drm_mode_destroy(newmode);
 			continue;
 		}
 

drivers/gpu/drm/drm_modes.c
@@ -85,12 +85,11 @@ EXPORT_SYMBOL(drm_mode_create);
 
 /**
  * drm_mode_destroy - remove a mode
- * @dev: DRM device
  * @mode: mode to remove
  *
- * Release @mode's unique ID, then free it @mode structure itself using kfree.
+ * Free @mode structure using kfree.
  */
-void drm_mode_destroy(struct drm_device *dev, struct drm_display_mode *mode)
+void drm_mode_destroy(struct drm_display_mode *mode)
 {
 	if (!mode)
 		return;
@@ -591,7 +590,7 @@ struct drm_display_mode *drm_analog_tv_mode(struct drm_device *dev,
 	return mode;
 
 err_free_mode:
-	drm_mode_destroy(dev, mode);
+	drm_mode_destroy(mode);
 	return NULL;
 }
 EXPORT_SYMBOL(drm_analog_tv_mode);
@@ -1842,7 +1841,7 @@ void drm_mode_prune_invalid(struct drm_device *dev,
 				drm_dbg_kms(dev, "Rejected mode: " DRM_MODE_FMT " (%s)\n",
 					    DRM_MODE_ARG(mode), drm_get_mode_status_name(mode->status));
 			}
-			drm_mode_destroy(dev, mode);
+			drm_mode_destroy(mode);
 		}
 	}
 }
@@ -1947,7 +1946,7 @@ void drm_connector_list_update(struct drm_connector *connector)
 			}
 
 			list_del(&pmode->head);
-			drm_mode_destroy(connector->dev, pmode);
+			drm_mode_destroy(pmode);
 			break;
 		}
 

drivers/gpu/drm/gma500/psb_intel_sdvo.c
@@ -1787,8 +1787,7 @@ static void psb_intel_sdvo_enc_destroy(struct drm_encoder *encoder)
 	struct psb_intel_sdvo *psb_intel_sdvo = to_psb_intel_sdvo(encoder);
 
 	if (psb_intel_sdvo->sdvo_lvds_fixed_mode != NULL)
-		drm_mode_destroy(encoder->dev,
-				 psb_intel_sdvo->sdvo_lvds_fixed_mode);
+		drm_mode_destroy(psb_intel_sdvo->sdvo_lvds_fixed_mode);
 
 	i2c_del_adapter(&psb_intel_sdvo->ddc);
 	gma_encoder_destroy(encoder);

drivers/gpu/drm/i915/display/intel_panel.c
@@ -401,7 +401,7 @@ static void intel_panel_destroy_probed_modes(struct intel_connector *connector)
 			    connector->base.base.id, connector->base.name,
 			    DRM_MODE_ARG(mode));
 		list_del(&mode->head);
-		drm_mode_destroy(display->drm, mode);
+		drm_mode_destroy(mode);
 	}
 }
 
@@ -558,7 +558,7 @@ void intel_panel_fini(struct intel_connector *connector)
 
 	list_for_each_entry_safe(fixed_mode, next, &panel->fixed_modes, head) {
 		list_del(&fixed_mode->head);
-		drm_mode_destroy(connector->base.dev, fixed_mode);
+		drm_mode_destroy(fixed_mode);
 	}
 }
 

drivers/gpu/drm/msm/dsi/dsi_host.c
@@ -2579,7 +2579,7 @@ int msm_dsi_host_set_display_mode(struct mipi_dsi_host *host,
 	struct msm_dsi_host *msm_host = to_msm_dsi_host(host);
 
 	if (msm_host->mode) {
-		drm_mode_destroy(msm_host->dev, msm_host->mode);
+		drm_mode_destroy(msm_host->mode);
 		msm_host->mode = NULL;
 	}
 

drivers/gpu/drm/nouveau/dispnv50/disp.c
@@ -1185,7 +1185,7 @@ nv50_mstc_get_modes(struct drm_connector *connector)
 		connector->display_info.bpc = 8;
 
 	if (mstc->native)
-		drm_mode_destroy(mstc->connector.dev, mstc->native);
+		drm_mode_destroy(mstc->native);
 	mstc->native = nouveau_conn_native_mode(&mstc->connector);
 	return ret;
 }

drivers/gpu/drm/nouveau/nouveau_connector.c
@@ -969,7 +969,7 @@ nouveau_connector_get_modes(struct drm_connector *connector)
 	/* destroy the native mode, the attached monitor could have changed.
 	 */
 	if (nv_connector->native_mode) {
-		drm_mode_destroy(dev, nv_connector->native_mode);
+		drm_mode_destroy(nv_connector->native_mode);
 		nv_connector->native_mode = NULL;
 	}
 

drivers/gpu/drm/vboxvideo/vbox_mode.c
@@ -782,7 +782,7 @@ static int vbox_fill_modes(struct drm_connector *connector, u32 max_x,
 	dev = vbox_connector->base.dev;
 	list_for_each_entry_safe(mode, iterator, &connector->modes, head) {
 		list_del(&mode->head);
-		drm_mode_destroy(dev, mode);
+		drm_mode_destroy(mode);
 	}
 
 	return drm_helper_probe_single_connector_modes(connector, max_x, max_y);

include/drm/drm_modes.h
@@ -454,7 +454,7 @@ struct drm_connector;
 struct drm_cmdline_mode;
 
 struct drm_display_mode *drm_mode_create(struct drm_device *dev);
-void drm_mode_destroy(struct drm_device *dev, struct drm_display_mode *mode);
+void drm_mode_destroy(struct drm_display_mode *mode);
 void drm_mode_convert_to_umode(struct drm_mode_modeinfo *out,
 			       const struct drm_display_mode *in);
 int drm_mode_convert_umode(struct drm_device *dev,

drivers/gpu/drm/tests/drm_kunit_helpers.c
@@ -337,7 +337,7 @@ static void kunit_action_drm_mode_destroy(void *ptr)
 {
 	struct drm_display_mode *mode = ptr;
 
-	drm_mode_destroy(NULL, mode);
+	drm_mode_destroy(mode);
 }
 
 /**