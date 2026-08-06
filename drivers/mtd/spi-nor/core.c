80d860618cd62346f1c3a72bccf8b7dab6433398
Chen Minqiang <ptpt52@gmail.com>
2026-08-06 19:37:33 +0800

    mtd: spi-nor: allow force unlocking via DT property
    Some SPI NOR flash chips (such as generic or unlisted chips used in vendor
    devices like Tenda AX12L Pro) have Block Protection (BP) bits set in the
    Status Register by bootloaders or factory settings, locking flash blocks.
    
    Because vendors frequently switch between various generic SPI NOR flash
    chips ("Flash Lottery"), it is impractical to upstream explicit chip ID
    flags (SNOR_F_HAS_LOCK) for every possible generic chip variant.
    
    This patch introduces support for the "linux,force-sr-unlock" Device Tree
    property:
    1. In spi_nor_init_flags(), set the SNOR_F_HAS_LOCK flag on the flash
       instance if "linux,force-sr-unlock" is present in the flash DT node. This
       allows spi_nor_late_init_params() to automatically populate default
       locking_ops without altering swp.c.
    2. In spi_nor_init(), trigger spi_nor_try_unlock_all() if "linux,force-sr-unlock"
       is set, invoking Linux kernel's native spi_nor_unlock() mechanism.
    
    Signed-off-by: Chen Minqiang <ptpt52@gmail.com>

--- drivers/mtd/spi-nor/core.c
+++ drivers/mtd/spi-nor/core.c
@@ -2956,6 +2956,9 @@ static void spi_nor_init_flags(struct spi_nor *nor)
 	if (of_property_read_bool(np, "no-wp"))
 		nor->flags |= SNOR_F_NO_WP;
 
+	if (of_property_read_bool(np, "linux,force-sr-unlock"))
+		nor->flags |= SNOR_F_HAS_LOCK;
+
 	if (flags & SPI_NOR_SWP_IS_VOLATILE)
 		nor->flags |= SNOR_F_SWP_IS_VOLATILE;
 
@@ -3332,7 +3335,8 @@ static int spi_nor_init(struct spi_nor *nor)
 	spi_nor_cache_sr_lock_bits(nor, NULL);
 	if (IS_ENABLED(CONFIG_MTD_SPI_NOR_SWP_DISABLE) ||
 	    (IS_ENABLED(CONFIG_MTD_SPI_NOR_SWP_DISABLE_ON_VOLATILE) &&
-	     nor->flags & SNOR_F_SWP_IS_VOLATILE)) {
+	     nor->flags & SNOR_F_SWP_IS_VOLATILE) ||
+	    of_property_read_bool(spi_nor_get_flash_node(nor), "linux,force-sr-unlock")) {
 		spi_nor_try_unlock_all(nor);
 	}
 
