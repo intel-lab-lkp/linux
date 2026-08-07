2440a7b0439531b524b9a06a2d29ce59e123b8ef
Kuniyuki Iwashima <kuniyu@google.com>
2026-08-07 23:28:46 +0000

    ipv6: Replace &nd_tbl with nd_table(net).
    We will allocate per-netns neigh_table in net->neigh_tables[].
    
    Let's replace &nd_tbl with nd_table(net).
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>

--- net/bridge/br_arp_nd_proxy.c
+++ net/bridge/br_arp_nd_proxy.c
@@ -468,7 +468,7 @@ void br_do_suppress_nd(struct sk_buff *skb, struct net_bridge *br,
 		return;
 	}
 
-	n = neigh_lookup(&nd_tbl, &msg->target, vlandev);
+	n = neigh_lookup(nd_table(dev_net(vlandev)), &msg->target, vlandev);
 	if (n) {
 		struct net_bridge_fdb_entry *f;
 
