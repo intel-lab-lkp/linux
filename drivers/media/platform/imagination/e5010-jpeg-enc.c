3ac44e6adacdaed5c0c091ba9e861c38b9caca1f
Brandon Brnich <b-brnich@ti.com>
2026-08-10 10:54:47 -0500

    media: imagination: e5010: Move e5010_init_device to Runtime Resume Hook
    The PM framework skips the runtime resume hook if the device was never on
    in the first place. This results in a crash in the system resume hook when
    a call to e5010_init_device attempts a write to a powered down device.
    
    Move e5010_init_device to the resume hook to ensure register write only
    occurs when device is properly powered on.
    
    Fixes: a1e294045885 ("media: imagination: Add E5010 JPEG Encoder driver")
    Cc: stable@vger.kernel.org
    Signed-off-by: Brandon Brnich <b-brnich@ti.com>
    Reviewed-by: Jai Luthra <jai.luthra@ideasonboard.com>

--- drivers/media/platform/imagination/e5010-jpeg-enc.c
+++ drivers/media/platform/imagination/e5010-jpeg-enc.c
@@ -1501,6 +1501,13 @@ static int e5010_runtime_resume(struct device *dev)
 		return ret;
 	}
 
+	ret = e5010_init_device(e5010);
+	if (ret) {
+		dev_err(dev, "Failed to re-enable e5010 device\n");
+		clk_disable_unprepare(e5010->clk);
+		return ret;
+	}
+
 	return 0;
 }
 
@@ -1532,12 +1539,6 @@ static int e5010_resume(struct device *dev)
 	ret = pm_runtime_force_resume(dev);
 	if (ret < 0)
 		return ret;
-
-	ret = e5010_init_device(e5010);
-	if (ret) {
-		dev_err(dev, "Failed to re-enable e5010 device\n");
-		return ret;
-	}
 
 	v4l2_m2m_resume(e5010->m2m_dev);
 
