b8c3425597de284f2a745969b97685f9a157a25d
Thomas Huth <thuth@redhat.com>
2026-08-10 14:58:55 +0200

    smb: server: Make sure that passkey is not leaked on the heap in user_config.c
    Use kfree_sensitive() to free the user->passkey (and the struct
    ksmbd_login_response in ksmbd_login_user() that contains the same
    information) to avoid that this information could leak somewhere
    else via the heap.
    
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- fs/smb/server/mgmt/user_config.c
+++ fs/smb/server/mgmt/user_config.c
@@ -27,7 +27,7 @@ struct ksmbd_user *ksmbd_login_user(const char *account)
 
 	user = ksmbd_alloc_user(resp, resp_ext);
 out:
-	kvfree(resp);
+	kvfree_sensitive(resp, sizeof(*resp));
 	return user;
 }
 
@@ -70,7 +70,7 @@ struct ksmbd_user *ksmbd_alloc_user(struct ksmbd_login_response *resp,
 
 err_free:
 	kfree(user->name);
-	kfree(user->passkey);
+	kfree_sensitive(user->passkey);
 	kfree(user);
 	return NULL;
 }
@@ -80,7 +80,7 @@ void ksmbd_free_user(struct ksmbd_user *user)
 	ksmbd_ipc_logout_request(user->name, user->flags);
 	kfree(user->sgid);
 	kfree(user->name);
-	kfree(user->passkey);
+	kfree_sensitive(user->passkey);
 	kfree(user);
 }
 
