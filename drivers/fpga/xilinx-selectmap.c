d9806404503de4fb4cf3d1f2ada0a6418fe8e455
Heiko Schocher <hs@nabladev.com>
2026-08-10 08:20:51 +0200

    driver: fpga: xilinx-selectmap: add csi and rdwr support
    The current driver requests the optional CSI_B and RDWR_B GPIOs but
    only configures their initial output state and never changes them
    afterwards. As a result, CSI_B and RDWR_B remain inactive or active
    throughout the configuration process.
    
    This may work on systems with a single FPGA where these signals do not
    need to be controlled by software, or where the signals are configured
    to their active state. But this does not not support systems with
    multiple FPGAs sharing a SelectMAP interface.
    
    On systems with multiple FPGAs sharing the same SelectMAP data bus,
    the driver must deassert this signals in probe, and actively control
    them during configuration.
    
    CSI_B (Chip Select, active low) selects the target FPGA. It is asserted
    before configuration data is transferred and deasserted afterwards so
    that only the intended device responds to bus transactions.
    
    RDWR_B (Read/Write, active low) controls the transfer direction on the
    SelectMAP interface. A low level selects write cycles, while a high
    level selects read cycles. During FPGA configuration the driver drives
    RDWR_B low before transferring the bitstream and restores it to its
    reading (high state) afterwards.
    
    With that info from the datasheet the driver is now changed to:
    
    - deassert the CSI_B and RDWR_B pin on probe and store the
      optional GPIO descriptors in private driver data.
    
    - toggle both signals around the configuration data transfer
    
    This allows multiple FPGAs to safely share a single SelectMAP interface.
    
    Signed-off-by: Heiko Schocher <hs@nabladev.com>

--- drivers/fpga/xilinx-selectmap.c
+++ drivers/fpga/xilinx-selectmap.c
@@ -19,6 +19,8 @@
 struct xilinx_selectmap_conf {
 	struct xilinx_fpga_core core;
 	void __iomem *base;
+	struct gpio_desc *csi_b;
+	struct gpio_desc *rdwr_b;
 };
 
 #define to_xilinx_selectmap_conf(obj) \
@@ -30,8 +32,23 @@ static int xilinx_selectmap_write(struct xilinx_fpga_core *core,
 	struct xilinx_selectmap_conf *conf = to_xilinx_selectmap_conf(core);
 	size_t i;
 
+	/*
+	 * Assert CSI_B and select write mode.
+	 *
+	 * UG570 states in note 4 in Figure "Continuous x8 SelectMAP Data
+	 * Loading", RDWR_B should be asserted before CSI_B to avoid
+	 * causing an ABORT on the next CCLK.
+	 *
+	 * To be sure, set first RDWR_B pin before activate CSI_B
+	 */
+	gpiod_set_value(conf->rdwr_b, 1);
+	gpiod_set_value(conf->csi_b, 1);
+
 	for (i = 0; i < count; ++i)
 		writeb(buf[i], conf->base);
+
+	gpiod_set_value(conf->csi_b, 0);
+	gpiod_set_value(conf->rdwr_b, 0);
 
 	return 0;
 }
@@ -39,7 +56,6 @@ static int xilinx_selectmap_write(struct xilinx_fpga_core *core,
 static int xilinx_selectmap_probe(struct platform_device *pdev)
 {
 	struct xilinx_selectmap_conf *conf;
-	struct gpio_desc *gpio;
 	void __iomem *base;
 
 	conf = devm_kzalloc(&pdev->dev, sizeof(*conf), GFP_KERNEL);
@@ -55,16 +71,18 @@ static int xilinx_selectmap_probe(struct platform_device *pdev)
 				     "ioremap error\n");
 	conf->base = base;
 
-	/* CSI_B is active low */
-	gpio = devm_gpiod_get_optional(&pdev->dev, "csi", GPIOD_OUT_HIGH);
-	if (IS_ERR(gpio))
-		return dev_err_probe(&pdev->dev, PTR_ERR(gpio),
+	/* CSI_B is active low, deassert signal */
+	conf->csi_b = devm_gpiod_get_optional(&pdev->dev, "csi",
+					      GPIOD_OUT_LOW);
+	if (IS_ERR(conf->csi_b))
+		return dev_err_probe(&pdev->dev, PTR_ERR(conf->csi_b),
 				     "Failed to get CSI_B gpio\n");
 
-	/* RDWR_B is active low */
-	gpio = devm_gpiod_get_optional(&pdev->dev, "rdwr", GPIOD_OUT_HIGH);
-	if (IS_ERR(gpio))
-		return dev_err_probe(&pdev->dev, PTR_ERR(gpio),
+	/* RDWR_B is active low, deassert signal */
+	conf->rdwr_b = devm_gpiod_get_optional(&pdev->dev, "rdwr",
+					       GPIOD_OUT_LOW);
+	if (IS_ERR(conf->rdwr_b))
+		return dev_err_probe(&pdev->dev, PTR_ERR(conf->rdwr_b),
 				     "Failed to get RDWR_B gpio\n");
 
 	return xilinx_core_probe(&conf->core);
