fe1a1c7a9fb94dbc625bc322325b896284f3504b
Michael Chan <michael.chan@broadcom.com>
2026-08-09 22:13:46 -0700

    bnxt_en: Set default MPC ring count
    If the firmware supports MPC channels and CONFIG_BNXT_TLS is set, set
    the default number of MPC channels.  These MPC rings will share MSIX
    with the TX rings.  The number of MPC channels for each type must not
    exceed the ethtool TX channel count.  When changing ethtool channels,
    bnxt_calc_dflt_mpc_rings() will determine the default MPC rings for
    the newly requested channel count and we call bnxt_check_rings() to
    check before we commit.  bnxt_set_dflt_mpc_rings() will set the
    committed MPC rings for the new committed channel count.
    
    We also add bnxt_trim_mpc_rings() to make final adjustments in case
    the number of reserved TX channels is less than expected.
    
    Reviewed-by: Ajit Khaparde <ajit.khaparde@broadcom.com>
    Reviewed-by: Andy Gospodarek <andrew.gospodarek@broadcom.com>
    Reviewed-by: Pavan Chebbi <pavan.chebbi@broadcom.com>
    Signed-off-by: Michael Chan <michael.chan@broadcom.com>

--- drivers/net/ethernet/broadcom/bnxt/bnxt_mpc.c
+++ drivers/net/ethernet/broadcom/bnxt/bnxt_mpc.c
@@ -3,6 +3,7 @@
 
 #include <linux/stddef.h>
 #include <linux/types.h>
+#include <linux/kernel.h>
 #include <linux/slab.h>
 #include <linux/bnxt/hsi.h>
 
@@ -44,4 +45,75 @@ int bnxt_mpc_cp_rings_in_use(struct bnxt *bp)
 	if (!mpc)
 		return 0;
 	return mpc->mpc_cp_rings;
+}
+
+void bnxt_trim_mpc_rings(struct bnxt *bp)
+{
+	struct bnxt_mpc_info *mpc = bp->mpc_info;
+	int max = bp->tx_nr_rings_per_tc;
+	u8 max_cp = 0;
+	int i;
+
+	if (!mpc)
+		return;
+
+	for (i = 0; i < BNXT_MPC_TYPE_MAX; i++) {
+		mpc->mpc_ring_count[i] = min_t(int, mpc->mpc_ring_count[i],
+					       max);
+		max_cp = max(max_cp, mpc->mpc_ring_count[i]);
+	}
+	mpc->mpc_cp_rings = max_cp;
+}
+
+void bnxt_calc_dflt_mpc_rings(struct bnxt *bp, int tx_nr_rings,
+			      int tx_nr_rings_per_tc, int rx_nr_rings,
+			      int *mpc_per_type, int *mpc_cp)
+{
+	struct bnxt_hw_resc *hw_resc = &bp->hw_resc;
+	int mpc_tce, avail;
+
+	*mpc_per_type = 0;
+	*mpc_cp = 0;
+
+	if (!BNXT_MPC_CRYPTO_CAPABLE(bp))
+		return;
+
+	avail = hw_resc->max_tx_rings - tx_nr_rings;
+	/* don't use more than 80% */
+	avail = avail * 4 / 5;
+
+	if (avail < (BNXT_MIN_MPC_TCE + BNXT_MIN_MPC_RCE))
+		return;
+
+	mpc_tce = min_t(int, avail / 2, tx_nr_rings_per_tc);
+	mpc_tce = min_t(int, mpc_tce, BNXT_DFLT_MPC_TCE);
+
+	avail = hw_resc->max_cp_rings - tx_nr_rings - rx_nr_rings;
+
+	if (avail < BNXT_MIN_MPC_TCE || avail < BNXT_MIN_MPC_RCE)
+		return;
+
+	mpc_tce = min(mpc_tce, avail);
+
+	/* TCE and RCE are sized equally, so the per-type count is also the
+	 * MPC CP ring count (max(mpc_tce, mpc_rce)).
+	 */
+	*mpc_per_type = mpc_tce;
+	*mpc_cp = mpc_tce;
+}
+
+void bnxt_set_dflt_mpc_rings(struct bnxt *bp)
+{
+	struct bnxt_mpc_info *mpc = bp->mpc_info;
+	int per_type, mpc_cp, i;
+
+	if (!BNXT_MPC_CRYPTO_CAPABLE(bp))
+		return;
+
+	bnxt_calc_dflt_mpc_rings(bp, bp->tx_nr_rings, bp->tx_nr_rings_per_tc,
+				 bp->rx_nr_rings, &per_type, &mpc_cp);
+
+	for (i = 0; i < BNXT_MPC_TYPE_MAX; i++)
+		mpc->mpc_ring_count[i] = per_type;
+	mpc->mpc_cp_rings = mpc_cp;
 }
