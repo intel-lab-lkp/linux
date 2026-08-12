302da2111405d75edc2d48341dc2a32644ea2200
Rafał Miłecki <rafal@milecki.pl>
2026-08-12 15:27:52 +0200

    net: dsa: b53: deal with Northstar devices using CPU port 5 or 7
    Northstar devices have 3 CPU ports. Ports 5 and 7 may be used but have
    their limitations. Warn user when CPU port 8 isn't used and add EAP
    setup workaround to avoid non-functional standalone ports.
    
    Fixes: 4227ea91e265 ("net: dsa: b53: prevent standalone from trying to forward to other ports")
    Cc: Semih Baskan <strst.gs@gmail.com>
    Signed-off-by: Rafał Miłecki <rafal@milecki.pl>
    Tested-by: Semih Baskan <strst.gs@gmail.com>

--- drivers/net/dsa/b53/b53_common.c
+++ drivers/net/dsa/b53/b53_common.c
@@ -332,6 +332,14 @@ static void b53_set_eap_mode(struct b53_device *dev, int port, int mode)
 {
 	u64 eap_conf;
 
+	/*
+	 * On Northstar setting EAP_MODE_SIMPLIFIED with CPU port 5/7 breaks
+	 * standalone ports, see:
+	 * https://lore.kernel.org/netdev/ce4d9b7b-aaf6-4796-94fb-8c3d6a1dcd4d@gmail.com/
+	 */
+	if (is5301x(dev) && !dsa_is_cpu_port(dev->ds, B53_CPU_PORT))
+		return;
+
 	if (is5325(dev) || is5365(dev) || dev->chip_id == BCM5389_DEVICE_ID)
 		return;
 
@@ -1280,6 +1288,10 @@ static int b53_setup(struct dsa_switch *ds)
 	unsigned int port;
 	u16 pvid;
 	int ret;
+
+	/* Discourage users from using port 5 or 7 due to limited features */
+	if (is5301x(dev) && !dsa_is_cpu_port(dev->ds, B53_CPU_PORT))
+		dev_warn(dev->dev, "switch to CPU port 8 to use all switch features\n");
 
 	/* Request bridge PVID untagged when DSA_TAG_PROTO_NONE is set
 	 * which forces the CPU port to be tagged in all VLANs.
