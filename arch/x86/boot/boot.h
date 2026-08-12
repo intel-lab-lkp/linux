intel-lab-lkp/linux

commit 32d0b9898029b7b3c7f161d31f57c4831d9049eb

x86 setup: early cmdline parser handle boolean options
This patch extends the early commandline parser to support boolean options.
The current version in mainline only supports parsing "option=arg" value pairs.

With this it should be easy making other messages like "Uncompressing kernel"
honour the "quiet" parameter, too.

Signed-off-by: Roland Kletzing <devzero@web.de>
Signed-off-by: H. Peter Anvin <hpa@zytor.com>
Signed-off-by: Ingo Molnar <mingo@elte.hu>
Signed-off-by: Thomas Gleixner <tglx@linutronix.de>

arch/x86/boot/cmdline.c
@@ -95,3 +95,68 @@ int cmdline_find_option(const char *option, char *buffer, int bufsize)
 
 	return len;
 }
+
+/*
+ * Find a boolean option (like quiet,noapic,nosmp....)
+ *
+ * Returns the position of that option (starts counting with 1)
+ * or 0 on not found
+ */
+int cmdline_find_option_bool(const char *option)
+{
+	u32 cmdline_ptr = boot_params.hdr.cmd_line_ptr;
+	addr_t cptr;
+	char c;
+	int pos =0 , wstart = 0;
+	const char *opptr = NULL;
+	enum {
+		st_wordstart,	/* Start of word/after whitespace */
+		st_wordcmp,	/* Comparing this word */
+		st_wordskip,	/* Miscompare, skip */
+	} state = st_wordstart;
+
+	if (!cmdline_ptr || cmdline_ptr >= 0x100000)
+		return -1;	/* No command line, or inaccessible */
+
+	cptr = cmdline_ptr & 0xf;
+	set_fs(cmdline_ptr >> 4);
+
+	while (cptr < 0x10000) {
+		c = rdfs8(cptr++);
+		pos++;
+
+		switch (state) {
+		case st_wordstart:
+			if (!c)
+				return 0;
+			else if (myisspace(c))
+				break;
+
+			state = st_wordcmp;
+			opptr = option;
+			wstart = pos;
+			/* fall through */
+
+		case st_wordcmp:
+			if (!*opptr)
+				if (!c || myisspace(c))
+					return wstart;
+				else
+					state = st_wordskip;
+			else if (!c)
+				return 0;
+			else if (c != *opptr++)
+				state = st_wordskip;
+			break;
+
+		case st_wordskip:
+			if (!c)
+				return 0;
+			else if (myisspace(c))
+				state = st_wordstart;
+			break;
+		}
+	}
+
+	return 0;	/* Buffer overrun */
+}

commit cf3f0a561f4f284465b113281137c6394927cce6

x86/cmdline: Return 0 from cmdline_find_option_bool() for no cmdline
cmdline_find_option_bool() is documented to return the position of the
option or 0 when the option is not found. However, the boot-time and
kernel-proper implementations return -1 when there is no command line,
and the real-mode wrapper also returns -1 when the command line is
inaccessible.

That makes normal boolean use of the helper wrong: callers testing the
return value directly treat an unavailable command line as if the option
was present.

Return 0 from cmdline_find_option_bool() for unavailable command lines.

Fixes: 32d0b9898029 ("x86 setup: early cmdline parser handle boolean options")
Signed-off-by: Thorsten Blum <thorsten.blum@linux.dev>

arch/x86/boot/boot.h
@@ -261,7 +261,7 @@ static inline int cmdline_find_option_bool(const char *option)
 	unsigned long cmd_line_ptr = boot_params.hdr.cmd_line_ptr;
 
 	if (cmd_line_ptr >= 0x100000)
-		return -1;      /* inaccessible */
+		return 0;	/* inaccessible */
 
 	return __cmdline_find_option_bool(cmd_line_ptr, option);
 }

arch/x86/boot/cmdline.c
@@ -94,8 +94,8 @@ int __cmdline_find_option(unsigned long cmdline_ptr, const char *option, char *b
 /*
  * Find a boolean option (like quiet,noapic,nosmp....)
  *
- * Returns the position of that option (starts counting with 1)
- * or 0 on not found
+ * Returns the position of that option (starts counting with 1),
+ * or 0 if the option is not found or the command line is unavailable.
  */
 int __cmdline_find_option_bool(unsigned long cmdline_ptr, const char *option)
 {
@@ -110,7 +110,7 @@ int __cmdline_find_option_bool(unsigned long cmdline_ptr, const char *option)
 	} state = st_wordstart;
 
 	if (!cmdline_ptr)
-		return -1;      /* No command line */
+		return 0;	/* No command line */
 
 	cptr = cmdline_ptr & 0xf;
 	set_fs(cmdline_ptr >> 4);

arch/x86/lib/cmdline.c
@@ -23,10 +23,11 @@ static inline int myisspace(u8 c)
  * @max_cmdline_size: the maximum size of cmdline
  * @option: option string to look for
  *
- * Returns the position of that @option (starts counting with 1)
- * or 0 on not found.  @option will only be found if it is found
- * as an entire word in @cmdline.  For instance, if @option="car"
- * then a cmdline which contains "cart" will not match.
+ * Returns the position of that @option (starts counting with 1),
+ * or 0 if the option is not found or the command line is unavailable.
+ * @option will only be found if it is found as an entire word in
+ * @cmdline.  For instance, if @option="car" then a cmdline which
+ * contains "cart" will not match.
  */
 static int
 __cmdline_find_option_bool(const char *cmdline, int max_cmdline_size,
@@ -42,7 +43,7 @@ __cmdline_find_option_bool(const char *cmdline, int max_cmdline_size,
 	} state = st_wordstart;
 
 	if (!cmdline)
-		return -1;      /* No command line */
+		return 0;	/* No command line */
 
 	/*
 	 * This 'pos' check ensures we do not overrun