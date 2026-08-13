5e8198e6b2b9d33a35494b7ca26a5a0d8e646846
Xuanqiang Luo <luoxuanqiang@kylinos.cn>
2026-08-13 21:29:45 +0800

    net: phy: propagate errors from default port setup
    phy_default_setup_single_port() ignores errors from phy_add_port() and
    always reports success. If a PHY driver attach_mdi_port() callback fails,
    the phy_port is leaked and PHY probing continues without the expected
    default port.
    
    Destroy the port and return the error.
    
    Fixes: 589e934d2735 ("net: phy: Introduce PHY ports representation")
    Signed-off-by: Xuanqiang Luo <luoxuanqiang@kylinos.cn>

--- drivers/net/phy/phy_device.c
+++ drivers/net/phy/phy_device.c
@@ -3458,6 +3458,7 @@ static int phy_default_setup_single_port(struct phy_device *phydev)
 {
 	struct phy_port *port = phy_port_alloc();
 	unsigned long mode;
+	int ret;
 
 	if (!port)
 		return -ENOMEM;
@@ -3484,9 +3485,11 @@ static int phy_default_setup_single_port(struct phy_device *phydev)
 		port->pairs = max_t(int, port->pairs,
 				    ethtool_linkmode_n_pairs(mode));
 
-	phy_add_port(phydev, port);
+	ret = phy_add_port(phydev, port);
+	if (ret)
+		phy_port_destroy(port);
 
-	return 0;
+	return ret;
 }
 
 static int of_phy_ports(struct phy_device *phydev)
