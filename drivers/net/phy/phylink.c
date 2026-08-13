01e36c1ac68b5bf07cc136935100a218428b847a
Christian Marangi <ansuelsmth@gmail.com>
2026-08-13 10:35:09 +0200

    net: phylink: save phylink instance fwnode on phylink_create
    The firmware node associated with the phylink instance might differ from
    the device firmware node passed in phylink_config as such device can
    reference the parent switch or other platform peripherals.
    
    Some function might need to reference this firmware node after
    phylink_create so add a dedicated fwnode entry in phylink internal struct
    and save it on phylink_create().
    
    Signed-off-by: Christian Marangi <ansuelsmth@gmail.com>

--- drivers/net/phy/phylink.c
+++ drivers/net/phy/phylink.c
@@ -44,6 +44,7 @@ struct phylink {
 	const struct phylink_mac_ops *mac_ops;
 	struct phylink_config *config;
 	struct phylink_pcs *pcs;
+	struct fwnode_handle *fwnode;
 	struct device *dev;
 	unsigned int old_link_state:1;
 
@@ -1994,6 +1995,7 @@ struct phylink *phylink_create(struct phylink_config *config,
 	INIT_LIST_HEAD(&pl->pcs_list);
 
 	pl->config = config;
+	pl->fwnode = fwnode_handle_get((struct fwnode_handle *)fwnode);
 	if (config->type == PHYLINK_NETDEV) {
 		pl->netdev = to_net_dev(config->dev);
 		netif_carrier_off(pl->netdev);
@@ -2078,6 +2080,7 @@ unlink_pcs_list:
 		pcs->phylink = NULL;
 	}
 free_pl:
+	fwnode_handle_put(pl->fwnode);
 	kfree(pl);
 	return ERR_PTR(ret);
 }
@@ -2107,6 +2110,8 @@ void phylink_destroy(struct phylink *pl)
 		pcs->phylink = NULL;
 		list_del(&pcs->list);
 	}
+
+	fwnode_handle_put(pl->fwnode);
 
 	kfree(pl);
 }
