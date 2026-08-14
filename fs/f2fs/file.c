f492c511b76e38dc66dcdb70e2bebb2c218a7466
Seongjae Jeong <jsjlee1020@gmail.com>
2026-08-14 10:52:34 +0000

    f2fs: fix O_DIRECT cleanup range for append writes
    When an O_DIRECT write falls back to buffered I/O,
    f2fs_flush_buffered_write() uses orig_pos to flush and invalidate
    the page cache.
    
    For O_APPEND writes, f2fs_write_checks() updates iocb->ki_pos to
    the end of the file after orig_pos has been saved. As a result,
    the cleanup range can differ from the actual buffered write range.
    
    Save the write position after f2fs_write_checks() and use it for
    the forced buffered I/O cleanup.
    
    Fixes: 92318f20d703 ("f2fs: preserve direct write semantics when buffering is forced")
    Signed-off-by: Seongjae Jeong <jsjlee1020@gmail.com>

--- fs/f2fs/file.c
+++ fs/f2fs/file.c
@@ -5644,6 +5644,7 @@ static ssize_t f2fs_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 	const loff_t pos = iocb->ki_pos;
 	const ssize_t count = iov_iter_count(from);
 	ssize_t ret;
+	loff_t bufio_start_pos;
 
 	if (unlikely(f2fs_cp_error(F2FS_I_SB(inode)))) {
 		ret = -EIO;
@@ -5673,6 +5674,8 @@ static ssize_t f2fs_file_write_iter(struct kiocb *iocb, struct iov_iter *from)
 	ret = f2fs_write_checks(iocb, from);
 	if (ret <= 0)
 		goto out_unlock;
+
+	bufio_start_pos = iocb->ki_pos;
 
 	/* Determine whether we will do a direct write or a buffered write. */
 	dio = f2fs_should_use_dio(inode, iocb, from);
@@ -5727,8 +5730,8 @@ out:
 	 */
 	if (ret > 0 && !dio && (iocb->ki_flags & IOCB_DIRECT))
 		f2fs_flush_buffered_write(iocb->ki_filp->f_mapping,
-					  orig_pos,
-					  orig_pos + ret - 1);
+					  bufio_start_pos,
+					  bufio_start_pos + ret - 1);
 
 	return ret;
 }
