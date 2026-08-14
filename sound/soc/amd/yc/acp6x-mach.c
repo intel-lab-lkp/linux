4527b3f1cf42eeb13a1ef3f08c6258d49caeb97f
Zhang Heng <zhangheng@kylinos.cn>
2026-08-14 21:14:41 +0800

    ASoC: amd: yc: Add DMI quirk for HP OMEN by HP Gaming Laptop 16-n0xxx
    Add a DMI quirk for the HP OMEN by HP Gaming Laptop 16-n0xxx fixing
    the issue where the internal microphone was not detected.
    
    Closes: https://bugzilla.kernel.org/show_bug.cgi?id=218997
    Signed-off-by: Zhang Heng <zhangheng@kylinos.cn>

--- sound/soc/amd/yc/acp6x-mach.c
+++ sound/soc/amd/yc/acp6x-mach.c
@@ -637,6 +637,13 @@ static const struct dmi_system_id yc_acp_quirk_table[] = {
 		.driver_data = &acp6x_card,
 		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "HP"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "OMEN by HP Gaming Laptop 16-n0xxx"),
+		}
+	},
+	{
+		.driver_data = &acp6x_card,
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "HP"),
 			DMI_MATCH(DMI_PRODUCT_NAME, "Victus by HP Gaming Laptop 15-fb1xxx"),
 		}
 	},
