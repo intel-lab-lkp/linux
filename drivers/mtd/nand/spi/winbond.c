532a28f52b8be62cdabffa72023135594e04daf1
Dominique Martinet <dominique.martinet@atmark-techno.com>
2026-08-14 08:12:11 +0000

    mtd: spinand: winbond: add support for W25N04LW
    Add support for Winbond W25N04LW 4Gbit SPI-NAND:
    > 1.8V 4G-bit
    > Raw serial SLC NAND flash memory
    > Dual/Quad SPI with 104MHz
    > buffer read, continuous read & sequential read
    
    Highlights copied from datasheet:
    – Page size: 4,352 Bytes (4096 + 256 Bytes)
    – Block size: 64 pages (256K + 16K Bytes)
    - Built-in 8-Bit ECC for memory array
    – ECC status bits indicate ECC results
    – Bad Block Management and LUT access
    – Software and Hardware Write-Protect
    – Power Supply Lock-Down and OTP protection
    – Unique ID and Parameter page
    – Ten 4KB OTP pages per die
    – Read Level Setting for Read Retry
    
    The 4096+256 byte page layout is as follow:
    - 4096 bytes ECC protected "main memory array", subdivided in 8
      512 bytes sectors
    - 128 bytes spare array, subdivided in 8 16 bytes sectors as follow:
      - 2 bytes bad block marker (no ECC protection)
      - 2 bytes "user data II" (no ECC protection)
      - 12 bytes "user data I" (ECC protected)
    - 128 bytes parity array, subdivided in 8 16 bytes sectors:
      - 13 bytes ECC for main data and user data
      - 3 unused bytes
    
    This commit does not support most advanced features such as continuous
    read, LUT management and OTP pages, and only provides basic usage.
    
    OOB layout was kept compatible with previous models (exposing both user
    data I and II), dependent on mtd->oobsize.
    
    Co-developed-by: Daisuke Mizobuchi <mizo@atmark-techno.com>
    Signed-off-by: Daisuke Mizobuchi <mizo@atmark-techno.com>
    Signed-off-by: Dominique Martinet <dominique.martinet@atmark-techno.com>

--- drivers/mtd/nand/spi/winbond.c
+++ drivers/mtd/nand/spi/winbond.c
@@ -314,10 +314,11 @@ static int w25n01kv_ooblayout_ecc(struct mtd_info *mtd, int section,
 static int w25n02kv_ooblayout_ecc(struct mtd_info *mtd, int section,
 				  struct mtd_oob_region *region)
 {
-	if (section > 3)
+	/* 4 sections for 128 bytes, 8 for 256 */
+	if (section >= mtd->oobsize / 32)
 		return -ERANGE;
 
-	region->offset = 64 + (16 * section);
+	region->offset = (mtd->oobsize / 2) + (16 * section);
 	region->length = 13;
 
 	return 0;
@@ -326,9 +327,16 @@ static int w25n02kv_ooblayout_ecc(struct mtd_info *mtd, int section,
 static int w25n02kv_ooblayout_free(struct mtd_info *mtd, int section,
 				   struct mtd_oob_region *region)
 {
-	if (section > 3)
+	if (section >= mtd->oobsize / 32)
 		return -ERANGE;
 
+	/*
+	 * For at least W25N04KW and W25N04LW, this region is actually
+	 * split in two:
+	 * ECC-protected "User Data I" at + 4 offset, length 12
+	 * unprotected "User Data II" at + 2 offset, length 2
+	 * This returns the full region for backwards compatibility.
+	 */
 	region->offset = (16 * section) + 2;
 	region->length = 14;
 
@@ -762,6 +770,15 @@ static const struct spinand_info winbond_spinand_table[] = {
 	SPINAND_INFO("W25N04KW", /* 1.8V */
 		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_DUMMY, 0xba, 0x23),
 		     NAND_MEMORG(1, 2048, 128, 64, 4096, 40, 1, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     0,
+		     SPINAND_ECCINFO(&w25n02kv_ooblayout, w25n02kv_ecc_get_status)),
+	SPINAND_INFO("W25N04LW", /* 1.8V */
+		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_DUMMY, 0xb2, 0x23),
+		     NAND_MEMORG(1, 4096, 256, 64, 2048, 40, 1, 1, 1),
 		     NAND_ECCREQ(8, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
 					      &write_cache_variants,
