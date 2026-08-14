88663088be9807da0e3f1e66c8afe64c6794b050
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2026-08-14 16:48:53 +0200

    vsprintf: Don't leak pointers for %ps without KALLSYMS enabled
    The "%ps" format modifier prints the name of the symbol which is more
    valuable in terms of debugging and does not leak the actual pointer.
    
    Without KALLSYMS it will leak the pointer which is not intended. The
    default policy for pointers is to print a hashed value and not to leak
    the actual pointer.
    
    Print "(unknown)" for any symbol resolution witout KALLSYMS enabled.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- lib/vsprintf.c
+++ lib/vsprintf.c
@@ -1008,7 +1008,7 @@ char *symbol_string(char *buf, char *end, void *ptr,
 
 	return string_nocheck(buf, end, sym, spec);
 #else
-	return special_hex_number(buf, end, value, sizeof(void *));
+	return string_nocheck(buf, end, "(unknown)", spec);
 #endif
 }
 
