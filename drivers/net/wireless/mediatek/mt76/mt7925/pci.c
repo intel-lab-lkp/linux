dfe09f311984042e3d89ff7408653fa1d7cbeb66
Eason Lai <eason.lai@mediatek.com>
2026-08-17 13:24:42 +0800

    wifi: mt76: mt7925: Fix MCU command timeouts during module unload
    Move MT76_REMOVED flag setting from mt7925_pci_remove() to after
    mt76_unregister_device() completes. Setting it too early causes
    mt792x_irq_handler() to drop MCU completion interrupts, timing out
    teardown commands (scan abort, BSS_INFO_UPDATE, etc).
    
    The flag must still be set before napi_disable() to prevent reset_work
    from re-enabling NAPI and causing deadlock. Also guard mt792x_reset()
    to skip queuing work when device is removed.
    
    With nothing associated it still costs 9 to 10 seconds, so a bare rmmod
    does reach this, just small enough there to read as normal teardown.
    
    Fixes: 896f4dfd68e1 ("wifi: mt76: mt7925: Fix unregister deadlock")
    
    Tested-by: Devin Wittmayer <lucid_duck@justthetip.ca>
    Signed-off-by: Eason Lai <eason.lai@mediatek.com>

--- drivers/net/wireless/mediatek/mt76/mt7925/pci.c
+++ drivers/net/wireless/mediatek/mt76/mt7925/pci.c
@@ -47,9 +47,10 @@ static void mt7925e_unregister_device(struct mt792x_dev *dev)
 	if (dev->phy.chip_cap & MT792x_CHIP_CAP_WF_RF_PIN_CTRL_EVT_EN)
 		wiphy_rfkill_stop_polling(hw->wiphy);
 
-	cancel_work_sync(&dev->reset_work);
 	cancel_work_sync(&dev->init_work);
 	mt76_unregister_device(&dev->mt76);
+	set_bit(MT76_REMOVED, &dev->mphy.state);
+	cancel_work_sync(&dev->reset_work);
 	mt76_for_each_q_rx(&dev->mt76, i)
 		napi_disable(&dev->mt76.napi[i]);
 	cancel_delayed_work_sync(&pm->ps_work);
@@ -725,7 +726,6 @@ static void mt7925_pci_remove(struct pci_dev *pdev)
 	struct mt76_dev *mdev = pci_get_drvdata(pdev);
 	struct mt792x_dev *dev = container_of(mdev, struct mt792x_dev, mt76);
 
-	set_bit(MT76_REMOVED, &mdev->phy.state);
 	mt7925e_unregister_device(dev);
 	devm_free_irq(&pdev->dev, pdev->irq, dev);
 	mt76_free_device(&dev->mt76);
