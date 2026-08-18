d996e509632dd920193af762886fdff77720bab6
Linmao Li <lilinmao@kylinos.cn>
2026-08-18 17:39:18 +0800

    serial: 8250_mxpcie: take the line settings from the new termios
    mxpcie8250_set_termios() reads the line settings out of
    port->state->port.tty, which is only set once the port has been opened.
    
    uart_set_options() builds a termios of its own and calls ->set_termios()
    with no tty behind it, so using such a board as the console
    (console=ttyS<n>) dereferences a NULL tty during console setup, as does
    attaching kgdboc to it and resuming a suspended console from
    uart_resume_port().
    
    Read the settings from the termios the serial core passes in instead.
    It holds the same values on the normal path - uart_change_line_settings()
    passes &tty->termios - and it is what serial8250_do_set_termios() right
    above already uses.
    
    Fixes: 55edf8511f47 ("serial: 8250_mxpcie: enable automatic RTS/CTS flow control")
    Signed-off-by: Linmao Li <lilinmao@kylinos.cn>

--- drivers/tty/serial/8250/8250_mxpcie.c
+++ drivers/tty/serial/8250/8250_mxpcie.c
@@ -217,8 +217,7 @@ static void mxpcie8250_set_termios(struct uart_port *port,
 				   const struct ktermios *old)
 {
 	struct uart_8250_port *up = up_to_u8250p(port);
-	struct tty_struct *tty = port->state->port.tty;
-	unsigned int cflag = tty->termios.c_cflag;
+	unsigned int cflag = new->c_cflag;
 	u8 efr, val;
 
 	serial8250_do_set_termios(port, new, old);
@@ -233,18 +232,20 @@ static void mxpcie8250_set_termios(struct uart_port *port,
 		up->port.status |= (UPSTAT_AUTORTS | UPSTAT_AUTOCTS);
 	}
 	/* Set on-chip software flow control character */
-	serial_out(up, MOXA_PUART_XON1, START_CHAR(tty));
-	serial_out(up, MOXA_PUART_XON2, START_CHAR(tty));
-	serial_out(up, MOXA_PUART_XOFF1, STOP_CHAR(tty));
-	serial_out(up, MOXA_PUART_XOFF2, STOP_CHAR(tty));
+	serial_out(up, MOXA_PUART_XON1, new->c_cc[VSTART]);
+	serial_out(up, MOXA_PUART_XON2, new->c_cc[VSTART]);
+	serial_out(up, MOXA_PUART_XOFF1, new->c_cc[VSTOP]);
+	serial_out(up, MOXA_PUART_XOFF2, new->c_cc[VSTOP]);
 
-	val = I_IXON(tty) ? MOXA_PUART_EFR_RX_FLOW_XON1_XOFF1 : MOXA_PUART_EFR_RX_FLOW_DISABLED;
+	val = (new->c_iflag & IXON) ? MOXA_PUART_EFR_RX_FLOW_XON1_XOFF1 :
+				      MOXA_PUART_EFR_RX_FLOW_DISABLED;
 	FIELD_MODIFY(MOXA_PUART_EFR_RX_FLOW_MASK, &efr, val);
 
-	val = I_IXOFF(tty) ? MOXA_PUART_EFR_TX_FLOW_XON1_XOFF1 : MOXA_PUART_EFR_TX_FLOW_DISABLED;
+	val = (new->c_iflag & IXOFF) ? MOXA_PUART_EFR_TX_FLOW_XON1_XOFF1 :
+				       MOXA_PUART_EFR_TX_FLOW_DISABLED;
 	FIELD_MODIFY(MOXA_PUART_EFR_TX_FLOW_MASK, &efr, val);
 
-	if (I_IXOFF(tty))
+	if (new->c_iflag & IXOFF)
 		up->port.status |= UPSTAT_AUTOXOFF;
 
 	serial_out(up, MOXA_PUART_EFR, efr);
