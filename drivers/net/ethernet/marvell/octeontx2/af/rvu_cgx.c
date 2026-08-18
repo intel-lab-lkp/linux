6e1bb1e2b73458da0869c436b2cc6a7f7edb7bcf
Sai Krishna <saikrishnag@marvell.com>
2026-08-18 09:17:28 +0530

    octeontx2: fix PTP RX enable mbox failure on VF
    When a VF enables PTP RX timestamping, the AF must notify the parent PF
    so it can adjust its RX data offset before CGX prepends the 8-byte
    timestamp header. Use the correct upcall mailbox pattern (wait for zero,
    then msg_send_up) instead of msg_send/wait_for_rsp, which causes the
    mbox to fail on VF.
    
    Add the CGX_PTP_RX_INFO upcall message and PF/VF handlers to propagate
    the PTP enable state to VFs.
    
    Fixes: 30e7ecb6441d ("octeontx2-af: Support for PTP notification to PF")
    Signed-off-by: Sai Krishna <saikrishnag@marvell.com>
    Signed-off-by: Ratheesh Kannoth <rkannoth@marvell.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_cgx.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_cgx.c
@@ -820,6 +820,24 @@ int rvu_mbox_handler_cgx_promisc_disable(struct rvu *rvu, struct msg_req *req,
 	return 0;
 }
 
+static void cgx_notify_up_ptp_info(struct rvu *rvu, int pf, bool enable)
+{
+	struct cgx_ptp_rx_info_msg *msg;
+
+	/* Send mbox message to PF */
+	msg = otx2_mbox_alloc_msg_cgx_ptp_rx_info(rvu, pf);
+	if (!msg) {
+		dev_err(rvu->dev, "failed to alloc message\n");
+		return;
+	}
+
+	msg->ptp_en = enable;
+
+	otx2_mbox_wait_for_zero(&rvu->afpf_wq_info.mbox_up, pf);
+
+	otx2_mbox_msg_send_up(&rvu->afpf_wq_info.mbox_up, pf);
+}
+
 static int rvu_cgx_ptp_rx_cfg(struct rvu *rvu, u16 pcifunc, bool enable)
 {
 	struct rvu_pfvf *pfvf = rvu_get_pfvf(rvu, pcifunc);
@@ -836,6 +854,8 @@ static int rvu_cgx_ptp_rx_cfg(struct rvu *rvu, u16 pcifunc, bool enable)
 	 */
 	if (!is_pf_cgxmapped(rvu, pf))
 		return -EPERM;
+
+	cgx_notify_up_ptp_info(rvu, pf, enable);
 
 	rvu_get_cgx_lmac_id(rvu->pf2cgxlmac_map[pf], &cgx_id, &lmac_id);
 	cgxd = rvu_cgx_pdata(cgx_id, rvu);
