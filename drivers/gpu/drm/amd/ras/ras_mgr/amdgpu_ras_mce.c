intel-lab-lkp/linux

commit 1c46b69712603e5f0a0978200bbbb382be6c9fa9

drm/amd/ras: Isolate ras mce handler with CONFIG_X86_MCE_AMD
Isolate ras mce handler with CONFIG_X86_MCE_AMD.

v2:
  Isolate all ras mce handlers.

Signed-off-by: YiPeng Chai <YiPeng.Chai@amd.com>
Reviewed-by: Hawking Zhang <Hawking.Zhang@amd.com>
Signed-off-by: Alex Deucher <alexander.deucher@amd.com>

drivers/gpu/drm/amd/ras/ras_mgr/amdgpu_ras_mce.c
@@ -27,6 +27,9 @@
 #include "amdgpu_ras_mce.h"
 #include <asm/mce.h>
 
+#ifdef CONFIG_X86_MCE_AMD
+#include <asm/mce.h>
+
 static int amdgpu_sys_mce_notifier(struct notifier_block *nb, unsigned long val, void *data);
 typedef int (*mce_notifier)(struct amdgpu_device *dev,
 			unsigned int id, unsigned long val, void *data);
@@ -232,6 +235,7 @@ static int amdgpu_ras_mce_notifier(struct amdgpu_device *adev,
 
 	return 0;
 }
+#endif
 
 int amdgpu_ras_mce_hw_init(struct amdgpu_device *adev)
 {
@@ -245,12 +249,16 @@ int amdgpu_ras_mce_hw_fini(struct amdgpu_device *adev)
 
 int amdgpu_ras_mce_sw_init(struct amdgpu_device *adev)
 {
+#ifdef CONFIG_X86_MCE_AMD
 	amdgpu_ras_register_mce_notifier(adev, amdgpu_ras_mce_notifier);
+#endif
 	return 0;
 }
 
 int amdgpu_ras_mce_sw_fini(struct amdgpu_device *adev)
 {
+#ifdef CONFIG_X86_MCE_AMD
 	amdgpu_ras_unregister_mce_notifier(adev);
+#endif
 	return 0;
 }

commit 11d6c2622f5c23c418b8710295409204103dbd8c

drm/amd/ras: Remove unguarded asm/mce.h include
asm/mce.h is x86-only, so the unconditional include broke non-x86
builds with "fatal error: asm/mce.h: No such file or directory". The
header is already included under CONFIG_X86_MCE_AMD with the code that
uses it, so remove the redundant unguarded copy.

Fixes: 1c46b6971260 ("drm/amd/ras: Isolate ras mce handler with CONFIG_X86_MCE_AMD")

Signed-off-by: Tony Qi <Haoming.Qi@amd.com>
Reviewed-by: Alex Deucher <alexander.deucher@amd.com>

drivers/gpu/drm/amd/ras/ras_mgr/amdgpu_ras_mce.c
@@ -25,7 +25,6 @@
 #include "amdgpu.h"
 #include "amdgpu_ras_mgr.h"
 #include "amdgpu_ras_mce.h"
-#include <asm/mce.h>
 
 #ifdef CONFIG_X86_MCE_AMD
 #include <asm/mce.h>