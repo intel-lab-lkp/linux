ecdda9f2d29112c8d68b9a6e1cf646a1e4c62a01
Juergen Gross <jgross@suse.com>
2026-08-19 12:23:12 +0200

    x86/msr: Remove rdmsr()
    rdmsr() has no users left. Delete it.
    
    Signed-off-by: Juergen Gross <jgross@suse.com>

--- arch/x86/include/asm/paravirt.h
+++ arch/x86/include/asm/paravirt.h
@@ -150,13 +150,6 @@ static inline int paravirt_write_msr_safe(u32 msr, u64 val)
 	return PVOP_CALL2(int, pv_ops, cpu.write_msr_safe, msr, val);
 }
 
-#define rdmsr(msr, val1, val2)			\
-do {						\
-	u64 _l = paravirt_read_msr(msr);	\
-	val1 = (u32)_l;				\
-	val2 = _l >> 32;			\
-} while (0)
-
 #define rdmsrq(msr, val)			\
 do {						\
 	val = paravirt_read_msr(msr);		\
