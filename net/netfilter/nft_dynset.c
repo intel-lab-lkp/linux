56ab2acaa68ef25300ae2037395fab83158aa9d4
Pablo Neira Ayuso <pablo@netfilter.org>
2026-08-19 15:09:21 +0200

    netfilter: nf_tables: skip double clone set expressions on element insert
    Both the dynset and newsetelem path clone the existing set expressions
    when setting set element expressions if no override expressions are
    provided. This results in a double clone, once to clone the template set
    expressions then another clone on the new element. Add a flag to
    annotate if userspace provides a override expression (ie. expression of
    the same type of the set but different configuration), otherwise borrow
    the existing expression from the set. Add conditionals to release
    expression iif they represent an override.
    
    This simplifies the existing logic and it also fixes a bug with the
    connlimit expression which results in a module refcount imbalance
    WARNING splat when resorting on the default set expressions.
    
    Fixes: 65038428b2c6 ("netfilter: nf_tables: allow to specify stateful expression in set definition")
    Fixes: fca05d4d61e6 ("netfilter: nft_dynset: honor stateful expressions in set definition")
    Reported-by: Xingyuan Mo <hdthky0@gmail.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- net/netfilter/nft_dynset.c
+++ net/netfilter/nft_dynset.c
@@ -19,7 +19,8 @@ struct nft_dynset {
 	u8				sreg_key;
 	u8				sreg_data;
 	bool				invert;
-	bool				expr;
+	bool				expr:1,
+					override_exprs:1;
 	u8				num_exprs;
 	u64				timeout;
 	struct nft_expr			*expr_array[NFT_SET_EXPR_MAX];
@@ -264,6 +265,7 @@ static int nft_dynset_init(const struct nft_ctx *ctx,
 			err = -EOPNOTSUPP;
 			goto err_expr_free;
 		}
+		priv->override_exprs = true;
 	} else if (tb[NFTA_DYNSET_EXPRESSIONS]) {
 		struct nft_expr *dynset_expr;
 		struct nlattr *tmp;
@@ -306,10 +308,10 @@ static int nft_dynset_init(const struct nft_ctx *ctx,
 			err = -EOPNOTSUPP;
 			goto err_expr_free;
 		}
+		priv->override_exprs = true;
 	} else if (set->num_exprs > 0) {
-		err = nft_set_elem_expr_clone(ctx, set, priv->expr_array);
-		if (err < 0)
-			return err;
+		for (i = 0; i < set->num_exprs; i++)
+			priv->expr_array[i] = set->exprs[i];
 
 		priv->num_exprs = set->num_exprs;
 	}
@@ -339,8 +341,10 @@ static int nft_dynset_init(const struct nft_ctx *ctx,
 	return 0;
 
 err_expr_free:
-	for (i = 0; i < priv->num_exprs; i++)
-		nft_expr_destroy(ctx, priv->expr_array[i]);
+	if (priv->override_exprs) {
+		for (i = 0; i < priv->num_exprs; i++)
+			nft_expr_destroy(ctx, priv->expr_array[i]);
+	}
 	return err;
 }
 
@@ -367,9 +371,10 @@ static void nft_dynset_destroy(const struct nft_ctx *ctx,
 	struct nft_dynset *priv = nft_expr_priv(expr);
 	int i;
 
-	for (i = 0; i < priv->num_exprs; i++)
-		nft_expr_destroy(ctx, priv->expr_array[i]);
-
+	if (priv->override_exprs) {
+		for (i = 0; i < priv->num_exprs; i++)
+			nft_expr_destroy(ctx, priv->expr_array[i]);
+	}
 	nf_tables_destroy_set(ctx, priv->set);
 }
 
