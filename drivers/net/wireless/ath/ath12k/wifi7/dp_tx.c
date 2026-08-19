498d78ac78aa113fb2b395b71a826687b98db5ba
Hariharan Ramanathan <hariharan.ramanathan@oss.qualcomm.com>
2026-08-19 11:02:41 +0530

    wifi: ath12k: rename wbm_status to htt_status in HTT TX completion
    The variable wbm_status in ath12k_dp_tx_process_htt_tx_complete()
    holds a value from the HTT TX WBM completion status field, not a
    generic WBM status. Rename it to htt_status to accurately reflect
    the field origin and improve code clarity.
    
    Also fix ts.acked assignment inside the HAL_WBM_REL_HTT_TX_COMP_STATUS_OK
    case to use true directly since the condition is always true within
    that case branch.
    
    Tested-on: QCN9274 hw2.0 PCI WLAN.WBE.1.6.r1-00402-QCAHKSWPL_SILICONZ-1
    
    Signed-off-by: Hariharan Ramanathan <hariharan.ramanathan@oss.qualcomm.com>
    Co-developed-by: Pardeep Kaur <pardeep.kaur@oss.qualcomm.com>
    Signed-off-by: Pardeep Kaur <pardeep.kaur@oss.qualcomm.com>

--- drivers/net/wireless/ath/ath12k/wifi7/dp_tx.c
+++ drivers/net/wireless/ath/ath12k/wifi7/dp_tx.c
@@ -575,19 +575,19 @@ ath12k_dp_tx_process_htt_tx_complete(struct ath12k_dp *dp, void *desc,
 {
 	struct htt_tx_wbm_completion *status_desc;
 	struct ath12k_dp_htt_wbm_tx_status ts = {};
-	enum hal_wbm_htt_tx_comp_status wbm_status;
+	enum hal_wbm_htt_tx_comp_status htt_status;
 	u16 peer_id;
 
 	status_desc = desc;
 
-	wbm_status = le32_get_bits(status_desc->info0,
+	htt_status = le32_get_bits(status_desc->info0,
 				   HTT_TX_WBM_COMP_INFO0_STATUS);
-	if (likely(wbm_status < MAX_FW_TX_STATUS))
-		dp->device_stats.fw_tx_status[wbm_status]++;
+	if (likely(htt_status < MAX_FW_TX_STATUS))
+		dp->device_stats.fw_tx_status[htt_status]++;
 
-	switch (wbm_status) {
+	switch (htt_status) {
 	case HAL_WBM_REL_HTT_TX_COMP_STATUS_OK:
-		ts.acked = (wbm_status == HAL_WBM_REL_HTT_TX_COMP_STATUS_OK);
+		ts.acked = true;
 		ts.ack_rssi = le32_get_bits(status_desc->info2,
 					    HTT_TX_WBM_COMP_INFO2_ACK_RSSI);
 
@@ -609,7 +609,7 @@ ath12k_dp_tx_process_htt_tx_complete(struct ath12k_dp *dp, void *desc,
 		 */
 		break;
 	default:
-		ath12k_warn(dp->ab, "Unknown htt wbm tx status %d\n", wbm_status);
+		ath12k_warn(dp->ab, "Unknown htt tx status %d\n", htt_status);
 		break;
 	}
 }
