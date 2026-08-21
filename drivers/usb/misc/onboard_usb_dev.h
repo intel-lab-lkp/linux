f0dc70e41443d631c0adaa2b4a9434e6f6fe7927
Fabrice Gasnier <fabrice.gasnier@foss.st.com>
2026-08-21 16:33:14 +0200

    usb: misc: onboard-hub: add support for Microchip USB2422
    Add support for the Microchip USB2422 USB2.0 Hub. It is similar to
    the USB2412. It provides two downstream ports. It is supplied by
    a single 3v3 power source, and provides a reset pin. So add the
    generic compatible, defined as "usbVID,PID", and reuse existing
    usb424_data.
    This hub can be found on stm32mp257f-dk board.
    
    Signed-off-by: Fabrice Gasnier <fabrice.gasnier@foss.st.com>

--- drivers/usb/misc/onboard_usb_dev.h
+++ drivers/usb/misc/onboard_usb_dev.h
@@ -137,6 +137,7 @@ static const struct onboard_dev_pdata xmos_xvf3500_data = {
 static const struct of_device_id onboard_dev_match[] = {
 	{ .compatible = "usb-a-connector", .data = &usb_a_conn_data, },
 	{ .compatible = "usb424,2412", .data = &microchip_usb424_data, },
+	{ .compatible = "usb424,2422", .data = &microchip_usb424_data, },
 	{ .compatible = "usb424,2514", .data = &microchip_usb2514_data, },
 	{ .compatible = "usb424,2517", .data = &microchip_usb424_data, },
 	{ .compatible = "usb424,2744", .data = &microchip_usb5744_data, },
