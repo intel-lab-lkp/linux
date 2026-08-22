4fa9a9140332b55ba37adf6500284c6d1b7d3a42
Keke Ming <ming.jvle@gmail.com>
2026-08-22 23:09:06 +0800

    uprobes: Free utask on dup_return_instance() failure
    dup_utask() installs the new uprobe_task in t->utask before copying
    return_instances. If dup_return_instance() fails, the partially copied
    utask is left attached to the child task.
    
    Free the partially copied utask before returning -ENOMEM.
    
    Also, dup_return_instance() copies the return_instance before fixing
    up extra_consumers.
    
    Here, if no deep copy is needed, clear the copied extra_consumers
    pointer.
    
    Signed-off-by: Keke Ming <ming.jvle@gmail.com>

--- kernel/events/uprobes.c
+++ kernel/events/uprobes.c
@@ -2125,6 +2125,8 @@ static struct return_instance *dup_return_instance(struct return_instance *old)
 			kfree(ri);
 			return NULL;
 		}
+	} else {
+		ri->extra_consumers = NULL;
 	}
 
 	return ri;
@@ -2147,8 +2149,10 @@ static int dup_utask(struct task_struct *t, struct uprobe_task *o_utask)
 	p = &n_utask->return_instances;
 	for (o = o_utask->return_instances; o; o = o->next) {
 		n = dup_return_instance(o);
-		if (!n)
+		if (!n) {
+			uprobe_free_utask(t);
 			return -ENOMEM;
+		}
 
 		/* if uprobe is non-NULL, we'll have an extra refcount for uprobe */
 		uprobe = hprobe_expire(&o->hprobe, true);
