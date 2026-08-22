5a9a93bdc629c8f906cb6c5e9ff9660b6243ede5
Joshua Crofts <joshua.crofts1@gmail.com>
2026-08-22 10:02:29 +0200

    iio: temperature: iqs620at-temp: make headers conform to IWYU
    Remove the catch-all kernel.h header and add the missing headers to
    enforce IWYU.
    
    Signed-off-by: Joshua Crofts <joshua.crofts1@gmail.com>

--- drivers/iio/temperature/iqs620at-temp.c
+++ drivers/iio/temperature/iqs620at-temp.c
@@ -5,12 +5,16 @@
  * Copyright (C) 2019 Jeff LaBundy <jeff@labundy.com>
  */
 
+#include <linux/array_size.h>
+#include <linux/bits.h>
 #include <linux/device.h>
-#include <linux/kernel.h>
 #include <linux/mfd/iqs62x.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
+#include <linux/types.h>
+
+#include <asm/byteorder.h>
 
 #include <linux/iio/iio.h>
 
