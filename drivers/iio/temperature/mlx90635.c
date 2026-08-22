e2b098a932aadb404c2ef366e2c812ec700d2a9f
Joshua Crofts <joshua.crofts1@gmail.com>
2026-08-22 10:02:35 +0200

    iio: temperature: mlx90635: make headers conform to IWYU
    Remove the catch-all kernel.h header and add the missing headers to
    enforce IWYU.
    
    Signed-off-by: Joshua Crofts <joshua.crofts1@gmail.com>

--- drivers/iio/temperature/mlx90635.c
+++ drivers/iio/temperature/mlx90635.c
@@ -6,7 +6,10 @@
  *
  * Driver for the Melexis MLX90635 I2C 16-bit IR thermopile sensor
  */
+
+#include <linux/array_size.h>
 #include <linux/bitfield.h>
+#include <linux/bits.h>
 #include <linux/delay.h>
 #include <linux/device.h>
 #include <linux/err.h>
@@ -14,13 +17,14 @@
 #include <linux/i2c.h>
 #include <linux/iopoll.h>
 #include <linux/jiffies.h>
-#include <linux/kernel.h>
 #include <linux/limits.h>
 #include <linux/math64.h>
 #include <linux/module.h>
+#include <linux/mutex.h>
 #include <linux/pm_runtime.h>
 #include <linux/regmap.h>
 #include <linux/regulator/consumer.h>
+#include <linux/types.h>
 
 #include <linux/iio/iio.h>
 
