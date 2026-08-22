intel-lab-lkp/linux

commit babcfade47371eea81fd7f24d892b5ff5b1786ea

[PATCH] pc8736x_gpio: fix re-modprobe errors: fix/finish cdev-init
- Switch from register_chrdev() to   (register|alloc)_chrdev_region().

- use a cdev.  This was intended for original patchset, but was
  overlooked.

  We use a single cdev for all pins (minor device-numbers), as gleaned
  from cs5535_gpio, and in contrast to whats currently done in scx200_gpio
  (which I'll fix soon)

Signed-off-by: Jim Cromie <jim.cromie@gmail.com>
Signed-off-by: Andrew Morton <akpm@osdl.org>
Signed-off-by: Linus Torvalds <torvalds@osdl.org>

drivers/char/pc8736x_gpio.c
@@ -259,7 +259,8 @@ static struct cdev pc8736x_gpio_cdev;
 
 static int __init pc8736x_gpio_init(void)
 {
-	int rc = 0;
+	int rc;
+	dev_t devid;
 
 	pdev = platform_device_alloc(DEVNAME, 0);
 	if (!pdev)
@@ -307,7 +308,14 @@ static int __init pc8736x_gpio_init(void)
 	}
 	dev_info(&pdev->dev, "GPIO ioport %x reserved\n", pc8736x_gpio_base);
 
-	rc = register_chrdev(major, DEVNAME, &pc8736x_gpio_fops);
+	if (major) {
+		devid = MKDEV(major, 0);
+		rc = register_chrdev_region(devid, PC8736X_GPIO_CT, DEVNAME);
+	} else {
+		rc = alloc_chrdev_region(&devid, 0, PC8736X_GPIO_CT, DEVNAME);
+		major = MAJOR(devid);
+	}
+
 	if (rc < 0) {
 		dev_err(&pdev->dev, "register-chrdev failed: %d\n", rc);
 		goto undo_request_region;
@@ -318,6 +326,11 @@ static int __init pc8736x_gpio_init(void)
 	}
 
 	pc8736x_init_shadow();
+
+	/* ignore minor errs, and succeed */
+	cdev_init(&pc8736x_gpio_cdev, &pc8736x_gpio_fops);
+	cdev_add(&pc8736x_gpio_cdev, devid, PC8736X_GPIO_CT);
+
 	return 0;
 
 undo_request_region:

commit b85c8eee65ec76256c774c3c05e59d5dc339cdca

char: pc8736x_gpio: check cdev_add() return value
cdev_add() can fail, but the driver ignored the return value and still
reported success. That leaves a registered chrdev region without usable
file operations.

Check the return value, report the error, and unwind the chrdev region
and I/O region on failure.

Fixes: babcfade4737 ("[PATCH] pc8736x_gpio: fix re-modprobe errors: fix/finish cdev-init")
Signed-off-by: Linkai Gong <gonglinkai@kylinos.cn>

drivers/char/pc8736x_gpio.c
@@ -321,12 +321,17 @@ static int __init pc8736x_gpio_init(void)
 
 	pc8736x_init_shadow();
 
-	/* ignore minor errs, and succeed */
 	cdev_init(&pc8736x_gpio_cdev, &pc8736x_gpio_fileops);
-	cdev_add(&pc8736x_gpio_cdev, devid, PC8736X_GPIO_CT);
+	rc = cdev_add(&pc8736x_gpio_cdev, devid, PC8736X_GPIO_CT);
+	if (rc) {
+		dev_err(&pdev->dev, "cdev_add failed: %d\n", rc);
+		goto undo_chrdev_region;
+	}
 
 	return 0;
 
+undo_chrdev_region:
+	unregister_chrdev_region(devid, PC8736X_GPIO_CT);
 undo_request_region:
 	release_region(pc8736x_gpio_base, PC8736X_GPIO_RANGE);
 undo_platform_dev_add: