7a093a5fc8c3880d1c123de6cd5a7b453c6fe26d
Vernon Yang <yanglincheng@kylinos.cn>
2026-08-24 17:29:34 +0800

    mm: khugepaged: fix folio is used after pte_unmap_unlock()
    After the page table lock has dropped, the folio can be freed
    concurrently. The trace_mm_khugepaged_scan_pmd() is left with
    a dangling folio pointer.
    
    So using the folio_pfn() before dropping the page table lock,
    closing use-after-free window.
    
    Fixes: 7d2eba0557c1 ("mm: add tracepoint for scanning pages")
    Cc: stable@vger.kernel.org
    Signed-off-by: Vernon Yang <yanglincheng@kylinos.cn>

--- mm/khugepaged.c
+++ mm/khugepaged.c
@@ -1618,6 +1618,7 @@ static enum scan_result collapse_scan_pmd(struct mm_struct *mm,
 	enum scan_result result = SCAN_FAIL;
 	struct page *page = NULL;
 	struct folio *folio = NULL;
+	unsigned long pfn = -1;
 	unsigned long addr;
 	unsigned long enabled_orders;
 	spinlock_t *ptl;
@@ -1780,6 +1781,8 @@ static enum scan_result collapse_scan_pmd(struct mm_struct *mm,
 		result = SCAN_SUCCEED;
 	}
 out_unmap:
+	if (folio)
+		pfn = folio_pfn(folio);
 	pte_unmap_unlock(pte, ptl);
 	if (result == SCAN_SUCCEED) {
 		/* collapse_huge_page() expects the lock to be dropped before calling */
@@ -1790,7 +1793,7 @@ out_unmap:
 		*lock_dropped = true;
 	}
 out:
-	trace_mm_khugepaged_scan_pmd(mm, folio, referenced,
+	trace_mm_khugepaged_scan_pmd(mm, pfn, referenced,
 				     none_or_zero, result, unmapped);
 	return result;
 }
