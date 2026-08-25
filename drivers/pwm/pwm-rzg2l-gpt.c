7bbd8c12f73802b8fbe85ebced2d4caf87f7e689
Biju Das <biju.das.jz@bp.renesas.com>
2026-08-25 16:48:33 +0100

    pwm: rzg2l-gpt: Add calculate_prescale() callback to struct rzg2l_gpt_info
    The RZ/G2L GPT prescaler steps are continuous powers of 4, while RZ/G3E
    uses powers of 2 but with a discontinuous sequence. Add a
    calculate_prescale function pointer to struct rzg2l_gpt_info to allow
    per-SoC prescaler selection logic. Replace the direct call to
    rzg2l_gpt_calculate_prescale() in rzg2l_gpt_round_waveform_tohw() with an
    indirect call through info->calculate_prescale(). Wire the existing
    rzg2l_gpt_calculate_prescale() into rzg2l_data to preserve current RZ/G2L
    behaviour.
    
    Reviewed-by: Tommaso Merciai <tommaso.merciai.xr@bp.renesas.com>
    Signed-off-by: Biju Das <biju.das.jz@bp.renesas.com>

--- drivers/pwm/pwm-rzg2l-gpt.c
+++ drivers/pwm/pwm-rzg2l-gpt.c
@@ -78,6 +78,7 @@
 #define RZG2L_MAX_TICKS		((u64)U32_MAX * RZG2L_MAX_SCALE_FACTOR)
 
 struct rzg2l_gpt_info {
+	u8 (*calculate_prescale)(u64 period);
 	u32 gtcr_tpcs;
 	u8 prescale_mult;
 };
@@ -293,7 +294,7 @@ static int rzg2l_gpt_round_waveform_tohw(struct pwm_chip *chip,
 			wfhw->prescale = rzg2l_gpt->prescale[ch];
 		}
 	} else {
-		wfhw->prescale = rzg2l_gpt_calculate_prescale(period_ticks);
+		wfhw->prescale = info->calculate_prescale(period_ticks);
 		rzg2l_gpt->prescale[ch] = wfhw->prescale;
 	}
 
@@ -507,6 +508,7 @@ static int rzg2l_gpt_probe(struct platform_device *pdev)
 }
 
 static const struct rzg2l_gpt_info rzg2l_data = {
+	.calculate_prescale = rzg2l_gpt_calculate_prescale,
 	.gtcr_tpcs = RZG2L_GTCR_TPCS,
 	.prescale_mult = 2,
 };
