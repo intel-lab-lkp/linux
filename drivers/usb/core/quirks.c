9cd5fe0247524e48f6574777e0de01bf2df4a504
Jie Deng <dengjie03@kylinos.cn>
2026-08-26 16:59:24 +0800

    usb: core: Ignore remote wakeup for Fintek ttyUSB device to fix S3 auto-wakeup
    The Fintek ttyUSB device (VID: 0x1934, PID: 0x1202)
    has wake-up capability enabled by default, and changes
    in the RTS/DTR signals can trigger a remote wake-up.
    This behavior causes the system to be automatically
    woken up from S3 (Suspend to RAM) state unexpectedly.
    
    To prevent this unwanted auto-wakeup and ensure the system
    can stay suspended, add the USB_QUIRK_IGNORE_REMOTE_WAKEUP
    quirk to the device entry in usb_quirk_list.
    
    Signed-off-by: Jie Deng <dengjie03@kylinos.cn>

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -528,6 +528,10 @@ static const struct usb_device_id usb_quirk_list[] = {
 	{ USB_DEVICE(0x1908, 0x1315), .driver_info =
 			USB_QUIRK_HONOR_BNUMINTERFACES },
 
+	/* Fintek ttyUSB Device */
+	{ USB_DEVICE(0x1934, 0x1202), .driver_info =
+		USB_QUIRK_IGNORE_REMOTE_WAKEUP },
+
 	/* Protocol and OTG Electrical Test Device */
 	{ USB_DEVICE(0x1a0a, 0x0200), .driver_info =
 			USB_QUIRK_LINEAR_UFRAME_INTR_BINTERVAL },
