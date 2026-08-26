4f30d3523bb3e014eb5c24ee050bddf03a4d4eb8
Ilya Leoshkevich <iii@linux.ibm.com>
2026-08-26 17:06:00 +0200

    s390: Warn if kernel command line contains non-printable EBCDIC characters
    Users may accidentally add multi-byte UTF-8 characters to zipl.conf
    parmline, for example, by copying snippets containing non-breaking
    spaces (\xC2\xA0) from web pages.
    
    The kernel will then interpret the entire command line as EBCDIC,
    making it unusable. Distinguish this situation from the legitimate
    EBCDIC conversion by looking for non-printable characters and issue
    a warning.
    
    Signed-off-by: Ilya Leoshkevich <iii@linux.ibm.com>

--- arch/s390/boot/ipl_parm.c
+++ arch/s390/boot/ipl_parm.c
@@ -173,12 +173,30 @@ static inline int has_ebcdic_char(const char *str)
 	return 0;
 }
 
+static inline bool has_nonprintable_char(const char *str)
+{
+	for (int i = 0; str[i]; i++) {
+		unsigned char c = (unsigned char)str[i];
+
+		/*
+		 * Open-code ASCII range comparisons, since isprint() accepts
+		 * high Latin-1 bytes
+		 */
+		if ((c < 0x09 || c > 0x0d) && (c < 0x20 || c > 0x7e))
+			return true;
+	}
+	return false;
+}
+
 void setup_boot_command_line(void)
 {
 	parmarea.command_line[COMMAND_LINE_SIZE - 1] = 0;
 	/* convert arch command line to ascii if necessary */
-	if (has_ebcdic_char(parmarea.command_line))
+	if (has_ebcdic_char(parmarea.command_line)) {
 		EBCASC(parmarea.command_line, COMMAND_LINE_SIZE);
+		if (has_nonprintable_char(parmarea.command_line))
+			boot_warn("Kernel command line was treated as EBCDIC, but contains non-printable characters\n");
+	}
 	/* copy arch command line */
 	strscpy(early_command_line, strim(parmarea.command_line));
 
