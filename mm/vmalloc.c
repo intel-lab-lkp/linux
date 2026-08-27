fac0cda14a5dc6950b2cbd23b6c263127b2015a6
David Stevens <stevensd@google.com>
2026-08-27 16:29:40 -0700

    mm/vmalloc: Skip vmallocinfo NUMA stats for VM_SPARSE
    Bail from show_numa_info() in vmalloc_info_show() for areas with
    VM_SPARSE set. This lets clients that allocate VM_SPARSE areas directly
    use vm_struct's nr_pages and pages fields.
    
    Signed-off-by: David Stevens <stevensd@google.com>

--- mm/vmalloc.c
+++ mm/vmalloc.c
@@ -5294,6 +5294,9 @@ static void show_numa_info(struct seq_file *m, struct vm_struct *v,
 	if (!counters)
 		return;
 
+	if (v->flags & VM_SPARSE)
+		return;
+
 	memset(counters, 0, nr_node_ids * sizeof(unsigned int));
 
 	for (nr = 0; nr < v->nr_pages; nr += step)
