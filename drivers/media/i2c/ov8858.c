5b918383d9e6d2f11177e073373e3f29437e859c
Maurizio Casciano <mauriziocasciano7@gmail.com>
2026-08-27 20:17:50 +0200

    media: ov8858: Add INT3477 ACPI ID
    Firmware may use INT3477 as the ACPI hardware ID for an OV8858 image
    sensor. Add an ACPI match table for that ID.
    
    Signed-off-by: Maurizio Casciano <mauriziocasciano7@gmail.com>

--- drivers/media/i2c/ov8858.c
+++ drivers/media/i2c/ov8858.c
@@ -9,6 +9,7 @@
 #include <linux/clk.h>
 #include <linux/delay.h>
 #include <linux/device.h>
+#include <linux/device-id/acpi.h>
 #include <linux/gpio/consumer.h>
 #include <linux/i2c.h>
 #include <linux/module.h>
@@ -2077,6 +2078,12 @@ static void ov8858_remove(struct i2c_client *client)
 	pm_runtime_set_suspended(&client->dev);
 }
 
+static const struct acpi_device_id ov8858_acpi_match[] = {
+	{ .id = "INT3477" },
+	{ }
+};
+MODULE_DEVICE_TABLE(acpi, ov8858_acpi_match);
+
 static const struct of_device_id ov8858_of_match[] = {
 	{ .compatible = "ovti,ov8858" },
 	{ /* sentinel */ },
@@ -2087,6 +2094,7 @@ static struct i2c_driver ov8858_i2c_driver = {
 	.driver = {
 		.name = "ov8858",
 		.pm = &ov8858_pm_ops,
+		.acpi_match_table = ov8858_acpi_match,
 		.of_match_table = ov8858_of_match,
 	},
 	.probe		= ov8858_probe,
