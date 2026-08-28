intel-lab-lkp/linux

commit 030f9f682e66ea3be214f673ca8ba53294eeecd2

media: ov7670: control clock along with power
This provides more power saving when the sensor is off.

While at that, do the delay on power/clock enable even if the sensor driver
itself doesn't control the GPIOs. This is required for the OLPC XO-1
platform, that lacks the proper power/reset properties in its DT, but
needs the delay after the sensor is clocked up.

Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>
Signed-off-by: Sakari Ailus <sakari.ailus@linux.intel.com>
Signed-off-by: Mauro Carvalho Chehab <mchehab+samsung@kernel.org>

drivers/media/i2c/ov7670.c
@@ -1625,14 +1625,17 @@ static void ov7670_power_on(struct v4l2_subdev *sd)
 	if (info->on)
 		return;
 
+	clk_prepare_enable(info->clk);
+
 	if (info->pwdn_gpio)
 		gpiod_set_value(info->pwdn_gpio, 0);
 	if (info->resetb_gpio) {
 		gpiod_set_value(info->resetb_gpio, 1);
 		usleep_range(500, 1000);
 		gpiod_set_value(info->resetb_gpio, 0);
-		usleep_range(3000, 5000);
 	}
+	if (info->pwdn_gpio || info->resetb_gpio || info->clk)
+		usleep_range(3000, 5000);
 
 	info->on = true;
 }
@@ -1643,6 +1646,8 @@ static void ov7670_power_off(struct v4l2_subdev *sd)
 
 	if (!info->on)
 		return;
+
+	clk_disable_unprepare(info->clk);
 
 	if (info->pwdn_gpio)
 		gpiod_set_value(info->pwdn_gpio, 1);
@@ -1864,23 +1869,19 @@ static int ov7670_probe(struct i2c_client *client,
 			return ret;
 	}
 
-	if (info->clk) {
-		ret = clk_prepare_enable(info->clk);
-		if (ret)
-			return ret;
+	ret = ov7670_init_gpio(client, info);
+	if (ret)
+		return ret;
 
+	ov7670_power_on(sd);
+
+	if (info->clk) {
 		info->clock_speed = clk_get_rate(info->clk) / 1000000;
 		if (info->clock_speed < 10 || info->clock_speed > 48) {
 			ret = -EINVAL;
-			goto clk_disable;
+			goto power_off;
 		}
 	}
-
-	ret = ov7670_init_gpio(client, info);
-	if (ret)
-		goto clk_disable;
-
-	ov7670_power_on(sd);
 
 	/* Make sure it's an ov7670 */
 	ret = ov7670_detect(sd);
@@ -1961,6 +1962,7 @@ static int ov7670_probe(struct i2c_client *client,
 	if (ret < 0)
 		goto entity_cleanup;
 
+	ov7670_power_off(sd);
 	return 0;
 
 entity_cleanup:
@@ -1969,11 +1971,8 @@ hdl_free:
 	v4l2_ctrl_handler_free(&info->hdl);
 power_off:
 	ov7670_power_off(sd);
-clk_disable:
-	clk_disable_unprepare(info->clk);
 	return ret;
 }
-
 
 static int ov7670_remove(struct i2c_client *client)
 {
@@ -1982,7 +1981,6 @@ static int ov7670_remove(struct i2c_client *client)
 
 	v4l2_async_unregister_subdev(sd);
 	v4l2_ctrl_handler_free(&info->hdl);
-	clk_disable_unprepare(info->clk);
 	media_entity_cleanup(&info->sd.entity);
 	ov7670_power_off(sd);
 	return 0;

commit 14664ba42c571f6f03886d1ef6c9374d39c64486

media: ov7670: Propagate clock enable failures
ov7670_power_on() ignores clk_prepare_enable() and proceeds to toggle
GPIOs and set info->on.  A failed clock enable can therefore be reported
as a powered sensor and later be paired with an unbalanced clock
disable.

Return the clock error before changing GPIO or software state, and
propagate it from the subdevice power operation and probe.

The issue was identified via static analysis and manually reviewed.

Fixes: 030f9f682e66 ("media: ov7670: control clock along with power")

Assisted-by: LLM
Signed-off-by: Pengpeng Hou <pengpeng@iscas.ac.cn>

drivers/media/i2c/ov7670.c
@@ -1648,14 +1648,17 @@ static int ov7670_s_register(struct v4l2_subdev *sd, const struct v4l2_dbg_regis
 }
 #endif
 
-static void ov7670_power_on(struct v4l2_subdev *sd)
+static int ov7670_power_on(struct v4l2_subdev *sd)
 {
 	struct ov7670_info *info = to_state(sd);
+	int ret;
 
 	if (info->on)
-		return;
+		return 0;
 
-	clk_prepare_enable(info->clk);
+	ret = clk_prepare_enable(info->clk);
+	if (ret)
+		return ret;
 
 	if (info->pwdn_gpio)
 		gpiod_set_value(info->pwdn_gpio, 0);
@@ -1668,6 +1671,8 @@ static void ov7670_power_on(struct v4l2_subdev *sd)
 		usleep_range(3000, 5000);
 
 	info->on = true;
+
+	return 0;
 }
 
 static void ov7670_power_off(struct v4l2_subdev *sd)
@@ -1688,12 +1693,15 @@ static void ov7670_power_off(struct v4l2_subdev *sd)
 static int ov7670_s_power(struct v4l2_subdev *sd, int on)
 {
 	struct ov7670_info *info = to_state(sd);
+	int ret;
 
 	if (info->on == on)
 		return 0;
 
 	if (on) {
-		ov7670_power_on(sd);
+		ret = ov7670_power_on(sd);
+		if (ret)
+			return ret;
 		ov7670_init(sd, 0);
 		ov7670_apply_fmt(sd);
 		ov7675_apply_framerate(sd);
@@ -1873,7 +1881,9 @@ static int ov7670_probe(struct i2c_client *client)
 	if (ret)
 		return ret;
 
-	ov7670_power_on(sd);
+	ret = ov7670_power_on(sd);
+	if (ret)
+		return ret;
 
 	if (info->clk) {
 		info->clock_speed = clk_get_rate(info->clk) / 1000000;