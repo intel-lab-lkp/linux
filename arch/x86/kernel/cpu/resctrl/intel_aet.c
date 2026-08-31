f0433bcf7bb9e692481086ecd7202e0c6d9380e0
Tony Luck <tony.luck@intel.com>
2026-08-31 10:44:08 -0700

    x86/resctrl: Honor rdt=perf option to force enable AET perf events
    resctrl can disable ("force_off") a feature supported by the platform if,
    for example, there is an erratum related to the feature or, in the case of AET
    (Application Energy Telemetry), when the event group has insufficient RMIDs. The
    "rdt=" kernel command line option lets the user override ("force_on") when
    a feature is disabled in such case. Users may also disable ("force_off")
    individual supported resctrl features they do not need.
    
    Linux enumerates AET once on first mount and skips this enumeration if
    the associated event group is supported but disabled. Since AET is only
    enumerated once, this check is guaranteed to only consider disabling via
    the rdt= kernel parameter.
    
    However, when transitioning to enumerate AET on every mount, it is no longer
    correct to skip enumeration simply because an event group is marked as
    disabled. It is then possible for resctrl to have force-disabled the event
    group during an earlier enumeration due to insufficient RMIDs, which would
    improperly cause the kernel to ignore a user's explicit rdt= override on
    subsequent mounts.
    
    Ensure the user's command line choices take precedence over system-level limits.
    
    Signed-off-by: Tony Luck <tony.luck@intel.com>
    Reviewed-by: Reinette Chatre <reinette.chatre@intel.com>

--- arch/x86/kernel/cpu/resctrl/intel_aet.c
+++ arch/x86/kernel/cpu/resctrl/intel_aet.c
@@ -228,7 +228,7 @@ static bool enable_events(struct event_group *e, struct pmt_feature_group *p)
 	struct rdt_resource *r = &rdt_resources_all[RDT_RESOURCE_PERF_PKG].r_resctrl;
 	int skipped_events = 0;
 
-	if (e->force_off)
+	if (e->force_off && !e->force_on)
 		return false;
 
 	if (!group_has_usable_regions(e, p))
