e17d3d8e6d30b2aaf0af5fb7dce9f6831543b936
Kuniyuki Iwashima <kuniyu@google.com>
2026-09-01 18:33:01 +0000

    neighbour: Namespacify neigh_tables.
    Now, neigh_table is ready to be namespacified.
    
    Let's allocate per-netns neigh_table in neigh_table_register()
    and call neigh_table_init() and neigh_sysctl_register() for it.
    
    proc_create_seq_data() is changed to proc_create_net_data().
    
    Since each netns has its own sysctl knobs, RTM_SETNEIGHTBL can
    configure the default table param (ifindex==0) and GC parameters
    per netns.
    
    To avoid potential regression, all the default parameters under
    /proc/sys/net/ipv{4,6}/neigh/default/, are inherited from init_net
    by default.
    
    This behaviour is controlled by a new sysctl knob,
    net.core.neigh_inherit_init_net:
    
      # sysctl net.core.neigh_inherit_init_net
      net.core.neigh_inherit_init_net = 1
      # sysctl net.ipv4.neigh.default.gc_thresh1
      net.ipv4.neigh.default.gc_thresh1 = 128
      # sysctl net.ipv4.neigh.default.gc_thresh1=129
      net.ipv4.neigh.default.gc_thresh1 = 129
      # unshare -n sysctl net.ipv4.neigh.default.gc_thresh1
      net.ipv4.neigh.default.gc_thresh1 = 129
    
    If it is turned off, all settings are reset in the new netns:
    
      # sysctl net.core.neigh_inherit_init_net=0
      net.core.neigh_inherit_init_net = 0
      # unshare -n sysctl net.ipv4.neigh.default.gc_thresh1
      net.ipv4.neigh.default.gc_thresh1 = 128
    
    The next patch will remove other unnecessary net_eq().
    
    Note that CONFIG_SYSCTL cannot be enabled without CONFIG_PROC_FS.
    
    Signed-off-by: Kuniyuki Iwashima <kuniyu@google.com>
    Reviewed-by: Nikolay Aleksandrov <razor@blackwall.org>

--- net/core/sysctl_net_core.c
+++ net/core/sysctl_net_core.c
@@ -21,6 +21,7 @@
 
 #include <net/ip.h>
 #include <net/sock.h>
+#include <net/neighbour.h>
 #include <net/net_ratelimit.h>
 #include <net/busy_poll.h>
 #include <net/pkt_sched.h>
@@ -52,6 +53,8 @@ EXPORT_SYMBOL(sysctl_fb_tunnels_only_for_init_net);
  */
 int sysctl_devconf_inherit_init_net __read_mostly;
 EXPORT_SYMBOL(sysctl_devconf_inherit_init_net);
+
+int sysctl_neigh_inherit_init_net __read_mostly = 1;
 
 #if IS_ENABLED(CONFIG_NET_FLOW_LIMIT) || IS_ENABLED(CONFIG_RPS)
 static int dump_cpumask(void *buffer, size_t *lenp, loff_t *ppos,
@@ -675,6 +678,15 @@ static struct ctl_table net_core_table[] = {
 		.mode		= 0644,
 		.proc_handler	= proc_do_skb_defer_max,
 		.extra1		= SYSCTL_ZERO,
+	},
+	{
+		.procname	= "neigh_inherit_init_net",
+		.data		= &sysctl_neigh_inherit_init_net,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec_minmax,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
 	},
 };
 
