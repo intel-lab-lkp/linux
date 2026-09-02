c7d0e0b3b53574e5f7d4f28751b246e7360ea4f7
Ricardo Robaina <rrobaina@redhat.com>
2026-09-02 11:43:41 -0300

    openrisc: pass pt_regs to audit_syscall_entry()
    audit_syscall_entry() now takes a pointer to pt_regs and extracts
    the syscall arguments itself via syscall_get_arguments(). Drop the
    individual argument registers from the call and pass regs instead.
    
    Signed-off-by: Ricardo Robaina <rrobaina@redhat.com>

--- arch/openrisc/kernel/ptrace.c
+++ arch/openrisc/kernel/ptrace.c
@@ -301,8 +301,7 @@ asmlinkage long do_syscall_trace_enter(struct pt_regs *regs)
 		 */
 		ret = -1L;
 
-	audit_syscall_entry(regs->gpr[11], regs->gpr[3], regs->gpr[4],
-			    regs->gpr[5], regs->gpr[6]);
+	audit_syscall_entry(regs->gpr[11], regs);
 
 	return ret ? : regs->gpr[11];
 }
