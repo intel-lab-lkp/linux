82ca2807e1aa033501803103cc7bee020b0e26df
Steven Rostedt <rostedt@goodmis.org>
2026-09-01 20:20:20 -0400

    ftrace: Synchronize the initialization of ftrace_ops
    There's some internal state that ftrace_ops needs to have set, but since
    it can be declared outside of the ftrace.c code, it calls
    ftrace_ops_init() on the ops in every global function. The issue is that
    if two tasks call it on the same ops at the same time it is possible to
    have the initialization of one corrupt the initialization of the other
    call.
    
    Create a ops_mutex to use to synchronize every initialization of the
    ftrace_ops. The mutex is taken within checking the ftrace_ops flag that
    states it was initializied but the flag is checked again after the mutex
    has been taken. Checking first outside the mutex allows it to shortcut
    having to take the mutex. But then the check needs to be done again after
    the mute is taken in case of races.
    
    Cc: stable@vger.kernel.org
    Fixes: f04f24fb7e48d ("ftrace, kprobes: Fix a deadlock on ftrace_regex_lock")
    Reported-by: sashiko-bot@kernel.org
    Close: https://lore.kernel.org/all/20260829025528.49A831F000E9@smtp.kernel.org/
    Signed-off-by: Steven Rostedt <rostedt@goodmis.org>

--- kernel/trace/ftrace.c
+++ kernel/trace/ftrace.c
@@ -75,6 +75,8 @@
 	.func_hash		= &opsname.local_hash,			\
 	.local_hash.regex_lock	= __MUTEX_INITIALIZER(opsname.local_hash.regex_lock), \
 	.subop_list		= LIST_HEAD_INIT(opsname.subop_list),
+/* Used only to synchronize the initialization of ftrace_ops */
+static DEFINE_MUTEX(ops_mutex);
 #else
 #define INIT_OPS_HASH(opsname)
 #endif
@@ -160,6 +162,10 @@ static inline void ftrace_ops_init(struct ftrace_ops *ops)
 {
 #ifdef CONFIG_DYNAMIC_FTRACE
 	if (!(ops->flags & FTRACE_OPS_FL_INITIALIZED)) {
+		guard(mutex)(&ops_mutex);
+		/* Could have been initialized before lock taken */
+		if (unlikely(ops->flags & FTRACE_OPS_FL_INITIALIZED))
+			return;
 		mutex_init(&ops->local_hash.regex_lock);
 		INIT_LIST_HEAD(&ops->subop_list);
 		ops->func_hash = &ops->local_hash;
