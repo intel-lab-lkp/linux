a31afbfd49e0f3deae07e8029291835770d0c8de
Ricardo Robaina <rrobaina@redhat.com>
2026-09-02 11:43:35 -0300

    alpha: pass pt_regs to audit_syscall_entry()
    audit_syscall_entry() now takes a pointer to pt_regs and extracts
    the syscall arguments itself via syscall_get_arguments(). Drop the
    individual argument registers from the call and pass regs instead.
    
    Signed-off-by: Ricardo Robaina <rrobaina@redhat.com>

--- arch/alpha/kernel/ptrace.c
+++ arch/alpha/kernel/ptrace.c
@@ -496,8 +496,7 @@ asmlinkage unsigned long syscall_trace_enter(void)
 	}
 
 #ifdef CONFIG_AUDITSYSCALL
-	audit_syscall_entry(syscall_get_nr(current, regs),
-		regs->r16, regs->r17, regs->r18, regs->r19);
+	audit_syscall_entry(syscall_get_nr(current, regs), regs);
 #endif
 	return syscall_get_nr(current, regs);
 }
