487d0663c9d4a4570eea58b70f1bfd22af875668
Liew Rui Yan <aethernet65535@gmail.com>
2026-09-02 16:18:25 +0800

    mm/damon/core: fix false positive in
    When setting goal_tuner to 'temporal', if the goal is achieved,
    quota->esz_bp will be set to zero.  In this case, damos_quota_is_full()
    will always return true, even if no regions have been tried to apply at
    all.  This causes qt_exceeds to increase unexpectedly.
    
    Fix it by:
    
    - Returning false in damos_quota_is_full() when quota->charged_sz is
      zero.
    - Adding an early exit in damon_do_apply_schemes() for schemes with zero
      esz.
    
    Fixes: c7ec7d5f6b3d ("mm/damon/core: handle <min_region_sz remaining quota as empty")
    Cc: <stable@vger.kernel.org> # v7.2.x
    Signed-off-by: Liew Rui Yan <aethernet65535@gmail.com>

--- mm/damon/core.c
+++ mm/damon/core.c
@@ -2655,6 +2655,8 @@ static bool damos_quota_is_full(struct damos_quota *quota,
 {
 	if (!damos_quota_is_set(quota))
 		return false;
+	if (!quota->charged_sz)
+		return false;
 	if (quota->charged_sz >= quota->esz)
 		return true;
 	/*
@@ -2748,6 +2750,9 @@ static void damon_do_apply_schemes(struct damon_ctx *c,
 			continue;
 
 		if (!s->wmarks.activated)
+			continue;
+
+		if (!quota->esz)
 			continue;
 
 		/* Check the quota */
