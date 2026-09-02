a6d872464c5eee4eb2230da8559c8341ab69c106
Ricardo Robaina <rrobaina@redhat.com>
2026-09-02 11:43:44 -0300

    sparc64: pass pt_regs to audit_syscall_entry()
    audit_syscall_entry() now takes a pointer to pt_regs and extracts
    the syscall arguments itself via syscall_get_arguments(). Drop the
    individual argument registers from the call and pass regs instead.
    
    Signed-off-by: Ricardo Robaina <rrobaina@redhat.com>

--- arch/sparc/kernel/ptrace_64.c
+++ arch/sparc/kernel/ptrace_64.c
@@ -1098,9 +1098,7 @@ asmlinkage int syscall_trace_enter(struct pt_regs *regs)
 	if (unlikely(test_thread_flag(TIF_SYSCALL_TRACEPOINT)))
 		trace_sys_enter(regs, regs->u_regs[UREG_G1]);
 
-	audit_syscall_entry(regs->u_regs[UREG_G1], regs->u_regs[UREG_I0],
-			    regs->u_regs[UREG_I1], regs->u_regs[UREG_I2],
-			    regs->u_regs[UREG_I3]);
+	audit_syscall_entry(regs->u_regs[UREG_G1], regs);
 
 	return ret;
 }
