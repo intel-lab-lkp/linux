6053e80054e73dd9651fc721c3a165df61ecc8c9
Ricardo Robaina <rrobaina@redhat.com>
2026-09-02 11:43:43 -0300

    sh: pass pt_regs to audit_syscall_entry()
    audit_syscall_entry() now takes a pointer to pt_regs and extracts
    the syscall arguments itself via syscall_get_arguments(). Drop the
    individual argument registers from the call and pass regs instead.
    
    Signed-off-by: Ricardo Robaina <rrobaina@redhat.com>

--- arch/sh/kernel/ptrace_32.c
+++ arch/sh/kernel/ptrace_32.c
@@ -466,8 +466,7 @@ asmlinkage long do_syscall_trace_enter(struct pt_regs *regs)
 	if (unlikely(test_thread_flag(TIF_SYSCALL_TRACEPOINT)))
 		trace_sys_enter(regs, regs->regs[0]);
 
-	audit_syscall_entry(regs->regs[3], regs->regs[4], regs->regs[5],
-			    regs->regs[6], regs->regs[7]);
+	audit_syscall_entry(regs->regs[3], regs);
 
 	return 0;
 }
