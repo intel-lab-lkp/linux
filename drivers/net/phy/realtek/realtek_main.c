2db92be3c4d706718707e55e1741698a8a59fc7c
Oleksij Rempel <o.rempel@pengutronix.de>
2026-09-03 11:22:10 +0200

    net: phy: realtek: use C45 for RTL8365MB-VC internal PHY MMD access
    The RTL8365MB-VC PHYs are integrated into the switch and have no MMD
    registers of their own. What phylib accesses as MMD registers (EEE)
    actually lives in the switch's OCP registers; the realtek DSA driver maps
    those MMD requests onto OCP and exposes them over C45.
    
    For a C22 PHY, phy_read_mmd()/phy_write_mmd() would use C22 indirect-MMD,
    which this hardware does not implement. Point .read_mmd/.write_mmd at
    genphy_{read,write}_mmd_c45() so phylib's MMD accesses go over C45 to the
    DSA driver's mapping instead.
    
    Signed-off-by: Oleksij Rempel <o.rempel@pengutronix.de>
    Reviewed-by: Andrew Lunn <andrew@lunn.ch>

--- drivers/net/phy/realtek/realtek_main.c
+++ drivers/net/phy/realtek/realtek_main.c
@@ -3388,6 +3388,8 @@ static struct phy_driver realtek_drvs[] = {
 	}, {
 		PHY_ID_MATCH_EXACT(0x001cc942),
 		.name		= "RTL8365MB-VC Gigabit Ethernet",
+		.read_mmd	= genphy_read_mmd_c45,
+		.write_mmd	= genphy_write_mmd_c45,
 		/* Interrupt handling analogous to RTL8366RB */
 		.config_intr	= genphy_no_config_intr,
 		.handle_interrupt = genphy_handle_interrupt_no_ack,
