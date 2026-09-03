51ad689bf1a2e79b3e2b86397fb5850d04fab0e1
Marek Vasut <marek.vasut+renesas@mailbox.org>
2026-09-03 22:50:54 +0200

    PCI: rcar-gen4: Add Application/Local register reset control
    The R-Car Gen4 PCIe controller does contain core reset, which puts
    the controller back into well defined state, but it does also tear
    down the PCIe link. This is called Application/Local register reset
    in the documentation.
    
    Deassert the core reset after the core clock have been enabled, and
    assert the core reset in case of error or when shutting down the
    controller.
    
    This also fixes reinitialization failure of the controller, where if
    the controller driver is unbind and bind again via sysfs attributes,
    the controller driver will fail to probe the second time because it
    fails to access the controller DBI, which returns all zeroes unless
    the core reset is toggled. The following commands trigger this fault
    on R-Car V4H:
    
    $ echo e65d0000.pcie > /sys/bus/platform/drivers/pcie-rcar-gen4/unbind
    $ echo e65d0000.pcie > /sys/bus/platform/drivers/pcie-rcar-gen4/bind
    
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

--- drivers/pci/controller/dwc/pcie-rcar-gen4.c
+++ drivers/pci/controller/dwc/pcie-rcar-gen4.c
@@ -197,6 +197,8 @@ static int rcar_gen4_pcie_common_init(struct rcar_gen4_pcie *rcar)
 		return ret;
 	}
 
+	reset_control_deassert(dw->core_rsts[DW_PCIE_CORE_RST].rstc);
+
 	if (!reset_control_status(dw->core_rsts[DW_PCIE_PWR_RST].rstc)) {
 		reset_control_assert(dw->core_rsts[DW_PCIE_PWR_RST].rstc);
 		/*
@@ -247,6 +249,7 @@ static int rcar_gen4_pcie_common_init(struct rcar_gen4_pcie *rcar)
 	return 0;
 
 err_unprepare:
+	reset_control_assert(dw->core_rsts[DW_PCIE_CORE_RST].rstc);
 	clk_bulk_disable_unprepare(DW_PCIE_NUM_CORE_CLKS, dw->core_clks);
 
 	return ret;
@@ -257,6 +260,7 @@ static void rcar_gen4_pcie_common_deinit(struct rcar_gen4_pcie *rcar)
 	struct dw_pcie *dw = &rcar->dw;
 
 	reset_control_assert(dw->core_rsts[DW_PCIE_PWR_RST].rstc);
+	reset_control_assert(dw->core_rsts[DW_PCIE_CORE_RST].rstc);
 	clk_bulk_disable_unprepare(DW_PCIE_NUM_CORE_CLKS, dw->core_clks);
 }
 
