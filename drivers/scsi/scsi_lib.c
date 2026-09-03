6dc8efef2cfc69b4d1d46b7b64b71397837f5c40
Damien Le Moal <dlemoal@kernel.org>
2026-09-03 12:42:01 +0900

    scsi: remove scsi_build_sense() and scsi_build_sense_buffer()
    Now that all code has been converted to use 16-bits sense codes and to
    initialize sense with scsi_set_sense() and scsi_set_sense_buffer(), remove
    the inline definitions of these new functions, change scsi_build_sense()
    and scsi_build_sense_buffer() to use a single 16-bits sense code as
    argument (instead of separate arguments for the ASC and ASCQ), and
    rename them to scsi_set_sense()  and scsi_set_sense_buffer().
    
    Signed-off-by: Damien Le Moal <dlemoal@kernel.org>

--- drivers/scsi/scsi_lib.c
+++ drivers/scsi/scsi_lib.c
@@ -3585,7 +3585,7 @@ int scsi_vpd_tpg_id(struct scsi_device *sdev, int *rel_id)
 EXPORT_SYMBOL(scsi_vpd_tpg_id);
 
 /**
- * scsi_build_sense - build sense data for a command
+ * scsi_set_sense - build sense data for a command
  * @scmd:	scsi command for which the sense should be formatted
  * @desc:	Sense format (non-zero == descriptor format,
  *              0 == fixed format)
@@ -3594,12 +3594,12 @@ EXPORT_SYMBOL(scsi_vpd_tpg_id);
  * @ascq:	Additional sense code qualifier
  *
  **/
-void scsi_build_sense(struct scsi_cmnd *scmd, int desc, u8 key, u8 asc, u8 ascq)
+void scsi_set_sense(struct scsi_cmnd *scmd, int desc, u8 key, u16 code)
 {
-	scsi_build_sense_buffer(desc, scmd->sense_buffer, key, asc, ascq);
+	scsi_set_sense_buffer(desc, scmd->sense_buffer, key, code);
 	scmd->result = SAM_STAT_CHECK_CONDITION;
 }
-EXPORT_SYMBOL_GPL(scsi_build_sense);
+EXPORT_SYMBOL_GPL(scsi_set_sense);
 
 #ifdef CONFIG_SCSI_LIB_KUNIT_TEST
 #include "scsi_lib_test.c"
