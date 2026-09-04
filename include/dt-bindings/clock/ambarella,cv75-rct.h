5bc27333a926874552c402a1a8e3401fbecdc2d3
Long Zhao <longzhao@ambarella.com>
2026-09-04 14:38:10 +0800

    dt-bindings: clock: add Ambarella CV75 RCT
    Document the CV75 RCT clock controller and the clock ID header used by
    DT consumers.
    
    Document #reset-cells for the RCT reset-provider role. Soft-reset line
    IDs and the reset provider driver are deferred; this series has no
    resets= consumers.
    
    Signed-off-by: Long Zhao <longzhao@ambarella.com>

---
+++ include/dt-bindings/clock/ambarella,cv75-rct.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only OR BSD-2-Clause */
+/*
+ * Copyright (C) 2026 Ambarella, Inc.
+ */
+
+#ifndef _DT_BINDINGS_CLOCK_AMBARELLA_CV75_RCT_H
+#define _DT_BINDINGS_CLOCK_AMBARELLA_CV75_RCT_H
+
+/* Boot / bus clocks */
+#define CV75_GCLK_UART0		0
+#define CV75_GCLK_CORE		1
+#define CV75_GCLK_AHB		2
+#define CV75_GCLK_APB		3
+
+#endif
