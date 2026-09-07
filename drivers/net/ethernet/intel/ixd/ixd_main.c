a76f0df842628e5a100e1a07cd182f5e99736b06
Michal Swiatkowski <michal.swiatkowski@linux.intel.com>
2026-09-07 12:24:17 +0200

    ixd: use interrupt for mailbox communication
    Switch from polling to interrupt for invoking mailbox operations when it
    is available.
    
    Add flag to store the information about the mailbox interrupt. If
    interrupt is turned off mailbox will be handled by periodic workqueue.
    If it is enabled interrupt will take care of it.
    
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Signed-off-by: Michal Swiatkowski <michal.swiatkowski@linux.intel.com>

--- drivers/net/ethernet/intel/ixd/ixd_main.c
+++ drivers/net/ethernet/intel/ixd/ixd_main.c
@@ -9,6 +9,7 @@
 MODULE_DESCRIPTION("Intel(R) Control Plane Function Device Driver");
 MODULE_IMPORT_NS("LIBIE_CP");
 MODULE_IMPORT_NS("LIBIE_PCI");
+MODULE_IMPORT_NS("LIBIE_IRQ");
 MODULE_LICENSE("GPL");
 
 /**
@@ -28,6 +29,7 @@ static void ixd_remove(struct pci_dev *pdev)
 	if (adapter->xnm)
 		libie_ctlq_xn_shutdown(adapter->xnm);
 	ixd_trigger_reset(adapter);
+	ixd_deinit_interrupts(adapter);
 	ixd_deinit_dflt_mbx(adapter);
 
 	libie_pci_unmap_all_mmio_regions(&adapter->cp_ctx.mmio_info);
