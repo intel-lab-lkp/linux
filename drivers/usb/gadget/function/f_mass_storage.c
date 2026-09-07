6b635c064349087e171eb0c765ec435c54db57b2
Keisuke Tsukuda <catnyaaan@outlook.com>
2026-09-07 08:58:42 +0000

    usb: gadget: f_mass_storage: ignore class requests while inactive
    SET_CONFIGURATION is handled asynchronously by the mass-storage thread.
    A class request can arrive after the composite core selects the function
    but before the thread installs common->fsg, or while the function is being
    disabled.
    
    fsg_setup() already rejects this state with -EOPNOTSUPP, but checking it
    through fsg_is_set() also emits a warning. Check the active function
    directly so normal requests during a configuration transition are rejected
    without hiding fsg_is_set() warnings from other internal paths.
    
    Fixes: 8876f5e7d3b2 ("USB: gadget: f_mass_storage: added eject callback")
    Reported-by: syzbot+d31109047baed8f3b590@syzkaller.appspotmail.com
    Closes: https://syzkaller.appspot.com/bug?extid=d31109047baed8f3b590
    Signed-off-by: Keisuke Tsukuda <tkdkei@outlook.jp>

--- drivers/usb/gadget/function/f_mass_storage.c
+++ drivers/usb/gadget/function/f_mass_storage.c
@@ -463,7 +463,7 @@ static int fsg_setup(struct usb_function *f,
 	u16			w_value = le16_to_cpu(ctrl->wValue);
 	u16			w_length = le16_to_cpu(ctrl->wLength);
 
-	if (!fsg_is_set(fsg->common))
+	if (!fsg->common->fsg)
 		return -EOPNOTSUPP;
 
 	++fsg->common->ep0_req_tag;	/* Record arrival of a new request */
