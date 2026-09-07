975e1c62cb1a746317d0bef71aee4c07d0c6359c
Marek Vasut <marek.vasut+renesas@mailbox.org>
2026-09-07 17:36:40 +0200

    PCI: rcar-gen4: Add missing PM ops
    The R-Car Gen4 PCIe controller is part of a power domain. On R-Car S4
    and V4H, this is an always-on power domain which is not shut down in
    suspend. On R-Car V4M, this is a dedicated A2PCIPHY power domain,
    which is shut down during suspend, and the controller loses state,
    which prevents the PCIe from working after resume.
    
    Fix this by adding generic suspend/resume noirq ops for the controller,
    which tear the link down on suspend, and restart it on resume. Use the
    same PM ops on all of R-Car Gen4 to gracefully suspend and resume the
    PCIe link on V4H and S4 too.
    
    Test case which demonstrates the problem on R-Car V4M:
    "
    $ hexdump -vC /sys/bus/pci/devices/0000:00:00.0/config > /tmp/pre
    $ echo s2idle > /sys/power/mem_sleep
    $ echo platform > /sys/power/pm_test
    $ echo mem > /sys/power/state
    $ hexdump -vC /sys/bus/pci/devices/0000:00:00.0/config > /tmp/post
    $ diff -Naru /tmp/pre /tmp/post
    ...
    -00000000  12 19 32 00 07 05 10 00  00 00 04 06 00 00 01 00
    +00000000  12 19 32 00 07 05 10 00  00 00 00 ff 00 00 80 00
                                              ^^^^^
                                        Class 0604->00ff
    "
    
    Fixes: 0d0c551011df ("PCI: rcar-gen4: Add R-Car Gen4 PCIe controller support for host mode")
    Tested-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Reported-by: Geert Uytterhoeven <geert+renesas@glider.be>
    Signed-off-by: Marek Vasut <marek.vasut+renesas@mailbox.org>

--- drivers/pci/controller/dwc/pcie-rcar-gen4.c
+++ drivers/pci/controller/dwc/pcie-rcar-gen4.c
@@ -853,6 +853,28 @@ static int rcar_gen4_pcie_ltssm_control(struct rcar_gen4_pcie *rcar, bool enable
 	return 0;
 }
 
+static int rcar_gen4_pcie_suspend_noirq(struct device *dev)
+{
+	struct rcar_gen4_pcie *rcar = dev_get_drvdata(dev);
+	struct dw_pcie *dw = &rcar->dw;
+
+	if (dw->mode != DW_PCIE_RC_TYPE)
+		return 0;
+
+	return dw_pcie_suspend_noirq(dw);
+}
+
+static int rcar_gen4_pcie_resume_noirq(struct device *dev)
+{
+	struct rcar_gen4_pcie *rcar = dev_get_drvdata(dev);
+	struct dw_pcie *dw = &rcar->dw;
+
+	if (dw->mode != DW_PCIE_RC_TYPE)
+		return 0;
+
+	return dw_pcie_resume_noirq(dw);
+}
+
 static struct rcar_gen4_pcie_drvdata drvdata_r8a779f0_pcie = {
 	.ltssm_control = r8a779f0_pcie_ltssm_control,
 	.mode = DW_PCIE_RC_TYPE,
@@ -896,10 +918,14 @@ static const struct of_device_id rcar_gen4_pcie_of_match[] = {
 };
 MODULE_DEVICE_TABLE(of, rcar_gen4_pcie_of_match);
 
+DEFINE_NOIRQ_DEV_PM_OPS(rcar_gen4_pcie_pm_ops,
+			rcar_gen4_pcie_suspend_noirq, rcar_gen4_pcie_resume_noirq);
+
 static struct platform_driver rcar_gen4_pcie_driver = {
 	.driver = {
 		.name = "pcie-rcar-gen4",
 		.of_match_table = rcar_gen4_pcie_of_match,
+		.pm = pm_sleep_ptr(&rcar_gen4_pcie_pm_ops),
 		.probe_type = PROBE_PREFER_ASYNCHRONOUS,
 	},
 	.probe = rcar_gen4_pcie_probe,
