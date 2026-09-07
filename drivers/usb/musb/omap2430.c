intel-lab-lkp/linux

commit 10ac7e7757f5e264599409441362de67760ba787

usb: musb: omap2430: Add support for idling phy when musb is idle
I noticed that musb is blocking core retention for omap4 unlike for
omap3. This is because for omap3 we have phy-twl4030-usb implement
it's own PM runtime to handle errata "VUSB3V1 VBUS overvoltage
debouncer not working when the PHY is powered down". That is done
in order to keep the USB PHY powered when phy-twl4030-usb is loaded.

For the other USB PHYs, we need to enable and disable the PHY based on
musb PM runtime. With the session bit based PM runtime for musb core,
we can now idle the USB PHY always when musb is idle.

Note that adding these calls will not affect the twl4030 driver
as it's phy functions will just query the PHY state without powering
the PHY on or off.

Signed-off-by: Tony Lindgren <tony@atomide.com>
Signed-off-by: Bin Liu <b-liu@ti.com>
Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

drivers/usb/musb/omap2430.c
@@ -531,6 +531,9 @@ static int omap2430_runtime_suspend(struct device *dev)
 
 	omap2430_low_level_exit(musb);
 
+	phy_power_off(musb->phy);
+	phy_exit(musb->phy);
+
 	return 0;
 }
 
@@ -541,6 +544,9 @@ static int omap2430_runtime_resume(struct device *dev)
 
 	if (!musb)
 		return 0;
+
+	phy_init(musb->phy);
+	phy_power_on(musb->phy);
 
 	omap2430_low_level_init(musb);
 	musb_writel(musb->mregs, OTG_INTERFSEL,

commit 8cd53275f8e56f7eba58cba9f4c7626673f6b973

usb: musb: omap2430: Propagate PHY errors from runtime resume
omap2430_runtime_resume() ignores phy_init() and phy_power_on()
failures and continues to initialize the glue state without a usable PHY.

Return initialization errors and call phy_exit() when power-on fails
after a successful initialization. Keep is_runtime_suspended set until the
PHY and glue state have both been restored.

The issue was found by our static-analysis tool and manually reviewed.

Fixes: 10ac7e7757f5 ("usb: musb: omap2430: Add support for idling phy when musb is idle")
Assisted-by: gpt 5
Signed-off-by: Pengpeng Hou <hppiscas@163.com>

drivers/usb/musb/omap2430.c
@@ -514,13 +514,21 @@ static int omap2430_runtime_resume(struct device *dev)
 {
 	struct omap2430_glue		*glue = dev_get_drvdata(dev);
 	struct musb			*musb = glue_to_musb(glue);
+	int ret;
 
 	if (!musb)
 		return 0;
 
 	if (!glue->phy_suspended) {
-		phy_init(musb->phy);
-		phy_power_on(musb->phy);
+		ret = phy_init(musb->phy);
+		if (ret)
+			return ret;
+
+		ret = phy_power_on(musb->phy);
+		if (ret) {
+			phy_exit(musb->phy);
+			return ret;
+		}
 	}
 
 	omap2430_low_level_init(musb);