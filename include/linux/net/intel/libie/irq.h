ff5c5dfec293c220938e0cb330648d7f02e9ca8e
Michal Swiatkowski <michal.swiatkowski@linux.intel.com>
2026-09-07 12:24:14 +0200

    libie, idpf: move parsing alloc vectors command to libie
    No functional changes. Just moving to make it available from other
    ipu drivers.
    
    Reviewed-by: Aleksandr Loktionov <aleksandr.loktionov@intel.com>
    Reviewed-by: Ahmed Zaki <ahmed.zaki@intel.com>
    Signed-off-by: Michal Swiatkowski <michal.swiatkowski@linux.intel.com>

--- include/linux/net/intel/libie/irq.h
+++ include/linux/net/intel/libie/irq.h
@@ -5,6 +5,7 @@
 #define __LIBIE_IRQ_H
 
 #include <linux/pci.h>
+#include <linux/net/intel/virtchnl2.h>
 
 /* In whole code in libie_irq index means the software 0-based irq index
  * for driver purpose, virq means the linux irq line number. Index can be used
@@ -105,5 +106,11 @@ struct msi_map libie_irq_alloc(struct libie_irq *irq, enum libie_irq_type type);
 void libie_irq_free(struct libie_irq *irq, struct msi_map map);
 int libie_irq_reserve(struct libie_irq *irq);
 void libie_put_irq(struct libie_irq *irq, unsigned int index);
+/* IPU specific functions. */
+void libie_irq_destroy_info(struct libie_irq_info *info);
+int libie_irq_create_info(struct libie_irq_info *info,
+			  const struct virtchnl2_get_capabilities *caps,
+			  const struct virtchnl2_alloc_vectors *vectors,
+			  const u16 num_vectors);
 
 #endif /* __LIBIE_IRQ_H */
