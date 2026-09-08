f218fc0e98a349ae04d23c9c2b7075d508fc5c9e
Zenghui Yu (Huawei) <zenghui.yu@linux.dev>
2026-09-08 21:44:05 +0800

    selftests/mm: init page sizes early in pagemap_ioctl test
    Initialize page_size and hpage_size before calling init_uffd(),
    hugetlb_setup_default(), etc.  That won't fix anything, but it is safer
    and saner to get these globals set up before doing other things.
    
    While at it, drop the page_size parameter of transact_test(), which is
    actually unnecessary.
    
    Link: https://lore.kernel.org/20260908134405.84448-1-zenghui.yu@linux.dev
    Signed-off-by: Zenghui Yu (Huawei) <zenghui.yu@linux.dev>
    Suggested-by: Andrew Morton <akpm@linux-foundation.org>
      Link: https://lore.kernel.org/20260628111329.9cfcd9c67925869307020aba@linux-foundation.org/
    Cc: David Hildenbrand (Arm) <david@kernel.org>
    Cc: Gregory Price <gourry@gourry.net>
    Cc: Liam R. Howlett <liam@infradead.org>
    Cc: Lorenzo Stoakes <ljs@kernel.org>
    Cc: Michal Hocko <mhocko@suse.com>
    Cc: Mike Rapoport <rppt@kernel.org>
    Cc: Shuah Khan <shuah@kernel.org>
    Cc: Suren Baghdasaryan <surenb@google.com>
    Cc: Vlastimil Babka <vbabka@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- tools/testing/selftests/mm/pagemap_ioctl.c
+++ tools/testing/selftests/mm/pagemap_ioctl.c
@@ -1488,7 +1488,7 @@ static void *thread_proc(void *mem)
 	return NULL;
 }
 
-static void transact_test(int page_size)
+static void transact_test(void)
 {
 	unsigned int i, count, extra_pages;
 	unsigned int c;
@@ -1652,6 +1652,9 @@ int main(int __attribute__((unused)) argc, char *argv[])
 
 	ksft_print_header();
 
+	page_size = getpagesize();
+	hpage_size = read_pmd_pagesize();
+
 	if (init_uffd())
 		ksft_exit_skip("Failed to initialize userfaultfd\n");
 
@@ -1659,9 +1662,6 @@ int main(int __attribute__((unused)) argc, char *argv[])
 		ksft_print_msg("HugeTLB test will be skipped\n");
 
 	ksft_set_plan(119);
-
-	page_size = getpagesize();
-	hpage_size = read_pmd_pagesize();
 
 	pagemap_fd = open(PAGEMAP, O_RDONLY);
 	if (pagemap_fd < 0)
@@ -1822,7 +1822,7 @@ int main(int __attribute__((unused)) argc, char *argv[])
 	mprotect_tests();
 
 	/* 13. Transact test */
-	transact_test(page_size);
+	transact_test();
 
 	/* 14. Sanity testing */
 	sanity_tests();
