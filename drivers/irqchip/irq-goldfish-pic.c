4c17b2bb5a7d695805418e1b4f2c779539cad2ed
Kuan-Wei Chiu <visitorckw@gmail.com>
2026-09-08 09:40:13 +0000

    irqchip/goldfish-pic: Allow selecting CONFIG_GOLDFISH_PIC on other architectures
    Drop the MIPS dependency from CONFIG_GOLDFISH_PIC so that other
    architectures can use this driver. Also update the file header comment
    to remove the MIPS reference.
    
    Signed-off-by: Kuan-Wei Chiu <visitorckw@gmail.com>

--- drivers/irqchip/irq-goldfish-pic.c
+++ drivers/irqchip/irq-goldfish-pic.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Driver for MIPS Goldfish Programmable Interrupt Controller.
+ * Driver for Goldfish Programmable Interrupt Controller.
  *
  * Author: Miodrag Dinic <miodrag.dinic@mips.com>
  */
