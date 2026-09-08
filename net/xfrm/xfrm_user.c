8b346c5af82817787a46aefd7686fbd98cc60515
Antony Antony <antony.antony@secunet.com>
2026-09-08 08:49:18 +0200

    xfrm: fix use-after-free of migrated state in xfrm_do_migrate_state()
    A concurrent delete can free the migrated state before
    xfrm_do_migrate_state() finishes using it.
    
    Fixes: a9d155ea9b44 ("xfrm: add XFRM_MSG_MIGRATE_STATE for single SA migration")
    Reported-by: Sashiko <sashiko-bot@kernel.org>
    Signed-off-by: Antony Antony <antony.antony@secunet.com>

--- net/xfrm/xfrm_user.c
+++ net/xfrm/xfrm_user.c
@@ -3479,6 +3479,7 @@ static int xfrm_do_migrate_state(struct sk_buff *skb, struct nlmsghdr *nlh,
 	__xfrm_state_delete(x);
 	spin_unlock_bh(&x->lock);
 
+	xfrm_state_hold(xc);
 	err = xfrm_state_migrate_install(x, xc, &m, extack);
 	if (err < 0) {
 		/*
@@ -3486,6 +3487,7 @@ static int xfrm_do_migrate_state(struct sk_buff *skb, struct nlmsghdr *nlh,
 		 * free under xfrm_cfg_mutex. Both SAs are gone if it does;
 		 * restoring x would risk SN/IV reuse.
 		 */
+		xfrm_state_put(xc);
 		goto out;
 	}
 
@@ -3504,6 +3506,7 @@ static int xfrm_do_migrate_state(struct sk_buff *skb, struct nlmsghdr *nlh,
 		err = 0;
 	}
 
+	xfrm_state_put(xc);
 out:
 	xfrm_state_put(x);
 	return err;
