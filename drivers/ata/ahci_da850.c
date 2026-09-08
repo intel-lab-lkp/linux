6f2ea10233fb9b2be41d636b8b974c3b2f2aa5ff
Li Jun <lijun01@kylinos.cn>
2026-09-08 10:59:15 +0800

    ata: ahci_da850: switch to DEFINE_SIMPLE_DEV_PM_OPS
    Replace the deprecated SIMPLE_DEV_PM_OPS macro with the recommended
    DEFINE_SIMPLE_DEV_PM_OPS.use pm_ptr(&ahci_da850_pm_ops) to complete
    the API migration and allow the structure to be correctly dropped
    when CONFIG_PM=n.
    
    Signed-off-by: Li Jun <lijun01@kylinos.cn>

--- drivers/ata/ahci_da850.c
+++ drivers/ata/ahci_da850.c
@@ -218,7 +218,7 @@ disable_resources:
 	return rc;
 }
 
-static SIMPLE_DEV_PM_OPS(ahci_da850_pm_ops, ahci_platform_suspend,
+static DEFINE_SIMPLE_DEV_PM_OPS(ahci_da850_pm_ops, ahci_platform_suspend,
 			 ahci_platform_resume);
 
 static const struct of_device_id ahci_da850_of_match[] = {
@@ -233,7 +233,7 @@ static struct platform_driver ahci_da850_driver = {
 	.driver = {
 		.name = DRV_NAME,
 		.of_match_table = ahci_da850_of_match,
-		.pm = &ahci_da850_pm_ops,
+		.pm = pm_ptr(&ahci_da850_pm_ops),
 	},
 };
 module_platform_driver(ahci_da850_driver);
