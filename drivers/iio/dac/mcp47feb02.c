425081984fa7dad1cdb34c464d4356c80bc4bef0
Ariana Lazar <ariana.lazar@microchip.com>
2026-09-09 17:18:47 +0300

    iio: dac: mcp47feb02: Increase EEPROM Programming Write Cycle Time
    Increase the EEPROM write polling timeout to prevent -ETIMEDOUT errors and
    incomplete nonvolatile write cycle, according to the time specified in the
    datasheets (11-16 ms).
    
    Signed-off-by: Ariana Lazar <ariana.lazar@microchip.com>

--- drivers/iio/dac/mcp47feb02.c
+++ drivers/iio/dac/mcp47feb02.c
@@ -449,7 +449,7 @@ static int mcp47feb02_write_to_eeprom(struct mcp47feb02_data *data, unsigned int
 	ret = regmap_read_poll_timeout(data->regmap, MCP47FEB02_GAIN_CTRL_STATUS_REG_ADDR,
 				       eewa_val,
 				       !(eewa_val & MCP47FEB02_GAIN_BIT_STATUS_EEWA_MASK),
-				       USEC_PER_MSEC, USEC_PER_MSEC * 5);
+				       USEC_PER_MSEC, USEC_PER_MSEC * 1500);
 	if (ret)
 		return ret;
 
@@ -509,7 +509,7 @@ static ssize_t store_eeprom_store(struct device *dev, struct device_attribute *a
 
 	ret = regmap_read_poll_timeout(data->regmap, MCP47FEB02_GAIN_CTRL_STATUS_REG_ADDR, eewa_val,
 				       !(eewa_val & MCP47FEB02_GAIN_BIT_STATUS_EEWA_MASK),
-				       USEC_PER_MSEC, USEC_PER_MSEC * 5);
+				       USEC_PER_MSEC, USEC_PER_MSEC * 1500);
 	if (ret)
 		return ret;
 
