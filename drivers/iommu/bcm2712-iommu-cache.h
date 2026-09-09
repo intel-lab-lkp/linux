b4200e121b87715523e18e25539399370d33d4d2
Daniel Drake <dan@reactivated.net>
2026-09-09 21:08:10 +0100

    iommu: Add Broadcom BCM2712 IOMMU driver
    This IOMMU translates memory access requests for the VC6 display
    pipeline and various multimedia devices in the Broadcom BCM2712 SoC used
    on Raspberry Pi 5.
    
    The driver uses the generic_pt framework to manage the 2-level page
    tables. It also provides support for the centralized L2 TLB (IOMMUC),
    which is shared across the SoC's IOMMU instances and must be flushed
    when modifying or unmapping page table entries.
    
    Adapted from Raspberry Pi's downstream bcm2712-iommu driver
    (original author Nick Hollinghurst).
    
    Reviewed-by: Jason Gunthorpe <jgg@nvidia.com>
    Signed-off-by: Daniel Drake <dan@reactivated.net>

---
+++ drivers/iommu/bcm2712-iommu-cache.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+#ifndef _BCM2712_IOMMU_CACHE_H
+#define _BCM2712_IOMMU_CACHE_H
+
+struct bcm2712_iommu_cache;
+
+void bcm2712_iommu_cache_flush(struct bcm2712_iommu_cache *cache);
+
+#endif
