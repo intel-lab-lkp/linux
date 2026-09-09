24054ade6fa4f90f82d5de131f2001cf9c65f932
Jisheng Zhang <jszhang@kernel.org>
2026-09-09 22:36:52 +0800

    spi: dw: use threaded interrupt and optimize the threaded ISR
    To avoid blocking for an excessive amount of time, eventually impacting
    on system responsiveness, hard interrupt handlers should finish
    executing in as little time as possible.
    
    Use threaded interrupt and move the SPI transfer handling to an
    interrupt thread for host mode and non enhanced spi for two reasons:
    
    "while the performance improvement will apply for both target mode
    has no control over the clocking of data by the host so is much more
    vulnerable to dropping data in a threaded interrupt if the system is
    loaded.  In host mode this isn't an issue since we'll simply stop
    clocking data while waiting for the scheduler to get round to the
    interrupt thread."
    
    In theory, we could do this for enhanced spi too, but I don't have the
    HW to test, so I leave enhanced spi interrupt routine as is, anyone
    has the HW can implement similar optimization in the future.
    
    After that, since the dw_reader() and dw_writer() are called in
    threaded ISR now, so we can delay the unmasking interrupts until no
    rx and tx action is taken, thus reduce the interrupt numbers further.
    
    Tested with below two cmds
    ./spidev_test -D /dev/spidev1.3 -s 30000000 -S 327680 -I 1
    
    ./spidev_test -D /dev/spidev1.3 -s 30000000 -S 327680 -I 1000
    ./rtla timerlat top -q -k -P f:95
    
    The first cmd is to check the interrupt numbers optmizaion result, the
    2nd cmd group is to check the threaded interrupt improvement.
    
    Before the patch:
    each 320KB spi spidev_test transfer triggers 33118 interrupts
    
    spidev_test reports ~22090 kbps
    and rtla reports:
                                         Timer Latency
      0 00:00:37   |          IRQ Timer Latency (us)        |         Thread Timer Latency (us)
    CPU COUNT      |      cur       min       avg       max |      cur       min       avg       max
      0 #9958      |        1         0        67    103394 |        6         4      2198    105031
      1 #36902     |        1         0         1        18 |        5         4         5        29
    
    After the patch:
    each 320KB spi spidev_test transfer only triggers 1 interrupts
    spidev_test reports ~23520 kbps
    and now rtla reports:
                                        Timer Latency
      0 00:00:58   |          IRQ Timer Latency (us)        |         Thread Timer Latency (us)
    CPU COUNT      |      cur       min       avg       max |      cur       min       avg       max
      0 #58362     |        1         0         0        29 |        6         3         4        56
      1 #58363     |        1         0         1        23 |        6         4         5        68
    
    In summary:
    before the patch	after the patch
    33118 interrutps	1 interrupts		reduced by 33117 times!
    103394 us max latency	29 us max latency	reduced by 3564 times!
    22090 kbps rate		23520 kbps rate		improved by 6.5%
    
    Signed-off-by: Jisheng Zhang <jszhang@kernel.org>

--- drivers/spi/spi-dw-core.c
+++ drivers/spi/spi-dw-core.c
@@ -132,10 +132,11 @@ static inline u32 dw_spi_rx_max(struct dw_spi *dws)
 	return min_t(u32, dws->rx_len, dw_readl(dws, DW_SPI_RXFLR));
 }
 
-static void dw_writer(struct dw_spi *dws)
+static u32 dw_writer(struct dw_spi *dws)
 {
 	u32 max = dw_spi_tx_max(dws);
 	u32 txw = 0;
+	u32 tx = 0;
 
 	while (max--) {
 		if (dws->tx) {
@@ -150,13 +151,16 @@ static void dw_writer(struct dw_spi *dws)
 		}
 		dw_write_io_reg(dws, DW_SPI_DR, txw);
 		--dws->tx_len;
+		++tx;
 	}
+	return tx;
 }
 
-static void dw_reader(struct dw_spi *dws)
+static u32 dw_reader(struct dw_spi *dws)
 {
 	u32 max = dw_spi_rx_max(dws);
 	u32 rxw;
+	u32 rx = 0;
 
 	while (max--) {
 		rxw = dw_read_io_reg(dws, DW_SPI_DR);
@@ -171,7 +175,9 @@ static void dw_reader(struct dw_spi *dws)
 			dws->rx += dws->n_bytes;
 		}
 		--dws->rx_len;
+		++rx;
 	}
+	return rx;
 }
 
 int dw_spi_check_status(struct dw_spi *dws, bool raw)
@@ -209,6 +215,62 @@ int dw_spi_check_status(struct dw_spi *dws, bool raw)
 	return ret;
 }
 EXPORT_SYMBOL_NS_GPL(dw_spi_check_status, "SPI_DW_CORE");
+
+static irqreturn_t dw_spi_irq_thread_fn(int irq, void *dev_id)
+{
+	struct spi_controller *ctlr = dev_id;
+	struct dw_spi *dws = spi_controller_get_devdata(ctlr);
+	u32 rx, tx, imask, mask = 0;
+	bool finalize = false;
+
+	do {
+		/*
+		 * Read data from the Rx FIFO every time we've got a chance executing
+		 * this method. If there is nothing left to receive, terminate the
+		 * procedure. Otherwise adjust the Rx FIFO Threshold level if it's a
+		 * final stage of the transfer. By doing so we'll get the next IRQ
+		 * right when the leftover incoming data is received.
+		 */
+		rx = dw_reader(dws);
+		if (!dws->rx_len) {
+			mask |= 0xff;
+			finalize = true;
+		} else if (dws->rx_len <= dw_readl(dws, DW_SPI_RXFTLR)) {
+			dw_writel(dws, DW_SPI_RXFTLR, dws->rx_len - 1);
+		}
+
+		/*
+		 * Send data out as much as possible. The Tx FIFO Empty IRQ will be
+		 * disabled after the data transmission is finished so not to
+		 * have the TXE IRQ flood at the final stage of the transfer.
+		 */
+		tx = dw_writer(dws);
+		if (!dws->tx_len)
+			mask |= DW_SPI_INT_TXEI;
+	} while (rx != 0 || tx != 0);
+
+	imask = DW_SPI_INT_TXEI | DW_SPI_INT_TXOI |
+		DW_SPI_INT_RXUI | DW_SPI_INT_RXOI | DW_SPI_INT_RXFI;
+	imask &= ~mask;
+	dw_spi_umask_intr(dws, imask);
+
+	if (finalize)
+		spi_finalize_current_transfer(dws->ctlr);
+
+	return IRQ_HANDLED;
+}
+
+static irqreturn_t dw_spi_host_handler(struct dw_spi *dws)
+{
+	if (dw_spi_check_status(dws, false)) {
+		spi_finalize_current_transfer(dws->ctlr);
+		return IRQ_HANDLED;
+	}
+
+	dw_spi_mask_intr(dws, 0xff);
+
+	return IRQ_WAKE_THREAD;
+}
 
 static irqreturn_t dw_spi_transfer_handler(struct dw_spi *dws)
 {
@@ -415,7 +477,10 @@ static void dw_spi_irq_setup(struct dw_spi *dws)
 	dw_writel(dws, DW_SPI_TXFTLR, level);
 	dw_writel(dws, DW_SPI_RXFTLR, level - 1);
 
-	dws->transfer_handler = dw_spi_transfer_handler;
+	if (spi_controller_is_target(dws->ctlr))
+		dws->transfer_handler = dw_spi_transfer_handler;
+	else
+		dws->transfer_handler = dw_spi_host_handler;
 
 	imask = DW_SPI_INT_TXEI | DW_SPI_INT_TXOI |
 		DW_SPI_INT_RXUI | DW_SPI_INT_RXOI | DW_SPI_INT_RXFI;
@@ -1305,8 +1370,8 @@ int dw_spi_add_controller(struct device *dev, struct dw_spi *dws)
 	/* Basic HW init */
 	dw_spi_hw_init(dev, dws);
 
-	ret = request_irq(dws->irq, dw_spi_irq, IRQF_SHARED, dev_name(dev),
-			  ctlr);
+	ret = request_threaded_irq(dws->irq, dw_spi_irq, dw_spi_irq_thread_fn,
+				   IRQF_SHARED, dev_name(dev), ctlr);
 	if (ret < 0 && ret != -ENOTCONN) {
 		dev_err(dev, "can not request IRQ\n");
 		goto err_free_ctlr;
