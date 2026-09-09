6a27f4d37c829b7527e0bd2348416c0d6e881705
Dinh Nguyen <dinguyen@kernel.org>
2026-09-09 08:12:52 -0500

    EDAC/altera: Fix code leak on dci allocation failure
    sashiko reports: If devres_open_group() fails, the function returns -ENOMEM
    without freeing the dci structure allocated earlier with edac_device_alloc_ctl_info().
    
    Free the dci structure if devres_open_group() fails.
    
    Fixes: c3eea1942a16 ("EDAC, altera: Add Altera L2 cache and OCRAM support")
    Cc: stable@vger.kernel.org
    Signed-off-by: Dinh Nguyen <dinguyen@kernel.org>

--- drivers/edac/altera_edac.c
+++ drivers/edac/altera_edac.c
@@ -1517,7 +1517,9 @@ static int altr_portb_setup(struct altr_edac_device_dev *device)
 	*altdev = *device;
 
 	if (!devres_open_group(device->edac->dev, altr_portb_setup, GFP_KERNEL))
+		edac_device_free_ctl_info(dci);
 		return -ENOMEM;
+	}
 
 	/* Update PortB specific values */
 	altdev->edac_dev_name = ecc_name;
