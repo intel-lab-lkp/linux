13713a277eff231d1113f6747ff02c279535da59
Jisheng Zhang <jszhang@kernel.org>
2026-09-09 22:36:51 +0800

    spi: dw: restore previous irq handling behavior when !ctlr->cur_msg
    Recent "enhanced" support changes the dw_spi_irq() behavior a bit
    when !!ctlr->cur_msg:
    
        if (!ctlr->cur_msg && dws->transfer_handler == dw_spi_transfer_handler) {
                dw_spi_mask_intr(dws, 0xff);
                return IRQ_HANDLED;
        }
    
    But it misses the dma case, where the transfer_handler ==
    dw_spi_dma_transfer_handler, so this changes the previous long time
    working behavior, let's restore the previous handling by only filtering
    out the dw_spi_enh_handler.
    
    Signed-off-by: Jisheng Zhang <jszhang@kernel.org>

--- drivers/spi/spi-dw-core.c
+++ drivers/spi/spi-dw-core.c
@@ -280,7 +280,7 @@ static irqreturn_t dw_spi_irq(int irq, void *dev_id)
 	if (!irq_status)
 		return IRQ_NONE;
 
-	if (!ctlr->cur_msg && dws->transfer_handler == dw_spi_transfer_handler) {
+	if (!ctlr->cur_msg && dws->transfer_handler != dw_spi_enh_handler) {
 		dw_spi_mask_intr(dws, 0xff);
 		return IRQ_HANDLED;
 	}
