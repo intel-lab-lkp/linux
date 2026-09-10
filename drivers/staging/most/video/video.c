076c173dc0460d850d4d121b6f0196342897d3e1
Muhammad Israr <7israr.work@gmail.com>
2026-09-10 17:44:03 +0500

    staging: most: video: add comments to mutex and spinlock definitions
    Add comments describing what the list_lock spinlock and lock mutex
    in struct most_video_dev protect, per checkpatch.pl's
    "definition without comment" check.
    
    list_lock protects the pending_mbos list. The mutex is registered
    as vdev->lock and is used by the V4L2 core to serialize
    video_device ioctl calls; it is not locked directly in this file.
    
    Signed-off-by: Muhammad Israr <7israr.work@gmail.com>

--- drivers/staging/most/video/video.c
+++ drivers/staging/most/video/video.c
@@ -33,6 +33,7 @@ struct most_video_dev {
 	bool mute;
 
 	struct list_head pending_mbos;
+	/* protects pending_mbos */
 	spinlock_t list_lock;
 
 	struct v4l2_device v4l2_dev;
@@ -40,6 +41,7 @@ struct most_video_dev {
 	struct video_device *vdev;
 	unsigned int ctrl_input;
 
+	/* registered as vdev->lock; serializes video_device ioctls */
 	struct mutex lock;
 
 	wait_queue_head_t wait_data;
