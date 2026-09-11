a34e5dc35c773db243ddb58b436869bfc966e5b3
Ben Horgan <ben.horgan@arm.com>
2026-09-11 17:06:08 +0100

    fs/resctrl: Allocate rmid_ptrs[] using kvzalloc_objs()
    On an MPAM system the number of entries in rmid_ptrs[] is the number of PARTIDs
    multiplied by the number of PMGs. The maximum possible number of PARTIDs is
    0x10000 and the maximum number of PMGs is 0x100. On a system pushing the limits
    of the MPAM specification this can be large enough to consistently fail the
    kzalloc_objs() allocation and hence fail to mount the resctrl filesystem.
    
    Switch to allocating rmid_ptrs[] using kvzalloc_objs() so that large allocations
    fall back to vmalloc().
    
    Fixes: 264c285999fc ("arm_mpam: resctrl: Add monitor initialisation and domain boilerplate")
    Signed-off-by: Ben Horgan <ben.horgan@arm.com>
    Reviewed-by: Tony Luck <tony.luck@intel.com>

--- fs/resctrl/monitor.c
+++ fs/resctrl/monitor.c
@@ -979,7 +979,7 @@ int setup_rmid_lru_list(void)
 		return 0;
 
 	idx_limit = resctrl_arch_system_num_rmid_idx();
-	rmid_ptrs = kzalloc_objs(struct rmid_entry, idx_limit);
+	rmid_ptrs = kvzalloc_objs(struct rmid_entry, idx_limit);
 	if (!rmid_ptrs)
 		return -ENOMEM;
 
@@ -1010,7 +1010,7 @@ void free_rmid_lru_list(void)
 		return;
 
 	mutex_lock(&rdtgroup_mutex);
-	kfree(rmid_ptrs);
+	kvfree(rmid_ptrs);
 	rmid_ptrs = NULL;
 	mutex_unlock(&rdtgroup_mutex);
 }
