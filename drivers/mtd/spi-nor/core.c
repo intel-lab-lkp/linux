d53335d9ec32c37c79057d1fd52f66ba757630f7
Miquel Raynal <miquel.raynal@bootlin.com>
2026-09-11 16:04:23 +0200

    mtd: spi-nor: Fix quad-enable for flashes with QER bit in SR1
    Some flashes (eg. from Macronix) do set BFPT_DWORD15_QER_SR1_BIT6, which
    means they do not have an SR2 to read from/write to. The new generic QE
    helper was supposed to accommodate this situation but in the last version
    that got merged, parts of that specific handling has been moved to a
    more contained location, swp.c (which needed most of the extra code),
    yet the Macronix case has been forgotten about in that generic QE
    handling helper. Booting with such flashes will always fail probing.
    
    Fix the situation by making sure SR2 reads just return 0 if
    unsupported. This is safe since there is no chip with a write SR2 path
    but no read SR2 path (which is now enforced in the SFDP parsing step).
    
    This way, callers still do not have to care about the internal device
    capabilities. Calling sr1_and_sr2 read/write helpers is safe in both
    directions (not risk to get a spurious error). The behavior for SR1-only
    chips is respected, the complexity in the core kept to its minimum.
    
    Reported-by: Jon Hunter <jonathanh@nvidia.com>
    Closes: https://lore.kernel.org/linux-mtd/178876719232.3543902.14451625037676421254.b4-ty@b4/T/#m5bc4ba6776436f2870ced0eb5789d229037ad840
    Fixes: 63489002d397 ("mtd: spi-nor: Refactor Read Status/Write Status support")
    Signed-off-by: Miquel Raynal <miquel.raynal@bootlin.com>

--- drivers/mtd/spi-nor/core.c
+++ drivers/mtd/spi-nor/core.c
@@ -849,7 +849,8 @@ int spi_nor_read_sr2(struct spi_nor *nor, u8 *sr2)
 
 /**
  * spi_nor_read_sr1_and_sr2() - Read SR1 then SR2
- * General purpose helper.
+ * General purpose helper, always safe to call. Will expectedly ignore
+ * SR2 on certain chips.
  *
  * @nor: the spi_nor structure
  * @sr: pointer to a valid 2-byte array
@@ -864,7 +865,12 @@ int spi_nor_read_sr1_and_sr2(struct spi_nor *nor, u8 *sr)
 	if (ret)
 		return ret;
 
-	return spi_nor_read_sr2(nor, &sr[1]);
+	if (nor->params->opcodes.read_sr2)
+		ret = spi_nor_read_sr2(nor, &sr[1]);
+	else
+		sr[1] = 0;
+
+	return ret;
 }
 
 /**
