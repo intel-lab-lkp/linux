42fdf52a8cd8a5a228af1cc9cac00ecbcc68c68d
Karl Mehltretter <kmehltretter@gmail.com>
2026-09-12 07:58:50 +0200

    media: v4l2-subdev: Fix media_entity_operations references in kernel-doc
    The kernel-doc comment of v4l2_subdev_link_validate() refers to
    media_entity_ops, which does not exist. The name is
    media_entity_operations. Say media_entity_operations.
    
    Fixes: 5fd3e2412ade ("media: v4l2-subdev: Support hybrid links in v4l2_subdev_link_validate()")
    Assisted-by: LLM
    Signed-off-by: Karl Mehltretter <kmehltretter@gmail.com>

--- include/media/v4l2-subdev.h
+++ include/media/v4l2-subdev.h
@@ -1236,10 +1236,10 @@ int v4l2_subdev_link_validate_default(struct v4l2_subdev *sd,
  * width, height and the media bus pixel code are equal on both
  * source and sink of the link.
  *
- * The function can be used as a drop-in &media_entity_ops.link_validate
+ * The function can be used as a drop-in &media_entity_operations.link_validate
  * implementation for v4l2_subdev instances. It supports all links between
  * subdevs, as well as links between subdevs and video devices, provided that
- * the video devices also implement their &media_entity_ops.link_validate
+ * the video devices also implement their &media_entity_operations.link_validate
  * operation.
  */
 int v4l2_subdev_link_validate(struct media_link *link);
