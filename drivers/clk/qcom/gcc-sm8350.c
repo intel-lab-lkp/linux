8d722d99cc560d3372bc315d13c115594396494a
Abel Vesa <abel.vesa@oss.qualcomm.com>
2026-09-14 15:47:30 +0300

    clk: qcom: gcc-sm8350: Tie the CX power domain to controller
    Enable runtime PM for the controller, allowing CX to be powered down
    once all GCC consumers have released their clocks and GDSCs.
    
    Reviewed-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Signed-off-by: Abel Vesa <abel.vesa@oss.qualcomm.com>

--- drivers/clk/qcom/gcc-sm8350.c
+++ drivers/clk/qcom/gcc-sm8350.c
@@ -3787,6 +3787,7 @@ static const struct qcom_cc_desc gcc_sm8350_desc = {
 	.num_resets = ARRAY_SIZE(gcc_sm8350_resets),
 	.gdscs = gcc_sm8350_gdscs,
 	.num_gdscs = ARRAY_SIZE(gcc_sm8350_gdscs),
+	.use_rpm = true,
 };
 
 static const struct of_device_id gcc_sm8350_match_table[] = {
