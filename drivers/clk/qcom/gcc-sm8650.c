f6026e2c55120b10168374d6dcb129d534458076
Abel Vesa <abel.vesa@oss.qualcomm.com>
2026-09-14 15:47:33 +0300

    clk: qcom: gcc-sm8650: Tie the CX power domain to controller
    Enable runtime PM for the controller, allowing CX to be powered down
    once all GCC consumers have released their clocks and GDSCs.
    
    Reviewed-by: Konrad Dybcio <konrad.dybcio@oss.qualcomm.com>
    Signed-off-by: Abel Vesa <abel.vesa@oss.qualcomm.com>

--- drivers/clk/qcom/gcc-sm8650.c
+++ drivers/clk/qcom/gcc-sm8650.c
@@ -3785,6 +3785,7 @@ static const struct qcom_cc_desc gcc_sm8650_desc = {
 	.num_resets = ARRAY_SIZE(gcc_sm8650_resets),
 	.gdscs = gcc_sm8650_gdscs,
 	.num_gdscs = ARRAY_SIZE(gcc_sm8650_gdscs),
+	.use_rpm = true,
 };
 
 static const struct of_device_id gcc_sm8650_match_table[] = {
