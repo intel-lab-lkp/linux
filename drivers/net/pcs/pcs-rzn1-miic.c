a6a643e781dd83f72ae8dc80ae1fbc1e16865c5f
Kyle Hendry <khendry@reliablecontrols.com>
2026-09-14 09:41:01 -0700

    net: pcs: rzn1-miic: Verify port number from dtb
    Add check to make sure port number is in range before writing
    to dt_val array
    
    Signed-off-by: Kyle Hendry <khendry@reliablecontrols.com>

--- drivers/net/pcs/pcs-rzn1-miic.c
+++ drivers/net/pcs/pcs-rzn1-miic.c
@@ -693,6 +693,14 @@ static int miic_parse_dt(struct miic *miic, u32 *mode_cfg)
 		if (of_property_read_u32(conv, "reg", &port))
 			continue;
 
+		if (port < miic->of_data->miic_port_start ||
+		    port > miic->of_data->miic_port_max) {
+			dev_err(miic->dev, "Port number out of range: %d\n", port);
+			of_node_put(conv);
+			kfree(dt_val);
+			return -EINVAL;
+		}
+
 		if (of_property_read_u32(conv, "renesas,miic-input", &conf))
 			continue;
 
