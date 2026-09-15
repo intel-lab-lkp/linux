a66731d90e781867486a6ca93850b793818ad34a
Arnd Bergmann <arnd@arndb.de>
2026-09-15 22:14:28 +0200

    drm/amd/display: fix unused function warnings
    When CONFIG_DRM_AMD_DC_DCE is disabled, most functions insde dce110_hwseq.c
    are no longer referenced, causing a lot of warnings such as
    
    drivers/gpu/drm/amd/amdgpu/../display/dc/hwss/dce110/dce110_hwseq.c:3510:13: error: 'dce110_enable_analog_link_output' defined but not used [-Werror=unused-function]
     3510 | static void dce110_enable_analog_link_output(
          |             ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    drivers/gpu/drm/amd/amdgpu/../display/dc/hwss/dce110/dce110_hwseq.c:3391:13: error: 'dce110_set_cursor_attribute' defined but not used [-Werror=unused-function]
     3391 | static void dce110_set_cursor_attribute(struct pipe_ctx *pipe_ctx)
    
    Replace the #ifdef with an IS_ENABLED() check that leads to an empty function
    in this configuration, so the compiler can eliminate the unused functions
    and not complain about it.
    
    Fixes: cd2cfe4d1504 ("drm/amd/display: gate DCE ASIC code behind CONFIG_DRM_AMD_DC_DCE")
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- drivers/gpu/drm/amd/display/dc/hwss/dce110/dce110_hwseq.c
+++ drivers/gpu/drm/amd/display/dc/hwss/dce110/dce110_hwseq.c
@@ -3618,7 +3618,6 @@ void dce110_disable_link_output(struct dc_link *link,
 	dc->link_srv->dp_trace_source_sequence(link, DPCD_SOURCE_SEQ_AFTER_DISABLE_LINK_PHY);
 }
 
-#if defined(CONFIG_DRM_AMD_DC_DCE)
 static const struct hw_sequencer_funcs dce110_funcs = {
 	.program_gamut_remap = program_gamut_remap,
 	.program_output_csc = program_output_csc,
@@ -3685,9 +3684,8 @@ static const struct hwseq_private_funcs dce110_private_funcs = {
 
 void dce110_hw_sequencer_construct(struct dc *dc)
 {
-	dc->hwss = dce110_funcs;
-	dc->hwseq->funcs = dce110_private_funcs;
+	if (IS_ENABLED(CONFIG_DRM_AMD_DC_DCE)) {
+		dc->hwss = dce110_funcs;
+		dc->hwseq->funcs = dce110_private_funcs;
+	}
 }
-
-#endif /* CONFIG_DRM_AMD_DC_DCE */
-
