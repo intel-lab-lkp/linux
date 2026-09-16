2ff4f35e2963e8bca3ef1ea2da766f8c9f845edf
Chen-Yu Tsai <wenst@chromium.org>
2026-09-16 11:33:23 +0800

    drm/fb-dma-helper: Add drm_fb_dma_get_gem_clipped_addr()
    drm_fb_dma_get_gem_addr() returns the DMA address to the "unclipped"
    framebuffer. However some display drivers want the "clipped" framebuffer
    instead, as they are also using the clipped coordinates to program the
    hardware.
    
    Some of these drivers are open-coding drm_fb_dma_get_gem_addr() with
    the source coordinates replaced, while others have been incorrectly
    converted to using drm_fb_dma_get_gem_addr(), which would end up
    causing incorrect parts of the framebuffer to be displayed if it were
    somehow clipped.
    
    Add drm_fb_dma_get_gem_clipped_addr(), a "clipped" version of
    drm_fb_dma_get_gem_addr() for these drivers to use.
    
    Cc: <stable@vger.kernel.org> # dependency for next patch
    Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>

--- drivers/gpu/drm/drm_fb_dma_helper.c
+++ drivers/gpu/drm/drm_fb_dma_helper.c
@@ -60,15 +60,16 @@ struct drm_gem_dma_object *drm_fb_dma_get_gem_obj(struct drm_framebuffer *fb,
 EXPORT_SYMBOL_GPL(drm_fb_dma_get_gem_obj);
 
 /**
- * drm_fb_dma_get_gem_addr() - Get DMA (bus) address for framebuffer, for pixel
- * formats where values are grouped in blocks this will get you the beginning of
- * the block
+ * drm_fb_dma_get_gem_addr() - Get DMA (bus) address for unclipped framebuffer,
+ * for pixel formats where values are grouped in blocks this will get you the
+ * beginning of the block
  * @fb: The framebuffer
  * @state: Which state of drm plane
  * @plane: Which plane
- * Return the DMA GEM address for given framebuffer.
  *
  * This function will usually be called from the PLANE callback functions.
+ *
+ * Return: GEM DMA address for given framebuffer, unclipped.
  */
 dma_addr_t drm_fb_dma_get_gem_addr(struct drm_framebuffer *fb,
 				   struct drm_plane_state *state,
@@ -85,6 +86,34 @@ dma_addr_t drm_fb_dma_get_gem_addr(struct drm_framebuffer *fb,
 								state->src_y >> 16);
 }
 EXPORT_SYMBOL_GPL(drm_fb_dma_get_gem_addr);
+
+/**
+ * drm_fb_dma_get_gem_clipped_addr() - Get DMA (bus) address for clipped
+ * framebuffer, for pixel formats where values are grouped in blocks this
+ * will get you the beginning of the block
+ * @fb: The framebuffer
+ * @state: Which state of drm plane
+ * @plane: Which plane
+ *
+ * This function will usually be called from the PLANE callback functions.
+ *
+ * Return: GEM DMA address for given framebuffer, clipped.
+ */
+dma_addr_t drm_fb_dma_get_gem_clipped_addr(struct drm_framebuffer *fb,
+					   struct drm_plane_state *state,
+					   unsigned int plane)
+{
+	struct drm_gem_dma_object *obj;
+
+	obj = drm_fb_dma_get_gem_obj(fb, plane);
+	if (!obj)
+		return 0;
+
+	return obj->dma_addr + drm_framebuffer_get_block_offset(fb, plane,
+								state->src.x1 >> 16,
+								state->src.y1 >> 16);
+}
+EXPORT_SYMBOL_GPL(drm_fb_dma_get_gem_clipped_addr);
 
 /**
  * drm_fb_dma_sync_non_coherent - Sync GEM object to non-coherent backing
