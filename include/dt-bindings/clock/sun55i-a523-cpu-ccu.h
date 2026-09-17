456307ab0d30d486a0fd9d6837eef32999f72915
Andre Przywara <andre.przywara@arm.com>
2026-09-18 00:39:11 +0200

    dt-bindings: clock: sun55i-a523-ccu: Add A523 CPU CCU clock controller
    There are four clock controllers in the A523 SoC, but only three are
    described in the DT binding so far.
    
    Add a description for the CPU CCU, which provides separate clocks for
    the two CPU clusters and the DSU interconnect.
    
    Signed-off-by: Andre Przywara <andre.przywara@arm.com>

---
+++ include/dt-bindings/clock/sun55i-a523-cpu-ccu.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: (GPL-2.0-only OR MIT) */
+/*
+ * Copyright 2025 Arm Ltd.
+ */
+
+#ifndef _DT_BINDINGS_CLK_SUN55I_A523_CPU_CCU_H_
+#define _DT_BINDINGS_CLK_SUN55I_A523_CPU_CCU_H_
+
+#define CLK_CPU_L		7
+#define CLK_CPU_DSU		8
+#define CLK_CPU_B		9
+
+#endif /* _DT_BINDINGS_CLK_SUN55I_A523_CPU_CCU_H_ */
