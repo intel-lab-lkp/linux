3072fa476de7cdbd5051372f315e47aed9c1b838
Guangshuo Li <lgs201920130244@gmail.com>
2026-09-18 13:13:40 +0800

    scsi: a100u2w: disable PCI device on remove
    inia100_probe_one() enables the PCI device with pci_enable_device().
    The probe failure paths disable the device before returning, but the
    normal remove path does not perform the corresponding cleanup.
    
    As a result, the PCI device enable state remains unbalanced after the
    driver is removed.
    
    Call pci_disable_device() from inia100_remove_one() to match the probe
    error path.
    
    This issue was found by manual code inspection.
    
    Fixes: 1da177e4c3f4 ("Linux-2.6.12-rc2")
    Cc: stable@vger.kernel.org
    Signed-off-by: Guangshuo Li <lgs201920130244@gmail.com>

--- drivers/scsi/a100u2w.c
+++ drivers/scsi/a100u2w.c
@@ -1204,6 +1204,7 @@ static void inia100_remove_one(struct pci_dev *pdev)
         release_region(shost->io_port, 256);
 
 	scsi_host_put(shost);
+	pci_disable_device(pdev);
 } 
 
 static const struct pci_device_id inia100_pci_tbl[] = {
