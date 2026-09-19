d6eab275a1d870a29abfc3167edd42296886f400
Abdelnasser Hussein <abdelnasserhussein11@gmail.com>
2026-09-19 11:55:13 +0300

    staging: iio: adc: ad7816: Protect sysfs attributes with mutex
    The sysfs store functions (ad7816_store_mode and ad7816_store_channel)
    modify the device state and toggle GPIO pins. If accessed concurrently
    by userspace during an ongoing SPI transfer, this could corrupt the
    SPI transaction.
    
    Add the existing chip->lock guard to these functions to serialize
    access and prevent race conditions.
    
    Suggested-by: Joshua Crofts <joshua.crofts1@gmail.com>
    Signed-off-by: Abdelnasser Hussein <abdelnasserhussein11@gmail.com>

--- drivers/staging/iio/adc/ad7816.c
+++ drivers/staging/iio/adc/ad7816.c
@@ -136,6 +136,7 @@ static ssize_t ad7816_store_mode(struct device *dev,
 	struct iio_dev *indio_dev = dev_to_iio_dev(dev);
 	struct ad7816_chip_info *chip = iio_priv(indio_dev);
 
+	guard(mutex)(&chip->lock);
 	if (strcmp(buf, "full") == 0) {
 		gpiod_set_value(chip->rdwr_pin, 1);
 		chip->mode = AD7816_FULL;
@@ -200,6 +201,7 @@ static ssize_t ad7816_store_channel(struct device *dev,
 		return -EINVAL;
 	}
 
+	guard(mutex)(&chip->lock);
 	chip->channel_id = data;
 
 	return len;
