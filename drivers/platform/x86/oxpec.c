86cc382a4239d7a92006ba40be0f34bdb66df371
Antheas Kapenekakis <lkml@antheas.dev>
2026-09-20 21:04:38 +0200

    platform/x86: oxpec: Add OneXPlayer 3 quirk
    The OneXPlayer 3 is a new Intel device by OneXPlayer. It uses the same
    registers as the G1 Intel edition. So, add a quirk for it. Battery
    bypass, capacity limits, fan curves, fan tachometer, and turbo button
    override were confirmed to work.
    
    Signed-off-by: Antheas Kapenekakis <lkml@antheas.dev>

--- drivers/platform/x86/oxpec.c
+++ drivers/platform/x86/oxpec.c
@@ -226,6 +226,13 @@ static const struct dmi_system_id dmi_table[] = {
 	{
 		.matches = {
 			DMI_MATCH(DMI_BOARD_VENDOR, "ONE-NETBOOK"),
+			DMI_EXACT_MATCH(DMI_BOARD_NAME, "ONEXPLAYER 3"),
+		},
+		.driver_data = (void *)oxp_g1_i,
+	},
+	{
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "ONE-NETBOOK"),
 			DMI_EXACT_MATCH(DMI_BOARD_NAME, "ONEXPLAYER mini A07"),
 		},
 		.driver_data = (void *)oxp_mini_amd_a07,
