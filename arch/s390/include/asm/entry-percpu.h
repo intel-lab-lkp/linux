8625fe9eb3a418580b0d28d6c2c750d688559e9b
Heiko Carstens <hca@linux.ibm.com>
2026-09-21 17:58:05 +0200

    s390/percpu: Rework to simplify percpu_entry() and percpu_exit()
    The percpu code section functionality uses a rather complex method to
    figure out if the register, which contains the address of the current
    cpu's percpu variable, needs to be adjusted.
    
    If an interrupt happens within a percpu code section (indicated by a
    lowcore field), the instruction at the interrupted location is
    checked. If it is not a specific AG instruction, the register needs to
    be updated. This mechanism needs to take kprobes into account, and
    enforces a specific instruction ordering.
    
    Mark Rutland provided a different solution for arm64 [1] which comes
    without such limitations, but requires to use one more instruction, and
    two more registers. Given that this simplifies percpu_entry() and
    percpu_exit() it seems to be worth to go that route.
    
    Change s390 to implement a similar approach. This requires to encode
    three register numbers into the "percpu_register" field, which is used
    to indicate if a percpu code section is executed.
    
    The used mviy instruction can write only one byte, which allows to
    encode only two register numbers. Use a register pair for the inline
    assemblies, and only encode the even register number of the register
    pair to work around this.
    
    The generated code changes like this for e.g. a simple this_cpu_inc():
    
    Old:
    
    c0 20 00 00 00 00  larl    %r2,c6 <foo+0x6> <-- load address of percpu var
    b9 04 00 32        lgr     %r3,%r2          <-- pointless copy of address
    eb 03 03 c0 00 52  mviy    960,3            <-- start of percpu code section
                                                    - gpr 3 contains percpu var address
    e3 30 03 b8 00 08  ag      %r3,952          <-- add percpu offset
    eb 01 30 00 00 7a  agsi    0(%r3),1         <-- atomic inc
    eb 00 03 c0 00 52  mviy    960,0            <-- end of percpu code section
    
    New:
    
    c0 10 00 00 00 00  larl    %r1,c6 <foo+0x6> <-- load address of percpu var
    eb 21 03 c0 00 52  mviy    960,33           <-- start of percpu code section
                                                    33 == 0x21:
    						- gpr 1 contains percpu var address
    						- gpr 2 used for percpu offset
      					        - gpr 2+1 == 3 used for current cpu's percpu var address
    e3 20 03 b8 00 04  lg      %r2,952          <-- load percpu offset
    41 32 10 00        la      %r3,0(%r2,%r1)   <-- generate current cpu's percpu var address
    eb 01 30 00 00 7a  agsi    0(%r3),1         <-- atomic inc
    eb 00 03 c0 00 52  mviy    960,0            <-- end of percpu code section
    
    In the above "new" code example 33 (0x21) is used as indicator value to
    mark that a percpu code section is executed. This value implies that
    registers 2 and 3 will (only) hold the percpu offset and the current
    cpu's percpu var address. Those registers will be updated by
    percpu_exit() if the process was migrated to a different cpu to contain
    the percpu offset and percpu var address of the new cpu.
    
    Note that because of the pointless lgr instruction in the "old" code
    example the two code sequences have identical size, and it looks like
    only one more register is used. However this is because of suboptimal
    gcc code generation.
    
    [1] https://lore.kernel.org/all/20260908151741.394589-1-mark.rutland@arm.com/
    
    Signed-off-by: Heiko Carstens <hca@linux.ibm.com>

--- arch/s390/include/asm/entry-percpu.h
+++ arch/s390/include/asm/entry-percpu.h
@@ -2,79 +2,42 @@
 #ifndef ARCH_S390_ENTRY_PERCPU_H
 #define ARCH_S390_ENTRY_PERCPU_H
 
-#include <linux/kprobes.h>
+#include <linux/bitfield.h>
 #include <linux/percpu.h>
 #include <asm/lowcore.h>
 #include <asm/ptrace.h>
-#include <asm/asm-offsets.h>
 
 static __always_inline void percpu_entry(struct pt_regs *regs)
 {
 	struct lowcore *lc = get_lowcore();
 
-	if (user_mode(regs))
-		return;
 	regs->cpu = lc->cpu_nr;
 	regs->percpu_register = lc->percpu_register;
 	lc->percpu_register = 0;
 }
 
-static __always_inline bool percpu_code_check(struct pt_regs *regs)
+static __always_inline void percpu_exit(struct pt_regs *regs)
 {
-	unsigned int insn, disp;
-	struct kprobe *p;
-
-	if (likely(user_mode(regs) || !regs->percpu_register))
-		return false;
-	/*
-	 * Within a percpu code section - check if the percpu base register
-	 * needs to be updated. This is the case if the PSW does not point to
-	 * the ADD instruction within the section.
-	 * - AG %rx,percpu_offset_in_lowcore(%r0,%r0)
-	 * which adds the percpu offset to the percpu base register.
-	 */
-	lockdep_assert_preemption_disabled();
-again:
-	insn = READ_ONCE(*(u16 *)psw_bits(regs->psw).ia);
-	if (unlikely(insn == BREAKPOINT_INSTRUCTION)) {
-		p = get_kprobe((void *)psw_bits(regs->psw).ia);
-		/*
-		 * If the kprobe is concurrently removed on a different CPU
-		 * it might not be found anymore. However text must have
-		 * been restored - try again.
-		 */
-		if (!p)
-			goto again;
-		insn = p->opcode;
-	}
-	if ((insn & 0xff0f) != 0xe300)
-		return true;
-	disp = LC_PERCPU_OFFSET;
-	if (machine_has_relocated_lowcore())
-		disp += LOWCORE_ALT_ADDRESS;
-	insn = (disp & 0xff000) >> 4 | (disp & 0x00fff) << 16 | 0x8;
-	if (*(u32 *)(psw_bits(regs->psw).ia + 2) != insn)
-		return true;
-	return false;
-}
-
-static __always_inline void percpu_exit(struct pt_regs *regs, bool needs_fixup)
-{
+	unsigned char regval, regpcp, regoff, regptr;
 	struct lowcore *lc = get_lowcore();
-	unsigned char reg;
 
-	if (user_mode(regs))
+	if (!regs->percpu_register)
 		return;
-	reg = regs->percpu_register;
-	lc->percpu_register = reg;
-	if (likely(!needs_fixup))
-		return;
-	/* Check if process has been migrated to a different CPU. */
+	regval = regs->percpu_register;
+	lc->percpu_register = regval;
+	/* Migrated to a different CPU? */
 	if (regs->cpu == lc->cpu_nr)
 		return;
-	/* Fixup percpu base register */
-	regs->gprs[reg] -= __per_cpu_offset[regs->cpu];
-	regs->gprs[reg] += lc->percpu_offset;
+	regpcp = FIELD_GET(PCPU_REG_PCP, regval);
+	regoff = FIELD_GET(PCPU_REG_OFF, regval);
+	regptr = regoff + 1;
+	/*
+	 * Update register 'regoff' with the current CPU's percpu offset,
+	 * and recalculate and update current CPU's percpu variable address
+	 * contained in register 'regptr'.
+	 */
+	regs->gprs[regoff] = lc->percpu_offset;
+	regs->gprs[regptr] = regs->gprs[regpcp] + regs->gprs[regoff];
 }
 
 #endif
