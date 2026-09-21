030d24533c0ba1d5e2db9ab74e77016ffd9df739
Ajay Kumar Nandam <ajay.nandam@oss.qualcomm.com>
2026-09-22 00:38:09 +0530

    ASoC: qcom: q6apm: clear g_apm on driver removal
    The global g_apm pointer is set during apm_probe() but never cleared
    in apm_remove(). After the driver is removed the devm-managed struct
    q6apm is freed, leaving g_apm dangling. A subsequent call to
    q6apm_is_adsp_ready() dereferences the freed pointer.
    
    Clear g_apm in apm_remove() before the component is unregistered so
    that q6apm_is_adsp_ready() returns false instead of triggering a
    use-after-free.
    
    Fixes: 5477518b8a0e ("ASoC: qdsp6: audioreach: add q6apm support")
    Signed-off-by: Ajay Kumar Nandam <ajay.nandam@oss.qualcomm.com>

--- sound/soc/qcom/qdsp6/q6apm.c
+++ sound/soc/qcom/qdsp6/q6apm.c
@@ -894,6 +894,7 @@ static int apm_probe(gpr_device_t *gdev)
 
 static void apm_remove(gpr_device_t *gdev)
 {
+	g_apm = NULL;
 	of_platform_depopulate(&gdev->dev);
 	snd_soc_unregister_component(&gdev->dev);
 }
