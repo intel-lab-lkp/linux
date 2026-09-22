9659bdd0486bab092f117797485c1d4ab3b8be34
Bibo Mao <maobibo@loongson.cn>
2026-09-22 20:10:57 +0800

    ACPI: scan: Add support for LoongArch in acpi_iommu_configure_id()
    acpi_iommu_configure_id() currently supports only IORT and VIOT. Add
    support for IOVT as well.
    
    Signed-off-by: Bibo Mao <maobibo@loongson.cn>
    Acked-by: Rafael J. Wysocki (Intel) <rafael@kernel.org>

--- drivers/acpi/scan.c
+++ drivers/acpi/scan.c
@@ -13,6 +13,7 @@
 #include <linux/kernel.h>
 #include <linux/acpi.h>
 #include <linux/acpi_iort.h>
+#include <linux/acpi_iovt.h>
 #include <linux/acpi_rimt.h>
 #include <linux/acpi_viot.h>
 #include <linux/iommu.h>
@@ -1626,6 +1627,8 @@ static int acpi_iommu_configure_id(struct device *dev, const u32 *id_in)
 	err = iort_iommu_configure_id(dev, id_in);
 	if (err && err != -EPROBE_DEFER)
 		err = rimt_iommu_configure_id(dev, id_in);
+	if (err && err != -EPROBE_DEFER)
+		err = iovt_iommu_configure_id(dev, id_in);
 	if (err && err != -EPROBE_DEFER)
 		err = viot_iommu_configure(dev);
 
